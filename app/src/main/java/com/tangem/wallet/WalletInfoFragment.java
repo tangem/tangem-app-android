4f4509074489a013ee26f960f3b7c89382fb60b1
Tangem <git@tangem.com>
2018-05-28 14:37:16 +0300

    Updated on 2026-09-25

--- app/src/main/java/com/tangem/wallet/WalletInfoFragment.java
+++ app/src/main/java/com/tangem/wallet/WalletInfoFragment.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
