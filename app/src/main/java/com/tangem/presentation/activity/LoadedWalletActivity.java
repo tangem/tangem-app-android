5d6b8b34f78b2db98c8024721ff390d35d3318fb
Tangem <git@tangem.com>
2018-05-29 13:28:36 +0300

    Updated on 2025-12-05

---
+++ app/src/main/java/com/tangem/presentation/activity/LoadedWalletActivity.java
@@ -0,0 +1,40 @@
+package com.tangem.presentation.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tangem.wallet.LoadedWalletActivityFragment;
+import com.tangem.wallet.R;
+
+
+public class LoadedWalletActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loaded_wallet);
+        MainActivity.commonInit(getApplicationContext());
+
+        if( getIntent().getExtras().containsKey(NfcAdapter.EXTRA_TAG) )
+        {
+            Tag tag=getIntent().getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            if (tag != null ) {
+                LoadedWalletActivityFragment fragment=(LoadedWalletActivityFragment)(getSupportFragmentManager().findFragmentById(R.id.loaded_wallet_fragment));
+                fragment.onTagDiscovered(tag);
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        LoadedWalletActivityFragment loadedWalletActivityFragment=(LoadedWalletActivityFragment) getSupportFragmentManager().findFragmentById(R.id.loaded_wallet_fragment);
+        Intent data= loadedWalletActivityFragment.prepareResultIntent();
+        data.putExtra("modification", "update");
+        setResult(Activity.RESULT_OK, data);
+        finish();
+    }
+}
