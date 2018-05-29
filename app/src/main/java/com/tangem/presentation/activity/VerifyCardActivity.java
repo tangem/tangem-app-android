7ba9b4cf7af0195b1ae736aa231fa3b23cd0403f
Tangem <git@tangem.com>
2018-05-29 13:28:36 +0300

    Updated on 2025-11-18

---
+++ app/src/main/java/com/tangem/presentation/activity/VerifyCardActivity.java
@@ -0,0 +1,30 @@
+package com.tangem.presentation.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tangem.wallet.R;
+import com.tangem.wallet.VerifyCardActivityFragment;
+
+public class VerifyCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_card);
+
+        MainActivity.commonInit(getApplicationContext());
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        VerifyCardActivityFragment verifyCardActivityFragment = (VerifyCardActivityFragment) getSupportFragmentManager().findFragmentById(R.id.verify_card_fragment);
+        Intent data = verifyCardActivityFragment.prepareResultIntent();
+        data.putExtra("modification", "update");
+        setResult(Activity.RESULT_OK, data);
+        finish();
+    }
+}
