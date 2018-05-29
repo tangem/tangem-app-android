e5c448e935ac56705296615369734a59278122ec
Tangem <git@tangem.com>
2018-05-29 21:45:37 +0300

    Updated on 2026-01-20

--- app/src/main/java/com/tangem/presentation/activity/LogoActivity.java
+++ app/src/main/java/com/tangem/presentation/activity/LogoActivity.java
@@ -3,7 +3,6 @@ package com.tangem.presentation.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -23,39 +22,33 @@ public class LogoActivity extends AppCompatActivity {
         }
     };
 
-    ImageView imgLogo;
+    private ImageView imgLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_logo);
 
-        imgLogo= (ImageView) findViewById(R.id.imgLogo);
-        // Set up the user interaction to manually show or hide the system UI.
-        imgLogo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {hide();
-            }
-        });
+        imgLogo = findViewById(R.id.imgLogo);
+
+        // set up the user interaction to manually show or hide the system UI.
+        imgLogo.setOnClickListener(view -> hide());
     }
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
 
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        TextView AppVersion = (TextView) findViewById(R.id.AppVersion);
+        // trigger the initial hide() shortly after the activity has been created, to briefly hint to the user that UI controls are available.
+        TextView AppVersion = findViewById(R.id.AppVersion);
         AppVersion.setText("BETA v." + BuildConfig.VERSION_NAME);
-        if( !getIntent().getBooleanExtra("skipAutoHide",false)) {
+        if (!getIntent().getBooleanExtra("skipAutoHide", false)) {
             delayedHide(1000);
         }
     }
 
     private void hide() {
-        Intent intent=new Intent(getBaseContext(),MainActivity.class);
+        Intent intent = new Intent(getBaseContext(), MainActivity.class);
         startActivity(intent);
         finish();
     }
