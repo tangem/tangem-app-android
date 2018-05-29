8c17a8d4f072f2ba08117cd489979ade62c61ba7
Tangem <git@tangem.com>
2018-05-29 13:28:36 +0300

    Updated on 2026-08-14

---
+++ app/src/main/java/com/tangem/presentation/activity/SendTransactionActivity.java
@@ -0,0 +1,219 @@
+package com.tangem.presentation.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.tangem.wallet.Blockchain;
+import com.tangem.wallet.CoinEngine;
+import com.tangem.wallet.CoinEngineFactory;
+import com.tangem.wallet.Electrum_Request;
+import com.tangem.wallet.Electrum_Task;
+import com.tangem.wallet.Infura_Request;
+import com.tangem.wallet.Infura_Task;
+import com.tangem.wallet.LastSignStorage;
+import com.tangem.wallet.R;
+import com.tangem.wallet.SharedData;
+import com.tangem.wallet.Tangem_Card;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.math.BigInteger;
+import java.util.List;
+
+public class SendTransactionActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    private Tangem_Card mCard;
+    private String tx;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_transaction);
+
+        MainActivity.commonInit(getApplicationContext());
+
+        progressBar = findViewById(R.id.progressBar);
+
+        Intent intent = getIntent();
+        mCard = new Tangem_Card(getIntent().getStringExtra("UID"));
+        mCard.LoadFromBundle(intent.getExtras().getBundle("Card"));
+        tx = intent.getStringExtra("TX");
+
+        CoinEngine engine = CoinEngineFactory.Create(mCard.getBlockchain());
+        if (mCard.getBlockchain() == Blockchain.Ethereum || mCard.getBlockchain() == Blockchain.EthereumTestNet || mCard.getBlockchain() == Blockchain.Token) {
+            ETHRequestTask task = new ETHRequestTask(mCard.getBlockchain());
+            Infura_Request req = Infura_Request.SendTransaction(mCard.getWallet(), tx);
+            req.setID(67);
+            req.setBlockchain(mCard.getBlockchain());
+            task.execute(req);
+        } else if (mCard.getBlockchain() == Blockchain.Bitcoin || mCard.getBlockchain() == Blockchain.BitcoinTestNet ) {
+            String nodeAddress = engine.GetNode(mCard);
+            int nodePort = engine.GetNodePort(mCard);
+            ConnectTask connectTask = new ConnectTask(nodeAddress, nodePort);
+            connectTask.execute(Electrum_Request.Broadcast(mCard.getWallet(), tx));
+        }
+        else if (mCard.getBlockchain() == Blockchain.BitcoinCash || mCard.getBlockchain() == Blockchain.BitcoinCashTestNet ) {
+            String nodeAddress = engine.GetNode(mCard);
+            int nodePort = engine.GetNodePort(mCard);
+            ConnectTask connectTask = new ConnectTask(nodeAddress, nodePort);
+            connectTask.execute(Electrum_Request.Broadcast(mCard.getWallet(), tx));
+        }
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keycode, KeyEvent e) {
+        switch (keycode) {
+            case KeyEvent.KEYCODE_BACK:
+                Toast.makeText(getBaseContext(),"Please wait while the payment is sent...",Toast.LENGTH_LONG).show();
+                return true;
+        }
+
+        return super.onKeyDown(keycode, e);
+    }
+
+    void FinishWithError(String Message) {
+        Intent intent = new Intent();
+        intent.putExtra("message", "Failed to send transaction. Try again.");
+        setResult(MainActivity.RESULT_CANCELED, intent);
+        finish();
+    }
+
+    void FinishWithSuccess() {
+        Intent intent = new Intent();
+        intent.putExtra("message", "Transaction has been successfully signed and sent to blockchain node. Wallet balance will be updated in a while");
+        setResult(MainActivity.RESULT_OK, intent);
+        finish();
+    }
+
+    private class ETHRequestTask extends Infura_Task {
+        ETHRequestTask(Blockchain blockchain){
+            super(blockchain);
+        }
+        @Override
+        protected void onPostExecute(List<Infura_Request> requests) {
+            super.onPostExecute(requests);
+            for (Infura_Request request : requests) {
+                try {
+                    if (request.error == null) {
+                        if (request.isMethod(Infura_Request.METHOD_ETH_SendRawTransaction)) {
+                            try {
+                                String hashTX = "";
+                                try {
+                                    String tmp = request.getResultString();
+                                    hashTX = tmp;
+                                }catch(JSONException e)
+                                {
+                                    JSONObject msg = request.getAnswer();
+                                    JSONObject err = msg.getJSONObject("error");
+                                    hashTX = err.getString("message");
+                                    LastSignStorage.setLastMessage(mCard.getWallet(), hashTX);
+                                    FinishWithError(hashTX);
+                                    return;
+                                }
+
+                                try {
+                                    if (hashTX.startsWith("0x") || hashTX.startsWith("0X")) {
+                                        hashTX = hashTX.substring(2);
+                                    }
+                                    BigInteger bigInt = new BigInteger(hashTX, 16); //TODO: очень плохой способ
+                                    LastSignStorage.setTxWasSend(mCard.getWallet());
+                                    LastSignStorage.setLastMessage(mCard.getWallet(), "");
+                                    BigInteger nonce = mCard.GetConfirmTXCount();
+                                    nonce.add(BigInteger.valueOf(1));
+                                    mCard.SetConfirmTXCount(nonce);
+                                    Log.e("TX_RESULT", hashTX);
+                                    FinishWithSuccess();
+                                }catch(Exception e)
+                                {
+                                    FinishWithError(hashTX);
+                                }
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                                FinishWithError(e.toString());
+                            }
+                        }
+                    } else if (request.error != null) {
+                        FinishWithError(request.error);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    FinishWithError(e.toString());
+                }
+            }
+        }
+    }
+
+    private class ConnectTask extends Electrum_Task {
+        public ConnectTask(String host, int port) {
+            super(host, port);
+        }
+
+        public ConnectTask(String host, int port, SharedData sharedData) {
+            super(host, port, sharedData);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(List<Electrum_Request> requests) {
+            super.onPostExecute(requests);
+            CoinEngine engine = CoinEngineFactory.Create(Blockchain.Bitcoin);
+
+            for (Electrum_Request request : requests) {
+                try {
+                    if (request.error == null) {
+                        if (request.isMethod(Electrum_Request.METHOD_SendTransaction)) {
+                            try {
+                                String hashTX = request.getResultString();
+
+                                try
+                                {
+                                    LastSignStorage.setLastMessage(mCard.getWallet(), hashTX);
+                                    if (hashTX.startsWith("0x") || hashTX.startsWith("0X")) {
+                                        hashTX = hashTX.substring(2);
+                                    }
+                                    BigInteger bigInt = new BigInteger(hashTX, 16); //TODO: очень плохой способ
+                                    LastSignStorage.setTxWasSend(mCard.getWallet());
+                                    LastSignStorage.setLastMessage(mCard.getWallet(), "");
+                                    Log.e("TX_RESULT", hashTX);
+                                    FinishWithSuccess();
+                                }catch(Exception e)
+                                {
+                                    engine.SwitchNode(null);
+                                    FinishWithError(hashTX);
+                                    return;
+                                }
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                                engine.SwitchNode(null);
+                                FinishWithError(e.toString());
+                            }
+                        }
+                    } else if (request.error != null) {
+                        engine.SwitchNode(null);
+                        FinishWithError(request.error);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    engine.SwitchNode(null);
+                    FinishWithError(e.toString());
+                }
+            }
+
+        }
+    }
+
+}
