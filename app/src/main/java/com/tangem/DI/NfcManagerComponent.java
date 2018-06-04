8e77bb3c6be6ea0b9d591f9662ebd7cce133bc49
Tangem <git@tangem.com>
2018-06-04 17:33:44 +0300

    Updated on 2025-12-22

---
+++ app/src/main/java/com/tangem/DI/NfcManagerComponent.java
@@ -0,0 +1,10 @@
+package com.tangem.DI;
+
+import com.tangem.domain.cardReader.NfcManager;
+
+//@Component
+public interface NfcManagerComponent {
+//    NfcManager getNfcManager(Activity activity, NfcAdapter.ReaderCallback readerCallback);
+    NfcManager getNfcManager();
+
+}
