6df6fefe2014838e7b6207c2c5a7171901db7fae
Tangem <git@tangem.com>
2018-06-22 14:38:12 +0300

    Updated on 2026-09-23

---
+++ app/src/main/java/com/tangem/domain/TestNode.kt
@@ -0,0 +1,9 @@
+package com.tangem.domain
+
+enum class TestNode(val host: String, val port: Int) {
+    arihanc_com("testnetnode.arihanc.com", 51001),
+    hsmiths_com("testnet.hsmiths.com", 53011),
+    qtornado_com("testnet.qtornado.com", 51001),
+    bauerj_eu("testnet1.bauerj.eu", 50001),
+
+}
