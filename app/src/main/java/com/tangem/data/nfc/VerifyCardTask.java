a8460067fb38741c7ccabb69afe107b98592e6de
Tangem <git@tangem.com>
2018-06-27 23:04:50 +0300

    Updated on 2026-04-06

--- app/src/main/java/com/tangem/data/nfc/VerifyCardTask.java
+++ app/src/main/java/com/tangem/data/nfc/VerifyCardTask.java
@@ -1,4 +1,4 @@
-package com.tangem.data.task;
+package com.tangem.data.nfc;
 
 import android.content.Context;
 import android.nfc.tech.IsoDep;
@@ -94,11 +94,11 @@ public class VerifyCardTask extends Thread {
 
     public void cancel(Boolean AllowInterrupt) {
         try {
-            if (this.isAlive()) {
+            if (isAlive()) {
                 isCancelled = true;
                 join(500);
             }
-            if (this.isAlive() && AllowInterrupt) {
+            if (isAlive() && AllowInterrupt) {
                 interrupt();
                 mNotifications.OnReadCancel();
             }
