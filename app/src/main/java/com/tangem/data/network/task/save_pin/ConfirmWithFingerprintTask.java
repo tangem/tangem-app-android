c048b19f7a8a39834714dbcaee33b48efed7b179
Tangem <git@tangem.com>
2018-07-10 17:31:35 +0300

    Updated on 2026-08-07

---
+++ app/src/main/java/com/tangem/data/network/task/save_pin/ConfirmWithFingerprintTask.java
@@ -0,0 +1,65 @@
+package com.tangem.data.network.task.save_pin;
+
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import com.tangem.domain.wallet.FingerprintHelper;
+import com.tangem.presentation.activity.SavePINActivity;
+import com.tangem.wallet.R;
+
+import java.lang.ref.WeakReference;
+
+import javax.crypto.Cipher;
+
+public class ConfirmWithFingerprintTask extends AsyncTask<Void, Void, Boolean> {
+    private WeakReference<SavePINActivity> reference;
+
+    public ConfirmWithFingerprintTask(SavePINActivity context) {
+        reference = new WeakReference<>(context);
+
+        reference.get().fingerprintHelper = new FingerprintHelper(reference.get());
+    }
+
+    @Override
+    protected Boolean doInBackground(Void... params) {
+        SavePINActivity savePINActivity = reference.get();
+
+        if (!savePINActivity.getKeyStore())
+            return false;
+
+        if (!savePINActivity.createNewKey(false))
+            return false;
+
+        if (!savePINActivity.getCipher())
+            return false;
+
+        return savePINActivity.initCipher(Cipher.ENCRYPT_MODE) && savePINActivity.initCryptObject();
+
+    }
+
+    @Override
+    protected void onPostExecute(final Boolean success) {
+        SavePINActivity savePINActivity = reference.get();
+
+        onCancelled();
+
+        if (!success) {
+            Toast.makeText(savePINActivity, R.string.pin_save_fail, Toast.LENGTH_LONG).show();
+        } else {
+            savePINActivity.print("Confirm PIN action using fingerprint!");
+            savePINActivity.fingerprintHelper.startAuth(savePINActivity.fingerprintManager, savePINActivity.cryptoObject);
+            savePINActivity.CreateFingerPrintConfirmationDialog();
+        }
+    }
+
+    @Override
+    protected void onCancelled() {
+        SavePINActivity savePINActivity = reference.get();
+
+        savePINActivity.mConfirmWithFingerprintTask = null;
+        if (savePINActivity.dFingerPrintConfirmation != null) {
+            savePINActivity.dFingerPrintConfirmation.cancel();
+        }
+    }
+
+}
