c4a2df87bc63bdb75bab5b160233908e1cf0d3d4
Tangem <git@tangem.com>
2018-07-18 12:14:21 +0300

    Updated on 2025-12-05

--- app/src/main/java/com/tangem/data/network/task/VerificationServerTask.java
+++ app/src/main/java/com/tangem/data/network/task/VerificationServerTask.java
@@ -8,35 +8,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class VerificationServerTask extends AsyncTask<VerificationServerProtocol.Request, Void, List<VerificationServerProtocol.Request>> {
-        public static final String hostURL ="https://tangem-webapp.appspot.com";
+    public static final String hostURL = "https://tangem-webapp.appspot.com";
 
-        public VerificationServerTask() {
-
-        }
-
-        protected List<VerificationServerProtocol.Request> doInBackground(VerificationServerProtocol.Request... requests) {
-            List<VerificationServerProtocol.Request> result = new ArrayList<>();
-            for (int i = 0; i < requests.length; i++) {
-                result.add(requests[i]);
-            }
-
-            for (VerificationServerProtocol.Request request : result) {
-                try {
-                    request.doPost(hostURL);
-                } catch (Exception e) {
-                    request.error = e.getMessage();
-                    if( request.error==null || request.error.isEmpty() )
-                    {
-                        request.error = e.getClass().getName();
-                    }
-                }
-            }
-
-            return result;
-        }
-
-        public String getValidationNodeDescription() {
-            return hostURL;
-        }
+    public VerificationServerTask() {
 
     }
+
+    protected List<VerificationServerProtocol.Request> doInBackground(VerificationServerProtocol.Request... requests) {
+        List<VerificationServerProtocol.Request> result = new ArrayList<>();
+        for (int i = 0; i < requests.length; i++) {
+            result.add(requests[i]);
+        }
+
+        for (VerificationServerProtocol.Request request : result) {
+            try {
+                request.doPost(hostURL);
+            } catch (Exception e) {
+                request.error = e.getMessage();
+                if (request.error == null || request.error.isEmpty()) {
+                    request.error = e.getClass().getName();
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public String getValidationNodeDescription() {
+        return hostURL;
+    }
+
+}
