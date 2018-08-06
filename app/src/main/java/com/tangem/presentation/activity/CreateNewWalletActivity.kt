83504785e145faef5e1711b41f908d152765236d
Tangem <git@tangem.com>
2018-08-06 14:50:03 +0300

    Updated on 2026-01-20

---
+++ app/src/main/java/com/tangem/presentation/activity/CreateNewWalletActivity.kt
@@ -0,0 +1,206 @@
+package com.tangem.presentation.activity
+
+import android.app.Activity
+import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.IsoDep
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.tangem.data.nfc.CreateNewWalletTask
+import com.tangem.domain.cardReader.CardProtocol
+import com.tangem.domain.cardReader.NfcManager
+import com.tangem.domain.wallet.TangemCard
+import com.tangem.presentation.dialog.NoExtendedLengthSupportDialog
+import com.tangem.presentation.dialog.WaitSecurityDelayDialog
+import com.tangem.util.Util
+import com.tangem.wallet.R
+import kotlinx.android.synthetic.main.activity_create_new_wallet.*
+
+class CreateNewWalletActivity : AppCompatActivity(), NfcAdapter.ReaderCallback, CardProtocol.Notifications {
+
+    companion object {
+        val TAG: String = CreateNewWalletActivity::class.java.simpleName
+
+        const val RESULT_INVALID_PIN = Activity.RESULT_FIRST_USER
+    }
+
+    private var card: TangemCard? = null
+    private var nfcManager: NfcManager? = null
+
+    private var createNewWalletTask: CreateNewWalletTask? = null
+    private var lastReadSuccess = true
+
+    private var progressBar: ProgressBar? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_new_wallet)
+
+        MainActivity.commonInit(applicationContext)
+
+        nfcManager = NfcManager(this, this)
+
+        card = TangemCard(intent.getStringExtra("UID"))
+        card!!.LoadFromBundle(intent.extras!!.getBundle("Card"))
+
+        tvCardId.text = card!!.cidDescription
+
+        progressBar = findViewById(R.id.progressBar)
+        progressBar!!.progressTintList = ColorStateList.valueOf(Color.DKGRAY)
+        progressBar!!.visibility = View.INVISIBLE
+    }
+
+    override fun onTagDiscovered(tag: Tag) {
+        try {
+            // get IsoDep handle and run cardReader thread
+            val isoDep = IsoDep.get(tag)
+                    ?: throw CardProtocol.TangemException(getString(R.string.wrong_tag_err))
+            val uid = tag.id
+            val sUID = Util.byteArrayToHexString(uid)
+//            Log.v(TAG, "UID: " + sUID);
+
+            if (sUID == card!!.uid) {
+                if (lastReadSuccess) {
+                    isoDep.timeout = card!!.pauseBeforePIN2 + 5000
+                } else {
+                    isoDep.timeout = card!!.pauseBeforePIN2 + 65000
+                }
+                createNewWalletTask = CreateNewWalletTask(this, card, nfcManager, isoDep, this)
+                createNewWalletTask!!.start()
+            } else {
+//                Log.d(TAG, "Mismatch card UID (" + sUID + " instead of " + mCard.getUID() + ")");
+                nfcManager!!.ignoreTag(isoDep.tag)
+            }
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        nfcManager!!.onResume()
+    }
+
+    public override fun onPause() {
+        nfcManager!!.onPause()
+        if (createNewWalletTask != null)
+            createNewWalletTask!!.cancel(true)
+        super.onPause()
+    }
+
+    public override fun onStop() {
+        // dismiss enable NFC dialog
+        nfcManager!!.onStop()
+        if (createNewWalletTask != null)
+            createNewWalletTask!!.cancel(true)
+        super.onStop()
+    }
+
+    override fun OnReadStart(cardProtocol: CardProtocol) {
+        progressBar!!.post {
+            progressBar!!.visibility = View.VISIBLE
+            progressBar!!.progress = 5
+        }
+    }
+
+    override fun OnReadFinish(cardProtocol: CardProtocol?) {
+        createNewWalletTask = null
+        if (cardProtocol != null) {
+            if (cardProtocol.error == null) {
+                progressBar!!.post {
+                    progressBar!!.progress = 100
+                    progressBar!!.progressTintList = ColorStateList.valueOf(Color.GREEN)
+                    val intent = Intent()
+                    intent.putExtra("UID", cardProtocol.card.uid)
+                    intent.putExtra("Card", cardProtocol.card.asBundle)
+                    setResult(Activity.RESULT_OK, intent)
+                    finish()
+                }
+            } else {
+                lastReadSuccess = false
+                if (cardProtocol.error is CardProtocol.TangemException_InvalidPIN) {
+                    progressBar!!.post {
+                        progressBar!!.progress = 100
+                        progressBar!!.progressTintList = ColorStateList.valueOf(Color.RED)
+                    }
+                    progressBar!!.postDelayed({
+                        try {
+                            progressBar!!.progress = 0
+                            progressBar!!.progressTintList = ColorStateList.valueOf(Color.DKGRAY)
+                            progressBar!!.visibility = View.INVISIBLE
+                            val intent = Intent()
+                            intent.putExtra("message", "Cannot create wallet. Make sure you enter correct PIN2!")
+                            intent.putExtra("UID", cardProtocol.card.uid)
+                            intent.putExtra("Card", cardProtocol.card.asBundle)
+                            setResult(RESULT_INVALID_PIN, intent)
+                            finish()
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                    }, 500)
+                } else {
+                    progressBar!!.post {
+                        if (cardProtocol.error is CardProtocol.TangemException_ExtendedLengthNotSupported) {
+                            if (!NoExtendedLengthSupportDialog.allReadyShowed) {
+                                NoExtendedLengthSupportDialog().show(fragmentManager, NoExtendedLengthSupportDialog.TAG)
+                            }
+                        } else {
+                            Toast.makeText(baseContext, R.string.try_to_scan_again, Toast.LENGTH_SHORT).show()
+                        }
+                        progressBar!!.progress = 100
+                        progressBar!!.progressTintList = ColorStateList.valueOf(Color.RED)
+                    }
+                }
+            }
+        }
+
+        progressBar!!.postDelayed({
+            try {
+                progressBar!!.progress = 0
+                progressBar!!.progressTintList = ColorStateList.valueOf(Color.DKGRAY)
+                progressBar!!.visibility = View.INVISIBLE
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }, 500)
+    }
+
+    override fun OnReadProgress(protocol: CardProtocol, progress: Int) {
+        progressBar!!.post { progressBar!!.progress = progress }
+    }
+
+    override fun OnReadCancel() {
+
+        createNewWalletTask = null
+
+        progressBar!!.postDelayed({
+            try {
+                progressBar!!.progress = 0
+                progressBar!!.progressTintList = ColorStateList.valueOf(Color.DKGRAY)
+                progressBar!!.visibility = View.INVISIBLE
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }, 500)
+    }
+
+    override fun OnReadWait(msec: Int) {
+        WaitSecurityDelayDialog.OnReadWait(this, msec)
+    }
+
+    override fun OnReadBeforeRequest(timeout: Int) {
+        WaitSecurityDelayDialog.onReadBeforeRequest(this, timeout)
+    }
+
+    override fun OnReadAfterRequest() {
+        WaitSecurityDelayDialog.onReadAfterRequest(this)
+    }
+
+}
