a450a92d4b2685f1b3d6f2092cdc1cdfe3950764
Tangem <git@tangem.com>
2018-08-08 19:51:44 +0300

    Updated on 2025-09-15

--- app/src/main/java/com/tangem/data/network/ServerURL.java
+++ app/src/main/java/com/tangem/data/network/ServerURL.java
@@ -1,7 +1,8 @@
 package com.tangem.data.network;
 
 public class ServerURL {
-    public static final String API_TANGEM = "https://tangem-webapp.appspot.com/";
+//    public static final String API_TANGEM = "https://tangem-webapp.appspot.com/";
+    public static final String API_TANGEM = "https://verify.tangem.com/";
 
 }
 
