a7209c5099a845ecbc8423e7a4b5d14c27a65852
Tangem <git@tangem.com>
2018-08-08 17:33:08 +0300

    Updated on 2026-02-02

--- app/src/main/java/com/tangem/presentation/activity/SendTransactionActivity.kt
+++ app/src/main/java/com/tangem/presentation/activity/SendTransactionActivity.kt
@@ -12,7 +12,6 @@ import com.tangem.data.network.request.InfuraRequest
 import com.tangem.data.network.task.send_transaction.ConnectTask
 import com.tangem.data.network.task.send_transaction.ETHRequestTask
 import com.tangem.domain.wallet.Blockchain
-import com.tangem.domain.wallet.CoinEngine
 import com.tangem.domain.wallet.CoinEngineFactory
 import com.tangem.domain.wallet.TangemCard
 import com.tangem.wallet.R
@@ -33,10 +32,10 @@ class SendTransactionActivity : AppCompatActivity() {
 
         val intent = intent
         card = TangemCard(getIntent().getStringExtra("UID"))
-        card!!.LoadFromBundle(intent.extras!!.getBundle("Card"))
+        card!!.loadFromBundle(intent.extras!!.getBundle("Card"))
         tx = intent.getStringExtra("TX")
 
-        val engine = CoinEngineFactory.Create(card!!.blockchain)
+        val engine = CoinEngineFactory.create(card!!.blockchain)
         if (card!!.blockchain == Blockchain.Ethereum || card!!.blockchain == Blockchain.EthereumTestNet || card!!.blockchain == Blockchain.Token) {
             val task = ETHRequestTask(this@SendTransactionActivity, card!!.blockchain)
             val req = InfuraRequest.SendTransaction(card!!.wallet, tx)
@@ -44,13 +43,13 @@ class SendTransactionActivity : AppCompatActivity() {
             req.setBlockchain(card!!.blockchain)
             task.execute(req)
         } else if (card!!.blockchain == Blockchain.Bitcoin || card!!.blockchain == Blockchain.BitcoinTestNet) {
-            val nodeAddress = engine!!.GetNode(card)
-            val nodePort = engine.GetNodePort(card)
+            val nodeAddress = engine!!.getNode(card)
+            val nodePort = engine.getNodePort(card)
             val connectTask = ConnectTask(this@SendTransactionActivity, nodeAddress, nodePort, 3)
             connectTask.execute(ElectrumRequest.Broadcast(card!!.wallet, tx))
         } else if (card!!.blockchain == Blockchain.BitcoinCash || card!!.blockchain == Blockchain.BitcoinCashTestNet) {
-            val nodeAddress = engine!!.GetNode(card)
-            val nodePort = engine.GetNodePort(card)
+            val nodeAddress = engine!!.getNode(card)
+            val nodePort = engine.getNodePort(card)
             val connectTask = ConnectTask(this@SendTransactionActivity, nodeAddress, nodePort, 3)
             connectTask.execute(ElectrumRequest.Broadcast(card!!.wallet, tx))
         }
