743e78cf0a7a0da7e7bbe0ed9392e1b8332571e0
Tangem <git@tangem.com>
2018-08-08 19:51:44 +0300

    Updated on 2026-05-04

--- app/src/main/java/com/tangem/data/network/Server.java
+++ app/src/main/java/com/tangem/data/network/Server.java
@@ -21,6 +21,7 @@ public class Server {
             public static final String INFO_VERIFY = URL_TANGEM + "info/version";
 
             public static final String CARD_VERIFY = URL_TANGEM + "card/verify";
+            public static final String VERIFY = URL_TANGEM + "verify";
             public static final String CARD_VALIDATE = URL_TANGEM + "card/validate";
             public static final String CARD_ACTIVATE = URL_TANGEM + "card/activate";
         }
