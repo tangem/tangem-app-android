bf07f13058f42df4803af3b795550c00330f0d63
Tangem <git@tangem.com>
2018-08-29 14:45:53 +0300

    Updated on 2026-07-23

--- app/src/main/java/com/tangem/data/network/ServerURL.java
+++ app/src/main/java/com/tangem/data/network/ServerURL.java
@@ -1,8 +1,9 @@
+
 package com.tangem.data.network;
 
 public class ServerURL {
-//    public static final String API_TANGEM = "https://tangem-webapp.appspot.com/";
     public static final String API_TANGEM = "https://verify.tangem.com/";
+    public static final String API_COINMARKET = "https://api.coinmarketcap.com/";
 
 }
 
