27b84acd96c4b58b78ae2e0d32b44afc8258b55b
Tangem <git@tangem.com>
2018-09-02 15:16:55 +0300

    Updated on 2026-04-15

--- app/src/main/java/com/tangem/presentation/fragment/SettingsDebug.kt
+++ app/src/main/java/com/tangem/presentation/fragment/SettingsDebug.kt
@@ -11,12 +11,6 @@ class SettingsDebug : PreferenceFragment() {
         super.onCreate(savedInstanceState)
         addPreferencesFromResource(R.xml.fr_settings_debug)
 
-        val debug_new_request_verify = findPreference(getString(R.string.key_debug_new_request_verify)) as SwitchPreference
-        val debug_new_request_verify_show_json = findPreference(getString(R.string.key_debug_new_request_verify_show_json)) as SwitchPreference
-
-        debug_new_request_verify.title = "Новый запрос verify card"
-        debug_new_request_verify_show_json.title = "Показать ответ"
-
     }
 
 }
