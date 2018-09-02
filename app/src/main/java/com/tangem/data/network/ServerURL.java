d9dc5a846caa62715da869cc6b4035474fe882a8
Tangem <git@tangem.com>
2018-09-02 15:16:55 +0300

    Updated on 2026-06-12

--- app/src/main/java/com/tangem/data/network/ServerURL.java
+++ app/src/main/java/com/tangem/data/network/ServerURL.java
@@ -1,13 +1,6 @@
-
 package com.tangem.data.network;
 
 public class ServerURL {
     public static final String API_TANGEM = "https://verify.tangem.com/";
     public static final String API_COINMARKET = "https://api.coinmarketcap.com/";
-
 }
-
-
-
-
-
