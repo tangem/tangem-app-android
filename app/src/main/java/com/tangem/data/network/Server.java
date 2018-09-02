3f896c6d93305772869409601101f465cb6a6362
Tangem <git@tangem.com>
2018-09-02 20:28:56 +0300

    Updated on 2026-03-25

--- app/src/main/java/com/tangem/data/network/Server.java
+++ app/src/main/java/com/tangem/data/network/Server.java
@@ -1,28 +1,16 @@
 package com.tangem.data.network;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Server {
-
-    public static Map<String, String> getHeader() {
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "application/json");
-        return headers;
-    }
 
     /**
      * https://tangem-webapp.appspot.com/
+     * https://tangem-services.appspot.com/
      */
     public static class ApiTangem {
         public static final String URL_TANGEM = ServerURL.API_TANGEM;
 
         public static class Method {
-            public static final String INFO_VERIFY = URL_TANGEM + "info/version";
-            public static final String CARD_VERIFY = URL_TANGEM + "card/verify";
             public static final String VERIFY = URL_TANGEM + "verify";
-            public static final String CARD_VALIDATE = URL_TANGEM + "card/validate";
-            public static final String CARD_ACTIVATE = URL_TANGEM + "card/activate";
         }
     }
 
