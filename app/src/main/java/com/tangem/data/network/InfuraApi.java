0b0a308080e60a93a86cb34e0fc46cb29ee31511
Tangem <git@tangem.com>
2018-09-05 17:17:13 +0300

    Updated on 2025-12-22

--- app/src/main/java/com/tangem/data/network/InfuraApi.java
+++ app/src/main/java/com/tangem/data/network/InfuraApi.java
@@ -6,9 +6,11 @@ import com.tangem.data.network.model.InfuraEthGasPriceResponse;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 public interface InfuraApi {
+    @Headers("Content-Type: application/json")
     @POST(Server.ApiInfura.Method.MAIN)
-    Call<InfuraEthGasPriceResponse> ethGasPrice(@Header("Content-Type") String contentType, @Body InfuraEthGasPriceBody body);
+    Call<InfuraEthGasPriceResponse> ethGasPrice(@Body InfuraEthGasPriceBody body);
 }
