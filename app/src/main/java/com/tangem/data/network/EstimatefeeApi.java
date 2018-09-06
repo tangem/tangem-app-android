8aae5750f3af7a6901a9c1dcd7450ba1c7643dba
Tangem <git@tangem.com>
2018-09-06 13:31:31 +0300

    Updated on 2026-03-27

--- app/src/main/java/com/tangem/data/network/EstimatefeeApi.java
+++ app/src/main/java/com/tangem/data/network/EstimatefeeApi.java
@@ -4,6 +4,12 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface EstimatefeeApi {
-    @GET(Server.ApiEstimatefee.Method.N)
-    Call<String> getEstimatefee();
+    @GET(Server.ApiEstimatefee.Method.N_2)
+    Call<String> getEstimateFeePriority();
+
+    @GET(Server.ApiEstimatefee.Method.N_3)
+    Call<String> getEstimateFeeNormal();
+
+    @GET(Server.ApiEstimatefee.Method.N_6)
+    Call<String> getEstimateFeeMinimal();
 }
