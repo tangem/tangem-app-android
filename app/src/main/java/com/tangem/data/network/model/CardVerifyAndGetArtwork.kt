ef5a04a2224d5f536c7cf9a80870e6a857ca10e8
Tangem <git@tangem.com>
2018-09-16 13:53:14 +0300

    Updated on 2025-12-22

--- app/src/main/java/com/tangem/data/network/model/CardVerifyAndGetArtwork.kt
+++ app/src/main/java/com/tangem/data/network/model/CardVerifyAndGetArtwork.kt
@@ -1,52 +1,46 @@
 package com.tangem.data.network.model
 
-import com.google.gson.annotations.SerializedName
+import android.os.Build
+import java.text.SimpleDateFormat
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+import java.util.*
 
-class CardVerifyAndGetArtwork
- {
-
-     data class Request(
-             @SerializedName("requests")
-             var requests: List<RequestItem>? = null
-     ) {
-
-         data class RequestItem(
-                 @SerializedName("CID")
-                 var CID: String = "",
-
-                 @SerializedName("publicKey")
-                 var publicKey: String = "",
-
-                 @SerializedName("artwork")
-                 var artwork: String = ""
-         )
-
-     }
-
-    data class Response(
-            @SerializedName("results")
-            var results: List<ResultItem>? = null
+class CardVerifyAndGetArtwork {
+    data class Request(
+            var requests: List<Item>? = null
     ) {
 
-        data class ResultItem(
-                @SerializedName("error")
-                var error: String = "",
-
-                @SerializedName("CID")
+        data class Item(
                 var CID: String = "",
-
-                @SerializedName("passed")
-                var passed: Boolean = false,
-
-                @SerializedName("artwork")
-                var artwork: String = "",
-
-                @SerializedName("batch")
-                var batch: String = "",
-
-                @SerializedName("update_date")
-                var update_date: String = ""
+                var publicKey: String = ""
         )
+
+    }
+
+    data class Response(
+            var results: List<Item>? = null
+    ) {
+
+        data class Item(
+                var error: String = "",
+                var CID: String = "",
+                var passed: Boolean = false,
+                var batch: String = "",
+                var artworkId: String = "",
+                var artworkHash: String = "",
+                var update_date: String = ""
+        ) {
+            fun getUpdateDate(): Instant? {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    return LocalDateTime.parse(update_date, DateTimeFormatter.ISO_LOCAL_DATE_TIME).toInstant(ZoneOffset.UTC)
+                }else{
+                    return SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS", Locale.US).parse(update_date).toInstant()
+                }
+            }
+        }
     }
 
 }
