739a44b6a1552320fd43c935a792cc03416db871
Tangem <git@tangem.com>
2018-10-08 17:49:28 +0300

    Updated on 2026-06-24

--- app/src/main/java/com/tangem/presentation/activity/SendTransactionActivity.kt
+++ app/src/main/java/com/tangem/presentation/activity/SendTransactionActivity.kt
@@ -51,27 +51,14 @@ class SendTransactionActivity : AppCompatActivity(), NfcAdapter.ReaderCallback  
 
         val engine = CoinEngineFactory.create(card!!.blockchain)
         if (card!!.blockchain == Blockchain.Ethereum || card!!.blockchain == Blockchain.EthereumTestNet || card!!.blockchain == Blockchain.Token) {
-//            val task = ETHRequestTask(this@SendTransactionActivity, card!!.blockchain)
-//            val req = InfuraRequest.SendTransaction(card!!.wallet, tx)
-//            req.id = 67
-//            req.setBlockchain(card!!.blockchain)
-//            task.execute(req)
 
             requestInfura(ServerApiHelper.INFURA_ETH_SEND_RAW_TRANSACTION, "")
 
         } else if (card!!.blockchain == Blockchain.Bitcoin || card!!.blockchain == Blockchain.BitcoinTestNet) {
-//            val nodeAddress = engine!!.getNode(card)
-//            val nodePort = engine.getNodePort(card)
-//            val connectTask = ConnectTask(this@SendTransactionActivity, nodeAddress, nodePort, 3)
-//            connectTask.execute(ElectrumRequest.broadcast(card!!.wallet, tx))
 
             requestElectrum(card!!, ElectrumRequest.broadcast(card!!.wallet, tx))
 
         } else if (card!!.blockchain == Blockchain.BitcoinCash || card!!.blockchain == Blockchain.BitcoinCashTestNet) {
-//            val nodeAddress = engine!!.getNode(card)
-//            val nodePort = engine.getNodePort(card)
-//            val connectTask = ConnectTask(this@SendTransactionActivity, nodeAddress, nodePort, 3)
-//            connectTask.execute(ElectrumRequest.broadcast(card!!.wallet, tx))
 
             requestElectrum(card!!, ElectrumRequest.broadcast(card!!.wallet, tx))
         }
