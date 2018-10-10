e581e4c4bf79beeafb4d210e9296889f5c3d9259
Tangem <git@tangem.com>
2018-10-10 22:04:20 +0300

    Updated on 2026-05-19

--- app/src/main/java/com/tangem/data/network/ServerURL.java
+++ app/src/main/java/com/tangem/data/network/ServerURL.java
@@ -1,9 +1,9 @@
 package com.tangem.data.network;
 
-public class ServerURL {
-    public static final String API_TANGEM = "https://verify.tangem.com/";
-    public static final String API_COINMARKETCAP = "https://api.coinmarketcap.com/";
-    public static final String API_INFURA = "https://mainnet.infura.io/";
-    public static final String API_ESTIMATEFEE = " https://estimatefee.com/";
-    public static final String API_UPDATE_VERSION = "https://raw.githubusercontent.com/";
+class ServerURL {
+    static final String API_TANGEM = "https://verify.tangem.com/";
+    static final String API_COINMARKETCAP = "https://api.coinmarketcap.com/";
+    static final String API_INFURA = "https://mainnet.infura.io/";
+    static final String API_ESTIMATEFEE = " https://estimatefee.com/";
+    static final String API_UPDATE_VERSION = "https://raw.githubusercontent.com/";
 }
