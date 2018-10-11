f58ba221963d3379fc48c53997ffc96f76adb19b
Tangem <git@tangem.com>
2018-10-11 22:50:31 +0300

    Updated on 2026-01-24

--- app/src/main/java/com/tangem/domain/BitcoinNode.kt
+++ app/src/main/java/com/tangem/domain/BitcoinNode.kt
@@ -5,6 +5,6 @@ enum class BitcoinNode(val host: String, val port: Int) {
     n14("electrum.vom-stausee.de", 50001),
     n15("helicarrier.bauerj.eu", 50001),
     n16("kirsche.emzy.de", 50001),
-    n17("spv.48.org", 50003),
-    n18("vps.hsmiths.com", 50001),
+//    n17("spv.48.org", 50003),
+//    n18("vps.hsmiths.com", 50001),
 }
