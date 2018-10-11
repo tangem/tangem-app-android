79b6234d7fdd68ddfab7b0ecea23ce5a9eccefaa
Tangem <git@tangem.com>
2018-10-11 13:26:25 +0300

    Updated on 2026-06-12

--- app/src/main/java/com/tangem/data/network/TangemApi.java
+++ app/src/main/java/com/tangem/data/network/TangemApi.java
@@ -1,8 +1,6 @@
 package com.tangem.data.network;
 
 import com.tangem.data.network.model.CardVerifyAndGetInfo;
-import com.tangem.data.network.model.CardVerifyBody;
-import com.tangem.data.network.model.CardVerifyResponse;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -14,15 +12,10 @@ import retrofit2.http.Query;
 
 public interface TangemApi {
     @Headers("Content-Type: application/json")
-    @POST(Server.ApiTangem.Method.VERIFY)
-    Call<CardVerifyResponse> getCardVerify(@Body CardVerifyBody body);
-
-    @Headers("Content-Type: application/json")
     @POST(Server.ApiTangem.Method.VERIFY_AND_GET_INFO)
     Call<CardVerifyAndGetInfo.Response> getCardVerifyAndGetInfo(@Body CardVerifyAndGetInfo.Request requestBody);
 
     @GET(Server.ApiTangem.Method.ARTWORK)
     Call<ResponseBody> getArtwork(@Query("artworkId") String artworkId, @Query("CID") String CID, @Query("publicKey") String publicKey);
-
 
 }
