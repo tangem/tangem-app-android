b84fcd0cdcf0e6ebbe8c60829153328c21b0e8f4
Tangem <git@tangem.com>
2018-10-18 17:49:35 +0300

    Updated on 2026-07-07

--- app/src/main/java/com/tangem/presentation/dialog/NoExtendedLengthSupportDialog.kt
+++ app/src/main/java/com/tangem/presentation/dialog/NoExtendedLengthSupportDialog.kt
@@ -12,14 +12,15 @@ class NoExtendedLengthSupportDialog : DialogFragment() {
     companion object {
         val TAG: String = NoExtendedLengthSupportDialog::class.java.simpleName
         var allReadyShowed = false
+        var message = ""// = R.string.the_nfc_adapter_length_apdu
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return AlertDialog.Builder(activity)
                 .setIcon(R.drawable.tangem_logo_small_new)
                 .setTitle(R.string.warning)
-                .setMessage(R.string.the_nfc_adapter_length_apdu)
-                .setPositiveButton(R.string.got_it) { _, _ -> NoExtendedLengthSupportDialog.allReadyShowed = true }
+                .setMessage(message)
+                .setPositiveButton(R.string.got_it) { _, _ -> NoExtendedLengthSupportDialog.allReadyShowed = false }
                 .create()
     }
 
