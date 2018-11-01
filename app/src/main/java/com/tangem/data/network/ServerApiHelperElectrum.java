1cfea3c3723af4ce484cde038c5af13fa3050054
Tangem <git@tangem.com>
2018-11-01 13:41:28 +0300

    Updated on 2026-09-25

--- app/src/main/java/com/tangem/data/network/ServerApiHelperElectrum.java
+++ app/src/main/java/com/tangem/data/network/ServerApiHelperElectrum.java
@@ -9,6 +9,7 @@ import com.tangem.domain.wallet.Blockchain;
 import com.tangem.domain.wallet.TangemCard;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -21,6 +22,15 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+
+import javax.net.SocketFactory;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLHandshakeException;
+import javax.net.ssl.SSLPeerUnverifiedException;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -41,6 +51,7 @@ public class ServerApiHelperElectrum {
 
     public interface ElectrumRequestDataListener {
         void onSuccess(ElectrumRequest electrumRequest);
+
         void onFail(String method);
     }
 
@@ -51,7 +62,8 @@ public class ServerApiHelperElectrum {
     public void electrumRequestData(TangemCard card, ElectrumRequest electrumRequest) {
         Observable<ElectrumRequest> checkElectrumDataObserver = Observable.just(electrumRequest)
 
-                .doOnNext(electrumRequest1 -> doElectrumRequest(card, electrumRequest))
+//                .doOnNext(electrumRequest1 -> doElectrumRequest(card, electrumRequest))
+                .doOnNext(electrumRequest1 -> testSslSocket(card, electrumRequest))
 
                 .flatMap(electrumRequest1 -> {
                     if (electrumRequest1.answerData == null) {
@@ -94,7 +106,63 @@ public class ServerApiHelperElectrum {
         });
     }
 
-    private List<ElectrumRequest> doElectrumRequest(TangemCard card, ElectrumRequest electrumRequest) throws ConnectException {
+    private List<ElectrumRequest> testSslSocket(TangemCard card, ElectrumRequest electrumRequest) {
+        SocketFactory sf = SSLSocketFactory.getDefault();
+        SSLSocket socket;
+
+        List<ElectrumRequest> result = new ArrayList<>();
+        Collections.addAll(result, electrumRequest);
+
+        try {
+//            socket = (SSLSocket) sf.createSocket("gmail.com", 443);
+            socket = (SSLSocket) sf.createSocket("electrum.hsmiths.com", 50002);
+            HostnameVerifier hv = HttpsURLConnection.getDefaultHostnameVerifier();
+            SSLSession s = socket.getSession();
+
+//            if (!hv.verify("mail.google.com", s)) {
+//                throw new SSLHandshakeException("Expected mail.google.com, found " + s.getPeerPrincipal());
+//            }
+
+
+            try {
+                OutputStream os = socket.getOutputStream();
+                OutputStreamWriter out = new OutputStreamWriter(os, "UTF-8");
+                InputStream is = socket.getInputStream();
+                BufferedReader in = new BufferedReader(new InputStreamReader(is));
+                electrumRequest.setID(1);
+
+                out.write(electrumRequest.getAsString() + "\n");
+                out.flush();
+
+                electrumRequest.answerData = in.readLine();
+//                electrumRequest.host = host;
+//                electrumRequest.port = port;
+                if (electrumRequest.answerData != null) {
+                    Log.i(TAG, ">> " + electrumRequest.answerData);
+                } else {
+                    electrumRequest.error = "No answer from server";
+                    Log.i(TAG, ">> <NULL>");
+                }
+
+            } catch (ConnectException e) {
+                e.printStackTrace();
+                Log.e(TAG, "electrumRequestData " + electrumRequest.getMethod() + " ConnectException " + e.getMessage());
+            } finally {
+//                Log.i(TAG, "electrumRequestData " + electrumRequest.getMethod() + " CLOSE");
+                socket.close();
+            }
+
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+
+
+    }
+
+    private List<ElectrumRequest> doElectrumRequest(TangemCard card, ElectrumRequest electrumRequest) {
         BitcoinNode bitcoinNode = BitcoinNode.values()[new Random().nextInt(BitcoinNode.values().length)];
 
         if (card.getBlockchain() == Blockchain.BitcoinTestNet || card.getBlockchain() == Blockchain.BitcoinCashTestNet) {
@@ -112,7 +180,7 @@ public class ServerApiHelperElectrum {
 
         try {
             Socket socket = App.getNetworkComponent().getSocket();
-             socket.connect(new InetSocketAddress(InetAddress.getByName(host), port));
+            socket.connect(new InetSocketAddress(InetAddress.getByName(host), port));
             Log.i(TAG, host + " " + port);
             try {
                 OutputStream os = socket.getOutputStream();
