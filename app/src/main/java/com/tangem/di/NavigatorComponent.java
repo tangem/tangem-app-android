e708185b252d9767718bdf5e92b91ceaac7032e0
Tangem <git@tangem.com>
2018-11-13 23:33:45 +0300

    Updated on 2026-08-19

--- app/src/main/java/com/tangem/di/NavigatorComponent.java
+++ app/src/main/java/com/tangem/di/NavigatorComponent.java
@@ -1,15 +1,20 @@
 package com.tangem.di;
 
 import com.tangem.presentation.activity.LogoActivity;
+import com.tangem.presentation.activity.MainActivity;
 
 import javax.inject.Singleton;
 
 import dagger.Component;
 
 @Singleton
-@Component(modules = {NavigatorModule.class})
+@Component(modules = {
+//        AppModule.class,
+        NavigatorModule.class})
 public interface NavigatorComponent {
 
     void inject(LogoActivity activity);
+
+    void inject(MainActivity activity);
 
 }
