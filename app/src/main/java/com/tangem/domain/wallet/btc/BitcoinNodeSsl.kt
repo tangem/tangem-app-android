40132d9ba4b89e52a98958832bd7c3fb6287e7bb
Tangem <git@tangem.com>
2018-11-19 15:53:24 +0300

    Updated on 2026-09-23

--- app/src/main/java/com/tangem/domain/wallet/btc/BitcoinNodeSsl.kt
+++
@@ -1,7 +0,0 @@
-package com.tangem.domain.wallet.btc
-
-enum class BitcoinNodeSsl(val host: String, val port: Int) {
-    n1("electrum.anduck.net", 50012),
-    n2("electrum.eff.ro", 50002),
-    n3("vps.hsmiths.com", 50002)
-}
