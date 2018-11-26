60725a018cd718710c30040e1a44102588ddfa91
Tangem <git@tangem.com>
2018-11-27 00:28:00 +0300

    Updated on 2026-02-06

--- app/src/main/java/com/tangem/presentation/dialog/PINSwapWarningDialog.java
+++ app/src/main/java/com/tangem/presentation/dialog/PINSwapWarningDialog.java
@@ -2,9 +2,9 @@ package com.tangem.presentation.dialog;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 
 import com.tangem.wallet.R;
 
