11679e1bd0d1fce3b1d076f3d670544cdf92fe41
Tangem <git@tangem.com>
2018-11-28 17:52:33 +0300

    Updated on 2026-05-07

---
+++ app/src/main/java/com/tangem/Constant.kt
@@ -0,0 +1,13 @@
+package com.tangem
+
+object Constant {
+
+
+    const val EXTRA_BLOCKCHAIN_DATA = "BLOCKCHAIN_DATA"
+
+    const val MESSAGE = "Message"
+    const val ERROR = "Error"
+
+    const val EXTRA_MODIFICATION = "modification"
+
+}
