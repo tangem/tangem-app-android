d6d8692e4566fab34b9a3c5539c2551792aca98e
Tangem <git@tangem.com>
2018-11-29 10:48:44 +0300

    Updated on 2025-11-26

--- app/src/main/java/com/tangem/domain/cardReader/Firmwares.java
+++ app/src/main/java/com/tangem/domain/cardReader/Firmwares.java
@@ -12,7 +12,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
-public class FW {
+public class Firmwares {
     private static JsonArray jaFirmwares = null;
 
     public static boolean needInit() {
