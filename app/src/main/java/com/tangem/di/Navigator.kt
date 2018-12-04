ce52a419f7beb09d48cab3a635405bd9a52bbb9c
Tangem <git@tangem.com>
2018-12-04 15:53:05 +0300

    Updated on 2026-02-25

--- app/src/main/java/com/tangem/di/Navigator.kt
+++ app/src/main/java/com/tangem/di/Navigator.kt
@@ -3,6 +3,7 @@ package com.tangem.di
 import android.app.Activity
 import android.nfc.Tag
 import android.os.Bundle
+import com.tangem.Constant
 import com.tangem.domain.wallet.CoinData
 import com.tangem.domain.wallet.TangemCard
 import com.tangem.presentation.activity.EmptyWalletActivity
@@ -18,15 +19,15 @@ class Navigator {
     }
 
     fun showLoadedWallet(context: Activity, lastTag: Tag, cardInfo: Bundle) {
-        context.startActivityForResult(LoadedWalletActivity.callingIntent(context, lastTag, cardInfo), MainActivity.REQUEST_CODE_SHOW_CARD_ACTIVITY)
+        context.startActivityForResult(LoadedWalletActivity.callingIntent(context, lastTag, cardInfo), Constant.REQUEST_CODE_SHOW_CARD_ACTIVITY)
     }
 
     fun showEmptyWallet(context: Activity) {
         context.startActivity(EmptyWalletActivity.callingIntent(context))
     }
 
-    fun showVerifyCard(context: Activity, card: TangemCard, coinData: CoinData, message: String, error: String) {
-        context.startActivityForResult(VerifyCardActivity.callingIntent(context, card, coinData, message, error), LoadedWallet.REQUEST_CODE_VERIFY_CARD)
+    fun showVerifyCard(context: Activity, card: TangemCard, coinData: CoinData) {
+        context.startActivityForResult(VerifyCardActivity.callingIntent(context, card, coinData), Constant.REQUEST_CODE_VERIFY_CARD)
     }
 
     fun showPinSwap(context: Activity) {
