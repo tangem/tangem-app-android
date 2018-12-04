ac123a1b18712b839b87c8d11ecdb3d373c4f172
Tangem <git@tangem.com>
2018-12-04 11:05:16 +0300

    Updated on 2026-03-27

--- app/src/main/java/com/tangem/presentation/activity/EmptyWalletActivity.kt
+++ app/src/main/java/com/tangem/presentation/activity/EmptyWalletActivity.kt
@@ -13,14 +13,14 @@ import android.support.v7.app.AppCompatActivity
 import android.text.Html
 import android.view.View
 import android.widget.Toast
-import com.tangem.data.nfc.VerifyCardTask
-import com.tangem.domain.cardReader.CardProtocol
-import com.tangem.domain.cardReader.NfcManager
-import com.tangem.domain.wallet.TangemCard
+import com.tangem.tangemcard.tasks.VerifyCardTask
+import com.tangem.tangemcard.reader.CardProtocol
+import com.tangem.tangemcard.reader.NfcManager
+import com.tangem.tangemcard.data.TangemCard
 import com.tangem.domain.wallet.TangemContext
 import com.tangem.presentation.dialog.NoExtendedLengthSupportDialog
 import com.tangem.presentation.dialog.WaitSecurityDelayDialog
-import com.tangem.util.Util
+import com.tangem.tangemcard.util.Util
 import com.tangem.wallet.R
 import kotlinx.android.synthetic.main.activity_empty_wallet.*
 
