dbd22fc140823bced306a247423bc1bdeb5d370b
Tangem <git@tangem.com>
2018-12-07 11:41:30 +0300

    Updated on 2025-11-18

--- app/src/main/java/com/tangem/data/network/ServerURL.java
+++ app/src/main/java/com/tangem/data/network/ServerURL.java
@@ -4,6 +4,6 @@ class ServerURL {
     static final String API_TANGEM = "https://verify.tangem.com/";
     static final String API_COINMARKETCAP = "https://api.coinmarketcap.com/";
     static final String API_INFURA = "https://mainnet.infura.io/";
-    static final String API_ESTIMATEFEE = " https://estimatefee.com/";
+    static final String API_ESTIMATEFEE = "https://estimatefee.com/";
     static final String API_UPDATE_VERSION = "https://raw.githubusercontent.com/";
 }
