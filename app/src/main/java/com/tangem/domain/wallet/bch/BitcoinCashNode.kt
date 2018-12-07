e1c14ec8ef979dc001935fce53be4be2f2c5cbe7
Tangem <git@tangem.com>
2018-12-07 17:38:02 +0300

    Updated on 2026-09-23

--- app/src/main/java/com/tangem/domain/wallet/bch/BitcoinCashNode.kt
+++ app/src/main/java/com/tangem/domain/wallet/bch/BitcoinCashNode.kt
@@ -1,8 +1,24 @@
 package com.tangem.domain.wallet.bch
 
-enum class BitcoinCashNode(val host: String, val port: Int) {
-    n1("electrumx-bch.cryptonermal.net", 50001),
-    n2("abc1.hsmiths.com", 60001),
-    n3("electrum.imaginary.cash", 50001),
-    n4("35.157.238.5", 51001),
+enum class BitcoinCashNode(val host: String, val port: Int, val proto: String) {
+    N_001("electrumx.hillsideinternet.com", 50002, "ssl"),
+    N_002("bch0.kister.net", 50002, "ssl"),
+    N_003("abc1.hsmiths.com", 60002, "ssl"),
+    N_004("bch.curalle.ovh", 50002, "ssl"),
+    N_005("207.180.215.112", 52002, "ssl"),
+    N_006("bch.imaginary.cash", 50002, "ssl"),
+    N_007("dedi.jochen-hoenicke.de", 51002, "ssl"),
+    N_008("crypto.mldlabs.com", 50002, "ssl"),
+    N_009("bch.electrumx.cash", 50002, "ssl"),
+    N_010("electroncash.cascharia.com", 50002, "ssl"),
+    N_011("bch.crypto.mldlabs.com", 50002, "ssl"),
+    N_012("electron-cash.dragon.zone", 50002, "ssl"),
+    N_013("electron.coinucopia.io", 50002, "ssl"),
+    N_014("blackie.c3-soft.com", 50002, "ssl"),
+    N_015("electroncash.ueo.ch", 51002, "ssl"),
+    N_016("electrum.imaginary.cash", 50002, "ssl"),
+    N_017("35.157.238.5", 51002, "ssl"),
+    N_018("bitcoincash.quangld.com", 50002, "ssl"),
+    N_019("bch.stitthappens.com", 50002, "ssl"),
+    N_020("electroncash.dk", 50002, "ssl"),
 }
