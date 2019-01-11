588510668b998ff30cea429727a3969406933621
Tangem <git@tangem.com>
2019-01-11 09:13:19 +0300

    Updated on 2026-05-14

--- app/src/main/java/com/tangem/data/network/ServerURL.java
+++ app/src/main/java/com/tangem/data/network/ServerURL.java
@@ -6,4 +6,5 @@ class ServerURL {
     static final String API_INFURA = "https://mainnet.infura.io/v3/";
     static final String API_ESTIMATEFEE = "https://estimatefee.com/";
     static final String API_UPDATE_VERSION = "https://raw.githubusercontent.com/";
+    static final String API_STELLAR = "https://horizon-testnet.stellar.org";
 }
