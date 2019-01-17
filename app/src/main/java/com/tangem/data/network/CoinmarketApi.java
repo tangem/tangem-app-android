9fcd5f5cb677e4f943c13c2a062da931d3e96db4
Tangem <git@tangem.com>
2019-01-17 12:55:20 +0300

    Updated on 2026-09-14

--- app/src/main/java/com/tangem/data/network/CoinmarketApi.java
+++ app/src/main/java/com/tangem/data/network/CoinmarketApi.java
@@ -2,12 +2,13 @@ package com.tangem.data.network;
 
 import com.tangem.data.network.model.RateInfoResponse;
 
-import java.util.List;
-
-import io.reactivex.Observable;
+import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.Query;
 
 public interface CoinmarketApi {
-    @GET(Server.ApiCoinmarket.Method.V1_TICKER_CONVERT)
-    Observable<List<RateInfoResponse>> getRateInfoList();
+    @Headers("X-CMC_PRO_API_KEY: 7850b957-6943-42eb-af73-8d536dd8f73e")
+    @GET(Server.ApiCoinmarket.Method.PRICE_CONVERSION)
+    Call<RateInfoResponse> getRateInfo(@Query("amount") int amount, @Query("symbol") String cryptoId);
 }
