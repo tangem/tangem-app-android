32114e179eb109a9d8d4571e033109b08223a950
Tangem <git@tangem.com>
2019-01-26 15:15:44 +0300

    Updated on 2025-11-03

--- app/src/main/java/com/tangem/presentation/dialog/NoExtendedLengthSupportDialog.kt
+++ app/src/main/java/com/tangem/presentation/dialog/NoExtendedLengthSupportDialog.kt
@@ -3,7 +3,7 @@ package com.tangem.presentation.dialog
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
-import android.support.v4.app.DialogFragment
+import androidx.fragment.app.DialogFragment
 
 import com.tangem.wallet.R
 
