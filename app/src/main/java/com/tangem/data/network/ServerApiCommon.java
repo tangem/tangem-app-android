b35419d2a1c757ab824539c086049895555d9dc9
Tangem <git@tangem.com>
2019-01-26 15:15:44 +0300

    Updated on 2025-12-18

--- app/src/main/java/com/tangem/data/network/ServerApiCommon.java
+++ app/src/main/java/com/tangem/data/network/ServerApiCommon.java
@@ -1,7 +1,7 @@
 package com.tangem.data.network;
 
 import android.annotation.SuppressLint;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.util.Log;
 
 import com.tangem.App;
