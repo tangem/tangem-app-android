0054bf6f9bc4f06e183555728c43f87c3baf5f2b
Tangem <git@tangem.com>
2019-01-26 15:15:44 +0300

    Updated on 2026-09-14

--- app/src/main/java/com/tangem/presentation/activity/PrepareTransactionActivity.kt
+++ app/src/main/java/com/tangem/presentation/activity/PrepareTransactionActivity.kt
@@ -7,7 +7,7 @@ import android.content.Intent
 import android.nfc.NfcAdapter
 import android.nfc.Tag
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.text.Html
 import android.view.View
 import android.view.inputmethod.EditorInfo
