0b55bd0d4c5cf1951b01e7121a366ace787d60b6
Tangem <git@tangem.com>
2019-01-26 15:15:44 +0300

    Updated on 2026-09-23

--- app/src/main/java/com/tangem/presentation/activity/ConfirmTransactionActivity.kt
+++ app/src/main/java/com/tangem/presentation/activity/ConfirmTransactionActivity.kt
@@ -5,7 +5,7 @@ import android.content.Intent
 import android.nfc.NfcAdapter
 import android.nfc.Tag
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.text.Editable
 import android.text.Html
 import android.text.TextWatcher
