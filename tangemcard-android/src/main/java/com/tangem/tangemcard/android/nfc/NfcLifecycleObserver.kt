a37f85ff11b95ff53f56e19d7aada5ce425158fc
Tangem <git@tangem.com>
2019-02-04 14:35:48 +0300

    Updated on 2026-06-12

--- tangemcard-android/src/main/java/com/tangem/tangemcard/android/nfc/NfcLifecycleObserver.kt
+++ tangemcard-android/src/main/java/com/tangem/tangemcard/android/nfc/NfcLifecycleObserver.kt
@@ -1,10 +1,15 @@
-package com.tangem.presentation.activity
+package com.tangem.tangemcard.android.nfc
 
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
 import com.tangem.tangemcard.android.reader.NfcManager
 
+/**
+ * Lifecycle observer for all activities and fragments with nfc
+ *
+ * @see NfcManager
+ */
 class NfcLifecycleObserver(private var nfcManager: NfcManager) : LifecycleObserver {
 
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
