aa325d0174c83353388c709859c838ad2ed24d13
Tangem <git@tangem.com>
2019-02-04 14:35:48 +0300

    Updated on 2026-04-06

--- app/src/main/java/com/tangem/presentation/activity/SendTransactionActivity.kt
+++ app/src/main/java/com/tangem/presentation/activity/SendTransactionActivity.kt
@@ -13,6 +13,7 @@ import com.tangem.domain.wallet.CoinEngineFactory
 import com.tangem.domain.wallet.TangemContext
 import com.tangem.presentation.event.TransactionFinishWithError
 import com.tangem.presentation.event.TransactionFinishWithSuccess
+import com.tangem.tangemcard.android.nfc.NfcLifecycleObserver
 import com.tangem.tangemcard.android.reader.NfcManager
 import com.tangem.util.UtilHelper
 import com.tangem.wallet.R
