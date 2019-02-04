aad9d9479bdd532fb8f15ec693873a0aea184e6b
Tangem <git@tangem.com>
2019-02-04 15:54:40 +0300

    Updated on 2026-08-14

--- app/src/main/java/com/tangem/presentation/dialog/PINSwapWarningDialog.java
+++ app/src/main/java/com/tangem/presentation/dialog/PINSwapWarningDialog.java
@@ -4,6 +4,8 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
 import com.tangem.wallet.R;
@@ -29,9 +31,9 @@ public class PINSwapWarningDialog extends DialogFragment {
         message = getArguments().getString(EXTRA_MESSAGE);
     }
 
+    @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-
         return new AlertDialog.Builder(getActivity())
                 .setIcon(R.drawable.tangem_logo_small_new)
                 .setTitle(R.string.your_money_is_at_risk)
