b4335b85a7e895417e0043612d3850405504d265
Tangem <git@tangem.com>
2019-02-04 12:26:56 +0300

    Updated on 2026-09-23

--- tangemcard-android/src/main/java/com/tangem/tangemcard/android/nfc/DeviceNFCAntennaLocation.kt
+++ tangemcard-android/src/main/java/com/tangem/tangemcard/android/nfc/DeviceNFCAntennaLocation.kt
@@ -1,8 +1,21 @@
 package com.tangem.tangemcard.android.nfc
 
+import android.content.Context
 import android.os.Build
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.DecelerateInterpolator
+import android.view.animation.Transformation
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import androidx.appcompat.widget.LinearLayoutCompat
 
-class DeviceNFCAntennaLocation {
+class DeviceNFCAntennaLocation(private var context: Context,
+                               private var ivHandCardHorizontal: ImageView,
+                               private var ivHandCardVertical: ImageView,
+                               private var llHand: LinearLayoutCompat,
+                               private var llNfc: LinearLayoutCompat) {
+
     companion object {
         const val CARD_ON_BACK = 0
         const val CARD_ON_FRONT = 1
@@ -16,7 +29,33 @@ class DeviceNFCAntennaLocation {
     var y: Float = 0.toFloat()
     var z: Int = 0
 
-    fun getAntennaLocation() {
+    fun init() {
+        getAntennaLocation()
+        setCardOrientation()
+        animate()
+    }
+
+    fun animate() {
+        val lp = llHand.layoutParams as RelativeLayout.LayoutParams
+        val lp2 = llNfc.layoutParams as RelativeLayout.LayoutParams
+        val dp = context.resources.displayMetrics.density
+        val lm = dp * (69 + x * 75)
+        lp.topMargin = (dp * (-100 + y * 250)).toInt()
+        lp2.topMargin = (dp * (-125 + y * 250)).toInt()
+        llNfc.layoutParams = lp2
+
+        val a = object : Animation() {
+            override fun applyTransformation(interpolatedTime: Float, t: Transformation) {
+                lp.leftMargin = (lm * interpolatedTime).toInt()
+                llHand.layoutParams = lp
+            }
+        }
+        a.duration = 2000
+        a.interpolator = DecelerateInterpolator()
+        llHand.startAnimation(a)
+    }
+
+    private fun getAntennaLocation() {
         val codename = Build.DEVICE
 
         // default values
@@ -34,6 +73,26 @@ class DeviceNFCAntennaLocation {
                 this.y = nfcLocation.y / 100f
                 this.z = nfcLocation.z
             }
+        }
+    }
+
+    private fun setCardOrientation() {
+        when (orientation) {
+            CARD_ORIENTATION_HORIZONTAL -> {
+                ivHandCardHorizontal.visibility = View.VISIBLE
+                ivHandCardVertical.visibility = View.GONE
+            }
+
+            CARD_ORIENTATION_VERTICAL -> {
+                ivHandCardVertical.visibility = View.VISIBLE
+                ivHandCardHorizontal.visibility = View.GONE
+            }
+        }
+
+        // set card z position
+        when (z) {
+            DeviceNFCAntennaLocation.CARD_ON_BACK -> llHand.elevation = 0.0f
+            DeviceNFCAntennaLocation.CARD_ON_FRONT -> llHand.elevation = 30.0f
         }
     }
 
