69bbd4685c13013cf5b3b5479810ea935b00a774
Tangem <git@tangem.com>
2019-02-20 13:13:18 +0300

    Updated on 2026-09-21

---
+++ app/src/main/java/com/tangem/data/network/model/AdaliteBody.java
@@ -0,0 +1,9 @@
+package com.tangem.data.network.model;
+
+public class AdaliteBody {
+    private String signedTx;
+
+    public AdaliteBody(String signedTx) {
+        this.signedTx = signedTx;
+    }
+}
