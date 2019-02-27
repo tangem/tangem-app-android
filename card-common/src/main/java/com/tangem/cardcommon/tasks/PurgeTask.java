8e3ad906b9842a585e42716054c32390e7dec808
Tangem <git@tangem.com>
2019-02-27 22:22:06 +0300

    Updated on 2026-09-01

---
+++ card-common/src/main/java/com/tangem/cardcommon/tasks/PurgeTask.java
@@ -0,0 +1,27 @@
+package com.tangem.cardcommon.tasks;
+
+import com.tangem.cardcommon.data.external.PINsProvider;
+import com.tangem.cardcommon.reader.NfcReader;
+import com.tangem.cardcommon.reader.CardProtocol;
+import com.tangem.cardcommon.data.TangemCard;
+import com.tangem.cardcommon.data.external.CardDataSubstitutionProvider;
+
+public class PurgeTask extends CustomReadCardTask {
+    public static final String TAG = PurgeTask.class.getSimpleName();
+
+    public PurgeTask(TangemCard card, NfcReader reader, CardDataSubstitutionProvider cardDataSubstitutionProvider, PINsProvider pinsProvider, CardProtocol.Notifications notifications) {
+        super(card, reader, cardDataSubstitutionProvider, pinsProvider, notifications);
+    }
+
+    @Override
+    public void run_Task() throws Exception {
+        if (mCard.getPauseBeforePIN2() > 0) {
+            mNotifications.onReadWait(mCard.getPauseBeforePIN2());
+        }
+        protocol.run_PurgeWallet(pinsProvider.getPIN2());
+        mNotifications.onReadProgress(protocol, 50);
+        protocol.run_Read();
+        mNotifications.onReadProgress(protocol, 100);
+
+    }
+}
