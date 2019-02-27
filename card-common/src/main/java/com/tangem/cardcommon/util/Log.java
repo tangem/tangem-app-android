94b06ba90eb8656a5def2c5e96169826d254577e
Tangem <git@tangem.com>
2019-02-27 22:22:06 +0300

    Updated on 2026-09-21

---
+++ card-common/src/main/java/com/tangem/cardcommon/util/Log.java
@@ -0,0 +1,13 @@
+package com.tangem.cardcommon.util;
+
+public class Log {
+    public static void i(String logTag, String message) {
+
+    }
+    public static void e(String logTag, String message) {
+
+    }
+    public static void v(String logTag, String message) {
+
+    }
+}
