1b6425f0eddc0b30569d65a9c1f0b61755033b10
Tangem <git@tangem.com>
2019-02-27 22:22:06 +0300

    Updated on 2026-06-10

--- app/src/main/java/com/tangem/data/Logger.java
+++ app/src/main/java/com/tangem/data/Logger.java
@@ -3,7 +3,7 @@ package com.tangem.data;
 import android.content.Context;
 import android.util.Log;
 
-import com.tangem.tangemcommon.util.Util;
+import com.tangem.cardcommon.util.Util;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
