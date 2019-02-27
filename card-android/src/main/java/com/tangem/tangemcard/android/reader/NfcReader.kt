b4a7ebc03605dcc512698d9bb79e05979c592d62
Tangem <git@tangem.com>
2019-02-27 22:22:06 +0300

    Updated on 2026-05-19

--- card-android/src/main/java/com/tangem/tangemcard/android/reader/NfcReader.kt
+++ card-android/src/main/java/com/tangem/tangemcard/android/reader/NfcReader.kt
@@ -5,7 +5,7 @@ import android.nfc.tech.IsoDep
 data class NfcReader(
         val nfcManager: NfcManager,
         val isoDep: IsoDep
-) : com.tangem.tangemcommon.reader.NfcReader {
+) : com.tangem.cardcommon.reader.NfcReader {
     override fun getId(): ByteArray {
         return isoDep.tag.id
     }
