18d434b2c0af92c1625b39daba66fef1d8a23d68
Tangem <git@tangem.com>
2019-02-27 22:22:06 +0300

    Updated on 2025-10-09

--- app/src/main/java/com/tangem/presentation/activity/PurgeActivity.kt
+++ app/src/main/java/com/tangem/presentation/activity/PurgeActivity.kt
@@ -24,9 +24,9 @@ import com.tangem.tangemcard.android.nfc.NfcLifecycleObserver
 import com.tangem.tangemcard.android.reader.NfcManager
 import com.tangem.tangemcard.android.reader.NfcReader
 import com.tangem.tangemcard.data.asBundle
-import com.tangem.tangemcommon.reader.CardProtocol
-import com.tangem.tangemcommon.tasks.PurgeTask
-import com.tangem.tangemcommon.util.Util
+import com.tangem.cardcommon.reader.CardProtocol
+import com.tangem.cardcommon.tasks.PurgeTask
+import com.tangem.cardcommon.util.Util
 import com.tangem.util.LOG
 import com.tangem.wallet.R
 import kotlinx.android.synthetic.main.activity_purge.*
