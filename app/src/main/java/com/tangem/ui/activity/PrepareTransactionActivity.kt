3d635c5eb4e261a1d64a8deedf25ef95b2ff9b8b
Tangem <git@tangem.com>
2019-02-28 11:26:36 +0300

    Updated on 2026-07-07

--- app/src/main/java/com/tangem/ui/activity/PrepareTransactionActivity.kt
+++ app/src/main/java/com/tangem/ui/activity/PrepareTransactionActivity.kt
@@ -19,8 +19,8 @@ import com.tangem.data.Blockchain
 import com.tangem.di.Navigator
 import com.tangem.domain.wallet.CoinEngineFactory
 import com.tangem.domain.wallet.TangemContext
-import com.tangem.cardandroid.android.nfc.NfcLifecycleObserver
-import com.tangem.cardandroid.android.reader.NfcManager
+import com.tangem.card_android.android.nfc.NfcLifecycleObserver
+import com.tangem.card_android.android.reader.NfcManager
 import com.tangem.util.UtilHelper
 import com.tangem.wallet.R
 import kotlinx.android.synthetic.main.activity_prepare_transaction.*
