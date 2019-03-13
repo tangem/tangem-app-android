f8aaf64fb68beda85b4edd541110f41a77ddaa7a
Tangem <git@tangem.com>
2019-03-13 18:07:13 +0300

    Updated on 2026-09-25

--- app/src/main/java/com/tangem/domain/wallet/ltc/LitecoinNode.kt
+++ app/src/main/java/com/tangem/domain/wallet/ltc/LitecoinNode.kt
@@ -4,8 +4,7 @@ enum class LitecoinNode(val host: String, val port: Int, val proto: String) {
 	N_001("backup.electrum-ltc.org", 443, "ssl"),
 	N_002("electrum-ltc.petrkr.net", 60002, "ssl"),
 	N_003("electrum-ltc.bysh.me", 50002, "ssl"),
-	N_004("e-3.claudioboxx.com", 50004, "ssl"),
-	N_005("e-1.claudioboxx.com", 50004, "ssl"),
-	N_006("e-2.claudioboxx.com", 50004, "ssl"),
-	N_007("electrum.ltc.xurious.com", 50002, "ssl"),
+	N_004("e-1.claudioboxx.com", 50004, "ssl"),
+	N_005("e-2.claudioboxx.com", 50004, "ssl"),
+	N_006("electrum.ltc.xurious.com", 50002, "ssl"),
 }
