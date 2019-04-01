96064b62790115a00ec7ca240efe68f0fceacc6a
Tangem <git@tangem.com>
2019-04-01 12:51:29 +0300

    Updated on 2026-03-25

--- ripple-core/src/main/java/com/ripple/core/types/shamap/TransactionResultVisitor.java
+++
@@ -1,6 +0,0 @@
-package com.ripple.core.types.shamap;
-import com.ripple.core.types.known.tx.result.TransactionResult;
-
-public interface TransactionResultVisitor {
-    public void onTransaction(TransactionResult tx);
-}
