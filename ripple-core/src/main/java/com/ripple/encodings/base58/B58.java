3c3d9395519b577d867faea3290dda07682b50b2
Tangem <git@tangem.com>
2019-04-01 12:51:29 +0300

    Updated on 2026-06-27

--- ripple-core/src/main/java/com/ripple/encodings/base58/B58.java
+++
@@ -1,9 +0,0 @@
-package com.ripple.encodings.base58;
-
-import com.ripple.encodings.basex.BaseX;
-
-public class B58 extends BaseX {
-    public B58(String alphabet) {
-        super(alphabet);
-    }
-}
