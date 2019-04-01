8d34e080627a45a01ff2cc189e9fbc1a2080278a
Tangem <git@tangem.com>
2019-04-01 12:51:29 +0300

    Updated on 2026-06-05

--- ripple-core/src/main/java/com/ripple/encodings/basex/EncodingFormatException.java
+++
@@ -1,7 +0,0 @@
-package com.ripple.encodings.basex;
-
-public class EncodingFormatException extends RuntimeException{
-    public EncodingFormatException(String message) {
-        super(message);
-    }
-}
