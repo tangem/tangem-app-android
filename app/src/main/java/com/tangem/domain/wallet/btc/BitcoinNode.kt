29acc7439b40192e69d8a5ba713abc2130c56139
Tangem <git@tangem.com>
2019-04-04 15:16:08 +0300

    Updated on 2026-09-25

--- app/src/main/java/com/tangem/domain/wallet/btc/BitcoinNode.kt
+++ app/src/main/java/com/tangem/domain/wallet/btc/BitcoinNode.kt
@@ -1,17 +1,11 @@
 package com.tangem.domain.wallet.btc
 
 enum class BitcoinNode(val host: String, val port: Int, val proto: String) {
-    N_001("electrum.coinop.cc", 50002, "ssl"),
-    N_002("dedi.jochen-hoenicke.de", 50002, "ssl"),
-    N_003("technetium.network", 50002, "ssl"),
-    N_004("electrum.coinucopia.io", 50002, "ssl"),
-    N_005("dimon.trimon.de", 50002, "ssl"),
-    N_006("btc.gravitech.net", 50002, "ssl"),
-    N_007("fn.48.org", 50002, "ssl"),
-    N_008("vps.hsmiths.com", 50002, "ssl"),
-    N_009("electrum.qtornado.com", 50002, "ssl"),
-    N_010("electrum2.eff.ro", 50002, "ssl"),
-    N_011("electrum.hsmiths.com", 995, "ssl"),
-    N_012("electrum.hsmiths.com", 50002, "ssl"),
-    N_013("tardis.bauerj.eu", 50002, "ssl"),
+    N_001("dedi.jochen-hoenicke.de", 50002, "ssl"),
+    N_002("technetium.network", 50002, "ssl"),
+    N_003("btc.gravitech.net", 50002, "ssl"),
+    N_004("fn.48.org", 50002, "ssl"),
+    N_005("vps.hsmiths.com", 50002, "ssl"),
+    N_006("electrum.hsmiths.com", 995, "ssl"),
+    N_007("electrum.hsmiths.com", 50002, "ssl"),
 }
