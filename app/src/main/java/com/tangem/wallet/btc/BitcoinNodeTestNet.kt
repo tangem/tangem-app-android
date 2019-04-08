7e34fc8e13ff67c39a00e33e35ebdd584963b775
Tangem <git@tangem.com>
2019-04-08 15:05:55 +0300

    Updated on 2026-06-01

---
+++ app/src/main/java/com/tangem/wallet/btc/BitcoinNodeTestNet.kt
@@ -0,0 +1,8 @@
+package com.tangem.wallet.btc
+
+enum class BitcoinNodeTestNet(val host: String, val port: Int) {
+    n1("testnetnode.arihanc.com", 51001),
+    n2("testnet.hsmiths.com", 53011),
+    n3("testnet.qtornado.com", 51001),
+    n4("testnet1.bauerj.eu", 50001),
+}
