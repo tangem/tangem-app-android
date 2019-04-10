30332466538999f2b1ecb292b81ee3a11b311a50
Tangem <git@tangem.com>
2019-04-10 12:49:54 +0300

    Updated on 2026-01-24

--- app/src/main/java/com/tangem/di/ToastHelperComponent.kt
+++ app/src/main/java/com/tangem/di/ToastHelperComponent.kt
@@ -1,6 +1,8 @@
 package com.tangem.di
 
 import com.tangem.ui.PrepareTransactionActivity
+import com.tangem.ui.activity.EmptyWalletActivity
+import com.tangem.ui.activity.LoadedWalletActivity
 import com.tangem.ui.activity.MainActivity
 import dagger.Component
 import javax.inject.Singleton
@@ -11,6 +13,10 @@ interface ToastHelperComponent {
 
     fun inject(activity: MainActivity)
 
+    fun inject(activity: LoadedWalletActivity)
+
     fun inject(activity: PrepareTransactionActivity)
+
+    fun inject(activity: EmptyWalletActivity)
 
 }
