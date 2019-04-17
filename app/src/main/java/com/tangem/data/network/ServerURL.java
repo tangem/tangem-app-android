ef75cfed25d5b2f4e2364c1de5c68a329e789df4
Tangem <git@tangem.com>
2019-04-17 18:04:03 +0300

    Updated on 2026-08-14

--- app/src/main/java/com/tangem/data/network/ServerURL.java
+++ app/src/main/java/com/tangem/data/network/ServerURL.java
@@ -7,4 +7,6 @@ class ServerURL {
     static final String API_ESTIMATEFEE = "https://estimatefee.com/";
     static final String API_UPDATE_VERSION = "https://raw.githubusercontent.com/";
     static final String API_ROOTSTOCK = "https://public-node.rsk.co/";
+    static final String API_BINANCE = "https://testnet-dex.binance.org/";
+    static final String API_BLOCKCYPHER = "https://api.blockcypher.com/";
 }
