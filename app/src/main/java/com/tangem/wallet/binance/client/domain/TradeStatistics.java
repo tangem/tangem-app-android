caf484df30a153c26fcb1db3f6cc4b11f177e278
Tangem <git@tangem.com>
2019-04-23 13:22:03 +0300

    Updated on 2025-12-05

---
+++ app/src/main/java/com/tangem/wallet/binance/client/domain/TradeStatistics.java
@@ -0,0 +1,8 @@
+package com.tangem.wallet.binance.client.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TradeStatistics {
+
+}
