b8bd15156811809d322525c59547261b45f742f9
Tangem <git@tangem.com>
2019-07-03 15:18:42 +0300

    Updated on 2026-06-10

---
+++ app/src/main/java/com/tangem/wallet/EOS/EosPushTransactionRequest.java
@@ -0,0 +1,8 @@
+package com.tangem.wallet.EOS;
+
+import io.jafka.jeos.core.request.chain.transaction.PushTransactionRequest;
+
+import java.io.Serializable;
+
+public class EosPushTransactionRequest extends PushTransactionRequest implements Serializable {
+}
