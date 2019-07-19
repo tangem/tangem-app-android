05c3357ae6d6a8adff2c04901566a163574bbe23
Tangem <git@tangem.com>
2019-07-19 13:58:16 +0300

    Updated on 2025-12-22

--- app/src/main/java/com/tangem/data/network/ServerApiEos.java
+++ app/src/main/java/com/tangem/data/network/ServerApiEos.java
@@ -2,11 +2,16 @@ package com.tangem.data.network;
 
 import android.util.Log;
 
-import com.tangem.wallet.CoinEngine;
+import com.tangem.wallet.eos.EosApiPush;
+import com.tangem.wallet.eos.EosPushTransactionRequest;
 
 import io.jafka.jeos.EosApi;
 import io.jafka.jeos.EosApiFactory;
+import io.jafka.jeos.core.request.chain.transaction.PushTransactionRequest;
 import io.jafka.jeos.core.response.chain.account.Account;
+import io.jafka.jeos.core.response.chain.transaction.PushedTransaction;
+import io.jafka.jeos.impl.EosApiServiceGenerator;
+import io.jafka.jeos.impl.EosChainApiService;
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -25,6 +30,19 @@ public class ServerApiEos {
                 .observeOn(AndroidSchedulers.mainThread());
 
         accountObservable.subscribe(accountObserver);
+    }
+
+    public static void sendTransaction(EosPushTransactionRequest req, Observer<PushedTransaction> sendObserver) {
+        Log.i(TAG, "new getBalance request");
+//        EosApi eosApi = EosApiFactory.create("https://api.eosdetroit.io:443"); //TODO: add random server request
+        EosApiPush eosApiPush = EosApiServiceGenerator.createService(EosApiPush.class, "https://api.eosdetroit.io:443"); //TODO: add random server request
+
+        Observable<PushedTransaction> sendObservable = Observable.just(new PushedTransaction())
+                .map(pushedTransaction -> EosApiServiceGenerator.executeSync(eosApiPush.pushTransaction(req)))
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+
+        sendObservable.subscribe(sendObserver);
     }
 
 }
