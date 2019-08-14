eb8371e540b430adf3ad2829621f0087aab3c11b
Tangem <git@tangem.com>
2019-08-14 16:12:30 +0300

    Updated on 2026-06-01

--- app/src/main/java/com/tangem/wallet/xlm/XlmEngine.java
+++ app/src/main/java/com/tangem/wallet/xlm/XlmEngine.java
@@ -88,13 +88,6 @@ public class XlmEngine extends CoinEngine {
     }
 
     @Override
-    public String getOfflineBalanceHTML() {
-        InternalAmount offlineInternalAmount = convertToInternalAmount(ctx.getCard().getOfflineBalance());
-        Amount offlineAmount = convertToAmount(offlineInternalAmount);
-        return offlineAmount.toDescriptionString(getDecimals());
-    }
-
-    @Override
     public boolean isBalanceNotZero() {
         if (coinData == null) return false;
         if (coinData.getBalance() == null) return false;
