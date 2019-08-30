ae24f58a353dda9f6a7a86bb47adb1879f74266d
Tangem <git@tangem.com>
2019-08-30 13:05:04 +0300

    Updated on 2025-11-18

--- app/src/main/java/com/tangem/wallet/bch/BtcCashEngine.java
+++ app/src/main/java/com/tangem/wallet/bch/BtcCashEngine.java
@@ -4,25 +4,25 @@ import android.net.Uri;
 import android.text.InputFilter;
 import android.util.Log;
 
+import com.tangem.card_common.data.TangemCard;
+import com.tangem.card_common.reader.CardProtocol;
+import com.tangem.card_common.tasks.SignTask;
+import com.tangem.card_common.util.Util;
 import com.tangem.data.network.ElectrumRequest;
 import com.tangem.data.network.ServerApiElectrum;
-import com.tangem.wallet.BCHUtils;
-import com.tangem.wallet.BTCUtils;
-import com.tangem.card_common.reader.CardProtocol;
-import com.tangem.wallet.BalanceValidator;
-import com.tangem.wallet.btc.BtcData;
-import com.tangem.wallet.CoinData;
-import com.tangem.wallet.CoinEngine;
-import com.tangem.card_common.data.TangemCard;
-import com.tangem.wallet.TangemContext;
-import com.tangem.wallet.Transaction;
-import com.tangem.wallet.UnspentOutputInfo;
-import com.tangem.card_common.tasks.SignTask;
 import com.tangem.util.CryptoUtil;
 import com.tangem.util.DecimalDigitsInputFilter;
 import com.tangem.util.DerEncodingUtil;
-import com.tangem.card_common.util.Util;
+import com.tangem.wallet.BCHUtils;
+import com.tangem.wallet.BTCUtils;
+import com.tangem.wallet.BalanceValidator;
+import com.tangem.wallet.CoinData;
+import com.tangem.wallet.CoinEngine;
 import com.tangem.wallet.R;
+import com.tangem.wallet.TangemContext;
+import com.tangem.wallet.Transaction;
+import com.tangem.wallet.UnspentOutputInfo;
+import com.tangem.wallet.btc.BtcData;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -105,13 +105,13 @@ public class BtcCashEngine extends CoinEngine {
     @Override
     public boolean isExtractPossible() {
         if (!hasBalanceInfo()) {
-            ctx.setMessage(R.string.cannot_obtain_data_from_blockchain);
+            ctx.setMessage(R.string.loaded_wallet_error_obtaining_blockchain_data);
         } else if (!isBalanceNotZero()) {
-            ctx.setMessage(R.string.wallet_empty);
+            ctx.setMessage(R.string.general_wallet_empty);
         } else if (awaitingConfirmation()) {
-            ctx.setMessage(R.string.please_wait_while_previous);
+            ctx.setMessage(R.string.loaded_wallet_message_wait);
         } else if (coinData.getUnspentTransactions().size() == 0) {
-            ctx.setMessage(R.string.please_wait_for_confirmation);
+            ctx.setMessage(R.string.loaded_wallet_message_refresh);
         } else {
             return true;
         }
