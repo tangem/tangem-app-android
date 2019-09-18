4d299c6acf82e0ca5a6e0a4e45ff181b7c799830
Tangem <git@tangem.com>
2019-09-18 11:17:40 +0300

    Updated on 2026-08-19

--- app/src/main/java/com/tangem/wallet/xrp/XrpEngine.java
+++ app/src/main/java/com/tangem/wallet/xrp/XrpEngine.java
@@ -198,39 +198,39 @@ public class XrpEngine extends CoinEngine {
         try {
             if (coinData.isAccountNotFound()) {
                 balanceValidator.setScore(0);
-                balanceValidator.setFirstLine("Account not found");
-                balanceValidator.setSecondLine("Load 20+ XRP to create account");
+                balanceValidator.setFirstLine(R.string.balance_validator_first_line_account_not_found);
+                balanceValidator.setSecondLine(R.string.balance_validator_second_line_create_account_xrp);
                 return false;
             }
 
             if (((ctx.getCard().getOfflineBalance() == null) && !ctx.getCoinData().isBalanceReceived()) || (!ctx.getCoinData().isBalanceReceived() && (ctx.getCard().getRemainingSignatures() != ctx.getCard().getMaxSignatures()))) {
                 balanceValidator.setScore(0);
-                balanceValidator.setFirstLine("Unknown balance");
-                balanceValidator.setSecondLine("Balance cannot be verified. Swipe down to refresh.");
+                balanceValidator.setFirstLine(R.string.balance_validator_first_line_unknown_balance);
+                balanceValidator.setSecondLine(R.string.balance_validator_second_line_unverified_balance);
                 return false;
             }
 
             if (coinData.hasUnconfirmed()) {
                 balanceValidator.setScore(0);
-                balanceValidator.setFirstLine("Transaction in progress");
-                balanceValidator.setSecondLine("Wait for confirmation in blockchain");
+                balanceValidator.setFirstLine(R.string.balance_validator_first_line_transaction_in_progress);
+                balanceValidator.setSecondLine(R.string.balance_validator_second_line_wait_for_confirmation);
                 return false;
             }
 
             if (coinData.isBalanceReceived()) {
                 balanceValidator.setScore(100);
-                balanceValidator.setFirstLine("Verified balance");
-                balanceValidator.setSecondLine("Balance confirmed in blockchain");
+                balanceValidator.setFirstLine(R.string.balance_validator_first_line_verified_balance);
+                balanceValidator.setSecondLine(R.string.balance_validator_second_line_confirmed_in_blockchain);
                 if (coinData.getBalanceInInternalUnits().isZero()) {
-                    balanceValidator.setFirstLine("Empty wallet");
-                    balanceValidator.setSecondLine("");
+                    balanceValidator.setFirstLine(R.string.balance_validator_first_line_empty_wallet);
+                    balanceValidator.setSecondLine(R.string.empty_string);
                 }
             }
 
             if ((ctx.getCard().getOfflineBalance() != null) && !coinData.isBalanceReceived() && (ctx.getCard().getRemainingSignatures() == ctx.getCard().getMaxSignatures()) && coinData.getBalanceInInternalUnits().notZero()) {
                 balanceValidator.setScore(80);
-                balanceValidator.setFirstLine("Verified offline balance");
-                balanceValidator.setSecondLine("Can't obtain balance from blockchain. Restore internet connection to be more confident. ");
+                balanceValidator.setFirstLine(R.string.balance_validator_first_line_verified_offline);
+                balanceValidator.setSecondLine(R.string.balance_validator_second_line_internet_to_get_balance);
             }
 
             return true;
