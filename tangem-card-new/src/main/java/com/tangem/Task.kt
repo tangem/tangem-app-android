b3f949080e2456b0d86f5a48231e4b53433ef924
Tangem <git@tangem.com>
2019-10-01 11:51:31 +0300

    Updated on 2026-05-14

---
+++ tangem-card-new/src/main/java/com/tangem/Task.kt
@@ -0,0 +1,114 @@
+package com.tangem
+
+abstract class Task(protected val delegate: CardManagerDelegate? = null, private val reader: CardReader) {
+
+    abstract fun run(cardEnvironment: CardEnvironment,
+                     callback: (result: TaskResult, cardEnvironment: CardEnvironment) -> Unit)
+
+    protected fun executeCommand(command: Command, cardEnvironment: CardEnvironment,
+                                 callback: (result: TaskResult) -> Unit) {
+
+        reader.sendApdu(command.serialize(cardEnvironment).serialize()) {
+            val parsedApdu = it.deserialize()
+            if (parsedApdu == null) callback.invoke(TaskResult.Error(CardError()))
+            else if (!parsedApdu.statusCompleted()) {
+                callback.invoke(TaskResult.Error(CardError(parsedApdu.status.code)))
+            } else {
+                val response = command.deserialize(parsedApdu)
+                callback.invoke(response)
+            }
+        }
+    }
+}
+
+
+class BasicTask(
+        private val command: Command,
+        delegate: CardManagerDelegate? = null,
+        reader: CardReader) : Task(delegate, reader) {
+
+    override fun run(cardEnvironment: CardEnvironment,
+                     callback: (result: TaskResult, cardEnvironment: CardEnvironment) -> Unit) {
+        executeCommand(command, cardEnvironment) {
+            callback.invoke(it, updateEnvironment())
+        }
+    }
+
+    private fun updateEnvironment() : CardEnvironment{
+        //TODO: set logic of changing CardEnvironment when needed
+        return CardEnvironment()
+    }
+}
+
+
+class SignTask(private val signCommandData: SignCommandData,
+               delegate: CardManagerDelegate? = null,
+               reader: CardReader) : Task(delegate, reader) {
+
+    private lateinit var callback: (result: TaskResult, cardEnvironment: CardEnvironment) -> Unit
+
+    override fun run(cardEnvironment: CardEnvironment,
+                     callback: (result: TaskResult, cardEnvironment: CardEnvironment) -> Unit) {
+
+        this.callback = callback
+
+        delegate?.openNfcPopup()
+        val signCommand = SignCommand(signCommandData)
+
+        executeCommand(signCommand, cardEnvironment) {
+            delegate?.closeNfcPopup()
+            run { callback(it, updateEnvironment()) }
+        }
+    }
+
+    private fun updateEnvironment() : CardEnvironment{
+        //TODO: set logic of changing CardEnvironment when needed
+        return CardEnvironment()
+    }
+}
+
+
+class ScanTask(delegate: CardManagerDelegate? = null, reader: CardReader) : Task(delegate, reader) {
+
+    private lateinit var card: Card
+
+    override fun run(cardEnvironment: CardEnvironment,
+                     callback: (result: TaskResult, cardEnvironment: CardEnvironment) -> Unit) {
+
+        delegate?.openNfcPopup()
+        val readCommand = ReadCardCommand(cardEnvironment.pin1)
+        executeCommand(readCommand, cardEnvironment) {
+
+            if (it is TaskResult.Success) {
+                card = it.resultData as Card
+                val checkWalletCommand = prepareCheckWalletCommand(cardEnvironment)
+
+                executeCommand(checkWalletCommand, cardEnvironment) {
+                    delegate?.closeNfcPopup()
+                    //TODO: pass check wallet response data
+                    run { callback(it, updateEnvironment()) }
+                }
+            } else {
+                //TODO: Handle error
+            }
+        }
+    }
+
+    private fun updateEnvironment() : CardEnvironment{
+        //TODO: set logic of changing CardEnvironment when needed
+        return CardEnvironment()
+    }
+
+    private fun prepareCheckWalletCommand(cardEnvironment: CardEnvironment): CheckWalletCommand {
+        val challenge = generateChallenge()
+        return CheckWalletCommand(
+                cardEnvironment.pin1,
+                card.cid,
+                challenge,
+                byteArrayOf())
+    }
+
+    private fun generateChallenge(): ByteArray {
+        return byteArrayOf()
+    }
+}
