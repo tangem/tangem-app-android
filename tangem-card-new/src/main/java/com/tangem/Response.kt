751b19bd8a384105c44caea87c38dfdfebe4a4b3
Tangem <git@tangem.com>
2019-10-18 18:30:41 +0300

    Updated on 2026-05-19

--- tangem-card-new/src/main/java/com/tangem/Response.kt
+++ tangem-card-new/src/main/java/com/tangem/Response.kt
@@ -1,16 +1,16 @@
 package com.tangem
-
-data class SignResponse(
-        val cid: String,
-        val signature: ByteArray,
-        val remainingSignatures: Int,
-        val signedHashes: Int
-)
-
-
-data class CardError(
-        val code: Int = 0
-)
-
+//
+//data class SignResponse(
+//        val cid: String,
+//        val signature: ByteArray,
+//        val remainingSignatures: Int,
+//        val signedHashes: Int
+//)
+//
+//
+//data class CardError(
+//        val code: Int = 0
+//)
+//
 
 
