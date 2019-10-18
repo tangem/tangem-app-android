a545f6788f29fc077e5e877a6fe0d8c412b8d0f8
Tangem <git@tangem.com>
2019-10-18 18:30:41 +0300

    Updated on 2026-05-07

--- tangem-card-new/src/main/java/com/tangem/CardManagerDelegate.kt
+++ tangem-card-new/src/main/java/com/tangem/CardManagerDelegate.kt
@@ -1,10 +1,12 @@
 package com.tangem
 
+import com.tangem.tasks.TaskError
+
 interface CardManagerDelegate {
 
     fun showSecurityDelay(seconds: Int)
     fun hideSecurityDelay()
-    fun requestPin(success: () -> String, error: (cardError: CardError) -> CardError)
+    fun requestPin(success: () -> String, error: (cardError: TaskError.CardError) -> TaskError.CardError)
 
     fun openNfcPopup()
     fun closeNfcPopup()
