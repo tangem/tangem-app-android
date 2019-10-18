15f5735dc272d264c9398424a5d0c318b4c5c305
Tangem <git@tangem.com>
2019-10-18 18:32:50 +0300

    Updated on 2026-09-25

---
+++ tangem-card-new/src/main/java/com/tangem/common/apdu/ResponseApdu.kt
@@ -0,0 +1,37 @@
+package com.tangem.common.apdu
+
+import com.tangem.common.tlv.Tlv
+import com.tangem.enums.Status
+
+class ResponseApdu(val data: ByteArray) {
+
+    val sw1: Int = 0x00FF and data[data.size - 2].toInt()
+    val sw2: Int = 0x00FF and data[data.size - 1].toInt()
+
+    val sw: Int = sw1 shl 8 or sw2
+
+    val status: Status = Status.byCode(sw)
+
+    fun getTlvData(encryptionKey: ByteArray? = null): List<Tlv>? {
+        val tlvs = when {
+            data.size < 2 -> null
+            data.size == 2 -> emptyList()
+            else -> Tlv.fromBytes(data.copyOf(data.size - 2))
+        }
+        return flattenNestedTlvs(tlvs)
+    }
+
+    private fun flattenNestedTlvs(tlvs: List<Tlv>?): List<Tlv>? =
+            tlvs?.flatMap {
+                if (it.tag.hasNestedTlv()) {
+                    Tlv.fromBytes(it.value)
+                } else {
+                    listOf(it)
+                }
+            }
+
+    private fun decrypt(encryptionKey: ByteArray) {
+
+    }
+
+}
