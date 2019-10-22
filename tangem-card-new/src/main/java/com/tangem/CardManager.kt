e94ebd081e0197e4fe4dcc933a0824d88959d2f5
Tangem <git@tangem.com>
2019-10-22 10:59:05 +0300

    Updated on 2026-07-23

--- tangem-card-new/src/main/java/com/tangem/CardManager.kt
+++ tangem-card-new/src/main/java/com/tangem/CardManager.kt
@@ -3,7 +3,11 @@ package com.tangem
 import com.tangem.commands.CommandResponse
 import com.tangem.commands.CommandSerializer
 import com.tangem.commands.SignCommandData
-import com.tangem.tasks.*
+import com.tangem.crypto.initCrypto
+import com.tangem.tasks.ScanEvent
+import com.tangem.tasks.ScanTask
+import com.tangem.tasks.Task
+import com.tangem.tasks.TaskResult
 
 class CardManager(
         private val reader: CardReader,
@@ -12,30 +16,30 @@ class CardManager(
 
     private val cardEnvironmentRepository = CardEnvironmentRepository(dataStorage)
 
-    fun scanCard(callback: (result: TaskResult) -> Unit) {
+    init {
+        initCrypto()
+    }
+
+    fun scanCard(callback: (result: ScanEvent, cardEnvironment: CardEnvironment) -> Unit) {
         val task = ScanTask(cardManagerDelegate, reader)
         runTask(task, cardEnvironmentRepository.cardEnvironment, callback)
     }
 
     fun sign(signCommandData: SignCommandData,
              callback: (result: TaskResult) -> Unit) {
-        val task = SignTask(signCommandData, cardManagerDelegate, reader)
-        runTask(task, cardEnvironmentRepository.cardEnvironment, callback)
+//        val task = SignTask<SignEvent>(signCommandData, cardManagerDelegate, reader)
+//        runTask(task, cardEnvironmentRepository.cardEnvironment, callback)
     }
 
-    fun runTask(task: Task, environment: CardEnvironment? = null,
-                            callback: (result: TaskResult) -> Unit) {
-        task.run(environment ?: cardEnvironmentRepository.cardEnvironment) {result, returnedEnvironment ->
-            //TODO: environment is kept in Repository
-            cardEnvironmentRepository.cardEnvironment = returnedEnvironment
-            callback.invoke(result)
-        }
+    fun <TaskEvent>runTask(task: Task<TaskEvent>, environment: CardEnvironment? = null,
+                            callback: (result: TaskEvent, cardEnvironment: CardEnvironment) -> Unit) {
+        task.run(environment ?: cardEnvironmentRepository.cardEnvironment, callback)
     }
 
     fun runCommand(commandSerializer: CommandSerializer<CommandResponse>,
                    environment: CardEnvironment? = null,
                    callback: (result: TaskResult) -> Unit) {
-        val task = BasicTask<CommandResponse>(commandSerializer, cardManagerDelegate, reader)
-        runTask(task, environment, callback)
+//        val task = BasicTask<CommandResponse>(commandSerializer, cardManagerDelegate, reader)
+//        runTask(task, environment, callback)
     }
 }
