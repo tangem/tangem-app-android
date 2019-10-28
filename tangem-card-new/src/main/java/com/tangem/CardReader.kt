679a3d85647acc77c2c7e49b892cda7cf128a8d1
Tangem <git@tangem.com>
2019-10-28 17:08:42 +0300

    Updated on 2026-09-25

--- tangem-card-new/src/main/java/com/tangem/CardReader.kt
+++ tangem-card-new/src/main/java/com/tangem/CardReader.kt
@@ -1,10 +1,11 @@
 package com.tangem
 
 import com.tangem.common.CompletionResult
+import com.tangem.common.apdu.CommandApdu
 import com.tangem.common.apdu.ResponseApdu
 
 interface CardReader {
     var readingActive: Boolean
-    fun sendApdu(apduSerialized: ByteArray, callback: (response: CompletionResult<ResponseApdu>) -> Unit)
+    fun transceiveApdu(apdu: CommandApdu, callback: (response: CompletionResult<ResponseApdu>) -> Unit)
     fun setStartSession()
 }
