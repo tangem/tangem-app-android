a1335903ad17f42ea08b3f294bd72dd55020d8a0
Tangem <git@tangem.com>
2019-10-28 17:08:42 +0300

    release/5.26.1

--- tangem-card-new/src/main/java/com/tangem/commands/CommandSerializer.kt
+++ tangem-card-new/src/main/java/com/tangem/commands/CommandSerializer.kt
@@ -3,59 +3,23 @@ package com.tangem.commands
 import com.tangem.CardEnvironment
 import com.tangem.common.apdu.CommandApdu
 import com.tangem.common.apdu.ResponseApdu
+import com.tangem.common.extentions.toInt
+import com.tangem.common.tlv.TlvTag
 import com.tangem.enums.Instruction
-import com.tangem.tasks.TaskError
-
-//interface TlvMappable {
-//    fun responseFromTlv(tlvList: List<Tlv>): CommandResponse?
-//}
 
 interface CommandResponse
-//    : TlvMappable {
-//
-//}
 
 
-abstract class CommandSerializer<T : CommandResponse>() {
+abstract class CommandSerializer<T : CommandResponse> {
 
     abstract val instruction: Instruction
     abstract val instructionCode: Int
 
     abstract fun serialize(cardEnvironment: CardEnvironment): CommandApdu
     abstract fun deserialize(cardEnvironment: CardEnvironment, responseApdu: ResponseApdu): T?
+
+    fun deserializeSecurityDelay(responseApdu: ResponseApdu, cardEnvironment: CardEnvironment): Int? {
+        val tlv = responseApdu.getTlvData(cardEnvironment.encryptionKey)
+        return tlv?.find { it.tag == TlvTag.Pause }?.value?.toInt()
+    }
 }
-
-sealed class CommandEvent {
-    class Success(val response: Any) : CommandEvent()
-    class Failure(val taskError: TaskError) : CommandEvent()
-    class UserCancellation : CommandEvent()
-}
-
-//class CheckWalletResponse(
-//        val cardId: String,
-//        val salt: ByteArray,
-//        val walletSignature: ByteArray
-//) : CommandResponse, TlvMappable by CheckWalletResponse {
-//
-//    companion object : TlvMappable {
-//        override fun responseFromTlv(tlvList: List<Tlv>): CommandResponse? {
-//            val mapper = TlvMapper(tlvList)
-//
-//            return try {
-//                CheckWalletResponse(
-//                        mapper.map(TlvTag.CardId),
-//                        mapper.map(TlvTag.Salt),
-//                        mapper.map(TlvTag.Signature)
-//                )
-//            } catch (exception: TlvMapperException) {
-//                null
-//            }
-//        }
-//
-//    }
-//}
-
-
-
-
-
