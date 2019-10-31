dd273e73951d483e8c9232eecebf730025c270ac
Tangem <git@tangem.com>
2019-10-31 13:48:32 +0300

    Updated on 2025-12-30

---
+++ tangemtest/src/main/java/com/tangem/tangemtest/TestApp.kt
@@ -0,0 +1,28 @@
+package com.tangem.tangemtest
+
+import android.app.Application
+import com.tangem.Log
+import com.tangem.LoggerInterface
+
+class TestApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Log.setLogger(
+                object : LoggerInterface {
+                    override fun i(logTag: String, message: String) {
+                        android.util.Log.i(logTag, message)
+                    }
+
+                    override fun e(logTag: String, message: String) {
+                        android.util.Log.e(logTag, message)
+                    }
+
+                    override fun v(logTag: String, message: String) {
+                        android.util.Log.v(logTag, message)
+                    }
+                }
+        )
+    }
+}
