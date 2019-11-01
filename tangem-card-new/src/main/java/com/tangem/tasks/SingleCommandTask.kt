ebfadfac53793d29206ed9df2838073fe49224c8
Tangem <git@tangem.com>
2019-11-01 10:51:08 +0300

    Updated on 2026-05-13

--- tangem-card-new/src/main/java/com/tangem/tasks/SingleCommandTask.kt
+++ tangem-card-new/src/main/java/com/tangem/tasks/SingleCommandTask.kt
@@ -3,6 +3,7 @@ package com.tangem.tasks
 import com.tangem.CardEnvironment
 import com.tangem.commands.CommandResponse
 import com.tangem.commands.CommandSerializer
+import com.tangem.common.CompletionResult
 
 class SingleCommandTask<Event : CommandResponse>(
         private val commandSerializer: CommandSerializer<Event>) : Task<Event>() {
@@ -10,8 +11,17 @@ class SingleCommandTask<Event : CommandResponse>(
     override fun onRun(cardEnvironment: CardEnvironment,
                        callback: (result: TaskEvent<Event>) -> Unit) {
         sendCommand(commandSerializer, cardEnvironment) {
-            delegate?.closeNfcPopup()
-            callback(it)
+            when (it) {
+                is CompletionResult.Success -> {
+                    delegate?.onTaskCompleted()
+                    callback(TaskEvent.Event(it.data))
+                    callback(TaskEvent.Completion())
+                }
+                is CompletionResult.Failure -> {
+                    if (it.error !is TaskError.UserCancelledError) delegate?.onTaskError()
+                    callback(TaskEvent.Completion(it.error))
+                }
+            }
         }
     }
 }
