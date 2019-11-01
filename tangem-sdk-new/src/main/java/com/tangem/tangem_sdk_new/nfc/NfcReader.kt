f8416019b484dddbfd1bf86177d69a9b530edf07
Tangem <git@tangem.com>
2019-11-01 10:53:51 +0300

    Updated on 2026-07-07

--- tangem-sdk-new/src/main/java/com/tangem/tangem_sdk_new/nfc/NfcReader.kt
+++ tangem-sdk-new/src/main/java/com/tangem/tangem_sdk_new/nfc/NfcReader.kt
@@ -1,4 +1,4 @@
-package com.tangem.tangem_sdk_new
+package com.tangem.tangem_sdk_new.nfc
 
 import android.nfc.Tag
 import android.nfc.tech.IsoDep
@@ -11,19 +11,29 @@ import com.tangem.tasks.TaskError
 
 class NfcReader : CardReader {
 
+    override var readingActive = false
+    var nfcEnabled = false
     var isoDep: IsoDep? = null
         set(value) {
+            // if tag is received, call connect first before transceiving data
             if (field == null) {
                 field = value
                 connect()
             }
+            // don't reassign when there's an active tag already
+            if (value == null) field = value
         }
-    override var readingActive = false
+
     var readingCancelled = false
-    set(value) {
-        if (value) readingActive = false
-        field = value
-    }
+        set(value) {
+            field = value
+            if (value) {
+                // Stops reading and sends failure callback to a task
+                // if reading is cancelled (when user closes nfc bottom sheet dialog).
+                readingActive = false
+                callback?.invoke(CompletionResult.Failure(TaskError.UserCancelledError()))
+            }
+        }
 
     var data: ByteArray? = null
     var callback: ((response: CompletionResult<ResponseApdu>) -> Unit)? = null
@@ -33,15 +43,16 @@ class NfcReader : CardReader {
         readingCancelled = false
     }
 
+    override fun closeSession() {
+        isoDep = null
+        readingActive = false
+    }
+
     override fun transceiveApdu(apdu: CommandApdu, callback: (response: CompletionResult<ResponseApdu>) -> Unit) {
         data = apdu.apduData
         this.callback = callback
         if (isoDep != null) {
-            try {
-                transceiveData()
-            } catch (exception: Exception) {
-                isoDep = null
-            }
+            transceiveData()
         }
     }
 
@@ -50,11 +61,18 @@ class NfcReader : CardReader {
             callback?.invoke(CompletionResult.Failure(TaskError.UserCancelledError()))
             return
         }
-        val rawResponse = isoDep?.transceive(data)
+        if (data == null) return
+
+        val rawResponse: ByteArray?
+        try {
+            rawResponse = isoDep?.transceive(data)
+        } catch (exception: Exception) {
+            isoDep = null
+            return
+        }
         if (rawResponse != null) {
             Log.i(this::class.simpleName!!, "Nfc response is received")
             data = null
-            readingActive = false
         }
         rawResponse?.let { callback?.invoke(CompletionResult.Success(ResponseApdu(it))) }
     }
@@ -69,7 +87,7 @@ class NfcReader : CardReader {
         isoDep?.connect()
         isoDep?.close()
         isoDep?.connect()
-        isoDep?.timeout = 120000
+        isoDep?.timeout = 240000
         Log.i(this::class.simpleName!!, "Nfc session is started")
 
     }
