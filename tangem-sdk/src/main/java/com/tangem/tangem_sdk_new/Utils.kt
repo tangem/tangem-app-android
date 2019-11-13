c936648f23a3c60985ed177b0cf05e311e38f661
Tangem <git@tangem.com>
2019-11-13 14:33:07 +0300

    Updated on 2026-09-25

--- tangem-sdk/src/main/java/com/tangem/tangem_sdk_new/Utils.kt
+++ tangem-sdk/src/main/java/com/tangem/tangem_sdk_new/Utils.kt
@@ -5,6 +5,6 @@ import android.os.Looper
 
 val uiHandler = Handler(Looper.getMainLooper())
 
- internal fun postUI(msTime: Long = 0, func: () -> Unit) {
+internal fun postUI(msTime: Long = 0, func: () -> Unit) {
     if (msTime > 0) uiHandler.postDelayed({ func() }, msTime) else uiHandler.post(func)
 }
