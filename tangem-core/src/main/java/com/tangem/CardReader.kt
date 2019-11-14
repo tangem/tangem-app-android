15894d256ec90c04b555baa61967e21b4cb32468
Tangem <git@tangem.com>
2019-11-14 12:00:59 +0300

    Updated on 2026-07-23

--- tangem-core/src/main/java/com/tangem/CardReader.kt
+++ tangem-core/src/main/java/com/tangem/CardReader.kt
@@ -4,9 +4,35 @@ import com.tangem.common.CompletionResult
 import com.tangem.common.apdu.CommandApdu
 import com.tangem.common.apdu.ResponseApdu
 
+/**
+ * Allows interaction between the phone or any other terminal and Tangem card.
+ *
+ * Its default implementation, NfcCardReader, is in our tangem-sdk module.
+ */
 interface CardReader {
+
+    /**
+     * [com.tangem.tasks.Task] sets it to true before the first command,
+     * and it should be set to false on completion of the task.
+     */
     var readingActive: Boolean
+
+    /**
+     * Sends data to the card and receives the reply.
+     *
+     * @param apdu data to be sent. [CommandApdu] serializes it to a [ByteArray]
+     * @param callback returns response from the card,
+     * [ResponseApdu] allows to convert raw data to [Tlv]
+     */
     fun transceiveApdu(apdu: CommandApdu, callback: (response: CompletionResult<ResponseApdu>) -> Unit)
+
+    /**
+     * Signals to [CardReader] to become ready to transceive data.
+     */
     fun startNfcSession()
+
+    /**
+     * Signals to [CardReader] that no further NFC transition is expected.
+     */
     fun closeSession()
 }
