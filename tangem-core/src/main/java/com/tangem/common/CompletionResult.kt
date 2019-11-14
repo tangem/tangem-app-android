7d6f5996653621587b1bcfcfb54cc100ef32325d
Tangem <git@tangem.com>
2019-11-14 17:19:15 +0300

    Updated on 2025-11-03

--- tangem-core/src/main/java/com/tangem/common/CompletionResult.kt
+++ tangem-core/src/main/java/com/tangem/common/CompletionResult.kt
@@ -5,7 +5,7 @@ import com.tangem.tasks.TaskError
 
 /**
  * Response class encapsulating successful and failed results.
- * [T] is a type of data that is returned in [Success].
+ * @param T Type of data that is returned in [Success].
  */
 sealed class CompletionResult<T> {
     class Success<T>(val data: T) : CompletionResult<T>()
