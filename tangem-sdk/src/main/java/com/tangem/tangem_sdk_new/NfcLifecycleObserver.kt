7cdf4132008e719cf4ddc552b84bd4c5f5f6bc49
Tangem <git@tangem.com>
2019-11-14 12:00:59 +0300

    Updated on 2026-06-12

--- tangem-sdk/src/main/java/com/tangem/tangem_sdk_new/NfcLifecycleObserver.kt
+++ tangem-sdk/src/main/java/com/tangem/tangem_sdk_new/NfcLifecycleObserver.kt
@@ -6,7 +6,9 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.OnLifecycleEvent
 import com.tangem.tangem_sdk_new.nfc.NfcManager
 
-
+/**
+ * [LifecycleObserver] for [NfcManager], helps to coordinate NFC modes with Activity lifecycle.
+ */
 class NfcLifecycleObserver(private var nfcManager: NfcManager) : LifecycleObserver {
 
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
