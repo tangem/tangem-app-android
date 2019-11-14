e51ed137576f4ada9a0f35a8f4c0085561a4531e
Tangem <git@tangem.com>
2019-11-14 12:00:59 +0300

    Updated on 2026-05-14

--- tangem-core/src/main/java/com/tangem/commands/CommandSerializer.kt
+++ tangem-core/src/main/java/com/tangem/commands/CommandSerializer.kt
@@ -2,22 +2,41 @@ package com.tangem.commands
 
 import com.tangem.CardEnvironment
 import com.tangem.common.apdu.CommandApdu
-import com.tangem.common.apdu.Instruction
 import com.tangem.common.apdu.ResponseApdu
 import com.tangem.common.extentions.toInt
 import com.tangem.common.tlv.TlvTag
 
+/**
+ * Simple interface for responses received after sending commands to Tangem cards.
+ */
 interface CommandResponse
 
-
+/**
+ * Abstract class for all Tangem card commands.
+ */
 abstract class CommandSerializer<T : CommandResponse> {
 
-    abstract val instruction: Instruction
-    abstract val instructionCode: Int
-
+    /**
+     * Serializes data into a [List] of [com.tangem.common.tlv.Tlv],
+     * then creates [CommandApdu] with this data.
+     *
+     * @return command data that can be converted to raw bytes with a method [CommandApdu.toBytes].
+     */
     abstract fun serialize(cardEnvironment: CardEnvironment): CommandApdu
+
+    /**
+     * Deserializes data, received from a card and stored in [ResponseApdu],
+     * into a [List] of [com.tangem.common.tlv.Tlv]. Then this method maps it into a [CommandResponse].
+     *
+     * @return card response, converted to a [CommandResponse] of a type [T].
+     */
     abstract fun deserialize(cardEnvironment: CardEnvironment, responseApdu: ResponseApdu): T?
 
+    /**
+     * Helper method to parse security delay information received from a card.
+     *
+     * @return Remaining security delay in milliseconds.
+     */
     fun deserializeSecurityDelay(responseApdu: ResponseApdu, cardEnvironment: CardEnvironment): Int? {
         val tlv = responseApdu.getTlvData(cardEnvironment.encryptionKey)
         return tlv?.find { it.tag == TlvTag.Pause }?.value?.toInt()
