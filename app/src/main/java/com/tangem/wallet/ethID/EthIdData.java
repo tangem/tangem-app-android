d42d604f8e0f092b2f7622eb928f6466a92fb3b7
Tangem <git@tangem.com>
2020-01-20 12:48:05 +0300

    Updated on 2026-04-06

--- app/src/main/java/com/tangem/wallet/ethID/EthIdData.java
+++ app/src/main/java/com/tangem/wallet/ethID/EthIdData.java
@@ -1,11 +1,14 @@
 package com.tangem.wallet.ethID;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import com.tangem.wallet.CoinData;
 
 public class EthIdData extends CoinData {
     private boolean hasApprovalTx = false;
-    private long approvalAddressNonce;
-    private byte[] CKDpub;
+    private Long approvalAddressNonce;
+    private byte[] CKDpub = null;
 
     public boolean isHasApprovalTx() {
         return hasApprovalTx;
@@ -29,5 +32,37 @@ public class EthIdData extends CoinData {
 
     public void setCKDpub(byte[] CKDpub) {
         this.CKDpub = CKDpub;
+    }
+
+    @Override
+    public void clearInfo() {
+        super.clearInfo();
+        hasApprovalTx = false;
+        approvalAddressNonce = null;
+    }
+
+    @Override
+    public void loadFromBundle(Bundle B) {
+        super.loadFromBundle(B);
+
+        if (B.containsKey("ApprovalAddressNonce")) {
+            approvalAddressNonce = B.getLong("ApprovalAddressNonce");
+        }
+        if (B.containsKey("CKDpub")) CKDpub = B.getByteArray("CKDpub");
+        hasApprovalTx = B.getBoolean("HasApprovalTx");
+    }
+
+    @Override
+    public void saveToBundle(Bundle B) {
+        super.saveToBundle(B);
+        try {
+            if (approvalAddressNonce != null) B.putLong("ApprovalAddressNonce", approvalAddressNonce);
+            if (CKDpub != null) B.putByteArray("CKDpub", CKDpub);
+            B.putBoolean("HasApprovalTx", hasApprovalTx);
+
+        } catch (Exception e) {
+            Log.e("Can't save to bundle ", e.getMessage());
+        }
+
     }
 }
