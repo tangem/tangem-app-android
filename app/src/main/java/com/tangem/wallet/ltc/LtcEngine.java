3aa8f2d72aeb35f58a2324e17d37aabfbb02de1b
Tangem <git@tangem.com>
2020-01-22 14:54:03 +0300

    Updated on 2025-10-09

--- app/src/main/java/com/tangem/wallet/ltc/LtcEngine.java
+++ app/src/main/java/com/tangem/wallet/ltc/LtcEngine.java
@@ -275,11 +275,11 @@ public class LtcEngine extends BtcEngine {
 //            return;
 //        }
 
-        if ((ctx.getCard().getOfflineBalance() != null) && !coinData.isBalanceReceived() && (ctx.getCard().getRemainingSignatures() == ctx.getCard().getMaxSignatures()) && coinData.getBalanceInInternalUnits().notZero()) {
-            balanceValidator.setScore(80);
-            balanceValidator.setFirstLine(R.string.balance_validator_first_line_verified_offline);
-            balanceValidator.setSecondLine(R.string.balance_validator_second_line_internet_to_verify_online);
-        }
+//        if ((ctx.getCard().getOfflineBalance() != null) && !coinData.isBalanceReceived() && ctx.getCard().getRemainingSignatures() == ctx.getCard().getMaxSignatures()) {
+//            balanceValidator.setScore(80);
+//            balanceValidator.setFirstLine(R.string.balance_validator_first_line_verified_offline);
+//            balanceValidator.setSecondLine(R.string.balance_validator_second_line_internet_to_verify_online);
+//        }
 
 //            if(card.getFailedBalanceRequestCounter()!=0) {
 //                score -= 5 * card.getFailedBalanceRequestCounter();
