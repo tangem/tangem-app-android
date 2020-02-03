90592d685886dde0781118634c614f7dbdeda970
Tangem <git@tangem.com>
2020-02-03 18:21:16 +0300

    Updated on 2025-12-22

--- blockchain/src/main/java/com/tangem/blockchain/bitcoin/network/api/BlockcypherApi.kt
+++ blockchain/src/main/java/com/tangem/blockchain/bitcoin/network/api/BlockcypherApi.kt
@@ -1,6 +1,6 @@
 package com.tangem.blockchain.bitcoin.network.api
 
-import com.tangem.blockchain.bitcoin.network.response.BlockcypherBody
+import com.squareup.moshi.JsonClass
 import com.tangem.blockchain.bitcoin.network.response.BlockcypherFee
 import com.tangem.blockchain.bitcoin.network.response.BlockcypherResponse
 import com.tangem.blockchain.bitcoin.network.response.BlockcypherTx
@@ -8,20 +8,20 @@ import retrofit2.http.*
 
 interface BlockcypherApi {
     @GET("v1/{blockchain}/{network}")
-    fun getFee(
+    suspend fun getFee(
             @Path("blockchain") blockchain: String,
             @Path("network") network: String
     ): BlockcypherFee
 
     @GET("v1/{blockchain}/{network}/addrs/{address}?unspentOnly=true&includeScript=true")
-    fun getAddressData(
+    suspend fun getAddressData(
             @Path("blockchain") blockchain: String,
             @Path("network") network: String,
             @Path("address") address: String
     ): BlockcypherResponse
 
     @GET("v1/{blockchain}/{network}/txs/{txHash}?includeHex=true")
-    fun getTransactions(
+    suspend fun getTransactions(
             @Path("blockchain") blockchain: String,
             @Path("network") network: String,
             @Path("txHash") txHash: String
@@ -29,10 +29,13 @@ interface BlockcypherApi {
 
     @Headers("Content-Type: application/json")
     @POST("v1/{blockchain}/{network}/txs/push")
-    fun sendTransaction(
+    suspend fun sendTransaction(
             @Path("blockchain") blockchain: String,
             @Path("network") network: String,
             @Body blockcypherBody: BlockcypherBody,
             @Query("token") token: String
-    ): BlockcypherResponse
+    ): BlockcypherTx
 }
+
+@JsonClass(generateAdapter = true)
+data class BlockcypherBody(val tx: String)
