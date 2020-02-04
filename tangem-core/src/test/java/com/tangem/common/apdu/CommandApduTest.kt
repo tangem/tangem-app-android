6c1ceb7d11be6acc58b3badcda2c43691dc4fe17
Tangem <git@tangem.com>
2020-02-04 15:26:20 +0300

    Updated on 2026-05-19

--- tangem-core/src/test/java/com/tangem/common/apdu/CommandApduTest.kt
+++ tangem-core/src/test/java/com/tangem/common/apdu/CommandApduTest.kt
@@ -2,7 +2,9 @@ package com.tangem.common.apdu
 
 import com.google.common.truth.Truth.assertThat
 import com.tangem.common.extensions.calculateSha256
+import com.tangem.common.extensions.toHexString
 import com.tangem.common.tlv.Tlv
+import com.tangem.common.tlv.TlvBuilder
 import com.tangem.common.tlv.TlvTag
 import org.junit.Test
 
@@ -11,12 +13,13 @@ class CommandApduTest {
 
     @Test
     fun `simple READ command to bytes`() {
-        val pinInBytes = byteArrayOf(-111, -76, -47, 66, -126, 63, 125, 32, -59, -16, -115, -10, -111,
-                34, -34, 67, -13, 95, 5, 122, -104, -115, -106, 25, -10, -45, 19, -124, -123, -55, -94, 3)
+        val pin = "000000"
+        val tlvBuilder = TlvBuilder()
+        tlvBuilder.append(TlvTag.Pin, pin)
         val commandApdu = CommandApdu(
                 Instruction.Read,
-                mutableListOf(Tlv(TlvTag.Pin, pinInBytes)))
-
+                tlvBuilder.serialize()
+        )
         val expected = byteArrayOf(0, -14, 0, 0, 0, 0, 34, 16, 32, -111, -76, -47, 66, -126, 63, 125,
             32, -59, -16, -115, -10, -111, 34, -34, 67, -13, 95, 5, 122, -104, -115, -106, 25, -10,
             -45, 19, -124, -123, -55, -94, 3)
@@ -27,17 +30,18 @@ class CommandApduTest {
 
     @Test
     fun `READ with terminal key to bytes`() {
-        val pinInBytes = byteArrayOf(-111, -76, -47, 66, -126, 63, 125, 32, -59, -16, -115, -10, -111,
-                34, -34, 67, -13, 95, 5, 122, -104, -115, -106, 25, -10, -45, 19, -124, -123, -55, -94, 3)
+        val pin = "000000"
         val terminalPublicKey = byteArrayOf(4, 80, -122, 58, -42, 74, -121, -82, -118, 47, -24, 60,
                 26, -15, -88, 64, 60, -75, 63, 83, -28, -122, -40, 81, 29, -83, -118, 4, -120, 126,
                 91, 35, 82, 44, -44, 112, 36, 52, 83, -94, -103, -6, -98, 119, 35, 119, 22, 16, 58,
                 -68, 17, -95, -33, 56, -123, 94, -42, -14, -18, 24, 126, -100, 88, 43, -90)
+        val tlvBuilder = TlvBuilder()
+        tlvBuilder.append(TlvTag.Pin, pin)
+        tlvBuilder.append(TlvTag.TerminalPublicKey, terminalPublicKey)
         val commandApdu = CommandApdu(
                 Instruction.Read,
-                mutableListOf(
-                        Tlv(TlvTag.Pin, pinInBytes),
-                        Tlv(TlvTag.TerminalPublicKey, terminalPublicKey)))
+                tlvBuilder.serialize()
+        )
 
         val expected = byteArrayOf(0, -14, 0, 0, 0, 0, 101, 16, 32, -111, -76, -47, 66, -126, 63,
             125, 32, -59, -16, -115, -10, -111, 34, -34, 67, -13, 95, 5, 122, -104, -115, -106, 25,
