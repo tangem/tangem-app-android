0d4556e6483b8763a332719abef1a2b55d37c0bd
Tangem <git@tangem.com>
2020-02-04 15:26:20 +0300

    Updated on 2026-06-01

--- tangem-core/src/main/java/com/tangem/commands/PurgeWalletCommand.kt
+++ tangem-core/src/main/java/com/tangem/commands/PurgeWalletCommand.kt
@@ -1,12 +1,13 @@
 package com.tangem.commands
 
-import com.tangem.CardEnvironment
+import com.tangem.common.CardEnvironment
 import com.tangem.common.apdu.CommandApdu
 import com.tangem.common.apdu.Instruction
 import com.tangem.common.apdu.ResponseApdu
 import com.tangem.common.extensions.calculateSha256
 import com.tangem.common.extensions.hexToBytes
 import com.tangem.common.tlv.Tlv
+import com.tangem.common.tlv.TlvBuilder
 import com.tangem.common.tlv.TlvMapper
 import com.tangem.common.tlv.TlvTag
 import com.tangem.tasks.TaskError
@@ -23,7 +24,7 @@ class PurgeWalletResponse(
 ) : CommandResponse
 
 /**
-* This command deletes all wallet data. If Is_Reusable flag is enabled during personalization,
+ * This command deletes all wallet data. If Is_Reusable flag is enabled during personalization,
 
  * If Is_Reusable flag is disabled, the card switches to ‘Purged’ state.
  * ‘Purged’ state is final, it makes the card useless.
@@ -34,12 +35,11 @@ class PurgeWalletCommand(
 ) : CommandSerializer<PurgeWalletResponse>() {
 
     override fun serialize(cardEnvironment: CardEnvironment): CommandApdu {
-        val tlvData = mutableListOf(
-                Tlv(TlvTag.Pin, cardEnvironment.pin1.calculateSha256()),
-                Tlv(TlvTag.CardId, cardId.hexToBytes()),
-                Tlv(TlvTag.Pin2, cardEnvironment.pin2.calculateSha256())
-        )
-        return CommandApdu(Instruction.PurgeWallet, tlvData)
+        val tlvBuilder = TlvBuilder()
+        tlvBuilder.append(TlvTag.Pin, cardEnvironment.pin1)
+        tlvBuilder.append(TlvTag.CardId, cardId)
+        tlvBuilder.append(TlvTag.Pin2, cardEnvironment.pin2)
+        return CommandApdu(Instruction.PurgeWallet, tlvBuilder.serialize())
     }
 
     override fun deserialize(cardEnvironment: CardEnvironment, responseApdu: ResponseApdu): PurgeWalletResponse? {
