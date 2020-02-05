10a7f5e81ded21157b8149aedeec6b785a0dd29e
Tangem <git@tangem.com>
2020-02-05 12:30:57 +0300

    Updated on 2026-02-11

--- tangem-core/src/main/java/com/tangem/tasks/SingleCommandTask.kt
+++ tangem-core/src/main/java/com/tangem/tasks/SingleCommandTask.kt
@@ -1,6 +1,7 @@
 package com.tangem.tasks
 
-import com.tangem.CardEnvironment
+import com.tangem.commands.Card
+import com.tangem.common.CardEnvironment
 import com.tangem.commands.CommandResponse
 import com.tangem.commands.CommandSerializer
 import com.tangem.common.CompletionResult
@@ -14,8 +15,11 @@ class SingleCommandTask<Event : CommandResponse>(
         private val command: CommandSerializer<Event>
 ) : Task<Event>() {
 
-    override fun onRun(cardEnvironment: CardEnvironment,
-                       callback: (result: TaskEvent<Event>) -> Unit) {
+    override fun onRun(
+            cardEnvironment: CardEnvironment,
+            currentCard: Card?,
+            callback: (result: TaskEvent<Event>) -> Unit
+    ) {
         sendCommand(command, cardEnvironment) { result ->
             when (result) {
                 is CompletionResult.Success -> {
