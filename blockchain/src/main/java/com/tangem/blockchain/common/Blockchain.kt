0c93323395f3b6ff5df7981dcf38a2d4ffd586d3
Tangem <git@tangem.com>
2020-02-11 13:22:13 +0300

    Updated on 2026-08-19

--- blockchain/src/main/java/com/tangem/blockchain/common/Blockchain.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/Blockchain.kt
@@ -7,6 +7,8 @@ import com.tangem.blockchain.ethereum.EthereumAddressValidator
 import com.tangem.blockchain.cardano.CardanoAddressFactory
 import com.tangem.blockchain.cardano.CardanoAddressValidator
 import com.tangem.blockchain.stellar.StellarAddressFactory
+import com.tangem.blockchain.xrp.XrpAddressFactory
+import com.tangem.blockchain.xrp.XrpAddressValidator
 import java.math.BigDecimal
 
 enum class Blockchain(
@@ -22,7 +24,7 @@ enum class Blockchain(
     Ethereum("ETH", "ETH", 18, "Ethereum", 0),
     Rootstock("", "", 18, "", 0),
     Cardano("CARDANO", "ADA", 6, "Cardano", 0),
-    Ripple("", "", 6, "", 0),
+    XRP("", "XRP", 6, "XRP Ledger", 0),
     Binance("", "", 8, "", 0),
     Stellar("XLM", "XLM", 7, "Stellar", 0);
 
@@ -40,7 +42,7 @@ enum class Blockchain(
             Ethereum -> EthereumAddressFactory.makeAddress(walletPublicKey)
 //            Rootstock -> RootstockAddressFactory.makeAddress(cardPublicKey)
             Cardano -> CardanoAddressFactory.makeAddress(walletPublicKey)
-//            Ripple -> RippleAddressFactory.makeAddress(cardPublicKey)
+            XRP -> XrpAddressFactory.makeAddress(walletPublicKey)
 //            Binance -> BinanceAddressFactory.makeAddress(cardPublicKey)
             Stellar -> StellarAddressFactory.makeAddress(walletPublicKey)
             else -> throw Exception("unsupported blockchain")
@@ -55,7 +57,7 @@ enum class Blockchain(
             Ethereum -> EthereumAddressValidator.validate(address)
 //            Rootstock -> RootstockAddressValidator.validate(address)
             Cardano -> CardanoAddressValidator.validate(address)
-//            Ripple -> RippleAddressValidator.validate(address)
+            XRP -> XrpAddressValidator.validate(address)
 //            Binance -> BinanceAddressValidator.validate(address)
 //            Stellar -> StellarAddressValidator.validate(address)
             else -> throw Exception("unsupported blockchain")
