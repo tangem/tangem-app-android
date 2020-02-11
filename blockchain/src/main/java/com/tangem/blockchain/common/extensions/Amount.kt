a8dc1140c90c970aeae3f562931364e97807fc4b
Tangem <git@tangem.com>
2020-02-11 15:14:37 +0300

    Updated on 2026-09-14

--- blockchain/src/main/java/com/tangem/blockchain/common/extensions/Amount.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/extensions/Amount.kt
@@ -3,6 +3,6 @@ package com.tangem.blockchain.common.extensions
 import com.tangem.blockchain.common.Amount
 import java.math.BigInteger
 
-fun Amount.bigIntegerValue(): BigInteger {
-    return this.value!!.movePointRight(this.decimals.toInt()).toBigInteger()
+fun Amount.bigIntegerValue(): BigInteger? {
+    return this.value?.movePointRight(this.decimals.toInt())?.toBigInteger()
 }
