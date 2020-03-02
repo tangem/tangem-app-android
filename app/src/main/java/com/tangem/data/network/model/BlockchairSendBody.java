6f13ab4e65b389d00c968dbea276802c44885f80
Tangem <git@tangem.com>
2020-03-02 12:21:18 +0300

    Updated on 2025-11-26

---
+++ app/src/main/java/com/tangem/data/network/model/BlockchairSendBody.java
@@ -0,0 +1,9 @@
+package com.tangem.data.network.model;
+
+public class BlockchairSendBody {
+    private String data;
+
+    public BlockchairSendBody(String data) {
+        this.data = data;
+    }
+}
