586dc1a563ac298c29f4a5b56c65463448532d27
Tangem <git@tangem.com>
2020-03-10 16:39:20 +0300

    Updated on 2026-08-14

---
+++ tangem-demo/src/main/java/com/tangem/tangemtest/card_use_cases/CardContext.kt
@@ -0,0 +1,31 @@
+package com.tangem.tangemtest.card_use_cases
+
+import androidx.fragment.app.FragmentActivity
+import com.tangem.CardManager
+import com.tangem.commands.Card
+import com.tangem.tangem_sdk_new.extensions.init
+
+/**
+[REDACTED_AUTHOR]
+ */
+class CardContext {
+    lateinit var cardManager: CardManager
+        private set
+
+    var card: Card? = null
+    var isVerified: Boolean = false
+
+    fun reset() {
+        isVerified = false
+        card = null
+    }
+
+    fun requireCard(): Card = card!!
+
+    fun init(activity: FragmentActivity): CardContext {
+        if (::cardManager.isInitialized) return this
+
+        cardManager = CardManager.init(activity)
+        return this
+    }
+}
