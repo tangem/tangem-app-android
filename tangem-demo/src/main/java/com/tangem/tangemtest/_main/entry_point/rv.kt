cf83acb2006824b2b90dd0b8caa89c24a046df66
Tangem <git@tangem.com>
2020-03-13 12:17:13 +0300

    Updated on 2026-03-27

--- tangem-demo/src/main/java/com/tangem/tangemtest/_main/entry_point/rv.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/_main/entry_point/rv.kt
@@ -1,69 +1,30 @@
 package com.tangem.tangemtest._main.entry_point
 
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
 import com.tangem.tangemtest.R
-import com.tangem.tangemtest.card_use_cases.CardContext
-import com.tangem.tangemtest.card_use_cases.actions.Action
-import com.tangem.tangemtest.commons.Bindable
-import com.tangem.tangemtest.commons.NavigateAction
+import com.tangem.tangemtest.commons.NavigateOptions
+import ru.dev.gbixahue.eu4d.lib.android._android.views.recycler_view.RvAdapter
+import ru.dev.gbixahue.eu4d.lib.android._android.views.recycler_view.RvCallback
+import ru.dev.gbixahue.eu4d.lib.android._android.views.recycler_view.RvVH
 
 /**
 [REDACTED_AUTHOR]
  */
-typealias RvCallback = (Int, Any?) -> Unit
+class RvActionsAdapter(callback: RvCallback<NavigateOptions>) : RvAdapter<RvActionsVH, NavigateOptions>(callback) {
 
-class RvActionsAdapter(
-        private val cardContext: CardContext,
-        private val callback: RvCallback
-) : RecyclerView.Adapter<RvActionsVH>() {
-
-    private val actionList = mutableListOf<NavigateAction>()
-
-    fun setItems(list: MutableList<NavigateAction>) {
-        actionList.clear()
-        actionList.addAll(list)
+    override fun createViewHolder(view: View, listener: RvCallback<NavigateOptions>?): RvActionsVH {
+        return RvActionsVH(view, listener)
     }
 
-    fun addToItems(list: MutableList<NavigateAction>) {
-        actionList.addAll(list)
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RvActionsVH {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.vh_actions, parent, false)
-        return RvActionsVH(view, cardContext.isVerified, callback)
-    }
-
-    override fun getItemCount(): Int = actionList.size
-
-    override fun onBindViewHolder(holder: RvActionsVH, position: Int) {
-        holder.bind(actionList[position])
-    }
+    override fun getLayoutId(): Int = R.layout.vh_actions
 }
 
-class RvActionsVH(
-        view: View,
-        private val cardIsVerified: Boolean,
-        private val callback: RvCallback
-) : RecyclerView.ViewHolder(view), Bindable<NavigateAction> {
-
+class RvActionsVH(itemView: View, callback: RvCallback<NavigateOptions>?) : RvVH<NavigateOptions>(itemView, callback) {
     private val tvAction = itemView.findViewById<TextView>(R.id.tv_title)
-    private val navigateRight = itemView.findViewById<TextView>(R.id.tv_navigate_right)
 
-    override fun bind(data: NavigateAction) {
-        tvAction.text = tvAction.context.getString(data.action.resId)
-        itemView.setOnClickListener { callback(adapterPosition, data.destinationId) }
-        switchItemBackground(data.action)
-    }
-
-    private fun switchItemBackground(action: Action) {
-        when (action) {
-            is Action.Scan -> {
-            }
-            else -> itemView.visibility = if (cardIsVerified) View.VISIBLE else View.GONE
-        }
+    override fun onDataBound(data: NavigateOptions) {
+        tvAction.text = tvAction.context.getString(data.name.resName)
+        itemView.setOnClickListener { invokeCallback() }
     }
 }
