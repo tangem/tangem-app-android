17e913cc9eaefa24a12ef16a9f8fceb8bac2af15
Tangem <git@tangem.com>
2020-03-18 16:35:59 +0300

    Updated on 2026-04-15

--- tangem-demo/src/main/java/com/tangem/tangemtest/card_use_cases/ui/SignActionFragment.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/card_use_cases/ui/SignActionFragment.kt
@@ -1,7 +1,7 @@
 package com.tangem.tangemtest.card_use_cases.ui
 
 import com.tangem.tangemtest.R
-import com.tangem.tangemtest.commons.Action
+import com.tangem.tangemtest.commons.ActionType
 
 /**
 [REDACTED_AUTHOR]
@@ -10,5 +10,5 @@ class SignActionFragment : BaseCardActionFragment() {
 
     override fun getLayoutId(): Int = R.layout.fg_action_card_sign
 
-    override fun getAction(): Action = Action.Sign
+    override fun getAction(): ActionType = ActionType.Sign
 }
