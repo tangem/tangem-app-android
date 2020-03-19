b122924c7d1eec373d5ccefe9d2defb266a7bbb4
Tangem <git@tangem.com>
2020-03-19 10:56:33 +0300

    Updated on 2026-05-07

--- tangem-demo/src/main/java/com/tangem/tangemtest/card_use_cases/view_models/ParamsViewModel.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/card_use_cases/view_models/ParamsViewModel.kt
@@ -9,8 +9,8 @@ import com.tangem.CardManager
 import com.tangem.commands.Card
 import com.tangem.common.tlv.TlvTag
 import com.tangem.tangemtest._arch.SingleLiveEvent
-import com.tangem.tangemtest.card_use_cases.models.params.manager.IncomingParameter
-import com.tangem.tangemtest.card_use_cases.models.params.manager.ParamsManager
+import com.tangem.tangemtest.card_use_cases.domain.params_manager.IncomingParameter
+import com.tangem.tangemtest.card_use_cases.domain.params_manager.ParamsManager
 import com.tangem.tangemtest.commons.performAction
 import com.tangem.tasks.ScanEvent
 import com.tangem.tasks.TaskError
