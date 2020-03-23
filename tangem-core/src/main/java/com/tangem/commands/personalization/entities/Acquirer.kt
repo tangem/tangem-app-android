8c66040e8765ad3d7346d0b556a208bb4c8d22b2
Tangem <git@tangem.com>
2020-03-23 18:17:43 +0300

    Updated on 2026-09-25

--- tangem-core/src/main/java/com/tangem/commands/personalization/entities/Acquirer.kt
+++
@@ -1,9 +0,0 @@
-package com.tangem.commands.personalization.entities
-
-import com.tangem.common.extensions.hexToBytes
-import com.tangem.crypto.CryptoUtils
-
-object Acquirer {
-    val privateKey = "21222324252627284771ED81F2BACF57479E4735EB1405083927372D40DA9E92".hexToBytes()
-    val publicKey: ByteArray by lazy { CryptoUtils.generatePublicKey(privateKey) }
-}
