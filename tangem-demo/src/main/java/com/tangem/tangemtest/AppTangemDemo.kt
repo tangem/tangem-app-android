f58b119ef24fcb2a93314e126243247571003144
Tangem <git@tangem.com>
2020-03-23 22:55:21 +0300

    Updated on 2026-08-07

--- tangem-demo/src/main/java/com/tangem/tangemtest/AppTangemDemo.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/AppTangemDemo.kt
@@ -1,6 +1,8 @@
 package com.tangem.tangemtest
 
 import android.app.Application
+import com.tangem.tangemtest._arch.structure.base.ULog
+import com.tangem.tangemtest._arch.structure.base.UnitLogger
 import com.tangem.tangemtest.commons.TangemLogger
 import ru.dev.gbixahue.eu4d.lib.android.global.log.Log
 
@@ -12,10 +14,11 @@ class AppTangemDemo : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        setLogger()
+        setupLoggers()
     }
 
-    private fun setLogger() {
+    private fun setupLoggers() {
         Log.setLogger(TangemLogger())
+        ULog.setLogger(UnitLogger())
     }
 }
