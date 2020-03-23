f9c8f357865c1bca0440a3d5fc5364461d600de9
Tangem <git@tangem.com>
2020-03-23 22:55:21 +0300

    release/5.26.1

---
+++ tangem-demo/src/main/java/com/tangem/tangemtest/_arch/structure/impl/items.kt
@@ -0,0 +1,22 @@
+package com.tangem.tangemtest._arch.structure.impl
+
+import com.tangem.tangemtest._arch.structure.base.DataUnit
+
+/**
+* [REDACTED_AUTHOR]
+ *
+ * Строительные элементы харакатеризующие тип поля
+ */
+class TextUnit(value: String? = null) : DataUnit<String>(StringViewModel(value))
+class EditTextUnit(value: String? = null) : DataUnit<String>(StringViewModel(value))
+class NumberUnit(value: Number? = null) : DataUnit<Number>(NumberViewModel(value))
+class BoolUnit(value: Boolean? = null) : DataUnit<Boolean>(BoolViewModel(value))
+
+class ListUnit(value: List<KeyValue>, selectedValue: Any)
+    : DataUnit<ModelHelper>(ListViewModel(ModelHelper(selectedValue, value))
+)
+
+fun DataUnit<*>.resName(resId: Int): DataUnit<*> {
+    payload["resName"] = resId
+    return this
+}
