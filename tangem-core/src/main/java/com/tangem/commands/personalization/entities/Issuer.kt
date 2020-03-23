d1a6b6cc29fc40452c34e48fdb51bd8e2c288024
Tangem <git@tangem.com>
2020-03-23 18:17:43 +0300

    Updated on 2026-04-15

--- tangem-core/src/main/java/com/tangem/commands/personalization/entities/Issuer.kt
+++ tangem-core/src/main/java/com/tangem/commands/personalization/entities/Issuer.kt
@@ -1,18 +1,10 @@
 package com.tangem.commands.personalization.entities
 
-import com.tangem.common.extensions.hexToBytes
-import com.tangem.crypto.CryptoUtils
+import com.tangem.common.KeyPair
 
-object Issuer {
-
-    val name = "TANGEM SDK"
-    val id = name + "\u0000"
-    val dataPrivateKey = "11121314151617184771ED81F2BACF57479E4735EB1405083927372D40DA9E92".hexToBytes()
-
-    val dataPublicKey: ByteArray by lazy { CryptoUtils.generatePublicKey(dataPrivateKey) }
-
-    val transactionPrivateKey =
-            "11121314151617184771ED81F2BACF57479E4735EB1405081918171615141312".hexToBytes()
-
-    val transactionPublicKey: ByteArray by lazy { CryptoUtils.generatePublicKey(transactionPrivateKey) }
-}
+data class Issuer(
+        val name: String,
+        val id: String,
+        val dataKeyPair: KeyPair,
+        val transactionKeyPair: KeyPair
+)
