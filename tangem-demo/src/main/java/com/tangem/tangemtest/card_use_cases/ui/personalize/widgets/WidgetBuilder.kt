aa0ed046d44c423023bd7feab0c45f8db7b2c284
Tangem <git@tangem.com>
2020-03-24 23:52:05 +0300

    Updated on 2025-11-03

--- tangem-demo/src/main/java/com/tangem/tangemtest/card_use_cases/ui/personalize/widgets/WidgetBuilder.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/card_use_cases/ui/personalize/widgets/WidgetBuilder.kt
@@ -1,32 +1,15 @@
 package com.tangem.tangemtest.card_use_cases.ui.personalize.widgets
 
-import android.view.View
 import android.view.ViewGroup
-import com.tangem.tangemtest.R
 import com.tangem.tangemtest._arch.structure.base.Block
 import com.tangem.tangemtest._arch.structure.base.DataUnit
 import com.tangem.tangemtest._arch.structure.base.Unit
 import com.tangem.tangemtest._arch.structure.impl.*
-import ru.dev.gbixahue.eu4d.lib.android._android.views.inflate
-
-/**
-[REDACTED_AUTHOR]
- */
-interface BlockWidget : ViewWidget
-
-class EmptyWidget(parent: ViewGroup) : BlockWidget {
-    override val view: View = parent.inflate(getLayoutId(), parent, false)
-    override fun getLayoutId(): Int = R.layout.w_empty
-}
-
-abstract class BaseBlockWidget<D>(parent: ViewGroup) : BaseViewWidget(parent), BlockWidget
-
-class LinearBlockWidget(
-        private val linearBlock: LinearBlock,
-        parent: ViewGroup
-) : BaseBlockWidget<List<UnitWidget<*>>>(parent) {
-    override fun getLayoutId(): Int = R.layout.w_personilize_block
-}
+import com.tangem.tangemtest.card_use_cases.ui.personalize.widgets.base.BlockWidget
+import com.tangem.tangemtest.card_use_cases.ui.personalize.widgets.base.EmptyWidget
+import com.tangem.tangemtest.card_use_cases.ui.personalize.widgets.base.UnitWidget
+import com.tangem.tangemtest.card_use_cases.ui.personalize.widgets.base.ViewWidget
+import com.tangem.tangemtest.card_use_cases.ui.personalize.widgets.impl.*
 
 class WidgetBuilder {
 
