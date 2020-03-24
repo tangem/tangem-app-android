4bcf26f2ea883f5f04b759991ff9f6540704fb25
Tangem <git@tangem.com>
2020-03-24 23:52:05 +0300

    Updated on 2026-02-02

--- tangem-demo/src/main/java/com/tangem/tangemtest/_arch/structure/base/unit_model.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/_arch/structure/base/unit_model.kt
@@ -4,28 +4,50 @@ package com.tangem.tangemtest._arch.structure.base
 /**
 [REDACTED_AUTHOR]
  */
-data class ViewState(
-        var isEnabled: Boolean = true,
-        var visibility: Int = 0x00000000,
-        var descriptionVisibility: Int = 0x00000008
-)
+typealias ValueChange<V> = (V?) -> kotlin.Unit
+typealias SafeValueChange<V> = (V) -> kotlin.Unit
 
 interface UnitViewModel<D> : Payload {
-    var viewState: ViewState?
+    val viewState: ViewState
     var data: D?
+    var onDataUpdated: ValueChange<D>?
 
-    fun updateData(data: D?)
+    fun updateDataByView(data: D?)
 }
 
-open class BaseUnitViewModel<D>(
-        override var data: D? = null
-) : UnitViewModel<D> {
+open class BaseUnitViewModel<D> : UnitViewModel<D> {
 
-    override var viewState: ViewState? = ViewState()
+    override val viewState: ViewState = ViewState()
     override val payload: MutableMap<String, Any?> = mutableMapOf()
 
-    override fun updateData(data: D?) {
-        ULog.d(this, "data changed: $data")
-        this.data = data
+    override var data: D? = null
+        set(value) {
+            if (handleDataUpdates(value)) field = value
+        }
+
+    override var onDataUpdated: ValueChange<D>? = null
+
+    protected open fun handleDataUpdates(value: D?): Boolean {
+        ULog.d(this, "handleDateUpdates: $value")
+        onDataUpdated?.invoke(value)
+        return true
     }
+
+    override fun updateDataByView(data: D?) {
+        ULog.d(this, "data changed: $data")
+        val callback = onDataUpdated
+        onDataUpdated = null
+        this.data = data
+        onDataUpdated = callback
+    }
+}
+
+class ViewState {
+    var descriptionVisibility: Int = 0x00000008
+        set(value) {
+            field = value
+            onDescriptionVisibilityChanged?.invoke(value)
+        }
+
+    var onDescriptionVisibilityChanged: SafeValueChange<Int>? = null
 }
