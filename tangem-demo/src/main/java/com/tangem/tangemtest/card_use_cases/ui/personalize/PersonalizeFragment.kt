b638cf35147ff60d5c8bb780e2087a0bbafdba1c
Tangem <git@tangem.com>
2020-03-24 23:52:05 +0300

    Updated on 2026-07-23

--- tangem-demo/src/main/java/com/tangem/tangemtest/card_use_cases/ui/personalize/PersonalizeFragment.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/card_use_cases/ui/personalize/PersonalizeFragment.kt
@@ -7,11 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.tangem.tangemtest.R
-import com.tangem.tangemtest.card_use_cases.ui.personalize.view_model.PersonalizeViewModel
-import com.tangem.tangemtest.card_use_cases.ui.personalize.view_model.PersonalizeViewModelFactory
+import com.tangem.tangemtest._main.MainViewModel
 import com.tangem.tangemtest.card_use_cases.ui.personalize.widgets.WidgetBuilder
 import ru.dev.gbixahue.eu4d.lib.android.global.log.Log
 import java.io.IOException
@@ -23,11 +23,13 @@ import java.nio.charset.Charset
  */
 class PersonalizeFragment : Fragment() {
 
-    protected val blockContainer: ViewGroup by lazy {
+    private val mainActivityVM: MainViewModel by activityViewModels()
+    private val viewModel: PersonalizeViewModel by viewModels { PersonalizeViewModelFactory(getPersonalizeJson()) }
+
+    private val blockContainer: ViewGroup by lazy {
         mainView.findViewById<LinearLayout>(R.id.ll_container)
     }
-    protected val viewModel: PersonalizeViewModel by viewModels { PersonalizeViewModelFactory(getPersonalizeJson()) }
-    protected lateinit var mainView: View
+    private lateinit var mainView: View
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         Log.d(this, "onCreateView")
@@ -40,7 +42,9 @@ class PersonalizeFragment : Fragment() {
 
         viewModel.ldBlockList.observe(viewLifecycleOwner, Observer { blocList ->
             blocList.forEach { WidgetBuilder().build(it, blockContainer) }
-
+        })
+        mainActivityVM.ldDescriptionSwitch.observe(viewLifecycleOwner, Observer {
+            viewModel.toggleDescriptionVisibility(it)
         })
     }
 
