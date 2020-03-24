f311f45cd4f8ecc41a25e86a6d1af68980bdcb9d
Tangem <git@tangem.com>
2020-03-24 19:29:44 +0300

    release/5.27.0

--- tangem-demo/src/main/java/com/tangem/tangemtest/_arch/structure/impl/items.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/_arch/structure/impl/items.kt
@@ -1,14 +1,15 @@
 package com.tangem.tangemtest._arch.structure.impl
 
 import com.tangem.tangemtest._arch.structure.base.DataUnit
+import com.tangem.tangemtest._arch.structure.base.Id
 
 
-class TextUnit(value: String? = null) : DataUnit<String>(StringViewModel(value))
-class EditTextUnit(value: String? = null) : DataUnit<String>(StringViewModel(value))
-class NumberUnit(value: Number? = null) : DataUnit<Number>(NumberViewModel(value))
-class BoolUnit(value: Boolean? = null) : DataUnit<Boolean>(BoolViewModel(value))
+class TextUnit(override val id: Id, value: String? = null) : DataUnit<String>(StringViewModel(value))
+class EditTextUnit(override val id: Id, value: String? = null) : DataUnit<String>(StringViewModel(value))
+class NumberUnit(override val id: Id, value: Number? = null) : DataUnit<Number>(NumberViewModel(value))
+class BoolUnit(override val id: Id, value: Boolean? = null) : DataUnit<Boolean>(BoolViewModel(value))
 
-class ListUnit(value: List<KeyValue>, selectedValue: Any)
+class ListUnit(override val id: Id, value: List<KeyValue>, selectedValue: Any)
     : DataUnit<ModelHelper>(ListViewModel(ModelHelper(selectedValue, value))
 )
 
