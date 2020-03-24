b1675e7f16659fe76582c86154a865637c191be5
Tangem <git@tangem.com>
2020-03-24 23:52:05 +0300

    Updated on 2026-06-01

--- tangem-demo/src/main/java/com/tangem/tangemtest/card_use_cases/ui/personalize/PersonalizeViewModel.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/card_use_cases/ui/personalize/PersonalizeViewModel.kt
@@ -1,14 +1,16 @@
-package com.tangem.tangemtest.card_use_cases.ui.personalize.view_model
+package com.tangem.tangemtest.card_use_cases.ui.personalize
 
+import android.view.View
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.google.gson.Gson
 import com.tangem.tangemtest._arch.structure.base.Block
-import com.tangem.tangemtest.card_use_cases.ui.personalize.personalize_converter.json_test.BlockToJsonConverter
-import com.tangem.tangemtest.card_use_cases.ui.personalize.personalize_converter.json_test.JsonBlockEnDe
-import com.tangem.tangemtest.card_use_cases.ui.personalize.personalize_converter.json_test.JsonToBlockConverter
-import com.tangem.tangemtest.card_use_cases.ui.personalize.personalize_converter.json_test.TestJsonDto
+import com.tangem.tangemtest._arch.structure.base.DataUnit
+import com.tangem.tangemtest.card_use_cases.ui.personalize.converter.BlockToJsonConverter
+import com.tangem.tangemtest.card_use_cases.ui.personalize.converter.JsonBlockEnDe
+import com.tangem.tangemtest.card_use_cases.ui.personalize.converter.JsonToBlockConverter
+import com.tangem.tangemtest.card_use_cases.ui.personalize.converter.TestJsonDto
 
 /**
 [REDACTED_AUTHOR]
@@ -25,5 +27,14 @@ class PersonalizeViewModel(private val jsonPersonalizeString: String) : ViewMode
         val enDe = JsonBlockEnDe(JsonToBlockConverter(), BlockToJsonConverter())
         val jsonDto = Gson().fromJson(jsonPersonalizeString, TestJsonDto::class.java)
         return enDe.decode(jsonDto)
+    }
+
+    fun toggleDescriptionVisibility(state: Boolean) {
+        ldBlockList.value?.forEach { block ->
+            block.unitList.forEach { unit ->
+                val vm = unit as? DataUnit<*> ?: return@forEach
+                vm.viewModel?.viewState?.descriptionVisibility = if (state) View.VISIBLE else View.GONE
+            }
+        }
     }
 }
