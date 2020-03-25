49e0d136ae580720d4bc6387f55e8000655c657b
Tangem <git@tangem.com>
2020-03-25 14:06:08 +0300

    Updated on 2025-09-17

--- tangem-demo/src/main/java/com/tangem/tangemtest/card_use_cases/ui/personalize/PersonalizeFragment.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/card_use_cases/ui/personalize/PersonalizeFragment.kt
@@ -10,9 +10,19 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
+import com.tangem.CardManager
+import com.tangem.tangem_sdk_new.extensions.init
 import com.tangem.tangemtest.R
 import com.tangem.tangemtest._main.MainViewModel
+import com.tangem.tangemtest.card_use_cases.domain.params_manager.ParamsManager
+import com.tangem.tangemtest.card_use_cases.domain.params_manager.ParamsManagerFactory
+import com.tangem.tangemtest.card_use_cases.ui.card.actions.ActionViewModelFactory
+import com.tangem.tangemtest.card_use_cases.ui.card.actions.ParamsViewModel
 import com.tangem.tangemtest.card_use_cases.ui.personalize.widgets.WidgetBuilder
+import com.tangem.tangemtest.commons.ActionType
+import kotlinx.android.synthetic.main.fg_personalize.*
 import ru.dev.gbixahue.eu4d.lib.android.global.log.Log
 import java.io.IOException
 import java.io.InputStream
@@ -24,7 +34,10 @@ import java.nio.charset.Charset
 class PersonalizeFragment : Fragment() {
 
     private val mainActivityVM: MainViewModel by activityViewModels()
-    private val viewModel: PersonalizeViewModel by viewModels { PersonalizeViewModelFactory(getPersonalizeJson()) }
+    private val personalizeVM: PersonalizeViewModel by viewModels { PersonalizeViewModelFactory(getPersonalizeJson()) }
+
+    private val paramsManager: ParamsManager by lazy { ParamsManagerFactory.createFactory().get(ActionType.Personalize)!! }
+    private val paramsVM: ParamsViewModel by viewModels() { ActionViewModelFactory(paramsManager) }
 
     private val blockContainer: ViewGroup by lazy {
         mainView.findViewById<LinearLayout>(R.id.ll_container)
@@ -40,12 +53,24 @@ class PersonalizeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.ldBlockList.observe(viewLifecycleOwner, Observer { blocList ->
+        personalizeVM.ldBlockList.observe(viewLifecycleOwner, Observer { blocList ->
             blocList.forEach { WidgetBuilder().build(it, blockContainer) }
         })
         mainActivityVM.ldDescriptionSwitch.observe(viewLifecycleOwner, Observer {
-            viewModel.toggleDescriptionVisibility(it)
+            personalizeVM.toggleDescriptionVisibility(it)
         })
+
+        paramsVM.setCardManager(CardManager.init(requireActivity()))
+        paramsVM.ldResponse.observe(viewLifecycleOwner, Observer {
+            Log.d(this, "action response: ${if (it.length > 50) it.substring(0..50) else it}")
+            showSnackbarMessage(it)
+        })
+        paramsVM.seError.observe(viewLifecycleOwner, Observer { showSnackbarMessage(it) })
+        fab_action?.setOnClickListener { paramsVM.invokeMainAction() }
+    }
+
+    protected fun showSnackbarMessage(message: String) {
+        Snackbar.make(mainView, message, BaseTransientBottomBar.LENGTH_SHORT).show()
     }
 
     private fun getPersonalizeJson(): String {
