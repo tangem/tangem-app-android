84bc79cbdc569df9e2f47c6bed91591830a2e3f7
Tangem <git@tangem.com>
2020-03-25 22:55:01 +0300

    Updated on 2026-07-01

---
+++ tangem-demo/src/main/java/com/tangem/tangemtest/cardUseCase/resources/MainResourceHolder.kt
@@ -0,0 +1,24 @@
+package com.tangem.tangemtest.cardUseCase.resources
+
+import com.tangem.tangemtest.R
+import com.tangem.tangemtest._arch.structure.Id
+import com.tangem.tangemtest.cardUseCase.resources.initializers.ActionResources
+import com.tangem.tangemtest.cardUseCase.resources.initializers.PersonalizeResources
+import ru.dev.gbixahue.eu4d.lib.kotlin.common.BaseTypedHolder
+
+/**
+[REDACTED_AUTHOR]
+ */
+open class ResourceHolder<T> : BaseTypedHolder<T, Resources>()
+
+object MainResourceHolder : ResourceHolder<Id>() {
+    init {
+        PersonalizeResources().init(this)
+        ActionResources().init(this)
+    }
+
+    inline fun <reified Res : Resources> safeGet(id: Id): Res {
+        val result = super.get(id) ?: Resources(R.string.unknown, R.string.unknown)
+        return result as Res
+    }
+}
