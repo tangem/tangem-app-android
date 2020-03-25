5d3cf4190496d8235029425077bceab67c1386fd
Tangem <git@tangem.com>
2020-03-26 02:08:01 +0300

    Updated on 2026-02-11

---
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/paramsManager/managers/SignParamsManager.kt
@@ -0,0 +1,24 @@
+package com.tangem.tangemtest.ucase.domain.paramsManager.managers
+
+import com.tangem.CardManager
+import com.tangem.common.tlv.TlvTag
+import com.tangem.tangemtest.ucase.domain.actions.SignAction
+import com.tangem.tangemtest.ucase.domain.paramsManager.ActionCallback
+import com.tangem.tangemtest.ucase.domain.paramsManager.IncomingParameter
+
+class SignParamsManager : BaseParamsManager(SignAction()) {
+    override fun createParamsList(): List<IncomingParameter> {
+        return listOf(
+                IncomingParameter(TlvTag.CardId, null),
+                IncomingParameter(TlvTag.TransactionOutHash, "Data used for hashing")
+        )
+    }
+
+    override fun invokeMainAction(cardManager: CardManager, callback: ActionCallback) {
+        action.executeMainAction(getAttrsForAction(cardManager), callback)
+    }
+
+    override fun getActionByTag(tag: TlvTag, cardManager: CardManager): ((ActionCallback) -> Unit)? {
+        return action.getActionByTag(tag, getAttrsForAction(cardManager))
+    }
+}
