9a743fb16a70aa5ddee61c09a5ccd70769fafd70
Tangem <git@tangem.com>
2020-03-25 22:43:14 +0300

    Updated on 2025-10-01

---
+++ tangem-demo/src/main/java/com/tangem/tangemtest/card_use_cases/ui/personalize/widgets/impl/item/TextWidget.kt
@@ -0,0 +1,17 @@
+package com.tangem.tangemtest.card_use_cases.ui.personalize.widgets.impl.item
+
+import android.view.ViewGroup
+import android.widget.TextView
+import com.tangem.tangemtest.R
+import com.tangem.tangemtest._arch.structure.impl.TextItem
+import com.tangem.tangemtest.card_use_cases.ui.personalize.widgets.abstraction.getResNameId
+
+class TextWidget(parent: ViewGroup, data: TextItem) : DescriptionWidget<String>(parent, data) {
+    override fun getLayoutId(): Int = R.layout.w_personalize_item_text
+
+    private val tvName = view.findViewById<TextView>(R.id.tv_name)
+
+    init {
+        tvName?.setText(getResNameId())
+    }
+}
