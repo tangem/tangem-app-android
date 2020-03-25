213ce92e046aff62bb55b50a5ea3fe1251cfe66f
Tangem <git@tangem.com>
2020-03-25 14:06:08 +0300

    Updated on 2026-08-07

--- tangem-demo/src/main/java/com/tangem/tangemtest/card_use_cases/domain/params_manager/params_managers.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/card_use_cases/domain/params_manager/params_managers.kt
@@ -94,6 +94,20 @@ class PersonalizeParamsManager : BaseParamsManager(PersonalizeAction()) {
     }
 
     override fun invokeMainAction(cardManager: CardManager, callback: ActionCallback) {
+        action.executeMainAction(getAttrsForAction(cardManager), callback)
+    }
+}
 
+class DepersonalizeParamsManager: BaseParamsManager(DepesonalizeAction()){
+    override fun createParamsList(): List<IncomingParameter> {
+        return listOf(IncomingParameter(TlvTag.CardId, null))
+    }
+
+    override fun invokeMainAction(cardManager: CardManager, callback: ActionCallback) {
+        action.executeMainAction(getAttrsForAction(cardManager), callback)
+    }
+
+    override fun getActionByTag(tag: TlvTag, cardManager: CardManager): ((ActionCallback) -> Unit)? {
+        return action.getActionByTag(tag, getAttrsForAction(cardManager))
     }
 }
