c259da38d60af1cbab4383903fdc76eefa3db570
Tangem <git@tangem.com>
2020-03-26 02:08:01 +0300

    Updated on 2026-06-02

---
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/actions/DepesonalizeAction.kt
@@ -0,0 +1,20 @@
+package com.tangem.tangemtest.ucase.domain.actions
+
+import com.tangem.common.tlv.TlvTag
+import com.tangem.tangemtest.ucase.domain.paramsManager.ActionCallback
+import com.tangem.tangemtest.ucase.domain.paramsManager.findParameter
+
+class DepesonalizeAction : BaseCardAction() {
+    override fun executeMainAction(attrs: AttrForAction, callback: ActionCallback) {
+        val cardId = attrs.paramsList.findParameter(TlvTag.CardId)?.data as? String ?: return
+
+        attrs.cardManager.depersonalize(cardId) { handleResponse(it, null, attrs, callback) }
+    }
+
+    override fun getActionByTag(tag: TlvTag, attrs: AttrForAction): ((ActionCallback) -> Unit)? {
+        return when (tag) {
+            TlvTag.CardId -> { callback -> ScanAction().executeMainAction(attrs, callback) }
+            else -> null
+        }
+    }
+}
