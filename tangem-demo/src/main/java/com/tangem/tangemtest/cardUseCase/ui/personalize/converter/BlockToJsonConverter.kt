0661beb9d1a01dcde510c6956e94e247034bf6ee
Tangem <git@tangem.com>
2020-03-26 02:08:01 +0300

    release/5.26.2

--- tangem-demo/src/main/java/com/tangem/tangemtest/cardUseCase/ui/personalize/converter/BlockToJsonConverter.kt
+++
@@ -1,9 +0,0 @@
-package com.tangem.tangemtest.cardUseCase.ui.personalize.converter
-
-import com.tangem.tangemtest._arch.structure.abstraction.Block
-import com.tangem.tangemtest.cardUseCase.ui.personalize.dto.TestJsonDto
-import ru.dev.gbixahue.eu4d.lib.kotlin.common.Converter
-
-class BlockToJsonConverter : Converter<List<Block>, TestJsonDto> {
-    override fun convert(from: List<Block>): TestJsonDto = TestJsonDto()
-}
