57f640d1de464e234caa923dd39cb015e395bf3c
Tangem <git@tangem.com>
2020-03-26 02:08:01 +0300

    Updated on 2026-08-07

---
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/paramsManager/ParamsManager.kt
@@ -0,0 +1,25 @@
+package com.tangem.tangemtest.ucase.domain.paramsManager
+
+import com.tangem.CardManager
+import com.tangem.common.tlv.TlvTag
+import com.tangem.tasks.TaskEvent
+
+typealias ActionResponse = TaskEvent<*>
+typealias AffectedList = List<IncomingParameter>
+typealias AffectedParamsCallback = (AffectedList) -> Unit
+typealias ActionCallback = (ActionResponse, AffectedList) -> Unit
+
+interface ParamsManager {
+
+    fun parameterChanged(tag: TlvTag, value: Any?, callback: AffectedParamsCallback? = null)
+    fun getParams(): List<IncomingParameter>
+    fun invokeMainAction(cardManager: CardManager, callback: ActionCallback)
+    fun getActionByTag(tag: TlvTag, cardManager: CardManager): ((ActionCallback) -> Unit)?
+
+}
+
+class IncomingParameter(val tlvTag: TlvTag, var data: Any? = null)
+
+fun List<IncomingParameter>.findParameter(tag: TlvTag): IncomingParameter? {
+    return firstOrNull { it.tlvTag == tag }
+}
---
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/personalize/ui/widgets/abstraction/ViewWidget.kt
@@ -0,0 +1,48 @@
+package com.tangem.tangemtest.ucase.variants.personalize.ui.widgets.abstraction
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tangem.tangemtest.R
+import com.tangem.tangemtest._arch.structure.DataHolder
+import com.tangem.tangemtest._arch.structure.abstraction.BaseItem
+import com.tangem.tangemtest._arch.structure.abstraction.ItemViewModel
+import com.tangem.tangemtest.ucase.resources.MainResourceHolder
+import com.tangem.tangemtest.ucase.resources.Resources
+import ru.dev.gbixahue.eu4d.lib.kotlin.common.LayoutHolder
+
+/**
+[REDACTED_AUTHOR]
+ */
+interface ViewWidget : LayoutHolder {
+    val view: View
+}
+
+interface DataWidget<D> : ViewWidget {
+    var dataItem: BaseItem<D>
+}
+
+interface BlockViewWidget : ViewWidget, DataHolder<List<ItemViewModel<*>>>
+
+abstract class BaseWidget<D>(
+        parent: ViewGroup,
+        override var dataItem: BaseItem<D>
+) : ViewWidget, DataWidget<D> {
+
+    override val view: View = inflate(getLayoutId(), parent)
+}
+
+abstract class BaseBlockWidget(parent: ViewGroup) : BlockViewWidget {
+    override val view: View = inflate(getLayoutId(), parent)
+}
+
+fun DataWidget<*>.getResNameId(): Int = MainResourceHolder.safeGet<Resources>(dataItem.id).resName
+
+fun DataWidget<*>.getResDescription(): Int? = MainResourceHolder.safeGet<Resources>(dataItem.id).resDescription
+
+internal fun inflate(id: Int, parent: ViewGroup): View {
+    val layoutId = if (id <= 0) R.layout.w_empty else id
+    val inflatedView = LayoutInflater.from(parent.context).inflate(layoutId, parent, false)
+    parent.addView(inflatedView)
+    return inflatedView
+}
