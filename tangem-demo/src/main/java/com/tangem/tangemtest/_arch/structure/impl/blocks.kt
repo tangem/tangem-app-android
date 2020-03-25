ecd56470d95ff0c7874a9080c5c1914ed1a0ba07
Tangem <git@tangem.com>
2020-03-25 22:43:14 +0300

    Updated on 2025-09-25

--- tangem-demo/src/main/java/com/tangem/tangemtest/_arch/structure/impl/blocks.kt
+++
@@ -1,9 +0,0 @@
-package com.tangem.tangemtest._arch.structure.impl
-
-import com.tangem.tangemtest._arch.structure.base.Id
-import com.tangem.tangemtest._arch.structure.base.ListUnitBlock
-
-/**
-[REDACTED_AUTHOR]
- */
-class LinearBlock(id: Id) : ListUnitBlock(id)
