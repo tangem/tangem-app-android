0dc3cfab7a3135f5d23cea5efd91b5909412d514
Tangem <git@tangem.com>
2020-03-26 02:08:01 +0300

    Updated on 2026-05-14

---
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/paramsManager/managers/BaseParamsManager.kt
@@ -0,0 +1,37 @@
+package com.tangem.tangemtest.ucase.domain.paramsManager.managers
+
+import com.tangem.CardManager
+import com.tangem.common.tlv.TlvTag
+import com.tangem.tangemtest.ucase.domain.actions.AttrForAction
+import com.tangem.tangemtest.ucase.domain.actions.CardAction
+import com.tangem.tangemtest.ucase.domain.paramsManager.*
+import com.tangem.tangemtest.ucase.domain.paramsManager.triggers.changeConsequence.ParamsChangeConsequence
+
+abstract class BaseParamsManager(protected val action: CardAction) : ParamsManager {
+
+    protected val paramsList: List<IncomingParameter> by lazy { createParamsList() }
+
+    abstract fun createParamsList(): List<IncomingParameter>
+
+    override fun parameterChanged(tag: TlvTag, value: Any?, callback: AffectedParamsCallback?) {
+        if (paramsList.isEmpty()) return
+        val foundParam = paramsList.findParameter(tag) ?: return
+
+        foundParam.data = value
+        applyChangesByAffectedParams(foundParam, callback)
+    }
+
+    override fun getParams(): MutableList<IncomingParameter> = paramsList.toMutableList()
+
+    override fun getActionByTag(tag: TlvTag, cardManager: CardManager): ((ActionCallback) -> Unit)? = null
+
+    // Use it if current parameter needs to be affect any other parameters
+    protected open fun applyChangesByAffectedParams(param: IncomingParameter, callback: AffectedParamsCallback?) {
+        getChangeConsequence()?.affectChanges(param, paramsList)?.let { callback?.invoke(it) }
+    }
+
+    protected open fun getChangeConsequence(): ParamsChangeConsequence? = null
+
+    protected fun getAttrsForAction(cardManager: CardManager)
+            : AttrForAction = AttrForAction(cardManager, paramsList, getChangeConsequence())
+}
