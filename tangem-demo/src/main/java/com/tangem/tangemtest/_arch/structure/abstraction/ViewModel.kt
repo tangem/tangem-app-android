7372d38d090e562c973a329f77a11e5e4027f2ce
Tangem <git@tangem.com>
2020-03-26 19:35:36 +0300

    Updated on 2026-09-23

--- tangem-demo/src/main/java/com/tangem/tangemtest/_arch/structure/abstraction/ViewModel.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/_arch/structure/abstraction/ViewModel.kt
@@ -13,6 +13,7 @@ typealias SafeValueChange<V> = (V) -> Unit
 interface ItemViewModel<D> : Payload {
     val viewState: ViewState
     var data: D?
+    var defaultData: D?
     var onDataUpdated: ValueChange<D>?
 
     fun updateDataByView(data: D?)
@@ -23,19 +24,32 @@ open class BaseItemViewModel<D> : ItemViewModel<D> {
     override val viewState: ViewState = ViewState()
     override val payload: MutableMap<String, Any?> = mutableMapOf()
 
+    // Don't update it directly from a View. Use for it updateDataByView()
     override var data: D? = null
         set(value) {
             if (handleDataUpdates(value)) field = value
         }
 
+    // Data for restoring initial value
+    override var defaultData: D? = null
+        set(value) {
+            field = value
+            data = value
+        }
+
+    // Use it for handling data updates in View
     override var onDataUpdated: ValueChange<D>? = null
 
+    // When data updates directly it invokes onDataUpdated
+    // return true = data will update
+    // return false = data won't update
     protected open fun handleDataUpdates(value: D?): Boolean {
         ILog.d(this, "handleDateUpdates: $value")
         onDataUpdated?.invoke(value)
         return true
     }
 
+    // Use it to update the data from a View. It disables onDataUpdated to prevent a callback loop
     override fun updateDataByView(data: D?) {
         ILog.d(this, "data changed: $data")
         val callback = onDataUpdated
