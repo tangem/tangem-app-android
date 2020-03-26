749ddcbacc2abfcc1e831920aca81bcfb7affd7a
Tangem <git@tangem.com>
2020-03-26 23:13:15 +0300

    Updated on 2026-02-02

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/resources/MainResourceHolder.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/resources/MainResourceHolder.kt
@@ -4,6 +4,7 @@ import com.tangem.tangemtest.R
 import com.tangem.tangemtest._arch.structure.Id
 import com.tangem.tangemtest.ucase.resources.initializers.ActionResources
 import com.tangem.tangemtest.ucase.resources.initializers.PersonalizeResources
+import com.tangem.tangemtest.ucase.resources.initializers.TlvResources
 import ru.dev.gbixahue.eu4d.lib.kotlin.common.BaseTypedHolder
 
 /**
@@ -15,6 +16,7 @@ object MainResourceHolder : ResourceHolder<Id>() {
     init {
         PersonalizeResources().init(this)
         ActionResources().init(this)
+        TlvResources().init(this)
     }
 
     inline fun <reified Res : Resources> safeGet(id: Id): Res {
