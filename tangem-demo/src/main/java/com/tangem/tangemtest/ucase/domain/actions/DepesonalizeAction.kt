deee3fde59afd2dc3cb36fbac519fc03f98c0f93
Tangem <git@tangem.com>
2020-03-26 23:13:15 +0300

    Updated on 2026-08-14

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/actions/DepesonalizeAction.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/actions/DepesonalizeAction.kt
@@ -1,22 +1,24 @@
 package com.tangem.tangemtest.ucase.domain.actions
 
-import com.tangem.common.tlv.TlvTag
+import com.tangem.tangemtest._arch.structure.Id
 import com.tangem.tangemtest.ucase.domain.paramsManager.ActionCallback
-import com.tangem.tangemtest.ucase.domain.paramsManager.findParameter
+import com.tangem.tangemtest.ucase.domain.paramsManager.findDataParameter
+import com.tangem.tangemtest.ucase.variants.TlvId
 
 /**
 [REDACTED_AUTHOR]
  */
 class DepesonalizeAction : BaseCardAction() {
     override fun executeMainAction(attrs: AttrForAction, callback: ActionCallback) {
-        val cardId = attrs.paramsList.findParameter(TlvTag.CardId)?.data as? String ?: return
+        val item = attrs.paramsList.findDataParameter(TlvId.CardId) ?: return
+        val cardId = item.viewModel.data as? String ?: return
 
         attrs.cardManager.depersonalize(cardId) { handleResponse(it, null, attrs, callback) }
     }
 
-    override fun getActionByTag(tag: TlvTag, attrs: AttrForAction): ((ActionCallback) -> Unit)? {
-        return when (tag) {
-            TlvTag.CardId -> { callback -> ScanAction().executeMainAction(attrs, callback) }
+    override fun getActionByTag(id: Id, attrs: AttrForAction): ((ActionCallback) -> Unit)? {
+        return when (id) {
+            TlvId.CardId -> { callback -> ScanAction().executeMainAction(attrs, callback) }
             else -> null
         }
     }
--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/paramsManager/managers/DepersonalizeParamsManager.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/paramsManager/managers/DepersonalizeParamsManager.kt
@@ -1,24 +1,26 @@
 package com.tangem.tangemtest.ucase.domain.paramsManager.managers
 
 import com.tangem.CardManager
-import com.tangem.common.tlv.TlvTag
+import com.tangem.tangemtest._arch.structure.Id
+import com.tangem.tangemtest._arch.structure.abstraction.Item
+import com.tangem.tangemtest._arch.structure.impl.EditTextItem
 import com.tangem.tangemtest.ucase.domain.actions.DepesonalizeAction
 import com.tangem.tangemtest.ucase.domain.paramsManager.ActionCallback
-import com.tangem.tangemtest.ucase.domain.paramsManager.IncomingParameter
+import com.tangem.tangemtest.ucase.variants.TlvId
 
 /**
 [REDACTED_AUTHOR]
  */
 class DepersonalizeParamsManager : BaseParamsManager(DepesonalizeAction()) {
-    override fun createParamsList(): List<IncomingParameter> {
-        return listOf(IncomingParameter(TlvTag.CardId, null))
+    override fun createParamsList(): List<Item> {
+        return listOf(EditTextItem(TlvId.CardId, null))
     }
 
     override fun invokeMainAction(cardManager: CardManager, callback: ActionCallback) {
         action.executeMainAction(getAttrsForAction(cardManager), callback)
     }
 
-    override fun getActionByTag(tag: TlvTag, cardManager: CardManager): ((ActionCallback) -> Unit)? {
-        return action.getActionByTag(tag, getAttrsForAction(cardManager))
+    override fun getActionByTag(id: Id, cardManager: CardManager): ((ActionCallback) -> Unit)? {
+        return action.getActionByTag(id, getAttrsForAction(cardManager))
     }
 }
