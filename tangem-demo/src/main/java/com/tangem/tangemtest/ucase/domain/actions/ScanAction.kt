4c437c548c17724e0d22d6c129cec7bd34d4fa4a
Tangem <git@tangem.com>
2020-03-26 03:53:41 +0300

    Updated on 2026-07-23

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/actions/ScanAction.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/actions/ScanAction.kt
@@ -3,6 +3,9 @@ package com.tangem.tangemtest.ucase.domain.actions
 import com.tangem.tangemtest.ucase.domain.paramsManager.ActionCallback
 import com.tangem.tangemtest.ucase.domain.paramsManager.triggers.afterAction.AfterScanModifier
 
+/**
+[REDACTED_AUTHOR]
+ */
 class ScanAction : BaseCardAction() {
     override fun executeMainAction(attrs: AttrForAction, callback: ActionCallback) {
         attrs.cardManager.scanCard { handleResponse(it, AfterScanModifier(), attrs, callback) }
