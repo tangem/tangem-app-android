6c63deab765fd514296b7a3e82c39303c913ac2c
Tangem <git@tangem.com>
2020-03-26 19:35:36 +0300

    Updated on 2026-05-07

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/personalize/converter/BlockToJsonConverter.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/personalize/converter/BlockToJsonConverter.kt
@@ -1,12 +1,18 @@
 package com.tangem.tangemtest.ucase.variants.personalize.converter
 
 import com.tangem.tangemtest._arch.structure.abstraction.Block
-import com.tangem.tangemtest.ucase.variants.personalize.dto.TestJsonDto
+import com.tangem.tangemtest.ucase.variants.personalize.dto.PersonalizeConfig
 import ru.dev.gbixahue.eu4d.lib.kotlin.common.Converter
 
 /**
 [REDACTED_AUTHOR]
  */
-class BlockToJsonConverter : Converter<List<Block>, TestJsonDto> {
-    override fun convert(from: List<Block>): TestJsonDto = TestJsonDto()
+class BlockToJsonConverter(
+        private val valueMapper: ValueMapper,
+        private val config: PersonalizeConfig
+) : Converter<List<Block>, PersonalizeConfig> {
+
+    override fun convert(from: List<Block>): PersonalizeConfig {
+        return valueMapper.mapOnObject(from, config)
+    }
 }
