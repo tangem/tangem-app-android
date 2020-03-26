0523a7b8ff1ed4b68d3742fe40eb1f5ec03b0b7f
Tangem <git@tangem.com>
2020-03-26 23:13:15 +0300

    Updated on 2026-06-01

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/paramsManager/managers/ScanParamsManager.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/paramsManager/managers/ScanParamsManager.kt
@@ -1,17 +1,18 @@
 package com.tangem.tangemtest.ucase.domain.paramsManager.managers
 
 import com.tangem.CardManager
-import com.tangem.common.tlv.TlvTag
+import com.tangem.tangemtest._arch.structure.abstraction.Item
+import com.tangem.tangemtest._arch.structure.impl.EditTextItem
 import com.tangem.tangemtest.ucase.domain.actions.ScanAction
 import com.tangem.tangemtest.ucase.domain.paramsManager.ActionCallback
-import com.tangem.tangemtest.ucase.domain.paramsManager.IncomingParameter
+import com.tangem.tangemtest.ucase.variants.TlvId
 
 /**
 [REDACTED_AUTHOR]
  */
 class ScanParamsManager : BaseParamsManager(ScanAction()) {
-    override fun createParamsList(): List<IncomingParameter> {
-        return listOf(IncomingParameter(TlvTag.CardId, null))
+    override fun createParamsList(): List<Item> {
+        return listOf(EditTextItem(TlvId.CardId, null))
     }
 
     override fun invokeMainAction(cardManager: CardManager, callback: ActionCallback) {
