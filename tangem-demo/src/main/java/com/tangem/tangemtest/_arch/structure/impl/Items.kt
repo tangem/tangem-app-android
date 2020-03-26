317b1c3075a618b4fcadc6191d7cd80ec6ecbf97
Tangem <git@tangem.com>
2020-03-26 03:53:41 +0300

    Updated on 2026-05-07

--- tangem-demo/src/main/java/com/tangem/tangemtest/_arch/structure/impl/Items.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/_arch/structure/impl/Items.kt
@@ -3,13 +3,15 @@ package com.tangem.tangemtest._arch.structure.impl
 import com.tangem.tangemtest._arch.structure.Id
 import com.tangem.tangemtest._arch.structure.abstraction.BaseItem
 
-
+/**
+[REDACTED_AUTHOR]
+ */
 class AnyItem(override val id: Id, value: Any? = null) : BaseItem<Any>(AnyViewModel(value))
 class TextItem(override val id: Id, value: String? = null) : BaseItem<String>(StringViewModel(value))
 class EditTextItem(override val id: Id, value: String? = null) : BaseItem<String>(StringViewModel(value))
 class NumberItem(override val id: Id, value: Number? = null) : BaseItem<Number>(NumberViewModel(value))
 class BoolItem(override val id: Id, value: Boolean? = null) : BaseItem<Boolean>(BoolViewModel(value))
 
-class ListItem(override val id: Id, value: List<KeyValue>, selectedValue: Any)
+class ListItem(override val id: Id, value: List<KeyValue>, selectedValue: Any?)
     : BaseItem<ListValueWrapper>(ListViewModel(ListValueWrapper(selectedValue, value))
 )
