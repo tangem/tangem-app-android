141dba522f8a5a11464689af2aad82d2c3148a85
Tangem <git@tangem.com>
2020-03-29 18:19:13 +0300

    Updated on 2025-09-17

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/paramsManager/managers/PersonalizeParamsManager.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/paramsManager/managers/PersonalizeParamsManager.kt
@@ -2,15 +2,17 @@ package com.tangem.tangemtest.ucase.domain.paramsManager.managers
 
 import com.tangem.CardManager
 import com.tangem.tangemtest._arch.structure.abstraction.Item
+import com.tangem.tangemtest._arch.structure.impl.EditTextItem
 import com.tangem.tangemtest.ucase.domain.actions.PersonalizeAction
 import com.tangem.tangemtest.ucase.domain.paramsManager.ActionCallback
+import com.tangem.tangemtest.ucase.variants.TlvId
 
 /**
 [REDACTED_AUTHOR]
  */
 class PersonalizeParamsManager : BaseParamsManager(PersonalizeAction()) {
     override fun createParamsList(): List<Item> {
-        return listOf()
+        return listOf(EditTextItem(TlvId.CardId))
     }
 
     override fun invokeMainAction(cardManager: CardManager, callback: ActionCallback) {
