106afb58885c0f30b15cc038ee209e22e5a5637f
Tangem <git@tangem.com>
2020-03-29 17:20:18 +0300

    Updated on 2025-12-22

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/personalize/ui/PersonalizeFragment.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/personalize/ui/PersonalizeFragment.kt
@@ -1,6 +1,5 @@
 package com.tangem.tangemtest.ucase.variants.personalize.ui
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -13,7 +12,10 @@ import androidx.lifecycle.Observer
 import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
 import com.tangem.CardManager
-import com.tangem.tangem_sdk_new.extensions.init
+import com.tangem.tangem_sdk_new.DefaultCardManagerDelegate
+import com.tangem.tangem_sdk_new.NfcLifecycleObserver
+import com.tangem.tangem_sdk_new.TerminalKeysStorage
+import com.tangem.tangem_sdk_new.nfc.NfcManager
 import com.tangem.tangemtest.R
 import com.tangem.tangemtest._arch.structure.abstraction.Block
 import com.tangem.tangemtest._main.MainViewModel
@@ -23,12 +25,11 @@ import com.tangem.tangemtest.ucase.domain.paramsManager.ParamsManagerFactory
 import com.tangem.tangemtest.ucase.resources.ActionType
 import com.tangem.tangemtest.ucase.ui.ActionViewModelFactory
 import com.tangem.tangemtest.ucase.ui.ParamsViewModel
+import com.tangem.tangemtest.ucase.variants.personalize.dto.CardManagerConfig
+import com.tangem.tangemtest.ucase.variants.personalize.dto.PersonalizeConfig
 import com.tangem.tangemtest.ucase.variants.personalize.ui.widgets.WidgetBuilder
 import kotlinx.android.synthetic.main.fg_personalize.*
 import ru.dev.gbixahue.eu4d.lib.android.global.log.Log
-import java.io.IOException
-import java.io.InputStream
-import java.nio.charset.Charset
 
 /**
 [REDACTED_AUTHOR]
@@ -36,7 +37,7 @@ import java.nio.charset.Charset
 class PersonalizeFragment : Fragment() {
 
     private val mainActivityVM: MainViewModel by activityViewModels()
-    private val personalizeVM: PersonalizeViewModel by viewModels { PersonalizeViewModelFactory(getPersonalizeJson()) }
+    private val personalizeVM: PersonalizeViewModel by viewModels { PersonalizeViewModelFactory(PersonalizeConfig()) }
 
     private val paramsManager: ParamsManager by lazy { ParamsManagerFactory.createFactory().get(ActionType.Personalize)!! }
     private val paramsVM: ParamsViewModel by viewModels() { ActionViewModelFactory(paramsManager) }
@@ -55,6 +56,8 @@ class PersonalizeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val cardManager = initCardManager() ?: return
+
         val blockList = mutableListOf<Block>()
         personalizeVM.ldBlockList.observe(viewLifecycleOwner, Observer { list ->
             blockList.clear()
@@ -65,7 +68,7 @@ class PersonalizeFragment : Fragment() {
             personalizeVM.toggleDescriptionVisibility(it)
         })
 
-        paramsVM.setCardManager(CardManager.init(requireActivity()))
+        paramsVM.setCardManager(cardManager)
         paramsVM.ldResponse.observe(viewLifecycleOwner, Observer {
             Log.d(this, "action response: ${if (it.length > 50) it.substring(0..50) else it}")
             showSnackbarMessage(it)
@@ -78,27 +81,37 @@ class PersonalizeFragment : Fragment() {
         }
     }
 
+    private fun initCardManager(): CardManager? {
+        val activity = activity ?: return null
+        val nfcManager = NfcManager().apply {
+            this.setCurrentActivity(activity)
+            activity.lifecycle.addObserver(NfcLifecycleObserver(this))
+        }
+        val cardManagerDelegate = DefaultCardManagerDelegate(nfcManager.reader).apply {
+            this.activity = activity
+        }
+        return CardManager(nfcManager.reader, cardManagerDelegate, CardManagerConfig.default()).apply {
+            this.setTerminalKeysService(TerminalKeysStorage(activity.application))
+        }
+    }
+
     protected fun showSnackbarMessage(message: String) {
         Snackbar.make(mainView, message, BaseTransientBottomBar.LENGTH_SHORT).show()
     }
 
-    private fun getPersonalizeJson(): String {
-        return getJsonFromAssets(requireContext(), "personalize_default.json") ?: "[]"
-    }
-
-    private fun getJsonFromAssets(context: Context, fileName: String): String? {
-        return try {
-            val stream: InputStream = context.assets.open(fileName)
-            val size: Int = stream.available()
-            val buffer = ByteArray(size)
-            stream.read(buffer)
-            stream.close()
-            String(buffer, Charset.forName("UTF-8"))
-        } catch (e: IOException) {
-            e.printStackTrace()
-            null
-        }
-    }
+//    private fun getJsonFromAssets(context: Context, fileName: String): String? {
+//        return try {
+//            val stream: InputStream = context.assets.open(fileName)
+//            val size: Int = stream.available()
+//            val buffer = ByteArray(size)
+//            stream.read(buffer)
+//            stream.close()
+//            String(buffer, Charset.forName("UTF-8"))
+//        } catch (e: IOException) {
+//            e.printStackTrace()
+//            null
+//        }
+//    }
 }
 
 
