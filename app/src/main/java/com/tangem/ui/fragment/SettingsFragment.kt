e4304f1680b2956a8572841d215941464d7087d1
Tangem <git@tangem.com>
2020-03-31 18:02:43 +0300

    Updated on 2026-05-19

--- app/src/main/java/com/tangem/ui/fragment/SettingsFragment.kt
+++ app/src/main/java/com/tangem/ui/fragment/SettingsFragment.kt
@@ -1,18 +1,24 @@
 package com.tangem.ui.fragment
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
 import androidx.navigation.fragment.NavHostFragment
 import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceCategory
 import androidx.preference.PreferenceFragmentCompat
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import com.tangem.util.Analytics
+import com.tangem.util.AnalyticsEvent
 import com.tangem.wallet.BuildConfig
 import com.tangem.wallet.R
 import kotlinx.android.synthetic.main.fragment_settings.*
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.pref_main)
 
@@ -38,6 +44,32 @@ class SettingsFragment : PreferenceFragmentCompat() {
         toolbar?.setNavigationOnClickListener {
             NavHostFragment.findNavController(this).popBackStack()
         }
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        if (key == getString(R.string.pref_analytics)) {
+            activity?.let {
+                val analytics = FirebaseAnalytics.getInstance(it)
+                if (!Analytics.isEnabled()) {
+                    analytics.logEvent(AnalyticsEvent.ANALYTICS_TURNED_OFF.event, bundleOf())
+                }
+                analytics.setAnalyticsCollectionEnabled(Analytics.isEnabled())
+            }
+            FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(Analytics.isEnabled())
+
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        preferenceScreen.sharedPreferences
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preferenceScreen.sharedPreferences
+                .unregisterOnSharedPreferenceChangeListener(this);
     }
 
     override fun onDestroyView() {
