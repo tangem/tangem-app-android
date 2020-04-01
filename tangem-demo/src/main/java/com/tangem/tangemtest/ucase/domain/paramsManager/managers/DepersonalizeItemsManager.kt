0311ebbf24aba619b74adda8fddaf85ab0bb42e5
Tangem <git@tangem.com>
2020-04-01 14:57:53 +0300

    Updated on 2026-04-06

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/paramsManager/managers/DepersonalizeItemsManager.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/paramsManager/managers/DepersonalizeItemsManager.kt
@@ -4,26 +4,23 @@ import com.tangem.CardManager
 import com.tangem.tangemtest._arch.structure.Id
 import com.tangem.tangemtest._arch.structure.abstraction.Item
 import com.tangem.tangemtest._arch.structure.impl.EditTextItem
-import com.tangem.tangemtest.ucase.domain.actions.SignAction
+import com.tangem.tangemtest.ucase.domain.actions.DepesonalizeAction
 import com.tangem.tangemtest.ucase.domain.paramsManager.ActionCallback
 import com.tangem.tangemtest.ucase.variants.TlvId
 
 /**
 [REDACTED_AUTHOR]
  */
-class SignParamsManager : BaseParamsManager(SignAction()) {
-    override fun createParamsList(): List<Item> {
-        return listOf(
-                EditTextItem(TlvId.CardId, null),
-                EditTextItem(TlvId.TransactionOutHash, "Data used for hashing")
-        )
+class DepersonalizeItemsManager : BaseItemsManager(DepesonalizeAction()) {
+    override fun createItemsList(): List<Item> {
+        return listOf(EditTextItem(TlvId.CardId, null))
     }
 
     override fun invokeMainAction(cardManager: CardManager, callback: ActionCallback) {
-        action.executeMainAction(getAttrsForAction(cardManager), callback)
+        action.executeMainAction(this, getAttrsForAction(cardManager), callback)
     }
 
     override fun getActionByTag(id: Id, cardManager: CardManager): ((ActionCallback) -> Unit)? {
-        return action.getActionByTag(id, getAttrsForAction(cardManager))
+        return action.getActionByTag(this, id, getAttrsForAction(cardManager))
     }
 }
