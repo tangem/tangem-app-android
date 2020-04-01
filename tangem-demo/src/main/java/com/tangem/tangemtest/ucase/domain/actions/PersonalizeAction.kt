7b330044278e6b04abff47fac6981c05b9f89383
Tangem <git@tangem.com>
2020-04-01 14:57:53 +0300

    Updated on 2026-04-15

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/actions/PersonalizeAction.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/actions/PersonalizeAction.kt
@@ -1,15 +1,17 @@
 package com.tangem.tangemtest.ucase.domain.actions
 
 import com.tangem.commands.personalization.entities.CardConfig
+import com.tangem.tangemtest._arch.structure.PayloadHolder
 import com.tangem.tangemtest.ucase.domain.paramsManager.ActionCallback
+import com.tangem.tangemtest.ucase.domain.paramsManager.PayloadKey
 import com.tangem.tangemtest.ucase.variants.personalize.dto.DefaultPersonalizeParams
 
 /**
 [REDACTED_AUTHOR]
  */
-class PersonalizeAction : BaseCardAction() {
-    override fun executeMainAction(attrs: AttrForAction, callback: ActionCallback) {
-        val cardConfig = attrs.payload.remove(cardConfig) as? CardConfig
+class PersonalizeAction : BaseAction() {
+    override fun executeMainAction(payload: PayloadHolder, attrs: AttrForAction, callback: ActionCallback) {
+        val cardConfig = attrs.payload.remove(PayloadKey.CardConfig) as? CardConfig
                 ?: throw IllegalArgumentException("CardConfig must be in the payloads of the ParamsManager")
 
         val issuer = DefaultPersonalizeParams.issuer()
@@ -17,11 +19,7 @@ class PersonalizeAction : BaseCardAction() {
         val manufacturer = DefaultPersonalizeParams.manufacturer()
 
         attrs.cardManager.personalize(cardConfig, issuer, manufacturer, acquirer) {
-            handleResponse(it, null, attrs, callback)
+            handleResult(payload, it, null, attrs, callback)
         }
-    }
-
-    companion object {
-        val cardConfig = "cardConfig"
     }
 }
--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/paramsManager/triggers/afterAction/AfterScanModifier.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/paramsManager/triggers/afterAction/AfterScanModifier.kt
@@ -1,7 +1,9 @@
 package com.tangem.tangemtest.ucase.domain.paramsManager.triggers.afterAction
 
+import com.tangem.tangemtest._arch.structure.PayloadHolder
 import com.tangem.tangemtest._arch.structure.abstraction.Item
-import com.tangem.tangemtest.ucase.domain.paramsManager.findDataParameter
+import com.tangem.tangemtest.ucase.domain.paramsManager.PayloadKey
+import com.tangem.tangemtest.ucase.domain.paramsManager.findDataItem
 import com.tangem.tangemtest.ucase.variants.TlvId
 import com.tangem.tasks.ScanEvent
 import com.tangem.tasks.TaskEvent
@@ -10,12 +12,14 @@ import com.tangem.tasks.TaskEvent
 [REDACTED_AUTHOR]
  */
 class AfterScanModifier : AfterActionModification {
-    override fun modify(taskEvent: TaskEvent<*>, paramsList: List<Item>): List<Item> {
-        val parameter = paramsList.findDataParameter(TlvId.CardId) ?: return listOf()
+    override fun modify(payload: PayloadHolder, taskEvent: TaskEvent<*>, itemList: List<Item>): List<Item> {
+        val foundItem = itemList.findDataItem(TlvId.CardId) ?: return listOf()
 
         return if (taskEvent is TaskEvent.Event && taskEvent.data is ScanEvent.OnReadEvent) {
-            parameter.viewModel.data = (taskEvent.data as ScanEvent.OnReadEvent).card.cardId
-            listOf(parameter)
+            val card = (taskEvent.data as ScanEvent.OnReadEvent).card
+            payload.set(PayloadKey.Card, card)
+            foundItem.setData(card.cardId)
+            listOf(foundItem)
         } else listOf()
     }
 
