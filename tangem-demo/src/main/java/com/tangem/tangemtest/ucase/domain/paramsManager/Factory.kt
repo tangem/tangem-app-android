8b1440146c9181a2dc16d971f3241ecfdcdca36b
Tangem <git@tangem.com>
2020-04-01 14:57:53 +0300

    Updated on 2026-08-14

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/paramsManager/Factory.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/paramsManager/Factory.kt
@@ -1,24 +1,24 @@
 package com.tangem.tangemtest.ucase.domain.paramsManager
 
-import com.tangem.tangemtest.ucase.domain.paramsManager.managers.DepersonalizeParamsManager
-import com.tangem.tangemtest.ucase.domain.paramsManager.managers.PersonalizeParamsManager
-import com.tangem.tangemtest.ucase.domain.paramsManager.managers.ScanParamsManager
-import com.tangem.tangemtest.ucase.domain.paramsManager.managers.SignParamsManager
+import com.tangem.tangemtest.ucase.domain.paramsManager.managers.DepersonalizeItemsManager
+import com.tangem.tangemtest.ucase.domain.paramsManager.managers.PersonalizeItemsManager
+import com.tangem.tangemtest.ucase.domain.paramsManager.managers.ScanItemsManager
+import com.tangem.tangemtest.ucase.domain.paramsManager.managers.SignItemsManager
 import com.tangem.tangemtest.ucase.resources.ActionType
 import ru.dev.gbixahue.eu4d.lib.kotlin.common.BaseTypedHolder
 
 /**
 [REDACTED_AUTHOR]
  */
-class ParamsManagerFactory : BaseTypedHolder<ActionType, ParamsManager>() {
+class ParamsManagerFactory : BaseTypedHolder<ActionType, ItemsManager>() {
 
     companion object {
         fun createFactory(): ParamsManagerFactory {
             return ParamsManagerFactory().apply {
-                register(ActionType.Scan, ScanParamsManager())
-                register(ActionType.Sign, SignParamsManager())
-                register(ActionType.Personalize, PersonalizeParamsManager())
-                register(ActionType.Depersonalize, DepersonalizeParamsManager())
+                register(ActionType.Scan, ScanItemsManager())
+                register(ActionType.Sign, SignItemsManager())
+                register(ActionType.Personalize, PersonalizeItemsManager())
+                register(ActionType.Depersonalize, DepersonalizeItemsManager())
             }
         }
     }
