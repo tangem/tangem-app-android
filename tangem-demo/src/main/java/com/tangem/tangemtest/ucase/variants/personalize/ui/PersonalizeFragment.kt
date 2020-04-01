1357bf92b2b603d4aab658331a647de339b827a0
Tangem <git@tangem.com>
2020-04-01 14:57:53 +0300

    Updated on 2025-12-30

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/personalize/ui/PersonalizeFragment.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/personalize/ui/PersonalizeFragment.kt
@@ -16,9 +16,9 @@ import com.tangem.tangem_sdk_new.extensions.init
 import com.tangem.tangemtest.R
 import com.tangem.tangemtest._arch.structure.abstraction.Block
 import com.tangem.tangemtest._main.MainViewModel
-import com.tangem.tangemtest.ucase.domain.actions.PersonalizeAction
-import com.tangem.tangemtest.ucase.domain.paramsManager.ParamsManager
+import com.tangem.tangemtest.ucase.domain.paramsManager.ItemsManager
 import com.tangem.tangemtest.ucase.domain.paramsManager.ParamsManagerFactory
+import com.tangem.tangemtest.ucase.domain.paramsManager.PayloadKey
 import com.tangem.tangemtest.ucase.resources.ActionType
 import com.tangem.tangemtest.ucase.ui.ActionViewModelFactory
 import com.tangem.tangemtest.ucase.ui.ParamsViewModel
@@ -35,8 +35,8 @@ class PersonalizeFragment : Fragment() {
     private val mainActivityVM: MainViewModel by activityViewModels()
     private val personalizeVM: PersonalizeViewModel by viewModels { PersonalizeViewModelFactory(PersonalizeConfig()) }
 
-    private val paramsManager: ParamsManager by lazy { ParamsManagerFactory.createFactory().get(ActionType.Personalize)!! }
-    private val paramsVM: ParamsViewModel by viewModels() { ActionViewModelFactory(paramsManager) }
+    private val itemsManager: ItemsManager by lazy { ParamsManagerFactory.createFactory().get(ActionType.Personalize)!! }
+    private val paramsVM: ParamsViewModel by viewModels() { ActionViewModelFactory(itemsManager) }
 
     private val blockContainer: ViewGroup by lazy {
         mainView.findViewById<LinearLayout>(R.id.ll_container)
@@ -72,41 +72,13 @@ class PersonalizeFragment : Fragment() {
 
         fab_action?.setOnClickListener {
             val cardConfig = personalizeVM.createCardConfig(personalizeVM.createConfig(blockList))
-            paramsVM.invokeMainAction(mutableMapOf(PersonalizeAction.cardConfig to cardConfig))
+            paramsVM.invokeMainAction(mutableMapOf(PayloadKey.CardConfig to cardConfig))
         }
     }
-
-//    private fun initCardManager(): CardManager? {
-//        val activity = activity ?: return null
-//        val nfcManager = NfcManager().apply {
-//            this.setCurrentActivity(activity)
-//            activity.lifecycle.addObserver(NfcLifecycleObserver(this))
-//        }
-//        val cardManagerDelegate = DefaultCardManagerDelegate(nfcManager.reader).apply {
-//            this.activity = activity
-//        }
-//        return CardManager(nfcManager.reader, cardManagerDelegate, DefaultPersonalizeParams.default()).apply {
-//            this.setTerminalKeysService(TerminalKeysStorage(activity.application))
-//        }
-//    }
 
     protected fun showSnackbarMessage(message: String) {
         Snackbar.make(mainView, message, BaseTransientBottomBar.LENGTH_SHORT).show()
     }
-
-//    private fun getJsonFromAssets(context: Context, fileName: String): String? {
-//        return try {
-//            val stream: InputStream = context.assets.open(fileName)
-//            val size: Int = stream.available()
-//            val buffer = ByteArray(size)
-//            stream.read(buffer)
-//            stream.close()
-//            String(buffer, Charset.forName("UTF-8"))
-//        } catch (e: IOException) {
-//            e.printStackTrace()
-//            null
-//        }
-//    }
 }
 
 
