ddcf8e0820f34757473a8bf040581d6b2d7ce005
Tangem <git@tangem.com>
2020-04-01 10:33:50 +0300

    Updated on 2026-03-27

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/actions/PersonalizeAction.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/actions/PersonalizeAction.kt
@@ -1,46 +1,23 @@
 package com.tangem.tangemtest.ucase.domain.actions
 
-import com.tangem.commands.personalization.CardConfig
-import com.tangem.tangemtest._arch.structure.impl.EditTextItem
-import com.tangem.tangemtest._arch.structure.impl.getData
+import com.tangem.commands.personalization.entities.CardConfig
 import com.tangem.tangemtest.ucase.domain.paramsManager.ActionCallback
-import com.tangem.tangemtest.ucase.variants.TlvId
-import com.tangem.tasks.TaskEvent
-import ru.dev.gbixahue.eu4d.lib.android.global.log.Log
+import com.tangem.tangemtest.ucase.variants.personalize.dto.DefaultPersonalizeParams
 
 /**
 [REDACTED_AUTHOR]
  */
 class PersonalizeAction : BaseCardAction() {
     override fun executeMainAction(attrs: AttrForAction, callback: ActionCallback) {
-        var cardId: String? = null
-        ScanAction().executeMainAction(attrs) { response, affectedList ->
-            when (response) {
-                is TaskEvent.Event -> {
-                    Log.d(this, "ScanAction:response: TaskEvent.Event")
-                    val item = affectedList.firstOrNull { it.id == TlvId.CardId } as? EditTextItem
-                            ?: return@executeMainAction
-                    cardId = item.getData()
-                    if (cardId == null) {
-                        handleResponse(response, null, attrs, callback)
-                        return@executeMainAction
-                    }
-                }
-                is TaskEvent.Completion -> {
-                    Log.d(this, "ScanAction:response: TaskEvent.Completion")
-                    if (cardId == null) {
-                        handleResponse(response, null, attrs, callback)
-                        return@executeMainAction
-                    }
+        val cardConfig = attrs.payload.remove(cardConfig) as? CardConfig
+                ?: throw IllegalArgumentException("CardConfig must be in the payloads of the ParamsManager")
 
-                    val cardConfig = attrs.payload.remove(cardConfig) as? CardConfig
-                            ?: throw IllegalArgumentException("CardConfig must be in the payloads of the ParamsManager")
+        val issuer = DefaultPersonalizeParams.issuer()
+        val acquirer = DefaultPersonalizeParams.acquirer()
+        val manufacturer = DefaultPersonalizeParams.manufacturer()
 
-                    attrs.cardManager.personalize(cardConfig, cardId!!) {
-                        handleResponse(it, null, attrs, callback)
-                    }
-                }
-            }
+        attrs.cardManager.personalize(cardConfig, issuer, manufacturer, acquirer) {
+            handleResponse(it, null, attrs, callback)
         }
     }
 
