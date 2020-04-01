f27f726a1799c6d5206808e32727b6c94cd9d87a
Tangem <git@tangem.com>
2020-04-01 14:57:53 +0300

    Updated on 2026-09-23

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/actions/SignAction.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/actions/SignAction.kt
@@ -1,32 +1,28 @@
 package com.tangem.tangemtest.ucase.domain.actions
 
 import com.tangem.tangemtest._arch.structure.Id
+import com.tangem.tangemtest._arch.structure.PayloadHolder
 import com.tangem.tangemtest.ucase.domain.paramsManager.ActionCallback
-import com.tangem.tangemtest.ucase.domain.paramsManager.findDataParameter
+import com.tangem.tangemtest.ucase.domain.paramsManager.findDataItem
 import com.tangem.tangemtest.ucase.variants.TlvId
 import ru.dev.gbixahue.eu4d.lib.kotlin.stringOf
 
 /**
 [REDACTED_AUTHOR]
  */
-class SignAction : BaseCardAction() {
-    override fun executeMainAction(attrs: AttrForAction, callback: ActionCallback) {
-        val dataForHashing = attrs.paramsList.findDataParameter(TlvId.TransactionOutHash) ?: return
+class SignAction : BaseAction() {
+    override fun executeMainAction(payload: PayloadHolder, attrs: AttrForAction, callback: ActionCallback) {
+        val dataForHashing = attrs.itemList.findDataItem(TlvId.TransactionOutHash) ?: return
+        val hash = dataForHashing.getData() as? ByteArray ?: return
+        val cardId = attrs.itemList.findDataItem(TlvId.CardId)?.viewModel?.data ?: return
 
-        val arHashes = createHashes(stringOf(dataForHashing.viewModel.data))
-        val cardId = attrs.paramsList.findDataParameter(TlvId.CardId)?.viewModel?.data ?: return
-
-        attrs.cardManager.sign(arHashes, stringOf(cardId)) { handleResponse(it, null, attrs, callback) }
+        attrs.cardManager.sign(arrayOf(hash), stringOf(cardId)) { handleResult(payload, it, null, attrs, callback) }
     }
 
-    override fun getActionByTag(id: Id, attrs: AttrForAction): ((ActionCallback) -> Unit)? {
+    override fun getActionByTag(payload: PayloadHolder, id: Id, attrs: AttrForAction): ((ActionCallback) -> Unit)? {
         return when (id) {
-            TlvId.CardId -> { callback -> ScanAction().executeMainAction(attrs, callback) }
+            TlvId.CardId -> { callback -> ScanAction().executeMainAction(payload, attrs, callback) }
             else -> null
         }
-    }
-
-    private fun createHashes(value: String): Array<ByteArray> {
-        return arrayOf(value.toByteArray())
     }
 }
