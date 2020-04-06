1ab57a5228a546c8b1f5e13939e57d5daffccbb0
Tangem <git@tangem.com>
2020-04-06 16:41:27 +0300

    release/5.26.1

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/resources/initializers/PersonalizeResources.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/resources/initializers/PersonalizeResources.kt
@@ -87,8 +87,8 @@ class PersonalizeResources() {
     private fun initProductMask(holder: TypedHolder<Id, Resources>) {
         holder.register(ProductMask.Note, Resources(R.string.pers_item_note, R.string.info_pers_item_note))
         holder.register(ProductMask.Tag, Resources(R.string.pers_item_tag, R.string.info_pers_item_tag))
-        holder.register(ProductMask.CardId, Resources(R.string.pers_item_id_card, R.string.info_pers_item_id_card))
-        holder.register(ProductMask.IssuerId, Resources(R.string.pers_item_id_issuer, R.string.info_pers_item_id_issuer))
+        holder.register(ProductMask.IdCard, Resources(R.string.pers_item_id_card, R.string.info_pers_item_id_card))
+        holder.register(ProductMask.IdIssuer, Resources(R.string.pers_item_id_issuer, R.string.info_pers_item_id_issuer))
     }
 
     private fun initSettingsMask(holder: TypedHolder<Id, Resources>) {
