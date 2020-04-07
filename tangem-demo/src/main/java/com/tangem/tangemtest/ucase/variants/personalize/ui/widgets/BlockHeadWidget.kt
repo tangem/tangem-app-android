4377ad03c88f2ac4d14076f58d8b5bf0ca2911ae
Tangem <git@tangem.com>
2020-04-07 16:13:02 +0300

    Updated on 2025-09-17

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/personalize/ui/widgets/BlockHeadWidget.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/personalize/ui/widgets/BlockHeadWidget.kt
@@ -1,15 +1,15 @@
-package com.tangem.tangemtest.ucase.variants.personalize.ui.widgets.impl.item
+package com.tangem.tangemtest.ucase.variants.personalize.ui.widgets
 
 import android.view.ViewGroup
 import android.widget.TextView
 import com.tangem.tangemtest.R
 import com.tangem.tangemtest._arch.structure.impl.TextItem
-import com.tangem.tangemtest.ucase.variants.personalize.ui.widgets.abstraction.getResNameId
+import com.tangem.tangemtest._arch.widget.abstraction.getResNameId
 
 /**
 [REDACTED_AUTHOR]
  */
-class TextWidget(parent: ViewGroup, data: TextItem) : DescriptionWidget<String>(parent, data) {
+class BlockHeadWidget(parent: ViewGroup, data: TextItem) : DescriptionWidget<String>(parent, data) {
     override fun getLayoutId(): Int = R.layout.w_personalize_item_text
 
     private val tvName = view.findViewById<TextView>(R.id.tv_name)
