e2aa59ce4e0c9582a4f869e59cb1643ada3fe67b
Tangem <git@tangem.com>
2020-04-08 00:07:11 +0300

    Updated on 2026-05-07

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/paramsManager/managers/PersonalizeItemsManager.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/paramsManager/managers/PersonalizeItemsManager.kt
@@ -4,12 +4,14 @@ import com.tangem.CardManager
 import com.tangem.tangemtest._arch.structure.abstraction.Item
 import com.tangem.tangemtest.ucase.domain.actions.PersonalizeAction
 import com.tangem.tangemtest.ucase.domain.paramsManager.ActionCallback
+import com.tangem.tangemtest.ucase.variants.personalize.converter.PersonalizeConfigConverter
+import com.tangem.tangemtest.ucase.variants.personalize.dto.PersonalizeConfig
 
 /**
 [REDACTED_AUTHOR]
  */
 class PersonalizeItemsManager : BaseItemsManager(PersonalizeAction()) {
-    override fun createItemsList(): List<Item> = listOf()
+    override fun createItemsList(): List<Item> = PersonalizeConfigConverter().convert(PersonalizeConfig())
 
     override fun invokeMainAction(cardManager: CardManager, callback: ActionCallback) {
         action.executeMainAction(this, getAttrsForAction(cardManager), callback)
