5cba1dbf445f8ff515d611e18fc0186e0f66fafc
Tangem <git@tangem.com>
2020-04-08 00:50:00 +0300

    Updated on 2025-12-18

--- tangem-demo/src/main/java/com/tangem/tangemtest/_main/MainViewModel.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/_main/MainViewModel.kt
@@ -2,14 +2,24 @@ package com.tangem.tangemtest._main
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.tangem.tasks.TaskEvent
+import ru.dev.gbixahue.eu4d.lib.android.global.log.Log
 
 /**
 [REDACTED_AUTHOR]
  */
 class MainViewModel : ViewModel() {
     val ldDescriptionSwitch = MutableLiveData<Boolean>(false)
+    val ldResponseEvent = MutableLiveData<TaskEvent<*>>()
 
     fun switchToggled(state: Boolean) {
         ldDescriptionSwitch.postValue(state)
+    }
+
+    fun changeResponseEvent(event: TaskEvent<*>?) {
+        Log.d(this, "changeResponseEvent")
+        val reqEvent = event ?: return
+
+        ldResponseEvent.value = reqEvent
     }
 }
