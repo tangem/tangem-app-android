d21e0b35f36cfe23925088de49430ebe11b1931a
Tangem <git@tangem.com>
2020-04-08 00:07:11 +0300

    Updated on 2025-10-01

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/personalize/ui/PersonalizeFragment.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/personalize/ui/PersonalizeFragment.kt
@@ -1,112 +1,62 @@
 package com.tangem.tangemtest.ucase.variants.personalize.ui
 
-import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.tangem.CardManager
-import com.tangem.tangem_sdk_new.extensions.init
 import com.tangem.tangemtest.R
 import com.tangem.tangemtest._arch.structure.Id
 import com.tangem.tangemtest._arch.structure.abstraction.Item
 import com.tangem.tangemtest._arch.widget.WidgetBuilder
-import com.tangem.tangemtest._main.MainViewModel
-import com.tangem.tangemtest.ucase.domain.paramsManager.ItemsManager
-import com.tangem.tangemtest.ucase.domain.paramsManager.ParamsManagerFactory
 import com.tangem.tangemtest.ucase.domain.paramsManager.PayloadKey
 import com.tangem.tangemtest.ucase.resources.ActionType
 import com.tangem.tangemtest.ucase.tunnel.ActionView
 import com.tangem.tangemtest.ucase.tunnel.CardError
 import com.tangem.tangemtest.ucase.tunnel.ItemError
-import com.tangem.tangemtest.ucase.ui.ActionViewModelFactory
-import com.tangem.tangemtest.ucase.ui.BaseFragment
-import com.tangem.tangemtest.ucase.ui.ParamsViewModel
-import com.tangem.tangemtest.ucase.variants.personalize.dto.PersonalizeConfig
+import com.tangem.tangemtest.ucase.ui.BaseCardActionFragment
 import com.tangem.tangemtest.ucase.variants.personalize.ui.widgets.PersonalizeItemBuilder
 import ru.dev.gbixahue.eu4d.lib.android.global.log.Log
 
 /**
 [REDACTED_AUTHOR]
  */
-class PersonalizeFragment : BaseFragment(), ActionView {
-
-    private lateinit var itemContainer: ViewGroup
-    private lateinit var actionFab: FloatingActionButton
-
-    private val mainActivityVM: MainViewModel by activityViewModels()
-    private val personalizeVM: PersonalizeViewModel by viewModels { PersonalizeViewModelFactory(PersonalizeConfig()) }
-    private val paramsVM: ParamsViewModel by viewModels() { ActionViewModelFactory(itemsManager) }
-
-    private val itemsManager: ItemsManager by lazy { ParamsManagerFactory.createFactory().get(ActionType.Personalize)!! }
+class PersonalizeFragment : BaseCardActionFragment(), ActionView {
 
     override fun getLayoutId(): Int = R.layout.fg_personalize
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        itemContainer = mainView.findViewById(R.id.ll_container)
-        actionFab = mainView.findViewById(R.id.fab_action)
-
-        paramsVM.setCardManager(CardManager.init(requireActivity()))
-        paramsVM.attachToPayload(mutableMapOf(PayloadKey.actionView to this as ActionView))
-
-        initFab()
-        createWidgets { subscribeToViewModelChanges() }
-    }
-
-    private fun initFab() {
+    override fun initFab() {
         actionFab.setOnClickListener { paramsVM.invokeMainAction() }
     }
 
-    private fun createWidgets(widgetCreatedCallback: () -> Unit) {
+    override fun createWidgets(widgetCreatedCallback: () -> Unit) {
         Log.d(this, "createWidgets")
-        val blockList = mutableListOf<Item>()
-        personalizeVM.ldBlockList.observe(viewLifecycleOwner, Observer { list ->
+        val itemList = mutableListOf<Item>()
+
+        paramsVM.ldItemList.observe(viewLifecycleOwner, Observer { list ->
             Log.d(this, "ldBlockList size: ${list.size}")
-            blockList.clear()
-            blockList.addAll(list)
-            blockList.forEach { WidgetBuilder(PersonalizeItemBuilder()).build(it, itemContainer) }
+            itemList.clear()
+            itemList.addAll(list)
+            itemList.forEach { WidgetBuilder(PersonalizeItemBuilder()).build(it, itemContainer) }
             paramsVM.attachToPayload(mutableMapOf(
                     PayloadKey.actionView to this as ActionView,
-                    PayloadKey.itemList to blockList
+                    PayloadKey.itemList to itemList
             ))
             widgetCreatedCallback()
         })
     }
 
-    private fun subscribeToViewModelChanges() {
+    override fun subscribeToViewModelChanges() {
         Log.d(this, "subscribeToViewModelChanges")
         listenEvent()
         listenError()
         listenDescriptionSwitchChanges()
     }
 
-    private fun listenEvent() {
+    override fun listenEvent() {
         paramsVM.seResponseEvent.observe(viewLifecycleOwner, Observer {
             mainActivityVM.changeResponseEvent(it)
             navigateTo(R.id.action_nav_card_action_to_response_screen)
         })
     }
 
-    private fun listenError() {
-        paramsVM.seError.observe(viewLifecycleOwner, Observer { showSnackbar(it) })
-
-    }
-
-    private fun listenDescriptionSwitchChanges() {
-        mainActivityVM.ldDescriptionSwitch.observe(viewLifecycleOwner, Observer {
-            personalizeVM.toggleDescriptionVisibility(it)
-        })
-    }
-
-    override fun enableActionFab(enable: Boolean) {
-        if (enable) actionFab.show() else actionFab.hide()
-    }
-
     override fun showSnackbar(id: Id) {
-//        MainResourceHolder.safeGet<>()
         when (id) {
             CardError.NotPersonalized -> showSnackbar(R.string.card_error_not_personalized)
             ItemError.BadSeries -> showSnackbar(R.string.card_error_bad_series)
@@ -114,6 +64,8 @@ class PersonalizeFragment : BaseFragment(), ActionView {
             else -> showSnackbar(requireContext().getString(R.string.unknown))
         }
     }
+
+    override fun getAction(): ActionType = ActionType.Personalize
 }
 
 
