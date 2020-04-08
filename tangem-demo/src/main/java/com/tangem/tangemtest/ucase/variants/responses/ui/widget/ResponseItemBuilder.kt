9637a262f54e0835ea289d64e87b17af11ba7383
Tangem <git@tangem.com>
2020-04-08 23:04:48 +0300

    Updated on 2025-12-22

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/responses/ui/widget/ResponseItemBuilder.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/responses/ui/widget/ResponseItemBuilder.kt
@@ -1,11 +1,11 @@
-package com.tangem.tangemtest.ucase.ui
+package com.tangem.tangemtest.ucase.variants.responses.ui.widget
 
 import android.view.ViewGroup
 import com.tangem.tangemtest._arch.structure.abstraction.BaseItem
+import com.tangem.tangemtest._arch.structure.impl.BoolItem
 import com.tangem.tangemtest._arch.structure.impl.TextItem
 import com.tangem.tangemtest._arch.widget.ItemWidgetBuilder
 import com.tangem.tangemtest._arch.widget.abstraction.ViewWidget
-import com.tangem.tangemtest.ucase.ui.widgets.ResponseTextWidget
 
 /**
 [REDACTED_AUTHOR]
@@ -14,6 +14,7 @@ class ResponseItemBuilder : ItemWidgetBuilder {
     override fun build(item: BaseItem<*>, parent: ViewGroup): ViewWidget? {
         return when (item) {
             is TextItem -> ResponseTextWidget(parent, item)
+            is BoolItem -> CheckBoxWidget(parent, item)
             else -> null
         }
     }
