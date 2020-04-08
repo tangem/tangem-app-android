1651e314ebfd96ff4a5cef263ddbe560cced5b92
Tangem <git@tangem.com>
2020-04-08 03:02:10 +0300

    Updated on 2026-08-27

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/paramsManager/managers/PersonalizeItemsManager.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/paramsManager/managers/PersonalizeItemsManager.kt
@@ -1,19 +1,45 @@
 package com.tangem.tangemtest.ucase.domain.paramsManager.managers
 
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
 import com.tangem.CardManager
 import com.tangem.tangemtest._arch.structure.abstraction.Item
 import com.tangem.tangemtest.ucase.domain.actions.PersonalizeAction
 import com.tangem.tangemtest.ucase.domain.paramsManager.ActionCallback
 import com.tangem.tangemtest.ucase.variants.personalize.converter.PersonalizeConfigConverter
 import com.tangem.tangemtest.ucase.variants.personalize.dto.PersonalizeConfig
+import ru.dev.gbixahue.eu4d.lib.android.global.log.Log
 
 /**
 [REDACTED_AUTHOR]
  */
-class PersonalizeItemsManager : BaseItemsManager(PersonalizeAction()) {
-    override fun createItemsList(): List<Item> = PersonalizeConfigConverter().convert(PersonalizeConfig())
+class PersonalizeItemsManager(
+        private val store: Store<PersonalizeConfig>
+) : BaseItemsManager(PersonalizeAction()), LifecycleObserver {
+
+    init {
+        Log.d(this, "new instance created")
+    }
+    private val converter = PersonalizeConfigConverter()
+
+    override fun createItemsList(): List<Item> {
+        val config = store.restore()
+        return converter.convert(config)
+    }
 
     override fun invokeMainAction(cardManager: CardManager, callback: ActionCallback) {
         action.executeMainAction(this, getAttrsForAction(cardManager), callback)
     }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun onDestroy() {
+        val config = converter.convert(itemList, PersonalizeConfig())
+        store.save(config)
+    }
+}
+
+interface Store<M> {
+    fun save(config: M)
+    fun restore(): M
 }
