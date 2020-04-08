0a2e97c34e043dfac60134e9b5d2c215c4b2f2ed
Tangem <git@tangem.com>
2020-04-09 00:37:03 +0300

    Updated on 2026-09-23

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/personalize/PersonalizationConfigStore.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/personalize/PersonalizationConfigStore.kt
@@ -6,21 +6,21 @@ import androidx.core.content.edit
 import com.google.gson.Gson
 import com.tangem.tangemtest.AppTangemDemo
 import com.tangem.tangemtest.commons.Store
-import com.tangem.tangemtest.ucase.variants.personalize.dto.PersonalizeConfig
+import com.tangem.tangemtest.ucase.variants.personalize.dto.PersonalizationConfig
 
-class PersonalizationConfigStore(context: Context) : Store<PersonalizeConfig> {
+class PersonalizationConfigStore(context: Context) : Store<PersonalizationConfig> {
 
     private val key = "personalization_config"
 
     private val sp: SharedPreferences = (context.applicationContext as AppTangemDemo).sharedPreferences()
     private val gson: Gson = Gson()
 
-    override fun save(config: PersonalizeConfig) {
+    override fun save(config: PersonalizationConfig) {
         sp.edit(true) { putString(key, gson.toJson(config)) }
     }
 
-    override fun restore(): PersonalizeConfig {
-        val json = sp.getString(key, gson.toJson(PersonalizeConfig()))
-        return gson.fromJson(json, PersonalizeConfig::class.java)
+    override fun restore(): PersonalizationConfig {
+        val json = sp.getString(key, gson.toJson(PersonalizationConfig()))
+        return gson.fromJson(json, PersonalizationConfig::class.java)
     }
 }
