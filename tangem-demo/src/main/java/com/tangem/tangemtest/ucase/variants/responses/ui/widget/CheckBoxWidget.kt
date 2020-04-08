d0215e6b12c4076fba32d017f6bcacbdbec18ea0
Tangem <git@tangem.com>
2020-04-08 23:04:48 +0300

    Updated on 2026-09-23

---
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/responses/ui/widget/CheckBoxWidget.kt
@@ -0,0 +1,25 @@
+package com.tangem.tangemtest.ucase.variants.responses.ui.widget
+
+import android.view.ViewGroup
+import com.google.android.material.checkbox.MaterialCheckBox
+import com.tangem.tangemtest.R
+import com.tangem.tangemtest._arch.structure.impl.BoolItem
+import com.tangem.tangemtest._arch.widget.abstraction.getName
+import com.tangem.tangemtest.ucase.variants.personalize.ui.widgets.DescriptionWidget
+import ru.dev.gbixahue.eu4d.lib.android._android.views.colorFrom
+
+/**
+[REDACTED_AUTHOR]
+ */
+class CheckBoxWidget(parent: ViewGroup, private val boolItem: BoolItem) : DescriptionWidget<Boolean>(parent, boolItem) {
+    override fun getLayoutId(): Int = R.layout.w_response_item_checkbox
+
+    private val switchItem = view.findViewById<MaterialCheckBox>(R.id.sw_item)
+
+    init {
+        switchItem.text = getName()
+        switchItem.isChecked = dataItem.getData() ?: false
+        switchItem.isEnabled = false
+        switchItem.setTextColor(switchItem.colorFrom(R.color.action_name))
+    }
+}
