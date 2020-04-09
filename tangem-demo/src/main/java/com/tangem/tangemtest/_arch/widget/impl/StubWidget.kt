072a17a97505f1b21c29fc45848feecf3da26736
Tangem <git@tangem.com>
2020-04-09 18:03:08 +0300

    Updated on 2026-03-25

--- tangem-demo/src/main/java/com/tangem/tangemtest/_arch/widget/impl/StubWidget.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/_arch/widget/impl/StubWidget.kt
@@ -2,13 +2,14 @@ package com.tangem.tangemtest._arch.widget.impl
 
 import android.view.ViewGroup
 import com.tangem.tangemtest.R
-import com.tangem.tangemtest._arch.structure.abstraction.ItemViewModel
-import com.tangem.tangemtest._arch.widget.abstraction.BaseBlockWidget
+import com.tangem.tangemtest._arch.structure.Id
+import com.tangem.tangemtest._arch.structure.abstraction.BaseItem
+import com.tangem.tangemtest._arch.structure.abstraction.BaseItemViewModel
+import com.tangem.tangemtest._arch.widget.abstraction.BaseViewWidget
 
 /**
 [REDACTED_AUTHOR]
  */
-class StubWidget(parent: ViewGroup) : BaseBlockWidget(parent) {
+class StubWidget(id: Id, parent: ViewGroup) : BaseViewWidget(parent, BaseItem(id, BaseItemViewModel())) {
     override fun getLayoutId(): Int = R.layout.w_empty
-    override var viewModel: List<ItemViewModel<*>> = listOf()
 }
