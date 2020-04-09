2a1eaf11e2b079dbbd859979c89a31fa6c89b2e9
Tangem <git@tangem.com>
2020-04-09 18:03:08 +0300

    Updated on 2026-05-13

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/responses/converter/CardConverter.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/responses/converter/CardConverter.kt
@@ -6,8 +6,9 @@ import com.tangem.commands.SettingsMask
 import com.tangem.tangemtest._arch.structure.Id
 import com.tangem.tangemtest._arch.structure.StringId
 import com.tangem.tangemtest._arch.structure.abstraction.Item
-import com.tangem.tangemtest._arch.structure.abstraction.ListItemBlock
+import com.tangem.tangemtest._arch.structure.abstraction.ItemGroup
 import com.tangem.tangemtest._arch.structure.abstraction.ModelToItems
+import com.tangem.tangemtest._arch.structure.abstraction.SimpleItemGroup
 import com.tangem.tangemtest._arch.structure.impl.BoolItem
 import com.tangem.tangemtest._arch.structure.impl.TextItem
 import com.tangem.tangemtest.ucase.variants.personalize.BlockId
@@ -31,55 +32,53 @@ class CardConverter : ModelToItems<Card> {
     }
 
     private fun simpleFields(from: Card): Item {
-        val block = createBlock(BlockId.Common)
-        block.addItem(TextItem(CardId.cardId, from.cardId))
-        block.addItem(TextItem(CardId.manufacturerName, from.manufacturerName))
-        block.addItem(TextItem(CardId.status, stringOf(from.status)))
-        block.addItem(TextItem(CardId.firmwareVersion, from.firmwareVersion))
-        block.addItem(TextItem(CardId.cardPublicKey, stringOf(from.cardPublicKey)))
-        block.addItem(TextItem(CardId.issuerPublicKey, stringOf(from.issuerPublicKey)))
-        block.addItem(TextItem(CardId.curve, stringOf(from.curve)))
-        block.addItem(TextItem(CardId.maxSignatures, stringOf(from.maxSignatures)))
-        block.addItem(TextItem(CardId.signingMethod, stringOf(from.signingMethod)))
-        block.addItem(TextItem(CardId.pauseBeforePin2, stringOf(from.pauseBeforePin2)))
-        block.addItem(TextItem(CardId.walletPublicKey, stringOf(from.walletPublicKey)))
-        block.addItem(TextItem(CardId.walletRemainingSignatures, stringOf(from.walletRemainingSignatures)))
-        block.addItem(TextItem(CardId.walletSignedHashes, stringOf(from.walletSignedHashes)))
-        block.addItem(TextItem(CardId.health, stringOf(from.health)))
-        block.addItem(TextItem(CardId.isActivated, stringOf(from.isActivated)))
-        block.addItem(TextItem(CardId.activationSeed, stringOf(from.activationSeed)))
-        block.addItem(TextItem(CardId.paymentFlowVersion, stringOf(from.paymentFlowVersion)))
-        block.addItem(TextItem(CardId.userCounter, stringOf(from.userCounter)))
+        val group = createGroup(BlockId.Common)
+        group.addItem(TextItem(CardId.cardId, from.cardId))
+        group.addItem(TextItem(CardId.manufacturerName, from.manufacturerName))
+        group.addItem(TextItem(CardId.status, stringOf(from.status)))
+        group.addItem(TextItem(CardId.firmwareVersion, from.firmwareVersion))
+        group.addItem(TextItem(CardId.cardPublicKey, stringOf(from.cardPublicKey)))
+        group.addItem(TextItem(CardId.issuerPublicKey, stringOf(from.issuerPublicKey)))
+        group.addItem(TextItem(CardId.curve, stringOf(from.curve)))
+        group.addItem(TextItem(CardId.maxSignatures, stringOf(from.maxSignatures)))
+        group.addItem(TextItem(CardId.signingMethod, stringOf(from.signingMethod)))
+        group.addItem(TextItem(CardId.pauseBeforePin2, stringOf(from.pauseBeforePin2)))
+        group.addItem(TextItem(CardId.walletPublicKey, stringOf(from.walletPublicKey)))
+        group.addItem(TextItem(CardId.walletRemainingSignatures, stringOf(from.walletRemainingSignatures)))
+        group.addItem(TextItem(CardId.walletSignedHashes, stringOf(from.walletSignedHashes)))
+        group.addItem(TextItem(CardId.health, stringOf(from.health)))
+        group.addItem(TextItem(CardId.isActivated, stringOf(from.isActivated)))
+        group.addItem(TextItem(CardId.activationSeed, stringOf(from.activationSeed)))
+        group.addItem(TextItem(CardId.paymentFlowVersion, stringOf(from.paymentFlowVersion)))
+        group.addItem(TextItem(CardId.userCounter, stringOf(from.userCounter)))
 //        block.addItem(TextItem(CardId.UserProtectedCounter, stringOf(from.userProtectedCounter)))
-        return block
+        return group
     }
 
     private fun cardData(from: Card): Item {
-        val block = createBlock(BlockId.Common)
-        val data = from.cardData ?: return block
-
-        val itemList = block.itemList
-        itemList.add(TextItem(CardDataId.batchId, data.batchId))
+        val group = createGroup(BlockId.Common)
+        val data = from.cardData ?: return group
+        group.addItem(TextItem(CardDataId.batchId, data.batchId))
 //        Format: Year (2 bytes) | Month (1 byte) | Day (1 byte)
-        itemList.add(TextItem(CardDataId.manufactureDateTime, stringOf(data.manufactureDateTime)))
-        itemList.add(TextItem(CardDataId.issuerName, data.issuerName))
-        itemList.add(TextItem(CardDataId.blockchainName, data.blockchainName))
-        itemList.add(TextItem(CardDataId.manufacturerSignature, stringOf(data.manufacturerSignature)))
-        itemList.add(TextItem(CardDataId.productMask, stringOf(data.productMask)))
-        itemList.add(TextItem(CardDataId.tokenSymbol, data.tokenSymbol))
-        itemList.add(TextItem(CardDataId.tokenContractAddress, data.tokenContractAddress))
-        itemList.add(TextItem(CardDataId.tokenDecimal, data.tokenSymbol))
+        group.addItem(TextItem(CardDataId.manufactureDateTime, stringOf(data.manufactureDateTime)))
+        group.addItem(TextItem(CardDataId.issuerName, data.issuerName))
+        group.addItem(TextItem(CardDataId.blockchainName, data.blockchainName))
+        group.addItem(TextItem(CardDataId.manufacturerSignature, stringOf(data.manufacturerSignature)))
+        group.addItem(TextItem(CardDataId.productMask, stringOf(data.productMask)))
+        group.addItem(TextItem(CardDataId.tokenSymbol, data.tokenSymbol))
+        group.addItem(TextItem(CardDataId.tokenContractAddress, data.tokenContractAddress))
+        group.addItem(TextItem(CardDataId.tokenDecimal, data.tokenSymbol))
 
-        return block
+        return group
     }
 
     private fun settingsMask(from: SettingsMask?): Item {
-        val block = createBlock(CardId.settingsMask)
-        val data = from ?: return block
+        val group = createGroup(CardId.settingsMask)
+        val data = from ?: return group
 
-        Settings.values().forEach { block.addItem(BoolItem(StringId(it.name), data.contains(it))) }
-        return block
+        Settings.values().forEach { group.addItem(BoolItem(StringId(it.name), data.contains(it))) }
+        return group
     }
 
-    private fun createBlock(id: Id): ListItemBlock = ListItemBlock(id).apply { addItem(TextItem(id)) }
+    private fun createGroup(id: Id): ItemGroup = SimpleItemGroup(id).apply { addItem(TextItem(id)) }
 }
