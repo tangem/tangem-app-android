e829a615bc8e2ebde6078adc5d96c11f15f280f2
Tangem <git@tangem.com>
2020-04-09 20:55:12 +0300

    Updated on 2025-11-03

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/responses/ui/ResponseFragment.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/responses/ui/ResponseFragment.kt
@@ -1,6 +1,7 @@
 package com.tangem.tangemtest.ucase.variants.responses.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
@@ -13,16 +14,11 @@ import com.tangem.tangemtest._main.MainViewModel
 import com.tangem.tangemtest.ucase.ui.BaseFragment
 import com.tangem.tangemtest.ucase.variants.responses.ResponseViewModel
 import com.tangem.tangemtest.ucase.variants.responses.ui.widget.ResponseItemBuilder
-import ru.dev.gbixahue.eu4d.lib.android.global.log.Log
 
 /**
 [REDACTED_AUTHOR]
  */
 open class ResponseFragment : BaseFragment() {
-
-    companion object Arg {
-        val response = "response"
-    }
 
     private val mainActivityVM: MainViewModel by activityViewModels()
     private val selfVM: ResponseViewModel by viewModels()
@@ -31,22 +27,28 @@ open class ResponseFragment : BaseFragment() {
 
     override fun getLayoutId(): Int = R.layout.fg_card_response
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        setTittle()
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    private fun setTittle() {
+        val titleId = selfVM.determineTitleId(mainActivityVM.responseEvent)
+        activity?.setTitle(titleId)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        mainActivityVM.ldResponseEvent.observe(viewLifecycleOwner, Observer { event ->
-            val builder = WidgetBuilder(ResponseItemBuilder())
-            val itemList = selfVM.createItemList(event)
-            itemList.forEach { builder.build(it, itemContainer) }
-        })
-        subscribeToViewModelChanges()
-    }
-
-    private fun subscribeToViewModelChanges() {
-        Log.d(this, "subscribeToViewModelChanges")
+        buildWidgets()
         listenDescriptionSwitchChanges()
     }
 
+    private fun buildWidgets() {
+        val builder = WidgetBuilder(ResponseItemBuilder())
+        val itemList = selfVM.createItemList(mainActivityVM.responseEvent)
+        itemList.forEach { builder.build(it, itemContainer) }
+    }
 
     private fun listenDescriptionSwitchChanges() {
         mainActivityVM.ldDescriptionSwitch.observe(viewLifecycleOwner, Observer {
