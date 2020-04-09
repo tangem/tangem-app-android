915550fe1be57c8f69eed8f3a7a0067bab7dcf71
Tangem <git@tangem.com>
2020-04-09 18:03:08 +0300

    Updated on 2026-09-23

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/personalize/ui/widgets/GroupTitleWidget.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/personalize/ui/widgets/GroupTitleWidget.kt
@@ -4,12 +4,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import com.tangem.tangemtest.R
 import com.tangem.tangemtest._arch.structure.impl.TextItem
-import com.tangem.tangemtest._arch.widget.abstraction.getName
 
 /**
 [REDACTED_AUTHOR]
  */
-class BlockHeadWidget(parent: ViewGroup, data: TextItem) : DescriptionWidget<String>(parent, data) {
+class GroupTitleWidget(parent: ViewGroup, data: TextItem) : DescriptionWidget(parent, data) {
     override fun getLayoutId(): Int = R.layout.w_personalize_item_text
 
     private val tvName = view.findViewById<TextView>(R.id.tv_name)
