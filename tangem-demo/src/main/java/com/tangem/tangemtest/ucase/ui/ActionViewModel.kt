907c288d06d7775e37ea5704d83df2062a7db15a
Tangem <git@tangem.com>
2020-04-10 15:16:11 +0300

    Updated on 2026-07-23

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/ui/ActionViewModel.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/ui/ActionViewModel.kt
@@ -14,13 +14,9 @@ import com.tangem.tangemtest._arch.structure.Id
 import com.tangem.tangemtest._arch.structure.Payload
 import com.tangem.tangemtest._arch.structure.abstraction.Item
 import com.tangem.tangemtest._arch.structure.abstraction.iterate
-import com.tangem.tangemtest.commons.performAction
 import com.tangem.tangemtest.ucase.domain.paramsManager.ItemsManager
 import com.tangem.tangemtest.ucase.domain.responses.ResponseJsonConverter
 import com.tangem.tangemtest.ucase.tunnel.ViewScreen
-import com.tangem.tasks.ScanEvent
-import com.tangem.tasks.TaskError
-import com.tangem.tasks.TaskEvent
 import ru.dev.gbixahue.eu4d.lib.android.global.log.Log
 
 /**
@@ -32,9 +28,9 @@ class ActionViewModelFactory(private val manager: ItemsManager) : ViewModelProvi
 
 class ActionViewModel(private val itemsManager: ItemsManager) : ViewModel(), LifecycleObserver {
 
-    val seResponseEvent = SingleLiveEvent<TaskEvent<*>>()
-    val seReadResponse = SingleLiveEvent<String>()
-    val seResponse = SingleLiveEvent<String>()
+    val seResponse = SingleLiveEvent<CommandResponse>()
+    val seResponseData = SingleLiveEvent<CommandResponse>()
+    val seResponseCardData = SingleLiveEvent<Card>()
 
     val ldItemList = MutableLiveData(itemsManager.getItems())
     val seError: MutableLiveData<String> = SingleLiveEvent()
@@ -58,88 +54,87 @@ class ActionViewModel(private val itemsManager: ItemsManager) : ViewModel(), Lif
 
     //invokes Scan, Sign etc...
     fun invokeMainAction() {
-        performAction(itemsManager, tangemSdk) { paramsManager, cardManager ->
-            performAction(itemsManager, tangemSdk, { paramsManager, cardManager ->
-                paramsManager.invokeMainAction(tangemSdk) { response, listOfChangedParams ->
-                    notifier.handleActionResult(response, listOfChangedParams)
-                }
-            })
+        if (!::tangemSdk.isInitialized) {
+            Log.e(this, "TangemSdk isn't initialized")
+            return
         }
-
-        fun getItemAction(id: Id): (() -> Unit)? {
-            val itemFunction = itemsManager.getActionByTag(id, tangemSdk) ?: return null
-
-            return {
-                itemFunction { response, listOfChangedParams ->
-                    notifier.handleActionResult(response, listOfChangedParams)
-                }
-            }
-        }
-
-        fun toggleDescriptionVisibility(state: Boolean) {
-            ldItemList.value?.iterate {
-                it.viewModel.viewState.descriptionVisibility = if (state) View.VISIBLE else View.GONE
-            }
-        }
-
-        fun attachToPayload(payload: Payload) {
-            itemsManager.attachPayload(payload)
-        }
-
-        @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-        fun viewOnDestroy() {
-            val keyList = mutableListOf<String>()
-            itemsManager.payload.filterValues { it is ViewScreen }.forEach { keyList.add(it.key) }
-            keyList.forEach { itemsManager.payload.remove(it) }
+        itemsManager.invokeMainAction(tangemSdk) { response, listOfChangedParams ->
+            notifier.handleActionResult(response, listOfChangedParams)
         }
     }
 
-    internal class Notifier(private val vm: ActionViewModel) {
+    fun getItemAction(id: Id): (() -> Unit)? {
+        val itemFunction = itemsManager.getActionByTag(id, tangemSdk) ?: return null
 
-        private var notShowedError: SessionError? = null
-        private val gson: Gson = ResponseJsonConverter().gson
-
-        fun handleActionResult(response: CompletionResult<*>, list: List<Item>) {
-            if (list.isNotEmpty()) notifyItemsChanged(list)
-            handleResponse(response)
-        }
-
-        @UiThread
-        fun notifyItemsChanged(list: List<Item>) {
-            vm.seChangedItems.postValue(list)
-        }
-
-        fun handleResponse(response: CompletionResult<*>) {
-            val taskEvent = response as? CompletionResult<CommandResponse> ?: return
-
-            when (commandResponse) {
-                is CompletionResult.Success -> handleDataEvent(commandResponse.data)
-                is CompletionResult.Failure -> handleError(commandResponse.error)
+        return {
+            itemFunction { response, listOfChangedParams ->
+                notifier.handleActionResult(response, listOfChangedParams)
             }
         }
+    }
 
-        private fun handleDataEvent(event: CommandResponse?) {
-            when (event) {
-                is ScanEvent.OnReadEvent -> vm.seReadResponse.postValue(gson.toJson(event))
-                is ScanEvent.OnVerifyEvent -> {
+    fun toggleDescriptionVisibility(state: Boolean) {
+        ldItemList.value?.iterate {
+            it.viewModel.viewState.descriptionVisibility = if (state) View.VISIBLE else View.GONE
+        }
+    }
+
+    fun attachToPayload(payload: Payload) {
+        itemsManager.attachPayload(payload)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun viewOnDestroy() {
+        val keyList = mutableListOf<String>()
+        itemsManager.payload.filterValues { it is ViewScreen }.forEach { keyList.add(it.key) }
+        keyList.forEach { itemsManager.payload.remove(it) }
+    }
+}
+
+internal class Notifier(private val vm: ActionViewModel) {
+
+    private var notShowedError: SessionError? = null
+    private val gson: Gson = ResponseJsonConverter().gson
+
+    fun handleActionResult(result: CompletionResult<*>, list: List<Item>) {
+        if (list.isNotEmpty()) notifyItemsChanged(list)
+        handleCompletionResult(result)
+    }
+
+    @UiThread
+    fun notifyItemsChanged(list: List<Item>) {
+        vm.seChangedItems.postValue(list)
+    }
+
+    fun handleCompletionResult(result: CompletionResult<*>) {
+        val commandResponse = result as? CompletionResult<CommandResponse> ?: return
+
+        when (commandResponse) {
+            is CompletionResult.Success -> handleData(commandResponse.data)
+            is CompletionResult.Failure -> handleError(commandResponse.error)
+        }
+    }
+
+    private fun handleData(event: CommandResponse?) {
+        vm.seResponse.postValue(event)
+        when (event) {
+            is Card -> vm.seResponseCardData.postValue(event)
+            else -> vm.seResponseData.postValue(event)
+        }
+    }
+
+    private fun handleError(error: SessionError) {
+        Log.d(this, "error = $error")
+        when (error) {
+            is SessionError.UserCancelled -> {
+                if (notShowedError == null) {
+                    vm.seError.postValue("User canceled the action")
+                } else {
+                    vm.seError.postValue("${notShowedError!!::class.simpleName}")
+                    notShowedError = null
                 }
-                else -> vm.seResponse.postValue(gson.toJson(event))
             }
-        }
-
-        private fun handleCompletionEvent(error: SessionError) {
-            Log.d(this, "error = ${taskEvent.error}")
-            when (taskEvent.error) {
-                is TaskError.UserCancelled -> {
-                    if (notShowedError == null) {
-                        vm.seError.postValue("User canceled the action")
-                    } else {
-                        vm.seError.postValue("${notShowedError!!::class.simpleName}")
-                        notShowedError = null
-                    }
-                }
-                else -> notShowedError = taskEvent.error
-            }
+            else -> notShowedError = error
         }
     }
 }
