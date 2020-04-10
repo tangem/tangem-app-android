6caa57ad7cbb83de7879e05526016ff6df9faade
Tangem <git@tangem.com>
2020-04-10 12:36:15 +0300

    Updated on 2026-08-19

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/paramsManager/managers/ScanItemsManager.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/paramsManager/managers/ScanItemsManager.kt
@@ -1,6 +1,6 @@
 package com.tangem.tangemtest.ucase.domain.paramsManager.managers
 
-import com.tangem.CardManager
+import com.tangem.TangemSdk
 import com.tangem.tangemtest._arch.structure.abstraction.Item
 import com.tangem.tangemtest.ucase.domain.actions.ScanAction
 import com.tangem.tangemtest.ucase.domain.paramsManager.ActionCallback
@@ -11,7 +11,7 @@ import com.tangem.tangemtest.ucase.domain.paramsManager.ActionCallback
 class ScanItemsManager : BaseItemsManager(ScanAction()) {
     override fun createItemsList(): List<Item> = listOf()
 
-    override fun invokeMainAction(cardManager: CardManager, callback: ActionCallback) {
-        action.executeMainAction(this, getAttrsForAction(cardManager), callback)
+    override fun invokeMainAction(tangemSdk: TangemSdk, callback: ActionCallback) {
+        action.executeMainAction(this, getAttrsForAction(tangemSdk), callback)
     }
 }
