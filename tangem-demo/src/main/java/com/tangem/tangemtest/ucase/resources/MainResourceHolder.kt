6171e52b5f7f3c367451a29606662b69ffab2380
Tangem <git@tangem.com>
2020-04-10 12:26:42 +0300

    Updated on 2026-07-23

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/resources/MainResourceHolder.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/resources/MainResourceHolder.kt
@@ -1,7 +1,10 @@
 package com.tangem.tangemtest.ucase.resources
 
+import com.tangem.tangemtest.AppTangemDemo
 import com.tangem.tangemtest.R
 import com.tangem.tangemtest._arch.structure.Id
+import com.tangem.tangemtest._arch.structure.StringId
+import com.tangem.tangemtest._arch.structure.StringResId
 import com.tangem.tangemtest.ucase.resources.initializers.ActionResources
 import com.tangem.tangemtest.ucase.resources.initializers.PersonalizationResources
 import com.tangem.tangemtest.ucase.resources.initializers.ResponseResources
@@ -19,6 +22,23 @@ object MainResourceHolder : ResourceHolder<Id>() {
         ActionResources().init(this)
         TlvResources().init(this)
         ResponseResources().init(this)
+    }
+
+    override fun get(type: Id): Resources? {
+        return when(type) {
+            is StringId -> getResourcesFromName(type.value)
+            is StringResId -> Resources(type.value)
+            else -> super.get(type)
+        }
+    }
+
+    private fun getResourcesFromName(resName: String): Resources? {
+        val context = AppTangemDemo.appInstance.applicationContext
+        val resNameId = context.resources.getIdentifier(resName, "string", context.packageName)
+        val resInfoId = context.resources.getIdentifier("info_$resName", "string", context.packageName)
+        if (resNameId == 0) return null
+
+        return Resources(resNameId, if (resInfoId == 0) null else resInfoId)
     }
 
     inline fun <reified Res : Resources> safeGet(id: Id): Res {
