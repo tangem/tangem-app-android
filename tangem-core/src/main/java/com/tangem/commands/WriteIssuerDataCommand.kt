05191b76fdbb4a944dbf6a28f41a61ed767f2caa
Tangem <git@tangem.com>
2020-04-10 18:22:35 +0300

    Updated on 2026-02-25

--- tangem-core/src/main/java/com/tangem/commands/WriteIssuerDataCommand.kt
+++ tangem-core/src/main/java/com/tangem/commands/WriteIssuerDataCommand.kt
@@ -1,7 +1,7 @@
 package com.tangem.commands
 
-import com.tangem.CardEnvironment
 import com.tangem.CardSession
+import com.tangem.SessionEnvironment
 import com.tangem.SessionError
 import com.tangem.commands.common.DefaultIssuerDataVerifier
 import com.tangem.commands.common.IssuerDataMode
@@ -76,7 +76,7 @@ class WriteIssuerDataCommand(
         )
     }
 
-    override fun serialize(environment: CardEnvironment): CommandApdu {
+    override fun serialize(environment: SessionEnvironment): CommandApdu {
         val tlvBuilder = TlvBuilder()
         tlvBuilder.append(TlvTag.Pin, environment.pin1)
         tlvBuilder.append(TlvTag.CardId, environment.card?.cardId)
@@ -91,7 +91,7 @@ class WriteIssuerDataCommand(
         )
     }
 
-    override fun deserialize(environment: CardEnvironment, apdu: ResponseApdu): WriteIssuerDataResponse {
+    override fun deserialize(environment: SessionEnvironment, apdu: ResponseApdu): WriteIssuerDataResponse {
         val tlvData = apdu.getTlvData(environment.encryptionKey)
                 ?: throw SessionError.DeserializeApduFailed()
 
