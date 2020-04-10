40784d7f658896f66a4ccd95f64ab9d213c7ca88
Tangem <git@tangem.com>
2020-04-10 12:26:42 +0300

    Updated on 2026-06-24

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/personalize/ui/widgets/BaseAppWidget.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/personalize/ui/widgets/BaseAppWidget.kt
@@ -2,7 +2,6 @@ package com.tangem.tangemtest.ucase.variants.personalize.ui.widgets
 
 import android.view.ViewGroup
 import com.tangem.tangemtest._arch.structure.StringId
-import com.tangem.tangemtest._arch.structure.StringResId
 import com.tangem.tangemtest._arch.structure.abstraction.Item
 import com.tangem.tangemtest._arch.widget.abstraction.BaseViewWidget
 import com.tangem.tangemtest._arch.widget.abstraction.ViewWidget
@@ -15,14 +14,15 @@ import ru.dev.gbixahue.eu4d.lib.android._android.views.stringFrom
  */
 abstract class BaseAppWidget(parent: ViewGroup, item: Item) : BaseViewWidget(parent, item) {
     override fun getName(): String {
-        return when (val id = item.id) {
-            is StringId -> id.value
-            is StringResId -> view.stringFrom(id.value)
-            else -> view.stringFrom(getResNameId())
-        }
+        val idName = if (item.id is StringId) (item.id as StringId).value else ""
+
+        val resources = getResources() ?: return idName
+        return view.stringFrom(resources.resName)
     }
 }
 
+fun ViewWidget.getResources(): Resources? = MainResourceHolder.get(item.id)
+
 fun ViewWidget.getResNameId(): Int = MainResourceHolder.safeGet<Resources>(item.id).resName
 
-fun ViewWidget.getResDescription(): Int? = MainResourceHolder.safeGet<Resources>(item.id).resDescription
+fun ViewWidget.getResDescriptionId(): Int? = MainResourceHolder.get(item.id)?.resDescription
