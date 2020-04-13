fa457a2adf4fdc6fa1f55fc652295b237974121d
Tangem <git@tangem.com>
2020-04-13 21:58:27 +0300

    Updated on 2026-04-06

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/personalize/PersonalizationConfigStore.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/personalize/PersonalizationConfigStore.kt
@@ -5,22 +5,46 @@ import android.content.SharedPreferences
 import androidx.core.content.edit
 import com.google.gson.Gson
 import com.tangem.tangemtest.AppTangemDemo
+import com.tangem.tangemtest.commons.KeyedStore
 import com.tangem.tangemtest.commons.Store
 import com.tangem.tangemtest.ucase.variants.personalize.dto.PersonalizationConfig
 
-class PersonalizationConfigStore(context: Context) : Store<PersonalizationConfig> {
+class PersonalizationConfigStore(context: Context) : Store<PersonalizationConfig>, KeyedStore<PersonalizationConfig> {
 
-    private val key = "personalization_config"
+    private val sharedPreferencesKey = "personalization_presets"
+    private val defaultKey = "default"
 
-    private val sp: SharedPreferences = (context.applicationContext as AppTangemDemo).sharedPreferences()
+    private val sp: SharedPreferences = (context.applicationContext as AppTangemDemo).sharedPreferences(sharedPreferencesKey)
     private val gson: Gson = Gson()
 
-    override fun save(config: PersonalizationConfig) {
-        sp.edit(true) { putString(key, gson.toJson(config)) }
+    override fun save(value: PersonalizationConfig) {
+        save(defaultKey, value)
     }
 
-    override fun restore(): PersonalizationConfig {
-        val json = sp.getString(key, gson.toJson(PersonalizationConfig()))
-        return gson.fromJson(json, PersonalizationConfig::class.java)
+    override fun restore(): PersonalizationConfig = restore(defaultKey)
+
+    override fun save(key: String, value: PersonalizationConfig) {
+        sp.edit(true) { putString(key, toJson(value)) }
     }
+
+    override fun restore(key: String): PersonalizationConfig {
+        val json = sp.getString(key, toJson(getDefaultConfig()))
+        return fromJson(json!!)
+    }
+
+    override fun restoreAll(): Map<String, PersonalizationConfig> {
+        val map = mutableMapOf<String, PersonalizationConfig>()
+        sp.all.forEach { map[it.key] = fromJson(it.value as String) }
+        return map.toSortedMap()
+    }
+
+    override fun delete(key: String) {
+        sp.edit(true) { remove(key) }
+    }
+
+    private fun getDefaultConfig(): PersonalizationConfig = PersonalizationConfig.default()
+
+    private fun toJson(value: PersonalizationConfig): String = gson.toJson(value)
+
+    private fun fromJson(json: String): PersonalizationConfig = gson.fromJson(json, PersonalizationConfig::class.java)
 }
