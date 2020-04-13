4636f5cf55fb872be8c10746b64162b2d1e9e1ed
Tangem <git@tangem.com>
2020-04-13 13:33:44 +0300

    Updated on 2026-08-19

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/scan/ui/ScanActionFragment.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/scan/ui/ScanActionFragment.kt
@@ -1,10 +1,17 @@
 package com.tangem.tangemtest.ucase.variants.scan.ui
 
+import android.os.Bundle
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.TextView
 import com.tangem.commands.Card
 import com.tangem.tangemtest.R
 import com.tangem.tangemtest.ucase.domain.paramsManager.ItemsManager
 import com.tangem.tangemtest.ucase.domain.paramsManager.managers.ScanItemsManager
 import com.tangem.tangemtest.ucase.ui.BaseCardActionFragment
+import ru.dev.gbixahue.eu4d.lib.android._android.views.dpToPx
 
 /**
 [REDACTED_AUTHOR]
@@ -13,9 +20,30 @@ class ScanActionFragment : BaseCardActionFragment() {
 
     override val itemsManager: ItemsManager by lazy { ScanItemsManager() }
 
-    override fun getLayoutId(): Int = R.layout.fg_action_card_scan
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    override fun initFab() {
+        val howToUseView = createHowToUse()
+        contentContainer.layoutParams = FrameLayout.LayoutParams(-1, -1, Gravity.CENTER)
+        contentContainer.addView(howToUseView)
+    }
+
+    private fun createHowToUse(): View {
+        val fl = FrameLayout(requireContext())
+        fl.layoutParams = FrameLayout.LayoutParams(-1, -1, Gravity.CENTER)
+        val tv = TextView(requireContext()).apply {
+            val padding = dpToPx(16f).toInt()
+            setPadding(padding, 0, padding, 0)
+            gravity = Gravity.CENTER
+            setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
+            setText(R.string.htu_scan_action)
+        }
+        fl.addView(tv)
+        return fl
+    }
+
+    override fun initViews() {
+        swrLayout.isEnabled = false
         actionFab.setOnClickListener { actionVM.invokeMainAction() }
     }
 
