6725d92b17cb99461055232838e1bc0d31f2595d
Tangem <git@tangem.com>
2020-04-13 13:33:44 +0300

    Updated on 2025-09-01

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/personalize/ui/PersonalizationFragment.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/personalize/ui/PersonalizationFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.lifecycle.Observer
+import androidx.transition.Fade
+import androidx.transition.TransitionManager
 import com.tangem.commands.Card
 import com.tangem.tangemtest.R
 import com.tangem.tangemtest._arch.structure.Id
@@ -24,6 +27,8 @@ import com.tangem.tangemtest.ucase.variants.personalize.PersonalizationConfigSto
 import com.tangem.tangemtest.ucase.variants.personalize.ui.widgets.PersonalizationItemBuilder
 import ru.dev.gbixahue.eu4d.lib.android._android.views.inflate
 import ru.dev.gbixahue.eu4d.lib.android.global.log.Log
+import ru.dev.gbixahue.eu4d.lib.android.global.threading.postUI
+import ru.dev.gbixahue.eu4d.lib.android.global.threading.postWork
 
 /**
 [REDACTED_AUTHOR]
@@ -32,17 +37,56 @@ class PersonalizationFragment : BaseCardActionFragment() {
 
     override val itemsManager: ItemsManager by lazy { PersonalizationItemsManager(PersonalizationConfigStore(requireContext())) }
 
-    override fun getLayoutId(): Int = R.layout.fg_personalization
+    override fun getLayoutId(): Int = R.layout.fg_base_action_layout
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         lifecycle.addObserver(itemsManager as PersonalizationItemsManager)
     }
 
+    override fun bindViews() {
+        super.bindViews()
+        swrLayout.isRefreshing = true
+    }
+
+    override fun initViews() {
+        actionFab.setOnClickListener { actionVM.invokeMainAction() }
+    }
+
+    override fun createWidgets(widgetCreatedCallback: () -> Unit) {
+        Log.d(this, "createWidgets")
+        val itemList = mutableListOf<Item>()
+
+        val maxDelay = 500
+        val timeStart = System.currentTimeMillis()
+        actionVM.ldItemList.observe(viewLifecycleOwner, Observer { list ->
+            Log.d(this, "ldBlockList size: ${list.size}")
+            itemList.addAll(list)
+            val llContainer = LinearLayout(requireContext()).apply { orientation = LinearLayout.VERTICAL }
+            postWork {
+                val builder = WidgetBuilder(PersonalizationItemBuilder())
+                itemList.forEach { builder.build(it, llContainer) }
+                actionVM.attachToPayload(mutableMapOf(
+                        PayloadKey.actionView to this as ActionView,
+                        PayloadKey.itemList to itemList
+                ))
+                val timeEnd = System.currentTimeMillis()
+                val diff = timeEnd - timeStart
+                postUI(maxDelay - diff) {
+                    TransitionManager.beginDelayedTransition(contentContainer, Fade())
+                    contentContainer.addView(llContainer)
+                    widgetCreatedCallback()
+                    swrLayout.isRefreshing = false
+                    swrLayout.isEnabled = false
+                }
+            }
+        })
+    }
+
     override fun widgetsWasCreated() {
         super.widgetsWasCreated()
 
-        val btnContainer = itemContainer.inflate<ViewGroup>(R.layout.view_simple_button)
+        val btnContainer = contentContainer.inflate<ViewGroup>(R.layout.view_simple_button)
         val btn = btnContainer.findViewById<Button>(R.id.button)
 
         val show = StringId("show")
@@ -55,28 +99,7 @@ class PersonalizationFragment : BaseCardActionFragment() {
             multiAction.state = if (it == show) hide else show
         }
         multiAction.performAction(hide)
-        itemContainer.addView(btnContainer)
-    }
-
-    override fun initFab() {
-        actionFab.setOnClickListener { actionVM.invokeMainAction() }
-    }
-
-    override fun createWidgets(widgetCreatedCallback: () -> Unit) {
-        Log.d(this, "createWidgets")
-        val itemList = mutableListOf<Item>()
-
-        actionVM.ldItemList.observe(viewLifecycleOwner, Observer { list ->
-            Log.d(this, "ldBlockList size: ${list.size}")
-            itemList.clear()
-            itemList.addAll(list)
-            itemList.forEach { WidgetBuilder(PersonalizationItemBuilder()).build(it, itemContainer) }
-            actionVM.attachToPayload(mutableMapOf(
-                    PayloadKey.actionView to this as ActionView,
-                    PayloadKey.itemList to itemList
-            ))
-            widgetCreatedCallback()
-        })
+        contentContainer.addView(btnContainer)
     }
 
     override fun showSnackbar(id: Id, additionalHandler: ((Id) -> Int)?) {
