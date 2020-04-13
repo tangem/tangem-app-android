404d9ddf5f1b69c445640c34cadb7537308c19c5
Tangem <git@tangem.com>
2020-04-13 19:28:25 +0300

    Updated on 2026-09-14

--- app/src/main/java/com/tangem/data/network/ServerApiTokenEmv.kt
+++ app/src/main/java/com/tangem/data/network/ServerApiTokenEmv.kt
@@ -1,25 +1,25 @@
 package com.tangem.data.network
 
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import com.tangem.data.network.model.TezosAccountResponse
+import com.tangem.data.network.model.TokenEmvGetTransferFeeAnswer
+import com.tangem.data.network.model.TokenEmvGetTransferFeeBody
+import com.tangem.data.network.model.TokenEmvTransferAnswer
 import com.tangem.data.network.model.TokenEmvTransferBody
 import com.tangem.tangem_card.util.Log
-import io.reactivex.CompletableObserver
-import io.reactivex.Single
 import io.reactivex.SingleObserver
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.functions.BiConsumer
 import io.reactivex.schedulers.Schedulers
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.util.concurrent.TimeUnit
 
 class ServerApiTokenEmv {
     private val TAG = ServerApiTokenEmv::class.java.simpleName
 
-    private val tangemServer = ""
+    private val tangemServer = "https://emvsupport.appspot.com/"
 
     private val tokenEmvApi = Retrofit.Builder()
             .baseUrl(tangemServer)
@@ -32,10 +32,20 @@ class ServerApiTokenEmv {
             .build()
             .create(TokenEmvApi::class.java)
 
-    fun transfer(tokenEmvTransferBody: TokenEmvTransferBody, transferObserver: CompletableObserver) {
+    fun transfer(tokenEmvTransferBody: TokenEmvTransferBody, transferObserver: SingleObserver<TokenEmvTransferAnswer>) {
         Log.i(TAG, "new transfer request")
 
         tokenEmvApi.transfer(tokenEmvTransferBody)
+                .timeout(30, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(transferObserver)
+    }
+
+    fun getTransferFee(tokenEmvGetTransferFeeBody: TokenEmvGetTransferFeeBody, transferObserver: SingleObserver<TokenEmvGetTransferFeeAnswer>) {
+        Log.i(TAG, "new get transfer fee request")
+
+        tokenEmvApi.getTransferFee(tokenEmvGetTransferFeeBody)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(transferObserver)
