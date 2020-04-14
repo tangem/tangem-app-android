6d2f347969d4a2b9d947689b90ad3ad0a6356729
Tangem <git@tangem.com>
2020-04-14 14:10:23 +0300

    release/5.26.1

--- tangem-demo/src/main/java/com/tangem/tangemtest/_main/MainActivity.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/_main/MainActivity.kt
@@ -3,9 +3,9 @@ package com.tangem.tangemtest._main
 import android.content.res.Resources
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.SwitchCompat
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -20,7 +20,7 @@ import ru.dev.gbixahue.eu4d.lib.android.global.log.Log
  */
 class MainActivity : AppCompatActivity() {
 
-    private val vm: MainViewModel by viewModels<MainViewModel>()
+    private val mainVM: MainViewModel by viewModels<MainViewModel>()
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,11 +52,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
-        val switchMenu = menu.findItem(R.id.action_toggle_description_visibility)
-        (switchMenu.actionView as? SwitchCompat)?.let {
-            it.setOnCheckedChangeListener { buttonView, isChecked -> vm.switchToggled(isChecked) }
+        menuInflater.inflate(R.menu.menu_activity_main, menu)
+        return true
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu): Boolean {
+        val switchItem = menu.findItem(R.id.action_toggle_description_visibility)
+        switchItem.isChecked = mainVM.descriptionSwitchState
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val result = when (item.itemId) {
+            R.id.action_toggle_description_visibility -> {
+                item.isChecked = !item.isChecked
+                mainVM.switchToggled(item.isChecked)
+            }
+            else -> null
         }
-        return super.onCreateOptionsMenu(menu)
+        return if (result == null) super.onOptionsItemSelected(item) else true
     }
 }
