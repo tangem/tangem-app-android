b5ff5e3b2c962e2d5fad027966f669a1a37a05e5
Tangem <git@tangem.com>
2020-04-16 18:34:15 +0300

    Updated on 2026-09-14

--- tangem-sdk/src/main/java/com/tangem/tangem_sdk_new/ui/NfcEnableDialog.kt
+++ tangem-sdk/src/main/java/com/tangem/tangem_sdk_new/ui/NfcEnableDialog.kt
@@ -1,30 +1,23 @@
 package com.tangem.tangem_sdk_new.ui
 
-import android.app.Dialog
+import android.app.Activity
 import android.content.Intent
-import android.os.Bundle
 import android.provider.Settings
-
 import androidx.appcompat.app.AlertDialog
-import androidx.fragment.app.DialogFragment
 import com.tangem.tangem_sdk_new.R
 
-class NfcEnableDialog : DialogFragment() {
-    companion object {
-        val TAG: String = NfcEnableDialog::class.java.simpleName
-    }
+class NfcEnableDialog {
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val builder = AlertDialog.Builder(requireContext())
+    fun show(activity: Activity) {
+        val builder = AlertDialog.Builder(activity)
         builder.setCancelable(false)
                 .setIcon(R.drawable.ic_action_nfc_gray)
                 .setTitle(R.string.dialog_nfc_enable_title)
                 .setMessage(R.string.dialog_nfc_enable_text)
                 .setPositiveButton(R.string.general_ok
                 ) { _, _ ->
-                    activity?.startActivity(Intent(Settings.ACTION_NFC_SETTINGS))
+                    activity.startActivity(Intent(Settings.ACTION_NFC_SETTINGS))
                 }
-        return builder.create()
+        builder.create().show()
     }
-
 }
