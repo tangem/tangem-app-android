512de202836b1a5976dc804d0fe200582c59b662
Tangem <git@tangem.com>
2020-04-29 10:20:17 +0300

    release/5.26.1

--- tangem-devkit/src/main/java/com/tangem/devkit/ucase/resources/initializers/TlvResources.kt
+++ tangem-devkit/src/main/java/com/tangem/devkit/ucase/resources/initializers/TlvResources.kt
@@ -16,5 +16,9 @@ class TlvResources {
     private fun initScan(holder: MainResourceHolder) {
         holder.register(TlvId.CardId, Resources(R.string.tlv_card_id, R.string.info_tlv_card_id))
         holder.register(TlvId.TransactionOutHash, Resources(R.string.tlv_transaction_out_hash, R.string.info_tlv_transaction_out_hash))
+        holder.register(TlvId.Counter, Resources(R.string.tlv_counter, R.string.info_tlv_counter))
+        holder.register(TlvId.IssuerData, Resources(R.string.tlv_issuer_data, R.string.info_tlv_issuer_data))
+        holder.register(TlvId.UserData, Resources(R.string.tlv_issuer_data, R.string.info_tlv_user_data))
+        holder.register(TlvId.ProtectedUserData, Resources(R.string.tlv_user_protected_data, R.string.info_tlv_protected_user_data))
     }
 }
