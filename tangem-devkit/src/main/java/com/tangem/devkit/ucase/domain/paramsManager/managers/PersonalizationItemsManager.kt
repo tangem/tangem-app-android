25a1a2d9885b4b32bc5f98b4bd81a0dd3af477a1
Tangem <git@tangem.com>
2020-04-29 12:04:40 +0300

    Updated on 2026-09-14

--- tangem-devkit/src/main/java/com/tangem/devkit/ucase/domain/paramsManager/managers/PersonalizationItemsManager.kt
+++ tangem-devkit/src/main/java/com/tangem/devkit/ucase/domain/paramsManager/managers/PersonalizationItemsManager.kt
@@ -2,12 +2,16 @@ package com.tangem.devkit.ucase.domain.paramsManager.managers
 
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.OnLifecycleEvent
+import com.google.gson.Gson
 import com.tangem.TangemSdk
 import com.tangem.devkit.commons.Store
 import com.tangem.devkit.ucase.domain.actions.PersonalizeAction
 import com.tangem.devkit.ucase.domain.paramsManager.ActionCallback
 import com.tangem.devkit.ucase.variants.personalize.converter.PersonalizationConfigConverter
+import com.tangem.devkit.ucase.variants.personalize.converter.PersonalizationJsonConverter
 import com.tangem.devkit.ucase.variants.personalize.dto.PersonalizationConfig
+import com.tangem.devkit.ucase.variants.personalize.dto.PersonalizationJson
+import ru.dev.gbixahue.eu4d.lib.android.global.log.Log
 
 /**
 [REDACTED_AUTHOR]
@@ -25,6 +29,27 @@ class PersonalizationItemsManager(
 
     override fun invokeMainAction(tangemSdk: TangemSdk, callback: ActionCallback) {
         action.executeMainAction(this, getAttrsForAction(tangemSdk), callback)
+    }
+
+    fun importJsonConfig(jsonString: String) {
+        if (jsonString.isEmpty()) return
+
+        val jsonDto = try {
+            Gson().fromJson(jsonString, PersonalizationJson::class.java)
+        } catch (ex: Exception) {
+            Log.e(this, "Can't convert imported string to Json object. Error: $ex")
+            return
+        }
+
+        val config = PersonalizationJsonConverter().aToB(jsonDto)
+        setItems(converter.convert(config))
+    }
+
+    fun exportJsonConfig(): String {
+        val config = converter.convert(itemList, PersonalizationConfig.default())
+        val jsonDto = PersonalizationJsonConverter().bToA(config)
+        val jsonString = Gson().toJson(jsonDto)
+        return jsonString
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
