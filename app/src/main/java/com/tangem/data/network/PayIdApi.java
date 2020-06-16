2ac5682b21e11b846ad02dcaa5ac4678c15429e6
Tangem <git@tangem.com>
2020-06-16 14:51:43 +0300

    Updated on 2026-08-19

--- app/src/main/java/com/tangem/data/network/PayIdApi.java
+++ app/src/main/java/com/tangem/data/network/PayIdApi.java
@@ -4,14 +4,12 @@ import com.tangem.data.network.model.PayIdResponse;
 
 import io.reactivex.Single;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.Path;
 
 public interface PayIdApi {
-    @Headers({
-            "Accept: application/xrpl-mainnet+json",
-            "PayID-Version: 1.0"
-    })
+    @Headers("PayID-Version: 1.0")
     @GET("{user}")
-    Single<PayIdResponse> getAddress(@Path("user") String user);
+    Single<PayIdResponse> getAddress(@Path("user") String user, @Header("Accept") String acceptHeader);
 }
