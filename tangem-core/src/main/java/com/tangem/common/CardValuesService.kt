0e98e0ce99836f6e4e759450e6d6bef8209b15fe
Tangem <git@tangem.com>
2020-06-30 15:47:01 +0300

    Updated on 2026-06-16

---
+++ tangem-core/src/main/java/com/tangem/common/CardValuesService.kt
@@ -0,0 +1,46 @@
+package com.tangem.common
+
+import com.squareup.sqldelight.EnumColumnAdapter
+import com.squareup.sqldelight.db.SqlDriver
+import com.tangem.CardValues
+import com.tangem.CardValuesEntityQueries
+import com.tangem.Database
+import com.tangem.SessionEnvironment
+
+interface CardValuesService {
+
+    fun saveValues(environment: SessionEnvironment)
+
+    fun getValues(cardId: String) : CardValues?
+
+}
+
+class CardValuesDbService(driver: SqlDriver) : CardValuesService {
+
+    private val cardValuesQueries: CardValuesEntityQueries
+
+    init {
+        val database = Database(driver, cardValuesAdapter = CardValues.Adapter(
+                cardVerificationAdapter = EnumColumnAdapter(),
+                cardValidationAdapter = EnumColumnAdapter(),
+                codeVerificationAdapter = EnumColumnAdapter()
+        ))
+        cardValuesQueries = database.cardValuesEntityQueries
+    }
+
+    override fun saveValues(environment: SessionEnvironment) {
+        environment.card?.cardId?.let {cardId ->
+            cardValuesQueries.insertOrReplace(
+                    cardId,
+                    environment.pin1?.isDefault ?: false,
+                    environment.pin2?.isDefault ?: false,
+                    environment.cardVerification, environment.cardVerification,
+                    environment.codeVerification
+            )
+        }
+    }
+
+    override fun getValues(cardId: String): CardValues?  =
+            cardValuesQueries.selectByCardId(cardId).executeAsOneOrNull()
+
+}
