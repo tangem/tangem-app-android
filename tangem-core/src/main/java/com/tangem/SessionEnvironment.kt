848d1d05f5ff94614cfc98513291088082c1874c
Tangem <git@tangem.com>
2020-06-30 15:47:01 +0300

    Updated on 2026-09-21

--- tangem-core/src/main/java/com/tangem/SessionEnvironment.kt
+++ tangem-core/src/main/java/com/tangem/SessionEnvironment.kt
@@ -2,7 +2,9 @@ package com.tangem
 
 import com.tangem.commands.Card
 import com.tangem.commands.EllipticCurve
-import com.tangem.common.extensions.calculateSha256
+import com.tangem.common.CardValuesService
+import com.tangem.common.PinCode
+import com.tangem.common.TerminalKeysService
 import com.tangem.crypto.CryptoUtils.generatePublicKey
 
 
@@ -13,38 +15,79 @@ import com.tangem.crypto.CryptoUtils.generatePublicKey
  * @property card  Current card, read by preflight [com.tangem.commands.ReadCommand].
  * @property terminalKeys generated terminal keys used in Linked Terminal feature.
  */
-data class SessionEnvironment(
-        var pin2: ByteArray = DEFAULT_PIN2.calculateSha256(),
-        var card: Card? = null,
-        var terminalKeys: KeyPair? = null,
-        var encryptionMode: EncryptionMode = EncryptionMode.NONE,
-        var encryptionKey: ByteArray? = null,
-        var cvc: ByteArray? = null,
-        var cardFilter: CardFilter = CardFilter(),
-        val handleErrors: Boolean = true
+class SessionEnvironment(
+        cardId: String?,
+        private val config: Config,
+        private val terminalKeysService: TerminalKeysService?,
+        private val cardValuesService: CardValuesService?
 ) {
 
-    var pin1: ByteArray = SessionEnvironment.pin1
-    private set
-    get() = SessionEnvironment.pin1
+    var pin1: PinCode?
+    var pin2: PinCode?
+    var cvc: ByteArray? = null
 
-    fun isCurrentPin1Default(): Boolean = Companion.pin1.contentEquals(DEFAULT_PIN.calculateSha256())
-    fun isCurrentPin2Default(): Boolean = pin2.contentEquals(DEFAULT_PIN2.calculateSha256())
+    var terminalKeys: KeyPair? = null
+    var cardFilter: CardFilter
+    val handleErrors: Boolean
 
-    fun setPin1(pin1: String) {
-        SessionEnvironment.pin1 = pin1.calculateSha256()
+    var encryptionMode: EncryptionMode
+    var encryptionKey: ByteArray? = null
+
+    var cardVerification: VerificationState
+    var cardValidation: VerificationState
+    var codeVerification: VerificationState
+
+    var card: Card? = null
+
+    init {
+        terminalKeys = if (config.linkedTerminal) terminalKeysService?.getKeys() else null
+        cardFilter = config.cardFilter
+        handleErrors = config.handleErrors
+
+        encryptionMode = config.encryptionMode
+
+        val cardValues = cardId?.let { cardValuesService?.getValues(cardId) }
+
+        cardVerification = cardValues?.cardVerification ?: VerificationState.NotVerified
+        cardValidation = cardValues?.cardValidation ?: VerificationState.NotVerified
+        codeVerification = cardValues?.codeVerification ?: VerificationState.NotVerified
+
+        pin1 = TangemSdk.pin1
+                ?: if (cardValues?.isPin1Default != false) {
+                    PinCode(config.defaultPin1, true)
+                } else {
+                    null
+                }
+
+        pin2 = cardId?.let { TangemSdk.pin2[it] }
+                ?: if (cardValues?.isPin2Default != false) {
+                    PinCode(config.defaultPin2, true)
+                } else {
+                    null
+                }
     }
 
-    fun setPin2(pin2: String) {
-        this.pin2 = pin2.calculateSha256()
+    fun restoreCardValues() {
+        val cardValues = this.card?.cardId?.let { cardValuesService?.getValues(it) }
+        cardVerification = cardValues?.cardVerification ?: VerificationState.NotVerified
+        cardValidation = cardValues?.cardValidation ?: VerificationState.NotVerified
+        codeVerification = cardValues?.codeVerification ?: VerificationState.NotVerified
+
+        if (cardValues?.isPin1Default == false && pin1?.isDefault == true) pin1 = null
+        if (cardValues?.isPin2Default == false && pin1?.isDefault == true) pin2 = null
     }
 
-    companion object {
-        const val DEFAULT_PIN = "000000"
-        const val DEFAULT_PIN2 = "000"
+    fun saveCardValues() {
+        if (config.savePin1InStaticField) {
+            TangemSdk.pin1 = pin1
+        }
+        if (config.savePin2InStaticField) {
+            card?.cardId?.let { cardId -> TangemSdk.pin2[cardId] = pin2 }
+        }
 
-        var pin1: ByteArray = DEFAULT_PIN.calculateSha256()
+        cardValuesService?.saveValues(this)
     }
+
 }
 
 /**
@@ -60,4 +103,8 @@ class KeyPair(val publicKey: ByteArray, val privateKey: ByteArray) {
 
     constructor(privateKey: ByteArray, curve: EllipticCurve = EllipticCurve.Secp256k1) :
             this(generatePublicKey(privateKey, curve), privateKey)
+}
+
+enum class VerificationState {
+    Passed, Offline, Failed, NotVerified
 }
