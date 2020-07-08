bb088fff3cc2ccf96ce20db0ac518e18ac3f8a22
Tangem <git@tangem.com>
2020-07-08 13:13:13 +0300

    Updated on 2026-05-04

--- tangem-core/src/main/java/com/tangem/SessionEnvironment.kt
+++ tangem-core/src/main/java/com/tangem/SessionEnvironment.kt
@@ -2,9 +2,7 @@ package com.tangem
 
 import com.tangem.commands.Card
 import com.tangem.commands.EllipticCurve
-import com.tangem.common.CardValuesStorage
 import com.tangem.common.PinCode
-import com.tangem.common.TerminalKeysService
 import com.tangem.crypto.CryptoUtils.generatePublicKey
 
 
@@ -27,79 +25,23 @@ import com.tangem.crypto.CryptoUtils.generatePublicKey
  *
  */
 class SessionEnvironment(
-        cardId: String?,
-        private val config: Config,
-        terminalKeysService: TerminalKeysService?,
-        private val cardValuesStorage: CardValuesStorage?
-) {
+        var pin1: PinCode? = PinCode(Config.DEFAULT_PIN_1, true),
+        var pin2: PinCode? = PinCode(Config.DEFAULT_PIN_2, true),
+        var cvc: ByteArray? = null,
 
-    var pin1: PinCode?
-    var pin2: PinCode?
-    var cvc: ByteArray? = null
+        var terminalKeys: KeyPair? = null,
+        var cardFilter: CardFilter = CardFilter(),
+        val handleErrors: Boolean = true,
 
-    var terminalKeys: KeyPair? = null
-    var cardFilter: CardFilter
-    val handleErrors: Boolean
+        var encryptionMode: EncryptionMode = EncryptionMode.NONE,
+        var encryptionKey: ByteArray? = null,
 
-    var encryptionMode: EncryptionMode
-    var encryptionKey: ByteArray? = null
+        var cardVerification: VerificationState = VerificationState.NotVerified,
+        var cardValidation: VerificationState = VerificationState.NotVerified,
+        var codeVerification: VerificationState = VerificationState.NotVerified,
 
-    var cardVerification: VerificationState
-    var cardValidation: VerificationState
-    var codeVerification: VerificationState
-
-    var card: Card? = null
-
-    init {
-        terminalKeys = if (config.linkedTerminal) terminalKeysService?.getKeys() else null
-        cardFilter = config.cardFilter
-        handleErrors = config.handleErrors
-
-        encryptionMode = config.encryptionMode
-
-        val cardValues = cardId?.let { cardValuesStorage?.getValues(cardId) }
-
-        cardVerification = cardValues?.cardVerification ?: VerificationState.NotVerified
-        cardValidation = cardValues?.cardValidation ?: VerificationState.NotVerified
-        codeVerification = cardValues?.codeVerification ?: VerificationState.NotVerified
-
-        pin1 = TangemSdk.pin1
-                ?: if (cardValues?.isPin1Default != false) {
-                    PinCode(config.defaultPin1, true)
-                } else {
-                    null
-                }
-
-        pin2 = cardId?.let { TangemSdk.pin2[it] }
-                ?: if (cardValues?.isPin2Default != false) {
-                    PinCode(config.defaultPin2, true)
-                } else {
-                    null
-                }
-    }
-
-    fun restoreCardValues() {
-        val cardValues = this.card?.cardId?.let { cardValuesStorage?.getValues(it) }
-        cardVerification = cardValues?.cardVerification ?: VerificationState.NotVerified
-        cardValidation = cardValues?.cardValidation ?: VerificationState.NotVerified
-        codeVerification = cardValues?.codeVerification ?: VerificationState.NotVerified
-
-        if (cardValues?.isPin1Default == false && pin1?.isDefault == true) pin1 = null
-        if (cardValues?.isPin2Default == false && pin1?.isDefault == true) pin2 = null
-    }
-
-    fun saveCardValues() {
-        if (config.savePin1InStaticField) {
-            TangemSdk.pin1 = pin1
-        }
-        if (config.savePin2InStaticField) {
-            card?.cardId?.let { cardId -> TangemSdk.pin2[cardId] = pin2 }
-        }
-
-        cardValuesStorage?.saveValues(this)
-    }
-
-}
+        var card: Card? = null
+)
 
 /**
  * All possible encryption modes.
