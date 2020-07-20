5b674563400903c15cdb9896f20d413a209ed177
Tangem <git@tangem.com>
2020-07-20 17:41:20 +0300

    Updated on 2025-09-17

---
+++ app/src/main/java/com/tangem/data/PayId.kt
@@ -0,0 +1,37 @@
+package com.tangem.data
+
+import java.util.*
+
+private val payIdSupported = EnumSet.of(
+        Blockchain.Ripple,
+        Blockchain.Ethereum,
+        Blockchain.Bitcoin,
+        Blockchain.Token,
+        Blockchain.Litecoin,
+        Blockchain.Stellar,
+        Blockchain.StellarAsset,
+        Blockchain.Binance,
+        Blockchain.Cardano,
+        Blockchain.Ducatus,
+        Blockchain.BitcoinCash,
+        Blockchain.Binance,
+        Blockchain.BinanceAsset,
+        Blockchain.Rootstock,
+        Blockchain.RootstockToken,
+        Blockchain.Tezos,
+        Blockchain.Eos,
+        Blockchain.Matic
+)
+
+fun Blockchain.isPayIdSupported(): Boolean {
+    return payIdSupported.contains(this)
+}
+
+fun Blockchain.getPayIdNetwork(): String {
+    return when (this) {
+        Blockchain.Ripple -> "XRPL"
+        Blockchain.Rootstock, Blockchain.RootstockToken -> "RSK"
+        else -> this.currency
+    }
+
+}
