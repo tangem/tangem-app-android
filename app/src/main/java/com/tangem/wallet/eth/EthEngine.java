82ae0baf64a5282dd974dd70fa22c45f5b146e3a
Tangem <git@tangem.com>
2020-07-20 22:58:59 +0300

    Updated on 2025-10-09

--- app/src/main/java/com/tangem/wallet/eth/EthEngine.java
+++ app/src/main/java/com/tangem/wallet/eth/EthEngine.java
@@ -145,18 +145,7 @@ public class EthEngine extends CoinEngine {
         }
 
         if (address.contains("$")) { // PayID
-            String[] addressParts = address.split("\\$");
-
-            if (addressParts.length != 2) {
-                return false;
-            }
-            String addressURL = "https://" + addressParts[1] + "/" + addressParts[0];
-            try {
-                new URL(addressURL).toURI();
-                return true;
-            } catch (Exception e) {
-                return false;
-            }
+            return validatePayId(address);
         }
 
         if (!address.startsWith("0x") && !address.startsWith("0X")) {
