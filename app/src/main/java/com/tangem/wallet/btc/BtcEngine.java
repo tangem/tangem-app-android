9d6a944983c7f63ac29b9dfc30f7ab722b2d72f3
Tangem <git@tangem.com>
2020-07-28 15:27:14 +0300

    Updated on 2026-09-23

--- app/src/main/java/com/tangem/wallet/btc/BtcEngine.java
+++ app/src/main/java/com/tangem/wallet/btc/BtcEngine.java
@@ -1087,7 +1087,7 @@ public class BtcEngine extends CoinEngine {
                         }
                     }
                     if (validateAddress(resolvedAddress)) {
-                        if (resolvedAddress.equals(coinData.getWallet())) {
+                        if (!resolvedAddress.equals(coinData.getWallet())) {
                             coinData.setResolvedPayIdAddress(resolvedAddress);
                             observer.onComplete();
                         } else {
