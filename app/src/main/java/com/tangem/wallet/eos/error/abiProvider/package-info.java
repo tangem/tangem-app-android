a459e7b4038a5e3540c04e97857ac1db16d9cd81
Tangem <git@tangem.com>
2020-08-27 12:14:38 +0300

    release/5.26.2

--- app/src/main/java/com/tangem/wallet/eos/error/abiProvider/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Provides the classes necessary for describe meaningful exceptions that occur during an ABI Provider implementation like:
- * {@link one.block.eosiojava.error.abiProvider.GetAbiError}
- */
-
-package com.tangem.wallet.eos.error.abiProvider;
