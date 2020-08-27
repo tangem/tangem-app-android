e382b092da803f89d71c4751360e1d250dce802e
Tangem <git@tangem.com>
2020-08-27 12:14:38 +0300

    Updated on 2026-01-20

--- app/src/main/java/com/tangem/wallet/eos/error/signatureProvider/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Provides the classes necessary to describe meaningful exceptions that occur during a signature
- * provider implementation like {@link one.block.eosiojava.error.signatureProvider.SignTransactionError}
- */
-
-package com.tangem.wallet.eos.error.signatureProvider;
