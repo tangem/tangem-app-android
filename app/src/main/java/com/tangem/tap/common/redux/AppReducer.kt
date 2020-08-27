3816bfa9c8499e1887810551de5a3ac24f77ed1a
Tangem <git@tangem.com>
2020-08-27 12:14:38 +0300

    Updated on 2025-12-18

---
+++ app/src/main/java/com/tangem/tap/common/redux/AppReducer.kt
@@ -0,0 +1,19 @@
+package com.tangem.tap.common.redux
+
+import com.tangem.tap.common.redux.navigation.navigationReducer
+import com.tangem.tap.features.wallet.redux.walletReducer
+import org.rekotlin.Action
+
+fun appReducer(action: Action, state: AppState?): AppState {
+    requireNotNull(state)
+    if (action is AppAction.RestoreState) return action.state
+    return AppState(
+        navigationState = navigationReducer(action, state),
+//        homeState = homeReducer(action, state),
+        walletState = walletReducer(action, state),
+    )
+}
+
+sealed class AppAction : Action {
+    data class RestoreState(val state: AppState) : AppAction()
+}
