570e77676749dd6f17c0818858986389899bb84a
Tangem <git@tangem.com>
2020-09-01 21:38:18 +0300

    Updated on 2026-06-12

---
+++ app/src/main/java/com/tangem/tap/features/send/ui/stateSubscribers/SendStateSubscriber.kt
@@ -0,0 +1,41 @@
+package com.tangem.tap.features.send.ui.stateSubscribers
+
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.transition.TransitionManager
+import com.tangem.tap.features.send.redux.FeeLayoutState
+import com.tangem.tap.features.send.redux.SendState
+import kotlinx.android.synthetic.main.btn_expand_collapse.*
+import kotlinx.android.synthetic.main.layout_send_network_fee.*
+import timber.log.Timber
+
+/**
+[REDACTED_AUTHOR]
+ */
+class SendStateSubscriber(fragment: Fragment) : FragmentStateSubscriber<SendState>(fragment) {
+
+    override fun updateWithNewState(fg: Fragment, state: SendState) {
+        when (state.lastChangedStateType) {
+            is FeeLayoutState -> handleFeeLayoutState(fg, state.feeLayoutState)
+        }
+    }
+
+    private fun handleFeeLayoutState(fg: Fragment, layoutState: FeeLayoutState) {
+        Timber.d("handleFeeLayoutState")
+        if (fg.llFeeContainer.visibility != layoutState.visibility) {
+            val rotationAngle = if (fg.imvExpandCollapse.rotation == 0f) 180f else 0f
+            fg.imvExpandCollapse.rotation = rotationAngle
+
+            (fg.llFeeContainer.parent?.parent as? ViewGroup)?.let { TransitionManager.beginDelayedTransition(it) }
+            fg.llFeeContainer.visibility = layoutState.visibility
+        }
+
+        if (fg.swLorem.isChecked != layoutState.swLoremIsChecked) {
+            fg.swLorem.isChecked = layoutState.swLoremIsChecked
+        }
+
+        if (fg.chipGroup.checkedChipId != layoutState.selectedFeeId) {
+            fg.chipGroup.check(layoutState.selectedFeeId)
+        }
+    }
+}
