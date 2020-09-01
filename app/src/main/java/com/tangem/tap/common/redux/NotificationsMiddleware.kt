0b75473cb6a401aedece48e3cfa9e87a62e6356f
Tangem <git@tangem.com>
2020-09-01 10:21:18 +0300

    Updated on 2026-08-14

--- app/src/main/java/com/tangem/tap/common/redux/NotificationsMiddleware.kt
+++ app/src/main/java/com/tangem/tap/common/redux/NotificationsMiddleware.kt
@@ -2,24 +2,33 @@ package com.tangem.tap.common.redux
 
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.google.android.material.snackbar.Snackbar
-import com.tangem.TangemError
+import com.tangem.tap.domain.TapError
 import com.tangem.tap.notificationsHandler
 import org.rekotlin.Action
 import org.rekotlin.Middleware
 import java.lang.ref.WeakReference
 
 class NotificationsHandler(coordinatorLayout: CoordinatorLayout) {
-    private val coordinatorLayoutWeak = WeakReference(coordinatorLayout)
+    private val basicCoordinatorLayout = WeakReference(coordinatorLayout)
+    private var baseLayout = basicCoordinatorLayout
+
+    fun replaceBaseLayout(coordinatorLayout: CoordinatorLayout) {
+        baseLayout = WeakReference(coordinatorLayout)
+    }
+
+    fun returnBaseLayout() {
+        baseLayout = basicCoordinatorLayout
+    }
 
     fun showNotification(message: String) {
-        coordinatorLayoutWeak.get()?.let { layout ->
+        baseLayout.get()?.let { layout ->
             Snackbar.make(layout, message, Snackbar.LENGTH_LONG)
-                .also { snackbar -> snackbar.show() }
+                    .also { snackbar -> snackbar.show() }
         }
     }
 
     fun showNotification(message: Int) {
-        coordinatorLayoutWeak.get()?.let {
+        baseLayout.get()?.let {
             showNotification(it.context.getString(message))
         }
     }
@@ -32,7 +41,7 @@ val notificationsMiddleware: Middleware<AppState> = { dispatch, state ->
                 notificationsHandler?.showNotification(action.messageResource)
             }
             if (action is ErrorAction) {
-                notificationsHandler?.showNotification(action.error.customMessage)
+                notificationsHandler?.showNotification(action.error.localizedMessage)
             }
             next(action)
         }
@@ -44,5 +53,5 @@ interface NotificationAction : Action {
 }
 
 interface ErrorAction : Action {
-    val error: TangemError
+    val error: TapError
 }
