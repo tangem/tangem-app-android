f6047e624235504effa833c02bb8a970775e607b
Tangem <git@tangem.com>
2020-09-01 10:21:18 +0300

    Updated on 2026-05-14

---
+++ app/src/main/java/com/tangem/tap/domain/TapErrors.kt
@@ -0,0 +1,10 @@
+package com.tangem.tap.domain
+
+import androidx.annotation.StringRes
+import com.tangem.wallet.R
+
+sealed class TapError(@StringRes val localizedMessage: Int): Throwable() {
+    object PayIdAlreadyCreated: TapError(R.string.error_payid_already_created)
+    object PayIdCreatingError: TapError(R.string.error_creating_payid)
+    object PayIdEmptyField: TapError(R.string.wallet_create_payid_empty)
+}
