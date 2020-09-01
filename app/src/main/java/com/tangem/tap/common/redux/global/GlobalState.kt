97f19fe45d059ce020923fbcc57a6f30f7ea272d
Tangem <git@tangem.com>
2020-09-01 12:20:32 +0300

    Updated on 2026-05-04

--- app/src/main/java/com/tangem/tap/common/redux/global/GlobalState.kt
+++ app/src/main/java/com/tangem/tap/common/redux/global/GlobalState.kt
@@ -3,11 +3,20 @@ package com.tangem.tap.common.redux.global
 import com.tangem.blockchain.common.WalletManager
 import com.tangem.commands.Card
 import org.rekotlin.StateType
+import java.math.BigDecimal
 
 data class GlobalState(
         val card: Card? = null,
         val walletManager: WalletManager? = null,
+        val fiatRates: FiatRates = FiatRates(emptyMap()),
 ) : StateType
 
+data class FiatRates(
+        val rates: Map<String, BigDecimal>
+) {
+    fun getRateForCryptoCurrency(currency: String): BigDecimal? {
+        return rates[currency]
+    }
+}
 
 
