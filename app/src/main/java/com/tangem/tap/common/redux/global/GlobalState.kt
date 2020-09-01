41da66c0962b870b79ef4cbc1caedb44f0708660
Tangem <git@tangem.com>
2020-09-01 14:07:06 +0300

    Updated on 2026-09-14

--- app/src/main/java/com/tangem/tap/common/redux/global/GlobalState.kt
+++ app/src/main/java/com/tangem/tap/common/redux/global/GlobalState.kt
@@ -2,12 +2,14 @@ package com.tangem.tap.common.redux.global
 
 import com.tangem.blockchain.common.WalletManager
 import com.tangem.commands.Card
+import com.tangem.tap.domain.TapWalletManager
 import org.rekotlin.StateType
 import java.math.BigDecimal
 
 data class GlobalState(
         val card: Card? = null,
         val walletManager: WalletManager? = null,
+        val tapWalletManager: TapWalletManager = TapWalletManager(),
         val fiatRates: FiatRates = FiatRates(emptyMap()),
 ) : StateType
 
