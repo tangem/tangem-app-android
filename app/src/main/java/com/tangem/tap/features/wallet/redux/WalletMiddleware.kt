4f109f4b53fa331caabd31e5a6a8c7bfafb9ee61
Tangem <git@tangem.com>
2020-09-01 14:07:06 +0300

    Updated on 2026-09-21

--- app/src/main/java/com/tangem/tap/features/wallet/redux/WalletMiddleware.kt
+++ app/src/main/java/com/tangem/tap/features/wallet/redux/WalletMiddleware.kt
@@ -6,14 +6,11 @@ import androidx.core.content.ContextCompat
 import com.tangem.commands.common.network.Result
 import com.tangem.common.CompletionResult
 import com.tangem.common.extensions.toHexString
-import com.tangem.tap.TapConfig
 import com.tangem.tap.common.extensions.copyToClipboard
 import com.tangem.tap.common.redux.AppState
 import com.tangem.tap.common.redux.global.GlobalAction
 import com.tangem.tap.domain.PayIdManager
 import com.tangem.tap.domain.TapError
-import com.tangem.tap.domain.isPayIdSupported
-import com.tangem.tap.network.coinmarketcap.CoinMarketCapService
 import com.tangem.tap.scope
 import com.tangem.tap.store
 import com.tangem.tap.tangemSdkManager
@@ -29,80 +26,17 @@ val walletMiddleware: Middleware<AppState> = { dispatch, state ->
             when (action) {
                 is WalletAction.LoadWallet -> {
                     scope.launch {
-                        val walletManager = store.state.globalState.walletManager
-                        if (walletManager == null) {
-                            store.dispatch(WalletAction.LoadWallet.Failure)
-                            return@launch
-                        }
-                        try {
-                            walletManager.update()
-                        } catch (ex: Exception) {
-                            withContext(Dispatchers.Main) {
-                                store.dispatch(WalletAction.LoadWallet.Failure)
-//                            callback(CompletionResult.Failure(BlockchainInternalErrorConverter.convert(ex)))
-                                next(action)
-                            }
-                        }
-                        withContext(Dispatchers.Main) {
-                            store.dispatch(WalletAction.LoadWallet.Success(walletManager.wallet))
-                        }
+                        store.state.globalState.tapWalletManager.loadWalletData()
+                    }
+                }
+                is WalletAction.LoadPayId -> {
+                    scope.launch {
+                        store.state.globalState.tapWalletManager.loadPayId()
                     }
                 }
                 is WalletAction.LoadFiatRate -> {
                     scope.launch {
-                        val blockchainCurrency = store.state.globalState.walletManager?.wallet?.blockchain?.currency
-                        val tokenCurrency = store.state.globalState.walletManager?.wallet?.token?.symbol
-
-                        val blockchainRate = blockchainCurrency?.let { CoinMarketCapService().getRate(it) }
-                        val tokenRate = tokenCurrency?.let { CoinMarketCapService().getRate(it) }
-
-                        withContext(Dispatchers.Main) {
-                            when (blockchainRate) {
-                                is Result.Success -> {
-                                    store.dispatch(GlobalAction.SetFiatRate(blockchainCurrency to blockchainRate.data)
-                                    )
-                                    store.dispatch(WalletAction.LoadFiatRate.Success(blockchainCurrency to blockchainRate.data)
-                                    )
-                                }
-                                is Result.Failure, null -> store.dispatch(WalletAction.LoadFiatRate.Failure)
-                            }
-                            when (tokenRate) {
-                                is Result.Success -> {
-                                    store.dispatch(GlobalAction.SetFiatRate(tokenCurrency to tokenRate.data)
-                                    )
-                                    store.dispatch(WalletAction.LoadFiatRate.Success(tokenCurrency to tokenRate.data)
-                                    )
-                                }
-                                is Result.Failure, null -> store.dispatch(WalletAction.LoadFiatRate.Failure)
-                            }
-                        }
-                    }
-                }
-                is WalletAction.LoadPayId -> {
-                    if (!TapConfig.usePayId ||
-                            store.state.walletState.payIdData.payIdState == PayIdState.Disabled ||
-                            store.state.globalState.walletManager?.wallet?.blockchain?.isPayIdSupported() == false) {
-                        next(action)
-                    }
-                    scope.launch {
-                        val cardId = store.state.globalState.card?.cardId
-                        val publicKey = store.state.globalState.card?.cardPublicKey
-                        if (cardId != null && publicKey != null) {
-                            val result = PayIdManager().getPayId(cardId, publicKey.toHexString())
-                            withContext(Dispatchers.Main) {
-                                when (result) {
-                                    is Result.Success -> {
-                                        val payId = result.data
-                                        if (payId == null) {
-                                            store.dispatch(WalletAction.LoadPayId.NotCreated)
-                                        } else {
-                                            store.dispatch(WalletAction.LoadPayId.Success(payId))
-                                        }
-                                    }
-                                    is Result.Failure -> store.dispatch(WalletAction.LoadPayId.Failure)
-                                }
-                            }
-                        }
+                        store.state.globalState.tapWalletManager.loadFiatRate()
                     }
                 }
                 is WalletAction.CreatePayId.CompleteCreatingPayId -> {
