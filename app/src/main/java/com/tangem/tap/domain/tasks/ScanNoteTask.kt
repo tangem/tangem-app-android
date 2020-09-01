1c3686c71c81a19e631c8e6c0a1bbed0b4979811
Tangem <git@tangem.com>
2020-09-01 10:21:18 +0300

    Updated on 2026-01-24

--- app/src/main/java/com/tangem/tap/domain/tasks/ScanNoteTask.kt
+++ app/src/main/java/com/tangem/tap/domain/tasks/ScanNoteTask.kt
@@ -5,20 +5,25 @@ import com.tangem.CardSessionRunnable
 import com.tangem.TangemSdkError
 import com.tangem.blockchain.common.WalletManager
 import com.tangem.blockchain.common.WalletManagerFactory
+import com.tangem.commands.Card
 import com.tangem.commands.CommandResponse
 import com.tangem.common.CompletionResult
 
-data class ScanNoteResponse(val walletManager: WalletManager) : CommandResponse
+data class ScanNoteResponse(
+        val walletManager: WalletManager,
+        val card: Card
+) : CommandResponse
 
 class ScanNoteTask : CardSessionRunnable<ScanNoteResponse> {
     override val requiresPin2 = false
 
     override fun run(session: CardSession, callback: (result: CompletionResult<ScanNoteResponse>) -> Unit) {
-        val walletManager = session.environment.card?.let { WalletManagerFactory.makeWalletManager(it) }
-        if (walletManager == null) {
+        val card = session.environment.card
+        val walletManager = card?.let { WalletManagerFactory.makeWalletManager(it) }
+        if (card == null || walletManager == null) {
             callback(CompletionResult.Failure(TangemSdkError.CardError()))
             return
         }
-        callback(CompletionResult.Success(ScanNoteResponse(walletManager)))
+        callback(CompletionResult.Success(ScanNoteResponse(walletManager, card)))
     }
 }
