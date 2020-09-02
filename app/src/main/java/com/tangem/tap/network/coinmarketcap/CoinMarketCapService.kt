6d2dd1c2e3c5199cde92ae96d55b3ed82779150b
Tangem <git@tangem.com>
2020-09-02 12:52:38 +0300

    Updated on 2026-06-27

--- app/src/main/java/com/tangem/tap/network/coinmarketcap/CoinMarketCapService.kt
+++ app/src/main/java/com/tangem/tap/network/coinmarketcap/CoinMarketCapService.kt
@@ -2,14 +2,16 @@ package com.tangem.tap.network.coinmarketcap
 
 import com.tangem.commands.common.network.Result
 import com.tangem.commands.common.network.performRequest
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.math.BigDecimal
 
 class CoinMarketCapService {
     private val api: CoinMarketCapApi by lazy { CoinMarketCapApi.create() }
 
-    suspend fun getRate(currency: String): Result<BigDecimal> {
+    suspend fun getRate(currency: String): Result<BigDecimal> = withContext(Dispatchers.IO) {
         val response = performRequest { api.getRateInfo(1, currency) }
-        return when (response) {
+        return@withContext when (response) {
             is Result.Success -> Result.Success(response.data.data.quote.usd.price)
             is Result.Failure -> response
         }
