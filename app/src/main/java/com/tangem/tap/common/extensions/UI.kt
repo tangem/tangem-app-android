b57c9279d83c3dc4ca2bca66793209e2ca170d04
Tangem <git@tangem.com>
2020-09-03 02:55:23 +0300

    Updated on 2026-02-06

--- app/src/main/java/com/tangem/tap/common/extensions/UI.kt
+++ app/src/main/java/com/tangem/tap/common/extensions/UI.kt
@@ -108,6 +108,14 @@ fun Context.copyToClipboard(value: Any, label: String = "") {
     clipboard.setPrimaryClip(clip)
 }
 
+fun Context.getFromClipboard(default: CharSequence? = null): CharSequence? {
+    val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as? ClipboardManager ?: return default
+    val clipData = clipboard.primaryClip ?: return default
+    if (clipData.itemCount == 0) return default
+
+    return clipData.getItemAt(0).text
+}
+
 fun Context.shareText(text: String) {
     val sendIntent: Intent = Intent().apply {
         action = Intent.ACTION_SEND
