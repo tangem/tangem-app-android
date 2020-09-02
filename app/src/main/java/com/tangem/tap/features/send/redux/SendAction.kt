bae7dee883c6b0b20349f2fc6549f1e5afc3ee9d
Tangem <git@tangem.com>
2020-09-02 22:16:51 +0300

    Updated on 2025-09-17

--- app/src/main/java/com/tangem/tap/features/send/redux/SendAction.kt
+++ app/src/main/java/com/tangem/tap/features/send/redux/SendAction.kt
@@ -10,5 +10,5 @@ interface SendAction : Action
 sealed class FeeLayout : SendAction {
     object ToggleFeeLayoutVisibility : FeeLayout()
     data class ChangeSelectedFee(val id: Int) : FeeLayout()
-    class ChangeLoremState(val isChecked: Boolean) : FeeLayout()
+    class ChangeIncludeFee(val isChecked: Boolean) : FeeLayout()
 }
