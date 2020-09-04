263111f9047fc3d03539a525fe68f245d6c33c9d
Tangem <git@tangem.com>
2020-09-04 20:14:19 +0300

    Updated on 2026-06-24

--- app/src/main/java/com/tangem/tap/features/send/redux/SendState.kt
+++ app/src/main/java/com/tangem/tap/features/send/redux/SendState.kt
@@ -18,10 +18,28 @@ data class SendState(
 class NoneState : StateType
 
 data class AddressPayIDState(
-        val value: String? = null,
-        val payIDWalletAddress: String? = null,
-        val error: String? = null,
-) : StateType
+        val etFieldValue: String? = null,
+        val walletAddress: String? = null,
+        val error: AddressPayIdVerifyAction.FailReason? = null,
+) : StateType {
+
+    fun isPayIdState():Boolean = walletAddress != null && walletAddress != etFieldValue
+
+    fun copyWalletAddress(address: String): AddressPayIDState {
+        return this.copy(etFieldValue = address, walletAddress = address, error = null)
+    }
+    fun copyError(address: String, error: AddressPayIdVerifyAction.FailReason): AddressPayIDState {
+        return this.copy(etFieldValue = address, error = error, walletAddress = null)
+    }
+
+    fun copyPayIdWalletAddress(payId: String, address: String): AddressPayIDState {
+        return this.copy(etFieldValue = payId, walletAddress = address, error = null)
+    }
+
+    fun copyPaiIdError(payId: String, error: AddressPayIdVerifyAction.FailReason): AddressPayIDState {
+        return this.copy(etFieldValue = payId, error = error, walletAddress = null)
+    }
+}
 
 data class FeeLayoutState(
         val visibility: Int = View.GONE,
