862ae099ad6065e2d9d80dbcc3938480344129c2
Tangem <git@tangem.com>
2020-09-04 20:14:19 +0300

    release/5.26.1

---
+++ app/src/main/java/com/tangem/tap/network/payid/PayIdVerifyService.kt
@@ -0,0 +1,21 @@
+package com.tangem.tap.network.payid
+
+import com.tangem.commands.common.network.Result
+import com.tangem.commands.common.network.performRequest
+import com.tangem.tap.network.createRetrofitInstance
+
+/**
+* [REDACTED_AUTHOR]
+ */
+class PayIdVerifyService(
+        private val baseUrl: String
+) {
+
+    private val api = createRetrofitInstance(baseUrl).create(PayIdVerifyApi::class.java)
+
+    suspend fun verifyAddress(user: String, network: String): Result<VerifyPayIdResponse> {
+        return performRequest { api.verifyAddress(user, createNetworkHeader(network)) }
+    }
+
+    private fun createNetworkHeader(network: String): String = "application/$network-mainnet+json"
+}
