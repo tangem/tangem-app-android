5f54c35f1a43179ba91d2684a8b9dc767491a2a8
Tangem <git@tangem.com>
2020-09-04 13:14:28 +0300

    Updated on 2026-09-25

--- app/src/main/java/com/tangem/tap/domain/tasks/ScanNoteTask.kt
+++ app/src/main/java/com/tangem/tap/domain/tasks/ScanNoteTask.kt
@@ -8,11 +8,14 @@ import com.tangem.blockchain.common.WalletManagerFactory
 import com.tangem.commands.Card
 import com.tangem.commands.CardStatus
 import com.tangem.commands.CommandResponse
+import com.tangem.commands.verifycard.VerifyCardCommand
+import com.tangem.commands.verifycard.VerifyCardResponse
 import com.tangem.common.CompletionResult
 
 data class ScanNoteResponse(
         val walletManager: WalletManager?,
-        val card: Card
+        val card: Card,
+        val verifyResponse: VerifyCardResponse? = null
 ) : CommandResponse
 
 class ScanNoteTask : CardSessionRunnable<ScanNoteResponse> {
@@ -20,7 +23,26 @@ class ScanNoteTask : CardSessionRunnable<ScanNoteResponse> {
 
     override fun run(session: CardSession, callback: (result: CompletionResult<ScanNoteResponse>) -> Unit) {
         val card = session.environment.card
-        callback(card.toScanNoteCompletionResult())
+        card ?: return callback(CompletionResult.Failure(TangemSdkError.CardError()))
+        if (card.status == CardStatus.Empty) {
+            return callback(CompletionResult.Success(ScanNoteResponse(null, card)))
+        }
+        val walletManager = WalletManagerFactory.makeWalletManager(card)
+                ?: return callback(CompletionResult.Failure(TangemSdkError.CardError()))
+
+        VerifyCardCommand(true).run(session) { result ->
+            when (result) {
+                is CompletionResult.Success -> {
+                    callback(CompletionResult.Success(ScanNoteResponse(
+                            walletManager, card, result.data
+                    )))
+                }
+                is CompletionResult.Failure -> {
+                    callback(CompletionResult.Failure(TangemSdkError.VerificationFailed()))
+                }
+            }
+        }
+
     }
 }
 
