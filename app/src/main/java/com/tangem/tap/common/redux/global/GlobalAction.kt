dfb0c07b381468dd5ab2ea29ae0f214bda91db3d
Tangem <git@tangem.com>
2020-09-07 10:27:14 +0300

    Updated on 2026-02-11

--- app/src/main/java/com/tangem/tap/common/redux/global/GlobalAction.kt
+++ app/src/main/java/com/tangem/tap/common/redux/global/GlobalAction.kt
@@ -1,13 +1,11 @@
 package com.tangem.tap.common.redux.global
 
-import com.tangem.blockchain.common.WalletManager
-import com.tangem.commands.Card
+import com.tangem.tap.domain.tasks.ScanNoteResponse
 import org.rekotlin.Action
 import java.math.BigDecimal
 
 sealed class GlobalAction : Action {
 
-    data class LoadCard(val card: Card) : GlobalAction()
-    data class LoadWalletManager(val walletManager: WalletManager) : GlobalAction()
+    data class SaveScanNoteResponse(val scanNoteResponse: ScanNoteResponse) : GlobalAction()
     data class SetFiatRate(val fiatRates: Pair<String, BigDecimal>) : GlobalAction()
 }
