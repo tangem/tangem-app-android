abbc4f9386ca07bfea4b0528ee8afe3a7e1ffaa0
Tangem <git@tangem.com>
2020-09-13 21:12:18 +0300

    Updated on 2025-09-17

---
+++ app/src/main/java/com/tangem/tap/features/send/redux/states/AddressPayIdState.kt
@@ -0,0 +1,62 @@
+package com.tangem.tap.features.send.redux.states
+
+import com.tangem.tap.features.send.redux.AddressPayIdVerifyAction
+import org.rekotlin.StateType
+
+data class AddressPayIdState(
+        val etFieldValue: String? = null,
+        val normalFieldValue: String? = null,
+        val truncatedFieldValue: String? = null,
+        val recipientWalletAddress: String? = null,
+        val error: AddressPayIdVerifyAction.Error? = null,
+        val truncateHandler: ((String) -> String)? = null
+) : StateType {
+
+    fun isReady(): Boolean = error == null && recipientWalletAddress?.isNotEmpty() ?: false
+
+    fun isPayIdState(): Boolean = recipientWalletAddress != null && recipientWalletAddress != normalFieldValue
+
+    fun copyWalletAddress(address: String): AddressPayIdState {
+        val truncated = truncateHandler?.invoke(address) ?: address
+        return this.copy(
+                etFieldValue = address,
+                normalFieldValue = address,
+                truncatedFieldValue = truncated,
+                recipientWalletAddress = address,
+                error = null
+        )
+    }
+
+    fun copyError(address: String, error: AddressPayIdVerifyAction.Error): AddressPayIdState {
+        val truncated = truncateHandler?.invoke(address) ?: address
+        return this.copy(
+                etFieldValue = address,
+                normalFieldValue = address,
+                truncatedFieldValue = truncated,
+                error = error,
+                recipientWalletAddress = null
+        )
+    }
+
+    fun copyPayIdWalletAddress(payId: String, address: String): AddressPayIdState {
+        val truncated = truncateHandler?.invoke(address) ?: address
+        return this.copy(
+                etFieldValue = payId,
+                normalFieldValue = payId,
+                truncatedFieldValue = truncated,
+                recipientWalletAddress = address,
+                error = null
+        )
+    }
+
+    fun copyPayIdError(payId: String, error: AddressPayIdVerifyAction.Error): AddressPayIdState {
+        val truncated = truncateHandler?.invoke(payId) ?: payId
+        return this.copy(
+                etFieldValue = payId,
+                normalFieldValue = payId,
+                truncatedFieldValue = truncated,
+                error = error,
+                recipientWalletAddress = null
+        )
+    }
+}
