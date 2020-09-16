8850375509d52c49b38c524f3c0750fbeb73052c
Tangem <git@tangem.com>
2020-09-16 21:18:42 +0300

    Updated on 2026-07-07

--- app/src/main/java/com/tangem/tap/features/details/redux/DetailsReducer.kt
+++ app/src/main/java/com/tangem/tap/features/details/redux/DetailsReducer.kt
@@ -1,6 +1,8 @@
 package com.tangem.tap.features.details.redux
 
 import com.tangem.commands.Card
+import com.tangem.commands.Settings
+import com.tangem.common.extensions.isZero
 import com.tangem.tap.common.redux.AppState
 import org.rekotlin.Action
 
@@ -16,11 +18,74 @@ private fun internalReduce(action: Action, state: AppState): DetailsState {
 
     var detailsState = state.detailsState
     when (action) {
-        is DetailsAction.SetCard -> {
-            detailsState = DetailsState(card = action.card, cardInfo = action.card.toCardInfo())
+        is DetailsAction.PrepareScreen -> {
+            detailsState = DetailsState(
+                    card = action.card, wallet = action.wallet,
+                    cardInfo = action.card.toCardInfo(),
+                    appCurrencyState = AppCurrencyState(
+                            action.fiatCurrencyName
+                    )
+            )
+        }
+        is DetailsAction.EraseWallet -> {
+            detailsState = handleEraseWallet(action, detailsState)
+        }
+        is DetailsAction.AppCurrencyAction -> {
+            detailsState = handleAppCurrencyAction(action, detailsState)
         }
     }
     return detailsState
+}
+
+private fun handleEraseWallet(action: DetailsAction.EraseWallet, state: DetailsState): DetailsState {
+    return when (action) {
+        DetailsAction.EraseWallet.Check -> {
+            val notAllowedByCard = state.card?.settingsMask?.contains(Settings.ProhibitPurgeWallet) == true
+            val notEmpty = state.wallet?.transactions?.isNullOrEmpty() != true ||
+                    state.wallet.amounts.toList().unzip().second.map { it.value?.isZero() }.contains(false)
+            val eraseWalletState = when {
+                notAllowedByCard -> EraseWalletState.NotAllowedByCard
+                notEmpty -> EraseWalletState.NotEmpty
+                else -> EraseWalletState.Allowed
+            }
+            state.copy(eraseWalletState = eraseWalletState)
+        }
+        DetailsAction.EraseWallet.Proceed -> {
+            if (state.eraseWalletState == EraseWalletState.Allowed) {
+                state.copy(confirmScreenState = ConfirmScreenState.EraseWallet)
+            } else {
+                state
+            }
+        }
+        DetailsAction.EraseWallet.Cancel -> state.copy(eraseWalletState = null)
+        DetailsAction.EraseWallet.Failure -> state.copy(eraseWalletState = null)
+        DetailsAction.EraseWallet.Success -> state.copy(eraseWalletState = null)
+        else -> state
+    }
+}
+
+private fun handleAppCurrencyAction(
+        action: DetailsAction.AppCurrencyAction, state: DetailsState
+): DetailsState {
+    return when (action) {
+        is DetailsAction.AppCurrencyAction.SetCurrencies -> {
+            state.copy(appCurrencyState = state.appCurrencyState.copy(fiatCurrencies = action.currencies))
+        }
+        DetailsAction.AppCurrencyAction.ChooseAppCurrency -> {
+            state.copy(appCurrencyState = state.appCurrencyState.copy(showAppCurrencyDialog = true))
+        }
+        DetailsAction.AppCurrencyAction.Cancel -> {
+            state.copy(appCurrencyState = state.appCurrencyState.copy(showAppCurrencyDialog = false))
+        }
+        is DetailsAction.AppCurrencyAction.SelectAppCurrency -> {
+            state.copy(
+                    appCurrencyState = state.appCurrencyState.copy(
+                            fiatCurrencyName = action.fiatCurrencyName, showAppCurrencyDialog = false
+                    )
+            )
+        }
+        else -> state
+    }
 }
 
 private fun Card.toCardInfo(): CardInfo? {
