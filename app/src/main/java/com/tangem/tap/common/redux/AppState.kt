45161c823af0a7f2eeb4cc98dc7b7a4fbee029c6
Tangem <git@tangem.com>
2020-09-16 21:18:42 +0300

    Updated on 2026-06-27

--- app/src/main/java/com/tangem/tap/common/redux/AppState.kt
+++ app/src/main/java/com/tangem/tap/common/redux/AppState.kt
@@ -1,8 +1,10 @@
 package com.tangem.tap.common.redux
 
 import com.tangem.tap.common.redux.global.GlobalState
+import com.tangem.tap.common.redux.global.globalMiddleware
 import com.tangem.tap.common.redux.navigation.NavigationState
 import com.tangem.tap.common.redux.navigation.navigationMiddleware
+import com.tangem.tap.features.details.redux.DetailsMiddleware
 import com.tangem.tap.features.details.redux.DetailsState
 import com.tangem.tap.features.home.redux.homeMiddleware
 import com.tangem.tap.features.send.redux.middlewares.sendMiddleware
@@ -13,18 +15,19 @@ import org.rekotlin.Middleware
 import org.rekotlin.StateType
 
 data class AppState(
-    val navigationState: NavigationState = NavigationState(),
-    val globalState: GlobalState = GlobalState(),
-    val walletState: WalletState = WalletState(),
-    val sendState: SendState = SendState(),
-    val detailsState: DetailsState = DetailsState()
+        val navigationState: NavigationState = NavigationState(),
+        val globalState: GlobalState = GlobalState(),
+        val walletState: WalletState = WalletState(),
+        val sendState: SendState = SendState(),
+        val detailsState: DetailsState = DetailsState()
 ) : StateType {
 
     companion object {
         fun getMiddleware(): List<Middleware<AppState>> {
             return listOf(
-                logMiddleware, navigationMiddleware, notificationsMiddleware,
-                homeMiddleware, walletMiddleware, sendMiddleware
+                    logMiddleware, navigationMiddleware, notificationsMiddleware, globalMiddleware,
+                    homeMiddleware, walletMiddleware, sendMiddleware,
+                    DetailsMiddleware().detailsMiddleware
             )
         }
     }
