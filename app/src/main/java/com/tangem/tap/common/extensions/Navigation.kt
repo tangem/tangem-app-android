cb6d47ea14e33c93989259c1ba10e224992fc787
Tangem <git@tangem.com>
2020-09-16 21:18:42 +0300

    Updated on 2026-08-07

--- app/src/main/java/com/tangem/tap/common/extensions/Navigation.kt
+++ app/src/main/java/com/tangem/tap/common/extensions/Navigation.kt
@@ -4,6 +4,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import com.tangem.tap.common.redux.navigation.AppScreen
+import com.tangem.tap.features.details.ui.DetailsConfirmFragment
 import com.tangem.tap.features.details.ui.DetailsFragment
 import com.tangem.tap.features.home.HomeFragment
 import com.tangem.tap.features.send.ui.SendFragment
@@ -38,5 +39,6 @@ private fun fragmentFactory(screen: AppScreen): Fragment {
         AppScreen.Wallet -> WalletFragment()
         AppScreen.Send -> SendFragment()
         AppScreen.Details -> DetailsFragment()
+        AppScreen.DetailsConfirm -> DetailsConfirmFragment()
     }
 }
