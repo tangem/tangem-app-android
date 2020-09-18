ca269055bc7f71b70fa8a2ece4d68f4eee053309
Tangem <git@tangem.com>
2020-09-18 13:34:31 +0300

    Updated on 2026-02-11

--- app/src/main/java/com/tangem/tap/domain/tasks/ScanNoteTask.kt
+++ app/src/main/java/com/tangem/tap/domain/tasks/ScanNoteTask.kt
@@ -10,6 +10,7 @@ import com.tangem.commands.CommandResponse
 import com.tangem.commands.verifycard.VerifyCardCommand
 import com.tangem.commands.verifycard.VerifyCardResponse
 import com.tangem.common.CompletionResult
+import com.tangem.tasks.ScanTask
 
 data class ScanNoteResponse(
         val walletManager: WalletManager?,
@@ -21,26 +22,32 @@ class ScanNoteTask : CardSessionRunnable<ScanNoteResponse> {
     override val requiresPin2 = false
 
     override fun run(session: CardSession, callback: (result: CompletionResult<ScanNoteResponse>) -> Unit) {
-        val card = session.environment.card
-        card ?: return callback(CompletionResult.Failure(TangemSdkError.CardError()))
-        val walletManager = try {
-            WalletManagerFactory.makeWalletManager(card)
-        } catch (exception: Exception) {
-            return callback(CompletionResult.Success(ScanNoteResponse(null, card)))
-        }
-        VerifyCardCommand(true).run(session) { result ->
+        ScanTask().run(session) { result ->
             when (result) {
+                is CompletionResult.Failure -> callback(CompletionResult.Failure(result.error))
+
                 is CompletionResult.Success -> {
-                    callback(CompletionResult.Success(ScanNoteResponse(
-                            walletManager, card, result.data
-                    )))
-                }
-                is CompletionResult.Failure -> {
-                    callback(CompletionResult.Failure(TangemSdkError.VerificationFailed()))
+                    val card = result.data
+                    val walletManager = try {
+                        WalletManagerFactory.makeWalletManager(card)
+                    } catch (exception: Exception) {
+                        return@run callback(CompletionResult.Success(ScanNoteResponse(null, card)))
+                    }
+                    VerifyCardCommand(true).run(session) { verifyResult ->
+                        when (verifyResult) {
+                            is CompletionResult.Success -> {
+                                callback(CompletionResult.Success(ScanNoteResponse(
+                                        walletManager, card, verifyResult.data
+                                )))
+                            }
+                            is CompletionResult.Failure -> {
+                                callback(CompletionResult.Failure(TangemSdkError.VerificationFailed()))
+                            }
+                        }
+                    }
                 }
             }
         }
-
     }
 }
 
