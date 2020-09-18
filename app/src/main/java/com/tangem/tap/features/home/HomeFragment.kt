d639962f8671ac21b1bc94eda87d20cabc08d14f
Tangem <git@tangem.com>
2020-09-18 13:34:31 +0300

    Updated on 2026-07-01

--- app/src/main/java/com/tangem/tap/features/home/HomeFragment.kt
+++ app/src/main/java/com/tangem/tap/features/home/HomeFragment.kt
@@ -5,11 +5,43 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.transition.TransitionInflater
 import com.tangem.tap.features.home.redux.HomeAction
+import com.tangem.tap.features.home.redux.HomeState
 import com.tangem.tap.store
 import com.tangem.wallet.R
 import kotlinx.android.synthetic.main.fragment_home.*
+import org.rekotlin.StoreSubscriber
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment(R.layout.fragment_home), StoreSubscriber<HomeState> {
+
+    override fun onStart() {
+        super.onStart()
+        store.subscribe(this) { state ->
+            state.skipRepeats { oldState, newState ->
+                oldState.homeState == newState.homeState
+            }.select { it.homeState }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        store.unsubscribe(this)
+    }
+
+
+    override fun newState(state: HomeState) {
+        if (activity == null) return
+requireContext().cacheDir
+        if (state.firstLaunch) {
+            tv_home_description.text = getText(R.string.home_do_you_have_card)
+            btn_shop.text = getText(R.string.home_button_no_card)
+            btn_yes.text = getText(R.string.home_button_yes)
+        } else {
+            tv_home_description.text = getText(R.string.home_welcome_back)
+            btn_shop.text = getText(R.string.home_button_shop)
+            btn_yes.text = getText(R.string.home_button_scan)
+        }
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
