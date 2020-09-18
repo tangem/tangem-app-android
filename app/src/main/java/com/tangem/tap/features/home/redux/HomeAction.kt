6a0710cc46fcdb1197be855e90f839e922dd99d5
Tangem <git@tangem.com>
2020-09-18 13:34:31 +0300

    release/5.26.1

--- app/src/main/java/com/tangem/tap/features/home/redux/HomeAction.kt
+++ app/src/main/java/com/tangem/tap/features/home/redux/HomeAction.kt
@@ -6,5 +6,8 @@ import org.rekotlin.Action
 sealed class HomeAction : Action {
     object ReadCard : HomeAction()
     data class GoToShop(val context: Context) : HomeAction()
+    object CheckIfFirstLaunch : HomeAction() {
+        data class Result(val firstLaunch: Boolean) : HomeAction()
+    }
 }
 
