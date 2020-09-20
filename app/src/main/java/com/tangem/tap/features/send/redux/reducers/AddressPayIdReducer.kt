7ca7dc220356fa5b13eaf10b5ed6070e4d65a36f
Tangem <git@tangem.com>
2020-09-20 21:04:18 +0300

    Updated on 2026-07-23

--- app/src/main/java/com/tangem/tap/features/send/redux/reducers/AddressPayIdReducer.kt
+++ app/src/main/java/com/tangem/tap/features/send/redux/reducers/AddressPayIdReducer.kt
@@ -6,6 +6,7 @@ import com.tangem.tap.features.send.redux.AddressPayIdVerifyAction.AddressVerifi
 import com.tangem.tap.features.send.redux.AddressPayIdVerifyAction.PayIdVerification
 import com.tangem.tap.features.send.redux.SendScreenAction
 import com.tangem.tap.features.send.redux.states.AddressPayIdState
+import com.tangem.tap.features.send.redux.states.InputViewValue
 import com.tangem.tap.features.send.redux.states.SendState
 
 /**
@@ -20,24 +21,43 @@ class AddressPayIdReducer : SendInternalReducer {
 
     private fun handleUiAction(action: AddressPayIdActionUi, sendState: SendState, state: AddressPayIdState): SendState {
         val result = when (action) {
-            is AddressPayIdActionUi.ChangeAddressOrPayId -> state
+            is AddressPayIdActionUi.HandleUserInput -> state
             is AddressPayIdActionUi.SetTruncateHandler -> state.copy(truncateHandler = action.handler)
             is AddressPayIdActionUi.TruncateOrRestore -> {
-                if (action.truncate) state.copy(etFieldValue = state.truncatedFieldValue)
-                else state.copy(etFieldValue = state.normalFieldValue)
+                val value = if (action.truncate) state.truncatedFieldValue ?: ""
+                else state.normalFieldValue ?: ""
+                state.copy(viewFieldValue = state.viewFieldValue.copy(value = value))
             }
+            is AddressPayIdActionUi.PasteAddressPayId -> return sendState
+            is AddressPayIdActionUi.CheckClipboard -> return sendState
+            is AddressPayIdActionUi.CheckAddressPayId -> return sendState
         }
         return updateLastState(sendState.copy(addressPayIdState = result), result)
     }
 
     private fun handleAction(action: AddressPayIdVerifyAction, sendState: SendState, state: AddressPayIdState): SendState {
         val result = when (action) {
-            is PayIdVerification.SetPayIdWalletAddress -> state.copyPayIdWalletAddress(action.payId, action.payIdWalletAddress)
-            is PayIdVerification.SetError -> state.copyPayIdError(action.payId, action.error)
-            is AddressVerification.SetWalletAddress -> state.copyWalletAddress(action.address)
-            is AddressVerification.SetError -> state.copyError(action.address, action.error)
+            is PayIdVerification.SetPayIdWalletAddress -> {
+                state.copy(
+                        viewFieldValue = InputViewValue(action.payId, action.isUserInput),
+                        normalFieldValue = action.payId,
+                        truncatedFieldValue = state.truncate(action.payId),
+                        recipientWalletAddress = action.payIdWalletAddress,
+                        error = null
+                )
+            }
+            is AddressVerification.SetWalletAddress -> {
+                state.copy(
+                        viewFieldValue = InputViewValue(action.address, action.isUserInput),
+                        normalFieldValue = action.address,
+                        truncatedFieldValue = state.truncate(action.address),
+                        recipientWalletAddress = action.address,
+                        error = null
+                )
+            }
             is AddressPayIdVerifyAction.ChangePasteBtnEnableState -> state.copy(pasteIsEnabled = action.isEnabled)
-            is AddressPayIdVerifyAction.VerifyClipboard -> state
+            is AddressVerification.SetAddressError -> state.copy(error = action.error, recipientWalletAddress = null)
+            is PayIdVerification.SetPayIdError -> state.copy(error = action.error, recipientWalletAddress = null)
         }
         return updateLastState(sendState.copy(addressPayIdState = result), result)
     }
