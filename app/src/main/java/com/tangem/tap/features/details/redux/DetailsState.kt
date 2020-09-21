c1135b98027febfd13a2b4ad6c95f133be602baf
Tangem <git@tangem.com>
2020-09-21 13:51:18 +0300

    Updated on 2026-06-01

--- app/src/main/java/com/tangem/tap/features/details/redux/DetailsState.kt
+++ app/src/main/java/com/tangem/tap/features/details/redux/DetailsState.kt
@@ -2,10 +2,12 @@ package com.tangem.tap.features.details.redux
 
 import com.tangem.blockchain.common.Wallet
 import com.tangem.commands.Card
+import com.tangem.tap.common.entities.Button
 import com.tangem.tap.common.entities.TapCurrency.Companion.DEFAULT_FIAT_CURRENCY
 import com.tangem.tap.common.redux.global.FiatCurrencyName
 import com.tangem.tap.network.coinmarketcap.FiatCurrency
 import org.rekotlin.StateType
+import java.util.*
 
 data class DetailsState(
         val card: Card? = null,
@@ -14,6 +16,7 @@ data class DetailsState(
         val appCurrencyState: AppCurrencyState = AppCurrencyState(),
         val eraseWalletState: EraseWalletState? = null,
         val confirmScreenState: ConfirmScreenState? = null,
+        val securityScreenState: SecurityScreenState? = null,
 ) : StateType
 
 data class CardInfo(
@@ -24,6 +27,14 @@ data class CardInfo(
 
 enum class EraseWalletState { Allowed, NotAllowedByCard, NotEmpty }
 enum class ConfirmScreenState { EraseWallet, LongTap, AccessCode, PassCode }
+data class SecurityScreenState(
+        val currentOption: SecurityOption = SecurityOption.LongTap,
+        val selectedOption: SecurityOption = currentOption,
+        val allowedOptions: EnumSet<SecurityOption> = EnumSet.allOf(SecurityOption::class.java),
+        val buttonProceed: Button = Button(true)
+)
+
+enum class SecurityOption { LongTap, PassCode, AccessCode }
 data class AppCurrencyState(
         val fiatCurrencyName: FiatCurrencyName = DEFAULT_FIAT_CURRENCY,
         val showAppCurrencyDialog: Boolean = false,
