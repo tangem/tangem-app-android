a00c07db5c0a3fbcf2e723d4d64365e21466934b
Tangem <git@tangem.com>
2020-10-21 13:22:29 +0300

    Updated on 2026-09-23

--- blockchain/src/main/java/com/tangem/blockchain/blockchains/binance/network/BinanceApi.kt
+++
@@ -1,8 +0,0 @@
-package com.tangem.blockchain.blockchains.binance.network
-
-import retrofit2.http.GET
-
-interface BinanceApi {
-    @GET("api/v1/fees")
-    suspend fun getFees(): List<BinanceFee>
-}
