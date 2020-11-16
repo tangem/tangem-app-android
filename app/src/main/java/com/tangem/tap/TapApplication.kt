cb6d043fe8fd8ed596023a55bb4a77afabc9737f
Tangem <git@tangem.com>
2020-11-16 17:10:55 +0300

    Updated on 2026-09-23

--- app/src/main/java/com/tangem/tap/TapApplication.kt
+++ app/src/main/java/com/tangem/tap/TapApplication.kt
@@ -1,10 +1,16 @@
 package com.tangem.tap
 
 import android.app.Application
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import com.tangem.tap.common.images.PicassoHelper
 import com.tangem.tap.common.redux.AppState
 import com.tangem.tap.common.redux.appReducer
+import com.tangem.tap.common.redux.global.GlobalAction
+import com.tangem.tap.domain.config.*
 import com.tangem.tap.network.NetworkConnectivity
+import com.tangem.tap.network.createMoshi
 import com.tangem.tap.persistence.PreferencesStorage
 import com.tangem.wallet.BuildConfig
 import org.rekotlin.Store
@@ -20,11 +26,33 @@ lateinit var preferencesStorage: PreferencesStorage
 class TapApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
+            Firebase.remoteConfig.setConfigSettingsAsync(remoteConfigSettings {
+                this.minimumFetchIntervalInSeconds = 60
+            })
+        } else {
+            Firebase.remoteConfig.setConfigSettingsAsync(remoteConfigSettings {
+                this.minimumFetchIntervalInSeconds = 3600
+            })
         }
+
         NetworkConnectivity.createInstance(store, this)
         preferencesStorage = PreferencesStorage(this)
         PicassoHelper.initPicassoWithCaching(this)
+
+        loadConfigs()
+    }
+
+
+    private fun loadConfigs() {
+        val moshiAdapter = createMoshi().adapter(ConfigModel::class.java)
+        val nameResolver = ConfigNameResolver.get()
+
+        val localLoader = LocalLoader(this, nameResolver, moshiAdapter)
+        val remoteLoader = RemoteLoader(nameResolver, moshiAdapter)
+        val configManager = ConfigManager(localLoader, remoteLoader)
+        configManager.load { store.dispatch(GlobalAction.SetConfigManager(configManager)) }
     }
 }
