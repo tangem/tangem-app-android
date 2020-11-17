4f2a263520447389b8cfdadd82afcccc446ccc34
Tangem <git@tangem.com>
2020-11-17 22:01:20 +0300

    Updated on 2026-08-19

---
+++ app/src/main/java/com/tangem/tap/domain/TapWorkarounds.kt
@@ -0,0 +1,15 @@
+import com.tangem.commands.Card
+
+class TapWorkarounds(val card: Card) {
+
+    val isStart2Coin: Boolean = card.cardData?.issuerName == START_2_COIN_ISSUER
+
+    fun isPayisPayIdEnabled(): Boolean {
+        if (isStart2Coin) return false
+        return true
+    }
+
+    companion object {
+        const val START_2_COIN_ISSUER = "start2coin"
+    }
+}
