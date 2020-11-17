3a543f4e75fd8a4ae58902f9c96e517bc9938a4b
Tangem <git@tangem.com>
2020-11-17 10:54:38 +0300

    release/5.26.1

--- app/src/main/java/com/tangem/tap/domain/config/ConfigManager.kt
+++ app/src/main/java/com/tangem/tap/domain/config/ConfigManager.kt
@@ -25,13 +25,17 @@ class ConfigManager(
     private val payIdIsEnabled = "payIdIsEnabled"
     private val useTopUp = "useTopUp"
     private val isStart2Coin = "isStart2Coin"
+    private val coinMarketCapKey = "coinMarketCapKey"
+    private val moonPayApiKey = "moonPayApiKey"
+    private val moonPayApiSecretKey = "moonPayApiSecretKey"
 
     var config: ConfigState = ConfigState()
         private set
 
     fun load(onComplete: VoidCallback? = null) {
-        localLoader.loadConfig {
-            it.features?.forEach { feature -> updateFeature(feature.name, feature.value) }
+        localLoader.loadConfig {config ->
+            config.features?.forEach { updateFeature(it.name, it.value) }
+            config.configValues?.forEach { updateKeys(it.name, it.value) }
         }
         remoteLoader.loadConfig {
             it.features?.forEach { feature -> updateFeature(feature.name, feature.value) }
@@ -51,6 +55,14 @@ class ConfigManager(
             payIdIsEnabled -> config = config.copy(payIdIsEnabled = newValue)
             useTopUp -> config = config.copy(useTopUp = newValue)
             isStart2Coin -> config = config.copy(isStart2Coin = newValue)
+        }
+    }
+
+    private fun updateKeys(name: String, value: String) {
+        when (name) {
+            coinMarketCapKey -> config = config.copy(coinMarketCapKey = value)
+            moonPayApiKey -> config = config.copy(moonPayApiKey = value)
+            moonPayApiSecretKey -> config = config.copy(moonPayApiSecretKey = value)
         }
     }
 }
