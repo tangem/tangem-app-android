0be5c7c0b60b82ddb16cb66587d8a6b52dd0a6d1
Tangem <git@tangem.com>
2020-11-20 15:58:21 +0300

    Updated on 2026-09-21

--- app/src/main/java/com/tangem/tap/TapConfig.kt
+++ app/src/main/java/com/tangem/tap/TapConfig.kt
@@ -2,8 +2,8 @@ package com.tangem.tap
 
 object TapConfig {
     const val usePayId: Boolean = true
-    const val useTopUp: Boolean = false
+    const val useTopUp: Boolean = true
     const val coinMarketCapKey = "f6622117-c043-47a0-8975-9d673ce484de"
-    const val moonPayApiKey = "pk_test_kc90oYTANy7UQdBavDKGfL4K9l6VEPE"
-    const val moonPayApiSecretKey = "sk_test_V8w4M19LbDjjYOt170s0tGuvXAgyEb1C"
+    const val moonPayApiKey = "pk_live_YTDl4Uei4411FXLAFe2rn8R2qWSiQcm"
+    const val moonPayApiSecretKey = "sk_live_7wMjoyGTGAB3i1suQHszCWRRqJBRPsQF"
 }
