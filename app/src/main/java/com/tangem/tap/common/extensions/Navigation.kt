7124c13da29d27e9f8a5fec45ba4861657d3095e
Tangem <git@tangem.com>
2020-12-09 20:39:18 +0300

    Updated on 2026-05-04

--- app/src/main/java/com/tangem/tap/common/extensions/Navigation.kt
+++ app/src/main/java/com/tangem/tap/common/extensions/Navigation.kt
@@ -18,13 +18,13 @@ import com.tangem.wallet.R
 
 fun FragmentActivity.openFragment(screen: AppScreen, addToBackStack: Boolean = true) {
     val transaction = this.supportFragmentManager.beginTransaction()
-            .replace(
+            .add(
                     R.id.fragment_container,
                     fragmentFactory(screen),
                     screen.name
             )
     if (addToBackStack && screen != AppScreen.Home) transaction.addToBackStack(null)
-    transaction.commit();
+    transaction.commitAllowingStateLoss();
 }
 
 fun FragmentActivity.popBackTo(screen: AppScreen?, inclusive: Boolean = false) {
