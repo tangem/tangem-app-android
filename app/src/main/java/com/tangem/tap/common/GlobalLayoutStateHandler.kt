df75a9fe2b304c6553f2ee0db4a86ff7d568140c
Tangem <git@tangem.com>
2020-12-11 16:59:23 +0300

    release/5.26.1

---
+++ app/src/main/java/com/tangem/tap/common/GlobalLayoutStateHandler.kt
@@ -0,0 +1,31 @@
+package com.tangem.tap.common
+
+import android.view.View
+import android.view.ViewTreeObserver
+
+/**
+* [REDACTED_AUTHOR]
+ */
+class GlobalLayoutStateHandler<T: View>(
+        private val view: T,
+        attachImmediately: Boolean = true
+) : ViewTreeObserver.OnGlobalLayoutListener {
+
+    var onStateChanged: ((T) -> Unit)? = null
+
+    init {
+        if (attachImmediately) attach()
+    }
+
+    fun attach() {
+        view.viewTreeObserver.addOnGlobalLayoutListener(this)
+    }
+
+    fun detach() {
+        view.viewTreeObserver.removeOnGlobalLayoutListener(this)
+    }
+
+    override fun onGlobalLayout() {
+        onStateChanged?.invoke(view)
+    }
+}
