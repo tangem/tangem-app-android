dffc6e0b1f730e8142149f00b097a4c6592085a3
Tangem <git@tangem.com>
2021-02-15 13:44:49 +0300

    Updated on 2026-07-23

--- app/src/main/java/com/tangem/tap/TapApplication.kt
+++ app/src/main/java/com/tangem/tap/TapApplication.kt
@@ -9,8 +9,8 @@ import com.tangem.tap.common.redux.AppState
 import com.tangem.tap.common.redux.appReducer
 import com.tangem.tap.common.redux.global.GlobalAction
 import com.tangem.tap.domain.config.ConfigManager
-import com.tangem.tap.domain.config.LocalLoader
-import com.tangem.tap.domain.config.RemoteLoader
+import com.tangem.tap.domain.config.FeaturesLocalLoader
+import com.tangem.tap.domain.config.FeaturesRemoteLoader
 import com.tangem.tap.network.NetworkConnectivity
 import com.tangem.tap.network.createMoshi
 import com.tangem.tap.persistence.PreferencesStorage
@@ -50,8 +50,8 @@ class TapApplication : Application() {
 
     private fun loadConfigs() {
         val moshi = createMoshi()
-        val localLoader = LocalLoader(this, moshi)
-        val remoteLoader = RemoteLoader(moshi)
+        val localLoader = FeaturesLocalLoader(this, moshi)
+        val remoteLoader = FeaturesRemoteLoader(moshi)
         val configManager = ConfigManager(localLoader, remoteLoader)
         configManager.load { store.dispatch(GlobalAction.SetConfigManager(configManager)) }
     }
