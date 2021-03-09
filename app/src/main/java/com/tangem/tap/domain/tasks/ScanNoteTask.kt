2c651dfd4e4a6c28327ea0722e7da372b7119ba2
Tangem <git@tangem.com>
2021-03-09 13:38:08 +0300

    Updated on 2026-09-14

--- app/src/main/java/com/tangem/tap/domain/tasks/ScanNoteTask.kt
+++ app/src/main/java/com/tangem/tap/domain/tasks/ScanNoteTask.kt
@@ -10,12 +10,12 @@ import com.tangem.commands.CommandResponse
 import com.tangem.commands.ReadIssuerDataCommand
 import com.tangem.commands.common.card.Card
 import com.tangem.commands.common.card.CardStatus
-import com.tangem.commands.common.card.masks.Product
 import com.tangem.commands.verifycard.VerifyCardCommand
 import com.tangem.commands.verifycard.VerifyCardResponse
 import com.tangem.common.CompletionResult
 import com.tangem.common.extensions.toHexString
 import com.tangem.tap.domain.TapSdkError
+import com.tangem.tap.domain.TapWorkarounds.isExcluded
 import com.tangem.tap.domain.twins.TwinCardsManager
 import com.tangem.tap.domain.twins.isTwinCard
 import com.tangem.tasks.ScanTask
@@ -113,26 +113,11 @@ class ScanNoteTask(val card: Card? = null) : CardSessionRunnable<ScanNoteRespons
     }
 
     private fun getErrorIfExcludedCard(card: Card): TangemError? {
-        val productMask = card.cardData?.productMask
-        if (productMask != null &&  // product mask is on cards v2.30 and later
-                !productMask.contains(Product.Note) && !productMask.contains(Product.TwinCard)) {
-            return TapSdkError.CardForDifferentApp
-        }
-        if (excludedBatches.contains(card.cardData?.batchId)) {
-            return TapSdkError.CardForDifferentApp
-        }
+        if (card.isExcluded()) return TapSdkError.CardForDifferentApp
         if (card.status == CardStatus.Purged) return TangemSdkError.CardIsPurged()
         if (card.status == CardStatus.NotPersonalized) return TangemSdkError.NotPersonalized()
-
         return null
     }
 
-    companion object {
-        private val excludedBatches = listOf(
-                "0027",
-                "0030",
-                "0031", //tags
-                "0079", //TOTHEMOON
-        )
-    }
+
 }
