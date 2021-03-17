18007c70b2ea8ef69f40161746a2fa566fcfdefb
Tangem <git@tangem.com>
2021-03-17 15:31:03 +0300

    Updated on 2026-06-12

--- app/src/main/java/com/tangem/tap/common/redux/global/GlobalAction.kt
+++ app/src/main/java/com/tangem/tap/common/redux/global/GlobalAction.kt
@@ -6,14 +6,10 @@ import com.tangem.tap.domain.configurable.warningMessage.WarningMessagesManager
 import com.tangem.tap.domain.tasks.ScanNoteResponse
 import com.tangem.tap.features.details.redux.SecurityOption
 import org.rekotlin.Action
-import java.math.BigDecimal
 
 sealed class GlobalAction : Action {
 
     data class SaveScanNoteResponse(val scanNoteResponse: ScanNoteResponse) : GlobalAction()
-    data class SetFiatRate(
-            val fiatRates: Pair<CryptoCurrencyName, BigDecimal>
-    ) : GlobalAction()
     data class ChangeAppCurrency(val appCurrency: FiatCurrencyName) : GlobalAction()
     object RestoreAppCurrency : GlobalAction() {
         data class Success(val appCurrency: FiatCurrencyName) : GlobalAction()
