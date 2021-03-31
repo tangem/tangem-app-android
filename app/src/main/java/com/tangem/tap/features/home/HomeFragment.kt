6850d3ff1ad730afda7b981b8e3d607b9d61237d
Tangem <git@tangem.com>
2021-03-31 20:13:41 +0300

    Updated on 2026-04-15

--- app/src/main/java/com/tangem/tap/features/home/HomeFragment.kt
+++ app/src/main/java/com/tangem/tap/features/home/HomeFragment.kt
@@ -1,17 +1,23 @@
 package com.tangem.tap.features.home
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.transition.TransitionInflater
+import com.tangem.tap.common.redux.global.StateDialog
 import com.tangem.tap.features.home.redux.HomeAction
+import com.tangem.tap.features.home.redux.HomeDialog
 import com.tangem.tap.features.home.redux.HomeState
+import com.tangem.tap.features.wallet.ui.dialogs.ScanFailsDialog
 import com.tangem.tap.store
 import com.tangem.wallet.R
 import kotlinx.android.synthetic.main.fragment_home.*
 import org.rekotlin.StoreSubscriber
 
 class HomeFragment : Fragment(R.layout.fragment_home), StoreSubscriber<HomeState> {
+
+    private var dialog: Dialog? = null
 
     override fun onStart() {
         super.onStart()
@@ -38,6 +44,19 @@ class HomeFragment : Fragment(R.layout.fragment_home), StoreSubscriber<HomeState
             tv_home_description.text = getText(R.string.home_welcome_back)
             btn_shop.text = getText(R.string.home_button_shop)
             btn_yes.text = getText(R.string.home_button_scan)
+        }
+        handleDialog(state.dialog)
+    }
+
+    private fun handleDialog(stateDialog: StateDialog?) {
+        when (stateDialog) {
+            is HomeDialog.ScanFailsDialog -> {
+                if (dialog == null) dialog = ScanFailsDialog.create(requireContext()).apply { show() }
+            }
+            else -> {
+                dialog?.dismiss()
+                dialog = null
+            }
         }
     }
 
