de536d183bc0340d1beb037d2f597deef12b59ba
Tangem <git@tangem.com>
2021-04-02 12:42:36 +0300

    Updated on 2026-07-23

--- app/src/main/java/com/tangem/tap/common/extensions/Token.kt
+++ app/src/main/java/com/tangem/tap/common/extensions/Token.kt
@@ -3,9 +3,14 @@ package com.tangem.tap.common.extensions
 import androidx.annotation.ColorInt
 import androidx.core.graphics.toColorInt
 import com.tangem.blockchain.common.Token
+import com.tangem.wallet.R
 
 @ColorInt
 fun Token.getColor(): Int {
-    return ("#" + this.contractAddress.subSequence(2..7).toString())
-            .toColorInt()
+    return try {
+        ("#" + this.contractAddress.subSequence(2..7).toString())
+                .toColorInt()
+    } catch (exception: Exception) {
+        R.color.lightGray4
+    }
 }
