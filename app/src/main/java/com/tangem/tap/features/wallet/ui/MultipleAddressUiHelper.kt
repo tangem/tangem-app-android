665eacdf8211614317c6b32547ec2428e285fb15
Tangem <git@tangem.com>
2021-04-08 14:33:59 +0300

    Updated on 2026-04-06

--- app/src/main/java/com/tangem/tap/features/wallet/ui/MultipleAddressUiHelper.kt
+++ app/src/main/java/com/tangem/tap/features/wallet/ui/MultipleAddressUiHelper.kt
@@ -5,7 +5,6 @@ import com.tangem.blockchain.blockchains.bitcoin.BitcoinAddressType
 import com.tangem.blockchain.blockchains.cardano.CardanoAddressType
 import com.tangem.blockchain.common.Blockchain
 import com.tangem.blockchain.common.address.AddressType
-import com.tangem.tap.common.redux.global.CryptoCurrencyName
 import com.tangem.wallet.R
 
 class MultipleAddressUiHelper {
@@ -18,13 +17,6 @@ class MultipleAddressUiHelper {
                 is CardanoAddressType.Shelley -> R.id.chip_default
                 else -> View.NO_ID
             }
-        }
-
-        fun idToType(id: Int, currencyName: CryptoCurrencyName?): AddressType? {
-            val blockchain = currencyName?.let { currency ->
-                Blockchain.fromCurrency(currency)
-            }
-            return idToType(id, blockchain)
         }
 
         fun idToType(id: Int, blockchain: Blockchain?): AddressType? {
