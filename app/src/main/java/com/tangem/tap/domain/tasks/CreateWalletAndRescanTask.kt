c88b94502aa869bcf6f38e6884258b7c94a9ff85
Tangem <git@tangem.com>
2021-04-22 13:53:48 +0300

    Updated on 2026-09-25

--- app/src/main/java/com/tangem/tap/domain/tasks/CreateWalletAndRescanTask.kt
+++ app/src/main/java/com/tangem/tap/domain/tasks/CreateWalletAndRescanTask.kt
@@ -2,26 +2,39 @@ package com.tangem.tap.domain.tasks
 
 import com.tangem.CardSession
 import com.tangem.CardSessionRunnable
-import com.tangem.commands.ReadCommand
+import com.tangem.FirmwareConstraints
+import com.tangem.TangemSdkError
+import com.tangem.commands.common.card.Card
 import com.tangem.common.CompletionResult
 import com.tangem.tasks.CreateWalletTask
+import com.tangem.tasks.PreflightReadCapable
+import com.tangem.tasks.PreflightReadSettings
+import com.tangem.tasks.PreflightReadTask
 
-class CreateWalletAndRescanTask : CardSessionRunnable<ScanNoteResponse> {
+class CreateWalletAndRescanTask : CardSessionRunnable<Card>, PreflightReadCapable {
     override val requiresPin2 = false
+    override fun preflightReadSettings() = PreflightReadSettings.FullCardRead
 
-    override fun run(session: CardSession, callback: (result: CompletionResult<ScanNoteResponse>) -> Unit) {
-        CreateWalletTask().run(session) { result ->
+    override fun run(session: CardSession, callback: (result: CompletionResult<Card>) -> Unit) {
+        val firmwareVerion = session.environment.card?.firmwareVersion
+        if (firmwareVerion == null) {
+            callback(CompletionResult.Failure(TangemSdkError.CardError()))
+            return
+        }
+        val task = if (firmwareVerion < FirmwareConstraints.AvailabilityVersions.walletData) {
+            CreateWalletTask()
+        } else {
+            CreateWalletsTask()
+        }
+
+        task.run(session) { result ->
             when (result) {
-                is CompletionResult.Success -> {
-                    ReadCommand().run(session) { readResult ->
-                        when (readResult) {
-                            is CompletionResult.Success -> ScanNoteTask(readResult.data).run(session, callback)
-                            is CompletionResult.Failure -> callback(CompletionResult.Failure(readResult.error))
-                        }
-                    }
-                }
+                is CompletionResult.Success ->
+                    PreflightReadTask(PreflightReadSettings.FullCardRead).run(session, callback)
                 is CompletionResult.Failure -> callback(CompletionResult.Failure(result.error))
             }
         }
     }
+
+
 }
--- app/src/main/java/com/tangem/tap/domain/twins/CreateFirstTwinWalletTask.kt
+++ app/src/main/java/com/tangem/tap/domain/twins/CreateFirstTwinWalletTask.kt
@@ -2,18 +2,20 @@ package com.tangem.tap.domain.twins
 
 import com.tangem.CardSession
 import com.tangem.CardSessionRunnable
-import com.tangem.commands.CreateWalletResponse
-import com.tangem.commands.PurgeWalletCommand
 import com.tangem.commands.common.card.CardStatus
+import com.tangem.commands.wallet.CreateWalletResponse
+import com.tangem.commands.wallet.PurgeWalletCommand
 import com.tangem.common.CompletionResult
+import com.tangem.common.TangemSdkConstants
+import com.tangem.tap.domain.extensions.getSingleWallet
 import com.tangem.tasks.CreateWalletTask
 
 class CreateFirstTwinWalletTask : CardSessionRunnable<CreateWalletResponse> {
     override val requiresPin2 = false
 
     override fun run(session: CardSession, callback: (result: CompletionResult<CreateWalletResponse>) -> Unit) {
-        if (session.environment.card?.walletPublicKey != null) {
-            PurgeWalletCommand().run(session) { response ->
+        if (session.environment.card?.getSingleWallet()?.publicKey != null) {
+            PurgeWalletCommand(TangemSdkConstants.getDefaultWalletIndex()).run(session) { response ->
                 when (response) {
                     is CompletionResult.Success -> {
                         session.environment.card = session.environment.card?.copy(status = CardStatus.Empty)
