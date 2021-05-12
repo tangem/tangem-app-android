0a2caf8bdbaf7255954d43fb29bf4c955ff360a5
Tangem <git@tangem.com>
2021-05-12 22:43:31 +0300

    Updated on 2026-06-16

--- app/src/main/java/com/tangem/tap/common/redux/global/GlobalMidlleware.kt
+++ app/src/main/java/com/tangem/tap/common/redux/global/GlobalMidlleware.kt
@@ -53,6 +53,9 @@ val globalMiddleware: Middleware<AppState> = { dispatch, appState ->
                 is GlobalAction.SendFeedback -> {
                     store.state.globalState.feedbackManager?.send(action.emailData)
                 }
+                is GlobalAction.UpdateWalletSignedHashes -> {
+                    store.dispatch(WalletAction.Warnings.CheckRemainingSignatures(action.remainingSignatures))
+                }
             }
             nextDispatch(action)
         }
