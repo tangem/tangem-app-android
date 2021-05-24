b9baadfc68ee787433109bf3d07a4129390b2f5e
Tangem <git@tangem.com>
2021-05-24 14:19:02 +0300

    release/5.26.1

--- app/src/main/java/com/tangem/tap/domain/extensions/WalletManagerFactory.kt
+++ app/src/main/java/com/tangem/tap/domain/extensions/WalletManagerFactory.kt
@@ -5,29 +5,31 @@ import com.tangem.blockchain.common.WalletManager
 import com.tangem.blockchain.common.WalletManagerFactory
 import com.tangem.commands.common.card.Card
 import com.tangem.commands.common.card.EllipticCurve
+import com.tangem.commands.wallet.CardWallet
 
-fun WalletManagerFactory.makeWalletManagerForApp(card: Card, blockchain: Blockchain): WalletManager? {
-    val curve = blockchain.getCurve() ?: return null
-    val publicKey = card.getWallets().firstOrNull { it.curve == curve }?.publicKey ?: return null
-    return makeWalletManager(card.cardId, publicKey, blockchain, curve)
+fun WalletManagerFactory.makeWalletManagerForApp(
+    card: Card,
+    blockchain: Blockchain,
+): WalletManager? {
+    val supportedCurves = blockchain.getSupportedCurves() ?: return null
+    val wallets = card.getWallets().filter { wallet -> supportedCurves.contains(wallet.curve) }
+    val wallet = selectWallet(wallets)
+    val publicKey = wallet?.publicKey ?: return null
+    val curveToUse = wallet.curve ?: return null
+    return makeWalletManager(card.cardId, publicKey, blockchain, curveToUse)
+}
+
+private fun selectWallet(wallets: List<CardWallet>): CardWallet? {
+ return when (wallets.size) {
+     0 -> null
+     1 -> wallets[0]
+     else -> wallets.firstOrNull { it.curve == EllipticCurve.Secp256k1 } ?: wallets[0]
+ }
 }
 
 fun WalletManagerFactory.makeWalletManagersForApp(
-        card: Card, blockchains: List<Blockchain>
+    card: Card, blockchains: List<Blockchain>,
 ): List<WalletManager> {
-    return makeWalletManagersForCurve(card, blockchains, EllipticCurve.Secp256k1) +
-            makeWalletManagersForCurve(card, blockchains, EllipticCurve.Ed25519)
+    return blockchains.mapNotNull { blockchain ->  makeWalletManagerForApp(card, blockchain) }
 }
 
-fun WalletManagerFactory.makeWalletManagersForCurve(
-        card: Card, blockchains: List<Blockchain>, curve: EllipticCurve
-): List<WalletManager> {
-    val blockchainsForCurve = blockchains.filter { it.getCurve() == curve }
-    val walletPublicKey = card.getWallets().firstOrNull { it.curve == curve }?.publicKey
-
-    return if (walletPublicKey != null) {
-        makeWalletManagers(card.cardId, walletPublicKey, blockchainsForCurve, curve)
-    } else {
-        emptyList()
-    }
-}
