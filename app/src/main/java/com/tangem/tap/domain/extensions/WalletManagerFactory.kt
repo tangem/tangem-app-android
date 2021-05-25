dea031dff31e3a7d5b63369a1d3f011439087b98
Tangem <git@tangem.com>
2021-05-25 20:47:59 +0300

    Updated on 2026-06-16

--- app/src/main/java/com/tangem/tap/domain/extensions/WalletManagerFactory.kt
+++ app/src/main/java/com/tangem/tap/domain/extensions/WalletManagerFactory.kt
@@ -6,6 +6,9 @@ import com.tangem.blockchain.common.WalletManagerFactory
 import com.tangem.commands.common.card.Card
 import com.tangem.commands.common.card.EllipticCurve
 import com.tangem.commands.wallet.CardWallet
+import com.tangem.common.extensions.hexToBytes
+import com.tangem.tap.domain.tasks.ScanNoteResponse
+import com.tangem.tap.domain.twins.isTwinCard
 
 fun WalletManagerFactory.makeWalletManagerForApp(
     card: Card,
@@ -31,4 +34,25 @@ fun WalletManagerFactory.makeWalletManagersForApp(
     card: Card, blockchains: List<Blockchain>,
 ): List<WalletManager> {
     return blockchains.mapNotNull { blockchain ->  makeWalletManagerForApp(card, blockchain) }
+}
+
+fun WalletManagerFactory.makePrimaryWalletManager(
+    data: ScanNoteResponse,
+): WalletManager? {
+    val card = data.card
+    val blockchain = card.getBlockchain()
+    val supportedCurves = blockchain?.getSupportedCurves() ?: return null
+    val wallets = card.getWallets().filter { wallet -> supportedCurves.contains(wallet.curve) }
+    val wallet = selectWallet(wallets)
+    val publicKey = wallet?.publicKey ?: return null
+    val curveToUse = wallet.curve ?: return null
+    return if (card.isTwinCard() && data.secondTwinPublicKey != null) {
+        makeMultisigWalletManager(
+            cardId = card.cardId,
+            walletPublicKey = publicKey, pairPublicKey = data.secondTwinPublicKey.hexToBytes(),
+            blockchain = blockchain, curve = curveToUse
+        )
+    } else {
+        makeWalletManager(card.cardId, publicKey, blockchain, curveToUse)
+    }
 }
