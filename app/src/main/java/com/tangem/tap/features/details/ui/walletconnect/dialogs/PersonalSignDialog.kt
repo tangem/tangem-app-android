bb936b0bb5846ef841eab0c6295880dd2eb819da
Tangem <git@tangem.com>
2021-06-07 21:27:28 +0300

    Updated on 2026-04-15

---
+++ app/src/main/java/com/tangem/tap/features/details/ui/walletconnect/dialogs/PersonalSignDialog.kt
@@ -0,0 +1,45 @@
+package com.tangem.tap.features.details.ui.walletconnect.dialogs
+
+import android.content.Context
+import androidx.appcompat.app.AlertDialog
+import com.tangem.tap.common.redux.global.GlobalAction
+import com.tangem.tap.features.details.redux.walletconnect.WalletConnectAction
+import com.tangem.tap.store
+import com.tangem.wallet.R
+import com.trustwallet.walletconnect.models.session.WCSession
+
+class PersonalSignDialog {
+    companion object {
+        fun create(
+            data: PersonalSignDialogData,
+            context: Context,
+        ): AlertDialog {
+            val message =
+                context.getString(R.string.wallet_connect_alert_sign_message, data.cardId) +
+                        context.getString(R.string.wallet_connect_personal_sign_message,
+                            data.dAppName,
+                            data.message)
+            return AlertDialog.Builder(context).apply {
+                setTitle(context.getString(R.string.wallet_connect))
+                setMessage(message)
+                setPositiveButton(context.getText(R.string.common_sign)) { _, _ ->
+                    store.dispatch(WalletConnectAction.SignMessage(data.session))
+                }
+                setNegativeButton(context.getText(R.string.common_reject)) { _, _ ->
+                    store.dispatch(WalletConnectAction.RejectRequest(data.session, data.id))
+                }
+                setOnDismissListener {
+                    store.dispatch(GlobalAction.HideDialog)
+                }
+            }.create()
+        }
+    }
+}
+
+data class PersonalSignDialogData(
+    val cardId: String,
+    val dAppName: String,
+    val message: String,
+    val session: WCSession,
+    val id: Long,
+)
