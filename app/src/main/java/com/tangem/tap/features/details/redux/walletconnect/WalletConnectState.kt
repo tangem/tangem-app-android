f9bc5cec76ce059a621c02452667f660413dbdfc
Tangem <git@tangem.com>
2021-06-07 21:27:28 +0300

    release/5.26.1

---
+++ app/src/main/java/com/tangem/tap/features/details/redux/walletconnect/WalletConnectState.kt
@@ -0,0 +1,70 @@
+package com.tangem.tap.features.details.redux.walletconnect
+
+import com.tangem.blockchain.common.Blockchain
+import com.tangem.blockchain.common.TransactionData
+import com.tangem.blockchain.common.WalletManager
+import com.tangem.common.extensions.hexToBytes
+import com.tangem.tap.common.redux.global.StateDialog
+import com.tangem.tap.features.details.ui.walletconnect.dialogs.PersonalSignDialogData
+import com.tangem.tap.features.details.ui.walletconnect.dialogs.TransactionRequestDialogData
+import com.trustwallet.walletconnect.models.WCPeerMeta
+import com.trustwallet.walletconnect.models.session.WCSession
+
+data class WalletConnectState(
+    val loading: Boolean = false,
+    val sessions: List<WalletConnectSession> = listOf(),
+)
+
+data class WalletConnectSession(
+    val peerId: String,
+    val remotePeerId: String?,
+    val wallet: WalletForSession,
+    val session: WCSession,
+    val peerMeta: WCPeerMeta,
+)
+
+data class WalletForSession(
+    val cardId: String,
+    val walletPublicKey: String,
+    val isTestNet: Boolean = false,
+) {
+    val chainId
+        get() = if (isTestNet) 4 else 1
+
+    fun getAddress(): String {
+        val blockchain = if (isTestNet) Blockchain.EthereumTestnet else Blockchain.Ethereum
+        return blockchain.makeAddresses(walletPublicKey.hexToBytes()).first().value
+    }
+}
+
+
+sealed class WalletConnectDialog : StateDialog {
+    data class ClipboardOrScanQr(val clipboardUri: String) : WalletConnectDialog()
+    data class ApproveWcSession(val session: WalletConnectSession) : WalletConnectDialog()
+    data class RequestTransaction(val dialogData: TransactionRequestDialogData) :
+        WalletConnectDialog()
+
+    data class PersonalSign(val data: PersonalSignDialogData) : WalletConnectDialog()
+}
+
+data class WcTransactionData(
+    val type: WcTransactionType,
+    val transaction: TransactionData,
+    val session: WalletConnectSession,
+    val id: Long,
+    val walletManager: WalletManager,
+    val dialogData: TransactionRequestDialogData,
+)
+
+enum class WcTransactionType {
+    EthSignTransaction,
+    EthSendTransaction,
+}
+
+data class WcPersonalSignData(
+    val hash: ByteArray,
+    val session: WalletConnectSession,
+    val id: Long,
+    val dialogData: PersonalSignDialogData,
+
+    )
