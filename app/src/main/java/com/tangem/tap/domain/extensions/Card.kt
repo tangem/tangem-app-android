41d8cb5c7cf44b41454e5af8103a945762560e1b
Tangem <git@tangem.com>
2021-08-03 09:56:11 +0300

    Updated on 2026-09-25

--- app/src/main/java/com/tangem/tap/domain/extensions/Card.kt
+++ app/src/main/java/com/tangem/tap/domain/extensions/Card.kt
@@ -1,48 +1,18 @@
 package com.tangem.tap.domain.extensions
 
-import com.tangem.FirmwareConstraints
-import com.tangem.blockchain.common.Blockchain
-import com.tangem.blockchain.common.Token
-import com.tangem.commands.common.card.Card
-import com.tangem.commands.common.card.CardStatus
-import com.tangem.commands.wallet.CardWallet
-import com.tangem.commands.wallet.WalletStatus
-import com.tangem.common.TangemSdkConstants
-
-fun Card.getToken(): Token? {
-    val symbol = cardData?.tokenSymbol ?: return null
-    val contractAddress = cardData?.tokenContractAddress ?: return null
-    val decimals = cardData?.tokenDecimal ?: return null
-    val blockchain = cardData?.blockchainName?.let { Blockchain.fromId(it) } ?: return null
-    if (symbol.isBlank() || contractAddress.isBlank()) return null
-    return Token(symbol, contractAddress, decimals, blockchain)
-}
-
-fun Card.getBlockchain(): Blockchain? {
-    val blockchainName: String = cardData?.blockchainName ?: return null
-    return Blockchain.fromId(blockchainName)
-}
+import com.tangem.common.card.Card
+import com.tangem.common.card.CardWallet
 
 fun Card.getSingleWallet(): CardWallet? {
-    return wallet(TangemSdkConstants.getDefaultWalletIndex())
-}
-
-fun Card.getStatus(): CardStatus {
-    if (firmwareVersion < FirmwareConstraints.AvailabilityVersions.walletData) return status!!
-
-    return if (wallets.any { it.status == WalletStatus.Loaded }) {
-        CardStatus.Loaded
-    } else {
-        CardStatus.Empty
-    }
+    return wallets.firstOrNull()
 }
 
 fun Card.hasSignedHashes(): Boolean {
-    return wallets.any { it.status == WalletStatus.Loaded && it.signedHashes ?: 0 > 0 }
+    return wallets.any { it.totalSignedHashes ?: 0 > 0 }
 }
 
 fun Card.signedHashesCount(): Int {
-    return wallets.map { it.signedHashes ?: 0 }.sum()
+    return wallets.map { it.totalSignedHashes ?: 0 }.sum()
 }
 
 val Card.remainingSignatures: Int?
