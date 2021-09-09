b1fc2739e37b8cf8801a704be5df5601188e0083
Tangem <git@tangem.com>
2021-09-09 21:37:07 +0300

    Updated on 2026-09-25

--- app/src/main/java/com/tangem/tap/common/entities/Button.kt
+++ app/src/main/java/com/tangem/tap/common/entities/Button.kt
@@ -1,3 +1,17 @@
 package com.tangem.tap.common.entities
 
+import com.tangem.tap.common.toggleWidget.ProgressState
+import com.tangem.tap.features.send.redux.states.ButtonState
+
 open class Button(val enabled: Boolean)
+
+open class IndeterminateProgressButton(
+        val state: ButtonState
+) : Button(state != ButtonState.DISABLED) {
+
+    val progressState: ProgressState
+        get() = when (state) {
+            ButtonState.PROGRESS -> ProgressState.Progress()
+            else -> ProgressState.None
+        }
+}
