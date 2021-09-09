c938b9fbe3d5533536ffe71536bb53a0b23dc8a1
Tangem <git@tangem.com>
2021-09-09 21:41:28 +0300

    Updated on 2026-08-14

--- app/src/main/java/com/tangem/tap/common/redux/navigation/NavigationAction.kt
+++ app/src/main/java/com/tangem/tap/common/redux/navigation/NavigationAction.kt
@@ -1,12 +1,17 @@
 package com.tangem.tap.common.redux.navigation
 
+import android.view.View
 import androidx.fragment.app.FragmentActivity
+import androidx.transition.TransitionSet
 import org.rekotlin.Action
 import java.lang.ref.WeakReference
 
 sealed class NavigationAction : Action {
-    data class NavigateTo(val screen: AppScreen, val addToBackstack: Boolean = true) :
-        NavigationAction()
+    data class NavigateTo(
+            val screen: AppScreen,
+            val addToBackstack: Boolean = true,
+            val fragmentShareTransition: FragmentShareTransition? = null
+    ) : NavigationAction()
 
     data class PopBackTo(val screen: AppScreen? = null) : NavigationAction()
 
@@ -15,3 +20,11 @@ sealed class NavigationAction : Action {
     data class ActivityCreated(val activity: WeakReference<FragmentActivity>) : NavigationAction()
     object ActivityDestroyed : NavigationAction()
 }
+
+data class FragmentShareTransition(
+        val shareElements: List<ShareElement>,
+        val enterTransitionSet: TransitionSet,
+        val exitTransitionSet: TransitionSet
+)
+
+data class ShareElement(val wView: WeakReference<View>, val name: String)
