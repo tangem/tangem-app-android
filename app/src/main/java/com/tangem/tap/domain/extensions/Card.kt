0b1ea80cdecede82711adbc725e093df44ae5cf8
Tangem <git@tangem.com>
2021-09-23 11:28:43 +0300

    Updated on 2026-06-17

--- app/src/main/java/com/tangem/tap/domain/extensions/Card.kt
+++ app/src/main/java/com/tangem/tap/domain/extensions/Card.kt
@@ -2,6 +2,9 @@ package com.tangem.tap.domain.extensions
 
 import com.tangem.common.card.Card
 import com.tangem.common.card.CardWallet
+import com.tangem.common.extensions.toHexString
+import com.tangem.operations.attestation.OnlineCardVerifier
+import com.tangem.tap.features.wallet.redux.Artwork
 
 fun Card.getSingleWallet(): CardWallet? {
     return wallets.firstOrNull()
@@ -9,12 +12,27 @@ fun Card.getSingleWallet(): CardWallet? {
 
 fun Card.hasWallets(): Boolean = wallets.isNotEmpty()
 
+fun Card.hasNoWallets(): Boolean = wallets.isEmpty()
+
+fun Card.hasSingleWallet(): Boolean = wallets.size == 1
+
 fun Card.hasSignedHashes(): Boolean {
     return wallets.any { it.totalSignedHashes ?: 0 > 0 }
 }
 
 fun Card.signedHashesCount(): Int {
     return wallets.map { it.totalSignedHashes ?: 0 }.sum()
+}
+
+fun Card.getArtworkUrl(artworkId: String?): String? {
+    return when {
+        artworkId != null -> {
+            OnlineCardVerifier.getUrlForArtwork(cardId, cardPublicKey.toHexString(), artworkId)
+        }
+        cardId.startsWith(Artwork.SERGIO_CARD_ID) -> Artwork.SERGIO_CARD_URL
+        cardId.startsWith(Artwork.MARTA_CARD_ID) -> Artwork.MARTA_CARD_URL
+        else -> null
+    }
 }
 
 val Card.remainingSignatures: Int?
