bd06bd62bd3fe2cb635fea63ab049e89b2d505ec
Tangem <git@tangem.com>
2021-09-23 12:49:34 +0300

    Updated on 2026-09-25

--- app/src/main/java/com/tangem/tap/features/home/redux/HomeMiddleware.kt
+++ app/src/main/java/com/tangem/tap/features/home/redux/HomeMiddleware.kt
@@ -1,80 +1,82 @@
 package com.tangem.tap.features.home.redux
 
-import android.content.Intent
-import android.net.Uri
-import androidx.core.content.ContextCompat.startActivity
-import com.tangem.common.CompletionResult
-import com.tangem.tap.common.analytics.FirebaseAnalyticsHandler
+import com.tangem.tap.common.entities.IndeterminateProgressButton
 import com.tangem.tap.common.redux.AppState
 import com.tangem.tap.common.redux.global.GlobalAction
 import com.tangem.tap.common.redux.navigation.AppScreen
 import com.tangem.tap.common.redux.navigation.NavigationAction
+import com.tangem.tap.domain.TapError
+import com.tangem.tap.features.onboarding.OnboardingService
+import com.tangem.tap.features.onboarding.redux.OnboardingAction
+import com.tangem.tap.features.send.redux.states.ButtonState
 import com.tangem.tap.preferencesStorage
-import com.tangem.tap.scope
 import com.tangem.tap.store
-import com.tangem.tap.tangemSdkManager
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import org.rekotlin.Middleware
 
 class HomeMiddleware {
-    val homeMiddleware: Middleware<AppState> = { dispatch, state ->
-        { next ->
-            { action ->
-                when (action) {
-                    is HomeAction.CheckIfFirstLaunch -> {
-                        store.dispatch(
-                                HomeAction.CheckIfFirstLaunch.Result(preferencesStorage.getCountOfLaunches() == 1)
-                        )
-                    }
-                    is HomeAction.ReadCard -> {
-                        scope.launch {
-                            val result = tangemSdkManager.scanNote(FirebaseAnalyticsHandler)
-                            withContext(Dispatchers.Main) {
-                                store.dispatch(GlobalAction.ScanFailsCounter.ChooseBehavior(result))
-                                when (result) {
-                                    is CompletionResult.Success -> {
-                                        tangemSdkManager.changeDisplayedCardIdNumbersCount(result.data.card)
-                                        store.dispatch(GlobalAction.RestoreAppCurrency)
-                                        store.state.globalState.tapWalletManager.onCardScanned(result.data)
-                                        showDisclaimerOrNavigateToWallet()
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    is HomeAction.GoToShop -> {
-                        val uri = Uri.parse(CARD_SHOP_URI)
-                        val intent = Intent(Intent.ACTION_VIEW, uri)
-                        startActivity(action.context, intent, null)
-                    }
-                }
-                next(action)
-            }
-        }
-    }
-
-    private fun showDisclaimerOrNavigateToWallet() {
-        if (!preferencesStorage.wasDisclaimerAccepted()) {
-            store.dispatch(NavigationAction.NavigateTo(AppScreen.Disclaimer))
-            return
-        }
-        if (store.state.walletState.twinCardsState != null) {
-            val showOnboarding = !preferencesStorage.wasTwinsOnboardingShown()
-            if (showOnboarding) {
-                store.dispatch(NavigationAction.NavigateTo(AppScreen.TwinsOnboarding))
-                return
-            }
-        }
-        store.dispatch(NavigationAction.NavigateTo(AppScreen.Wallet))
-    }
-
     companion object {
-        private const val CARD_SHOP_URI =
+        val handler = homeMiddleware
+
+        const val CARD_SHOP_URI =
                 "https://shop.tangem.com/?afmc=1i&utm_campaign=1i&utm_source=leaddyno&utm_medium=affiliate"
     }
 }
 
+private val homeMiddleware: Middleware<AppState> = { dispatch, state ->
+    { next ->
+        { action ->
+            when (action) {
+                is HomeAction.Init -> {
+                    store.dispatch(GlobalAction.RestoreAppCurrency)
+                    store.dispatch(GlobalAction.GetMoonPayUserStatus)
+                    store.dispatch(HomeAction.SetTermsOfUseState(preferencesStorage.wasDisclaimerAccepted()))
+                }
+                is HomeAction.ReadCard -> {
+                    if (!preferencesStorage.wasDisclaimerAccepted()) {
+                        store.dispatch(NavigationAction.NavigateTo(AppScreen.Disclaimer))
+                    } else {
+                        launchOnboardingService()
+                    }
+                }
+                is HomeAction.GoToShop -> store.dispatch(HomeAction.SetOpenUrl(HomeMiddleware.CARD_SHOP_URI))
+            }
+            next(action)
+        }
+    }
+}
 
+private fun launchOnboardingService() {
+    fun changeButtonState(state: ButtonState) {
+        val btnState = IndeterminateProgressButton(state)
+        store.dispatch(HomeAction.ChangeScanCardButtonState(btnState))
+    }
+    changeButtonState(ButtonState.PROGRESS)
 
+    val walletManager = store.state.globalState.tapWalletManager
+    val service = OnboardingService(
+            walletManager,
+            preferencesStorage.usedCardsPrefStorage
+    )
+
+    service.onReadyToProceed = {
+        store.dispatch(OnboardingAction.SetData(it))
+        changeButtonState(ButtonState.ENABLED)
+        store.dispatch(NavigationAction.NavigateTo(it.onboardingScreen))
+    }
+
+    service.onFailedToProceedToOnboardingCase = {
+        changeButtonState(ButtonState.ENABLED)
+        walletManager.onCardScanned(it)
+        store.dispatch(NavigationAction.NavigateTo(AppScreen.Wallet))
+    }
+
+    service.onError = {
+        changeButtonState(ButtonState.ENABLED)
+        store.dispatch(GlobalAction.DebugShowError(TapError.CustomError(it)))
+    }
+
+    store.dispatch(GlobalAction.ReadCard(service::onSuccess, {
+        changeButtonState(ButtonState.ENABLED)
+        store.dispatch(GlobalAction.DebugShowError(TapError.ScanCardError))
+    }))
+}
