c279e48fdd7c1929353c634747975af155033bc2
Tangem <git@tangem.com>
2021-09-25 01:51:33 +0300

    Updated on 2026-08-07

--- app/src/main/java/com/tangem/tap/common/extensions/Store.kt
+++ app/src/main/java/com/tangem/tap/common/extensions/Store.kt
@@ -1,6 +1,8 @@
 package com.tangem.tap.common.extensions
 
-import com.tangem.tap.common.redux.AppState
+import com.tangem.tap.common.redux.StateDialog
+import com.tangem.tap.common.redux.global.GlobalAction
+import com.tangem.tap.common.redux.navigation.NavigationAction
 import com.tangem.tap.scope
 import com.tangem.tap.store
 import kotlinx.coroutines.Dispatchers
@@ -8,8 +10,36 @@ import kotlinx.coroutines.launch
 import org.rekotlin.Action
 import org.rekotlin.Store
 
-fun Store<AppState>.dispatchOnMain(action: Action) {
+fun Store<*>.dispatchOnMain(action: Action) {
     scope.launch(Dispatchers.Main) {
         store.dispatch(action)
+    }
+}
+
+fun Store<*>.dispatchOpenUrl(url: String) {
+    store.dispatch(NavigationAction.OpenUrl(url))
+}
+
+fun Store<*>.dispatchNotification(resId: Int) {
+    scope.launch(Dispatchers.Main) {
+        store.dispatch(GlobalAction.ShowNotification(resId))
+    }
+}
+
+fun Store<*>.dispatchToastNotification(resId: Int) {
+    scope.launch(Dispatchers.Main) {
+        store.dispatch(GlobalAction.ShowToastNotification(resId))
+    }
+}
+
+fun Store<*>.dispatchDialogShow(dialog: StateDialog) {
+    scope.launch(Dispatchers.Main) {
+        store.dispatch(GlobalAction.ShowDialog(dialog))
+    }
+}
+
+fun Store<*>.dispatchDialogHide() {
+    scope.launch(Dispatchers.Main) {
+        store.dispatch(GlobalAction.HideDialog)
     }
 }
