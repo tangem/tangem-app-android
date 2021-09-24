474d9f971a2191ae3dce4694355a878371242b94
Tangem <git@tangem.com>
2021-09-24 18:28:40 +0300

    Updated on 2026-08-19

--- app/src/main/java/com/tangem/tap/common/redux/navigation/NavigationAction.kt
+++ app/src/main/java/com/tangem/tap/common/redux/navigation/NavigationAction.kt
@@ -2,18 +2,15 @@ package com.tangem.tap.common.redux.navigation
 
 import android.view.View
 import androidx.fragment.app.FragmentActivity
-import androidx.transition.ChangeBounds
-import androidx.transition.ChangeImageTransform
-import androidx.transition.ChangeTransform
 import androidx.transition.TransitionSet
 import org.rekotlin.Action
 import java.lang.ref.WeakReference
 
 sealed class NavigationAction : Action {
     data class NavigateTo(
-            val screen: AppScreen,
-            val addToBackstack: Boolean = true,
-            val fragmentShareTransition: FragmentShareTransition? = null
+        val screen: AppScreen,
+        val addToBackstack: Boolean = true,
+        val fragmentShareTransition: FragmentShareTransition? = null
     ) : NavigationAction()
 
     data class PopBackTo(val screen: AppScreen? = null) : NavigationAction()
@@ -25,19 +22,9 @@ sealed class NavigationAction : Action {
 }
 
 data class FragmentShareTransition(
-        val shareElements: List<ShareElement>,
-        val enterTransitionSet: TransitionSet,
-        val exitTransitionSet: TransitionSet
+    val shareElements: List<ShareElement>,
+    val enterTransitionSet: TransitionSet,
+    val exitTransitionSet: TransitionSet
 )
 
 data class ShareElement(val wView: WeakReference<View>, val name: String)
-
-//TODO: remove transition example
-class DetailsTransition : TransitionSet() {
-    init {
-        ordering = ORDERING_TOGETHER;
-        addTransition(ChangeBounds())
-                .addTransition(ChangeTransform())
-                .addTransition(ChangeImageTransform())
-    }
-}
