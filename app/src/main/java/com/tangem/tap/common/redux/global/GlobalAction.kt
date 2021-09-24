d0e7edb31dbde617d73c52d61fe6c7c7cfd08a49
Tangem <git@tangem.com>
2021-09-25 01:51:33 +0300

    Updated on 2026-09-14

--- app/src/main/java/com/tangem/tap/common/redux/global/GlobalAction.kt
+++ app/src/main/java/com/tangem/tap/common/redux/global/GlobalAction.kt
@@ -4,6 +4,9 @@ import com.tangem.blockchain.common.WalletManager
 import com.tangem.common.CompletionResult
 import com.tangem.common.core.TangemError
 import com.tangem.tap.common.redux.DebugErrorAction
+import com.tangem.tap.common.redux.NotificationAction
+import com.tangem.tap.common.redux.StateDialog
+import com.tangem.tap.common.redux.ToastNotificationAction
 import com.tangem.tap.domain.TapError
 import com.tangem.tap.domain.configurable.config.ConfigManager
 import com.tangem.tap.domain.configurable.warningMessage.WarningMessage
@@ -16,6 +19,14 @@ import com.tangem.tap.network.moonpay.MoonPayUserStatus
 import org.rekotlin.Action
 
 sealed class GlobalAction : Action {
+
+    // notifications
+    data class ShowNotification(override val messageResource: Int) : GlobalAction(), NotificationAction
+    data class ShowToastNotification(override val messageResource: Int) : GlobalAction(), ToastNotificationAction
+
+    // dialogs
+    data class ShowDialog(val stateDialog: StateDialog) : GlobalAction()
+    object HideDialog : GlobalAction()
 
     data class DebugShowError(override val error: TapError) : GlobalAction(), DebugErrorAction
 
@@ -55,9 +66,6 @@ sealed class GlobalAction : Action {
 
     data class SendFeedback(val emailData: EmailData) : GlobalAction()
     data class UpdateFeedbackInfo(val walletManagers: List<WalletManager>) : GlobalAction()
-
-    data class ShowDialog(val stateDialog: StateDialog) : GlobalAction()
-    object HideDialog : GlobalAction()
 
     object GetMoonPayUserStatus : GlobalAction() {
         data class Success(val moonPayUserStatus: MoonPayUserStatus) : GlobalAction()
