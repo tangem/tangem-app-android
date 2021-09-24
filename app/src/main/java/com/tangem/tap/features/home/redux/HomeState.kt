e1646bf4b9ffd4b0dcd762d67d50d13baeff37ad
Tangem <git@tangem.com>
2021-09-25 01:51:33 +0300

    Updated on 2026-09-23

--- app/src/main/java/com/tangem/tap/features/home/redux/HomeState.kt
+++ app/src/main/java/com/tangem/tap/features/home/redux/HomeState.kt
@@ -1,19 +1,12 @@
 package com.tangem.tap.features.home.redux
 
 import com.tangem.tap.common.entities.IndeterminateProgressButton
-import com.tangem.tap.common.redux.global.StateDialog
 import com.tangem.tap.common.redux.navigation.FragmentShareTransition
 import com.tangem.tap.features.send.redux.states.ButtonState
 import org.rekotlin.StateType
 
 data class HomeState(
-        val openUrl: String? = null,
-        val isDisclaimerAccepted: Boolean = false,
-        val shareTransition: FragmentShareTransition? = null,
-        val btnScanState: IndeterminateProgressButton = IndeterminateProgressButton(ButtonState.ENABLED),
-        val dialog: StateDialog? = null,
+    val isDisclaimerAccepted: Boolean = false,
+    val shareTransition: FragmentShareTransition? = null,
+    val btnScanState: IndeterminateProgressButton = IndeterminateProgressButton(ButtonState.ENABLED),
 ) : StateType
-
-sealed class HomeDialog : StateDialog {
-    object ScanFailsDialog : HomeDialog()
-}
