fea6bed2c4145e274e7da4635104836b44e06196
Tangem <git@tangem.com>
2021-09-25 01:51:33 +0300

    Updated on 2025-12-30

--- app/src/main/java/com/tangem/tap/features/home/HomeFragment.kt
+++ app/src/main/java/com/tangem/tap/features/home/HomeFragment.kt
@@ -1,15 +1,9 @@
 package com.tangem.tap.features.home
 
-import android.app.Dialog
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.transition.TransitionInflater
-import com.tangem.tap.common.extensions.safeStartActivity
-import com.tangem.tap.common.extensions.toast
-import com.tangem.tap.common.redux.global.StateDialog
 import com.tangem.tap.common.redux.navigation.FragmentShareTransition
 import com.tangem.tap.common.redux.navigation.ShareElement
 import com.tangem.tap.common.toggleWidget.IndeterminateProgressButtonWidget
@@ -17,9 +11,7 @@ import com.tangem.tap.common.toggleWidget.ViewStateWidget
 import com.tangem.tap.common.transitions.FrontCardEnterTransition
 import com.tangem.tap.common.transitions.FrontCardExitTransition
 import com.tangem.tap.features.home.redux.HomeAction
-import com.tangem.tap.features.home.redux.HomeDialog
 import com.tangem.tap.features.home.redux.HomeState
-import com.tangem.tap.features.wallet.ui.dialogs.ScanFailsDialog
 import com.tangem.tap.store
 import com.tangem.wallet.R
 import kotlinx.android.synthetic.main.fragment_home.*
@@ -28,9 +20,7 @@ import java.lang.ref.WeakReference
 
 class HomeFragment : Fragment(R.layout.fragment_home), StoreSubscriber<HomeState> {
 
-    lateinit var btnScanCard: ViewStateWidget
-
-    private var dialog: Dialog? = null
+    private lateinit var btnScanCard: ViewStateWidget
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -74,34 +64,8 @@ class HomeFragment : Fragment(R.layout.fragment_home), StoreSubscriber<HomeState
     override fun newState(state: HomeState) {
         if (activity == null) return
 
-        handleOpenUrl(state)
-        handleDialog(state.dialog)
-
         btnScanCard.changeState(state.btnScanState.progressState)
         btnScanCard.mainView.isEnabled = state.btnScanState.enabled
-    }
-
-    private fun handleOpenUrl(state: HomeState) {
-        if (state.openUrl == null) return
-        val context = context ?: return
-
-        val uri = Uri.parse(state.openUrl)
-        val intent = Intent(Intent.ACTION_VIEW, uri)
-        context.safeStartActivity(intent, null, { toast(it.toString()) }, {
-            store.dispatch(HomeAction.SetOpenUrl(null))
-        })
-    }
-
-    private fun handleDialog(stateDialog: StateDialog?) {
-        when (stateDialog) {
-            is HomeDialog.ScanFailsDialog -> {
-                if (dialog == null) dialog = ScanFailsDialog.create(requireContext()).apply { show() }
-            }
-            else -> {
-                dialog?.dismiss()
-                dialog = null
-            }
-        }
     }
 
     override fun onDestroyView() {
