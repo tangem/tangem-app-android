62b90541dd19e92c74a476d4c331572cd44dbf26
Tangem <git@tangem.com>
2021-10-02 18:01:56 +0300

    Updated on 2025-11-03

--- app/src/main/java/com/tangem/tap/domain/UrlBitmapLoader.kt
+++ app/src/main/java/com/tangem/tap/domain/UrlBitmapLoader.kt
@@ -2,6 +2,8 @@ package com.tangem.tap.domain
 
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
+import android.os.Handler
+import android.os.Looper
 import com.squareup.picasso.Picasso
 import com.squareup.picasso.Target
 import com.tangem.common.services.Result
@@ -10,10 +12,12 @@ import com.tangem.common.services.Result
 [REDACTED_AUTHOR]
  */
 class UrlBitmapLoader {
+    private val mainHandler = Handler(Looper.getMainLooper())
+
     fun loadBitmap(url: String, callback: (Result<Bitmap>) -> Unit) {
         val target = DownloadTarget(callback)
         protectedFromGarbageCollectorTargets.add(target)
-        Picasso.get().load(url).into(target)
+        mainHandler.post { Picasso.get().load(url).into(target) }
     }
 
     fun loadBitmap(url: String, target: DownloadTarget) {
