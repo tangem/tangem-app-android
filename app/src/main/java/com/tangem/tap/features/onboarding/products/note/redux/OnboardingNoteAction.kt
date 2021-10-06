7111aa9fbdbb8fd12a6b6b725db48d3ffe067aa4
Tangem <git@tangem.com>
2021-10-06 22:30:19 +0300

    Updated on 2026-06-27

--- app/src/main/java/com/tangem/tap/features/onboarding/products/note/redux/OnboardingNoteAction.kt
+++ app/src/main/java/com/tangem/tap/features/onboarding/products/note/redux/OnboardingNoteAction.kt
@@ -1,16 +1,31 @@
 package com.tangem.tap.features.onboarding.products.note.redux
 
-import com.tangem.blockchain.common.WalletManager
+import com.tangem.tap.features.onboarding.service.OnboardingWalletBalance
 import org.rekotlin.Action
 
 /**
 [REDACTED_AUTHOR]
  */
 sealed class OnboardingNoteAction : Action {
-    object Init : OnboardingNoteAction()
+    // from user, ui
+    data class SetResources(val resources: AndroidResources) : OnboardingNoteAction()
     object CreateWallet : OnboardingNoteAction()
     object TopUp : OnboardingNoteAction()
+    object ShowAddressInfoDialog : OnboardingNoteAction()
+
+    // from redux
+    object DetermineStepOfScreen : OnboardingNoteAction()
     object Done : OnboardingNoteAction()
 
-    data class SetWalletManager(val walletManager: WalletManager) : OnboardingNoteAction()
+    data class SetStepOfScreen(val step: OnboardingNoteStep) : OnboardingNoteAction()
+
+    sealed class Balance {
+        object Update : OnboardingNoteAction()
+        data class Set(val balance: OnboardingWalletBalance) : OnboardingNoteAction()
+    }
+
+    sealed class Confetti {
+        object Show : OnboardingNoteAction()
+        object Hide : OnboardingNoteAction()
+    }
 }
