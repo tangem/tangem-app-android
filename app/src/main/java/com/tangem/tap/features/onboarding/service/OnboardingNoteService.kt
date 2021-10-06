9462e48ce8f89137e6094423dff5b40429f31e5b
Tangem <git@tangem.com>
2021-10-06 22:30:19 +0300

    Updated on 2025-11-03

---
+++ app/src/main/java/com/tangem/tap/features/onboarding/service/OnboardingNoteService.kt
@@ -0,0 +1,21 @@
+package com.tangem.tap.features.onboarding.service
+
+import com.tangem.blockchain.common.WalletManagerFactory
+import com.tangem.tap.common.redux.navigation.AppScreen
+import com.tangem.tap.domain.tasks.ScanNoteResponse
+import com.tangem.tap.persistence.UsedCardsPrefStorage
+
+/**
+[REDACTED_AUTHOR]
+ */
+class OnboardingNoteService(
+    override val fromScreen: AppScreen,
+    override var scanResponse: ScanNoteResponse,
+    cardInfoStorage: UsedCardsPrefStorage,
+    walletManagerFactory: WalletManagerFactory,
+) : ProductOnboardingService(cardInfoStorage, walletManagerFactory) {
+
+    override fun navigateToScreen(): AppScreen {
+        return AppScreen.OnboardingNote
+    }
+}
