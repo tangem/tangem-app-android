3298e6c335b199f3ee19df86dece0b564a622e63
Tangem <git@tangem.com>
2021-10-13 18:54:49 +0300

    Updated on 2026-08-19

--- app/src/main/java/com/tangem/tap/common/redux/global/GlobalState.kt
+++ app/src/main/java/com/tangem/tap/common/redux/global/GlobalState.kt
@@ -6,7 +6,7 @@ import com.tangem.tap.domain.PayIdManager
 import com.tangem.tap.domain.TapWalletManager
 import com.tangem.tap.domain.configurable.config.ConfigManager
 import com.tangem.tap.domain.configurable.warningMessage.WarningMessagesManager
-import com.tangem.tap.domain.tasks.ScanNoteResponse
+import com.tangem.tap.domain.tasks.product.ScanResponse
 import com.tangem.tap.features.feedback.FeedbackManager
 import com.tangem.tap.features.onboarding.service.ProductOnboardingService
 import com.tangem.tap.network.coinmarketcap.CoinMarketCapService
@@ -14,19 +14,19 @@ import com.tangem.tap.network.moonpay.MoonPayUserStatus
 import org.rekotlin.StateType
 
 data class GlobalState(
-        val scanNoteResponse: ScanNoteResponse? = null,
-        val cardVerifiedOnline: Boolean = false,
-        val tapWalletManager: TapWalletManager = TapWalletManager(),
-        val payIdManager: PayIdManager = PayIdManager(),
-        val coinMarketCapService: CoinMarketCapService = CoinMarketCapService(),
-        val configManager: ConfigManager? = null,
-        val warningManager: WarningMessagesManager? = null,
-        val feedbackManager: FeedbackManager? = null,
-        val appCurrency: FiatCurrencyName = DEFAULT_FIAT_CURRENCY,
-        val scanCardFailsCounter: Int = 0,
-        val dialog: StateDialog? = null,
-        val moonPayUserStatus: MoonPayUserStatus? = null,
-        val onboardingService: ProductOnboardingService? = null,
+    val scanResponse: ScanResponse? = null,
+    val cardVerifiedOnline: Boolean = false,
+    val tapWalletManager: TapWalletManager = TapWalletManager(),
+    val payIdManager: PayIdManager = PayIdManager(),
+    val coinMarketCapService: CoinMarketCapService = CoinMarketCapService(),
+    val configManager: ConfigManager? = null,
+    val warningManager: WarningMessagesManager? = null,
+    val feedbackManager: FeedbackManager? = null,
+    val appCurrency: FiatCurrencyName = DEFAULT_FIAT_CURRENCY,
+    val scanCardFailsCounter: Int = 0,
+    val dialog: StateDialog? = null,
+    val moonPayUserStatus: MoonPayUserStatus? = null,
+    val onboardingService: ProductOnboardingService? = null,
 ) : StateType
 
 typealias CryptoCurrencyName = String
