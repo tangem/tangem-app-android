48204a7f31c009cc283e904e5ca24e3c102cb800
Tangem <git@tangem.com>
2021-10-14 22:11:44 +0300

    Updated on 2026-08-19

--- app/src/main/java/com/tangem/tap/common/leapfrogWidget/Animations.kt
+++ app/src/main/java/com/tangem/tap/common/leapfrogWidget/Animations.kt
@@ -32,14 +32,14 @@ fun createProgressListener(duration: Long, listener: ProgressListener?): Animato
     return valueAnimator
 }
 
-fun View.unfoldAnimation(animDuration: Long, properties: AnimationProperties): AnimatorSet {
-    val translate = ObjectAnimator.ofFloat(this, View.TRANSLATION_Y, properties.yTranslationStart, properties.yTranslationEnd)
+fun View.unfoldAnimation(animDuration: Long, properties: LeapViewProperties): AnimatorSet {
+    val translate = ObjectAnimator.ofFloat(this, View.TRANSLATION_Y, properties.yTranslation)
     translate.duration = animDuration
     return AnimatorSet().apply { playTogether(translate) }
 }
 
-fun View.foldAnimation(animDuration: Long, properties: AnimationProperties): AnimatorSet {
-    val translate = ObjectAnimator.ofFloat(this, View.TRANSLATION_Y, properties.yTranslationStart, properties.yTranslationEnd)
+fun View.foldAnimation(animDuration: Long, properties: LeapViewProperties): AnimatorSet {
+    val translate = ObjectAnimator.ofFloat(this, View.TRANSLATION_Y, properties.yTranslation)
     translate.duration = animDuration
 
     return AnimatorSet().apply {
@@ -47,17 +47,17 @@ fun View.foldAnimation(animDuration: Long, properties: AnimationProperties): Ani
     }
 }
 
-fun View.leapAnimation(animDuration: Long, properties: AnimationProperties, overLift: Float): AnimatorSet {
+fun View.leapAnimation(animDuration: Long, properties: LeapViewProperties, overLift: Float): AnimatorSet {
     val halfDuration = animDuration / 2
 
-    val upTo = (height.toFloat() + properties.yTranslationStart + overLift) * -1
-    val translateUp = ObjectAnimator.ofFloat(this, View.TRANSLATION_Y, properties.yTranslationStart, upTo)
+    val upTo = (height.toFloat() + overLift) * -1
+    val translateUp = ObjectAnimator.ofFloat(this, View.TRANSLATION_Y, upTo)
     translateUp.duration = halfDuration
     translateUp.interpolator = LinearInterpolator()
 
     val scaleDuration = halfDuration - halfDuration / 5
-    val scaleX = ObjectAnimator.ofFloat(this, View.SCALE_X, properties.scaleStart, properties.scaleEnd)
-    val scaleY = ObjectAnimator.ofFloat(this, View.SCALE_Y, properties.scaleStart, properties.scaleEnd)
+    val scaleX = ObjectAnimator.ofFloat(this, View.SCALE_X, properties.scale)
+    val scaleY = ObjectAnimator.ofFloat(this, View.SCALE_Y, properties.scale)
     val scale = AnimatorSet().apply {
         startDelay = scaleDuration
         duration = scaleDuration
@@ -65,37 +65,28 @@ fun View.leapAnimation(animDuration: Long, properties: AnimationProperties, over
         playTogether(scaleX, scaleY)
     }
 
-    val elevation = ValueAnimator.ofFloat(properties.elevationStart, properties.elevationEnd)
-    elevation.addUpdateListener { this.elevation = (it.animatedValue as? Float) ?: properties.elevationStart }
-    elevation.startDelay = halfDuration
-    elevation.duration = 100
-
-    val translateDown = ObjectAnimator.ofFloat(this, View.TRANSLATION_Y, upTo, properties.yTranslationEnd)
+    val elevation = elevationAnimator(properties.elevationStart, properties.elevationEnd, halfDuration, 100)
+    val translateDown = ObjectAnimator.ofFloat(this, View.TRANSLATION_Y, properties.yTranslation)
     translateDown.startDelay = halfDuration
     translateDown.duration = halfDuration
     translateDown.interpolator = LinearInterpolator()
 
     return AnimatorSet().apply {
-        playTogether(
-                translateUp,
-                scale,
-                elevation,
-                translateDown
-        )
+        playTogether(translateUp, scale, elevation, translateDown)
     }
 }
 
-fun View.leapBackAnimation(animDuration: Long, properties: AnimationProperties, calculator: PropertyCalculator): AnimatorSet {
+fun View.leapBackAnimation(animDuration: Long, properties: LeapViewProperties, calculator: PropertyCalculator): AnimatorSet {
     val halfDuration = animDuration / 2
 
     val upTo = ((height).toFloat() - calculator.yTranslationFactor) * -1
-    val translateUp = ObjectAnimator.ofFloat(this, View.TRANSLATION_Y, properties.yTranslationStart, upTo)
+    val translateUp = ObjectAnimator.ofFloat(this, View.TRANSLATION_Y, upTo)
     translateUp.duration = halfDuration
     translateUp.interpolator = LinearInterpolator()
 
     val scaleDuration = halfDuration - halfDuration / 5
-    val scaleX = ObjectAnimator.ofFloat(this, View.SCALE_X, properties.scaleStart, properties.scaleEnd)
-    val scaleY = ObjectAnimator.ofFloat(this, View.SCALE_Y, properties.scaleStart, properties.scaleEnd)
+    val scaleX = ObjectAnimator.ofFloat(this, View.SCALE_X, properties.scale)
+    val scaleY = ObjectAnimator.ofFloat(this, View.SCALE_Y, properties.scale)
     val scale = AnimatorSet().apply {
         startDelay = scaleDuration
         duration = scaleDuration
@@ -103,34 +94,24 @@ fun View.leapBackAnimation(animDuration: Long, properties: AnimationProperties, 
         playTogether(scaleX, scaleY)
     }
 
-    val elevation = ValueAnimator.ofFloat(properties.elevationStart, properties.elevationEnd)
-    elevation.addUpdateListener { this.elevation = (it.animatedValue as? Float) ?: properties.elevationStart }
-    elevation.startDelay = halfDuration
-    elevation.duration = 100
-
-    val translateDown = ObjectAnimator.ofFloat(this, View.TRANSLATION_Y, upTo, properties.yTranslationEnd)
+    val elevation = elevationAnimator(properties.elevationStart, properties.elevationEnd, halfDuration, 100)
+    val translateDown = ObjectAnimator.ofFloat(this, View.TRANSLATION_Y, properties.yTranslation)
     translateDown.startDelay = halfDuration
     translateDown.duration = halfDuration
 
     return AnimatorSet().apply {
-        playTogether(
-                translateUp,
-                scale,
-                elevation,
-                translateDown,
-        )
+        playTogether(translateUp, scale, elevation, translateDown)
     }
 }
 
-fun View.pullUpAnimation(leapDuration: Long, properties: AnimationProperties): AnimatorSet {
+fun View.pullUpAnimation(leapDuration: Long, properties: LeapViewProperties): AnimatorSet {
     val pullDuration = leapDuration / 2
     val delayDuration = leapDuration / 2
 
-    val translateUp = ObjectAnimator.ofFloat(this, View.TRANSLATION_Y, properties.yTranslationStart, properties.yTranslationEnd)
-    val scaleX = ObjectAnimator.ofFloat(this, View.SCALE_X, properties.scaleStart, properties.scaleEnd)
-    val scaleY = ObjectAnimator.ofFloat(this, View.SCALE_Y, properties.scaleStart, properties.scaleEnd)
-    val elevation = ValueAnimator.ofFloat(properties.elevationStart, properties.elevationEnd)
-    elevation.addUpdateListener { this.elevation = (it.animatedValue as? Float) ?: properties.elevationStart }
+    val translateUp = ObjectAnimator.ofFloat(this, View.TRANSLATION_Y, properties.yTranslation)
+    val scaleX = ObjectAnimator.ofFloat(this, View.SCALE_X, properties.scale)
+    val scaleY = ObjectAnimator.ofFloat(this, View.SCALE_Y, properties.scale)
+    val elevation = elevationAnimator(properties.elevationStart, properties.elevationEnd)
 
     return AnimatorSet().apply {
         startDelay = delayDuration
@@ -140,15 +121,14 @@ fun View.pullUpAnimation(leapDuration: Long, properties: AnimationProperties): A
     }
 }
 
-fun View.pullDownAnimation(leapDuration: Long, properties: AnimationProperties): AnimatorSet {
+fun View.pullDownAnimation(leapDuration: Long, properties: LeapViewProperties): AnimatorSet {
     val pullDuration = leapDuration / 2
     val delayDuration = leapDuration / 4
 
-    val translate = ObjectAnimator.ofFloat(this, View.TRANSLATION_Y, properties.yTranslationStart, properties.yTranslationEnd)
-    val scaleX = ObjectAnimator.ofFloat(this, View.SCALE_X, properties.scaleStart, properties.scaleEnd)
-    val scaleY = ObjectAnimator.ofFloat(this, View.SCALE_Y, properties.scaleStart, properties.scaleEnd)
-    val elevation = ValueAnimator.ofFloat(properties.elevationStart, properties.elevationEnd)
-    elevation.addUpdateListener { this.elevation = (it.animatedValue as? Float) ?: properties.elevationStart }
+    val translate = ObjectAnimator.ofFloat(this, View.TRANSLATION_Y, properties.yTranslation)
+    val scaleX = ObjectAnimator.ofFloat(this, View.SCALE_X, properties.scale)
+    val scaleY = ObjectAnimator.ofFloat(this, View.SCALE_Y, properties.scale)
+    val elevation = elevationAnimator(properties.elevationStart, properties.elevationEnd)
 
     return AnimatorSet().apply {
         startDelay = delayDuration
@@ -156,4 +136,12 @@ fun View.pullDownAnimation(leapDuration: Long, properties: AnimationProperties):
         interpolator = DecelerateInterpolator()
         playTogether(translate, scaleX, scaleY, elevation)
     }
+}
+
+fun View.elevationAnimator(start: Float, end: Float, startDelay: Long? = null, duration: Long? = null): Animator {
+    val elevationAnimator = ValueAnimator.ofFloat(start, end)
+    elevationAnimator.addUpdateListener { this.elevation = (it.animatedValue as? Float) ?: start }
+    startDelay?.let { elevationAnimator.startDelay = it }
+    duration?.let { elevationAnimator.duration = it }
+    return elevationAnimator
 }
