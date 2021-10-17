b7fe5c9511dad528c86ad6a1a4c24655a88156e5
Tangem <git@tangem.com>
2021-10-17 22:40:55 +0300

    Updated on 2026-06-10

--- app/src/main/java/com/tangem/tap/features/onboarding/products/otherCards/redux/OnboardingOtherCardsState.kt
+++ app/src/main/java/com/tangem/tap/features/onboarding/products/otherCards/redux/OnboardingOtherCardsState.kt
@@ -1,17 +1,15 @@
 package com.tangem.tap.features.onboarding.products.otherCards.redux
 
-import android.graphics.Bitmap
-import com.tangem.tap.features.onboarding.service.OnboardingOtherCardsService
+import com.tangem.tap.features.wallet.redux.Artwork
 import org.rekotlin.StateType
 
 /**
 [REDACTED_AUTHOR]
  */
 data class OnboardingOtherCardsState(
-    val onboardingService: OnboardingOtherCardsService? = null,
-        // UI
+    // UI
+    val cardArtwork: Artwork? = null,
     val showConfetti: Boolean = false,
-    val artworkBitmap: Bitmap? = null,
     val currentStep: OnboardingOtherCardsStep = OnboardingOtherCardsStep.None,
     val steps: List<OnboardingOtherCardsStep> = OnboardingOtherCardsStep.values().toList(),
 ) : StateType {
