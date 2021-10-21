e2459a4211cd7fb10cb5ffa5dd410266cbf3041a
Tangem <git@tangem.com>
2021-10-21 20:42:12 +0300

    Updated on 2026-06-10

--- app/src/main/java/com/tangem/tap/features/details/redux/DetailsState.kt
+++ app/src/main/java/com/tangem/tap/features/details/redux/DetailsState.kt
@@ -6,41 +6,41 @@ import com.tangem.common.card.Card
 import com.tangem.tap.common.entities.Button
 import com.tangem.tap.common.entities.TapCurrency.Companion.DEFAULT_FIAT_CURRENCY
 import com.tangem.tap.common.redux.global.FiatCurrencyName
-import com.tangem.tap.features.details.redux.twins.CreateTwinWalletState
+import com.tangem.tap.features.twins.redux.CreateTwinWalletState
 import com.tangem.tap.network.coinmarketcap.FiatCurrency
 import org.rekotlin.StateType
 import java.util.*
 
 data class DetailsState(
-        val card: Card? = null,
-        val wallets: List<Wallet> = emptyList(),
-        val cardInfo: CardInfo? = null,
-        val appCurrencyState: AppCurrencyState = AppCurrencyState(),
-        val eraseWalletState: EraseWalletState? = null,
-        val confirmScreenState: ConfirmScreenState? = null,
-        val securityScreenState: SecurityScreenState? = null,
-        val createTwinWalletState: CreateTwinWalletState? = null,
-        val cardTermsOfUseUrl: Uri? = null,
+    val card: Card? = null,
+    val wallets: List<Wallet> = emptyList(),
+    val cardInfo: CardInfo? = null,
+    val appCurrencyState: AppCurrencyState = AppCurrencyState(),
+    val eraseWalletState: EraseWalletState? = null,
+    val confirmScreenState: ConfirmScreenState? = null,
+    val securityScreenState: SecurityScreenState? = null,
+    val createTwinWalletState: CreateTwinWalletState? = null,
+    val cardTermsOfUseUrl: Uri? = null,
 ) : StateType
 
 data class CardInfo(
-        val cardId: String,
-        val issuer: String,
-        val signedHashes: Int,
+    val cardId: String,
+    val issuer: String,
+    val signedHashes: Int,
 )
 
 enum class EraseWalletState { Allowed, NotAllowedByCard, NotEmpty }
 enum class ConfirmScreenState { EraseWallet, LongTap, AccessCode, PassCode }
 data class SecurityScreenState(
-        val currentOption: SecurityOption = SecurityOption.LongTap,
-        val selectedOption: SecurityOption = currentOption,
-        val allowedOptions: EnumSet<SecurityOption> = EnumSet.allOf(SecurityOption::class.java),
-        val buttonProceed: Button = Button(true),
+    val currentOption: SecurityOption = SecurityOption.LongTap,
+    val selectedOption: SecurityOption = currentOption,
+    val allowedOptions: EnumSet<SecurityOption> = EnumSet.allOf(SecurityOption::class.java),
+    val buttonProceed: Button = Button(true),
 )
 
 enum class SecurityOption { LongTap, PassCode, AccessCode }
 data class AppCurrencyState(
-        val fiatCurrencyName: FiatCurrencyName = DEFAULT_FIAT_CURRENCY,
-        val showAppCurrencyDialog: Boolean = false,
-        val fiatCurrencies: List<FiatCurrency>? = null,
+    val fiatCurrencyName: FiatCurrencyName = DEFAULT_FIAT_CURRENCY,
+    val showAppCurrencyDialog: Boolean = false,
+    val fiatCurrencies: List<FiatCurrency>? = null,
 )
