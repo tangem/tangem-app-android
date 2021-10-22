f91b7bcb71f675cf2c6bfa02c77c7fdf48da34f7
Tangem <git@tangem.com>
2021-10-22 05:54:34 +0300

    Updated on 2026-01-20

--- app/src/main/java/com/tangem/tap/features/twins/redux/TwinCardsState.kt
+++ app/src/main/java/com/tangem/tap/features/twins/redux/TwinCardsState.kt
@@ -2,22 +2,22 @@ package com.tangem.tap.features.twins.redux
 
 import com.tangem.tap.domain.tasks.product.ScanResponse
 import com.tangem.tap.domain.twins.TwinCardNumber
+import com.tangem.tap.domain.twins.TwinCardsManager
 import org.rekotlin.StateType
 
 /**
 [REDACTED_AUTHOR]
  */
 data class TwinCardsState(
+    val twinCardsManager: TwinCardsManager? = null,
     // if cardNumber is not set -> the scanned card is not Twin
     val cardNumber: TwinCardNumber? = null,
     val secondCardId: String? = null,
     val showTwinOnboarding: Boolean = false,
     val isCreatingTwinCardsAllowed: Boolean = false,
-    val createWalletState: CreateTwinWalletState? = null
-) : StateType {
-    val isReadyToUse: Boolean
-        get() = cardNumber != null
-}
+    val createWalletState: CreateTwinWalletState? = null,
+    val resources: TwinCardsResources = TwinCardsResources(),
+) : StateType
 
 data class CreateTwinWalletState(
     val scanResponse: ScanResponse?,
@@ -26,9 +26,18 @@ data class CreateTwinWalletState(
     val step: CreateTwinWalletStep = CreateTwinWalletStep.FirstStep,
 ) {
     val showAlert: Boolean
-        get() = mode != CreateTwinWalletMode.RecreateWallet
+        get() = step != CreateTwinWalletStep.FirstStep
 }
 
 enum class CreateTwinWalletStep { FirstStep, SecondStep, ThirdStep }
 
 enum class CreateTwinWalletMode { CreateWallet, RecreateWallet }
+
+data class TwinCardsResources(
+    val strings: Strings = Strings()
+) {
+    //TODO: create global structure for StringRes. Set it from the MainActivity.
+    data class Strings(
+        val walletIsNotEmpty: Int = -1
+    )
+}
