b40e3e90105f863c12a6c537f1d70a8ccc97254e
Tangem <git@tangem.com>
2021-11-11 02:32:19 +0300

    Updated on 2026-09-21

--- app/src/main/java/com/tangem/tap/domain/extensions/MoonpayStatus.kt
+++ app/src/main/java/com/tangem/tap/domain/extensions/MoonpayStatus.kt
@@ -1,19 +1,38 @@
 package com.tangem.tap.domain.extensions
 
 import com.tangem.blockchain.common.Blockchain
+import com.tangem.tap.features.wallet.redux.Currency
 import com.tangem.tap.network.moonpay.MoonpayStatus
 
 /**
 [REDACTED_AUTHOR]
  */
-fun MoonpayStatus.buyIsAllowed(blockchain: Blockchain): Boolean {
-    if (blockchain == Blockchain.Unknown || blockchain == Blockchain.BSC) return false
+fun MoonpayStatus.buyIsAllowed(currency: Currency): Boolean {
+    if (!isBuyAllowed) return false
 
-    return isBuyAllowed && availableToBuy.contains(blockchain.currency)
+    return when (currency) {
+        is Currency.Blockchain -> {
+            if (currency.blockchain == Blockchain.Unknown || currency.blockchain == Blockchain.BSC) {
+                false
+            } else {
+                availableToBuy.contains(currency.currencySymbol)
+            }
+        }
+        is Currency.Token -> false
+    }
 }
 
-fun MoonpayStatus.sellIsAllowed(blockchain: Blockchain): Boolean {
-    if (blockchain == Blockchain.Unknown || blockchain == Blockchain.BSC) return false
+fun MoonpayStatus.sellIsAllowed(currency: Currency): Boolean {
+    if (!isSellAllowed) return false
 
-    return isSellAllowed && availableToSell.contains(blockchain.currency)
+    return when (currency) {
+        is Currency.Blockchain -> {
+            if (currency.blockchain == Blockchain.Unknown || currency.blockchain == Blockchain.BSC) {
+                false
+            } else {
+                availableToSell.contains(currency.currencySymbol)
+            }
+        }
+        is Currency.Token -> false
+    }
 }
