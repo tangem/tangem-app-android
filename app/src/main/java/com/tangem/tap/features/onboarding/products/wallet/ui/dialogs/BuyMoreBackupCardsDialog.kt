c26fdb17a0a64fc740202e664e96f060a0e0517e
Tangem <git@tangem.com>
2021-12-02 18:18:50 +0300

    Updated on 2026-03-25

---
+++ app/src/main/java/com/tangem/tap/features/onboarding/products/wallet/ui/dialogs/BuyMoreBackupCardsDialog.kt
@@ -0,0 +1,28 @@
+package com.tangem.tap.features.onboarding.products.wallet.ui.dialogs
+
+import android.content.Context
+import androidx.appcompat.app.AlertDialog
+import com.tangem.tap.common.redux.global.GlobalAction
+import com.tangem.tap.features.onboarding.products.wallet.redux.BackupAction
+import com.tangem.tap.store
+import com.tangem.wallet.R
+
+class BuyMoreBackupCardsDialog {
+        companion object {
+            fun create(context: Context): AlertDialog {
+                return AlertDialog.Builder(context).apply {
+                    setTitle(R.string.alert_title)
+                    setMessage(R.string.onboarding_alert_message_not_max_backup_cards_added)
+                    setPositiveButton(R.string.common_continue) { _, _ ->
+                        store.dispatch(BackupAction.ShowAccessCodeInfoScreen)
+                    }
+                    setNegativeButton(R.string.onboarding_button_buy_more_cards) { _, _ ->
+                        store.dispatch(BackupAction.GoToShop)
+                    }
+                    setOnDismissListener {
+                        store.dispatch(GlobalAction.HideDialog)
+                    }
+                }.create()
+            }
+        }
+}
