6794bf0dccdc7a20779cedb21487199fe7d502be
Tangem <git@tangem.com>
2021-12-02 18:18:50 +0300

    Updated on 2026-09-21

--- app/src/main/java/com/tangem/tap/features/onboarding/products/wallet/redux/OnboardingWalletAction.kt
+++ app/src/main/java/com/tangem/tap/features/onboarding/products/wallet/redux/OnboardingWalletAction.kt
@@ -1,19 +1,65 @@
 package com.tangem.tap.features.onboarding.products.wallet.redux
 
+import android.graphics.Bitmap
+import com.tangem.tap.features.wallet.redux.Artwork
 import org.rekotlin.Action
 
-/**
-[REDACTED_AUTHOR]
- */
 sealed class OnboardingWalletAction : Action {
     object Init : OnboardingWalletAction()
+    object GetToCreateWalletStep : OnboardingWalletAction()
     object CreateWallet : OnboardingWalletAction()
-    object TopUp : OnboardingWalletAction()
     object Done : OnboardingWalletAction()
+    object FinishOnboarding : OnboardingWalletAction()
 
     object ProceedBackup : OnboardingWalletAction()
 
-    sealed class Backup : Action {
+    object LoadArtwork : OnboardingWalletAction()
+    class SetArtworkUrl(val artwork: Artwork) : OnboardingWalletAction()
 
+    object OnBackPressed : OnboardingWalletAction()
+}
+
+sealed class BackupAction : Action {
+
+    object DetermineBackupStep : BackupAction()
+    object IntroduceBackup : BackupAction()
+    object StartBackup : BackupAction()
+    object DismissBackup : BackupAction()
+    object ScanPrimaryCard : BackupAction()
+
+    object CheckForUnfinishedBackup : BackupAction()
+
+    object StartAddingBackupCards : BackupAction()
+    object AddBackupCard : BackupAction() {
+        object Success : BackupAction()
     }
+
+    data class LoadBackupCardArtwork(
+        val cardId: CardId,
+        val cardPublicKey: ByteArray,
+    ) : BackupAction() {
+        data class Success(val cardId: CardId, val artwork: Bitmap)
+    }
+
+    object GoToShop : BackupAction()
+    object FinishAddingBackupCards : BackupAction()
+
+    object ShowAccessCodeInfoScreen : BackupAction()
+    object ShowEnterAccessCodeScreen : BackupAction()
+    data class CheckAccessCode(val accessCode: String) : BackupAction()
+    data class SetAccessCodeError(val error: AccessCodeError?) : BackupAction()
+    data class SaveFirstAccessCode(val accessCode: String) : BackupAction()
+    object ShowReenterAccessCodeScreen : BackupAction()
+    data class SaveAccessCodeConfirmation(val accessCodeConfirmation: String) : BackupAction()
+
+    data class PrepareToWriteBackupCard(val cardNumber: Int) : BackupAction()
+    data class WriteBackupCard(val cardNumber: Int) : BackupAction()
+
+    object PrepareToWritePrimaryCard : BackupAction()
+    object WritePrimaryCard: BackupAction()
+
+    object FinishBackup : BackupAction()
+    object DiscardBackup : BackupAction()
+    object ResumeBackup : BackupAction()
+
 }
