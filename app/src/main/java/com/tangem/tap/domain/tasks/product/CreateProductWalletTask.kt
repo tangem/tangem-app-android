2bb7ac5516183da465fd45651cf6311ad43394ef
Tangem <git@tangem.com>
2021-12-07 15:14:06 +0300

    Updated on 2026-09-14

--- app/src/main/java/com/tangem/tap/domain/tasks/product/CreateProductWalletTask.kt
+++ app/src/main/java/com/tangem/tap/domain/tasks/product/CreateProductWalletTask.kt
@@ -1,25 +1,29 @@
+import com.tangem.blockchain.common.Blockchain
 import com.tangem.common.CompletionResult
 import com.tangem.common.card.Card
+import com.tangem.common.card.CardWallet
 import com.tangem.common.card.EllipticCurve
 import com.tangem.common.card.FirmwareVersion
 import com.tangem.common.core.CardSession
 import com.tangem.common.core.CardSessionRunnable
 import com.tangem.common.core.TangemSdkError
 import com.tangem.common.extensions.guard
-import com.tangem.operations.PreflightReadMode
-import com.tangem.operations.PreflightReadTask
+import com.tangem.operations.derivation.DeriveWalletPublicKeysTask
+import com.tangem.operations.derivation.ExtendedPublicKeyList
 import com.tangem.operations.wallet.CreateWalletCommand
 import com.tangem.operations.wallet.CreateWalletResponse
+import com.tangem.tap.common.extensions.toMapKey
 import com.tangem.tap.domain.ProductType
 import com.tangem.tap.domain.TapWorkarounds.getTangemNoteBlockchain
-import com.tangem.tap.domain.tasks.product.CreateProductWalletsTask
+import com.tangem.tap.domain.tasks.product.CreateWalletsTask
 import com.tangem.tap.domain.tasks.product.ProductCommandProcessor
 import com.tangem.tap.domain.tasks.product.getCurvesForNonCreatedWallets
+import com.tangem.tap.store
 
 /**
 [REDACTED_AUTHOR]
  */
-class CreateProductWalletAndRescanTask(
+class CreateProductWalletTask(
     private val type: ProductType,
 ) : CardSessionRunnable<Card> {
 
@@ -28,33 +32,19 @@ class CreateProductWalletAndRescanTask(
             callback(CompletionResult.Failure(TangemSdkError.CardError()))
             return
         }
-        CreateProductWalletTask(card, type).run(session) { result ->
-            when (result) {
-                is CompletionResult.Success -> PreflightReadTask(PreflightReadMode.FullCardRead).run(
-                    session,
-                    callback)
-                is CompletionResult.Failure -> callback(CompletionResult.Failure(result.error))
-            }
-        }
-    }
-}
 
-private class CreateProductWalletTask(
-    private val card: Card,
-    private val type: ProductType,
-) : CardSessionRunnable<CreateWalletResponse> {
-
-    override fun run(
-        session: CardSession,
-        callback: (result: CompletionResult<CreateWalletResponse>) -> Unit,
-    ) {
         val commandProcessor = when (type) {
             ProductType.Note -> CreateWalletTangemNote()
             ProductType.Twins -> throw UnsupportedOperationException("Use the TwinCardsManager to create a wallet")
             ProductType.Wallet -> CreateWalletTangemWallet()
             else -> CreateWalletOtherCards()
         }
-        commandProcessor.proceed(card, session, callback)
+        commandProcessor.proceed(card, session) {
+            when (it) {
+                is CompletionResult.Success -> callback(CompletionResult.Success(session.environment.card!!))
+                is CompletionResult.Failure -> callback(CompletionResult.Failure(it.error))
+            }
+        }
     }
 }
 
@@ -85,12 +75,12 @@ private class CreateWalletTangemNote : ProductCommandProcessor<CreateWalletRespo
                 intersectCurves[0]
             }
             CreateWalletCommand(curve).run(session, callback)
-
         }
     }
 }
 
 private class CreateWalletTangemWallet : ProductCommandProcessor<CreateWalletResponse> {
+
     override fun proceed(
         card: Card,
         session: CardSession,
@@ -98,14 +88,35 @@ private class CreateWalletTangemWallet : ProductCommandProcessor<CreateWalletRes
     ) {
         val supportedCurves = setOf(EllipticCurve.Secp256k1, EllipticCurve.Ed25519)
         val curves = card.getCurvesForNonCreatedWallets().intersect(supportedCurves).toList()
-        CreateProductWalletsTask(curves).run(session) { result ->
+        CreateWalletsTask(curves).run(session) { result ->
             when (result) {
-                is CompletionResult.Success ->
-                    callback(CompletionResult.Success(result.data.createWalletResponses[0]))
+                is CompletionResult.Success -> {
+                    val response = result.data.createWalletResponses[0]
+                    val derivationPaths = listOf(Blockchain.Bitcoin, Blockchain.Ethereum)
+                        .mapNotNull { it.derivationPath() }
+
+                    DeriveWalletPublicKeysTask(response.wallet.publicKey, derivationPaths).run(session) {
+                        when (it) {
+                            is CompletionResult.Success -> {
+                                updateDerivedKeys(response.wallet, it.data)
+                                callback(CompletionResult.Success(response))
+                            }
+                            is CompletionResult.Failure -> callback(CompletionResult.Failure(it.error))
+                        }
+                    }
+                }
                 is CompletionResult.Failure -> callback(CompletionResult.Failure(result.error))
             }
         }
     }
+}
+
+//TODO: updating derived wallet public keys - make it better later
+private fun updateDerivedKeys(wallet: CardWallet, derivedKeys: ExtendedPublicKeyList) {
+    val onboardingManager = store.state.globalState.onboardingManager ?: return
+    onboardingManager.scanResponse = onboardingManager.scanResponse.copy(
+        derivedKeys = mapOf(wallet.publicKey.toMapKey() to derivedKeys)
+    )
 }
 
 private class CreateWalletOtherCards : ProductCommandProcessor<CreateWalletResponse> {
@@ -117,9 +128,9 @@ private class CreateWalletOtherCards : ProductCommandProcessor<CreateWalletRespo
     ) {
         val firmwareVersion = card.firmwareVersion
         val task = if (firmwareVersion < FirmwareVersion.MultiWalletAvailable) {
-            CreateProductWalletsTask(listOf(card.supportedCurves.first()))
+            CreateWalletsTask(listOf(card.supportedCurves.first()))
         } else {
-            CreateProductWalletsTask(card.getCurvesForNonCreatedWallets())
+            CreateWalletsTask(card.getCurvesForNonCreatedWallets())
         }
 
         task.run(session) { result ->
