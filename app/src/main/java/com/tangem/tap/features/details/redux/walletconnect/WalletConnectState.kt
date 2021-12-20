5a4e83db881a6b6cf3466f7e7eb8a8a4cb92297a
Tangem <git@tangem.com>
2021-12-20 23:16:27 +0300

    Updated on 2026-09-01

--- app/src/main/java/com/tangem/tap/features/details/redux/walletconnect/WalletConnectState.kt
+++ app/src/main/java/com/tangem/tap/features/details/redux/walletconnect/WalletConnectState.kt
@@ -1,10 +1,12 @@
 package com.tangem.tap.features.details.redux.walletconnect
 
 import com.squareup.moshi.JsonClass
+import com.tangem.blockchain.common.Blockchain
 import com.tangem.blockchain.common.TransactionData
 import com.tangem.blockchain.common.WalletManager
 import com.tangem.common.hdWallet.DerivationPath
 import com.tangem.tap.common.redux.StateDialog
+import com.tangem.tap.domain.tasks.product.ScanResponse
 import com.tangem.tap.features.details.ui.walletconnect.dialogs.PersonalSignDialogData
 import com.tangem.tap.features.details.ui.walletconnect.dialogs.TransactionRequestDialogData
 import com.trustwallet.walletconnect.models.WCPeerMeta
@@ -13,6 +15,7 @@ import com.trustwallet.walletconnect.models.session.WCSession
 data class WalletConnectState(
     val loading: Boolean = false,
     val sessions: List<WalletConnectSession> = listOf(),
+    val scanResponse: ScanResponse? = null
 )
 
 data class WalletConnectSession(
@@ -21,18 +24,58 @@ data class WalletConnectSession(
     val wallet: WalletForSession,
     val session: WCSession,
     val peerMeta: WCPeerMeta,
-)
+) {
+    fun getAddress(): String? {
+        val key = wallet.derivedPublicKey ?: wallet.walletPublicKey ?: return null
+        return wallet.blockchain?.makeAddresses(key)?.first()?.value
+    }
+}
 
 @JsonClass(generateAdapter = true)
 data class WalletForSession(
     val cardId: String,
-    val walletPublicKey: ByteArray,
+    val walletPublicKey: ByteArray?,
     val derivedPublicKey: ByteArray?,
     val derivationPath: DerivationPath?,
     val isTestNet: Boolean = false,
+    val blockchain: Blockchain? = if (isTestNet) Blockchain.EthereumTestnet else Blockchain.Ethereum
 ) {
-    val chainId
-        get() = if (isTestNet) 4 else 1
+
+    fun getBlockchainForSession(): Blockchain {
+        return blockchain ?: if (isTestNet) Blockchain.EthereumTestnet else Blockchain.Ethereum
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as WalletForSession
+
+        if (cardId != other.cardId) return false
+        if (walletPublicKey != null) {
+            if (other.walletPublicKey == null) return false
+            if (!walletPublicKey.contentEquals(other.walletPublicKey)) return false
+        } else if (other.walletPublicKey != null) return false
+        if (derivedPublicKey != null) {
+            if (other.derivedPublicKey == null) return false
+            if (!derivedPublicKey.contentEquals(other.derivedPublicKey)) return false
+        } else if (other.derivedPublicKey != null) return false
+        if (derivationPath != other.derivationPath) return false
+        if (isTestNet != other.isTestNet) return false
+        if (blockchain != other.blockchain) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = cardId.hashCode()
+        result = 31 * result + (walletPublicKey?.contentHashCode() ?: 0)
+        result = 31 * result + (derivedPublicKey?.contentHashCode() ?: 0)
+        result = 31 * result + (derivationPath?.hashCode() ?: 0)
+        result = 31 * result + isTestNet.hashCode()
+        result = 31 * result + (blockchain?.hashCode() ?: 0)
+        return result
+    }
 }
 
 
