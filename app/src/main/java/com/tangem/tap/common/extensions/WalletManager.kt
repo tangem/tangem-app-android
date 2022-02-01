2be367c49ea08dbd96ca87d3099083431a8a718f
Tangem <git@tangem.com>
2022-02-01 18:26:33 +0300

    Updated on 2026-06-27

--- app/src/main/java/com/tangem/tap/common/extensions/WalletManager.kt
+++ app/src/main/java/com/tangem/tap/common/extensions/WalletManager.kt
@@ -1,11 +1,11 @@
 package com.tangem.tap.common.extensions
 
+import com.tangem.blockchain.common.BlockchainSdkError
 import com.tangem.blockchain.common.Wallet
 import com.tangem.blockchain.common.WalletManager
 import com.tangem.common.services.Result
 import com.tangem.tap.domain.TapError
 import com.tangem.tap.domain.extensions.amountToCreateAccount
-import com.tangem.tap.domain.extensions.isNoAccountError
 import com.tangem.tap.domain.getFirstToken
 import com.tangem.tap.domain.topup.TradeCryptoHelper
 import com.tangem.tap.features.wallet.redux.AddressData
@@ -26,7 +26,7 @@ suspend fun WalletManager.safeUpdate(): Result<Wallet> = try {
         val blockchain = wallet.blockchain
         val amountToCreateAccount = blockchain.amountToCreateAccount(wallet.getFirstToken())
 
-        if (blockchain.isNoAccountError(exception) && amountToCreateAccount != null) {
+        if (exception is BlockchainSdkError.AccountNotFound && amountToCreateAccount != null) {
             Result.Failure(TapError.WalletManagerUpdate.NoAccountError(amountToCreateAccount.toString()))
         } else {
             val message = exception.localizedMessage ?: "An error has occurred. Try later"
