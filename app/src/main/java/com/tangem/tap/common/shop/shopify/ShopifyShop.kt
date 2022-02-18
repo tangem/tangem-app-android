96157214093d416554dd332b16f38137bfe7d4bb
Tangem <git@tangem.com>
2022-02-18 14:54:55 +0300

    Updated on 2026-02-02

--- app/src/main/java/com/tangem/tap/common/shop/shopify/ShopifyShop.kt
+++ app/src/main/java/com/tangem/tap/common/shop/shopify/ShopifyShop.kt
@@ -1,5 +1,8 @@
 package com.tangem.tap.common.shop.shopify
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class ShopifyShop(
     val domain: String,
     val storefrontApiKey: String,
