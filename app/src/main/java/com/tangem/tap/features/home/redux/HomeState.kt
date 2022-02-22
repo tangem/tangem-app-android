d40b7fad65790d3b72c3be170c8fc04ebbb149c5
Tangem <git@tangem.com>
2022-02-22 23:49:06 +0300

    Updated on 2026-02-25

--- app/src/main/java/com/tangem/tap/features/home/redux/HomeState.kt
+++ app/src/main/java/com/tangem/tap/features/home/redux/HomeState.kt
@@ -1,12 +1,10 @@
 package com.tangem.tap.features.home.redux
 
 import com.tangem.tap.common.entities.IndeterminateProgressButton
-import com.tangem.tap.common.redux.navigation.FragmentShareTransition
 import com.tangem.tap.features.send.redux.states.ButtonState
 import org.rekotlin.StateType
 
 data class HomeState(
     val shouldScanCardOnResume: Boolean = false,
-    val shareTransition: FragmentShareTransition? = null,
     val btnScanState: IndeterminateProgressButton = IndeterminateProgressButton(ButtonState.ENABLED),
 ) : StateType
