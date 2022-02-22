2be0d7560858d0be8e4d87ee71bf35753ea38b38
Tangem <git@tangem.com>
2022-02-22 23:49:06 +0300

    Updated on 2026-09-14

--- app/src/main/java/com/tangem/tap/features/home/redux/HomeAction.kt
+++ app/src/main/java/com/tangem/tap/features/home/redux/HomeAction.kt
@@ -1,7 +1,6 @@
 package com.tangem.tap.features.home.redux
 
 import com.tangem.tap.common.entities.IndeterminateProgressButton
-import com.tangem.tap.common.redux.navigation.FragmentShareTransition
 import org.rekotlin.Action
 
 sealed class HomeAction : Action {
@@ -10,10 +9,9 @@ sealed class HomeAction : Action {
     object GoToShop : HomeAction()
 
     // internal
-    data class ShouldScanCardOnResume(val shouldScanCard: Boolean):HomeAction()
+    data class ShouldScanCardOnResume(val shouldScanCard: Boolean) : HomeAction()
     object Init : HomeAction()
 
-    data class SetFragmentShareTransition(val shareTransition: FragmentShareTransition?) : HomeAction()
     data class SetTermsOfUseState(val isDisclaimerAccepted: Boolean) : HomeAction()
     data class ChangeScanCardButtonState(val state: IndeterminateProgressButton) : HomeAction()
 
