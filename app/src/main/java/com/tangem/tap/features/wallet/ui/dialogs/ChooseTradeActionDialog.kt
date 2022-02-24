9a2a67110ec963d89d6a57aaa5f00a8c5dd69d85
Tangem <git@tangem.com>
2022-02-24 13:43:33 +0300

    Updated on 2026-09-01

--- app/src/main/java/com/tangem/tap/features/wallet/ui/dialogs/ChooseTradeActionDialog.kt
+++ app/src/main/java/com/tangem/tap/features/wallet/ui/dialogs/ChooseTradeActionDialog.kt
@@ -1,30 +1,36 @@
 package com.tangem.tap.features.wallet.ui.dialogs
 
 import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.tangem.tap.features.wallet.redux.WalletAction
 import com.tangem.tap.store
-import com.tangem.wallet.R
-import kotlinx.android.synthetic.main.dialog_wallet_trade.*
+import com.tangem.wallet.databinding.DialogWalletTradeBinding
 
 class ChooseTradeActionDialog(context: Context) : BottomSheetDialog(context) {
 
-    init {
-        this.setContentView(R.layout.dialog_wallet_trade)
+    var binding: DialogWalletTradeBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DialogWalletTradeBinding.inflate(LayoutInflater.from(context))
+        setContentView(binding!!.root)
     }
 
     override fun show() {
         super.show()
 
         this.setOnDismissListener {
+            binding = null
             store.dispatch(WalletAction.HideDialog)
         }
 
-        dialog_btn_buy.setOnClickListener {
+        binding!!.dialogBtnBuy.setOnClickListener {
             store.dispatch(WalletAction.TradeCryptoAction.Buy)
             dismiss()
         }
-        dialog_btn_sell.setOnClickListener {
+        binding!!.dialogBtnSell.setOnClickListener {
             store.dispatch(WalletAction.TradeCryptoAction.Sell)
             dismiss()
         }
