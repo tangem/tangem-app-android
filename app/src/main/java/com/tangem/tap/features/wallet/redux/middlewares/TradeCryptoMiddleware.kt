b0f72dc886326ba549c09a6b9cd499f5ee65d0c4
Tangem <git@tangem.com>
2022-03-03 09:38:35 +0300

    Updated on 2026-05-13

--- app/src/main/java/com/tangem/tap/features/wallet/redux/middlewares/TradeCryptoMiddleware.kt
+++ app/src/main/java/com/tangem/tap/features/wallet/redux/middlewares/TradeCryptoMiddleware.kt
@@ -6,17 +6,16 @@ import com.tangem.tap.common.extensions.dispatchOnMain
 import com.tangem.tap.common.redux.AppState
 import com.tangem.tap.common.redux.navigation.AppScreen
 import com.tangem.tap.common.redux.navigation.NavigationAction
-import com.tangem.tap.domain.topup.TopUpManager
-import com.tangem.tap.domain.topup.TradeCryptoHelper
 import com.tangem.tap.features.demo.DemoHelper
 import com.tangem.tap.features.send.redux.PrepareSendScreen
 import com.tangem.tap.features.send.redux.SendAction
 import com.tangem.tap.features.wallet.redux.Currency
 import com.tangem.tap.features.wallet.redux.WalletAction
+import com.tangem.tap.network.exchangeServices.CurrencyExchangeManager
+import com.tangem.tap.network.exchangeServices.buyErc20Tokens
 import com.tangem.tap.scope
 import com.tangem.tap.store
 import kotlinx.coroutines.launch
-import timber.log.Timber
 
 
 class TradeCryptoMiddleware {
@@ -33,44 +32,38 @@ class TradeCryptoMiddleware {
 
     private fun startExchange(action: WalletAction.TradeCryptoAction) {
         val selectedWalletData = store.state.walletState.getSelectedWalletData()
-        val config = store.state.globalState.configManager?.config ?: return
+        val exchangeManager = store.state.globalState.currencyExchangeManager ?: return
         val addresses = selectedWalletData?.walletAddresses ?: return
         if (addresses.list.isEmpty()) return
+        val appCurrency = store.state.globalState.appCurrency
 
         val defaultAddress = addresses.list[0].address
         val currency = selectedWalletData.currency
-        val currencySymbol = selectedWalletData.currency?.currencySymbol ?: return
+        val currencySymbol = selectedWalletData.currency.currencySymbol
 
         val exchangeAction = if (action is WalletAction.TradeCryptoAction.Buy) {
-            TradeCryptoHelper.Action.Buy
+            CurrencyExchangeManager.Action.Buy
         } else {
-            TradeCryptoHelper.Action.Sell
+            CurrencyExchangeManager.Action.Sell
         }
 
-        if (exchangeAction == TradeCryptoHelper.Action.Buy &&
+        if (exchangeAction == CurrencyExchangeManager.Action.Buy &&
             currency is Currency.Token && currency.blockchain.isTestnet()
         ) {
             val walletManager = store.state.walletState.getWalletManager(currency.token)
             if (walletManager !is EthereumWalletManager) return
 
-            scope.launch {
-                TopUpManager().topUpTestErc20Tokens(
-                    walletManager = walletManager, token = currency.token
-                )
-            }
+            scope.launch { exchangeManager.buyErc20Tokens(walletManager, currency.token) }
             return
         }
 
-        val url = TradeCryptoHelper.getUrl(
+        exchangeManager.getUrl(
             action = exchangeAction,
-            blockchain = currency?.blockchain,
+            blockchain = currency.blockchain,
             cryptoCurrencyName = currencySymbol,
-            walletAddress = defaultAddress,
-            apiKey = config.moonPayApiKey,
-            secretKey = config.moonPayApiSecretKey
-        )
-        Timber.d("Moonpay $exchangeAction URL: $url")
-        store.dispatchOnMain(NavigationAction.OpenUrl(url))
+            fiatCurrency = appCurrency,
+            walletAddress = defaultAddress)?.let { store.dispatchOnMain(NavigationAction.OpenUrl(it)) }
+
     }
 
     private fun preconfigureAndOpenSendScreen(action: WalletAction.TradeCryptoAction.SendCrypto) {
@@ -91,8 +84,11 @@ class TradeCryptoMiddleware {
     }
 
     private fun openReceiptUrl(transactionId: String) {
+        val exchangeManager = store.state.globalState.currencyExchangeManager ?: return
+
         store.dispatchOnMain(NavigationAction.PopBackTo())
-        val url = TradeCryptoHelper.getSellCryptoReceiptUrl(transactionId)
-        store.dispatchOnMain(NavigationAction.OpenUrl(url))
+        exchangeManager.getSellCryptoReceiptUrl(CurrencyExchangeManager.Action.Sell, transactionId)?.let {
+            store.dispatchOnMain(NavigationAction.OpenUrl(it))
+        }
     }
 }
