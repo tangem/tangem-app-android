c845e0952ad4e4f20c2c3a1103e176bf52e4432a
Tangem <git@tangem.com>
2022-03-17 23:13:05 +0300

    Updated on 2026-09-21

--- app/src/main/java/com/tangem/tap/features/wallet/redux/WalletAction.kt
+++ app/src/main/java/com/tangem/tap/features/wallet/redux/WalletAction.kt
@@ -74,12 +74,6 @@ sealed class WalletAction : Action {
         class CheckRemainingSignatures(val remainingSignatures: Int?) : Warnings()
     }
 
-    data class UpdateWallet(val blockchain: Blockchain? = null) : WalletAction() {
-        object ScheduleUpdatingWallet : WalletAction()
-        data class Success(val wallet: Wallet) : WalletAction()
-        data class Failure(val errorMessage: String? = null) : WalletAction()
-    }
-
     data class LoadFiatRate(
         val wallet: Wallet? = null, val currency: Currency? = null,
     ) : WalletAction() {
