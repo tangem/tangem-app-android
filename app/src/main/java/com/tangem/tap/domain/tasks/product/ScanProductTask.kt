d0a119968e61a9e706d69ca20aaceeffef9e5d11
Tangem <git@tangem.com>
2022-04-07 13:32:11 +0300

    Updated on 2026-07-23

--- app/src/main/java/com/tangem/tap/domain/tasks/product/ScanProductTask.kt
+++ app/src/main/java/com/tangem/tap/domain/tasks/product/ScanProductTask.kt
@@ -1,12 +1,10 @@
 package com.tangem.tap.domain.tasks.product
 
 import com.tangem.blockchain.common.Blockchain
-import com.tangem.blockchain.common.Token
 import com.tangem.common.CompletionResult
 import com.tangem.common.card.Card
 import com.tangem.common.card.EllipticCurve
 import com.tangem.common.card.FirmwareVersion
-import com.tangem.common.card.WalletData
 import com.tangem.common.core.CardSession
 import com.tangem.common.core.CardSessionRunnable
 import com.tangem.common.core.TangemError
@@ -16,66 +14,21 @@ import com.tangem.common.extensions.guard
 import com.tangem.common.extensions.toHexString
 import com.tangem.common.extensions.toMapKey
 import com.tangem.common.hdWallet.DerivationPath
-import com.tangem.operations.CommandResponse
+import com.tangem.domain.common.*
+import com.tangem.domain.common.TapWorkarounds.isExcluded
+import com.tangem.domain.common.TapWorkarounds.isNotSupportedInThatRelease
 import com.tangem.operations.PreflightReadMode
 import com.tangem.operations.PreflightReadTask
 import com.tangem.operations.ScanTask
 import com.tangem.operations.backup.PrimaryCard
 import com.tangem.operations.backup.StartPrimaryCardLinkingTask
 import com.tangem.operations.derivation.DeriveMultipleWalletPublicKeysTask
-import com.tangem.operations.derivation.ExtendedPublicKeysMap
 import com.tangem.operations.issuerAndUserData.ReadIssuerDataCommand
-import com.tangem.tap.domain.ProductType
 import com.tangem.tap.domain.TapSdkError
-import com.tangem.tap.domain.TapWorkarounds
-import com.tangem.tap.domain.TapWorkarounds.getTangemNoteBlockchain
-import com.tangem.tap.domain.TapWorkarounds.isExcluded
-import com.tangem.tap.domain.TapWorkarounds.isNotSupportedInThatRelease
 import com.tangem.tap.domain.extensions.getPrimaryCurve
 import com.tangem.tap.domain.extensions.getSingleWallet
 import com.tangem.tap.domain.tokens.CurrenciesRepository
-import com.tangem.tap.domain.twins.TwinsHelper
 import com.tangem.tap.preferencesStorage
-
-data class ScanResponse(
-    val card: Card,
-    val productType: ProductType,
-    val walletData: WalletData?,
-    val secondTwinPublicKey: String? = null,
-    val derivedKeys: Map<KeyWalletPublicKey, ExtendedPublicKeysMap> = mapOf(),
-    val primaryCard: PrimaryCard? = null
-) : CommandResponse {
-
-    fun getBlockchain(): Blockchain {
-        if (productType == ProductType.Note) return getTangemNoteBlockchain(card)
-            ?: return Blockchain.Unknown
-        val blockchainName: String = walletData?.blockchain ?: return Blockchain.Unknown
-        return Blockchain.fromId(blockchainName)
-    }
-
-    fun getPrimaryToken(): Token? {
-        val cardToken = walletData?.token ?: return null
-        return Token(
-            cardToken.name,
-            cardToken.symbol,
-            cardToken.contractAddress,
-            cardToken.decimals,
-            Blockchain.fromId(walletData.blockchain)
-        )
-    }
-
-    fun isTangemNote(): Boolean = productType == ProductType.Note
-    fun isTangemWallet(): Boolean = productType == ProductType.Wallet
-    fun isTangemTwins(): Boolean = productType == ProductType.Twins
-
-    fun supportsHdWallet(): Boolean = card.settings.isHDWalletAllowed
-    fun supportsBackup(): Boolean = card.settings.isBackupAllowed
-
-    fun twinsIsTwinned(): Boolean =
-        card.isTangemTwins() && walletData != null && secondTwinPublicKey != null
-}
-
-typealias KeyWalletPublicKey = ByteArrayKey
 
 class ScanProductTask(
     val card: Card? = null,
@@ -131,8 +84,6 @@ class ScanProductTask(
         return null
     }
 }
-
-private fun Card.isTangemTwins(): Boolean = TwinsHelper.getTwinCardNumber(cardId) != null
 
 private class ScanNoteProcessor : ProductCommandProcessor<ScanResponse> {
     override fun proceed(
