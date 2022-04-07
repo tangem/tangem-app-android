1e2517a68409df0012ae902167a0317c343e53e2
Tangem <git@tangem.com>
2022-04-07 13:02:03 +0300

    Updated on 2026-09-25

--- app/src/main/java/com/tangem/tap/common/compose/OutlinedTextFieldWidget.kt
+++ app/src/main/java/com/tangem/tap/common/compose/OutlinedTextFieldWidget.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -34,6 +35,7 @@ fun OutlinedTextFieldWidget(
     error: DomainError? = null,
     errorConverter: ErrorConverter<String>? = null,
     debounceTextChanges: Long = 400,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
     onTextChanged: (String) -> Unit,
 ) {
     val placeholder = placeholderId?.let { stringResource(id = it) } ?: placeholder
@@ -57,6 +59,7 @@ fun OutlinedTextFieldWidget(
             singleLine = true,
             enabled = isEnabled,
             isError = error != null,
+            visualTransformation = visualTransformation,
         )
         errorConverter?.let { TextFieldErrorWidget(error, it) }
     }
