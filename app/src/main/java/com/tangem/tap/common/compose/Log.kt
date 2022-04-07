5945fdd9c70ebc105ed781387bbec463ee5e620f
Tangem <git@tangem.com>
2022-04-07 13:02:03 +0300

    Updated on 2026-02-11

---
+++ app/src/main/java/com/tangem/tap/common/compose/Log.kt
@@ -0,0 +1,12 @@
+package com.tangem.tap.common.compose
+
+import androidx.compose.runtime.Composable
+import timber.log.Timber
+
+/**
+ * Simple logger for all recompositions
+ */
+@Composable
+fun LogSideEffect(message: String) {
+    Timber.w("SideEffect: $message")
+}
