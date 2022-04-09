1dc67769d743b102f0ac5f098ed148696979fbbd
Tangem <git@tangem.com>
2022-04-09 14:25:04 +0300

    Updated on 2026-07-01

--- app/src/main/java/com/tangem/tap/common/extensions/compose/Color.kt
+++ app/src/main/java/com/tangem/tap/common/extensions/compose/Color.kt
@@ -10,7 +10,11 @@ import androidx.core.graphics.red
 /**
 [REDACTED_AUTHOR]
  */
-fun Color.argb(): Int {
+fun Color.toAndroidGraphicsColor(): Int {
     val argb = this.toArgb()
     return android.graphics.Color.argb(argb.alpha, argb.red, argb.green, argb.blue)
+}
+
+fun Color.parse(hexColor: String): Color {
+    return Color(hexColor.removePrefix("#").toInt(16))
 }
