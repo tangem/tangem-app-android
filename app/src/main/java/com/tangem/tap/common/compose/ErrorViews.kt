314b33a0378f6b9cf12dee1d2018eb5b26703f24
Tangem <git@tangem.com>
2022-04-12 16:51:32 +0300

    Updated on 2026-08-19

--- app/src/main/java/com/tangem/tap/common/compose/ErrorViews.kt
+++ app/src/main/java/com/tangem/tap/common/compose/ErrorViews.kt
@@ -1,11 +1,16 @@
 package com.tangem.tap.common.compose
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 /**
 [REDACTED_AUTHOR]
@@ -22,4 +27,15 @@ fun ErrorView(
         modifier = modifier,
         style = style
     )
+}
+
+@Preview
+@Composable
+fun ErrorViewTest() {
+    Scaffold(
+    ) {
+        Box(Modifier.padding(16.dp)) {
+            ErrorView(text = "Some error description")
+        }
+    }
 }
