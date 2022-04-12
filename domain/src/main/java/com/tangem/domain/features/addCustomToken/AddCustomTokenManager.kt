51737fd384d0806388bbfd775cd04edd0d6f2b5e
Tangem <git@tangem.com>
2022-04-12 16:12:00 +0300

    Updated on 2026-09-23

--- domain/src/main/java/com/tangem/domain/features/addCustomToken/AddCustomTokenManager.kt
+++ domain/src/main/java/com/tangem/domain/features/addCustomToken/AddCustomTokenManager.kt
@@ -1,9 +1,9 @@
 package com.tangem.domain.features.addCustomToken
 
 import com.tangem.common.services.Result
-import com.tangem.network.api.tangemTech.CoinsCheckAddressResponse
-import com.tangem.network.api.tangemTech.TangemAuthInterceptor
+import com.tangem.network.api.tangemTech.Coins
 import com.tangem.network.api.tangemTech.TangemTechService
+import com.tangem.network.common.AddHeaderInterceptor
 
 /**
 [REDACTED_AUTHOR]
@@ -12,20 +12,52 @@ class AddCustomTokenManager(
     private val tangemTechService: TangemTechService
 ) {
 
-    suspend fun findContractAddress(
+    suspend fun checkAddress(
         contractAddress: String,
         networkId: String? = null
-    ): List<CoinsCheckAddressResponse.Token> {
-        val result = tangemTechService.coinsCheckAddress(contractAddress, networkId)
+    ): List<Coins.CheckAddressResponse.Token> {
+        val result = tangemTechService.coins.checkAddress(contractAddress, networkId)
         return when (result) {
             is Result.Success -> {
-                result.data.tokens
+                val resultTokens = result.data.tokens
+                val newTokensList = mutableListOf<Coins.CheckAddressResponse.Token>()
+                resultTokens.forEach {
+                    val contractsWithTheSameAddress = it.contracts.filter { it.address == contractAddress }
+                    if (contractsWithTheSameAddress.isNotEmpty()) {
+                        val newToken = it.copy(contracts = contractsWithTheSameAddress)
+                        newTokensList.add(newToken)
+                    }
+                }
+                when {
+                    // https://tangem.slack.com/archives/GMXC6PP71/p1649672562078679
+                    newTokensList.size > 1 -> listOf(newTokensList[0])
+                    else -> newTokensList
+                }
+            }
+            is Result.Failure -> emptyList()
+        }
+    }
+
+    suspend fun tokens(): List<Coins.TokensResponse.Token> {
+        val result = tangemTechService.coins.tokens()
+        return when (result) {
+            is Result.Success -> {
+                val currencies = result.data.tokens
+                currencies.filter {
+                    it.contracts.isNullOrEmpty()
+                }
             }
             is Result.Failure -> emptyList()
         }
     }
 
     fun attachAuthKey(authKey: String) {
-        tangemTechService.addHeaderInterceptors(listOf(TangemAuthInterceptor(authKey)))
+        tangemTechService.addHeaderInterceptors(listOf(
+            CardPublicKeyHttpInterceptor(authKey),
+        ))
     }
 }
+
+private class CardPublicKeyHttpInterceptor(cardPublicKeyHex: String) : AddHeaderInterceptor(mapOf(
+    "card_public_key" to cardPublicKeyHex,
+))
