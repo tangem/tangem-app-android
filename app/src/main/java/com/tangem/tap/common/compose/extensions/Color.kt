ea7fc0320065e66e6a78b3003a2d899b186bd046
Tangem <git@tangem.com>
2022-04-12 16:51:32 +0300

    Updated on 2026-09-01

--- app/src/main/java/com/tangem/tap/common/compose/extensions/Color.kt
+++ app/src/main/java/com/tangem/tap/common/compose/extensions/Color.kt
@@ -1,4 +1,4 @@
-package com.tangem.tap.common.extensions.compose
+package com.tangem.tap.common.compose.extensions
 
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
