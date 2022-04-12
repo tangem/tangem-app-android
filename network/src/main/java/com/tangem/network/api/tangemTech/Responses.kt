2e4f34cd3217460192f1fb586137de7b22934d79
Tangem <git@tangem.com>
2022-04-12 16:12:00 +0300

    Updated on 2026-08-07

--- network/src/main/java/com/tangem/network/api/tangemTech/Responses.kt
+++ network/src/main/java/com/tangem/network/api/tangemTech/Responses.kt
@@ -5,46 +5,57 @@ import java.math.BigDecimal
 /**
 [REDACTED_AUTHOR]
  */
-data class CoinsPricesResponse(
-    val prices: List<CoinPrice>
-)
+interface HttpResponse
+interface TangemTechResponse : HttpResponse
 
-data class CoinPrice(
-    val name: String,
-    val price: BigDecimal,
-)
+sealed class Coins : TangemTechResponse {
+    data class PricesResponse(val prices: List<Price>) : Coins() {
+        data class Price(
+            val name: String,
+            val price: BigDecimal,
+        )
+    }
 
-data class CoinsCheckAddressResponse(
-    val imageHost: String,
-    val tokens: List<Token>,
-    val total: Int,
-) {
-    data class Token(
-        val id: String,
-        val name: String,
-        val symbol: String,
-        val active: Boolean,
-        val contracts: List<Contract>
-    ) {
-        data class Contract(
-            val networkId: String,
-            val address: String,
-            val decimalCount: BigDecimal?,
-            val active: Boolean
+    data class CheckAddressResponse(val imageHost: String, val tokens: List<Token>, val total: Int) : Coins() {
+        data class Token(
+            val id: String,
+            val name: String,
+            val symbol: String,
+            val active: Boolean,
+            val contracts: List<Contract>
+        ) {
+            data class Contract(
+                val networkId: String,
+                val address: String,
+                val decimalCount: BigDecimal?,
+                val active: Boolean
+            )
+        }
+    }
+
+    data class TokensResponse(val imageHost: String, val tokens: List<Token>, val total: Int) : Coins() {
+        data class Token(
+            val id: String,
+            val name: String,
+            val symbol: String,
+            val contracts: List<Contract>?
+        ) {
+            data class Contract(
+                val networkId: String,
+                val address: String,
+                val decimalCount: BigDecimal?,
+            )
+        }
+    }
+
+    data class CurrenciesResponse(val currencies: List<Currency>) {
+        data class Currency(
+            val id: String,
+            val code: String,
+            val name: String,
+            val rateBTC: String,
+            val unit: String,
+            val type: String,
         )
     }
 }
-
-data class CoinsCurrenciesResponse(
-    val currencies: List<Currency>,
-) {
-    data class Currency(
-        val id: String,
-        val code: String,
-        val name: String,
-        val rateBTC: String,
-        val unit: String,
-        val type: String,
-    )
-}
-
