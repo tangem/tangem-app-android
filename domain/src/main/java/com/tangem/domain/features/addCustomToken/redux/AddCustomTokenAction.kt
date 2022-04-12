2f851acafa2c8876443292ee5eb9943005389783
Tangem <git@tangem.com>
2022-04-12 16:17:06 +0300

    Updated on 2026-01-20

--- domain/src/main/java/com/tangem/domain/features/addCustomToken/redux/AddCustomTokenAction.kt
+++ domain/src/main/java/com/tangem/domain/features/addCustomToken/redux/AddCustomTokenAction.kt
@@ -1,34 +1,33 @@
 package com.tangem.domain.features.addCustomToken.redux
 
 import com.tangem.blockchain.common.Blockchain
+import com.tangem.domain.common.form.Field
 import com.tangem.domain.common.form.FieldId
 import com.tangem.domain.features.addCustomToken.AddCustomTokenError
 import com.tangem.domain.features.addCustomToken.AddCustomTokenWarning
 import com.tangem.domain.features.addCustomToken.CustomTokenFieldId
-import com.tangem.network.api.tangemTech.CoinsCheckAddressResponse
+import com.tangem.network.api.tangemTech.Coins
 import org.rekotlin.Action
 
 /**
 [REDACTED_AUTHOR]
  */
 sealed class AddCustomTokenAction : Action {
-    // initializing actions
-    data class SetTangemTechAuthHeader(val cardPublicKeyHex: String) : AddCustomTokenAction()
-
-
     // from user, ui
-    object OnBackPressed : AddCustomTokenAction()
-    data class OnTokenContractAddressChanged(val value: String) : AddCustomTokenAction()
-    data class OnTokenNetworkChanged(val value: Blockchain) : AddCustomTokenAction()
-    data class OnTokenDerivationPathChanged(val value: String) : AddCustomTokenAction()
-    data class OnTokenFieldChanged(val id: FieldId, val value: String) : AddCustomTokenAction()
+    object OnCreate : AddCustomTokenAction()
+    object OnDestroy : AddCustomTokenAction()
+    data class OnTokenFieldChanged(val id: FieldId, val value: Field.Data<String>) : AddCustomTokenAction()
+    data class OnTokenContractAddressChanged(val value: Field.Data<String>) : AddCustomTokenAction()
+    data class OnTokenNetworkChanged(val value: Field.Data<Blockchain>) : AddCustomTokenAction()
+    data class OnTokenDerivationPathChanged(val value: Field.Data<Blockchain>) : AddCustomTokenAction()
+    data class OnTokenDecimalsChanged(val value: Field.Data<String>) : AddCustomTokenAction()
+    data class OnCustomTokenSelected(val any: Any = Unit) : AddCustomTokenAction()
 
 
-    // from redux
-    object UpdateForm : AddCustomTokenAction()
+    data class UpdateForm(val state: AddCustomTokenState) : AddCustomTokenAction()
     data class FillTokenFields(
-        val token: CoinsCheckAddressResponse.Token,
-        val contract: CoinsCheckAddressResponse.Token.Contract,
+        val token: Coins.CheckAddressResponse.Token,
+        val contract: Coins.CheckAddressResponse.Token.Contract,
     ) : AddCustomTokenAction()
 
     sealed class Error : AddCustomTokenAction() {
@@ -37,7 +36,14 @@ sealed class AddCustomTokenAction : Action {
     }
 
     sealed class Warning : AddCustomTokenAction() {
-        data class Add(val warning: AddCustomTokenWarning) : Warning()
-        data class Remove(val warning: AddCustomTokenWarning) : Warning()
+        data class Add(val warnings: Set<AddCustomTokenWarning>) : Warning()
+        data class Remove(val warnings: Set<AddCustomTokenWarning>) : Warning()
+        data class Replace(val remove: Set<AddCustomTokenWarning>, val add: Set<AddCustomTokenWarning>) : Warning()
+    }
+
+    // To change the screenState
+    sealed class Screen : AddCustomTokenAction() {
+        data class UpdateTokenFields(val pairs: List<Pair<FieldId, ViewStates.TokenField>>) : Screen()
+        data class UpdateAddButton(val addButton: ViewStates.AddButton) : Screen()
     }
 }
