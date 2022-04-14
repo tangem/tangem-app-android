74c7e47e6b30481327c6f3cb0f5cb5795d422efa
Tangem <git@tangem.com>
2022-04-14 17:28:33 +0300

    Updated on 2026-01-20

--- app/src/main/java/com/tangem/tap/features/tokens/addCustomToken/compose/HangingOverKeyboardView.kt
+++ app/src/main/java/com/tangem/tap/features/tokens/addCustomToken/compose/HangingOverKeyboardView.kt
@@ -1,7 +1,5 @@
 package com.tangem.tap.features.tokens.addCustomToken.compose
 
-import android.content.Context
-import android.util.TypedValue
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.padding
@@ -21,36 +19,19 @@ import com.tangem.tap.common.compose.Keyboard
 fun HangingOverKeyboardView(
     modifier: Modifier = Modifier,
     keyboardState: State<Keyboard>,
-    defaultBottomPadding: Dp = 0.dp,
-    spaceBetweenKeyboard: Dp = 10.dp,
-    calculateWithActionBarHeight: Boolean = true,
+    spaceBetweenKeyboard: Dp = 0.dp,
     content: @Composable() (BoxScope.() -> Unit)
 ) {
-    fun getActionBarHeight(context: Context): Int {
-        val typedValue = TypedValue()
-        return if (context.theme.resolveAttribute(android.R.attr.actionBarSize, typedValue, true)) {
-            val data = typedValue.data
-            val displayMetrics = context.resources.displayMetrics
-            TypedValue.complexToDimensionPixelSize(data, displayMetrics)
-        } else {
-            0
-        }
-    }
 
     val context = LocalContext.current
-    val calculatedPadding = when (keyboardState.value) {
-        Keyboard.Closed -> defaultBottomPadding
+    val padding = when (keyboardState.value) {
+        Keyboard.Closed -> 0.dp
         is Keyboard.Opened -> {
             val keyboardHeight = (keyboardState.value as Keyboard.Opened).height
             val keyboardPadding = context.pxToDp(keyboardHeight.toFloat()).dp
-            if (calculateWithActionBarHeight) {
-                val actionBarHeight = context.pxToDp(getActionBarHeight(context).toFloat()).dp
-                keyboardPadding + spaceBetweenKeyboard - actionBarHeight
-            } else {
-                keyboardPadding + spaceBetweenKeyboard
-            }
-
+            keyboardPadding + spaceBetweenKeyboard
         }
     }
-    Box(modifier.padding(bottom = calculatedPadding)) { content() }
+
+    Box(modifier.padding(bottom = padding)) { content() }
 }
