ca29a8b57ba629fac7b491b927cac698ef2128fd
Tangem <git@tangem.com>
2022-04-14 17:28:33 +0300

    release/5.26.1

--- domain/src/main/java/com/tangem/domain/features/addCustomToken/CompleteData.kt
+++ domain/src/main/java/com/tangem/domain/features/addCustomToken/CompleteData.kt
@@ -1,8 +1,8 @@
 package com.tangem.domain.features.addCustomToken
 
 import com.tangem.blockchain.common.Blockchain
+import com.tangem.blockchain.common.Token
 import com.tangem.domain.common.form.BaseFieldDataConverter
-import com.tangem.domain.common.form.FieldDataConverter
 import com.tangem.domain.common.form.FieldId
 
 /**
@@ -14,47 +14,44 @@ enum class CompleteDataType {
 
 sealed class CompleteData() {
 
-    companion object {
-        fun createDataConverter(completeDataType: CompleteDataType): FieldDataConverter<out CompleteData> =
-            when (completeDataType) {
-                CompleteDataType.Blockchain -> CustomBlockchain.Converter()
-                CompleteDataType.Token -> CustomToken.Converter()
-            }
-    }
-
     class CustomBlockchain(
-        val selectedNetwork: Blockchain,
+        val network: Blockchain,
         val derivationPath: String?
     ) : CompleteData() {
 
         class Converter : BaseFieldDataConverter<CustomBlockchain>() {
-            override fun getConvertedData(): CustomBlockchain = CustomBlockchain(
-                collectedData[CustomTokenFieldId.Network] as Blockchain,
-                collectedData[CustomTokenFieldId.DerivationPath] as? String,
-            )
+            override fun getConvertedData(): CustomBlockchain {
+                val network = collectedData[CustomTokenFieldId.Network] as Blockchain
+                val derivationPath = collectedData[CustomTokenFieldId.DerivationPath] as? String
+                return CustomBlockchain(network, derivationPath)
+            }
 
             override fun getIdToCollect(): List<FieldId> = listOf(CustomTokenFieldId.Network, CustomTokenFieldId.DerivationPath)
         }
     }
 
     class CustomToken(
-        val contractAddress: String,
-        val selectedNetwork: Blockchain,
-        val name: String,
-        val tokenSymbol: String,
-        val decimals: Int,
+        val token: Token,
+        val network: Blockchain,
         val derivationPath: String?,
     ) : CompleteData() {
 
-        class Converter : BaseFieldDataConverter<CustomToken>() {
-            override fun getConvertedData(): CustomToken = CustomToken(
-                collectedData[CustomTokenFieldId.ContractAddress] as String,
-                collectedData[CustomTokenFieldId.Network] as Blockchain,
-                collectedData[CustomTokenFieldId.Name] as String,
-                collectedData[CustomTokenFieldId.Symbol] as String,
-                collectedData[CustomTokenFieldId.Decimals] as Int,
-                collectedData[CustomTokenFieldId.DerivationPath] as? String,
-            )
+        class Converter(val tokenId: String?) : BaseFieldDataConverter<CustomToken>() {
+
+            override fun getConvertedData(): CustomToken {
+                val token = Token(
+                    name = collectedData[CustomTokenFieldId.Name] as String,
+                    symbol = collectedData[CustomTokenFieldId.Symbol] as String,
+                    contractAddress = collectedData[CustomTokenFieldId.ContractAddress] as String,
+                    decimals = (collectedData[CustomTokenFieldId.Decimals] as String).toInt(),
+                    id = tokenId,
+                )
+                return CustomToken(
+                    token,
+                    collectedData[CustomTokenFieldId.Network] as Blockchain,
+                    collectedData[CustomTokenFieldId.DerivationPath] as? String,
+                )
+            }
 
             override fun getIdToCollect(): List<FieldId> = CustomTokenFieldId.values().toList()
         }
