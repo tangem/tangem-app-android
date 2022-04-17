6d30e7a07066bf006f95473ac5e47af99811f498
Tangem <git@tangem.com>
2022-04-17 17:15:20 +0300

    Updated on 2026-07-07

--- domain/src/main/java/com/tangem/domain/redux/global/DomainGlobalState.kt
+++ domain/src/main/java/com/tangem/domain/redux/global/DomainGlobalState.kt
@@ -2,12 +2,19 @@ package com.tangem.domain.redux.global
 
 import com.tangem.domain.DomainDialog
 import com.tangem.domain.common.ScanResponse
+import com.tangem.network.api.tangemTech.TangemTechService
 
 /**
 [REDACTED_AUTHOR]
  */
-//TODO: refactoring: is alias for the GlobalState
 data class DomainGlobalState(
+    // there is a part of mirrors from the GlobalState
     val scanResponse: ScanResponse? = null,
+    //
+    val networkServices: NetworkServices = NetworkServices(),
     val dialog: DomainDialog? = null,
+)
+
+data class NetworkServices(
+    val tangemTechService: TangemTechService = TangemTechService()
 )
