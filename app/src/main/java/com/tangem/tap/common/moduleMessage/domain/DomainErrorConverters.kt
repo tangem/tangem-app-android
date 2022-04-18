fb74bef23448081f8d041e9e5f6de4461145a7c1
Tangem <git@tangem.com>
2022-04-18 17:13:28 +0300

    Updated on 2026-05-14

--- app/src/main/java/com/tangem/tap/common/moduleMessage/domain/DomainErrorConverters.kt
+++ app/src/main/java/com/tangem/tap/common/moduleMessage/domain/DomainErrorConverters.kt
@@ -1,10 +1,9 @@
-package com.tangem.tap.features.tokens.addCustomToken
+package com.tangem.tap.common.moduleMessage.domain
 
 import android.content.Context
+import com.tangem.common.module.ModuleMessageConverter
+import com.tangem.domain.AddCustomTokenError
 import com.tangem.domain.DomainError
-import com.tangem.domain.ErrorConverter
-import com.tangem.domain.features.addCustomToken.AddCustomTokenError
-import com.tangem.domain.features.addCustomToken.AddCustomTokenWarning
 import com.tangem.wallet.R
 
 /**
@@ -12,27 +11,23 @@ import com.tangem.wallet.R
  */
 class DomainErrorConverter(
     private val context: Context
-) : ErrorConverter<String> {
-
-    override fun convertError(error: DomainError): String {
-        val errorMessage = when (error) {
-            is AddCustomTokenError -> AddCustomTokenConverter(context).convertError(error)
-            else -> null
-        }
-        return errorMessage?.let { it } ?: "Unknown error: ${error::class.java.simpleName}"
+) : ModuleMessageConverter<DomainError, String?> {
+    override fun convert(message: DomainError): String? = when (message) {
+        is AddCustomTokenError -> AddCustomTokenConverter(context).convert(message)
+        else -> null
     }
 }
 
 private class AddCustomTokenConverter(
     private val context: Context
-) : ErrorConverter<String> {
+) : ModuleMessageConverter<DomainError, String?> {
 
-    override fun convertError(error: DomainError): String {
-        val customTokenError = (error as? AddCustomTokenError) ?: throw UnsupportedOperationException()
+    override fun convert(message: DomainError): String? {
+        val customTokenError = (message as? AddCustomTokenError) ?: throw UnsupportedOperationException()
 
         val rawMessage = when (customTokenError) {
-            AddCustomTokenWarning.PotentialScamToken -> R.string.custom_token_validation_error_not_found
-            AddCustomTokenWarning.TokenAlreadyAdded -> R.string.custom_token_validation_error_already_added
+            AddCustomTokenError.Warning.PotentialScamToken -> R.string.custom_token_validation_error_not_found
+            AddCustomTokenError.Warning.TokenAlreadyAdded -> R.string.custom_token_validation_error_already_added
             AddCustomTokenError.InvalidContractAddress -> R.string.custom_token_creation_error_invalid_contract_address
             AddCustomTokenError.NetworkIsNotSelected -> R.string.custom_token_creation_error_network_not_selected
             AddCustomTokenError.InvalidDerivationPath -> R.string.custom_token_creation_error_invalid_derivation_path
@@ -42,10 +37,11 @@ private class AddCustomTokenConverter(
             AddCustomTokenError.FieldIsEmpty -> R.string.custom_token_creation_error_required_field
             else -> null
         }
+
         return when (rawMessage) {
             is Int -> context.getString(rawMessage)
             is String -> rawMessage
-            else -> "Unknown error: ${customTokenError::class.java.simpleName}"
+            else -> null
         }
     }
 }
