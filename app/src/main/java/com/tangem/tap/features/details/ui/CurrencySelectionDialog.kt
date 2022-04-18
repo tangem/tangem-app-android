9d4f2bebe9f0d1643605e745c424c6ad02c8a734
Tangem <git@tangem.com>
2022-04-18 22:26:51 +0300

    Updated on 2026-03-25

--- app/src/main/java/com/tangem/tap/features/details/ui/CurrencySelectionDialog.kt
+++ app/src/main/java/com/tangem/tap/features/details/ui/CurrencySelectionDialog.kt
@@ -2,10 +2,10 @@ package com.tangem.tap.features.details.ui
 
 import android.content.Context
 import androidx.appcompat.app.AlertDialog
+import com.tangem.network.api.tangemTech.Coins
 import com.tangem.tap.common.extensions.toFormattedString
 import com.tangem.tap.common.redux.global.FiatCurrencyName
 import com.tangem.tap.features.details.redux.DetailsAction
-import com.tangem.tap.network.coinmarketcap.FiatCurrency
 import com.tangem.tap.store
 import com.tangem.wallet.R
 
@@ -13,11 +13,11 @@ class CurrencySelectionDialog {
 
     var dialog: AlertDialog? = null
 
-    fun show(currencies: List<FiatCurrency>, currentAppCurrency: FiatCurrencyName, context: Context) {
+    fun show(currencies: List<Coins.CurrenciesResponse.Currency>, currentAppCurrency: FiatCurrencyName, context: Context) {
 
         if (dialog == null) {
             val currenciesToShow = currencies.map { it.toFormattedString() }.toTypedArray()
-            var currentSelection = currencies.indexOfFirst { it.symbol == currentAppCurrency }
+            var currentSelection = currencies.indexOfFirst { it.code == currentAppCurrency }
 
             dialog = AlertDialog.Builder(context)
                     .setTitle(context.getString(R.string.details_row_title_currency))
@@ -26,7 +26,7 @@ class CurrencySelectionDialog {
                     }
                     .setPositiveButton(context.getString(R.string.common_done)) { _, _ ->
                         val selectedCurrency = currencies[currentSelection]
-                        store.dispatch(DetailsAction.AppCurrencyAction.SelectAppCurrency(selectedCurrency.symbol))
+                        store.dispatch(DetailsAction.AppCurrencyAction.SelectAppCurrency(selectedCurrency.code))
                     }
                     .setOnDismissListener {
                         store.dispatch(DetailsAction.AppCurrencyAction.Cancel)
