fc995dd6826e0e0100f8925db9ace3554ef43a2d
Tangem <git@tangem.com>
2022-04-21 19:38:58 +0300

    Updated on 2026-09-25

--- app/src/main/java/com/tangem/tap/persistence/FiatCurrenciesPrefStorage.kt
+++ app/src/main/java/com/tangem/tap/persistence/FiatCurrenciesPrefStorage.kt
@@ -3,7 +3,7 @@ package com.tangem.tap.persistence
 import android.content.SharedPreferences
 import androidx.core.content.edit
 import com.tangem.common.json.MoshiJsonConverter
-import com.tangem.network.api.tangemTech.Coins
+import com.tangem.network.api.tangemTech.CurrenciesResponse
 
 /**
 [REDACTED_AUTHOR]
@@ -21,14 +21,14 @@ class FiatCurrenciesPrefStorage(
         }
     }
 
-    fun save(currencies: List<Coins.CurrenciesResponse.Currency>) {
+    fun save(currencies: List<CurrenciesResponse.Currency>) {
         val json: String = converter.toJson(currencies)
         return preferences.edit().putString(FIAT_CURRENCIES_KEY, json).apply()
     }
 
-    fun restore(): List<Coins.CurrenciesResponse.Currency> {
+    fun restore(): List<CurrenciesResponse.Currency> {
         val json = preferences.getString(FIAT_CURRENCIES_KEY, "")
-        val type = converter.typedList(Coins.CurrenciesResponse.Currency::class.java)
+        val type = converter.typedList(CurrenciesResponse.Currency::class.java)
         if (json.isNullOrBlank()) return emptyList()
 
         return converter.fromJson(json, type) ?: emptyList()
