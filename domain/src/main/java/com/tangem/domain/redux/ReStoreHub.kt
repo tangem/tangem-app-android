5f8354147b79e720c95fe3a6e324e92e0b94ca38
Tangem <git@tangem.com>
2022-04-22 09:08:34 +0300

    Updated on 2026-09-25

--- domain/src/main/java/com/tangem/domain/redux/ReStoreHub.kt
+++ domain/src/main/java/com/tangem/domain/redux/ReStoreHub.kt
@@ -102,11 +102,22 @@ internal abstract class BaseStoreHub<State>(
     }
 
     protected abstract suspend fun handleAction(action: Action, storeState: DomainState, cancel: ValueCallback<Action>)
-    protected abstract fun reduceAction(action: Action, state: State): State
+
+    @Deprecated(
+        replaceWith = ReplaceWith("ReStoreReducer<T>"),
+        message = "must return a Reducer instance. The reducer must be a separate component - this closes " +
+            "access to the states that can be obtained from ReStoreHub"
+    )
+    abstract fun reduceAction(action: Action, state: State): State
+//    protected abstract fun getReducer(): ReStoreReducer<State>
 
     protected abstract fun getHubState(storeState: DomainState): State
     protected abstract fun updateStoreState(storeState: DomainState, newHubState: State): DomainState
 
+}
+
+internal interface ReStoreReducer<State> {
+    fun reduceAction(action: Action, state: State): State
 }
 
 internal suspend inline fun ReStoreHub<*, *>.dispatchOnMain(vararg actions: Action) {
