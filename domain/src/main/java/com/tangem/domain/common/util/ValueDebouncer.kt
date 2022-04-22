a32e5d6527a1b8a78fdfb39429bee40f86fbead3
Tangem <git@tangem.com>
2022-04-22 18:13:05 +0300

    Updated on 2025-09-25

--- domain/src/main/java/com/tangem/domain/common/util/ValueDebouncer.kt
+++ domain/src/main/java/com/tangem/domain/common/util/ValueDebouncer.kt
@@ -10,31 +10,37 @@ import kotlinx.coroutines.launch
 [REDACTED_AUTHOR]
  */
 class ValueDebouncer<T>(
-    private val debounce: Long = 400,
-    private val onValueChanged: (T?) -> Unit
+    private val initialValue: T,
+    private val debounceDuration: Long = 400,
+    private val onValueChanged: (T) -> Unit,
+    private val onEmitValueReceived: (T) -> Unit = {},
 ) {
 
-    private var value: T? = null
-    private val debounceScope = CoroutineScope(Job() + Dispatchers.Main)
-    private val flow = MutableStateFlow(value)
+    var emittedValue: T = initialValue
+        private set
+    var debounced: T = initialValue
+        private set
+
+    private val debounceScope: CoroutineScope = CoroutineScope(Job() + Dispatchers.Main)
+    private val flow = MutableStateFlow(debounced)
 
     init {
-        initFlow()
-    }
-
-    private fun initFlow() {
         debounceScope.launch {
-            flow.filter { if (value == null) true else value != it }
-                .debounce(debounce)
+            flow.filter { if (debounced == null) true else debounced != it }
+                .debounce(debounceDuration)
                 .onEach {
-                    value = it
+                    debounced = it
                     onValueChanged(it)
                 }
                 .collect()
         }
     }
 
+    fun isDebounced(value: T): Boolean = this.debounced == value
+
     fun emmit(emmitValue: T) {
+        emittedValue = emmitValue
+        onEmitValueReceived.invoke(emmitValue)
         debounceScope.launch { flow.emit(emmitValue) }
     }
 }
