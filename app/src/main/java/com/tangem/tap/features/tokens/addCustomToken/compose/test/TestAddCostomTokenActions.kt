39877feeedbd21d588bbab5e78dda4b85c4bf8d5
Tangem <git@tangem.com>
2022-04-29 12:50:05 +0300

    Updated on 2026-09-01

--- app/src/main/java/com/tangem/tap/features/tokens/addCustomToken/compose/test/TestAddCostomTokenActions.kt
+++ app/src/main/java/com/tangem/tap/features/tokens/addCustomToken/compose/test/TestAddCostomTokenActions.kt
@@ -63,7 +63,7 @@ private fun AllInOne() {
         address = "0x1111111111111111112111111111111111111113"
     )
     ContractAddressButton(
-        name = "full unk",
+        name = "full custom",
         address = "0x3019BF2a2eF8040C242C9a4c5c4BD4C81678b2A1",
         tokenNetwork = Blockchain.Ethereum,
         tokenName = "Test unknown",
