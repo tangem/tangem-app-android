f4e2e37ae157aa026e9262c1e458e647932df76e
Tangem <git@tangem.com>
2022-05-25 09:53:58 +0300

    release/5.26.1

--- domain/src/main/java/com/tangem/domain/redux/extensions/Dispatch.kt
+++ domain/src/main/java/com/tangem/domain/redux/extensions/Dispatch.kt
@@ -1,6 +1,5 @@
 package com.tangem.domain.redux.extensions
 
-import com.tangem.domain.common.extensions.withIOContext
 import com.tangem.domain.common.extensions.withMainContext
 import com.tangem.domain.redux.domainStore
 import org.rekotlin.Action
@@ -10,8 +9,4 @@ import org.rekotlin.Action
  */
 internal suspend inline fun dispatchOnMain(vararg actions: Action) {
     withMainContext { actions.forEach { domainStore.dispatch(it) } }
-}
-
-internal suspend inline fun dispatchOnIO(vararg actions: Action) {
-    withIOContext { actions.forEach { domainStore.dispatch(it) } }
 }
