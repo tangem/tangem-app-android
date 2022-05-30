d9e51736c92bdba78e91d69e118b7fdc396cd2f9
Tangem <git@tangem.com>
2022-05-30 17:35:48 +0400

    Updated on 2026-09-23

--- app/src/main/java/com/tangem/tap/features/tokens/ui/compose/ListOfCurrencies.kt
+++ app/src/main/java/com/tangem/tap/features/tokens/ui/compose/ListOfCurrencies.kt
@@ -4,13 +4,14 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.tangem.blockchain.common.Blockchain
+import com.tangem.tap.common.compose.extensions.OnBottomReached
 import com.tangem.tap.domain.tokens.Currency
 import com.tangem.tap.features.tokens.redux.ContractAddress
 import com.tangem.tap.features.tokens.redux.TokenWithBlockchain
@@ -18,7 +19,7 @@ import com.tangem.tap.features.tokens.redux.TokenWithBlockchain
 
 @Composable
 fun ListOfCurrencies(
-    header: @Composable ()->Unit,
+    header: @Composable () -> Unit,
     currencies: List<Currency>,
     nonRemovableTokens: List<ContractAddress>,
     nonRemovableBlockchains: List<Blockchain>,
@@ -42,34 +43,31 @@ fun ListOfCurrencies(
         expandedCurrencies.value = mutableList
     }
 
+    val listState = rememberLazyListState()
+
     LazyColumn(
+        state = listState,
         modifier = Modifier
             .fillMaxSize(),
         contentPadding = PaddingValues(bottom = 90.dp)
     ) {
         item { header() }
         itemsIndexed(currencies) { index, currency ->
-            val lastIndex = currencies.lastIndex
-            if (currencies.isNotEmpty()) {
-                if (index + 40 == lastIndex) {
-                    SideEffect { onLoadMore() }
-                }
-                CurrencyItem(
-                    currency = currency,
-                    nonRemovableTokens = nonRemovableTokens,
-                    nonRemovableBlockchains = nonRemovableBlockchains,
-                    addedTokens = addedTokens,
-                    addedBlockchains = addedBlockchains,
-                    allowToAdd = allowToAdd,
-                    expanded = expandedCurrencies.value.contains(currency.id),
-                    onCurrencyClick = onCurrencyClick,
-                    onAddCurrencyToggled = onAddCurrencyToggled,
-                    onNetworkItemClicked = onNetworkItemClicked
-                )
-            }
-
+            CurrencyItem(
+                currency = currency,
+                nonRemovableTokens = nonRemovableTokens,
+                nonRemovableBlockchains = nonRemovableBlockchains,
+                addedTokens = addedTokens,
+                addedBlockchains = addedBlockchains,
+                allowToAdd = allowToAdd,
+                expanded = expandedCurrencies.value.contains(currency.id),
+                onCurrencyClick = onCurrencyClick,
+                onAddCurrencyToggled = onAddCurrencyToggled,
+                onNetworkItemClicked = onNetworkItemClicked
+            )
         }
     }
+    listState.OnBottomReached(loadMoreThreshold = 40) { onLoadMore() }
 }
 
 val Currency.fullName: String
