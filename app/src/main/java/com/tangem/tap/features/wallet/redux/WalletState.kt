3a913ab72562facf2c6b12b06d70f9944684b236
Tangem <git@tangem.com>
2022-06-01 13:06:47 +0300

    Updated on 2025-10-13

--- app/src/main/java/com/tangem/tap/features/wallet/redux/WalletState.kt
+++ app/src/main/java/com/tangem/tap/features/wallet/redux/WalletState.kt
@@ -1,9 +1,15 @@
 package com.tangem.tap.features.wallet.redux
 
 import android.graphics.Bitmap
-import com.tangem.blockchain.common.*
+import com.tangem.blockchain.common.Amount
+import com.tangem.blockchain.common.Blockchain
+import com.tangem.blockchain.common.DerivationStyle
+import com.tangem.blockchain.common.Token
+import com.tangem.blockchain.common.Wallet
+import com.tangem.blockchain.common.WalletManager
 import com.tangem.blockchain.common.address.AddressType
 import com.tangem.common.extensions.isZero
+import com.tangem.domain.common.TapWorkarounds.derivationStyle
 import com.tangem.domain.common.extensions.canHandleToken
 import com.tangem.domain.common.extensions.toCoinId
 import com.tangem.domain.features.addCustomToken.CustomCurrency
@@ -19,15 +25,21 @@ import com.tangem.tap.domain.extensions.sellIsAllowed
 import com.tangem.tap.domain.tokens.models.BlockchainNetwork
 import com.tangem.tap.features.onboarding.products.twins.redux.TwinCardsState
 import com.tangem.tap.features.tokens.redux.TokenWithBlockchain
-import com.tangem.tap.features.wallet.models.*
+import com.tangem.tap.features.wallet.models.PendingTransaction
+import com.tangem.tap.features.wallet.models.TotalBalance
+import com.tangem.tap.features.wallet.models.WalletRent
+import com.tangem.tap.features.wallet.models.WalletWarning
+import com.tangem.tap.features.wallet.models.hasPendingTransactions
+import com.tangem.tap.features.wallet.models.hasSendableAmounts
+import com.tangem.tap.features.wallet.models.isSendableAmount
 import com.tangem.tap.features.wallet.redux.reducers.calculateTotalFiatAmount
 import com.tangem.tap.features.wallet.redux.reducers.findTotalBalanceState
 import com.tangem.tap.features.wallet.ui.BalanceStatus
 import com.tangem.tap.features.wallet.ui.BalanceWidgetData
 import com.tangem.tap.network.exchangeServices.CurrencyExchangeManager
 import com.tangem.tap.store
-import org.rekotlin.StateType
 import java.math.BigDecimal
+import org.rekotlin.StateType
 import kotlin.properties.ReadOnlyProperty
 
 data class WalletState(
@@ -271,12 +283,23 @@ data class WalletState(
 
     private fun updateTotalBalance(): WalletState {
         return if (wallets.isNotEmpty()) {
-            val walletsData = wallets.flatMap(WalletStore::walletsData)
+            val globalState = store.state.globalState
+            val walletsData = wallets
+                .flatMap(WalletStore::walletsData)
+                .filterNot { wallet ->
+                    wallet.currency.isCustomCurrency(
+                        derivationStyle = globalState
+                            .scanResponse
+                            ?.card
+                            ?.derivationStyle
+                    )
+                }
+
             this.copy(
                 totalBalance = TotalBalance(
                     state = walletsData.findTotalBalanceState(),
                     fiatAmount = walletsData.calculateTotalFiatAmount(),
-                    fiatCurrency = store.state.globalState.appCurrency
+                    fiatCurrency = globalState.appCurrency
                 )
             )
         } else this.copy(
