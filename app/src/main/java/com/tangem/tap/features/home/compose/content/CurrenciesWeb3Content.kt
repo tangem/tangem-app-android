917a32d0ea802dadf19335f88cee5b89a1f89ff8
Tangem <git@tangem.com>
2022-06-08 04:07:06 +0300

    Updated on 2026-09-23

---
+++ app/src/main/java/com/tangem/tap/features/home/compose/content/CurrenciesWeb3Content.kt
@@ -0,0 +1,161 @@
+package com.tangem.tap.features.home.compose.content
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import com.tangem.tangem_sdk_new.extensions.dpToPx
+import com.tangem.tangem_sdk_new.extensions.pxToDp
+import com.tangem.tap.common.compose.SpacerH
+import com.tangem.tap.common.compose.extensions.toPx
+import com.tangem.tap.common.extensions.isEven
+import com.tangem.tap.features.home.compose.uiTools.HorizontalSlidingImage
+import com.tangem.wallet.R
+
+@Composable
+fun StoriesCurrenciesContent(
+    paused: Boolean,
+    duration: Int,
+) {
+    val currencyDrawableList = remember {
+        listOf(
+            R.drawable.currency0,
+            R.drawable.currency1,
+            R.drawable.currency2,
+            R.drawable.currency3,
+            R.drawable.currency4,
+        )
+    }
+
+    val screenWidth = LocalConfiguration.current.screenWidthDp.dp
+    val decreaseRate = remember { 1f / currencyDrawableList.size }
+    val designItemHeight = remember { 82.dp }
+
+    LightenBox {
+        Column(modifier = Modifier.graphicsLayer(clip = false)) {
+            currencyDrawableList.forEachIndexed { index, drawableResId ->
+                val painter = painterResource(id = drawableResId)
+                val scaledItemSize = scaleToDesignSize(painter.dpSize(), designItemHeight = designItemHeight)
+                val itemOversizedScreenWidthBy = scaledItemSize.width - screenWidth
+                val moveItemToStartOfScreen = itemOversizedScreenWidthBy / 2
+
+                val chessOffset = if (index.isEven()) 0.dp else scaledItemSize.halfHeight()
+                val animateFrom = chessOffset - moveItemToStartOfScreen
+                val animateTo = 50.dp - (50.dp * index * decreaseRate)
+
+                HorizontalSlidingImage(
+                    paused = paused,
+                    duration = duration,
+                    painter = painter,
+                    itemSize = scaledItemSize,
+                    startOffset = animateFrom.toPx(),
+                    targetOffset = animateTo.toPx(),
+                    contentDescription = "Currency row",
+                )
+                SpacerH(12.dp)
+            }
+        }
+    }
+}
+
+@Composable
+fun StoriesWeb3Content(
+    paused: Boolean,
+    duration: Int,
+) {
+    val dappsItemList = remember {
+        listOf(
+            R.drawable.dapps0,
+            R.drawable.dapps1,
+            R.drawable.dapps2,
+            R.drawable.dapps3,
+            R.drawable.dapps4,
+            R.drawable.dapps5,
+        )
+    }
+    val screenWidth = LocalConfiguration.current.screenWidthDp.dp
+    val decreaseRate = remember { 1f / dappsItemList.size }
+    val designItemHeight = 50.dp
+
+    LightenBox {
+        Column(modifier = Modifier.graphicsLayer(clip = false)) {
+            dappsItemList.forEachIndexed { index, drawableResId ->
+                val painter = painterResource(id = drawableResId)
+                val scaledItemSize = scaleToDesignSize(painter.dpSize(), designItemHeight = designItemHeight)
+                val itemOversizedScreenWidthBy = scaledItemSize.width - screenWidth
+                val moveItemToStartOfScreen = itemOversizedScreenWidthBy / 2
+
+                val chessOffset = if (index.isEven()) 0.dp else scaledItemSize.width / 3
+                val animateFrom = chessOffset - moveItemToStartOfScreen
+                val animateTo = 90.dp - (90.dp * index * decreaseRate)
+
+                HorizontalSlidingImage(
+                    paused = paused,
+                    duration = duration,
+                    painter = painter,
+                    itemSize = scaledItemSize,
+                    startOffset = animateFrom.toPx(),
+                    targetOffset = animateTo.toPx(),
+                    contentDescription = "Web3 row",
+                )
+                SpacerH(14.dp)
+            }
+        }
+
+    }
+}
+
+@Composable
+private fun LightenBox(content: @Composable () -> Unit) {
+    Box() {
+        content()
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 170.dp)
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            Color.White.copy(alpha = 0.3f),
+                            Color.White
+                        )
+                    )
+                )
+        ) {}
+    }
+}
+
+@Composable
+private fun Painter.dpSize(): DpSize = DpSize(
+    intrinsicSize.width.pxToDp().dp,
+    intrinsicSize.height.pxToDp().dp,
+)
+
+@Composable
+private fun Float.dpToPx(): Float = LocalContext.current.dpToPx(this)
+
+@Composable
+private fun Float.pxToDp(): Float = LocalContext.current.pxToDp(this)
+
+private fun scaleToDesignSize(itemSize: DpSize, designItemHeight: Dp): DpSize {
+    val scaleRate = itemSize.height / designItemHeight
+    return itemSize / scaleRate
+}
+
+private fun DpSize.halfWidth(): Dp = this.width / 2
+
+private fun DpSize.halfHeight(): Dp = this.height / 2
