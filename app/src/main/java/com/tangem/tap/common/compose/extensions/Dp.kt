058ed547cd395d6e6596dbb3937908ebc501d9ce
Tangem <git@tangem.com>
2022-06-08 16:12:43 +0300

    Updated on 2026-09-25

--- app/src/main/java/com/tangem/tap/common/compose/extensions/Dp.kt
+++ app/src/main/java/com/tangem/tap/common/compose/extensions/Dp.kt
@@ -3,6 +3,7 @@ package com.tangem.tap.common.compose.extensions
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpSize
 
 /**
 [REDACTED_AUTHOR]
@@ -12,3 +13,7 @@ fun Dp.toPx(): Float {
     val currentDp = this
     return with(LocalDensity.current) { currentDp.toPx() }
 }
+
+fun DpSize.halfWidth(): Dp = this.width / 2
+
+fun DpSize.halfHeight(): Dp = this.height / 2
