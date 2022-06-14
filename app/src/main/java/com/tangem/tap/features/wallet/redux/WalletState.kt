e73fe0e1e95cecdc26a29fe9caba6b379f2cd438
Tangem <git@tangem.com>
2022-06-14 12:11:57 +0400

    release/5.26.1

--- app/src/main/java/com/tangem/tap/features/wallet/redux/WalletState.kt
+++ app/src/main/java/com/tangem/tap/features/wallet/redux/WalletState.kt
@@ -1,17 +1,10 @@
 package com.tangem.tap.features.wallet.redux
 
 import android.graphics.Bitmap
-import com.tangem.blockchain.common.Amount
-import com.tangem.blockchain.common.Blockchain
-import com.tangem.blockchain.common.DerivationStyle
-import com.tangem.blockchain.common.Token
-import com.tangem.blockchain.common.Wallet
-import com.tangem.blockchain.common.WalletManager
+import com.tangem.blockchain.common.*
 import com.tangem.blockchain.common.address.AddressType
 import com.tangem.common.extensions.isZero
 import com.tangem.domain.common.extensions.canHandleToken
-import com.tangem.domain.common.extensions.toCoinId
-import com.tangem.domain.features.addCustomToken.CustomCurrency
 import com.tangem.tap.common.entities.Button
 import com.tangem.tap.common.entities.FiatCurrency
 import com.tangem.tap.common.extensions.toQrCode
@@ -23,22 +16,15 @@ import com.tangem.tap.domain.extensions.buyIsAllowed
 import com.tangem.tap.domain.extensions.sellIsAllowed
 import com.tangem.tap.domain.tokens.models.BlockchainNetwork
 import com.tangem.tap.features.onboarding.products.twins.redux.TwinCardsState
-import com.tangem.tap.features.tokens.redux.TokenWithBlockchain
-import com.tangem.tap.features.wallet.models.PendingTransaction
-import com.tangem.tap.features.wallet.models.TotalBalance
-import com.tangem.tap.features.wallet.models.WalletRent
-import com.tangem.tap.features.wallet.models.WalletWarning
-import com.tangem.tap.features.wallet.models.hasPendingTransactions
-import com.tangem.tap.features.wallet.models.hasSendableAmounts
-import com.tangem.tap.features.wallet.models.isSendableAmount
+import com.tangem.tap.features.wallet.models.*
 import com.tangem.tap.features.wallet.redux.reducers.calculateTotalFiatAmount
 import com.tangem.tap.features.wallet.redux.reducers.findProgressState
 import com.tangem.tap.features.wallet.ui.BalanceStatus
 import com.tangem.tap.features.wallet.ui.BalanceWidgetData
 import com.tangem.tap.network.exchangeServices.CurrencyExchangeManager
 import com.tangem.tap.store
-import java.math.BigDecimal
 import org.rekotlin.StateType
+import java.math.BigDecimal
 import kotlin.properties.ReadOnlyProperty
 
 data class WalletState(
@@ -437,87 +423,6 @@ data class WalletData(
     private fun blockchainAmountIsEmpty(): Boolean = currencyData.blockchainAmount?.isZero() == true
 
     private fun tokenAmountIsEmpty(): Boolean = currencyData.amount?.isZero() == true
-}
-
-sealed interface Currency {
-
-    val coinId: String?
-        get() = when (this) {
-            is Blockchain -> blockchain.toCoinId()
-            is Token -> token.id
-        }
-    val blockchain: com.tangem.blockchain.common.Blockchain
-    val currencySymbol: CryptoCurrencyName
-    val derivationPath: String?
-
-    data class Token(
-        val token: com.tangem.blockchain.common.Token,
-        override val blockchain: com.tangem.blockchain.common.Blockchain,
-        override val derivationPath: String?
-    ) : Currency {
-        override val currencySymbol = token.symbol
-    }
-
-    data class Blockchain(
-        override val blockchain: com.tangem.blockchain.common.Blockchain,
-        override val derivationPath: String?
-    ) : Currency {
-        override val currencySymbol: CryptoCurrencyName = blockchain.currency
-    }
-
-    fun isCustomCurrency(derivationStyle: DerivationStyle?): Boolean {
-        if (this is Token && this.token.id == null) return true
-
-        if (derivationPath == null || derivationStyle == null) return false
-
-        return derivationPath != blockchain.derivationPath(derivationStyle)?.rawPath
-    }
-
-    fun isBlockchain(): Boolean = this is Blockchain
-
-    fun isToken(): Boolean = this is Token
-
-    companion object {
-        fun fromBlockchainNetwork(
-            blockchainNetwork: BlockchainNetwork,
-            token: com.tangem.blockchain.common.Token? = null
-        ): Currency {
-            return if (token != null) {
-                Token(
-                    token = token,
-                    blockchain = blockchainNetwork.blockchain,
-                    derivationPath = blockchainNetwork.derivationPath
-                )
-            } else {
-                Blockchain(
-                    blockchain = blockchainNetwork.blockchain,
-                    derivationPath = blockchainNetwork.derivationPath
-                )
-            }
-        }
-
-        fun fromCustomCurrency(customCurrency: CustomCurrency): Currency {
-            return when (customCurrency) {
-                is CustomCurrency.CustomBlockchain -> Blockchain(
-                    blockchain = customCurrency.network,
-                    derivationPath = customCurrency.derivationPath?.rawPath
-                )
-                is CustomCurrency.CustomToken -> Token(
-                    token = customCurrency.token,
-                    blockchain = customCurrency.network,
-                    derivationPath = customCurrency.derivationPath?.rawPath,
-                )
-            }
-        }
-
-        fun fromTokenWithBlockchain(tokenWithBlockchain: TokenWithBlockchain): Token {
-            return Token(
-                token = tokenWithBlockchain.token,
-                blockchain = tokenWithBlockchain.blockchain,
-                derivationPath = null
-            )
-        }
-    }
 }
 
 data class WalletStore(
