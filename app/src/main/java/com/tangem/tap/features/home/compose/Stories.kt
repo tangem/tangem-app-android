11743d58d4f504595f3e9bd4c5fc8932632fc476
Tangem <git@tangem.com>
2022-06-29 16:42:35 +0300

    Updated on 2025-11-03

--- app/src/main/java/com/tangem/tap/features/home/compose/Stories.kt
+++ app/src/main/java/com/tangem/tap/features/home/compose/Stories.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.tangem.tap.common.compose.SpacerS24
@@ -189,4 +190,10 @@ fun StoriesScreen(
 private fun MutableState<Int>.duration(): Int = when (this.value) {
     1 -> 8000
     else -> 6000
+}
+
+@Preview
+@Composable
+fun StoriesScreenPreview() {
+    StoriesScreen(onScanButtonClick = {}, onShopButtonClick = {}, onSearchTokensClick = {})
 }
