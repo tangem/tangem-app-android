8f1e07286880e63bbc6c5a59b48cc8fcd13b0688
Tangem <git@tangem.com>
2022-07-04 21:15:15 +0300

    Updated on 2025-09-17

--- app/src/main/java/com/tangem/tap/features/tokens/redux/TokensMiddleware.kt
+++ app/src/main/java/com/tangem/tap/features/tokens/redux/TokensMiddleware.kt
@@ -44,13 +44,13 @@ class TokensMiddleware {
                 when (action) {
                     is TokensAction.LoadCurrencies -> handleLoadCurrencies(action.scanResponse)
                     is TokensAction.SaveChanges -> handleSaveChanges(action)
-                    is TokensAction.PrepareAndNavigateToAddCustomToken -> handleAddingCustomToken(
-                        action
-                    )
+                    is TokensAction.PrepareAndNavigateToAddCustomToken -> {
+                        handleAddingCustomToken(action)
+                    }
                     is TokensAction.SetSearchInput -> {
                         handleLoadCurrencies(
                             scanResponse = store.state.globalState.scanResponse,
-                            action.searchInput
+                            newSearchInput = action.searchInput
                         )
                     }
                     is TokensAction.LoadMore -> {
@@ -138,7 +138,9 @@ class TokensMiddleware {
             )
         )
 
-        if (tokensToAdd.isEmpty() && blockchainsToAdd.isEmpty()) {
+        if (tokensToAdd.isEmpty() && tokensToRemove.isEmpty()
+            && blockchainsToAdd.isEmpty() && blockchainsToRemove.isEmpty()
+        ) {
             store.dispatchDebugErrorNotification("Nothing to save")
             store.dispatch(NavigationAction.PopBackTo())
             return
