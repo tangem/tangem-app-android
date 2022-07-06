80ba1b401c50b1fc0d23917e70da5bf09550ca6c
Tangem <git@tangem.com>
2022-07-06 18:07:27 +0400

    Updated on 2026-01-20

--- app/src/main/java/com/tangem/tap/common/redux/navigation/NavigationState.kt
+++ app/src/main/java/com/tangem/tap/common/redux/navigation/NavigationState.kt
@@ -16,7 +16,7 @@ enum class AppScreen {
     OnboardingNote, OnboardingWallet, OnboardingTwins, OnboardingOther,
     Wallet, WalletDetails,
     Send,
-    Details, DetailsConfirm, DetailsSecurity,
+    Details, DetailsConfirm, DetailsSecurity, SecurityAndPrivacy, ChangeAccessCode,
     AddTokens, AddCustomToken,
     WalletConnectSessions,
     QrScan
