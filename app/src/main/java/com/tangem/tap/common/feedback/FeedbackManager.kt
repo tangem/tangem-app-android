3af32b59caa75046f61f333216dd20eb2aaf270b
Tangem <git@tangem.com>
2022-07-13 00:19:07 +0300

    Updated on 2026-09-14

--- app/src/main/java/com/tangem/tap/common/feedback/FeedbackManager.kt
+++ app/src/main/java/com/tangem/tap/common/feedback/FeedbackManager.kt
@@ -1,14 +1,22 @@
 package com.tangem.tap.common.feedback
 
-import android.app.Activity
+import android.content.Context
 import com.tangem.domain.common.TapWorkarounds
 import com.tangem.tap.common.extensions.sendEmail
 import com.tangem.tap.common.log.TangemLogCollector
+import com.tangem.tap.common.zendesk.ZendeskConfig
+import com.tangem.tap.foregroundActivityObserver
+import com.tangem.tap.withForegroundActivity
 import java.io.File
 import java.io.FileWriter
 import java.io.StringWriter
 import timber.log.Timber
-
+import zendesk.configurations.Configuration
+import zendesk.core.Zendesk
+import zendesk.support.Support
+import zendesk.support.request.RequestConfiguration
+import zendesk.support.requestlist.RequestListActivity
+import zendesk.support.requestlist.RequestListConfiguration
 
 /**
 [REDACTED_AUTHOR]
@@ -17,38 +25,47 @@ class FeedbackManager(
     val infoHolder: AdditionalFeedbackInfo,
     private val logCollector: TangemLogCollector,
 ) {
-
-    private lateinit var activity: Activity
-
-    fun updateActivity(activity: Activity) {
-        this.activity = activity
+    fun initChat(
+        context: Context,
+        zendeskConfig: ZendeskConfig,
+    ) {
+        Zendesk.INSTANCE.init(
+            /* context = */ context,
+            /* zendeskUrl = */ zendeskConfig.url,
+            /* applicationId = */ zendeskConfig.appId,
+            /* oauthClientId = */ zendeskConfig.clientId,
+        )
+        Support.INSTANCE.init(Zendesk.INSTANCE)
     }
 
     fun sendEmail(feedbackData: FeedbackData, onFail: ((Exception) -> Unit)? = null) {
-        if (!this::activity.isInitialized) return
-
         feedbackData.prepare(infoHolder)
-        val fileLog = if (feedbackData is ScanFailsEmail) createLogFile() else null
-        activity.sendEmail(
-            email = getSupportEmail(),
-            subject = activity.getString(feedbackData.subjectResId),
-            message = feedbackData.joinTogether(activity, infoHolder),
-            file = fileLog,
-            onFail = onFail
-        )
-    }
-
-    private fun getSupportEmail(): String {
-        return if (TapWorkarounds.isStart2CoinIssuer(infoHolder.cardIssuer)) {
-            S2C_SUPPORT_EMAIL
-        } else {
-            DEFAULT_SUPPORT_EMAIL
+        foregroundActivityObserver.withForegroundActivity { activity ->
+            val fileLog = if (feedbackData is ScanFailsEmail) createLogFile(activity) else null
+            activity.sendEmail(
+                email = getSupportEmail(),
+                subject = activity.getString(feedbackData.subjectResId),
+                message = feedbackData.joinTogether(activity, infoHolder),
+                file = fileLog,
+                onFail = onFail
+            )
         }
     }
 
-    private fun createLogFile(): File? {
+    fun openChat(feedbackData: FeedbackData) {
+        feedbackData.prepare(infoHolder)
+        foregroundActivityObserver.withForegroundActivity { activity ->
+            RequestListActivity.builder()
+                .show(
+                    /* context = */ activity,
+                    /* configurations = */ buildConfigs(activity, feedbackData)
+                )
+        }
+    }
+
+    private fun createLogFile(context: Context): File? {
         return try {
-            val file = File(activity.filesDir, "logs.txt")
+            val file = File(context.filesDir, "logs.txt")
             file.delete()
             file.createNewFile()
 
@@ -60,8 +77,32 @@ class FeedbackManager(
             logCollector.clearLogs()
             file
         } catch (ex: Exception) {
-            Timber.e(ex, "Can't create a file for email attachment")
+            Timber.e(ex, "Can't create the logs file")
             null
+        }
+    }
+
+    private fun buildConfigs(
+        context: Context,
+        feedbackData: FeedbackData,
+    ): List<Configuration> {
+        return listOf(
+            // Request configuration
+            RequestConfiguration.Builder()
+                .withRequestSubject(context.getString(feedbackData.subjectResId))
+                .config(),
+            // Request list configuration
+            RequestListConfiguration.Builder()
+                .withContactUsButtonVisible(true)
+                .config(),
+        )
+    }
+
+    private fun getSupportEmail(): String {
+        return if (TapWorkarounds.isStart2CoinIssuer(infoHolder.cardIssuer)) {
+            S2C_SUPPORT_EMAIL
+        } else {
+            DEFAULT_SUPPORT_EMAIL
         }
     }
 
