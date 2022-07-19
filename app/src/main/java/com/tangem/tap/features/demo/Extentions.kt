092d308378d6115be33c7217fffd75c44a55da19
iiiburnyiii <iiiburnyiii@gmail.com>
2022-07-19 17:53:58 +0300

    AND-1954 Remove the card ID when signing a transaction

--- app/src/main/java/com/tangem/tap/features/demo/Extentions.kt
+++ app/src/main/java/com/tangem/tap/features/demo/Extentions.kt
@@ -1,10 +1,10 @@
 package com.tangem.tap.features.demo
 
-import com.tangem.blockchain.common.WalletManager
+import com.tangem.common.card.Card
 import com.tangem.domain.common.ScanResponse
 
 /**
  * Created by Anton Zhilenkov on 21/02/2022.
  */
 fun ScanResponse.isDemoCard(): Boolean = DemoHelper.isDemoCardId(card.cardId)
-fun WalletManager.isDemoWallet(): Boolean = DemoHelper.isDemoCardId(wallet.cardId)
+fun Card.isDemoCard(): Boolean = DemoHelper.isDemoCardId(cardId)
