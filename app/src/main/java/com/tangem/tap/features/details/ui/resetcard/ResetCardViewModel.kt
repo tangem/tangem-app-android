4b15e7f3199f3fb203c3b994f716d694ee7bc955
Tangem <git@tangem.com>
2022-08-03 16:26:40 +0400

    Updated on 2025-11-26

--- app/src/main/java/com/tangem/tap/features/details/ui/resetcard/ResetCardViewModel.kt
+++ app/src/main/java/com/tangem/tap/features/details/ui/resetcard/ResetCardViewModel.kt
@@ -1,6 +1,5 @@
 package com.tangem.tap.features.details.ui.resetcard
 
-import android.content.Context
 import com.tangem.tap.common.redux.AppState
 import com.tangem.tap.features.details.redux.CardSettingsState
 import com.tangem.tap.features.details.redux.DetailsAction
@@ -8,11 +7,11 @@ import org.rekotlin.Store
 
 class ResetCardViewModel(private val store: Store<AppState>) {
 
-    fun updateState(state: CardSettingsState?, context: Context?): ResetCardScreenState {
+    fun updateState(state: CardSettingsState?): ResetCardScreenState {
         return ResetCardScreenState(
             accepted = state?.resetConfirmed ?: false,
             onAcceptWarningToggleClick = { store.dispatch(DetailsAction.ResetToFactory.Confirm(it)) },
-            onResetButtonClick = { store.dispatch(DetailsAction.ResetToFactory.Proceed) }
+            onResetButtonClick = { store.dispatch(DetailsAction.ResetToFactory.Proceed) },
         )
     }
 }
