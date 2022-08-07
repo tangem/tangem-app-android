8cdaaa6b668f7d54a0bb34cb591154a7c018187b
Tangem <git@tangem.com>
2022-08-07 22:32:40 +0400

    Updated on 2026-09-14

--- app/src/main/java/com/tangem/tap/features/details/ui/common/TangemSwitch.kt
+++ app/src/main/java/com/tangem/tap/features/details/ui/common/TangemSwitch.kt
@@ -1,87 +1,116 @@
 package com.tangem.tap.features.details.ui.common
 
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.indication
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.Fill
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.tangem.wallet.R
 
 @Composable
 fun TangemSwitch(
-    checked: Boolean,
-    onCheckedChange: (Boolean) -> Unit,
     modifier: Modifier = Modifier,
-    width: Dp = 48.dp,
-    height: Dp = 24.dp,
-    checkedTrackColor: Color = colorResource(id = R.color.control_checked),
-    uncheckedTrackColor: Color = colorResource(id = R.color.icon_informative),
-    selectorColor: Color = colorResource(id = R.color.background_secondary),
-    gapBetweenThumbAndTrackEdge: Dp = 3.dp,
+    enabledColor: Color = colorResource(id = R.color.control_checked),
+    disabledColor: Color = colorResource(id = R.color.icon_informative),
+    size: Dp = 48.dp,
+    checked: Boolean = false,
+    onCheckedChange: (Boolean) -> Unit,
 ) {
-    val thumbRadius = (height / 2) - gapBetweenThumbAndTrackEdge
-    val animatePosition = animateFloatAsState(
-        targetValue = if (checked)
-            with(LocalDensity.current) { (width - thumbRadius - gapBetweenThumbAndTrackEdge).toPx() }
-        else
-            with(LocalDensity.current) { (thumbRadius + gapBetweenThumbAndTrackEdge).toPx() },
-    )
-
-    Canvas(
-        modifier = modifier
-            .size(width = width, height = height)
-            .pointerInput(Unit) {
-                detectTapGestures(
-                    onTap = {
-                        onCheckedChange(checked)
-                    },
-                )
-            },
+    val transition = updateTransition(checked, label = "SwitchState")
+    val color by transition.animateColor(
+        transitionSpec = {
+            tween(200, easing = FastOutLinearInEasing)
+        },
+        label = "",
     ) {
-        drawRoundRect(
-            color = if (checked) checkedTrackColor else uncheckedTrackColor,
-            cornerRadius = CornerRadius(x = 35.dp.toPx(), y = 35.dp.toPx()),
-            style = Fill,
-        )
-
-        drawCircle(
-            color = selectorColor,
-            radius = thumbRadius.toPx(),
-            center = Offset(
-                x = animatePosition.value,
-                y = size.height / 2,
-            ),
-        )
+        when (it) {
+            true -> enabledColor
+            false -> disabledColor
+        }
+    }
+    val interactionSource = remember { MutableInteractionSource() }
+    val clickable = Modifier.clickable(
+        interactionSource = interactionSource,
+        indication = null,
+    ) {
+        if (!checked) {
+            onCheckedChange(true)
+        } else {
+            onCheckedChange(false)
+        }
     }
 
-    Spacer(modifier = Modifier.height(18.dp))
-}
-
-@Preview
-@Composable
-fun TangemSwitchPreview() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
+    Box(
+        modifier = Modifier
+            .then(clickable)
+            .indication(
+                interactionSource = MutableInteractionSource(),
+                indication = rememberRipple(
+                    bounded = true,
+                    radius = 100.dp,
+                    color = Color.Transparent,
+                ),
+            ),
     ) {
-        TangemSwitch(true, {})
+        BoxWithConstraints(
+            modifier = modifier
+                .width(size)
+                .height(size / 2)
+                .indication(MutableInteractionSource(), null)
+                .background(color = color, shape = RoundedCornerShape(100)),
+            contentAlignment = Alignment.CenterStart,
+        ) {
+            val roundCardSize = this.maxWidth / 2
+            val xOffset by transition.animateDp(
+                transitionSpec = {
+                    tween(150, easing = LinearOutSlowInEasing)
+                },
+                label = "xOffset",
+            ) { state ->
+                when (state) {
+                    false -> 0.dp
+                    true -> this.maxWidth - roundCardSize
+                }
+            }
+
+            Card(
+                modifier = Modifier
+                    .size(this.maxWidth / 2)
+                    .offset(x = xOffset, y = 0.dp)
+                    .padding(3.dp),
+                shape = RoundedCornerShape(100),
+                backgroundColor = Color.White,
+                border = BorderStroke(
+                    if (!checked) 0.5.dp else 0.dp,
+                    color = Color.LightGray,
+                ),
+            ) {
+            }
+        }
     }
 }
