66e0434cf7e188dfeab4060b124f6e668dd81e41
Tangem <git@tangem.com>
2022-08-08 15:12:02 +0400

    Updated on 2026-08-14

--- app/src/main/java/com/tangem/tap/features/details/ui/walletconnect/dialogs/ApproveWcSessionDialog.kt
+++ app/src/main/java/com/tangem/tap/features/details/ui/walletconnect/dialogs/ApproveWcSessionDialog.kt
@@ -2,34 +2,43 @@ package com.tangem.tap.features.details.ui.walletconnect.dialogs
 
 import android.content.Context
 import androidx.appcompat.app.AlertDialog
+import com.tangem.blockchain.common.Blockchain
 import com.tangem.tap.common.redux.global.GlobalAction
 import com.tangem.tap.features.details.redux.walletconnect.WalletConnectAction
+import com.tangem.tap.features.details.redux.walletconnect.WalletConnectDialog
 import com.tangem.tap.features.details.redux.walletconnect.WalletConnectSession
 import com.tangem.tap.store
 import com.tangem.wallet.R
 
 class ApproveWcSessionDialog {
     companion object {
-        fun create(session: WalletConnectSession, context: Context): AlertDialog {
+        fun create(session: WalletConnectSession, networks: List<Blockchain>, context: Context): AlertDialog {
             val message = context.getString(
                 R.string.wallet_connect_request_session_start,
                 session.wallet.cardId,
                 session.peerMeta.name,
-                session.peerMeta.url
+                session.peerMeta.url,
             )
             return AlertDialog.Builder(context).apply {
                 setTitle(context.getString(R.string.wallet_connect))
                 setMessage(message)
                 setPositiveButton(context.getText(R.string.common_start)) { _, _ ->
-                    store.dispatch(WalletConnectAction.ApproveSession(
-                        session.session
-                    ))
+                    store.dispatch(
+                        WalletConnectAction.ChooseNetwork(
+                            session.wallet.blockchain!!,
+                        ),
+                    )
+                }
+                if (networks.size > 1) {
+                    setNeutralButton(context.getText(R.string.wallet_connect_select_network)) { _, _ ->
+                        store.dispatch(WalletConnectAction.SelectNetwork(networks))
+                    }
                 }
                 setNegativeButton(context.getText(R.string.common_reject)) { _, _ ->
                     store.dispatch(WalletConnectAction.FailureEstablishingSession(session.session))
                 }
                 setOnDismissListener {
-                    store.dispatch(GlobalAction.HideDialog)
+                    store.dispatch(GlobalAction.HideDialog(WalletConnectDialog.ApproveWcSession(session, networks)))
                 }
             }.create()
         }
