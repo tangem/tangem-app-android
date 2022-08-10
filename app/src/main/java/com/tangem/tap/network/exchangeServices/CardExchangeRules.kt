a31962c9179b2642a0745e7857ae291f299e6f4b
Tangem <git@tangem.com>
2022-08-10 21:09:54 +0300

    Updated on 2026-09-23

---
+++ app/src/main/java/com/tangem/tap/network/exchangeServices/CardExchangeRules.kt
@@ -0,0 +1,54 @@
+package com.tangem.tap.network.exchangeServices
+
+import com.tangem.common.card.Card
+import com.tangem.domain.common.TapWorkarounds.isStart2Coin
+import com.tangem.tap.features.demo.isDemoCard
+import com.tangem.tap.features.wallet.models.Currency
+
+/**
+[REDACTED_AUTHOR]
+ */
+class CardExchangeRules(
+    val cardProvider: () -> Card?,
+) : ExchangeRules {
+
+    override fun isBuyAllowed(): Boolean {
+        val card = cardProvider() ?: return false
+
+        return when {
+            card.isDemoCard() -> false
+            card.isStart2Coin -> false
+            else -> true
+        }
+    }
+
+    override fun isSellAllowed(): Boolean {
+        val card = cardProvider() ?: return false
+
+        return when {
+            card.isDemoCard() -> false
+            card.isStart2Coin -> false
+            else -> true
+        }
+    }
+
+    override fun availableForBuy(currency: Currency): Boolean {
+        val card = cardProvider() ?: return false
+
+        return when {
+            card.isDemoCard() -> false
+            card.isStart2Coin -> false
+            else -> true
+        }
+    }
+
+    override fun availableForSell(currency: Currency): Boolean {
+        val card = cardProvider() ?: return false
+
+        return when {
+            card.isDemoCard() -> false
+            card.isStart2Coin -> false
+            else -> true
+        }
+    }
+}
