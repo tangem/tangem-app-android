7391ffd8cf617bc1c2e02de4bb42adde8126b9a8
Tangem <git@tangem.com>
2022-08-10 23:01:52 +0400

    release/5.26.1

--- app/src/main/java/com/tangem/tap/features/details/ui/walletconnect/dialogs/ChooseNetworkDialog.kt
+++ app/src/main/java/com/tangem/tap/features/details/ui/walletconnect/dialogs/ChooseNetworkDialog.kt
@@ -5,22 +5,26 @@ import androidx.appcompat.app.AlertDialog
 import com.tangem.blockchain.common.Blockchain
 import com.tangem.tap.common.redux.global.GlobalAction
 import com.tangem.tap.features.details.redux.walletconnect.WalletConnectAction
+import com.tangem.tap.features.details.redux.walletconnect.WalletConnectSession
 import com.tangem.tap.store
 import com.tangem.wallet.R
 
 object ChooseNetworkDialog {
     fun create(
-        blockchains: List<Blockchain>,
+        session: WalletConnectSession,
+        networks: List<Blockchain>,
         context: Context,
     ): AlertDialog {
         return AlertDialog.Builder(context)
             .setTitle(context.getString(R.string.wallet_connect_select_network))
-            .setNegativeButton(context.getString(R.string.common_cancel)) { _, _ -> /* no-op */ }
+            .setNegativeButton(context.getString(R.string.common_cancel)) { _, _ ->
+                store.dispatch(WalletConnectAction.FailureEstablishingSession(session.session))
+            }
             .setOnDismissListener {
                 store.dispatch(GlobalAction.HideDialog())
             }
-            .setSingleChoiceItems(blockchains.map { it.fullName }.toTypedArray(), 0) { _, which ->
-                blockchains.getOrNull(which)?.let { selectedBlockchain ->
+            .setSingleChoiceItems(networks.map { it.fullName }.toTypedArray(), 0) { _, which ->
+                networks.getOrNull(which)?.let { selectedBlockchain ->
                     store.dispatch(
                         WalletConnectAction.ChooseNetwork(
                             blockchain = selectedBlockchain,
