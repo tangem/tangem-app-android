c23ab4ddbebcf2dadaae78091ca313e1cc9efbe8
Tangem <git@tangem.com>
2022-09-10 13:31:45 +0300

    Updated on 2026-08-07

--- domain/src/test/java/com/tangem/domain/features/BlockchainNetworkIdTests.kt
+++ domain/src/test/java/com/tangem/domain/features/BlockchainNetworkIdTests.kt
@@ -6,24 +6,18 @@ import com.tangem.domain.common.extensions.fromNetworkId
 import com.tangem.domain.common.extensions.toNetworkId
 import org.junit.Test
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class BlockchainNetworkIdTests {
     @Test
     fun checkAppropriateImplementationForNetworkIds() {
         val unimplementedIds = Blockchain.values()
             .toMutableList()
-            .apply { remove(Blockchain.Unknown) }.map {
-                val networkId = it.toNetworkId()
-                networkId to Blockchain.fromNetworkId(networkId)
-            }.filter { it.second == null }
+            .apply { remove(Blockchain.Unknown) }
+            .map { it to Blockchain.fromNetworkId(it.toNetworkId()) }
+            .filter { it.second == null }
 
         if (unimplementedIds.isEmpty()) return
 
-        val message = unimplementedIds.joinToString("\n") { it.first }
+        val message = unimplementedIds.joinToString("\n") { it.first.fullName }
         Truth.assert_().withMessage(message).fail()
     }
 }
