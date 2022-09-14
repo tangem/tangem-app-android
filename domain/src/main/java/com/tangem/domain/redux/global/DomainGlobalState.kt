f1e153b6dd4b752452e8fcb275a7fd6f19f5dedc
Tangem <git@tangem.com>
2022-09-14 20:03:06 +0300

    Updated on 2026-09-25

--- domain/src/main/java/com/tangem/domain/redux/global/DomainGlobalState.kt
+++ domain/src/main/java/com/tangem/domain/redux/global/DomainGlobalState.kt
@@ -1,6 +1,8 @@
 package com.tangem.domain.redux.global
 
 import com.tangem.domain.DomainDialog
+import com.tangem.domain.common.LogConfig
+import com.tangem.domain.common.NetworkLogConfig
 import com.tangem.domain.common.ScanResponse
 import com.tangem.network.api.tangemTech.TangemTechService
 
@@ -12,10 +14,12 @@ data class DomainGlobalState(
     // It updates on GlobalAction.SaveScanNoteResponse -> DomainGlobalAction.SaveScanNoteResponse(scanResponse)
     val scanResponse: ScanResponse? = null,
     //
-    val networkServices: NetworkServices = NetworkServices(),
+    val logConfig: LogConfig = LogConfig.buildBased(),
+    val networkServices: NetworkServices = NetworkServices(logConfig.network),
     val dialog: DomainDialog? = null,
 )
 
 data class NetworkServices(
-    val tangemTechService: TangemTechService = TangemTechService()
+    val networkLogConfig: NetworkLogConfig,
+    val tangemTechService: TangemTechService = TangemTechService(networkLogConfig.tangemTechService)
 )
