2c521010d93ea88d3d37332ac22794880263abce
Tangem <git@tangem.com>
2022-09-15 01:00:26 +0400

    Updated on 2026-06-12

--- app/src/main/java/com/tangem/tap/features/wallet/ui/images/CurrencyIconView.kt
+++ app/src/main/java/com/tangem/tap/features/wallet/ui/images/CurrencyIconView.kt
@@ -4,13 +4,11 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.TextView
-import androidx.annotation.DrawableRes
 import androidx.constraintlayout.utils.widget.ImageFilterView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
 import com.tangem.blockchain.common.DerivationStyle
 import com.tangem.tangem_sdk_new.extensions.dpToPx
-import com.tangem.tap.common.extensions.getRoundIconRes
 import com.tangem.tap.features.wallet.models.Currency
 import com.tangem.wallet.databinding.ViewCurrencyIconBinding
 import kotlin.math.roundToInt
@@ -24,29 +22,16 @@ class CurrencyIconView @JvmOverloads constructor(
         LayoutInflater.from(context),
         this,
     )
-
     private val currencyImageView: ImageFilterView
         get() = binding.ivCurrency
-
     private val currencyTextView: TextView
         get() = binding.tvTokenLetter
-
-    private var isBlockchainIconVisible: Boolean
-        get() = binding.ivBlockchain.isVisible
-        set(value) = binding.ivBlockchain::isVisible.set(value)
-
-    private var isBadgeVisible: Boolean
-        get() = binding.badge.isVisible
-        set(value) = binding.badge::isVisible.set(value)
-
-    @DrawableRes
-    private var blockchainIconRes: Int? = null
-        set(value) {
-            if (value != null && value != field && isBlockchainIconVisible) {
-                binding.ivBlockchain.setImageResource(value)
-                field = value
-            }
-        }
+    private var isBlockchainBadgeVisible: Boolean
+        get() = binding.ivBlockchainBadge.isVisible
+        set(value) = binding.ivBlockchainBadge::isVisible.set(value)
+    private var isCustomCurrencyBadgeVisible: Boolean
+        get() = binding.customBadge.isVisible
+        set(value) = binding.customBadge::isVisible.set(value)
 
     init {
         minWidth = dpToPx(48f).roundToInt()
@@ -57,9 +42,7 @@ class CurrencyIconView @JvmOverloads constructor(
         currency: Currency,
         derivationStyle: DerivationStyle?,
     ) {
-        isBlockchainIconVisible = currency.isToken()
-        isBadgeVisible = currency.isCustomCurrency(derivationStyle)
-        blockchainIconRes = currency.blockchain.getRoundIconRes()
+        isCustomCurrencyBadgeVisible = currency.isCustomCurrency(derivationStyle)
 
         CurrencyIconRequest(
             currencyImageView = currencyImageView,
@@ -67,5 +50,17 @@ class CurrencyIconView @JvmOverloads constructor(
             token = (currency as? Currency.Token)?.token,
             blockchain = currency.blockchain,
         ).load()
+
+        if (currency.isToken()) {
+            // load a blockchain icon into the blockchain badge
+            isBlockchainBadgeVisible = true
+
+            CurrencyIconRequest(
+                currencyImageView = binding.ivBlockchainBadge,
+                currencyTextView = null,
+                token = null,
+                blockchain = currency.blockchain,
+            ).load()
+        }
     }
 }
