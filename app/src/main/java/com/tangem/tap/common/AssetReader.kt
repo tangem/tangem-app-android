903ae5b876dec22ac913f45920472e7113d3e4e8
Tangem <git@tangem.com>
2022-09-15 20:16:47 +0300

    Updated on 2026-06-10

--- app/src/main/java/com/tangem/tap/common/AssetReader.kt
+++ app/src/main/java/com/tangem/tap/common/AssetReader.kt
@@ -2,16 +2,12 @@ package com.tangem.tap.common
 
 import android.content.Context
 import com.tangem.tap.common.extensions.readAssetAsString
-import com.tangem.tap.common.extensions.readFile
-import com.tangem.tap.common.extensions.rewriteFile
 
 /**
 [REDACTED_AUTHOR]
  */
 interface AssetReader {
     fun readAssetAsString(name: String): String
-    fun readFile(fileName: String): String
-    fun rewriteFile(content: String, fileName: String)
 }
 
 class AndroidAssetReader(
@@ -20,13 +16,5 @@ class AndroidAssetReader(
 
     override fun readAssetAsString(name: String): String {
         return context.readAssetAsString(name)
-    }
-
-    override fun readFile(fileName: String): String {
-        return context.readFile(fileName)
-    }
-
-    override fun rewriteFile(content: String, fileName: String) {
-        context.rewriteFile(content, fileName)
     }
 }
