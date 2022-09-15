b930aa7d991e6ca0215ad72cc7fe24bf4de6c4f9
Tangem <git@tangem.com>
2022-09-15 20:36:34 +0300

    Updated on 2026-09-14

--- domain/src/main/java/com/tangem/domain/common/LogConfig.kt
+++ domain/src/main/java/com/tangem/domain/common/LogConfig.kt
@@ -1,51 +1,15 @@
 package com.tangem.domain.common
 
-import com.tangem.domain.features.BuildConfig
-
-data class LogConfig(
-    val imageLoader: Boolean,
-    val storeAction: Boolean,
-    val zendesk: Boolean,
-    val network: NetworkLogConfig,
-) {
-
-    companion object {
-        fun buildBased(): LogConfig = LogConfig(
-            imageLoader = BuildConfig.DEBUG,
-            storeAction = BuildConfig.DEBUG,
-            zendesk = BuildConfig.DEBUG,
-            network = NetworkLogConfig.buildBased(),
-        )
-
-        fun custom(): LogConfig = LogConfig(
-            imageLoader = false,
-            storeAction = false,
-            zendesk = false,
-            network = NetworkLogConfig.custom(),
-        )
-    }
+object LogConfig {
+    const val imageLoader: Boolean = false
+    const val storeAction: Boolean = false
+    const val zendesk: Boolean = false
+    val network: NetworkLogConfig = NetworkLogConfig
 }
 
-data class NetworkLogConfig(
-    val mercuryoService: Boolean,
-    val moonPayService: Boolean,
-    val tangemTechService: Boolean,
-    val blockchainSdkNetwork: Boolean,
-) {
-
-    companion object {
-        fun buildBased(): NetworkLogConfig = NetworkLogConfig(
-            mercuryoService = BuildConfig.DEBUG,
-            moonPayService = BuildConfig.DEBUG,
-            tangemTechService = BuildConfig.DEBUG,
-            blockchainSdkNetwork = BuildConfig.DEBUG,
-        )
-
-        fun custom(): NetworkLogConfig = NetworkLogConfig(
-            mercuryoService = false,
-            moonPayService = false,
-            tangemTechService = false,
-            blockchainSdkNetwork = true,
-        )
-    }
+object NetworkLogConfig {
+    const val mercuryoService: Boolean = false
+    const val moonPayService: Boolean = false
+    const val tangemTechService: Boolean = false
+    const val blockchainSdkNetwork: Boolean = false
 }
