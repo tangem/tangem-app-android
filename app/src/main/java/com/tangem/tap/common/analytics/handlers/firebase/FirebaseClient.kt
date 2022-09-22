49c4e2bb7083c2650301802975e8e815d7c9fc89
Tangem <git@tangem.com>
2022-09-23 02:14:22 +0300

    Updated on 2026-04-08

---
+++ app/src/main/java/com/tangem/tap/common/analytics/handlers/firebase/FirebaseClient.kt
@@ -0,0 +1,36 @@
+package com.tangem.tap.common.analytics.handlers.firebase
+
+import android.os.Bundle
+import androidx.core.os.bundleOf
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.crashlytics.ktx.crashlytics
+import com.google.firebase.ktx.Firebase
+import com.tangem.tap.common.analytics.api.ErrorEventLogger
+import com.tangem.tap.common.analytics.api.EventLogger
+
+/**
+[REDACTED_AUTHOR]
+ */
+interface FirebaseAnalyticsClient : EventLogger, ErrorEventLogger
+
+internal class FirebaseClient : FirebaseAnalyticsClient {
+
+    private val fbAnalytics = Firebase.analytics
+    private val fbCrashlytics = Firebase.crashlytics
+
+    override fun logEvent(event: String, params: Map<String, String>) {
+        fbAnalytics.logEvent(event, params.toBundle())
+    }
+
+    override fun logErrorEvent(error: Throwable, params: Map<String, String>) {
+        params.forEach { fbCrashlytics.setCustomKey(it.key, it.value) }
+        fbCrashlytics.recordException(error)
+    }
+
+    private fun Map<String, String>.toBundle(): Bundle = bundleOf(*this.toList().toTypedArray())
+
+    companion object {
+        const val ORDER_EVENT = FirebaseAnalytics.Event.PURCHASE
+    }
+}
