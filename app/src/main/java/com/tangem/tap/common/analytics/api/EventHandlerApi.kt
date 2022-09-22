6f926a66cd4a37956cc653e11ab020eac253b7cb
Tangem <git@tangem.com>
2022-09-23 02:14:22 +0300

    Updated on 2026-08-27

--- app/src/main/java/com/tangem/tap/common/analytics/api/EventHandlerApi.kt
+++ app/src/main/java/com/tangem/tap/common/analytics/api/EventHandlerApi.kt
@@ -1,19 +1,23 @@
-package com.tangem.tap.common.analytics
+package com.tangem.tap.common.analytics.api
 
 import com.shopify.buy3.Storefront
 import com.tangem.blockchain.common.BlockchainError
 import com.tangem.common.card.Card
 import com.tangem.common.core.TangemSdkError
+import com.tangem.tap.common.analytics.Analytics
+import com.tangem.tap.common.analytics.AnalyticsEvent
+import com.tangem.tap.common.analytics.AnalyticsParam
 import com.tangem.tap.common.extensions.filterNotNull
 
-interface EventHandler {
+/**
+[REDACTED_AUTHOR]
+ */
+interface AnalyticsEventHandler {
     fun handleEvent(
         event: String,
         params: Map<String, String> = emptyMap(),
     )
-}
 
-interface AnalyticsEventHandler : EventHandler {
     fun handleAnalyticsEvent(
         event: AnalyticsEvent,
         params: Map<String, String> = emptyMap(),
@@ -25,17 +29,11 @@ interface AnalyticsEventHandler : EventHandler {
         card: Card?,
         blockchain: String? = null,
         params: Map<String, String> = emptyMap(),
-    ): Map<String, String> {
-        return mapOf(
-            AnalyticsParam.FIRMWARE.param to card?.firmwareVersion?.stringValue,
-            AnalyticsParam.BATCH_ID.param to card?.batchId,
-            AnalyticsParam.BLOCKCHAIN.param to blockchain,
-        ).filterNotNull() + params
-    }
-}
-
-interface ShopifyOrderEventHandler {
-    fun handleShopifyOrderEvent(order: Storefront.Order)
+    ): Map<String, String> = mapOf(
+        AnalyticsParam.FIRMWARE.param to card?.firmwareVersion?.stringValue,
+        AnalyticsParam.BATCH_ID.param to card?.batchId,
+        AnalyticsParam.BLOCKCHAIN.param to blockchain,
+    ).filterNotNull() + params
 }
 
 interface ErrorEventHandler {
@@ -63,4 +61,8 @@ interface BlockchainSdkErrorEventHandler {
         params: Map<AnalyticsParam, String> = mapOf(),
         card: Card? = null,
     )
+}
+
+interface ShopifyOrderEventHandler {
+    fun handleShopifyOrderEvent(order: Storefront.Order)
 }
