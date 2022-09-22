e97cb41fdbc8b684c5d7da753d4661ecfc2b98bb
Tangem <git@tangem.com>
2022-09-23 02:14:22 +0300

    Updated on 2026-08-14

---
+++ app/src/main/java/com/tangem/tap/common/analytics/AnalyticsEventsLogger.kt
@@ -0,0 +1,26 @@
+package com.tangem.tap.common.analytics
+
+import com.tangem.common.json.MoshiJsonConverter
+import com.tangem.tap.common.analytics.api.ErrorEventLogger
+import com.tangem.tap.common.analytics.api.EventLogger
+import timber.log.Timber
+
+class AnalyticsEventsLogger(
+    private val name: String,
+    private val jsonConverter: MoshiJsonConverter,
+) : EventLogger, ErrorEventLogger {
+
+    override fun logEvent(event: String, params: Map<String, String>) {
+        Timber.d(jsonConverter.prettyPrint(PrintEventModel(name, event, params)))
+    }
+
+    override fun logErrorEvent(error: Throwable, params: Map<String, String>) {
+        Timber.e(error, jsonConverter.prettyPrint(PrintEventModel(name, "error", params)))
+    }
+}
+
+private data class PrintEventModel(
+    val client: String,
+    val event: String,
+    val params: Map<String, String>,
+)
