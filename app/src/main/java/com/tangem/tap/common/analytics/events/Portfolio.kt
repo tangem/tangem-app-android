2da18a0aaf9c1af70b97c9c3590a8294abab4c4f
Tangem <git@tangem.com>
2022-09-28 02:31:49 +0300

    Updated on 2026-09-14

---
+++ app/src/main/java/com/tangem/tap/common/analytics/events/Portfolio.kt
@@ -0,0 +1,15 @@
+package com.tangem.tap.common.analytics.events
+
+/**
+[REDACTED_AUTHOR]
+ */
+
+sealed class Portfolio(
+    event: String,
+    params: Map<String, String> = mapOf(),
+) : AnalyticsEvent("Portfolio", event, params) {
+
+    class Refreshed : Portfolio("Refreshed")
+    class ButtonManageTokens : Portfolio("Button - Manage Tokens")
+    class TokenTapped : Portfolio("Token is Tapped")
+}
