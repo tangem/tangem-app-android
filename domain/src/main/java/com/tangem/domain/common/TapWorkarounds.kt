7eae39d0dcdf3d8fce0643f6e5f2e410b75a0eb1
Tangem <git@tangem.com>
2022-10-01 20:16:49 +0500

    release/5.26.1

--- domain/src/main/java/com/tangem/domain/common/TapWorkarounds.kt
+++ domain/src/main/java/com/tangem/domain/common/TapWorkarounds.kt
@@ -16,8 +16,15 @@ object TapWorkarounds {
 
     val Card.isStart2Coin: Boolean
         get() = isStart2CoinIssuer(issuer.name)
+
     val Card.isSaltPay: Boolean
-        get() = saltPayCardIds.contains(cardId) || saltPayBatches.contains(batchId)
+        get() = isSaltPayVisa || isSaltPayTangem
+
+    val Card.isSaltPayVisa: Boolean
+        get() = saltPayVisaBatches.contains(batchId)
+
+    val Card.isSaltPayTangem: Boolean
+        get() = saltPayTangemCardIds.contains(cardId)
 
     val Card.isTestCard: Boolean
         get() = batchId == TEST_CARD_BATCH && cardId.startsWith(TEST_CARD_ID_STARTS_WITH)
@@ -60,7 +67,7 @@ object TapWorkarounds {
     )
 
     private val excludedIssuers = listOf(
-        "TTM BANK"
+        "TTM BANK",
     )
 
     private val tangemWalletBatches = listOf("AC01")
@@ -84,11 +91,9 @@ object TapWorkarounds {
         "AC01", "AC02", "CB95",
     )
 
-    val saltPayCardIds = listOf(
-        "AE02000000000194",
-        "AC03000000076195",
-        "AC79000000000012", // TODO: remove my testing CIDs
-        "AC79000000000004",// TODO: remove my testing CIDs
+    val saltPayTangemCardIds = listOf(
+        "AC01000000000015", // TODO: remove testing CIDs
+        "AC03000000000088", // TODO: remove testing CIDs
         "AC03000000076070",
         "AC03000000076088",
         "AC03000000076096",
@@ -105,17 +110,32 @@ object TapWorkarounds {
         "AC03000000076203",
         "AC03000000076211",
         "AC03000000076229",
-// [REDACTED_TODO_COMMENT]
+        // added 01.10.2022
+        "AC01000000033503",
+        "AC01000000033594",
+        "AC01000000033586",
+        "AC01000000034477",
+        "AC01000000032760",
+        "AC01000000033867",
+        "AC01000000032653",
+        "AC01000000032752",
+        "AC01000000034485",
+        "AC01000000033644",
+        "AC01000000037454",
+        "AC01000000037462",
     )
 
-    val saltPayBatches = listOf("AE02")
+    private val saltPayVisaBatches = listOf(
+        "AE02",
+        "AE03",
+    )
 
     val saltPayToken: Token
         get() = Token(
             name = "Wrapped xDAI",
-            "WxDAI",
-            "0x4346186e7461cB4DF06bCFCB4cD591423022e417",
-            18,
+            symbol = "WxDAI",
+            contractAddress = "0x4346186e7461cB4DF06bCFCB4cD591423022e417",
+            decimals = 18,
             id = "xdai",
         )
 }
