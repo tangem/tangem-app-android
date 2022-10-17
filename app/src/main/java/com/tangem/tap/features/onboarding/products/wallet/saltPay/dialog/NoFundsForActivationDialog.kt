9cab8a6c61ce912999e3b613553850ddd0f07c34
Tangem <git@tangem.com>
2022-10-17 19:57:07 +0500

    release/5.26.1

---
+++ app/src/main/java/com/tangem/tap/features/onboarding/products/wallet/saltPay/dialog/NoFundsForActivationDialog.kt
@@ -0,0 +1,33 @@
+package com.tangem.tap.features.onboarding.products.wallet.saltPay.dialog
+
+import android.app.Dialog
+import android.content.Context
+import androidx.appcompat.app.AlertDialog
+import com.tangem.tap.common.feedback.SupportInfo
+import com.tangem.tap.common.redux.global.GlobalAction
+import com.tangem.tap.store
+import com.tangem.wallet.R
+
+/**
+* [REDACTED_AUTHOR]
+ */
+class NoFundsForActivationDialog {
+    companion object {
+        fun create(context: Context): Dialog {
+            return AlertDialog.Builder(context).apply {
+                setTitle(R.string.saltpay_error_no_gas_title)
+                setMessage(R.string.saltpay_error_no_gas_message)
+// [REDACTED_TODO_COMMENT]
+                setPositiveButton(R.string.onboarding_supplement_button_kyc_waiting) { _, _ ->
+                    store.dispatch(GlobalAction.OpenChat(SupportInfo()))
+                }
+                setNegativeButton(R.string.common_cancel) { _, _ ->
+                }
+                setOnDismissListener {
+                    store.dispatch(GlobalAction.HideDialog)
+                }
+                setCancelable(false)
+            }.create()
+        }
+    }
+}
