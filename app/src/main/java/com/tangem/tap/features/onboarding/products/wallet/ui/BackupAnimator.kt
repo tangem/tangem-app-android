3c99bebd2f9828be6a52f94ea2bfaeef0f699dd7
Tangem <git@tangem.com>
2022-10-20 00:20:17 +0500

    Updated on 2026-06-05

---
+++ app/src/main/java/com/tangem/tap/features/onboarding/products/wallet/ui/BackupAnimator.kt
@@ -0,0 +1,292 @@
+package com.tangem.tap.features.onboarding.products.wallet.ui
+
+import com.google.android.material.button.MaterialButton
+import com.tangem.tap.common.extensions.show
+import com.tangem.tap.features.onboarding.products.wallet.redux.BackupState
+import com.tangem.tap.features.onboarding.products.wallet.ui.BackupCardType.FIRST_BACKUP
+import com.tangem.tap.features.onboarding.products.wallet.ui.BackupCardType.ORIGIN
+import com.tangem.tap.features.onboarding.products.wallet.ui.BackupCardType.SECOND_BACKUP
+import com.tangem.wallet.databinding.FragmentOnboardingWalletBinding
+
+/**
+[REDACTED_AUTHOR]
+ */
+interface BackupAnimator {
+    fun updateBackupState(backupState: BackupState)
+    fun setupCreateWalletState()
+    fun showBackupIntro(state: BackupState)
+    fun showScanOriginCard()
+    fun showAddBackupCards(state: BackupState, backupCardsCounts: Int)
+    fun showWritePrimaryCard(state: BackupState)
+    fun showWriteBackupCard(state: BackupState, backupCard: Int)
+    fun showSuccess(onEnd: () -> Unit)
+}
+
+class WalletBackupAnimator(
+    private val cardsWidget: WalletCardsWidget,
+) : BackupAnimator {
+
+    private val viewState: State = when (cardsWidget.leapfrogWidget.getViewsCount()) {
+        2 -> State.TwoCards
+        3 -> State.TreeCards
+        else -> throw UnsupportedOperationException()
+    }
+
+    private var currentStep = 0
+
+    override fun updateBackupState(backupState: BackupState) {
+    }
+
+    override fun setupCreateWalletState() {
+        currentStep = STEP_CREATE_WALLET
+
+        cardsWidget.toFolded(false)
+
+        ORIGIN.alpha(1f)
+        FIRST_BACKUP.alpha(1f)
+        SECOND_BACKUP.alpha(1f)
+    }
+
+    override fun showBackupIntro(state: BackupState) {
+        currentStep = STEP_BACKUP_INTRO
+
+        ORIGIN.show()
+        FIRST_BACKUP.show()
+        SECOND_BACKUP.show()
+
+        cardsWidget.toWelcome()
+    }
+
+    override fun showScanOriginCard() {
+        currentStep = STEP_SCAN_ORIGIN_CARD
+
+        cardsWidget.toFolded()
+    }
+
+    override fun showAddBackupCards(state: BackupState, backupCardsCounts: Int) {
+        when (backupCardsCounts) {
+            0 -> {
+                when (currentStep) {
+                    STEP_CREATE_WALLET -> {
+                        cardsWidget.toFolded(false) { cardsWidget.toFan() }
+
+                        FIRST_BACKUP.animateAlpha(0.6f, 0)
+                        SECOND_BACKUP.animateAlpha(0.2f, 400)
+                    }
+                    STEP_BACKUP_INTRO -> {
+                        cardsWidget.toFolded(duration = 200) { cardsWidget.toFan() }
+                        FIRST_BACKUP.animateAlpha(0.6f, 400, 250)
+                        SECOND_BACKUP.animateAlpha(0.2f, 400, 250)
+                    }
+                    STEP_SCAN_ORIGIN_CARD -> {
+                        cardsWidget.toFan()
+                        FIRST_BACKUP.animateAlpha(0.6f, 400, 250)
+                        SECOND_BACKUP.animateAlpha(0.2f, 400, 250)
+                    }
+                }
+            }
+            1 -> {
+                cardsWidget.toFan(false)
+                FIRST_BACKUP.alpha(1f)
+                SECOND_BACKUP.alpha(0.2f)
+            }
+            2 -> {
+                cardsWidget.toFan(false)
+                FIRST_BACKUP.alpha(1f)
+                SECOND_BACKUP.alpha(1f)
+            }
+        }
+
+        currentStep = STEP_ADD_BACKUP_CARDS
+    }
+
+    override fun showWritePrimaryCard(state: BackupState) {
+        currentStep = STEP_WRITE_PRIMARY_CARD
+
+        cardsWidget.toLeapfrog(
+            onEndFold = {
+                FIRST_BACKUP.alpha(0.4f)
+                SECOND_BACKUP.alpha(0.2f)
+            },
+        )
+    }
+
+    override fun showWriteBackupCard(state: BackupState, backupCard: Int) {
+        currentStep = STEP_WRITE_BACKUP_CARD
+
+        when (viewState) {
+            State.TwoCards -> {
+                FIRST_BACKUP.alpha(1f)
+                cardsWidget.leapfrogWidget.leap {
+                    ORIGIN.alpha(0.4f)
+                }
+            }
+            State.TreeCards -> {
+                when (backupCard) {
+                    1 -> {
+                        FIRST_BACKUP.alpha(1f)
+                        cardsWidget.leapfrogWidget.leap {
+                            SECOND_BACKUP.alpha(0.4f)
+                            ORIGIN.alpha(0.2f)
+                        }
+                    }
+                    2 -> {
+                        SECOND_BACKUP.alpha(1f)
+                        cardsWidget.leapfrogWidget.leap {
+                            ORIGIN.alpha(0.4f)
+                            FIRST_BACKUP.alpha(0.2f)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun showSuccess(onEnd: () -> Unit) {
+        currentStep = STEP_SUCCESS
+        cardsWidget.leapfrogWidget.fold(onEnd = onEnd)
+    }
+
+    private enum class State {
+        TreeCards,
+        TwoCards
+    }
+
+    private fun BackupCardType.animateAlpha(
+        alpha: Float,
+        duration: Long = 0L,
+        startDelay: Long = 0L,
+    ) {
+        when (this) {
+            ORIGIN -> {
+                cardsWidget.getOriginCardView().animate().apply {
+                    alpha(alpha)
+                    this.startDelay = startDelay
+                    this.duration = duration
+                }
+            }
+            FIRST_BACKUP -> {
+                cardsWidget.getFirstBackupCardView().animate().apply {
+                    alpha(alpha)
+                    this.startDelay = startDelay
+                    this.duration = duration
+                }
+            }
+            SECOND_BACKUP -> {
+                if (viewState != State.TreeCards) return
+                cardsWidget.getSecondBackupCardView().animate().apply {
+                    alpha(alpha)
+                    this.startDelay = startDelay
+                    this.duration = duration
+                }
+            }
+        }
+    }
+
+    private fun BackupCardType.alpha(alpha: Float) {
+        when (this) {
+            ORIGIN -> cardsWidget.getOriginCardView().alpha = alpha
+            FIRST_BACKUP -> cardsWidget.getFirstBackupCardView().alpha = alpha
+            SECOND_BACKUP -> {
+                if (viewState != State.TreeCards) return
+                cardsWidget.getSecondBackupCardView().alpha = alpha
+            }
+        }
+    }
+
+    private fun BackupCardType.show() {
+        when (this) {
+            ORIGIN -> cardsWidget.getOriginCardView().show()
+            FIRST_BACKUP -> cardsWidget.getFirstBackupCardView().show()
+            SECOND_BACKUP -> {
+                if (viewState != State.TreeCards) return
+                cardsWidget.getSecondBackupCardView().show()
+            }
+        }
+    }
+
+    companion object {
+        private const val STEP_CREATE_WALLET = 0
+        private const val STEP_BACKUP_INTRO = 1
+        private const val STEP_SCAN_ORIGIN_CARD = 2
+        private const val STEP_ADD_BACKUP_CARDS = 3
+        private const val STEP_WRITE_PRIMARY_CARD = 4
+        private const val STEP_WRITE_BACKUP_CARD = 5
+        private const val STEP_SUCCESS = 6
+    }
+}
+
+class TestBackupAnimation(
+    private val animator: BackupAnimator,
+    binding: FragmentOnboardingWalletBinding,
+) : BackupAnimator {
+
+    private lateinit var state: BackupState
+    private var steps = 0
+
+    init {
+        init(binding, this)
+    }
+
+    fun setStep(step: Int, onStepUpdate: (Int) -> Unit = {}) {
+        steps = step
+        when (steps) {
+            0 -> setupCreateWalletState()
+            1 -> showBackupIntro(state)
+            2 -> showScanOriginCard()
+            3 -> showAddBackupCards(state, 0)
+            4 -> showAddBackupCards(state, 1)
+            5 -> showAddBackupCards(state, 2)
+            6 -> showWritePrimaryCard(state)
+            7 -> showWriteBackupCard(state, 1)
+            8 -> showWriteBackupCard(state, 2)
+            else -> setStep(0)
+        }
+        onStepUpdate(steps)
+    }
+
+    fun onNext(onStepUpdate: (Int) -> Unit) {
+        steps++
+        setStep(steps, onStepUpdate)
+    }
+
+    override fun updateBackupState(backupState: BackupState) {
+        this.state = backupState
+    }
+
+    override fun setupCreateWalletState() {
+        animator.setupCreateWalletState()
+    }
+
+    override fun showBackupIntro(state: BackupState) {
+        animator.showBackupIntro(state)
+    }
+
+    override fun showScanOriginCard() {
+        animator.showScanOriginCard()
+    }
+
+    override fun showAddBackupCards(state: BackupState, backupCardsCounts: Int) {
+        animator.showAddBackupCards(state, backupCardsCounts)
+    }
+
+    override fun showWritePrimaryCard(state: BackupState) {
+        animator.showWritePrimaryCard(state)
+    }
+
+    override fun showWriteBackupCard(state: BackupState, backupCard: Int) {
+        animator.showWriteBackupCard(state, backupCard)
+    }
+
+    override fun showSuccess(onEnd: () -> Unit) {
+        animator.showSuccess(onEnd)
+    }
+
+    companion object {
+        fun init(binding: FragmentOnboardingWalletBinding, animator: TestBackupAnimation) {
+            val button = MaterialButton(binding.onboardingRoot.context).apply { text = "Next" }
+            button.setOnClickListener { animator.onNext { button.text = "$it" } }
+            binding.onboardingRoot.addView(button, 0)
+        }
+    }
+}
