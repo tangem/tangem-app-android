b9cece3f51b00d48a85dc48fa359d18e892b917f
Tangem <git@tangem.com>
2022-10-20 12:15:38 +0500

    release/5.26.1

---
+++ app/src/main/java/com/tangem/tap/features/onboarding/OnboardingMenuProvider.kt
@@ -0,0 +1,27 @@
+package com.tangem.tap.features.onboarding
+
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.core.view.MenuProvider
+import com.tangem.tap.common.feedback.SupportInfo
+import com.tangem.tap.common.redux.global.GlobalAction
+import com.tangem.tap.store
+import com.tangem.wallet.R
+
+/**
+* [REDACTED_AUTHOR]
+ */
+class OnboardingMenuProvider : MenuProvider {
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.menu_onboarding, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean = when (menuItem.itemId) {
+        R.id.menu_item_chat_support -> {
+            store.dispatch(GlobalAction.OpenChat(SupportInfo()))
+            true
+        }
+        else -> false
+    }
+}
