488c99e244f590ebeea511487d8e664f5733ac9c
Tangem <git@tangem.com>
2022-10-24 21:31:05 +0500

    release/5.26.1

--- app/src/main/java/com/tangem/tap/features/onboarding/OnboardingSaltPayHelper.kt
+++ app/src/main/java/com/tangem/tap/features/onboarding/OnboardingSaltPayHelper.kt
@@ -3,10 +3,10 @@ package com.tangem.tap.features.onboarding
 import com.tangem.common.services.Result
 import com.tangem.domain.common.ScanResponse
 import com.tangem.domain.common.extensions.successOr
-import com.tangem.tap.features.onboarding.products.wallet.saltPay.SaltPayRegistrationManager
+import com.tangem.tap.features.onboarding.products.wallet.saltPay.SaltPayActivationManager
 import com.tangem.tap.features.onboarding.products.wallet.saltPay.message.SaltPayRegistrationError
 import com.tangem.tap.features.onboarding.products.wallet.saltPay.redux.SaltPayRegistrationStep
-import com.tangem.tap.features.onboarding.products.wallet.saltPay.redux.updateSaltPayStatus
+import com.tangem.tap.features.onboarding.products.wallet.saltPay.redux.updateActivationStatus
 
 /**
 * [REDACTED_AUTHOR]
@@ -16,10 +16,10 @@ class OnboardingSaltPayHelper {
 
         suspend fun isOnboardingCase(
             scanResponse: ScanResponse,
-            manager: SaltPayRegistrationManager,
+            manager: SaltPayActivationManager,
         ): Result<Boolean> {
             return try {
-                val status = manager.updateSaltPayStatus(
+                val status = manager.updateActivationStatus(
                     amountToClaim = null,
                     step = SaltPayRegistrationStep.None,
                 ).successOr { return it }
@@ -36,12 +36,12 @@ class OnboardingSaltPayHelper {
 
         suspend fun testProceedToOnboarding(
             scanResponse: ScanResponse,
-            manager: SaltPayRegistrationManager,
+            manager: SaltPayActivationManager,
         ): Result<Boolean> = Result.Success(true)
 
         suspend fun testProceedToMainScreen(
             scanResponse: ScanResponse,
-            manager: SaltPayRegistrationManager,
+            manager: SaltPayActivationManager,
         ): Result<Boolean> = Result.Success(false)
     }
 }
