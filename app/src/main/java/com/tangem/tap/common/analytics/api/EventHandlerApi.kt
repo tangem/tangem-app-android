ca8addad1bc2d12d34d5b37b55c1d37d6a9b87f7
Tangem <git@tangem.com>
2022-10-25 21:35:45 +0500

    Updated on 2026-09-25

--- app/src/main/java/com/tangem/tap/common/analytics/api/EventHandlerApi.kt
+++ app/src/main/java/com/tangem/tap/common/analytics/api/EventHandlerApi.kt
@@ -7,6 +7,7 @@ import com.tangem.common.core.TangemSdkError
 import com.tangem.tap.common.analytics.AnalyticsAnOld
 import com.tangem.tap.common.analytics.AnalyticsEventAnOld
 import com.tangem.tap.common.analytics.AnalyticsParamAnOld
+import com.tangem.tap.common.analytics.events.AnalyticsEvent
 import com.tangem.tap.common.extensions.filterNotNull
 
 /**
@@ -19,11 +20,25 @@ interface AnalyticsEventHandler {
     )
 
     fun handleAnalyticsEvent(
+        event: AnalyticsEvent,
+        card: Card? = null,
+        blockchain: String? = null,
+    ) {
+        handleEvent(
+            event = prepareEventString(event.category, event.event),
+            params = prepareParams(card, blockchain, event.params),
+        )
+    }
+
+    @Deprecated("Migrate to AnalyticsEvent")
+    fun handleAnalyticsEvent(
         event: AnalyticsEventAnOld,
         params: Map<String, String> = emptyMap(),
         card: Card? = null,
         blockchain: String? = null,
-    )
+    ) {
+        handleEvent(event.event, prepareParams(card, blockchain, params))
+    }
 
     fun prepareParams(
         card: Card?,
@@ -34,6 +49,10 @@ interface AnalyticsEventHandler {
         AnalyticsParamAnOld.BATCH_ID.param to card?.batchId,
         AnalyticsParamAnOld.BLOCKCHAIN.param to blockchain,
     ).filterNotNull() + params
+
+    fun prepareEventString(category: String, event: String): String {
+        return "[$category] $event"
+    }
 }
 
 interface ErrorEventHandler {
--- app/src/main/java/com/tangem/tap/common/analytics/handlers/amplitude/AmplitudeAnalyticsHandler.kt
+++ app/src/main/java/com/tangem/tap/common/analytics/handlers/amplitude/AmplitudeAnalyticsHandler.kt
@@ -1,7 +1,5 @@
 package com.tangem.tap.common.analytics.handlers.amplitude
 
-import com.tangem.common.card.Card
-import com.tangem.tap.common.analytics.AnalyticsEventAnOld
 import com.tangem.tap.common.analytics.api.AnalyticsEventHandler
 
 class AmplitudeAnalyticsHandler(
@@ -10,14 +8,5 @@ class AmplitudeAnalyticsHandler(
 
     override fun handleEvent(event: String, params: Map<String, String>) {
         client.logEvent(event, params)
-    }
-
-    override fun handleAnalyticsEvent(
-        event: AnalyticsEventAnOld,
-        params: Map<String, String>,
-        card: Card?,
-        blockchain: String?,
-    ) {
-        handleEvent(event.event, prepareParams(card, blockchain, params))
     }
 }
