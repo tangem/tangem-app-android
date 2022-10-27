be1d4a482e0b8e0623e4c7df6bcd19a56513239c
Tangem <git@tangem.com>
2022-10-27 18:36:42 +0500

    Updated on 2026-09-23

---
+++ app/src/main/java/com/tangem/tap/common/analytics/GlobalAnalyticsEventHandler.kt
@@ -0,0 +1,18 @@
+package com.tangem.tap.common.analytics
+
+import com.tangem.tap.common.analytics.api.AnalyticsEventHandler
+import com.tangem.tap.common.analytics.api.ErrorEventHandler
+import com.tangem.tap.common.analytics.api.SdkErrorEventHandler
+import com.tangem.tap.common.analytics.api.ShopifyOrderEventHandler
+
+interface GlobalAnalyticsEventHandler : AnalyticsEventHandler,
+    ErrorEventHandler,
+    SdkErrorEventHandler,
+    ShopifyOrderEventHandler {
+
+    fun addHandler(name: String, handler: AnalyticsEventHandler)
+
+    fun removeHandler(name: String): AnalyticsEventHandler?
+
+    fun attachToAllEvents(key: String, value: String)
+}
--- app/src/main/java/com/tangem/tap/common/analytics/handlers/appsFlyer/AppsFlyerLogClient.kt
+++ app/src/main/java/com/tangem/tap/common/analytics/handlers/appsFlyer/AppsFlyerLogClient.kt
@@ -1,13 +1,16 @@
 package com.tangem.tap.common.analytics.handlers.appsFlyer
 
+import com.tangem.common.json.MoshiJsonConverter
 import com.tangem.tap.common.analytics.AnalyticsEventsLogger
 
 /**
 [REDACTED_AUTHOR]
  */
 internal class AppsFlyerLogClient(
-    private val logger: AnalyticsEventsLogger,
+    jsonConverter: MoshiJsonConverter,
 ) : AppsFlyerAnalyticsClient {
+
+    private val logger = AnalyticsEventsLogger(AppsFlyerAnalyticsHandler.ID, jsonConverter)
 
     override fun logEvent(event: String, params: Map<String, String>) {
         logger.logEvent(event, params)
