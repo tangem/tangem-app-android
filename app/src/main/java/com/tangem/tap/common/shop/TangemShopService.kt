001857a89fdce0fbe18a4bb28ae8e7915ab5ccb8
Tangem <git@tangem.com>
2022-10-28 12:02:07 +0500

    Updated on 2026-06-27

--- app/src/main/java/com/tangem/tap/common/shop/TangemShopService.kt
+++ app/src/main/java/com/tangem/tap/common/shop/TangemShopService.kt
@@ -4,8 +4,7 @@ import android.app.Application
 import android.content.Intent
 import com.google.android.gms.wallet.PaymentData
 import com.shopify.buy3.Storefront
-import com.tangem.common.extensions.ifNotNull
-import com.tangem.tap.common.analytics.GlobalAnalyticsEventHandler
+import com.tangem.tap.common.analytics.Analytics
 import com.tangem.tap.common.extensions.filterNotNull
 import com.tangem.tap.common.shop.data.ProductType
 import com.tangem.tap.common.shop.data.TangemProduct
@@ -196,10 +195,10 @@ class TangemShopService(application: Application, shopifyShop: ShopifyShop) {
         return checkouts[productType]!!.webUrl
     }
 
-    suspend fun waitForCheckout(productType: ProductType, analyticsHandler: GlobalAnalyticsEventHandler?) {
+    suspend fun waitForCheckout(productType: ProductType) {
         val result = shopifyService.checkout(true, checkouts[productType]!!.id)
         result.onSuccess {
-            ifNotNull(analyticsHandler, it.order) { handler, order -> handler.send(order) }
+            it.order?.let { Analytics.send(it) }
         }
     }
 
