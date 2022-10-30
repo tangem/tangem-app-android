7a4af64c3df0a61e780358cbed0a6c20b909a21f
Tangem <git@tangem.com>
2022-10-30 20:44:26 +0500

    Updated on 2026-05-07

--- app/src/main/java/com/tangem/tap/common/analytics/handlers/amplitude/AmplitudeAnalyticsHandler.kt
+++ app/src/main/java/com/tangem/tap/common/analytics/handlers/amplitude/AmplitudeAnalyticsHandler.kt
@@ -20,7 +20,7 @@ class AmplitudeAnalyticsHandler(
     class Builder : AnalyticsHandlerBuilder {
         override fun build(data: AnalyticsHandlerBuilder.Data): AnalyticsEventHandler? = when {
             !data.isDebug -> AmplitudeClient(data.application, data.config.amplitudeApiKey)
-            data.isDebug && data.logConfig.appsFlyer -> AmplitudeLogClient(data.jsonConverter)
+            data.isDebug && data.logConfig.amplitude -> AmplitudeLogClient(data.jsonConverter)
             else -> null
         }?.let { AmplitudeAnalyticsHandler(it) }
     }
