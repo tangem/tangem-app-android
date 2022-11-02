25c5145cde3a8cf11af5613a31620c9e6a72fd22
Tangem <git@tangem.com>
2022-11-02 17:52:24 +0500

    Updated on 2026-07-07

--- app/src/main/java/com/tangem/tap/common/analytics/events/Shop.kt
+++ app/src/main/java/com/tangem/tap/common/analytics/events/Shop.kt
@@ -1,9 +1,6 @@
 package com.tangem.tap.common.analytics.events
 
-import com.shopify.buy3.Storefront
-import com.tangem.common.Converter
 import com.tangem.tap.common.extensions.filterNotNull
-import com.tangem.tap.common.shop.data.ProductType
 
 /**
 [REDACTED_AUTHOR]
@@ -15,36 +12,18 @@ sealed class Shop(
 
     class ScreenOpened : IntroductionProcess("Shop Screen Opened")
 
-    class Purchased(params: Map<String, String>) : Shop(
+    class Purchased(sku: String, count: String, amount: String, couponCode: String?) : Shop(
         event = "Purchased",
-        params = params,
+        params = mapOf(
+            "SKU" to sku,
+            "Count" to count,
+            "Amount" to amount,
+            "Coupon Code" to couponCode,
+        ).filterNotNull(),
     )
 
     class Redirected(partnerName: String) : Shop(
         event = "Redirected",
         params = mapOf("Partner" to partnerName),
     )
-}
-
-class OrderToParamsConverter : Converter<Pair<Storefront.Order, ProductType>, Map<String, String>> {
-    override fun convert(value: Pair<Storefront.Order, ProductType>): Map<String, String> {
-        val order = value.first
-        val productType = value.second
-
-        val code = (order.discountApplications.edges.firstOrNull()?.node as? Storefront.DiscountCodeApplication)?.code
-        val sku = order.lineItems.edges.firstOrNull()?.node?.variant?.sku ?: productType.sku
-
-        val count = when (productType) {
-            ProductType.WALLET_2_CARDS -> "2"
-            ProductType.WALLET_3_CARDS -> "3"
-        }
-        val amount = "${order.totalPriceV2.amount} ${order.totalPriceV2.currencyCode.name}"
-
-        return mapOf(
-            "Coupon Code" to code,
-            "SKU" to sku,
-            "Count" to count,
-            "Amount" to amount,
-        ).filterNotNull()
-    }
 }
