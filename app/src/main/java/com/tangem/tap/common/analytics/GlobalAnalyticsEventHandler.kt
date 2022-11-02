1788f64bddb1c71061db8728527e81e197dc193f
Tangem <git@tangem.com>
2022-11-02 19:26:45 +0500

    Updated on 2026-09-21

--- app/src/main/java/com/tangem/tap/common/analytics/GlobalAnalyticsEventHandler.kt
+++ app/src/main/java/com/tangem/tap/common/analytics/GlobalAnalyticsEventHandler.kt
@@ -1,18 +1,21 @@
 package com.tangem.tap.common.analytics
 
+import com.tangem.tap.common.analytics.api.AnalyticsEventFilter
 import com.tangem.tap.common.analytics.api.AnalyticsEventHandler
 import com.tangem.tap.common.analytics.api.ErrorEventHandler
 import com.tangem.tap.common.analytics.api.SdkErrorEventHandler
-import com.tangem.tap.common.analytics.api.ShopifyOrderEventHandler
 
 interface GlobalAnalyticsEventHandler : AnalyticsEventHandler,
     ErrorEventHandler,
-    SdkErrorEventHandler,
-    ShopifyOrderEventHandler {
+    SdkErrorEventHandler {
 
     fun addHandler(name: String, handler: AnalyticsEventHandler)
 
     fun removeHandler(name: String): AnalyticsEventHandler?
+
+    fun addFilter(filter: AnalyticsEventFilter)
+
+    fun removeFilter(filter: AnalyticsEventFilter)
 
     fun attachToAllEvents(key: String, value: String)
 }
