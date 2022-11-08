bf415ad8b9c9aa5bdddc6fa638430b262819e076
Tangem <git@tangem.com>
2022-11-08 18:37:46 +0500

    Updated on 2026-07-07

--- app/src/main/java/com/tangem/tap/common/analytics/api/EventFilterApi.kt
+++ app/src/main/java/com/tangem/tap/common/analytics/api/EventFilterApi.kt
@@ -4,10 +4,23 @@ import com.tangem.tap.common.analytics.events.AnalyticsEvent
 
 /**
 [REDACTED_AUTHOR]
+ * The filter is used to separate events to send and determine which handler it can be processed with.
  */
 interface AnalyticsEventFilter {
+    /**
+     * Recognizes the event and checks if the given filter can be applied to the event.
+     */
     fun canBeAppliedTo(event: AnalyticsEvent): Boolean
+
+    /**
+     * An internal filter check that, on external or internal conditions, recognizes the possibility of
+     * sending an event.
+     */
     fun canBeSent(event: AnalyticsEvent): Boolean
+
+    /**
+     * Performs a check to see if the event can be dispatched by a specific handler
+     */
     fun canBeConsumedByHandler(handler: AnalyticsEventHandler, event: AnalyticsEvent): Boolean
 }
 
