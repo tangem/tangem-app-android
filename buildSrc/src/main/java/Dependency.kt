605e1f2426e3476af39cf10a11aa660b896ed60b
Tangem <git@tangem.com>
2022-11-11 15:25:06 +0300

    Updated on 2026-09-21

--- buildSrc/src/main/java/Dependency.kt
+++ buildSrc/src/main/java/Dependency.kt
@@ -42,6 +42,7 @@ object Library {
         "com.google.android.gms:play-services-wallet:" + Versions.googlePlayServicesWallet
     const val hilt = "com.google.dagger:hilt-android:" + Versions.hilt
     const val hiltKapt = "com.google.dagger:hilt-compiler:" + Versions.hilt
+    const val jodatime = "net.danlew:android.joda:" + Versions.jodatime
     const val kotsonGsonExt = "com.github.salomonbrys.kotson:kotson:" + Versions.kotsonGsonExt
     const val lottie = "com.airbnb.android:lottie:" + Versions.lottie
     const val materialComponent = "com.google.android.material:material:" + Versions.googleMaterialComponent
