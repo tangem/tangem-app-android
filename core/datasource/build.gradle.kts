4f3912b4b8d03910746945e0a651385476cb09af
Tangem <git@tangem.com>
2022-11-11 14:07:23 +0300

    Updated on 2026-09-25

---
+++ core/datasource/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+
+    defaultConfig {
+        compileSdk = AppConfig.compileSdkVersion
+        minSdk = AppConfig.minSdkVersion
+        targetSdk = AppConfig.targetSdkVersion
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+        isCoreLibraryDesugaringEnabled = false
+    }
+}
+
+dependencies {
+
+    /** DI */
+    implementation(Library.hilt)
+    implementation(Library.hiltKapt)
+
+    /** Coroutines */
+    implementation(Library.coroutine)
+
+    /** Logging */
+    implementation(Library.timber)
+
+    /** Network */
+    implementation(Library.retrofit)
+    implementation(Library.retrofitMoshiConverter)
+    implementation(Library.moshi)
+    implementation(Library.moshiKotlin)
+    implementation(Library.okHttp)
+    implementation(Library.okHttpLogging)
+}
