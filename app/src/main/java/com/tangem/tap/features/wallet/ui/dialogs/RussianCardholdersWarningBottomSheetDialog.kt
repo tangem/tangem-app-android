d797fa35d1cd57e5429159f31082693bdd862e15
Tangem <git@tangem.com>
2022-11-15 00:17:49 +0500

    Updated on 2026-03-27

--- app/src/main/java/com/tangem/tap/features/wallet/ui/dialogs/RussianCardholdersWarningBottomSheetDialog.kt
+++ app/src/main/java/com/tangem/tap/features/wallet/ui/dialogs/RussianCardholdersWarningBottomSheetDialog.kt
@@ -5,14 +5,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.tangem.tap.common.analytics.Analytics
+import com.tangem.tap.common.analytics.events.AnalyticsParam
+import com.tangem.tap.common.analytics.events.Onboarding
 import com.tangem.tap.common.analytics.events.Token
 import com.tangem.tap.common.extensions.dispatchDialogHide
+import com.tangem.tap.common.extensions.dispatchOpenUrl
 import com.tangem.tap.common.redux.navigation.NavigationAction
 import com.tangem.tap.features.wallet.redux.WalletAction
+import com.tangem.tap.features.wallet.redux.models.WalletDialog
 import com.tangem.tap.store
 import com.tangem.wallet.databinding.DialogRussiansCardholdersWarningBinding
 
-class RussianCardholdersWarningBottomSheetDialog(context: Context) : BottomSheetDialog(context) {
+class RussianCardholdersWarningBottomSheetDialog(
+    context: Context,
+    private val dialogData: WalletDialog.RussianCardholdersWarningDialog.Data?,
+) : BottomSheetDialog
+    (context) {
 
     private var binding: DialogRussiansCardholdersWarningBinding? = null
 
@@ -32,7 +40,13 @@ class RussianCardholdersWarningBottomSheetDialog(context: Context) : BottomSheet
         }
 
         binding?.btnYes?.setOnClickListener {
-            store.dispatch(WalletAction.TradeCryptoAction.Buy(checkUserLocation = false))
+            if (dialogData != null) {
+                val currencyType = AnalyticsParam.CurrencyType.Blockchain(dialogData.blockchain)
+                Analytics.send(Onboarding.Topup.ButtonBuyCrypto(currencyType))
+                store.dispatchOpenUrl(dialogData.topUpUrl)
+            } else {
+                store.dispatch(WalletAction.TradeCryptoAction.Buy(checkUserLocation = false))
+            }
             dismiss()
         }
         binding?.btnNo?.setOnClickListener {
