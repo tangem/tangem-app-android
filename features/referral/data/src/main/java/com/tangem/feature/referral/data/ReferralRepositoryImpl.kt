6019eee2f12f548c314e4decd813c6835680d278
Tangem <git@tangem.com>
2022-11-15 10:26:15 +0300

    Updated on 2026-07-07

---
+++ features/referral/data/src/main/java/com/tangem/feature/referral/data/ReferralRepositoryImpl.kt
@@ -0,0 +1,43 @@
+package com.tangem.feature.referral.data
+
+import com.tangem.datasource.api.referral.ReferralApi
+import com.tangem.datasource.api.referral.models.StartReferralBody
+import com.tangem.feature.referral.converters.ReferralConverter
+import com.tangem.feature.referral.domain.ReferralRepository
+import com.tangem.feature.referral.domain.models.ReferralData
+import com.tangem.utils.coroutines.CoroutineDispatcherProvider
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+internal class ReferralRepositoryImpl @Inject constructor(
+    private val referralApi: ReferralApi,
+    private val referralConverter: ReferralConverter,
+    private val coroutineDispatcher: CoroutineDispatcherProvider,
+) : ReferralRepository {
+
+    override suspend fun getReferralStatus(walletId: String): ReferralData {
+        return withContext(coroutineDispatcher.io) {
+            referralConverter.convert(referralApi.getReferralStatus(walletId))
+        }
+    }
+
+    override suspend fun startReferral(
+        walletId: String,
+        networkId: String,
+        tokenId: String,
+        address: String,
+    ): ReferralData {
+        return withContext(coroutineDispatcher.io) {
+            referralConverter.convert(
+                referralApi.startReferral(
+                    StartReferralBody(
+                        walletId = walletId,
+                        networkId = networkId,
+                        tokenId = tokenId,
+                        address = address,
+                    ),
+                ),
+            )
+        }
+    }
+}
