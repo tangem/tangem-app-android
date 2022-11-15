6f670546e55480d46632d21eac6c1fdd7465555c
Tangem <git@tangem.com>
2022-11-15 17:26:57 +0300

    Updated on 2026-02-11

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,15 @@ include(":app")
 include(":domain")
 include(":network")
 include(":common")
+
+// region Core modules
 include(":core:datasource")
 include(":core:utils")
+include(":core:ui")
+// endregion Core modules
+
+// region Feature modules
 include(":features:referral:data")
 include(":features:referral:domain")
 include(":features:referral:presentation")
+// endregion Feature modules
