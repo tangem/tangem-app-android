2d310a83d93f2757634c38a0f84fe77ce9505bb6
Tangem <git@tangem.com>
2022-11-16 16:28:08 +0300

    Updated on 2026-05-07

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -109,6 +109,7 @@ dependencies {
     implementation(project(":domain"))
     implementation(project(":network"))
     implementation(project(":common"))
+    implementation(project(":libs:crypto"))
 
     /** AndroidX libraries */
     implementation(AndroidX.coreKtx)
