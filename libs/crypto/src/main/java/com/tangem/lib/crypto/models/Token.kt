9e8a7ef21e1406b9f40e63978106d7d42348b57a
Tangem <git@tangem.com>
2022-11-17 21:46:18 +0300

    Updated on 2026-08-19

--- libs/crypto/src/main/java/com/tangem/lib/crypto/models/Token.kt
+++ libs/crypto/src/main/java/com/tangem/lib/crypto/models/Token.kt
@@ -1,10 +1,10 @@
-package com.tangem.crypto.models
+package com.tangem.lib.crypto.models
 
 data class Token(
     val id: String,
     val name: String,
     val symbol: String,
-    val networkId: String?,
+    val networkId: String,
     val contractAddress: String?,
     val decimalCount: Int?,
 )
