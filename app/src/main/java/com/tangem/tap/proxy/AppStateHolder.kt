d0de9d71ae093546163f271798ddfbfeedef8ac2
Tangem <git@tangem.com>
2022-11-18 18:06:25 +0300

    Updated on 2026-06-17

--- app/src/main/java/com/tangem/tap/proxy/AppStateHolder.kt
+++ app/src/main/java/com/tangem/tap/proxy/AppStateHolder.kt
@@ -2,10 +2,12 @@ package com.tangem.tap.proxy
 
 import com.tangem.common.card.Card
 import com.tangem.domain.common.ScanResponse
+import com.tangem.tap.features.wallet.redux.WalletState
 
-class CardStateHolder {
+class AppStateHolder {
 
     val scanResponse: ScanResponse? = null
+    val walletState: WalletState? = null
 
     fun getActualCard(): Card? {
         return scanResponse?.card
