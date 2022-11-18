e6d80adc6c05c092b70300ad31264a1be258449e
Tangem <git@tangem.com>
2022-11-18 13:00:15 +0300

    release/5.26.1

---
+++ features/referral/presentation/src/main/java/com/tangem/feature/referral/ui/ParticipateBottomBlock.kt
@@ -0,0 +1,50 @@
+package com.tangem.feature.referral.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.tangem.core.ui.components.PrimaryEndIconButton
+import com.tangem.core.ui.res.TangemTheme
+import com.tangem.feature.referral.presentation.R
+
+@Preview(widthDp = 360, showBackground = true)
+@Composable
+fun Preview_ParticipateBottomBlock_InLightTheme() {
+    TangemTheme(isDarkTheme = false) {
+        Column(Modifier.background(MaterialTheme.colors.primary)) {
+            ParticipateBottomBlock(onAgreementClicked = {}, onParticipateClicked = {})
+        }
+    }
+}
+
+@Preview(widthDp = 360, showBackground = true)
+@Composable
+fun Preview_ParticipateBottomBlock_InDarkTheme() {
+    TangemTheme(isDarkTheme = true) {
+        Column(Modifier.background(MaterialTheme.colors.primary)) {
+            ParticipateBottomBlock(onAgreementClicked = {}, onParticipateClicked = {})
+        }
+    }
+}
+
+@Composable
+fun ParticipateBottomBlock(onAgreementClicked: () -> Unit, onParticipateClicked: () -> Unit) {
+    Column {
+        AgreementText(
+            firstPartResId = R.string.referral_tou_not_enroled_prefix,
+            onClicked = onAgreementClicked,
+        )
+        PrimaryEndIconButton(
+            modifier = Modifier.padding(all = dimensionResource(id = R.dimen.spacing16)),
+            text = stringResource(id = R.string.referral_button_participate),
+            iconResId = R.drawable.ic_tangem,
+            onClicked = onParticipateClicked,
+        )
+    }
+}
