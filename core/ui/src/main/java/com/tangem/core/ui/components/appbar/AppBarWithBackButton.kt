a05a9fddabf4a7ff59c997ac0dfb5ac51b7413b5
Tangem <git@tangem.com>
2022-11-19 17:02:42 +0300

    Updated on 2026-09-25

--- core/ui/src/main/java/com/tangem/core/ui/components/appbar/AppBarWithBackButton.kt
+++ core/ui/src/main/java/com/tangem/core/ui/components/appbar/AppBarWithBackButton.kt
@@ -1,6 +1,5 @@
-package com.tangem.core.ui.components
+package com.tangem.core.ui.components.appbar
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
@@ -48,7 +47,6 @@ fun PreviewAppBarWithBackButtonInDarkTheme() {
 fun AppBarWithBackButton(text: String? = null, onBackClick: () -> Unit) {
     Row(
         modifier = Modifier
-            .background(MaterialTheme.colors.primaryVariant)
             .padding(all = dimensionResource(R.dimen.spacing16))
             .fillMaxWidth(),
         horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.spacing16)),
