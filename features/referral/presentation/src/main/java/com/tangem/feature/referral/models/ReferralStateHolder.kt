3931cfe3d601734a8db6281a1c1ab0a78fcbff77
Tangem <git@tangem.com>
2022-11-21 17:21:57 +0300

    release/5.27.0

--- features/referral/presentation/src/main/java/com/tangem/feature/referral/models/ReferralStateHolder.kt
+++ features/referral/presentation/src/main/java/com/tangem/feature/referral/models/ReferralStateHolder.kt
@@ -3,36 +3,39 @@ package com.tangem.feature.referral.models
 data class ReferralStateHolder(
     val headerState: HeaderState,
     val referralInfoState: ReferralInfoState,
-    val effects: Effects,
-    val agreementBottomSheetState: AgreementBottomSheetState
+    val errorToast: ErrorToast,
 ) {
 
     data class HeaderState(val onBackClicked: () -> Unit)
 
-    sealed interface ReferralInfoContentState
+    sealed interface ReferralInfoContentState {
+        val award: String
+        val discount: String
+        val url: String
+    }
 
     sealed interface ReferralInfoState {
-
         data class ParticipantContent(
-            val award: String,
+            override val award: String,
             val address: String,
-            val discount: String,
-            val onParticipateClicked: () -> Unit,
+            override val discount: String,
+            val purchasedWalletCount: Int,
+            val code: String,
+            override val url: String,
         ) : ReferralInfoState, ReferralInfoContentState
 
         data class NonParticipantContent(
-            val award: String,
-            val discount: String,
-            val purchasedWalletCount: Int,
-            val code: String,
-            val onCopyClicked: () -> Unit,
-            val onShareClicked: () -> Unit,
+            override val award: String,
+            override val discount: String,
+            override val url: String,
+            val onParticipateClicked: () -> Unit,
         ) : ReferralInfoState, ReferralInfoContentState
 
         object Loading : ReferralInfoState
     }
 
-    data class Effects(val showErrorToast: Boolean)
-
-    data class AgreementBottomSheetState(val url: String)
+    data class ErrorToast(
+        val visibility: Boolean,
+        val changeVisibility: () -> Unit
+    )
 }
