319c389d8f7ec3e9b7c3e2754e4d1722d544cc7e
Tangem <git@tangem.com>
2022-11-21 23:17:36 +0300

    Updated on 2026-04-06

--- core/ui/src/main/java/com/tangem/core/ui/res/IconColor.kt
+++ core/ui/src/main/java/com/tangem/core/ui/res/IconColor.kt
@@ -3,7 +3,19 @@ package com.tangem.core.ui.res
 import androidx.compose.material.Colors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import com.tangem.core.ui.res.TangemColorPalette.Amaranth
+import com.tangem.core.ui.res.TangemColorPalette.Black
+import com.tangem.core.ui.res.TangemColorPalette.Dark1
+import com.tangem.core.ui.res.TangemColorPalette.Dark2
+import com.tangem.core.ui.res.TangemColorPalette.Dark4
+import com.tangem.core.ui.res.TangemColorPalette.Dark6
+import com.tangem.core.ui.res.TangemColorPalette.Light4
+import com.tangem.core.ui.res.TangemColorPalette.Light5
+import com.tangem.core.ui.res.TangemColorPalette.Meadow
+import com.tangem.core.ui.res.TangemColorPalette.Tangerine
+import com.tangem.core.ui.res.TangemColorPalette.White
 
+@Deprecated("Use TangemTheme.colors")
 enum class IconColorType(val lightColor: Color, val darkColor: Color) {
     PRIMARY1(lightColor = Black, darkColor = White),
     PRIMARY2(lightColor = White, darkColor = Dark6),
