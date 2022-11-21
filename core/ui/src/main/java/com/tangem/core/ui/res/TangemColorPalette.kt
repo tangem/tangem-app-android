1f05b4323cedc984c5530239bc242d836d31eaab
Tangem <git@tangem.com>
2022-11-21 23:17:36 +0300

    Updated on 2026-09-23

---
+++ core/ui/src/main/java/com/tangem/core/ui/res/TangemColorPalette.kt
@@ -0,0 +1,48 @@
+package com.tangem.core.ui.res
+
+import androidx.compose.ui.graphics.Color
+
+@Suppress("unused")
+object TangemColorPalette {
+    // region Base
+    val Black = Color(0xFF000000)
+    val White = Color(0xFFFFFFFF)
+    // endregion Base
+
+    // region Dark
+    val Dark1 = Color(0xFF919191)
+    val Dark2 = Color(0xFF656565)
+    val Dark3 = Color(0xFF494949)
+    val Dark4 = Color(0xFF3B3B3B)
+    val Dark5 = Color(0xFF303030)
+    val Dark6 = Color(0xFF1E1E1E)
+    // endregion Dark
+
+    // region Light
+    val Light1 = Color(0xFFF5F5F5)
+    val Light2 = Color(0xFFEBEBEB)
+    val Light3 = Color(0xFFD3D3D3)
+    val Light4 = Color(0xFFC9C9C9)
+    val Light5 = Color(0xFFB0B0B0)
+    // endregion Light
+
+    // region Green
+    val Meadow = Color(0xFF1ACE80)
+    val MagicMint = Color(0xFFA3EBCC)
+    val DarkGreen = Color(0xFF06311F)
+    // endregion Green
+
+    // region Blue
+    val Cobalt = Color(0xFF0029FF)
+    val Azure = Color(0xFF007AFF)
+    // endregion Blue
+
+    // region Red
+    val Amaranth = Color(0xFFDE1010)
+    val Flamingo = Color(0xFFED7979)
+    // endregion Red
+
+    // region Yellow
+    val Tangerine = Color(0xFFFFB71B)
+    // endregion Yellow
+}
