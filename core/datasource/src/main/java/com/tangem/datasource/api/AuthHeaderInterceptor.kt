93e4d963f8077f6cb81af0d366b45195687693d6
vladislav kozarez <Kozarez.V.Se@sberbank.ru>
2022-11-21 20:39:13 +0300

    AND-2464 fixes to api calls, connect layers and call flow

--- core/datasource/src/main/java/com/tangem/datasource/api/AuthHeaderInterceptor.kt
+++ core/datasource/src/main/java/com/tangem/datasource/api/AuthHeaderInterceptor.kt
@@ -9,16 +9,17 @@ class AuthHeaderInterceptor(
 ) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
-        val request = chain.request().newBuilder().apply {
-            addHeader(
-                HEADER_CARD_KEY, authProvider.getAuthToken(),
-            )
-        }.build()
+        val request = chain.request()
+            .newBuilder()
+            .addHeader(HEADER_CARD_KEY, authProvider.getCardPublicKey())
+            .addHeader(HEADER_CARD_ID, authProvider.getCardId())
+            .build()
 
         return chain.proceed(request)
     }
 
     companion object {
         const val HEADER_CARD_KEY = "card_public_key"
+        const val HEADER_CARD_ID = "card_id"
     }
 }
