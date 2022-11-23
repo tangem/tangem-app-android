22cb9411e9af68487657f71996d474b84ad8f7a2
Tangem <git@tangem.com>
2022-11-23 13:19:57 +0300

    Updated on 2026-09-21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -119,6 +119,9 @@ dependencies {
     implementation(project(":features:referral:presentation"))
     implementation(project(":features:referral:domain"))
     implementation(project(":features:referral:data"))
+    implementation(project(":features:swap:presentation"))
+    implementation(project(":features:swap:domain"))
+    implementation(project(":features:swap:data"))
 
     /** AndroidX libraries */
     implementation(AndroidX.coreKtx)
