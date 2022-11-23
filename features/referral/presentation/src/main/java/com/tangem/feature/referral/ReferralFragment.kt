177f55d0710977530c86bd5a09a94a78975c7ae3
Tangem <git@tangem.com>
2022-11-23 13:49:15 +0300

    Updated on 2026-09-23

--- features/referral/presentation/src/main/java/com/tangem/feature/referral/ReferralFragment.kt
+++ features/referral/presentation/src/main/java/com/tangem/feature/referral/ReferralFragment.kt
@@ -5,19 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
+import androidx.core.view.WindowCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.tangem.feature.referral.router.ReferralRouter
 import com.tangem.feature.referral.ui.ReferralScreen
 import com.tangem.feature.referral.viewmodels.ReferralViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.lang.ref.WeakReference
 
 @AndroidEntryPoint
 class ReferralFragment : Fragment() {
-    private val viewModel by viewModels<ReferralViewModel>().apply {
-        // TODO("[REDACTED_JIRA]") this.value.setOnBackClicked()
-    }
+    private val viewModel by viewModels<ReferralViewModel>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        activity?.window?.let { WindowCompat.setDecorFitsSystemWindows(it, true) }
+        viewModel.setRouter(ReferralRouter(fragmentManager = WeakReference(parentFragmentManager)))
         return ComposeView(inflater.context).apply {
             setContent {
                 ReferralScreen(stateHolder = viewModel.uiState)
