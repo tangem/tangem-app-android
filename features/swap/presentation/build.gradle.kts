23ac535c763750810b6d856daf7515d6608c724e
Tangem <git@tangem.com>
2022-11-23 13:19:57 +0300

    Updated on 2026-05-04

---
+++ features/swap/presentation/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+
+    defaultConfig {
+        compileSdk = AppConfig.compileSdkVersion
+        minSdk = AppConfig.minSdkVersion
+        targetSdk = AppConfig.targetSdkVersion
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+        isCoreLibraryDesugaringEnabled = false
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = Versions.compose
+    }
+}
+
+dependencies {
+    /** Core modules */
+    implementation(project(":core:utils"))
+    implementation(project(":core:ui"))
+
+    /** AndroidX */
+    implementation(AndroidX.appCompat)
+    implementation(AndroidX.fragmentKtx)
+    implementation(AndroidX.lifecycleViewModelKtx)
+
+    /** Compose */
+    implementation(Compose.foundation)
+    implementation(Compose.material)
+    implementation(Compose.uiTooling)
+
+    /** Domain */
+    implementation(project(":features:swap:domain"))
+
+    /** Other libraries */
+    implementation(Library.composeShimmer)
+
+    /** DI */
+    implementation(Library.hilt)
+    kapt(Library.hiltKapt)
+}
