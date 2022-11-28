edc17ec45d0272bd93077057b602368672855513
Tangem <git@tangem.com>
2022-11-28 18:08:46 +0300

    Updated on 2026-08-19

--- app/src/main/java/com/tangem/tap/common/redux/navigation/NavigationMiddleware.kt
+++ app/src/main/java/com/tangem/tap/common/redux/navigation/NavigationMiddleware.kt
@@ -2,13 +2,15 @@ package com.tangem.tap.common.redux.navigation
 
 import android.content.Intent
 import com.tangem.tap.common.CustomTabsManager
+import com.tangem.tap.common.extensions.dispatchOnMain
 import com.tangem.tap.common.extensions.openFragment
 import com.tangem.tap.common.extensions.popBackTo
 import com.tangem.tap.common.extensions.shareText
 import com.tangem.tap.common.redux.AppState
+import com.tangem.tap.store
 import org.rekotlin.Middleware
 
-val navigationMiddleware: Middleware<AppState> = { dispatch, state ->
+val navigationMiddleware: Middleware<AppState> = { _, state ->
     { next ->
         { action ->
             if (action is NavigationAction) {
@@ -16,16 +18,24 @@ val navigationMiddleware: Middleware<AppState> = { dispatch, state ->
                 when (action) {
                     is NavigationAction.NavigateTo -> {
                         navState?.activity?.get()?.openFragment(
-                                action.screen,
-                                action.addToBackstack,
-                                action.fragmentShareTransition
+                            screen = action.screen,
+                            addToBackstack = action.addToBackstack,
+                            fgShareTransition = action.fragmentShareTransition,
                         )
                     }
                     is NavigationAction.PopBackTo -> {
-                        if (action.screen == AppScreen.Home) {
-                            navState?.activity?.get()?.popBackTo(null, true)
-                        } else {
-                            navState?.activity?.get()?.popBackTo(action.screen)
+                        when (val screen = action.screen) {
+                            AppScreen.Home,
+                            AppScreen.Welcome,
+                            -> {
+                                navState?.activity?.get()?.popBackTo(screen = null, inclusive = true)
+                                if (navState?.backStack?.contains(screen) != true) {
+                                    store.dispatchOnMain(NavigationAction.NavigateTo(screen))
+                                }
+                            }
+                            else -> {
+                                navState?.activity?.get()?.popBackTo(screen = action.screen)
+                            }
                         }
                     }
                     is NavigationAction.OpenUrl -> {
@@ -39,10 +49,11 @@ val navigationMiddleware: Middleware<AppState> = { dispatch, state ->
                         navState?.activity?.get()?.startActivity(intent)
                     }
                     is NavigationAction.Share -> {
-                        navState?.activity?.get()?.let {
-                            it.shareText(action.data)
-                        }
+                        navState?.activity?.get()?.shareText(action.data)
                     }
+                    is NavigationAction.ActivityCreated,
+                    is NavigationAction.ActivityDestroyed,
+                    -> Unit
                 }
             }
             next(action)
