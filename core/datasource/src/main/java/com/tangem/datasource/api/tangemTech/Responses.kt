a3f87b4469ce6e97254c2e735c91b1f9fac07b79
Tangem <git@tangem.com>
2022-11-28 17:07:30 +0300

    Updated on 2026-09-14

--- core/datasource/src/main/java/com/tangem/datasource/api/tangemTech/Responses.kt
+++ core/datasource/src/main/java/com/tangem/datasource/api/tangemTech/Responses.kt
@@ -11,7 +11,7 @@ sealed interface TangemTechResponse : HttpResponse
 data class CoinsResponse(
     val imageHost: String?,
     val coins: List<Coin>,
-    val total: Int
+    val total: Int,
 ) : TangemTechResponse {
 
     data class Coin(
@@ -19,13 +19,14 @@ data class CoinsResponse(
         val name: String,
         val symbol: String,
         val active: Boolean,
-        val networks: List<Network> = listOf()
+        val networks: List<Network> = listOf(),
     ) : TangemTechResponse {
 
         data class Network(
             val networkId: String,
             val contractAddress: String? = null,
             val decimalCount: BigDecimal? = null,
+            val exchangeable: Boolean? = false,
         ) : TangemTechResponse
     }
 }
