2cef29915a09421f032db4307b41c7da86c04f48
Tangem <git@tangem.com>
2022-11-28 18:26:31 +0300

    release/5.26.1

--- app/src/main/java/com/tangem/tap/features/saveWallet/redux/SaveWalletState.kt
+++ app/src/main/java/com/tangem/tap/features/saveWallet/redux/SaveWalletState.kt
@@ -1,5 +1,18 @@
 package com.tangem.tap.features.saveWallet.redux
 
+import com.tangem.common.core.TangemError
+import com.tangem.domain.common.ScanResponse
 import org.rekotlin.StateType
 
-class SaveWalletState : StateType
+data class SaveWalletState(
+    val additionalInfo: WalletAdditionalInfo? = null,
+    val isSaveInProgress: Boolean = false,
+    val needEnrollBiometrics: Boolean = false,
+    val error: TangemError? = null,
+) : StateType {
+    data class WalletAdditionalInfo(
+        val scanResponse: ScanResponse,
+        val accessCode: String?,
+        val backupCardsIds: Set<String>?,
+    )
+}
