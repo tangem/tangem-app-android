613f68358c88db8c73302967febf44927b256c77
iiiburnyiii <iiiburnyiii@gmail.com>
2022-11-28 18:08:46 +0300

    AND-2495 Implement the welcome screen redux

--- app/src/main/java/com/tangem/tap/features/welcome/redux/WelcomeState.kt
+++ app/src/main/java/com/tangem/tap/features/welcome/redux/WelcomeState.kt
@@ -1,5 +1,10 @@
 package com.tangem.tap.features.welcome.redux
 
+import com.tangem.common.core.TangemError
 import org.rekotlin.StateType
 
-class WelcomeState : StateType
+data class WelcomeState(
+    val isUnlockWithBiometricsInProgress: Boolean = false,
+    val isUnlockWithCardInProgress: Boolean = false,
+    val error: TangemError? = null,
+) : StateType
