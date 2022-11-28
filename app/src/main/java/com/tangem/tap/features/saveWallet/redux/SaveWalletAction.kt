6006156d6e918ad2a5c0e2bfb09492d27fdb8d75
Tangem <git@tangem.com>
2022-11-28 18:26:31 +0300

    Updated on 2026-07-23

--- app/src/main/java/com/tangem/tap/features/saveWallet/redux/SaveWalletAction.kt
+++ app/src/main/java/com/tangem/tap/features/saveWallet/redux/SaveWalletAction.kt
@@ -1,9 +1,16 @@
 package com.tangem.tap.features.saveWallet.redux
 
 import com.tangem.common.core.TangemError
+import com.tangem.domain.common.ScanResponse
 import org.rekotlin.Action
 
 internal sealed interface SaveWalletAction : Action {
+    data class ProvideAdditionalInfo(
+        val scanResponse: ScanResponse,
+        val accessCode: String?,
+        val backupCardsIds: Set<String>?,
+    ) : SaveWalletAction
+
     object Save : SaveWalletAction {
         object Success : SaveWalletAction
         data class Error(val error: TangemError) : SaveWalletAction
@@ -12,6 +19,10 @@ internal sealed interface SaveWalletAction : Action {
     object Dismiss : SaveWalletAction
 
     object CloseError : SaveWalletAction
+    object EnrollBiometrics : SaveWalletAction {
+        object Enroll : SaveWalletAction
+        object Cancel : SaveWalletAction
+    }
 
     object SaveWalletWasShown : SaveWalletAction
 }
