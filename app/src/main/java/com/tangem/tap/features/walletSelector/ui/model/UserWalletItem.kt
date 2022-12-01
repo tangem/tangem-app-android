cb5de1f1ffef347022b023679ab63f9fb26988b8
Tangem <git@tangem.com>
2022-12-01 14:54:01 +0300

    Updated on 2026-07-23

--- app/src/main/java/com/tangem/tap/features/walletSelector/ui/model/UserWalletItem.kt
+++ app/src/main/java/com/tangem/tap/features/walletSelector/ui/model/UserWalletItem.kt
@@ -5,42 +5,38 @@ import com.tangem.tap.features.details.ui.cardsettings.TextReference
 import com.tangem.wallet.R
 
 @Immutable
-internal data class UserWalletItem(
-    val id: String,
-    val name: String,
-    val imageUrl: String,
-    val balance: Balance,
-    val type: Type,
-) {
+internal sealed interface UserWalletItem {
+    val id: String
+    val name: String
+    val imageUrl: String
+    val balance: Balance
+
+    val headerText: TextReference
+        get() = when (this) {
+            is MultiCurrencyUserWalletItem -> TextReference.Res(R.string.user_wallet_list_multi_header)
+            is SingleCurrencyUserWalletItem -> TextReference.Res(R.string.user_wallet_list_single_header)
+        }
+
     @Immutable
     data class Balance(
         val amount: String,
         val isLoading: Boolean,
-    ) {
-        companion object {
-            val Loading = Balance(
-                amount = "0.00",
-                isLoading = true,
-            )
-        }
-    }
-
-    sealed interface Type {
-        val headerText: TextReference
-
-        @Immutable
-        class MultiCurrency(
-            val tokensCount: Int,
-            val cardsInWallet: Int,
-        ) : Type {
-            override val headerText: TextReference
-                get() = TextReference.Res(R.string.user_wallet_list_multi_header)
-        }
-
-        @Immutable
-        class SingleCurrency(val tokenName: String) : Type {
-            override val headerText: TextReference
-                get() = TextReference.Res(R.string.user_wallet_list_single_header)
-        }
-    }
+    )
 }
+
+internal data class MultiCurrencyUserWalletItem(
+    override val id: String,
+    override val name: String,
+    override val imageUrl: String,
+    override val balance: UserWalletItem.Balance,
+    val tokensCount: Int,
+    val cardsInWallet: Int,
+) : UserWalletItem
+
+internal data class SingleCurrencyUserWalletItem(
+    override val id: String,
+    override val name: String,
+    override val imageUrl: String,
+    override val balance: UserWalletItem.Balance,
+    val tokenName: String,
+) : UserWalletItem
