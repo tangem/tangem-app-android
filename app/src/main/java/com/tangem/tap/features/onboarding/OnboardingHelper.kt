06071396f33f1b1d2d20ee4180367010c5dc2cb2
Tangem <git@tangem.com>
2022-12-01 14:40:31 +0300

    Updated on 2025-09-15

--- app/src/main/java/com/tangem/tap/features/onboarding/OnboardingHelper.kt
+++ app/src/main/java/com/tangem/tap/features/onboarding/OnboardingHelper.kt
@@ -2,8 +2,18 @@ package com.tangem.tap.features.onboarding
 
 import com.tangem.domain.common.ProductType
 import com.tangem.domain.common.ScanResponse
+import com.tangem.tap.common.extensions.dispatchOnMain
+import com.tangem.tap.common.extensions.onCardScanned
 import com.tangem.tap.common.redux.navigation.AppScreen
+import com.tangem.tap.common.redux.navigation.NavigationAction
+import com.tangem.tap.features.saveWallet.redux.SaveWalletAction
 import com.tangem.tap.preferencesStorage
+import com.tangem.tap.scope
+import com.tangem.tap.store
+import com.tangem.tap.tangemSdkManager
+import com.tangem.tap.userWalletsListManager
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 /**
 [REDACTED_AUTHOR]
@@ -38,6 +48,43 @@ class OnboardingHelper {
                 ProductType.Twins -> AppScreen.OnboardingTwins
                 ProductType.SaltPay -> AppScreen.OnboardingWallet
             }
+        }
+
+        fun trySaveWalletAndNavigateToWalletScreen(
+            scanResponse: ScanResponse,
+            accessCode: String? = null,
+            backupCardsIds: List<String>? = null,
+        ) {
+            when {
+                userWalletsListManager.hasSavedUserWallets -> scope.launch {
+                    delay(timeMillis = 1_200)
+                    store.dispatchOnMain(
+                        SaveWalletAction.ProvideBackupInfo(
+                            scanResponse = scanResponse,
+                            accessCode = accessCode,
+                            backupCardsIds = backupCardsIds?.toSet(),
+                        ),
+                    )
+                    store.dispatchOnMain(SaveWalletAction.Save)
+                }
+                tangemSdkManager.canUseBiometry &&
+                    preferencesStorage.shouldShowSaveWallet -> scope.launch {
+                    delay(timeMillis = 1_200)
+                    store.dispatchOnMain(
+                        SaveWalletAction.ProvideBackupInfo(
+                            scanResponse = scanResponse,
+                            accessCode = accessCode,
+                            backupCardsIds = backupCardsIds?.toSet(),
+                        ),
+                    )
+                    store.dispatchOnMain(NavigationAction.NavigateTo(AppScreen.SaveWallet))
+                }
+                else -> scope.launch {
+                    store.onCardScanned(scanResponse)
+                }
+            }
+
+            store.dispatchOnMain(NavigationAction.NavigateTo(AppScreen.Wallet))
         }
     }
 }
