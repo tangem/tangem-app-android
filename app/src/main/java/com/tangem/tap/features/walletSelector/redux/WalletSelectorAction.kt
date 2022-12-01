9ebbcad5cbf1b1006f9b205504553d89e47f7872
Tangem <git@tangem.com>
2022-12-01 14:40:31 +0300

    Updated on 2026-02-02

--- app/src/main/java/com/tangem/tap/features/walletSelector/redux/WalletSelectorAction.kt
+++ app/src/main/java/com/tangem/tap/features/walletSelector/redux/WalletSelectorAction.kt
@@ -1,9 +1,33 @@
 package com.tangem.tap.features.walletSelector.redux
 
 import com.tangem.common.core.TangemError
+import com.tangem.domain.common.util.UserWalletId
+import com.tangem.tap.common.entities.FiatCurrency
+import com.tangem.tap.domain.model.UserWallet
+import com.tangem.tap.domain.model.WalletStoreModel
 import org.rekotlin.Action
 
 internal sealed interface WalletSelectorAction : Action {
+    data class UserWalletsLoaded(
+        val userWallets: List<UserWallet>,
+    ) : WalletSelectorAction
+
+    data class SelectedWalletChanged(
+        val selectedWallet: UserWallet,
+    ) : WalletSelectorAction
+
+    data class IsLockedChanged(
+        val isLocked: Boolean,
+    ) : WalletSelectorAction
+
+    data class WalletStoresChanged(
+        val walletsStores: Map<UserWalletId, List<WalletStoreModel>>,
+    ) : WalletSelectorAction
+
+    data class BalanceLoaded(
+        val userWalletModel: UserWalletModel,
+    ) : WalletSelectorAction
+
     object UnlockWithBiometry : WalletSelectorAction {
         object Success : WalletSelectorAction
         data class Error(val error: TangemError) : WalletSelectorAction
@@ -27,5 +51,10 @@ internal sealed interface WalletSelectorAction : Action {
         data class Error(val error: TangemError) : WalletSelectorAction
     }
 
+    data class ChangeAppCurrency(
+        val fiatCurrency: FiatCurrency,
+    ) : WalletSelectorAction
+
+    data class HandleError(val error: TangemError) : WalletSelectorAction
     object CloseError : WalletSelectorAction
 }
