4b08a5f6055231b97593e66d2f51bb8854c7430f
Tangem <git@tangem.com>
2022-12-01 14:54:01 +0300

    Updated on 2026-08-14

--- app/src/main/java/com/tangem/tap/features/walletSelector/ui/WalletSelectorScreenState.kt
+++ app/src/main/java/com/tangem/tap/features/walletSelector/ui/WalletSelectorScreenState.kt
@@ -2,16 +2,16 @@ package com.tangem.tap.features.walletSelector.ui
 
 import androidx.compose.runtime.Immutable
 import com.tangem.common.core.TangemError
-import com.tangem.tap.common.entities.FiatCurrency
+import com.tangem.tap.features.walletSelector.ui.model.MultiCurrencyUserWalletItem
 import com.tangem.tap.features.walletSelector.ui.model.RenameWalletDialog
-import com.tangem.tap.features.walletSelector.ui.model.UserWalletItem
+import com.tangem.tap.features.walletSelector.ui.model.SingleCurrencyUserWalletItem
 
 @Immutable
 internal data class WalletSelectorScreenState(
-    val wallets: List<UserWalletItem> = listOf(),
+    val multiCurrencyWallets: List<MultiCurrencyUserWalletItem> = emptyList(),
+    val singleCurrencyWallets: List<SingleCurrencyUserWalletItem> = emptyList(),
     val selectedWalletId: String? = null,
-    val fiatCurrency: FiatCurrency = FiatCurrency.Default,
-    val isLocked: Boolean = true,
+    val isLocked: Boolean = false,
     val editingWalletsIds: List<String> = listOf(),
     val renameWalletDialog: RenameWalletDialog? = null,
     val showAddCardProgress: Boolean = false,
