521c3364a8d1fa6e7cb9852b04de4f528f87f504
Tangem <git@tangem.com>
2022-12-01 14:40:31 +0300

    Updated on 2026-06-12

---
+++ app/src/main/java/com/tangem/tap/features/walletSelector/redux/UserWalletModel.kt
@@ -0,0 +1,22 @@
+package com.tangem.tap.features.walletSelector.redux
+
+import com.tangem.tap.domain.model.TotalFiatBalance
+
+data class UserWalletModel(
+    val id: String,
+    val name: String,
+    val artworkUrl: String,
+    val type: Type,
+    val fiatBalance: TotalFiatBalance,
+) {
+    sealed interface Type {
+        data class SingleCurrency(
+            val blockchainName: String?,
+        ) : Type
+
+        data class MultiCurrency(
+            val tokensCount: Int,
+            val cardsInWallet: Int,
+        ) : Type
+    }
+}
