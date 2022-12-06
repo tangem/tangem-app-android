6123991cb76b1b4e764d603726162ad1d45ee39f
Tangem <git@tangem.com>
2022-12-06 20:56:19 +0300

    Updated on 2026-06-24

---
+++ app/src/main/java/com/tangem/tap/features/details/ui/appsettings/components/EnrollBiometricsCard.kt
@@ -0,0 +1,92 @@
+package com.tangem.tap.features.details.ui.appsettings.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.tangem.core.ui.components.SpacerH4
+import com.tangem.core.ui.components.SpacerW16
+import com.tangem.core.ui.res.TangemTheme
+import com.tangem.wallet.R
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+internal fun EnrollBiometricsCard(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) {
+    Surface(
+        modifier = modifier,
+        color = TangemTheme.colors.background.primary,
+        shape = TangemTheme.shapes.roundedCornersLarge,
+        onClick = onClick,
+    ) {
+        Row(
+            modifier = Modifier.padding(all = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly,
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_alert_circle_24),
+                tint = TangemTheme.colors.icon.attention,
+                contentDescription = null,
+            )
+            SpacerW16()
+            Column {
+                Text(
+                    text = stringResource(id = R.string.app_settings_enable_biometrics_title),
+                    style = TangemTheme.typography.subtitle1,
+                    color = TangemTheme.colors.text.primary1,
+                )
+                SpacerH4()
+                Text(
+                    text = stringResource(id = R.string.app_settings_enable_biometrics_description),
+                    style = TangemTheme.typography.body2,
+                    color = TangemTheme.colors.text.secondary,
+                )
+            }
+        }
+    }
+}
+
+// region Preview
+@Composable
+private fun EnrollBiometricsCardSample(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .background(TangemTheme.colors.background.secondary),
+    ) {
+        EnrollBiometricsCard(onClick = {})
+    }
+}
+
+@Preview(showBackground = true, widthDp = 360)
+@Composable
+private fun EnrollBiometricsCardPreview_Light() {
+    TangemTheme {
+        EnrollBiometricsCardSample()
+    }
+}
+
+@Preview(showBackground = true, widthDp = 360)
+@Composable
+private fun EnrollBiometricsCardPreview_Dark() {
+    TangemTheme(isDark = true) {
+        EnrollBiometricsCardSample()
+    }
+}
+// endregion Preview
