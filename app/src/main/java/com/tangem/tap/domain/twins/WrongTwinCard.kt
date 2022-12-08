d9ace6ea6e6dc845c1a22c3f7fa8183f43dd1f5e
Tangem <git@tangem.com>
2022-12-08 10:54:38 +0300

    Updated on 2026-06-17

--- app/src/main/java/com/tangem/tap/domain/twins/WrongTwinCard.kt
+++ app/src/main/java/com/tangem/tap/domain/twins/WrongTwinCard.kt
@@ -5,8 +5,7 @@ import com.tangem.domain.common.TwinCardNumber
 import com.tangem.tap.tangemSdkManager
 import com.tangem.wallet.R
 
-data class WrongTwinCard(private val twinCardNumber: TwinCardNumber) : TangemError {
-    override val code: Int = 50005
+data class WrongTwinCard(private val twinCardNumber: TwinCardNumber) : TangemError(code = 50005) {
     override var customMessage: String = tangemSdkManager.getString(
         R.string.twin_error_same_card,
         twinCardNumber.number,
