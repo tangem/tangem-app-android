b7f0d0a3d447a536ee5e93a34123add22327a31b
Tangem <git@tangem.com>
2022-12-08 16:58:37 +0300

    Updated on 2026-08-07

--- features/swap/data/src/main/java/com/tangem/feature/swap/converters/QuotesConverter.kt
+++ features/swap/data/src/main/java/com/tangem/feature/swap/converters/QuotesConverter.kt
@@ -1,10 +1,11 @@
 package com.tangem.feature.swap.converters
 
 import com.tangem.datasource.api.oneinch.models.QuoteResponse
-import com.tangem.feature.swap.domain.models.QuoteModel
+import com.tangem.feature.swap.domain.models.data.QuoteModel
 import com.tangem.utils.converter.Converter
+import javax.inject.Inject
 
-class QuotesConverter : Converter<QuoteResponse, QuoteModel> {
+class QuotesConverter @Inject constructor(): Converter<QuoteResponse, QuoteModel> {
 
     override fun convert(value: QuoteResponse): QuoteModel {
         return QuoteModel(
