83d62ad3258232c71c4f3d37f2fdc3993c37ede3
Tangem <git@tangem.com>
2022-12-09 15:28:30 +0300

    release/5.26.1

--- core/res/build.gradle.kts
+++ core/res/build.gradle.kts
@@ -18,4 +18,18 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    buildTypes {
+        create("debug_beta") {
+            initWith(getByName("release"))
+            BuildConfigFieldFactory(
+                fields = listOf(
+                    Field.Environment("release"),
+                    Field.TestActionEnabled(true),
+                    Field.LogEnabled(true),
+                ),
+                builder = ::buildConfigField,
+            ).create()
+        }
+    }
 }
