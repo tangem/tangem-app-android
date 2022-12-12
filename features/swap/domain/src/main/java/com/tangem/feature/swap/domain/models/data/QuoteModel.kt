854faab3b4ec6d6bf4e0ede17c9c324bd3661fa9
Tangem <git@tangem.com>
2022-12-12 16:26:33 +0300

    Updated on 2026-08-27

--- features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/data/QuoteModel.kt
+++ features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/data/QuoteModel.kt
@@ -3,5 +3,6 @@ package com.tangem.feature.swap.domain.models.data
 data class QuoteModel(
     val fromTokenAmount: String,
     val toTokenAmount: String,
-    val estimatedGas: Int
+    val estimatedGas: Int,
+    val isAllowedToSpend: Boolean = false,
 )
