3e8b165f855682ca4fcc3fd0359e1fdb5622ecc7
Tangem <git@tangem.com>
2022-12-12 16:26:33 +0300

    Updated on 2026-06-12

--- features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/SwapDataHolder.kt
+++ features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/SwapDataHolder.kt
@@ -3,6 +3,7 @@ package com.tangem.feature.swap.domain.models
 import com.tangem.feature.swap.domain.models.data.QuoteModel
 
 data class SwapDataHolder(
-    val quoteModel: QuoteModel,
-    val amount: String,
+    val quoteModel: QuoteModel? = null,
+    val amountToSwap: String? = null,
+    val networkId: String? = null,
 )
