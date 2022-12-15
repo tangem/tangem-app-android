4a50fba0ae76d219faeaefeb41cf24ff7ab802db
Tangem <git@tangem.com>
2022-12-15 12:45:36 +0300

    Updated on 2026-01-20

--- features/swap/data/src/main/java/com/tangem/feature/swap/converters/QuotesConverter.kt
+++ features/swap/data/src/main/java/com/tangem/feature/swap/converters/QuotesConverter.kt
@@ -1,30 +1,17 @@
 package com.tangem.feature.swap.converters
 
-import com.tangem.datasource.api.oneinch.BaseOneInchResponse
 import com.tangem.datasource.api.oneinch.models.QuoteResponse
-import com.tangem.feature.swap.domain.models.AggregatedSwapDataModel
-import com.tangem.feature.swap.domain.models.data.QuoteModel
-import com.tangem.feature.swap.domain.models.data.mapErrors
+import com.tangem.feature.swap.domain.models.data.SwapState.QuoteModel
 import com.tangem.utils.converter.Converter
 import javax.inject.Inject
 
-class QuotesConverter @Inject constructor() : Converter<BaseOneInchResponse<QuoteResponse>, AggregatedSwapDataModel<QuoteModel>> {
+class QuotesConverter @Inject constructor() : Converter<QuoteResponse, QuoteModel> {
 
-    override fun convert(value: BaseOneInchResponse<QuoteResponse>): AggregatedSwapDataModel<QuoteModel> {
-        val body = value.body
-        return if (body != null) {
-            AggregatedSwapDataModel(
-                QuoteModel(
-                    fromTokenAmount = body.fromTokenAmount,
-                    toTokenAmount = body.toTokenAmount,
-                    estimatedGas = body.estimatedGas,
-                ),
-            )
-        } else {
-            AggregatedSwapDataModel(
-                null,
-                mapErrors(value.errorDto?.description),
-            )
-        }
+    override fun convert(value: QuoteResponse): QuoteModel {
+        return QuoteModel(
+            fromTokenAmount = value.fromTokenAmount,
+            toTokenAmount = value.toTokenAmount,
+            estimatedGas = value.estimatedGas,
+        )
     }
 }
