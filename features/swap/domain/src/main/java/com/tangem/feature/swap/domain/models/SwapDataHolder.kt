42da7aa1766026656e2896da6f6d48869e21dee7
Tangem <git@tangem.com>
2022-12-15 12:45:36 +0300

    Updated on 2026-05-13

--- features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/SwapDataHolder.kt
+++ features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/SwapDataHolder.kt
@@ -1,9 +1,10 @@
 package com.tangem.feature.swap.domain.models
 
-import com.tangem.feature.swap.domain.models.data.QuoteModel
+import com.tangem.feature.swap.domain.models.data.SwapState.QuoteModel
 
 data class SwapDataHolder(
-    val quoteModel: QuoteModel? = null,
+    val quoteModel:
+    QuoteModel? = null,
     val amountToSwap: String? = null,
     val networkId: String? = null,
 )
