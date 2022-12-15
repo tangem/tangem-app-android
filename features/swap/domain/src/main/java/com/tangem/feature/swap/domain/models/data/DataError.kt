7a8d224a3b7025f993f113a723897ded8675ae31
Tangem <git@tangem.com>
2022-12-15 12:45:36 +0300

    Updated on 2026-03-27

--- features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/data/DataError.kt
+++ features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/data/DataError.kt
@@ -3,16 +3,19 @@ package com.tangem.feature.swap.domain.models.data
 enum class DataError {
     NO_ERROR,
     UNKNOWN_ERROR,
-    INSUFFICIENT_LIQUIDITY
+    INSUFFICIENT_LIQUIDITY,
+    CANNOT_SYNC
 }
 
 fun mapErrors(error: String?): DataError {
     return if (error == null) {
         DataError.UNKNOWN_ERROR
-    } else when (error) {
-        INSUFFICIENT_LIQUIDITY_ERROR -> DataError.INSUFFICIENT_LIQUIDITY
+    } else when {
+        error == INSUFFICIENT_LIQUIDITY_ERROR -> DataError.INSUFFICIENT_LIQUIDITY
+        error.startsWith(CANNOT_SYNC_ERROR) -> DataError.CANNOT_SYNC
         else -> DataError.UNKNOWN_ERROR
     }
 }
 
 private const val INSUFFICIENT_LIQUIDITY_ERROR = "insufficient liquidity"
+private const val CANNOT_SYNC_ERROR = "cannot sync"
