e4a07ee170be8779632d90d6e494d7e543bdb79c
Tangem <git@tangem.com>
2022-12-15 12:45:36 +0300

    Updated on 2026-07-23

--- features/swap/domain/src/main/java/com/tangem/feature/swap/domain/SwapInteractorImpl.kt
+++ features/swap/domain/src/main/java/com/tangem/feature/swap/domain/SwapInteractorImpl.kt
@@ -1,9 +1,9 @@
 package com.tangem.feature.swap.domain
 
 import com.tangem.feature.swap.domain.cache.SwapDataCache
-import com.tangem.feature.swap.domain.models.SwapResultModel
 import com.tangem.feature.swap.domain.models.data.Currency
-import com.tangem.feature.swap.domain.models.data.QuoteModel
+import com.tangem.feature.swap.domain.models.data.DataError
+import com.tangem.feature.swap.domain.models.data.SwapState
 import com.tangem.feature.swap.domain.models.data.TransactionModel
 import com.tangem.lib.crypto.UserWalletManager
 import javax.inject.Inject
@@ -28,12 +28,20 @@ internal class SwapInteractorImpl @Inject constructor(
         TODO("Not yet implemented")
     }
 
-    override suspend fun findBestQuote(fromTokenAddress: String, toTokenAddress: String, amount: String): QuoteModel {
+    override suspend fun givePermissionToSwap(tokenAddress: String) {
+        val oneInchAddress = repository.addressForTrust()
+        val transactionData = repository.dataToApprove(tokenAddress)
+        //todo sign transaction (next tasks)
+    }
+
+    override suspend fun findBestQuote(fromTokenAddress: String, toTokenAddress: String, amount: String): SwapState {
         val networkId = cache.getNetworkId()
         val isAllowedToSpend = if (networkId.isNullOrEmpty()) {
             false
         } else {
-            repository.checkTokensSpendAllowance(fromTokenAddress, userWalletManager.getWalletAddress(networkId)) != "0"
+            val allowance =
+                repository.checkTokensSpendAllowance(fromTokenAddress, userWalletManager.getWalletAddress(networkId))
+            allowance.error == DataError.NO_ERROR && allowance.dataModel != "0"
         }
         repository.findBestQuote(fromTokenAddress, toTokenAddress, amount).let { quotes ->
             val quoteDataModel = quotes.dataModel
@@ -41,37 +49,37 @@ internal class SwapInteractorImpl @Inject constructor(
                 cache.cacheSwapParams(quoteDataModel.copy(isAllowedToSpend = isAllowedToSpend), amount)
                 return quoteDataModel
             } else {
-                error("") //todo handle error in domain layer(task [REDACTED_TASK_KEY])
+                return SwapState.SwapError(quotes.error)
             }
         }
     }
 
-    override suspend fun onSwap(): SwapResultModel {
+    override suspend fun onSwap(): SwapState {
         val quoteModel = cache.getLastQuote()
         val amountToSwap = cache.getAmountToSwap()
         val networkId = cache.getNetworkId()
         if (quoteModel != null && amountToSwap != null && networkId != null) {
-            val swapData = repository.prepareSwapTransaction(
+            repository.prepareSwapTransaction(
                 fromTokenAddress = quoteModel.fromTokenAmount,
                 toTokenAddress = quoteModel.toTokenAmount,
                 amount = amountToSwap,
                 slippage = DEFAULT_SLIPPAGE,
                 fromWalletAddress = getWalletAddress(networkId),
-            ).dataModel
-            if (swapData != null) {
-                signTransactionData(swapData.transaction) //todo implement
-                return SwapResultModel.SwapSuccess(
-                    quoteModel.fromTokenAmount,
-                    quoteModel.toTokenAmount,
-                )
-            } else {
-                error("") //todo handle error in domain layer(task [REDACTED_TASK_KEY])
+            ).let {
+                val swapData = it.dataModel
+                if (swapData != null) {
+                    signTransactionData(swapData.transaction) //todo implement
+                    return SwapState.SwapSuccess(
+                        quoteModel.fromTokenAmount,
+                        quoteModel.toTokenAmount,
+                    )
+                } else {
+                    return SwapState.SwapError(it.error)
+                }
             }
+        } else {
+            throw IllegalStateException("cache is empty, call 'findBestQuote' first")
         }
-        return SwapResultModel.SwapError(
-            0,
-            "",
-        )
     }
 
     private fun getWalletAddress(networkId: String): String {
