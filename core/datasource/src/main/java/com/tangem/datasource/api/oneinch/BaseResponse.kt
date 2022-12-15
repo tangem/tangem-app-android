f9b608d36c126e9ccd54207f6d9a01cee80d76a2
Tangem <git@tangem.com>
2022-12-15 12:45:36 +0300

    Updated on 2026-09-23

--- core/datasource/src/main/java/com/tangem/datasource/api/oneinch/BaseResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.tangem.datasource.api.oneinch
-
-import com.tangem.datasource.api.oneinch.models.SwapErrorDto
-
-data class BaseOneInchResponse<T>(
-    val body: T?,
-    val errorDto: SwapErrorDto?,
-)
