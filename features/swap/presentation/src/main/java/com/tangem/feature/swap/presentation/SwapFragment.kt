733fd4f63b45fa3179d30394c0a2d8c4b7e7ccc8
Tangem <git@tangem.com>
2022-12-19 21:32:53 +0400

    Updated on 2026-06-17

--- features/swap/presentation/src/main/java/com/tangem/feature/swap/presentation/SwapFragment.kt
+++ features/swap/presentation/src/main/java/com/tangem/feature/swap/presentation/SwapFragment.kt
@@ -1,16 +1,21 @@
 package com.tangem.feature.swap.presentation
 
 import android.os.Bundle
-import android.transition.TransitionInflater
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.animation.Crossfade
 import androidx.compose.ui.platform.ComposeView
 import androidx.core.view.WindowCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.tangem.feature.swap.router.SwapRouter
+import com.tangem.feature.swap.router.SwapScreen
+import com.tangem.feature.swap.ui.SwapScreen
+import com.tangem.feature.swap.ui.SwapSuccessScreen
 import com.tangem.feature.swap.viewmodels.SwapViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.lang.ref.WeakReference
 
 @AndroidEntryPoint
 class SwapFragment : Fragment() {
@@ -19,10 +24,24 @@ class SwapFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         activity?.window?.let { WindowCompat.setDecorFitsSystemWindows(it, true) }
-        viewModel
+        viewModel.setRouter(SwapRouter(fragmentManager = WeakReference(parentFragmentManager)))
         return ComposeView(inflater.context).apply {
             setContent {
-                //todo add compose screen here
+                Crossfade(targetState = viewModel.currentScreen) { screen ->
+                    when (screen) {
+                        SwapScreen.Main -> SwapScreen(stateHolder = viewModel.uiState)
+                        SwapScreen.Success -> {
+                            val successState = viewModel.uiState.successState
+                            if (successState != null) {
+                                SwapSuccessScreen(state = successState, viewModel.uiState.onBackClicked)
+                            } else {
+                                SwapScreen(stateHolder = viewModel.uiState)
+                            }
+                        }
+                        SwapScreen.SelectToken -> SwapScreen(stateHolder = viewModel.uiState)// TODO: use proper screen
+                    }
+                }
+
             }
         }
     }
