34a8f59683d7de1f2731a0aeb90640f599ae3b6a
Andrey Khokhlov <ahohlov@tangem.com>
2022-12-19 13:53:10 +0300

    AND-2642 Sync initial message strings

--- app/src/main/java/com/tangem/tap/features/details/ui/resetcard/ResetCardViewModel.kt
+++ app/src/main/java/com/tangem/tap/features/details/ui/resetcard/ResetCardViewModel.kt
@@ -3,12 +3,18 @@ package com.tangem.tap.features.details.ui.resetcard
 import com.tangem.tap.common.redux.AppState
 import com.tangem.tap.features.details.redux.CardSettingsState
 import com.tangem.tap.features.details.redux.DetailsAction
+import com.tangem.wallet.R
 import org.rekotlin.Store
 
 class ResetCardViewModel(private val store: Store<AppState>) {
 
     fun updateState(state: CardSettingsState?): ResetCardScreenState {
         return ResetCardScreenState(
+            descriptionResId = if (state?.card?.backupStatus?.isActive == true) {
+                R.string.reset_card_with_backup_to_factory_message
+            } else {
+                R.string.reset_card_without_backup_to_factory_message
+            },
             accepted = state?.resetConfirmed ?: false,
             onAcceptWarningToggleClick = { store.dispatch(DetailsAction.ResetToFactory.Confirm(it)) },
             onResetButtonClick = { store.dispatch(DetailsAction.ResetToFactory.Proceed) },
