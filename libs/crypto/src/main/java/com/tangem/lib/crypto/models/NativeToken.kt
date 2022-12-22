31101095ce01a013ef535867f769912a7f036d85
Tangem <git@tangem.com>
2022-12-22 16:25:58 +0300

    Updated on 2026-03-27

--- libs/crypto/src/main/java/com/tangem/lib/crypto/models/NativeToken.kt
+++
@@ -1,8 +0,0 @@
-package com.tangem.lib.crypto.models
-
-data class NativeToken(
-    override val id: String,
-    override val name: String,
-    override val symbol: String,
-    override val networkId: String,
-) : Currency(id, name, symbol, networkId)
