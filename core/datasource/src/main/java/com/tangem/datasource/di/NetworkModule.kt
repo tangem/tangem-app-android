c66743404788a1d3ac1c8b1dd34c3ff0f3e08cda
Tangem <git@tangem.com>
2022-12-22 16:25:58 +0300

    Updated on 2026-04-15

--- core/datasource/src/main/java/com/tangem/datasource/di/NetworkModule.kt
+++ core/datasource/src/main/java/com/tangem/datasource/di/NetworkModule.kt
@@ -4,10 +4,8 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.tangem.datasource.api.AuthHeaderInterceptor
 import com.tangem.datasource.api.common.BigDecimalAdapter
-import com.tangem.datasource.api.oneinch.OneInchApi
 import com.tangem.datasource.api.referral.ReferralApi
 import com.tangem.datasource.api.tangemTech.TangemTechApi
-import com.tangem.datasource.di.qualifiers.OneInchEthereum
 import com.tangem.lib.auth.AuthProvider
 import dagger.Module
 import dagger.Provides
@@ -37,26 +35,6 @@ class NetworkModule {
             .client(okHttpClient)
             .build()
             .create(TangemTechApi::class.java)
-    }
-
-    @Provides
-    @Singleton
-    @OneInchEthereum
-    fun provideOneInchEthereumApi(moshi: Moshi): OneInchApi {
-        return Retrofit.Builder()
-            .addConverterFactory(
-                MoshiConverterFactory.create(moshi),
-            )
-            .baseUrl(ONE_INCH_ETHER_BASE_URL)
-            .client(
-                OkHttpClient.Builder()
-                    .addInterceptor(
-                        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY),
-                    )
-                    .build(),
-            )
-            .build()
-            .create(OneInchApi::class.java)
     }
 
     @Provides
@@ -95,6 +73,5 @@ class NetworkModule {
     private companion object {
         const val PROD_TANGEM_TECH_BASE_URL = "https://api.tangem-tech.com/v1/"
         const val DEV_TANGEM_TECH_BASE_URL = "https://devapi.tangem-tech.com/v1/"
-        const val ONE_INCH_ETHER_BASE_URL = "https://api.1inch.io/v5.0/1/"
     }
 }
