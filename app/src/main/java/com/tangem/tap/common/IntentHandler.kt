9648497fe231a335eab0fb45b70c4c8afd38c67a
Tangem <git@tangem.com>
2022-12-26 15:06:51 +0400

    Updated on 2026-06-17

--- app/src/main/java/com/tangem/tap/common/IntentHandler.kt
+++ app/src/main/java/com/tangem/tap/common/IntentHandler.kt
@@ -3,6 +3,7 @@ package com.tangem.tap.common
 import android.content.Intent
 import android.nfc.NfcAdapter
 import android.nfc.Tag
+import com.tangem.tap.common.extensions.removePrefixOrNull
 import com.tangem.tap.common.redux.navigation.AppScreen
 import com.tangem.tap.common.redux.navigation.NavigationAction
 import com.tangem.tap.domain.walletconnect.WalletConnectManager
@@ -14,14 +15,20 @@ import timber.log.Timber
 
 class IntentHandler {
 
-    private val TRANSACTION_ID_PARAM = "transactionId"
-    private val CURRENCY_CODE_PARAM = "baseCurrencyCode"
-    private val CURRENCY_AMOUNT_PARAM = "baseCurrencyAmount"
-    private val DEPOSIT_WALLET_ADDRESS_PARAM = "depositWalletAddress"
-
     fun handleWalletConnectLink(intent: Intent?) {
-        if (intent?.scheme == WalletConnectManager.WC_SCHEME) {
-            store.dispatch(WalletConnectAction.HandleDeepLink(intent.data?.toString()))
+        val wcUri = when (intent?.scheme) {
+            WalletConnectManager.WC_SCHEME -> {
+                intent.data?.toString()
+            }
+            TANGEM_SCHEME -> {
+                intent.data?.toString()?.removePrefixOrNull(TANGEM_WC_PREFIX)
+            }
+            else -> {
+                null
+            }
+        }
+        if (wcUri != null) {
+            store.dispatch(WalletConnectAction.HandleDeepLink(wcUri))
         }
     }
 
@@ -54,16 +61,25 @@ class IntentHandler {
 
             Timber.d("MoonPay Sell: $amount $currency to $destinationAddress")
 
-            store.dispatch(WalletAction.TradeCryptoAction.SendCrypto(
-                currencyId = currency,
-                amount = amount,
-                destinationAddress = destinationAddress,
-                transactionId = transactionID
-            ))
+            store.dispatch(
+                WalletAction.TradeCryptoAction.SendCrypto(
+                    currencyId = currency,
+                    amount = amount,
+                    destinationAddress = destinationAddress,
+                    transactionId = transactionID,
+                ),
+            )
         } catch (exception: Exception) {
             Timber.d("Not MoonPay URL")
         }
     }
 
-
+    companion object {
+        private const val TRANSACTION_ID_PARAM = "transactionId"
+        private const val CURRENCY_CODE_PARAM = "baseCurrencyCode"
+        private const val CURRENCY_AMOUNT_PARAM = "baseCurrencyAmount"
+        private const val DEPOSIT_WALLET_ADDRESS_PARAM = "depositWalletAddress"
+        private const val TANGEM_SCHEME = "tangem"
+        private const val TANGEM_WC_PREFIX = "tangem://wc?uri="
+    }
 }
