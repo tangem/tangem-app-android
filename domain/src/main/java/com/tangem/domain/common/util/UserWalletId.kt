42b153309bc25175fc953194d51070160c09f9a2
Tangem <git@tangem.com>
2022-12-27 11:10:34 +0300

    Updated on 2026-09-23

--- domain/src/main/java/com/tangem/domain/common/util/UserWalletId.kt
+++ domain/src/main/java/com/tangem/domain/common/util/UserWalletId.kt
@@ -1,17 +1,15 @@
 package com.tangem.domain.common.util
 
-import com.tangem.common.extensions.calculateSha256
 import com.tangem.common.extensions.hexToBytes
 import com.tangem.common.extensions.toHexString
-import com.tangem.domain.common.extensions.calculateHmacSha256
 
 class UserWalletId(
     val stringValue: String,
 ) {
     val value = stringValue.hexToBytes()
 
-    constructor(walletPublicKey: ByteArray?) : this(
-        stringValue = walletPublicKey?.let { calculateUserWalletId(it).toHexString() } ?: "",
+    constructor(value: ByteArray?) : this(
+        stringValue = value?.toHexString() ?: "",
     )
 
     override fun equals(other: Any?): Boolean {
@@ -33,11 +31,3 @@ class UserWalletId(
         }
     }
 }
-
-private fun calculateUserWalletId(publicKey: ByteArray): ByteArray {
-    val message = MESSAGE_FOR_WALLET_ID.toByteArray()
-    val keyHash = publicKey.calculateSha256()
-    return message.calculateHmacSha256(keyHash)
-}
-
-private const val MESSAGE_FOR_WALLET_ID = "UserWalletID"
