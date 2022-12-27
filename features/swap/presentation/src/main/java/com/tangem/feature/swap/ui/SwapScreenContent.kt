ce4482d1bb66e8284158d7bcdc0923fbce3e432a
Tangem <git@tangem.com>
2022-12-27 15:29:08 +0400

    Updated on 2026-07-23

--- features/swap/presentation/src/main/java/com/tangem/feature/swap/ui/SwapScreenContent.kt
+++ features/swap/presentation/src/main/java/com/tangem/feature/swap/ui/SwapScreenContent.kt
@@ -1,9 +1,12 @@
 package com.tangem.feature.swap.ui
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -14,17 +17,24 @@ import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.tangem.core.ui.components.ClickableWarningCard
+import com.tangem.core.ui.components.Keyboard
 import com.tangem.core.ui.components.PrimaryButtonIconRight
+import com.tangem.core.ui.components.RefreshableWaringCard
 import com.tangem.core.ui.components.SmallInfoCard
 import com.tangem.core.ui.components.SmallInfoCardWithWarning
 import com.tangem.core.ui.components.SpacerH16
+import com.tangem.core.ui.components.keyboardAsState
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.feature.swap.models.FeeState
 import com.tangem.feature.swap.models.SwapButton
@@ -40,40 +50,71 @@ internal fun SwapScreenContent(
     state: SwapStateHolder,
     onPermissionWarningClick: () -> Unit,
 ) {
-    Column(
-        modifier = modifier
-            .fillMaxWidth()
-            .background(color = TangemTheme.colors.background.primary)
-            .verticalScroll(rememberScrollState())
-            .padding(
-                start = TangemTheme.dimens.spacing16,
-                end = TangemTheme.dimens.spacing16,
-                top = TangemTheme.dimens.spacing16,
-                bottom = TangemTheme.dimens.spacing32,
-            ),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(TangemTheme.dimens.spacing16),
+    val keyboard by keyboardAsState()
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Transparent),
     ) {
 
-        MainInfo(state)
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .background(color = TangemTheme.colors.background.primary)
+                .verticalScroll(rememberScrollState())
+                .padding(
+                    start = TangemTheme.dimens.spacing16,
+                    end = TangemTheme.dimens.spacing16,
+                    top = TangemTheme.dimens.spacing16,
+                    bottom = TangemTheme.dimens.spacing32,
+                ),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(TangemTheme.dimens.spacing16),
+        ) {
 
-        FeeItem(feeState = state.fee, currency = state.networkCurrency)
+            MainInfo(state)
 
-        if (state.warnings.isNotEmpty()) {
-            SwapWarnings(
-                warnings = state.warnings,
-                onApproveWarningClick = onPermissionWarningClick,
+            FeeItem(feeState = state.fee, currency = state.networkCurrency)
+
+            if (state.warnings.isNotEmpty()) {
+                SwapWarnings(
+                    warnings = state.warnings,
+                    onApproveWarningClick = onPermissionWarningClick,
+                )
+            }
+
+            PrimaryButtonIconRight(
+                modifier = Modifier.fillMaxWidth(),
+                text = stringResource(id = R.string.swapping_swap),
+                icon = painterResource(id = R.drawable.ic_tangem_24),
+                enabled = state.swapButton.enabled,
+                showProgress = state.swapButton.loading,
+                onClick = state.swapButton.onClick,
             )
         }
 
-        PrimaryButtonIconRight(
-            modifier = Modifier.fillMaxWidth(),
-            text = stringResource(id = R.string.swapping_swap),
-            icon = painterResource(id = R.drawable.ic_tangem_24),
-            enabled = state.swapButton.enabled,
-            showProgress = state.swapButton.loading,
-            onClick = state.swapButton.onClick,
-        )
+        AnimatedVisibility(
+            visible = keyboard is Keyboard.Opened,
+            modifier = Modifier
+                .align(Alignment.BottomCenter),
+        ) {
+            Text(
+                text = stringResource(id = R.string.send_max_amount_label),
+                style = TangemTheme.typography.button,
+                color = TangemTheme.colors.text.primary1,
+                modifier = Modifier
+                    .fillMaxWidth()
+
+                    .background(TangemTheme.colors.button.secondary)
+                    .clickable { state.onMaxAmountSelected }
+                    .padding(
+                        horizontal = TangemTheme.dimens.spacing14,
+                        vertical = TangemTheme.dimens.spacing16,
+                    ),
+                textAlign = TextAlign.Start,
+            )
+        }
     }
 }
 
@@ -171,6 +212,13 @@ private fun SwapWarnings(
                         onClick = onApproveWarningClick,
                     )
                 }
+                is SwapWarning.GenericWarning -> {
+                    RefreshableWaringCard(
+                        title = stringResource(id = R.string.common_warning),
+                        description = warning.message,
+                        onClick = warning.onClick,
+                    )
+                }
                 // is SwapWarning.RateExpired -> {
                 //     RefreshableWaringCard(
                 //         title = stringResource(id = R.string.),
@@ -186,7 +234,7 @@ private fun SwapWarnings(
 // region preview
 
 private val sendCard = SwapCardData(
-    type = TransactionCardType.SendCard("123", false),
+    type = TransactionCardType.SendCard("123", false, {}),
     amount = "1 000 000",
     amountEquivalent = "1 000 000",
     tokenIconUrl = "",
