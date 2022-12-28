8b5898e6ea29b0dcde2dd559fe769cf019a505d3
Tangem <git@tangem.com>
2022-12-28 14:49:41 +0300

    Updated on 2025-11-18

--- app/src/main/java/com/tangem/tap/domain/configurable/config/ConfigManager.kt
+++ app/src/main/java/com/tangem/tap/domain/configurable/config/ConfigManager.kt
@@ -2,7 +2,7 @@ package com.tangem.tap.domain.configurable.config
 
 import com.tangem.blockchain.common.BlockchainSdkConfig
 import com.tangem.blockchain.common.BlockchairCredentials
-import com.tangem.blockchain.common.QuickNodeBscCredentials
+import com.tangem.blockchain.common.QuickNodeCredentials
 import com.tangem.tap.common.shop.shopify.ShopifyShop
 import com.tangem.tap.common.zendesk.ZendeskConfig
 import com.tangem.tap.domain.configurable.Loader
@@ -91,9 +91,13 @@ class ConfigManager {
                     authToken = values.blockchairAuthorizationToken,
                 ),
                 blockcypherTokens = values.blockcypherTokens,
-                quickNodeBscCredentials = QuickNodeBscCredentials(
+                quickNodeCredentials = QuickNodeCredentials(
                     apiKey = values.quiknodeApiKey,
                     subdomain = values.quiknodeSubdomain,
+                ),
+                bscQuickNodeCredentials = QuickNodeCredentials(
+                    apiKey = values.bscQuiknodeApiKey,
+                    subdomain = values.bscQuiknodeSubdomain,
                 ),
                 infuraProjectId = values.infuraProjectId,
                 tronGridApiKey = values.tronGridApiKey,
@@ -116,9 +120,13 @@ class ConfigManager {
                     authToken = values.blockchairAuthorizationToken,
                 ),
                 blockcypherTokens = values.blockcypherTokens,
-                quickNodeBscCredentials = QuickNodeBscCredentials(
+                quickNodeCredentials = QuickNodeCredentials(
                     apiKey = values.quiknodeApiKey,
                     subdomain = values.quiknodeSubdomain,
+                ),
+                bscQuickNodeCredentials = QuickNodeCredentials(
+                    apiKey = values.bscQuiknodeApiKey,
+                    subdomain = values.bscQuiknodeSubdomain,
                 ),
                 infuraProjectId = values.infuraProjectId,
             ),
