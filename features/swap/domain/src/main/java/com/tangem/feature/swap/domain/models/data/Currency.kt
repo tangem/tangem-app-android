fa9a57150cc1d7f621ce7052237de4a764a861f1
Tangem <git@tangem.com>
2022-12-29 19:41:26 +0400

    Updated on 2026-05-19

--- features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/data/Currency.kt
+++ features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/data/Currency.kt
@@ -1,21 +1,25 @@
 package com.tangem.feature.swap.domain.models.data
 
-sealed class Currency(
-    open val id: String,
-    open val name: String,
-    open val symbol: String,
-    open val networkId: String,
-    open val logoUrl: String,
-) {
+import kotlinx.serialization.Serializable
 
+@Serializable
+sealed class Currency {
+    abstract val id: String
+    abstract val name: String
+    abstract val symbol: String
+    abstract val networkId: String
+    abstract val logoUrl: String
+
+    @Serializable
     data class NativeToken(
         override val id: String,
         override val name: String,
         override val symbol: String,
         override val networkId: String,
         override val logoUrl: String,
-    ) : Currency(id, name, symbol, networkId, logoUrl)
+    ) : Currency()
 
+    @Serializable
     data class NonNativeToken(
         override val id: String,
         override val name: String,
@@ -24,5 +28,5 @@ sealed class Currency(
         override val logoUrl: String,
         val contractAddress: String,
         val decimalCount: Int,
-    ) : Currency(id, name, symbol, networkId, logoUrl)
+    ) : Currency()
 }
