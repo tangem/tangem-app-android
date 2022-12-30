79e2f6243706d558334ca5fbd24ab87d754ba2ee
Tangem <git@tangem.com>
2022-12-30 17:39:37 +0300

    Updated on 2026-09-21

--- app/src/main/java/com/tangem/tap/features/wallet/data/WalletRepositoryImpl.kt
+++ app/src/main/java/com/tangem/tap/features/wallet/data/WalletRepositoryImpl.kt
@@ -3,18 +3,21 @@ package com.tangem.tap.features.wallet.data
 import com.tangem.datasource.api.tangemTech.TangemTechApi
 import com.tangem.datasource.api.tangemTech.models.CurrenciesResponse
 import com.tangem.tap.features.wallet.domain.WalletRepository
-import kotlinx.coroutines.Dispatchers
+import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import kotlinx.coroutines.withContext
 
 /**
  * Implementation of repository for Wallet feature
  *
  * @property tangemTechApi API for server requests
+ * @property dispatchers   coroutine dispatcher provider
  */
-class WalletRepositoryImpl(private val tangemTechApi: TangemTechApi) : WalletRepository {
+class WalletRepositoryImpl(
+    private val tangemTechApi: TangemTechApi,
+    private val dispatchers: CoroutineDispatcherProvider,
+) : WalletRepository {
 
-    override suspend fun getCurrencyList(): CurrenciesResponse =
-        withContext(Dispatchers.IO) { // TODO("After adding DI") replace with CoroutineDispatcherProvider
-            tangemTechApi.getCurrencyList()
-        }
+    override suspend fun getCurrencyList(): CurrenciesResponse = withContext(dispatchers.io) {
+        tangemTechApi.getCurrencyList()
+    }
 }
