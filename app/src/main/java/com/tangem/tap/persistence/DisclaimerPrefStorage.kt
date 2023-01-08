955306dd0ef96ecd1e86478a98ddbe96e4c1bf69
Tangem <git@tangem.com>
2023-01-09 00:35:28 +0300

    Updated on 2026-06-17

--- app/src/main/java/com/tangem/tap/persistence/DisclaimerPrefStorage.kt
+++ app/src/main/java/com/tangem/tap/persistence/DisclaimerPrefStorage.kt
@@ -10,21 +10,11 @@ class DisclaimerPrefStorage(
     private val preferences: SharedPreferences,
 ) {
 
-    var hasTangemTosAccepted: Boolean
-        get() = preferences.getBoolean(KEY_TANGEM_TOS, false)
-        set(value) = preferences.edit { putBoolean(KEY_TANGEM_TOS, value) }
+    fun accept(disclaimerKey: String) {
+        preferences.edit { putBoolean(disclaimerKey, true) }
+    }
 
-    var hasStart2CoinTosAccepted: Boolean
-        get() = preferences.getBoolean(KEY_START_2_COIN_TOS, false)
-        set(value) = preferences.edit { putBoolean(KEY_START_2_COIN_TOS, value) }
-
-    var hasSaltPayTosAccepted: Boolean
-        get() = preferences.getBoolean(KEY_SALT_PAY_TOS, false)
-        set(value) = preferences.edit { putBoolean(KEY_SALT_PAY_TOS, value) }
-
-    companion object {
-        private const val KEY_TANGEM_TOS = "tangem_tos_accepted"
-        private const val KEY_START_2_COIN_TOS = "s2c_tos_accepted"
-        private const val KEY_SALT_PAY_TOS = "saltPay_tos_accepted"
+    fun isAccepted(disclaimerKey: String): Boolean {
+        return preferences.getBoolean(disclaimerKey, false)
     }
 }
