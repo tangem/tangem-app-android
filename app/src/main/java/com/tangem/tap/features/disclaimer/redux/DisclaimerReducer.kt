8d41f473560d15a048667c79b3e19d0c004739a8
Tangem <git@tangem.com>
2023-01-09 00:35:28 +0300

    Updated on 2026-09-21

--- app/src/main/java/com/tangem/tap/features/disclaimer/redux/DisclaimerReducer.kt
+++ app/src/main/java/com/tangem/tap/features/disclaimer/redux/DisclaimerReducer.kt
@@ -15,15 +15,19 @@ private fun internalReduce(action: Action, state: AppState): DisclaimerState {
     val disclaimerState = state.disclaimerState
 
     return when (action) {
-        is DisclaimerAction.Show -> disclaimerState.copy(
-            callback = action.callback,
+        is DisclaimerAction.SetDisclaimer -> disclaimerState.copy(
+            disclaimer = action.disclaimer,
         )
-        is DisclaimerAction.UpdateState -> disclaimerState.copy(
-            type = action.type,
-            accepted = action.accepted,
+        is DisclaimerAction.Show -> disclaimerState.copy(
+            showedFromScreen = action.fromScreen,
+            callback = action.callback,
         )
         is DisclaimerAction.AcceptDisclaimer, is DisclaimerAction.OnBackPressed -> disclaimerState.copy(
             callback = null,
+            progressState = null,
+        )
+        is DisclaimerAction.OnProgressStateChanged -> disclaimerState.copy(
+            progressState = action.state,
         )
         else -> disclaimerState
     }
