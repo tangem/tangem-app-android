7beb471b8d3698b196d30c95bb62579794a27cec
Tangem <git@tangem.com>
2023-01-09 13:56:57 +0300

    Updated on 2026-01-20

--- app/src/main/java/com/tangem/tap/common/extensions/Specific.kt
+++ app/src/main/java/com/tangem/tap/common/extensions/Specific.kt
@@ -11,6 +11,7 @@ import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
 import java.util.*
 
+//todo move extensions to utils
 fun BigDecimal.toFormattedString(
     decimals: Int, roundingMode: RoundingMode = RoundingMode.DOWN, locale: Locale = Locale.US
 ): String {
