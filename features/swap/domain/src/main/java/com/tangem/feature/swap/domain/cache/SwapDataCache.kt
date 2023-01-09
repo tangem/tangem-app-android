21b304106c08480b0536a8a43a59eab444b496c8
Tangem <git@tangem.com>
2023-01-09 13:56:57 +0300

    Updated on 2026-03-27

--- features/swap/domain/src/main/java/com/tangem/feature/swap/domain/cache/SwapDataCache.kt
+++ features/swap/domain/src/main/java/com/tangem/feature/swap/domain/cache/SwapDataCache.kt
@@ -2,17 +2,17 @@ package com.tangem.feature.swap.domain.cache
 
 import com.tangem.feature.swap.domain.models.ExchangeCurrencies
 import com.tangem.feature.swap.domain.models.data.Currency
+import com.tangem.feature.swap.domain.models.data.SwapAmount
 import com.tangem.feature.swap.domain.models.data.SwapState.QuoteModel
-import java.math.BigDecimal
 
 interface SwapDataCache {
 
     fun cacheNetworkId(networkId: String)
-    fun cacheSwapParams(quoteModel: QuoteModel, amount: BigDecimal, fromCurrency: Currency, toCurrency: Currency)
+    fun cacheSwapParams(quoteModel: QuoteModel, amount: SwapAmount, fromCurrency: Currency, toCurrency: Currency)
     fun cacheAvailableToSwapTokens(networkId: String, tokens: List<Currency>)
     fun getAvailableTokens(networkId: String): List<Currency>
     fun getLastQuote(): QuoteModel?
-    fun getAmountToSwap(): BigDecimal?
+    fun getAmountToSwap(): SwapAmount?
     fun getNetworkId(): String?
     fun getExchangeCurrencies(): ExchangeCurrencies?
 }
