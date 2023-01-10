03ee846bee06ad81840d51ba4754f625eb38def0
Tangem <git@tangem.com>
2023-01-10 14:16:28 +0400

    Updated on 2026-08-27

--- app/src/main/java/com/tangem/tap/domain/tokens/converters/CurrencyConverter.kt
+++ app/src/main/java/com/tangem/tap/domain/tokens/converters/CurrencyConverter.kt
@@ -1,14 +1,14 @@
 package com.tangem.tap.domain.tokens.converters
 
-import com.tangem.datasource.api.tangemTech.models.TokenBody
+import com.tangem.datasource.api.tangemTech.models.UserTokensResponse
 import com.tangem.domain.common.extensions.toNetworkId
 import com.tangem.tap.features.wallet.models.Currency
 import com.tangem.utils.converter.Converter
 
-/** Converter from domain model [Currency] to data model [TokenBody] */
-object CurrencyConverter : Converter<Currency, TokenBody> {
+/** Converter from domain model [Currency] to data model [UserTokensResponse.Token] */
+object CurrencyConverter : Converter<Currency, UserTokensResponse.Token> {
 
-    override fun convert(value: Currency) = TokenBody(
+    override fun convert(value: Currency) = UserTokensResponse.Token(
         id = value.coinId,
         networkId = value.blockchain.toNetworkId(),
         derivationPath = value.derivationPath,
