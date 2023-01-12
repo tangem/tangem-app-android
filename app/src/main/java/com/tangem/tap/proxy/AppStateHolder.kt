df6b0884c5ade51f33689fffa4f9113ecf02811c
Tangem <git@tangem.com>
2023-01-12 16:54:51 +0300

    Updated on 2026-02-25

--- app/src/main/java/com/tangem/tap/proxy/AppStateHolder.kt
+++ app/src/main/java/com/tangem/tap/proxy/AppStateHolder.kt
@@ -3,6 +3,7 @@ package com.tangem.tap.proxy
 import com.tangem.TangemSdk
 import com.tangem.domain.common.CardDTO
 import com.tangem.domain.common.ScanResponse
+import com.tangem.tap.common.entities.FiatCurrency
 import com.tangem.tap.common.redux.AppState
 import com.tangem.tap.domain.TangemSdkManager
 import com.tangem.tap.domain.tokens.UserTokensRepository
@@ -16,7 +17,7 @@ import javax.inject.Inject
  * Holds objects from old modules, that missing in DI graph
  * Object sets manually to use in new modules and [AppStateHolder] proxies its to DI
  */
-class AppStateHolder @Inject constructor(){
+class AppStateHolder @Inject constructor() {
 
     var scanResponse: ScanResponse? = null
     var walletState: WalletState? = null
@@ -26,6 +27,7 @@ class AppStateHolder @Inject constructor(){
     var tangemSdk: TangemSdk? = null
     var walletStoresManager: WalletStoresManager? = null
     var userWalletsListManager: UserWalletsListManager? = null
+    var appFiatCurrency: FiatCurrency = FiatCurrency.Default
 
     fun getActualCard(): CardDTO? {
         return scanResponse?.card
