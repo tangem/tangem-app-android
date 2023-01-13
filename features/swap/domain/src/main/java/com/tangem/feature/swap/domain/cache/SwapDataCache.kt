83571f3bd571f2662171e402c77e4c634fa0edb7
Tangem <git@tangem.com>
2023-01-13 17:01:04 +0300

    Updated on 2026-08-14

--- features/swap/domain/src/main/java/com/tangem/feature/swap/domain/cache/SwapDataCache.kt
+++ features/swap/domain/src/main/java/com/tangem/feature/swap/domain/cache/SwapDataCache.kt
@@ -1,17 +1,23 @@
 package com.tangem.feature.swap.domain.cache
 
+import com.tangem.feature.swap.domain.models.ApproveModel
 import com.tangem.feature.swap.domain.models.Currency
 import com.tangem.feature.swap.domain.models.QuoteModel
 import com.tangem.feature.swap.domain.models.SwapAmount
+import com.tangem.feature.swap.domain.models.SwapDataModel
 import com.tangem.feature.swap.domain.models.cache.ExchangeCurrencies
 
 interface SwapDataCache {
 
     fun cacheNetworkId(networkId: String)
     fun cacheSwapParams(quoteModel: QuoteModel, amount: SwapAmount, fromCurrency: Currency, toCurrency: Currency)
+    fun cacheApproveTransactionData(approve: ApproveModel)
+    fun cacheSwapData(swapDataModel: SwapDataModel)
     fun cacheAvailableToSwapTokens(networkId: String, tokens: List<Currency>)
     fun getAvailableTokens(networkId: String): List<Currency>
+    fun getApproveTransactionData(): ApproveModel?
     fun getLastQuote(): QuoteModel?
+    fun getLastSwapData(): SwapDataModel?
     fun getAmountToSwap(): SwapAmount?
     fun getNetworkId(): String?
     fun getExchangeCurrencies(): ExchangeCurrencies?
