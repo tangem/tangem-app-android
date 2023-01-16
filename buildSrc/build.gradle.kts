6e4de6262129580cb4c8d6bde86f76202ae3720c
Tangem <git@tangem.com>
2023-01-16 19:28:11 +0800

    Updated on 2026-09-21

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,5 +3,14 @@ plugins {
 }
 
 repositories {
+    google()
     mavenCentral()
+}
+
+dependencies {
+    implementation("com.android.tools.build:gradle:7.1.3")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+    //TODO("Upgrade version to 1.22.0 after upgrading kotlin version to 1.7.21")
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.21.0")
+    implementation("com.squareup:javapoet:1.13.0")
 }
