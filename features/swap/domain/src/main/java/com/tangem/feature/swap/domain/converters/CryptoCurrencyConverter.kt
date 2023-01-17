e50784d5384bd6b719d9f59dd36cd7007382408d
Tangem <git@tangem.com>
2023-01-17 16:23:33 +0300

    Updated on 2026-05-19

--- features/swap/domain/src/main/java/com/tangem/feature/swap/domain/converters/CryptoCurrencyConverter.kt
+++ features/swap/domain/src/main/java/com/tangem/feature/swap/domain/converters/CryptoCurrencyConverter.kt
@@ -1,14 +1,15 @@
 package com.tangem.feature.swap.domain.converters
 
-import com.tangem.feature.swap.domain.models.Currency
-import com.tangem.utils.converter.Converter
+import com.tangem.feature.swap.domain.models.domain.Currency
+import com.tangem.utils.converter.TwoWayConverter
+import com.tangem.lib.crypto.models.Currency as CryptoCurrency
 
-class CryptoCurrencyConverter : Converter<Currency, com.tangem.lib.crypto.models.Currency> {
+class CryptoCurrencyConverter : TwoWayConverter<Currency, CryptoCurrency> {
 
-    override fun convert(value: Currency): com.tangem.lib.crypto.models.Currency {
+    override fun convert(value: Currency): CryptoCurrency {
         return when (value) {
             is Currency.NonNativeToken -> {
-                com.tangem.lib.crypto.models.Currency.NonNativeToken(
+                CryptoCurrency.NonNativeToken(
                     id = value.id,
                     name = value.name,
                     symbol = value.symbol,
@@ -18,11 +19,36 @@ class CryptoCurrencyConverter : Converter<Currency, com.tangem.lib.crypto.models
                 )
             }
             is Currency.NativeToken -> {
-                com.tangem.lib.crypto.models.Currency.NativeToken(
+                CryptoCurrency.NativeToken(
                     id = value.id,
                     name = value.name,
                     symbol = value.symbol,
                     networkId = value.networkId,
+                )
+            }
+        }
+    }
+
+    override fun convertBack(value: CryptoCurrency): Currency {
+        return when (value) {
+            is CryptoCurrency.NonNativeToken -> {
+                Currency.NonNativeToken(
+                    id = value.id,
+                    name = value.name,
+                    symbol = value.symbol,
+                    networkId = value.networkId,
+                    contractAddress = value.contractAddress,
+                    decimalCount = value.decimalCount,
+                    logoUrl = "",
+                )
+            }
+            is CryptoCurrency.NativeToken -> {
+                Currency.NativeToken(
+                    id = value.id,
+                    name = value.name,
+                    symbol = value.symbol,
+                    networkId = value.networkId,
+                    logoUrl = "",
                 )
             }
         }
