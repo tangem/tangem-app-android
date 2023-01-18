020f52e23e195bdec01a28156c5087b282a5509c
Tangem <git@tangem.com>
2023-01-18 18:05:18 +0800

    Updated on 2026-06-17

--- app/src/main/java/com/tangem/tap/common/extensions/Token.kt
+++ app/src/main/java/com/tangem/tap/common/extensions/Token.kt
@@ -6,19 +6,23 @@ import androidx.core.graphics.luminance
 import androidx.core.graphics.toColorInt
 import com.tangem.blockchain.common.Token
 
+@Suppress("MagicNumber")
 @ColorInt
 fun Token.getColor(isTestnet: Boolean = false): Int {
     val defaultColor = "#C7C7CC".toColorInt() // equivalent to R.color.lightGray4
 
-    return if (isTestnet)
+    return if (isTestnet) {
         defaultColor
-    else try {
-        ("#" + this.contractAddress.subSequence(2..7).toString()).toColorInt()
-    } catch (exception: Exception) {
-        defaultColor
+    } else {
+        try {
+            ("#" + this.contractAddress.subSequence(2..7).toString()).toColorInt()
+        } catch (exception: Exception) {
+            defaultColor
+        }
     }
 }
 
+@Suppress("MagicNumber")
 @ColorInt
 fun Token.getTextColor(isTestnet: Boolean = false): Int = when {
     isTestnet -> Color.WHITE
