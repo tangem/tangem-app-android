c46dc3bbb7b748a44748bd466a32e1af4c23a86d
Tangem <git@tangem.com>
2023-01-19 12:51:42 +0300

    Updated on 2026-06-05

--- app/src/main/java/com/tangem/tap/features/details/ui/walletconnect/WalletConnectFragment.kt
+++ app/src/main/java/com/tangem/tap/features/details/ui/walletconnect/WalletConnectFragment.kt
@@ -10,6 +10,8 @@ import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.transition.TransitionInflater
 import com.google.accompanist.appcompattheme.AppCompatTheme
+import com.tangem.tap.common.analytics.Analytics
+import com.tangem.tap.common.analytics.events.WalletConnect
 import com.tangem.tap.common.redux.navigation.NavigationAction
 import com.tangem.tap.features.details.redux.walletconnect.WalletConnectAction
 import com.tangem.tap.features.details.redux.walletconnect.WalletConnectState
@@ -23,6 +25,7 @@ class WalletConnectFragment : Fragment(), StoreSubscriber<WalletConnectState> {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Analytics.send(WalletConnect.ScreenOpened())
         val inflater = TransitionInflater.from(requireContext())
         enterTransition = inflater.inflateTransition(android.R.transition.fade)
         exitTransition = inflater.inflateTransition(android.R.transition.fade)
