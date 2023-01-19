102fb64dfdef7d0be6190374386f09fd67f774c4
Tangem <git@tangem.com>
2023-01-19 20:27:59 +0800

    release/5.27.0

--- app/src/main/java/com/tangem/tap/features/details/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/tangem/tap/features/details/ui/details/DetailsFragment.kt
@@ -9,8 +9,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.transition.TransitionInflater
-import com.tangem.core.ui.res.TangemTheme
 import com.tangem.core.analytics.Analytics
+import com.tangem.core.ui.res.TangemTheme
 import com.tangem.tap.common.analytics.events.Settings
 import com.tangem.tap.common.redux.navigation.NavigationAction
 import com.tangem.tap.features.details.redux.DetailsState
@@ -43,7 +43,7 @@ class DetailsFragment : Fragment(), StoreSubscriber<DetailsState> {
                 TangemTheme {
                     DetailsScreen(
                         state = detailsScreenState.value,
-                        onBackPressed = { store.dispatch(NavigationAction.PopBackTo()) },
+                        onBackClick = { store.dispatch(NavigationAction.PopBackTo()) },
                     )
                 }
             }
