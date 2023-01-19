a4fc4569901488c3e6f1289ee459b0487a2d51ea
Tangem <git@tangem.com>
2023-01-19 15:09:30 +0300

    Updated on 2026-07-23

--- app/src/main/java/com/tangem/tap/features/welcome/redux/WelcomeReducer.kt
+++ app/src/main/java/com/tangem/tap/features/welcome/redux/WelcomeReducer.kt
@@ -12,7 +12,7 @@ internal object WelcomeReducer {
 
     private fun internalReduce(action: WelcomeAction, state: WelcomeState): WelcomeState {
         return when (action) {
-            is WelcomeAction.HandleDeepLink -> state.copy(deepLinkIntent = action.intent)
+            is WelcomeAction.HandleIntentIfNeeded -> state.copy(intent = action.intent)
             is WelcomeAction.ProceedWithBiometrics -> state.copy(isUnlockWithBiometricsInProgress = true)
             is WelcomeAction.ProceedWithCard -> state.copy(isUnlockWithCardInProgress = true)
             is WelcomeAction.ProceedWithBiometrics.Error -> state.copy(
