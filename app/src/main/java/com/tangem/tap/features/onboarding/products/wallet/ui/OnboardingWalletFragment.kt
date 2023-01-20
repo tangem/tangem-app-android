d94fcef61602978471233207992e33795b2bf049
Tangem <git@tangem.com>
2023-01-20 18:52:52 +0800

    Updated on 2025-09-25

--- app/src/main/java/com/tangem/tap/features/onboarding/products/wallet/ui/OnboardingWalletFragment.kt
+++ app/src/main/java/com/tangem/tap/features/onboarding/products/wallet/ui/OnboardingWalletFragment.kt
@@ -78,7 +78,7 @@ class OnboardingWalletFragment : BaseFragment(R.layout.fragment_onboarding_walle
             binding.tabLayoutBackupInfo,
             binding.viewPagerBackupInfo,
         ) { tab, position ->
-            //Some implementation
+            // Some implementation
         }.attach()
 
         (activity as? AppCompatActivity)?.setSupportActionBar(binding.toolbar)
