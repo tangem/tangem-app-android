8a3accc0c34f1abd62e9817198f45e3c2fd6461b
Tangem <git@tangem.com>
2023-01-21 02:23:53 +0300

    release/5.26.1

--- app/src/main/java/com/tangem/tap/features/details/ui/appsettings/AppSettingsViewModel.kt
+++ app/src/main/java/com/tangem/tap/features/details/ui/appsettings/AppSettingsViewModel.kt
@@ -2,35 +2,33 @@ package com.tangem.tap.features.details.ui.appsettings
 
 import com.tangem.tap.common.extensions.dispatchOnMain
 import com.tangem.tap.common.redux.AppState
+import com.tangem.tap.features.details.redux.AppSetting
 import com.tangem.tap.features.details.redux.DetailsAction
 import com.tangem.tap.features.details.redux.DetailsState
-import com.tangem.tap.features.details.redux.PrivacySetting
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import org.rekotlin.Store
 
 class AppSettingsViewModel(private val store: Store<AppState>) {
-    private val _uiState = MutableStateFlow(updateState(store.state.detailsState))
-    val uiState: StateFlow<AppSettingsScreenState> = _uiState
 
     fun updateState(state: DetailsState): AppSettingsScreenState {
-        return AppSettingsScreenState(
-            settings = mapOf(
-                PrivacySetting.SaveWallets to state.saveWallets,
-                PrivacySetting.SaveAccessCode to state.saveAccessCodes,
-            ),
-            showEnrollBiometricsCard = state.needEnrollBiometrics,
-            isTogglesEnabled = !state.needEnrollBiometrics,
-            onSettingToggled = { privacySetting, enabled ->
-                onSettingsToggled(privacySetting, enabled)
-            },
-            onEnrollBiometrics = {
-                store.dispatchOnMain(DetailsAction.AppSettings.EnrollBiometrics)
-            },
-        )
+        return with(state.appSettingsState) {
+            AppSettingsScreenState(
+                settings = mapOf(
+                    AppSetting.SaveWallets to saveWallets,
+                    AppSetting.SaveAccessCode to saveAccessCodes,
+                ),
+                showEnrollBiometricsCard = needEnrollBiometrics,
+                isTogglesEnabled = !needEnrollBiometrics && !isInProgress,
+                onSettingToggled = { privacySetting, enabled ->
+                    onSettingsToggled(privacySetting, enabled)
+                },
+                onEnrollBiometrics = {
+                    store.dispatchOnMain(DetailsAction.AppSettings.EnrollBiometrics)
+                },
+            )
+        }
     }
 
-    private fun onSettingsToggled(setting: PrivacySetting, enable: Boolean) {
+    private fun onSettingsToggled(setting: AppSetting, enable: Boolean) {
         store.dispatch(DetailsAction.AppSettings.SwitchPrivacySetting(enable = enable, setting = setting))
     }
 
