6fcb8781b64f85abb697a2ad4c8c4723bcd2dfd9
Tangem <git@tangem.com>
2023-01-23 17:01:05 +0300

    Updated on 2025-12-22

--- features/swap/domain/src/main/java/com/tangem/feature/swap/domain/cache/SwapDataCache.kt
+++ features/swap/domain/src/main/java/com/tangem/feature/swap/domain/cache/SwapDataCache.kt
@@ -1,30 +1,16 @@
 package com.tangem.feature.swap.domain.cache
 
-import com.tangem.feature.swap.domain.models.SwapAmount
-import com.tangem.feature.swap.domain.models.cache.ExchangeCurrencies
-import com.tangem.feature.swap.domain.models.domain.ApproveModel
 import com.tangem.feature.swap.domain.models.domain.Currency
-import com.tangem.feature.swap.domain.models.domain.QuoteModel
-import com.tangem.feature.swap.domain.models.domain.SwapDataModel
+import java.math.BigDecimal
 
 interface SwapDataCache {
 
-    fun cacheNetworkId(networkId: String)
-    fun cacheQuoteData(quoteModel: QuoteModel)
-    fun cacheSwapData(swapDataModel: SwapDataModel)
-    fun cacheAmountToSwap(amount: SwapAmount)
-    fun cacheApproveTransactionData(approve: ApproveModel)
     fun cacheAvailableToSwapTokens(networkId: String, tokens: List<Currency>)
     fun cacheInWalletTokens(tokens: List<Currency>)
     fun cacheLoadedTokens(tokens: List<Currency>)
-    fun cacheExchangeCurrencies(fromToken: Currency, toToken: Currency)
+    fun cacheLastFeeForNetwork(fee: BigDecimal, networkId: String)
     fun getAvailableTokens(networkId: String): List<Currency>
-    fun getApproveTransactionData(): ApproveModel?
-    fun getLastQuote(): QuoteModel?
-    fun getLastSwapData(): SwapDataModel?
-    fun getNetworkId(): String?
-    fun getAmountToSwap(): SwapAmount?
-    fun getExchangeCurrencies(): ExchangeCurrencies?
     fun getInWalletTokens(): List<Currency>
     fun getLoadedTokens(): List<Currency>
+    fun getLastFeeForNetwork(networkId: String): BigDecimal?
 }
