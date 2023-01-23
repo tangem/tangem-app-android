875401f6eec345784edc56532984cde0f1a74ca7
Tangem <git@tangem.com>
2023-01-23 22:13:52 +0300

    Updated on 2026-05-13

--- app/src/main/java/com/tangem/tap/common/analytics/events/MyWallets.kt
+++ app/src/main/java/com/tangem/tap/common/analytics/events/MyWallets.kt
@@ -9,12 +9,12 @@ sealed class MyWallets(
 
     class MyWalletsScreenOpened : MyWallets(event = "My Wallets Screen Opened")
     class CardWasScanned : MyWallets(event = "Card Was Scanned")
-    class WalletUnlockTapped : MyWallets(event = "Wallet Unlock Tapped")
 
     sealed class Button {
         class ScanNewCard : MyWallets(event = "Button - Scan New Card")
         class UnlockWithBiometrics : MyWallets(event = "Button - Unlock all with Face ID")
         class EditWalletTapped : MyWallets(event = "Button - Edit Wallet Tapped")
         class DeleteWalletTapped : MyWallets(event = "Button - Delete Wallet Tapped")
+        class WalletUnlockTapped : MyWallets(event = "Button - Wallet Unlock Tapped")
     }
 }
