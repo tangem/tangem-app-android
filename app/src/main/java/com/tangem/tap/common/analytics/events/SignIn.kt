648db37b5db44d391670429ddc54159096c5ecc2
Tangem <git@tangem.com>
2023-01-23 17:01:14 +0300

    Updated on 2025-10-09

---
+++ app/src/main/java/com/tangem/tap/common/analytics/events/SignIn.kt
@@ -0,0 +1,17 @@
+package com.tangem.tap.common.analytics.events
+
+import com.tangem.core.analytics.AnalyticsEvent
+
+/**
+[REDACTED_AUTHOR]
+ */
+sealed class SignIn(
+    event: String,
+    params: Map<String, String> = mapOf(),
+    error: Throwable? = null,
+) : AnalyticsEvent("Sign In", event, params, error) {
+
+    class ScreenOpened : SignIn(event = "Sing In Screen Opened")
+    class ButtonBiometricSignIn : SignIn(event = "Button - Biometric Sign In")
+    class ButtonCardSignIn : SignIn(event = "Button - Card Sign In")
+}
