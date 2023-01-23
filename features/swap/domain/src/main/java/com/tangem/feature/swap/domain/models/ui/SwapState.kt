15bc9ee94e71c4767f9dd4282bd2f183c21c0281
Tangem <git@tangem.com>
2023-01-23 17:01:05 +0300

    Updated on 2026-06-24

--- features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/ui/SwapState.kt
+++ features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/ui/SwapState.kt
@@ -2,26 +2,28 @@ package com.tangem.feature.swap.domain.models.ui
 
 import com.tangem.feature.swap.domain.models.DataError
 import com.tangem.feature.swap.domain.models.SwapAmount
+import com.tangem.feature.swap.domain.models.domain.ApproveModel
+import com.tangem.feature.swap.domain.models.domain.PreparedSwapConfigState
+import com.tangem.feature.swap.domain.models.domain.SwapDataModel
 
 sealed interface SwapState {
 
     data class QuotesLoadedState(
-        val fromTokenAmount: SwapAmount,
-        val toTokenAmount: SwapAmount,
-        val fromTokenAddress: String,
-        val toTokenAddress: String,
-        val fromTokenWalletBalance: String,
-        val fromTokenFiatBalance: String,
-        val toTokenWalletBalance: String,
-        val toTokenFiatBalance: String,
+        val fromTokenInfo: TokenSwapInfo,
+        val toTokenInfo: TokenSwapInfo,
         val fee: String,
-        val isAllowedToSpend: Boolean = false,
+        val networkCurrency: String,
+        val preparedSwapConfigState: PreparedSwapConfigState,
         val permissionState: PermissionDataState = PermissionDataState.Empty,
+        val swapDataModel: SwapDataModel? = null,
     ) : SwapState
 
-    data class SwapError(
-        val errorType: DataError,
+    data class EmptyAmountState(
+        val fromTokenWalletBalance: String,
+        val toTokenWalletBalance: String,
     ) : SwapState
+
+    data class SwapError(val error: DataError) : SwapState
 }
 
 sealed class PermissionDataState {
@@ -32,6 +34,7 @@ sealed class PermissionDataState {
         val walletAddress: String,
         val spenderAddress: String,
         val fee: String,
+        val requestApproveData: RequestApproveStateData,
     ) : PermissionDataState()
 
     object PermissionFailed : PermissionDataState()
@@ -40,3 +43,15 @@ sealed class PermissionDataState {
 
     object Empty : PermissionDataState()
 }
+
+data class TokenSwapInfo(
+    val tokenAmount: SwapAmount,
+    val tokenId: String,
+    val tokenWalletBalance: String,
+    val tokenFiatBalance: String,
+)
+
+data class RequestApproveStateData(
+    val estimatedGas: Int,
+    val approveModel: ApproveModel,
+)
