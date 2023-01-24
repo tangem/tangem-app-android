1cc9caefa12e00b6f0575305c11bef2622ad18d8
Tangem <git@tangem.com>
2023-01-24 15:07:35 +0300

    Updated on 2026-09-14

--- app/src/main/java/com/tangem/tap/domain/walletStores/repository/implementation/utils/WalletStoreOperations.kt
+++ app/src/main/java/com/tangem/tap/domain/walletStores/repository/implementation/utils/WalletStoreOperations.kt
@@ -104,6 +104,9 @@ private inline fun List<WalletStoreModel>.replaceWalletStores(
 
     walletStoresToUpdate.forEach { walletStoreToUpdate ->
         val index = mutableStores.indexOfFirst(walletStoreToUpdate::isSameWalletStore)
+        // Can be possible if user hides wallet store when it's tokens is loading
+        if (index == -1) return@forEach
+
         val currentWalletStore = mutableStores[index]
         val updatedWalletStore = update(currentWalletStore)
 
@@ -124,6 +127,7 @@ private inline fun List<WalletStoreModel>.replaceWalletStores(
 }
 
 internal fun WalletStoreModel.isSameWalletStore(other: WalletStoreModel): Boolean {
-    return this.blockchain == other.blockchain &&
+    return this.userWalletId == other.userWalletId &&
+        this.blockchain == other.blockchain &&
         this.derivationPath == other.derivationPath
 }
