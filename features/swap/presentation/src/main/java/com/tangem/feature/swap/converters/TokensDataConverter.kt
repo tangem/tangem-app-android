e569b475c92fd6c06932f6ff6df09569b68d2a75
Tangem <git@tangem.com>
2023-01-30 10:55:23 +0300

    Updated on 2026-09-23

--- features/swap/presentation/src/main/java/com/tangem/feature/swap/converters/TokensDataConverter.kt
+++ features/swap/presentation/src/main/java/com/tangem/feature/swap/converters/TokensDataConverter.kt
@@ -1,33 +1,26 @@
 package com.tangem.feature.swap.converters
 
+import com.tangem.feature.swap.domain.models.domain.Currency
+import com.tangem.feature.swap.domain.models.domain.NetworkInfo
 import com.tangem.feature.swap.domain.models.ui.FoundTokensState
 import com.tangem.feature.swap.domain.models.ui.TokenWithBalance
 import com.tangem.feature.swap.models.Network
 import com.tangem.feature.swap.models.SwapSelectTokenStateHolder
 import com.tangem.feature.swap.models.TokenBalanceData
 import com.tangem.feature.swap.models.TokenToSelect
-import com.tangem.utils.converter.Converter
 
 class TokensDataConverter(
     private val onSearchEntered: (String) -> Unit,
     private val onTokenSelected: (String) -> Unit,
-) : Converter<FoundTokensState, SwapSelectTokenStateHolder> {
+) {
 
-    private var networkName: String? = null
-    private var blockchainId: String? = null
-
-    fun setNetworkData(networkName: String, blockchainId: String) {
-        this.networkName = networkName
-        this.blockchainId = blockchainId
-    }
-
-    override fun convert(value: FoundTokensState): SwapSelectTokenStateHolder {
+    fun convertWithNetwork(value: FoundTokensState, network: NetworkInfo): SwapSelectTokenStateHolder {
         return SwapSelectTokenStateHolder(
             addedTokens = value.tokensInWallet.map { tokenWithBalanceToTokenToSelect(it) },
             otherTokens = value.loadedTokens.map { tokenWithBalanceToTokenToSelect(it) },
             onSearchEntered = onSearchEntered,
             onTokenSelected = onTokenSelected,
-            network = Network(networkName ?: "", blockchainId ?: ""),
+            network = Network(network.name, network.blockchainId),
         )
     }
 
@@ -37,6 +30,7 @@ class TokensDataConverter(
             name = tokenWithBalance.token.name,
             symbol = tokenWithBalance.token.symbol,
             iconUrl = tokenWithBalance.token.logoUrl,
+            isNative = tokenWithBalance.token is Currency.NativeToken,
             addedTokenBalanceData = TokenBalanceData(
                 amount = tokenWithBalance.tokenBalanceData?.amount,
                 amountEquivalent = tokenWithBalance.tokenBalanceData?.amountEquivalent,
