2aae459e4e183a52b666cf7d882d15ab6cd5b0e7
Tangem <git@tangem.com>
2023-01-30 18:21:13 +0300

    Updated on 2026-08-07

--- features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/ui/TxState.kt
+++ features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/ui/TxState.kt
@@ -2,7 +2,12 @@ package com.tangem.feature.swap.domain.models.ui
 
 sealed class TxState {
 
-    data class TxSent(val fromAmount: String? = null, val toAmount: String? = null) : TxState()
+    data class TxSent(
+        val fromAmount: String? = null,
+        val toAmount: String? = null,
+        val txAddress: String,
+    ) : TxState()
+
     object UserCancelled : TxState()
     object BlockchainError : TxState()
     object TangemSdkError : TxState()
