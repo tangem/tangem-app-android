097ae110883fabd76c6d4112c0a727395328a09d
Tangem <git@tangem.com>
2023-02-01 12:51:21 +0300

    Updated on 2026-04-08

--- app/src/main/java/com/tangem/tap/features/walletSelector/ui/components/RenameWalletDialogContent.kt
+++ app/src/main/java/com/tangem/tap/features/walletSelector/ui/components/RenameWalletDialogContent.kt
@@ -1,12 +1,6 @@
 package com.tangem.tap.features.walletSelector.ui.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,65 +10,39 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.window.Dialog
-import com.tangem.core.ui.components.OutlineTextField
-import com.tangem.core.ui.components.SpacerH16
-import com.tangem.core.ui.components.SpacerH24
-import com.tangem.core.ui.components.SpacerW8
-import com.tangem.core.ui.components.TextButton
+import com.tangem.core.ui.components.AdditionalTextInputDialogParams
+import com.tangem.core.ui.components.DialogButton
+import com.tangem.core.ui.components.TextInputDialog
 import com.tangem.core.ui.res.TangemTheme
-import com.tangem.tap.features.walletSelector.ui.model.RenameWalletDialog
+import com.tangem.tap.features.walletSelector.ui.model.DialogModel
 import com.tangem.wallet.R
 
 @Composable
-internal fun RenameWalletDialogContent(dialog: RenameWalletDialog) {
-    Dialog(onDismissRequest = dialog.onCancel) {
-        Column(
-            modifier = Modifier
-                .background(
-                    shape = TangemTheme.shapes.roundedCornersLarge,
-                    color = TangemTheme.colors.background.plain,
-                )
-                .padding(all = TangemTheme.dimens.spacing24),
-        ) {
-            var value by remember {
-                mutableStateOf(TextFieldValue(text = dialog.currentName))
-            }
-
-            Text(
-                modifier = Modifier.fillMaxWidth(),
-                text = stringResource(R.string.user_wallet_list_rename_popup_title),
-                style = TangemTheme.typography.h3,
-                color = TangemTheme.colors.text.primary1,
-            )
-            SpacerH16()
-            OutlineTextField(
-                value = value,
-                label = stringResource(R.string.user_wallet_list_rename_popup_placeholder),
-                onValueChange = { newValue ->
-                    value = newValue
-                },
-            )
-            SpacerH24()
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.End,
-            ) {
-                TextButton(
-                    text = stringResource(id = R.string.common_cancel),
-                    onClick = dialog.onCancel,
-                )
-                SpacerW8()
-                TextButton(
-                    text = stringResource(id = R.string.common_save),
-                    enabled = value.text.isNotEmpty() && value.text != dialog.currentName,
-                    onClick = {
-                        dialog.onApply(value.text)
-                    },
-                )
-            }
-        }
+internal fun RenameWalletDialogContent(dialog: DialogModel.RenameWalletDialog) {
+    var value by remember {
+        mutableStateOf(TextFieldValue(text = dialog.currentName))
     }
+
+    TextInputDialog(
+        fieldValue = value,
+        confirmButton = DialogButton(
+            title = stringResource(id = R.string.common_save),
+            enabled = value.text.isNotEmpty() && value.text != dialog.currentName,
+            onClick = { dialog.onConfirm(value.text) },
+        ),
+        onDismissDialog = dialog.onDismiss,
+        onValueChange = { newValue ->
+            value = newValue
+        },
+        title = stringResource(R.string.user_wallet_list_rename_popup_title),
+        dismissButton = DialogButton(
+            title = stringResource(id = R.string.common_cancel),
+            onClick = dialog.onDismiss,
+        ),
+        textFieldParams = AdditionalTextInputDialogParams(
+            label = stringResource(R.string.user_wallet_list_rename_popup_placeholder),
+        ),
+    )
 }
 
 // region Preview
@@ -83,10 +51,9 @@ private fun RenameWalletDialogContentSample(
     modifier: Modifier = Modifier,
 ) {
     Column(
-        modifier = modifier
-            .background(TangemTheme.colors.background.primary),
+        modifier = modifier,
     ) {
-        RenameWalletDialogContent(dialog = RenameWalletDialog("", {}, {}))
+        RenameWalletDialogContent(dialog = DialogModel.RenameWalletDialog("", {}, {}))
     }
 }
 
