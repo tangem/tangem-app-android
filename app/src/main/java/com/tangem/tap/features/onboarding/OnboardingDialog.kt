de499d5b987e0aa21c421ad44d2fab579f5d4ec7
Tangem <git@tangem.com>
2023-02-01 10:32:03 +0300

    Updated on 2026-09-01

---
+++ app/src/main/java/com/tangem/tap/features/onboarding/OnboardingDialog.kt
@@ -0,0 +1,12 @@
+package com.tangem.tap.features.onboarding
+
+import com.tangem.common.extensions.VoidCallback
+import com.tangem.tap.common.redux.StateDialog
+
+/**
+[REDACTED_AUTHOR]
+ */
+sealed class OnboardingDialog : StateDialog {
+    object TwinningProcessNotCompleted : OnboardingDialog()
+    data class InterruptOnboarding(val onOk: VoidCallback) : OnboardingDialog()
+}
