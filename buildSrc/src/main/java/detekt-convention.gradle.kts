0c88336ff713ebc71855d4a5242ce5c3565517de
Tangem <git@tangem.com>
2023-02-07 17:32:08 +0800

    Updated on 2026-09-01

--- buildSrc/src/main/java/detekt-convention.gradle.kts
+++ buildSrc/src/main/java/detekt-convention.gradle.kts
@@ -4,19 +4,23 @@ plugins {
     id("io.gitlab.arturbosch.detekt")
 }
 
-tasks.withType<Detekt> {
+detekt {
     parallel = true
     ignoreFailures = false
     autoCorrect = true
     buildUponDefaultConfig = true
-
     config.setFrom(rootProject.files("config/detekt/detekt.yml"))
+}
+
+tasks.withType<Detekt> {
     include("**/*.kt")
     exclude("**/resources/**", "**/build/**")
     reports {
+        sarif {
+            required.set(false)
+        }
         txt {
             required.set(true)
-            outputLocation.set(rootProject.file("build/reports/detekt.txt"))
         }
     }
 
