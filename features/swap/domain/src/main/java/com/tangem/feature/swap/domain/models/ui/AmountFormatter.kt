e626b41f3da875ac21d9206060f15326794a6a4f
Tangem <git@tangem.com>
2023-02-07 14:59:17 +0300

    Updated on 2026-06-27

--- features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/ui/AmountFormatter.kt
+++ features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/ui/AmountFormatter.kt
@@ -1,7 +1,6 @@
 package com.tangem.feature.swap.domain.models.ui
 
 import com.tangem.feature.swap.domain.models.SwapAmount
-import com.tangem.lib.crypto.models.ProxyAmount
 import com.tangem.utils.toFormattedCurrencyString
 import java.math.BigDecimal
 
@@ -16,17 +15,6 @@ class AmountFormatter {
      */
     fun formatSwapAmountToUI(swapAmount: SwapAmount, currency: String): String {
         return swapAmount.value.toFormattedCurrencyString(swapAmount.decimals, currency)
-    }
-
-    /**
-     * Use to convert crypto amount [ProxyAmount] to UI representation
-     *
-     * @param proxyAmount [ProxyAmount]
-     * @param currency currency symbol
-     * @return formatted [String]
-     */
-    fun formatProxyAmountToUI(proxyAmount: ProxyAmount, currency: String): String {
-        return proxyAmount.value.toFormattedCurrencyString(proxyAmount.decimals, currency)
     }
 
     /**
