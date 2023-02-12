124004f983faa14e184850f9053519718195deed
Tangem <git@tangem.com>
2023-02-12 18:25:51 +0300

    Updated on 2026-03-27

--- app/src/main/java/com/tangem/tap/features/wallet/ui/BalanceWidget.kt
+++ app/src/main/java/com/tangem/tap/features/wallet/ui/BalanceWidget.kt
@@ -119,7 +119,8 @@ class BalanceWidget(
                 tvErrorDescriptions.text =
                     fragment.getString(
                         R.string.no_account_generic,
-                        data.amountToCreateAccount, data.currencySymbol,
+                        data.amountToCreateAccount,
+                        data.currencySymbol,
                     )
             }
             BalanceStatus.UnknownBlockchain -> with(binding.lBalanceError) {
