7a9c4e1f7124f5cc9dc49575625c913c822dc57b
Tangem <git@tangem.com>
2023-02-13 16:31:30 +0300

    Updated on 2026-09-23

--- features/swap/data/src/main/java/com/tangem/feature/swap/converters/SwapConverter.kt
+++ features/swap/data/src/main/java/com/tangem/feature/swap/converters/SwapConverter.kt
@@ -2,13 +2,12 @@ package com.tangem.feature.swap.converters
 
 import com.tangem.datasource.api.oneinch.models.SwapResponse
 import com.tangem.datasource.api.oneinch.models.TransactionDto
+import com.tangem.feature.swap.domain.models.createFromAmountWithOffset
 import com.tangem.feature.swap.domain.models.domain.SwapDataModel
 import com.tangem.feature.swap.domain.models.domain.TransactionModel
-import com.tangem.feature.swap.domain.models.createFromAmountWithOffset
 import com.tangem.utils.converter.Converter
-import javax.inject.Inject
 
-class SwapConverter @Inject constructor() : Converter<SwapResponse, SwapDataModel> {
+class SwapConverter : Converter<SwapResponse, SwapDataModel> {
 
     override fun convert(value: SwapResponse): SwapDataModel {
         return SwapDataModel(
