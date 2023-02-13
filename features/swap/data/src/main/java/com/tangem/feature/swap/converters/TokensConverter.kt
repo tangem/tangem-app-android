81590112a8739a23451cba5452df3d1dcef493dd
Tangem <git@tangem.com>
2023-02-13 16:31:30 +0300

    Updated on 2026-05-13

--- features/swap/data/src/main/java/com/tangem/feature/swap/converters/TokensConverter.kt
+++ features/swap/data/src/main/java/com/tangem/feature/swap/converters/TokensConverter.kt
@@ -3,9 +3,8 @@ package com.tangem.feature.swap.converters
 import com.tangem.datasource.api.tangemTech.models.CoinsResponse
 import com.tangem.feature.swap.domain.models.domain.Currency
 import com.tangem.utils.converter.Converter
-import javax.inject.Inject
 
-class TokensConverter @Inject constructor() : Converter<CoinsResponse.Coin, Currency> {
+class TokensConverter : Converter<CoinsResponse.Coin, Currency> {
 
     override fun convert(value: CoinsResponse.Coin): Currency {
         val network = value.networks.first()
