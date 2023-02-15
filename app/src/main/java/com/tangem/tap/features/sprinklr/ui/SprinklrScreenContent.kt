b72467389dabc58e33d2ed2dbca89f9757eb1477
Tangem <git@tangem.com>
2023-02-15 21:32:17 +0300

    Updated on 2026-03-27

---
+++ app/src/main/java/com/tangem/tap/features/sprinklr/ui/SprinklrScreenContent.kt
@@ -0,0 +1,25 @@
+package com.tangem.tap.features.sprinklr.ui
+
+import android.annotation.SuppressLint
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.google.accompanist.web.WebView
+import com.google.accompanist.web.rememberWebViewState
+
+@SuppressLint("SetJavaScriptEnabled")
+@Composable
+internal fun SprinklrScreenContent(
+    state: SprinklrScreenState,
+    modifier: Modifier = Modifier,
+) {
+    WebView(
+        modifier = modifier,
+        state = rememberWebViewState(url = state.initialUrl),
+        onCreated = { webView ->
+            with(webView.settings) {
+                javaScriptEnabled = true
+                domStorageEnabled = true
+            }
+        },
+    )
+}
