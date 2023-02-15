cfacac20c728614abbf7901ae880f23ad0e84b28
Tangem <git@tangem.com>
2023-02-15 21:32:17 +0300

    Updated on 2026-06-05

--- app/src/main/java/com/tangem/tap/common/chat/ChatManager.kt
+++ app/src/main/java/com/tangem/tap/common/chat/ChatManager.kt
@@ -5,18 +5,21 @@ import com.tangem.tap.ForegroundActivityObserver
 import com.tangem.tap.common.chat.opener.ChatOpener
 import com.tangem.tap.common.chat.opener.implementation.SprinklrChatOpener
 import com.tangem.tap.common.chat.opener.implementation.ZendeskChatOpener
+import com.tangem.tap.common.redux.AppState
 import com.tangem.tap.persistence.PreferencesStorage
+import org.rekotlin.Store
 
 class ChatManager(
     private val preferencesStorage: PreferencesStorage,
     private val foregroundActivityObserver: ForegroundActivityObserver,
+    private val store: Store<AppState>,
 ) {
     private val openers = mutableMapOf<ChatConfig, ChatOpener>()
 
     fun open(config: ChatConfig, feedbackDataBuilder: (Context) -> String) {
         val opener = openers.getOrPut(config) {
             when (config) {
-                is SprinklrConfig -> SprinklrChatOpener()
+                is SprinklrConfig -> SprinklrChatOpener(config, store)
                 is ZendeskConfig -> ZendeskChatOpener(config, preferencesStorage, foregroundActivityObserver)
             }
         }
