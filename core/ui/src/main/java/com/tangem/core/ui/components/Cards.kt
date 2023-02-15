51a17999808e31700e85ea9f1742f2e0c2f220c0
Tangem <git@tangem.com>
2023-02-15 12:23:50 +0300

    Updated on 2026-06-01

--- core/ui/src/main/java/com/tangem/core/ui/components/Cards.kt
+++ core/ui/src/main/java/com/tangem/core/ui/components/Cards.kt
@@ -2,6 +2,7 @@ package com.tangem.core.ui.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -20,10 +21,13 @@ import androidx.compose.material.Icon
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import com.tangem.core.ui.R
 import com.tangem.core.ui.res.TangemTheme
 import com.valentinilk.shimmer.shimmer
@@ -53,6 +57,39 @@ fun SmallInfoCard(
             startText = startText,
             endText = endText,
             isLoading = isLoading,
+            disclaimer = null,
+        )
+    }
+}
+
+/**
+ * Small card with text information attached to the edges
+ *
+ * @param startText text information attached to the left edge
+ * @param endText   text information attached to the right edge
+ * @param disclaimer closable description text
+ * @param isLoading  if true, shimmer is shown instead of [startText] and [endText]
+ *
+ * @see <a href = "https://www.figma.com/file/14ISV23YB1yVW1uNVwqrKv/Android?node-id=1007%3A709&t=X19ZWlYP0rnmdcDO-4"
+ * >Figma component</a>
+ */
+@Composable
+fun SmallInfoCardWithDisclaimer(
+    startText: String,
+    endText: String,
+    disclaimer: String,
+    isLoading: Boolean = false,
+) {
+    Surface(
+        shape = RoundedCornerShape(TangemTheme.dimens.radius12),
+        color = TangemTheme.colors.background.primary,
+        elevation = TangemTheme.dimens.elevation2,
+    ) {
+        CardInfoBox(
+            startText = startText,
+            endText = endText,
+            isLoading = isLoading,
+            disclaimer = disclaimer,
         )
     }
 }
@@ -63,6 +100,7 @@ fun SmallInfoCard(
  * @param startText text information attached to the left edge
  * @param endText   text information attached to the right edge
  * @param warningText text of the warning
+ * @param disclaimer closable description text
  *
  * @see <a href = "https://www.figma.com/file/14ISV23YB1yVW1uNVwqrKv/Android?node-id=515%3A1541&t=qYGQJdtf1eeUPdkR-1"
  * >Figma component</a>
@@ -72,6 +110,7 @@ fun SmallInfoCardWithWarning(
     startText: String,
     endText: String,
     warningText: String,
+    disclaimer: String? = null,
 ) {
     Surface(
         shape = RoundedCornerShape(TangemTheme.dimens.size12),
@@ -83,7 +122,7 @@ fun SmallInfoCardWithWarning(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            CardInfoBox(startText = startText, endText = endText)
+            CardInfoBox(startText = startText, endText = endText, disclaimer = disclaimer)
 
             Divider(
                 color = TangemTheme.colors.stroke.primary,
@@ -238,44 +277,117 @@ fun IconWithTitleAndDescription(
 private fun CardInfoBox(
     startText: String,
     endText: String,
+    disclaimer: String?,
     isLoading: Boolean = false,
 ) {
+    val isOpened = remember { mutableStateOf(false) }
+    Column {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .heightIn(TangemTheme.dimens.size48)
+                .clickable { isOpened.value = !isOpened.value }
+                .padding(
+                    horizontal = TangemTheme.dimens.spacing16,
+                    vertical = TangemTheme.dimens.spacing12,
+                ),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                text = startText,
+                color = TangemTheme.colors.text.tertiary,
+                maxLines = 1,
+                style = TangemTheme.typography.subtitle2,
+            )
+
+            if (isLoading) {
+                ShimmerItem(width = TangemTheme.dimens.size40, height = TangemTheme.dimens.size12)
+            } else {
+                Row {
+                    Text(
+                        text = endText,
+                        color = TangemTheme.colors.text.primary1,
+                        maxLines = 1,
+                        style = TangemTheme.typography.body2,
+                    )
+                    if (disclaimer != null) {
+                        val chevronIcon = if (isOpened.value) {
+                            painterResource(id = R.drawable.ic_chevron_up_24)
+                        } else {
+                            painterResource(id = R.drawable.ic_chevron_24)
+                        }
+                        Icon(
+                            modifier = Modifier.size(TangemTheme.dimens.size20),
+                            painter = chevronIcon,
+                            tint = TangemTheme.colors.icon.primary1,
+                            contentDescription = null,
+                        )
+                    }
+                }
+            }
+        }
+        if (isOpened.value && disclaimer != null) {
+            DisclaimerItem(disclaimer)
+        }
+    }
+}
+
+@Composable
+private fun ShimmerItem(width: Dp, height: Dp) {
+    Box(
+        modifier = Modifier
+            .size(width = width, height = height)
+            .shimmer()
+            .background(
+                color = TangemTheme.colors.button.secondary,
+                shape = RoundedCornerShape(TangemTheme.dimens.radius3),
+            ),
+    )
+}
+
+@Composable
+private fun DisclaimerItem(disclaimer: String) {
+    Divider(
+        color = TangemTheme.colors.stroke.primary,
+        thickness = TangemTheme.dimens.size0_5,
+        modifier = Modifier.padding(horizontal = TangemTheme.dimens.spacing12),
+    )
+
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .heightIn(TangemTheme.dimens.size48)
             .padding(
-                horizontal = TangemTheme.dimens.spacing16,
-                vertical = TangemTheme.dimens.spacing12,
+                top = TangemTheme.dimens.spacing14,
+                bottom = TangemTheme.dimens.spacing16,
+                start = TangemTheme.dimens.spacing16,
+                end = TangemTheme.dimens.spacing16,
             ),
-        horizontalArrangement = Arrangement.SpaceBetween,
+        horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        Text(
-            text = startText,
-            color = TangemTheme.colors.text.tertiary,
-            maxLines = 1,
-            style = TangemTheme.typography.subtitle2,
-        )
-
-        if (isLoading) {
-            Box(
-                modifier = Modifier
-                    .size(width = TangemTheme.dimens.size40, height = TangemTheme.dimens.size12)
-                    .shimmer()
-                    .background(
-                        color = TangemTheme.colors.button.secondary,
-                        shape = RoundedCornerShape(TangemTheme.dimens.radius3),
-                    ),
-            )
-        } else {
-            Text(
-                text = endText,
-                color = TangemTheme.colors.text.primary1,
-                maxLines = 1,
-                style = TangemTheme.typography.body2,
+        Box(
+            modifier = Modifier
+                .background(
+                    color = TangemTheme.colors.background.secondary,
+                    shape = CircleShape,
+                )
+                .height(TangemTheme.dimens.size40)
+                .width(TangemTheme.dimens.size40),
+            contentAlignment = Alignment.Center,
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_heart_24),
+                tint = TangemTheme.colors.icon.secondary,
+                contentDescription = null,
             )
         }
+        SpacerW12()
+        Text(
+            text = disclaimer,
+            color = TangemTheme.colors.text.tertiary,
+            style = TangemTheme.typography.body2,
+        )
     }
 }
 
@@ -287,7 +399,19 @@ private fun CardInfoBox(
 @Composable
 private fun CardsPreview() {
     Column(modifier = Modifier.fillMaxWidth()) {
-        SmallInfoCard(startText = "Balance", endText = "0.4405434 BTC")
+        SmallInfoCard(
+            startText = "Balance",
+            endText = "0.4405434 BTC",
+        )
+
+        SpacerH32()
+
+        SmallInfoCardWithDisclaimer(
+            startText = "Balance",
+            endText = "0.4405434 BTC",
+            disclaimer = "Not enough funds for fee on your Polygon wallet  to create a transaction." +
+                "Top up your Polygon wallet first.",
+        )
 
         SpacerH32()
 
@@ -295,6 +419,8 @@ private fun CardsPreview() {
             startText = "Balance",
             endText = "0.4405434 BTC",
             warningText = "Not enough funds for fee on your Polygon wallet  to create a transaction. " +
+                "Top up your Polygon wallet first.",
+            disclaimer = "Not enough funds for fee on your Polygon wallet  to create a transaction." +
                 "Top up your Polygon wallet first.",
         )
 
@@ -335,6 +461,7 @@ private fun Preview_InfoCardWithWarning_InDarkTheme() {
 private fun Preview_SimpleInfoCard_InLightTheme() {
     TangemTheme(isDark = false) {
         SmallInfoCard(startText = "Balance", endText = "0.4405434 BTC")
+        SmallInfoCardWithDisclaimer(startText = "Balance", endText = "0.4405434 BTC", disclaimer = "test")
     }
 }
 
@@ -343,6 +470,7 @@ private fun Preview_SimpleInfoCard_InLightTheme() {
 private fun Preview_SimpleInfoCard_InDarkTheme() {
     TangemTheme(isDark = true) {
         SmallInfoCard(startText = "Balance", endText = "0.4405434 BTC")
+        SmallInfoCardWithDisclaimer(startText = "Balance", endText = "0.4405434 BTC", disclaimer = "test")
     }
 }
 // endregion Preview
