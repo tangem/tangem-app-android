0189a7cd987b2fa8aafed3e5575a30843b767226
Tangem <git@tangem.com>
2023-02-17 07:53:30 +0300

    Updated on 2026-05-13

--- app/src/main/java/com/tangem/tap/features/onboarding/products/wallet/saltPay/redux/OnboardingSaltPayState.kt
+++ app/src/main/java/com/tangem/tap/features/onboarding/products/wallet/saltPay/redux/OnboardingSaltPayState.kt
@@ -2,19 +2,10 @@ package com.tangem.tap.features.onboarding.products.wallet.saltPay.redux
 
 import com.tangem.blockchain.common.Amount
 import com.tangem.blockchain.common.Blockchain
-import com.tangem.blockchain.common.WalletManagerFactory
-import com.tangem.common.extensions.guard
 import com.tangem.domain.common.SaltPayWorkaround
-import com.tangem.domain.common.ScanResponse
-import com.tangem.datasource.api.paymentology.PaymentologyApiService
 import com.tangem.tap.common.toggleWidget.WidgetState
-import com.tangem.tap.domain.extensions.makeSaltPayWalletManager
-import com.tangem.tap.features.onboarding.products.wallet.saltPay.GnosisRegistrator
-import com.tangem.tap.features.onboarding.products.wallet.saltPay.KYCProvider
 import com.tangem.tap.features.onboarding.products.wallet.saltPay.SaltPayActivationManager
-import com.tangem.tap.features.onboarding.products.wallet.saltPay.SaltPayConfig
 import com.tangem.tap.features.wallet.redux.ProgressState
-import com.tangem.tap.store
 import java.math.BigDecimal
 
 /**
@@ -23,7 +14,6 @@ import java.math.BigDecimal
 data class OnboardingSaltPayState(
     @Transient
     val saltPayManager: SaltPayActivationManager = SaltPayActivationManager.stub(),
-    val saltPayConfig: SaltPayConfig = SaltPayConfig.stub(),
     val pinCode: String? = null,
     val accessCode: String? = null,
     val amountToClaim: Amount? = null,
@@ -37,57 +27,7 @@ data class OnboardingSaltPayState(
     val mainButtonState: WidgetState
         get() = if (inProgress) ProgressState.Loading else ProgressState.Done
 
-    fun readyToClaim(): Boolean = amountToClaim != null
-
     val pinLength: Int = 4
-
-    companion object {
-        fun initDependency(scanResponse: ScanResponse): Pair<SaltPayActivationManager, SaltPayConfig> {
-            val globalState = store.state.globalState
-            val saltPayConfig = globalState.configManager?.config?.saltPayConfig.guard {
-                throw NullPointerException("SaltPayConfig is not initialized")
-            }
-            val gnosisRegistrator = makeGnosisRegistrator(
-                scanResponse = scanResponse,
-                wmFactory = globalState.tapWalletManager.walletManagerFactory,
-            )
-            val registrationManager = makeSaltPayRegistrationManager(
-                scanResponse = scanResponse,
-                gnosisRegistrator = gnosisRegistrator,
-                paymentologyService = store.state.domainNetworks.paymentologyService,
-                kycProvider = saltPayConfig.kycProvider,
-            )
-            return registrationManager to saltPayConfig
-        }
-
-        fun makeSaltPayRegistrationManager(
-            scanResponse: ScanResponse,
-            gnosisRegistrator: GnosisRegistrator,
-            paymentologyService: PaymentologyApiService,
-            kycProvider: KYCProvider,
-        ): SaltPayActivationManager {
-            if (!scanResponse.isSaltPay()) {
-                throw IllegalArgumentException("Can't initialize the OnboardingSaltPayMiddleware if card is not SalPay")
-            }
-
-            return SaltPayActivationManager(
-                cardId = scanResponse.card.cardId,
-                cardPublicKey = scanResponse.card.cardPublicKey,
-                kycProvider = kycProvider,
-                paymentologyService = paymentologyService,
-                gnosisRegistrator = gnosisRegistrator,
-            )
-        }
-
-        fun makeGnosisRegistrator(
-            scanResponse: ScanResponse,
-            wmFactory: WalletManagerFactory,
-        ): GnosisRegistrator {
-            return GnosisRegistrator(
-                walletManager = wmFactory.makeSaltPayWalletManager(scanResponse),
-            )
-        }
-    }
 }
 
 enum class SaltPayActivationStep {
