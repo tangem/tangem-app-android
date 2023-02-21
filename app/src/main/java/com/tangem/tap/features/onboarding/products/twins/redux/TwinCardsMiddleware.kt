dc546700b4ff1b07052db8dd24f5b01ddcc1c24d
Tangem <git@tangem.com>
2023-02-21 18:15:57 +0300

    Updated on 2026-08-27

--- app/src/main/java/com/tangem/tap/features/onboarding/products/twins/redux/TwinCardsMiddleware.kt
+++ app/src/main/java/com/tangem/tap/features/onboarding/products/twins/redux/TwinCardsMiddleware.kt
@@ -322,13 +322,26 @@ private fun handle(action: Action, dispatch: DispatchFunction) {
 
                     OnboardingHelper.onInterrupted()
                     store.dispatch(TwinCardsAction.CardsManager.Release)
+
                     action.shouldResetTwinCardsWidget(shouldReturnCardBack) {
-                        store.dispatch(NavigationAction.PopBackTo(AppScreen.Home))
+                        store.dispatchOnMain(NavigationAction.PopBackTo(getPopBackScreen()))
                     }
                 }
                 store.dispatchDialogShow(OnboardingDialog.InterruptOnboarding(onOkCallback))
             }
         }
         else -> Unit
+    }
+}
+
+private fun getPopBackScreen(): AppScreen {
+    return if (userWalletsListManager.hasSavedUserWallets) {
+        if (userWalletsListManager.isLockedSync) {
+            AppScreen.Welcome
+        } else {
+            AppScreen.Wallet
+        }
+    } else {
+        AppScreen.Home
     }
 }
