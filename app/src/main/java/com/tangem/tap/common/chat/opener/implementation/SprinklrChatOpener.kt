3580c213bd4b35c776829ddc1619118076bbdb6c
Tangem <git@tangem.com>
2023-02-21 11:35:14 +0300

    release/5.26.1

--- app/src/main/java/com/tangem/tap/common/chat/opener/implementation/SprinklrChatOpener.kt
+++ app/src/main/java/com/tangem/tap/common/chat/opener/implementation/SprinklrChatOpener.kt
@@ -1,22 +1,27 @@
 package com.tangem.tap.common.chat.opener.implementation
 
 import android.content.Context
+import android.content.Intent
+import com.tangem.tap.ForegroundActivityObserver
 import com.tangem.tap.common.chat.SprinklrConfig
 import com.tangem.tap.common.chat.opener.ChatOpener
-import com.tangem.tap.common.extensions.dispatchOnMain
 import com.tangem.tap.common.redux.AppState
-import com.tangem.tap.common.redux.navigation.AppScreen
-import com.tangem.tap.common.redux.navigation.NavigationAction
 import com.tangem.tap.features.sprinklr.redux.SprinklrAction
+import com.tangem.tap.features.sprinklr.ui.SprinklrActivity
+import com.tangem.tap.withForegroundActivity
 import org.rekotlin.Store
 
 internal class SprinklrChatOpener(
     private val userId: String,
     private val config: SprinklrConfig,
     private val store: Store<AppState>,
+    private val foregroundActivityObserver: ForegroundActivityObserver,
 ) : ChatOpener {
     override fun open(feedbackDataBuilder: (Context) -> String) {
         store.dispatch(SprinklrAction.Init(userId, config))
-        store.dispatchOnMain(NavigationAction.NavigateTo(AppScreen.Sprinklr))
+        foregroundActivityObserver.withForegroundActivity { activity ->
+            val intent = Intent(activity, SprinklrActivity::class.java)
+            activity.startActivity(intent)
+        }
     }
 }
