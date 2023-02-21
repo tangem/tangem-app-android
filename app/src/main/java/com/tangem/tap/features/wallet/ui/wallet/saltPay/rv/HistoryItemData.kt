595b0c9a52590eb6806bb7aac3e28ebe567990bc
Tangem <git@tangem.com>
2023-02-21 13:42:17 +0300

    Updated on 2026-09-14

--- app/src/main/java/com/tangem/tap/features/wallet/ui/wallet/saltPay/rv/HistoryItemData.kt
+++ app/src/main/java/com/tangem/tap/features/wallet/ui/wallet/saltPay/rv/HistoryItemData.kt
@@ -16,7 +16,7 @@ sealed class HistoryItemData(
     val itemId: Long,
 ) {
     data class Date(val date: String) : HistoryItemData(0, date.hashCode().toLong())
-    data class TransactionData(val data: HistoryTransactionData) : HistoryItemData(1, data.hash.hashCode().toLong())
+    data class TransactionData(val data: HistoryTransactionData) : HistoryItemData(1, data.address.hashCode().toLong())
 }
 
 data class HistoryTransactionData(
@@ -32,9 +32,11 @@ data class HistoryTransactionData(
         else -> PendingTransactionType.Unknown
     }
 
-    val hash: String = transactionData.hash?.let {
-        "${it.substring(0..5)}...${it.substring(it.length - 4)}"
-    } ?: ""
+    val address: String = when (transactionType) {
+        PendingTransactionType.Incoming -> transactionData.sourceAddress.reduceAddress()
+        PendingTransactionType.Outgoing -> transactionData.destinationAddress.reduceAddress()
+        PendingTransactionType.Unknown -> "Unknown address"
+    }
 
     val time: String = transactionData.date?.let {
         SimpleDateFormat("HH:mm").format(it.time)
@@ -71,4 +73,6 @@ data class HistoryTransactionData(
 
         return date
     }
+
+    private fun String.reduceAddress(): String = "${substring(0..5)}...${substring(length - 4)}"
 }
