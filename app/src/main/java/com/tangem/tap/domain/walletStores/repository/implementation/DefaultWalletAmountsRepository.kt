d3c6b29d4e7e24f2e7533789f49de7aa8f787326
Tangem <git@tangem.com>
2023-02-22 16:49:05 +0300

    Updated on 2025-12-18

--- app/src/main/java/com/tangem/tap/domain/walletStores/repository/implementation/DefaultWalletAmountsRepository.kt
+++ app/src/main/java/com/tangem/tap/domain/walletStores/repository/implementation/DefaultWalletAmountsRepository.kt
@@ -442,7 +442,7 @@ internal class DefaultWalletAmountsRepository(
                 .toMutableList()
                 .apply {
                     replaceByOrAdd(walletManager) {
-                        it.wallet.blockchain == it.wallet.blockchain
+                        it.wallet.blockchain == walletManager.wallet.blockchain
                     }
                 }
 
