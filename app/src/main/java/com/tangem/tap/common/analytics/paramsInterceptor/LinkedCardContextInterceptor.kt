abb5125c77da868c2b0c0458e7ee95216b9dfe72
Tangem <git@tangem.com>
2023-02-22 17:18:44 +0300

    Updated on 2025-11-26

---
+++ app/src/main/java/com/tangem/tap/common/analytics/paramsInterceptor/LinkedCardContextInterceptor.kt
@@ -0,0 +1,26 @@
+package com.tangem.tap.common.analytics.paramsInterceptor
+
+import com.tangem.core.analytics.AnalyticsEvent
+import com.tangem.core.analytics.api.ParamsInterceptor
+import com.tangem.domain.common.ScanResponse
+
+/**
+[REDACTED_AUTHOR]
+ */
+class LinkedCardContextInterceptor(
+    private val scanResponse: ScanResponse,
+    val parent: LinkedCardContextInterceptor? = null,
+) : ParamsInterceptor {
+
+    private val contextInterceptor = CardContextInterceptor(scanResponse)
+
+    override fun id(): String = LinkedCardContextInterceptor.id()
+
+    override fun canBeAppliedTo(event: AnalyticsEvent): Boolean = contextInterceptor.canBeAppliedTo(event)
+
+    override fun intercept(params: MutableMap<String, String>) = contextInterceptor.intercept(params)
+
+    companion object {
+        fun id(): String = LinkedCardContextInterceptor::class.java.simpleName
+    }
+}
