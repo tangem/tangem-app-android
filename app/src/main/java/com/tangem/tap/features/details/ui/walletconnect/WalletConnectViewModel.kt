4dd6fbc424ce81017b81e88f8466b2bc5b0a69fc
Tangem <git@tangem.com>
2023-03-02 00:22:28 +0400

    Updated on 2026-09-25

--- app/src/main/java/com/tangem/tap/features/details/ui/walletconnect/WalletConnectViewModel.kt
+++ app/src/main/java/com/tangem/tap/features/details/ui/walletconnect/WalletConnectViewModel.kt
@@ -4,23 +4,35 @@ import com.tangem.tap.common.redux.AppState
 import com.tangem.tap.features.details.redux.walletconnect.WalletConnectAction
 import com.tangem.tap.features.details.redux.walletconnect.WalletConnectSession
 import com.tangem.tap.features.details.redux.walletconnect.WalletConnectState
+import kotlinx.collections.immutable.toImmutableList
 import org.rekotlin.Store
+import timber.log.Timber
 
 class WalletConnectViewModel(private val store: Store<AppState>) {
     fun updateState(state: WalletConnectState): WalletConnectScreenState {
+        Timber.d("WC2 Sessions: ${state.wc2Sessions}")
+        val sessions = state.sessions.map { wcSession -> WcSessionForScreen.fromSession(wcSession) } + state.wc2Sessions
         return WalletConnectScreenState(
-            state.sessions.map { wcSession -> WcSessionForScreen.fromSession(wcSession) },
+            sessions.toImmutableList(),
             isLoading = state.loading,
-            onRemoveSession = { sessionUri -> onRemoveSession(sessionUri, state.sessions) },
+            onRemoveSession = { sessionUri -> onRemoveSession(sessionUri, state.sessions, state.wc2Sessions) },
             onAddSession = { copiedUri -> store.dispatch(WalletConnectAction.StartWalletConnect(copiedUri)) },
         )
     }
 
-    private fun onRemoveSession(sessionUri: String, sessions: List<WalletConnectSession>) {
+    private fun onRemoveSession(
+        sessionUri: String,
+        sessions: List<WalletConnectSession>,
+        wc2sessions: List<WcSessionForScreen>,
+    ) {
         sessions
             .firstOrNull { it.session.toUri() == sessionUri }
             ?.let { baseSession ->
-                store.dispatch(WalletConnectAction.DisconnectSession(baseSession.session))
+                store.dispatch(WalletConnectAction.DisconnectSession(baseSession.session.topic, baseSession.session))
+                return
             }
+        wc2sessions.firstOrNull { it.sessionId == sessionUri }?.let {
+            store.dispatch(WalletConnectAction.DisconnectSession(sessionUri, null))
+        }
     }
 }
