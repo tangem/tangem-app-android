dc3a3305c4cc01aab138d520774bb1f392da0c51
Tangem <git@tangem.com>
2023-03-02 00:22:28 +0400

    Updated on 2026-04-06

--- core/datasource/src/main/java/com/tangem/datasource/config/models/Config.kt
+++ core/datasource/src/main/java/com/tangem/datasource/config/models/Config.kt
@@ -18,4 +18,5 @@ data class Config(
     val shopify: ShopifyShop? = null,
     val zendesk: ZendeskConfig? = null,
     val swapReferrerAccount: SwapReferrerAccount? = null,
+    val walletConnectProjectId: String = "",
 )
