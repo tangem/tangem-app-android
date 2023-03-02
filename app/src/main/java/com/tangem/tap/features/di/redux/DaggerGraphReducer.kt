106b278ca91ae01b0a330da15fb2ccdc50f635c1
Tangem <git@tangem.com>
2023-03-02 23:27:34 +0800

    Updated on 2026-09-21

---
+++ app/src/main/java/com/tangem/tap/features/di/redux/DaggerGraphReducer.kt
@@ -0,0 +1,20 @@
+package com.tangem.tap.features.di.redux
+
+import com.tangem.tap.common.redux.AppState
+import org.rekotlin.Action
+
+object DaggerGraphReducer {
+    fun reduce(action: Action, state: AppState): DaggerGraphState {
+        if (action !is DaggerGraphAction) return state.daggerGraphState
+
+        return internalReduce(action, state)
+    }
+
+    private fun internalReduce(action: DaggerGraphAction, state: AppState): DaggerGraphState {
+        return when (action) {
+            is DaggerGraphAction.SetDependencies -> state.daggerGraphState.copy(
+                assetReader = action.assetReader,
+            )
+        }
+    }
+}
