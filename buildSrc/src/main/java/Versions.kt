1b13611480796f04794fb2911d02ebed7201f3e9
Tangem <git@tangem.com>
2023-03-02 15:32:41 +0300

    Updated on 2026-05-04

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -61,10 +61,10 @@ object Versions {
     // endregion Other libraries
 
     // region Tangem
-    const val tangemBlockchainSdk = "develop-172"
+    const val tangemBlockchainSdk = "develop-173"
     // const val tangemBlockchainSdk = "0.0.1" // Keep it! - used for local builds
 
-    const val tangemCardSdk = "develop-191"
+    const val tangemCardSdk = "develop-198"
     // const val tangemCardSdk = "0.0.1" // Keep it! - used for local builds
     // endregion Tangem
 
