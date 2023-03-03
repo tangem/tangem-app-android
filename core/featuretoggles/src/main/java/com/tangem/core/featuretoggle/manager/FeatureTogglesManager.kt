bf802cd57a0e3072d9ee817f2dab102fd415cc63
Tangem <git@tangem.com>
2023-03-03 17:18:36 +0800

    Updated on 2026-07-23

---
+++ core/featuretoggles/src/main/java/com/tangem/core/featuretoggle/manager/FeatureTogglesManager.kt
@@ -0,0 +1,17 @@
+package com.tangem.core.featuretoggle.manager
+
+import com.tangem.core.featuretoggle.FeatureToggle
+
+/**
+ * Component for getting information about the availability of feature toggles
+ *
+[REDACTED_AUTHOR]
+ */
+interface FeatureTogglesManager {
+
+    /** Initialize manager */
+    suspend fun init()
+
+    /** Check feature toggle [toggle] availability */
+    fun isFeatureEnabled(toggle: FeatureToggle): Boolean
+}
