995badb5a441e854207a09f202a702ca3301406e
Tangem <git@tangem.com>
2023-03-06 18:10:51 +0300

    Updated on 2026-05-19

--- features/swap/presentation/src/main/java/com/tangem/feature/swap/viewmodels/SwapProcessDataState.kt
+++ features/swap/presentation/src/main/java/com/tangem/feature/swap/viewmodels/SwapProcessDataState.kt
@@ -1,15 +1,14 @@
 package com.tangem.feature.swap.viewmodels
 
-import com.tangem.feature.swap.domain.models.domain.ApproveModel
 import com.tangem.feature.swap.domain.models.domain.Currency
-import com.tangem.feature.swap.domain.models.domain.SwapDataModel
+import com.tangem.feature.swap.domain.models.ui.RequestApproveStateData
+import com.tangem.feature.swap.domain.models.ui.SwapStateData
 
 data class SwapProcessDataState(
     val networkId: String,
     val fromCurrency: Currency? = null,
     val toCurrency: Currency? = null,
     val amount: String? = null,
-    val estimatedGas: Int? = null,
-    val approveModel: ApproveModel? = null,
-    val swapModel: SwapDataModel? = null,
+    val approveDataModel: RequestApproveStateData? = null,
+    val swapDataModel: SwapStateData? = null,
 )
