2e8d40642feeb7a2482dec14a03edf8063175346
Tangem <git@tangem.com>
2023-03-07 17:25:38 +0300

    Updated on 2026-02-25

--- core/featuretoggles/build.gradle.kts
+++ core/featuretoggles/build.gradle.kts
@@ -3,52 +3,7 @@ plugins {
     kotlin("android")
     kotlin("kapt")
     id("com.google.dagger.hilt.android")
-}
-
-android {
-    defaultConfig {
-        compileSdk = AppConfig.compileSdkVersion
-        minSdk = AppConfig.minSdkVersion
-        targetSdk = AppConfig.targetSdkVersion
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    buildTypes {
-        release {
-            BuildConfigFieldFactory(
-                fields = listOf(Field.TesterMenuAvailability(false)),
-                builder = ::buildConfigField,
-            ).create()
-        }
-
-        debug {
-            BuildConfigFieldFactory(
-                fields = listOf(Field.TesterMenuAvailability(true)),
-                builder = ::buildConfigField,
-            ).create()
-        }
-
-        create("debug_beta") {
-            initWith(getByName("release"))
-            BuildConfigFieldFactory(
-                fields = listOf(
-                    Field.Environment("release"),
-                    Field.TestActionEnabled(true),
-                    Field.LogEnabled(true),
-                    Field.TesterMenuAvailability(true),
-                ),
-                builder = ::buildConfigField,
-            ).create()
-        }
-    }
+    id("configuration")
 }
 
 dependencies {
