f02758963e904184891e77e2e0f108db39b4eab9
Tangem <git@tangem.com>
2023-03-07 17:09:56 +0300

    Updated on 2025-10-13

---
+++ plugins/configuration/src/main/kotlin/com/tangem/plugin/configuration/configurations/extension/BaseExtensionConfigurations.kt
@@ -0,0 +1,33 @@
+package com.tangem.plugin.configuration.configurations.extension
+
+import com.android.build.gradle.BaseExtension
+import com.tangem.plugin.configuration.model.AppConfig
+import com.tangem.plugin.configuration.utils.findVersion
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+
+internal fun BaseExtension.configureCompileSdk() {
+    compileSdkVersion(AppConfig.compileSdkVersion)
+}
+
+internal fun BaseExtension.configureCompilerOptions() {
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+internal fun BaseExtension.configureCompose(project: Project) {
+    val useCompose = with(project.path) {
+        contains(":ui") ||
+            contains(":presentation") ||
+            contains(":app") || // TODO: [REDACTED_JIRA]
+            contains(":tester:impl") // TODO: Rename module
+    }
+    buildFeatures.compose = useCompose
+    if (useCompose) {
+        composeOptions {
+            kotlinCompilerExtensionVersion = project.findVersion(alias = "compose-compiler").requiredVersion
+        }
+    }
+}
