979c2ae4f45b0adddfbf8b47cb56cacdf9c5ef6a
Tangem <git@tangem.com>
2023-03-09 16:09:51 +0800

    Updated on 2026-06-10

---
+++ features/tester/api/src/main/java/com/tangem/features/tester/api/TesterRouter.kt
@@ -0,0 +1,12 @@
+package com.tangem.features.tester.api
+
+/**
+ * Outer tester feature router
+ *
+[REDACTED_AUTHOR]
+ */
+interface TesterRouter {
+
+    /** Open tester menu */
+    fun startTesterScreen()
+}
