943840ef5df7f80e6d8a42d9169ffe81a2de151b
Tangem <git@tangem.com>
2023-03-10 18:28:31 +0800

    Updated on 2026-02-11

--- app/src/main/java/com/tangem/tap/proxy/redux/DaggerGraphAction.kt
+++ app/src/main/java/com/tangem/tap/proxy/redux/DaggerGraphAction.kt
@@ -1,12 +1,16 @@
 package com.tangem.tap.proxy.redux
 
 import com.tangem.datasource.asset.AssetReader
+import com.tangem.datasource.connection.NetworkConnectionManager
 import com.tangem.features.tester.api.TesterRouter
 import org.rekotlin.Action
 
 sealed interface DaggerGraphAction : Action {
 
-    data class SetApplicationDependencies(val assetReader: AssetReader) : DaggerGraphAction
+    data class SetApplicationDependencies(
+        val assetReader: AssetReader,
+        val networkConnectionManager: NetworkConnectionManager,
+    ) : DaggerGraphAction
 
     data class SetActivityDependencies(val testerRouter: TesterRouter) : DaggerGraphAction
 }
