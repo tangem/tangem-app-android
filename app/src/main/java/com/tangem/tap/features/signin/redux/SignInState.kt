29325d957d945c6b7203c11e28f40517877f8618
Tangem <git@tangem.com>
2023-03-21 23:07:50 +0800

    Updated on 2026-02-02

---
+++ app/src/main/java/com/tangem/tap/features/signin/redux/SignInState.kt
@@ -0,0 +1,8 @@
+package com.tangem.tap.features.signin.redux
+
+import com.tangem.tap.common.analytics.events.Basic
+
+/**
+[REDACTED_AUTHOR]
+ */
+data class SignInState(val type: Basic.SignedIn.SignInType? = null)
