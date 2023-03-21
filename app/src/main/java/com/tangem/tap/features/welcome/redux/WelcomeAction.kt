e1fbc8d6f221ac5128dee68a59448753e9731516
Tangem <git@tangem.com>
2023-03-21 23:07:50 +0800

    Updated on 2026-06-12

--- app/src/main/java/com/tangem/tap/features/welcome/redux/WelcomeAction.kt
+++ app/src/main/java/com/tangem/tap/features/welcome/redux/WelcomeAction.kt
@@ -5,7 +5,6 @@ import com.tangem.common.core.TangemError
 import org.rekotlin.Action
 
 internal sealed interface WelcomeAction : Action {
-    object OnCreate : WelcomeAction
     object ProceedWithBiometrics : WelcomeAction {
         object Success : WelcomeAction
         data class Error(val error: TangemError) : WelcomeAction
