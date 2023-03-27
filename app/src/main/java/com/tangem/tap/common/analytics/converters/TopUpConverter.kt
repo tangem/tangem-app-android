2c9f8e2bc02875ac5febdfc176f655afe5836bca
Tangem <git@tangem.com>
2023-03-27 17:47:53 +0300

    Updated on 2026-08-19

--- app/src/main/java/com/tangem/tap/common/analytics/converters/TopUpConverter.kt
+++ app/src/main/java/com/tangem/tap/common/analytics/converters/TopUpConverter.kt
@@ -1,67 +1,17 @@
 package com.tangem.tap.common.analytics.converters
 
 import com.tangem.common.Converter
-import com.tangem.common.extensions.isZero
 import com.tangem.domain.common.CardTypesResolver
-import com.tangem.tap.common.analytics.events.AnalyticsParam
 import com.tangem.tap.common.analytics.events.Basic
-import com.tangem.tap.common.analytics.filters.TopUpFilter
-import com.tangem.tap.domain.model.WalletDataModel
-import java.math.BigDecimal
 
 /**
 [REDACTED_AUTHOR]
  */
-class TopUpEventConverter : Converter<TopUpEventConverter.Data, Basic.ToppedUp?> {
+class TopUpEventConverter : Converter<CardTypesResolver, Basic.ToppedUp?> {
 
-    override fun convert(value: Data): Basic.ToppedUp? {
-        val (cardTypesResolver, walletDataModels, userWalletIdValue, isToppedUpInPast) = value
-        val paramCardCurrency = ParamCardCurrencyConverter().convert(cardTypesResolver) ?: return null
+    override fun convert(value: CardTypesResolver): Basic.ToppedUp? {
+        val paramCardCurrency = ParamCardCurrencyConverter().convert(value) ?: return null
 
-        val cardBalanceState = BalanceCalculator(walletDataModels).calculate().toCardBalanceState()
-        val filterData = TopUpFilter.Data(
-            walletId = userWalletIdValue,
-            cardBalanceState = cardBalanceState,
-            isToppedUpInPast = isToppedUpInPast,
-        )
-        return Basic.ToppedUp(paramCardCurrency).apply {
-            this.filterData = filterData
-        }
+        return Basic.ToppedUp(paramCardCurrency)
     }
-
-    private fun BigDecimal.toCardBalanceState(): AnalyticsParam.CardBalanceState = when {
-        isZero() -> AnalyticsParam.CardBalanceState.Empty
-        else -> AnalyticsParam.CardBalanceState.Full
-    }
-
-    data class Data(
-        val cardTypesResolver: CardTypesResolver,
-        val walletDataModels: List<WalletDataModel>,
-        val userWalletIdValue: String,
-        val isToppedUpInPast: Boolean,
-    )
-}
-
-private interface IBalanceCalculator {
-    fun calculate(): BigDecimal
-}
-
-private class BalanceCalculator(
-    private val walletDataModels: List<WalletDataModel>,
-) : IBalanceCalculator {
-
-    override fun calculate(): BigDecimal {
-        val singleToken = walletDataModels
-            .filter { it.currency.isToken() }
-            .firstOrNull { it.isCardSingleToken }
-
-        val totalAmount = singleToken?.status?.amount
-            ?: walletDataModels.calculateTotalCryptoAmount()
-
-        return totalAmount
-    }
-
-    private fun List<WalletDataModel>.calculateTotalCryptoAmount(): BigDecimal = this
-        .map { it.status.amount }
-        .reduce(BigDecimal::plus)
 }
