405db54d05bade68f2676cbd4578753cc1193dbe
Tangem <git@tangem.com>
2023-03-31 16:10:58 +0300

    release/5.26.1

--- core/utils/src/main/java/com/tangem/utils/coroutines/CoroutineExt.kt
+++ core/utils/src/main/java/com/tangem/utils/coroutines/CoroutineExt.kt
@@ -3,13 +3,21 @@ package com.tangem.utils.coroutines
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 suspend fun <R> runCatching(dispatcher: CoroutineDispatcher, block: suspend () -> R): Result<R> {
     return runCatching {
         withContext(dispatcher) { block() }
+    }
+}
+
+suspend inline fun ifActive(crossinline block: suspend () -> Unit) = coroutineScope {
+    if (isActive) {
+        block()
     }
 }
 
