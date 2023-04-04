fb819cf0de80ca654e2ee72a777a5b485476f61d
Tangem <git@tangem.com>
2023-04-04 13:05:00 +0300

    release/5.26.1

--- app/src/main/java/com/tangem/tap/features/demo/DefaultDemoModeDatasource.kt
+++ app/src/main/java/com/tangem/tap/features/demo/DefaultDemoModeDatasource.kt
@@ -1,10 +1,10 @@
 package com.tangem.tap.features.demo
 
-import com.tangem.datasource.demo.DemoModeData
+import com.tangem.datasource.demo.DemoModeDatasource
 import com.tangem.tap.proxy.AppStateHolder
 import javax.inject.Inject
 
-class DefaultDemoModeData @Inject constructor(private val appStateHolder: AppStateHolder) : DemoModeData {
+class DefaultDemoModeDatasource @Inject constructor(private val appStateHolder: AppStateHolder) : DemoModeDatasource {
 
     override val isDemoModeActive: Boolean
         get() = appStateHolder.scanResponse?.let { DemoHelper.isDemoCard(it) } == true
