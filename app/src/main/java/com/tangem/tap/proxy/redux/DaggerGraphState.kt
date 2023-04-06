069605e151f23f15d3a82749e378c0f7e972a599
Tangem <git@tangem.com>
2023-04-06 22:57:30 +0800

    Updated on 2026-08-14

--- app/src/main/java/com/tangem/tap/proxy/redux/DaggerGraphState.kt
+++ app/src/main/java/com/tangem/tap/proxy/redux/DaggerGraphState.kt
@@ -3,12 +3,14 @@ package com.tangem.tap.proxy.redux
 import com.tangem.datasource.asset.AssetReader
 import com.tangem.datasource.connection.NetworkConnectionManager
 import com.tangem.features.tester.api.TesterRouter
+import com.tangem.tap.features.tokens.api.featuretoggles.TokensListFeatureToggles
 import org.rekotlin.StateType
 
 data class DaggerGraphState(
     val assetReader: AssetReader? = null,
     val testerRouter: TesterRouter? = null,
     val networkConnectionManager: NetworkConnectionManager? = null,
+    val tokensListFeatureToggles: TokensListFeatureToggles? = null,
 ) : StateType {
 
     inline fun <reified T> get(getDependency: DaggerGraphState.() -> T?): T {
