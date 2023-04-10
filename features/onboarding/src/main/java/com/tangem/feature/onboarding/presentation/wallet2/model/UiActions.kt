31ba2ff349cf1524623fb6afd1cf7fb048727f01
Tangem <git@tangem.com>
2023-04-10 14:55:39 +0300

    Updated on 2026-08-07

--- features/onboarding/src/main/java/com/tangem/feature/onboarding/presentation/wallet2/model/UiActions.kt
+++ features/onboarding/src/main/java/com/tangem/feature/onboarding/presentation/wallet2/model/UiActions.kt
@@ -6,45 +6,45 @@ import androidx.compose.ui.text.input.TextFieldValue
 [REDACTED_AUTHOR]
  */
 data class UiActions(
-    val introActions: IntroUiAction = IntroUiAction(),
-    val aboutActions: AboutUiAction = AboutUiAction(),
-    val yourSeedPhraseActions: YourSeedPhraseUiAction = YourSeedPhraseUiAction(),
-    val checkSeedPhraseActions: CheckSeedPhraseUiAction = CheckSeedPhraseUiAction(),
-    val importSeedPhraseActions: ImportSeedPhraseUiAction = ImportSeedPhraseUiAction(),
-    val menuChatClick: () -> Unit = {},
+    val introActions: IntroUiAction,
+    val aboutActions: AboutUiAction,
+    val yourSeedPhraseActions: YourSeedPhraseUiAction,
+    val checkSeedPhraseActions: CheckSeedPhraseUiAction,
+    val importSeedPhraseActions: ImportSeedPhraseUiAction,
+    val menuChatClick: () -> Unit,
 )
 
 data class IntroUiAction(
-    val buttonCreateWalletClick: () -> Unit = {},
-    val buttonOtherOptionsClick: () -> Unit = {},
+    val buttonCreateWalletClick: () -> Unit,
+    val buttonOtherOptionsClick: () -> Unit,
 )
 
 data class AboutUiAction(
-    val buttonReadMoreAboutSeedPhraseClick: () -> Unit = {},
-    val buttonGenerateSeedPhraseClick: () -> Unit = {},
-    val buttonImportSeedPhraseClick: () -> Unit = {},
+    val buttonReadMoreAboutSeedPhraseClick: () -> Unit,
+    val buttonGenerateSeedPhraseClick: () -> Unit,
+    val buttonImportSeedPhraseClick: () -> Unit,
 )
 
 data class YourSeedPhraseUiAction(
-    val buttonContinueClick: () -> Unit = {},
+    val buttonContinueClick: () -> Unit,
 )
 
 data class CheckSeedPhraseUiAction(
-    val secondTextFieldAction: TextFieldUiAction = TextFieldUiAction(),
-    val seventhTextFieldAction: TextFieldUiAction = TextFieldUiAction(),
-    val eleventhTextFieldAction: TextFieldUiAction = TextFieldUiAction(),
-    val buttonCreateWalletClick: () -> Unit = {},
+    val secondTextFieldAction: TextFieldUiAction,
+    val seventhTextFieldAction: TextFieldUiAction,
+    val eleventhTextFieldAction: TextFieldUiAction,
+    val buttonCreateWalletClick: () -> Unit,
 )
 
 data class ImportSeedPhraseUiAction(
-    val phraseTextFieldAction: TextFieldUiAction = TextFieldUiAction(),
-    val suggestedPhraseClick: (Int) -> Unit = {},
-    val buttonCreateWalletClick: () -> Unit = {},
+    val phraseTextFieldAction: TextFieldUiAction,
+    val suggestedPhraseClick: (Int) -> Unit,
+    val buttonCreateWalletClick: () -> Unit,
 )
 
 data class TextFieldUiAction(
-    val onFocusChanged: (Boolean) -> Unit = { },
-    val onTextFieldChanged: (TextFieldValue) -> Unit = { },
+    val onFocusChanged: (Boolean) -> Unit,
+    val onTextFieldChanged: (TextFieldValue) -> Unit,
 )
 
 enum class SeedPhraseField {
