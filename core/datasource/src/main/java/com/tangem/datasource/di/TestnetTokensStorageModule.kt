a1cec33bc6ab638e87ddf5c7215d4d0f2ef9672a
Tangem <git@tangem.com>
2023-04-10 21:23:27 +0900

    Updated on 2025-10-09

---
+++ core/datasource/src/main/java/com/tangem/datasource/di/TestnetTokensStorageModule.kt
@@ -0,0 +1,27 @@
+package com.tangem.datasource.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.adapter
+import com.tangem.datasource.asset.AssetReader
+import com.tangem.datasource.local.testnet.DefaultTestnetTokensStorage
+import com.tangem.datasource.local.testnet.TestnetTokensStorage
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+/**
+[REDACTED_AUTHOR]
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+object TestnetTokensStorageModule {
+
+    @OptIn(ExperimentalStdlibApi::class)
+    @Provides
+    @Singleton
+    fun providesTestnetTokensStorage(assetReader: AssetReader, @SdkMoshi moshi: Moshi): TestnetTokensStorage {
+        return DefaultTestnetTokensStorage(assetReader = assetReader, adapter = moshi.adapter())
+    }
+}
