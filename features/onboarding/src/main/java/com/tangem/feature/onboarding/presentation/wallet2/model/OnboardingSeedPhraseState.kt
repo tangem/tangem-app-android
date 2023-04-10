12d0f97396abcaa97d93cd2a33e88f1697cc7f63
Tangem <git@tangem.com>
2023-04-10 14:55:39 +0300

    Updated on 2026-07-07

--- features/onboarding/src/main/java/com/tangem/feature/onboarding/presentation/wallet2/model/OnboardingSeedPhraseState.kt
+++ features/onboarding/src/main/java/com/tangem/feature/onboarding/presentation/wallet2/model/OnboardingSeedPhraseState.kt
@@ -1,6 +1,5 @@
 package com.tangem.feature.onboarding.presentation.wallet2.model
 
-import androidx.annotation.StringRes
 import androidx.compose.ui.text.input.TextFieldValue
 import com.tangem.feature.onboarding.domain.SeedPhraseError
 
@@ -8,13 +7,13 @@ import com.tangem.feature.onboarding.domain.SeedPhraseError
 [REDACTED_AUTHOR]
  */
 data class OnboardingSeedPhraseState(
-    val step: OnboardingSeedPhraseStep = OnboardingSeedPhraseStep.Intro,
-    val introState: IntroState = IntroState(),
-    val aboutState: AboutState = AboutState(),
-    val yourSeedPhraseState: YourSeedPhraseState = YourSeedPhraseState(),
-    val checkSeedPhraseState: CheckSeedPhraseState = CheckSeedPhraseState(),
-    val importSeedPhraseState: ImportSeedPhraseState = ImportSeedPhraseState(),
-    val menuButtonChat: ButtonState = ButtonState(),
+    val step: OnboardingSeedPhraseStep,
+    val introState: IntroState,
+    val aboutState: AboutState,
+    val yourSeedPhraseState: YourSeedPhraseState,
+    val checkSeedPhraseState: CheckSeedPhraseState,
+    val importSeedPhraseState: ImportSeedPhraseState,
+    val menuButtonChat: ButtonState,
     val isOnboardingFinished: Boolean = false,
 )
 
@@ -23,43 +22,42 @@ enum class OnboardingSeedPhraseStep {
 }
 
 data class IntroState(
-    val buttonCreateWallet: ButtonState = ButtonState(),
-    val buttonOtherOptions: ButtonState = ButtonState(),
+    val buttonCreateWallet: ButtonState,
+    val buttonOtherOptions: ButtonState,
 )
 
 data class AboutState(
-    val buttonReadMoreAboutSeedPhrase: ButtonState = ButtonState(),
-    val buttonGenerateSeedPhrase: ButtonState = ButtonState(),
-    val buttonImportSeedPhrase: ButtonState = ButtonState(),
+    val buttonReadMoreAboutSeedPhrase: ButtonState,
+    val buttonGenerateSeedPhrase: ButtonState,
+    val buttonImportSeedPhrase: ButtonState,
 )
 
 data class YourSeedPhraseState(
     val mnemonicComponents: List<String> = listOf(),
-    val buttonContinue: ButtonState = ButtonState(),
+    val buttonContinue: ButtonState,
 )
 
 data class CheckSeedPhraseState(
-    val tvSecondPhrase: TextFieldState = TextFieldState(),
-    val tvSeventhPhrase: TextFieldState = TextFieldState(),
-    val tvEleventhPhrase: TextFieldState = TextFieldState(),
-    val buttonCreateWallet: ButtonState = ButtonState(),
+    val tvSecondPhrase: TextFieldState,
+    val tvSeventhPhrase: TextFieldState,
+    val tvEleventhPhrase: TextFieldState,
+    val buttonCreateWallet: ButtonState,
 )
 
 data class ImportSeedPhraseState(
-    val tvSeedPhrase: TextFieldState = TextFieldState(),
+    val tvSeedPhrase: TextFieldState,
+    val onSuggestedPhraseClick: (Int) -> Unit,
+    val buttonCreateWallet: ButtonState,
     val invalidWords: Set<String> = emptySet(),
     val suggestionsList: List<String> = emptyList(),
     val error: SeedPhraseError? = null,
-    val onSuggestedPhraseClick: (Int) -> Unit = {},
-    val buttonCreateWallet: ButtonState = ButtonState(),
 )
 
 data class TextFieldState(
+    val onTextFieldValueChanged: (TextFieldValue) -> Unit,
     val textFieldValue: TextFieldValue = TextFieldValue(),
     val label: String? = null,
-    @StringRes val labelRes: Int? = null,
     val isError: Boolean = false,
     val isFocused: Boolean = false,
-    val onTextFieldValueChanged: (TextFieldValue) -> Unit = {},
     val onFocusChanged: (Boolean) -> Unit = {},
 )
