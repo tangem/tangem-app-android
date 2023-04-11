c1b46d8dcad62602103e1afba7e3eed9ab98e39f
Tangem <git@tangem.com>
2023-04-11 12:42:05 +0300

    Updated on 2026-05-07

--- app/src/main/java/com/tangem/tap/features/tokens/redux/TokensState.kt
+++ app/src/main/java/com/tangem/tap/features/tokens/redux/TokensState.kt
@@ -6,13 +6,13 @@ import com.tangem.blockchain.common.Token
 import com.tangem.domain.common.ScanResponse
 import com.tangem.domain.common.extensions.canHandleToken
 import com.tangem.domain.common.extensions.fromNetworkId
+import com.tangem.tap.domain.model.WalletDataModel
 import com.tangem.tap.domain.tokens.Currency
-import com.tangem.tap.features.wallet.redux.WalletData
 import org.rekotlin.StateType
 import com.tangem.tap.features.wallet.models.Currency.Token as CurrencyToken
 
 data class TokensState(
-    val addedWallets: List<WalletData> = emptyList(),
+    val addedWallets: List<WalletDataModel> = emptyList(),
     val addedTokens: List<TokenWithBlockchain> = emptyList(),
     val addedBlockchains: List<Blockchain> = emptyList(),
     val currencies: List<Currency> = emptyList(),
@@ -31,17 +31,9 @@ data class TokensState(
 
 typealias ContractAddress = String
 
-fun List<WalletData>.toTokensContractAddresses(): List<ContractAddress> {
-    return mapNotNull { (it.currency as? CurrencyToken)?.token?.contractAddress }.distinct()
-}
-
-fun List<WalletData>.toNonCustomTokens(derivationStyle: DerivationStyle?): List<Token> {
-    return filter { !it.currency.isCustomCurrency(derivationStyle) }
-        .mapNotNull { (it.currency as? CurrencyToken)?.token }
-        .distinct()
-}
-
-fun List<WalletData>.toNonCustomTokensWithBlockchains(derivationStyle: DerivationStyle?): List<TokenWithBlockchain> {
+fun List<WalletDataModel>.toNonCustomTokensWithBlockchains(
+    derivationStyle: DerivationStyle?,
+): List<TokenWithBlockchain> {
     return mapNotNull {
         if (it.currency !is CurrencyToken) return@mapNotNull null
         if (it.currency.isCustomCurrency(derivationStyle)) return@mapNotNull null
@@ -49,7 +41,7 @@ fun List<WalletData>.toNonCustomTokensWithBlockchains(derivationStyle: Derivatio
     }.distinct()
 }
 
-fun List<WalletData>.toNonCustomBlockchains(derivationStyle: DerivationStyle?): List<Blockchain> {
+fun List<WalletDataModel>.toNonCustomBlockchains(derivationStyle: DerivationStyle?): List<Blockchain> {
     return mapNotNull {
         if (it.currency.isCustomCurrency(derivationStyle)) {
             null
