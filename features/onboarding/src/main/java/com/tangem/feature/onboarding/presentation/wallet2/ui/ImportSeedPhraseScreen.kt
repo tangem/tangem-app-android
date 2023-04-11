3569b70789facf5e8e6d853b1f1746306b568be9
Tangem <git@tangem.com>
2023-04-11 15:58:35 +0300

    Updated on 2026-04-06

--- features/onboarding/src/main/java/com/tangem/feature/onboarding/presentation/wallet2/ui/ImportSeedPhraseScreen.kt
+++ features/onboarding/src/main/java/com/tangem/feature/onboarding/presentation/wallet2/ui/ImportSeedPhraseScreen.kt
@@ -1,37 +1,33 @@
 package com.tangem.feature.onboarding.presentation.wallet2.ui
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideIn
+import androidx.compose.animation.slideOut
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.IntOffset
 import com.tangem.core.ui.components.PrimaryButton
 import com.tangem.core.ui.components.PrimaryButtonIconLeft
-import com.tangem.core.ui.components.SpacerW8
+import com.tangem.core.ui.components.TangemTextFieldsDefault
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.feature.onboarding.R
 import com.tangem.feature.onboarding.presentation.wallet2.model.ImportSeedPhraseState
-import com.tangem.feature.onboarding.presentation.wallet2.model.OnboardingDescription
+import com.tangem.feature.onboarding.presentation.wallet2.ui.components.DescriptionSubTitleText
 import com.tangem.feature.onboarding.presentation.wallet2.ui.components.OnboardingActionBlock
 import com.tangem.feature.onboarding.presentation.wallet2.ui.components.OnboardingDescriptionBlock
 
@@ -40,38 +36,30 @@ import com.tangem.feature.onboarding.presentation.wallet2.ui.components.Onboardi
  */
 @Composable
 fun ImportSeedPhraseScreen(
+    state: ImportSeedPhraseState,
     modifier: Modifier = Modifier,
-    state: ImportSeedPhraseState = ImportSeedPhraseState(),
 ) {
     Column(
         modifier = modifier.fillMaxSize(),
     ) {
         Box(
-            modifier = Modifier.weight(0.6f),
-        ) {
-            OnboardingDescriptionBlock(
-                modifier = Modifier.align(Alignment.Center),
-                descriptionsList = listOf(
-                    OnboardingDescription(
-                        subTitle = "To import your wallet, enter your secret recovery phrase in the fields below or scan a QR-code",
-                    ),
-                ),
-            )
-        }
-        Box(
-            modifier = Modifier
-                .padding(horizontal = TangemTheme.dimens.size16)
-                .weight(1f),
-        ) {
-            PhraseBlock(state)
-        }
-
-        Box(
             modifier = Modifier.weight(1f),
         ) {
-            SuggestionsBlock(state)
+            Column {
+                OnboardingDescriptionBlock(modifier) {
+                    DescriptionSubTitleText(text = stringResource(id = R.string.onboarding_seed_import_message))
+                }
+                PhraseBlock(
+                    modifier = Modifier
+                        .padding(top = TangemTheme.dimens.size62)
+                        .padding(horizontal = TangemTheme.dimens.size16),
+                    state = state,
+                )
+                SuggestionsBlock(
+                    state = state,
+                )
+            }
         }
-
         Box(
             modifier = Modifier.wrapContentSize(),
         ) {
@@ -94,65 +82,63 @@ fun ImportSeedPhraseScreen(
 }
 
 @Composable
-private fun PhraseBlock(state: ImportSeedPhraseState) {
-    OutlinedTextField(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(TangemTheme.dimens.size142),
-        // value = TextFieldValue(text = state.tvPhrase.text),
-        value = TextFieldValue(text = "state.tvPhrase.text"),
-        onValueChange = {  },
-        // textStyle = TangemTheme.typography.body1,
-        // singleLine = false,
-    )
-}
-
-@Composable
-private fun SuggestionsBlock(state: ImportSeedPhraseState) {
-    LazyRow(
-        modifier = Modifier.fillMaxSize(),
+private fun PhraseBlock(
+    state: ImportSeedPhraseState,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
     ) {
-        items(state.suggestionsList.size) { index ->
-            PrimaryButton(
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(TangemTheme.dimens.size142),
+            value = state.tvSeedPhrase.textFieldValue,
+            onValueChange = state.tvSeedPhrase.onTextFieldValueChanged,
+            textStyle = TangemTheme.typography.body1,
+            singleLine = false,
+            colors = TangemTextFieldsDefault.defaultTextFieldColors,
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(TangemTheme.dimens.size32),
+        ) {
+            Text(
                 modifier = Modifier
-                    .padding(all = TangemTheme.dimens.size4)
-                    .background(color = TangemTheme.colors.background.action),
-                text = state.suggestionsList[index],
-                onClick = { state.onSuggestedPhraseClick(index) },
+                    .fillMaxSize(),
+                text = "TODO: implement error message",
+                style = TangemTheme.typography.caption.copy(
+                    color = TangemTheme.colors.text.warning,
+                ),
             )
         }
     }
 }
 
 @Composable
-private fun ReadMoreBlock() {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 16.dp),
+private fun SuggestionsBlock(
+    state: ImportSeedPhraseState,
+    modifier: Modifier = Modifier,
+) {
+    AnimatedVisibility(
+        enter = fadeIn() + slideIn(initialOffset = { IntOffset(200, 0) }),
+        exit = slideOut(targetOffset = { IntOffset(-200, 0) }) + fadeOut(),
+        visible = state.suggestionsList.isNotEmpty(),
     ) {
-        val shape = RoundedCornerShape(24.dp)
-        Row(
-            modifier = Modifier
-                .align(Alignment.Center)
-                .clip(shape)
-                .border(1.dp, Color.Gray, shape)
-                .clickable(
-                    onClick = {},
-                )
-                .padding(
-                    horizontal = 16.dp,
-                    vertical = 8.dp,
-                ),
+        LazyRow(
+            modifier = modifier.fillMaxSize(),
+            contentPadding = PaddingValues(horizontal = TangemTheme.dimens.size16),
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_arrow_top_right),
-                contentDescription = "Go away icon",
-            )
-            SpacerW8()
-            Text(
-                text = stringResource(id = R.string.onboarding_seed_button_read_more),
-            )
+            items(state.suggestionsList.size) { index ->
+                PrimaryButton(
+                    modifier = Modifier
+                        .height(TangemTheme.dimens.size46)
+                        .padding(all = TangemTheme.dimens.size4),
+                    text = state.suggestionsList[index],
+                    onClick = { state.onSuggestedPhraseClick(index) },
+                )
+            }
         }
     }
 }
