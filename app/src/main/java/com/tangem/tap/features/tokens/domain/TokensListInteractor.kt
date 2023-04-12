c4141a01ad91e42c51458c38a220f4de6154323b
Tangem <git@tangem.com>
2023-04-12 16:33:33 +0900

    Updated on 2025-09-01

---
+++ app/src/main/java/com/tangem/tap/features/tokens/domain/TokensListInteractor.kt
@@ -0,0 +1,16 @@
+package com.tangem.tap.features.tokens.domain
+
+import androidx.paging.PagingData
+import com.tangem.tap.features.tokens.domain.models.Token
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Interactor of tokens list feature
+ *
+[REDACTED_AUTHOR]
+ */
+internal interface TokensListInteractor {
+
+    /** Get tokens list using filter by text [searchText] */
+    fun getTokensList(searchText: String): Flow<PagingData<Token>>
+}
