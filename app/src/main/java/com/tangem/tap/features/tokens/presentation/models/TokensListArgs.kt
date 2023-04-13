bfb2338a1ab148a62a5e903bafea779b54ebd350
Tangem <git@tangem.com>
2023-04-13 16:19:43 +0900

    Updated on 2026-08-14

---
+++ app/src/main/java/com/tangem/tap/features/tokens/presentation/models/TokensListArgs.kt
@@ -0,0 +1,22 @@
+package com.tangem.tap.features.tokens.presentation.models
+
+import com.tangem.blockchain.common.Blockchain
+import com.tangem.tap.features.tokens.redux.TokenWithBlockchain
+import com.tangem.tap.store
+
+/**
+ * Required data for tokens list screen
+ * FIXME("Necessary to avoid using redux state")
+ *
+[REDACTED_AUTHOR]
+ */
+class TokensListArgs {
+    /** Tokens list screen mode */
+    val isManageAccess: Boolean get() = store.state.tokensState.allowToAdd
+
+    /** Tokens list that accessible from the main screen */
+    val mainScreenTokenList: List<TokenWithBlockchain> get() = store.state.tokensState.addedTokens
+
+    /** Blockchains list that accessible from the main screen */
+    val mainScreenBlockchainList: List<Blockchain> get() = store.state.tokensState.addedBlockchains
+}
--- app/src/main/java/com/tangem/tap/features/tokens/presentation/states/TokensListToolbarState.kt
+++ app/src/main/java/com/tangem/tap/features/tokens/presentation/states/TokensListToolbarState.kt
@@ -6,39 +6,39 @@ sealed interface TokensListToolbarState {
     /** Callback to be invoked when BackButton is being clicked */
     val onBackButtonClick: () -> Unit
 
-    /** Callback to be invoked when SearchButton is being clicked */
-    val onSearchButtonClick: () -> Unit
-
     /** Toolbar state as title */
     sealed interface Title : TokensListToolbarState {
 
         /** Toolbar title id from resources */
         val titleResId: Int
 
+        /** Callback to be invoked when SearchButton is being clicked */
+        val onSearchButtonClick: () -> Unit
+
         /**
          * Title state that is available only for read
          *
-         * @property titleResId          toolbar title id from resources
          * @property onBackButtonClick   callback to be invoked when BackButton is being clicked
+         * @property titleResId          toolbar title id from resources
          * @property onSearchButtonClick callback to be invoked when SearchButton is being clicked
          */
-        data class ReadAccess(
-            override val titleResId: Int,
+        data class Read(
             override val onBackButtonClick: () -> Unit,
+            override val titleResId: Int,
             override val onSearchButtonClick: () -> Unit,
         ) : Title
 
         /**
-         * Title state that is available for read and edit
+         * Title state that is available for read and manage
          *
-         * @property titleResId            toolbar title id from resources
          * @property onBackButtonClick     callback to be invoked when BackButton is being clicked
+         * @property titleResId            toolbar title id from resources
          * @property onSearchButtonClick   callback to be invoked when SearchButton is being clicked
          * @property onAddCustomTokenClick callback to be invoked when AddCustomTokenButton is being clicked
          */
-        data class ManageAccess(
-            override val titleResId: Int,
+        data class Manage(
             override val onBackButtonClick: () -> Unit,
+            override val titleResId: Int,
             override val onSearchButtonClick: () -> Unit,
             val onAddCustomTokenClick: () -> Unit,
         ) : Title
@@ -47,15 +47,13 @@ sealed interface TokensListToolbarState {
     /**
      * Toolbar state as input field
      *
-     * @property onBackButtonClick   callback to be invoked when BackButton is being clicked
-     * @property onSearchButtonClick callback to be invoked when SearchButton is being clicked
-     * @property value               input value
-     * @property onValueChange       lambda to be invoked when search value is being changed
-     * @property onCleanButtonClick  callback to be invoked when CleanButton is being clicked
+     * @property onBackButtonClick  callback to be invoked when BackButton is being clicked
+     * @property value              input value
+     * @property onValueChange      lambda to be invoked when search value is being changed
+     * @property onCleanButtonClick callback to be invoked when CleanButton is being clicked
      */
-    data class SearchInputField(
+    data class InputField(
         override val onBackButtonClick: () -> Unit,
-        override val onSearchButtonClick: () -> Unit,
         val value: String,
         val onValueChange: (String) -> Unit,
         val onCleanButtonClick: () -> Unit,
