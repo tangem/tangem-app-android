2188a61dbf21c0768e96307f9854e03114ec7fa2
Tangem <git@tangem.com>
2023-04-14 20:47:58 +0900

    Updated on 2025-12-05

--- app/src/main/java/com/tangem/tap/features/tokens/presentation/router/DefaultTokensListRouter.kt
+++ app/src/main/java/com/tangem/tap/features/tokens/presentation/router/DefaultTokensListRouter.kt
@@ -1,11 +1,17 @@
 package com.tangem.tap.features.tokens.presentation.router
 
+import com.tangem.tap.common.extensions.dispatchDialogShow
+import com.tangem.tap.common.extensions.dispatchNotification
+import com.tangem.tap.common.redux.AppDialog
 import com.tangem.tap.common.redux.navigation.NavigationAction
 import com.tangem.tap.features.tokens.redux.TokensAction
+import com.tangem.tap.features.wallet.redux.models.WalletDialog
 import com.tangem.tap.store
+import com.tangem.wallet.R
 
 /**
  * Default implementation of tokens list router
+ * FIXME("Necessary to avoid using redux actions")
  *
 [REDACTED_AUTHOR]
  */
@@ -18,5 +24,30 @@ internal class DefaultTokensListRouter : TokensListRouter {
 
     override fun openAddCustomTokenScreen() {
         store.dispatch(TokensAction.PrepareAndNavigateToAddCustomToken)
+    }
+
+    override fun showAddressCopiedNotification() {
+        store.dispatchNotification(R.string.contract_address_copied_message)
+    }
+
+    override fun openUnableHideMainTokenAlert(tokenName: String, tokenSymbol: String) {
+        store.dispatchDialogShow(
+            dialog = WalletDialog.TokensAreLinkedDialog(currencyTitle = tokenName, currencySymbol = tokenSymbol),
+        )
+    }
+
+    override fun openRemoveWalletAlert(tokenName: String, onOkClick: () -> Unit) {
+        store.dispatchDialogShow(
+            dialog = WalletDialog.RemoveWalletDialog(currencyTitle = tokenName, onOk = onOkClick),
+        )
+    }
+
+    override fun openUnsupportedSoltanaNetworkAlert() {
+        store.dispatchDialogShow(
+            AppDialog.SimpleOkDialogRes(
+                headerId = R.string.common_warning,
+                messageId = R.string.alert_manage_tokens_unsupported_message,
+            ),
+        )
     }
 }
