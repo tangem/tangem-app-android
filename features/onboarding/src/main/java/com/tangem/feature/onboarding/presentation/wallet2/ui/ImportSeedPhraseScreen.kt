e7557659d3eef522753bbbf7379e6719af1535a8
Tangem <git@tangem.com>
2023-04-14 14:08:53 +0300

    Updated on 2026-08-14

--- features/onboarding/src/main/java/com/tangem/feature/onboarding/presentation/wallet2/ui/ImportSeedPhraseScreen.kt
+++ features/onboarding/src/main/java/com/tangem/feature/onboarding/presentation/wallet2/ui/ImportSeedPhraseScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
@@ -60,9 +59,7 @@ fun ImportSeedPhraseScreen(
                 )
             }
         }
-        Box(
-            modifier = Modifier.wrapContentSize(),
-        ) {
+        Box {
             OnboardingActionBlock(
                 firstActionContent = {
                     PrimaryButtonIconLeft(
