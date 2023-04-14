f1b2efeaeb7ccb481073e885595b72d587d255a4
Tangem <git@tangem.com>
2023-04-14 20:47:58 +0900

    Updated on 2026-07-23

--- app/src/main/java/com/tangem/tap/features/tokens/domain/TokensListInteractor.kt
+++ app/src/main/java/com/tangem/tap/features/tokens/domain/TokensListInteractor.kt
@@ -1,7 +1,9 @@
 package com.tangem.tap.features.tokens.domain
 
 import androidx.paging.PagingData
+import com.tangem.blockchain.common.Blockchain
 import com.tangem.tap.features.tokens.domain.models.Token
+import com.tangem.tap.features.tokens.redux.TokenWithBlockchain
 import kotlinx.coroutines.flow.Flow
 
 /**
@@ -13,4 +15,12 @@ internal interface TokensListInteractor {
 
     /** Get tokens list using filter by text [searchText] */
     fun getTokensList(searchText: String): Flow<PagingData<Token>>
+
+    /**
+     * Save added tokens
+     *
+     * @param tokens      tokens list that need to save
+     * @param blockchains blockchains list that need to save
+     */
+    suspend fun saveChanges(tokens: List<TokenWithBlockchain>, blockchains: List<Blockchain>)
 }
