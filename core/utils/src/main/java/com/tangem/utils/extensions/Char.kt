b50b22f3b61c9cd005d319820fd6219da77ba8e3
Tangem <git@tangem.com>
2023-04-16 21:14:54 +0300

    release/5.26.1

---
+++ core/utils/src/main/java/com/tangem/utils/extensions/Char.kt
@@ -0,0 +1,7 @@
+package com.tangem.utils.extensions
+
+/**
+* [REDACTED_AUTHOR]
+ */
+fun Char.isNotWhitespace(): Boolean = !this.isWhitespace()
+
---
+++ features/onboarding/src/main/java/com/tangem/feature/onboarding/presentation/wallet2/ui/ImportSeedPhraseStateBuilder.kt
@@ -0,0 +1,55 @@
+package com.tangem.feature.onboarding.presentation.wallet2.ui
+
+import androidx.compose.ui.text.input.TextFieldValue
+import com.tangem.feature.onboarding.domain.SeedPhraseError
+import com.tangem.feature.onboarding.presentation.wallet2.model.OnboardingSeedPhraseState
+
+/**
+* [REDACTED_AUTHOR]
+ */
+class ImportSeedPhraseStateBuilder {
+
+    fun updateTextField(
+        uiState: OnboardingSeedPhraseState,
+        textFieldValue: TextFieldValue,
+    ): OnboardingSeedPhraseState = uiState.copy(
+        importSeedPhraseState = uiState.importSeedPhraseState.copy(
+            tvSeedPhrase = uiState.importSeedPhraseState.tvSeedPhrase.copy(
+                textFieldValue = textFieldValue,
+            ),
+        ),
+    )
+
+    fun updateCreateWalletButton(
+        uiState: OnboardingSeedPhraseState,
+        enabled: Boolean,
+    ): OnboardingSeedPhraseState {
+        return uiState.copy(
+            importSeedPhraseState = uiState.importSeedPhraseState.copy(
+                buttonCreateWallet = uiState.importSeedPhraseState.buttonCreateWallet.copy(
+                    enabled = enabled,
+                ),
+            ),
+        )
+    }
+
+    fun updateInvalidWords(
+        uiState: OnboardingSeedPhraseState,
+        invalidWords: Set<String>,
+    ): OnboardingSeedPhraseState = uiState.copy(
+        importSeedPhraseState = uiState.importSeedPhraseState.copy(
+            invalidWords = invalidWords,
+        ),
+    )
+
+    fun updateError(
+        uiState: OnboardingSeedPhraseState,
+        error: SeedPhraseError?,
+    ): OnboardingSeedPhraseState {
+        return uiState.copy(
+            importSeedPhraseState = uiState.importSeedPhraseState.copy(
+                error = error,
+            ),
+        )
+    }
+}
