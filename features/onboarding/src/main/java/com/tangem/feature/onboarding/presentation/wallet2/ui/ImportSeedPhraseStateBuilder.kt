e92887baf384e2837f226585b25fb07ca794e26c
Tangem <git@tangem.com>
2023-04-16 21:54:28 +0300

    Updated on 2026-09-01

--- features/onboarding/src/main/java/com/tangem/feature/onboarding/presentation/wallet2/ui/ImportSeedPhraseStateBuilder.kt
+++ features/onboarding/src/main/java/com/tangem/feature/onboarding/presentation/wallet2/ui/ImportSeedPhraseStateBuilder.kt
@@ -1,6 +1,8 @@
 package com.tangem.feature.onboarding.presentation.wallet2.ui
 
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.TextFieldValue
+import com.tangem.feature.onboarding.domain.InsertSuggestionResult
 import com.tangem.feature.onboarding.domain.SeedPhraseError
 import com.tangem.feature.onboarding.presentation.wallet2.model.OnboardingSeedPhraseState
 
@@ -41,6 +43,26 @@ class ImportSeedPhraseStateBuilder {
             invalidWords = invalidWords,
         ),
     )
+
+    fun updateSuggestions(
+        uiState: OnboardingSeedPhraseState,
+        suggestions: List<String>,
+    ): OnboardingSeedPhraseState = uiState.copy(
+        importSeedPhraseState = uiState.importSeedPhraseState.copy(
+            suggestionsList = suggestions,
+        ),
+    )
+
+    fun insertSuggestionWord(
+        uiState: OnboardingSeedPhraseState,
+        insertResult: InsertSuggestionResult,
+    ): OnboardingSeedPhraseState {
+        val newTextFieldValue = uiState.importSeedPhraseState.tvSeedPhrase.textFieldValue.copy(
+            text = insertResult.text,
+            selection = TextRange(insertResult.cursorPosition, insertResult.cursorPosition),
+        )
+        return updateTextField(uiState, newTextFieldValue)
+    }
 
     fun updateError(
         uiState: OnboardingSeedPhraseState,
