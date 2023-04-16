0978e95de663397ec212e3838b57c1d60d24273d
Tangem <git@tangem.com>
2023-04-17 00:21:38 +0400

    Updated on 2026-09-23

--- app/src/main/java/com/tangem/tap/features/details/ui/securitymode/SecurityModeScreen.kt
+++ app/src/main/java/com/tangem/tap/features/details/ui/securitymode/SecurityModeScreen.kt
@@ -2,27 +2,19 @@ package com.tangem.tap.features.details.ui.securitymode
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.RadioButton
-import androidx.compose.material.RadioButtonDefaults
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.tangem.tap.common.compose.TangemTypography
 import com.tangem.tap.features.details.redux.SecurityOption
 import com.tangem.tap.features.details.ui.common.DetailsMainButton
+import com.tangem.tap.features.details.ui.common.DetailsRadioButtonElement
 import com.tangem.tap.features.details.ui.common.ScreenTitle
 import com.tangem.tap.features.details.ui.common.SettingsScreensScaffold
 import com.tangem.wallet.R
@@ -74,39 +66,12 @@ fun SecurityOption(option: SecurityOption, state: SecurityModeScreenState) {
         SecurityOption.AccessCode -> R.string.details_manage_security_access_code_description
     }
 
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .selectable(
-                selected = selected,
-                onClick = { state.onNewModeSelected(option) },
-            )
-            .padding(start = 20.dp, end = 20.dp, top = 16.dp, bottom = 16.dp),
-    ) {
-        RadioButton(
-            selected = selected,
-            onClick = null,
-            modifier = Modifier.padding(end = 20.dp),
-            colors = RadioButtonDefaults.colors(
-                unselectedColor = colorResource(id = R.color.icon_secondary),
-                selectedColor = colorResource(id = R.color.icon_accent),
-            ),
-        )
-
-        Column {
-            Text(
-                text = stringResource(id = title),
-                style = TangemTypography.subtitle1,
-                color = colorResource(id = R.color.text_primary_1),
-            )
-            Spacer(modifier = Modifier.size(4.dp))
-            Text(
-                text = stringResource(id = subtitle),
-                style = TangemTypography.body2,
-                color = colorResource(id = R.color.text_secondary),
-            )
-        }
-    }
+    DetailsRadioButtonElement(
+        title = stringResource(id = title),
+        subtitle = stringResource(id = subtitle),
+        selected = selected,
+        onClick = { state.onNewModeSelected(option) },
+    )
 }
 
 @Preview
