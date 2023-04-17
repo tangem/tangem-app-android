9b1d280ceae6387b51c7bcdddbd0f05c579bb47c
Tangem <git@tangem.com>
2023-04-17 23:00:54 +0900

    Updated on 2025-12-30

--- app/src/main/java/com/tangem/tap/features/tokens/impl/presentation/states/TokensListStateHolder.kt
+++ app/src/main/java/com/tangem/tap/features/tokens/impl/presentation/states/TokensListStateHolder.kt
@@ -17,6 +17,9 @@ internal sealed interface TokensListStateHolder {
     /** Loading state */
     val isLoading: Boolean
 
+    /** Flag that determines if warning block is visible */
+    val isDifferentAddressesBlockVisible: Boolean
+
     /** Tokens list */
     val tokens: Flow<PagingData<TokenItemState>>
 
@@ -26,33 +29,50 @@ internal sealed interface TokensListStateHolder {
     /**
      * Util function that allow to make a copy
      *
-     * @param toolbarState             toolbar state
-     * @param isLoading                loading state
-     * @param tokens                   tokens list
-     * @param onTokensLoadStateChanged callback to be invoked when tokens loading state is been changed
+     * @param toolbarState                     toolbar state
+     * @param isLoading                        loading state
+     * @param isDifferentAddressesBlockVisible flag that determines if warning block is visible
+     * @param tokens                           tokens list
+     * @param onTokensLoadStateChanged         callback to be invoked when tokens loading state is been changed
      */
     fun copySealed(
         toolbarState: TokensListToolbarState = this.toolbarState,
         isLoading: Boolean = this.isLoading,
+        isDifferentAddressesBlockVisible: Boolean = this.isDifferentAddressesBlockVisible,
         tokens: Flow<PagingData<TokenItemState>> = this.tokens,
         onTokensLoadStateChanged: (LoadState) -> Unit = this.onTokensLoadStateChanged,
     ): TokensListStateHolder {
         return when (this) {
-            is ManageContent -> copy(toolbarState, isLoading, tokens, onTokensLoadStateChanged)
-            is ReadContent -> copy(toolbarState, isLoading, tokens, onTokensLoadStateChanged)
+            is ManageContent -> copy(
+                toolbarState,
+                isLoading,
+                isDifferentAddressesBlockVisible,
+                tokens,
+                onTokensLoadStateChanged,
+            )
+            is ReadContent -> copy(
+                toolbarState,
+                isLoading,
+                isDifferentAddressesBlockVisible,
+                tokens,
+                onTokensLoadStateChanged,
+            )
         }
     }
 
     /**
      * State screen that is available only for read
      *
-     * @property toolbarState             toolbar state
-     * @property tokens                   tokens list
-     * @property onTokensLoadStateChanged callback to be invoked when tokens loading state is been changed
+     * @property toolbarState                     toolbar state
+     * @property isLoading                        loading state
+     * @property isDifferentAddressesBlockVisible flag that determines if warning block is visible
+     * @property tokens                           tokens list
+     * @property onTokensLoadStateChanged         callback to be invoked when tokens loading state is been changed
      */
     data class ReadContent(
         override val toolbarState: TokensListToolbarState,
         override val isLoading: Boolean,
+        override val isDifferentAddressesBlockVisible: Boolean,
         override val tokens: Flow<PagingData<TokenItemState>>,
         override val onTokensLoadStateChanged: (LoadState) -> Unit,
     ) : TokensListStateHolder
@@ -60,14 +80,17 @@ internal sealed interface TokensListStateHolder {
     /**
      * State screen that is available for read and manage
      *
-     * @property toolbarState             toolbar state
-     * @property tokens                   tokens list
-     * @property onTokensLoadStateChanged callback to be invoked when tokens loading state is been changed
+     * @property toolbarState                     toolbar state
+     * @property isLoading                        loading state
+     * @property isDifferentAddressesBlockVisible flag that determines if warning block is visible
+     * @property tokens                           tokens list
+     * @property onTokensLoadStateChanged         callback to be invoked when tokens loading state is been changed
      * @property onSaveButtonClick        callback to be invoked when SaveButton is being clicked
      */
     data class ManageContent(
         override val toolbarState: TokensListToolbarState,
         override val isLoading: Boolean,
+        override val isDifferentAddressesBlockVisible: Boolean,
         override val tokens: Flow<PagingData<TokenItemState>>,
         override val onTokensLoadStateChanged: (LoadState) -> Unit,
         val onSaveButtonClick: () -> Unit,
