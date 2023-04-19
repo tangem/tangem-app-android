3de12236430adec641a2cb681908c2b4c8429c97
iiiburnyiii <iiiburnyiii@gmail.com>
2023-04-19 18:48:28 +0300

    AND-3251 Implement :domain:core module

---
+++ domain/core/src/main/kotlin/com/tangem/domain/core/chain/ChainProcessor.kt
@@ -0,0 +1,34 @@
+package com.tangem.domain.core.chain
+
+import arrow.core.Either
+
+abstract class ChainProcessor<E, R> {
+    private val chains: MutableList<Chain<E, R>> = mutableListOf()
+
+    protected fun addChains(vararg chains: Chain<E, R>) {
+        this.chains.addAll(chains)
+    }
+
+    protected suspend fun launchChains(
+        initial: R,
+        returnOnFirstError: Boolean = true,
+    ): Either<E, R> {
+        return launchChains(initial = Either.Right(initial), returnOnFirstError)
+    }
+
+    private suspend fun launchChains(
+        initial: Either<E, R>,
+        returnOnFirstError: Boolean = true,
+    ): Either<E, R> {
+        var result = initial
+
+        chains.forEach { chain ->
+            result = chain(result)
+                .onRight {
+                    if (returnOnFirstError) return result
+                }
+        }
+
+        return result
+    }
+}
