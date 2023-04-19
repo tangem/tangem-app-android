cb20219a97695a7e82b1ecd160ac3c67fd3e7d93
Anton Zhilenkov <azhilenkov@tangem.com>
2023-04-19 20:12:05 +0300

    AND-3176 wallet2: added docs for the PartWordFinder

--- features/onboarding/src/main/java/com/tangem/feature/onboarding/domain/PartWordFinder.kt
+++ features/onboarding/src/main/java/com/tangem/feature/onboarding/domain/PartWordFinder.kt
@@ -3,10 +3,14 @@ package com.tangem.feature.onboarding.domain
 import com.tangem.utils.extensions.isNotWhitespace
 
 /**
- * Created by Anton Zhilenkov on 07.04.2023.
+ * @author by Anton Zhilenkov on 07.04.2023.
+ * Class which provides helper functions to determine lead or last part of words for suggestions
  */
 internal class PartWordFinder {
 
+    /**
+     * Searches and return part of a word on the left side of the [cursorPosition] into the [text]
+     */
     fun getLeadPartOfWord(text: String, cursorPosition: Int): String? {
         val charText = text.toCharArray().asList()
         if (cursorPosition > charText.size || cursorPosition < 0) {
@@ -41,6 +45,10 @@ internal class PartWordFinder {
         return null
     }
 
+    /**
+     * Returns a substring of the [word] which excludes the first word part found on the left of [cursorPosition] in
+     * the [text]
+     */
     fun getLastPartOfWord(text: String, word: String, cursorPosition: Int): String {
         val leadPart = getLeadPartOfWord(text, cursorPosition) ?: return ""
 
