70d2df19ce3cf45100d92de9865b94f079e932c7
Anton Zhilenkov <azhilenkov@tangem.com>
2023-04-19 15:47:17 +0300

    AND-3179 wallet2: fixed PR issues

--- features/onboarding/src/main/java/com/tangem/feature/onboarding/presentation/wallet2/viewmodel/SeedPhraseErrorConverter.kt
+++ features/onboarding/src/main/java/com/tangem/feature/onboarding/presentation/wallet2/viewmodel/SeedPhraseErrorConverter.kt
@@ -8,12 +8,10 @@ import com.tangem.feature.onboarding.domain.SeedPhraseError
 /**
  * Created by Anton Zhilenkov on 06.04.2023.
  */
-class SeedPhraseErrorConverter(
-    private val context: Context,
-) : ModuleMessageConverter<SeedPhraseError?, String?> {
+class SeedPhraseErrorConverter : ModuleMessageConverter<Pair<Context, SeedPhraseError?>, String?> {
 
-    override fun convert(message: SeedPhraseError?): String? {
-        message ?: return null
+    override fun convert(source: Pair<Context, SeedPhraseError?>): String? {
+        val (context, message) = source
 
         val convertedMessage = when (message) {
             SeedPhraseError.InvalidEntropyLength -> {
