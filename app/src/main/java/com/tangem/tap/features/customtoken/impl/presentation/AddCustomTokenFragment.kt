5542405bc355a54f575dbdba1c461c7268bc4357
Tangem <git@tangem.com>
2023-04-24 16:14:31 +0800

    release/5.26.1

---
+++ app/src/main/java/com/tangem/tap/features/customtoken/impl/presentation/AddCustomTokenFragment.kt
@@ -0,0 +1,12 @@
+package com.tangem.tap.features.customtoken.impl.presentation
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+/**
+ * Add custom token screen
+ *
+* [REDACTED_AUTHOR]
+ */
+@AndroidEntryPoint
+internal class AddCustomTokenFragment : Fragment()
