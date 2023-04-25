2688b57ee1345eef2d73db8e2d3e1c1811288915
Tangem <git@tangem.com>
2023-04-25 09:37:15 +0300

    Updated on 2025-11-18

--- features/onboarding/src/main/java/com/tangem/feature/onboarding/presentation/wallet2/model/UiActions.kt
+++ features/onboarding/src/main/java/com/tangem/feature/onboarding/presentation/wallet2/model/UiActions.kt
@@ -45,8 +45,9 @@ data class ImportSeedPhraseUiAction(
 data class TextFieldUiAction(
     val onTextFieldChanged: (TextFieldValue) -> Unit,
     val onFocusChanged: (Boolean) -> Unit = {},
-    
 )
+
+
 
 enum class SeedPhraseField {
     Second,
