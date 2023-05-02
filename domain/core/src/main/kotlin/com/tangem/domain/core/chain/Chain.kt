c2a124b8a55d1fa1588a803179bf5b0dd83b640f
Tangem <git@tangem.com>
2023-05-03 00:29:14 +0300

    Updated on 2026-05-04

--- domain/core/src/main/kotlin/com/tangem/domain/core/chain/Chain.kt
+++ domain/core/src/main/kotlin/com/tangem/domain/core/chain/Chain.kt
@@ -1,7 +1,6 @@
 package com.tangem.domain.core.chain
 
 import arrow.core.Either
-import arrow.core.EitherNel
 
 /**
  * A chain in the [ChainProcessor] class for processing a chain of operations with the ability to handle errors.
@@ -12,8 +11,8 @@ interface Chain<E, R> {
 
     /**
      * Invokes the chain with the previous chain result as input and returns an [Either] result.
-     * @param previousChainResult the previous chain result as an [EitherNel]
+     * @param previousChainResult the previous chain result as an [R]
      * @return the result of the chain processing as an [Either]
      */
-    suspend operator fun invoke(previousChainResult: EitherNel<E, R>): Either<E, R>
+    suspend operator fun invoke(previousChainResult: R): Either<E, R>
 }
