b18144cfc409906f63a7fbea56c94f2315d72f67
Doston Kamalov <dkamalov@tangem.com>
2023-05-10 11:03:23 +0500

    AND-3527 Change internet connection detecting strategy

--- core/datasource/src/main/java/com/tangem/datasource/di/NetworkConnectionModule.kt
+++ core/datasource/src/main/java/com/tangem/datasource/di/NetworkConnectionModule.kt
@@ -1,7 +1,7 @@
 package com.tangem.datasource.di
 
-import com.tangem.datasource.connection.AndroidNetworkConnectionManager
 import com.tangem.datasource.connection.NetworkConnectionManager
+import com.tangem.datasource.connection.RealInternetConnectionManager
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -15,6 +15,6 @@ internal interface NetworkConnectionModule {
     @Binds
     @Singleton
     fun bindNetworkConnectionManager(
-        androidNetworkConnectionManager: AndroidNetworkConnectionManager,
+        realInternetConnectionManager: RealInternetConnectionManager,
     ): NetworkConnectionManager
 }
