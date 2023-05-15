b81872c8cbe26c0e3d8bde506b1543c61892e1d0
Tangem <git@tangem.com>
2023-05-15 18:14:04 +0300

    Updated on 2026-09-23

--- features/referral/data/src/main/java/com/tangem/feature/referral/di/ReferralRepositoryModule.kt
+++ features/referral/data/src/main/java/com/tangem/feature/referral/di/ReferralRepositoryModule.kt
@@ -5,6 +5,7 @@ import com.tangem.datasource.demo.DemoModeDatasource
 import com.tangem.feature.referral.converters.ReferralConverter
 import com.tangem.feature.referral.data.ReferralRepositoryImpl
 import com.tangem.feature.referral.domain.ReferralRepository
+import com.tangem.lib.auth.AuthProvider
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import dagger.Module
 import dagger.Provides
@@ -23,12 +24,14 @@ class ReferralRepositoryModule {
         referralConverter: ReferralConverter,
         coroutineDispatcherProvider: CoroutineDispatcherProvider,
         demoModeDatasource: DemoModeDatasource,
+        authProvider: AuthProvider,
     ): ReferralRepository {
         return ReferralRepositoryImpl(
             referralApi = tangemTechApi,
             referralConverter = referralConverter,
             coroutineDispatcher = coroutineDispatcherProvider,
             demoModeDatasource = demoModeDatasource,
+            authProvider = authProvider,
         )
     }
 }
