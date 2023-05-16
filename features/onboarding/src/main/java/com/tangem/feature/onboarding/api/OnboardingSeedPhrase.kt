6e0924fa5da4dede467b3b1ceaffc9fca88f1cc2
Tangem <git@tangem.com>
2023-05-16 13:50:36 +0400

    Updated on 2026-09-01

--- features/onboarding/src/main/java/com/tangem/feature/onboarding/api/OnboardingSeedPhrase.kt
+++ features/onboarding/src/main/java/com/tangem/feature/onboarding/api/OnboardingSeedPhrase.kt
@@ -1,12 +1,21 @@
 package com.tangem.feature.onboarding.api
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import com.tangem.core.ui.res.TangemTheme
-import com.tangem.feature.onboarding.presentation.wallet2.ui.*
+import com.tangem.feature.onboarding.presentation.wallet2.ui.AboutSeedPhraseScreen
+import com.tangem.feature.onboarding.presentation.wallet2.ui.CheckSeedPhraseScreen
+import com.tangem.feature.onboarding.presentation.wallet2.ui.ImportSeedPhraseScreen
+import com.tangem.feature.onboarding.presentation.wallet2.ui.IntroScreen
+import com.tangem.feature.onboarding.presentation.wallet2.ui.YourSeedPhraseScreen
 import com.tangem.feature.onboarding.presentation.wallet2.viewmodel.SeedPhraseScreen
 import com.tangem.feature.onboarding.presentation.wallet2.viewmodel.SeedPhraseViewModel
 
@@ -20,7 +29,7 @@ class OnboardingSeedPhrase : OnboardingSeedPhraseApi {
         BackHandler(onBack = viewModel.uiState.onBackClick)
         TangemTheme {
             Column {
-                ProgressIndicator(viewModel.progress, maxProgress)
+                ProgressIndicator(viewModel.progress.collectAsState(0).value, maxProgress)
                 Content(viewModel)
             }
         }
@@ -48,12 +57,13 @@ private fun ProgressIndicator(progress: Int, maxProgress: Int) {
 
 @Composable
 private fun Content(viewModel: SeedPhraseViewModel) {
-    when (viewModel.currentScreen) {
+    when (viewModel.currentScreen.collectAsState().value) {
         SeedPhraseScreen.Intro -> {
             IntroScreen(
                 state = viewModel.uiState.introState,
             )
         }
+
         SeedPhraseScreen.AboutSeedPhrase -> {
             AboutSeedPhraseScreen(
                 state = viewModel.uiState.aboutState,
