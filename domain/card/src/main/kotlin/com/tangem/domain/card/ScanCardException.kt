d3d0ae18ed091b8fe72a46b99cd78cef05c749eb
Tangem <git@tangem.com>
2023-05-17 16:06:36 +0300

    Updated on 2025-12-22

--- domain/card/src/main/kotlin/com/tangem/domain/card/ScanCardException.kt
+++ domain/card/src/main/kotlin/com/tangem/domain/card/ScanCardException.kt
@@ -2,13 +2,13 @@ package com.tangem.domain.card
 
 // TODO: May be add new error types
 sealed class ScanCardException : Exception() {
-    object WrongCardId : ScanCardException()
-
     object UserCancelled : ScanCardException()
 
     object WrongAccessCode : ScanCardException()
 
     open class ChainException : ScanCardException()
 
-    class UnknownException(override val cause: Exception) : ScanCardException()
+    data class UnknownException(override val cause: Exception) : ScanCardException()
+
+    data class WrongCardId(val cardId: String) : ScanCardException()
 }
