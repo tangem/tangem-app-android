8884fea1606a648e2dbe77890298ea4d63928909
Tangem <git@tangem.com>
2023-05-17 16:06:36 +0300

    release/5.26.1

--- app/src/main/java/com/tangem/tap/proxy/redux/DaggerGraphAction.kt
+++ app/src/main/java/com/tangem/tap/proxy/redux/DaggerGraphAction.kt
@@ -1,9 +1,13 @@
 package com.tangem.tap.proxy.redux
 
+import com.tangem.domain.card.ScanCardUseCase
 import com.tangem.features.tester.api.TesterRouter
 import org.rekotlin.Action
 
 sealed interface DaggerGraphAction : Action {
 
-    data class SetActivityDependencies(val testerRouter: TesterRouter) : DaggerGraphAction
+    data class SetActivityDependencies(
+        val testerRouter: TesterRouter,
+        val scanCardUseCase: ScanCardUseCase,
+    ) : DaggerGraphAction
 }
--- domain/card/src/main/kotlin/com/tangem/domain/card/ScanCardException.kt
+++ domain/card/src/main/kotlin/com/tangem/domain/card/ScanCardException.kt
@@ -1,13 +1,13 @@
 package com.tangem.domain.card
 // [REDACTED_TODO_COMMENT]
 sealed class ScanCardException : Exception() {
-    object WrongCardId : ScanCardException()
-
     object UserCancelled : ScanCardException()
 
     object WrongAccessCode : ScanCardException()
 
     open class ChainException : ScanCardException()
 
-    class UnknownException(override val cause: Exception) : ScanCardException()
+    data class UnknownException(override val cause: Exception) : ScanCardException()
+
+    data class WrongCardId(val cardId: String) : ScanCardException()
 }
