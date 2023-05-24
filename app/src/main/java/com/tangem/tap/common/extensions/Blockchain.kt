5a96230f0438a40906cedd998d9d75238f02e984
Tangem <git@tangem.com>
2023-05-24 21:27:50 +0500

    Updated on 2026-09-21

--- app/src/main/java/com/tangem/tap/common/extensions/Blockchain.kt
+++ app/src/main/java/com/tangem/tap/common/extensions/Blockchain.kt
@@ -41,6 +41,8 @@ fun Blockchain.getGreyedOutIconRes(): Int {
         Blockchain.Kava, Blockchain.KavaTestnet -> R.drawable.ic_kava_no_color
         Blockchain.Ravencoin, Blockchain.RavencoinTestnet -> R.drawable.ic_ravencoin_no_color
         Blockchain.Cosmos, Blockchain.CosmosTestnet -> R.drawable.ic_cosmos_no_color
+        Blockchain.TerraV1 -> R.drawable.ic_terra_no_color
+        Blockchain.TerraV2 -> R.drawable.ic_terra2_no_color
         else -> R.drawable.ic_tangem_logo
     }
 }
