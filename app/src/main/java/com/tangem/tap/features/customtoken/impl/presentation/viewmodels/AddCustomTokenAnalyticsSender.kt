0f8553c3056d8f6857f84f7d56884cb14a334cb6
Andrey Khokhlov <ahohlov@tangem.com>
2023-05-26 15:38:58 +0800

    AND-3594, AND-3613, AND-3614 Fix refactoring add custom token screen bugs #2

--- app/src/main/java/com/tangem/tap/features/customtoken/impl/presentation/viewmodels/AddCustomTokenAnalyticsSender.kt
+++ app/src/main/java/com/tangem/tap/features/customtoken/impl/presentation/viewmodels/AddCustomTokenAnalyticsSender.kt
@@ -1,8 +1,8 @@
 package com.tangem.tap.features.customtoken.impl.presentation.viewmodels
 
 import com.tangem.core.analytics.api.AnalyticsEventHandler
+import com.tangem.domain.features.addCustomToken.CustomCurrency
 import com.tangem.tap.common.analytics.events.ManageTokens
-import com.tangem.tap.features.wallet.models.Currency
 
 /** Analytics sender for tokens list screen */
 class AddCustomTokenAnalyticsSender(private val analyticsEventHandler: AnalyticsEventHandler) {
@@ -11,25 +11,7 @@ class AddCustomTokenAnalyticsSender(private val analyticsEventHandler: Analytics
         analyticsEventHandler.send(ManageTokens.CustomToken.ScreenOpened)
     }
 
-    fun sendWhenAddTokenButtonClicked(currency: Currency, address: String) {
-        analyticsEventHandler.send(
-            when (currency) {
-                is Currency.Blockchain -> {
-                    ManageTokens.CustomToken.TokenWasAdded.Blockchain(
-                        derivationPath = currency.derivationPath,
-                        blockchain = currency.blockchain,
-                    )
-                }
-
-                is Currency.Token -> {
-                    ManageTokens.CustomToken.TokenWasAdded.Token(
-                        symbol = currency.currencySymbol,
-                        derivationPath = currency.derivationPath,
-                        blockchain = currency.blockchain,
-                        contractAddress = address,
-                    )
-                }
-            },
-        )
+    fun sendWhenAddTokenButtonClicked(customCurrency: CustomCurrency) {
+        analyticsEventHandler.send(ManageTokens.CustomToken.TokenWasAdded(customCurrency))
     }
 }
