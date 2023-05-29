5977cf56d1a0baa566908f479cdcc59c0c27b13e
Tangem <git@tangem.com>
2023-05-29 17:15:11 +0800

    Updated on 2026-09-23

--- app/src/main/java/com/tangem/tap/features/tokens/impl/presentation/ui/TokensListScreen.kt
+++ app/src/main/java/com/tangem/tap/features/tokens/impl/presentation/ui/TokensListScreen.kt
@@ -5,12 +5,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -141,7 +136,7 @@ private fun TokensListContent(
             item { DifferentAddressesWarning() }
         }
 
-        items(items = tokens, key = TokenItemState::fullName) {
+        items(items = tokens, key = TokenItemState::id) {
             it?.let { TokenItem(model = it) }
         }
     }
