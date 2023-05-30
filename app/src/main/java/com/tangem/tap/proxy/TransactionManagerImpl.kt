f593523b9a56d349f988bbcbd3753cb8cb1c169c
Tangem <git@tangem.com>
2023-05-30 14:44:10 +0300

    Updated on 2026-09-14

--- app/src/main/java/com/tangem/tap/proxy/TransactionManagerImpl.kt
+++ app/src/main/java/com/tangem/tap/proxy/TransactionManagerImpl.kt
@@ -200,9 +200,10 @@ class TransactionManagerImpl(
                 // for not EVM blockchains set gasLimit ZERO for now
                 when (fee.data) {
                     is TransactionFee.Single -> {
+                        val amount = (fee.data as TransactionFee.Single).value
                         val singleFee = ProxyFee(
                             gasLimit = BigInteger.ZERO,
-                            fee = convertToProxyAmount(amount = (fee.data as TransactionFee.Single).value),
+                            fee = convertToProxyAmount(amount = amount),
                         )
                         ProxyFees(
                             minFee = singleFee,
