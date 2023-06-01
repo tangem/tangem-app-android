a98a595a4912a949da63dcc55791e13b14721270
Tangem <git@tangem.com>
2023-06-01 20:10:08 +0800

    Updated on 2026-04-08

---
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/router/DefaultWalletRouter.kt
@@ -0,0 +1,51 @@
+package com.tangem.feature.wallet.presentation.router
+
+import androidx.activity.compose.BackHandler
+import androidx.compose.runtime.Composable
+import androidx.fragment.app.Fragment
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.tangem.core.ui.res.TangemTheme
+import com.tangem.feature.wallet.presentation.WalletFragment
+import com.tangem.feature.wallet.presentation.ui.WalletScreen
+import com.tangem.feature.wallet.presentation.viewmodels.WalletViewModel
+import kotlin.properties.Delegates
+
+/** Default implementation of wallet feature router */
+internal class DefaultWalletRouter : InnerWalletRouter {
+
+    private var navController: NavHostController by Delegates.notNull()
+
+    override fun getEntryFragment(): Fragment = WalletFragment.create()
+
+    @Composable
+    override fun Initialize() {
+        TangemTheme {
+            NavHost(
+                navController = rememberNavController().apply { navController = this },
+                startDestination = WalletScreens.WALLET.name,
+            ) {
+                composable(WalletScreens.WALLET.name) {
+                    val viewModel = hiltViewModel<WalletViewModel>().apply { router = this@DefaultWalletRouter }
+                    WalletScreen(state = viewModel.uiState)
+                }
+
+                composable(WalletScreens.ORGANIZE_TOKENS.name) {
+                    BackHandler(onBack = ::popBackStack)
+                    // TODO: Add OrganizeTokensScreen
+                }
+            }
+        }
+    }
+
+    override fun popBackStack() {
+        navController.popBackStack()
+    }
+
+    override fun openOrganizeTokensScreen() {
+        navController.navigate(WalletScreens.ORGANIZE_TOKENS.name)
+    }
+}
