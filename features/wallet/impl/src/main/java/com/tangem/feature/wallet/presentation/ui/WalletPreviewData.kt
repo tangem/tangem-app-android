3a9d846342c0353785a773f5af714678fccb20fa
Tangem <git@tangem.com>
2023-06-01 17:31:20 +0800

    release/5.26.1

---
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/ui/WalletPreviewData.kt
@@ -0,0 +1,41 @@
+package com.tangem.feature.wallet.presentation.ui
+
+import com.tangem.core.ui.R
+import com.tangem.feature.wallet.presentation.state.WalletCardState
+import java.util.UUID
+
+internal object WalletPreviewData {
+
+    val walletCardContent = WalletCardState.Content(
+        id = UUID.randomUUID().toString(),
+        title = "Wallet 1",
+        balance = "8923,05 $",
+        additionalInfo = "3 cards • Seed enabled",
+        imageResId = R.drawable.ill_businessman_3d,
+        onClick = {},
+    )
+
+    val walletCardLoading = WalletCardState.Loading(
+        id = UUID.randomUUID().toString(),
+        title = "Wallet 1",
+        additionalInfo = "3 cards • Seed enabled",
+        imageResId = R.drawable.ill_businessman_3d,
+        onClick = {},
+    )
+
+    val walletCardHiddenContent = WalletCardState.HiddenContent(
+        id = UUID.randomUUID().toString(),
+        title = "Wallet 1",
+        additionalInfo = "3 cards • Seed enabled",
+        imageResId = R.drawable.ill_businessman_3d,
+        onClick = {},
+    )
+
+    val walletCardError = WalletCardState.Error(
+        id = UUID.randomUUID().toString(),
+        title = "Wallet 1",
+        additionalInfo = "3 cards • Seed enabled",
+        imageResId = R.drawable.ill_businessman_3d,
+        onClick = {},
+    )
+}
