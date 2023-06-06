0c0e3165504ddd263d54747612906a9ae389c71f
Tangem <git@tangem.com>
2023-06-06 14:25:38 +0300

    Updated on 2026-08-07

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/organizetokens/OrganizeTokensViewModel.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/organizetokens/OrganizeTokensViewModel.kt
@@ -5,11 +5,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.tangem.feature.wallet.presentation.common.WalletPreviewData
+import com.tangem.feature.wallet.presentation.organizetokens.OrganizeTokensStateHolder.HeaderConfig
 import com.tangem.feature.wallet.presentation.router.InnerWalletRouter
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.collections.immutable.toPersistentList
 import javax.inject.Inject
 import kotlin.properties.Delegates
 
+// FIXME: Implemented with preview data
 @HiltViewModel
 internal class OrganizeTokensViewModel @Inject constructor() : ViewModel() {
 
@@ -18,5 +21,26 @@ internal class OrganizeTokensViewModel @Inject constructor() : ViewModel() {
     var uiState: OrganizeTokensStateHolder by mutableStateOf(getInitialState())
         private set
 
-    private fun getInitialState(): OrganizeTokensStateHolder = WalletPreviewData.organizeTokensState
+    private fun getInitialState(): OrganizeTokensStateHolder = WalletPreviewData.organizeTokensState.copy(
+        itemsState = OrganizeTokensListState.Ungrouped(
+            items = WalletPreviewData.draggableTokens,
+        ),
+        header = HeaderConfig(
+            onSortByBalanceClick = { /* no-op */ },
+            onGroupByNetworkClick = this::toggleTokensByNetworkGrouping,
+        ),
+    )
+
+    private fun toggleTokensByNetworkGrouping() {
+        val newListState = when (val itemsState = uiState.itemsState) {
+            is OrganizeTokensListState.GroupedByNetwork -> OrganizeTokensListState.Ungrouped(
+                items = itemsState.items.filterIsInstance<DraggableItem.Token>().toPersistentList(),
+            )
+            is OrganizeTokensListState.Ungrouped -> OrganizeTokensListState.GroupedByNetwork(
+                items = WalletPreviewData.draggableItems,
+            )
+        }
+
+        uiState = uiState.copy(itemsState = newListState)
+    }
 }
