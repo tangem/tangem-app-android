ab8588bbc89d6db4aa9d22af73ba1cfea53155c0
Tangem <git@tangem.com>
2023-06-07 14:09:21 +0300

    Updated on 2026-03-27

---
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/common/state/TokenListState.kt
@@ -0,0 +1,35 @@
+package com.tangem.feature.wallet.presentation.common.state
+
+import androidx.compose.runtime.Immutable
+import kotlinx.collections.immutable.ImmutableList
+
+@Immutable
+internal sealed interface TokenListState {
+
+    data class GroupedByNetwork(
+        val groups: ImmutableList<NetworkGroupState>,
+    ) : TokenListState
+
+    data class Ungrouped(
+        val tokens: ImmutableList<TokenItemState>,
+    ) : TokenListState
+}
+
+@Immutable
+internal sealed interface NetworkGroupState {
+    val id: String
+    val networkName: String
+    val tokens: ImmutableList<TokenItemState>
+
+    data class Draggable(
+        override val id: String,
+        override val networkName: String,
+        override val tokens: ImmutableList<TokenItemState.Draggable>,
+    ) : NetworkGroupState
+
+    data class Content(
+        override val id: String,
+        override val networkName: String,
+        override val tokens: ImmutableList<TokenItemState>,
+    ) : NetworkGroupState
+}
