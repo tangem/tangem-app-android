e8c2f0a431e0ac3d2d891a4ee9ee915cb8a2c9a0
Tangem <git@tangem.com>
2023-06-07 14:09:21 +0300

    Updated on 2026-09-21

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/state/TokenUIState.kt
+++
@@ -1,9 +0,0 @@
-package com.tangem.feature.wallet.presentation.state
-
-internal enum class TokenUIState {
-    LOADED, LOADING
-}
-
-internal enum class TokenOptionsUIState {
-    VISIBLE, HIDDEN, DRAG, UNREACHABLE
-}
