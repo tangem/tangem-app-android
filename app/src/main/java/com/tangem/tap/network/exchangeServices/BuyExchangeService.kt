dae1550c9b33f5d2aaff8973c30652f978ced19a
Tangem <git@tangem.com>
2023-06-09 17:11:34 +0500

    Updated on 2026-08-19

--- app/src/main/java/com/tangem/tap/network/exchangeServices/BuyExchangeService.kt
+++ app/src/main/java/com/tangem/tap/network/exchangeServices/BuyExchangeService.kt
@@ -1,36 +1,26 @@
 package com.tangem.tap.network.exchangeServices
 
 import com.tangem.blockchain.common.Blockchain
-import com.tangem.domain.models.scan.ProductType
 import com.tangem.tap.features.wallet.models.Currency
 import com.tangem.tap.network.exchangeServices.mercuryo.MercuryoService
-import com.tangem.tap.network.exchangeServices.utorg.UtorgExchangeService
 import com.tangem.tap.scope
 import kotlinx.coroutines.launch
 
 /**
 [REDACTED_AUTHOR]
  * Temporary wrapper for the buy services. Service switches based on selected product type.
- * Just now - UtorgService used only for the SaltPay cards
  */
 internal class BuyExchangeService(
-    private val productTypeProvider: () -> ProductType?,
     private val mercuryoService: MercuryoService,
-    private val utorgService: UtorgExchangeService,
 ) : ExchangeService, ExchangeUrlBuilder {
 
     init {
         scope.launch {
             mercuryoService.update()
-            utorgService.update()
         }
     }
 
-    private val currentService: ExchangeService
-        get() = when (productTypeProvider.invoke()) {
-            ProductType.SaltPay -> utorgService
-            else -> mercuryoService
-        }
+    private val currentService: ExchangeService = mercuryoService
 
     override suspend fun update() {
         currentService.update()
