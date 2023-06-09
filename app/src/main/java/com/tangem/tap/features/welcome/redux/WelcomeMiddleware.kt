f49d60e65ee489a2696db5098a13ea3e9e90999c
Tangem <git@tangem.com>
2023-06-09 12:58:56 +0300

    Updated on 2026-09-01

--- app/src/main/java/com/tangem/tap/features/welcome/redux/WelcomeMiddleware.kt
+++ app/src/main/java/com/tangem/tap/features/welcome/redux/WelcomeMiddleware.kt
@@ -6,7 +6,6 @@ import com.tangem.common.doOnFailure
 import com.tangem.common.doOnResult
 import com.tangem.common.doOnSuccess
 import com.tangem.common.flatMap
-import com.tangem.common.map
 import com.tangem.domain.models.scan.ScanResponse
 import com.tangem.tap.*
 import com.tangem.tap.common.analytics.events.AnalyticsParam
@@ -63,16 +62,12 @@ internal class WelcomeMiddleware {
 
     private fun disableUserWalletsSaving() = scope.launch {
         userWalletsListManager.clear()
+            .flatMap { walletStoresManager.clear() }
             .flatMap { tangemSdkManager.clearSavedUserCodes() }
-            .map {
-                preferencesStorage.shouldSaveUserWallets = false
-                preferencesStorage.shouldSaveAccessCodes = false
-            }
             .doOnFailure { e ->
                 Timber.e(e, "Unable to clear user wallets")
             }
             .doOnResult {
-                // !!! Workaround !!!
                 store.dispatchWithMain(WelcomeAction.CloseError)
                 store.dispatchWithMain(NavigationAction.PopBackTo(AppScreen.Home))
             }
