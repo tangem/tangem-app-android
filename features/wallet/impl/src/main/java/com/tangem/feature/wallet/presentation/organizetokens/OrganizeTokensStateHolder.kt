cb007f29c45cdbfd9a61d460c6969c29142c65b2
iiiburnyiii <iiiburnyiii@gmail.com>
2023-06-09 10:42:22 +0300

    AND-3654 Implement drag-n-drop sorting on organize tokens screen

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/organizetokens/OrganizeTokensStateHolder.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/organizetokens/OrganizeTokensStateHolder.kt
@@ -2,12 +2,14 @@ package com.tangem.feature.wallet.presentation.organizetokens
 
 import androidx.compose.runtime.Immutable
 import com.tangem.feature.wallet.presentation.common.state.TokenItemState
-import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.PersistentList
 import kotlinx.collections.immutable.toPersistentList
+import org.burnoutcrew.reorderable.ItemPosition
 
 internal data class OrganizeTokensStateHolder(
     val header: HeaderConfig,
     val itemsState: OrganizeTokensListState,
+    val dragConfig: DragConfig,
     val actions: ActionsConfig,
 ) {
 
@@ -20,44 +22,79 @@ internal data class OrganizeTokensStateHolder(
         val onApplyClick: () -> Unit,
         val onCancelClick: () -> Unit,
     )
+
+    data class DragConfig(
+        val onItemDragged: (from: ItemPosition, to: ItemPosition) -> Unit,
+        val canDragItemOver: (dragOver: ItemPosition, dragging: ItemPosition) -> Boolean,
+        val onItemDragEnd: () -> Unit,
+        val onDragStart: (item: DraggableItem) -> Unit,
+    )
 }
 
 @Immutable
-internal sealed class OrganizeTokensListState {
-    abstract val items: ImmutableList<DraggableItem>
+internal sealed interface OrganizeTokensListState {
+    val items: PersistentList<DraggableItem>
 
     data class GroupedByNetwork(
-        override val items: ImmutableList<DraggableItem>,
-    ) : OrganizeTokensListState()
+        override val items: PersistentList<DraggableItem>,
+    ) : OrganizeTokensListState
 
     data class Ungrouped(
-        override val items: ImmutableList<DraggableItem.Token>,
-    ) : OrganizeTokensListState()
+        override val items: PersistentList<DraggableItem.Token>,
+    ) : OrganizeTokensListState
 
     @Suppress("UNCHECKED_CAST")
-    inline fun updateItems(update: (List<DraggableItem>) -> List<DraggableItem>): OrganizeTokensListState {
+    fun updateItems(update: (PersistentList<DraggableItem>) -> List<DraggableItem>): OrganizeTokensListState {
         val updatedItems = update(this.items).toPersistentList()
 
         return when (this) {
             is GroupedByNetwork -> this.copy(items = updatedItems)
-            is Ungrouped -> this.copy(items = updatedItems as ImmutableList<DraggableItem.Token>)
+            is Ungrouped -> this.copy(items = updatedItems as PersistentList<DraggableItem.Token>)
         }
     }
 }
 
+/**
+ * Helper class for the DND list items
+ *
+ * @property id ID of the item
+ * */
 @Immutable
 internal sealed interface DraggableItem {
     val id: String
 
+    /**
+     * Item for network group header.
+     *
+     * @property id ID of the network group
+     * @property networkName network group name
+     * */
     data class GroupHeader(
         override val id: String,
         val networkName: String,
     ) : DraggableItem
 
+    /**
+     * Item for token.
+     *
+     * @property tokenItemState state of the token item
+     * @property groupId ID of the network group which contains this token
+     * @property id ID of the token
+     * */
     data class Token(
         val tokenItemState: TokenItemState.Draggable,
         val groupId: String,
     ) : DraggableItem {
         override val id: String = tokenItemState.id
     }
+
+    /**
+     * Helper item used to detect possible positions where a network group can be placed.
+     * Used only on [OrganizeTokensListState.GroupedByNetwork] and placed between network groups.
+     *
+     * @property id ID of the placeholder
+     * */
+    data class GroupPlaceholder(
+        override val id: String,
+    ) : DraggableItem
 }
