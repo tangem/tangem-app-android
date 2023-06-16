a7c88b7d7362c3fdf0a65d665866b4fbcc20f151
Tangem <git@tangem.com>
2023-06-16 23:56:26 +0800

    Updated on 2026-09-21

--- app/src/main/java/com/tangem/tap/features/send/ui/EditTextCustomPaste.kt
+++ app/src/main/java/com/tangem/tap/features/send/ui/EditTextCustomPaste.kt
@@ -5,13 +5,15 @@ import android.content.Context
 import android.util.AttributeSet
 import com.google.android.material.textfield.TextInputEditText
 
-class EditTextCustomPaste @JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0,
-) : TextInputEditText(context, attrs, defStyleAttr) {
+class EditTextCustomPaste : TextInputEditText {
 
     private var onSystemPasteButtonClickListener: (() -> Unit)? = null
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
     fun setOnSystemPasteButtonClickListener(callback: () -> Unit) {
         onSystemPasteButtonClickListener = callback
