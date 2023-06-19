42626e86a24a8561e116c2c1e1d53aa77cd6ef31
Tangem <git@tangem.com>
2023-06-19 08:51:46 +0300

    Updated on 2026-06-12

---
+++ features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/domain/api/Learn2earnInteractor.kt
@@ -0,0 +1,18 @@
+package com.tangem.feature.learn2earn.domain.api
+
+import android.net.Uri
+import com.tangem.feature.learn2earn.domain.models.RedirectConsequences
+
+/**
+[REDACTED_AUTHOR]
+ */
+interface Learn2earnInteractor {
+
+    suspend fun init()
+    fun isNeedToShowViewOnStoriesScreen(): Boolean
+    suspend fun isNeedToShowViewOnWalletScreen(walletId: String): Boolean
+    fun getBasicAuthHeaders(): Map<String, String>
+    fun buildUriForStories(): Uri
+    fun buildUriForMainPage(walletId: String, cardId: String, cardPubKey: String): Uri
+    fun handleWebViewRedirect(uri: Uri): RedirectConsequences
+}
