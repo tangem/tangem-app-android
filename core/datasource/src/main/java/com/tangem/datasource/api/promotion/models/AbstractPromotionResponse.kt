5e8a4b9b70ba05b25c864162e16e69b43eb385c9
Tangem <git@tangem.com>
2023-06-19 08:47:01 +0300

    Updated on 2026-06-01

---
+++ core/datasource/src/main/java/com/tangem/datasource/api/promotion/models/AbstractPromotionResponse.kt
@@ -0,0 +1,20 @@
+package com.tangem.datasource.api.promotion.models
+
+import com.squareup.moshi.Json
+
+/**
+[REDACTED_AUTHOR]
+ */
+abstract class AbstractPromotionResponse {
+
+    abstract val error: Error?
+
+    fun isError(): Boolean = error != null
+
+    data class Error(
+        @Json(name = "code") val code: Int,
+        @Json(name = "description") val description: String,
+    ) {
+        companion object
+    }
+}
