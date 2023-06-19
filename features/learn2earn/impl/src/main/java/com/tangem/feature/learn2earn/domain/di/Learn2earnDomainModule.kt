60b223f5b7ea1ff03d9153d6d10069ebbe775813
Tangem <git@tangem.com>
2023-06-19 08:51:46 +0300

    Updated on 2026-07-01

---
+++ features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/domain/di/Learn2earnDomainModule.kt
@@ -0,0 +1,39 @@
+package com.tangem.feature.learn2earn.domain.di
+
+import android.content.Context
+import com.tangem.feature.learn2earn.data.api.Learn2earnRepository
+import com.tangem.feature.learn2earn.domain.DefaultLearn2earnInteractor
+import com.tangem.feature.learn2earn.domain.api.Learn2earnInteractor
+import com.tangem.feature.learn2earn.presentation.Learn2earnRouter
+import com.tangem.lib.auth.BasicAuthProvider
+import com.tangem.utils.extensions.toWeakReference
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class Learn2earnDomainModule {
+
+    @Provides
+    @Singleton
+    fun provideRouter(@ApplicationContext context: Context): Learn2earnRouter {
+        return Learn2earnRouter(context.toWeakReference())
+    }
+
+    @Provides
+    @Singleton
+    fun provideInteractor(
+        repository: Learn2earnRepository,
+        basicAuthProvider: BasicAuthProvider,
+    ): Learn2earnInteractor {
+        return DefaultLearn2earnInteractor(
+            repository = repository,
+            basicAuthProvider = basicAuthProvider,
+            userCountryCodeProvider = { "ru" },
+        )
+    }
+}
