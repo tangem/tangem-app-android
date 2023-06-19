eddbd44cea19aa3325cb00f5179dc30e45e45142
Tangem <git@tangem.com>
2023-06-19 08:50:47 +0300

    Updated on 2026-04-15

--- features/learn2earn/impl/build.gradle.kts
+++ features/learn2earn/impl/build.gradle.kts
@@ -8,37 +8,44 @@ plugins {
 }
 
 dependencies {
-    /** AndroidX */
-    implementation(deps.androidx.activity.compose)
-    implementation(deps.androidx.core.ktx)
-    implementation(deps.androidx.appCompat)
-    implementation(deps.androidx.constraintLayout)
-
-    /** Compose */
-    implementation(deps.compose.material)
-    implementation(deps.compose.foundation)
-    implementation(deps.compose.constraintLayout)
-    implementation(deps.compose.ui)
-    implementation(deps.compose.ui.tooling)
-    implementation(deps.compose.animation)
-    implementation(deps.compose.navigation)
-    implementation(deps.compose.navigation.hilt)
-
     /** Core modules */
     implementation(project(":common"))
     implementation(project(":core:featuretoggles"))
     implementation(project(":core:datasource"))
-    implementation(project(":core:analytics"))
     implementation(project(":core:utils"))
     implementation(project(":core:ui"))
     implementation(project(":core:res"))
+    implementation(project(":data:source:preferences"))
+    implementation(project(":libs:auth"))
+
+    implementation(deps.material)
+
+    /** AndroidX */
+    implementation(deps.androidx.core.ktx)
+    implementation(deps.androidx.appCompat)
+    implementation(deps.androidx.activity.compose)
+    implementation(deps.androidx.constraintLayout)
+    implementation(deps.androidx.browser)
+
+    /** Compose */
+    implementation(deps.compose.material)
+    implementation(deps.compose.foundation)
+    implementation(deps.compose.ui)
+    implementation(deps.compose.ui.tooling)
 
     /** Tangem libraries */
-    // TODO: fixme: delete if later it doesn't needed
+    // TODO: 1inch: delete if later it doesn't needed
     // implementation(deps.tangem.card.core)
     // implementation(deps.tangem.card.android) {
     //     exclude(module = "joda-time")
     // }
+
+    /** Network */
+    implementation(deps.krateSharedPref)
+    implementation(deps.moshi)
+    implementation(deps.moshi.kotlin)
+    implementation(deps.retrofit)
+    implementation(deps.retrofit.moshi)
 
     /** Other libraries */
     implementation(deps.kotlin.immutable.collections)
