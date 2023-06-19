4dccd47b84b8eadf78c8604f35adfb254fef89fb
Tangem <git@tangem.com>
2023-06-19 08:51:46 +0300

    Updated on 2026-03-27

---
+++ features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/domain/models/CardType.kt
@@ -0,0 +1,9 @@
+package com.tangem.feature.learn2earn.domain.models
+
+/**
+[REDACTED_AUTHOR]
+ */
+enum class CardType(val typeName: String) {
+    NEW("new-card"),
+    EXISTED("existed-card"),
+}
