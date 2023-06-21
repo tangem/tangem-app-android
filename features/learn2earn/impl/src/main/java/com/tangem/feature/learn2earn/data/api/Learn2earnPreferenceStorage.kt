e15b95ec4cabdc142e1bc74fa4924e10049ebea4
Tangem <git@tangem.com>
2023-06-21 13:25:40 +0300

    Updated on 2026-08-07

--- features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/data/api/Learn2earnPreferenceStorage.kt
+++ features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/data/api/Learn2earnPreferenceStorage.kt
@@ -4,7 +4,10 @@ package com.tangem.feature.learn2earn.data.api
 [REDACTED_AUTHOR]
  */
 interface Learn2earnPreferenceStorage {
+
     var promoCode: String?
+
     var promotionInfo: String?
+
     var alreadyReceivedAward: Boolean
 }
