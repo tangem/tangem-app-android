c2c4d459ba579fdcf488d5440a6771b1e43e79ef
Tangem <git@tangem.com>
2023-06-21 13:25:40 +0300

    Updated on 2026-06-17

--- core/datasource/src/main/java/com/tangem/datasource/api/promotion/PromotionApi.kt
+++ core/datasource/src/main/java/com/tangem/datasource/api/promotion/PromotionApi.kt
@@ -4,35 +4,26 @@ import com.tangem.datasource.api.promotion.models.*
 import retrofit2.http.*
 
 /**
+ *
+ * Promotion API
+ * @see <a href = "https://www.notion.so/tangem/Promotion-Program-API-0907159c3fdb4975aac761be632f44da">Documentation<a/>
 [REDACTED_AUTHOR]
- * Api details
- * https://www.notion.so/tangem/Promotion-Program-API-0907159c3fdb4975aac761be632f44da
  */
 interface PromotionApi {
 
-    
-    @Headers("Cache-Control: max-age=$CACHE_CONTROL_SECONDS")
+    @Headers("Cache-Control: max-age=3600")
     @GET("promotion")
     suspend fun getPromotionInfo(@Query("programName") name: String): PromotionInfoResponse
 
-    
     @POST("promotion/code/validate")
-    suspend fun codeValidate(@Body request: CodeValidateRequestBody): CodeValidateResponse
+    suspend fun validateCode(@Body request: CodeValidateRequestBody): CodeValidateResponse
 
-    
     @POST("promotion/code/award")
-    suspend fun codeAward(@Body request: CodeAwardRequestBody): CodeAwardResponse
+    suspend fun requestAwardByCode(@Body request: CodeAwardRequestBody): CodeAwardResponse
 
-    
     @POST("promotion/validate")
     suspend fun validate(@Body request: ValidateRequestBody): ValidateResponse
 
-    
     @GET("promotion/award")
-    suspend fun award(@Body request: AwardRequestBody): AwardResponse
-
-    companion object {
-        const val ONE_INCH_TIMEOUT_MS = 5000L
-        const val CACHE_CONTROL_SECONDS = 3600
-    }
+    suspend fun requestAward(@Body request: AwardRequestBody): AwardResponse
 }
