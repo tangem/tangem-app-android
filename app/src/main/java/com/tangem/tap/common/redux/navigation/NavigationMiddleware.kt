f28c5049c603789eec3bde65092d03699a90bceb
Anton Zhilenkov <azhilenkov@tangem.com>
2023-06-21 10:24:57 +0300

    AND-3818 block popUp action to the Wallet screen if a user already navigate back from WalletSelector, SaveWallet screens

--- app/src/main/java/com/tangem/tap/common/redux/navigation/NavigationMiddleware.kt
+++ app/src/main/java/com/tangem/tap/common/redux/navigation/NavigationMiddleware.kt
@@ -12,80 +12,84 @@ import com.tangem.tap.common.extensions.popBackTo
 import com.tangem.tap.common.extensions.shareText
 import com.tangem.tap.common.redux.AppState
 import com.tangem.tap.store
+import org.rekotlin.Action
 import org.rekotlin.Middleware
+import timber.log.Timber
 
 val navigationMiddleware: Middleware<AppState> = { _, state ->
     { next ->
         { action ->
-            if (action is NavigationAction) {
-                val navState = state()?.navigationState
-                when (action) {
-                    is NavigationAction.NavigateTo -> {
-                        navState?.activity?.get()?.openFragment(
-                            screen = action.screen,
-                            addToBackstack = action.addToBackstack,
-                            fgShareTransition = action.fragmentShareTransition,
-                            bundle = action.bundle,
-                        )
-                    }
-                    is NavigationAction.PopBackTo -> {
-                        if (navState?.backStack?.lastOrNull() != action.screen) {
-                            when (val screen = action.screen) {
-                                AppScreen.Home,
-                                AppScreen.Welcome,
-                                -> {
-                                    if (navState?.backStack?.contains(screen) == false) {
-                                        // Pop back to activity
-                                        navState.activity?.get()?.popBackTo(screen = null, inclusive = true)
-                                        store.dispatchOnMain(NavigationAction.NavigateTo(screen))
-                                    } else {
-                                        navState?.activity?.get()?.popBackTo(screen, action.inclusive)
-                                    }
-                                }
-                                else -> {
-                                    navState?.activity?.get()?.popBackTo(screen, action.inclusive)
-                                }
-                            }
-                        }
-                    }
-                    is NavigationAction.OpenUrl -> {
-                        navState?.activity?.get()?.let {
-                            CustomTabsManager().openUrl(action.url, it)
-                        }
-                    }
-                    is NavigationAction.OpenDocument -> {
-                        val intent = Intent(Intent.ACTION_VIEW)
-                        intent.data = action.url
-                        navState?.activity?.get()?.startActivity(intent)
-                    }
-                    is NavigationAction.OpenBiometricsSettings -> {
-                        val settingsAction = when {
-                            Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> {
-                                Settings.ACTION_BIOMETRIC_ENROLL
-                            }
-                            else -> {
-                                Settings.ACTION_SECURITY_SETTINGS
-                            }
-                        }
-                        val intent = Intent(settingsAction).apply {
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                                putExtra(
-                                    Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
-                                    BiometricManager.Authenticators.BIOMETRIC_STRONG,
-                                )
-                            }
-                        }
-                        activityResultCaller.activityResultLauncher?.launch(intent)
-                    }
-                    is NavigationAction.Share -> {
-                        navState?.activity?.get()?.shareText(action.data)
-                    }
-                    is NavigationAction.ActivityCreated,
-                    is NavigationAction.ActivityDestroyed,
-                    -> Unit
-                }
-            }
+            handleNavigation(action, state)
             next(action)
         }
+    }
+}
+
+private fun handleNavigation(navAction: Action, state: () -> AppState?) {
+    val action = navAction as? NavigationAction ?: return
+    val navState = state()?.navigationState ?: return
+
+    when (action) {
+        is NavigationAction.NavigateTo -> {
+            navState.activity?.get()?.openFragment(
+                screen = action.screen,
+                addToBackstack = action.addToBackstack,
+                fgShareTransition = action.fragmentShareTransition,
+                bundle = action.bundle,
+            )
+        }
+        is NavigationAction.PopBackTo -> {
+            val lastFromBackStack = navState.backStack.lastOrNull()
+            if (lastFromBackStack == action.screen) return
+            if (action.popOnlyFrom != null && action.popOnlyFrom != lastFromBackStack) {
+                Timber.w("PopBackTo action was blocked")
+                return
+            }
+
+            val activity = navState.activity?.get()
+            when (val screen = action.screen) {
+                AppScreen.Home, AppScreen.Welcome -> {
+                    if (!navState.backStack.contains(screen)) {
+                        // Pop back to activity
+                        activity?.popBackTo(screen = null, inclusive = true)
+                        store.dispatchOnMain(NavigationAction.NavigateTo(screen))
+                    } else {
+                        activity?.popBackTo(screen, action.inclusive)
+                    }
+                }
+                else -> {
+                    activity?.popBackTo(screen, action.inclusive)
+                }
+            }
+        }
+        is NavigationAction.OpenUrl -> {
+            navState.activity?.get()?.let {
+                CustomTabsManager().openUrl(action.url, it)
+            }
+        }
+        is NavigationAction.OpenDocument -> {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = action.url
+            navState.activity?.get()?.startActivity(intent)
+        }
+        is NavigationAction.OpenBiometricsSettings -> {
+            val settingsAction = when {
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> Settings.ACTION_BIOMETRIC_ENROLL
+                else -> Settings.ACTION_SECURITY_SETTINGS
+            }
+            val intent = Intent(settingsAction).apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    putExtra(
+                        Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                        BiometricManager.Authenticators.BIOMETRIC_STRONG,
+                    )
+                }
+            }
+            activityResultCaller.activityResultLauncher?.launch(intent)
+        }
+        is NavigationAction.Share -> {
+            navState.activity?.get()?.shareText(action.data)
+        }
+        else -> Unit
     }
 }
