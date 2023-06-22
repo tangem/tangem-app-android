44cd64e2d02a456b88db57b10681a1a7faba2f5b
Tangem <git@tangem.com>
2023-06-22 12:21:57 +0300

    Updated on 2026-02-06

--- app/src/main/java/com/tangem/tap/features/customtoken/impl/presentation/ui/AddCustomTokenScreen.kt
+++ app/src/main/java/com/tangem/tap/features/customtoken/impl/presentation/ui/AddCustomTokenScreen.kt
@@ -1,6 +1,7 @@
 package com.tangem.tap.features.customtoken.impl.presentation.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.datasource.CollectionPreviewParameterProvider
@@ -15,10 +16,10 @@ import com.tangem.tap.features.customtoken.impl.presentation.states.AddCustomTok
 [REDACTED_AUTHOR]
  */
 @Composable
-internal fun AddCustomTokenScreen(stateHolder: AddCustomTokenStateHolder) {
+internal fun AddCustomTokenScreen(stateHolder: AddCustomTokenStateHolder, modifier: Modifier = Modifier) {
     when (stateHolder) {
-        is AddCustomTokenStateHolder.Content -> AddCustomTokenContent(state = stateHolder)
-        is AddCustomTokenStateHolder.TestContent -> AddCustomTokenTestContent(state = stateHolder)
+        is AddCustomTokenStateHolder.Content -> AddCustomTokenContent(stateHolder, modifier)
+        is AddCustomTokenStateHolder.TestContent -> AddCustomTokenTestContent(stateHolder, modifier)
     }
 }
 
