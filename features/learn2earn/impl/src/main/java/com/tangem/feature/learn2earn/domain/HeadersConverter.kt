cc1b155b12ef9df716b2adc1a83430bae2c28f5d
Tangem <git@tangem.com>
2023-06-26 19:53:17 +0300

    Updated on 2026-05-14

---
+++ features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/domain/HeadersConverter.kt
@@ -0,0 +1,25 @@
+package com.tangem.feature.learn2earn.domain
+
+import com.tangem.utils.converter.TwoWayConverter
+
+/**
+ * Converter which helps prepare a list of headers to put it into the Intent and extract them later.
+[REDACTED_AUTHOR]
+ */
+internal class HeadersConverter : TwoWayConverter<Map<String, String>, ArrayList<String>> {
+
+    override fun convert(value: Map<String, String>): ArrayList<String> {
+        val list = value.toList().map {
+            "${it.first}$SPLIT_SIGN${it.second}"
+        }
+        return ArrayList(list)
+    }
+
+    override fun convertBack(value: ArrayList<String>): Map<String, String> {
+        return value.map { it.split(SPLIT_SIGN) }.associate { it[0] to it[1] }
+    }
+
+    private companion object {
+        const val SPLIT_SIGN = ":"
+    }
+}
