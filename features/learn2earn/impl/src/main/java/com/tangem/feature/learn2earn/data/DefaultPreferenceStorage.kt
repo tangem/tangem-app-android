76303ff2b46e4ac1b2046df1d4b99b93cefb1483
Tangem <git@tangem.com>
2023-06-26 19:41:43 +0300

    Updated on 2026-09-25

--- features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/data/DefaultPreferenceStorage.kt
+++ features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/data/DefaultPreferenceStorage.kt
@@ -3,7 +3,6 @@ package com.tangem.feature.learn2earn.data
 import android.content.Context
 import com.tangem.feature.learn2earn.data.api.Learn2earnPreferenceStorage
 import hu.autsoft.krate.SimpleKrate
-import hu.autsoft.krate.booleanPref
 import hu.autsoft.krate.default.withDefault
 import hu.autsoft.krate.stringPref
 
@@ -14,9 +13,7 @@ internal class DefaultPreferenceStorage(
     context: Context,
 ) : SimpleKrate(context = context, name = "Lear2earnPromotion"), Learn2earnPreferenceStorage {
 
-    override var promoCode: String? by stringPref().withDefault(null)
-
     override var promotionInfo: String? by stringPref().withDefault(null)
 
-    override var alreadyReceivedAward: Boolean by booleanPref().withDefault(false)
+    override var userData: String? by stringPref().withDefault(null)
 }
