f4b46e39698c111ef1e93ce1aed6d7b1da5a7f38
Tangem <git@tangem.com>
2023-06-26 20:00:37 +0300

    Updated on 2026-05-07

--- features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/presentation/webView/Learn2earnWebViewActivity.kt
+++ features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/presentation/webView/Learn2earnWebViewActivity.kt
@@ -1,15 +1,11 @@
-package com.tangem.feature.learn2earn.presentation
+package com.tangem.feature.learn2earn.presentation.webView
 
 import android.os.Bundle
 import android.view.MenuItem
-import android.webkit.WebResourceRequest
 import android.webkit.WebView
-import android.webkit.WebViewClient
 import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
-import com.tangem.feature.learn2earn.domain.models.RedirectConsequences
-import com.tangem.feature.learn2earn.domain.models.WebViewHelper
 import com.tangem.feature.learn2earn.impl.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,7 +15,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class Learn2earnWebViewActivity : AppCompatActivity() {
 
-    private val learn2earnViewModel by viewModels<Learn2earnViewModel>()
+    private val viewModel by viewModels<WebViewViewModel>()
 
     private lateinit var actionBar: ActionBar
     private lateinit var webView: WebView
@@ -28,24 +24,27 @@ class Learn2earnWebViewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
 
+        val webViewData = viewModel.extractData(intent)
+
         setSupportActionBar(findViewById(R.id.toolbar))
         actionBar = supportActionBar!!
         actionBar.setDisplayHomeAsUpEnabled(true)
         actionBar.setDisplayShowHomeEnabled(true)
-        actionBar.title = learn2earnViewModel.webViewUri.authority
-
+        actionBar.title = webViewData.uri.authority
         webView = findViewById(R.id.web_view)
+
         webView.settings.javaScriptEnabled = true
         webView.settings.domStorageEnabled = true
         webView.settings.loadsImagesAutomatically = true
 
         webView.webViewClient = Learn2earnWebViewClient(
-            helper = learn2earnViewModel,
+            helper = viewModel,
+            headers = webViewData.headers,
             finishSessionHandler = { finish() },
         )
         webView.loadUrl(
-            learn2earnViewModel.webViewUri.toString(),
-            learn2earnViewModel.getWebViewHeaders(),
+            webViewData.uri.toString(),
+            webViewData.headers,
         )
     }
 
@@ -66,25 +65,9 @@ class Learn2earnWebViewActivity : AppCompatActivity() {
 
         super.onDestroy()
     }
-}
 
-private class Learn2earnWebViewClient(
-    private val helper: WebViewHelper,
-    private val finishSessionHandler: () -> Unit,
-) : WebViewClient() {
-
-    override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
-        when (helper.handleWebViewRedirect(request.url)) {
-            RedirectConsequences.NOTHING -> Unit
-            RedirectConsequences.PROCEED -> {
-                val headers = helper.getWebViewHeaders().toMutableMap()
-                request.requestHeaders?.let { headers.putAll(it) }
-                view.loadUrl(request.url.toString(), headers)
-            }
-            RedirectConsequences.FINISH_SESSION -> {
-                finishSessionHandler.invoke()
-            }
-        }
-        return true
+    companion object {
+        const val EXTRA_WEB_URI = "webViewUri"
+        const val EXTRA_WEB_HEADERS = "webViewHeaders"
     }
 }
