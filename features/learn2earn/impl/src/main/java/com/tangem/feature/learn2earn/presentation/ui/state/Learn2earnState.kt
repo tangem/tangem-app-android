61f16e68d805e5ad5a2e2597b884e5f952e7cc46
Tangem <git@tangem.com>
2023-06-26 20:00:37 +0300

    release/5.27.0

---
+++ features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/presentation/ui/state/Learn2earnState.kt
@@ -0,0 +1,68 @@
+package com.tangem.feature.learn2earn.presentation.ui.state
+
+import com.tangem.core.ui.extensions.TextReference
+import com.tangem.feature.learn2earn.domain.models.PromotionError
+import com.tangem.feature.learn2earn.impl.R
+
+/**
+[REDACTED_AUTHOR]
+ */
+data class Learn2earnState(
+    val storyScreenState: StoryScreenState,
+    val mainScreenState: MainScreenState,
+) {
+    companion object
+}
+
+data class StoryScreenState(
+    val isVisible: Boolean,
+    val onClick: () -> Unit,
+)
+
+data class MainScreenState(
+    val isVisible: Boolean,
+    val onClick: () -> Unit,
+    val description: Description,
+    val showProgress: Boolean,
+    val logoAlphaValue: Float = 1f,
+    val dialog: Dialog? = null,
+) {
+
+    sealed class Description(val title: TextReference, val subtitle: TextReference) {
+
+        class Learn(awardAmount: String) : Description(
+            TextReference.Res(R.string.main_learn_title),
+            TextReference.Res(R.string.main_learn_subtitle, listOf(awardAmount)),
+        )
+
+        object GetBonus : Description(
+            TextReference.Res(R.string.main_get_bonus_title),
+            TextReference.Res(R.string.main_get_bonus_subtitle),
+        )
+    }
+
+    sealed class Dialog {
+
+        data class Claimed(
+            val onOk: () -> Unit,
+            val onDismissRequest: () -> Unit,
+        ) : Dialog()
+
+        data class PromoCodeNotRegistered(
+            val onOk: () -> Unit,
+            val onCancel: () -> Unit,
+            val onDismissRequest: () -> Unit,
+        ) : Dialog()
+
+        data class Error(
+            val error: PromotionError,
+            val onOk: () -> Unit,
+            val onDismissRequest: () -> Unit,
+        ) : Dialog()
+    }
+}
+
+class UiActions(
+    val buttonStoryClick: () -> Unit,
+    val buttonMainClick: () -> Unit,
+)
