35608949c34ee769fe6bdf332b72e00a04cb31ec
Tangem <git@tangem.com>
2023-06-26 19:41:43 +0300

    release/5.27.0

--- features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/data/api/UsedCardsPreferenceStorage.kt
+++
@@ -1,9 +0,0 @@
-package com.tangem.feature.learn2earn.data.api
-
-/**
-[REDACTED_AUTHOR]
- * An interface that encapsulates access to deprecated PreferencesDataSource.usedCardsPrefStorage.
- */
-interface UsedCardsPreferenceStorage {
-    fun isHadActivatedCards(): Boolean
-}
