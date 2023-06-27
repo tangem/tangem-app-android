3dd9e8d9e8cd79d11e06e03d1b49126a2312e760
Tangem <git@tangem.com>
2023-06-27 11:36:30 +0300

    Updated on 2026-08-27

--- features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/presentation/ui/Learn2earnMainPageScreen.kt
+++ features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/presentation/ui/Learn2earnMainPageScreen.kt
@@ -4,18 +4,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.tangem.core.ui.res.TangemTheme
-import com.tangem.feature.learn2earn.presentation.Learn2earnViewModel
+import com.tangem.feature.learn2earn.presentation.ui.state.Learn2earnState
 
 /**
 [REDACTED_AUTHOR]
  */
 @Composable
-fun Learn2earnMainPageScreen(viewModel: Learn2earnViewModel, modifier: Modifier = Modifier) {
+fun Learn2earnMainPageScreen(state: Learn2earnState, modifier: Modifier = Modifier) {
     GetBonusView(
-        state = viewModel.uiState.mainScreenState,
+        state = state.mainScreenState,
         modifier = modifier
             .padding(horizontal = TangemTheme.dimens.size16)
             .padding(vertical = TangemTheme.dimens.size8),
     )
-    Learn2earnDialogs(viewModel.uiState.mainScreenState.dialog)
+    Learn2earnDialogs(state.mainScreenState.dialog)
 }
