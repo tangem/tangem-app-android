82793a89609d99a346990864124863581e10e7b9
Tangem <git@tangem.com>
2023-06-28 14:21:48 +0300

    Updated on 2026-06-05

--- core/datasource/src/main/java/com/tangem/datasource/utils/RequestHeader.kt
+++ core/datasource/src/main/java/com/tangem/datasource/utils/RequestHeader.kt
@@ -7,24 +7,14 @@ import com.tangem.lib.auth.AuthProvider
  *
  * @param pairs header name and header value pairs
  */
-sealed class RequestHeader(vararg pairs: Pair<String, String>) {
+sealed class RequestHeader(vararg pairs: Pair<String, () -> String>) {
 
     /** Header list */
-    val values: List<Pair<String, String>> = pairs.toList()
-
-    object CacheControlHeader : RequestHeader("Cache-Control" to "max-age=600")
-
-    class AuthenticationHeader(authProvider: AuthProvider) : RequestHeader(
-        "card_id" to authProvider.getCardId(),
-        "card_public_key" to authProvider.getCardPublicKey(),
-    )
-}
-
-sealed class LazyRequestHeader(vararg pairs: Pair<String, () -> String>) {
-
     val values: List<Pair<String, () -> String>> = pairs.toList()
 
-    class LazyAuthenticationHeader(authProvider: AuthProvider) : LazyRequestHeader(
+    object CacheControlHeader : RequestHeader("Cache-Control" to { "max-age=600" })
+
+    class AuthenticationHeader(authProvider: AuthProvider) : RequestHeader(
         "card_id" to { authProvider.getCardId() },
         "card_public_key" to { authProvider.getCardPublicKey() },
     )
