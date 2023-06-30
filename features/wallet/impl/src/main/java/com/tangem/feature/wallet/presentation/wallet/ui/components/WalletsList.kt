5ce45072537283e33615f8c6bacec3edc4b5be95
Tangem <git@tangem.com>
2023-06-30 18:58:27 +0800

    Updated on 2026-09-23

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/components/WalletsList.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/components/WalletsList.kt
@@ -1,15 +1,17 @@
 package com.tangem.feature.wallet.presentation.wallet.ui.components
 
-import androidx.compose.animation.core.*
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,20 +19,24 @@ import androidx.compose.ui.unit.dp
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.feature.wallet.presentation.common.WalletPreviewData
 import com.tangem.feature.wallet.presentation.wallet.state.WalletCardState
-import kotlinx.collections.immutable.ImmutableList
-import kotlinx.collections.immutable.persistentListOf
+import com.tangem.feature.wallet.presentation.wallet.state.WalletsListConfig
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.flow.FlowCollector
+
+private const val MIN_OFFSET = -2
+private const val MAX_OFFSET = 2
 
 /**
- * Wallets list
+ * Wallets list component
  *
- * @param wallets  list of wallet state
+ * @param config   config
  * @param modifier modifier
  *
 [REDACTED_AUTHOR]
  */
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, InternalCoroutinesApi::class)
 @Composable
-internal fun WalletCardsList(wallets: ImmutableList<WalletCardState>, modifier: Modifier = Modifier) {
+internal fun WalletsList(config: WalletsListConfig, modifier: Modifier = Modifier) {
     val horizontalCardPadding = TangemTheme.dimens.spacing16
     val itemWidth = LocalConfiguration.current.screenWidthDp.dp - horizontalCardPadding * 2
 
@@ -38,12 +44,27 @@ internal fun WalletCardsList(wallets: ImmutableList<WalletCardState>, modifier: 
     LazyRow(
         modifier = modifier.background(color = TangemTheme.colors.background.secondary),
         state = lazyListState,
+        contentPadding = PaddingValues(horizontal = TangemTheme.dimens.spacing16),
         horizontalArrangement = Arrangement.spacedBy(TangemTheme.dimens.spacing8),
         flingBehavior = rememberSnapFlingBehavior(lazyListState = lazyListState),
     ) {
-        items(items = wallets, key = WalletCardState::id) { state ->
+        items(items = config.wallets, key = WalletCardState::id) { state ->
             WalletCard(state = state, modifier = Modifier.width(itemWidth))
         }
+    }
+
+    LaunchedEffect(lazyListState) {
+        snapshotFlow(lazyListState::firstVisibleItemScrollOffset)
+            .collect(
+                collector = FlowCollector { firstVisibleItemScrollOffset ->
+                    val itemSize = lazyListState.layoutInfo.visibleItemsInfo.firstOrNull()?.size ?: 1
+                    val offset = lazyListState.firstVisibleItemIndex * itemSize + firstVisibleItemScrollOffset
+                    val currentIndex = offset / itemSize
+                    if (offset - currentIndex * itemSize in MIN_OFFSET..MAX_OFFSET) {
+                        config.onWalletChange(currentIndex)
+                    }
+                },
+            )
     }
 }
 
@@ -51,14 +72,7 @@ internal fun WalletCardsList(wallets: ImmutableList<WalletCardState>, modifier: 
 @Composable
 private fun Preview_WalletHeader_LightTheme() {
     TangemTheme(isDark = false) {
-        WalletCardsList(
-            wallets = persistentListOf(
-                WalletPreviewData.walletCardContentState,
-                WalletPreviewData.walletCardLoadingState,
-                WalletPreviewData.walletCardHiddenContentState,
-                WalletPreviewData.walletCardErrorState,
-            ),
-        )
+        WalletsList(config = WalletPreviewData.walletListConfig)
     }
 }
 
@@ -66,13 +80,6 @@ private fun Preview_WalletHeader_LightTheme() {
 @Composable
 private fun Preview_WalletHeader_DarkTheme() {
     TangemTheme(isDark = true) {
-        WalletCardsList(
-            wallets = persistentListOf(
-                WalletPreviewData.walletCardContentState,
-                WalletPreviewData.walletCardLoadingState,
-                WalletPreviewData.walletCardHiddenContentState,
-                WalletPreviewData.walletCardErrorState,
-            ),
-        )
+        WalletsList(config = WalletPreviewData.walletListConfig)
     }
 }
