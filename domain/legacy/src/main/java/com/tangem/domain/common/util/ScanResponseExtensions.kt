8cf8f455e973b25b693bc6c90c0b093fbd07da95
Tangem <git@tangem.com>
2023-06-30 09:26:53 +0800

    Updated on 2026-06-27

--- domain/legacy/src/main/java/com/tangem/domain/common/util/ScanResponseExtensions.kt
+++ domain/legacy/src/main/java/com/tangem/domain/common/util/ScanResponseExtensions.kt
@@ -4,22 +4,13 @@ import com.tangem.blockchain.common.Blockchain
 import com.tangem.common.card.EllipticCurve
 import com.tangem.common.extensions.toMapKey
 import com.tangem.crypto.hdWallet.DerivationPath
-import com.tangem.domain.common.CardTypesResolver
-import com.tangem.domain.common.TangemCardTypesResolver
 import com.tangem.domain.common.TapWorkarounds.isTangemTwins
 import com.tangem.domain.common.TapWorkarounds.isTestCard
 import com.tangem.domain.models.scan.ScanResponse
 
-val ScanResponse.cardTypesResolver: CardTypesResolver
-    get() = TangemCardTypesResolver(
-        card = card,
-        productType = productType,
-        walletData = walletData,
-    )
-
 fun ScanResponse.twinsIsTwinned(): Boolean = card.isTangemTwins && walletData != null && secondTwinPublicKey != null
+
 fun ScanResponse.supportsHdWallet(): Boolean = card.settings.isHDWalletAllowed
-fun ScanResponse.supportsBackup(): Boolean = card.settings.isBackupAllowed
 
 fun ScanResponse.hasDerivation(blockchain: Blockchain, rawDerivationPath: String): Boolean {
     return hasDerivation(blockchain, DerivationPath(rawDerivationPath))
