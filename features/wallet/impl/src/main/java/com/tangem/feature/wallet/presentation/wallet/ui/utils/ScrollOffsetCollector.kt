1726c5c3cbd8f27da900da4136a8e33790f5bbb4
Tangem <git@tangem.com>
2023-07-02 16:05:28 +0800

    Updated on 2026-03-27

---
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/utils/ScrollOffsetCollector.kt
@@ -0,0 +1,37 @@
+package com.tangem.feature.wallet.presentation.wallet.ui.utils
+
+import androidx.compose.foundation.lazy.LazyListItemInfo
+import kotlinx.coroutines.flow.FlowCollector
+import kotlin.math.abs
+
+/**
+ * Flow collector for scroll items tracking.
+ * If first visible item offset is greater than half item size, then [callback] be invoked.
+ * If last visible item offset is greater than half item size, then [callback] be invoked.
+ *
+ * @property callback lambda be invoked when current scroll items is changed
+ *
+[REDACTED_AUTHOR]
+ */
+internal class ScrollOffsetCollector(
+    private val callback: (Int) -> Unit,
+) : FlowCollector<List<LazyListItemInfo>> {
+
+    private val LazyListItemInfo.halfItemSize get() = size.div(other = 2)
+
+    override suspend fun emit(value: List<LazyListItemInfo>) {
+        val firstItem = requireNotNull(value.firstOrNull()) {
+            "At least 1 item should be visible in list"
+        }
+
+        val lastItem = requireNotNull(value.lastOrNull()) {
+            "At least 1 item should be visible in list"
+        }
+
+        if (abs(firstItem.offset) > firstItem.halfItemSize) {
+            callback(firstItem.index + 1)
+        } else if (abs(lastItem.offset) > lastItem.halfItemSize) {
+            callback(lastItem.index - 1)
+        }
+    }
+}
