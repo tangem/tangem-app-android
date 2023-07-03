7fccbec7a24acc4368bd8c2b5376e465cf543227
Tangem <git@tangem.com>
2023-07-03 13:35:16 +0300

    Updated on 2025-12-05

--- features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/data/di/Learn2earnDataModule.kt
+++ features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/data/di/Learn2earnDataModule.kt
@@ -2,7 +2,6 @@ package com.tangem.feature.learn2earn.data.di
 
 import android.content.Context
 import com.squareup.moshi.Moshi
-import com.tangem.core.featuretoggle.manager.FeatureTogglesManager
 import com.tangem.datasource.api.promotion.PromotionApi
 import com.tangem.datasource.di.NetworkMoshi
 import com.tangem.datasource.di.PromotionOneInch
@@ -10,10 +9,6 @@ import com.tangem.feature.learn2earn.data.DefaultLearn2earnRepository
 import com.tangem.feature.learn2earn.data.DefaultPreferenceStorage
 import com.tangem.feature.learn2earn.data.api.Learn2earnPreferenceStorage
 import com.tangem.feature.learn2earn.data.api.Learn2earnRepository
-import com.tangem.feature.learn2earn.data.toggles.DevLearn2earnFeatureToggleManager
-import com.tangem.feature.learn2earn.data.toggles.Learn2earnFeatureToggleManager
-import com.tangem.feature.learn2earn.data.toggles.ProdLearn2earnFeatureToggleManager
-import com.tangem.feature.learn2earn.impl.BuildConfig
 import com.tangem.utils.coroutines.AppCoroutineDispatcherProvider
 import dagger.Module
 import dagger.Provides
@@ -24,17 +19,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class Learn2earnDataModule {
-
-    @Provides
-    @Singleton
-    fun provideFeatureToggle(featureToggleManager: FeatureTogglesManager): Learn2earnFeatureToggleManager {
-        return if (BuildConfig.DEBUG) {
-            DevLearn2earnFeatureToggleManager()
-        } else {
-            ProdLearn2earnFeatureToggleManager(featureToggleManager)
-        }
-    }
+internal class Learn2earnDataModule {
 
     @Provides
     @Singleton
@@ -45,14 +30,12 @@ class Learn2earnDataModule {
     @Provides
     @Singleton
     fun provideRepository(
-        featureToggleManager: Learn2earnFeatureToggleManager,
         preferenceStorage: Learn2earnPreferenceStorage,
         @NetworkMoshi moshi: Moshi,
         @PromotionOneInch promotionApi: PromotionApi,
         dispatchers: AppCoroutineDispatcherProvider,
     ): Learn2earnRepository {
         return DefaultLearn2earnRepository(
-            featureToggleManager = featureToggleManager,
             preferencesStorage = preferenceStorage,
             api = promotionApi,
             dispatchers = dispatchers,
