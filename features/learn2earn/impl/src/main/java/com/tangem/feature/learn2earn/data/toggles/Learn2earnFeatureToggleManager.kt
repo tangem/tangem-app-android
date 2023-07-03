183eafe3df8a52bbb95a5458b53f04ce0edd4ec7
Tangem <git@tangem.com>
2023-07-03 10:57:25 +0300

    Updated on 2026-09-14

---
+++ features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/data/toggles/Learn2earnFeatureToggleManager.kt
@@ -0,0 +1,11 @@
+package com.tangem.feature.learn2earn.data.toggles
+
+/**
+ * Feature toggles manager implementation of "learn2earn" feature
+ *
+[REDACTED_AUTHOR]
+ */
+interface Learn2earnFeatureToggleManager {
+
+    val isLearn2earnEnabled: Boolean
+}
