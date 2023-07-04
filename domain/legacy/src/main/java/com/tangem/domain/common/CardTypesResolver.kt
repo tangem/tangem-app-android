2b71f3a11c2c3dd0236ae3fae36257c7e8647893
Tangem <git@tangem.com>
2023-07-04 23:23:08 +0800

    Updated on 2025-12-05

--- domain/legacy/src/main/java/com/tangem/domain/common/CardTypesResolver.kt
+++ domain/legacy/src/main/java/com/tangem/domain/common/CardTypesResolver.kt
@@ -1,23 +1,17 @@
-package com.tangem.domain.card
+package com.tangem.domain.common
 
 import com.tangem.blockchain.common.Blockchain
 import com.tangem.blockchain.common.Token
 
-interface CardTypeResolver {
-
+interface CardTypesResolver {
     fun isTangemNote(): Boolean
-
     fun isTangemWallet(): Boolean
-
     fun isWallet2(): Boolean
-
     fun isTangemTwins(): Boolean
-
     fun isStart2Coin(): Boolean
 
     fun isMultiwalletAllowed(): Boolean
 
     fun getBlockchain(): Blockchain
-
     fun getPrimaryToken(): Token?
 }
