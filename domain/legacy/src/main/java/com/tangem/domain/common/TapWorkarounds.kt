36c0f4a8afd0108a7cf0c5a3197920a66904c4ad
Tangem <git@tangem.com>
2023-07-04 23:23:08 +0800

    Updated on 2026-02-11

--- domain/legacy/src/main/java/com/tangem/domain/common/TapWorkarounds.kt
+++ domain/legacy/src/main/java/com/tangem/domain/common/TapWorkarounds.kt
@@ -1,13 +1,14 @@
 package com.tangem.domain.common
 
+import com.tangem.blockchain.common.Blockchain
 import com.tangem.blockchain.common.DerivationStyle
+import com.tangem.common.card.Card
 import com.tangem.domain.models.scan.CardDTO
-import java.util.Locale
+import java.util.*
 
 /**
 [REDACTED_AUTHOR]
  */
-@Deprecated(message = "Use CardTypeResolver. [REDACTED_JIRA]")
 object TapWorkarounds {
     private const val START_2_COIN_ISSUER = "start2coin"
     private const val TEST_CARD_BATCH = "99FF"
@@ -43,11 +44,34 @@ object TapWorkarounds {
     val CardDTO.isNotSupportedInThatRelease: Boolean
         get() = false
 
+    private val tangemNoteBatches = mapOf(
+        "AB01" to Blockchain.Bitcoin,
+        "AB02" to Blockchain.Ethereum,
+        "AB03" to Blockchain.CardanoShelley,
+        "AB04" to Blockchain.Dogecoin,
+        "AB05" to Blockchain.BSC,
+        "AB06" to Blockchain.XRP,
+        "AB07" to Blockchain.Bitcoin,
+        "AB08" to Blockchain.Ethereum,
+        "AB09" to Blockchain.Bitcoin, // new batches for 3.34
+        "AB10" to Blockchain.Ethereum,
+        "AB11" to Blockchain.Bitcoin,
+        "AB12" to Blockchain.Ethereum,
+    )
+
     private val excludedBatches = listOf("0027", "0030", "0031", "0035")
 
     private val excludedIssuers = listOf("TTM BANK")
 
+    @Deprecated(
+        "Now blockchain is read form files (CardTypesResolver.getBlockchain), " +
+            "but for previously saved cards this method is still used",
+    )
+    fun CardDTO.getTangemNoteBlockchain(): Blockchain? = tangemNoteBatches[batchId]
+
     fun isStart2CoinIssuer(cardIssuer: String?): Boolean {
         return cardIssuer?.lowercase(Locale.US) == START_2_COIN_ISSUER
     }
+
+    fun Card.getTangemNoteBlockchain(): Blockchain? = tangemNoteBatches[batchId] ?: null
 }
