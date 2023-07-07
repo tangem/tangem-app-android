bfd2daa0b854d0ad6e13dd0d5d6c7cf03b61da2f
Tangem <git@tangem.com>
2023-07-07 16:55:06 +0300

    Updated on 2026-08-14

--- features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/domain/api/WebViewResultHandler.kt
+++ features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/domain/api/WebViewResultHandler.kt
@@ -1,5 +1,7 @@
 package com.tangem.feature.learn2earn.domain.api
 
+import com.tangem.feature.learn2earn.analytics.Learn2earnEvents
+
 /**
  * Handler that helps determine a result of the Learn2earnWebViewActivity webView actions.
  *
@@ -10,6 +12,8 @@ interface WebViewResultHandler {
 }
 
 sealed class WebViewResult {
-    object PromoCodeReceived : WebViewResult()
+    object Empty : WebViewResult()
+    data class PromoCode(val promoCode: String) : WebViewResult()
     object ReadyForAward : WebViewResult()
+    data class AnalyticsEvent(val event: Learn2earnEvents) : WebViewResult()
 }
