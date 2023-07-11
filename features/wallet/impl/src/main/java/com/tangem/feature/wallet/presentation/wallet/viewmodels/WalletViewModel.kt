21861bb88a07e2f154d52a6b2802484161d664a9
Tangem <git@tangem.com>
2023-07-11 22:21:20 +0800

    Updated on 2025-12-22

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/viewmodels/WalletViewModel.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/viewmodels/WalletViewModel.kt
@@ -5,12 +5,18 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tangem.common.doOnFailure
+import com.tangem.common.doOnSuccess
+import com.tangem.domain.card.ScanCardProcessor
 import com.tangem.domain.tokens.GetTokenListUseCase
 import com.tangem.feature.wallet.presentation.common.WalletPreviewData
 import com.tangem.feature.wallet.presentation.router.InnerWalletRouter
 import com.tangem.feature.wallet.presentation.wallet.state.WalletStateHolder
 import com.tangem.feature.wallet.presentation.wallet.state.WalletTopBarConfig
+import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import kotlin.properties.Delegates
 
@@ -23,6 +29,8 @@ import kotlin.properties.Delegates
 internal class WalletViewModel @Inject constructor(
     @Suppress("unused") // TODO: [REDACTED_JIRA]
     private val getTokenListUseCase: GetTokenListUseCase,
+    private val scanCardProcessor: ScanCardProcessor,
+    private val dispatchers: CoroutineDispatcherProvider,
 ) : ViewModel() {
 
     /** Feature router */
@@ -34,15 +42,31 @@ internal class WalletViewModel @Inject constructor(
 
     // TODO: [REDACTED_TASK_KEY] Use production data instead of WalletPreviewData
     private fun getInitialState(): WalletStateHolder = WalletPreviewData.multicurrencyWalletScreenState.copy(
-        onBackClick = { router.popBackStack() },
-        topBarConfig = WalletTopBarConfig(
-            onScanCardClick = { router.openOrganizeTokensScreen() },
-            onMoreClick = { router.openDetailsScreen() },
-        ),
+        onBackClick = ::onBackClick,
+        topBarConfig = createTopBarConfig(),
         walletsListConfig = WalletPreviewData.multicurrencyWalletScreenState.walletsListConfig.copy(
             onWalletChange = ::selectWallet,
         ),
     )
+
+    private fun onBackClick() {
+        router.popBackStack()
+    }
+
+    private fun createTopBarConfig(): WalletTopBarConfig {
+        return WalletTopBarConfig(
+            onScanCardClick = ::onScanCardClick,
+            onMoreClick = { router.openDetailsScreen() },
+        )
+    }
+
+    private fun onScanCardClick() {
+        viewModelScope.launch(dispatchers.io) {
+            scanCardProcessor.scan(allowsRequestAccessCodeFromRepository = true)
+                .doOnSuccess { /* [REDACTED_TODO_COMMENT] */ }
+                .doOnFailure { /* [REDACTED_TODO_COMMENT] */ }
+        }
+    }
 
     // TODO: [REDACTED_TASK_KEY] Use production data instead of WalletPreviewData
     private fun selectWallet(index: Int) {
