bf3eddade9f0292106cb10b7d1b7bb587726a76c
Tangem <git@tangem.com>
2023-07-12 09:52:20 +0300

    Updated on 2026-05-07

---
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/repository/NetworksRepository.kt
@@ -0,0 +1,26 @@
+package com.tangem.domain.tokens.repository
+
+import arrow.core.Either
+import com.tangem.domain.tokens.error.TokensError
+import com.tangem.domain.tokens.model.Network
+import com.tangem.domain.tokens.model.NetworkStatus
+import com.tangem.domain.tokens.model.Token
+import com.tangem.domain.wallets.models.UserWalletId
+import kotlinx.coroutines.flow.Flow
+
+// FIXME: Use Raise as context instead of Effect when context receivers become stable
+// [REDACTED_JIRA]
+
+/**
+ * Repository for everything related to the blockchain networks
+ * */
+interface NetworksRepository {
+
+    fun getNetworks(networksIds: Set<Network.ID>): Either<TokensError, Set<Network>>
+
+    fun getNetworkStatuses(
+        userWalletId: UserWalletId,
+        networks: Map<Network.ID, Set<Token.ID>>,
+        refresh: Boolean,
+    ): Flow<Either<TokensError, Set<NetworkStatus>>>
+}
