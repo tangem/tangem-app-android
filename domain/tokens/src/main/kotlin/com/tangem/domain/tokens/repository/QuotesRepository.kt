588c6c7006dcca3f9921cbb86b09c3f7fd816789
Tangem <git@tangem.com>
2023-07-12 09:52:20 +0300

    Updated on 2025-12-30

---
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/repository/QuotesRepository.kt
@@ -0,0 +1,18 @@
+package com.tangem.domain.tokens.repository
+
+import arrow.core.Either
+import com.tangem.domain.tokens.error.TokensError
+import com.tangem.domain.tokens.model.Quote
+import com.tangem.domain.tokens.model.Token
+import kotlinx.coroutines.flow.Flow
+
+// FIXME: Use Raise as context instead of Effect when context receivers become stable
+// [REDACTED_JIRA]
+
+/**
+ * Repository for everything related to the quotes of tokens
+ * */
+interface QuotesRepository {
+
+    fun getQuotes(tokensIds: Set<Token.ID>, refresh: Boolean): Flow<Either<TokensError, Set<Quote>>>
+}
