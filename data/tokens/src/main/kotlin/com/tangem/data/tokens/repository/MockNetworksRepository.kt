25839771512bcbdd7095ee0522de7afeb1801d67
Tangem <git@tangem.com>
2023-07-13 09:44:52 +0300

    Updated on 2026-06-05

--- data/tokens/src/main/kotlin/com/tangem/data/tokens/repository/MockNetworksRepository.kt
+++ data/tokens/src/main/kotlin/com/tangem/data/tokens/repository/MockNetworksRepository.kt
@@ -1,6 +1,8 @@
 package com.tangem.data.tokens.repository
 
 import arrow.core.Either
+import arrow.core.right
+import com.tangem.data.tokens.mock.MockNetworks
 import com.tangem.domain.tokens.error.TokensError
 import com.tangem.domain.tokens.model.Network
 import com.tangem.domain.tokens.model.NetworkStatus
@@ -8,11 +10,15 @@ import com.tangem.domain.tokens.model.Token
 import com.tangem.domain.tokens.repository.NetworksRepository
 import com.tangem.domain.wallets.models.UserWalletId
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 
-internal class DefaultNetworksRepository : NetworksRepository {
+internal class MockNetworksRepository : NetworksRepository {
 
     override fun getNetworks(networksIds: Set<Network.ID>): Either<TokensError, Set<Network>> {
-        TODO("Not yet implemented")
+        return MockNetworks.networks
+            .filter { it.id in networksIds }
+            .toSet()
+            .right()
     }
 
     override fun getNetworkStatuses(
@@ -20,6 +26,11 @@ internal class DefaultNetworksRepository : NetworksRepository {
         networks: Map<Network.ID, Set<Token.ID>>,
         refresh: Boolean,
     ): Flow<Either<TokensError, Set<NetworkStatus>>> {
-        TODO("Not yet implemented")
+        return flowOf(
+            MockNetworks.networksStatuses
+                .filter { it.networkId in networks.keys }
+                .toSet()
+                .right(),
+        )
     }
 }
