d59ca228734f961f3fd25f546b29a0aec60685b7
Tangem <git@tangem.com>
2023-07-13 09:44:52 +0300

    Updated on 2025-12-22

--- data/tokens/src/main/kotlin/com/tangem/data/tokens/repository/MockQuotesRepository.kt
+++ data/tokens/src/main/kotlin/com/tangem/data/tokens/repository/MockQuotesRepository.kt
@@ -1,15 +1,23 @@
 package com.tangem.data.tokens.repository
 
 import arrow.core.Either
+import arrow.core.right
+import com.tangem.data.tokens.mock.MockQuotes
 import com.tangem.domain.tokens.error.TokensError
 import com.tangem.domain.tokens.model.Quote
 import com.tangem.domain.tokens.model.Token
 import com.tangem.domain.tokens.repository.QuotesRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 
-internal class DefaultQuotesRepository : QuotesRepository {
+internal class MockQuotesRepository : QuotesRepository {
 
     override fun getQuotes(tokensIds: Set<Token.ID>, refresh: Boolean): Flow<Either<TokensError, Set<Quote>>> {
-        TODO("Not yet implemented")
+        return flowOf(
+            MockQuotes.quotes
+                .filter { it.tokenId in tokensIds }
+                .toSet()
+                .right(),
+        )
     }
 }
