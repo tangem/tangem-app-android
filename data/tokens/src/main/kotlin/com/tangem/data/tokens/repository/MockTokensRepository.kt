539ebaacdddabea1d00fb23da9fa37c7eaa72df6
Tangem <git@tangem.com>
2023-07-13 09:44:52 +0300

    Updated on 2026-08-19

--- data/tokens/src/main/kotlin/com/tangem/data/tokens/repository/MockTokensRepository.kt
+++ data/tokens/src/main/kotlin/com/tangem/data/tokens/repository/MockTokensRepository.kt
@@ -1,32 +1,34 @@
 package com.tangem.data.tokens.repository
 
 import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
+import com.tangem.data.tokens.mock.MockTokens
 import com.tangem.domain.tokens.error.TokensError
 import com.tangem.domain.tokens.model.Token
 import com.tangem.domain.tokens.repository.TokensRepository
 import com.tangem.domain.wallets.models.UserWalletId
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 
-internal class DefaultTokensRepository : TokensRepository {
+internal class MockTokensRepository : TokensRepository {
 
     override suspend fun sortTokens(
         userWalletId: UserWalletId,
         sortedTokensIds: Set<Token.ID>,
         isGrouped: Boolean,
         isSortedByBalance: Boolean,
-    ): Either<TokensError, Unit> {
-        TODO("Not yet implemented")
-    }
+    ): Either<TokensError, Unit> = Unit.right()
 
     override fun getTokens(userWalletId: UserWalletId, refresh: Boolean): Flow<Either<TokensError, Set<Token>>> {
-        TODO("Not yet implemented")
+        return flowOf(value = MockTokens.tokens[userWalletId]?.right() ?: TokensError.EmptyTokens.left())
     }
 
     override fun isTokensGrouped(userWalletId: UserWalletId): Flow<Either<TokensError, Boolean>> {
-        TODO("Not yet implemented")
+        return flowOf(MockTokens.isGrouped[userWalletId]?.right() ?: TokensError.EmptyTokens.left())
     }
 
     override fun isTokensSortedByBalance(userWalletId: UserWalletId): Flow<Either<TokensError, Boolean>> {
-        TODO("Not yet implemented")
+        return flowOf(MockTokens.isSortedByBalance[userWalletId]?.right() ?: TokensError.EmptyTokens.left())
     }
 }
