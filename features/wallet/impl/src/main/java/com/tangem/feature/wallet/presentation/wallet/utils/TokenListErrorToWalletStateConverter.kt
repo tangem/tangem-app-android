d48902f7e9e8dd9d781d7f88b4ee46afa4889a92
Tangem <git@tangem.com>
2023-07-15 01:47:52 +0300

    Updated on 2026-08-27

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/utils/TokenListErrorToWalletStateConverter.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/utils/TokenListErrorToWalletStateConverter.kt
@@ -1,15 +1,15 @@
 package com.tangem.feature.wallet.presentation.wallet.utils
 
-import com.tangem.domain.tokens.error.TokensError
+import com.tangem.domain.tokens.error.TokenListError
 import com.tangem.feature.wallet.presentation.wallet.state.WalletStateHolder
 import com.tangem.utils.converter.Converter
 
-internal class TokenErrorToWalletStateConverter(
+internal class TokenListErrorToWalletStateConverter(
     private val currentState: WalletStateHolder,
-) : Converter<TokensError, WalletStateHolder> {
+) : Converter<TokenListError, WalletStateHolder> {
 
     // TODO: [REDACTED_JIRA]
-    override fun convert(value: TokensError): WalletStateHolder {
+    override fun convert(value: TokenListError): WalletStateHolder {
         return currentState
     }
 }
