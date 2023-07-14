f805bbb64206dde0159a3ce504da07cf7d47fa63
Tangem <git@tangem.com>
2023-07-15 01:47:52 +0300

    Updated on 2026-09-23

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/operations/TokensStatusesOperations.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/operations/TokensStatusesOperations.kt
@@ -1,9 +1,10 @@
-package com.tangem.domain.tokens.utils
+package com.tangem.domain.tokens.operations
 
 import arrow.core.NonEmptySet
 import arrow.core.raise.Raise
 import arrow.core.toNonEmptySetOrNull
-import com.tangem.domain.tokens.error.TokensError
+import com.tangem.domain.core.raise.DelegatedRaise
+import com.tangem.domain.tokens.GetTokenListUseCase
 import com.tangem.domain.tokens.model.*
 import com.tangem.domain.tokens.repository.NetworksRepository
 import com.tangem.domain.tokens.repository.QuotesRepository
@@ -14,15 +15,33 @@ import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.withContext
 
 @Suppress("LongParameterList")
-internal class TokensStatusesOperations(
+internal class TokensStatusesOperations<E>(
     private val tokensRepository: TokensRepository,
     private val quotesRepository: QuotesRepository,
     private val networksRepository: NetworksRepository,
     private val userWalletId: UserWalletId,
     private val refresh: Boolean,
     private val dispatchers: CoroutineDispatcherProvider,
-    raise: Raise<TokensError>,
-) : Raise<TokensError> by raise {
+    raise: Raise<E>,
+    transformError: (Error) -> E,
+) : DelegatedRaise<TokensStatusesOperations.Error, E>(raise, transformError) {
+
+    constructor(
+        userWalletId: UserWalletId,
+        refresh: Boolean,
+        useCase: GetTokenListUseCase,
+        raise: Raise<E>,
+        transformError: (Error) -> E,
+    ) : this(
+        tokensRepository = useCase.tokensRepository,
+        quotesRepository = useCase.quotesRepository,
+        networksRepository = useCase.networksRepository,
+        userWalletId = userWalletId,
+        refresh = refresh,
+        dispatchers = useCase.dispatchers,
+        raise = raise,
+        transformError = transformError,
+    )
 
     fun getTokensStatusesFlow(): Flow<Set<TokenStatus>> {
         return getTokens().flatMapConcat {
@@ -46,37 +65,43 @@ internal class TokensStatusesOperations(
         quotes: Set<Quote>,
         networkStatuses: Set<NetworkStatus>,
     ): Set<TokenStatus> = withContext(dispatchers.single) {
-        tokens.map { token ->
+        tokens.mapTo(hashSetOf()) { token ->
             val quote = quotes.firstOrNull { it.tokenId == token.id }
             val networkStatus = networkStatuses.firstOrNull { it.networkId == token.networkId }
 
             createStatus(token, quote, networkStatus)
-        }.toSet()
+        }
     }
 
     private suspend fun createStatus(token: Token, quote: Quote?, networkStatus: NetworkStatus?): TokenStatus {
-        val operations = TokenStatusOperations(token, quote, networkStatus, dispatchers, raise = this)
-        return operations.createTokenStatus()
+        val tokenStatusOperations = TokenStatusOperations(
+            token = token,
+            quote = quote,
+            networkStatus = networkStatus,
+            dispatchers = dispatchers,
+        )
+
+        return tokenStatusOperations.createTokenStatus()
     }
 
     private fun getTokens(): Flow<Set<Token>> {
         return tokensRepository.getTokens(userWalletId, refresh)
-            .onEmpty { raise(TokensError.EmptyTokens) }
-            .map { it.bind() }
+            .catch { raise(Error.DataError(it)) }
+            .onEmpty { raise(Error.EmptyTokens) }
             .flowOn(dispatchers.io)
     }
 
     private fun getQuotes(tokensIds: NonEmptySet<Token.ID>): Flow<Set<Quote>> {
         return quotesRepository.getQuotes(tokensIds, refresh)
-            .onEmpty { raise(TokensError.EmptyQuotes) }
-            .map { it.bind() }
+            .catch { raise(Error.DataError(it)) }
+            .onEmpty { raise(Error.EmptyQuotes) }
             .flowOn(dispatchers.io)
     }
 
     private fun getNetworksStatues(groupedTokens: Map<Network.ID, NonEmptySet<Token.ID>>): Flow<Set<NetworkStatus>> {
         return networksRepository.getNetworkStatuses(userWalletId, groupedTokens, refresh)
-            .onEmpty { raise(TokensError.EmptyNetworkStatues) }
-            .map { it.bind() }
+            .catch { raise(Error.DataError(it)) }
+            .onEmpty { raise(Error.EmptyNetworksStatuses) }
             .flowOn(dispatchers.io)
     }
 
@@ -85,10 +110,21 @@ internal class TokensStatusesOperations(
             tokens
                 .groupBy { it.networkId }
                 .mapValues { (_, tokens) ->
-                    tokens.toNonEmptySetOrNull()
-                        ?.map { it.id }
-                        ?.toNonEmptySet()
-                        ?: raise(TokensError.EmptyTokens)
+                    // Can not be empty
+                    tokens.toNonEmptySetOrNull()!!
+                        .map { it.id }
+                        .toNonEmptySet()
                 }
         }
+
+    sealed class Error {
+
+        object EmptyTokens : Error()
+
+        object EmptyQuotes : Error()
+
+        object EmptyNetworksStatuses : Error()
+
+        data class DataError(val cause: Throwable) : Error()
+    }
 }
