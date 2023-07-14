710f9112cdeb224b9038647b0e80db696df9e9a4
iiiburnyiii <iiiburnyiii@gmail.com>
2023-07-15 01:47:52 +0300

    AND-3813 Updated tokens operations

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/utils/TokenListErrorToWalletStateConverter.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/utils/TokenListErrorToWalletStateConverter.kt
@@ -1,15 +1,15 @@
 package com.tangem.feature.wallet.presentation.wallet.utils
 
-import com.tangem.domain.tokens.error.TokensError
+import com.tangem.domain.tokens.error.TokenListError
 import com.tangem.feature.wallet.presentation.wallet.state.WalletStateHolder
 import com.tangem.utils.converter.Converter
 
-internal class TokenErrorToWalletStateConverter(
+internal class TokenListErrorToWalletStateConverter(
     private val currentState: WalletStateHolder,
-) : Converter<TokensError, WalletStateHolder> {
+) : Converter<TokenListError, WalletStateHolder> {
 
     // TODO: https://tangem.atlassian.net/browse/AND-4021
-    override fun convert(value: TokensError): WalletStateHolder {
+    override fun convert(value: TokenListError): WalletStateHolder {
         return currentState
     }
 }
