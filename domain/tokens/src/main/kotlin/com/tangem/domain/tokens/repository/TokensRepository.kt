edae9fe659806d834b5ccd6513135fd8ca2013e1
Tangem <git@tangem.com>
2023-07-15 01:47:52 +0300

    Updated on 2026-07-23

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/repository/TokensRepository.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/repository/TokensRepository.kt
@@ -1,7 +1,5 @@
 package com.tangem.domain.tokens.repository
 
-import arrow.core.Either
-import com.tangem.domain.tokens.error.TokensError
 import com.tangem.domain.tokens.model.Token
 import com.tangem.domain.wallets.models.UserWalletId
 import kotlinx.coroutines.flow.Flow
@@ -19,11 +17,11 @@ interface TokensRepository {
         sortedTokensIds: Set<Token.ID>,
         isGrouped: Boolean,
         isSortedByBalance: Boolean,
-    ): Either<TokensError, Unit>
+    )
 
-    fun getTokens(userWalletId: UserWalletId, refresh: Boolean): Flow<Either<TokensError, Set<Token>>>
+    fun getTokens(userWalletId: UserWalletId, refresh: Boolean): Flow<Set<Token>>
 
-    fun isTokensGrouped(userWalletId: UserWalletId): Flow<Either<TokensError, Boolean>>
+    fun isTokensGrouped(userWalletId: UserWalletId): Flow<Boolean>
 
-    fun isTokensSortedByBalance(userWalletId: UserWalletId): Flow<Either<TokensError, Boolean>>
+    fun isTokensSortedByBalance(userWalletId: UserWalletId): Flow<Boolean>
 }
