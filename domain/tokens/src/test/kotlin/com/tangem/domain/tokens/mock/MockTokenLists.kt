5928a6d95f5634eeaeb1896292508d6cb2d42ff6
Tangem <git@tangem.com>
2023-07-15 15:20:48 +0300

    Updated on 2026-09-25

--- domain/tokens/src/test/kotlin/com/tangem/domain/tokens/mock/MockTokenLists.kt
+++ domain/tokens/src/test/kotlin/com/tangem/domain/tokens/mock/MockTokenLists.kt
@@ -1,8 +1,10 @@
 package com.tangem.domain.tokens.mock
 
-import arrow.core.nonEmptySetOf
+import arrow.core.NonEmptySet
 import arrow.core.toNonEmptySetOrNull
-import com.tangem.domain.tokens.model.NetworkGroup
+import com.tangem.domain.tokens.mock.MockNetworksGroups.failedNetworksGroups
+import com.tangem.domain.tokens.mock.MockNetworksGroups.loadedNetworksGroups
+import com.tangem.domain.tokens.mock.MockNetworksGroups.sortedNetworksGroups
 import com.tangem.domain.tokens.model.TokenList
 import com.tangem.domain.tokens.model.TokenStatus
 import java.math.BigDecimal
@@ -13,72 +15,59 @@ internal object MockTokenLists {
     const val isGrouped = false
     const val isSortedByBalance = false
 
-    val networkGroup1 = NetworkGroup(
-        network = MockNetworks.network1,
-        tokens = MockTokensStates.tokenStates
-            .filter { it.networkId == MockNetworks.network1.id }
-            .toNonEmptySetOrNull()!!,
-    )
-
-    val networkGroup2 = NetworkGroup(
-        network = MockNetworks.network2,
-        tokens = MockTokensStates.tokenStates
-            .filter { it.networkId == MockNetworks.network2.id }
-            .toNonEmptySetOrNull()!!,
-    )
-
-    val networkGroup3 = NetworkGroup(
-        network = MockNetworks.network3,
-        tokens = MockTokensStates.tokenStates
-            .filter { it.networkId == MockNetworks.network3.id }
-            .toNonEmptySetOrNull()!!,
-    )
-
-    val networksGroups = nonEmptySetOf(networkGroup1, networkGroup2, networkGroup3)
-
-    val sortedNetworksGroups = networksGroups.map { group ->
-        group.copy(
-            tokens = MockTokensStates.loadedTokensStates
-                .filter { it.networkId == group.network.id }
-                .sortedByDescending { it.value.fiatAmount }
-                .toNonEmptySetOrNull()!!,
-        )
-    }
-        .sortedByDescending { group ->
-            group.tokens.sumOf { it.value.fiatAmount!! }
-        }
-        .toNonEmptySetOrNull()!!
-
     val notInitializedTokenList = TokenList.NotInitialized
 
-    val groupedTokenList = TokenList.GroupedByNetwork(
-        groups = networksGroups,
+    val emptyGroupedTokenList = TokenList.GroupedByNetwork(
+        groups = emptySet(),
         totalFiatBalance = TokenList.FiatBalance.Failed,
         sortedBy = TokenList.SortType.NONE,
     )
 
-    val ungroupedTokenList = TokenList.Ungrouped(
-        tokens = MockTokensStates.tokenStates,
+    val emptyUngroupedTokenList = TokenList.Ungrouped(
+        tokens = emptySet(),
         totalFiatBalance = TokenList.FiatBalance.Failed,
         sortedBy = TokenList.SortType.NONE,
     )
 
-    val loadingUngroupedTokenList = with(ungroupedTokenList) {
+    val failedGroupedTokenList = TokenList.GroupedByNetwork(
+        groups = failedNetworksGroups,
+        totalFiatBalance = TokenList.FiatBalance.Failed,
+        sortedBy = TokenList.SortType.NONE,
+    )
+
+    val failedUngroupedTokenList = TokenList.Ungrouped(
+        tokens = MockTokensStates.failedTokenStates,
+        totalFiatBalance = TokenList.FiatBalance.Failed,
+        sortedBy = TokenList.SortType.NONE,
+    )
+
+    val loadingUngroupedTokenList = with(failedUngroupedTokenList) {
         copy(
-            tokens = tokens.map { it.copy(value = TokenStatus.Loading) }.toSet(),
+            tokens = tokens.map { it.copy(value = TokenStatus.Loading) }.toNonEmptySetOrNull()!!,
             totalFiatBalance = TokenList.FiatBalance.Loading,
         )
     }
 
-    val sortedUngroupedTokenList: TokenList.Ungrouped
+    val loadingGroupedTokenList = with(failedGroupedTokenList) {
+        copy(
+            totalFiatBalance = TokenList.FiatBalance.Loading,
+            groups = groups.map { group ->
+                group.copy(
+                    tokens = group.tokens
+                        .map { it.copy(value = TokenStatus.Loading) }
+                        .toNonEmptySetOrNull()!!,
+                )
+            }.toNonEmptySetOrNull()!!,
+        )
+    }
+
+    val unsortedUngroupedTokenList: TokenList.Ungrouped
         get() {
             val tokens = MockTokensStates.loadedTokensStates
-                .sortedByDescending { it.value.fiatAmount }
-                .toNonEmptySetOrNull()!!
 
-            return ungroupedTokenList.copy(
+            return failedUngroupedTokenList.copy(
                 tokens = tokens,
-                sortedBy = TokenList.SortType.BALANCE,
+                sortedBy = TokenList.SortType.NONE,
                 totalFiatBalance = TokenList.FiatBalance.Loaded(
                     amount = tokens.sumOf { it.value.fiatAmount ?: BigDecimal.ZERO },
                     isAllAmountsSummarized = true,
@@ -86,19 +75,41 @@ internal object MockTokenLists {
             )
         }
 
-    val sortedGroupedTokenList: TokenList.GroupedByNetwork
+    val unsortedGroupedTokenList: TokenList.GroupedByNetwork
         get() {
-            val groups = sortedNetworksGroups.toSet()
+            val groups = loadedNetworksGroups
 
-            return groupedTokenList.copy(
+            return failedGroupedTokenList.copy(
                 groups = groups,
-                sortedBy = TokenList.SortType.BALANCE,
+                sortedBy = TokenList.SortType.NONE,
                 totalFiatBalance = TokenList.FiatBalance.Loaded(
                     amount = groups
-                        .flatMap { it.tokens }
+                        .flatMap { it.tokens as NonEmptySet<TokenStatus> }
                         .sumOf { it.value.fiatAmount ?: BigDecimal.ZERO },
                     isAllAmountsSummarized = true,
                 ),
+            )
+        }
+
+    val sortedUngroupedTokenList: TokenList.Ungrouped
+        get() {
+            val tokens = MockTokensStates.loadedTokensStates
+                .sortedByDescending { it.value.fiatAmount }
+                .toNonEmptySetOrNull()!!
+
+            return unsortedUngroupedTokenList.copy(
+                tokens = tokens,
+                sortedBy = TokenList.SortType.BALANCE,
+            )
+        }
+
+    val sortedGroupedTokenList: TokenList.GroupedByNetwork
+        get() {
+            val groups = sortedNetworksGroups
+
+            return unsortedGroupedTokenList.copy(
+                groups = groups,
+                sortedBy = TokenList.SortType.BALANCE,
             )
         }
 }
