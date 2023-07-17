0bc2688036f05a75458aab899799244bb68c0e6a
Tangem <git@tangem.com>
2023-07-17 10:52:46 +0300

    release/5.26.1

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/WalletStateHolder.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/WalletStateHolder.kt
@@ -7,11 +7,12 @@ import kotlinx.collections.immutable.ImmutableList
 /**
  * Wallet screen state holder
  *
- * @property onBackClick       lambda be invoked when back button is clicked
- * @property topBarConfig      top bar config
- * @property walletsListConfig wallets list config
- * @property contentItems      content items
- * @property notifications     notifications
+ * @property onBackClick            lambda be invoked when back button is clicked
+ * @property topBarConfig           top bar config
+ * @property walletsListConfig      wallets list config
+ * @property pullToRefreshConfig    pull to refresh config
+ * @property contentItems           content items
+ * @property notifications          notifications
  *
 * [REDACTED_AUTHOR]
  */
@@ -19,6 +20,7 @@ internal sealed class WalletStateHolder(
     open val onBackClick: () -> Unit,
     open val topBarConfig: WalletTopBarConfig,
     open val walletsListConfig: WalletsListConfig,
+    open val pullToRefreshConfig: WalletPullToRefreshConfig,
     open val contentItems: ImmutableList<WalletContentItemState>,
     open val notifications: ImmutableList<WalletNotification>,
 ) {
@@ -26,40 +28,58 @@ internal sealed class WalletStateHolder(
     /**
      * Multi currency wallet content state
      *
-     * @property onBackClick           lambda be invoked when back button is clicked
-     * @property topBarConfig          top bar config
-     * @property walletsListConfig     wallets list config
-     * @property contentItems          content items
-     * @property notifications         notifications
-     * @property onOrganizeTokensClick lambda be invoked when organize tokens button is clicked
+     * @property onBackClick            lambda be invoked when back button is clicked
+     * @property topBarConfig           top bar config
+     * @property walletsListConfig      wallets list config
+     * @property pullToRefreshConfig    pull to refresh config
+     * @property contentItems           content items
+     * @property notifications          notifications
+     * @property onOrganizeTokensClick  lambda be invoked when organize tokens button is clicked
      */
     data class MultiCurrencyContent(
         override val onBackClick: () -> Unit,
         override val topBarConfig: WalletTopBarConfig,
         override val walletsListConfig: WalletsListConfig,
+        override val pullToRefreshConfig: WalletPullToRefreshConfig,
         override val contentItems: ImmutableList<WalletContentItemState.MultiCurrencyItem>,
         override val notifications: ImmutableList<WalletNotification>,
         val onOrganizeTokensClick: () -> Unit,
-    ) : WalletStateHolder(onBackClick, topBarConfig, walletsListConfig, contentItems, notifications)
+    ) : WalletStateHolder(
+        onBackClick = onBackClick,
+        topBarConfig = topBarConfig,
+        walletsListConfig = walletsListConfig,
+        pullToRefreshConfig = pullToRefreshConfig,
+        contentItems = contentItems,
+        notifications = notifications,
+    )
 
     /**
      * Single currency wallet content state
      *
-     * @property onBackClick           lambda be invoked when back button is clicked
-     * @property topBarConfig          top bar config
-     * @property walletsListConfig     wallets list config
-     * @property contentItems          content items
-     * @property notifications         notifications
-     * @property buttons               manage buttons
-     * @property marketPriceBlockState market price block state
+     * @property onBackClick            lambda be invoked when back button is clicked
+     * @property topBarConfig           top bar config
+     * @property walletsListConfig      wallets list config
+     * @property pullToRefreshConfig    pull to refresh config
+     * @property contentItems           content items
+     * @property notifications          notifications
+     * @property buttons                manage buttons
+     * @property marketPriceBlockState  market price block state
      */
     data class SingleCurrencyContent(
         override val onBackClick: () -> Unit,
         override val topBarConfig: WalletTopBarConfig,
         override val walletsListConfig: WalletsListConfig,
+        override val pullToRefreshConfig: WalletPullToRefreshConfig,
         override val contentItems: ImmutableList<WalletContentItemState.SingleCurrencyItem>,
         override val notifications: ImmutableList<WalletNotification>,
         val buttons: ImmutableList<ActionButtonConfig>,
         val marketPriceBlockState: MarketPriceBlockState,
-    ) : WalletStateHolder(onBackClick, topBarConfig, walletsListConfig, contentItems, notifications)
+    ) : WalletStateHolder(
+        onBackClick = onBackClick,
+        topBarConfig = topBarConfig,
+        walletsListConfig = walletsListConfig,
+        pullToRefreshConfig = pullToRefreshConfig,
+        contentItems = contentItems,
+        notifications = notifications,
+    )
 }
