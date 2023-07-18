81cc1386bfb105d1f33ea18251ed3ef46b8f2aa3
Tangem <git@tangem.com>
2023-07-18 21:55:50 +0500

    Updated on 2026-05-04

---
+++ features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/TokenDetailsState.kt
@@ -0,0 +1,6 @@
+package com.tangem.feature.tokendetails.presentation.tokendetails.state
+
+data class TokenDetailsState(
+    val topAppBarConfig: TokenDetailsTopAppBarConfig,
+    val tokenInfoBlockState: TokenInfoBlockState,
+)
