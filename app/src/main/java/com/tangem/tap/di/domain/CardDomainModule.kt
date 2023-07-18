c82c2b3cfba4809daaa5f31a82f303c11a140596
Tangem <git@tangem.com>
2023-07-18 21:11:12 +0800

    Updated on 2026-08-14

--- app/src/main/java/com/tangem/tap/di/domain/CardDomainModule.kt
+++ app/src/main/java/com/tangem/tap/di/domain/CardDomainModule.kt
@@ -1,6 +1,10 @@
 package com.tangem.tap.di.domain
 
+import com.tangem.domain.card.GetAccessCodeSavingStatusUseCase
+import com.tangem.domain.card.GetBiometricsStatusUseCase
 import com.tangem.domain.card.ScanCardProcessor
+import com.tangem.domain.card.SetAccessCodeRequestPolicyUseCase
+import com.tangem.domain.card.repository.CardSdkConfigRepository
 import com.tangem.tap.domain.scanCard.DefaultScanCardProcessor
 import dagger.Module
 import dagger.Provides
@@ -15,4 +19,28 @@ internal object CardDomainModule {
     @Provides
     @Singleton
     fun provideScanCardUseCase(): ScanCardProcessor = DefaultScanCardProcessor()
+
+    @Provides
+    @Singleton
+    fun provideGetBiometricsStatusUseCase(
+        cardSdkConfigRepository: CardSdkConfigRepository,
+    ): GetBiometricsStatusUseCase {
+        return GetBiometricsStatusUseCase(cardSdkConfigRepository = cardSdkConfigRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSetAccessCodeRequestPolicyUseCase(
+        cardSdkConfigRepository: CardSdkConfigRepository,
+    ): SetAccessCodeRequestPolicyUseCase {
+        return SetAccessCodeRequestPolicyUseCase(cardSdkConfigRepository = cardSdkConfigRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetAccessCodeSavingStatusUseCase(
+        cardSdkConfigRepository: CardSdkConfigRepository,
+    ): GetAccessCodeSavingStatusUseCase {
+        return GetAccessCodeSavingStatusUseCase(cardSdkConfigRepository = cardSdkConfigRepository)
+    }
 }
