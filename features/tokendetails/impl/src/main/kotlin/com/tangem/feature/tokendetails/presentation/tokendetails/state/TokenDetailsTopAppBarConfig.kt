676142c455a55a0876de41839df80310b682c22a
Tangem <git@tangem.com>
2023-07-18 21:55:50 +0500

    Updated on 2025-11-03

---
+++ features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/TokenDetailsTopAppBarConfig.kt
@@ -0,0 +1,6 @@
+package com.tangem.feature.tokendetails.presentation.tokendetails.state
+
+data class TokenDetailsTopAppBarConfig(
+    val onBackClick: () -> Unit,
+    val onMoreClick: () -> Unit,
+)
