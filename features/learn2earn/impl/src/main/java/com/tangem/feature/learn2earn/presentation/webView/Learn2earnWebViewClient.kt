4f9eaa02f384431cca80da87bea74e2936ba2f2d
Tangem <git@tangem.com>
2023-07-18 18:06:59 +0300

    Updated on 2026-06-02

--- features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/presentation/webView/Learn2earnWebViewClient.kt
+++ features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/presentation/webView/Learn2earnWebViewClient.kt
@@ -2,7 +2,7 @@ package com.tangem.feature.learn2earn.presentation.webView
 
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
-import android.webkit.WebViewClient
+import com.google.accompanist.web.AccompanistWebViewClient
 import com.tangem.feature.learn2earn.domain.api.WebViewAction
 import com.tangem.feature.learn2earn.domain.api.WebViewRedirectHandler
 import timber.log.Timber
@@ -11,7 +11,7 @@ internal class Learn2earnWebViewClient(
     private val redirectHandler: WebViewRedirectHandler,
     private val headers: Map<String, String>,
     private val finishSessionHandler: () -> Unit,
-) : WebViewClient() {
+) : AccompanistWebViewClient() {
 
     override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
         Timber.d("url: ${request.url}")
