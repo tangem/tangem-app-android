ffa5559e88eb7635210cd8944fd5e83efe9b9463
Tangem <git@tangem.com>
2023-07-19 14:15:27 +0500

    Updated on 2025-10-09

--- features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/viewmodels/TokenDetailsViewModel.kt
+++ features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/viewmodels/TokenDetailsViewModel.kt
@@ -4,11 +4,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.tangem.feature.tokendetails.presentation.router.InnerTokenDetailsRouter
 import com.tangem.feature.tokendetails.presentation.tokendetails.TokenDetailsPreviewData
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import kotlin.properties.Delegates
+
+private const val LOADING_DELAY = 4_000L
 
 @HiltViewModel
 internal class TokenDetailsViewModel @Inject constructor() : ViewModel() {
@@ -17,6 +22,14 @@ internal class TokenDetailsViewModel @Inject constructor() : ViewModel() {
 
     var uiState by mutableStateOf(getInitialState())
         private set
+
+    init {
+        // simulate loading state
+        viewModelScope.launch {
+            delay(LOADING_DELAY)
+            uiState = uiState.copy(tokenBalanceBlockState = TokenDetailsPreviewData.balanceContent)
+        }
+    }
 
     private fun getInitialState() = TokenDetailsPreviewData.tokenDetailsState.copy(
         topAppBarConfig = TokenDetailsPreviewData.tokenDetailsTopAppBarConfig.copy(
