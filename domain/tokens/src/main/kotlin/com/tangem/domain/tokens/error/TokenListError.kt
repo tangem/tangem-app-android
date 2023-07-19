0e2ffea23b2cf7b3a5e338aef10398d1044f2663
Tangem <git@tangem.com>
2023-07-19 14:18:24 +0300

    Updated on 2026-09-14

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/error/TokenListError.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/error/TokenListError.kt
@@ -1,8 +1,6 @@
 package com.tangem.domain.tokens.error
 
 import com.tangem.domain.tokens.model.TokenList
-import com.tangem.domain.tokens.operations.TokenListOperations
-import com.tangem.domain.tokens.operations.TokensStatusesOperations
 
 sealed class TokenListError {
 
@@ -11,21 +9,4 @@ sealed class TokenListError {
     data class UnableToSortTokenList(val unsortedTokenList: TokenList.Ungrouped) : TokenListError()
 
     data class DataError(val cause: Throwable) : TokenListError()
-
-    internal companion object {
-
-        fun fromTokenStatusesOperations(error: TokensStatusesOperations.Error): TokenListError = when (error) {
-            is TokensStatusesOperations.Error.DataError -> DataError(error.cause)
-            is TokensStatusesOperations.Error.EmptyNetworksStatuses,
-            is TokensStatusesOperations.Error.EmptyQuotes,
-            is TokensStatusesOperations.Error.EmptyTokens,
-            -> EmptyTokens
-        }
-
-        fun fromTokenListOperations(error: TokenListOperations.Error): TokenListError = when (error) {
-            is TokenListOperations.Error.DataError -> DataError(error.cause)
-            is TokenListOperations.Error.UnableToSortTokenList -> UnableToSortTokenList(error.unsortedTokenList)
-            is TokenListOperations.Error.UnableToGroupTokenList -> UnableToSortTokenList(error.ungroupedTokenList)
-        }
-    }
 }
