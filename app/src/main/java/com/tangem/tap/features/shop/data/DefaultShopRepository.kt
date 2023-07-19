7852f27af0c978204880ffbcc6d799583683b9fb
Tangem <git@tangem.com>
2023-07-19 18:22:26 +0300

    Updated on 2026-09-25

--- app/src/main/java/com/tangem/tap/features/shop/data/DefaultShopRepository.kt
+++ app/src/main/java/com/tangem/tap/features/shop/data/DefaultShopRepository.kt
@@ -2,10 +2,13 @@ package com.tangem.tap.features.shop.data
 
 import com.tangem.datasource.api.tangemTech.TangemTechApi
 import com.tangem.datasource.api.tangemTech.models.ShopResponse
+import com.tangem.domain.common.extensions.withIOContext
 import com.tangem.tap.features.shop.domain.ShopRepository
+import com.tangem.tap.features.shop.domain.models.SalesProduct
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import com.tangem.utils.coroutines.runCatching
 import timber.log.Timber
+import java.util.Locale
 
 /**
  * Default implementation of shop feature repository
@@ -20,6 +23,8 @@ internal class DefaultShopRepository(
     private val dispatchers: CoroutineDispatcherProvider,
 ) : ShopRepository {
 
+    private val salesProductConverter = SalesProductConverter()
+
     override suspend fun isShopifyOrderingAvailable(): Boolean {
         return runCatching(dispatchers.io) { tangemTechApi.getShopInfo(name = SHOPIFY_NAME) }
             .fold(
@@ -31,7 +36,24 @@ internal class DefaultShopRepository(
             )
     }
 
+    override suspend fun getSalesProductInfo(): List<SalesProduct> {
+        return withIOContext {
+            val salesInfo = tangemTechApi.getSalesInfo(locale = getLocaleName(), shops = SHOPIFY_NAME)
+            salesProductConverter.convert(salesInfo)
+        }
+    }
+
+    private fun getLocaleName(): String {
+        return if (Locale.getDefault().language == "ru") {
+            RU_LOCALE
+        } else {
+            EN_LOCALE
+        }
+    }
+
     private companion object {
-        const val SHOPIFY_NAME = "shopify"
+        private const val SHOPIFY_NAME = "shopify"
+        private const val RU_LOCALE = "ru"
+        private const val EN_LOCALE = "en"
     }
 }
