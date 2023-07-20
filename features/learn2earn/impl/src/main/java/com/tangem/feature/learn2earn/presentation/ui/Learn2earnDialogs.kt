dc8da91f569fa1d9c0a3e95a72eda3b65d4cb030
Anton Zhilenkov <azhilenkov@tangem.com>
2023-07-20 11:11:43 +0300

    AND-4042 1inch: if a promotion response has a network error - will show a dialog with the message common_server_unavailable

--- features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/presentation/ui/Learn2earnDialogs.kt
+++ features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/presentation/ui/Learn2earnDialogs.kt
@@ -5,7 +5,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import com.tangem.core.ui.components.TextButton
-import com.tangem.feature.learn2earn.domain.models.PromotionError
+import com.tangem.core.ui.extensions.resolveReference
 import com.tangem.feature.learn2earn.impl.R
 import com.tangem.feature.learn2earn.presentation.ui.state.MainScreenState
 
@@ -68,18 +68,12 @@ private fun PromoCodeNotRegisteredDialog(dialog: MainScreenState.Dialog.PromoCod
 
 @Composable
 private fun ErrorDialog(dialog: MainScreenState.Dialog.Error) {
-    val errorMessage = if (dialog.error is PromotionError.NetworkUnreachable) {
-        stringResource(id = R.string.common_server_unavailable)
-    } else {
-        dialog.error.description
-    }
-
     AlertDialog(
         title = {
             Text(text = stringResource(id = R.string.common_error))
         },
         text = {
-            Text(text = errorMessage)
+            Text(text = dialog.textReference.resolveReference())
         },
         confirmButton = {
             TextButton(
