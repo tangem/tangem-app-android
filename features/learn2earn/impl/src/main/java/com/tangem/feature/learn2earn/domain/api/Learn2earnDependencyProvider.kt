8ad3e34989d64f525a876e003a95907ddbfc4f6a
Anton Zhilenkov <azhilenkov@tangem.com>
2023-07-20 15:57:36 +0300

    AND-4059 1inch: refactoring with new web view logic (#1193)
    * AND-4059 1inch: changed logic for new webView api
    * AND-4033 1inch: fixed showing 1inch widget for non-wallet cards

--- features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/domain/api/Learn2earnDependencyProvider.kt
+++ features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/domain/api/Learn2earnDependencyProvider.kt
@@ -1,11 +1,15 @@
 package com.tangem.feature.learn2earn.domain.api
 
+import com.tangem.domain.common.CardTypesResolver
+
 /**
  * Interface, a wrapper that allows us to get values from the AppStateHolder located in the app module
  *
  * @author Anton Zhilenkov on 28.06.2023.
  */
 interface Learn2earnDependencyProvider {
+
+    fun getCardTypeResolver(): CardTypesResolver?
 
     fun getLocaleProvider(): () -> String
 
