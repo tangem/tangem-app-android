2d7e2c6ba2c7e641608bb60a2d4039c3f7d006b6
Tangem <git@tangem.com>
2023-07-24 19:42:06 +0300

    Updated on 2025-12-22

---
+++ app/src/main/java/com/tangem/tap/data/RuntimeUserWalletsStore.kt
@@ -0,0 +1,21 @@
+package com.tangem.tap.data
+
+import com.tangem.datasource.local.userwallet.UserWalletsStore
+import com.tangem.domain.wallets.legacy.WalletsStateHolder
+import com.tangem.domain.wallets.models.UserWallet
+import com.tangem.domain.wallets.models.UserWalletId
+import kotlinx.coroutines.flow.firstOrNull
+
+// FIXME: Workaround, remove it once the normal UserWalletsStore has been implemented
+// [REDACTED_JIRA]
+internal class RuntimeUserWalletsStore(
+    private val walletsStateHolder: WalletsStateHolder,
+) : UserWalletsStore {
+
+    override suspend fun getSync(userWalletId: UserWalletId): UserWallet? {
+        return walletsStateHolder.userWalletsListManager
+            ?.userWallets
+            ?.firstOrNull()
+            ?.firstOrNull { it.walletId == userWalletId }
+    }
+}
