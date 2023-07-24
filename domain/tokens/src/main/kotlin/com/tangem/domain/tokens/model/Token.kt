a7e78a03476d4e22e2bc76ef4eff75195168e501
Tangem <git@tangem.com>
2023-07-24 20:05:31 +0300

    Updated on 2026-09-14

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/model/Token.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/model/Token.kt
@@ -8,9 +8,24 @@ data class Token(
     val iconUrl: String?,
     val decimals: Int,
     val isCustom: Boolean,
-    val isCoin: Boolean,
+    val contractAddress: String?,
+    val derivationPath: String?,
 ) {
 
+    init {
+        require(name.isNotBlank()) { "Token name must not be blank" }
+        require(symbol.isNotBlank()) { "Token symbol must not be blank" }
+        require(iconUrl?.isNotBlank() ?: true) { "Token icon URL must not be blank" }
+        require(decimals > 0) { "Token decimal must not be less then zero, but it is: $decimals" }
+        require(contractAddress?.isNotBlank() ?: true) { "Token contract address must not be blank" }
+        require(derivationPath?.isNotBlank() ?: true) { "Token derivation path must not be blank" }
+    }
+
     @JvmInline
-    value class ID(val value: String)
+    value class ID(val value: String) {
+
+        init {
+            require(value.isNotBlank()) { "Token ID value must not be blank" }
+        }
+    }
 }
