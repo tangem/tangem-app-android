3658c315e1d90f116445b991d30c20b8ffd385ec
Tangem <git@tangem.com>
2023-07-24 11:41:13 +0300

    Updated on 2026-09-23

---
+++ core/datasource/src/main/java/com/tangem/datasource/local/token/FileUserTokensStore.kt
@@ -0,0 +1,28 @@
+package com.tangem.datasource.local.token
+
+import com.squareup.moshi.Moshi
+import com.tangem.datasource.api.tangemTech.models.UserTokensResponse
+import com.tangem.datasource.files.FileReader
+import com.tangem.datasource.local.datastore.FileDataStore
+import kotlinx.coroutines.flow.Flow
+
+internal class FileUserTokensStore(fileReader: FileReader, moshi: Moshi) : UserTokensStore {
+
+    private val store = FileDataStore<UserTokensResponse>(
+        fileNameProvider = { key -> "user_tokens_$key" },
+        fileReader = fileReader,
+        adapter = moshi.adapter(UserTokensResponse::class.java),
+    )
+
+    override fun get(userWalletId: String): Flow<UserTokensResponse> {
+        return store.get(userWalletId)
+    }
+
+    override suspend fun getSync(userWalletId: String): UserTokensResponse? {
+        return store.getSync(userWalletId)
+    }
+
+    override suspend fun store(userWalletId: String, tokens: UserTokensResponse) {
+        store.store(userWalletId, tokens)
+    }
+}
