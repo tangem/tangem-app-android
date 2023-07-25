94759f6bd0113dbbae20e0ebcd16b80827c56503
Tangem <git@tangem.com>
2023-07-25 12:15:26 +0300

    Updated on 2026-07-07

--- data/tokens/build.gradle.kts
+++ data/tokens/build.gradle.kts
@@ -10,25 +10,35 @@ android {
 }
 
 dependencies {
+
+    /** Project - Domain */
     implementation(projects.domain.core)
     implementation(projects.domain.models)
     implementation(projects.domain.tokens)
     implementation(projects.domain.demo)
     implementation(projects.domain.wallets.models)
 
+    /** Project - Data */
     implementation(projects.core.datasource)
+    implementation(projects.data.common)
 
+    /** Project - Utils */
+    implementation(projects.core.utils)
     // FIXME: For blockchain extensions, remove after refactoring
     implementation(projects.domain.legacy)
 
-    implementation(deps.kotlin.coroutines)
-    implementation(deps.arrow.core)
+    /** Tangem SDKs */
+    implementation(deps.tangem.blockchain)
+    implementation(deps.tangem.card.core)
 
+    /** DI */
     implementation(deps.hilt.core)
     kapt(deps.hilt.kapt)
 
+    /** Other */
+    implementation(deps.kotlin.coroutines)
+    implementation(deps.arrow.core)
+    implementation(deps.moshi.kotlin)
+    implementation(deps.jodatime)
     implementation(deps.timber)
-
-    implementation(deps.tangem.blockchain)
-    implementation(deps.tangem.card.core)
 }
