83e3a45b4156df03028c4c7b78d1dfb33e7273fb
Tangem <git@tangem.com>
2023-07-25 12:15:26 +0300

    Updated on 2026-06-16

--- data/tokens/src/main/kotlin/com/tangem/data/tokens/di/TokensDataModule.kt
+++ data/tokens/src/main/kotlin/com/tangem/data/tokens/di/TokensDataModule.kt
@@ -1,11 +1,16 @@
 package com.tangem.data.tokens.di
 
+import com.tangem.data.common.cache.CacheRegistry
+import com.tangem.data.tokens.repository.DefaultTokensRepository
 import com.tangem.data.tokens.repository.MockNetworksRepository
 import com.tangem.data.tokens.repository.MockQuotesRepository
-import com.tangem.data.tokens.repository.MockTokensRepository
+import com.tangem.datasource.api.tangemTech.TangemTechApi
+import com.tangem.datasource.local.token.UserTokensStore
+import com.tangem.datasource.local.userwallet.UserWalletsStore
 import com.tangem.domain.tokens.repository.NetworksRepository
 import com.tangem.domain.tokens.repository.QuotesRepository
 import com.tangem.domain.tokens.repository.TokensRepository
+import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -18,7 +23,15 @@ internal object TokensDataModule {
 
     @Provides
     @Singleton
-    fun provideTokensRepository(): TokensRepository = MockTokensRepository()
+    fun provideTokensRepository(
+        tangemTechApi: TangemTechApi,
+        userTokensStore: UserTokensStore,
+        userWalletsStore: UserWalletsStore,
+        cacheRegistry: CacheRegistry,
+        dispatchers: CoroutineDispatcherProvider,
+    ): TokensRepository {
+        return DefaultTokensRepository(tangemTechApi, userTokensStore, userWalletsStore, cacheRegistry, dispatchers)
+    }
 
     @Provides
     @Singleton
