34fb7ce8074081bb2f8508b0bfd3ffdec8b915f5
Tangem <git@tangem.com>
2023-07-26 15:12:13 +0800

    release/5.26.1

--- app/src/main/java/com/tangem/tap/features/demo/DemoHelper.kt
+++ app/src/main/java/com/tangem/tap/features/demo/DemoHelper.kt
@@ -1,18 +1,7 @@
 package com.tangem.tap.features.demo
 
-import com.tangem.blockchain.common.Amount
-import com.tangem.blockchain.common.TransactionData
-import com.tangem.blockchain.common.TransactionSender
-import com.tangem.blockchain.common.TransactionSigner
-import com.tangem.blockchain.common.WalletManager
-import com.tangem.blockchain.common.toBlockchainSdkError
-import com.tangem.blockchain.common.transaction.Fee
-import com.tangem.blockchain.common.transaction.TransactionFee
-import com.tangem.blockchain.extensions.Result
-import com.tangem.blockchain.extensions.SimpleResult
-import com.tangem.common.CompletionResult
+import com.tangem.domain.demo.DemoConfig
 import com.tangem.domain.models.scan.ScanResponse
-import com.tangem.domain.common.demo.DemoConfig
 import com.tangem.tap.common.extensions.dispatchNotification
 import com.tangem.tap.common.redux.AppState
 import com.tangem.tap.features.details.redux.DetailsAction
@@ -22,13 +11,6 @@ import com.tangem.tap.features.wallet.redux.WalletAction
 import com.tangem.tap.store
 import com.tangem.wallet.R
 import org.rekotlin.Action
-
-/**
-* [REDACTED_AUTHOR]
- */
-interface DemoMiddleware {
-    fun tryHandle(config: DemoConfig, scanResponse: ScanResponse, action: Action): Boolean
-}
 
 object DemoHelper {
     val config = DemoConfig()
@@ -68,63 +50,10 @@ object DemoHelper {
         return false
     }
 
-    fun injectDemoBalance(walletManager: WalletManager?) {
-        val manager = walletManager ?: return
-
-        val blockchain = walletManager.wallet.blockchain
-        val amount = config.getBalance(blockchain)
-        manager.wallet.setAmount(amount)
-    }
-
     private fun getScanResponse(appState: () -> AppState?): ScanResponse? {
         val state = appState() ?: return null
 
         return state.globalState.onboardingState.onboardingManager?.scanResponse
             ?: state.globalState.scanResponse
-    }
-}
-
-class DemoTransactionSender(private val walletManager: WalletManager) : TransactionSender {
-
-    override suspend fun getFee(amount: Amount, destination: String): Result<TransactionFee> {
-        val blockchain = walletManager.wallet.blockchain
-        return Result.Success(
-            TransactionFee.Choosable(
-                minimum = Fee.Common(Amount(minimumFee, blockchain)),
-                normal = Fee.Common(Amount(normalFee, blockchain)),
-                priority = Fee.Common(Amount(priorityFee, blockchain)),
-            ),
-        )
-    }
-
-    @Suppress("MagicNumber")
-    override suspend fun send(transactionData: TransactionData, signer: TransactionSigner): SimpleResult {
-        val dataToSign = randomString(32).toByteArray()
-        val signerResponse = signer.sign(
-            hash = dataToSign,
-            publicKey = walletManager.wallet.publicKey,
-        )
-        return when (signerResponse) {
-            is CompletionResult.Success -> SimpleResult.Failure(Exception(ID).toBlockchainSdkError())
-            is CompletionResult.Failure -> SimpleResult.fromTangemSdkError(signerResponse.error)
-        }
-    }
-
-    private fun randomInt(from: Int, to: Int): Int = kotlin.random.Random.nextInt(from, to)
-
-    private fun randomString(length: Int): String {
-        val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
-        return (1..length)
-            .map { randomInt(0, charPool.size) }
-            .map(charPool::get)
-            .joinToString("")
-    }
-
-    companion object {
-        val ID = DemoTransactionSender::class.java.simpleName
-
-        private val minimumFee = 0.0001.toBigDecimal()
-        private val normalFee = 0.0002.toBigDecimal()
-        private val priorityFee = 0.0003.toBigDecimal()
     }
 }
--- domain/card/build.gradle.kts
+++ domain/card/build.gradle.kts
@@ -1,14 +1,22 @@
 plugins {
-    alias(deps.plugins.kotlin.jvm)
+    alias(deps.plugins.android.library)
+    alias(deps.plugins.kotlin.android)
     id("configuration")
 }
 
+android {
+    namespace = "com.tangem.domain.card"
+}
+
 dependencies {
+    implementation(projects.core.analytics.models)
+
+    implementation(projects.domain.demo)
     implementation(projects.domain.core)
+    implementation(projects.domain.legacy)
 // [REDACTED_TODO_COMMENT]
     implementation(projects.domain.models)
 
-    implementation(projects.core.analytics.models)
 
     implementation(deps.tangem.card.core)
 }
