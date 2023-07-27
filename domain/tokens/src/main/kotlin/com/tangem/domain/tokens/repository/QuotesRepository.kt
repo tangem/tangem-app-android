96663a0f96707b53719be5a79731dbdad3deb9b1
Tangem <git@tangem.com>
2023-07-27 11:44:04 +0300

    Updated on 2026-09-23

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/repository/QuotesRepository.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/repository/QuotesRepository.kt
@@ -12,9 +12,9 @@ interface QuotesRepository {
     /**
      * Retrieves the quotes for a set of specified cryptocurrencies, identified by their unique IDs.
      *
-     * @param tokensIds The unique identifiers of the cryptocurrencies for which quotes are to be retrieved.
+     * @param currenciesIds The unique identifiers of the cryptocurrencies for which quotes are to be retrieved.
      * @param refresh A boolean flag indicating whether the data should be refreshed.
      * @return A [Flow] emitting a set of quotes corresponding to the specified cryptocurrencies.
      */
-    fun getQuotes(tokensIds: Set<CryptoCurrency.ID>, refresh: Boolean): Flow<Set<Quote>>
+    fun getQuotes(currenciesIds: Set<CryptoCurrency.ID>, refresh: Boolean): Flow<Set<Quote>>
 }
