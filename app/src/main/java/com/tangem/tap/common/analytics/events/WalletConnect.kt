f941d6b47dd05b0cd75622a9e52f0b8125e0e1a6
Tangem <git@tangem.com>
2023-07-27 17:29:54 +0400

    Updated on 2026-04-06

--- app/src/main/java/com/tangem/tap/common/analytics/events/WalletConnect.kt
+++ app/src/main/java/com/tangem/tap/common/analytics/events/WalletConnect.kt
@@ -1,6 +1,7 @@
 package com.tangem.tap.common.analytics.events
 
 import com.tangem.core.analytics.models.AnalyticsEvent
+import com.tangem.tap.common.extensions.filterNotNull
 
 /**
 [REDACTED_AUTHOR]
@@ -12,16 +13,51 @@ sealed class WalletConnect(
 ) : AnalyticsEvent("Wallet Connect", event, params, error) {
 
     class ScreenOpened : WalletConnect(event = "WC Screen Opened")
-    class NewSessionEstablished(dAppName: String) : WalletConnect(
+    class NewSessionEstablished(dAppName: String, dAppUrl: String) : WalletConnect(
         event = "New Session Established",
         params = mapOf(
             AnalyticsParam.DAPP_NAME to dAppName,
+            AnalyticsParam.DAPP_URL to dAppUrl,
         ),
     )
 
-    class SessionDisconnected : WalletConnect("Session Disconnected")
-    class RequestSigned : WalletConnect("Request Signed")
+    class SessionDisconnected(dAppName: String, dAppUrl: String) : WalletConnect(
+        event = "Session Disconnected",
+        params = mapOf(
+            AnalyticsParam.DAPP_NAME to dAppName,
+            AnalyticsParam.DAPP_URL to dAppUrl,
+        ),
+    )
 
-    class SignError(error: Throwable) : WalletConnect("Sign", error = error)
-    class TransactionError(error: Throwable) : WalletConnect("Transaction", error = error)
+    class RequestHandled(
+        params: RequestHandledParams,
+    ) : WalletConnect(
+        event = "Request Handled",
+        params = params.toParamsMap(),
+    )
+
+    data class RequestHandledParams(
+        val dAppName: String,
+        val dAppUrl: String,
+        val methodName: String,
+        val blockchain: String,
+        val errorCode: String? = null,
+    ) {
+        fun toParamsMap(): Map<String, String> {
+            val validation = if (errorCode == null) Validation.SUCCESS.param else Validation.FAIL.param
+            return mapOf(
+                AnalyticsParam.DAPP_NAME to dAppName,
+                AnalyticsParam.DAPP_URL to dAppUrl,
+                AnalyticsParam.METHOD_NAME to methodName,
+                AnalyticsParam.BLOCKCHAIN to blockchain,
+                AnalyticsParam.VALIDATION to validation,
+                if (errorCode != null) AnalyticsParam.ERROR_CODE to errorCode else null to null,
+            ).filterNotNull()
+        }
+    }
+
+    enum class Validation(val param: String) {
+        SUCCESS("Success"),
+        FAIL("Fail"),
+    }
 }
