492f68843b2c903a7657d54d579dfc38df460151
Tangem <git@tangem.com>
2023-07-27 11:44:04 +0300

    Updated on 2026-01-24

--- data/tokens/src/main/kotlin/com/tangem/data/tokens/repository/MockQuotesRepository.kt
+++ data/tokens/src/main/kotlin/com/tangem/data/tokens/repository/MockQuotesRepository.kt
@@ -1,19 +1,23 @@
 package com.tangem.data.tokens.repository
 
-import com.tangem.data.tokens.mock.MockQuotes
 import com.tangem.domain.tokens.model.CryptoCurrency
 import com.tangem.domain.tokens.model.Quote
 import com.tangem.domain.tokens.repository.QuotesRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
+import java.math.BigDecimal
 
 internal class MockQuotesRepository : QuotesRepository {
 
-    override fun getQuotes(tokensIds: Set<CryptoCurrency.ID>, refresh: Boolean): Flow<Set<Quote>> {
+    override fun getQuotes(currenciesIds: Set<CryptoCurrency.ID>, refresh: Boolean): Flow<Set<Quote>> {
         return flowOf(
-            MockQuotes.quotes
-                .filter { it.currencyId in tokensIds }
-                .toSet(),
+            currenciesIds.map {
+                Quote(
+                    currencyId = it,
+                    fiatRate = BigDecimal.ZERO,
+                    priceChange = BigDecimal.ZERO,
+                )
+            }.toSet(),
         )
     }
 }
