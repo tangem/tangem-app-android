c27622e46dd7e8e7445ab93ecd32ae33000be848
Tangem <git@tangem.com>
2023-07-27 11:44:04 +0300

    Updated on 2026-02-11

--- core/utils/src/main/java/com/tangem/utils/converter/Converter.kt
+++ core/utils/src/main/java/com/tangem/utils/converter/Converter.kt
@@ -1,8 +1,14 @@
 package com.tangem.utils.converter
 
-interface Converter<I, O> {
+interface Converter<I : Any, O : Any?> {
+
     fun convert(value: I): O
-    fun convertList(input: List<I>): List<O> {
-        return input.map { convert(it) }
+
+    fun convertList(input: Collection<I>): List<O> {
+        return input.map(::convert)
+    }
+
+    fun convertSet(input: Collection<I>): Set<O> {
+        return input.mapTo(hashSetOf(), ::convert)
     }
 }
