79e00666d209bc0bf4491fba74a4ab6f2fa06bb7
Tangem <git@tangem.com>
2023-07-28 10:18:27 +0300

    Updated on 2025-09-17

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/ApplyTokenListSortingUseCase.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/ApplyTokenListSortingUseCase.kt
@@ -7,10 +7,12 @@ import arrow.core.raise.either
 import arrow.core.raise.ensureNotNull
 import arrow.core.toNonEmptySetOrNull
 import com.tangem.domain.tokens.error.TokenListSortingError
+import com.tangem.domain.tokens.model.Network
 import com.tangem.domain.tokens.model.Token
 import com.tangem.domain.tokens.repository.TokensRepository
 import com.tangem.domain.wallets.models.UserWalletId
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.withContext
 
 class ApplyTokenListSortingUseCase(
@@ -20,18 +22,57 @@ class ApplyTokenListSortingUseCase(
 
     suspend operator fun invoke(
         userWalletId: UserWalletId,
-        sortedTokens: Set<Token>,
-        isGrouped: Boolean,
+        sortedTokensIds: Set<Pair<Network.ID, Token.ID>>,
+        isGroupedByNetwork: Boolean,
         isSortedByBalance: Boolean,
     ): Either<TokenListSortingError, Unit> {
         return withContext(dispatchers.default) {
             either {
-                val nonEmptyTokens = ensureNotNull(sortedTokens.toNonEmptySetOrNull()) {
-                    TokenListSortingError.TokenListIsEmpty
-                }
-
-                applySorting(userWalletId, nonEmptyTokens, isGrouped, isSortedByBalance)
+                applySorting(
+                    userWalletId = userWalletId,
+                    tokens = sortTokens(sortedTokensIds, getTokens(userWalletId)),
+                    isGrouped = isGroupedByNetwork,
+                    isSortedByBalance = isSortedByBalance,
+                )
             }
+        }
+    }
+
+    private suspend fun Raise<TokenListSortingError>.sortTokens(
+        sortedTokensIds: Set<Pair<Network.ID, Token.ID>>,
+        unsortedTokens: Set<Token>,
+    ): Set<Token> = withContext(dispatchers.default) {
+        val nonEmptySortedTokensIds = ensureNotNull(sortedTokensIds.toNonEmptySetOrNull()) {
+            TokenListSortingError.TokenListIsEmpty
+        }
+
+        val sortedTokens = sortedMapOf<Int, Token>()
+
+        unsortedTokens.forEach { token ->
+            val index = nonEmptySortedTokensIds.indexOfFirst { (networkId, tokenId) ->
+                networkId == token.networkId && tokenId == token.id
+            }
+
+            if (index >= 0) {
+                sortedTokens[index] = token
+            } else {
+                raise(TokenListSortingError.UnableToSortTokenList)
+            }
+        }
+
+        ensureNotNull(sortedTokens.values.toNonEmptySetOrNull()) {
+            TokenListSortingError.TokenListIsEmpty
+        }
+    }
+
+    private suspend fun Raise<TokenListSortingError>.getTokens(userWalletId: UserWalletId): Set<Token> {
+        val tokens = catch(
+            block = { tokensRepository.getMultiCurrencyWalletTokens(userWalletId, refresh = false).firstOrNull() },
+            catch = { raise(TokenListSortingError.DataError(it)) },
+        )
+
+        return ensureNotNull(tokens?.toNonEmptySetOrNull()) {
+            TokenListSortingError.TokenListIsEmpty
         }
     }
 
