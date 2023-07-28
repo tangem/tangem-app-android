e8eb5d88f8b555b29edf1752096516f31f6d811d
Tangem <git@tangem.com>
2023-07-28 10:50:24 +0300

    Updated on 2026-09-01

---
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/organizetokens/utils/converter/error/TokenListSortingErrorConverter.kt
@@ -0,0 +1,18 @@
+package com.tangem.feature.wallet.presentation.organizetokens.utils.converter.error
+
+import com.tangem.common.Provider
+import com.tangem.domain.tokens.error.TokenListSortingError
+import com.tangem.feature.wallet.presentation.organizetokens.OrganizeTokensState
+import com.tangem.feature.wallet.presentation.organizetokens.utils.converter.InProgressStateConverter
+import com.tangem.utils.converter.Converter
+
+internal class TokenListSortingErrorConverter(
+    private val currentState: Provider<OrganizeTokensState>,
+    private val inProgressStateConverter: InProgressStateConverter,
+) : Converter<TokenListSortingError, OrganizeTokensState> {
+
+    // TODO: [REDACTED_JIRA]
+    override fun convert(value: TokenListSortingError): OrganizeTokensState {
+        return inProgressStateConverter.convertBack(currentState())
+    }
+}
