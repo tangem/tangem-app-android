46b6ac04e3fe4c7f86214269a239d85f1e6e9b20
Tangem <git@tangem.com>
2023-07-31 19:44:09 +0300

    Updated on 2026-02-11

--- data/tokens/src/main/kotlin/com/tangem/data/tokens/utils/ResponseTokensFactory.kt
+++ data/tokens/src/main/kotlin/com/tangem/data/tokens/utils/ResponseTokensFactory.kt
@@ -2,11 +2,9 @@ package com.tangem.data.tokens.utils
 
 import com.tangem.blockchain.common.Blockchain
 import com.tangem.datasource.api.tangemTech.models.UserTokensResponse
-import com.tangem.domain.common.TapWorkarounds.derivationStyle
 import com.tangem.domain.common.extensions.fromNetworkId
 import com.tangem.domain.demo.DemoConfig
 import com.tangem.domain.models.scan.CardDTO
-import com.tangem.domain.tokens.model.Network
 import com.tangem.domain.tokens.model.Token
 import timber.log.Timber
 import com.tangem.blockchain.common.Token as SdkToken
@@ -29,17 +27,22 @@ internal class ResponseTokensFactory(private val demoConfig: DemoConfig) {
         }
 
         val sdkToken = createSdkToken(token)
+        val (tokenId, iconUrl) = if (sdkToken == null) {
+            getCoinId(blockchain) to getCoinIconUrl(blockchain)
+        } else {
+            getTokenId(blockchain, sdkToken) to getTokenIconUrl(blockchain, sdkToken)
+        }
 
         return Token(
-            id = getTokenId(sdkToken, blockchain),
-            networkId = Network.ID(blockchain.id),
+            id = tokenId,
+            networkId = getNetworkId(blockchain),
             name = token.name,
             symbol = token.symbol,
             decimals = token.decimals,
-            iconUrl = getCoinOrTokenIconUrl(sdkToken, blockchain),
+            iconUrl = iconUrl,
             contractAddress = token.contractAddress,
             derivationPath = token.derivationPath,
-            isCustom = isCustomToken(token.id, token.derivationPath, card.derivationStyle, blockchain),
+            isCustom = isCustomToken(tokenId),
         )
     }
 
