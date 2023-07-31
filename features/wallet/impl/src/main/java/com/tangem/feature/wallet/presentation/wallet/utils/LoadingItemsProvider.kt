e4b40e5f49757bed32864129016d90a1060d96dd
Mama1emon <andrew.khokhlove@gmail.com>
2023-07-31 15:02:33 +0800

    AND-3758 Change WalletStateHolder because of txhistory items have flow type

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/utils/LoadingItemsProvider.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/utils/LoadingItemsProvider.kt
@@ -1,15 +1,15 @@
 package com.tangem.feature.wallet.presentation.wallet.utils
 
 import com.tangem.feature.wallet.presentation.common.state.TokenItemState
-import com.tangem.feature.wallet.presentation.wallet.state.WalletContentItemState.MultiCurrencyItem
-import kotlinx.collections.immutable.PersistentList
-import kotlinx.collections.immutable.toPersistentList
+import com.tangem.feature.wallet.presentation.wallet.state.content.WalletTokensListState
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
 
 internal object LoadingItemsProvider {
 
-    fun getLoadingMultiCurrencyTokens(): PersistentList<MultiCurrencyItem> {
-        return List(size = 5) { TokenItemState.Loading }
-            .map { MultiCurrencyItem.Token(it) }
-            .toPersistentList()
+    fun getLoadingMultiCurrencyTokens(): ImmutableList<WalletTokensListState.TokensListItemState.Token> {
+        return buildList(capacity = 5) {
+            add(WalletTokensListState.TokensListItemState.Token(state = TokenItemState.Loading))
+        }.toImmutableList()
     }
 }
