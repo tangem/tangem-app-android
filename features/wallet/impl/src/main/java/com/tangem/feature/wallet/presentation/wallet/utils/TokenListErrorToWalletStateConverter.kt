dd9c97bf5c1042974a4a37d4c2acd4786e72495b
Mama1emon <andrew.khokhlove@gmail.com>
2023-07-31 15:37:49 +0800

    AND-3758 Use GetTransactionUseCase

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/utils/TokenListErrorToWalletStateConverter.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/utils/TokenListErrorToWalletStateConverter.kt
@@ -1,15 +1,16 @@
 package com.tangem.feature.wallet.presentation.wallet.utils
 
+import com.tangem.common.Provider
 import com.tangem.domain.tokens.error.TokenListError
 import com.tangem.feature.wallet.presentation.wallet.state.WalletStateHolder
 import com.tangem.utils.converter.Converter
 
 internal class TokenListErrorToWalletStateConverter(
-    private val currentState: WalletStateHolder,
+    private val currentStateProvider: Provider<WalletStateHolder>,
 ) : Converter<TokenListError, WalletStateHolder> {
 
     // TODO: https://tangem.atlassian.net/browse/AND-4021
     override fun convert(value: TokenListError): WalletStateHolder {
-        return currentState
+        return currentStateProvider()
     }
 }
