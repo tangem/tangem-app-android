8e3ad11beb11aba252774754625789fa88125db6
Tangem <git@tangem.com>
2023-07-31 19:44:09 +0300

    Updated on 2026-08-19

--- data/tokens/src/main/kotlin/com/tangem/data/tokens/utils/TokensOperations.kt
+++ data/tokens/src/main/kotlin/com/tangem/data/tokens/utils/TokensOperations.kt
@@ -1,59 +1,26 @@
 package com.tangem.data.tokens.utils
 
 import com.tangem.blockchain.common.Blockchain
-import com.tangem.blockchain.common.DerivationStyle
 import com.tangem.blockchain.common.IconsUtil
 import com.tangem.domain.common.TapWorkarounds.derivationStyle
 import com.tangem.domain.common.extensions.toCoinId
+import com.tangem.domain.common.extensions.toNetworkId
 import com.tangem.domain.models.scan.CardDTO
-import com.tangem.domain.tokens.model.Token
+import com.tangem.domain.tokens.model.Network
 import com.tangem.blockchain.common.Token as SdkToken
+import com.tangem.domain.tokens.model.Token as DomainToken
 
 private const val DEFAULT_TOKENS_ICONS_HOST = "https://s3.eu-central-1.amazonaws.com/tangem.api/coins"
 private const val TOKEN_ICON_SIZE = "large"
 private const val TOKEN_ICON_EXT = "png"
 
-internal fun isCustomToken(
-    tokenId: String?,
-    tokenDerivationPath: String?,
-    cardDerivationStyle: DerivationStyle?,
-    blockchain: Blockchain,
-): Boolean {
-    if (tokenId == null) return true
+private const val COIN_ID_PREFIX = "coin_"
+private const val TOKEN_ID_PREFIX = "token_"
+private const val CUSTOM_TOKEN_ID_PREFIX = "custom_token_"
+private const val TOKEN_ID_DELIMITER = '#'
 
-    if (tokenDerivationPath == null || cardDerivationStyle == null) return false
-
-    return tokenDerivationPath != blockchain.derivationPath(cardDerivationStyle)?.rawPath
-}
-
-internal fun getTokenId(token: SdkToken?, blockchain: Blockchain): Token.ID {
-    val tokenId = token?.id
-
-    return when {
-        token == null -> getCoinId(blockchain)
-        tokenId == null -> getCustomTokenId(token.contractAddress, blockchain)
-        else -> Token.ID(tokenId)
-    }
-}
-
-internal fun getCoinId(blockchain: Blockchain): Token.ID {
-    val value = blockchain.toCoinId()
-
-    return Token.ID(value)
-}
-
-internal fun getCoinOrTokenIconUrl(token: SdkToken?, blockchain: Blockchain): String? {
-    val tokenId = token?.id
-
-    return when {
-        token == null -> getCoinIconId(blockchain)
-        tokenId == null -> IconsUtil.getTokenIconUri(blockchain, token)?.toString()
-        else -> getTokenIconUrlFromDefaultHost(tokenId)
-    }
-}
-
-internal fun getCoinIconId(blockchain: Blockchain): String {
-    return getTokenIconUrlFromDefaultHost(blockchain.toCoinId())
+internal fun isCustomToken(tokenId: DomainToken.ID): Boolean {
+    return tokenId.value.startsWith(CUSTOM_TOKEN_ID_PREFIX)
 }
 
 internal fun getDerivationPath(blockchain: Blockchain, card: CardDTO): String? {
@@ -64,12 +31,75 @@ internal fun getDerivationPath(blockchain: Blockchain, card: CardDTO): String? {
     }
 }
 
-private fun getCustomTokenId(contractAddress: String, blockchain: Blockchain): Token.ID {
-    val value = "custom_${blockchain.id}_$contractAddress"
+internal fun getBlockchain(networkId: Network.ID): Blockchain {
+    return Blockchain.fromId(networkId.value)
+}
 
-    return Token.ID(value)
+internal fun getNetworkId(blockchain: Blockchain): Network.ID {
+    val value = blockchain.id
+
+    return Network.ID(value)
+}
+
+internal fun getCoinId(blockchain: Blockchain): DomainToken.ID {
+    return getTokenOrCoinId(blockchain, token = null)
+}
+
+internal fun getTokenId(blockchain: Blockchain, token: SdkToken): DomainToken.ID {
+    return getTokenOrCoinId(blockchain, token)
+}
+
+internal fun getResponseTokenId(token: DomainToken): String? {
+    return token.id.value.substringAfter(TOKEN_ID_DELIMITER)
+        .takeUnless { token.isCustom }
+}
+
+internal fun getTokenIconUrl(blockchain: Blockchain, token: SdkToken): String? {
+    val tokenId = token.id
+
+    return if (tokenId == null) {
+        IconsUtil.getTokenIconUri(blockchain, token)?.toString()
+    } else {
+        getTokenIconUrlFromDefaultHost(tokenId)
+    }
+}
+
+internal fun getCoinIconUrl(blockchain: Blockchain): String? {
+    val coinId = when (blockchain) {
+        Blockchain.Unknown -> null
+        Blockchain.TerraV1, Blockchain.TerraV2 -> blockchain.toCoinId()
+        else -> blockchain.toNetworkId()
+    }
+
+    return coinId?.let(::getTokenIconUrlFromDefaultHost)
+}
+
+private fun getTokenOrCoinId(blockchain: Blockchain, token: SdkToken?): DomainToken.ID {
+    val sdkTokenId = token?.id
+    val (prefix, suffix) = when {
+        token == null -> COIN_ID_PREFIX to blockchain.toCoinId()
+        sdkTokenId == null -> CUSTOM_TOKEN_ID_PREFIX to token.contractAddress
+        else -> TOKEN_ID_PREFIX to sdkTokenId
+    }
+
+    val value = buildString {
+        append(prefix)
+        append(blockchain.id)
+        append(TOKEN_ID_DELIMITER)
+        append(suffix.lowercase())
+    }
+
+    return DomainToken.ID(value)
 }
 
 private fun getTokenIconUrlFromDefaultHost(tokenId: String): String {
-    return "$DEFAULT_TOKENS_ICONS_HOST/$TOKEN_ICON_SIZE/$tokenId.$TOKEN_ICON_EXT"
+    return buildString {
+        append(DEFAULT_TOKENS_ICONS_HOST)
+        append('/')
+        append(TOKEN_ICON_SIZE)
+        append('/')
+        append(tokenId)
+        append('.')
+        append(TOKEN_ICON_EXT)
+    }
 }
