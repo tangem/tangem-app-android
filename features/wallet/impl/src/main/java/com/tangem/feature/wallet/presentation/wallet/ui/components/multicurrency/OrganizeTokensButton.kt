c27d0d761713cb612ee6e993600ad33939da4284
Tangem <git@tangem.com>
2023-08-01 15:25:12 +0800

    Updated on 2026-09-23

---
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/components/multicurrency/OrganizeTokensButton.kt
@@ -0,0 +1,29 @@
+package com.tangem.feature.wallet.presentation.wallet.ui.components.multicurrency
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.tangem.core.ui.components.buttons.actions.ActionButtonConfig
+import com.tangem.core.ui.components.buttons.actions.RoundedActionButton
+import com.tangem.feature.wallet.impl.R
+
+/**
+ * Organize tokens button
+ *
+ * @param onClick  callback, if null button is disabled
+ * @param modifier modifier
+ *
+[REDACTED_AUTHOR]
+ */
+@Composable
+internal fun OrganizeTokensButton(onClick: (() -> Unit)?, modifier: Modifier = Modifier) {
+    RoundedActionButton(
+        config = ActionButtonConfig(
+            text = stringResource(id = R.string.organize_tokens_title),
+            iconResId = R.drawable.ic_filter_24,
+            onClick = onClick ?: {},
+            enabled = onClick != null,
+        ),
+        modifier = modifier,
+    )
+}
