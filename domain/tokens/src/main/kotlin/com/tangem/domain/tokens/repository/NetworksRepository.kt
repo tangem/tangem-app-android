1c0fa789e08b08d2fade03370001c0530e64c049
Tangem <git@tangem.com>
2023-08-01 14:31:41 +0300

    Updated on 2026-09-23

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/repository/NetworksRepository.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/repository/NetworksRepository.kt
@@ -11,8 +11,22 @@ import kotlinx.coroutines.flow.Flow
  * */
 interface NetworksRepository {
 
+    /**
+     * Retrieves the details of the specified blockchain networks, identified by their unique IDs.
+     *
+     * @param networksIds The unique identifiers of the networks to be retrieved.
+     * @return A set of [Network] objects corresponding to the specified network IDs.
+     */
     fun getNetworks(networksIds: Set<Network.ID>): Set<Network>
 
+    /**
+     * Retrieves the statuses of specified blockchain networks for a specific user wallet.
+     *
+     * @param userWalletId The unique identifier of the user wallet.
+     * @param networks A map of network IDs to sets of cryptocurrency IDs, representing the networks for which statuses are to be retrieved.
+     * @param refresh A boolean flag indicating whether the data should be refreshed.
+     * @return A [Flow] emitting a set of [NetworkStatus] objects corresponding to the specified networks.
+     */
     fun getNetworkStatuses(
         userWalletId: UserWalletId,
         networks: Map<Network.ID, Set<CryptoCurrency.ID>>,
