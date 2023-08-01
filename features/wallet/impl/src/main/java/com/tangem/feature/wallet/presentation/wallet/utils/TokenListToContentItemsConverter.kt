cd65cb9fec8285ac4701dc72efbfcd66164c6003
Tangem <git@tangem.com>
2023-08-01 15:25:12 +0800

    Updated on 2026-09-25

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/utils/TokenListToContentItemsConverter.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/utils/TokenListToContentItemsConverter.kt
@@ -3,10 +3,11 @@ package com.tangem.feature.wallet.presentation.wallet.utils
 import com.tangem.domain.tokens.model.NetworkGroup
 import com.tangem.domain.tokens.model.TokenList
 import com.tangem.domain.tokens.model.TokenStatus
-import com.tangem.feature.wallet.presentation.wallet.state.WalletContentItemState.MultiCurrencyItem
+import com.tangem.feature.wallet.presentation.wallet.state.content.WalletTokensListState
+import com.tangem.feature.wallet.presentation.wallet.state.content.WalletTokensListState.TokensListItemState
 import com.tangem.feature.wallet.presentation.wallet.utils.LoadingItemsProvider.getLoadingMultiCurrencyTokens
+import com.tangem.feature.wallet.presentation.wallet.viewmodels.WalletClickCallbacks
 import com.tangem.utils.converter.Converter
-import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.PersistentList
 import kotlinx.collections.immutable.mutate
 import kotlinx.collections.immutable.persistentListOf
@@ -15,36 +16,44 @@ internal class TokenListToContentItemsConverter(
     isWalletContentHidden: Boolean,
     fiatCurrencyCode: String,
     fiatCurrencySymbol: String,
-) : Converter<TokenList, ImmutableList<MultiCurrencyItem>> {
+    private val clickCallbacks: WalletClickCallbacks,
+) : Converter<TokenList, WalletTokensListState> {
 
     private val tokenStatusConverter = TokenStatusToTokenItemConverter(
-        isWalletContentHidden,
-        fiatCurrencyCode,
-        fiatCurrencySymbol,
+        isWalletContentHidden = isWalletContentHidden,
+        fiatCurrencyCode = fiatCurrencyCode,
+        fiatCurrencySymbol = fiatCurrencySymbol,
     )
 
-    override fun convert(value: TokenList): ImmutableList<MultiCurrencyItem> {
-        return when (value) {
-            is TokenList.GroupedByNetwork -> value.mapToMultiCurrencyItems()
-            is TokenList.Ungrouped -> value.mapToMultiCurrencyItems()
-            is TokenList.NotInitialized -> getLoadingMultiCurrencyTokens()
-        }
+    override fun convert(value: TokenList): WalletTokensListState {
+        return WalletTokensListState.Content(
+            items = when (value) {
+                is TokenList.GroupedByNetwork -> value.mapToMultiCurrencyItems()
+                is TokenList.Ungrouped -> value.mapToMultiCurrencyItems()
+                is TokenList.NotInitialized -> getLoadingMultiCurrencyTokens()
+            },
+            onOrganizeTokensClick = if (value.totalFiatBalance is TokenList.FiatBalance.Loaded) {
+                clickCallbacks::onOrganizeTokensClick
+            } else {
+                null
+            },
+        )
     }
 
-    private fun TokenList.GroupedByNetwork.mapToMultiCurrencyItems(): PersistentList<MultiCurrencyItem> {
+    private fun TokenList.GroupedByNetwork.mapToMultiCurrencyItems(): PersistentList<TokensListItemState> {
         return groups.fold(initial = persistentListOf()) { acc, group ->
             acc.mutate { it.addGroup(group) }
         }
     }
 
-    private fun TokenList.Ungrouped.mapToMultiCurrencyItems(): PersistentList<MultiCurrencyItem> {
+    private fun TokenList.Ungrouped.mapToMultiCurrencyItems(): PersistentList<TokensListItemState> {
         return tokens.fold(initial = persistentListOf()) { acc, token ->
             acc.mutate { it.addToken(token) }
         }
     }
 
-    private fun MutableList<MultiCurrencyItem>.addGroup(group: NetworkGroup): List<MultiCurrencyItem> {
-        this.add(MultiCurrencyItem.NetworkGroupTitle(group.network.name))
+    private fun MutableList<TokensListItemState>.addGroup(group: NetworkGroup): List<TokensListItemState> {
+        this.add(TokensListItemState.NetworkGroupTitle(group.network.name))
 
         group.tokens.forEach { token ->
             this.addToken(token)
@@ -53,10 +62,10 @@ internal class TokenListToContentItemsConverter(
         return this
     }
 
-    private fun MutableList<MultiCurrencyItem>.addToken(token: TokenStatus): List<MultiCurrencyItem> {
+    private fun MutableList<TokensListItemState>.addToken(token: TokenStatus): List<TokensListItemState> {
         val tokenItemState = tokenStatusConverter.convert(token)
 
-        this.add(MultiCurrencyItem.Token(tokenItemState))
+        this.add(TokensListItemState.Token(tokenItemState))
 
         return this
     }
