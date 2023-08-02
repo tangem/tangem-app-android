5e281f274868a36a82151962fcc5a2fba87443a1
Tangem <git@tangem.com>
2023-08-02 16:20:16 +0300

    Updated on 2026-02-11

---
+++ domain/legacy/src/main/java/com/tangem/domain/walletmanager/WalletManagerFacade.kt
@@ -0,0 +1,27 @@
+package com.tangem.domain.walletmanager
+
+import com.tangem.domain.tokens.model.CryptoCurrency
+import com.tangem.domain.tokens.model.Network
+import com.tangem.domain.walletmanager.model.UpdateWalletManagerResult
+import com.tangem.domain.wallets.models.UserWalletId
+
+// TODO: Move to its own module
+/**
+ * A facade for managing wallets.
+ */
+interface WalletManagerFacade {
+
+    /**
+     * Updates the wallet manager associated with a user's wallet and network.
+     *
+     * @param userWalletId The ID of the user's wallet.
+     * @param networkId The network ID.
+     * @param extraTokens Additional tokens.
+     * @return The result of updating the wallet manager.
+     */
+    suspend fun update(
+        userWalletId: UserWalletId,
+        networkId: Network.ID,
+        extraTokens: Set<CryptoCurrency.Token>,
+    ): UpdateWalletManagerResult
+}
