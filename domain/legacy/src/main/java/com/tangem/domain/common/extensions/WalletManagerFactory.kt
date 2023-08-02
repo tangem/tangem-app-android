968ed78ff869d1e40c06e21f7db69165a053707f
Tangem <git@tangem.com>
2023-08-02 16:20:16 +0300

    Updated on 2026-07-23

--- domain/legacy/src/main/java/com/tangem/domain/common/extensions/WalletManagerFactory.kt
+++ domain/legacy/src/main/java/com/tangem/domain/common/extensions/WalletManagerFactory.kt
@@ -1,17 +1,14 @@
-package com.tangem.tap.domain.extensions
+package com.tangem.domain.common.extensions
 
 import com.tangem.blockchain.common.*
 import com.tangem.common.card.EllipticCurve
 import com.tangem.common.extensions.hexToBytes
 import com.tangem.common.extensions.toMapKey
-import com.tangem.crypto.hdWallet.DerivationPath
-import com.tangem.domain.common.BlockchainNetwork
 import com.tangem.domain.common.TapWorkarounds.isTestCard
 import com.tangem.domain.common.TapWorkarounds.useOldStyleDerivation
 import com.tangem.domain.common.util.cardTypesResolver
 import com.tangem.domain.models.scan.CardDTO
 import com.tangem.domain.models.scan.ScanResponse
-import com.tangem.tap.features.wallet.models.Currency
 
 fun WalletManagerFactory.makeWalletManagerForApp(
     scanResponse: ScanResponse,
@@ -38,7 +35,7 @@ fun WalletManagerFactory.makeWalletManagerForApp(
                 curve = wallet.curve,
             )
         }
-        scanResponse.card.isHdWalletAllowedByApp && seedKey != null && derivationParams != null -> {
+        scanResponse.card.settings.isHDWalletAllowed && seedKey != null && derivationParams != null -> {
             val derivedKeys = scanResponse.derivedKeys[wallet.publicKey.toMapKey()]
             val derivationPath = when (derivationParams) {
                 is DerivationParams.Default -> blockchain.derivationPath(derivationParams.style)
@@ -64,23 +61,8 @@ fun WalletManagerFactory.makeWalletManagerForApp(
     }
 }
 
-fun WalletManagerFactory.makeWalletManagerForApp(
-    scanResponse: ScanResponse,
-    blockchainNetwork: BlockchainNetwork,
-): WalletManager? {
-    return makeWalletManagerForApp(
-        scanResponse,
-        blockchain = blockchainNetwork.blockchain,
-        derivationParams = getDerivationParams(blockchainNetwork.derivationPath, scanResponse.card),
-    )
-}
-
-private fun getDerivationParams(derivationPath: String?, card: CardDTO): DerivationParams? {
-    return derivationPath?.let {
-        DerivationParams.Custom(
-            DerivationPath(it),
-        )
-    } ?: if (!card.settings.isHDWalletAllowed) {
+private fun getDerivationParams(card: CardDTO): DerivationParams? {
+    return if (!card.settings.isHDWalletAllowed) {
         null
     } else if (card.useOldStyleDerivation) {
         DerivationParams.Default(DerivationStyle.LEGACY)
@@ -89,30 +71,13 @@ private fun getDerivationParams(derivationPath: String?, card: CardDTO): Derivat
     }
 }
 
-fun WalletManagerFactory.makeWalletManagerForApp(scanResponse: ScanResponse, currency: Currency): WalletManager? {
-    return makeWalletManagerForApp(
-        scanResponse,
-        blockchain = currency.blockchain,
-        derivationParams = getDerivationParams(currency.derivationPath, scanResponse.card),
-    )
-}
-
-fun WalletManagerFactory.makeWalletManagersForApp(
-    scanResponse: ScanResponse,
-    blockchains: List<Currency>,
-): List<WalletManager> {
-    return blockchains
-        .filter { it.isBlockchain() }
-        .mapNotNull { this.makeWalletManagerForApp(scanResponse, it) }
-}
-
 fun WalletManagerFactory.makePrimaryWalletManager(scanResponse: ScanResponse): WalletManager? {
     val blockchain = if (scanResponse.card.isTestCard) {
         scanResponse.cardTypesResolver.getBlockchain().getTestnetVersion() ?: return null
     } else {
         scanResponse.cardTypesResolver.getBlockchain()
     }
-    val derivationParams = getDerivationParams(null, scanResponse.card)
+    val derivationParams = getDerivationParams(scanResponse.card)
     return makeWalletManagerForApp(
         scanResponse = scanResponse,
         blockchain = blockchain,
