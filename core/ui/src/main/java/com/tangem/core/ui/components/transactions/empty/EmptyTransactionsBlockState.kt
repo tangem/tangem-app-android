97ec69932c8e68b57c4faae01f33dbf27c5e145f
Tangem <git@tangem.com>
2023-08-03 22:59:39 +0800

    Updated on 2026-08-07

--- core/ui/src/main/java/com/tangem/core/ui/components/transactions/empty/EmptyTransactionsBlockState.kt
+++ core/ui/src/main/java/com/tangem/core/ui/components/transactions/empty/EmptyTransactionsBlockState.kt
@@ -1,37 +1,45 @@
 package com.tangem.core.ui.components.transactions.empty
 
-import androidx.annotation.DrawableRes
 import com.tangem.core.ui.R
 import com.tangem.core.ui.components.buttons.actions.ActionButtonConfig
 import com.tangem.core.ui.extensions.TextReference
 
-sealed class EmptyTransactionsBlockState {
+sealed class EmptyTransactionsBlockState(
+    val iconRes: Int,
+    val text: TextReference,
+    val actionButtonConfig: ActionButtonConfig,
+) {
 
-    @get:DrawableRes
-    abstract val iconRes: Int
+    class FailedToLoad(onClick: () -> Unit) : EmptyTransactionsBlockState(
+        actionButtonConfig = ActionButtonConfig(
+            text = TextReference.Res(R.string.common_reload),
+            iconResId = R.drawable.ic_refresh_24,
+            onClick = onClick,
+            enabled = true,
+        ),
+        iconRes = R.drawable.ic_alert_history_64,
+        text = TextReference.Res(R.string.transaction_history_error_failed_to_load),
+    )
 
-    abstract val text: TextReference
+    class Empty(onClick: (() -> Unit)?) : EmptyTransactionsBlockState(
+        actionButtonConfig = ActionButtonConfig(
+            text = TextReference.Res(R.string.common_buy),
+            iconResId = R.drawable.ic_plus_24,
+            onClick = onClick ?: {},
+            enabled = onClick != null,
+        ),
+        iconRes = R.drawable.img_coin_64,
+        text = TextReference.Res(R.string.transaction_history_empty_transactions),
+    )
 
-    abstract val actionButtonConfig: ActionButtonConfig
-
-    data class FailedToLoad(
-        override val actionButtonConfig: ActionButtonConfig,
-    ) : EmptyTransactionsBlockState() {
-        override val iconRes: Int = R.drawable.ic_alert_history_64
-        override val text: TextReference = TextReference.Res(R.string.transaction_history_error_failed_to_load)
-    }
-
-    data class Empty(
-        override val actionButtonConfig: ActionButtonConfig,
-    ) : EmptyTransactionsBlockState() {
-        override val iconRes: Int = R.drawable.img_coin_64
-        override val text: TextReference = TextReference.Res(R.string.transaction_history_empty_transactions)
-    }
-
-    data class NotImplemented(
-        override val actionButtonConfig: ActionButtonConfig,
-    ) : EmptyTransactionsBlockState() {
-        override val iconRes: Int = R.drawable.ic_compass_64
-        override val text: TextReference = TextReference.Res(R.string.transaction_history_not_supported_description)
-    }
+    class NotImplemented(onClick: () -> Unit) : EmptyTransactionsBlockState(
+        actionButtonConfig = ActionButtonConfig(
+            text = TextReference.Res(R.string.common_explore_transaction_history),
+            iconResId = R.drawable.ic_arrow_top_right_24,
+            onClick = onClick,
+            enabled = true,
+        ),
+        iconRes = R.drawable.ic_compass_64,
+        text = TextReference.Res(R.string.transaction_history_not_supported_description),
+    )
 }
