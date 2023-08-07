f1d3eb1b45e4878cf35fa81950321ce0d2686d10
Tangem <git@tangem.com>
2023-08-07 18:09:33 +0800

    Updated on 2026-09-14

---
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/components/common/WalletNotifications.kt
@@ -0,0 +1,20 @@
+package com.tangem.feature.wallet.presentation.wallet.ui.components.common
+
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.Modifier
+import com.tangem.core.ui.components.notifications.Notification
+import com.tangem.feature.wallet.presentation.wallet.state.WalletNotification
+import kotlinx.collections.immutable.ImmutableList
+
+/**
+ * Wallet notifications
+ *
+ * @param configs  list of notifications
+ * @param modifier modifier
+ *
+[REDACTED_AUTHOR]
+ */
+internal fun LazyListScope.notifications(configs: ImmutableList<WalletNotification>, modifier: Modifier = Modifier) {
+    items(items = configs, itemContent = { Notification(state = it.state, modifier = modifier) })
+}
