5267c44e8e0f9b6837b2e69ee76b6a2f724dee2c
Tangem <git@tangem.com>
2023-08-07 18:09:33 +0800

    Updated on 2025-10-01

---
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/components/singlecurrency/SingleCurrencyMarketPriceBlock.kt
@@ -0,0 +1,18 @@
+package com.tangem.feature.wallet.presentation.wallet.ui.components.singlecurrency
+
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.ui.Modifier
+import com.tangem.core.ui.components.marketprice.MarketPriceBlock
+import com.tangem.core.ui.components.marketprice.MarketPriceBlockState
+
+/**
+ * Single currency market price block
+ *
+ * @param state    component state
+ * @param modifier modifier
+ *
+[REDACTED_AUTHOR]
+ */
+internal fun LazyListScope.marketPriceBlock(state: MarketPriceBlockState, modifier: Modifier = Modifier) {
+    item { MarketPriceBlock(state = state, modifier = modifier) }
+}
