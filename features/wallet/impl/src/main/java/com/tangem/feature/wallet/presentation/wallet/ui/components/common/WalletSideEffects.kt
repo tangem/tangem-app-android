2b2195bb8d01410c1e56ece90ab9522b048f1466
Tangem <git@tangem.com>
2023-08-07 18:09:33 +0800

    Updated on 2026-06-24

---
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/components/common/WalletSideEffects.kt
@@ -0,0 +1,36 @@
+package com.tangem.feature.wallet.presentation.wallet.ui.components.common
+
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.snapshotFlow
+import com.tangem.feature.wallet.presentation.wallet.state.WalletsListConfig
+import com.tangem.feature.wallet.presentation.wallet.ui.utils.ScrollOffsetCollector
+
+/**
+ * Wallet screen side effects
+ *
+ * @param lazyListState     lazy list state
+ * @param walletsListConfig wallets list config
+ *
+[REDACTED_AUTHOR]
+ */
+@Composable
+internal fun WalletSideEffects(lazyListState: LazyListState, walletsListConfig: WalletsListConfig) {
+    LaunchedEffect(key1 = walletsListConfig.selectedWalletIndex) {
+        lazyListState.scrollToItem(walletsListConfig.selectedWalletIndex)
+    }
+
+    val dragInteraction = lazyListState.interactionSource.interactions.collectAsState(initial = null)
+    LaunchedEffect(key1 = lazyListState, key2 = walletsListConfig.onWalletChange) {
+        snapshotFlow { lazyListState.layoutInfo.visibleItemsInfo }
+            .collect(
+                collector = ScrollOffsetCollector(
+                    lazyListState = lazyListState,
+                    dragInteraction = dragInteraction,
+                    callback = walletsListConfig.onWalletChange,
+                ),
+            )
+    }
+}
