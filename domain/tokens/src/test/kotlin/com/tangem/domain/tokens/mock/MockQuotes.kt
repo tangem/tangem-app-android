0359fa78d745a4477ed9afe461c62fc1b0c013fa
iiiburnyiii <iiiburnyiii@gmail.com>
2023-08-09 18:29:10 +0300

    AND-4049 Improved the cryptocurrency quote model

--- domain/tokens/src/test/kotlin/com/tangem/domain/tokens/mock/MockQuotes.kt
+++ domain/tokens/src/test/kotlin/com/tangem/domain/tokens/mock/MockQuotes.kt
@@ -8,61 +8,61 @@ import java.math.BigDecimal
 internal object MockQuotes {
 
     val quote1 = Quote(
-        currencyId = MockTokens.token1.id,
+        rawCurrencyId = MockTokens.token1.id.rawCurrencyId!!,
         fiatRate = BigDecimal("1.23"),
         priceChange = BigDecimal("0.01"),
     )
 
     val quote2 = Quote(
-        currencyId = MockTokens.token2.id,
+        rawCurrencyId = MockTokens.token2.id.rawCurrencyId!!,
         fiatRate = BigDecimal("2.34"),
         priceChange = BigDecimal("-0.02"),
     )
 
     val quote3 = Quote(
-        currencyId = MockTokens.token3.id,
+        rawCurrencyId = MockTokens.token3.id.rawCurrencyId!!,
         fiatRate = BigDecimal("3.45"),
         priceChange = BigDecimal("0.03"),
     )
 
     val quote4 = Quote(
-        currencyId = MockTokens.token4.id,
+        rawCurrencyId = MockTokens.token4.id.rawCurrencyId!!,
         fiatRate = BigDecimal("4.56"),
         priceChange = BigDecimal("-0.04"),
     )
 
     val quote5 = Quote(
-        currencyId = MockTokens.token5.id,
+        rawCurrencyId = MockTokens.token5.id.rawCurrencyId!!,
         fiatRate = BigDecimal("5.67"),
         priceChange = BigDecimal("0.05"),
     )
 
     val quote6 = Quote(
-        currencyId = MockTokens.token6.id,
+        rawCurrencyId = MockTokens.token6.id.rawCurrencyId!!,
         fiatRate = BigDecimal("6.78"),
         priceChange = BigDecimal("-0.06"),
     )
 
     val quote7 = Quote(
-        currencyId = MockTokens.token7.id,
+        rawCurrencyId = MockTokens.token7.id.rawCurrencyId!!,
         fiatRate = BigDecimal("7.89"),
         priceChange = BigDecimal("0.07"),
     )
 
     val quote8 = Quote(
-        currencyId = MockTokens.token8.id,
+        rawCurrencyId = MockTokens.token8.id.rawCurrencyId!!,
         fiatRate = BigDecimal("8.90"),
         priceChange = BigDecimal("-0.08"),
     )
 
     val quote9 = Quote(
-        currencyId = MockTokens.token9.id,
+        rawCurrencyId = MockTokens.token9.id.rawCurrencyId!!,
         fiatRate = BigDecimal("9.01"),
         priceChange = BigDecimal("0.09"),
     )
 
     val quote10 = Quote(
-        currencyId = MockTokens.token10.id,
+        rawCurrencyId = MockTokens.token10.id.rawCurrencyId!!,
         fiatRate = BigDecimal("10.12"),
         priceChange = BigDecimal("-0.10"),
     )
