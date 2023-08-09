10970b0a086752128949168af918a5c142e83d0c
Tangem <git@tangem.com>
2023-08-09 17:49:23 +0800

    Updated on 2026-05-07

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/factory/txhistory/WalletLoadedTxHistoryConverter.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/factory/txhistory/WalletLoadedTxHistoryConverter.kt
@@ -3,18 +3,14 @@ package com.tangem.feature.wallet.presentation.wallet.state.factory.txhistory
 import androidx.paging.PagingData
 import arrow.core.Either
 import com.tangem.common.Provider
-import com.tangem.core.ui.components.marketprice.MarketPriceBlockState
 import com.tangem.domain.common.CardTypesResolver
 import com.tangem.domain.txhistory.models.TxHistoryItem
 import com.tangem.domain.txhistory.models.TxHistoryListError
 import com.tangem.feature.wallet.presentation.wallet.state.WalletSingleCurrencyState
-import com.tangem.feature.wallet.presentation.wallet.state.WalletStateHolder
-import com.tangem.feature.wallet.presentation.wallet.state.components.WalletManageButton
+import com.tangem.feature.wallet.presentation.wallet.state.WalletState
 import com.tangem.feature.wallet.presentation.wallet.state.components.WalletTxHistoryState
 import com.tangem.feature.wallet.presentation.wallet.viewmodels.WalletClickIntents
 import com.tangem.utils.converter.Converter
-import kotlinx.collections.immutable.ImmutableList
-import kotlinx.collections.immutable.persistentListOf
 import kotlinx.coroutines.flow.Flow
 
 /**
@@ -27,10 +23,10 @@ import kotlinx.coroutines.flow.Flow
 [REDACTED_AUTHOR]
  */
 internal class WalletLoadedTxHistoryConverter(
-    private val currentStateProvider: Provider<WalletStateHolder>,
+    private val currentStateProvider: Provider<WalletState>,
     private val currentCardTypeResolverProvider: Provider<CardTypesResolver>,
     private val clickIntents: WalletClickIntents,
-) : Converter<Either<TxHistoryListError, Flow<PagingData<TxHistoryItem>>>, WalletStateHolder> {
+) : Converter<Either<TxHistoryListError, Flow<PagingData<TxHistoryItem>>>, WalletState> {
 
     private val walletTxHistoryItemFlowConverter by lazy {
         WalletTxHistoryItemFlowConverter(
@@ -39,12 +35,12 @@ internal class WalletLoadedTxHistoryConverter(
         )
     }
 
-    override fun convert(value: Either<TxHistoryListError, Flow<PagingData<TxHistoryItem>>>): WalletStateHolder {
+    override fun convert(value: Either<TxHistoryListError, Flow<PagingData<TxHistoryItem>>>): WalletState {
         return value.fold(ifLeft = ::convertError, ifRight = ::convert)
     }
 
-    private fun convertError(error: TxHistoryListError): WalletStateHolder {
-        return currentStateProvider().copySingleCurrencyContent(
+    private fun convertError(error: TxHistoryListError): WalletState {
+        return requireNotNull(currentStateProvider() as? WalletSingleCurrencyState.Content).copy(
             txHistoryState = when (error) {
                 is TxHistoryListError.DataError -> {
                     WalletTxHistoryState.Error(onReloadClick = clickIntents::onReloadClick)
@@ -53,40 +49,9 @@ internal class WalletLoadedTxHistoryConverter(
         )
     }
 
-    private fun convert(items: Flow<PagingData<TxHistoryItem>>): WalletStateHolder {
-        return currentStateProvider().copySingleCurrencyContent(
+    private fun convert(items: Flow<PagingData<TxHistoryItem>>): WalletState {
+        return requireNotNull(currentStateProvider() as? WalletSingleCurrencyState.Content).copy(
             txHistoryState = walletTxHistoryItemFlowConverter.convert(value = items),
         )
-    }
-
-    private fun WalletStateHolder.copySingleCurrencyContent(
-        txHistoryState: WalletTxHistoryState,
-    ): WalletSingleCurrencyState {
-        return WalletSingleCurrencyState.Content(
-            onBackClick = onBackClick,
-            topBarConfig = topBarConfig,
-            walletsListConfig = walletsListConfig,
-            pullToRefreshConfig = pullToRefreshConfig,
-            notifications = notifications,
-            bottomSheetConfig = bottomSheetConfig,
-            buttons = getButtons(),
-            marketPriceBlockState = getLoadingMarketPriceBlockState(),
-            txHistoryState = txHistoryState,
-        )
-    }
-
-    // TODO: [REDACTED_JIRA]
-    private fun getButtons(): ImmutableList<WalletManageButton> {
-        return persistentListOf(
-            WalletManageButton.Buy(onClick = {}),
-            WalletManageButton.Send(onClick = {}),
-            WalletManageButton.Receive(onClick = {}),
-            WalletManageButton.Exchange(onClick = {}),
-            WalletManageButton.CopyAddress(onClick = {}),
-        )
-    }
-
-    private fun getLoadingMarketPriceBlockState(): MarketPriceBlockState {
-        return MarketPriceBlockState.Loading(currencyName = currentCardTypeResolverProvider().getBlockchain().currency)
     }
 }
