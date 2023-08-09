7df5fec89d188af3b16266d9f68c0710ae5f6bff
Tangem <git@tangem.com>
2023-08-09 17:12:38 +0800

    Updated on 2026-09-25

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/components/multicurrency/MultiCurrencyContentItem.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/components/multicurrency/MultiCurrencyContentItem.kt
@@ -2,6 +2,7 @@ package com.tangem.feature.wallet.presentation.wallet.ui.components.multicurrenc
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.tangem.core.ui.extensions.resolveReference
 import com.tangem.feature.wallet.presentation.common.component.NetworkGroupItem
 import com.tangem.feature.wallet.presentation.common.component.TokenItem
 import com.tangem.feature.wallet.presentation.wallet.state.components.WalletTokensListState
@@ -18,7 +19,7 @@ import com.tangem.feature.wallet.presentation.wallet.state.components.WalletToke
 internal fun MultiCurrencyContentItem(state: WalletTokensListState.TokensListItemState, modifier: Modifier = Modifier) {
     when (state) {
         is WalletTokensListState.TokensListItemState.NetworkGroupTitle -> {
-            NetworkGroupItem(networkName = state.networkName, modifier = modifier)
+            NetworkGroupItem(networkName = state.value.resolveReference(), modifier = modifier)
         }
         is WalletTokensListState.TokensListItemState.Token -> {
             TokenItem(state = state.state, modifier = modifier)
