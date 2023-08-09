5a83b8d3311201e4b1672d7f2d2ab5dffd0b09f2
Tangem <git@tangem.com>
2023-08-09 11:18:23 +0500

    Updated on 2025-11-26

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/components/WalletLockedContentState.kt
+++
@@ -1,9 +0,0 @@
-package com.tangem.feature.wallet.presentation.wallet.state.components
-
-/**
- * Wallet locked content state.
- * It allows to divide the locked content of multi-currency and single-currency wallets.
- *
-[REDACTED_AUTHOR]
- */
-internal sealed interface WalletLockedContentState
