49c1ce9d14c10a71ac5a6abbf73e4feea54b0eea
Doston Kamalov <dkamalov@tangem.com>
2023-08-09 11:18:23 +0500

    AND-4209 Moved transactions composables to core module

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/components/WalletLockedContentState.kt
+++
@@ -1,9 +0,0 @@
-package com.tangem.feature.wallet.presentation.wallet.state.components
-
-/**
- * Wallet locked content state.
- * It allows to divide the locked content of multi-currency and single-currency wallets.
- *
- * @author Andrew Khokhlov on 28/07/2023
- */
-internal sealed interface WalletLockedContentState
