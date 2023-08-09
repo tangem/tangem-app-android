cb2e9464d0b749b72f87c2021a6110da5e42fe3a
Tangem <git@tangem.com>
2023-08-09 13:24:48 +0300

    Updated on 2026-06-02

--- data/tokens/src/main/kotlin/com/tangem/data/tokens/repository/MockQuotesRepository.kt
+++ data/tokens/src/main/kotlin/com/tangem/data/tokens/repository/MockQuotesRepository.kt
@@ -3,21 +3,27 @@ package com.tangem.data.tokens.repository
 import com.tangem.domain.tokens.model.CryptoCurrency
 import com.tangem.domain.tokens.model.Quote
 import com.tangem.domain.tokens.repository.QuotesRepository
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.channelFlow
 import java.math.BigDecimal
+import kotlin.random.Random
 
 internal class MockQuotesRepository : QuotesRepository {
 
     override fun getQuotes(currenciesIds: Set<CryptoCurrency.ID>, refresh: Boolean): Flow<Set<Quote>> {
-        return flowOf(
-            currenciesIds.map {
+        return channelFlow {
+            val quotes = currenciesIds.map {
                 Quote(
                     currencyId = it,
                     fiatRate = BigDecimal.ZERO,
                     priceChange = BigDecimal.ZERO,
                 )
-            }.toSet(),
-        )
+            }.toSet()
+
+            delay(Random.nextLong(from = 200, until = 2_000))
+
+            send(quotes)
+        }
     }
 }
