a86d3a3ed9968db4f1867b868d847083f131c75f
Tangem <git@tangem.com>
2023-08-09 20:20:50 +0300

    Updated on 2026-03-25

---
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/organizetokens/model/OrganizeTokensState.kt
@@ -0,0 +1,36 @@
+package com.tangem.feature.wallet.presentation.organizetokens.model
+
+import androidx.compose.runtime.Immutable
+import org.burnoutcrew.reorderable.ItemPosition
+
+@Immutable
+internal data class OrganizeTokensState(
+    val onBackClick: () -> Unit,
+    val itemsState: OrganizeTokensListState,
+    val header: HeaderConfig,
+    val actions: ActionsConfig,
+    val dndConfig: DragAndDropConfig,
+) {
+
+    data class HeaderConfig(
+        val isEnabled: Boolean = false,
+        val isSortedByBalance: Boolean = false,
+        val isGrouped: Boolean = false,
+        val onSortClick: () -> Unit,
+        val onGroupClick: () -> Unit,
+    )
+
+    data class ActionsConfig(
+        val canApply: Boolean = false,
+        val showApplyProgress: Boolean = false,
+        val onApplyClick: () -> Unit,
+        val onCancelClick: () -> Unit,
+    )
+
+    data class DragAndDropConfig(
+        val onItemDragged: (ItemPosition, ItemPosition) -> Unit,
+        val canDragItemOver: (ItemPosition, ItemPosition) -> Boolean,
+        val onItemDragEnd: () -> Unit,
+        val onDragStart: (DraggableItem) -> Unit,
+    )
+}
