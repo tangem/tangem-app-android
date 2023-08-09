a17f81cbbe81fccfb7912577a363e1c2b5bd260b
Tangem <git@tangem.com>
2023-08-09 11:18:23 +0500

    Updated on 2026-07-07

--- core/ui/src/main/java/com/tangem/core/ui/components/transactions/TxHistoryTitle.kt
+++ core/ui/src/main/java/com/tangem/core/ui/components/transactions/TxHistoryTitle.kt
@@ -1,9 +1,8 @@
-package com.tangem.feature.wallet.presentation.wallet.ui.components.singlecurrency
+package com.tangem.core.ui.components.transactions
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -11,17 +10,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.tangem.core.ui.res.TangemTheme
-import com.tangem.feature.wallet.impl.R
-import com.tangem.feature.wallet.presentation.wallet.state.components.WalletTxHistoryState.TxHistoryItemState
+import com.tangem.core.ui.R
 
-/**
- * Transactions block title
- *
- * @param config   config
- * @param modifier modifier
- */
 @Composable
-internal fun TxHistoryTitle(config: TxHistoryItemState.Title, modifier: Modifier = Modifier) {
+internal fun TxHistoryTitle(config: TxHistoryState.TxHistoryItemState.Title, modifier: Modifier = Modifier) {
     Row(
         modifier = modifier
             .background(TangemTheme.colors.background.primary)
@@ -40,7 +32,7 @@ internal fun TxHistoryTitle(config: TxHistoryItemState.Title, modifier: Modifier
             modifier = Modifier.clickable(onClick = config.onExploreClick),
             horizontalArrangement = Arrangement.spacedBy(space = TangemTheme.dimens.spacing4),
         ) {
-            Icon(
+            androidx.compose.material3.Icon(
                 painter = painterResource(id = R.drawable.ic_compass_24),
                 contentDescription = null,
                 modifier = Modifier.size(size = TangemTheme.dimens.size18),
@@ -59,7 +51,7 @@ internal fun TxHistoryTitle(config: TxHistoryItemState.Title, modifier: Modifier
 @Composable
 private fun Preview_TransactionsBlockTitle_Light() {
     TangemTheme(isDark = false) {
-        TxHistoryTitle(config = TxHistoryItemState.Title(onExploreClick = {}))
+        TxHistoryTitle(config = TxHistoryState.TxHistoryItemState.Title(onExploreClick = {}))
     }
 }
 
@@ -67,6 +59,6 @@ private fun Preview_TransactionsBlockTitle_Light() {
 @Composable
 private fun Preview_TransactionsBlockTitle_Dark() {
     TangemTheme(isDark = true) {
-        TxHistoryTitle(config = TxHistoryItemState.Title(onExploreClick = {}))
+        TxHistoryTitle(config = TxHistoryState.TxHistoryItemState.Title(onExploreClick = {}))
     }
 }
