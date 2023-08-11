2cd503f578663ce07da7391f78ae86b01f54a085
Tangem <git@tangem.com>
2023-08-11 17:30:05 +0800

    Updated on 2026-09-21

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/WalletState.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/WalletState.kt
@@ -30,6 +30,20 @@ internal sealed class WalletState {
 
         /** Bottom sheet config */
         abstract val bottomSheetConfig: WalletBottomSheetConfig?
+
+        /**
+         * Util function that allow to make a copy
+         *
+         * @param walletsListConfig wallets list config
+         */
+        fun copySealed(walletsListConfig: WalletsListConfig = this.walletsListConfig): ContentState {
+            return when (this) {
+                is WalletMultiCurrencyState.Content -> copy(walletsListConfig = walletsListConfig)
+                is WalletMultiCurrencyState.Locked -> copy(walletsListConfig = walletsListConfig)
+                is WalletSingleCurrencyState.Content -> copy(walletsListConfig = walletsListConfig)
+                is WalletSingleCurrencyState.Locked -> copy(walletsListConfig = walletsListConfig)
+            }
+        }
     }
 
     /**
