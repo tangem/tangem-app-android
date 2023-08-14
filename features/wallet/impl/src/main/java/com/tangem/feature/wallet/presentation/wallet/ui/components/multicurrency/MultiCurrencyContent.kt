d4dfeb9666b86cc3d862facc096c67c09c863a41
Tangem <git@tangem.com>
2023-08-14 17:19:17 +0800

    Updated on 2025-09-15

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/components/multicurrency/MultiCurrencyContent.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/components/multicurrency/MultiCurrencyContent.kt
@@ -1,11 +1,26 @@
 package com.tangem.feature.wallet.presentation.wallet.ui.components.multicurrency
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import com.tangem.core.ui.decorations.roundedShapeItemDecoration
+import com.tangem.core.ui.res.TangemTheme
+import com.tangem.feature.wallet.impl.R
 import com.tangem.feature.wallet.presentation.wallet.state.components.WalletTokensListState
+import kotlinx.collections.immutable.ImmutableList
+
+private const val NON_CONTENT_TOKENS_LIST_KEY = "NON_CONTENT_TOKENS_LIST"
 
 /**
  * LazyList extension for [WalletTokensListState]
@@ -15,10 +30,20 @@ import com.tangem.feature.wallet.presentation.wallet.state.components.WalletToke
  *
 [REDACTED_AUTHOR]
  */
-@OptIn(ExperimentalFoundationApi::class)
 internal fun LazyListScope.tokensListItems(state: WalletTokensListState, modifier: Modifier = Modifier) {
+    when (state) {
+        is WalletTokensListState.ContentState -> contentItems(items = state.items, modifier = modifier)
+        WalletTokensListState.Empty -> nonContentItem(modifier = modifier)
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+private fun LazyListScope.contentItems(
+    items: ImmutableList<WalletTokensListState.TokensListItemState>,
+    modifier: Modifier = Modifier,
+) {
     itemsIndexed(
-        items = state.items,
+        items = items,
         key = { _, item ->
             when (item) {
                 is WalletTokensListState.TokensListItemState.NetworkGroupTitle -> item.value.hashCode()
@@ -33,9 +58,40 @@ internal fun LazyListScope.tokensListItems(state: WalletTokensListState, modifie
                     .animateItemPlacement()
                     .roundedShapeItemDecoration(
                         currentIndex = index,
-                        lastIndex = state.items.lastIndex,
+                        lastIndex = items.lastIndex,
                     ),
             )
         },
     )
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+private fun LazyListScope.nonContentItem(modifier: Modifier = Modifier) {
+    item(
+        key = NON_CONTENT_TOKENS_LIST_KEY,
+        contentType = NON_CONTENT_TOKENS_LIST_KEY,
+    ) {
+        Column(
+            modifier = modifier
+                .animateItemPlacement()
+                .padding(top = TangemTheme.dimens.spacing96),
+            verticalArrangement = Arrangement.spacedBy(space = TangemTheme.dimens.spacing16),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_empty_64),
+                contentDescription = null,
+                modifier = Modifier.size(size = TangemTheme.dimens.size64),
+                tint = TangemTheme.colors.icon.inactive,
+            )
+
+            Text(
+                text = stringResource(id = R.string.main_empty_tokens_list_message),
+                modifier = Modifier.padding(horizontal = TangemTheme.dimens.spacing48),
+                color = TangemTheme.colors.text.tertiary,
+                textAlign = TextAlign.Center,
+                style = TangemTheme.typography.caption,
+            )
+        }
+    }
 }
