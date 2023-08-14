48b8c4fd4c2ed1268745825e5c8ecfe11ce39bbf
Tangem <git@tangem.com>
2023-08-14 18:05:43 +0300

    release/5.26.1

--- plugins/configuration/src/main/kotlin/com/tangem/plugin/configuration/configurations/extension/BaseExtensionConfigurations.kt
+++ plugins/configuration/src/main/kotlin/com/tangem/plugin/configuration/configurations/extension/BaseExtensionConfigurations.kt
@@ -20,12 +20,14 @@ internal fun BaseExtension.configureCompilerOptions() {
 internal fun BaseExtension.configureCompose(project: Project) {
     val useCompose = with(project.path) {
         contains(":ui") ||
-            contains(":onboarding") || // TODO: divide on api/impl after migrating all onboarding to module
-            contains(":presentation") ||
-            contains(":app") || // TODO: https://tangem.atlassian.net/browse/AND-3190
-            contains(":impl")
+            contains(Regex(pattern = ":onboarding\$")) || // TODO: divide on api/impl after migrating all onboarding to module
+            contains(Regex(pattern = ":presentation\$")) ||
+            contains(Regex(pattern = ":app\$")) || // TODO: https://tangem.atlassian.net/browse/AND-3190
+            contains(Regex(pattern = ":impl\$"))
     }
+
     buildFeatures.compose = useCompose
+    
     if (useCompose) {
         composeOptions {
             kotlinCompilerExtensionVersion = project.findVersion(alias = "compose-compiler").requiredVersion
