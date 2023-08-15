cc80c4d0156b4f5838c60d93eb412e0a87b85066
Tangem <git@tangem.com>
2023-08-15 13:24:38 +0300

    Updated on 2026-08-27

--- domain/tokens/build.gradle.kts
+++ domain/tokens/build.gradle.kts
@@ -4,11 +4,17 @@ plugins {
 }
 
 dependencies {
+
+    /** Project - Domain */
     implementation(projects.domain.core)
     implementation(projects.domain.tokens.models)
     implementation(projects.domain.wallets.models)
+    implementation(projects.domain.appCurrency.models)
+
+    /** Project - Other */
     implementation(projects.core.utils)
 
+    /** Tests */
     testImplementation(deps.test.junit)
     testImplementation(deps.test.coroutine)
 }
