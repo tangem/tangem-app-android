9cff88db0f041e9f543d0b237105dd3362fdacdd
Tangem <git@tangem.com>
2023-08-15 14:08:30 +0300

    Updated on 2026-09-25

--- core/datasource/src/main/java/com/tangem/datasource/di/AppCurrencyDataModule.kt
+++ core/datasource/src/main/java/com/tangem/datasource/di/AppCurrencyDataModule.kt
@@ -1,10 +1,18 @@
 package com.tangem.datasource.di
 
-import com.tangem.datasource.local.appcurrency.MockSelectedAppCurrencyStore
+import android.content.Context
+import com.squareup.moshi.Moshi
+import com.tangem.datasource.api.tangemTech.models.CurrenciesResponse
+import com.tangem.datasource.local.appcurrency.AvailableAppCurrenciesStore
 import com.tangem.datasource.local.appcurrency.SelectedAppCurrencyStore
+import com.tangem.datasource.local.appcurrency.implementation.DefaultAvailableAppCurrenciesStore
+import com.tangem.datasource.local.appcurrency.implementation.DefaultSelectedAppCurrencyStore
+import com.tangem.datasource.local.datastore.RuntimeDataStore
+import com.tangem.datasource.local.datastore.SharedPreferencesDataStore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -14,7 +22,24 @@ internal object AppCurrencyDataModule {
 
     @Provides
     @Singleton
-    fun provideSelectedAppCurrencyStore(): SelectedAppCurrencyStore {
-        return MockSelectedAppCurrencyStore()
+    fun provideAvailableAppCurrenciesStore(): AvailableAppCurrenciesStore {
+        return DefaultAvailableAppCurrenciesStore(
+            dataStore = RuntimeDataStore(),
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideSelectedAppCurrencyStore(
+        @ApplicationContext context: Context,
+        @NetworkMoshi moshi: Moshi,
+    ): SelectedAppCurrencyStore {
+        return DefaultSelectedAppCurrencyStore(
+            dataStore = SharedPreferencesDataStore(
+                preferencesName = "selected_app_currency",
+                context = context,
+                adapter = moshi.adapter(CurrenciesResponse.Currency::class.java),
+            ),
+        )
     }
 }
