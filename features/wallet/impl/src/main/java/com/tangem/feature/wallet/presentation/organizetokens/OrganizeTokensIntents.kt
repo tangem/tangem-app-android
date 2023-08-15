17af9f301cdb34d3ffbaec5568967dba1d64dde0
Tangem <git@tangem.com>
2023-08-15 17:25:50 +0300

    Updated on 2026-09-23

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/organizetokens/OrganizeTokensIntents.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/organizetokens/OrganizeTokensIntents.kt
@@ -1,8 +1,5 @@
 package com.tangem.feature.wallet.presentation.organizetokens
 
-import com.tangem.feature.wallet.presentation.organizetokens.model.DraggableItem
-import org.burnoutcrew.reorderable.ItemPosition
-
 internal interface OrganizeTokensIntents {
 
     fun onBackClick()
@@ -14,12 +11,4 @@ internal interface OrganizeTokensIntents {
     fun onApplyClick()
 
     fun onCancelClick()
-
-    fun onItemDragged(from: ItemPosition, to: ItemPosition)
-
-    fun canDragItemOver(dragOver: ItemPosition, dragging: ItemPosition): Boolean
-
-    fun onItemDraggingStart(item: DraggableItem)
-
-    fun onItemDraggingEnd()
 }
