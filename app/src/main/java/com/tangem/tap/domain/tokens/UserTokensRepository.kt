7fcf8ecd14b4127df6aabaa9df08f691ee0198d0
Tangem <git@tangem.com>
2023-08-15 19:00:10 +0300

    Updated on 2026-02-02

--- app/src/main/java/com/tangem/tap/domain/tokens/UserTokensRepository.kt
+++ app/src/main/java/com/tangem/tap/domain/tokens/UserTokensRepository.kt
@@ -1,7 +1,7 @@
 package com.tangem.tap.domain.tokens
 
 import android.content.Context
-import com.tangem.blockchain.common.DerivationStyle
+import com.tangem.blockchain.common.derivation.DerivationStyle
 import com.tangem.common.core.TangemSdkError
 import com.tangem.datasource.api.tangemTech.TangemTechApi
 import com.tangem.datasource.api.tangemTech.TangemTechService
@@ -9,6 +9,7 @@ import com.tangem.datasource.api.tangemTech.models.UserTokensResponse
 import com.tangem.datasource.connection.NetworkConnectionManager
 import com.tangem.datasource.files.AndroidFileReader
 import com.tangem.domain.common.BlockchainNetwork
+import com.tangem.domain.common.extensions.derivationPath
 import com.tangem.domain.models.scan.CardDTO
 import com.tangem.domain.userwallets.UserWalletIdBuilder
 import com.tangem.tap.domain.tokens.converters.CurrencyConverter
