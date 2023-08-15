f21d0460704e2cf4564c2dd0214e10a592f19890
Tangem <git@tangem.com>
2023-08-15 15:59:41 +0500

    Updated on 2026-07-07

--- features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/viewmodels/TokenDetailsViewModel.kt
+++ features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/viewmodels/TokenDetailsViewModel.kt
@@ -3,10 +3,15 @@ package com.tangem.feature.tokendetails.presentation.tokendetails.viewmodels
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.tangem.domain.tokens.models.CryptoCurrency
 import com.tangem.feature.tokendetails.presentation.router.InnerTokenDetailsRouter
 import com.tangem.feature.tokendetails.presentation.tokendetails.TokenDetailsPreviewData
+import com.tangem.feature.tokendetails.presentation.tokendetails.state.TokenInfoBlockState
+import com.tangem.features.tokendetails.impl.R
+import com.tangem.features.tokendetails.navigation.TokenDetailsRouter
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -16,9 +21,14 @@ import kotlin.properties.Delegates
 private const val LOADING_DELAY = 4_000L
 
 @HiltViewModel
-internal class TokenDetailsViewModel @Inject constructor() : ViewModel() {
+internal class TokenDetailsViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+) : ViewModel() {
 
-    var router: InnerTokenDetailsRouter by Delegates.notNull()
+    private val cryptoCurrency: CryptoCurrency = savedStateHandle[TokenDetailsRouter.SELECTED_CURRENCY_KEY]
+        ?: error("no expected parameter CryptoCurrency found")
+
+    var router by Delegates.notNull<InnerTokenDetailsRouter>()
 
     var uiState by mutableStateOf(getInitialState())
         private set
@@ -37,6 +47,19 @@ internal class TokenDetailsViewModel @Inject constructor() : ViewModel() {
     private fun getInitialState() = TokenDetailsPreviewData.tokenDetailsState.copy(
         topAppBarConfig = TokenDetailsPreviewData.tokenDetailsTopAppBarConfig.copy(
             onBackClick = ::onBackClick,
+        ),
+        tokenInfoBlockState = TokenInfoBlockState(
+            name = cryptoCurrency.name,
+            iconUrl = requireNotNull(cryptoCurrency.iconUrl),
+            currency = when (cryptoCurrency) {
+                is CryptoCurrency.Coin -> TokenInfoBlockState.Currency.Native
+                is CryptoCurrency.Token -> TokenInfoBlockState.Currency.Token(
+                    networkName = cryptoCurrency.standardType.name,
+                    blockchainName = cryptoCurrency.blockchainName,
+                    // TODO: [REDACTED_JIRA]
+                    networkIcon = R.drawable.img_eth_22,
+                )
+            },
         ),
     )
 
