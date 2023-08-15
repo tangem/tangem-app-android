e6647be6de781ba857eb0c40603e8a1e3ad02f59
Tangem <git@tangem.com>
2023-08-15 17:25:50 +0300

    release/5.26.1

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/organizetokens/OrganizeTokensStateHolder.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/organizetokens/OrganizeTokensStateHolder.kt
@@ -1,12 +1,12 @@
 package com.tangem.feature.wallet.presentation.organizetokens
 
 import com.tangem.common.Provider
+import com.tangem.domain.appcurrency.model.AppCurrency
 import com.tangem.domain.tokens.error.TokenListError
 import com.tangem.domain.tokens.error.TokenListSortingError
 import com.tangem.domain.tokens.model.TokenList
 import com.tangem.feature.wallet.presentation.organizetokens.model.OrganizeTokensListState
 import com.tangem.feature.wallet.presentation.organizetokens.model.OrganizeTokensState
-import com.tangem.feature.wallet.presentation.organizetokens.utils.common.updateSorting
 import com.tangem.feature.wallet.presentation.organizetokens.utils.converter.InProgressStateConverter
 import com.tangem.feature.wallet.presentation.organizetokens.utils.converter.TokenListToStateConverter
 import com.tangem.feature.wallet.presentation.organizetokens.utils.converter.error.TokenListErrorConverter
@@ -17,22 +17,17 @@ import com.tangem.feature.wallet.presentation.organizetokens.utils.converter.ite
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.*
 
-@Suppress("unused", "MemberVisibilityCanBePrivate") // TODO: Will be used in next MR
 internal class OrganizeTokensStateHolder(
     private val intents: OrganizeTokensIntents,
-    private val fiatCurrencyCode: String,
-    private val fiatCurrencySymbol: String,
+    private val appCurrencyProvider: Provider<AppCurrency>,
     private val onSubscription: () -> Unit,
-    scope: CoroutineScope,
+    stateFlowScope: CoroutineScope,
 ) {
 
     private val stateFlowInternal: MutableStateFlow<OrganizeTokensState> = MutableStateFlow(getInitialState())
 
     private val tokenListConverter by lazy {
-        val tokensConverter = CryptoCurrencyToDraggableItemConverter(
-            fiatCurrencyCode = fiatCurrencyCode,
-            fiatCurrencySymbol = fiatCurrencySymbol,
-        )
+        val tokensConverter = CryptoCurrencyToDraggableItemConverter(appCurrencyProvider)
         val itemsConverter = TokenListToListStateConverter(
             tokensConverter = tokensConverter,
             groupsConverter = NetworkGroupToDraggableItemsConverter(tokensConverter),
@@ -56,17 +51,13 @@ internal class OrganizeTokensStateHolder(
     val stateFlow: StateFlow<OrganizeTokensState> = stateFlowInternal
         .onSubscription { onSubscription() }
         .stateIn(
-            scope = scope,
+            scope = stateFlowScope,
             started = SharingStarted.WhileSubscribed(),
             initialValue = getInitialState(),
         )
 
-    var tokenList: TokenList? = null
-        private set
-
     fun updateStateWithTokenList(tokenList: TokenList) {
         updateState { tokenListConverter.convert(tokenList) }
-        this.tokenList = tokenList
     }
 
     fun updateStateToDisplayProgress() {
@@ -75,16 +66,6 @@ internal class OrganizeTokensStateHolder(
 
     fun updateStateToHideProgress() {
         updateState { inProgressStateConverter.convertBack(value = this) }
-    }
-
-    fun updateStateWithManualSorting(itemsState: OrganizeTokensListState) {
-        updateState {
-            copy(
-                header = header.copy(isSortedByBalance = false),
-                itemsState = itemsState,
-            )
-        }
-        tokenList = tokenList?.updateSorting(isSortedByBalance = false)
     }
 
     fun updateStateWithError(error: TokenListError) {
@@ -107,11 +88,12 @@ internal class OrganizeTokensStateHolder(
                 onApplyClick = intents::onApplyClick,
                 onCancelClick = intents::onCancelClick,
             ),
+// [REDACTED_TODO_COMMENT]
             dndConfig = OrganizeTokensState.DragAndDropConfig(
-                onItemDragged = intents::onItemDragged,
-                onDragStart = intents::onItemDraggingStart,
-                onItemDragEnd = intents::onItemDraggingEnd,
-                canDragItemOver = intents::canDragItemOver,
+                onItemDragged = { _, _ -> },
+                onDragStart = { },
+                onItemDragEnd = { },
+                canDragItemOver = { _, _ -> false },
             ),
         )
     }
