244c5006d554ff3037054949be5cd2c2b8a55ddf
Tangem <git@tangem.com>
2023-08-17 17:26:28 +0500

    Updated on 2026-03-25

--- features/tokendetails/impl/build.gradle.kts
+++ features/tokendetails/impl/build.gradle.kts
@@ -26,21 +26,34 @@ dependencies {
     implementation(deps.compose.accompanist.systemUiController)
     implementation(deps.compose.coil)
 
-    implementation(deps.kotlin.immutable.collections)
     implementation(deps.arrow.core)
+    implementation(deps.kotlin.immutable.collections)
+    implementation(deps.tangem.blockchain)
+    implementation(deps.tangem.card.core)
+    implementation(deps.timber)
 
     /** DI */
     implementation(deps.hilt.android)
     kapt(deps.hilt.kapt)
 
     /** Core modules */
+    implementation(projects.common)
     implementation(projects.core.featuretoggles)
-    implementation(projects.core.ui)
     implementation(projects.core.navigation)
+    implementation(projects.core.ui)
+    implementation(projects.core.utils)
 
+    /** Domain modules */
+    implementation(projects.domain.appCurrency)
+    implementation(projects.domain.appCurrency.models)
+    implementation(projects.domain.legacy)
+    implementation(projects.domain.models)
+    implementation(projects.domain.tokens)
     implementation(projects.domain.tokens.models)
     implementation(projects.domain.txhistory)
     implementation(projects.domain.txhistory.models)
+    implementation(projects.domain.wallets)
+    implementation(projects.domain.wallets.models)
 
     /** Feature Apis */
     implementation(projects.features.tokendetails.api)
