a0c7657be6f53bbe39bc61c8fd04c15763083fcb
Tangem <git@tangem.com>
2023-08-18 12:48:55 +0300

    Updated on 2026-09-01

--- data/tokens/src/main/kotlin/com/tangem/data/tokens/utils/NetworkConverter.kt
+++ data/tokens/src/main/kotlin/com/tangem/data/tokens/utils/NetworkConverter.kt
@@ -3,22 +3,13 @@ package com.tangem.data.tokens.utils
 import com.tangem.blockchain.common.Blockchain
 import com.tangem.domain.tokens.models.Network
 import com.tangem.utils.converter.Converter
-import timber.log.Timber
 
 internal class NetworkConverter : Converter<Network.ID, Network?> {
 
     override fun convert(value: Network.ID): Network? {
         val blockchain = Blockchain.fromId(value.value)
 
-        if (blockchain == Blockchain.Unknown) {
-            Timber.e("Unable to convert Unknown blockchain to the domain network model")
-            return null
-        }
-
-        return Network(
-            id = value,
-            name = blockchain.fullName,
-        )
+        return getNetwork(blockchain)
     }
 
     override fun convertList(input: Collection<Network.ID>): List<Network> {
