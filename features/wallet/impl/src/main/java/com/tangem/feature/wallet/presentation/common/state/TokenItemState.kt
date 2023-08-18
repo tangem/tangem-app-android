9edfdd12bda1cdf88b8f93682564197d6d6b2d90
Tangem <git@tangem.com>
2023-08-18 13:00:01 +0300

    Updated on 2026-02-11

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/common/state/TokenItemState.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/common/state/TokenItemState.kt
@@ -17,60 +17,65 @@ internal sealed interface TokenItemState {
     /**
      * Content token state
      *
-     * @property id                  unique id
-     * @property tokenIconUrl        token icon url
-     * @property tokenIconResId      token icon resource id
-     * @property networkIconResId    network icon resource id, may be null if it is a coin
-     * @property name                token name
-     * @property amount              amount of token
-     * @property hasPending          pending tx in blockchain
-     * @property tokenOptions        state for token options
+     * @property id                       unique id
+     * @property tokenIconUrl             token icon url
+     * @property tokenIconResId           token icon resource id
+     * @property networkBadgeIconResId    network badge icon resource id, may be null if it is a coin
+     * @property name                     token name
+     * @property amount                   amount of token
+     * @property hasPending               pending tx in blockchain
+     * @property tokenOptions             state for token options
+     * @property isTestnet                indicates whether the token is from test network or not
+     * @property onClick                  callback which will be called when an item is clicked
      */
     data class Content(
         override val id: String,
         val tokenIconUrl: String?,
         @DrawableRes val tokenIconResId: Int,
-        @DrawableRes val networkIconResId: Int?,
+        @DrawableRes val networkBadgeIconResId: Int?,
         val name: String,
         val amount: String,
         val hasPending: Boolean,
         val tokenOptions: TokenOptionsState,
+        val isTestnet: Boolean,
         val onClick: () -> Unit,
     ) : TokenItemState
 
     /**
      * Draggable token state
      *
-     * @property id                  unique id
-     * @property tokenIconUrl        token icon url
-     * @property tokenIconResId      token icon resource id
-     * @property networkIconResId    network icon resource id, may be null if it is a coin
-     * @property name                token name
-     * @property fiatAmount          fiat amount of token
+     * @property id                       unique id
+     * @property tokenIconUrl             token icon url
+     * @property tokenIconResId           token icon resource id
+     * @property networkBadgeIconResId    network badge icon resource id, may be null if it is a coin
+     * @property name                     token name
+     * @property fiatAmount               fiat amount of token
+     * @property isTestnet                indicates whether the token is from test network or not
      */
     data class Draggable(
         override val id: String,
         val tokenIconUrl: String?,
         @DrawableRes val tokenIconResId: Int,
-        @DrawableRes val networkIconResId: Int?,
+        @DrawableRes val networkBadgeIconResId: Int?,
         val name: String,
         val fiatAmount: String,
+        val isTestnet: Boolean,
     ) : TokenItemState
 
     /**
      * Unreachable token state
      *
-     * @property id                  token id
-     * @property tokenIconUrl        token icon url
-     * @property tokenIconResId      token icon resource id
-     * @property networkIconResId    network icon resource id, may be null if it is a coin
-     * @property name                token name
+     * @property id                       token id
+     * @property tokenIconUrl             token icon url
+     * @property tokenIconResId           token icon resource id
+     * @property networkBadgeIconResId    network badge icon resource id, may be null if it is a coin
+     * @property name                     token name
      */
     data class Unreachable(
         override val id: String,
         val tokenIconUrl: String?,
         @DrawableRes val tokenIconResId: Int,
-        @DrawableRes val networkIconResId: Int?,
+        @DrawableRes val networkBadgeIconResId: Int?,
         val name: String,
     ) : TokenItemState
 
