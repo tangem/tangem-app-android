5d235b1baaed7f83abc4b07b19ebd2a5524065a3
Tangem <git@tangem.com>
2023-08-22 18:49:27 +0500

    Updated on 2026-09-23

--- features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/ui/TokenDetailsScreen.kt
+++ features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/ui/TokenDetailsScreen.kt
@@ -1,14 +1,17 @@
 package com.tangem.feature.tokendetails.presentation.tokendetails.ui
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.tangem.core.ui.components.marketprice.MarketPriceBlock
+import com.tangem.core.ui.components.marketprice.MarketPriceBlockState
+import com.tangem.core.ui.components.transactions.state.TxHistoryState
+import com.tangem.core.ui.components.transactions.txHistoryItems
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.feature.tokendetails.presentation.tokendetails.TokenDetailsPreviewData
 import com.tangem.feature.tokendetails.presentation.tokendetails.state.TokenDetailsState
@@ -22,16 +25,36 @@ internal fun TokenDetailsScreen(state: TokenDetailsState) {
         topBar = { TokenDetailsTopAppBar(config = state.topAppBarConfig) },
         containerColor = TangemTheme.colors.background.secondary,
     ) { scaffoldPaddings ->
-        Column(
+        val txHistoryItems = if (state.txHistoryState is TxHistoryState.Content) {
+            state.txHistoryState.contentItems.collectAsLazyPagingItems()
+        } else {
+            null
+        }
+        val betweenItemsPadding = TangemTheme.dimens.spacing12
+        val horizontalPadding = TangemTheme.dimens.spacing16
+        val itemModifier = Modifier
+            .padding(top = betweenItemsPadding)
+            .padding(horizontal = horizontalPadding)
+        LazyColumn(
             modifier = Modifier
                 .padding(paddingValues = scaffoldPaddings)
-                .padding(horizontal = TangemTheme.dimens.spacing16)
                 .fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(TangemTheme.dimens.spacing12),
         ) {
-            TokenInfoBlock(state = state.tokenInfoBlockState)
-            TokenDetailsBalanceBlock(state = state.tokenBalanceBlockState)
-            MarketPriceBlock(state = state.marketPriceBlockState)
+            item {
+                TokenInfoBlock(
+                    modifier = Modifier
+                        .padding(top = TangemTheme.dimens.spacing4)
+                        .padding(horizontal = horizontalPadding),
+                    state = state.tokenInfoBlockState,
+                )
+            }
+            item { TokenDetailsBalanceBlock(modifier = itemModifier, state = state.tokenBalanceBlockState) }
+            item(
+                key = MarketPriceBlockState::class.java,
+                contentType = MarketPriceBlockState::class.java,
+                content = { MarketPriceBlock(modifier = itemModifier, state = state.marketPriceBlockState) },
+            )
+            txHistoryItems(state = state.txHistoryState, txHistoryItems = txHistoryItems)
         }
     }
 }
