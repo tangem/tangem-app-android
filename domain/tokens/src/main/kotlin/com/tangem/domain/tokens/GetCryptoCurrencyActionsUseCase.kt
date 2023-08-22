de66777c85b3ea540178568e0f0d2a8f53e7d9ff
Tangem <git@tangem.com>
2023-08-22 12:41:19 +0100

    release/5.26.1

---
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/GetCryptoCurrencyActionsUseCase.kt
@@ -0,0 +1,31 @@
+package com.tangem.domain.tokens
+
+import com.tangem.domain.tokens.model.TokenActionsState
+import com.tangem.domain.wallets.models.UserWalletId
+import com.tangem.utils.coroutines.CoroutineDispatcherProvider
+import kotlinx.coroutines.flow.*
+
+class GetCryptoCurrencyActionsUseCase(
+    private val dispatchers: CoroutineDispatcherProvider,
+) {
+
+    operator fun invoke(userWalletId: UserWalletId, tokenId: String): Flow<TokenActionsState> {
+        return flow {
+            emit(getMockState(userWalletId, tokenId))
+        }.flowOn(dispatchers.io)
+    }
+// [REDACTED_TODO_COMMENT]
+    private fun getMockState(userWalletId: UserWalletId, tokenId: String): TokenActionsState {
+        return TokenActionsState(
+            walletId = userWalletId,
+            tokenId = tokenId,
+            states = listOf(
+                TokenActionsState.ActionState.Buy(true),
+                TokenActionsState.ActionState.Sell(true),
+                TokenActionsState.ActionState.Receive(true),
+                TokenActionsState.ActionState.Swap(true),
+                TokenActionsState.ActionState.Sell(true),
+            ),
+        )
+    }
+}
