2bdfe9490c010fa6ffdfcfc4a0c916c529c072b9
Tangem <git@tangem.com>
2023-08-23 15:02:05 +0800

    Updated on 2025-12-05

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/factory/WalletLoadedTokensListConverter.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/factory/WalletLoadedTokensListConverter.kt
@@ -6,6 +6,7 @@ import com.tangem.domain.appcurrency.model.AppCurrency
 import com.tangem.domain.common.CardTypesResolver
 import com.tangem.domain.tokens.error.TokenListError
 import com.tangem.domain.tokens.model.TokenList
+import com.tangem.domain.wallets.models.UserWallet
 import com.tangem.feature.wallet.presentation.wallet.state.WalletMultiCurrencyState
 import com.tangem.feature.wallet.presentation.wallet.state.WalletState
 import com.tangem.feature.wallet.presentation.wallet.state.factory.WalletLoadedTokensListConverter.LoadedTokensListModel
@@ -19,7 +20,7 @@ import com.tangem.utils.converter.Converter
  *
  * @property currentStateProvider  current ui state provider
  * @param cardTypeResolverProvider card type resolver
- * @param isLockedWalletProvider   current wallet is locked or not provider
+ * @param currentWalletProvider    current wallet provider
  * @param clickIntents             screen click intents
  *
 [REDACTED_AUTHOR]
@@ -28,14 +29,14 @@ internal class WalletLoadedTokensListConverter(
     private val currentStateProvider: Provider<WalletState>,
     appCurrencyProvider: Provider<AppCurrency>,
     cardTypeResolverProvider: Provider<CardTypesResolver>,
-    isLockedWalletProvider: Provider<Boolean>,
+    currentWalletProvider: Provider<UserWallet>,
     clickIntents: WalletClickIntents,
 ) : Converter<LoadedTokensListModel, WalletMultiCurrencyState.Content> {
 
     private val tokenListStateConverter = TokenListToWalletStateConverter(
         currentStateProvider = currentStateProvider,
         cardTypeResolverProvider = cardTypeResolverProvider,
-        isLockedWalletProvider = isLockedWalletProvider,
+        currentWalletProvider = currentWalletProvider,
         appCurrencyProvider = appCurrencyProvider,
         isWalletContentHidden = false, // TODO: [REDACTED_JIRA]
         clickIntents = clickIntents,
