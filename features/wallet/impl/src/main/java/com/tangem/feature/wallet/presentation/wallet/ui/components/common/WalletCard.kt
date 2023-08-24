a1aeab5232c62d9c4b79f56a946f2564c196b508
Tangem <git@tangem.com>
2023-08-24 16:19:54 +0800

    Updated on 2026-05-04

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/components/common/WalletCard.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/components/common/WalletCard.kt
@@ -1,23 +1,38 @@
 package com.tangem.feature.wallet.presentation.wallet.ui.components.common
 
 import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.datasource.CollectionPreviewParameterProvider
+import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintLayoutScope
@@ -25,8 +40,10 @@ import androidx.constraintlayout.compose.Dimension
 import com.tangem.core.ui.components.FontSizeRange
 import com.tangem.core.ui.components.RectangleShimmer
 import com.tangem.core.ui.components.ResizableText
+import com.tangem.core.ui.components.wallets.RenameWalletDialogContent
 import com.tangem.core.ui.extensions.TextReference
 import com.tangem.core.ui.extensions.resolveReference
+import com.tangem.core.ui.res.TangemColorPalette
 import com.tangem.core.ui.res.TangemDimens
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.feature.wallet.impl.R
@@ -44,7 +61,12 @@ import com.tangem.feature.wallet.presentation.wallet.state.components.WalletCard
 @Composable
 internal fun WalletCard(state: WalletCardState, modifier: Modifier = Modifier) {
     @Suppress("DestructuringDeclarationWithTooManyEntries")
-    CardContainer(onClick = state.onClick, modifier = modifier) {
+    CardContainer(
+        name = state.title,
+        onDeleteClick = { state.onDeleteClick(state.id) },
+        onRenameClick = { state.onRenameClick(state.id, it) },
+        modifier = modifier,
+    ) {
         val (title, balance, additionalText, image) = createRefs()
 
         val contentVerticalMargin = TangemTheme.dimens.spacing12
@@ -92,16 +114,43 @@ internal fun WalletCard(state: WalletCardState, modifier: Modifier = Modifier) {
 
 @Composable
 private fun CardContainer(
-    onClick: (() -> Unit)?,
+    name: String,
+    onDeleteClick: () -> Unit,
+    onRenameClick: (String) -> Unit,
     modifier: Modifier = Modifier,
-    content: @Composable ConstraintLayoutScope.() -> Unit,
+    content: @Composable (ConstraintLayoutScope.() -> Unit),
 ) {
+    var isMenuVisible by rememberSaveable { mutableStateOf(value = false) }
+    var pressOffset by remember { mutableStateOf(value = DpOffset.Zero) }
+    var itemHeight by remember { mutableStateOf(value = 0.dp) }
+
+    val density = LocalDensity.current
+    val interactionSource = remember { MutableInteractionSource() }
+    val haptic = LocalHapticFeedback.current
+
     Surface(
-        modifier = modifier.defaultMinSize(minHeight = TangemTheme.dimens.size108),
+        modifier = modifier
+            .defaultMinSize(minHeight = TangemTheme.dimens.size108)
+            .onSizeChanged { itemHeight = with(density) { it.height.toDp() } }
+            .clip(shape = TangemTheme.shapes.roundedCornersXMedium)
+            .indication(interactionSource = interactionSource, indication = LocalIndication.current)
+            .pointerInput(true) {
+                detectTapGestures(
+                    onLongPress = {
+                        haptic.performHapticFeedback(HapticFeedbackType.LongPress)
+                        isMenuVisible = true
+                        pressOffset = DpOffset(x = it.x.toDp(), y = it.y.toDp())
+                    },
+                    onPress = {
+                        val press = PressInteraction.Press(it)
+                        interactionSource.emit(press)
+                        tryAwaitRelease()
+                        interactionSource.emit(PressInteraction.Release(press))
+                    },
+                )
+            },
         shape = TangemTheme.shapes.roundedCornersXMedium,
         color = TangemTheme.colors.background.primary,
-        onClick = onClick ?: {},
-        enabled = onClick != null,
     ) {
         ConstraintLayout(
             modifier = Modifier
@@ -111,6 +160,50 @@ private fun CardContainer(
             content()
         }
     }
+
+    var isRenameWalletDialogVisible by rememberSaveable { mutableStateOf(value = false) }
+
+    DropdownMenu(
+        expanded = isMenuVisible,
+        onDismissRequest = { isMenuVisible = false },
+        modifier = Modifier.background(color = TangemTheme.colors.background.secondary),
+        offset = pressOffset.copy(y = pressOffset.y - itemHeight),
+    ) {
+        MenuItem(
+            textResId = R.string.common_rename,
+            imageVector = Icons.Outlined.Edit,
+            onClick = {
+                isMenuVisible = false
+                isRenameWalletDialogVisible = true
+            },
+        )
+        MenuItem(textResId = R.string.common_delete, imageVector = Icons.Outlined.Delete, onClick = onDeleteClick)
+    }
+
+    if (isRenameWalletDialogVisible) {
+        RenameWalletDialogContent(
+            name = name,
+            onConfirm = {
+                onRenameClick(it)
+                isRenameWalletDialogVisible = false
+            },
+            onDismiss = { isRenameWalletDialogVisible = false },
+        )
+    }
+}
+
+@Composable
+private fun MenuItem(@StringRes textResId: Int, imageVector: ImageVector, onClick: () -> Unit) {
+    DropdownMenuItem(
+        text = { Text(text = stringResource(id = textResId), style = TangemTheme.typography.subtitle2) },
+        modifier = Modifier.background(color = TangemTheme.colors.background.secondary),
+        trailingIcon = { Icon(imageVector = imageVector, contentDescription = null) },
+        onClick = onClick,
+        colors = MenuDefaults.itemColors(
+            textColor = TangemTheme.colors.text.primary1,
+            trailingIconColor = TangemColorPalette.Dark6,
+        ),
+    )
 }
 
 @Composable
