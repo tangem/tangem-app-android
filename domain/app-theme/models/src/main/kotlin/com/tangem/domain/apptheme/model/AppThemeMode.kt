1d07e0c8a08803af68d61e9548733d1bb2ef1e47
Tangem <git@tangem.com>
2023-08-24 16:20:10 +0300

    Updated on 2026-07-23

---
+++ domain/app-theme/models/src/main/kotlin/com/tangem/domain/apptheme/model/AppThemeMode.kt
@@ -0,0 +1,30 @@
+package com.tangem.domain.apptheme.model
+
+/**
+ * Enumerates the possible modes for the application's theme.
+ */
+enum class AppThemeMode {
+    /**
+     * Forces the dark theme mode regardless of system settings.
+     */
+    FORCE_DARK,
+
+    /**
+     * Forces the light theme mode regardless of system settings.
+     */
+    FORCE_LIGHT,
+
+    /**
+     * Follows the system-wide theme mode.
+     */
+    FOLLOW_SYSTEM,
+
+    ;
+
+    companion object {
+        /**
+         * The default [AppThemeMode].
+         */
+        val DEFAULT: AppThemeMode = FORCE_LIGHT
+    }
+}
