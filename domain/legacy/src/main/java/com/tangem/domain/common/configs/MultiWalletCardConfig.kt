34c3e92ac32f71a180d8a76e608709204bdcede2
Tangem <git@tangem.com>
2023-08-28 10:51:17 +0300

    release/5.26.1

--- domain/legacy/src/main/java/com/tangem/domain/common/configs/MultiWalletCardConfig.kt
+++ domain/legacy/src/main/java/com/tangem/domain/common/configs/MultiWalletCardConfig.kt
@@ -4,15 +4,16 @@ import com.tangem.blockchain.common.Blockchain
 import com.tangem.common.card.EllipticCurve
 import timber.log.Timber
 
-object GenericCardConfig : CardConfig {
+object MultiWalletCardConfig : CardConfig {
     override val mandatoryCurves: List<EllipticCurve>
         get() = listOf(
             EllipticCurve.Secp256k1,
             EllipticCurve.Ed25519,
+            EllipticCurve.Bls12381G2Aug,
         )
 
     /**
-     * Old logic to determine primary curve for blockchain
+     * Old logic to determine primary curve for blockchain in TangemWallet
      */
     override fun primaryCurve(blockchain: Blockchain): EllipticCurve? {
         return when {
@@ -21,6 +22,9 @@ object GenericCardConfig : CardConfig {
             }
             blockchain.getSupportedCurves().contains(EllipticCurve.Ed25519) -> {
                 EllipticCurve.Ed25519
+            }
+            blockchain.getSupportedCurves().contains(EllipticCurve.Bls12381G2Aug) -> {
+                EllipticCurve.Bls12381G2Aug
             }
             else -> {
                 Timber.e("Unsupported blockchain, curve not found")
