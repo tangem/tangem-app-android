06d7cc2ec1253737734713ed82489632d0d5e1df
Tangem <git@tangem.com>
2023-08-29 15:36:58 +0800

    Updated on 2026-09-14

--- features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/router/DefaultTokenDetailsRouter.kt
+++ features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/router/DefaultTokenDetailsRouter.kt
@@ -2,20 +2,20 @@ package com.tangem.feature.tokendetails.presentation.router
 
 import androidx.fragment.app.Fragment
 import com.tangem.core.navigation.NavigationAction
-import com.tangem.core.navigation.NavigationStateHolder
+import com.tangem.core.navigation.ReduxNavController
 import com.tangem.feature.tokendetails.presentation.TokenDetailsFragment
 
 internal class DefaultTokenDetailsRouter(
-    private val navigationStateHolder: NavigationStateHolder,
+    private val reduxNavController: ReduxNavController,
 ) : InnerTokenDetailsRouter {
 
     override fun getEntryFragment(): Fragment = TokenDetailsFragment()
 
     override fun popBackStack() {
-        navigationStateHolder.navigate(NavigationAction.PopBackTo())
+        reduxNavController.navigate(NavigationAction.PopBackTo())
     }
 
     override fun openUrl(url: String) {
-        navigationStateHolder.navigate(NavigationAction.OpenUrl(url = url))
+        reduxNavController.navigate(NavigationAction.OpenUrl(url = url))
     }
 }
