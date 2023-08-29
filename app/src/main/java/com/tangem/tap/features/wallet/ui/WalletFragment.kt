0f238ed6dcf693576fc91a9a784a34080f038430
Tangem <git@tangem.com>
2023-08-29 09:42:37 +0300

    Updated on 2026-09-25

--- app/src/main/java/com/tangem/tap/features/wallet/ui/WalletFragment.kt
+++ app/src/main/java/com/tangem/tap/features/wallet/ui/WalletFragment.kt
@@ -25,7 +25,7 @@ import com.badoo.mvicore.modelWatcher
 import com.tangem.core.analytics.Analytics
 import com.tangem.core.navigation.AppScreen
 import com.tangem.core.navigation.NavigationAction
-import com.tangem.core.ui.fragments.setStatusBarColor
+import com.tangem.core.ui.extensions.setStatusBarColor
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.core.ui.utils.OneTouchClickListener
 import com.tangem.datasource.connection.NetworkConnectionManager
