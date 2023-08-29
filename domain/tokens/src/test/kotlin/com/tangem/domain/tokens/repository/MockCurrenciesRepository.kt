62374a80a3fe5bfb109fa94cb398b8f724549ce8
Tangem <git@tangem.com>
2023-08-29 13:55:53 +0500

    Updated on 2026-05-07

--- domain/tokens/src/test/kotlin/com/tangem/domain/tokens/repository/MockCurrenciesRepository.kt
+++ domain/tokens/src/test/kotlin/com/tangem/domain/tokens/repository/MockCurrenciesRepository.kt
@@ -6,10 +6,12 @@ import com.tangem.domain.core.error.DataError
 import com.tangem.domain.tokens.models.CryptoCurrency
 import com.tangem.domain.wallets.models.UserWalletId
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 
 internal class MockCurrenciesRepository(
     private val sortTokensResult: Either<DataError, Unit>,
+    private val removeCurrencyResult: Either<DataError, Unit>,
     private val token: Either<DataError, CryptoCurrency>,
     private val tokens: Flow<Either<DataError, List<CryptoCurrency>>>,
     private val isGrouped: Flow<Either<DataError, Boolean>>,
@@ -36,6 +38,17 @@ internal class MockCurrenciesRepository(
         tokensIdsAfterSortingApply = currencies
         isTokensGroupedAfterSortingApply = isGroupedByNetwork
         isTokensSortedByBalanceAfterSortingApply = isSortedByBalance
+    }
+
+    override suspend fun removeCurrency(userWalletId: UserWalletId, currency: CryptoCurrency) {
+        removeCurrencyResult.onLeft { throw it }
+    }
+
+    override suspend fun getMultiCurrencyWalletCurrenciesSync(
+        userWalletId: UserWalletId,
+        refresh: Boolean,
+    ): List<CryptoCurrency> {
+        return tokens.first().getOrElse { e -> throw e }
     }
 
     override suspend fun getSingleCurrencyWalletPrimaryCurrency(userWalletId: UserWalletId): CryptoCurrency {
