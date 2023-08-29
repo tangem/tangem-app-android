7017d331820677dcb1c9ef9a4896685c1b4ee5dd
Tangem <git@tangem.com>
2023-08-29 09:42:37 +0300

    Updated on 2026-09-23

--- core/ui/src/main/java/com/tangem/core/ui/screen/ComposeBottomSheetFragment.kt
+++ core/ui/src/main/java/com/tangem/core/ui/screen/ComposeBottomSheetFragment.kt
@@ -1,7 +1,6 @@
-package com.tangem.core.ui.fragments
+package com.tangem.core.ui.screen
 
 import android.app.Dialog
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,19 +9,45 @@ import androidx.annotation.FloatRange
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ComposeView
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.tangem.core.ui.R
 import com.tangem.core.ui.res.TangemTheme
 
-abstract class ComposeBottomSheetFragment<ScreenState> : BottomSheetDialogFragment(), ComposeScreen<ScreenState> {
+/**
+ * An abstract base class for bottom sheet dialogs that use Compose for UI rendering.
+ * Extends [BottomSheetDialogFragment] and implements [ComposeScreen] interface.
+ */
+abstract class ComposeBottomSheetFragment : BottomSheetDialogFragment(), ComposeScreen {
+
+    /**
+     * The initial state of the bottom sheet. Default is [BottomSheetBehavior.STATE_EXPANDED].
+     */
     open val initialBottomSheetState = BottomSheetBehavior.STATE_EXPANDED
 
+    /**
+     * The fraction of the screen height that the bottom sheet should take when expanded.
+     * Default is `null`, indicating that the height will be determined by the content.
+     */
     @FloatRange(from = 0.0, to = 1.0)
     open val expandedHeightFraction: Float? = null
+
+    override val screenModifier: Modifier
+        @Composable
+        @ReadOnlyComposable
+        get() = Modifier
+            .fillMaxWidth()
+            .let {
+                if (expandedHeightFraction != null) it.fillMaxHeight(expandedHeightFraction!!) else it
+            }
+            .background(
+                color = TangemTheme.colors.background.plain,
+                shape = TangemTheme.shapes.bottomSheet,
+            )
 
     override fun getTheme(): Int = R.style.AppTheme_TransparentBottomSheetDialog
 
@@ -39,26 +64,5 @@ abstract class ComposeBottomSheetFragment<ScreenState> : BottomSheetDialogFragme
         }
 
         return dialog
-    }
-
-    override fun createComposeView(context: Context): View {
-        return ComposeView(context).apply {
-            setContent {
-                TangemTheme {
-                    ScreenContent(
-                        state = provideState().value,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .let {
-                                if (expandedHeightFraction != null) it.fillMaxHeight(expandedHeightFraction!!) else it
-                            }
-                            .background(
-                                color = TangemTheme.colors.background.plain,
-                                shape = TangemTheme.shapes.bottomSheet,
-                            ),
-                    )
-                }
-            }
-        }
     }
 }
