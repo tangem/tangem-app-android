9fdc1a092b75f05c740720ff2625b1f68eaf0f66
Tangem <git@tangem.com>
2023-08-31 17:41:00 +0300

    Updated on 2025-12-18

--- app/src/main/java/com/tangem/tap/features/details/ui/details/DetailsViewModel.kt
+++ app/src/main/java/com/tangem/tap/features/details/ui/details/DetailsViewModel.kt
@@ -7,16 +7,25 @@ import com.tangem.core.navigation.AppScreen
 import com.tangem.core.navigation.NavigationAction
 import com.tangem.domain.common.util.cardTypesResolver
 import com.tangem.tap.common.analytics.events.Settings
+import com.tangem.tap.common.extensions.dispatchWithMain
 import com.tangem.tap.common.feedback.FeedbackEmail
 import com.tangem.tap.common.feedback.SupportInfo
 import com.tangem.tap.common.redux.AppState
 import com.tangem.tap.common.redux.global.GlobalAction
+import com.tangem.tap.features.details.redux.DetailsAction
 import com.tangem.tap.features.details.redux.DetailsState
 import com.tangem.tap.features.disclaimer.redux.DisclaimerAction
 import com.tangem.tap.features.home.LocaleRegionProvider
 import com.tangem.tap.features.home.RUSSIA_COUNTRY_CODE
 import com.tangem.tap.features.wallet.redux.WalletAction
+import com.tangem.tap.scope
+import com.tangem.tap.userWalletsListManager
 import com.tangem.wallet.BuildConfig
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import org.rekotlin.Store
 
 class DetailsViewModel(private val store: Store<AppState>) {
@@ -24,35 +33,39 @@ class DetailsViewModel(private val store: Store<AppState>) {
     var detailsScreenState: MutableState<DetailsScreenState> = mutableStateOf(updateState(store.state.detailsState))
         private set
 
-    @Suppress("ComplexMethod")
-    fun updateState(state: DetailsState): DetailsScreenState {
-        val cardTypesResolver = state.scanResponse?.cardTypesResolver
-        val settings = SettingsElement.values().mapNotNull {
-            when (it) {
-                SettingsElement.WalletConnect -> {
-                    if (cardTypesResolver?.isMultiwalletAllowed() == true) it else null
-                }
-                SettingsElement.SendFeedback -> it
-                SettingsElement.LinkMoreCards -> if (state.createBackupAllowed) it else null
-                SettingsElement.PrivacyPolicy -> {
-                    if (state.privacyPolicyUrl != null) it else null
-                }
-                SettingsElement.AppSettings -> if (state.appSettingsState.isBiometricsAvailable) it else null
-                SettingsElement.AppCurrency -> if (cardTypesResolver?.isMultiwalletAllowed() != true) it else null
-                SettingsElement.ReferralProgram -> if (cardTypesResolver?.isTangemWallet() == true) it else null
-                SettingsElement.TesterMenu -> if (BuildConfig.TESTER_MENU_ENABLED) it else null
-                else -> it
-            }
-        }
+    init {
+        bootstrapScreenState()
+    }
 
+    fun updateState(state: DetailsState): DetailsScreenState {
         return DetailsScreenState(
-            elements = settings,
+            elements = createSettingsItems(state),
             tangemLinks = getSocialLinks(),
             tangemVersion = getTangemAppVersion(),
             appCurrency = state.appCurrency.name,
             onItemsClick = { handleClickingSettingsItem(it) },
             onSocialNetworkClick = { handleSocialNetworkClick(it) },
         )
+    }
+
+    @Suppress("ComplexMethod")
+    private fun createSettingsItems(state: DetailsState): List<SettingsElement> {
+        val scanResponse = state.scanResponse ?: return emptyList()
+        val cardTypesResolver = scanResponse.cardTypesResolver
+
+        return SettingsElement.values().mapNotNull {
+            when (it) {
+                SettingsElement.WalletConnect -> if (cardTypesResolver.isMultiwalletAllowed()) it else null
+                SettingsElement.SendFeedback -> it
+                SettingsElement.LinkMoreCards -> if (state.createBackupAllowed) it else null
+                SettingsElement.PrivacyPolicy -> if (state.privacyPolicyUrl != null) it else null
+                SettingsElement.AppSettings -> if (state.appSettingsState.isBiometricsAvailable) it else null
+                SettingsElement.AppCurrency -> if (cardTypesResolver.isMultiwalletAllowed()) null else it
+                SettingsElement.ReferralProgram -> if (cardTypesResolver.isTangemWallet()) it else null
+                SettingsElement.TesterMenu -> if (BuildConfig.TESTER_MENU_ENABLED) it else null
+                else -> it
+            }
+        }
     }
 
     private fun handleSocialNetworkClick(link: SocialNetworkLink) {
@@ -117,5 +130,15 @@ class DetailsViewModel(private val store: Store<AppState>) {
         val versionCode: Int = BuildConfig.VERSION_CODE
         val versionName: String = BuildConfig.VERSION_NAME
         return "$versionName ($versionCode)"
+    }
+
+    private fun bootstrapScreenState() {
+        userWalletsListManager.selectedUserWallet
+            .distinctUntilChanged()
+            .onEach { selectedUserWallet ->
+                store.dispatchWithMain(DetailsAction.PrepareScreen(selectedUserWallet.scanResponse))
+            }
+            .flowOn(Dispatchers.IO)
+            .launchIn(scope)
     }
 }
