211e6cf14927cf2cb0f98d83605e003bb86fe95f
Tangem <git@tangem.com>
2023-08-31 10:46:03 +0300

    Updated on 2026-08-27

--- app/src/main/java/com/tangem/tap/di/domain/TokensDomainModule.kt
+++ app/src/main/java/com/tangem/tap/di/domain/TokensDomainModule.kt
@@ -17,6 +17,16 @@ internal object TokensDomainModule {
 
     @Provides
     @ViewModelScoped
+    fun provideFetchTokenListUseCase(
+        currenciesRepository: CurrenciesRepository,
+        quotesRepository: QuotesRepository,
+        networksRepository: NetworksRepository,
+    ): FetchTokenListUseCase {
+        return FetchTokenListUseCase(currenciesRepository, networksRepository, quotesRepository)
+    }
+
+    @Provides
+    @ViewModelScoped
     fun provideGetTokenListUseCase(
         currenciesRepository: CurrenciesRepository,
         quotesRepository: QuotesRepository,
@@ -42,8 +52,8 @@ internal object TokensDomainModule {
         quotesRepository: QuotesRepository,
         networksRepository: NetworksRepository,
         dispatchers: CoroutineDispatcherProvider,
-    ): GetCurrencyUseCase {
-        return GetCurrencyUseCase(currenciesRepository, quotesRepository, networksRepository, dispatchers)
+    ): GetCurrencyStatusUpdatesUseCase {
+        return GetCurrencyStatusUpdatesUseCase(currenciesRepository, quotesRepository, networksRepository, dispatchers)
     }
 
     @Provides
@@ -53,8 +63,23 @@ internal object TokensDomainModule {
         quotesRepository: QuotesRepository,
         networksRepository: NetworksRepository,
         dispatchers: CoroutineDispatcherProvider,
-    ): GetPrimaryCurrencyUseCase {
-        return GetPrimaryCurrencyUseCase(currenciesRepository, quotesRepository, networksRepository, dispatchers)
+    ): GetPrimaryCurrencyStatusUpdatesUseCase {
+        return GetPrimaryCurrencyStatusUpdatesUseCase(
+            currenciesRepository,
+            quotesRepository,
+            networksRepository,
+            dispatchers,
+        )
+    }
+
+    @Provides
+    @ViewModelScoped
+    fun provideFetchCurrencyStatusUseCase(
+        currenciesRepository: CurrenciesRepository,
+        quotesRepository: QuotesRepository,
+        networksRepository: NetworksRepository,
+    ): FetchCurrencyStatusUseCase {
+        return FetchCurrencyStatusUseCase(currenciesRepository, networksRepository, quotesRepository)
     }
 
     @Provides
