4d2691387779a1e8926890218a1910f114bc8dd2
Tangem <git@tangem.com>
2023-08-31 10:46:03 +0300

    Updated on 2025-10-09

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/GetPrimaryCurrencyStatusUpdatesUseCase.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/GetPrimaryCurrencyStatusUpdatesUseCase.kt
@@ -1,10 +1,9 @@
 package com.tangem.domain.tokens
 
 import arrow.core.Either
-import com.tangem.domain.tokens.error.CurrencyError
+import com.tangem.domain.tokens.error.CurrencyStatusError
 import com.tangem.domain.tokens.error.mapper.mapToCurrencyError
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
-import com.tangem.domain.tokens.models.CryptoCurrency
 import com.tangem.domain.tokens.operations.CurrenciesStatusesOperations
 import com.tangem.domain.tokens.repository.CurrenciesRepository
 import com.tangem.domain.tokens.repository.NetworksRepository
@@ -14,14 +13,14 @@ import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import kotlinx.coroutines.flow.*
 
 /**
- * Use case for fetching the status of a specific cryptocurrency associated with a user wallet.
+ * Use case for fetching the status of the primary cryptocurrency associated with a user wallet.
  *
  * @property currenciesRepository Repository for managing and fetching cryptocurrencies.
  * @property quotesRepository Repository for managing and fetching cryptocurrency quotes.
  * @property networksRepository Repository for managing and fetching information related to blockchain networks.
  * @property dispatchers Provides coroutine dispatchers.
  */
-class GetCurrencyUseCase(
+class GetPrimaryCurrencyStatusUpdatesUseCase(
     private val currenciesRepository: CurrenciesRepository,
     private val quotesRepository: QuotesRepository,
     private val networksRepository: NetworksRepository,
@@ -32,34 +31,26 @@ class GetCurrencyUseCase(
      * Invokes the use case.
      *
      * @param userWalletId The unique identifier of the user's wallet.
-     * @param currencyId The unique identifier of the cryptocurrency.
      * @param refresh A boolean flag indicating whether the data should be refreshed.
-     * @return A [Flow] emitting either a [CurrencyError] or a [CryptoCurrencyStatus], indicating the result of the fetch operation.
+     * @return A [Flow] emitting either a [CurrencyStatusError] or a [CryptoCurrencyStatus], indicating the result of the fetch operation.
      */
-    operator fun invoke(
-        userWalletId: UserWalletId,
-        currencyId: CryptoCurrency.ID,
-        refresh: Boolean = false,
-    ): Flow<Either<CurrencyError, CryptoCurrencyStatus>> {
+    operator fun invoke(userWalletId: UserWalletId): Flow<Either<CurrencyStatusError, CryptoCurrencyStatus>> {
         return flow {
-            emitAll(getCurrency(userWalletId, currencyId, refresh))
+            emitAll(getPrimaryCurrency(userWalletId))
         }.flowOn(dispatchers.io)
     }
 
-    private suspend fun getCurrency(
+    private suspend fun getPrimaryCurrency(
         userWalletId: UserWalletId,
-        currencyId: CryptoCurrency.ID,
-        refresh: Boolean,
-    ): Flow<Either<CurrencyError, CryptoCurrencyStatus>> {
+    ): Flow<Either<CurrencyStatusError, CryptoCurrencyStatus>> {
         val operations = CurrenciesStatusesOperations(
             currenciesRepository = currenciesRepository,
             quotesRepository = quotesRepository,
             networksRepository = networksRepository,
             userWalletId = userWalletId,
-            refresh = refresh,
         )
 
-        return operations.getCurrencyStatusFlow(currencyId).map { maybeCurrency ->
+        return operations.getPrimaryCurrencyStatusFlow().map { maybeCurrency ->
             maybeCurrency.mapLeft(CurrenciesStatusesOperations.Error::mapToCurrencyError)
         }
     }
