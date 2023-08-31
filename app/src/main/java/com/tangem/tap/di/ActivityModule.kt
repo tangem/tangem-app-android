6b7464f704f261f549e750bbf39e83a8955b34cf
Tangem <git@tangem.com>
2023-08-31 16:58:05 +0300

    Updated on 2026-07-01

--- app/src/main/java/com/tangem/tap/di/ActivityModule.kt
+++ app/src/main/java/com/tangem/tap/di/ActivityModule.kt
@@ -3,8 +3,11 @@ package com.tangem.tap.di
 import android.content.Context
 import com.tangem.domain.card.ScanCardUseCase
 import com.tangem.domain.card.repository.CardSdkConfigRepository
+import com.tangem.domain.exchange.RampStateManager
 import com.tangem.tap.domain.TangemSdkManager
 import com.tangem.tap.domain.scanCard.repository.DefaultScanCardRepository
+import com.tangem.tap.network.exchangeServices.DefaultRampManager
+import com.tangem.tap.proxy.AppStateHolder
 import com.tangem.tap.userTokensRepository
 import dagger.Module
 import dagger.Provides
@@ -39,5 +42,11 @@ internal object ActivityModule {
                 tangemSdkManager = tangemSdkManager,
             ),
         )
+    }
+
+    @Provides
+    @Singleton
+    fun provideDefaultRampManager(appStateHolder: AppStateHolder): RampStateManager {
+        return DefaultRampManager(appStateHolder.exchangeService)
     }
 }
