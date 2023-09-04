5bc2980f0b453761c9e9f2eed8cbdb7dd0d0f3dd
Tangem <git@tangem.com>
2023-09-04 16:46:04 +0800

    Updated on 2026-07-07

--- app/src/main/java/com/tangem/tap/proxy/redux/DaggerGraphState.kt
+++ app/src/main/java/com/tangem/tap/proxy/redux/DaggerGraphState.kt
@@ -6,6 +6,7 @@ import com.tangem.domain.appcurrency.repository.AppCurrencyRepository
 import com.tangem.domain.card.ScanCardProcessor
 import com.tangem.domain.card.ScanCardUseCase
 import com.tangem.domain.card.repository.CardSdkConfigRepository
+import com.tangem.domain.tokens.repository.CurrenciesRepository
 import com.tangem.domain.walletmanager.WalletManagersFacade
 import com.tangem.features.tester.api.TesterRouter
 import com.tangem.features.tokendetails.featuretoggles.TokenDetailsFeatureToggles
@@ -37,6 +38,9 @@ data class DaggerGraphState(
     val appCurrencyRepository: AppCurrencyRepository? = null,
     val walletManagersFacade: WalletManagersFacade? = null,
     val appStateHolder: AppStateHolder? = null,
+
+    // FIXME: It is used only for TokensList screen. Remove after refactoring of TokensList
+    val currenciesRepository: CurrenciesRepository? = null,
 ) : StateType {
 
     inline fun <reified T> get(getDependency: DaggerGraphState.() -> T?): T {
