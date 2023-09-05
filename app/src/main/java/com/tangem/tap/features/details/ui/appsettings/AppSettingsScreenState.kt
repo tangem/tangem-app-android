5a169e6211756916c448a899eb7797ce6136bb76
Tangem <git@tangem.com>
2023-09-05 10:00:14 +0300

    Updated on 2026-08-14

--- app/src/main/java/com/tangem/tap/features/details/ui/appsettings/AppSettingsScreenState.kt
+++ app/src/main/java/com/tangem/tap/features/details/ui/appsettings/AppSettingsScreenState.kt
@@ -1,11 +1,48 @@
 package com.tangem.tap.features.details.ui.appsettings
 
-import com.tangem.tap.features.details.redux.AppSetting
+import androidx.annotation.DrawableRes
+import androidx.compose.runtime.Immutable
+import com.tangem.core.ui.extensions.TextReference
+import kotlinx.collections.immutable.ImmutableList
 
-internal data class AppSettingsScreenState(
-    val settings: Map<AppSetting, Boolean> = emptyMap(),
-    val showEnrollBiometricsCard: Boolean = false,
-    val isTogglesEnabled: Boolean = false,
-    val onSettingToggled: (AppSetting, Boolean) -> Unit = { _, _ -> /* no-op */ },
-    val onEnrollBiometrics: () -> Unit = { /* no-op */ },
-)
+@Immutable
+internal sealed class AppSettingsScreenState {
+
+    object Loading : AppSettingsScreenState()
+
+    data class Content(
+        val items: ImmutableList<Item>,
+        val alert: Alert?,
+    ) : AppSettingsScreenState()
+
+    @Immutable
+    sealed class Item {
+
+        abstract val id: String
+
+        data class Card(
+            override val id: String,
+            @DrawableRes val iconResId: Int,
+            val title: TextReference,
+            val description: TextReference,
+            val onClick: () -> Unit,
+        ) : Item()
+
+        data class Switch(
+            override val id: String,
+            val title: TextReference,
+            val description: TextReference,
+            val isEnabled: Boolean,
+            val isChecked: Boolean,
+            val onCheckedChange: (Boolean) -> Unit,
+        ) : Item()
+    }
+
+    data class Alert(
+        val title: TextReference,
+        val description: TextReference,
+        val confirmText: TextReference,
+        val onConfirm: () -> Unit,
+        val onDismiss: () -> Unit,
+    )
+}
--- app/src/main/java/com/tangem/tap/features/details/ui/appsettings/AppSettingsViewModel.kt
+++ app/src/main/java/com/tangem/tap/features/details/ui/appsettings/AppSettingsViewModel.kt
@@ -1,35 +1,31 @@
 package com.tangem.tap.features.details.ui.appsettings
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import com.tangem.tap.common.extensions.dispatchOnMain
 import com.tangem.tap.common.redux.AppState
 import com.tangem.tap.features.details.redux.AppSetting
+import com.tangem.tap.features.details.redux.AppSettingsState
 import com.tangem.tap.features.details.redux.DetailsAction
 import com.tangem.tap.features.details.redux.DetailsState
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
 import org.rekotlin.Store
 
 internal class AppSettingsViewModel(private val store: Store<AppState>) {
 
-    fun updateState(state: DetailsState): AppSettingsScreenState {
-        return with(state.appSettingsState) {
-            AppSettingsScreenState(
-                settings = mapOf(
-                    AppSetting.SaveWallets to saveWallets,
-                    AppSetting.SaveAccessCode to saveAccessCodes,
-                ),
-                showEnrollBiometricsCard = needEnrollBiometrics,
-                isTogglesEnabled = !needEnrollBiometrics && !isInProgress,
-                onSettingToggled = { privacySetting, enabled ->
-                    onSettingsToggled(privacySetting, enabled)
-                },
-                onEnrollBiometrics = {
-                    store.dispatchOnMain(DetailsAction.AppSettings.EnrollBiometrics)
-                },
-            )
-        }
-    }
+    private val itemsFactory = AppSettingsItemsFactory()
+    private val alertsFactory = AppSettingsAlertsFactory()
 
-    private fun onSettingsToggled(setting: AppSetting, enable: Boolean) {
-        store.dispatch(DetailsAction.AppSettings.SwitchPrivacySetting(enable = enable, setting = setting))
+    var uiState: AppSettingsScreenState by mutableStateOf(AppSettingsScreenState.Loading)
+        private set
+
+    fun updateState(state: DetailsState) {
+        uiState = AppSettingsScreenState.Content(
+            items = buildItems(state.appSettingsState),
+            alert = (uiState as? AppSettingsScreenState.Content)?.alert,
+        )
     }
 
     fun checkBiometricsStatus() {
@@ -38,5 +34,86 @@ internal class AppSettingsViewModel(private val store: Store<AppState>) {
 
     fun refreshBiometricsStatus() {
         store.dispatch(DetailsAction.AppSettings.CheckBiometricsStatus(awaitStatusChange = true))
+    }
+
+    private fun buildItems(state: AppSettingsState): ImmutableList<AppSettingsScreenState.Item> {
+        val items = buildList {
+            if (state.needEnrollBiometrics) {
+                itemsFactory.createEnrollBiometricsCard(onClick = ::enrollBiometrics).let(::add)
+            }
+
+            if (state.isBiometricsAvailable) {
+                val canUseBiometrics = !state.needEnrollBiometrics && !state.isInProgress
+
+                itemsFactory.createSaveWalletsSwitch(
+                    isChecked = state.saveWallets,
+                    isEnabled = canUseBiometrics,
+                    onCheckedChange = ::onSaveWalletsToggled,
+                ).let(::add)
+
+                itemsFactory.createSaveAccessCodeSwitch(
+                    isChecked = state.saveAccessCodes,
+                    isEnabled = canUseBiometrics,
+                    onCheckedChange = ::onSaveAccessCodesToggled,
+                ).let(::add)
+            }
+        }
+
+        return items.toImmutableList()
+    }
+
+    private fun enrollBiometrics() {
+        store.dispatchOnMain(DetailsAction.AppSettings.EnrollBiometrics)
+    }
+
+    private fun onSaveWalletsToggled(isChecked: Boolean) {
+        if (isChecked) {
+            onSettingsToggled(AppSetting.SaveWallets, enable = true)
+        } else {
+            updateContentState {
+                copy(
+                    alert = alertsFactory.createDeleteSavedWalletsAlert(
+                        onDelete = {
+                            onSettingsToggled(AppSetting.SaveWallets, enable = false)
+                            dismissDialog()
+                        },
+                        onDismiss = ::dismissDialog,
+                    ),
+                )
+            }
+        }
+    }
+
+    private fun onSaveAccessCodesToggled(isChecked: Boolean) {
+        if (isChecked) {
+            onSettingsToggled(AppSetting.SaveAccessCode, enable = true)
+        } else {
+            updateContentState {
+                copy(
+                    alert = alertsFactory.createDeleteSavedAccessCodesAlert(
+                        onDelete = {
+                            onSettingsToggled(AppSetting.SaveAccessCode, enable = false)
+                            dismissDialog()
+                        },
+                        onDismiss = ::dismissDialog,
+                    ),
+                )
+            }
+        }
+    }
+
+    private fun onSettingsToggled(setting: AppSetting, enable: Boolean) {
+        store.dispatch(DetailsAction.AppSettings.SwitchPrivacySetting(enable = enable, setting = setting))
+    }
+
+    private fun dismissDialog() {
+        updateContentState { copy(alert = null) }
+    }
+
+    private fun updateContentState(block: AppSettingsScreenState.Content.() -> AppSettingsScreenState.Content) {
+        uiState = when (val state = uiState) {
+            is AppSettingsScreenState.Content -> block(state)
+            is AppSettingsScreenState.Loading -> state
+        }
     }
 }
