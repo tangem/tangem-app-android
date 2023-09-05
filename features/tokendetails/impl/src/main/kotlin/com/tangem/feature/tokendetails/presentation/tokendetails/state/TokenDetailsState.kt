2074a21443c05d555ff3795f17669761b0185243
Tangem <git@tangem.com>
2023-09-05 11:26:59 +0500

    Updated on 2026-08-27

--- features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/TokenDetailsState.kt
+++ features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/TokenDetailsState.kt
@@ -1,14 +1,17 @@
 package com.tangem.feature.tokendetails.presentation.tokendetails.state
 
 import com.tangem.core.ui.components.marketprice.MarketPriceBlockState
+import com.tangem.core.ui.components.transactions.state.TransactionState
 import com.tangem.core.ui.components.transactions.state.TxHistoryState
 import com.tangem.feature.tokendetails.presentation.tokendetails.state.components.TokenDetailsDialogConfig
+import kotlinx.collections.immutable.PersistentList
 
 internal data class TokenDetailsState(
     val topAppBarConfig: TokenDetailsTopAppBarConfig,
     val tokenInfoBlockState: TokenInfoBlockState,
     val tokenBalanceBlockState: TokenDetailsBalanceBlockState,
     val marketPriceBlockState: MarketPriceBlockState,
+    val pendingTxs: PersistentList<TransactionState>,
     val txHistoryState: TxHistoryState,
     val dialogConfig: TokenDetailsDialogConfig?,
 )
