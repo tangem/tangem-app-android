7a12635a90063fbd135076b4d12027ce7626986b
Tangem <git@tangem.com>
2023-09-05 10:00:14 +0300

    Updated on 2026-06-01

--- app/src/main/java/com/tangem/tap/features/details/ui/appsettings/components/CardItem.kt
+++ app/src/main/java/com/tangem/tap/features/details/ui/appsettings/components/CardItem.kt
@@ -1,11 +1,6 @@
 package com.tangem.tap.features.details.ui.appsettings.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.Surface
@@ -14,24 +9,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.datasource.CollectionPreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import com.tangem.core.ui.components.SpacerH4
 import com.tangem.core.ui.components.SpacerW16
+import com.tangem.core.ui.extensions.resolveReference
 import com.tangem.core.ui.res.TangemTheme
-import com.tangem.wallet.R
+import com.tangem.tap.features.details.ui.appsettings.AppSettingsItemsFactory
+import com.tangem.tap.features.details.ui.appsettings.AppSettingsScreenState.Item
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
-internal fun EnrollBiometricsCard(onClick: () -> Unit) {
+internal fun CardItem(item: Item.Card, modifier: Modifier = Modifier) {
     Surface(
-        modifier = Modifier
-            .padding(horizontal = TangemTheme.dimens.spacing8)
-            .fillMaxWidth(),
-        color = TangemTheme.colors.background.primary,
+        modifier = modifier.fillMaxWidth(),
+        color = TangemTheme.colors.button.disabled,
         shape = TangemTheme.shapes.roundedCornersLarge,
-        onClick = onClick,
+        onClick = item.onClick,
     ) {
         Row(
             modifier = Modifier.padding(all = 16.dp),
@@ -39,20 +35,20 @@ internal fun EnrollBiometricsCard(onClick: () -> Unit) {
             horizontalArrangement = Arrangement.SpaceEvenly,
         ) {
             Icon(
-                painter = painterResource(id = R.drawable.ic_alert_circle_24),
+                painter = painterResource(id = item.iconResId),
                 tint = TangemTheme.colors.icon.attention,
                 contentDescription = null,
             )
             SpacerW16()
             Column {
                 Text(
-                    text = stringResource(id = R.string.app_settings_enable_biometrics_title),
+                    text = item.title.resolveReference(),
                     style = TangemTheme.typography.subtitle1,
                     color = TangemTheme.colors.text.primary1,
                 )
                 SpacerH4()
                 Text(
-                    text = stringResource(id = R.string.app_settings_enable_biometrics_description),
+                    text = item.description.resolveReference(),
                     style = TangemTheme.typography.body2,
                     color = TangemTheme.colors.text.secondary,
                 )
@@ -62,28 +58,29 @@ internal fun EnrollBiometricsCard(onClick: () -> Unit) {
 }
 
 // region Preview
-@Composable
-private fun EnrollBiometricsCardSample(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier.background(TangemTheme.colors.background.secondary),
-    ) {
-        EnrollBiometricsCard(onClick = {})
-    }
-}
-
 @Preview(showBackground = true, widthDp = 360)
 @Composable
-private fun EnrollBiometricsCardPreview_Light() {
+private fun CardItemPreview_Light(@PreviewParameter(CardItemProvider::class) item: Item.Card) {
     TangemTheme {
-        EnrollBiometricsCardSample()
+        CardItem(item = item)
     }
 }
 
 @Preview(showBackground = true, widthDp = 360)
 @Composable
-private fun EnrollBiometricsCardPreview_Dark() {
+private fun CardItemPreview_Dark(@PreviewParameter(CardItemProvider::class) item: Item.Card) {
     TangemTheme(isDark = true) {
-        EnrollBiometricsCardSample()
+        CardItem(item = item)
     }
 }
+
+private class CardItemProvider : CollectionPreviewParameterProvider<Item.Card>(
+    collection = buildList {
+        val itemsFactory = AppSettingsItemsFactory()
+
+        itemsFactory.createEnrollBiometricsCard(
+            onClick = { /* no-op */ },
+        ).let(::add)
+    },
+)
 // endregion Preview
