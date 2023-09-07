93b0b05e697a9efe4d4f804001b058440ae47a11
Tangem <git@tangem.com>
2023-09-07 10:51:42 +0300

    Updated on 2026-08-14

--- app/src/main/java/com/tangem/tap/features/details/ui/appsettings/components/SettingsSelectorDialog.kt
+++ app/src/main/java/com/tangem/tap/features/details/ui/appsettings/components/SettingsSelectorDialog.kt
@@ -5,29 +5,26 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.datasource.CollectionPreviewParameterProvider
-import com.tangem.core.ui.components.BasicDialog
 import com.tangem.core.ui.components.DialogButton
+import com.tangem.core.ui.components.SelectorDialog
 import com.tangem.core.ui.extensions.resolveReference
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.tap.features.details.ui.appsettings.AppSettingsDialogsFactory
 import com.tangem.tap.features.details.ui.appsettings.AppSettingsScreenState.Dialog
 import com.tangem.wallet.R
+import kotlinx.collections.immutable.toImmutableList
 
 @Composable
-internal fun SettingsAlertDialog(dialog: Dialog.Alert) {
-    BasicDialog(
+internal fun SettingsSelectorDialog(dialog: Dialog.Selector) {
+    SelectorDialog(
         title = dialog.title.resolveReference(),
-        message = dialog.description.resolveReference(),
-        isDismissable = false,
+        selectedItemIndex = dialog.selectedItemIndex,
+        items = dialog.items.map { it.resolveReference() }.toImmutableList(),
         confirmButton = DialogButton(
-            title = dialog.confirmText.resolveReference(),
-            warning = true,
-            onClick = dialog.onConfirm,
-        ),
-        dismissButton = DialogButton(
-            title = stringResource(id = R.string.common_cancel),
+            title = stringResource(R.string.common_cancel),
             onClick = dialog.onDismiss,
         ),
+        onSelect = dialog.onSelect,
         onDismissDialog = dialog.onDismiss,
     )
 }
@@ -35,26 +32,27 @@ internal fun SettingsAlertDialog(dialog: Dialog.Alert) {
 // region Preview
 @Preview(showBackground = true, widthDp = 360)
 @Composable
-private fun AlertDialogPreview_Light(@PreviewParameter(AlertDialogProvider::class) dialog: Dialog.Alert) {
-    TangemTheme {
-        SettingsAlertDialog(dialog = dialog)
+private fun SettingsSelectorDialogPreview_Light(@PreviewParameter(DialogProvider::class) param: Dialog.Selector) {
+    TangemTheme(isDark = false) {
+        SettingsSelectorDialog(param)
     }
 }
 
 @Preview(showBackground = true, widthDp = 360)
 @Composable
-private fun AlertDialogPreview_Dark(@PreviewParameter(AlertDialogProvider::class) dialog: Dialog.Alert) {
+private fun SettingsSelectorDialogPreview_Dark(@PreviewParameter(DialogProvider::class) param: Dialog.Selector) {
     TangemTheme(isDark = true) {
-        SettingsAlertDialog(dialog = dialog)
+        SettingsSelectorDialog(param)
     }
 }
 
-private class AlertDialogProvider : CollectionPreviewParameterProvider<Dialog.Alert>(
-    collection = buildList {
-        val dialogsFactory = AppSettingsDialogsFactory()
-
-        dialogsFactory.createDeleteSavedAccessCodesAlert({}, {}).let(::add)
-        dialogsFactory.createDeleteSavedWalletsAlert({}, {}).let(::add)
-    },
+private class DialogProvider : CollectionPreviewParameterProvider<Dialog.Selector>(
+    collection = listOf(
+        AppSettingsDialogsFactory().createThemeModeSelectorDialog(
+            selectedModeIndex = 0,
+            onSelect = {},
+            onDismiss = {},
+        ),
+    ),
 )
 // endregion Preview
