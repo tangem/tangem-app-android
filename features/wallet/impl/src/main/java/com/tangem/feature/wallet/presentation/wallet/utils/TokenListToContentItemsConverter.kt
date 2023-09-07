69124222ea7ab4f9a2364b5651f405b3e597f263
Tangem <git@tangem.com>
2023-09-07 18:40:38 +0300

    Updated on 2026-07-01

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/utils/TokenListToContentItemsConverter.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/utils/TokenListToContentItemsConverter.kt
@@ -7,8 +7,8 @@ import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.domain.tokens.model.NetworkGroup
 import com.tangem.domain.tokens.model.TokenList
 import com.tangem.feature.wallet.presentation.wallet.state.components.WalletTokensListState
+import com.tangem.feature.wallet.presentation.wallet.state.components.WalletTokensListState.OrganizeTokensButtonState
 import com.tangem.feature.wallet.presentation.wallet.state.components.WalletTokensListState.TokensListItemState
-import com.tangem.feature.wallet.presentation.wallet.utils.LoadingItemsProvider.getLoadingMultiCurrencyTokens
 import com.tangem.feature.wallet.presentation.wallet.viewmodels.WalletClickIntents
 import com.tangem.utils.converter.Converter
 import kotlinx.collections.immutable.PersistentList
@@ -28,26 +28,15 @@ internal class TokenListToContentItemsConverter(
     )
 
     override fun convert(value: TokenList): WalletTokensListState {
-        val isEmptyList = when (value) {
-            is TokenList.GroupedByNetwork -> value.groups.isEmpty()
-            is TokenList.NotInitialized -> false
-            is TokenList.Ungrouped -> value.currencies.isEmpty()
-        }
-
-        return if (isEmptyList) {
-            WalletTokensListState.Empty
-        } else {
-            WalletTokensListState.Content(
-                items = when (value) {
-                    is TokenList.GroupedByNetwork -> value.mapToMultiCurrencyItems()
-                    is TokenList.Ungrouped -> value.mapToMultiCurrencyItems()
-                    is TokenList.NotInitialized -> getLoadingMultiCurrencyTokens()
-                },
-                onOrganizeTokensClick = if (value.totalFiatBalance is TokenList.FiatBalance.Loaded) {
-                    clickIntents::onOrganizeTokensClick
-                } else {
-                    null
-                },
+        return when (value) {
+            is TokenList.NotInitialized -> WalletTokensListState.Loading()
+            is TokenList.GroupedByNetwork -> WalletTokensListState.Content(
+                items = value.mapToMultiCurrencyItems(),
+                organizeTokensButton = value.mapToOrganizeTokensButtonState(),
+            )
+            is TokenList.Ungrouped -> WalletTokensListState.Content(
+                items = value.mapToMultiCurrencyItems(),
+                organizeTokensButton = value.mapToOrganizeTokensButtonState(),
             )
         }
     }
@@ -62,6 +51,20 @@ internal class TokenListToContentItemsConverter(
         return currencies.fold(initial = persistentListOf()) { acc, token ->
             acc.mutate { it.addToken(token) }
         }
+    }
+
+    private fun TokenList.GroupedByNetwork.mapToOrganizeTokensButtonState(): OrganizeTokensButtonState {
+        return getOrganizeTokensButtonState(
+            isLoading = totalFiatBalance is TokenList.FiatBalance.Loading,
+            currenciesSize = groups.flatMap(NetworkGroup::currencies).size,
+        )
+    }
+
+    private fun TokenList.Ungrouped.mapToOrganizeTokensButtonState(): OrganizeTokensButtonState {
+        return getOrganizeTokensButtonState(
+            isLoading = totalFiatBalance is TokenList.FiatBalance.Loading,
+            currenciesSize = currencies.size,
+        )
     }
 
     private fun MutableList<TokensListItemState>.addGroup(group: NetworkGroup): List<TokensListItemState> {
@@ -80,5 +83,16 @@ internal class TokenListToContentItemsConverter(
         this.add(TokensListItemState.Token(tokenItemState))
 
         return this
+    }
+
+    private fun getOrganizeTokensButtonState(isLoading: Boolean, currenciesSize: Int): OrganizeTokensButtonState {
+        return if (currenciesSize > 1) {
+            OrganizeTokensButtonState.Visible(
+                isEnabled = !isLoading,
+                onClick = clickIntents::onOrganizeTokensClick,
+            )
+        } else {
+            OrganizeTokensButtonState.Hidden
+        }
     }
 }
