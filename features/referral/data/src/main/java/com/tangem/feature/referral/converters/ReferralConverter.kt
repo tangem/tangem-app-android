164b6e3c71c249520a47b0434c0f832f7ea5f89c
Tangem <git@tangem.com>
2023-09-07 13:04:47 +0300

    Updated on 2026-08-14

--- features/referral/data/src/main/java/com/tangem/feature/referral/converters/ReferralConverter.kt
+++ features/referral/data/src/main/java/com/tangem/feature/referral/converters/ReferralConverter.kt
@@ -1,13 +1,17 @@
 package com.tangem.feature.referral.converters
 
+import android.text.format.DateUtils
 import com.tangem.datasource.api.tangemTech.models.ReferralResponse
-import com.tangem.feature.referral.domain.models.DiscountType
-import com.tangem.feature.referral.domain.models.ReferralData
-import com.tangem.feature.referral.domain.models.ReferralInfo
-import com.tangem.feature.referral.domain.models.TokenData
+import com.tangem.feature.referral.domain.models.*
 import com.tangem.utils.converter.Converter
+import com.tangem.utils.extensions.isToday
+import com.tangem.utils.extensions.isYesterday
 import com.tangem.utils.safeValueOf
+import org.joda.time.DateTime
+import org.joda.time.DateTimeZone
+import org.joda.time.format.DateTimeFormatterBuilder
 import org.joda.time.format.ISODateTimeFormat
+import java.util.Locale
 import javax.inject.Inject
 
 class ReferralConverter @Inject constructor() : Converter<ReferralResponse, ReferralData> {
@@ -16,6 +20,7 @@ class ReferralConverter @Inject constructor() : Converter<ReferralResponse, Refe
         val conditions = value.conditions
         val referral = value.referral
         val tokenConverter = TokenConverter()
+        val expectedAwardsConverter = ExpectedAwardsConverter()
         return if (referral != null) {
             ReferralData.ParticipantData(
                 award = conditions.awards.firstOrNull()?.amount ?: 0,
@@ -32,6 +37,9 @@ class ReferralConverter @Inject constructor() : Converter<ReferralResponse, Refe
                         ISODateTimeFormat.dateTimeParser().parseDateTime(referral.termsAcceptedAt)
                     },
                 ),
+                expectedAwards = value.expectedAwards?.let {
+                    expectedAwardsConverter.convert(it)
+                },
             )
         } else {
             ReferralData.NonParticipantData(
@@ -41,6 +49,47 @@ class ReferralConverter @Inject constructor() : Converter<ReferralResponse, Refe
                 tosLink = conditions.tosLink,
                 tokens = tokenConverter.convertList(conditions.awards.map { it.token }),
             )
+        }
+    }
+}
+
+private class ExpectedAwardsConverter : Converter<ReferralResponse.ExpectedAwards, ExpectedAwards> {
+
+    /** Example, 2 Aug, 2023 */
+    private val dateFormatter by lazy {
+        DateTimeFormatterBuilder()
+            .appendDayOfMonth(1)
+            .appendLiteral(' ')
+            .appendMonthOfYearShortText()
+            .appendLiteral(", ")
+            .appendYear(4, 4)
+            .toFormatter()
+            .withLocale(Locale.getDefault())
+    }
+
+    override fun convert(value: ReferralResponse.ExpectedAwards): ExpectedAwards {
+        return ExpectedAwards(
+            numberOfWallets = value.numberOfWallets,
+            expectedAwards = value.list.map {
+                ExpectedAward(
+                    paymentDate = it.paymentDate.toDateTimeAtStartOfDay().millis.toDateFormat(),
+                    amount = "${it.amount} ${it.currency}",
+                )
+            },
+        )
+    }
+
+    private fun Long.toDateFormat(): String {
+        val localDate = DateTime(this, DateTimeZone.getDefault())
+        return if (localDate.isToday() || localDate.isYesterday()) {
+            DateUtils.getRelativeTimeSpanString(
+                this,
+                DateTime.now().millis,
+                DateUtils.DAY_IN_MILLIS,
+                DateUtils.FORMAT_ABBREV_RELATIVE,
+            ).toString()
+        } else {
+            dateFormatter.print(localDate)
         }
     }
 }
