b4648849c2137763316057baef71fb9d0b3d9f96
Tangem <git@tangem.com>
2023-09-11 16:57:06 +0400

    Updated on 2025-09-17

--- features/referral/presentation/src/main/java/com/tangem/feature/referral/ReferralFragment.kt
+++ features/referral/presentation/src/main/java/com/tangem/feature/referral/ReferralFragment.kt
@@ -1,25 +1,29 @@
 package com.tangem.feature.referral
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ComposeView
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.transition.TransitionInflater
+import com.tangem.core.ui.components.SystemBarsEffect
 import com.tangem.core.ui.res.TangemTheme
+import com.tangem.core.ui.screen.ComposeFragment
+import com.tangem.core.ui.theme.AppThemeModeHolder
 import com.tangem.feature.referral.presentation.R
 import com.tangem.feature.referral.router.ReferralRouter
 import com.tangem.feature.referral.ui.ReferralScreen
 import com.tangem.feature.referral.viewmodels.ReferralViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import java.lang.ref.WeakReference
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class ReferralFragment : Fragment() {
+class ReferralFragment : ComposeFragment() {
+
+    @Inject
+    override lateinit var appThemeModeHolder: AppThemeModeHolder
+
     private val viewModel by viewModels<ReferralViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,19 +33,17 @@ class ReferralFragment : Fragment() {
         exitTransition = inflater.inflateTransition(R.transition.fade)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    @Composable
+    override fun ScreenContent(modifier: Modifier) {
         viewModel.setRouter(ReferralRouter(fragmentManager = WeakReference(parentFragmentManager)))
         viewModel.onScreenOpened()
-        return ComposeView(inflater.context).apply {
-            isTransitionGroup = true
-            setContent {
-                TangemTheme {
-                    ReferralScreen(
-                        modifier = Modifier.systemBarsPadding(),
-                        stateHolder = viewModel.uiState,
-                    )
-                }
-            }
-        }
+
+        val backgroundColor = TangemTheme.colors.background.secondary
+        SystemBarsEffect { setSystemBarsColor(backgroundColor) }
+
+        ReferralScreen(
+            modifier = Modifier.systemBarsPadding(),
+            stateHolder = viewModel.uiState,
+        )
     }
 }
