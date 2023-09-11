627845489f1e86cda62cd23d551c40c089188421
Tangem <git@tangem.com>
2023-09-11 16:33:28 +0300

    Updated on 2025-10-01

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/factory/TokenActionsProvider.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/factory/TokenActionsProvider.kt
@@ -1,6 +1,8 @@
 package com.tangem.feature.wallet.presentation.wallet.state.factory
 
-import com.tangem.domain.tokens.model.CryptoCurrencyStatus
+import com.tangem.core.ui.extensions.TextReference
+import com.tangem.core.ui.extensions.resourceReference
+import com.tangem.domain.tokens.model.TokenActionsState
 import com.tangem.feature.wallet.impl.R
 import com.tangem.feature.wallet.presentation.common.state.TokenItemState
 import com.tangem.feature.wallet.presentation.wallet.state.TokenActionButtonConfig
@@ -14,35 +16,57 @@ import kotlinx.collections.immutable.toImmutableList
  * @property clickIntents screen click intents
  *
  */
+@Suppress("UnusedPrivateMember") // will be used in next PRs
 internal class TokenActionsProvider(private val clickIntents: WalletClickIntents) {
 
-    fun provideActions(cryptoCurrencyStatus: CryptoCurrencyStatus): ImmutableList<TokenActionButtonConfig> {
-        // TODO: [REDACTED_JIRA]
-        return mockTokenActionButtonConfig(cryptoCurrencyStatus).toImmutableList()
+    fun provideActions(tokenActions: TokenActionsState): ImmutableList<TokenActionButtonConfig> {
+        return convertTokenActionsState(tokenActions)
     }
 
-    private fun mockTokenActionButtonConfig(cryptoCurrencyStatus: CryptoCurrencyStatus): List<TokenActionButtonConfig> {
-        return listOf(
-            TokenActionButtonConfig(
-                text = "Send",
-                iconResId = R.drawable.ic_plus_24,
-                onClick = { clickIntents.onMultiCurrencySendClick(cryptoCurrencyStatus) },
-            ),
-            TokenActionButtonConfig(
-                text = "Buy",
-                iconResId = R.drawable.ic_plus_24,
-                onClick = {},
-            ),
-            TokenActionButtonConfig(
-                text = "Sell",
-                iconResId = R.drawable.ic_plus_24,
-                onClick = {},
-            ),
-            TokenActionButtonConfig(
-                text = "Swap",
-                iconResId = R.drawable.ic_plus_24,
-                onClick = {},
-            ),
+    private fun convertTokenActionsState(
+        tokenActionsState: TokenActionsState,
+    ): ImmutableList<TokenActionButtonConfig> {
+        return tokenActionsState.states
+            .map(::tokenActionStateMapper)
+            .toImmutableList()
+    }
+
+    private fun tokenActionStateMapper(actionsState: TokenActionsState.ActionState): TokenActionButtonConfig {
+        val title: TextReference
+        val icon: Int
+        val action: () -> Unit
+        when (actionsState) {
+            is TokenActionsState.ActionState.Buy -> {
+                title = resourceReference(R.string.common_buy)
+                icon = R.drawable.ic_plus_24
+                action = { }
+            }
+            is TokenActionsState.ActionState.Receive -> {
+                title = resourceReference(R.string.common_receive)
+                icon = R.drawable.ic_arrow_down_24
+                action = { }
+            }
+            is TokenActionsState.ActionState.Sell -> {
+                title = resourceReference(R.string.common_sell)
+                icon = R.drawable.ic_currency_24
+                action = { }
+            }
+            is TokenActionsState.ActionState.Send -> {
+                title = resourceReference(R.string.common_send)
+                icon = R.drawable.ic_arrow_up_24
+                action = { }
+            }
+            is TokenActionsState.ActionState.Swap -> {
+                title = resourceReference(R.string.common_swap)
+                icon = R.drawable.ic_exchange_horizontal_24
+                action = { }
+            }
+        }
+        return TokenActionButtonConfig(
+            text = title,
+            iconResId = icon,
+            onClick = action,
+            enabled = actionsState.enabled,
         )
     }
 }
