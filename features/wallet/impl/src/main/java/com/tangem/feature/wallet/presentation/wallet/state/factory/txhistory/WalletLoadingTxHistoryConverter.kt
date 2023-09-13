b3e9cf86329a0af09303a6becc22bb61923c8e34
Tangem <git@tangem.com>
2023-09-13 13:34:38 +0300

    Updated on 2025-10-13

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/factory/txhistory/WalletLoadingTxHistoryConverter.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/factory/txhistory/WalletLoadingTxHistoryConverter.kt
@@ -2,41 +2,45 @@ package com.tangem.feature.wallet.presentation.wallet.state.factory.txhistory
 
 import androidx.paging.PagingData
 import arrow.core.Either
+import com.tangem.common.Converter
 import com.tangem.common.Provider
 import com.tangem.core.ui.components.transactions.state.TransactionState
 import com.tangem.core.ui.components.transactions.state.TxHistoryState.*
+import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.domain.txhistory.models.TxHistoryStateError
 import com.tangem.feature.wallet.presentation.wallet.state.WalletSingleCurrencyState
 import com.tangem.feature.wallet.presentation.wallet.state.WalletState
 import com.tangem.feature.wallet.presentation.wallet.viewmodels.WalletClickIntents
-import com.tangem.utils.converter.Converter
 import kotlinx.coroutines.flow.update
 
 /**
  * Converter from loading tx history state to [WalletSingleCurrencyState.Content]
  *
- * @property currentStateProvider            current state provider
- * @property clickIntents                    screen click intents
+ * @property currentStateProvider                    current state provider
+ * @property clickIntents                            screen click intents
  *
 [REDACTED_AUTHOR]
  */
 internal class WalletLoadingTxHistoryConverter(
     private val currentStateProvider: Provider<WalletState>,
     private val clickIntents: WalletClickIntents,
-) : Converter<Either<TxHistoryStateError, Int>, WalletState> {
+) : Converter<WalletLoadingTxHistoryConverter.WalletLoadingTxHistoryModel, WalletState> {
 
-    override fun convert(value: Either<TxHistoryStateError, Int>): WalletState {
-        return value.fold(ifLeft = ::convertError, ifRight = ::convert)
+    override fun convert(value: WalletLoadingTxHistoryModel): WalletState {
+        return value.historyLoadingState.fold(
+            ifLeft = { convertError(it, value.cryptoCurrencyStatus) },
+            ifRight = ::convertRight,
+        )
     }
 
-    private fun convertError(error: TxHistoryStateError): WalletState {
+    private fun convertError(error: TxHistoryStateError, cryptoCurrencyStatus: CryptoCurrencyStatus): WalletState {
         val state = currentStateProvider()
 
         return if (state is WalletSingleCurrencyState.Content) {
             state.copy(
                 txHistoryState = when (error) {
                     is TxHistoryStateError.EmptyTxHistories -> {
-                        Empty(onBuyClick = clickIntents::onBuyClick)
+                        Empty(onBuyClick = { clickIntents.onBuyClick(cryptoCurrencyStatus) })
                     }
                     is TxHistoryStateError.DataError -> {
                         Error(onReloadClick = clickIntents::onReloadClick)
@@ -51,7 +55,7 @@ internal class WalletLoadingTxHistoryConverter(
         }
     }
 
-    private fun convert(value: Int): WalletState {
+    private fun convertRight(value: Int): WalletState {
         val state = currentStateProvider()
         val txHistoryContent = (state as? WalletSingleCurrencyState.Content)?.txHistoryState as? Content
 
@@ -71,4 +75,9 @@ internal class WalletLoadingTxHistoryConverter(
 
         return state
     }
+
+    data class WalletLoadingTxHistoryModel(
+        val historyLoadingState: Either<TxHistoryStateError, Int>,
+        val cryptoCurrencyStatus: CryptoCurrencyStatus,
+    )
 }
