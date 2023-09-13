2f46a60a44a8b54f0520e445e3d539305865d737
Tangem <git@tangem.com>
2023-09-13 13:26:01 +0300

    Updated on 2026-01-24

--- app/src/main/java/com/tangem/tap/features/details/ui/details/DetailsViewModel.kt
+++ app/src/main/java/com/tangem/tap/features/details/ui/details/DetailsViewModel.kt
@@ -5,6 +5,10 @@ import androidx.compose.runtime.mutableStateOf
 import com.tangem.core.analytics.Analytics
 import com.tangem.core.navigation.AppScreen
 import com.tangem.core.navigation.NavigationAction
+import com.tangem.core.ui.event.StateEvent
+import com.tangem.core.ui.event.consumedEvent
+import com.tangem.core.ui.event.triggeredEvent
+import com.tangem.core.ui.extensions.TextReference
 import com.tangem.domain.common.util.cardTypesResolver
 import com.tangem.tap.common.analytics.events.Settings
 import com.tangem.tap.common.extensions.dispatchWithMain
@@ -21,6 +25,9 @@ import com.tangem.tap.features.wallet.redux.WalletAction
 import com.tangem.tap.scope
 import com.tangem.tap.userWalletsListManager
 import com.tangem.wallet.BuildConfig
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.toImmutableList
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flowOn
@@ -42,82 +49,64 @@ internal class DetailsViewModel(private val store: Store<AppState>) {
             elements = createSettingsItems(state),
             tangemLinks = getSocialLinks(),
             tangemVersion = getTangemAppVersion(),
-            onItemsClick = { handleClickingSettingsItem(it) },
-            onSocialNetworkClick = { handleSocialNetworkClick(it) },
+            showSnackbar = triggerErrorSnackbarIfNeeded(state.error),
+            onSocialNetworkClick = ::handleSocialNetworkClick,
         )
     }
 
-    @Suppress("ComplexMethod")
-    private fun createSettingsItems(state: DetailsState): List<SettingsElement> {
-        val scanResponse = state.scanResponse ?: return emptyList()
+    private fun createSettingsItems(state: DetailsState): ImmutableList<SettingsItem> {
+        val scanResponse = state.scanResponse ?: return persistentListOf()
         val cardTypesResolver = scanResponse.cardTypesResolver
 
-        return SettingsElement.values().mapNotNull {
-            when (it) {
-                SettingsElement.WalletConnect -> if (cardTypesResolver.isMultiwalletAllowed()) it else null
-                SettingsElement.SendFeedback -> it
-                SettingsElement.LinkMoreCards -> if (state.createBackupAllowed) it else null
-                SettingsElement.PrivacyPolicy -> if (state.privacyPolicyUrl != null) it else null
-                SettingsElement.AppSettings -> if (state.appSettingsState.isBiometricsAvailable) it else null
-                SettingsElement.ReferralProgram -> if (cardTypesResolver.isTangemWallet()) it else null
-                SettingsElement.TesterMenu -> if (BuildConfig.TESTER_MENU_ENABLED) it else null
-                else -> it
-            }
-        }
+        return buildList {
+            SettingsItem.WalletConnect(::navigateToWalletConnect)
+                .takeIf { cardTypesResolver.isMultiwalletAllowed() }
+                ?.let(::add)
+
+            SettingsItem.AddWallet(showProgress = state.isScanningInProgress, ::scanAndSaveUserWallet)
+                .takeIf { state.appSettingsState.saveWallets }
+                ?.let(::add)
+
+            SettingsItem.ScanWallet(showProgress = state.isScanningInProgress, ::scanAndSaveUserWallet)
+                .takeUnless { state.appSettingsState.saveWallets }
+                ?.let(::add)
+
+            SettingsItem.LinkMoreCards(::linkMoreCards)
+                .takeIf { state.createBackupAllowed }
+                ?.let(::add)
+
+            SettingsItem.CardSettings(::navigateToCardSettings)
+                .let(::add)
+
+            SettingsItem.AppSettings(::navigateToAppSettings)
+                .let(::add)
+
+            SettingsItem.Chat(::navigateToChat)
+                .let(::add)
+
+            SettingsItem.SendFeedback(::sendFeedback)
+                .let(::add)
+
+            SettingsItem.ReferralProgram(::navigateToReferralProgram)
+                .takeIf { cardTypesResolver.isTangemWallet() }
+                ?.let(::add)
+
+            SettingsItem.TermsOfService(::navigateToToS)
+                .let(::add)
+
+            SettingsItem.TesterMenu(::navigateToTesterMenu)
+                .takeIf { BuildConfig.TESTER_MENU_ENABLED }
+                ?.let(::add)
+        }.toImmutableList()
     }
 
-    private fun handleSocialNetworkClick(link: SocialNetworkLink) {
-        Analytics.send(Settings.ButtonSocialNetwork(link.network))
-        store.dispatch(NavigationAction.OpenUrl(link.url))
-    }
-
-    private fun handleClickingSettingsItem(item: SettingsElement) {
-        when (item) {
-            SettingsElement.WalletConnect -> {
-                Analytics.send(Settings.ButtonWalletConnect())
-                store.dispatch(NavigationAction.NavigateTo(AppScreen.WalletConnectSessions))
-            }
-            SettingsElement.Chat -> {
-                Analytics.send(Settings.ButtonChat())
-                store.dispatch(GlobalAction.OpenChat(SupportInfo()))
-            }
-            SettingsElement.SendFeedback -> {
-                Analytics.send(Settings.ButtonSendFeedback())
-                store.dispatch(GlobalAction.SendEmail(FeedbackEmail()))
-            }
-            SettingsElement.CardSettings -> {
-                Analytics.send(Settings.ButtonCardSettings())
-                store.dispatch(NavigationAction.NavigateTo(AppScreen.CardSettings))
-            }
-            SettingsElement.AppSettings -> {
-                Analytics.send(Settings.ButtonAppSettings())
-                store.dispatch(NavigationAction.NavigateTo(AppScreen.AppSettings))
-            }
-            SettingsElement.LinkMoreCards -> {
-                Analytics.send(Settings.ButtonCreateBackup())
-                store.dispatch(WalletAction.MultiWallet.BackupWallet)
-            }
-            SettingsElement.TermsOfService -> {
-                store.dispatch(DisclaimerAction.Show(AppScreen.Details))
-            }
-            SettingsElement.PrivacyPolicy -> {
-                // TODO: To be available later
-            }
-            SettingsElement.ReferralProgram -> {
-                store.dispatch(NavigationAction.NavigateTo(AppScreen.ReferralProgram))
-            }
-            SettingsElement.TesterMenu -> {
-                store.state.daggerGraphState.testerRouter?.startTesterScreen()
-            }
-        }
-    }
-
-    private fun getSocialLinks(): List<SocialNetworkLink> {
-        val locale = LocaleRegionProvider().getRegion()
-        return if (locale.lowercase() == RUSSIA_COUNTRY_CODE) {
-            TangemSocialAccounts.accountsRu
+    private fun triggerErrorSnackbarIfNeeded(text: TextReference?): StateEvent<TextReference> {
+        return if (text == null) {
+            consumedEvent()
         } else {
-            TangemSocialAccounts.accountsEn
+            triggeredEvent(text) {
+                store.dispatch(DetailsAction.DismissError)
+            }
         }
     }
 
@@ -125,6 +114,66 @@ internal class DetailsViewModel(private val store: Store<AppState>) {
         val versionCode: Int = BuildConfig.VERSION_CODE
         val versionName: String = BuildConfig.VERSION_NAME
         return "$versionName ($versionCode)"
+    }
+
+    private fun navigateToTesterMenu() {
+        store.state.daggerGraphState.testerRouter?.startTesterScreen()
+    }
+
+    private fun navigateToToS() {
+        store.dispatch(DisclaimerAction.Show(AppScreen.Details))
+    }
+
+    private fun navigateToReferralProgram() {
+        store.dispatch(NavigationAction.NavigateTo(AppScreen.ReferralProgram))
+    }
+
+    private fun sendFeedback() {
+        Analytics.send(Settings.ButtonSendFeedback())
+        store.dispatch(GlobalAction.SendEmail(FeedbackEmail()))
+    }
+
+    private fun navigateToChat() {
+        Analytics.send(Settings.ButtonChat())
+        store.dispatch(GlobalAction.OpenChat(SupportInfo()))
+    }
+
+    private fun navigateToAppSettings() {
+        Analytics.send(Settings.ButtonAppSettings())
+        store.dispatch(NavigationAction.NavigateTo(AppScreen.AppSettings))
+    }
+
+    private fun navigateToCardSettings() {
+        Analytics.send(Settings.ButtonCardSettings())
+        store.dispatch(NavigationAction.NavigateTo(AppScreen.CardSettings))
+    }
+
+    private fun linkMoreCards() {
+        Analytics.send(Settings.ButtonCreateBackup())
+        store.dispatch(WalletAction.MultiWallet.BackupWallet)
+    }
+
+    private fun scanAndSaveUserWallet() {
+        store.dispatch(DetailsAction.ScanAndSaveUserWallet)
+    }
+
+    private fun navigateToWalletConnect() {
+        Analytics.send(Settings.ButtonWalletConnect())
+        store.dispatch(NavigationAction.NavigateTo(AppScreen.WalletConnectSessions))
+    }
+
+    private fun handleSocialNetworkClick(link: SocialNetworkLink) {
+        Analytics.send(Settings.ButtonSocialNetwork(link.network))
+        store.dispatch(NavigationAction.OpenUrl(link.url))
+    }
+
+    private fun getSocialLinks(): ImmutableList<SocialNetworkLink> {
+        val locale = LocaleRegionProvider().getRegion()
+        return if (locale.lowercase() == RUSSIA_COUNTRY_CODE) {
+            TangemSocialAccounts.accountsRu
+        } else {
+            TangemSocialAccounts.accountsEn
+        }
     }
 
     private fun bootstrapScreenState() {
