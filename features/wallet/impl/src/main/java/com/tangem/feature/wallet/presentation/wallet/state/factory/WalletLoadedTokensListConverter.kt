d6c35b7213bcbb74bd69516e94fb2552d39d9aa6
Tangem <git@tangem.com>
2023-09-15 15:59:45 +0800

    release/5.26.1

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/factory/WalletLoadedTokensListConverter.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/factory/WalletLoadedTokensListConverter.kt
@@ -3,7 +3,6 @@ package com.tangem.feature.wallet.presentation.wallet.state.factory
 import arrow.core.Either
 import com.tangem.common.Provider
 import com.tangem.domain.appcurrency.model.AppCurrency
-import com.tangem.domain.common.CardTypesResolver
 import com.tangem.domain.tokens.error.TokenListError
 import com.tangem.domain.tokens.model.TokenList
 import com.tangem.domain.wallets.models.UserWallet
@@ -17,10 +16,11 @@ import com.tangem.utils.converter.Converter
 /**
  * Converter from loaded [TokenListError] or [TokenList] to [WalletMultiCurrencyState]
  *
- * @property currentStateProvider  current ui state provider
- * @param cardTypeResolverProvider card type resolver
- * @param currentWalletProvider    current wallet provider
- * @param clickIntents             screen click intents
+ * @property currentStateProvider    current ui state provider
+ * @property tokenListErrorConverter converter of tokens list
+ * @param appCurrencyProvider        app currency provider
+ * @param currentWalletProvider      current wallet provider
+ * @param clickIntents               screen click intents
  *
 * [REDACTED_AUTHOR]
  */
@@ -28,14 +28,12 @@ internal class WalletLoadedTokensListConverter(
     private val currentStateProvider: Provider<WalletState>,
     private val tokenListErrorConverter: TokenListErrorConverter,
     appCurrencyProvider: Provider<AppCurrency>,
-    cardTypeResolverProvider: Provider<CardTypesResolver>,
     currentWalletProvider: Provider<UserWallet>,
     clickIntents: WalletClickIntents,
 ) : Converter<Either<TokenListError, TokenList>, WalletState> {
 
     private val tokenListStateConverter = TokenListToWalletStateConverter(
         currentStateProvider = currentStateProvider,
-        cardTypeResolverProvider = cardTypeResolverProvider,
         currentWalletProvider = currentWalletProvider,
         appCurrencyProvider = appCurrencyProvider,
         isWalletContentHidden = false, // TODO: https://tangem.atlassian.net/browse/AND-4007
