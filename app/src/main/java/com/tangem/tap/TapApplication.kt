1e154b5833620ecf113f40a0345765265637859f
Tangem <git@tangem.com>
2023-09-15 13:50:13 +0300

    Updated on 2026-09-14

--- app/src/main/java/com/tangem/tap/TapApplication.kt
+++ app/src/main/java/com/tangem/tap/TapApplication.kt
@@ -62,6 +62,7 @@ import com.tangem.tap.domain.walletStores.repository.di.provideDefaultImplementa
 import com.tangem.tap.domain.walletconnect.WalletConnectRepository
 import com.tangem.tap.domain.walletconnect2.domain.WalletConnectSessionsRepository
 import com.tangem.tap.features.customtoken.api.featuretoggles.CustomTokenFeatureToggles
+import com.tangem.tap.features.details.featuretoggles.DetailsFeatureToggles
 import com.tangem.tap.proxy.AppStateHolder
 import com.tangem.tap.proxy.redux.DaggerGraphState
 import com.tangem.wallet.BuildConfig
@@ -176,6 +177,9 @@ class TapApplication : Application(), ImageLoaderFactory {
     @Inject
     lateinit var appThemeModeRepository: AppThemeModeRepository
 
+    @Inject
+    lateinit var detailsFeatureToggles: DetailsFeatureToggles
+
     override fun onCreate() {
         super.onCreate()
 
@@ -199,6 +203,7 @@ class TapApplication : Application(), ImageLoaderFactory {
                     appStateHolder = appStateHolder,
                     currenciesRepository = currenciesRepository,
                     appThemeModeRepository = appThemeModeRepository,
+                    detailsFeatureToggles = detailsFeatureToggles,
                 ),
             ),
         )
