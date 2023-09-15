9e09716a68e0ece259c02aec81fc77715f2486df
Tangem <git@tangem.com>
2023-09-15 13:50:13 +0300

    Updated on 2026-08-07

--- app/src/main/java/com/tangem/tap/features/details/redux/DetailsAction.kt
+++ app/src/main/java/com/tangem/tap/features/details/redux/DetailsAction.kt
@@ -84,6 +84,10 @@ sealed class DetailsAction : Action {
         data class ChangeAppThemeMode(
             val appThemeMode: AppThemeMode,
         ) : AppSettings()
+
+        data class ChangeAppCurrency(
+            val fiatCurrency: FiatCurrency,
+        ) : AppSettings()
     }
 
     data class ChangeAppCurrency(val fiatCurrency: FiatCurrency) : DetailsAction()
