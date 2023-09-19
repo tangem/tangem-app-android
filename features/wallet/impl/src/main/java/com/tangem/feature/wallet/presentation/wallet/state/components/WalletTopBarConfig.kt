38d29643dffbe88992373664a0510539bfc4d4c8
Tangem <git@tangem.com>
2023-09-19 17:03:45 +0800

    Updated on 2026-02-11

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/components/WalletTopBarConfig.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/components/WalletTopBarConfig.kt
@@ -3,7 +3,6 @@ package com.tangem.feature.wallet.presentation.wallet.state.components
 /**
  * Wallet screen top bar config
  *
- * @property onScanCardClick lambda be invoked when scan card button is clicked
- * @property onMoreClick     lambda be invoked when more button is clicked
+ * @property onDetailsClick lambda be invoked when details button is clicked
  */
-data class WalletTopBarConfig(val onScanCardClick: () -> Unit, val onMoreClick: () -> Unit)
+internal data class WalletTopBarConfig(val onDetailsClick: () -> Unit)
