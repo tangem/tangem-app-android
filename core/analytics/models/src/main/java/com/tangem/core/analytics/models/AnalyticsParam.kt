72c9f2d27af745525606874c63c7d1324c359a50
Tangem <git@tangem.com>
2023-09-20 11:45:42 +0300

    Updated on 2026-09-14

--- core/analytics/models/src/main/java/com/tangem/core/analytics/models/AnalyticsParam.kt
+++ core/analytics/models/src/main/java/com/tangem/core/analytics/models/AnalyticsParam.kt
@@ -1,22 +1,6 @@
-package com.tangem.tap.common.analytics.events
-
-import com.tangem.tap.features.details.redux.SecurityOption
+package com.tangem.core.analytics.models
 
 sealed class AnalyticsParam {
-
-    sealed class CurrencyType(val value: String) {
-        class Currency(currency: com.tangem.tap.features.wallet.models.Currency) : CurrencyType(currency.currencySymbol)
-        class Blockchain(blockchain: com.tangem.blockchain.common.Blockchain) : CurrencyType(blockchain.currency)
-        class Token(token: com.tangem.blockchain.common.Token) : CurrencyType(token.symbol)
-        class FiatCurrency(fiatCurrency: com.tangem.tap.common.entities.FiatCurrency) : CurrencyType(fiatCurrency.code)
-        class Amount(amount: com.tangem.blockchain.common.Amount) : CurrencyType(amount.currencySymbol)
-    }
-
-    // MultiCurrency or CurrencyType
-    sealed class CardCurrency(val value: String) {
-        object MultiCurrency : CardCurrency("Multicurrency")
-        class SingleCurrency(type: CurrencyType) : CardCurrency(type.value)
-    }
 
     sealed class CardBalanceState(val value: String) {
         object Empty : CardBalanceState("Empty")
@@ -37,23 +21,14 @@ sealed class AnalyticsParam {
         object Off : OnOffState("Off")
     }
 
+    sealed class OrganizeSortType(val value: String) {
+        object ByBalance : OrganizeSortType("By Balance")
+        object Manually : OrganizeSortType("Manually")
+    }
+
     sealed class UserCode(val value: String) {
         object AccessCode : UserCode("Access Code")
         object Passcode : UserCode("Passcode")
-    }
-
-    sealed class SecurityMode(val value: String) {
-        object AccessCode : SecurityMode("Access Code")
-        object Passcode : SecurityMode("Passcode")
-        object LongTap : SecurityMode("Long Tap")
-
-        companion object {
-            fun from(option: SecurityOption): SecurityMode = when (option) {
-                SecurityOption.AccessCode -> AccessCode
-                SecurityOption.PassCode -> Passcode
-                SecurityOption.LongTap -> LongTap
-            }
-        }
     }
 
     sealed class AccessCodeRecoveryStatus(val value: String) {
