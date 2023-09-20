84ab178edb791698046a818c1f3ab3b671589baa
Tangem <git@tangem.com>
2023-09-20 21:51:44 +0800

    Updated on 2026-07-23

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/components/WalletBottomSheetConfig.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/components/WalletBottomSheetConfig.kt
@@ -4,7 +4,8 @@ import androidx.annotation.DrawableRes
 import androidx.compose.ui.graphics.Color
 import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfigContent
 import com.tangem.core.ui.extensions.TextReference
-import com.tangem.core.ui.extensions.WrappedList
+import com.tangem.core.ui.extensions.resourceReference
+import com.tangem.core.ui.extensions.wrappedList
 import com.tangem.core.ui.res.TangemColorPalette
 import com.tangem.feature.wallet.impl.R
 
@@ -13,7 +14,6 @@ import com.tangem.feature.wallet.impl.R
  *
 [REDACTED_AUTHOR]
  */
-// TODO: Finalize notification strings [REDACTED_JIRA]
 sealed class WalletBottomSheetConfig(
     open val title: TextReference,
     open val subtitle: TextReference,
@@ -29,61 +29,24 @@ sealed class WalletBottomSheetConfig(
         @DrawableRes val iconResId: Int? = null,
     )
 
-    data class UnlockWallets(
-        val onUnlockClick: () -> Unit,
-        val onScanClick: () -> Unit,
-    ) : WalletBottomSheetConfig(
-        title = TextReference.Str(value = "Unlock needed"),
-        subtitle = TextReference.Str(
-            value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor " +
-                "incididunt ut labore et dolore magna aliqua.",
+    data class UnlockWallets(val onUnlockClick: () -> Unit, val onScanClick: () -> Unit) : WalletBottomSheetConfig(
+        title = resourceReference(id = R.string.common_unlock_needed),
+        subtitle = resourceReference(
+            id = R.string.unlock_wallet_description_full,
+            formatArgs = wrappedList(
+                resourceReference(R.string.common_biometrics),
+            ),
         ),
         iconResId = R.drawable.ic_locked_24,
         tint = TangemColorPalette.Black,
-        primaryButtonConfig = ButtonConfig(text = TextReference.Str(value = "Unlock"), onClick = onUnlockClick),
+        primaryButtonConfig = ButtonConfig(
+            text = resourceReference(id = R.string.user_wallet_list_unlock_all),
+            onClick = onUnlockClick,
+        ),
         secondaryButtonConfig = ButtonConfig(
-            text = TextReference.Str(value = "Scan card"),
+            text = resourceReference(id = R.string.welcome_unlock_card),
             onClick = onScanClick,
             iconResId = R.drawable.ic_tangem_24,
-        ),
-    )
-
-    data class LikeTangemApp(
-        val onRateTheAppClick: () -> Unit,
-        val onShareClick: () -> Unit,
-    ) : WalletBottomSheetConfig(
-        title = TextReference.Str(value = "Like Tangem App?"),
-        subtitle = TextReference.Str(value = "How was your experience with our app? Let us know:"),
-        iconResId = R.drawable.ic_star_24,
-        tint = TangemColorPalette.Tangerine,
-        primaryButtonConfig = ButtonConfig(
-            text = TextReference.Str(value = "Rate the app"),
-            onClick = onRateTheAppClick,
-        ),
-        secondaryButtonConfig = ButtonConfig(
-            text = TextReference.Str(value = "Share feedback"),
-            onClick = onShareClick,
-        ),
-    )
-
-    data class CriticalWarningAlreadySignedHashes(
-        val onOkClick: () -> Unit,
-        val onCancelClick: () -> Unit,
-    ) : WalletBottomSheetConfig(
-        title = TextReference.Res(
-            id = R.string.warning_important_security_info,
-            formatArgs = WrappedList(listOf("\u26A0")),
-        ),
-        subtitle = TextReference.Res(id = R.string.alert_signed_hashes_message),
-        iconResId = R.drawable.img_attention_20,
-        tint = null,
-        primaryButtonConfig = ButtonConfig(
-            text = TextReference.Res(id = R.string.common_ok),
-            onClick = onOkClick,
-        ),
-        secondaryButtonConfig = ButtonConfig(
-            text = TextReference.Res(id = R.string.common_cancel),
-            onClick = onCancelClick,
         ),
     )
 }
