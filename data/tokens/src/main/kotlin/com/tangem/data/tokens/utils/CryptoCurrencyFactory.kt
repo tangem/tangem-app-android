06d408bcf74bb4371838430ec35ec07b64a2eee3
Tangem <git@tangem.com>
2023-09-21 20:15:54 +0300

    Updated on 2026-09-25

--- data/tokens/src/main/kotlin/com/tangem/data/tokens/utils/CryptoCurrencyFactory.kt
+++ data/tokens/src/main/kotlin/com/tangem/data/tokens/utils/CryptoCurrencyFactory.kt
@@ -3,7 +3,7 @@ package com.tangem.data.tokens.utils
 import com.tangem.blockchain.common.Blockchain
 import com.tangem.domain.common.DerivationStyleProvider
 import com.tangem.domain.common.extensions.toCoinId
-import com.tangem.domain.tokens.models.CryptoCurrency
+import com.tangem.domain.tokens.model.CryptoCurrency
 import timber.log.Timber
 import com.tangem.blockchain.common.Token as SdkToken
 
