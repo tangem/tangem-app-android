c6f49df5996dd4f77f02b9469da8d93d7984ebdd
Anton Tkachev <iiiburnyiii@gmail.com>
2023-09-21 09:45:07 +0300

    AND-4634 Implemented the local store for networks statuses

--- data/tokens/src/main/kotlin/com/tangem/data/tokens/repository/DefaultNetworksRepository.kt
+++ data/tokens/src/main/kotlin/com/tangem/data/tokens/repository/DefaultNetworksRepository.kt
@@ -4,6 +4,7 @@ import com.tangem.data.common.cache.CacheRegistry
 import com.tangem.data.tokens.utils.CardCryptoCurrenciesFactory
 import com.tangem.data.tokens.utils.NetworkStatusFactory
 import com.tangem.data.tokens.utils.ResponseCryptoCurrenciesFactory
+import com.tangem.datasource.local.network.NetworksStatusesStore
 import com.tangem.datasource.local.token.UserTokensStore
 import com.tangem.datasource.local.userwallet.UserWalletsStore
 import com.tangem.domain.demo.DemoConfig
@@ -15,12 +16,15 @@ import com.tangem.domain.walletmanager.WalletManagersFacade
 import com.tangem.domain.walletmanager.model.UpdateWalletManagerResult
 import com.tangem.domain.wallets.models.UserWalletId
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
-import com.tangem.utils.extensions.addOrReplace
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.cancellable
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.collectLatest
 import timber.log.Timber
 
 internal class DefaultNetworksRepository(
+    private val networksStatusesStore: NetworksStatusesStore,
     private val walletManagersFacade: WalletManagersFacade,
     private val userWalletsStore: UserWalletsStore,
     private val userTokensStore: UserTokensStore,
@@ -33,14 +37,13 @@ internal class DefaultNetworksRepository(
     private val responseCurrenciesFactory by lazy { ResponseCryptoCurrenciesFactory(demoConfig) }
     private val networkStatusFactory by lazy { NetworkStatusFactory() }
 
-    private val networksStatuses: MutableStateFlow<Set<NetworkStatus>> = MutableStateFlow(hashSetOf())
-
     override fun getNetworkStatusesUpdates(
         userWalletId: UserWalletId,
         networks: Set<Network>,
     ): Flow<Set<NetworkStatus>> = channelFlow {
         launch(dispatchers.io) {
-            networksStatuses.collect(::send)
+            networksStatusesStore.get(userWalletId)
+                .collectLatest(::send)
         }
 
         withContext(dispatchers.io) {
@@ -54,7 +57,7 @@ internal class DefaultNetworksRepository(
         refresh: Boolean,
     ): Set<NetworkStatus> = withContext(dispatchers.io) {
         fetchNetworksStatusesIfCacheExpired(userWalletId, networks, refresh)
-        networksStatuses.first().toSet()
+        networksStatusesStore.getSyncOrNull(userWalletId).orEmpty()
     }
 
     private suspend fun fetchNetworksStatusesIfCacheExpired(
@@ -102,9 +105,7 @@ internal class DefaultNetworksRepository(
             currencies = currencies.toSet(),
         )
 
-        networksStatuses.update { statuses ->
-            statuses.addOrReplace(networkStatus) { it.network == networkStatus.network }
-        }
+        networksStatusesStore.store(userWalletId, networkStatus)
     }
 
     private suspend fun getCurrencies(userWalletId: UserWalletId, network: Network): Sequence<CryptoCurrency> {
