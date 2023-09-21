7d1c625f7b5b7985f1eb0043e2c1ae0b24519fa5
Tangem <git@tangem.com>
2023-09-21 20:15:54 +0300

    Updated on 2026-01-20

---
+++ core/datasource/src/main/java/com/tangem/datasource/local/network/NetworksStatusesStore.kt
@@ -0,0 +1,14 @@
+package com.tangem.datasource.local.network
+
+import com.tangem.domain.tokens.model.NetworkStatus
+import com.tangem.domain.wallets.models.UserWalletId
+import kotlinx.coroutines.flow.Flow
+
+interface NetworksStatusesStore {
+
+    fun get(key: UserWalletId): Flow<Set<NetworkStatus>>
+
+    suspend fun getSyncOrNull(key: UserWalletId): Set<NetworkStatus>?
+
+    suspend fun store(key: UserWalletId, value: NetworkStatus)
+}
