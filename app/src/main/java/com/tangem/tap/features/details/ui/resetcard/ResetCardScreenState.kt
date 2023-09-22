18b65a69eac6837c36d92ac1efebd3f727ed4f60
Tangem <git@tangem.com>
2023-09-22 11:36:12 +0300

    Updated on 2025-09-01

--- app/src/main/java/com/tangem/tap/features/details/ui/resetcard/ResetCardScreenState.kt
+++ app/src/main/java/com/tangem/tap/features/details/ui/resetcard/ResetCardScreenState.kt
@@ -5,7 +5,10 @@ import com.tangem.tap.features.details.ui.cardsettings.TextReference
 internal data class ResetCardScreenState(
     val accepted: Boolean = false,
     val descriptionText: TextReference,
-    val onAcceptWarningToggleClick: (Boolean) -> Unit,
+    val acceptCondition1Checked: Boolean = false,
+    val acceptCondition2Checked: Boolean = false,
+    val onAcceptCondition1ToggleClick: (Boolean) -> Unit,
+    val onAcceptCondition2ToggleClick: (Boolean) -> Unit,
     val onResetButtonClick: () -> Unit,
 ) {
     val resetButtonEnabled: Boolean
