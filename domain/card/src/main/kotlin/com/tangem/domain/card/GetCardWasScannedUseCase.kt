8cf3d393a4731587cbdc66ae9c20582c40b2b0a7
Tangem <git@tangem.com>
2023-09-22 16:53:59 +0800

    Updated on 2026-04-06

--- domain/card/src/main/kotlin/com/tangem/domain/card/GetCardWasScannedUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.tangem.domain.card
-
-import com.tangem.domain.card.repository.CardRepository
-
-class GetCardWasScannedUseCase(private val cardRepository: CardRepository) {
-
-    suspend operator fun invoke(cardId: String): Boolean = cardRepository.wasCardScanned(cardId)
-}
