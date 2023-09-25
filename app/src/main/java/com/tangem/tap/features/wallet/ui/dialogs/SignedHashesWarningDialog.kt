e1cee0b66003db9fee053e790536921856a52d42
Tangem <git@tangem.com>
2023-09-26 00:36:00 +0400

    Updated on 2026-09-21

--- app/src/main/java/com/tangem/tap/features/wallet/ui/dialogs/SignedHashesWarningDialog.kt
+++ app/src/main/java/com/tangem/tap/features/wallet/ui/dialogs/SignedHashesWarningDialog.kt
@@ -2,6 +2,7 @@ package com.tangem.tap.features.wallet.ui.dialogs
 
 import android.content.Context
 import androidx.appcompat.app.AlertDialog
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.tangem.tap.common.redux.global.GlobalAction
 import com.tangem.tap.domain.configurable.warningMessage.WarningMessagesManager
 import com.tangem.tap.features.wallet.redux.WalletAction
@@ -10,7 +11,7 @@ import com.tangem.wallet.R
 
 object SignedHashesWarningDialog {
     fun create(context: Context): AlertDialog {
-        return AlertDialog.Builder(context).apply {
+        return MaterialAlertDialogBuilder(context, R.style.CustomMaterialDialog).apply {
             setTitle(context.getString(R.string.warning_important_security_info, "\u26A0"))
             setMessage(R.string.alert_signed_hashes_message)
             setPositiveButton(R.string.common_understand) { _, _ ->
