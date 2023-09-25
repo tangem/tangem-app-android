0990c4136aa99461577ad770e3142c78612f43d5
Tangem <git@tangem.com>
2023-09-25 09:55:08 +0800

    Updated on 2025-11-03

--- data/settings/src/main/java/com/tangem/data/settings/di/SettingsDataModule.kt
+++ data/settings/src/main/java/com/tangem/data/settings/di/SettingsDataModule.kt
@@ -1,7 +1,13 @@
 package com.tangem.data.settings.di
 
+import com.tangem.data.settings.DefaultAppRatingRepository
 import com.tangem.data.settings.DefaultSettingsRepository
 import com.tangem.data.source.preferences.PreferencesDataSource
+import com.tangem.datasource.local.settings.AppLaunchCountStore
+import com.tangem.datasource.local.settings.AppRatingShowingCountStore
+import com.tangem.datasource.local.settings.FundsFoundDateInMillisStore
+import com.tangem.datasource.local.settings.UserInteractingStatusStore
+import com.tangem.domain.settings.repositories.AppRatingRepository
 import com.tangem.domain.settings.repositories.SettingsRepository
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import dagger.Module
@@ -22,6 +28,24 @@ internal object SettingsDataModule {
     ): SettingsRepository {
         return DefaultSettingsRepository(
             preferencesDataSource = preferencesDataSource,
+            dispatchers = dispatchers,
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideAppRatingRepository(
+        fundsFoundDateInMillisStore: FundsFoundDateInMillisStore,
+        appLaunchCountStore: AppLaunchCountStore,
+        appRatingShowingCountStore: AppRatingShowingCountStore,
+        userInteractingStatusStore: UserInteractingStatusStore,
+        dispatchers: CoroutineDispatcherProvider,
+    ): AppRatingRepository {
+        return DefaultAppRatingRepository(
+            fundsFoundDateInMillisStore = fundsFoundDateInMillisStore,
+            appLaunchCountStore = appLaunchCountStore,
+            appRatingShowingCountStore = appRatingShowingCountStore,
+            userInteractingStatusStore = userInteractingStatusStore,
             dispatchers = dispatchers,
         )
     }
