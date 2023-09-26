396005938b3f2617bab75645da776e55af335b37
Tangem <git@tangem.com>
2023-09-26 16:57:53 +0500

    Updated on 2026-01-20

--- features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/factory/txhistory/TokenDetailsTxHistoryItemFlowConverter.kt
+++ features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/factory/txhistory/TokenDetailsTxHistoryItemFlowConverter.kt
@@ -6,6 +6,7 @@ import com.tangem.common.Provider
 import com.tangem.core.ui.components.transactions.state.TransactionState
 import com.tangem.core.ui.components.transactions.state.TxHistoryState
 import com.tangem.core.ui.components.transactions.state.TxHistoryState.TxHistoryItemState
+import com.tangem.core.ui.utils.DateTimeFormatters
 import com.tangem.domain.txhistory.models.TxHistoryItem
 import com.tangem.feature.tokendetails.presentation.tokendetails.state.TokenDetailsState
 import com.tangem.feature.tokendetails.presentation.tokendetails.viewmodels.TokenDetailsClickIntents
@@ -20,8 +21,6 @@ import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 import org.joda.time.DateTime
 import org.joda.time.DateTimeZone
-import org.joda.time.format.DateTimeFormatterBuilder
-import java.util.Locale
 
 internal class TokenDetailsTxHistoryItemFlowConverter(
     private val currentStateProvider: Provider<TokenDetailsState>,
@@ -32,28 +31,6 @@ internal class TokenDetailsTxHistoryItemFlowConverter(
 
     private val txHistoryItemConverter by lazy {
         TokenDetailsTxHistoryTransactionStateConverter(symbol = symbol, decimals = decimals)
-    }
-
-    /** Example, 2 Aug, 2023 */
-    private val dateFormatter by lazy {
-        DateTimeFormatterBuilder()
-            .appendDayOfMonth(1)
-            .appendLiteral(' ')
-            .appendMonthOfYearShortText()
-            .appendLiteral(", ")
-            .appendYear(4, 4)
-            .toFormatter()
-            .withLocale(Locale.getDefault())
-    }
-
-    /** Example, 13:35 */
-    private val timeFormatter by lazy {
-        DateTimeFormatterBuilder()
-            .appendHourOfDay(1)
-            .appendLiteral(':')
-            .appendMinuteOfHour(2)
-            .toFormatter()
-            .withLocale(Locale.getDefault())
     }
 
     override fun convert(value: Flow<PagingData<TxHistoryItem>>): TxHistoryState {
@@ -140,7 +117,7 @@ internal class TokenDetailsTxHistoryItemFlowConverter(
 
     /**
      * If [this] timestamp is today or yesterday, returns relative date,
-     * otherwise returns formatting date by [dateFormatter]
+     * otherwise returns formatting date.
      */
     private fun Long.toDateFormat(): String {
         val localDate = DateTime(this, DateTimeZone.getDefault())
@@ -152,13 +129,11 @@ internal class TokenDetailsTxHistoryItemFlowConverter(
                 DateUtils.FORMAT_ABBREV_RELATIVE,
             ).toString()
         } else {
-            dateFormatter.print(localDate)
+            DateTimeFormatters.formatDate(date = localDate)
         }
     }
 
     private fun String.toTimeFormat(): String {
-        return timeFormatter.print(
-            DateTime(this.toLong(), DateTimeZone.getDefault()),
-        )
+        return DateTimeFormatters.formatTime(time = DateTime(this.toLong(), DateTimeZone.getDefault()))
     }
 }
