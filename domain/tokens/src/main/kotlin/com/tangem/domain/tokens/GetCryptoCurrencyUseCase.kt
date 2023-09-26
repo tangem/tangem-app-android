2720c399b616904e0dee86f628942d6d2b027517
Tangem <git@tangem.com>
2023-09-26 18:14:45 +0500

    Updated on 2025-09-17

---
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/GetCryptoCurrencyUseCase.kt
@@ -0,0 +1,56 @@
+package com.tangem.domain.tokens
+
+import arrow.core.Either
+import arrow.core.raise.Raise
+import arrow.core.raise.catch
+import arrow.core.raise.either
+import com.tangem.domain.tokens.error.CurrencyStatusError
+import com.tangem.domain.tokens.model.CryptoCurrency
+import com.tangem.domain.tokens.repository.CurrenciesRepository
+import com.tangem.domain.wallets.models.UserWalletId
+
+class GetCryptoCurrencyUseCase(
+    private val currenciesRepository: CurrenciesRepository,
+) {
+
+    /**
+     * Returns specific cryptocurrency for a given user wallet.
+     *
+     * @param userWalletId The ID of the user's wallet.
+     * @param id The ID of the cryptocurrency.
+     * @return An [Either] representing success (Right) or an error (Left) in fetching the status.
+     */
+    suspend operator fun invoke(
+        userWalletId: UserWalletId,
+        id: CryptoCurrency.ID,
+    ): Either<CurrencyStatusError, CryptoCurrency> {
+        return either { getCurrency(userWalletId, id) }
+    }
+
+    /**
+     * Returns the primary cryptocurrency for a given user wallet.
+     *
+     * @param userWalletId The ID of the user's wallet.
+     * @return An [Either] representing success (Right) or an error (Left) in fetching the status.
+     */
+    suspend operator fun invoke(userWalletId: UserWalletId): Either<CurrencyStatusError, CryptoCurrency> {
+        return either { getPrimaryCurrency(userWalletId) }
+    }
+
+    private suspend fun Raise<CurrencyStatusError>.getCurrency(
+        userWalletId: UserWalletId,
+        id: CryptoCurrency.ID,
+    ): CryptoCurrency {
+        return catch(
+            block = { currenciesRepository.getMultiCurrencyWalletCurrency(userWalletId, id) },
+            catch = { raise(CurrencyStatusError.DataError(it)) },
+        )
+    }
+
+    private suspend fun Raise<CurrencyStatusError>.getPrimaryCurrency(userWalletId: UserWalletId): CryptoCurrency {
+        return catch(
+            block = { currenciesRepository.getSingleCurrencyWalletPrimaryCurrency(userWalletId) },
+            catch = { raise(CurrencyStatusError.DataError(it)) },
+        )
+    }
+}
