d1d35e45466951b36e508652b1f0ff5f37720bc4
Tangem <git@tangem.com>
2023-09-26 15:27:06 +0300

    Updated on 2026-07-01

--- app/src/main/java/com/tangem/tap/proxy/di/ProxyModule.kt
+++ app/src/main/java/com/tangem/tap/proxy/di/ProxyModule.kt
@@ -6,6 +6,7 @@ import com.tangem.core.featuretoggle.manager.FeatureTogglesManager
 import com.tangem.domain.card.repository.CardSdkConfigRepository
 import com.tangem.domain.common.CardTypesResolver
 import com.tangem.domain.common.util.cardTypesResolver
+import com.tangem.domain.tokens.repository.CurrenciesRepository
 import com.tangem.domain.walletmanager.WalletManagersFacade
 import com.tangem.feature.learn2earn.domain.api.Learn2earnDependencyProvider
 import com.tangem.features.wallet.featuretoggles.WalletFeatureToggles
@@ -66,9 +67,13 @@ class ProxyModule {
 
     @Provides
     @Singleton
-    fun provideDerivationManager(appStateHolder: AppStateHolder): DerivationManager {
+    fun provideDerivationManager(
+        appStateHolder: AppStateHolder,
+        currenciesRepository: CurrenciesRepository,
+    ): DerivationManager {
         return DerivationManagerImpl(
             appStateHolder = appStateHolder,
+            currenciesRepository = currenciesRepository,
         )
     }
 
