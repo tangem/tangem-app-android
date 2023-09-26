223a558f90f23cb9565e4f54643d03bc8fd2b843
Tangem <git@tangem.com>
2023-09-26 11:30:20 +0300

    Updated on 2026-08-14

--- app/src/main/java/com/tangem/tap/features/customtoken/impl/presentation/AddCustomTokenFragment.kt
+++ app/src/main/java/com/tangem/tap/features/customtoken/impl/presentation/AddCustomTokenFragment.kt
@@ -5,14 +5,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.transition.TransitionInflater
 import com.tangem.core.ui.components.SystemBarsEffect
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.core.ui.screen.ComposeFragment
 import com.tangem.core.ui.theme.AppThemeModeHolder
 import com.tangem.tap.features.customtoken.impl.presentation.ui.AddCustomTokenScreen
 import com.tangem.tap.features.customtoken.impl.presentation.viewmodels.AddCustomTokenViewModel
-import com.tangem.wallet.R
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -26,12 +24,6 @@ internal class AddCustomTokenFragment : ComposeFragment() {
 
     @Inject
     override lateinit var appThemeModeHolder: AppThemeModeHolder
-
-    override fun TransitionInflater.inflateTransitions(): Boolean {
-        enterTransition = inflateTransition(R.transition.fade)
-        exitTransition = inflateTransition(R.transition.fade)
-        return true
-    }
 
     @Composable
     override fun ScreenContent(modifier: Modifier) {
