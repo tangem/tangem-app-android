f84ce56a610d10ed7f510a27e5c9f8e72e9526ab
Tangem <git@tangem.com>
2023-09-26 13:20:05 +0300

    Updated on 2025-10-01

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/WalletAlertState.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/WalletAlertState.kt
@@ -1,9 +1,16 @@
 package com.tangem.feature.wallet.presentation.wallet.state
 
 import androidx.compose.runtime.Immutable
+import com.tangem.core.ui.extensions.TextReference
 
 @Immutable
 internal sealed class WalletAlertState {
 
     data class WalletAlreadySignedHashes(val onUnderstandClick: () -> Unit) : WalletAlertState()
+
+    data class DefaultAlert(
+        val title: TextReference,
+        val message: TextReference,
+        val onActionClick: (() -> Unit)?,
+    ) : WalletAlertState()
 }
