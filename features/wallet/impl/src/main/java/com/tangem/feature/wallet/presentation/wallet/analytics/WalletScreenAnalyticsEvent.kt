7ef669eaa82b7183f3bb600b5ecebd23787d30e4
Tangem <git@tangem.com>
2023-09-26 13:03:07 +0300

    Updated on 2026-07-23

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/analytics/WalletScreenAnalyticsEvent.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/analytics/WalletScreenAnalyticsEvent.kt
@@ -16,7 +16,6 @@ sealed class WalletScreenAnalyticsEvent(
         params = mapOf("State" to state.value),
     )
 
-    // TODO [REDACTED_JIRA]
     class NoticeRateAppButton(result: AnalyticsParam.RateApp) : WalletScreenAnalyticsEvent(
         event = "Notice - Rate The App Button Tapped",
         params = mapOf("Result" to result.value),
