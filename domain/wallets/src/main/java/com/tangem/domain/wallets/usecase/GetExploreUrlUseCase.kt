3601f97b1c5483cace51f147da65a970bf511738
Tangem <git@tangem.com>
2023-09-26 12:23:07 +0300

    release/5.26.1

--- domain/wallets/src/main/java/com/tangem/domain/wallets/usecase/GetExploreUrlUseCase.kt
+++ domain/wallets/src/main/java/com/tangem/domain/wallets/usecase/GetExploreUrlUseCase.kt
@@ -2,13 +2,18 @@ package com.tangem.domain.wallets.usecase
 
 import arrow.core.raise.catch
 import com.tangem.domain.tokens.model.Network
+import com.tangem.blockchain.common.address.AddressType
 import com.tangem.domain.walletmanager.WalletManagersFacade
 import com.tangem.domain.wallets.models.UserWalletId
 // [REDACTED_TODO_COMMENT]
 class GetExploreUrlUseCase(private val walletsManagersFacade: WalletManagersFacade) {
 // [REDACTED_TODO_COMMENT]
-    suspend operator fun invoke(userWalletId: UserWalletId, network: Network): String {
-        return catch({ walletsManagersFacade.getExploreUrl(userWalletId, network) }) {
+    suspend operator fun invoke(
+        userWalletId: UserWalletId,
+        network: Network,
+        addressType: AddressType = AddressType.Default,
+    ): String {
+        return catch({ walletsManagersFacade.getExploreUrl(userWalletId, network, addressType) }) {
             ""
         }
     }
