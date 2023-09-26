d55502d43e7d781f17b7d1717c2aed83f607633c
Tangem <git@tangem.com>
2023-09-26 13:58:15 +0300

    Updated on 2026-08-27

--- app/src/main/java/com/tangem/tap/features/details/ui/cardsettings/CardSettingsFragment.kt
+++ app/src/main/java/com/tangem/tap/features/details/ui/cardsettings/CardSettingsFragment.kt
@@ -1,58 +1,36 @@
 package com.tangem.tap.features.details.ui.cardsettings
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.fragment.app.viewModels
 import com.tangem.core.navigation.NavigationAction
 import com.tangem.core.ui.screen.ComposeFragment
 import com.tangem.core.ui.theme.AppThemeModeHolder
 import com.tangem.tap.features.details.redux.DetailsAction
-import com.tangem.tap.features.details.redux.DetailsState
 import com.tangem.tap.store
 import dagger.hilt.android.AndroidEntryPoint
-import org.rekotlin.StoreSubscriber
 import javax.inject.Inject
 
 @AndroidEntryPoint
-internal class CardSettingsFragment : ComposeFragment(), StoreSubscriber<DetailsState> {
+internal class CardSettingsFragment : ComposeFragment() {
 
     @Inject
     override lateinit var appThemeModeHolder: AppThemeModeHolder
 
-    private val viewModel = CardSettingsViewModel(store)
-
-    private var screenState: MutableState<CardSettingsScreenState> =
-        mutableStateOf(viewModel.updateState(store.state.detailsState.cardSettingsState))
+    private val viewModel: CardSettingsViewModel by viewModels()
 
     @Composable
     override fun ScreenContent(modifier: Modifier) {
+        LocalLifecycleOwner.current.lifecycle.addObserver(viewModel)
+
         CardSettingsScreen(
             modifier = modifier,
-            state = screenState.value,
+            state = viewModel.screenState.value,
             onBackClick = {
                 store.dispatch(DetailsAction.ResetCardSettingsData)
                 store.dispatch(NavigationAction.PopBackTo())
             },
         )
-    }
-
-    override fun onStart() {
-        super.onStart()
-        store.subscribe(this) { state ->
-            state.skipRepeats { oldState, newState ->
-                oldState.detailsState == newState.detailsState
-            }.select { it.detailsState }
-        }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        store.unsubscribe(this)
-    }
-
-    override fun newState(state: DetailsState) {
-        if (activity == null || view == null) return
-        screenState.value = viewModel.updateState(state.cardSettingsState)
     }
 }
