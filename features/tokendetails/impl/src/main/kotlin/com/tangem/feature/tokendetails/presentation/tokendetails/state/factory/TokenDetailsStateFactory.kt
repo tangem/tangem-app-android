7a211451a0bdf5619132b68f70ff341a2c323dc2
Tangem <git@tangem.com>
2023-09-26 12:23:07 +0300

    Updated on 2025-10-01

--- features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/factory/TokenDetailsStateFactory.kt
+++ features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/factory/TokenDetailsStateFactory.kt
@@ -5,6 +5,7 @@ import arrow.core.Either
 import com.tangem.blockchain.common.address.Address
 import com.tangem.common.Provider
 import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfig
+import com.tangem.core.ui.components.bottomsheets.chooseaddress.ChooseAddressBottomSheetConfig
 import com.tangem.core.ui.components.bottomsheets.tokenreceive.AddressModel
 import com.tangem.core.ui.components.bottomsheets.tokenreceive.TokenReceiveBottomSheetConfig
 import com.tangem.domain.appcurrency.model.AppCurrency
@@ -159,6 +160,27 @@ internal class TokenDetailsStateFactory(
                             type = AddressModel.Type.valueOf(it.type.name),
                         )
                     },
+                ),
+            ),
+        )
+    }
+
+    fun getStateWithChooseAddressBottomSheet(
+        addresses: List<Address>,
+        onAddressTypeClick: (AddressModel) -> Unit,
+    ): TokenDetailsState {
+        return currentStateProvider().copy(
+            bottomSheetConfig = TangemBottomSheetConfig(
+                isShow = true,
+                onDismissRequest = clickIntents::onDismissBottomSheet,
+                content = ChooseAddressBottomSheetConfig(
+                    addressModels = addresses.map {
+                        AddressModel(
+                            value = it.value,
+                            type = AddressModel.Type.valueOf(it.type.name),
+                        )
+                    },
+                    onClick = onAddressTypeClick,
                 ),
             ),
         )
