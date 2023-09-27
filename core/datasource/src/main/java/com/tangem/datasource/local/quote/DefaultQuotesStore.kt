e9caa34185438c161a44a301c8a459b2d9b903f7
Tangem <git@tangem.com>
2023-09-27 17:08:13 +0300

    Updated on 2026-08-27

--- core/datasource/src/main/java/com/tangem/datasource/local/quote/DefaultQuotesStore.kt
+++ core/datasource/src/main/java/com/tangem/datasource/local/quote/DefaultQuotesStore.kt
@@ -5,6 +5,7 @@ import com.tangem.datasource.local.datastore.core.StringKeyDataStore
 import com.tangem.datasource.local.quote.model.StoredQuote
 import com.tangem.domain.tokens.model.CryptoCurrency
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.combine
 
 internal class DefaultQuotesStore(
@@ -12,16 +13,24 @@ internal class DefaultQuotesStore(
 ) : QuotesStore {
 
     override fun get(currenciesIds: Set<CryptoCurrency.ID>): Flow<Set<StoredQuote>> {
-        val flows = currenciesIds.mapNotNull { currencyId ->
-            dataStore.get(currencyId.rawCurrencyId ?: return@mapNotNull null)
-        }
+        return channelFlow {
+            val flows = currenciesIds.mapNotNull { currencyId ->
+                currencyId.rawCurrencyId?.let(dataStore::get)
+            }
 
-        return combine(flows) { quotes -> quotes.toSet() }
+            if (dataStore.isEmpty() || flows.isEmpty()) {
+                send(emptySet())
+            }
+
+            combine(flows) { quotes -> quotes.toSet() }.collect(::send)
+        }
     }
 
     override suspend fun store(response: QuotesResponse) {
-        response.quotes.forEach { (rawCurrencyId, quote) ->
-            dataStore.store(rawCurrencyId, StoredQuote(rawCurrencyId, quote))
+        val quotes = response.quotes.mapValues { (id, quote) ->
+            StoredQuote(id, quote)
         }
+
+        dataStore.store(quotes)
     }
 }
