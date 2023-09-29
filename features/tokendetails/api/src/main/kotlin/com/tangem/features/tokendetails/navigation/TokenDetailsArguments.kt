ee1cdf05c2ea352222bb5f7dfb4ed2776f27887e
Tangem <git@tangem.com>
2023-09-29 12:40:50 +0500

    Updated on 2026-09-01

--- features/tokendetails/api/src/main/kotlin/com/tangem/features/tokendetails/navigation/TokenDetailsArguments.kt
+++ features/tokendetails/api/src/main/kotlin/com/tangem/features/tokendetails/navigation/TokenDetailsArguments.kt
@@ -19,10 +19,12 @@ data class TokenDetailsArguments(
         object Native : CoinType()
 
         /**
+         * @param isCustom - Indicates whether the currency is a custom user-added currency or not.
          * @param standardName - token standard. Samples: ERC20, BEP20, BEP2, TRC20 and etc.
          * @param networkName - token's blockchain name. Ethereum, Tron and etc.
          */
         data class Token(
+            val isCustom: Boolean,
             val standardName: String,
             val networkName: String,
             @DrawableRes val networkIcon: Int,
