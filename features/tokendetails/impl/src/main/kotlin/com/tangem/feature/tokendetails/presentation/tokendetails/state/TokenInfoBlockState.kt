05af718eeb08b87e3ef51efff7f2eff21313b449
Tangem <git@tangem.com>
2023-10-02 13:21:24 +0500

    Updated on 2026-09-01

--- features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/TokenInfoBlockState.kt
+++ features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/TokenInfoBlockState.kt
@@ -1,12 +1,15 @@
 package com.tangem.feature.tokendetails.presentation.tokendetails.state
 
 import androidx.annotation.DrawableRes
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.graphics.Color
 
 internal data class TokenInfoBlockState(
     val name: String,
-    val iconUrl: String?,
+    val iconState: IconState,
     val currency: Currency,
 ) {
+    @Immutable
     sealed class Currency {
         object Native : Currency()
 
@@ -20,5 +23,30 @@ internal data class TokenInfoBlockState(
             val networkName: String,
             @DrawableRes val networkIcon: Int,
         ) : Currency()
+    }
+
+    @Immutable
+    sealed class IconState {
+
+        abstract val isGrayscale: Boolean
+
+        data class CoinIcon(
+            val url: String?,
+            @DrawableRes val fallbackResId: Int,
+            override val isGrayscale: Boolean,
+        ) : IconState()
+
+        data class TokenIcon(
+            val url: String?,
+            val fallbackTint: Color,
+            val fallbackBackground: Color,
+            override val isGrayscale: Boolean,
+        ) : IconState()
+
+        data class CustomTokenIcon(
+            val tint: Color,
+            val background: Color,
+            override val isGrayscale: Boolean,
+        ) : IconState()
     }
 }
