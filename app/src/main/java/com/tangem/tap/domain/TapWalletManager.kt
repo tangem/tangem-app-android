f87587deb2d1196a0ca2a0af195f7711486c6af7
Tangem <git@tangem.com>
2023-10-03 16:36:42 +0300

    Updated on 2026-09-23

--- app/src/main/java/com/tangem/tap/domain/TapWalletManager.kt
+++ app/src/main/java/com/tangem/tap/domain/TapWalletManager.kt
@@ -92,11 +92,7 @@ class TapWalletManager(
             store.dispatchWalletAction(action = WalletAction.Warnings.CheckIfNeeded)
         }
         setupWalletConnectV2(userWallet)
-
-        val walletFeatureToggles = store.state.daggerGraphState.get(DaggerGraphState::walletFeatureToggles)
-        if (!walletFeatureToggles.isRedesignedScreenEnabled) {
-            loadData(userWallet = userWallet, refresh = refresh)
-        }
+        loadData(userWallet = userWallet, refresh = refresh)
     }
 
     private fun Store<AppState>.dispatchWalletAction(action: WalletAction) {
@@ -161,21 +157,31 @@ class TapWalletManager(
             }
     }
 
-    private fun getAccountsForWc(wcInteractor: WalletConnectInteractor): List<Account> {
-        return store.state.walletState.walletManagers
-            .mapNotNull {
-                val wallet = it.wallet
-                val chainId = wcInteractor.blockchainHelper.networkIdToChainIdOrNull(
-                    wallet.blockchain.toNetworkId(),
+    private suspend fun getAccountsForWc(wcInteractor: WalletConnectInteractor): List<Account> {
+        val walletManagerToggles = store.state.daggerGraphState
+            .get(DaggerGraphState::walletFeatureToggles)
+        val walletManagers = if (walletManagerToggles.isRedesignedScreenEnabled) {
+            val walletManagerFacade = store.state.daggerGraphState
+                .get(DaggerGraphState::walletManagersFacade)
+            val userWallet = userWalletsListManager.selectedUserWalletSync ?: return emptyList()
+            walletManagerFacade.getStoredWalletManagers(userWallet.walletId)
+        } else {
+            store.state.walletState.walletManagers
+        }
+
+        return walletManagers.mapNotNull {
+            val wallet = it.wallet
+            val chainId = wcInteractor.blockchainHelper.networkIdToChainIdOrNull(
+                wallet.blockchain.toNetworkId(),
+            )
+            chainId?.let {
+                Account(
+                    chainId,
+                    wallet.address,
+                    wallet.publicKey.derivationPath?.rawPath,
                 )
-                chainId?.let {
-                    Account(
-                        chainId,
-                        wallet.address,
-                        wallet.publicKey.derivationPath?.rawPath,
-                    )
-                }
             }
+        }
     }
 
     fun updateConfigManager(data: ScanResponse) {
