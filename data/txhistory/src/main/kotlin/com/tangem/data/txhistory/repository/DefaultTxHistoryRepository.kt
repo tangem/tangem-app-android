d9af2a085652803d7a19af7eeae6a5e5e7e7fc33
Anton Tkachev <iiiburnyiii@gmail.com>
2023-10-06 20:57:55 +0400

    AND-4808 [Tx History] Added caching for transactions history

--- data/txhistory/src/main/kotlin/com/tangem/data/txhistory/repository/DefaultTxHistoryRepository.kt
+++ data/txhistory/src/main/kotlin/com/tangem/data/txhistory/repository/DefaultTxHistoryRepository.kt
@@ -3,7 +3,9 @@ package com.tangem.data.txhistory.repository
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
+import com.tangem.data.common.cache.CacheRegistry
 import com.tangem.data.txhistory.repository.paging.TxHistoryPagingSource
+import com.tangem.datasource.local.txhistory.TxHistoryItemsStore
 import com.tangem.datasource.local.userwallet.UserWalletsStore
 import com.tangem.domain.tokens.model.CryptoCurrency
 import com.tangem.domain.tokens.model.Network
@@ -13,50 +15,57 @@ import com.tangem.domain.txhistory.models.TxHistoryStateError
 import com.tangem.domain.txhistory.repository.TxHistoryRepository
 import com.tangem.domain.walletmanager.WalletManagersFacade
 import com.tangem.domain.wallets.models.UserWallet
+import com.tangem.domain.wallets.models.UserWalletId
 import kotlinx.coroutines.flow.Flow
 
 class DefaultTxHistoryRepository(
+    private val cacheRegistry: CacheRegistry,
     private val walletManagersFacade: WalletManagersFacade,
     private val userWalletsStore: UserWalletsStore,
+    private val txHistoryItemsStore: TxHistoryItemsStore,
 ) : TxHistoryRepository {
 
-    override suspend fun getTxHistoryItemsCount(network: Network): Int {
-        val userWallet = getUserWallet()
+    override suspend fun getTxHistoryItemsCount(userWalletId: UserWalletId, network: Network): Int {
+        val userWallet = getUserWallet(userWalletId)
         val state = walletManagersFacade.getTxHistoryState(
             userWalletId = userWallet.walletId,
             network = network,
         )
         return when (state) {
             is TxHistoryState.Failed.FetchError -> throw TxHistoryStateError.DataError(state.exception)
-            TxHistoryState.NotImplemented -> throw TxHistoryStateError.TxHistoryNotImplemented
-            TxHistoryState.Success.Empty -> throw TxHistoryStateError.EmptyTxHistories
+            is TxHistoryState.NotImplemented -> throw TxHistoryStateError.TxHistoryNotImplemented
+            is TxHistoryState.Success.Empty -> throw TxHistoryStateError.EmptyTxHistories
             is TxHistoryState.Success.HasTransactions -> state.txCount
         }
     }
 
-    override fun getTxHistoryItems(currency: CryptoCurrency, pageSize: Int): Flow<PagingData<TxHistoryItem>> {
-        val userWallet = getUserWallet()
-        return Pager(
+    override fun getTxHistoryItems(
+        userWalletId: UserWalletId,
+        currency: CryptoCurrency,
+        pageSize: Int,
+        refresh: Boolean,
+    ): Flow<PagingData<TxHistoryItem>> {
+        val pager = Pager(
             config = PagingConfig(
                 pageSize = pageSize,
                 initialLoadSize = pageSize,
             ),
             pagingSourceFactory = {
                 TxHistoryPagingSource(
-                    loadPage = { page: Int, pageSize: Int ->
-                        walletManagersFacade.getTxHistoryItems(
-                            userWalletId = userWallet.walletId,
-                            currency = currency,
-                            page = page,
-                            pageSize = pageSize,
-                        )
-                    },
+                    sourceParams = TxHistoryPagingSource.Params(userWalletId, currency, pageSize, refresh),
+                    txHistoryItemsStore = txHistoryItemsStore,
+                    walletManagersFacade = walletManagersFacade,
+                    cacheRegistry = cacheRegistry,
                 )
             },
-        ).flow
+        )
+
+        return pager.flow
     }
 
-    private fun getUserWallet(): UserWallet = requireNotNull(userWalletsStore.selectedUserWalletOrNull) {
-        "Selected wallet must not be null"
+    private suspend fun getUserWallet(userWalletId: UserWalletId): UserWallet {
+        return requireNotNull(userWalletsStore.getSyncOrNull(userWalletId)) {
+            "Unable to find user wallet with provided ID: $userWalletId"
+        }
     }
 }
