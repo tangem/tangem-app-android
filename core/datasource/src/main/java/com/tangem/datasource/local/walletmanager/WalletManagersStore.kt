7eb17c71bb5c7bb653a218389088ffb4a9e8ca7a
Tangem <git@tangem.com>
2023-10-09 11:49:43 +0800

    Updated on 2026-08-07

--- core/datasource/src/main/java/com/tangem/datasource/local/walletmanager/WalletManagersStore.kt
+++ core/datasource/src/main/java/com/tangem/datasource/local/walletmanager/WalletManagersStore.kt
@@ -3,8 +3,11 @@ package com.tangem.datasource.local.walletmanager
 import com.tangem.blockchain.common.Blockchain
 import com.tangem.blockchain.common.WalletManager
 import com.tangem.domain.wallets.models.UserWalletId
+import kotlinx.coroutines.flow.Flow
 
 interface WalletManagersStore {
+
+    fun getAll(userWalletId: UserWalletId): Flow<List<WalletManager>>
 
     suspend fun getSyncOrNull(
         userWalletId: UserWalletId,
