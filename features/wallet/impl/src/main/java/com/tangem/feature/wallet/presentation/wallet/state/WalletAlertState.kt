b83da41ee9ccfe18412050d6496f8a19a7e0d1a2
Tangem <git@tangem.com>
2023-10-10 18:07:39 +0800

    Updated on 2026-08-07

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/WalletAlertState.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/WalletAlertState.kt
@@ -2,12 +2,28 @@ package com.tangem.feature.wallet.presentation.wallet.state
 
 import androidx.compose.runtime.Immutable
 import com.tangem.core.ui.extensions.TextReference
+import com.tangem.core.ui.extensions.resourceReference
+import com.tangem.feature.wallet.impl.R
 
 @Immutable
 internal sealed class WalletAlertState {
+
+    abstract val title: TextReference?
+    abstract val message: TextReference
+    open val confirmButtonText: TextReference = resourceReference(id = R.string.common_ok)
+    open val isWarningConfirmButton: Boolean = false
+    abstract val onConfirmClick: (() -> Unit)?
+
     data class DefaultAlert(
-        val title: TextReference,
-        val message: TextReference,
-        val onActionClick: (() -> Unit)?,
+        override val title: TextReference,
+        override val message: TextReference,
+        override val onConfirmClick: (() -> Unit)?,
     ) : WalletAlertState()
+
+    data class RemoveWalletAlert(override val onConfirmClick: (() -> Unit)?) : WalletAlertState() {
+        override val title: TextReference? = null
+        override val message: TextReference = resourceReference(id = R.string.user_wallet_list_delete_prompt)
+        override val confirmButtonText: TextReference = resourceReference(id = R.string.common_delete)
+        override val isWarningConfirmButton: Boolean = true
+    }
 }
