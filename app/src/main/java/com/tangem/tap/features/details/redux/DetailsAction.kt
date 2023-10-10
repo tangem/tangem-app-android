d9edf8e3a003ded41bc1fd036367ae603da6803a
Tangem <git@tangem.com>
2023-10-10 14:50:19 +0800

    Updated on 2026-08-27

--- app/src/main/java/com/tangem/tap/features/details/redux/DetailsAction.kt
+++ app/src/main/java/com/tangem/tap/features/details/redux/DetailsAction.kt
@@ -14,6 +14,7 @@ sealed class DetailsAction : Action {
     data class PrepareScreen(
         val scanResponse: ScanResponse,
         val darkThemeSwitchEnabled: Boolean,
+        val shouldSaveUserWallets: Boolean,
     ) : DetailsAction()
 
     object ReCreateTwinsWallet : DetailsAction()
