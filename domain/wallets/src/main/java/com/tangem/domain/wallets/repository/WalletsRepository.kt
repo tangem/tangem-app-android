afcde269ce124dbdb4beb0b319608abb62d28588
Tangem <git@tangem.com>
2023-10-10 14:50:19 +0800

    Updated on 2026-08-14

--- domain/wallets/src/main/java/com/tangem/domain/wallets/repository/WalletsRepository.kt
+++ domain/wallets/src/main/java/com/tangem/domain/wallets/repository/WalletsRepository.kt
@@ -1,6 +1,14 @@
 package com.tangem.domain.wallets.repository
 
+import kotlinx.coroutines.flow.Flow
+
 interface WalletsRepository {
 
-    suspend fun shouldSaveUserWallets(): Boolean
+    suspend fun initialize()
+
+    suspend fun shouldSaveUserWalletsSync(): Boolean
+
+    fun shouldSaveUserWallets(): Flow<Boolean>
+
+    suspend fun saveShouldSaveUserWallets(item: Boolean)
 }
