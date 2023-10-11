aa905ed4afd33aaf1c5ffb2f64cb3e4b24bd3c8e
Tangem <git@tangem.com>
2023-10-11 10:57:11 +0400

    release/5.26.2

--- features/swap/presentation/src/main/java/com/tangem/feature/swap/ui/TransactionCard.kt
+++ features/swap/presentation/src/main/java/com/tangem/feature/swap/ui/TransactionCard.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -13,11 +14,11 @@ import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.ripple.rememberRipple
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -27,15 +28,16 @@ import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.SubcomposeAsyncImage
 import coil.request.ImageRequest
 import com.tangem.core.ui.R
 import com.tangem.core.ui.components.*
 import com.tangem.core.ui.res.TangemTheme
+import com.tangem.core.ui.utils.ImageBackgroundContrastChecker
 import com.tangem.feature.swap.models.SwapWarning
 import com.tangem.feature.swap.models.TransactionCardType
+import kotlinx.coroutines.launch
 
 @Suppress("LongParameterList")
 @Composable
@@ -265,47 +267,13 @@ fun Token(
         verticalArrangement = Arrangement.Bottom,
         horizontalAlignment = Alignment.End,
     ) {
-        Box(
-            modifier = Modifier
-                .padding(end = TangemTheme.dimens.spacing16)
-                .size(TangemTheme.dimens.size42),
-        ) {
-            val tokenImageModifier = Modifier
-                .align(Alignment.BottomStart)
-                .size(TangemTheme.dimens.size36)
-
-            val data = tokenIconUrl.ifEmpty {
-                iconPlaceholder
-            }
-            SubcomposeAsyncImage(
-                modifier = tokenImageModifier,
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data(data)
-                    .crossfade(true)
-                    .build(),
-                loading = { CircleShimmer(modifier = tokenImageModifier) },
-                contentDescription = tokenCurrency,
-            )
-
-            if (networkIconRes != null) {
-                Box(
-                    modifier = Modifier
-                        .align(Alignment.TopEnd)
-                        .size(TangemTheme.dimens.size18)
-                        .background(color = Color.White, shape = CircleShape),
-                    contentAlignment = Alignment.Center,
-                ) {
-                    Image(
-                        modifier = Modifier.padding(all = 0.5.dp),
-                        painter = painterResource(id = networkIconRes),
-                        contentDescription = null,
-                    )
-                }
-            }
-        }
-
+        TokenIcon(
+            tokenIconUrl = tokenIconUrl,
+            tokenCurrency = tokenCurrency,
+            iconPlaceholder = iconPlaceholder,
+            networkIconRes = networkIconRes,
+        )
         SpacerH4()
-
         Text(
             text = tokenCurrency,
             color = TangemTheme.colors.text.primary1,
@@ -314,6 +282,75 @@ fun Token(
             textAlign = TextAlign.Center,
             modifier = Modifier.defaultMinSize(minWidth = TangemTheme.dimens.size80),
         )
+    }
+}
+
+@Composable
+private fun TokenIcon(
+    tokenIconUrl: String,
+    tokenCurrency: String,
+    @DrawableRes iconPlaceholder: Int? = null,
+    @DrawableRes networkIconRes: Int? = null,
+) {
+    val itemBackgroundColor = TangemTheme.colors.background.primary.toArgb()
+    var iconBackgroundColor by remember { mutableStateOf(Color.Transparent) }
+    val isDarkTheme = isSystemInDarkTheme()
+    val coroutineScope = rememberCoroutineScope()
+
+    Box(
+        modifier = Modifier
+            .padding(end = TangemTheme.dimens.spacing16)
+            .size(TangemTheme.dimens.size42),
+    ) {
+        val tokenImageModifier = Modifier
+            .align(Alignment.BottomStart)
+            .size(TangemTheme.dimens.size36)
+            .background(
+                color = iconBackgroundColor,
+                shape = TangemTheme.shapes.roundedCorners8,
+            )
+
+        val data = tokenIconUrl.ifEmpty {
+            iconPlaceholder
+        }
+        SubcomposeAsyncImage(
+            modifier = tokenImageModifier,
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(data)
+                .crossfade(true)
+                .allowHardware(false)
+                .listener(
+                    onSuccess = { _, result ->
+                        if (isDarkTheme) {
+                            coroutineScope.launch {
+                                val color = ImageBackgroundContrastChecker(
+                                    drawable = result.drawable,
+                                    backgroundColor = itemBackgroundColor,
+                                ).getContrastColorIfNeeded(isDarkTheme)
+                                iconBackgroundColor = color
+                            }
+                        }
+                    },
+                ).build(),
+            loading = { CircleShimmer(modifier = tokenImageModifier) },
+            contentDescription = tokenCurrency,
+        )
+
+        if (networkIconRes != null) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .size(TangemTheme.dimens.size18)
+                    .background(color = TangemTheme.colors.background.primary, shape = CircleShape),
+                contentAlignment = Alignment.Center,
+            ) {
+                Image(
+                    modifier = Modifier.padding(all = TangemTheme.dimens.spacing0_5),
+                    painter = painterResource(id = networkIconRes),
+                    contentDescription = null,
+                )
+            }
+        }
     }
 }
 
