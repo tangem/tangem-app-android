a90c79d4f8b6f761ce3c31a98b45a4b6a7a26109
Tangem <git@tangem.com>
2023-10-11 19:13:33 +0800

    Updated on 2026-09-25

--- app/src/main/java/com/tangem/tap/TapApplication.kt
+++ app/src/main/java/com/tangem/tap/TapApplication.kt
@@ -45,7 +45,6 @@ import com.tangem.tap.common.analytics.handlers.appsFlyer.AppsFlyerAnalyticsHand
 import com.tangem.tap.common.analytics.handlers.firebase.FirebaseAnalyticsHandler
 import com.tangem.tap.common.analytics.topup.TopUpController
 import com.tangem.tap.common.chat.ChatManager
-import com.tangem.tap.common.extensions.dispatchOnMain
 import com.tangem.tap.common.feedback.AdditionalFeedbackInfo
 import com.tangem.tap.common.feedback.FeedbackManager
 import com.tangem.tap.common.images.createCoilImageLoader
@@ -240,10 +239,10 @@ internal class TapApplication : Application(), ImageLoaderFactory {
         // TODO: Try to performance and user experience.
         //  [REDACTED_JIRA]
         runBlocking {
+            walletsRepository.initialize()
             initUserWalletsListManager()
             featureTogglesManager.init()
             appRatingRepository.initialize()
-            walletsRepository.initialize()
             // learn2earnInteractor.init()
         }
 
@@ -416,6 +415,6 @@ internal class TapApplication : Application(), ImageLoaderFactory {
             UserWalletsListManager.provideRuntimeImplementation()
         }
 
-        store.dispatchOnMain(GlobalAction.UpdateUserWalletsListManager(manager))
+        store.dispatch(GlobalAction.UpdateUserWalletsListManager(manager))
     }
 }
