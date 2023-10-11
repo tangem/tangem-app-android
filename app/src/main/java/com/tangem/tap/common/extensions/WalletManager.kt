be16b182cd6b6384485ebcea2a2eb05c5bfafc2c
Tangem <git@tangem.com>
2023-10-11 20:52:19 +0500

    Updated on 2026-09-01

--- app/src/main/java/com/tangem/tap/common/extensions/WalletManager.kt
+++ app/src/main/java/com/tangem/tap/common/extensions/WalletManager.kt
@@ -1,6 +1,5 @@
 package com.tangem.tap.common.extensions
 
-import com.tangem.blockchain.common.Blockchain
 import com.tangem.blockchain.common.BlockchainSdkError
 import com.tangem.blockchain.common.Wallet
 import com.tangem.blockchain.common.WalletManager
@@ -81,12 +80,4 @@ fun WalletManager?.getAddressData(): WalletDataModel.AddressData? {
 
     val addressDataList = wallet.createAddressesData()
     return if (addressDataList.isEmpty()) null else addressDataList[0]
-}
-
-fun <T> WalletManager.Companion.stub(): T {
-    val wallet = Wallet(Blockchain.Unknown, setOf(), Wallet.PublicKey(byteArrayOf(), null), setOf())
-    return object : WalletManager(wallet) {
-        override val currentHost: String = ""
-        override suspend fun update() {}
-    } as T
 }
