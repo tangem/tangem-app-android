3e121f082eca82980f67b62beb16f1705fe11715
Tangem <git@tangem.com>
2023-10-12 11:51:59 +0400

    Updated on 2026-02-06

---
+++ features/manage-tokens/impl/src/main/java/com/tangem/managetokens/di/ManageTokensRouterModule.kt
@@ -0,0 +1,20 @@
+package com.tangem.managetokens.di
+
+import com.tangem.features.managetokens.navigation.ManageTokensRouter
+import com.tangem.managetokens.presentation.router.DefaultManageTokensRouter
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.scopes.ActivityScoped
+
+@Module
+@InstallIn(ActivityComponent::class)
+internal object ManageTokensRouterModule {
+
+    @Provides
+    @ActivityScoped
+    fun provideManageTokensRouter(): ManageTokensRouter {
+        return DefaultManageTokensRouter()
+    }
+}
