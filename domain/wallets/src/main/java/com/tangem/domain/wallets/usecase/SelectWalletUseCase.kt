f86b7d46a520ef771b15514ee78aafbcd2513d96
Tangem <git@tangem.com>
2023-10-15 10:28:52 +0800

    Updated on 2026-08-19

--- domain/wallets/src/main/java/com/tangem/domain/wallets/usecase/SelectWalletUseCase.kt
+++ domain/wallets/src/main/java/com/tangem/domain/wallets/usecase/SelectWalletUseCase.kt
@@ -3,11 +3,11 @@ package com.tangem.domain.wallets.usecase
 import arrow.core.Either
 import arrow.core.left
 import arrow.core.raise.either
-import arrow.core.raise.ensureNotNull
 import arrow.core.right
 import com.tangem.common.doOnFailure
 import com.tangem.common.doOnSuccess
 import com.tangem.domain.wallets.legacy.WalletsStateHolder
+import com.tangem.domain.wallets.legacy.ensureUserWalletListManagerNotNull
 import com.tangem.domain.wallets.models.SelectWalletError
 import com.tangem.domain.wallets.models.UserWalletId
 
@@ -22,8 +22,8 @@ class SelectWalletUseCase(private val walletsStateHolder: WalletsStateHolder) {
 
     suspend operator fun invoke(userWalletId: UserWalletId): Either<SelectWalletError, Unit> {
         return either {
-            val userWalletsListManager = ensureNotNull(
-                value = walletsStateHolder.userWalletsListManager,
+            val userWalletsListManager = ensureUserWalletListManagerNotNull(
+                walletsStateHolder = walletsStateHolder,
                 raise = { SelectWalletError.DataError },
             )
 
