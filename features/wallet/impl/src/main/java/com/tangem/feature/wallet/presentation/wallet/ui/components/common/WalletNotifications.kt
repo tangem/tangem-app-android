84a26b6ca1a13ba6fb65f41db6d0369924c12792
Tangem <git@tangem.com>
2023-10-16 15:00:18 +0300

    Updated on 2026-09-14

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/components/common/WalletNotifications.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/components/common/WalletNotifications.kt
@@ -29,10 +29,9 @@ internal fun LazyListScope.notifications(configs: ImmutableList<WalletNotificati
                 modifier = modifier.animateItemPlacement(),
                 iconTint = when (it) {
                     is WalletNotification.Critical -> TangemTheme.colors.icon.warning
-                    is WalletNotification.MissingAddresses -> TangemTheme.colors.icon.accent
+                    is WalletNotification.Informational -> TangemTheme.colors.icon.accent
                     is WalletNotification.RateApp -> TangemTheme.colors.icon.attention
                     is WalletNotification.UnlockWallets -> TangemTheme.colors.icon.primary1
-                    is WalletNotification.NoAccount -> TangemTheme.colors.icon.accent
                     is WalletNotification.Warning -> null
                 },
             )
