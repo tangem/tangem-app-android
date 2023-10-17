40faab82791d6ad27dd80b2feb9590ee26c99a27
Tangem <git@tangem.com>
2023-10-17 11:19:08 +0300

    Updated on 2026-08-27

--- app/src/main/java/com/tangem/tap/common/chat/ChatManager.kt
+++ app/src/main/java/com/tangem/tap/common/chat/ChatManager.kt
@@ -2,37 +2,31 @@ package com.tangem.tap.common.chat
 
 import android.content.Context
 import android.os.Build
+import com.tangem.data.source.preferences.PreferencesDataSource
 import com.tangem.datasource.config.models.ChatConfig
 import com.tangem.datasource.config.models.SprinklrConfig
 import com.tangem.datasource.config.models.ZendeskConfig
-import com.tangem.data.source.preferences.PreferencesDataSource
 import com.tangem.tap.ForegroundActivityObserver
 import com.tangem.tap.common.chat.opener.ChatOpener
 import com.tangem.tap.common.chat.opener.implementation.SprinklrChatOpener
 import com.tangem.tap.common.chat.opener.implementation.ZendeskChatOpener
-import com.tangem.tap.common.redux.AppState
-import org.rekotlin.Store
 import java.io.File
 
 class ChatManager(
     private val preferencesStorage: PreferencesDataSource,
     private val foregroundActivityObserver: ForegroundActivityObserver,
-    private val store: Store<AppState>,
 ) {
     private val openers = mutableMapOf<ChatConfig, ChatOpener>()
 
     fun open(config: ChatConfig, createLogsFile: (Context) -> File?, createFeedbackFile: (Context) -> File?) {
         val opener = openers.getOrPut(config) {
             when (config) {
-                is SprinklrConfig -> SprinklrChatOpener(getSprinklrUserId(), config, store, foregroundActivityObserver)
+                is SprinklrConfig -> SprinklrChatOpener(config, foregroundActivityObserver)
                 is ZendeskConfig -> ZendeskChatOpener(getZendeskUserId(), config, foregroundActivityObserver)
             }
         }
 
-        opener.open(
-            createFeedbackFile = createFeedbackFile,
-            createLogsFile = createLogsFile,
-        )
+        opener.open(createFeedbackFile, createLogsFile)
     }
 
     private fun getZendeskUserId(): String {
@@ -40,18 +34,6 @@ class ChatManager(
             preferencesStorage.zendeskFirstLaunchTime = System.currentTimeMillis()
         }
 
-        return getChatUserId(preferencesStorage.zendeskFirstLaunchTime!!)
-    }
-
-    private fun getSprinklrUserId(): String {
-        if (preferencesStorage.sprinklrFirstLaunchTime == null) {
-            preferencesStorage.sprinklrFirstLaunchTime = System.currentTimeMillis()
-        }
-
-        return getChatUserId(preferencesStorage.sprinklrFirstLaunchTime!!)
-    }
-
-    private fun getChatUserId(firstLaunchTimeMillis: Long): String {
-        return "${firstLaunchTimeMillis}${Build.MODEL}".hashCode().toString()
+        return "${preferencesStorage.zendeskFirstLaunchTime}${Build.MODEL}".hashCode().toString()
     }
 }
