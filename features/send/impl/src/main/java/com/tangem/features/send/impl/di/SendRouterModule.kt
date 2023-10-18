62de2d2136f28b8732620ce906fd1ab42ffd6571
Tangem <git@tangem.com>
2023-10-18 18:47:15 +0300

    Updated on 2026-03-27

---
+++ features/send/impl/src/main/java/com/tangem/features/send/impl/di/SendRouterModule.kt
@@ -0,0 +1,23 @@
+package com.tangem.features.send.impl.di
+
+import com.tangem.features.send.api.navigation.SendRouter
+import com.tangem.features.send.impl.navigation.DefaultSendRouter
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.scopes.ActivityScoped
+
+/**
+ * DI module provides implementation of [SendRouter]
+ */
+@Module
+@InstallIn(ActivityComponent::class)
+internal object SendRouterModule {
+
+    @Provides
+    @ActivityScoped
+    fun provideSendRouter(): SendRouter {
+        return DefaultSendRouter()
+    }
+}
