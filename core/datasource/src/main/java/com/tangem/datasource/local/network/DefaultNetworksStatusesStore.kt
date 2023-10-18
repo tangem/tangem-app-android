c0476fc000f669e54249ad509339dc5dc5a5c62e
Tangem <git@tangem.com>
2023-10-18 23:46:37 +0800

    Updated on 2026-06-02

--- core/datasource/src/main/java/com/tangem/datasource/local/network/DefaultNetworksStatusesStore.kt
+++ core/datasource/src/main/java/com/tangem/datasource/local/network/DefaultNetworksStatusesStore.kt
@@ -5,20 +5,26 @@ import com.tangem.datasource.local.datastore.core.StringKeyDataStoreDecorator
 import com.tangem.domain.tokens.model.NetworkStatus
 import com.tangem.domain.wallets.models.UserWalletId
 import com.tangem.utils.extensions.addOrReplace
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 internal class DefaultNetworksStatusesStore(
     dataStore: StringKeyDataStore<Set<NetworkStatus>>,
 ) : NetworksStatusesStore, StringKeyDataStoreDecorator<UserWalletId, Set<NetworkStatus>>(dataStore) {
+
+    private val mutex = Mutex()
 
     override fun provideStringKey(key: UserWalletId): String {
         return key.stringValue
     }
 
     override suspend fun store(key: UserWalletId, value: NetworkStatus) {
-        val newValues = getSyncOrNull(key)
-            ?.addOrReplace(value) { it.network == value.network }
-            ?: setOf(value)
+        mutex.withLock {
+            val newValues = getSyncOrNull(key)
+                ?.addOrReplace(value) { it.network == value.network }
+                ?: setOf(value)
 
-        store(key, newValues)
+            store(key, newValues)
+        }
     }
 }
