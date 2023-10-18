4d20e36b87a1d0deb00e919a0eff66c1086c0323
Tangem <git@tangem.com>
2023-10-18 17:31:46 +0300

    Updated on 2026-04-06

--- app/src/main/java/com/tangem/tap/features/wallet/ui/dialogs/SignedHashesWarningDialog.kt
+++ app/src/main/java/com/tangem/tap/features/wallet/ui/dialogs/SignedHashesWarningDialog.kt
@@ -9,11 +9,13 @@ import com.tangem.tap.features.wallet.redux.WalletAction
 import com.tangem.tap.store
 import com.tangem.wallet.R
 
+// TODO: Delete with WalletFeatureToggles
+@Deprecated(message = "Used only in old wallet screen")
 object SignedHashesWarningDialog {
     fun create(context: Context): AlertDialog {
         return MaterialAlertDialogBuilder(context, R.style.CustomMaterialDialog).apply {
-            setTitle(context.getString(R.string.warning_important_security_info, "\u26A0"))
-            setMessage(R.string.alert_signed_hashes_message)
+            // setTitle(context.getString(R.string.warning_important_security_info, "\u26A0"))
+            // setMessage(R.string.alert_signed_hashes_message)
             setPositiveButton(R.string.common_understand) { _, _ ->
                 store.dispatch(WalletAction.Warnings.CheckHashesCount.SaveCardId)
                 store.dispatch(
