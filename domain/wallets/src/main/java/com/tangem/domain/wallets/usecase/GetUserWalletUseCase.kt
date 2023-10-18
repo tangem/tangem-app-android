b00aee3517b4b3723d49dbbba5c6c8d56ed5f701
Tangem <git@tangem.com>
2023-10-18 17:31:46 +0300

    Updated on 2026-06-24

--- domain/wallets/src/main/java/com/tangem/domain/wallets/usecase/GetUserWalletUseCase.kt
+++ domain/wallets/src/main/java/com/tangem/domain/wallets/usecase/GetUserWalletUseCase.kt
@@ -4,6 +4,7 @@ import arrow.core.Either
 import arrow.core.raise.either
 import arrow.core.raise.ensureNotNull
 import com.tangem.domain.wallets.legacy.WalletsStateHolder
+import com.tangem.domain.wallets.legacy.ensureUserWalletListManagerNotNull
 import com.tangem.domain.wallets.models.GetUserWalletError
 import com.tangem.domain.wallets.models.UserWallet
 import com.tangem.domain.wallets.models.UserWalletId
@@ -12,13 +13,9 @@ import kotlinx.coroutines.flow.firstOrNull
 class GetUserWalletUseCase(private val walletsStateHolder: WalletsStateHolder) {
 
     suspend operator fun invoke(userWalletId: UserWalletId): Either<Any, UserWallet> = either {
-        val userWalletsListManager = ensureNotNull(
-            value = walletsStateHolder.userWalletsListManager,
-            raise = {
-                val error = IllegalStateException("User wallets list manager not initialized")
-
-                GetUserWalletError.DataError(error)
-            },
+        val userWalletsListManager = ensureUserWalletListManagerNotNull(
+            walletsStateHolder = walletsStateHolder,
+            raise = GetUserWalletError::DataError,
         )
 
         val userWallets = userWalletsListManager.userWallets.firstOrNull().orEmpty()
