6262e45e0268a26bf63a85899000642ca71c2a3e
Tangem <git@tangem.com>
2023-10-19 21:19:11 +0800

    Updated on 2026-08-19

--- app/src/main/java/com/tangem/tap/features/home/HomeFragment.kt
+++ app/src/main/java/com/tangem/tap/features/home/HomeFragment.kt
@@ -21,12 +21,12 @@ import com.tangem.core.ui.components.SystemBarsEffect
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.domain.tokens.TokensAction
 import com.tangem.tap.common.analytics.events.IntroductionProcess
+import com.tangem.tap.common.redux.AppState
 import com.tangem.tap.features.home.compose.StoriesScreen
 import com.tangem.tap.features.home.redux.HomeAction
 import com.tangem.tap.features.home.redux.HomeState
 import com.tangem.tap.store
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 import org.rekotlin.StoreSubscriber
 
 @AndroidEntryPoint
@@ -51,14 +51,9 @@ class HomeFragment : Fragment(), StoreSubscriber<HomeState> {
         return ComposeView(inflater.context).apply {
             setContent {
                 TangemTheme {
-                    BackHandler {
-                        requireActivity().finish()
-                    }
+                    BackHandler(onBack = requireActivity()::finish)
                     SystemBarsEffect {
-                        setSystemBarsColor(
-                            color = Color.Transparent,
-                            darkIcons = false,
-                        )
+                        setSystemBarsColor(color = Color.Transparent, darkIcons = false)
                     }
                     ScreenContent()
                 }
@@ -70,10 +65,10 @@ class HomeFragment : Fragment(), StoreSubscriber<HomeState> {
         super.onStart()
         activity?.window?.let { WindowCompat.setDecorFitsSystemWindows(it, false) }
 
-        store.subscribe(this) { state ->
-            state.skipRepeats { oldState, newState ->
-                oldState.homeState == newState.homeState
-            }.select { it.homeState }
+        store.subscribe(subscriber = this) { state ->
+            state
+                .skipRepeats { oldState, newState -> oldState.homeState == newState.homeState }
+                .select(AppState::homeState)
         }
     }
 
@@ -101,11 +96,7 @@ class HomeFragment : Fragment(), StoreSubscriber<HomeState> {
             onLearn2earnClick = {}, // learn2earnViewModel.uiState.storyScreenState.onClick,
             onScanButtonClick = {
                 Analytics.send(IntroductionProcess.ButtonScanCard())
-                lifecycleScope.launch {
-                    store.dispatch(
-                        HomeAction.ReadCard(scope = this),
-                    )
-                }
+                store.dispatch(action = HomeAction.ReadCard(scope = requireActivity().lifecycleScope))
             },
             onShopButtonClick = {
                 Analytics.send(IntroductionProcess.ButtonBuyCards())
