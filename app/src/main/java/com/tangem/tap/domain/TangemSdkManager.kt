6c9edd99222eb15d646087eefd1922e9071f55da
Tangem <git@tangem.com>
2023-10-19 12:42:05 +0300

    Updated on 2026-06-16

--- app/src/main/java/com/tangem/tap/domain/TangemSdkManager.kt
+++ app/src/main/java/com/tangem/tap/domain/TangemSdkManager.kt
@@ -1,6 +1,7 @@
 package com.tangem.tap.domain
 
 import android.content.res.Resources
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import com.tangem.Message
 import com.tangem.TangemSdk
@@ -81,12 +82,13 @@ class TangemSdkManager(private val cardSdkConfigRepository: CardSdkConfigReposit
 
     suspend fun createProductWallet(scanResponse: ScanResponse): CompletionResult<CreateProductWalletTaskResponse> {
         return runTaskAsync(
-            CreateProductWalletTask(
+            runnable = CreateProductWalletTask(
                 cardTypesResolver = scanResponse.cardTypesResolver,
                 derivationStyleProvider = scanResponse.derivationStyleProvider,
             ),
-            scanResponse.card.cardId,
-            Message(resources.getString(R.string.initial_message_create_wallet_body)),
+            cardId = scanResponse.card.cardId,
+            initialMessage = Message(resources.getString(R.string.initial_message_create_wallet_body)),
+            iconScanRes = if (scanResponse.cardTypesResolver.isRing()) R.drawable.img_hand_scan_ring else null,
         )
     }
 
@@ -94,7 +96,7 @@ class TangemSdkManager(private val cardSdkConfigRepository: CardSdkConfigReposit
         scanResponse: ScanResponse,
         mnemonic: String,
     ): CompletionResult<CreateProductWalletTaskResponse> {
-        val mnemonic = try {
+        val defaultMnemonic = try {
             DefaultMnemonic(mnemonic, tangemSdk.wordlist)
         } catch (e: TangemSdkError.MnemonicException) {
             return CompletionResult.Failure(e)
@@ -103,7 +105,7 @@ class TangemSdkManager(private val cardSdkConfigRepository: CardSdkConfigReposit
             CreateProductWalletTask(
                 scanResponse.cardTypesResolver,
                 derivationStyleProvider = scanResponse.derivationStyleProvider,
-                mnemonic,
+                defaultMnemonic,
             ),
             scanResponse.card.cardId,
             Message(resources.getString(R.string.initial_message_create_wallet_body)),
@@ -213,9 +215,10 @@ class TangemSdkManager(private val cardSdkConfigRepository: CardSdkConfigReposit
         cardId: String? = null,
         initialMessage: Message? = null,
         accessCode: String? = null,
+        @DrawableRes iconScanRes: Int? = null,
     ): CompletionResult<T> = withContext(Dispatchers.Main) {
         suspendCancellableCoroutine { continuation ->
-            tangemSdk.startSessionWithRunnable(runnable, cardId, initialMessage, accessCode) { result ->
+            tangemSdk.startSessionWithRunnable(runnable, cardId, initialMessage, accessCode, iconScanRes) { result ->
                 if (continuation.isActive) continuation.resume(result)
             }
         }
