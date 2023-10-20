aa077cb5a31450109ebd0d72438e2ec9fb737951
Tangem <git@tangem.com>
2023-10-20 15:42:49 +0800

    Updated on 2026-06-05

--- domain/tokens/models/src/main/java/com/tangem/domain/tokens/models/analytics/TokenScreenAnalyticsEvent.kt
+++ domain/tokens/models/src/main/java/com/tangem/domain/tokens/models/analytics/TokenScreenAnalyticsEvent.kt
@@ -1,62 +1,67 @@
-package com.tangem.feature.tokendetails.presentation.tokendetails.analytics
+package com.tangem.domain.tokens.models.analytics
 
 import com.tangem.core.analytics.models.AnalyticsEvent
 
 /**
 [REDACTED_AUTHOR]
  */
-sealed class TokenScreenEvent(
+sealed class TokenScreenAnalyticsEvent(
     event: String,
     params: Map<String, String> = mapOf(),
     error: Throwable? = null,
 ) : AnalyticsEvent("Token", event, params, error) {
 
-    class Refreshed(token: String) : TokenScreenEvent(
+    class Refreshed(token: String) : TokenScreenAnalyticsEvent(
         event = "Refreshed",
         params = mapOf("Token" to token),
     )
 
-    class ButtonRemoveToken(token: String) : TokenScreenEvent(
+    class ButtonRemoveToken(token: String) : TokenScreenAnalyticsEvent(
         "Button - Remove Token",
         params = mapOf("Token" to token),
     )
 
-    class ButtonExplore(token: String) : TokenScreenEvent(
+    class ButtonExplore(token: String) : TokenScreenAnalyticsEvent(
         event = "Button - Explore",
         params = mapOf("Token" to token),
     )
 
-    class ButtonReload(token: String) : TokenScreenEvent(
+    class ButtonReload(token: String) : TokenScreenAnalyticsEvent(
         event = "Button - Reload",
         params = mapOf("Token" to token),
     )
 
-    class ButtonBuy(token: String) : TokenScreenEvent(
+    class ButtonBuy(token: String) : TokenScreenAnalyticsEvent(
         event = "Button - Buy",
         params = mapOf("Token" to token),
     )
 
-    class ButtonSell(token: String) : TokenScreenEvent(
+    class ButtonSell(token: String) : TokenScreenAnalyticsEvent(
         event = "Button - Sell",
         params = mapOf("Token" to token),
     )
 
-    class ButtonExchange(token: String) : TokenScreenEvent(
+    class ButtonExchange(token: String) : TokenScreenAnalyticsEvent(
         event = "Button - Exchange",
         params = mapOf("Token" to token),
     )
 
-    class ButtonSend(token: String) : TokenScreenEvent(
+    class ButtonSend(token: String) : TokenScreenAnalyticsEvent(
         event = "Button - Send",
         params = mapOf("Token" to token),
     )
 
-    class ButtonReceive(token: String) : TokenScreenEvent(
+    class ButtonReceive(token: String) : TokenScreenAnalyticsEvent(
         event = "Button - Receive",
         params = mapOf("Token" to token),
     )
 
-    class Bought(token: String) : TokenScreenEvent(
+    class ButtonCopyAddress(token: String) : TokenScreenAnalyticsEvent(
+        event = "Button - Copy Address",
+        params = mapOf("Token" to token),
+    )
+
+    class Bought(token: String) : TokenScreenAnalyticsEvent(
         event = "Token Bought",
         params = mapOf("Token" to token),
     )
