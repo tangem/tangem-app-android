29d491c8bf7fd2039f020e983802e18cb3a3b9e3
Tangem <git@tangem.com>
2023-10-20 10:01:38 +0400

    Updated on 2026-03-27

---
+++ features/manage-tokens/impl/src/main/java/com/tangem/managetokens/presentation/managetokens/state/previewdata/DerivationNotificationStatePreviewData.kt
@@ -0,0 +1,11 @@
+package com.tangem.managetokens.presentation.managetokens.state.previewdata
+
+import com.tangem.managetokens.presentation.managetokens.state.DerivationNotificationState
+
+object DerivationNotificationStatePreviewData {
+    val state = DerivationNotificationState(
+        totalNeeded = 3,
+        missingAddressesCount = 2,
+        onGenerateClick = {},
+    )
+}
