56ff1ab9c00ae5006268ec290c75b99cafc855a5
Tangem <git@tangem.com>
2023-10-22 20:01:40 +0800

    Updated on 2026-09-14

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/viewmodels/WalletViewModel.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/viewmodels/WalletViewModel.kt
@@ -350,7 +350,7 @@ internal class WalletViewModel @Inject constructor(
         }.ifEmpty { return }
 
         viewModelScope.launch(dispatchers.io) {
-            derivePublicKeysUseCase(cardId = scanResponse.card.cardId, derivations = derivations)
+            derivePublicKeysUseCase(cardId = null, derivations = derivations)
                 .onRight {
                     val newDerivedKeys = it.entries
                     val oldDerivedKeys = scanResponse.derivedKeys
