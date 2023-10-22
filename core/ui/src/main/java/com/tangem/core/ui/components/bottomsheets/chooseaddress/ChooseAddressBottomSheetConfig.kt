9e437fafde709851107b33cd80e906b1a1af338b
Tangem <git@tangem.com>
2023-10-22 09:55:02 +0800

    Updated on 2026-06-02

--- core/ui/src/main/java/com/tangem/core/ui/components/bottomsheets/chooseaddress/ChooseAddressBottomSheetConfig.kt
+++ core/ui/src/main/java/com/tangem/core/ui/components/bottomsheets/chooseaddress/ChooseAddressBottomSheetConfig.kt
@@ -2,8 +2,9 @@ package com.tangem.core.ui.components.bottomsheets.chooseaddress
 
 import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfigContent
 import com.tangem.core.ui.components.bottomsheets.tokenreceive.AddressModel
+import kotlinx.collections.immutable.ImmutableList
 
 class ChooseAddressBottomSheetConfig(
-    val addressModels: List<AddressModel>,
+    val addressModels: ImmutableList<AddressModel>,
     val onClick: (AddressModel) -> Unit,
 ) : TangemBottomSheetConfigContent
