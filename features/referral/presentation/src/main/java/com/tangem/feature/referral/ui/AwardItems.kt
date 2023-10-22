f1adf8dcaa0eb67606986e3f223e342e743bb721
Tangem <git@tangem.com>
2023-10-22 22:12:21 +0800

    Updated on 2025-12-05

--- features/referral/presentation/src/main/java/com/tangem/feature/referral/ui/AwardItems.kt
+++ features/referral/presentation/src/main/java/com/tangem/feature/referral/ui/AwardItems.kt
@@ -1,8 +1,8 @@
 package com.tangem.feature.referral.ui
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
