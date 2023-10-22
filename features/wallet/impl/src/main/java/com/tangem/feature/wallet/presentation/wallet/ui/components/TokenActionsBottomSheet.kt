c9cf5757d0106c482e06ccc53fb7a312ab88f809
Tangem <git@tangem.com>
2023-10-22 09:55:02 +0800

    Updated on 2026-09-01

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/components/TokenActionsBottomSheet.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/components/TokenActionsBottomSheet.kt
@@ -2,16 +2,14 @@ package com.tangem.feature.wallet.presentation.wallet.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.BottomSheetDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.datasource.CollectionPreviewParameterProvider
 import com.tangem.core.ui.components.SimpleSettingsRow
+import com.tangem.core.ui.components.bottomsheets.TangemBottomSheet
+import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfig
 import com.tangem.core.ui.extensions.resolveReference
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.feature.wallet.presentation.common.WalletPreviewData
@@ -19,24 +17,16 @@ import com.tangem.feature.wallet.presentation.wallet.state.ActionsBottomSheetCon
 import com.tangem.feature.wallet.presentation.wallet.state.TokenActionButtonConfig
 import kotlinx.collections.immutable.ImmutableList
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-internal fun TokenActionsBottomSheet(config: ActionsBottomSheetConfig) {
-    ModalBottomSheet(
-        onDismissRequest = config.onDismissRequest,
-        sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true),
-        containerColor = TangemTheme.colors.background.primary,
-        dragHandle = { BottomSheetDefaults.DragHandle() },
-    ) {
-        ActionsBottomSheetContent(config.actions)
+internal fun TokenActionsBottomSheet(config: TangemBottomSheetConfig) {
+    TangemBottomSheet<ActionsBottomSheetConfig>(config = config) {
+        ActionsBottomSheetContent(actions = it.actions)
     }
 }
 
 @Composable
 private fun ActionsBottomSheetContent(actions: ImmutableList<TokenActionButtonConfig>) {
-    Column(
-        modifier = Modifier.background(TangemTheme.colors.background.primary),
-    ) {
+    Column(modifier = Modifier.background(TangemTheme.colors.background.primary)) {
         actions.forEach { action ->
             if (action.enabled) {
                 SimpleSettingsRow(
