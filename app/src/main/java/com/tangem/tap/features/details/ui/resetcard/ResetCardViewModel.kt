fe31827cbfaaf797d1c9f6062560241290ce7a2d
Tangem <git@tangem.com>
2023-10-23 15:10:39 +0300

    release/5.26.1

--- app/src/main/java/com/tangem/tap/features/details/ui/resetcard/ResetCardViewModel.kt
+++ app/src/main/java/com/tangem/tap/features/details/ui/resetcard/ResetCardViewModel.kt
@@ -1,5 +1,8 @@
 package com.tangem.tap.features.details.ui.resetcard
 
+import com.tangem.domain.common.util.cardTypesResolver
+import com.tangem.domain.models.scan.CardDTO
+import com.tangem.domain.models.scan.ScanResponse
 import com.tangem.tap.common.redux.AppState
 import com.tangem.tap.features.details.redux.CardSettingsState
 import com.tangem.tap.features.details.redux.DetailsAction
@@ -9,14 +12,28 @@ import org.rekotlin.Store
 
 internal class ResetCardViewModel(private val store: Store<AppState>) {
 
-    fun updateState(state: CardSettingsState?): ResetCardScreenState {
+    fun updateState(
+        scanResponse: ScanResponse,
+        state: CardSettingsState?,
+    ): ResetCardScreenState.ResetCardScreenContent {
         val descriptionText = state?.cardInfo
             ?.toResetCardDescriptionText()
             ?: TextReference.Str(value = "")
 
-        return ResetCardScreenState(
+        val warningsToShow = buildList {
+            add(ResetCardScreenState.WarningsToReset.LOST_WALLET_ACCESS)
+
+            val cardTypesResolver = scanResponse.cardTypesResolver
+            val isTangemWallet = cardTypesResolver.isTangemWallet() || cardTypesResolver.isWallet2()
+            if (isTangemWallet && state?.card?.backupStatus is CardDTO.BackupStatus.Active) {
+                add(ResetCardScreenState.WarningsToReset.LOST_PASSWORD_RESTORE)
+            }
+        }
+
+        return ResetCardScreenState.ResetCardScreenContent(
             accepted = state?.resetButtonEnabled ?: false,
             descriptionText = descriptionText,
+            warningsToShow = warningsToShow,
             acceptCondition1Checked = state?.condition1Checked ?: false,
             acceptCondition2Checked = state?.condition2Checked ?: false,
             onAcceptCondition1ToggleClick = { store.dispatch(DetailsAction.ResetToFactory.AcceptCondition1(it)) },
