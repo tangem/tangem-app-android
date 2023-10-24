72198aae5fcc8aa1a3e366448a67e98f1e182988
Tangem <git@tangem.com>
2023-10-24 14:26:49 +0400

    Updated on 2026-04-08

--- app/src/main/java/com/tangem/tap/domain/walletconnect2/app/TangemWcBlockchainHelper.kt
+++ app/src/main/java/com/tangem/tap/domain/walletconnect2/app/TangemWcBlockchainHelper.kt
@@ -1,23 +1,28 @@
 package com.tangem.tap.domain.walletconnect2.app
 
-import com.tangem.blockchain.common.*
-import com.tangem.domain.common.extensions.*
-import com.tangem.tap.domain.walletconnect2.domain.*
+import com.tangem.blockchain.common.Blockchain
+import com.tangem.domain.common.extensions.fromNetworkId
+import com.tangem.domain.common.extensions.toNetworkId
+import com.tangem.tap.domain.walletconnect2.domain.WcBlockchainHelper
 
 class TangemWcBlockchainHelper : WcBlockchainHelper {
     override fun chainIdToNetworkIdOrNull(chainId: String): String? {
-        val parsed = chainId.split(CHAIN_SEPARATOR)
-        if (parsed.size != 2) return null
-        when {
-            parsed.first() == EVM_NAMESPACE -> {
-                val chainIdInt = parsed[1].toIntOrNull() ?: return null
-                return Blockchain.fromChainId(chainIdInt)?.toNetworkId()
+        val parsedId = chainId.parseId() ?: return null
+        val blockchain = parsedId.chainIdToBlockchain()
+
+        return blockchain?.toNetworkId()
+    }
+
+    override fun chainIdToMissingNetworkNameOrNull(chainId: String): String? {
+        val parsedId = chainId.parseId() ?: return null
+        val blockchain = parsedId.chainIdToBlockchain()
+
+        return blockchain?.fullName
+            ?: if (parsedId.first == EVM_NAMESPACE) {
+                chainId
+            } else {
+                parsedId.first.replaceFirstChar(Char::titlecase)
             }
-            else -> {
-                val blockchain = Blockchain.fromNetworkId(parsed.first())
-                return if (supportedNonEvmBlockchains.contains(blockchain)) blockchain?.toNetworkId() else null
-            }
-        }
     }
 
     override fun networkIdToChainIdOrNull(networkId: String): String? {
@@ -54,6 +59,26 @@ private fun Blockchain.getCaip2Namespace(): String? {
         this.isEvm() -> EVM_NAMESPACE
         supportedNonEvmBlockchains.contains(this) -> this.toNetworkId()
         else -> null
+    }
+}
+
+private fun String.parseId(): Pair<String, String>? {
+    val parsed = this.split(CHAIN_SEPARATOR)
+    if (parsed.size != 2) return null
+
+    return parsed[0] to parsed[1]
+}
+
+private fun Pair<String, String>.chainIdToBlockchain(): Blockchain? {
+    return when (first) {
+        EVM_NAMESPACE -> {
+            second.toIntOrNull()
+                ?.let(Blockchain::fromChainId)
+        }
+        else -> {
+            Blockchain.fromNetworkId(networkId = first)
+                .takeIf(supportedNonEvmBlockchains::contains)
+        }
     }
 }
 
