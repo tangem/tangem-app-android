c2ccf2debb894656e840534483053b6cf9d2e080
Tangem <git@tangem.com>
2023-10-25 12:09:02 +0500

    Updated on 2026-02-25

--- app/src/main/java/com/tangem/tap/di/domain/WalletManagersFacadeModule.kt
+++ app/src/main/java/com/tangem/tap/di/domain/WalletManagersFacadeModule.kt
@@ -1,6 +1,9 @@
 package com.tangem.tap.di.domain
 
+import android.content.Context
+import com.squareup.moshi.Moshi
 import com.tangem.datasource.config.ConfigManager
+import com.tangem.datasource.di.SdkMoshi
 import com.tangem.datasource.local.userwallet.UserWalletsStore
 import com.tangem.datasource.local.walletmanager.WalletManagersStore
 import com.tangem.domain.walletmanager.DefaultWalletManagersFacade
@@ -8,6 +11,7 @@ import com.tangem.domain.walletmanager.WalletManagersFacade
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -21,7 +25,15 @@ internal object WalletManagersFacadeModule {
         walletManagersStore: WalletManagersStore,
         userWalletsStore: UserWalletsStore,
         configManager: ConfigManager,
+        @ApplicationContext appContext: Context,
+        @SdkMoshi moshi: Moshi,
     ): WalletManagersFacade {
-        return DefaultWalletManagersFacade(walletManagersStore, userWalletsStore, configManager)
+        return DefaultWalletManagersFacade(
+            walletManagersStore = walletManagersStore,
+            userWalletsStore = userWalletsStore,
+            configManager = configManager,
+            assetManager = appContext.assets,
+            moshi = moshi,
+        )
     }
 }
