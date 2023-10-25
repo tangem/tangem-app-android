d00aff07904ff34b0b397ebd1100ef2f611dfea8
Tangem <git@tangem.com>
2023-10-25 12:09:02 +0500

    Updated on 2026-05-14

--- features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/factory/txhistory/TokenDetailsTxHistoryToTransactionStateConverter.kt
+++ features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/factory/txhistory/TokenDetailsTxHistoryToTransactionStateConverter.kt
@@ -2,6 +2,9 @@ package com.tangem.feature.tokendetails.presentation.tokendetails.state.factory.
 
 import com.tangem.core.ui.components.transactions.state.TransactionState
 import com.tangem.core.ui.extensions.TextReference
+import com.tangem.core.ui.extensions.resourceReference
+import com.tangem.core.ui.extensions.stringReference
+import com.tangem.core.ui.extensions.wrappedList
 import com.tangem.core.ui.utils.DateTimeFormatters
 import com.tangem.domain.txhistory.models.TxHistoryItem
 import com.tangem.feature.tokendetails.presentation.tokendetails.viewmodels.TokenDetailsClickIntents
@@ -22,121 +25,65 @@ internal class TokenDetailsTxHistoryToTransactionStateConverter(
         return createTransactionStateItem(item = value)
     }
 
-    // TODO: Finalize transaction types [REDACTED_JIRA]
-    @Suppress("LongMethod")
     private fun createTransactionStateItem(item: TxHistoryItem): TransactionState {
-        return when (val type = item.type) {
-            TxHistoryItem.TransactionType.Transfer -> mapTransfer(item)
-            TxHistoryItem.TransactionType.Approve -> mapApprove(item)
-            TxHistoryItem.TransactionType.Swap -> mapSwap(item)
-            TxHistoryItem.TransactionType.Deposit -> TransactionState.Custom(
-                txHash = item.txHash,
-                address = item.direction.extractAddress(),
-                amount = item.getAmount(),
-                timestamp = item.timestampInMillis.toTimeFormat(),
-                status = item.status.tiUiStatus(),
-                direction = item.direction.toUiDirection(),
-                title = TextReference.Str("Deposit"),
-                subtitle = item.direction.extractAddress(),
-                onClick = { clickIntents.onTransactionClick(item.txHash) },
-            )
-            TxHistoryItem.TransactionType.Submit -> TransactionState.Custom(
-                txHash = item.txHash,
-                address = item.direction.extractAddress(),
-                amount = item.getAmount(),
-                timestamp = item.timestampInMillis.toTimeFormat(),
-                status = item.status.tiUiStatus(),
-                direction = item.direction.toUiDirection(),
-                title = TextReference.Str("Submit"),
-                subtitle = item.direction.extractAddress(),
-                onClick = { clickIntents.onTransactionClick(item.txHash) },
-            )
-            TxHistoryItem.TransactionType.Supply -> TransactionState.Custom(
-                txHash = item.txHash,
-                address = item.direction.extractAddress(),
-                amount = item.getAmount(),
-                timestamp = item.timestampInMillis.toTimeFormat(),
-                status = item.status.tiUiStatus(),
-                direction = item.direction.toUiDirection(),
-                title = TextReference.Str("Supply"),
-                subtitle = item.direction.extractAddress(),
-                onClick = { clickIntents.onTransactionClick(item.txHash) },
-            )
-            TxHistoryItem.TransactionType.Unoswap -> TransactionState.Custom(
-                txHash = item.txHash,
-                address = item.direction.extractAddress(),
-                amount = item.getAmount(),
-                timestamp = item.timestampInMillis.toTimeFormat(),
-                status = item.status.tiUiStatus(),
-                direction = item.direction.toUiDirection(),
-                title = TextReference.Str("Unoswap"),
-                subtitle = item.direction.extractAddress(),
-                onClick = { clickIntents.onTransactionClick(item.txHash) },
-            )
-            TxHistoryItem.TransactionType.Withdraw -> TransactionState.Custom(
-                txHash = item.txHash,
-                address = item.direction.extractAddress(),
-                amount = item.getAmount(),
-                timestamp = item.timestampInMillis.toTimeFormat(),
-                status = item.status.tiUiStatus(),
-                direction = item.direction.toUiDirection(),
-                title = TextReference.Str("Withdraw"),
-                subtitle = item.direction.extractAddress(),
-                onClick = { clickIntents.onTransactionClick(item.txHash) },
-            )
-            is TxHistoryItem.TransactionType.Custom -> TransactionState.Custom(
-                txHash = item.txHash,
-                address = item.direction.extractAddress(),
-                amount = item.getAmount(),
-                timestamp = item.timestampInMillis.toTimeFormat(),
-                status = item.status.tiUiStatus(),
-                direction = item.direction.toUiDirection(),
-                title = TextReference.Str(type.id),
-                subtitle = item.direction.extractAddress(),
-                onClick = { clickIntents.onTransactionClick(item.txHash) },
-            )
+        return TransactionState.Content(
+            txHash = item.txHash,
+            address = stringReference("mocked address"),
+            amount = item.getAmount(),
+            timestamp = item.timestampInMillis.toTimeFormat(),
+            status = item.status.tiUiStatus(),
+            direction = item.extractDirection(),
+            iconRes = item.extractIcon(),
+            title = item.extractTitle(),
+            subtitle = item.extractSubtitle(),
+            onClick = { clickIntents.onTransactionClick(item.txHash) },
+        )
+    }
+
+    private fun TxHistoryItem.extractIcon(): Int = if (status == TxHistoryItem.TransactionStatus.Failed) {
+        R.drawable.ic_close_24
+    } else {
+        when (type) {
+            is TxHistoryItem.TransactionType.Approve -> R.drawable.ic_doc_24
+            is TxHistoryItem.TransactionType.Operation,
+            is TxHistoryItem.TransactionType.Swap,
+            is TxHistoryItem.TransactionType.Transfer,
+            is TxHistoryItem.TransactionType.UnknownOperation,
+            -> if (isOutgoing) R.drawable.ic_arrow_up_24 else R.drawable.ic_arrow_down_24
         }
     }
 
-    private fun mapTransfer(item: TxHistoryItem): TransactionState {
-        return TransactionState.Transfer(
-            txHash = item.txHash,
-            address = item.direction.extractAddress(),
-            amount = item.getAmount(),
-            timestamp = item.timestampInMillis.toTimeFormat(),
-            status = item.status.tiUiStatus(),
-            direction = item.direction.toUiDirection(),
-            onClick = { clickIntents.onTransactionClick(item.txHash) },
-        )
+    private fun TxHistoryItem.extractTitle(): TextReference = when (val type = type) {
+        is TxHistoryItem.TransactionType.Approve -> resourceReference(R.string.common_approval)
+        is TxHistoryItem.TransactionType.Operation -> stringReference(type.name)
+        is TxHistoryItem.TransactionType.Swap -> resourceReference(R.string.common_swap)
+        is TxHistoryItem.TransactionType.Transfer -> resourceReference(R.string.common_transfer)
+        is TxHistoryItem.TransactionType.UnknownOperation -> resourceReference(R.string.transaction_history_operation)
     }
 
-    private fun mapApprove(item: TxHistoryItem): TransactionState {
-        return TransactionState.Approve(
-            txHash = item.txHash,
-            address = item.direction.extractAddress(),
-            amount = item.getAmount(),
-            timestamp = item.timestampInMillis.toTimeFormat(),
-            status = item.status.tiUiStatus(),
-            direction = item.direction.toUiDirection(),
-            onClick = { clickIntents.onTransactionClick(item.txHash) },
-        )
-    }
-    private fun mapSwap(item: TxHistoryItem): TransactionState {
-        return TransactionState.Swap(
-            txHash = item.txHash,
-            address = item.direction.extractAddress(),
-            amount = item.getAmount(),
-            timestamp = item.timestampInMillis.toTimeFormat(),
-            status = item.status.tiUiStatus(),
-            direction = item.direction.toUiDirection(),
-            onClick = { clickIntents.onTransactionClick(item.txHash) },
-        )
-    }
-
-    private fun TxHistoryItem.TransactionDirection.extractAddress(): TextReference = when (val addr = address) {
-        TxHistoryItem.Address.Multiple -> TextReference.Res(R.string.transaction_history_multiple_addresses)
-        is TxHistoryItem.Address.Single -> TextReference.Str(addr.rawAddress.toBriefAddressFormat())
-    }
+    private fun TxHistoryItem.extractSubtitle(): TextReference =
+        when (val interactionAddress = interactionAddressType) {
+            is TxHistoryItem.InteractionAddressType.Contract -> resourceReference(
+                id = R.string.transaction_history_contract_address,
+                formatArgs = wrappedList(interactionAddress.address.toBriefAddressFormat()),
+            )
+            is TxHistoryItem.InteractionAddressType.Multiple -> resourceReference(
+                id = if (isOutgoing) {
+                    R.string.transaction_history_transaction_to_address
+                } else {
+                    R.string.transaction_history_transaction_from_address
+                },
+                formatArgs = wrappedList(resourceReference(R.string.transaction_history_multiple_addresses)),
+            )
+            is TxHistoryItem.InteractionAddressType.User -> resourceReference(
+                id = if (isOutgoing) {
+                    R.string.transaction_history_transaction_to_address
+                } else {
+                    R.string.transaction_history_transaction_from_address
+                },
+                formatArgs = wrappedList(interactionAddress.address.toBriefAddressFormat()),
+            )
+        }
 
     private fun TxHistoryItem.TransactionStatus.tiUiStatus() = when (this) {
         TxHistoryItem.TransactionStatus.Confirmed -> TransactionState.Content.Status.Confirmed
@@ -148,16 +95,11 @@ internal class TokenDetailsTxHistoryToTransactionStateConverter(
         return DateTimeFormatters.formatTime(time = DateTime(this, DateTimeZone.getDefault()))
     }
 
-    private fun TxHistoryItem.TransactionDirection.toUiDirection() = when (this) {
-        is TxHistoryItem.TransactionDirection.Incoming -> TransactionState.Content.Direction.INCOMING
-        is TxHistoryItem.TransactionDirection.Outgoing -> TransactionState.Content.Direction.OUTGOING
-    }
+    private fun TxHistoryItem.extractDirection() =
+        if (isOutgoing) TransactionState.Content.Direction.OUTGOING else TransactionState.Content.Direction.INCOMING
 
     private fun TxHistoryItem.getAmount(): String {
-        val prefix = when (direction) {
-            is TxHistoryItem.TransactionDirection.Incoming -> "+"
-            is TxHistoryItem.TransactionDirection.Outgoing -> "-"
-        }
+        val prefix = if (isOutgoing) "-" else "+"
         return prefix + amount.toFormattedCurrencyString(currency = symbol, decimals = decimals)
     }
 }
