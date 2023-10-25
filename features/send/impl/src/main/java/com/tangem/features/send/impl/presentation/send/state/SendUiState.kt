286d0406d8272c46628ea1172c5f9f74a343a57e
Tangem <git@tangem.com>
2023-10-25 19:44:25 +0300

    release/5.26.1

--- features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/send/state/SendUiState.kt
+++ features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/send/state/SendUiState.kt
@@ -5,6 +5,7 @@ import com.tangem.core.ui.components.currency.tokenicon.TokenIconState
 import com.tangem.domain.appcurrency.model.AppCurrency
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.features.send.impl.presentation.send.state.fields.SendTextField
+import com.tangem.features.send.impl.presentation.send.viewmodel.SendClickIntents
 import kotlinx.collections.immutable.PersistentList
 
 /**
@@ -16,39 +17,51 @@ internal sealed class SendUiState {
     /** States with content */
     sealed class Content : SendUiState() {
 
-        abstract val nextButtonEnabled: Boolean
+        /** Is primary button enabled */
+        abstract val isPrimaryButtonEnabled: Boolean
+
+        /** Click intents */
+        abstract val clickIntents: SendClickIntents
 
         /** Initial state */
         data class Initial(
-            override val nextButtonEnabled: Boolean = false,
+            override val isPrimaryButtonEnabled: Boolean = false,
+            override val clickIntents: SendClickIntents,
         ) : Content()
 
         /** Amount state */
         data class AmountState(
-            override val nextButtonEnabled: Boolean = false,
+            override val isPrimaryButtonEnabled: Boolean = false,
+            override val clickIntents: SendClickIntents,
             val walletName: String,
             val walletBalance: String,
             val tokenIconState: TokenIconState,
             val cryptoCurrencyStatus: CryptoCurrencyStatus,
             val appCurrency: AppCurrency,
             val isFiatValue: Boolean,
-            val segmentedButtonConfig: PersistentList<SendAmountSegmentedButtonConfig>,
+            val segmentedButtonConfig: PersistentList<SendAmountSegmentedButtonsConfig>,
             val amountTextField: SendTextField.Amount,
         ) : Content()
 // [REDACTED_TODO_COMMENT]
         /** Recipient state */
         data class RecipientState(
-            override val nextButtonEnabled: Boolean = false,
+            override val isPrimaryButtonEnabled: Boolean = false,
+            override val clickIntents: SendClickIntents,
         ) : Content()
 // [REDACTED_TODO_COMMENT]
         /** Fee and speed state */
         data class FeeState(
-            override val nextButtonEnabled: Boolean = false,
+            override val isPrimaryButtonEnabled: Boolean = false,
+            override val clickIntents: SendClickIntents,
         ) : Content()
 // [REDACTED_TODO_COMMENT]
         /** Send state */
         data class SendState(
-            override val nextButtonEnabled: Boolean = true,
+            override val isPrimaryButtonEnabled: Boolean = true,
+            override val clickIntents: SendClickIntents,
         ) : Content()
     }
+
+    /** Dismiss screen */
+    object Dismiss : SendUiState()
 }
--- features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/send/ui/SendScreen.kt
+++ features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/send/ui/SendScreen.kt
@@ -16,7 +16,7 @@ import com.tangem.core.ui.res.TangemTheme
 import com.tangem.features.send.impl.presentation.send.state.SendUiState
 
 @Composable
-fun SendScreen() {
+internal fun SendScreen(uiState: SendUiState.Content) {
     Column(
         modifier = Modifier
             .imePadding()
@@ -37,13 +37,17 @@ fun SendScreen() {
                 .weight(1f)
                 .scrollable(state = rememberScrollState(), orientation = Orientation.Vertical),
         ) {
-            SendScreenContent()
+            SendScreenContent(uiState)
         }
-        SendNavigationButtons(uiState = SendUiState.Content.Initial())
+        SendNavigationButtons(uiState)
     }
 }
 
 @Composable
-fun SendScreenContent() {
-// [REDACTED_TODO_COMMENT]
+private fun SendScreenContent(uiState: SendUiState.Content) {
+    when (uiState) {
+        is SendUiState.Content.AmountState -> SendAmountContent(uiState)
+        else -> { /*todo other states*/
+        }
+    }
 }
