bd7d4860b799587f8e676515a0a2e34567f7a05b
Tangem <git@tangem.com>
2023-10-25 12:09:02 +0500

    Updated on 2025-10-13

--- domain/legacy/src/main/java/com/tangem/domain/walletmanager/utils/SdkTransactionHistoryItemConverter.kt
+++ domain/legacy/src/main/java/com/tangem/domain/walletmanager/utils/SdkTransactionHistoryItemConverter.kt
@@ -1,44 +1,93 @@
 package com.tangem.domain.walletmanager.utils
 
+import android.content.res.AssetManager
+import com.squareup.moshi.Moshi
 import com.tangem.blockchain.common.txhistory.TransactionHistoryItem
 import com.tangem.domain.txhistory.models.TxHistoryItem
 import com.tangem.utils.converter.Converter
 import com.tangem.blockchain.common.txhistory.TransactionHistoryItem as SdkTransactionHistoryItem
 
-internal class SdkTransactionHistoryItemConverter : Converter<SdkTransactionHistoryItem, TxHistoryItem> {
+internal class SdkTransactionHistoryItemConverter(
+    assetManager: AssetManager,
+    moshi: Moshi,
+) : Converter<SdkTransactionHistoryItem, TxHistoryItem> {
+
+    private val typeConverter by lazy {
+        SdkTransactionTypeConverter(assetManager, moshi)
+    }
 
     override fun convert(value: SdkTransactionHistoryItem): TxHistoryItem = TxHistoryItem(
         txHash = value.txHash,
         timestampInMillis = value.timestamp,
-        direction = value.direction.toDomain(),
+        isOutgoing = value.isOutgoing,
+        destinationType = value.destinationType.toDomain(),
+        sourceType = value.sourceType.toDomain(),
+        interactionAddressType = value.extractInteractionAddressType(),
         status = when (value.status) {
-            TransactionHistoryItem.TransactionStatus.Confirmed -> TxHistoryItem.TransactionStatus.Confirmed
-            TransactionHistoryItem.TransactionStatus.Failed -> TxHistoryItem.TransactionStatus.Failed
-            TransactionHistoryItem.TransactionStatus.Unconfirmed -> TxHistoryItem.TransactionStatus.Unconfirmed
+            SdkTransactionHistoryItem.TransactionStatus.Confirmed -> TxHistoryItem.TransactionStatus.Confirmed
+            SdkTransactionHistoryItem.TransactionStatus.Failed -> TxHistoryItem.TransactionStatus.Failed
+            SdkTransactionHistoryItem.TransactionStatus.Unconfirmed -> TxHistoryItem.TransactionStatus.Unconfirmed
         },
-        type = when (val type = value.type) {
-            TransactionHistoryItem.TransactionType.Transfer -> TxHistoryItem.TransactionType.Transfer
-            TransactionHistoryItem.TransactionType.Approve -> TxHistoryItem.TransactionType.Approve
-            is TransactionHistoryItem.TransactionType.Custom -> TxHistoryItem.TransactionType.Custom(type.id)
-            TransactionHistoryItem.TransactionType.Deposit -> TxHistoryItem.TransactionType.Deposit
-            TransactionHistoryItem.TransactionType.Submit -> TxHistoryItem.TransactionType.Submit
-            TransactionHistoryItem.TransactionType.Supply -> TxHistoryItem.TransactionType.Supply
-            TransactionHistoryItem.TransactionType.Swap -> TxHistoryItem.TransactionType.Swap
-            TransactionHistoryItem.TransactionType.Unoswap -> TxHistoryItem.TransactionType.Unoswap
-            TransactionHistoryItem.TransactionType.Withdraw -> TxHistoryItem.TransactionType.Withdraw
-        },
+        type = typeConverter.convert(value.type),
         amount = requireNotNull(value.amount.value) { "Transaction amount value must not be null" },
     )
 
-    private fun TransactionHistoryItem.TransactionDirection.toDomain() = when (this) {
-        is TransactionHistoryItem.TransactionDirection.Incoming ->
-            TxHistoryItem.TransactionDirection.Incoming(address.toDomain())
-        is TransactionHistoryItem.TransactionDirection.Outgoing ->
-            TxHistoryItem.TransactionDirection.Outgoing(address.toDomain())
+    private fun SdkTransactionHistoryItem.SourceType.toDomain(): TxHistoryItem.SourceType = when (this) {
+        is TransactionHistoryItem.SourceType.Single -> TxHistoryItem.SourceType.Single(address)
+        is TransactionHistoryItem.SourceType.Multiple -> TxHistoryItem.SourceType.Multiple(addresses)
     }
 
-    private fun TransactionHistoryItem.Address.toDomain(): TxHistoryItem.Address = when (this) {
-        TransactionHistoryItem.Address.Multiple -> TxHistoryItem.Address.Multiple
-        is TransactionHistoryItem.Address.Single -> TxHistoryItem.Address.Single(rawAddress = rawAddress)
+    private fun SdkTransactionHistoryItem.DestinationType.toDomain(): TxHistoryItem.DestinationType = when (this) {
+        is SdkTransactionHistoryItem.DestinationType.Single -> TxHistoryItem.DestinationType.Single(
+            addressType.toDomain(),
+        )
+        is SdkTransactionHistoryItem.DestinationType.Multiple -> TxHistoryItem.DestinationType.Multiple(
+            addressTypes.map { it.toDomain() },
+        )
+    }
+
+    private fun SdkTransactionHistoryItem.AddressType.toDomain(): TxHistoryItem.AddressType = when (this) {
+        is SdkTransactionHistoryItem.AddressType.Contract -> TxHistoryItem.AddressType.Contract(address)
+        is SdkTransactionHistoryItem.AddressType.User -> TxHistoryItem.AddressType.User(address)
+    }
+
+    private fun SdkTransactionHistoryItem.extractInteractionAddressType(): TxHistoryItem.InteractionAddressType {
+        return when (type) {
+            SdkTransactionHistoryItem.TransactionType.Transfer -> if (isOutgoing) {
+                mapToInteractionAddressType(destinationType = destinationType)
+            } else {
+                mapToInteractionAddressType(sourceType = sourceType)
+            }
+            is SdkTransactionHistoryItem.TransactionType.ContractMethod -> mapToInteractionAddressType(destinationType)
+        }
+    }
+
+    private fun mapToInteractionAddressType(
+        destinationType: SdkTransactionHistoryItem.DestinationType,
+    ): TxHistoryItem.InteractionAddressType {
+        return when (destinationType) {
+            is TransactionHistoryItem.DestinationType.Multiple -> TxHistoryItem.InteractionAddressType.Multiple(
+                destinationType.addressTypes.map { it.address },
+            )
+            is TransactionHistoryItem.DestinationType.Single -> when (destinationType.addressType) {
+                is TransactionHistoryItem.AddressType.Contract -> TxHistoryItem.InteractionAddressType.Contract(
+                    destinationType.addressType.address,
+                )
+                is TransactionHistoryItem.AddressType.User -> TxHistoryItem.InteractionAddressType.User(
+                    destinationType.addressType.address,
+                )
+            }
+        }
+    }
+
+    private fun mapToInteractionAddressType(
+        sourceType: SdkTransactionHistoryItem.SourceType,
+    ): TxHistoryItem.InteractionAddressType {
+        return when (sourceType) {
+            is TransactionHistoryItem.SourceType.Multiple -> TxHistoryItem.InteractionAddressType.Multiple(
+                sourceType.addresses,
+            )
+            is TransactionHistoryItem.SourceType.Single -> TxHistoryItem.InteractionAddressType.User(sourceType.address)
+        }
     }
 }
