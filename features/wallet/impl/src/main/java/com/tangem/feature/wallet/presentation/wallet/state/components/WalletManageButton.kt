a83daf539af8836daf9ac04820439cfda4711e28
Tangem <git@tangem.com>
2023-10-30 21:18:27 +0800

    Updated on 2026-05-13

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/components/WalletManageButton.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/components/WalletManageButton.kt
@@ -15,6 +15,9 @@ import com.tangem.feature.wallet.impl.R
 @Immutable
 internal sealed class WalletManageButton(val config: ActionButtonConfig) {
 
+    /** Is click enabled */
+    abstract val enabled: Boolean
+
     /** Lambda be invoked when manage button is clicked */
     abstract val onClick: () -> Unit
 
@@ -24,7 +27,7 @@ internal sealed class WalletManageButton(val config: ActionButtonConfig) {
      * @property enabled button click availability
      * @property onClick lambda be invoked when Buy button is clicked
      */
-    data class Buy(val enabled: Boolean, override val onClick: () -> Unit) : WalletManageButton(
+    data class Buy(override val enabled: Boolean, override val onClick: () -> Unit) : WalletManageButton(
         config = ActionButtonConfig(
             text = TextReference.Res(id = R.string.common_buy),
             iconResId = R.drawable.ic_plus_24,
@@ -39,7 +42,7 @@ internal sealed class WalletManageButton(val config: ActionButtonConfig) {
      * @property enabled button click availability
      * @property onClick lambda be invoked when Send button is clicked
      */
-    data class Send(val enabled: Boolean, override val onClick: () -> Unit) : WalletManageButton(
+    data class Send(override val enabled: Boolean, override val onClick: () -> Unit) : WalletManageButton(
         config = ActionButtonConfig(
             text = TextReference.Res(id = R.string.common_send),
             iconResId = R.drawable.ic_arrow_up_24,
@@ -53,12 +56,12 @@ internal sealed class WalletManageButton(val config: ActionButtonConfig) {
      *
      * @property onClick lambda be invoked when Receive button is clicked
      */
-    data class Receive(override val onClick: () -> Unit) : WalletManageButton(
+    data class Receive(override val enabled: Boolean, override val onClick: () -> Unit) : WalletManageButton(
         config = ActionButtonConfig(
             text = TextReference.Res(id = R.string.common_receive),
             iconResId = R.drawable.ic_arrow_down_24,
             onClick = onClick,
-            enabled = true,
+            enabled = enabled,
         ),
     )
 
@@ -68,7 +71,7 @@ internal sealed class WalletManageButton(val config: ActionButtonConfig) {
      * @property enabled button click availability
      * @property onClick lambda be invoked when Sell button is clicked
      */
-    data class Sell(val enabled: Boolean, override val onClick: () -> Unit) : WalletManageButton(
+    data class Sell(override val enabled: Boolean, override val onClick: () -> Unit) : WalletManageButton(
         config = ActionButtonConfig(
             text = TextReference.Res(id = R.string.common_sell),
             iconResId = R.drawable.ic_currency_24,
@@ -83,7 +86,7 @@ internal sealed class WalletManageButton(val config: ActionButtonConfig) {
      * @property enabled button click availability
      * @property onClick lambda be invoked when Swap button is clicked
      */
-    data class Swap(val enabled: Boolean, override val onClick: () -> Unit) : WalletManageButton(
+    data class Swap(override val enabled: Boolean, override val onClick: () -> Unit) : WalletManageButton(
         config = ActionButtonConfig(
             text = TextReference.Res(id = R.string.common_swap),
             iconResId = R.drawable.ic_exchange_vertical_24,
