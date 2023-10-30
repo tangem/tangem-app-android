16bf5a5263938ae9cea04485f333a2a33381aef3
Tangem <git@tangem.com>
2023-10-30 18:31:31 +0500

    Updated on 2026-08-07

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/viewmodels/WalletViewModel.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/viewmodels/WalletViewModel.kt
@@ -819,18 +819,18 @@ internal class WalletViewModel @Inject constructor(
 
     private fun openExplorer() {
         val state = uiState as? WalletState.ContentState ?: return
-        val currencyNetwork = singleWalletCryptoCurrencyStatus?.currency?.network ?: return
+        val currency = singleWalletCryptoCurrencyStatus?.currency ?: return
 
         viewModelScope.launch(dispatchers.main) {
             val userWalletId = getWallet(state.walletsListConfig.selectedWalletIndex).walletId
 
-            val addresses = walletManagersFacade.getAddress(userWalletId = userWalletId, network = currencyNetwork)
+            val addresses = walletManagersFacade.getAddress(userWalletId = userWalletId, network = currency.network)
 
             if (addresses.size == 1) {
                 router.openUrl(
                     url = getExploreUrlUseCase(
                         userWalletId = userWalletId,
-                        network = currencyNetwork,
+                        currency = currency,
                         addressType = AddressType.Default,
                     ),
                 )
@@ -848,7 +848,7 @@ internal class WalletViewModel @Inject constructor(
                         onClick = {
                             onAddressTypeSelected(
                                 userWalletId = userWalletId,
-                                currencyNetwork = currencyNetwork,
+                                currency = currency,
                                 addressModel = it,
                             )
                         },
@@ -860,14 +860,14 @@ internal class WalletViewModel @Inject constructor(
 
     private fun onAddressTypeSelected(
         userWalletId: UserWalletId,
-        currencyNetwork: Network,
+        currency: CryptoCurrency,
         addressModel: AddressModel,
     ) {
         viewModelScope.launch(dispatchers.main) {
             router.openUrl(
                 url = getExploreUrlUseCase(
                     userWalletId = userWalletId,
-                    network = currencyNetwork,
+                    currency = currency,
                     addressType = AddressType.valueOf(addressModel.type.name),
                 ),
             )
