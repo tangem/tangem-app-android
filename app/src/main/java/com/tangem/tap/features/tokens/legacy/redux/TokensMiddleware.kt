3853f90da8986a5f3b832bd75e45864a2a198fbc
Tangem <git@tangem.com>
2023-10-30 23:32:10 +0800

    release/5.27.0

--- app/src/main/java/com/tangem/tap/features/tokens/legacy/redux/TokensMiddleware.kt
+++ app/src/main/java/com/tangem/tap/features/tokens/legacy/redux/TokensMiddleware.kt
@@ -93,11 +93,9 @@ object TokensMiddleware {
                         updatedScanResponse = it,
                         currencyList = currencyList,
                     )
-                    store.dispatchOnMain(NavigationAction.PopBackTo())
                 }
             } else {
                 submitNewAdd(userWalletId = action.userWallet.walletId, scanResponse, currencyList = currencyList)
-                store.dispatchOnMain(NavigationAction.PopBackTo())
             }
         }
     }
@@ -388,6 +386,8 @@ object TokensMiddleware {
             )
 
             addCryptoCurrenciesUseCase(userWalletId, currencyList)
+
+            store.dispatchOnMain(NavigationAction.PopBackTo())
         }
     }
 
