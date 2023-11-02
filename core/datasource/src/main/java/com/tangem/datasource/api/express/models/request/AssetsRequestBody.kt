9370c2b8d1d54685fb57b4e097a60d7ef68f4ff1
Tangem <git@tangem.com>
2023-11-02 18:50:03 +0200

    release/5.26.1

--- core/datasource/src/main/java/com/tangem/datasource/api/express/models/request/AssetsRequestBody.kt
+++ core/datasource/src/main/java/com/tangem/datasource/api/express/models/request/AssetsRequestBody.kt
@@ -1,8 +1,8 @@
-package com.tangem.feature.swap.models.request
+package com.tangem.datasource.api.express.models.request
 
 import com.squareup.moshi.Json
 
-internal data class AssetsRequestBody(
+data class AssetsRequestBody(
     @Json(name = "filter") val filter: List<LeastTokenInfo>?,
 )
 
