d05a6271de02ec73472ea86c2fdbfc49c607b355
Tangem <git@tangem.com>
2023-11-02 17:49:08 +0200

    Updated on 2026-08-14

---
+++ features/swap/data/src/main/java/com/tangem/feature/swap/models/response/ExchangeDataResponse.kt
@@ -0,0 +1,41 @@
+package com.tangem.feature.swap.models.response
+
+import com.squareup.moshi.Json
+import java.math.BigDecimal
+
+data class ExchangeDataResponse(
+    @Json(name = "toAmount")
+    val toAmount: BigDecimal,
+
+    @Json(name = "txType")
+    val txType: TxType,
+
+    @Json(name = "txId")
+    val txId: String, // inner tangem-express transaction id
+
+    @Json(name = "txFrom")
+    val txFrom: String?, // account for debiting tokens (same as toAddress) if DEX, null if CEX
+
+    @Json(name = "txTo")
+    val txTo: String, // swap smart-contract address if DEX, address for sending transaction if CEX
+
+    @Json(name = "txData")
+    val txData: String?, // transaction data if DEX, null if CEX
+
+    @Json(name = "txValue")
+    val txValue: BigDecimal, // amount (same as fromAmount)
+
+    @Json(name = "externalTxId")
+    val externalTxId: String?, // null if DEX, provider transaction id if CEX
+
+    @Json(name = "externalTxUrl")
+    val externalTxUrl: String?, // null if DEX, url of provider exchange status page if CEX
+)
+
+enum class TxType {
+    @Json(name = "send")
+    SEND,
+
+    @Json(name = "swap")
+    SWAP,
+}
