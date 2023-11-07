209116e3015a2531d4a1156a029dabbbd102590b
Tangem <git@tangem.com>
2023-11-07 12:54:49 +0200

    Updated on 2026-09-21

--- app/src/main/java/com/tangem/tap/network/auth/di/AuthModule.kt
+++ app/src/main/java/com/tangem/tap/network/auth/di/AuthModule.kt
@@ -1,7 +1,11 @@
 package com.tangem.tap.network.auth.di
 
+import com.tangem.datasource.config.ConfigManager
+import com.tangem.datasource.local.userwallet.UserWalletsStore
 import com.tangem.lib.auth.AuthProvider
+import com.tangem.lib.auth.ExpressAuthProvider
 import com.tangem.tap.network.auth.AuthProviderImpl
+import com.tangem.tap.network.auth.ExpressAuthProviderImpl
 import com.tangem.tap.proxy.AppStateHolder
 import dagger.Module
 import dagger.Provides
@@ -17,5 +21,17 @@ class AuthModule {
     @Singleton
     fun provideAuthProvider(appStateHolder: AppStateHolder): AuthProvider {
         return AuthProviderImpl(appStateHolder)
+    }
+
+    @Provides
+    @Singleton
+    fun provideExpressAuthProvider(
+        userWalletsStore: UserWalletsStore,
+        configManager: ConfigManager,
+    ): ExpressAuthProvider {
+        return ExpressAuthProviderImpl(
+            userWalletsStore = userWalletsStore,
+            configManager = configManager
+        )
     }
 }
