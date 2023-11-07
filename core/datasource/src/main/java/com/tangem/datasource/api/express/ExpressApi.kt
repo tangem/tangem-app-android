5fa25f477cfe361b4f9a922a208227877dbd4355
Tangem <git@tangem.com>
2023-11-07 14:27:16 +0200

    release/5.26.1

--- core/datasource/src/main/java/com/tangem/datasource/api/express/ExpressApi.kt
+++ core/datasource/src/main/java/com/tangem/datasource/api/express/ExpressApi.kt
@@ -16,7 +16,7 @@ import java.math.BigDecimal
  */
 @Suppress("LongParameterList")
 interface ExpressApi {
-// [REDACTED_TODO_COMMENT]
+    
     @POST("assets")
     suspend fun getAssets(
         @Body body: AssetsRequestBody,
