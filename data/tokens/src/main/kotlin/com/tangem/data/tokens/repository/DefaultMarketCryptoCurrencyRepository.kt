cb55e1ce53e34fc46014c6ba46468d437763d506
Tangem <git@tangem.com>
2023-11-09 14:42:49 +0200

    Updated on 2026-04-15

--- data/tokens/src/main/kotlin/com/tangem/data/tokens/repository/DefaultMarketCryptoCurrencyRepository.kt
+++ data/tokens/src/main/kotlin/com/tangem/data/tokens/repository/DefaultMarketCryptoCurrencyRepository.kt
@@ -1,22 +1,28 @@
 package com.tangem.data.tokens.repository
 
 import com.tangem.blockchain.common.Blockchain
-import com.tangem.datasource.local.token.UserMarketCoinsStore
+import com.tangem.datasource.api.express.models.TangemExpressValues.EMPTY_CONTRACT_ADDRESS_VALUE
+import com.tangem.datasource.local.token.AssetsStore
 import com.tangem.domain.common.extensions.toNetworkId
 import com.tangem.domain.tokens.model.CryptoCurrency
 import com.tangem.domain.tokens.repository.MarketCryptoCurrencyRepository
 import com.tangem.domain.wallets.models.UserWalletId
 
 class DefaultMarketCryptoCurrencyRepository(
-    private val userMarketCoinsStore: UserMarketCoinsStore,
+    private val assetsStore: AssetsStore,
 ) : MarketCryptoCurrencyRepository {
 
-    override suspend fun isExchangeable(userWalletId: UserWalletId, cryptoCurrencyId: CryptoCurrency.ID): Boolean {
+    override suspend fun isExchangeable(userWalletId: UserWalletId, cryptoCurrency: CryptoCurrency): Boolean {
+        val cryptoCurrencyId = cryptoCurrency.id
         val blockchain = Blockchain.fromId(cryptoCurrencyId.rawNetworkId)
         val apiNetworkId = blockchain.toNetworkId()
-        return userMarketCoinsStore.getSyncOrNull(userWalletId)?.coins
-            ?.firstOrNull { it.id == cryptoCurrencyId.rawCurrencyId }
-            ?.networks
-            ?.firstOrNull { it.networkId == apiNetworkId }?.exchangeable ?: false
+        val contractAddress = (cryptoCurrency as? CryptoCurrency.Token)?.contractAddress ?: EMPTY_CONTRACT_ADDRESS_VALUE
+
+        return assetsStore.getSyncOrNull(userWalletId)?.find {
+            it.network == apiNetworkId &&
+                it.token == cryptoCurrencyId.rawCurrencyId &&
+                it.contractAddress == contractAddress &&
+                it.isActive
+        }?.exchangeAvailable ?: false
     }
 }
