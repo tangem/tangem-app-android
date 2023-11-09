a833d01d9a47b924497de4ce2e595f9b157ff7d2
Tangem <git@tangem.com>
2023-11-09 13:44:23 +0200

    Updated on 2026-09-01

---
+++ core/datasource/src/main/java/com/tangem/datasource/api/express/models/response/ExchangeProvider.kt
@@ -0,0 +1,28 @@
+package com.tangem.datasource.api.express.models.response
+
+import com.squareup.moshi.Json
+
+data class ExchangeProvider(
+    @Json(name = "id")
+    val id: Int,
+
+    @Json(name = "name")
+    val name: String,
+
+    @Json(name = "id")
+    val type: ExchangeProviderType,
+
+    @Json(name = "imageLarge")
+    val imageLargeUrl: Int,
+
+    @Json(name = "imageSmall")
+    val imageSmallUrl: Int,
+)
+
+enum class ExchangeProviderType {
+    @Json(name = "dex")
+    DEX,
+
+    @Json(name = "cex")
+    CEX,
+}
