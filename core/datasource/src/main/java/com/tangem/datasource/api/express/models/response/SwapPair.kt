2effd3ba1a91ceb061a1f4359ec668fd710443de
Tangem <git@tangem.com>
2023-11-09 13:44:23 +0200

    Updated on 2026-07-23

---
+++ core/datasource/src/main/java/com/tangem/datasource/api/express/models/response/SwapPair.kt
@@ -0,0 +1,32 @@
+package com.tangem.datasource.api.express.models.response
+
+import com.squareup.moshi.Json
+import com.tangem.datasource.api.express.models.request.LeastTokenInfo
+
+data class SwapPair(
+    @Json(name = "from")
+    val from: LeastTokenInfo,
+
+    @Json(name = "to")
+    val to: LeastTokenInfo,
+
+    @Json(name = "providers")
+    val providers: List<SwapPairProvider>,
+
+)
+
+data class SwapPairProvider(
+    @Json(name = "providerId")
+    val providerId: Int,
+
+    @Json(name = "rateType")
+    val rateType: RateType,
+)
+
+enum class RateType {
+    @Json(name = "float")
+    FLOAT,
+
+    @Json(name = "fixed")
+    FIXED,
+}
