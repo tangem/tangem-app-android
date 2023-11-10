d972efcf78f617fe0e581814a8c97af1f68ade25
Tangem <git@tangem.com>
2023-11-10 21:52:01 +0300

    Updated on 2026-09-21

--- features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/state/amount/SendAmountStateConverter.kt
+++ features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/state/amount/SendAmountStateConverter.kt
@@ -1,64 +1,55 @@
 package com.tangem.features.send.impl.presentation.state.amount
 
-import arrow.core.Either
 import com.tangem.common.Provider
 import com.tangem.core.ui.components.currency.tokenicon.converter.CryptoCurrencyToIconStateConverter
 import com.tangem.core.ui.extensions.stringReference
 import com.tangem.core.ui.utils.BigDecimalFormatter.formatCryptoAmount
 import com.tangem.core.ui.utils.BigDecimalFormatter.formatFiatAmount
 import com.tangem.domain.appcurrency.model.AppCurrency
-import com.tangem.domain.tokens.error.CurrencyStatusError
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.domain.wallets.models.UserWallet
-import com.tangem.features.send.impl.presentation.state.SendUiState
+import com.tangem.features.send.impl.presentation.state.SendStates
 import com.tangem.features.send.impl.presentation.state.fields.SendAmountFieldConverter
-import com.tangem.features.send.impl.presentation.viewmodel.SendClickIntents
 import com.tangem.utils.converter.Converter
 import kotlinx.collections.immutable.persistentListOf
+import kotlinx.coroutines.flow.MutableStateFlow
 
 internal class SendAmountStateConverter(
-    private val currentStateProvider: Provider<SendUiState>,
     private val appCurrencyProvider: Provider<AppCurrency>,
-    private val userWalletProvider: Provider<UserWallet?>,
-    private val clickIntents: SendClickIntents,
+    private val userWalletProvider: Provider<UserWallet>,
+    private val cryptoCurrencyStatusProvider: Provider<CryptoCurrencyStatus>,
     private val iconStateConverter: CryptoCurrencyToIconStateConverter,
     private val sendAmountFieldConverter: SendAmountFieldConverter,
-) : Converter<Either<CurrencyStatusError, CryptoCurrencyStatus>, SendUiState> {
+) : Converter<Unit, SendStates.AmountState> {
 
-    override fun convert(value: Either<CurrencyStatusError, CryptoCurrencyStatus>): SendUiState {
-        val userWallet = userWalletProvider() ?: return currentStateProvider()
+    override fun convert(value: Unit): SendStates.AmountState {
+        val userWallet = userWalletProvider()
         val appCurrency = appCurrencyProvider()
-        return value.fold(
-            ifLeft = {
-                // TODO add error handling
-                currentStateProvider()
-            },
-            ifRight = {
-                val fiat = formatFiatAmount(it.value.fiatAmount, appCurrency.code, appCurrency.symbol)
-                val crypto = formatCryptoAmount(it.value.amount, it.currency.symbol, it.currency.decimals)
-                SendUiState.Content.AmountState(
-                    cryptoCurrencyStatus = it,
-                    walletName = userWallet.name,
-                    walletBalance = "$crypto ($fiat)",
-                    tokenIconState = iconStateConverter.convert(it),
-                    appCurrency = appCurrency,
-                    amountTextField = sendAmountFieldConverter.convert(Unit),
-                    isFiatValue = false,
-                    clickIntents = clickIntents,
-                    segmentedButtonConfig = persistentListOf(
-                        SendAmountSegmentedButtonsConfig(
-                            title = stringReference(it.currency.symbol),
-                            iconState = iconStateConverter.convert(it),
-                            isFiat = false,
-                        ),
-                        SendAmountSegmentedButtonsConfig(
-                            title = stringReference(appCurrency.code),
-                            iconState = iconStateConverter.convert(it),
-                            isFiat = true,
-                        ),
-                    ),
-                )
-            },
+        val status = cryptoCurrencyStatusProvider()
+        val fiat = formatFiatAmount(status.value.fiatAmount, appCurrency.code, appCurrency.symbol)
+        val crypto = formatCryptoAmount(status.value.amount, status.currency.symbol, status.currency.decimals)
+
+        return SendStates.AmountState(
+            appCurrency = appCurrency,
+            cryptoCurrencyStatus = status,
+            walletName = userWallet.name,
+            walletBalance = "$crypto ($fiat)",
+            tokenIconState = iconStateConverter.convert(status),
+            amountTextField = MutableStateFlow(sendAmountFieldConverter.convert(Unit)),
+            isFiatValue = false,
+            isPrimaryButtonEnabled = false,
+            segmentedButtonConfig = persistentListOf(
+                SendAmountSegmentedButtonsConfig(
+                    title = stringReference(status.currency.symbol),
+                    iconState = iconStateConverter.convert(status),
+                    isFiat = false,
+                ),
+                SendAmountSegmentedButtonsConfig(
+                    title = stringReference(appCurrency.code),
+                    iconState = iconStateConverter.convert(status),
+                    isFiat = true,
+                ),
+            ),
         )
     }
 }
