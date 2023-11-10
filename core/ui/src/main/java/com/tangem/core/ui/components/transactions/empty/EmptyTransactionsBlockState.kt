20482394fe24ef3fad31665d138eb7903eab1f7d
Tangem <git@tangem.com>
2023-11-10 13:49:50 +0500

    Updated on 2026-08-07

--- core/ui/src/main/java/com/tangem/core/ui/components/transactions/empty/EmptyTransactionsBlockState.kt
+++ core/ui/src/main/java/com/tangem/core/ui/components/transactions/empty/EmptyTransactionsBlockState.kt
@@ -5,42 +5,64 @@ import com.tangem.core.ui.components.buttons.actions.ActionButtonConfig
 import com.tangem.core.ui.extensions.TextReference
 
 sealed class EmptyTransactionsBlockState(
-    val actionButtonConfig: ActionButtonConfig? = null,
+    val buttonsState: ButtonsState,
     val iconRes: Int,
     val text: TextReference,
-    val onExploreClick: (() -> Unit)?,
 ) {
 
+    sealed class ButtonsState {
+        data class SingleButton(val actionButtonConfig: ActionButtonConfig) : ButtonsState()
+        data class PairButtons(
+            val firstButtonConfig: ActionButtonConfig,
+            val secondButtonConfig: ActionButtonConfig,
+        ) : ButtonsState()
+    }
+
     data class FailedToLoad(
-        val onClick: () -> Unit,
+        val onReload: () -> Unit,
         val onExplore: () -> Unit,
     ) : EmptyTransactionsBlockState(
-        actionButtonConfig = ActionButtonConfig(
-            text = TextReference.Res(R.string.common_reload),
-            iconResId = R.drawable.ic_refresh_24,
-            onClick = onClick,
-            enabled = true,
+        buttonsState = ButtonsState.PairButtons(
+            firstButtonConfig = ActionButtonConfig(
+                text = TextReference.Res(R.string.common_reload),
+                iconResId = R.drawable.ic_refresh_24,
+                onClick = onReload,
+                enabled = true,
+            ),
+            secondButtonConfig = ActionButtonConfig(
+                text = TextReference.Res(R.string.common_explore),
+                iconResId = R.drawable.ic_arrow_top_right_24,
+                onClick = onExplore,
+                enabled = true,
+            ),
         ),
         iconRes = R.drawable.ic_alert_history_64,
         text = TextReference.Res(R.string.transaction_history_error_failed_to_load),
-        onExploreClick = onExplore,
     )
 
     data class Empty(val onExplore: (() -> Unit)) : EmptyTransactionsBlockState(
+        buttonsState = ButtonsState.SingleButton(
+            actionButtonConfig = ActionButtonConfig(
+                text = TextReference.Res(R.string.common_explore),
+                iconResId = R.drawable.ic_arrow_top_right_24,
+                onClick = onExplore,
+                enabled = true,
+            ),
+        ),
         iconRes = R.drawable.ic_empty_token_64,
         text = TextReference.Res(R.string.transaction_history_empty_transactions),
-        onExploreClick = onExplore,
     )
 
-    data class NotImplemented(val onClick: () -> Unit) : EmptyTransactionsBlockState(
-        actionButtonConfig = ActionButtonConfig(
-            text = TextReference.Res(R.string.common_explore_transaction_history),
-            iconResId = R.drawable.ic_arrow_top_right_24,
-            onClick = onClick,
-            enabled = true,
+    data class NotImplemented(val onExplore: () -> Unit) : EmptyTransactionsBlockState(
+        buttonsState = ButtonsState.SingleButton(
+            actionButtonConfig = ActionButtonConfig(
+                text = TextReference.Res(R.string.common_explore_transaction_history),
+                iconResId = R.drawable.ic_arrow_top_right_24,
+                onClick = onExplore,
+                enabled = true,
+            ),
         ),
         iconRes = R.drawable.ic_compass_64,
         text = TextReference.Res(R.string.transaction_history_not_supported_description),
-        onExploreClick = null,
     )
 }
