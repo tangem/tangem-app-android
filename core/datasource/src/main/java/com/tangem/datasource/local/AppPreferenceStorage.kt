05ac512f2d5791eb0ce46c02f4223e67aa74871f
Mama1emon <andrew.khokhlove@gmail.com>
2023-11-13 21:21:39 +0800

    AND-4902 Refactor the current decision to jetpack datastore

--- core/datasource/src/main/java/com/tangem/datasource/local/AppPreferenceStorage.kt
+++ core/datasource/src/main/java/com/tangem/datasource/local/AppPreferenceStorage.kt
@@ -5,6 +5,7 @@ package com.tangem.datasource.local
  *
  * @author Andrew Khokhlov on 08/02/2023
  */
+@Deprecated(message = "Use AppPreferencesStore", level = DeprecationLevel.WARNING)
 interface AppPreferenceStorage {
 
     /** Json config with feature toggles 'ToggleName: String - Availability: Boolean' */
