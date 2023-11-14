150425f170d4f11c2833bed8896190f78fe289be
Tangem <git@tangem.com>
2023-11-14 14:22:43 +0200

    Updated on 2026-08-27

--- data/tokens/src/main/kotlin/com/tangem/data/tokens/repository/DefaultMarketCryptoCurrencyRepository.kt
+++ data/tokens/src/main/kotlin/com/tangem/data/tokens/repository/DefaultMarketCryptoCurrencyRepository.kt
@@ -1,9 +1,7 @@
 package com.tangem.data.tokens.repository
 
-import com.tangem.blockchain.common.Blockchain
 import com.tangem.datasource.api.express.models.TangemExpressValues.EMPTY_CONTRACT_ADDRESS_VALUE
 import com.tangem.datasource.local.token.AssetsStore
-import com.tangem.domain.common.extensions.toNetworkId
 import com.tangem.domain.tokens.model.CryptoCurrency
 import com.tangem.domain.tokens.repository.MarketCryptoCurrencyRepository
 import com.tangem.domain.wallets.models.UserWalletId
@@ -13,14 +11,11 @@ class DefaultMarketCryptoCurrencyRepository(
 ) : MarketCryptoCurrencyRepository {
 
     override suspend fun isExchangeable(userWalletId: UserWalletId, cryptoCurrency: CryptoCurrency): Boolean {
-        val cryptoCurrencyId = cryptoCurrency.id
-        val blockchain = Blockchain.fromId(cryptoCurrencyId.rawNetworkId)
-        val apiNetworkId = blockchain.toNetworkId()
         val contractAddress = (cryptoCurrency as? CryptoCurrency.Token)?.contractAddress ?: EMPTY_CONTRACT_ADDRESS_VALUE
 
         return assetsStore.getSyncOrNull(userWalletId)?.find {
-            it.network == apiNetworkId &&
-                it.token == cryptoCurrencyId.rawCurrencyId &&
+            it.network == cryptoCurrency.network.backendId &&
+                it.token == cryptoCurrency.id.rawCurrencyId &&
                 it.contractAddress == contractAddress &&
                 it.isActive
         }?.exchangeAvailable ?: false
