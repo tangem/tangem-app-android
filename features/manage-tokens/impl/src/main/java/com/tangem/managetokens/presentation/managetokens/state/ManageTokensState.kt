61268890fecbe5f335a24d655dc5d3f0a643bb1a
Tangem <git@tangem.com>
2023-11-14 15:44:02 +0400

    release/5.26.1

---
+++ features/manage-tokens/impl/src/main/java/com/tangem/managetokens/presentation/managetokens/state/ManageTokensState.kt
@@ -0,0 +1,24 @@
+package com.tangem.managetokens.presentation.managetokens.state
+
+import androidx.paging.PagingData
+import com.tangem.core.ui.event.StateEvent
+import com.tangem.managetokens.presentation.common.state.ChooseWalletState
+import com.tangem.managetokens.presentation.common.state.Event
+import kotlinx.coroutines.flow.Flow
+
+internal data class ManageTokensState(
+    val searchBarState: SearchBarState,
+    val tokens: Flow<PagingData<TokenItemState>>,
+    val isLoading: Boolean,
+    val addCustomTokenButton: AddCustomTokenButton,
+    val chooseWalletState: ChooseWalletState,
+    val derivationNotification: DerivationNotificationState? = null,
+    val selectedToken: TokenItemState.Loaded? = null,
+    val showChooseWalletScreen: Boolean = false,
+    val event: StateEvent<Event>,
+)
+
+data class AddCustomTokenButton(
+    val isVisible: Boolean,
+    val onClick: () -> Unit,
+)
