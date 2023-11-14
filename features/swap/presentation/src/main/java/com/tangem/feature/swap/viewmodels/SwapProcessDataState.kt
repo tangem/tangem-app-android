f5696110ce1f7276fd80e144c8d3a6c6a7e7bd5d
Tangem <git@tangem.com>
2023-11-14 14:22:43 +0200

    Updated on 2026-09-14

--- features/swap/presentation/src/main/java/com/tangem/feature/swap/viewmodels/SwapProcessDataState.kt
+++ features/swap/presentation/src/main/java/com/tangem/feature/swap/viewmodels/SwapProcessDataState.kt
@@ -1,14 +1,21 @@
 package com.tangem.feature.swap.viewmodels
 
+import com.tangem.domain.tokens.model.CryptoCurrency
 import com.tangem.feature.swap.domain.models.domain.Currency
 import com.tangem.feature.swap.domain.models.ui.RequestApproveStateData
 import com.tangem.feature.swap.domain.models.ui.SwapStateData
 import com.tangem.feature.swap.domain.models.ui.TxFee
 
 data class SwapProcessDataState(
+    // Initial network id
     val networkId: String,
+    @Deprecated("used in old swap mechanism")
     val fromCurrency: Currency? = null,
+    @Deprecated("used in old swap mechanism")
     val toCurrency: Currency? = null,
+    val fromCryptoCurrency: CryptoCurrency? = null,
+    val toCryptoCurrency: CryptoCurrency? = null,
+    // Amount from input
     val amount: String? = null,
     val approveDataModel: RequestApproveStateData? = null,
     val swapDataModel: SwapStateData? = null,
