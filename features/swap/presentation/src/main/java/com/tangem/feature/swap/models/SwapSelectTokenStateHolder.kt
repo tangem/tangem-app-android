53c8986c8103b4244f601c5270c5f6498fb97d5a
Tangem <git@tangem.com>
2023-11-14 15:30:46 +0300

    Updated on 2026-08-27

--- features/swap/presentation/src/main/java/com/tangem/feature/swap/models/SwapSelectTokenStateHolder.kt
+++ features/swap/presentation/src/main/java/com/tangem/feature/swap/models/SwapSelectTokenStateHolder.kt
@@ -1,27 +1,35 @@
 package com.tangem.feature.swap.models
 
+import com.tangem.core.ui.components.currency.tokenicon.TokenIconState
+import kotlinx.collections.immutable.ImmutableList
+
 data class SwapSelectTokenStateHolder(
-    val addedTokens: List<TokenToSelect>,
-    val otherTokens: List<TokenToSelect>,
+    val availableTokens: ImmutableList<TokenToSelectState>,
+    val unavailableTokens: ImmutableList<TokenToSelectState>,
     val network: Network,
     val onSearchEntered: (String) -> Unit,
     val onTokenSelected: (String) -> Unit,
-)
-
-data class TokenToSelect(
-    val id: String,
-    val name: String,
-    val symbol: String,
-    val iconUrl: String,
-    val isNative: Boolean,
-    val available: Boolean = true,
-    val addedTokenBalanceData: TokenBalanceData? = null,
 )
 
 data class Network(
     val name: String,
     val blockchainId: String,
 )
+
+sealed class TokenToSelectState {
+
+    data class Title(val title: String) : TokenToSelectState()
+
+    data class TokenToSelect(
+        val id: String,
+        val name: String,
+        val symbol: String,
+        val isNative: Boolean,
+        val tokenIcon: TokenIconState,
+        val available: Boolean = true,
+        val addedTokenBalanceData: TokenBalanceData? = null,
+    ) : TokenToSelectState()
+}
 
 data class TokenBalanceData(
     val amount: String?,
