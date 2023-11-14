0cfd26d7889bc903525ac76aab365950c7e8a29c
Tangem <git@tangem.com>
2023-11-14 15:44:02 +0400

    Updated on 2026-09-21

---
+++ features/manage-tokens/impl/src/main/java/com/tangem/managetokens/presentation/managetokens/ui/ChooseNetworkScreen.kt
@@ -0,0 +1,205 @@
+package com.tangem.managetokens.presentation.managetokens.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.material.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.tangem.core.ui.components.SpacerH
+import com.tangem.core.ui.components.SpacerW
+import com.tangem.core.ui.components.WarningCardTitleOnly
+import com.tangem.core.ui.decorations.roundedShapeItemDecoration
+import com.tangem.core.ui.res.TangemTheme
+import com.tangem.features.managetokens.impl.R
+import com.tangem.managetokens.presentation.common.state.ChooseWalletState
+import com.tangem.managetokens.presentation.common.state.previewdata.ChooseWalletStatePreviewData
+import com.tangem.managetokens.presentation.common.ui.components.NetworkItem
+import com.tangem.managetokens.presentation.common.ui.components.SimpleSelectionBlock
+import com.tangem.managetokens.presentation.managetokens.state.ChooseNetworkState
+import com.tangem.managetokens.presentation.managetokens.state.TokenItemState
+import com.tangem.managetokens.presentation.managetokens.state.previewdata.TokenItemStatePreviewData
+
+@Composable
+internal fun ChooseNetworkScreen(
+    state: TokenItemState.Loaded,
+    walletState: ChooseWalletState,
+    modifier: Modifier = Modifier,
+) {
+    val networkState = state.chooseNetworkState
+    LazyColumn(
+        modifier = modifier
+            .background(TangemTheme.colors.background.tertiary)
+            .padding(TangemTheme.dimens.spacing16),
+    ) {
+        item {
+            Text(
+                text = stringResource(id = R.string.manage_tokens_network_selector_title),
+                color = TangemTheme.colors.text.primary1,
+                style = TangemTheme.typography.subtitle1,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth(),
+            )
+        }
+
+        when (walletState) {
+            is ChooseWalletState.Choose -> {
+                item {
+                    SpacerH(height = TangemTheme.dimens.spacing10)
+                }
+                item {
+                    SimpleSelectionBlock(
+                        title = stringResource(id = R.string.manage_tokens_network_selector_wallet),
+                        subtitle = walletState.selectedWallet?.walletName ?: "",
+                        onClick = walletState.onChooseWalletClick,
+                    )
+                }
+            }
+            ChooseWalletState.NoSelection -> Unit
+            is ChooseWalletState.Warning -> {
+                item {
+                    SpacerH(height = TangemTheme.dimens.spacing10)
+                }
+                item {
+                    WarningCardTitleOnly(
+                        title = stringResource(id = R.string.manage_tokens_wallet_support_only_one_network_title),
+                    )
+                }
+            }
+        }
+
+        item {
+            SpacerH(height = TangemTheme.dimens.spacing16)
+        }
+
+        item {
+            if (networkState.nativeNetworks.isNotEmpty()) {
+                NativeNetworks(networkState = networkState, tokenState = state)
+            }
+        }
+
+        if (networkState.nonNativeNetworks.isNotEmpty()) {
+            item {
+                NonNativeNetworksHeader(networkState.onNonNativeNetworkHintClick)
+            }
+            item {
+                SpacerH(height = TangemTheme.dimens.spacing8)
+            }
+            item {
+                this@LazyColumn.NonNativeNetworks(networkState = networkState, tokenState = state)
+            }
+        }
+    }
+}
+
+@Composable
+private fun NativeNetworks(networkState: ChooseNetworkState, tokenState: TokenItemState.Loaded) {
+    Column {
+        Text(
+            text = stringResource(id = R.string.manage_tokens_network_selector_native_title),
+            color = TangemTheme.colors.text.tertiary,
+            style = TangemTheme.typography.caption1,
+        )
+        SpacerH(height = TangemTheme.dimens.spacing2)
+        Text(
+            text = stringResource(id = R.string.manage_tokens_network_selector_native_subtitle),
+            color = TangemTheme.colors.text.tertiary,
+            style = TangemTheme.typography.caption2,
+        )
+        SpacerH(height = TangemTheme.dimens.spacing8)
+
+        networkState.nativeNetworks.forEachIndexed { index, network ->
+            NetworkItem(
+                state = network,
+                tokenState = tokenState,
+                modifier = Modifier
+                    .roundedShapeItemDecoration(
+                        currentIndex = index,
+                        lastIndex = networkState.nativeNetworks.lastIndex,
+                        addDefaultPadding = false,
+                    ),
+            )
+        }
+        SpacerH(height = TangemTheme.dimens.spacing16)
+    }
+}
+
+@Composable
+private fun LazyListScope.NonNativeNetworks(networkState: ChooseNetworkState, tokenState: TokenItemState.Loaded) {
+    items(
+        count = networkState.nonNativeNetworks.count(),
+        key = { index -> networkState.nonNativeNetworks[index].id },
+    ) { index ->
+        NetworkItem(
+            state = networkState.nonNativeNetworks[index],
+            tokenState = tokenState,
+            modifier = Modifier
+                .roundedShapeItemDecoration(
+                    currentIndex = index,
+                    lastIndex = networkState.nonNativeNetworks.lastIndex,
+                    addDefaultPadding = false,
+                ),
+        )
+    }
+    item {
+        SpacerH(height = TangemTheme.dimens.spacing16)
+    }
+}
+
+@Composable
+private fun NonNativeNetworksHeader(onNonNativeNetworkHintClick: () -> Unit) {
+    Column {
+        Row {
+            Text(
+                text = stringResource(id = R.string.manage_tokens_network_selector_non_native_title),
+                color = TangemTheme.colors.text.tertiary,
+                style = TangemTheme.typography.caption1,
+            )
+            SpacerW(width = TangemTheme.dimens.spacing2)
+            Icon(
+                painter = painterResource(id = R.drawable.ic_information_24),
+                tint = TangemTheme.colors.icon.inactive,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(TangemTheme.dimens.size16)
+                    .clickable { onNonNativeNetworkHintClick() },
+            )
+        }
+        SpacerH(height = TangemTheme.dimens.spacing2)
+        Text(
+            text = stringResource(id = R.string.manage_tokens_network_selector_non_native_subtitle),
+            color = TangemTheme.colors.text.tertiary,
+            style = TangemTheme.typography.caption2,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun Preview_ChooseNetworkScreen_Light() {
+    TangemTheme(isDark = false) {
+        ChooseNetworkScreen(
+            state = TokenItemStatePreviewData.loadedPriceDown as TokenItemState.Loaded,
+            walletState = ChooseWalletStatePreviewData.state,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun Preview_ChooseNetworkScreen_Dark() {
+    TangemTheme(isDark = true) {
+        ChooseNetworkScreen(
+            state = TokenItemStatePreviewData.loadedPriceDown as TokenItemState.Loaded,
+            walletState = ChooseWalletStatePreviewData.state,
+        )
+    }
+}
