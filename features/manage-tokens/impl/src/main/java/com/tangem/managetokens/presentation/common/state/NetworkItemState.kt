07b2c2eed7cb7327968ece0ed97d5c73a4f804f1
Tangem <git@tangem.com>
2023-11-14 15:44:02 +0400

    Updated on 2026-07-23

---
+++ features/manage-tokens/impl/src/main/java/com/tangem/managetokens/presentation/common/state/NetworkItemState.kt
@@ -0,0 +1,90 @@
+package com.tangem.managetokens.presentation.common.state
+
+import androidx.compose.runtime.MutableState
+import com.tangem.blockchain.common.Blockchain
+import com.tangem.core.ui.extensions.getActiveIconRes
+import com.tangem.core.ui.extensions.getGreyedOutIconRes
+import com.tangem.managetokens.presentation.managetokens.state.TokenItemState
+
+/**
+ * Network item state
+ *
+ * @property name           network name
+ * @property protocolName   network protocol name
+ * @property id             network id
+ * @property blockchain     blockchain
+ * @property iconRes        network icon id from resources
+ */
+internal sealed interface NetworkItemState {
+
+    val name: String
+    val protocolName: String
+    val id: String
+    val blockchain: Blockchain
+
+    val iconRes: Int
+        get() = when (this) {
+            is Selectable -> this.iconResId
+            is Toggleable -> this.iconResId.value
+        }
+
+    /**
+     * Network item state that can be added and deleted
+     *
+     * @property name           network name
+     * @property protocolName   network protocol name
+     * @property id             network id
+     * @property blockchain     blockchain
+     * @property iconResId      network icon id from resources
+     * @property isMainNetwork  flag that determines if the network is the main network for the token
+     * @property isAdded        flag that determines if the user has saved the network
+     * @property address        contract address
+     * @property decimals       decimal count
+     * @property onToggleClick  lambda be invoked when switch is been toggled
+     */
+    @Suppress("LongParameterList")
+    class Toggleable(
+        override val name: String,
+        override val protocolName: String,
+        override val id: String,
+        override val blockchain: Blockchain,
+        val iconResId: MutableState<Int>,
+        val isMainNetwork: Boolean,
+        val isAdded: MutableState<Boolean>,
+        val address: String?,
+        val decimals: Int?,
+        val onToggleClick: (TokenItemState.Loaded, Toggleable) -> Unit,
+    ) : NetworkItemState {
+
+        /**
+         * Change toggle state [isAdded].
+         *
+         * It is a hack that helps us to change element of flow
+         */
+        fun changeToggleState() {
+            val reverseState = !isAdded.value
+            isAdded.value = reverseState
+            iconResId.value = if (reverseState) getActiveIconRes(blockchain.id) else getGreyedOutIconRes(blockchain.id)
+        }
+    }
+
+    /**
+     * Network item state that can be selected
+     *
+     * @property name           network name
+     * @property protocolName   network protocol name
+     * @property iconResId      network icon id from resources
+     * @property id             network id
+     * @property blockchain     blockchain
+     * @property onNetworkClick lambda be invoked when network item is been clicked
+     *
+     */
+    class Selectable(
+        override val name: String,
+        override val protocolName: String,
+        val iconResId: Int,
+        override val id: String,
+        override val blockchain: Blockchain,
+        val onNetworkClick: (NetworkItemState) -> Unit,
+    ) : NetworkItemState
+}
