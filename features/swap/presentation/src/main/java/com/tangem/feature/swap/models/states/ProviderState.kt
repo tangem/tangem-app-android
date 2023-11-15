6a997772e8661b1913b9d83767c8048d51a083c6
Tangem <git@tangem.com>
2023-11-15 18:22:53 +0300

    Updated on 2025-10-01

--- features/swap/presentation/src/main/java/com/tangem/feature/swap/models/states/ProviderState.kt
+++ features/swap/presentation/src/main/java/com/tangem/feature/swap/models/states/ProviderState.kt
@@ -2,15 +2,42 @@ package com.tangem.feature.swap.models.states
 
 sealed class ProviderState {
 
-    object Loading : ProviderState()
+    abstract val onProviderClick: ((String) -> Unit)?
+    abstract val id: String
+
+    data class Loading(
+        override val id: String = "",
+        override val onProviderClick: ((String) -> Unit)? = null,
+    ) : ProviderState()
 
     data class Content(
-        val id: String,
+        override val id: String,
         val name: String,
         val type: String,
         val iconUrl: String,
-        val isBestTrade: Boolean,
         val rate: String,
-        val onProviderClick: () -> Unit,
+        val selectionType: SelectionType,
+        val additionalBadge: AdditionalBadge,
+        val percentLowerThenBest: Float?,
+        override val onProviderClick: (String) -> Unit,
     ) : ProviderState()
+
+    data class Unavailable(
+        override val id: String,
+        val name: String,
+        val type: String,
+        val iconUrl: String,
+        val alertText: String,
+        override val onProviderClick: ((String) -> Unit)? = null,
+    ) : ProviderState()
+
+    sealed class AdditionalBadge {
+        object BestTrade : AdditionalBadge()
+        object Empty : AdditionalBadge()
+        object PermissionRequired : AdditionalBadge()
+    }
+
+    enum class SelectionType {
+        NONE, CLICK, SELECT
+    }
 }
