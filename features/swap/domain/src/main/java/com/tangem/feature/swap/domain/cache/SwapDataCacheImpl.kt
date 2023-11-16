1788ff50c535e450e364001d28f9aaff76974767
Tangem <git@tangem.com>
2023-11-16 14:04:21 +0200

    Updated on 2026-05-14

--- features/swap/domain/src/main/java/com/tangem/feature/swap/domain/cache/SwapDataCacheImpl.kt
+++ features/swap/domain/src/main/java/com/tangem/feature/swap/domain/cache/SwapDataCacheImpl.kt
@@ -2,7 +2,7 @@ package com.tangem.feature.swap.domain.cache
 
 import com.tangem.feature.swap.domain.models.SwapAmount
 import com.tangem.feature.swap.domain.models.domain.Currency
-import com.tangem.feature.swap.domain.models.ui.TokenWithBalance
+import com.tangem.feature.swap.domain.models.ui.TokenWithBalanceExpress
 import java.math.BigDecimal
 
 class SwapDataCacheImpl : SwapDataCache {
@@ -10,28 +10,28 @@ class SwapDataCacheImpl : SwapDataCache {
     private val availableTokensForNetwork: MutableMap<String, List<Currency>> = mutableMapOf()
     private val feesForNetworks: MutableMap<String, BigDecimal> = mutableMapOf()
     private val tokensBalances: MutableMap<String, Map<String, SwapAmount>> = mutableMapOf()
-    private val lastInWalletTokens = mutableListOf<TokenWithBalance>()
-    private val lastLoadedTokens = mutableListOf<TokenWithBalance>()
+    private val lastInWalletTokens = mutableListOf<TokenWithBalanceExpress>()
+    private val lastLoadedTokens = mutableListOf<TokenWithBalanceExpress>()
 
     override fun cacheLastFeeForNetwork(fee: BigDecimal, networkId: String) {
         feesForNetworks[networkId] = fee
     }
 
-    override fun cacheInWalletTokens(tokens: List<TokenWithBalance>) {
+    override fun cacheInWalletTokens(tokens: List<TokenWithBalanceExpress>) {
         lastInWalletTokens.clear()
         lastInWalletTokens.addAll(tokens)
     }
 
-    override fun cacheLoadedTokens(tokens: List<TokenWithBalance>) {
+    override fun cacheLoadedTokens(tokens: List<TokenWithBalanceExpress>) {
         lastLoadedTokens.clear()
         lastLoadedTokens.addAll(tokens)
     }
 
-    override fun getInWalletTokens(): List<TokenWithBalance> {
+    override fun getInWalletTokens(): List<TokenWithBalanceExpress> {
         return lastInWalletTokens
     }
 
-    override fun getLoadedTokens(): List<TokenWithBalance> {
+    override fun getLoadedTokens(): List<TokenWithBalanceExpress> {
         return lastLoadedTokens
     }
 
