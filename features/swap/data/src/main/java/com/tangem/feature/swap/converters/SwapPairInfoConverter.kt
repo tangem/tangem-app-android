48a95142ed1ff0b21a8b24753441299a6588ee27
Tangem <git@tangem.com>
2023-11-20 14:28:16 +0200

    Updated on 2026-08-07

--- features/swap/data/src/main/java/com/tangem/feature/swap/converters/SwapPairInfoConverter.kt
+++ features/swap/data/src/main/java/com/tangem/feature/swap/converters/SwapPairInfoConverter.kt
@@ -1,15 +1,15 @@
 package com.tangem.feature.swap.converters
 
-import com.tangem.datasource.api.express.models.response.RateType
 import com.tangem.datasource.api.express.models.response.SwapPair
 import com.tangem.datasource.api.express.models.response.SwapPairProvider
 import com.tangem.feature.swap.domain.models.domain.LeastTokenInfo
 import com.tangem.feature.swap.domain.models.domain.SwapPairLeast as SwapPairDomain
-import com.tangem.feature.swap.domain.models.domain.SwapPairProvider as SwapPairProviderDomain
-import com.tangem.feature.swap.domain.models.domain.RateType as RateTypeDomain
+import com.tangem.feature.swap.domain.models.domain.SwapProvider as SwapPairProviderDomain
 import com.tangem.utils.converter.Converter
 
 class SwapPairInfoConverter : Converter<SwapPair, SwapPairDomain> {
+
+    private val rateTypeConverter = RateTypeConverter()
 
     override fun convert(value: SwapPair): SwapPairDomain {
         return SwapPairDomain(
@@ -30,14 +30,8 @@ class SwapPairInfoConverter : Converter<SwapPair, SwapPairDomain> {
     private fun convertProvider(swapPairProvider: SwapPairProvider): SwapPairProviderDomain {
         return SwapPairProviderDomain(
             providerId = swapPairProvider.providerId,
-            rateTypes = swapPairProvider.rateTypes.map { convertRateType(it) },
+            rateTypes = swapPairProvider.rateTypes.map { rateTypeConverter.convert(it) },
         )
     }
 
-    private fun convertRateType(rateType: RateType): RateTypeDomain {
-        return when (rateType) {
-            RateType.FIXED -> RateTypeDomain.FIXED
-            RateType.FLOAT -> RateTypeDomain.FLOAT
-        }
-    }
 }
