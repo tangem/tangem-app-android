0e77e5d84e5f660cc342d6486871a4d63d7b5e0f
Tangem <git@tangem.com>
2023-11-21 01:02:10 +0400

    Updated on 2026-06-10

---
+++ app/src/main/java/com/tangem/tap/common/ui/ScanFailsDialog.kt
@@ -0,0 +1,30 @@
+package com.tangem.tap.common.ui
+
+import android.content.Context
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.tangem.core.analytics.Analytics
+import com.tangem.tap.common.analytics.events.IntroductionProcess
+import com.tangem.tap.common.extensions.dispatchDialogHide
+import com.tangem.tap.common.feedback.ScanFailsEmail
+import com.tangem.tap.common.redux.global.GlobalAction
+import com.tangem.tap.store
+import com.tangem.wallet.R
+
+/**
+[REDACTED_AUTHOR]
+ */
+object ScanFailsDialog {
+    fun create(context: Context): AlertDialog {
+        return MaterialAlertDialogBuilder(context, R.style.CustomMaterialDialog).apply {
+            setTitle(context.getString(R.string.common_warning))
+            setMessage(R.string.alert_troubleshooting_scan_card_title)
+            setPositiveButton(R.string.alert_button_request_support) { _, _ ->
+                Analytics.send(IntroductionProcess.ButtonRequestSupport())
+                store.dispatch(GlobalAction.SendEmail(ScanFailsEmail()))
+            }
+            setNeutralButton(R.string.common_cancel) { _, _ -> }
+            setOnDismissListener { store.dispatchDialogHide() }
+        }.create()
+    }
+}
