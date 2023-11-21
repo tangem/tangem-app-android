11599073771801b5f34cb54eebd44ce78fd02fd7
Tangem <git@tangem.com>
2023-11-21 15:05:17 +0400

    Updated on 2025-09-25

--- common/src/main/java/com/tangem/common/module/ModuleMessage.kt
+++ common/src/main/java/com/tangem/common/module/ModuleMessage.kt
@@ -4,6 +4,7 @@ package com.tangem.common.module
 [REDACTED_AUTHOR]
  * The base object for communication between modules
  */
+@Deprecated("Will be removed")
 interface ModuleMessage
 
 /**
@@ -11,12 +12,14 @@ interface ModuleMessage
  * @property message the error description
  * @property data any data that can help in the part where this error is being handled
  */
+@Deprecated("Will be removed")
 abstract class ModuleError : Throwable(), ModuleMessage {
     abstract val code: Int
     abstract override val message: String
     abstract val data: Any?
 }
 
+@Deprecated("Will be removed")
 interface ModuleMessageConverter<ModuleMessage, R> {
     fun convert(message: ModuleMessage): R
 }
