801e198d3a0c21b2b638a53906ef5c601d6d3b74
Tangem <git@tangem.com>
2023-11-21 15:05:17 +0400

    Updated on 2026-09-25

--- app/src/main/java/com/tangem/tap/common/extensions/ViewGroup.kt
+++ app/src/main/java/com/tangem/tap/common/extensions/ViewGroup.kt
@@ -3,15 +3,9 @@ package com.tangem.tap.common.extensions
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewParent
-import androidx.core.view.forEach
 import androidx.transition.AutoTransition
 import androidx.transition.Transition
 import androidx.transition.TransitionManager
-import com.google.android.material.chip.Chip
-import com.google.android.material.chip.ChipGroup
-import com.tangem.tap.common.GlobalLayoutStateHandler
-import timber.log.Timber
 
 /**
 [REDACTED_AUTHOR]
@@ -20,30 +14,6 @@ fun ViewGroup.inflate(viewToInflate: Int, attachToRoot: Boolean = false): View {
     return LayoutInflater.from(context).inflate(viewToInflate, this, attachToRoot)
 }
 
-fun ViewParent?.beginDelayedTransition(transition: Transition = AutoTransition()) {
-    if (this == null) Timber.e("Can't invoke beginDelayedTransition, because parent is NULL")
-    (this as? ViewGroup)?.beginDelayedTransition(transition)
-}
-
 fun ViewGroup.beginDelayedTransition(transition: Transition = AutoTransition()) {
     TransitionManager.beginDelayedTransition(this, transition)
-}
-
-fun View.beginDelayedTransition(transition: Transition = AutoTransition()) {
-    (this as? ViewGroup)?.beginDelayedTransition(transition)
-}
-
-fun ChipGroup.fitChipsByGroupWidth() {
-    val layoutStateHandler = GlobalLayoutStateHandler(this)
-    layoutStateHandler.onStateChanged = stateHandler@{
-        if (it.childCount < 2) {
-            layoutStateHandler.detach()
-            return@stateHandler
-        }
-
-        val spacingBetweenViews = it.chipSpacingHorizontal * (it.childCount - 1)
-        val width = (it.width - spacingBetweenViews) / it.childCount
-        it.forEach { chip -> (chip as? Chip)?.width = width }
-        layoutStateHandler.detach()
-    }
 }
