9c94dbea71abc3cca43f090c112d20b40893115a
Tangem <git@tangem.com>
2023-11-21 13:47:40 +0200

    Updated on 2026-09-25

--- core/datasource/src/main/java/com/tangem/datasource/api/express/models/response/ExchangeQuoteResponse.kt
+++ core/datasource/src/main/java/com/tangem/datasource/api/express/models/response/ExchangeQuoteResponse.kt
@@ -4,9 +4,23 @@ import com.squareup.moshi.Json
 import java.math.BigDecimal
 
 data class ExchangeQuoteResponse(
+
+    @Json(name = "fromAmount")
+    val fromAmount: BigDecimal,
+
+    @Json(name = "fromDecimals")
+    val fromDecimals: Int,
+
     @Json(name = "toAmount")
     val toAmount: BigDecimal,
 
+    @Json(name = "toDecimals")
+    val toDecimals: Int,
+
     @Json(name = "allowanceContract")
     val allowanceContract: String?,
+
+    @Json(name = "minAmount")
+    val minAmount: BigDecimal,
+
 )
