70cc40a4811e2ccaa35674d9cd576cd6a291681c
Tangem <git@tangem.com>
2023-11-21 17:44:15 +0200

    Updated on 2026-08-07

--- features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/domain/ExchangeQuote.kt
+++ features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/domain/ExchangeQuote.kt
@@ -1,8 +1,6 @@
 package com.tangem.feature.swap.domain.models.domain
 
-import java.math.BigDecimal
-
 data class ExchangeQuote(
-    val toAmount: BigDecimal,
-    val allowanceContract: String?
+    val toAmount: String,
+    val allowanceContract: String?,
 )
