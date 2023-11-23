27300b4f04cbb4648f5f730e86d3bf0999742531
Tangem <git@tangem.com>
2023-11-23 12:08:17 +0200

    Updated on 2026-08-07

--- features/swap/presentation/src/main/java/com/tangem/feature/swap/ui/SwapScreen.kt
+++ features/swap/presentation/src/main/java/com/tangem/feature/swap/ui/SwapScreen.kt
@@ -2,19 +2,15 @@ package com.tangem.feature.swap.ui
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import com.tangem.core.ui.components.appbar.AppBarWithBackButton
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.feature.swap.models.SwapStateHolder
 import com.tangem.feature.swap.models.states.ChooseProviderBottomSheetConfig
 import com.tangem.feature.swap.models.states.GivePermissionBottomSheetConfig
-import com.tangem.feature.swap.presentation.R
 
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
