a6b2b2ec14bb39f09a2f4bd93e612663c66ecb9b
Tangem <git@tangem.com>
2023-11-23 12:03:56 +0200

    Updated on 2026-07-23

--- features/swap/data/src/main/java/com/tangem/feature/swap/di/SwapDataModule.kt
+++ features/swap/data/src/main/java/com/tangem/feature/swap/di/SwapDataModule.kt
@@ -6,6 +6,8 @@ import com.tangem.datasource.api.oneinch.OneInchErrorsHandler
 import com.tangem.datasource.api.tangemTech.TangemTechApi
 import com.tangem.datasource.config.ConfigManager
 import com.tangem.domain.walletmanager.WalletManagersFacade
+import com.tangem.domain.wallets.legacy.UserWalletsListManager
+import com.tangem.domain.wallets.legacy.WalletsStateHolder
 import com.tangem.feature.swap.SwapRepositoryImpl
 import com.tangem.feature.swap.domain.SwapRepository
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
@@ -29,6 +31,7 @@ class SwapDataModule {
         coroutineDispatcher: CoroutineDispatcherProvider,
         configManager: ConfigManager,
         walletManagerFacade: WalletManagersFacade,
+        walletsStateHolder: WalletsStateHolder
     ): SwapRepository {
         return SwapRepositoryImpl(
             tangemTechApi = tangemTechApi,
@@ -38,6 +41,7 @@ class SwapDataModule {
             coroutineDispatcher = coroutineDispatcher,
             configManager = configManager,
             walletManagersFacade = walletManagerFacade,
+            walletsStateHolder = walletsStateHolder,
         )
     }
 }
