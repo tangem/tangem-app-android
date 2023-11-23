5afc120a316eec722838a3240ba9ce3587273feb
Tangem <git@tangem.com>
2023-11-23 15:29:21 +0300

    Updated on 2026-09-23

--- features/swap/presentation/src/main/java/com/tangem/feature/swap/models/states/ChooseProviderBottomSheetConfig.kt
+++ features/swap/presentation/src/main/java/com/tangem/feature/swap/models/states/ChooseProviderBottomSheetConfig.kt
@@ -3,7 +3,7 @@ package com.tangem.feature.swap.models.states
 import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfigContent
 import kotlinx.collections.immutable.ImmutableList
 
-class ChooseProviderBottomSheetConfig(
+data class ChooseProviderBottomSheetConfig(
     val selectedProviderId: String,
     val providers: ImmutableList<ProviderState>,
 ) : TangemBottomSheetConfigContent
