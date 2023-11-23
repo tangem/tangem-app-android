d0b4de9086c826794bbf2c63fca7f71c8d0aaacd
Tangem <git@tangem.com>
2023-11-23 12:08:17 +0200

    release/5.26.1

--- features/swap/domain/src/main/java/com/tangem/feature/swap/domain/cache/SwapDataCache.kt
+++ features/swap/domain/src/main/java/com/tangem/feature/swap/domain/cache/SwapDataCache.kt
@@ -1,16 +1,13 @@
 package com.tangem.feature.swap.domain.cache
 
 import com.tangem.feature.swap.domain.models.SwapAmount
-import com.tangem.feature.swap.domain.models.domain.Currency
 import com.tangem.feature.swap.domain.models.ui.TokenWithBalanceExpress
-import java.math.BigDecimal
 
 interface SwapDataCache {
 
     fun cacheInWalletTokens(tokens: List<TokenWithBalanceExpress>)
     fun cacheLoadedTokens(tokens: List<TokenWithBalanceExpress>)
     fun cacheBalances(networkId: String, derivationPath: String?, balances: Map<String, SwapAmount>)
-
 
     fun getInWalletTokens(): List<TokenWithBalanceExpress>
     fun getLoadedTokens(): List<TokenWithBalanceExpress>
