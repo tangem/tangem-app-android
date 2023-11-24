2730e15986d22f3802ccf7317f0c327aa620adc1
Tangem <git@tangem.com>
2023-11-24 15:10:02 +0400

    Updated on 2026-06-17

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/utils/CurrencyStatusErrorConverter.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/utils/CurrencyStatusErrorConverter.kt
@@ -1,9 +1,9 @@
 package com.tangem.feature.wallet.presentation.wallet.utils
 
-import com.tangem.common.Provider
 import com.tangem.domain.tokens.error.CurrencyStatusError
 import com.tangem.feature.wallet.presentation.wallet.state.WalletSingleCurrencyState
 import com.tangem.feature.wallet.presentation.wallet.state.WalletState
+import com.tangem.utils.Provider
 import com.tangem.utils.converter.Converter
 
 // TODO: Implement this
