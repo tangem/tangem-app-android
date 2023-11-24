635ca7adcbef3f528285af2927631d291db724c8
Tangem <git@tangem.com>
2023-11-24 20:46:33 +0400

    Updated on 2026-02-06

--- core/analytics/models/src/main/java/com/tangem/core/analytics/models/AnalyticsEvent.kt
+++ core/analytics/models/src/main/java/com/tangem/core/analytics/models/AnalyticsEvent.kt
@@ -8,4 +8,12 @@ open class AnalyticsEvent(
     val event: String,
     var params: Map<String, String> = mapOf(),
     val error: Throwable? = null,
-)
+) {
+
+    val id: String = "[$category] $event"
+}
+
+interface OneTimeAnalyticsEvent {
+
+    val oneTimeEventId: String
+}
