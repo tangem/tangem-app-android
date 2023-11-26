65743100f2d8a0024828d00b3db037a493647f0c
Tangem <git@tangem.com>
2023-11-27 00:35:31 +0300

    Updated on 2026-07-07

--- core/datasource/src/main/java/com/tangem/datasource/api/express/TangemExpressApi.kt
+++ core/datasource/src/main/java/com/tangem/datasource/api/express/TangemExpressApi.kt
@@ -8,7 +8,6 @@ import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Query
-import java.math.BigDecimal
 
 /**
  * Interface of Tangem Express API (new swap mechanism)
