89272c6ff61960628a99969353bf5ef58baa898f
Tangem <git@tangem.com>
2023-11-27 00:35:31 +0300

    Updated on 2026-04-08

--- features/swap/presentation/src/main/java/com/tangem/feature/swap/viewmodels/SwapProcessDataState.kt
+++ features/swap/presentation/src/main/java/com/tangem/feature/swap/viewmodels/SwapProcessDataState.kt
@@ -1,25 +1,29 @@
 package com.tangem.feature.swap.viewmodels
 
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
-import com.tangem.feature.swap.domain.models.domain.Currency
+import com.tangem.feature.swap.domain.models.domain.SwapDataModel
 import com.tangem.feature.swap.domain.models.domain.SwapProvider
-import com.tangem.feature.swap.domain.models.ui.*
+import com.tangem.feature.swap.domain.models.ui.RequestApproveStateData
+import com.tangem.feature.swap.domain.models.ui.SwapState
+import com.tangem.feature.swap.domain.models.ui.TokensDataStateExpress
+import com.tangem.feature.swap.domain.models.ui.TxFee
 
 data class SwapProcessDataState(
     // Initial network id
     val networkId: String,
-    @Deprecated("used in old swap mechanism")
-    val fromCurrency: Currency? = null,
-    @Deprecated("used in old swap mechanism")
-    val toCurrency: Currency? = null,
     val fromCryptoCurrency: CryptoCurrencyStatus? = null,
     val toCryptoCurrency: CryptoCurrencyStatus? = null,
     // Amount from input
     val amount: String? = null,
     val approveDataModel: RequestApproveStateData? = null,
-    val swapDataModel: SwapStateData? = null,
-    val selectedFee: TxFee? = null,
+    val swapDataModel: SwapDataModel? = null,
+    val selectedFee: TxFee? = null, // todo
     val tokensDataState: TokensDataStateExpress? = null,
     val selectedProvider: SwapProvider? = null,
     val lastLoadedSwapStates: Map<SwapProvider, SwapState> = emptyMap(),
-)
+) {
+
+    fun getCurrentLoadedSwapState(): SwapState.QuotesLoadedState? {
+        return lastLoadedSwapStates[selectedProvider] as? SwapState.QuotesLoadedState
+    }
+}
