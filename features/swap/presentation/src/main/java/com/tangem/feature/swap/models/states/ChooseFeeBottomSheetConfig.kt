bc44787b461dc34bc67b1e3a34473e0085376533
Tangem <git@tangem.com>
2023-11-27 00:35:31 +0300

    Updated on 2026-08-07

--- features/swap/presentation/src/main/java/com/tangem/feature/swap/models/states/ChooseFeeBottomSheetConfig.kt
+++ features/swap/presentation/src/main/java/com/tangem/feature/swap/models/states/ChooseFeeBottomSheetConfig.kt
@@ -4,8 +4,8 @@ import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfigContent
 import com.tangem.feature.swap.domain.models.ui.FeeType
 import kotlinx.collections.immutable.ImmutableList
 
-class ChooseFeeBottomSheetConfig(
+data class ChooseFeeBottomSheetConfig(
     val selectedFee: FeeType,
     val onSelectFeeType: (FeeType) -> Unit,
-    val feeItems: ImmutableList<FeeItemState>,
+    val feeItems: ImmutableList<FeeItemState.Content>,
 ) : TangemBottomSheetConfigContent
