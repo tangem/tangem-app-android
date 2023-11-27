714ea256d1485c950f55110df355684bfec1b1dd
Tangem <git@tangem.com>
2023-11-27 18:37:48 +0300

    Updated on 2026-05-13

--- features/swap/presentation/src/main/java/com/tangem/feature/swap/models/SwapSuccessStateHolder.kt
+++ features/swap/presentation/src/main/java/com/tangem/feature/swap/models/SwapSuccessStateHolder.kt
@@ -1,7 +1,20 @@
 package com.tangem.feature.swap.models
 
+import com.tangem.core.ui.components.currency.tokenicon.TokenIconState
+import com.tangem.core.ui.extensions.TextReference
+import com.tangem.feature.swap.domain.models.domain.SwapProvider
+
 data class SwapSuccessStateHolder(
-    val fromTokenAmount: String,
-    val toTokenAmount: String,
+    val timestamp: Long,
+    val txUrl: String,
+    val fee: TextReference,
+    val rate: TextReference,
+    val selectedProvider: SwapProvider,
+    val fromTokenAmount: TextReference,
+    val toTokenAmount: TextReference,
+    val fromTokenFiatAmount: TextReference,
+    val toTokenFiatAmount: TextReference,
+    val fromTokenIconState: TokenIconState?,
+    val toTokenIconState: TokenIconState?,
     val onSecondaryButtonClick: () -> Unit,
 )
