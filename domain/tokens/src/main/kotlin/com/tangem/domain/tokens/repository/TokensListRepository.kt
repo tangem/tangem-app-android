21d7473612295661b61576e45d29e5a9c8103622
Tangem <git@tangem.com>
2023-11-28 16:43:57 +0100

    release/5.26.1

---
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/repository/TokensListRepository.kt
@@ -0,0 +1,20 @@
+package com.tangem.domain.tokens.repository
+
+import androidx.paging.PagingData
+import com.tangem.domain.tokens.model.Token
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Repository interface for managing operations related to a complete set of tokens
+ * (not associated with any specific card).
+ * */
+interface TokensListRepository {
+
+    /**
+     * Retrieves a list of available tokens with quotes, can be filtered based on the provided search text.
+     *
+     * @param searchText The search text used to filter tokens.
+     * @return A [Flow] emitting [PagingData] containing the tokens with quotes matching the search criteria.
+     */
+    fun getTokens(searchText: String?): Flow<PagingData<Token>>
+}
