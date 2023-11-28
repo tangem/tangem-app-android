8204c6c2717fd9184b71e07017d7dedfe37ce120
Tangem <git@tangem.com>
2023-11-28 23:00:19 +0200

    Updated on 2026-06-16

--- features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/DataError.kt
+++ features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/DataError.kt
@@ -1,10 +1,34 @@
 package com.tangem.feature.swap.domain.models
 
-sealed class DataError {
-    object UnknownError : DataError()
-    data class Error(val message: String) : DataError()
-}
+import java.math.BigDecimal
 
-fun mapErrors(error: String?): DataError {
-    return error?.let { DataError.Error(it) } ?: DataError.UnknownError
+sealed class DataError {
+
+    abstract val code: Int
+
+    data class BadRequest(override val code: Int) : DataError()
+
+    data class ExchangeProviderNotFoundError(override val code: Int): DataError()
+
+    data class ExchangeProviderNotActiveError(override val code: Int): DataError()
+
+    data class ExchangeProviderNotAvailableError(override val code: Int): DataError()
+
+    data class ExchangeNotPossibleError(override val code: Int): DataError()
+
+    data class ExchangeTooSmallAmountError(override val code: Int, val amount: SwapAmount): DataError()
+
+    data class ExchangeNotEnoughAllowanceError(override val code: Int, val currentAllowance: BigDecimal): DataError()
+
+    data class ExchangeNotEnoughBalanceError(override val code: Int): DataError()
+
+    data class ExchangeInvalidAddressError(override val code: Int): DataError()
+
+    data class ExchangeInvalidFromDecimalsError(
+        override val code: Int,
+        val receivedFromDecimals: Int,
+        val expressFromDecimals: Int
+    ): DataError()
+
+    data class UnknownError(override val code: Int = -1): DataError()
 }
