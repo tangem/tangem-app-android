a7bbc2ccd99b305c5d2b9d3ca79d3f701896a697
Tangem <git@tangem.com>
2023-11-28 23:01:32 +0200

    Updated on 2025-09-15

--- features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/DataError.kt
+++ features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/DataError.kt
@@ -8,27 +8,27 @@ sealed class DataError {
 
     data class BadRequest(override val code: Int) : DataError()
 
-    data class ExchangeProviderNotFoundError(override val code: Int): DataError()
+    data class ExchangeProviderNotFoundError(override val code: Int) : DataError()
 
-    data class ExchangeProviderNotActiveError(override val code: Int): DataError()
+    data class ExchangeProviderNotActiveError(override val code: Int) : DataError()
 
-    data class ExchangeProviderNotAvailableError(override val code: Int): DataError()
+    data class ExchangeProviderNotAvailableError(override val code: Int) : DataError()
 
-    data class ExchangeNotPossibleError(override val code: Int): DataError()
+    data class ExchangeNotPossibleError(override val code: Int) : DataError()
 
-    data class ExchangeTooSmallAmountError(override val code: Int, val amount: SwapAmount): DataError()
+    data class ExchangeTooSmallAmountError(override val code: Int, val amount: SwapAmount) : DataError()
 
-    data class ExchangeNotEnoughAllowanceError(override val code: Int, val currentAllowance: BigDecimal): DataError()
+    data class ExchangeNotEnoughAllowanceError(override val code: Int, val currentAllowance: BigDecimal) : DataError()
 
-    data class ExchangeNotEnoughBalanceError(override val code: Int): DataError()
+    data class ExchangeNotEnoughBalanceError(override val code: Int) : DataError()
 
-    data class ExchangeInvalidAddressError(override val code: Int): DataError()
+    data class ExchangeInvalidAddressError(override val code: Int) : DataError()
 
     data class ExchangeInvalidFromDecimalsError(
         override val code: Int,
         val receivedFromDecimals: Int,
-        val expressFromDecimals: Int
-    ): DataError()
+        val expressFromDecimals: Int,
+    ) : DataError()
 
-    data class UnknownError(override val code: Int = -1): DataError()
+    data class UnknownError(override val code: Int = -1) : DataError()
 }
