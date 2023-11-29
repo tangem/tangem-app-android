1425d14d6b423cd99c480c127b2e15890e6a7159
Tangem <git@tangem.com>
2023-11-29 16:07:00 +0300

    Updated on 2026-09-23

---
+++ features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/components/ExchangeStatusNotifications.kt
@@ -0,0 +1,38 @@
+package com.tangem.feature.tokendetails.presentation.tokendetails.state.components
+
+import androidx.compose.runtime.Immutable
+import com.tangem.core.ui.components.notifications.NotificationConfig
+import com.tangem.core.ui.extensions.TextReference
+import com.tangem.features.tokendetails.impl.R
+
+@Immutable
+internal sealed class ExchangeStatusNotifications(val config: NotificationConfig) {
+
+    data class NeedVerification(
+        val onGoToProviderClick: () -> Unit,
+    ) : ExchangeStatusNotifications(
+        config = NotificationConfig(
+            title = TextReference.Res(R.string.express_exchange_notification_verification_title),
+            subtitle = TextReference.Res(R.string.express_exchange_notification_verification_text),
+            iconResId = R.drawable.ic_alert_triangle_20,
+            buttonsState = NotificationConfig.ButtonsState.SecondaryButtonConfig(
+                text = TextReference.Res(R.string.express_go_to_provider),
+                onClick = onGoToProviderClick,
+            ),
+        ),
+    )
+
+    data class Failed(
+        val onGoToProviderClick: () -> Unit,
+    ) : ExchangeStatusNotifications(
+        config = NotificationConfig(
+            title = TextReference.Res(R.string.express_exchange_notification_failed_title),
+            subtitle = TextReference.Res(R.string.express_exchange_notification_failed_text),
+            iconResId = R.drawable.ic_alert_circle_24,
+            buttonsState = NotificationConfig.ButtonsState.SecondaryButtonConfig(
+                text = TextReference.Res(R.string.express_go_to_provider),
+                onClick = onGoToProviderClick,
+            ),
+        ),
+    )
+}
