08b4c408fe4467934b99e3a18e226402fbda769c
Tangem <git@tangem.com>
2023-11-29 12:10:09 +0300

    release/5.26.1

---
+++ features/swap/data/src/main/java/com/tangem/feature/swap/converters/ExchangeStatusConverter.kt
@@ -0,0 +1,19 @@
+package com.tangem.feature.swap.converters
+
+import com.tangem.datasource.api.express.models.response.ExchangeStatusResponse
+import com.tangem.feature.swap.domain.models.domain.ExchangeStatus
+import com.tangem.feature.swap.domain.models.domain.ExchangeStatusModel
+import com.tangem.utils.converter.Converter
+
+internal class ExchangeStatusConverter : Converter<ExchangeStatusResponse, ExchangeStatusModel> {
+    override fun convert(value: ExchangeStatusResponse): ExchangeStatusModel {
+        return ExchangeStatusModel(
+            providerId = value.providerId,
+            status = ExchangeStatus.values().firstOrNull {
+                it.name.lowercase() == value.externalStatus.name.lowercase()
+            },
+            txId = value.externalTxId,
+            txUrl = value.externalTxUrl,
+        )
+    }
+}
