4d9e8addad620544c414a7dc303d012f63563e68
Tangem <git@tangem.com>
2023-11-29 16:07:00 +0300

    Updated on 2026-08-27

--- features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/SwapTransactionsState.kt
+++ features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/SwapTransactionsState.kt
@@ -2,21 +2,25 @@ package com.tangem.feature.tokendetails.presentation.tokendetails.state
 
 import com.tangem.core.ui.components.currency.tokenicon.TokenIconState
 import com.tangem.feature.swap.domain.models.domain.ExchangeStatus
+import com.tangem.feature.swap.domain.models.domain.SwapProvider
+import com.tangem.feature.tokendetails.presentation.tokendetails.state.components.ExchangeStatusNotifications
 import kotlinx.collections.immutable.PersistentList
-import java.math.BigDecimal
 
 internal data class SwapTransactionsState(
     val txId: String,
-    val providerId: Int,
+    val provider: SwapProvider,
     val txUrl: String? = null,
-    val rate: BigDecimal,
     val timestamp: Long,
-    val status: PersistentList<ExchangeStatusState>,
+    val statuses: PersistentList<ExchangeStatusState>,
     val activeStatus: ExchangeStatus?,
+    val fiatSymbol: String,
+    val notification: ExchangeStatusNotifications? = null,
     val toCryptoAmount: String,
+    val toCryptoSymbol: String,
     val toFiatAmount: String,
     val toCurrencyIcon: TokenIconState,
     val fromCryptoAmount: String,
+    val fromCryptoSymbol: String,
     val fromFiatAmount: String,
     val fromCurrencyIcon: TokenIconState,
     val onClick: () -> Unit,
@@ -25,7 +29,6 @@ internal data class SwapTransactionsState(
 
 internal class ExchangeStatusState(
     val status: ExchangeStatus,
-    val text: String,
     val isActive: Boolean,
     val isDone: Boolean,
 )
