d313abf99501c0058ed3d22cd95d5dd64ef11fca
Tangem <git@tangem.com>
2023-12-05 12:47:48 +0300

    Updated on 2026-04-15

--- features/swap/presentation/src/main/java/com/tangem/feature/swap/viewmodels/SwapViewModel.kt
+++ features/swap/presentation/src/main/java/com/tangem/feature/swap/viewmodels/SwapViewModel.kt
@@ -748,7 +748,11 @@ internal class SwapViewModel @Inject constructor(
                     )
                 }
             },
-            onBuyClick = {},
+            onBuyClick = {
+                swapInteractor.getSelectedWallet()?.let {
+                    swapRouter.openTokenDetails(it.walletId, swapInteractor.getNativeToken(dataState.networkId))
+                }
+            },
         )
     }
 
