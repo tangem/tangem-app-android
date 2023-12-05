98f70506f122710e5afc1b291adc4304cb8c50c9
Tangem <git@tangem.com>
2023-12-05 11:48:06 +0200

    Updated on 2026-09-14

--- features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/ui/TokensDataStateExpress.kt
+++ features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/ui/TokensDataStateExpress.kt
@@ -1,6 +1,5 @@
 package com.tangem.feature.swap.domain.models.ui
 
-import com.tangem.domain.tokens.model.CryptoCurrency
 import com.tangem.feature.swap.domain.models.domain.CryptoCurrencySwapInfo
 
 data class TokensDataStateExpress(
@@ -11,19 +10,4 @@ data class TokensDataStateExpress(
 data class CurrenciesGroup(
     val available: List<CryptoCurrencySwapInfo>,
     val unavailable: List<CryptoCurrencySwapInfo>,
-)
-
-data class FoundTokensStateExpress(
-    val tokensInWallet: List<TokenWithBalanceExpress>,
-    val loadedTokens: List<TokenWithBalanceExpress>,
-)
-
-data class TokenWithBalanceExpress(
-    val token: CryptoCurrency,
-    val tokenBalanceData: TokenBalanceDataExpress? = null,
-)
-
-data class TokenBalanceDataExpress(
-    val amount: String?,
-    val amountEquivalent: String?,
 )
