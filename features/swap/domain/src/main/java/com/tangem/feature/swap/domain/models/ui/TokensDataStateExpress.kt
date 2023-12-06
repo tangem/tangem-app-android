0f260d2aa0274e0d474bec60d85bfbd0a996550c
Tangem <git@tangem.com>
2023-12-06 17:35:18 +0200

    Updated on 2025-11-26

--- features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/ui/TokensDataStateExpress.kt
+++ features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/ui/TokensDataStateExpress.kt
@@ -5,7 +5,15 @@ import com.tangem.feature.swap.domain.models.domain.CryptoCurrencySwapInfo
 data class TokensDataStateExpress(
     val fromGroup: CurrenciesGroup,
     val toGroup: CurrenciesGroup,
-)
+) {
+    companion object {
+        val EMPTY =
+            TokensDataStateExpress(
+                fromGroup = CurrenciesGroup(emptyList(), emptyList()),
+                toGroup = CurrenciesGroup(emptyList(), emptyList()),
+            )
+    }
+}
 
 data class CurrenciesGroup(
     val available: List<CryptoCurrencySwapInfo>,
