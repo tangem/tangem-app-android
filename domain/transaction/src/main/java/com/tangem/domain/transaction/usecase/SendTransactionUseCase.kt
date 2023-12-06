5777e985a53789b06df433d674c804c63b406167
Tangem <git@tangem.com>
2023-12-06 16:33:25 +0300

    release/5.26.1

--- domain/transaction/src/main/java/com/tangem/domain/transaction/usecase/SendTransactionUseCase.kt
+++ domain/transaction/src/main/java/com/tangem/domain/transaction/usecase/SendTransactionUseCase.kt
@@ -3,13 +3,17 @@ package com.tangem.domain.transaction.usecase
 import arrow.core.Either
 import arrow.core.left
 import arrow.core.right
+import com.tangem.blockchain.common.BlockchainSdkError
 import com.tangem.blockchain.common.TransactionData
 import com.tangem.blockchain.extensions.SimpleResult
+import com.tangem.blockchain.network.ResultChecker
+import com.tangem.common.core.TangemSdkError
 import com.tangem.domain.card.repository.CardSdkConfigRepository
 import com.tangem.domain.common.TapWorkarounds.isStart2Coin
 import com.tangem.domain.demo.IsDemoCardUseCase
 import com.tangem.domain.tokens.model.Network
 import com.tangem.domain.transaction.error.SendTransactionError
+import com.tangem.domain.transaction.error.SendTransactionError.Companion.USER_CANCELLED_ERROR_CODE
 import com.tangem.domain.walletmanager.WalletManagersFacade
 import com.tangem.domain.wallets.models.UserWallet
 
@@ -52,10 +56,40 @@ class SendTransactionUseCase(
             ifRight = { result ->
                 when (result) {
                     is SimpleResult.Success -> true.right()
-                    is SimpleResult.Failure -> SendTransactionError.NetworkError(result.error.message).left()
+                    is SimpleResult.Failure -> handleError(result).left()
                 }
             },
             ifLeft = { it.left() },
         )
+    }
+
+    private fun handleError(result: SimpleResult.Failure): SendTransactionError {
+        if (ResultChecker.isNetworkError(result)) return SendTransactionError.NetworkError(result.error.message)
+        val error = result.error as? BlockchainSdkError ?: return SendTransactionError.UnknownError()
+        when (error) {
+            is BlockchainSdkError.WrappedTangemError -> {
+                val errorByCode = mapErrorByCode(error)
+                if (errorByCode != null) {
+                    return errorByCode
+                }
+                val tangemSdkError = error.tangemError as? TangemSdkError ?: return SendTransactionError.UnknownError()
+                if (tangemSdkError is TangemSdkError.UserCancelled) return SendTransactionError.UserCancelledError
+                return SendTransactionError.TangemSdkError(tangemSdkError.code, tangemSdkError.cause)
+            }
+            else -> {
+                return SendTransactionError.TangemSdkError(error.code, error.cause)
+            }
+        }
+    }
+
+    private fun mapErrorByCode(error: BlockchainSdkError.WrappedTangemError): SendTransactionError? {
+        return when (error.code) {
+            USER_CANCELLED_ERROR_CODE -> {
+                return SendTransactionError.UserCancelledError
+            }
+            else -> {
+                null
+            }
+        }
     }
 }
