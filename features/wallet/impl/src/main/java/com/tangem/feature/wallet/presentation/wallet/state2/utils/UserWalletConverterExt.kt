14f2994a64c82cfb721d4830068f900a5ad16f49
Anton Tkachev <iiiburnyiii@gmail.com>
2023-12-06 11:48:07 +0400

    AND-5207 [Wallet] Updated state transformers with VISA wallet state

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state2/utils/UserWalletConverterExt.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state2/utils/UserWalletConverterExt.kt
@@ -7,10 +7,17 @@ import com.tangem.feature.wallet.presentation.wallet.state2.WalletState
 internal inline fun UserWallet.createStateByWalletType(
     multiCurrencyCreator: () -> WalletState.MultiCurrency,
     singleCurrencyCreator: () -> WalletState.SingleCurrency,
-): WalletState {
-    return if (isWalletWithTokens()) multiCurrencyCreator() else singleCurrencyCreator()
+    visaWalletCreator: () -> WalletState.Visa,
+): WalletState = when {
+    isVisaWallet() -> visaWalletCreator()
+    isWalletWithTokens() -> multiCurrencyCreator()
+    else -> singleCurrencyCreator()
 }
 
 private fun UserWallet.isWalletWithTokens(): Boolean {
     return isMultiCurrency || scanResponse.cardTypesResolver.isSingleWalletWithToken()
+}
+
+private fun UserWallet.isVisaWallet(): Boolean {
+    return scanResponse.cardTypesResolver.isVisaWallet()
 }
