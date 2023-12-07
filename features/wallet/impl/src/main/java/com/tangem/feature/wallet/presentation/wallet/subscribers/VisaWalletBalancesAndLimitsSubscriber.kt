1504b5817e8b270134a7a17902c493825b45ad23
Tangem <git@tangem.com>
2023-12-07 19:52:50 +0400

    Updated on 2026-03-25

---
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/subscribers/VisaWalletBalancesAndLimitsSubscriber.kt
@@ -0,0 +1,23 @@
+package com.tangem.feature.wallet.presentation.wallet.subscribers
+
+import com.tangem.domain.wallets.models.UserWallet
+import com.tangem.feature.wallet.presentation.wallet.state2.WalletStateHolderV2
+import com.tangem.feature.wallet.presentation.wallet.state2.transformers.SetBalancesAndLimitsTransformer
+import com.tangem.feature.wallet.presentation.wallet.viewmodels.intents.WalletClickIntentsV2
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+
+internal class VisaWalletBalancesAndLimitsSubscriber(
+    private val userWallet: UserWallet,
+    private val stateHolder: WalletStateHolderV2,
+    private val clickIntents: WalletClickIntentsV2,
+) : WalletSubscriber() {
+
+    // TODO: Implement in [REDACTED_JIRA]
+    override fun create(coroutineScope: CoroutineScope): Flow<*> = suspend {
+        delay(timeMillis = 500)
+        stateHolder.update(SetBalancesAndLimitsTransformer(userWallet, clickIntents))
+    }.asFlow()
+}
