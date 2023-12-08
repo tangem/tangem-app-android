2b5bd8bedc7f0c969aa448ca01fe82dbcc48e7d1
Vlad Kozarez <vladks32@gmail.com>
2023-12-08 18:05:57 +0300

    AND-5499 show unavailable provider

--- features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/ui/TokensDataStateExpress.kt
+++ features/swap/domain/src/main/java/com/tangem/feature/swap/domain/models/ui/TokensDataStateExpress.kt
@@ -1,16 +1,19 @@
 package com.tangem.feature.swap.domain.models.ui
 
 import com.tangem.feature.swap.domain.models.domain.CryptoCurrencySwapInfo
+import com.tangem.feature.swap.domain.models.domain.SwapProvider
 
 data class TokensDataStateExpress(
     val fromGroup: CurrenciesGroup,
     val toGroup: CurrenciesGroup,
+    val allProviders: List<SwapProvider>,
 ) {
     companion object {
         val EMPTY =
             TokensDataStateExpress(
                 fromGroup = CurrenciesGroup(emptyList(), emptyList()),
                 toGroup = CurrenciesGroup(emptyList(), emptyList()),
+                allProviders = emptyList(),
             )
     }
 }
