0a0bfdc2062b4c773ba85920e693072c59aeae17
Ilya Shulgin <shulgin.ilya.i@gmail.com>
2023-12-11 11:13:58 +0200

    AND-5518 Added express exception handling.

--- features/swap/data/src/main/java/com/tangem/feature/swap/SwapRepositoryImpl.kt
+++ features/swap/data/src/main/java/com/tangem/feature/swap/SwapRepositoryImpl.kt
@@ -9,6 +9,7 @@ import com.tangem.blockchain.common.Blockchain
 import com.tangem.blockchain.common.Token
 import com.tangem.blockchain.extensions.Result
 import com.tangem.data.tokens.utils.CryptoCurrencyFactory
+import com.tangem.datasource.api.common.response.ApiResponse
 import com.tangem.datasource.api.common.response.ApiResponseError
 import com.tangem.datasource.api.common.response.getOrThrow
 import com.tangem.datasource.api.express.TangemExpressApi
@@ -28,6 +29,7 @@ import com.tangem.domain.wallets.models.UserWalletId
 import com.tangem.feature.swap.converters.*
 import com.tangem.feature.swap.domain.SwapRepository
 import com.tangem.feature.swap.domain.models.DataError
+import com.tangem.feature.swap.domain.models.ExpressException
 import com.tangem.feature.swap.domain.models.createFromAmountWithOffset
 import com.tangem.feature.swap.domain.models.data.AggregatedSwapDataModel
 import com.tangem.feature.swap.domain.models.domain.*
@@ -62,36 +64,45 @@ internal class SwapRepositoryImpl @Inject constructor(
         currencyList: List<CryptoCurrency>,
     ): List<SwapPairLeast> {
         return withContext(coroutineDispatcher.io) {
-            val initial = NetworkLeastTokenInfo(
-                contractAddress = initialCurrency.contractAddress,
-                network = initialCurrency.network,
-            )
-            val currenciesList = currencyList.map { leastTokenInfoConverter.convert(it) }
-
-            val pairs = async {
-                getPairsInternal(
-                    from = arrayListOf(initial),
-                    to = currenciesList,
+            try {
+                val initial = NetworkLeastTokenInfo(
+                    contractAddress = initialCurrency.contractAddress,
+                    network = initialCurrency.network,
                 )
+                val currenciesList = currencyList.map { leastTokenInfoConverter.convert(it) }
+
+                val pairs = async {
+                    getPairsInternal(
+                        from = arrayListOf(initial),
+                        to = currenciesList,
+                    )
+                }
+
+                val reversedPairs = async {
+                    getPairsInternal(
+                        from = currenciesList,
+                        to = arrayListOf(initial),
+                    )
+                }
+
+                val allPairs = pairs.await() + reversedPairs.await()
+
+                val providers = tangemExpressApi.getProviders().getOrThrow()
+
+                return@withContext swapPairInfoConverter.convert(
+                    SwapPairsWithProviders(
+                        swapPair = allPairs,
+                        providers = providers,
+                    ),
+                )
+            } catch (exception: Exception) {
+                if (exception is ApiResponseError.HttpException) {
+                    throw ExpressException(errorsDataConverter.convert(exception.errorBody?: ""))
+                } else {
+                    throw exception
+                }
             }
 
-            val reversedPairs = async {
-                getPairsInternal(
-                    from = currenciesList,
-                    to = arrayListOf(initial),
-                )
-            }
-
-            val allPairs = pairs.await() + reversedPairs.await()
-
-            val providers = tangemExpressApi.getProviders().getOrThrow()
-
-            return@withContext swapPairInfoConverter.convert(
-                SwapPairsWithProviders(
-                    swapPair = allPairs,
-                    providers = providers,
-                ),
-            )
         }
     }
 
