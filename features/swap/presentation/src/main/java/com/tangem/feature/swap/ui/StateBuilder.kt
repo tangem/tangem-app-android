475d52fd8b966adb0dc9cd6ac0a0ec679cfcc44b
Tangem <git@tangem.com>
2023-12-13 12:06:36 +0300

    Updated on 2026-09-23

--- features/swap/presentation/src/main/java/com/tangem/feature/swap/ui/StateBuilder.kt
+++ features/swap/presentation/src/main/java/com/tangem/feature/swap/ui/StateBuilder.kt
@@ -12,11 +12,11 @@ import com.tangem.domain.appcurrency.model.AppCurrency
 import com.tangem.domain.tokens.model.CryptoCurrency
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.feature.swap.converters.TokensDataConverter
-import com.tangem.feature.swap.domain.models.domain.*
-import com.tangem.feature.swap.domain.models.ui.*
 import com.tangem.feature.swap.domain.models.DataError
 import com.tangem.feature.swap.domain.models.SwapAmount
+import com.tangem.feature.swap.domain.models.domain.*
 import com.tangem.feature.swap.domain.models.formatToUIRepresentation
+import com.tangem.feature.swap.domain.models.ui.*
 import com.tangem.feature.swap.models.*
 import com.tangem.feature.swap.models.states.*
 import com.tangem.feature.swap.presentation.R
@@ -651,9 +651,17 @@ internal class StateBuilder(
     }
 
     fun loadingPermissionState(uiState: SwapStateHolder): SwapStateHolder {
+        val warnings = uiState.warnings.filterNot { it is SwapWarning.PermissionNeeded }.toMutableList()
+        warnings.add(
+            0,
+            SwapWarning.TransactionInProgressWarning(
+                title = resourceReference(R.string.swapping_pending_transaction_title),
+                description = resourceReference(R.string.swapping_pending_transaction_subtitle),
+            ),
+        )
         return uiState.copy(
             permissionState = SwapPermissionState.InProgress,
-            warnings = uiState.warnings.filterNot { it is SwapWarning.PermissionNeeded },
+            warnings = warnings,
         )
     }
 
