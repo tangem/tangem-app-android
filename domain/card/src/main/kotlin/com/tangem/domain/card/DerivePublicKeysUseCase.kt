ca3d5b1f0909eed5821f0a4e20dee45ff8c74e0c
Tangem <git@tangem.com>
2023-12-15 15:08:07 +0300

    Updated on 2026-02-11

--- domain/card/src/main/kotlin/com/tangem/domain/card/DerivePublicKeysUseCase.kt
+++ domain/card/src/main/kotlin/com/tangem/domain/card/DerivePublicKeysUseCase.kt
@@ -3,12 +3,18 @@ package com.tangem.domain.card
 import arrow.core.Either
 import com.tangem.common.extensions.ByteArrayKey
 import com.tangem.crypto.hdWallet.DerivationPath
+import com.tangem.domain.tokens.model.CryptoCurrency
+import com.tangem.domain.wallets.models.UserWalletId
 import com.tangem.operations.derivation.DerivationTaskResponse
 
+// FIXME: Cover with tests [REDACTED_JIRA]
 interface DerivePublicKeysUseCase {
 
+    @Deprecated(message = "Use invoke(cardId: String?, derivations: Map<ByteArrayKey, List<DerivationPath>>) instead")
     suspend operator fun invoke(
         cardId: String? = null,
         derivations: Map<ByteArrayKey, List<DerivationPath>>,
     ): Either<Unit, DerivationTaskResponse>
+
+    suspend operator fun invoke(userWalletId: UserWalletId, currencies: List<CryptoCurrency>): Either<Throwable, Unit>
 }
