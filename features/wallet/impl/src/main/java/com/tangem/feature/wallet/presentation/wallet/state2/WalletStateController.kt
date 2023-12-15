43c8cb47a4c9148a1f04bc6aa7b0f3436ab9d332
Tangem <git@tangem.com>
2023-12-15 11:52:00 +0400

    Updated on 2026-08-14

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state2/WalletStateController.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state2/WalletStateController.kt
@@ -1,11 +1,14 @@
 package com.tangem.feature.wallet.presentation.wallet.state2
 
+import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfigContent
 import com.tangem.core.ui.event.consumedEvent
 import com.tangem.domain.wallets.models.UserWalletId
 import com.tangem.feature.wallet.presentation.wallet.state.components.WalletTopBarConfig
 import com.tangem.feature.wallet.presentation.wallet.state2.model.NOT_INITIALIZED_WALLET_INDEX
 import com.tangem.feature.wallet.presentation.wallet.state2.model.WalletScreenState
 import com.tangem.feature.wallet.presentation.wallet.state2.model.WalletState
+import com.tangem.feature.wallet.presentation.wallet.state2.transformers.CloseBottomSheetTransformer
+import com.tangem.feature.wallet.presentation.wallet.state2.transformers.OpenBottomSheetTransformer
 import com.tangem.feature.wallet.presentation.wallet.state2.transformers.WalletScreenStateTransformer
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -45,6 +48,16 @@ internal class WalletStateController @Inject constructor() {
 
     fun getSelectedWalletId(): UserWalletId {
         return with(value) { wallets[selectedWalletIndex].walletCardState.id }
+    }
+
+    fun showBottomSheet(content: TangemBottomSheetConfigContent, userWalletId: UserWalletId = getSelectedWalletId()) {
+        update(
+            OpenBottomSheetTransformer(
+                userWalletId = userWalletId,
+                content = content,
+                onDismissBottomSheet = { update(CloseBottomSheetTransformer(userWalletId)) },
+            ),
+        )
     }
 
     private fun getInitialState(): WalletScreenState {
