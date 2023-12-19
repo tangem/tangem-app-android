be96bc1d9d20254417d185f90fffe59b6ed58330
Tangem <git@tangem.com>
2023-12-19 20:29:07 +0200

    release/5.26.2

---
+++ core/datasource/src/main/java/com/tangem/datasource/di/StatusCodeInterceptor.kt
@@ -0,0 +1,49 @@
+package com.tangem.datasource.di
+
+import okhttp3.Interceptor
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.Response
+import okhttp3.ResponseBody.Companion.toResponseBody
+import timber.log.Timber
+
+class StatusCodeInterceptor : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalResponse = chain.proceed(chain.request())
+
+
+        if (shouldInterceptResponse(originalResponse)) {
+            Timber.e("StatusCodeInterceptor INTERCEPTED%s", originalResponse.request.url.toString())
+
+            val body = getBody().toResponseBody("application/json".toMediaTypeOrNull())
+            val code = getCode()
+
+            return originalResponse.newBuilder()
+                .code(code)
+                .body(body)
+                .build()
+        }
+
+        return originalResponse
+    }
+
+    private fun shouldInterceptResponse(response: Response): Boolean {
+        return response.request.url.toString().contains("exchange-quote")
+            // && response.request.url.toString().contains("changenow")
+    }
+
+
+    private fun getCode() : Int {
+        return 400
+    }
+
+    private fun getBody() : String {
+        return "\"error\": {\n" +
+            "        \"code\": 2290,\n" +
+            "        \"description\": \"Core: receivedDecimals is not equal to expressDecimals\",\n" +
+            "        \"message\": \"Not valid\",\n" +
+            "        \"receivedToDecimals\": 5,\n" +
+            "        \"expressToDecimals\": 5\n" +
+            "    }"
+    }
+}
