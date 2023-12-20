542b34102cfd0d1440b453a468167035871607a2
Tangem <git@tangem.com>
2023-12-20 18:44:47 +0300

    Updated on 2025-12-18

--- features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/state/fee/FeeSelectorState.kt
+++ features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/state/fee/FeeSelectorState.kt
@@ -3,19 +3,18 @@ package com.tangem.features.send.impl.presentation.state.fee
 import androidx.compose.runtime.Immutable
 import com.tangem.blockchain.common.transaction.TransactionFee
 import com.tangem.features.send.impl.presentation.state.fields.SendTextField
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
 
 @Immutable
 internal sealed class FeeSelectorState {
 
     object Loading : FeeSelectorState()
 
-    object Empty : FeeSelectorState()
-
     data class Content(
         val fees: TransactionFee,
         val selectedFee: FeeType = FeeType.MARKET,
-        val customValues: MutableStateFlow<List<SendTextField.CustomFee>> = MutableStateFlow(emptyList()),
+        val customValues: ImmutableList<SendTextField.CustomFee> = persistentListOf(),
     ) : FeeSelectorState()
 }
 
