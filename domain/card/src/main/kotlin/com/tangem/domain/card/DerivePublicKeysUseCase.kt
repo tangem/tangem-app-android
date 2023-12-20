65010fb2e7a573f9abee9af362f7897581276370
Mama1emon <andrew.khokhlove@gmail.com>
2023-12-20 13:34:54 +0300

    AND-5150 Cover DerivePublicKeysUseCase with tests

--- domain/card/src/main/kotlin/com/tangem/domain/card/DerivePublicKeysUseCase.kt
+++ domain/card/src/main/kotlin/com/tangem/domain/card/DerivePublicKeysUseCase.kt
@@ -7,9 +7,10 @@ import com.tangem.domain.tokens.model.CryptoCurrency
 import com.tangem.domain.wallets.models.UserWalletId
 import com.tangem.operations.derivation.DerivationTaskResponse
 
-// FIXME: Cover with tests https://tangem.atlassian.net/browse/AND-5150
+// TODO: Convert to class https://tangem.atlassian.net/browse/AND-5618
 interface DerivePublicKeysUseCase {
 
+    // TODO: delete https://tangem.atlassian.net/browse/AND-5618
     @Deprecated(message = "Use invoke(cardId: String?, derivations: Map<ByteArrayKey, List<DerivationPath>>) instead")
     suspend operator fun invoke(
         cardId: String? = null,
