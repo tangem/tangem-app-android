939c778030022473861a243b5d2f86b065840838
Tangem <git@tangem.com>
2023-12-22 16:56:44 +0300

    Updated on 2026-08-19

--- features/manage-tokens/impl/src/main/java/com/tangem/managetokens/ManageTokensFragment.kt
+++ features/manage-tokens/impl/src/main/java/com/tangem/managetokens/ManageTokensFragment.kt
@@ -2,15 +2,12 @@ package com.tangem.managetokens
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.tangem.core.ui.components.SystemBarsEffect
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.core.ui.screen.ComposeFragment
 import com.tangem.core.ui.theme.AppThemeModeHolder
 import com.tangem.features.managetokens.navigation.ManageTokensRouter
-import com.tangem.managetokens.presentation.managetokens.ui.ManageTokensScreen
-import com.tangem.managetokens.presentation.managetokens.viewmodels.ManageTokensViewModel
+import com.tangem.managetokens.presentation.router.InnerManageTokensRouter
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -23,23 +20,18 @@ internal class ManageTokensFragment : ComposeFragment() {
     @Inject
     lateinit var manageTokensRouter: ManageTokensRouter
 
-    // private val internalManageTokensRouter: InnerManageTokensRouter
-    //     get() = requireNotNull(manageTokensRouter as? InnerManageTokensRouter) {
-    //         "internalManageTokensRouter should be instance of InnerManageTokensRouter"
-    //     }
+    private val innerManageTokensRouter: InnerManageTokensRouter
+        get() = requireNotNull(manageTokensRouter as? InnerManageTokensRouter) {
+            "internalManageTokensRouter should be instance of InnerManageTokensRouter"
+        }
 
     @Composable
     override fun ScreenContent(modifier: Modifier) {
-        val viewModel = hiltViewModel<ManageTokensViewModel>()
-        // viewModel.router = [REDACTED_EMAIL]
-        //
-        LocalLifecycleOwner.current.lifecycle.addObserver(viewModel)
-
         val systemBarsColor = TangemTheme.colors.background.secondary
         SystemBarsEffect {
             setSystemBarsColor(systemBarsColor)
         }
 
-        ManageTokensScreen(state = viewModel.uiState)
+        innerManageTokensRouter.Initialize(viewModelStoreOwner = this)
     }
 }
