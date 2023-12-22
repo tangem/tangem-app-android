5ae5003397d8b28b11cf2b36f91bc00ec3280c40
Tangem <git@tangem.com>
2023-12-22 16:56:44 +0300

    Updated on 2025-12-30

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/repository/TokensListRepository.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/repository/TokensListRepository.kt
@@ -1,6 +1,7 @@
 package com.tangem.domain.tokens.repository
 
 import androidx.paging.PagingData
+import com.tangem.domain.tokens.model.FoundToken
 import com.tangem.domain.tokens.model.Token
 import kotlinx.coroutines.flow.Flow
 
@@ -18,4 +19,25 @@ interface TokensListRepository {
      * @throws com.tangem.datasource.api.common.response.ApiResponseError
      */
     fun getTokens(searchText: String?): Flow<PagingData<Token>>
+
+    /**
+     * Retrieves a token information with the specified contract address on the provided network.
+     *
+     * @param contractAddress   contract address of the token
+     * @param networkId         network of the token
+     * @return [FoundToken]     object containing token information or null if no token is found with the provided
+     * contract address
+     */
+    @Throws
+    suspend fun findToken(contractAddress: String, networkId: String): FoundToken?
+
+    /**
+     * Validates a contract address on a particular network.
+     *
+     * @param contractAddress   contract address of the token
+     * @param networkId         network of the token
+     * @return [Boolean]        true is address is valid (possible on the network), false if its format is not
+     * supported on the network
+     */
+    fun validateAddress(contractAddress: String, networkId: String): Boolean
 }
