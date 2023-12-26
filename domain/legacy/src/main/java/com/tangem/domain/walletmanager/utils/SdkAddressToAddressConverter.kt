262324094921661018d272aa7c3854c086e38826
Tangem <git@tangem.com>
2023-12-26 10:05:09 +0300

    Updated on 2025-11-26

---
+++ domain/legacy/src/main/java/com/tangem/domain/walletmanager/utils/SdkAddressToAddressConverter.kt
@@ -0,0 +1,24 @@
+package com.tangem.domain.walletmanager.utils
+
+import com.tangem.blockchain.common.address.AddressType
+import com.tangem.domain.walletmanager.model.Address
+import com.tangem.utils.converter.Converter
+import com.tangem.blockchain.common.address.Address as SdkAddress
+
+/**
+ * Convert [SdkAddress] to [Address]
+ *
+[REDACTED_AUTHOR]
+ */
+internal object SdkAddressToAddressConverter : Converter<SdkAddress, Address> {
+
+    override fun convert(value: SdkAddress): Address {
+        return Address(
+            value = value.value,
+            type = when (value.type) {
+                AddressType.Default -> Address.Type.Primary
+                AddressType.Legacy -> Address.Type.Secondary
+            },
+        )
+    }
+}
