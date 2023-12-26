e4441f6e4d0f67aa085ec4e2ca61db028ed6047f
Tangem <git@tangem.com>
2023-12-26 18:35:44 +0200

    Updated on 2025-12-22

--- features/qr-scanning/impl/src/main/java/com/tangem/feature/qrscanning/QrScanningFragment.kt
+++ features/qr-scanning/impl/src/main/java/com/tangem/feature/qrscanning/QrScanningFragment.kt
@@ -1,31 +1,35 @@
-package com.tangem.tap.features.details.ui.walletconnect
+package com.tangem.feature.qrscanning
 
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.*
-import androidx.activity.OnBackPressedCallback
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.google.common.util.concurrent.ListenableFuture
-import com.otaliastudios.cameraview.CameraView
-import com.tangem.core.navigation.NavigationAction
-import com.tangem.tap.features.details.redux.walletconnect.WalletConnectAction
-import com.tangem.tap.features.details.ui.walletconnect.dialogs.PreviewBinder
-import com.tangem.tap.store
-import com.tangem.wallet.R
-import com.tangem.wallet.databinding.LayoutQrScanningBinding
+import com.tangem.feature.qrscanning.impl.R
+import com.tangem.feature.qrscanning.impl.databinding.LayoutQrScanningBinding
+import com.tangem.feature.qrscanning.inner.PreviewBinder
+import dagger.hilt.android.AndroidEntryPoint
+// import com.tangem.core.navigation.NavigationAction
+// import com.tangem.tap.features.details.redux.walletconnect.WalletConnectAction
+// import com.tangem.tap.features.details.ui.walletconnect.dialogs.PreviewBinder
+// import com.tangem.tap.store
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
-internal class QrScanFragment : Fragment(R.layout.layout_qr_scanning) {
+@AndroidEntryPoint
+internal class QrScanningFragment : Fragment(R.layout.layout_qr_scanning) {
 
     private val binding: LayoutQrScanningBinding by viewBinding(LayoutQrScanningBinding::bind)
 
     private val binder = PreviewBinder()
+
+    private val viewModel by viewModels<QrScanningViewModel>()
 
     private var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>? = null
     private var cameraExecutor: ExecutorService? = null
@@ -33,14 +37,6 @@ internal class QrScanFragment : Fragment(R.layout.layout_qr_scanning) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setFitSystemWindows(fit = true)
-        activity?.onBackPressedDispatcher?.addCallback(
-            this,
-            object : OnBackPressedCallback(true) {
-                override fun handleOnBackPressed() {
-                    store.dispatch(NavigationAction.PopBackTo())
-                }
-            },
-        )
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -61,10 +57,10 @@ internal class QrScanFragment : Fragment(R.layout.layout_qr_scanning) {
                     cameraProvider = requireNotNull(cameraProvider),
                     cameraExecutor = requireNotNull(cameraExecutor),
                     onScanned = { result ->
-                        store.dispatch(NavigationAction.PopBackTo())
+                        parentFragmentManager.popBackStack()
                         setFitSystemWindows(fit = false)
                         if (result.isNotBlank()) {
-                            store.dispatch(WalletConnectAction.OpenSession(result))
+                            viewModel.onQrScanned(result)
                         }
                     },
                 )
@@ -83,11 +79,10 @@ internal class QrScanFragment : Fragment(R.layout.layout_qr_scanning) {
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        if (requestCode != CameraView.PERMISSION_REQUEST_CODE) return
+        if (requestCode != PERMISSION_REQUEST_CODE) return
 
         if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-            store.dispatch(WalletConnectAction.NotifyCameraPermissionIsRequired)
-            store.dispatch(NavigationAction.PopBackTo())
+            parentFragmentManager.popBackStack()
         }
     }
 
@@ -97,12 +92,17 @@ internal class QrScanFragment : Fragment(R.layout.layout_qr_scanning) {
     }
 
     private fun requestPermission() {
-        requestPermissions(arrayOf(Manifest.permission.CAMERA), CameraView.PERMISSION_REQUEST_CODE)
+        requestPermissions(arrayOf(Manifest.permission.CAMERA), PERMISSION_REQUEST_CODE)
     }
 
     private fun setFitSystemWindows(fit: Boolean) {
         activity?.window?.let {
             WindowCompat.setDecorFitsSystemWindows(it, fit)
         }
+    }
+
+    companion object {
+
+        fun create() = QrScanningFragment()
     }
 }
