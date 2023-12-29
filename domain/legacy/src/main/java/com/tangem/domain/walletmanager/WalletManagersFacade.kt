b446e2ee27c88f2fea644699506385966299efcc
Tangem <git@tangem.com>
2023-12-29 13:38:50 +0300

    Updated on 2026-08-19

--- domain/legacy/src/main/java/com/tangem/domain/walletmanager/WalletManagersFacade.kt
+++ domain/legacy/src/main/java/com/tangem/domain/walletmanager/WalletManagersFacade.kt
@@ -139,8 +139,40 @@ interface WalletManagersFacade {
     /**
      * Returns value which indicates if the account balance drops below the existential deposit value, it will be
      * deactivated and any remaining funds will be destroyed.
+     *
+     * [REDACTED_TODO_COMMENT]
      */
+    @Deprecated("Will be removed in future")
     suspend fun getExistentialDeposit(userWalletId: UserWalletId, network: Network): BigDecimal?
+
+    /**
+     * Returns reserve amount which is required to create an account
+     *
+     * [REDACTED_TODO_COMMENT]
+     */
+    @Deprecated("Will be removed in future")
+    suspend fun getReserveAmount(userWalletId: UserWalletId, network: Network): BigDecimal?
+
+    /**
+     * Returns true if account with [address] was reserved with minimum amount
+     *
+     * [REDACTED_TODO_COMMENT]
+     */
+    @Deprecated("Will be removed in future")
+    suspend fun checkIfAccountFunded(userWalletId: UserWalletId, network: Network, address: String): Boolean
+
+    /**
+     * Checks if transaction amount is within the UTXO limit
+     *
+     * [REDACTED_TODO_COMMENT]
+     */
+    @Deprecated("Will be removed in future")
+    suspend fun checkUtxoAmountLimit(
+        userWalletId: UserWalletId,
+        network: Network,
+        amount: BigDecimal,
+        fee: BigDecimal,
+    ): UtxoAmountLimit?
 
     @Deprecated("Will be removed in future")
     fun getAll(userWalletId: UserWalletId): Flow<List<WalletManager>>
