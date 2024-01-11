225053992c59de084266141b02129846de8c14fb
Tangem <git@tangem.com>
2024-01-11 20:50:18 +0300

    release/5.26.1

--- features/send/impl/src/main/java/com/tangem/features/send/impl/navigation/DefaultSendRouter.kt
+++ features/send/impl/src/main/java/com/tangem/features/send/impl/navigation/DefaultSendRouter.kt
@@ -7,6 +7,8 @@ import com.tangem.core.navigation.NavigationAction
 import com.tangem.core.navigation.ReduxNavController
 import com.tangem.domain.tokens.model.CryptoCurrency
 import com.tangem.domain.wallets.models.UserWalletId
+import com.tangem.feature.qrscanning.QrScanningRouter
+import com.tangem.feature.qrscanning.SourceType
 import com.tangem.features.send.impl.presentation.SendFragment
 import com.tangem.features.tokendetails.navigation.TokenDetailsRouter
 
@@ -27,6 +29,18 @@ internal class DefaultSendRouter(
                 bundle = bundleOf(
                     TokenDetailsRouter.USER_WALLET_ID_KEY to userWalletId.stringValue,
                     TokenDetailsRouter.CRYPTO_CURRENCY_KEY to currency,
+                ),
+            ),
+        )
+    }
+
+    override fun openQrCodeScanner(network: String) {
+        reduxNavController.navigate(
+            action = NavigationAction.NavigateTo(
+                screen = AppScreen.QrScanning,
+                bundle = bundleOf(
+                    QrScanningRouter.SOURCE_KEY to SourceType.SEND,
+                    QrScanningRouter.NETWORK_KEY to network,
                 ),
             ),
         )
