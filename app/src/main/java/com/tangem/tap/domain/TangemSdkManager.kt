ea3d77db8189df981a0cf7ba71f163c578738356
Tangem <git@tangem.com>
2024-01-16 23:24:09 +0300

    Updated on 2026-09-01

--- app/src/main/java/com/tangem/tap/domain/TangemSdkManager.kt
+++ app/src/main/java/com/tangem/tap/domain/TangemSdkManager.kt
@@ -257,6 +257,9 @@ class TangemSdkManager(
             filter = CardFilter(
                 allowedCardTypes = FirmwareVersion.FirmwareType.values().toList(),
                 maxFirmwareVersion = FirmwareVersion(major = 6, minor = 33),
+                batchIdFilter = CardFilter.Companion.ItemFilter.Deny(
+                    items = setOf("0027", "0030", "0031", "0035", "DA88"),
+                ),
             ),
         )
     }
