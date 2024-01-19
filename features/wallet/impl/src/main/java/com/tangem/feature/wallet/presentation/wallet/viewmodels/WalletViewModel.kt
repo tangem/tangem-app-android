4d20a3d102fad16a76c8a7c0ab7ce3c03c6390a3
Tangem <git@tangem.com>
2024-01-19 18:22:59 +0300

    Updated on 2026-06-27

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/viewmodels/WalletViewModel.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/viewmodels/WalletViewModel.kt
@@ -1231,7 +1231,6 @@ internal class WalletViewModel @Inject constructor(
                         }
                         is CryptoCurrencyStatus.NoQuote -> AnalyticsParam.CardBalanceState.NoRate
                         is CryptoCurrencyStatus.Unreachable,
-                        is CryptoCurrencyStatus.UnreachableWithoutAddresses,
                         -> AnalyticsParam.CardBalanceState.BlockchainError
                         is CryptoCurrencyStatus.MissedDerivation,
                         is CryptoCurrencyStatus.Loading,
