80624daf77cc15c21a1d3032fb94e7804cf7caa8
Tangem <git@tangem.com>
2024-01-19 16:20:25 +0300

    Updated on 2026-06-10

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/analytics/utils/WalletWarningsAnalyticsSender.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/analytics/utils/WalletWarningsAnalyticsSender.kt
@@ -1,10 +1,11 @@
 package com.tangem.feature.wallet.presentation.wallet.analytics.utils
 
 import com.tangem.core.analytics.api.AnalyticsEventHandler
+import com.tangem.core.analytics.models.AnalyticsEvent
 import com.tangem.feature.wallet.presentation.wallet.analytics.WalletScreenAnalyticsEvent.MainScreen
 import com.tangem.feature.wallet.presentation.wallet.state.components.WalletNotification
+import com.tangem.feature.wallet.presentation.wallet.state2.model.WalletState
 import dagger.hilt.android.scopes.ViewModelScoped
-import kotlinx.collections.immutable.ImmutableList
 import javax.inject.Inject
 
 @ViewModelScoped
@@ -12,29 +13,40 @@ internal class WalletWarningsAnalyticsSender @Inject constructor(
     private val analyticsEventHandler: AnalyticsEventHandler,
 ) {
 
-    fun send(warnings: ImmutableList<WalletNotification>) {
-        val analyticsEvents = warnings.mapNotNull { warning ->
-            when (warning) {
-                is WalletNotification.Critical.DevCard -> MainScreen.DevelopmentCard
-                is WalletNotification.Informational.DemoCard -> MainScreen.DemoCard
-                is WalletNotification.RateApp -> MainScreen.HowDoYouLikeTangem
-                is WalletNotification.Warning.NumberOfSignedHashesIncorrect -> MainScreen.CardSignedTransactions
-                is WalletNotification.Warning.TestNetCard -> MainScreen.TestnetCard
-                is WalletNotification.UnlockWallets -> MainScreen.WalletUnlock
-                is WalletNotification.Critical.FailedCardValidation -> MainScreen.ProductSampleCard
-                is WalletNotification.Warning.MissingBackup -> MainScreen.BackupYourWallet
-                is WalletNotification.Informational.MissingAddresses,
-                is WalletNotification.Informational.NoAccount,
-                is WalletNotification.Warning.LowSignatures,
-                is WalletNotification.Warning.NetworksUnreachable,
-                is WalletNotification.Warning.SomeNetworksUnreachable,
-                is WalletNotification.SwapPromo,
-                -> null
-            }
-        }
+    fun send(displayedUiState: WalletState?, newWarnings: List<WalletNotification>) {
+        if (newWarnings.isEmpty()) return
+        if (displayedUiState == null || displayedUiState.pullToRefreshConfig.isRefreshing) return
 
-        analyticsEvents.forEach { event ->
+        val eventsFromNewWarnings = getEvents(newWarnings)
+        val eventsFromDisplayedWarnings = getEvents(displayedUiState.warnings)
+        val eventsToSend = eventsFromNewWarnings.filter { it !in eventsFromDisplayedWarnings }
+
+        eventsToSend.forEach { event ->
             analyticsEventHandler.send(event)
+        }
+    }
+
+    private fun getEvents(warnings: List<WalletNotification>): Set<AnalyticsEvent> {
+        return warnings.mapNotNullTo(mutableSetOf(), ::getEvent)
+    }
+
+    private fun getEvent(warning: WalletNotification): AnalyticsEvent? {
+        return when (warning) {
+            is WalletNotification.Critical.DevCard -> MainScreen.DevelopmentCard
+            is WalletNotification.Critical.FailedCardValidation -> MainScreen.ProductSampleCard
+            is WalletNotification.Warning.MissingBackup -> MainScreen.BackupYourWallet
+            is WalletNotification.Warning.NumberOfSignedHashesIncorrect -> MainScreen.CardSignedTransactions
+            is WalletNotification.Warning.TestNetCard -> MainScreen.TestnetCard
+            is WalletNotification.Informational.DemoCard -> MainScreen.DemoCard
+            is WalletNotification.Informational.MissingAddresses -> MainScreen.MissingAddresses
+            is WalletNotification.RateApp -> MainScreen.HowDoYouLikeTangem
+            is WalletNotification.UnlockWallets -> MainScreen.WalletUnlock
+            is WalletNotification.Informational.NoAccount,
+            is WalletNotification.Warning.LowSignatures,
+            is WalletNotification.Warning.SomeNetworksUnreachable,
+            is WalletNotification.Warning.NetworksUnreachable,
+            is WalletNotification.SwapPromo,
+            -> null
         }
     }
 }
