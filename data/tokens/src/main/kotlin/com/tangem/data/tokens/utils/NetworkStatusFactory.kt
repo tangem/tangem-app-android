54c9c7e329bdc7e64ecf4cd391caeb4b0eb636f8
Tangem <git@tangem.com>
2024-01-19 18:22:59 +0300

    Updated on 2026-08-27

--- data/tokens/src/main/kotlin/com/tangem/data/tokens/utils/NetworkStatusFactory.kt
+++ data/tokens/src/main/kotlin/com/tangem/data/tokens/utils/NetworkStatusFactory.kt
@@ -19,9 +19,8 @@ internal class NetworkStatusFactory {
             network = network,
             value = when (result) {
                 is UpdateWalletManagerResult.MissedDerivation -> NetworkStatus.MissedDerivation
-                is UpdateWalletManagerResult.UnreachableWithoutAddresses -> NetworkStatus.UnreachableWithoutAddresses
                 is UpdateWalletManagerResult.Unreachable -> NetworkStatus.Unreachable(
-                    address = getNetworkAddress(result.selectedAddress, result.addresses),
+                    address = getNetworkAddressOrNull(result.selectedAddress, result.addresses),
                 )
                 is UpdateWalletManagerResult.NoAccount -> NetworkStatus.NoAccount(
                     address = getNetworkAddress(result.selectedAddress, result.addresses),
@@ -94,6 +93,14 @@ internal class NetworkStatusFactory {
 
     private fun createCurrentTransactions(transactions: Set<CryptoCurrencyTransaction>): Set<TxHistoryItem> {
         return transactions.mapTo(hashSetOf()) { it.txHistoryItem }
+    }
+
+    private fun getNetworkAddressOrNull(selectedAddress: String?, availableAddresses: Set<Address>?): NetworkAddress? {
+        if (selectedAddress == null || availableAddresses == null) {
+            return null
+        }
+
+        return getNetworkAddress(selectedAddress, availableAddresses)
     }
 
     private fun getNetworkAddress(selectedAddress: String, availableAddresses: Set<Address>): NetworkAddress {
