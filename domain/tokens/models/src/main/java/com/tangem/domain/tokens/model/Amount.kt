415e1fb7da06c86af14d671f77943e903a0dcd2e
Tangem <git@tangem.com>
2024-01-25 15:39:56 +0300

    Updated on 2026-08-27

---
+++ domain/tokens/models/src/main/java/com/tangem/domain/tokens/model/Amount.kt
@@ -0,0 +1,30 @@
+package com.tangem.domain.tokens.model
+
+import java.math.BigDecimal
+
+data class Amount(
+    val currencySymbol: String,
+    val value: BigDecimal? = null,
+    val decimals: Int,
+    val type: AmountType = AmountType.CoinType,
+)
+
+sealed class AmountType {
+    object CoinType : AmountType()
+    object ReserveType : AmountType()
+    data class TokenType(val token: CryptoCurrency.Token) : AmountType()
+    data class FiatType(val code: String) : AmountType()
+}
+
+/** Converts `BigDecimal` [cryptoCurrency] to [Amount] */
+fun BigDecimal.convertToAmount(cryptoCurrency: CryptoCurrency) = Amount(
+    currencySymbol = cryptoCurrency.symbol,
+    value = this,
+    decimals = cryptoCurrency.decimals,
+    type = when (cryptoCurrency) {
+        is CryptoCurrency.Coin -> AmountType.CoinType
+        is CryptoCurrency.Token -> AmountType.TokenType(
+            token = cryptoCurrency,
+        )
+    },
+)
