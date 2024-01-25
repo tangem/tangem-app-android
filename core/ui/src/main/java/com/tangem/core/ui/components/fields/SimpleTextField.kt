3e72cc84afe02d38838ddc7ba25ca323889f62aa
Tangem <git@tangem.com>
2024-01-25 15:39:56 +0300

    Updated on 2026-08-07

--- core/ui/src/main/java/com/tangem/core/ui/components/fields/SimpleTextField.kt
+++ core/ui/src/main/java/com/tangem/core/ui/components/fields/SimpleTextField.kt
@@ -3,14 +3,18 @@ package com.tangem.core.ui.components.fields
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import com.tangem.core.ui.extensions.TextReference
 import com.tangem.core.ui.extensions.resolveReference
@@ -19,6 +23,7 @@ import com.tangem.core.ui.res.TangemTheme
 /**
  * Simple text field with placeholder
  */
+@Suppress("ReusedModifierInstance")
 @Composable
 fun SimpleTextField(
     value: String,
@@ -29,32 +34,72 @@ fun SimpleTextField(
     visualTransformation: VisualTransformation = VisualTransformation.None,
     keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
     color: Color = TangemTheme.colors.text.primary1,
+    textStyle: TextStyle = TangemTheme.typography.body2.copy(color = color),
     readOnly: Boolean = false,
+    decorationBox: (@Composable (innerTextField: @Composable () -> Unit) -> Unit)? = null,
 ) {
-    val focusRequester = remember { FocusRequester() }
-    BasicTextField(
-        value = value,
-        onValueChange = onValueChange,
-        textStyle = TangemTheme.typography.body2.copy(color = color),
-        cursorBrush = SolidColor(TangemTheme.colors.text.primary1),
-        singleLine = singleLine,
-        readOnly = readOnly,
-        visualTransformation = visualTransformation,
-        keyboardOptions = keyboardOptions,
-        decorationBox = { textValue ->
-            Box {
-                if (value.isBlank() && placeholder != null) {
-                    Text(
-                        text = placeholder.resolveReference(),
-                        style = TangemTheme.typography.body2,
-                        color = TangemTheme.colors.text.disabled,
-                        modifier = Modifier,
-                    )
-                }
-                textValue()
-            }
-        },
-        modifier = modifier
-            .focusRequester(focusRequester),
+    var textFieldValueState by remember {
+        mutableStateOf(
+            TextFieldValue(
+                text = value,
+                selection = when {
+                    value.isEmpty() -> TextRange.Zero
+                    else -> TextRange(value.length, value.length)
+                },
+            ),
+        )
+    }
+    val focusRequester = remember { FocusRequester.Default }
+    val customTextSelectionColors = TextSelectionColors(
+        handleColor = TangemTheme.colors.text.secondary,
+        backgroundColor = TangemTheme.colors.text.secondary.copy(alpha = 0.4f),
     )
+
+    val textFieldValue = textFieldValueState.copy(text = value)
+
+    SideEffect {
+        if (textFieldValue.selection != textFieldValueState.selection ||
+            textFieldValue.composition != textFieldValueState.composition
+        ) {
+            textFieldValueState = textFieldValue
+        }
+    }
+
+    var lastTextValue by remember(value) { mutableStateOf(value) }
+
+    CompositionLocalProvider(LocalTextSelectionColors provides customTextSelectionColors) {
+        BasicTextField(
+            value = textFieldValue,
+            onValueChange = { newTextFieldValueState ->
+                textFieldValueState = newTextFieldValueState
+
+                val stringChangedSinceLastInvocation = lastTextValue != newTextFieldValueState.text
+                lastTextValue = newTextFieldValueState.text
+
+                if (stringChangedSinceLastInvocation) {
+                    onValueChange(newTextFieldValueState.text)
+                }
+            },
+            textStyle = textStyle.copy(color = color),
+            cursorBrush = SolidColor(TangemTheme.colors.text.primary1),
+            singleLine = singleLine,
+            readOnly = readOnly,
+            visualTransformation = visualTransformation,
+            keyboardOptions = keyboardOptions,
+            decorationBox = decorationBox ?: { textValue ->
+                Box {
+                    if (value.isBlank() && placeholder != null) {
+                        Text(
+                            text = placeholder.resolveReference(),
+                            style = textStyle,
+                            color = TangemTheme.colors.text.disabled,
+                        )
+                    }
+                    textValue()
+                }
+            },
+            modifier = modifier
+                .focusRequester(focusRequester),
+        )
+    }
 }
