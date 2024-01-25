1e80b905c36a989e2408e1f0e197594ecd5e5308
Tangem <git@tangem.com>
2024-01-25 15:39:56 +0300

    Updated on 2026-08-19

--- core/ui/src/main/java/com/tangem/core/ui/components/fields/visualtransformations/AmountVisualTransformation.kt
+++ core/ui/src/main/java/com/tangem/core/ui/components/fields/visualtransformations/AmountVisualTransformation.kt
@@ -5,28 +5,49 @@ import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.OffsetMapping
 import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.text.input.VisualTransformation
+import com.tangem.core.ui.utils.formatWithThousands
+import java.text.DecimalFormat
 
 class AmountVisualTransformation(
-    private val symbol: String,
+    private val decimals: Int,
+    private val symbol: String? = null,
+    private val decimalFormat: DecimalFormat = DecimalFormat(),
 ) : VisualTransformation {
-    override fun filter(text: AnnotatedString): TransformedText {
-        return TransformedText(
-            buildAnnotatedString {
-                append(text)
-                if (text.isNotBlank()) {
-                    append(" ")
-                    append(symbol)
-                }
-            },
-            object : OffsetMapping {
-                override fun originalToTransformed(offset: Int): Int {
-                    return text.length
-                }
 
-                override fun transformedToOriginal(offset: Int): Int {
-                    return text.length
+    override fun filter(text: AnnotatedString): TransformedText {
+        val formattedText = decimalFormat.formatWithThousands(
+            text.text,
+            decimals,
+        )
+        val groupingSymbol = decimalFormat.decimalFormatSymbols.groupingSeparator
+        return TransformedText(
+            text = buildAnnotatedString {
+                append(formattedText)
+                if (formattedText.isNotEmpty() && symbol != null) {
+                    append(" $symbol")
                 }
             },
+            offsetMapping = OffsetMappingImpl(text.text, formattedText, groupingSymbol),
         )
+    }
+
+    private class OffsetMappingImpl(
+        private val text: String,
+        private val formattedText: String,
+        private val gropingSymbol: Char,
+    ) : OffsetMapping {
+        override fun originalToTransformed(offset: Int): Int {
+            var noneDigitCount = 0
+            var i = 0
+            while (i < offset + noneDigitCount) {
+                if (formattedText.getOrNull(i++) == gropingSymbol) noneDigitCount++
+            }
+            return (offset + noneDigitCount).coerceIn(0, formattedText.length)
+        }
+
+        override fun transformedToOriginal(offset: Int): Int {
+            val noneDigitCount = formattedText.take(offset).count { it == gropingSymbol }
+            return (offset - noneDigitCount).coerceIn(0, text.length)
+        }
     }
 }
