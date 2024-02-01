84ce2aee8683ddc599a68736811cddc51df83e7b
Tangem <git@tangem.com>
2024-02-01 17:04:15 +0300

    release/5.26.1

--- features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/state/fee/SendFeeCustomFieldConverter.kt
+++ features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/state/fee/SendFeeCustomFieldConverter.kt
@@ -1,79 +1,47 @@
 package com.tangem.features.send.impl.presentation.state.fee
 
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import com.tangem.blockchain.common.transaction.Fee
-import com.tangem.core.ui.extensions.resourceReference
-import com.tangem.core.ui.extensions.stringReference
-import com.tangem.core.ui.utils.BigDecimalFormatter
 import com.tangem.domain.appcurrency.model.AppCurrency
-import com.tangem.features.send.impl.R
+import com.tangem.domain.tokens.model.CryptoCurrencyStatus
+import com.tangem.features.send.impl.presentation.state.fee.custom.EthereumCustomFeeConverter
 import com.tangem.features.send.impl.presentation.state.fields.SendTextField
 import com.tangem.features.send.impl.presentation.viewmodel.SendClickIntents
 import com.tangem.utils.Provider
 import com.tangem.utils.converter.Converter
-import com.tangem.utils.toFormattedString
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 
 internal class SendFeeCustomFieldConverter(
     private val clickIntents: SendClickIntents,
     private val appCurrencyProvider: Provider<AppCurrency>,
+    private val cryptoCurrencyStatusProvider: Provider<CryptoCurrencyStatus>,
 ) : Converter<Fee, ImmutableList<SendTextField.CustomFee>> {
 
+    private val ethereumCustomFeeConverter by lazy {
+        EthereumCustomFeeConverter(
+            clickIntents = clickIntents,
+            appCurrencyProvider = appCurrencyProvider,
+            cryptoCurrencyStatusProvider = cryptoCurrencyStatusProvider,
+        )
+    }
+
     override fun convert(value: Fee): ImmutableList<SendTextField.CustomFee> {
-        val ethereumFee = value as? Fee.Ethereum ?: return persistentListOf()
-        val appCurrency = appCurrencyProvider()
-
-        val maxFeeFiat = BigDecimalFormatter.formatFiatAmount(
-            fiatAmount = ethereumFee.amount.value,
-            fiatCurrencyCode = appCurrency.code,
-            fiatCurrencySymbol = appCurrency.symbol,
-        )
-
-        return persistentListOf(
-            SendTextField.CustomFee(
-                value = ethereumFee.amount.value?.toFormattedString(ethereumFee.amount.decimals).orEmpty(),
-                decimals = ethereumFee.amount.decimals,
-                symbol = ethereumFee.amount.currencySymbol,
-                onValueChange = { clickIntents.onCustomFeeValueChange(0, it) },
-                keyboardOptions = KeyboardOptions(
-                    imeAction = ImeAction.Next,
-                    keyboardType = KeyboardType.Number,
-                ),
-                title = resourceReference(R.string.send_max_fee),
-                footer = resourceReference(R.string.send_max_fee_footer),
-                label = stringReference(maxFeeFiat),
-            ),
-            SendTextField.CustomFee(
-                value = ethereumFee.gasPrice.toString(),
-                decimals = 0,
-                symbol = ETHEREUM_UNIT,
-                title = resourceReference(R.string.send_gas_price),
-                footer = resourceReference(R.string.send_gas_price_footer),
-                onValueChange = { clickIntents.onCustomFeeValueChange(1, it) },
-                keyboardOptions = KeyboardOptions(
-                    imeAction = ImeAction.Next,
-                    keyboardType = KeyboardType.Number,
-                ),
-            ),
-            SendTextField.CustomFee(
-                value = ethereumFee.gasLimit.toString(),
-                decimals = 0,
-                symbol = null,
-                title = resourceReference(R.string.send_gas_limit),
-                footer = resourceReference(R.string.send_gas_limit_footer),
-                onValueChange = { clickIntents.onCustomFeeValueChange(2, it) },
-                keyboardOptions = KeyboardOptions(
-                    imeAction = ImeAction.Done,
-                    keyboardType = KeyboardType.Number,
-                ),
-            ),
-        )
+        return when (value) {
+            is Fee.Ethereum -> {
+                ethereumCustomFeeConverter.convert(value)
+            }
+            else -> persistentListOf()
+        }
     }
 
-    companion object {
-        private const val ETHEREUM_UNIT = "GWEI"
-    }
+    fun onValueChange(feeSelectorState: FeeSelectorState.Content, index: Int, value: String) = feeSelectorState.copy(
+        customValues = when (feeSelectorState.fees.normal) {
+            is Fee.Ethereum -> ethereumCustomFeeConverter.onValueChange(
+                customValues = feeSelectorState.customValues,
+                index = index,
+                value = value,
+            )
+            else -> feeSelectorState.customValues
+        },
+    )
 }
