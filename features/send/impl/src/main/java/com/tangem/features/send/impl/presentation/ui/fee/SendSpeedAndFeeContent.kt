f728e7c8f6329a4e7973dc6c62c98a5195be88f4
Tangem <git@tangem.com>
2024-02-02 15:34:06 +0300

    Updated on 2026-09-14

--- features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/ui/fee/SendSpeedAndFeeContent.kt
+++ features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/ui/fee/SendSpeedAndFeeContent.kt
@@ -15,14 +15,15 @@ import com.tangem.core.ui.components.notifications.Notification
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.features.send.impl.presentation.state.SendStates
 import com.tangem.features.send.impl.presentation.state.fee.FeeSelectorState
+import com.tangem.features.send.impl.presentation.state.fee.FeeType
 import com.tangem.features.send.impl.presentation.state.fee.SendFeeNotification
 import com.tangem.features.send.impl.presentation.viewmodel.SendClickIntents
 import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
 
 private const val FEE_SELECTOR_KEY = "FEE_SELECTOR_KEY"
 private const val FEE_CUSTOM_KEY = "FEE_CUSTOM_KEY"
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 internal fun SendSpeedAndFeeContent(state: SendStates.FeeState?, clickIntents: SendClickIntents) {
     if (state == null) return
@@ -36,46 +37,92 @@ internal fun SendSpeedAndFeeContent(state: SendStates.FeeState?, clickIntents: S
                 horizontal = TangemTheme.dimens.spacing16,
             ),
     ) {
-        item(
-            key = FEE_SELECTOR_KEY,
-        ) {
-            SendSpeedSelector(
-                state = state,
-                clickIntents = clickIntents,
-                modifier = Modifier.animateItemPlacement(),
-            )
-        }
+        feeSelector(state, clickIntents)
+        topNotifications(notifications)
         customFee(feeSendState)
-        notifications(notifications)
-        subtractButton(
-            receivedAmount = state.receivedAmount,
-            isSubtract = state.isSubtract,
-            isSubtractAvailable = state.isSubtractAvailable,
-            clickIntents = clickIntents,
-        )
+        middleNotifications(notifications)
+        subtractButton(state, clickIntents)
+        bottomNotifications(notifications)
     }
 }
 
 @OptIn(ExperimentalFoundationApi::class)
-internal fun LazyListScope.notifications(configs: ImmutableList<SendFeeNotification>, modifier: Modifier = Modifier) {
+private fun LazyListScope.feeSelector(state: SendStates.FeeState, clickIntents: SendClickIntents) {
+    item(
+        key = FEE_SELECTOR_KEY,
+    ) {
+        SendSpeedSelector(
+            state = state,
+            clickIntents = clickIntents,
+            modifier = Modifier.animateItemPlacement(),
+        )
+    }
+}
+
+private fun LazyListScope.topNotifications(
+    configs: ImmutableList<SendFeeNotification>,
+    modifier: Modifier = Modifier,
+) {
+    notifications(
+        configs = configs.filter {
+            it is SendFeeNotification.Error.ExceedsBalance ||
+                it is SendFeeNotification.Warning.NetworkFeeUnreachable
+        }.toImmutableList(),
+        modifier = modifier,
+    )
+}
+
+private fun LazyListScope.middleNotifications(
+    configs: ImmutableList<SendFeeNotification>,
+    modifier: Modifier = Modifier,
+) {
+    notifications(
+        configs = configs.filter {
+            it is SendFeeNotification.Warning.TooLow ||
+                it is SendFeeNotification.Warning.TooHigh
+        }.toImmutableList(),
+        modifier = modifier,
+    )
+}
+
+private fun LazyListScope.bottomNotifications(
+    configs: ImmutableList<SendFeeNotification>,
+    modifier: Modifier = Modifier,
+) {
+    notifications(
+        configs = configs.filterIsInstance<SendFeeNotification.Warning.NetworkCoverage>().toImmutableList(),
+        isLast = true,
+        modifier = modifier,
+    )
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+private fun LazyListScope.notifications(
+    configs: ImmutableList<SendFeeNotification>,
+    modifier: Modifier = Modifier,
+    isLast: Boolean = false,
+) {
     items(
         items = configs,
         key = { it::class.java },
         contentType = { it::class.java },
         itemContent = {
+            val bottomPadding = if (isLast) TangemTheme.dimens.spacing12 else TangemTheme.dimens.spacing0
             Notification(
                 config = it.config,
                 modifier = modifier
-                    .padding(top = TangemTheme.dimens.spacing12)
+                    .padding(
+                        top = TangemTheme.dimens.spacing12,
+                        bottom = bottomPadding,
+                    )
                     .animateItemPlacement(),
                 containerColor = when (it) {
                     is SendFeeNotification.Error.ExceedsBalance,
                     is SendFeeNotification.Warning.NetworkFeeUnreachable,
-                    -> TangemTheme.colors.background.primary
+                    -> TangemTheme.colors.background.action
                     else -> TangemTheme.colors.button.disabled
                 },
                 iconTint = when (it) {
-                    is SendFeeNotification.Informational -> TangemTheme.colors.icon.accent
                     is SendFeeNotification.Error.ExceedsBalance -> {
                         if (it.config.buttonsState == null) {
                             TangemTheme.colors.icon.warning
@@ -116,20 +163,30 @@ internal fun LazyListScope.customFee(feeSendState: FeeSelectorState, modifier: M
 
 @OptIn(ExperimentalFoundationApi::class)
 internal fun LazyListScope.subtractButton(
-    receivedAmount: String,
-    isSubtract: Boolean,
-    isSubtractAvailable: Boolean,
+    state: SendStates.FeeState,
     clickIntents: SendClickIntents,
     modifier: Modifier = Modifier,
 ) {
+    val receivedAmount = state.receivedAmount
+    val isSubtract = state.isSubtract
+    val isSubtractAvailable = state.isSubtractAvailable
+    val feeSendState = state.feeSelectorState
     if (isSubtractAvailable) {
         item {
+            val feeStateContent = feeSendState as? FeeSelectorState.Content
+            val isCustomAvailable = feeStateContent?.customValues.isNullOrEmpty().not()
+            val isCustomSelected = feeStateContent?.selectedFee == FeeType.CUSTOM
+            val topPadding = if (isCustomSelected && isCustomAvailable) {
+                TangemTheme.dimens.spacing12
+            } else {
+                TangemTheme.dimens.spacing20
+            }
             SendSpeedSubtract(
                 receivingAmount = receivedAmount,
                 isSubtract = isSubtract,
                 onSelectClick = clickIntents::onSubtractSelect,
                 modifier = modifier
-                    .padding(vertical = TangemTheme.dimens.spacing12)
+                    .padding(top = topPadding)
                     .animateItemPlacement(),
             )
         }
