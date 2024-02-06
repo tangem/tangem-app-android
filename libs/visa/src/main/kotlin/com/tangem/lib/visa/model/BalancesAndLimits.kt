22f0a67dd78edbaa55d86ec874c476f50f7bc25c
Anton Tkachev <iiiburnyiii@gmail.com>
2024-02-06 22:35:29 +0300

    AND-5939 [VISA] Implemented smart-contract information provider

---
+++ libs/visa/src/main/kotlin/com/tangem/lib/visa/model/BalancesAndLimits.kt
@@ -0,0 +1,43 @@
+package com.tangem.lib.visa.model
+
+import org.joda.time.Instant
+import java.math.BigDecimal
+import java.math.BigInteger
+
+data class BalancesAndLimits(
+    val balances: Balances,
+    val oldLimits: Limits,
+    val newLimits: Limits,
+    val limitsChangeDate: Instant,
+) {
+
+    data class Balances(
+        val total: BigDecimal,
+        val verified: BigDecimal,
+        val available: Available,
+        val blocked: BigDecimal,
+        val debt: BigDecimal,
+        val pendingRefund: BigDecimal,
+    ) {
+
+        data class Available(
+            val forPayment: BigDecimal,
+            val forWithdrawal: BigDecimal,
+            val forDebtPayment: BigDecimal,
+        )
+    }
+
+    data class Limits(
+        val spendLimit: Limit,
+        val noOtpLimit: Limit,
+        val singleTransactionLimit: BigDecimal,
+        val expirationDate: Instant,
+        val spendPeriodSeconds: BigInteger,
+    ) {
+
+        data class Limit(
+            val limit: BigDecimal,
+            val spent: BigDecimal,
+        )
+    }
+}
