8165280b0d12bde3f575f05914d021429de12514
Tangem <git@tangem.com>
2024-02-07 14:14:11 +0300

    Updated on 2026-06-16

--- libs/visa/build.gradle.kts
+++ libs/visa/build.gradle.kts
@@ -1,17 +1,30 @@
 plugins {
-    alias(deps.plugins.kotlin.jvm)
+    alias(deps.plugins.android.library)
+    alias(deps.plugins.kotlin.android)
     id("configuration")
+}
+
+android {
+    namespace = "com.tangem.libs.visa"
 }
 
 dependencies {
 
     /** Project */
     implementation(projects.core.utils)
+    implementation(projects.core.datasource)
+    implementation(projects.data.common)
+
+    /** Libs - Network */
+    implementation(deps.moshi.kotlin)
+    implementation(deps.okHttp)
+    implementation(deps.okHttp.prettyLogging)
+    implementation(deps.retrofit)
+    implementation(deps.retrofit.moshi)
 
     /** Libs - Other */
     implementation(deps.web3j.core)
     implementation(deps.kotlin.coroutines)
     implementation(deps.arrow.fx)
     implementation(deps.jodatime)
-    implementation(deps.okHttp.prettyLogging)
 }
