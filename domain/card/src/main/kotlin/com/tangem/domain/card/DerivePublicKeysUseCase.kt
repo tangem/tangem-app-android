13646a8967975ef8be7f3f6241da320d8e3e6a24
Tangem <git@tangem.com>
2024-02-07 14:44:58 +0300

    release/5.26.1

--- domain/card/src/main/kotlin/com/tangem/domain/card/DerivePublicKeysUseCase.kt
+++ domain/card/src/main/kotlin/com/tangem/domain/card/DerivePublicKeysUseCase.kt
@@ -1,19 +1,17 @@
 package com.tangem.domain.card
 
 import arrow.core.Either
-import com.tangem.common.extensions.ByteArrayKey
-import com.tangem.crypto.hdWallet.DerivationPath
+import com.tangem.domain.card.repository.DerivationsRepository
 import com.tangem.domain.tokens.model.CryptoCurrency
 import com.tangem.domain.wallets.models.UserWalletId
-import com.tangem.operations.derivation.DerivationTaskResponse
-// [REDACTED_TODO_COMMENT]
-interface DerivePublicKeysUseCase {
-// [REDACTED_TODO_COMMENT]
-    @Deprecated(message = "Use invoke(cardId: String?, derivations: Map<ByteArrayKey, List<DerivationPath>>) instead")
-    suspend operator fun invoke(
-        cardId: String? = null,
-        derivations: Map<ByteArrayKey, List<DerivationPath>>,
-    ): Either<Unit, DerivationTaskResponse>
 
-    suspend operator fun invoke(userWalletId: UserWalletId, currencies: List<CryptoCurrency>): Either<Throwable, Unit>
+class DerivePublicKeysUseCase(
+    private val derivationsRepository: DerivationsRepository,
+) {
+
+    suspend operator fun invoke(userWalletId: UserWalletId, currencies: List<CryptoCurrency>): Either<Throwable, Unit> {
+        return Either.catch {
+            derivationsRepository.derivePublicKeys(userWalletId, currencies)
+        }
+    }
 }
