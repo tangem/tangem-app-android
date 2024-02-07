b74092c6d7c33a416125183bd36784ed8bb3108c
Tangem <git@tangem.com>
2024-02-07 12:15:41 +0300

    release/5.26.1

--- data/visa/src/main/kotlin/com/tangem/data/visa/di/VisaDataModule.kt
+++ data/visa/src/main/kotlin/com/tangem/data/visa/di/VisaDataModule.kt
@@ -1,7 +1,13 @@
 package com.tangem.data.visa.di
 
-import com.tangem.data.visa.DummyVisaRepository
+import com.tangem.data.common.cache.CacheRegistry
+import com.tangem.data.visa.BuildConfig
+import com.tangem.data.visa.DefaultVisaRepository
+import com.tangem.datasource.api.tangemTech.TangemTechApi
+import com.tangem.datasource.local.userwallet.UserWalletsStore
 import com.tangem.domain.visa.repository.VisaRepository
+import com.tangem.lib.visa.VisaContractInfoProvider
+import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,7 +20,23 @@ internal object VisaDataModule {
 
     @Provides
     @Singleton
-    fun provideVisaRepository(): VisaRepository {
-        return DummyVisaRepository()
+    fun provideVisaRepository(
+        tangemTechApi: TangemTechApi,
+        cacheRegistry: CacheRegistry,
+        userWalletsStore: UserWalletsStore,
+        dispatchers: CoroutineDispatcherProvider,
+    ): VisaRepository {
+        val contractInfoProvider = VisaContractInfoProvider.Builder(
+            isNetworkLoggingEnabled = BuildConfig.LOG_ENABLED,
+            dispatchers = dispatchers,
+        ).build()
+
+        return DefaultVisaRepository(
+            contractInfoProvider,
+            tangemTechApi,
+            cacheRegistry,
+            userWalletsStore,
+            dispatchers,
+        )
     }
 }
