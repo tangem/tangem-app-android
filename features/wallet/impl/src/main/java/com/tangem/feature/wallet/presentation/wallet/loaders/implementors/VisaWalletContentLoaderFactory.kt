c4f3f4438daa4b232d0cac235de36651f83ee205
Tangem <git@tangem.com>
2024-02-08 11:38:02 +0300

    Updated on 2026-04-08

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/loaders/implementors/VisaWalletContentLoaderFactory.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/loaders/implementors/VisaWalletContentLoaderFactory.kt
@@ -1,9 +1,7 @@
 package com.tangem.feature.wallet.presentation.wallet.loaders.implementors
 
-import com.tangem.domain.tokens.GetPrimaryCurrencyStatusUpdatesUseCase
-import com.tangem.domain.txhistory.usecase.GetTxHistoryItemsCountUseCase
-import com.tangem.domain.txhistory.usecase.GetTxHistoryItemsUseCase
 import com.tangem.domain.visa.GetVisaCurrencyUseCase
+import com.tangem.domain.visa.GetVisaTxHistoryUseCase
 import com.tangem.domain.wallets.models.UserWallet
 import com.tangem.feature.wallet.presentation.wallet.state2.WalletStateController
 import com.tangem.feature.wallet.presentation.wallet.viewmodels.intents.WalletClickIntentsV2
@@ -11,13 +9,10 @@ import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
 @ViewModelScoped
-@Suppress("LongParameterList")
 internal class VisaWalletContentLoaderFactory @Inject constructor(
     private val stateHolder: WalletStateController,
-    private val getPrimaryCurrencyStatusUpdatesUseCase: GetPrimaryCurrencyStatusUpdatesUseCase,
-    private val txHistoryItemsCountUseCase: GetTxHistoryItemsCountUseCase,
-    private val txHistoryItemsUseCase: GetTxHistoryItemsUseCase,
     private val getVisaCurrencyUseCase: GetVisaCurrencyUseCase,
+    private val getVisaTxHistoryUseCase: GetVisaTxHistoryUseCase,
 ) {
 
     fun create(userWallet: UserWallet, clickIntents: WalletClickIntentsV2, isRefresh: Boolean): WalletContentLoader {
@@ -25,11 +20,9 @@ internal class VisaWalletContentLoaderFactory @Inject constructor(
             userWallet = userWallet,
             clickIntents = clickIntents,
             isRefresh = isRefresh,
-            stateHolder = stateHolder,
-            getPrimaryCurrencyStatusUpdatesUseCase = getPrimaryCurrencyStatusUpdatesUseCase,
-            txHistoryItemsCountUseCase = txHistoryItemsCountUseCase,
-            txHistoryItemsUseCase = txHistoryItemsUseCase,
+            stateController = stateHolder,
             getVisaCurrencyUseCase = getVisaCurrencyUseCase,
+            getVisaTxHistoryUseCase = getVisaTxHistoryUseCase,
         )
     }
 }
