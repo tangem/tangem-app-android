9760ead774d06a39b95c50351a7225690c368c50
Tangem <git@tangem.com>
2024-02-08 14:06:22 +0300

    Updated on 2026-06-27

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/WalletAlert.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/WalletAlert.kt
@@ -1,20 +1,26 @@
 package com.tangem.feature.wallet.presentation.wallet.ui
 
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
+import com.tangem.core.ui.components.AdditionalTextInputDialogParams
 import com.tangem.core.ui.components.BasicDialog
 import com.tangem.core.ui.components.DialogButton
+import com.tangem.core.ui.components.TextInputDialog
 import com.tangem.core.ui.extensions.resolveReference
 import com.tangem.feature.wallet.impl.R
 import com.tangem.feature.wallet.presentation.wallet.state.WalletAlertState
 
 @Composable
 internal fun WalletAlert(state: WalletAlertState, onDismiss: () -> Unit) {
-    DefaultAlert(state = state, onDismiss = onDismiss)
+    when (state) {
+        is WalletAlertState.Basic -> BasicAlert(state, onDismiss)
+        is WalletAlertState.TextInput -> TextInputAlert(state, onDismiss)
+    }
 }
 
 @Composable
-private fun DefaultAlert(state: WalletAlertState, onDismiss: () -> Unit) {
+private fun BasicAlert(state: WalletAlertState.Basic, onDismiss: () -> Unit) {
     val confirmButton: DialogButton
     val dismissButton: DialogButton?
 
@@ -47,5 +53,29 @@ private fun DefaultAlert(state: WalletAlertState, onDismiss: () -> Unit) {
         onDismissDialog = onDismiss,
         title = state.title?.resolveReference(),
         dismissButton = dismissButton,
+    )
+}
+
+@Composable
+private fun TextInputAlert(state: WalletAlertState.TextInput, onDismiss: () -> Unit) {
+    var value by remember { mutableStateOf(TextFieldValue(text = state.text)) }
+
+    TextInputDialog(
+        fieldValue = value,
+        confirmButton = DialogButton(
+            title = state.confirmButtonText.resolveReference(),
+            enabled = value.text.isNotEmpty() && value.text != state.text,
+            onClick = {
+                state.onConfirmClick(value.text)
+                onDismiss()
+            },
+        ),
+        onDismissDialog = onDismiss,
+        onValueChange = { value = it },
+        title = state.title.resolveReference(),
+        dismissButton = DialogButton(title = stringResource(id = R.string.common_cancel), onClick = onDismiss),
+        textFieldParams = AdditionalTextInputDialogParams(
+            label = state.label.resolveReference(),
+        ),
     )
 }
