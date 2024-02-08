1d227216676ff860cc2d0f04bd816ff4cbfad681
Tangem <git@tangem.com>
2024-02-08 14:06:22 +0300

    Updated on 2026-09-23

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state2/transformers/InitializeWalletsTransformer.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state2/transformers/InitializeWalletsTransformer.kt
@@ -90,7 +90,7 @@ internal class InitializeWalletsTransformer(
             title = name,
             additionalInfo = WalletAdditionalInfoFactory.resolve(wallet = this),
             imageResId = WalletImageResolver.resolve(userWallet = this),
-            onRenameClick = clickIntents::onRenameClick,
+            onRenameClick = clickIntents::onRenameBeforeConfirmationClick,
             onDeleteClick = clickIntents::onDeleteBeforeConfirmationClick,
         )
     }
