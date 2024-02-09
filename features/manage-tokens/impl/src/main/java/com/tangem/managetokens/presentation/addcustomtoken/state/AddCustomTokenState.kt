4a4bf53971c59c6bf2f3317d4904139063fe2359
Tangem <git@tangem.com>
2024-02-09 13:40:19 +0100

    Updated on 2026-03-25

--- features/manage-tokens/impl/src/main/java/com/tangem/managetokens/presentation/addcustomtoken/state/AddCustomTokenState.kt
+++ features/manage-tokens/impl/src/main/java/com/tangem/managetokens/presentation/addcustomtoken/state/AddCustomTokenState.kt
@@ -1,4 +1,4 @@
-package com.tangem.managetokens.presentation.customtokens.state
+package com.tangem.managetokens.presentation.addcustomtoken.state
 
 import com.tangem.core.ui.event.StateEvent
 import com.tangem.core.ui.event.consumedEvent
