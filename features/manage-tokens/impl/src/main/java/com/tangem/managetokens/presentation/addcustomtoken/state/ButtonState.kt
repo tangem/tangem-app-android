8c6dcbae01bb4e3e3f16981d4c80685b7c5aa9b0
Tangem <git@tangem.com>
2024-02-09 13:40:19 +0100

    Updated on 2026-03-27

--- features/manage-tokens/impl/src/main/java/com/tangem/managetokens/presentation/addcustomtoken/state/ButtonState.kt
+++ features/manage-tokens/impl/src/main/java/com/tangem/managetokens/presentation/addcustomtoken/state/ButtonState.kt
@@ -1,4 +1,4 @@
-package com.tangem.managetokens.presentation.customtokens.state
+package com.tangem.managetokens.presentation.addcustomtoken.state
 
 internal data class ButtonState(
     val isEnabled: Boolean,
