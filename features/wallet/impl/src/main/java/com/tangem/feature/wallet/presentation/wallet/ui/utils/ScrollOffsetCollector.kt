412d17022318f281b894fe0b0a72a2d2a0cec006
Tangem <git@tangem.com>
2024-02-09 14:09:56 +0300

    Updated on 2026-05-19

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/utils/ScrollOffsetCollector.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/utils/ScrollOffsetCollector.kt
@@ -2,8 +2,6 @@ package com.tangem.feature.wallet.presentation.wallet.ui.utils
 
 import androidx.compose.foundation.lazy.LazyListItemInfo
 import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.runtime.State
-import com.tangem.feature.wallet.presentation.wallet.state.components.WalletsListConfig
 import kotlinx.coroutines.flow.FlowCollector
 import kotlin.math.abs
 
@@ -12,46 +10,40 @@ import kotlin.math.abs
  * If first visible item offset is greater than half item size, then change selected wallet index.
  * If last visible item offset is greater than half item size, then change selected wallet index.
  *
+ * @param selectedWalletIndex selected wallet index
  * @property lazyListState     lazy list state
- * @property walletsListConfig wallets list config
- * @property isAutoScroll      check if last scrolling is auto scroll
+ * @property onWalletChange    callback that will be invoked on wallet change
  *
 [REDACTED_AUTHOR]
  */
 internal class ScrollOffsetCollector(
+    selectedWalletIndex: Int,
     private val lazyListState: LazyListState,
-    private val walletsListConfig: WalletsListConfig,
-    private val isAutoScroll: State<Boolean>,
+    private val onWalletChange: (Int) -> Unit,
 ) : FlowCollector<List<LazyListItemInfo>> {
 
     private val LazyListItemInfo.halfItemSize
         get() = size.div(other = 2)
 
-    private var currentIndex = walletsListConfig.selectedWalletIndex
+    private var currentIndex = selectedWalletIndex
 
     override suspend fun emit(value: List<LazyListItemInfo>) {
-        // Auto scroll must not change wallet
-        if (isAutoScroll.value) {
-            currentIndex = walletsListConfig.selectedWalletIndex
-            return
-        }
-
         if (!lazyListState.isScrollInProgress || value.size <= 1) return
 
         val firstItem = value.firstOrNull() ?: return
         val lastItem = value.lastOrNull() ?: return
 
         if (abs(firstItem.offset) > firstItem.halfItemSize) {
-            onWalletChange(newIndex = firstItem.index + 1)
+            selectIndex(newIndex = firstItem.index + 1)
         } else if (abs(lastItem.offset) > lastItem.halfItemSize) {
-            onWalletChange(newIndex = lastItem.index - 1)
+            selectIndex(newIndex = lastItem.index - 1)
         }
     }
 
-    private fun onWalletChange(newIndex: Int) {
+    private fun selectIndex(newIndex: Int) {
         if (currentIndex != newIndex) {
             currentIndex = newIndex
-            walletsListConfig.onWalletChange(newIndex)
+            onWalletChange(newIndex)
         }
     }
 }
