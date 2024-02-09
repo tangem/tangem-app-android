71fbcdc8efde0d46cb9394fd1043bea9bfe66b72
Tangem <git@tangem.com>
2024-02-09 11:43:29 +0300

    Updated on 2026-09-01

---
+++ domain/visa/src/main/kotlin/com/tangem/domain/visa/model/VisaTxDetails.kt
@@ -0,0 +1,40 @@
+package com.tangem.domain.visa.model
+
+import org.joda.time.DateTime
+import java.math.BigDecimal
+import java.util.Currency
+
+data class VisaTxDetails(
+    val id: String,
+    val type: String,
+    val status: String,
+    val blockchainAmount: BigDecimal,
+    val blockchainFee: BigDecimal,
+    val transactionAmount: BigDecimal,
+    val transactionCurrencyCode: Int,
+    val merchantName: String?,
+    val merchantCity: String?,
+    val merchantCountryCode: String?,
+    val merchantCategoryCode: String?,
+    val fiatCurrency: Currency,
+    val requests: List<Request>,
+) {
+
+    data class Request(
+        val id: String,
+        val billingAmount: BigDecimal,
+        val billingCurrencyCode: Int,
+        val blockchainAmount: BigDecimal,
+        val blockchainFee: BigDecimal,
+        val errorCode: Int,
+        val requestDate: DateTime,
+        val requestStatus: String,
+        val requestType: String,
+        val transactionAmount: BigDecimal,
+        val txCurrencyCode: Int,
+        val txHash: String?,
+        val txStatus: String?,
+        val exploreUrl: String?,
+        val fiatCurrency: Currency,
+    )
+}
