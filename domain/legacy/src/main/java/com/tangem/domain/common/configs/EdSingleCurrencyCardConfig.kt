3206f11ea15ce2532d66797af8163fd2588fa98b
Vlad Kozarez <vladks32@gmail.com>
2024-02-09 15:50:51 +0300

    AND-6032 added config for old cards with ed only curve

---
+++ domain/legacy/src/main/java/com/tangem/domain/common/configs/EdSingleCurrencyCardConfig.kt
@@ -0,0 +1,22 @@
+package com.tangem.domain.common.configs
+
+import com.tangem.blockchain.common.Blockchain
+import com.tangem.common.card.EllipticCurve
+import timber.log.Timber
+
+object EdSingleCurrencyCardConfig : CardConfig {
+
+    override val mandatoryCurves: List<EllipticCurve> = listOf(EllipticCurve.Ed25519)
+
+    override fun primaryCurve(blockchain: Blockchain): EllipticCurve? {
+        return when {
+            blockchain.getSupportedCurves().contains(EllipticCurve.Ed25519) -> {
+                EllipticCurve.Ed25519
+            }
+            else -> {
+                Timber.e("Unsupported blockchain, curve not found")
+                null
+            }
+        }
+    }
+}
