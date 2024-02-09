d143f21d028d914cfdf532b35741f4665423d63f
Mama1emon <andrew.khokhlove@gmail.com>
2024-02-09 17:23:06 +0300

    AND-6055 Hide tokens: app crashes if trying to hide main token from Nodl card

--- features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/TokenDetailsTopAppBarConfig.kt
+++ features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/TokenDetailsTopAppBarConfig.kt
@@ -2,5 +2,5 @@ package com.tangem.feature.tokendetails.presentation.tokendetails.state
 
 internal data class TokenDetailsTopAppBarConfig(
     val onBackClick: () -> Unit,
-    val tokenDetailsAppBarMenuConfig: TokenDetailsAppBarMenuConfig,
+    val tokenDetailsAppBarMenuConfig: TokenDetailsAppBarMenuConfig?,
 )
