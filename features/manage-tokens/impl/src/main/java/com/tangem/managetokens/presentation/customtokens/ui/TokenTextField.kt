0dc920d0e94968ff59708d22ee3e94cce78a818a
Tangem <git@tangem.com>
2024-02-09 10:31:06 +0100

    Updated on 2026-08-07

--- features/manage-tokens/impl/src/main/java/com/tangem/managetokens/presentation/customtokens/ui/TokenTextField.kt
+++ features/manage-tokens/impl/src/main/java/com/tangem/managetokens/presentation/customtokens/ui/TokenTextField.kt
@@ -6,7 +6,11 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
@@ -20,6 +24,11 @@ internal fun TokenTextField(
     placeholder: String,
     keyboardType: KeyboardType = KeyboardType.Text,
 ) {
+    val isInitiallyComposed = remember { mutableStateOf(false) }
+    LaunchedEffect(key1 = true) {
+        isInitiallyComposed.value = true
+    }
+
     BasicTextField(
         value = state.value,
         onValueChange = state.onValueChange,
@@ -32,7 +41,12 @@ internal fun TokenTextField(
         ),
         cursorBrush = SolidColor(TangemTheme.colors.icon.primary1),
         modifier = Modifier
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .onFocusChanged {
+                if (!it.isFocused && isInitiallyComposed.value) {
+                    state.onFocusExit()
+                }
+            },
         decorationBox = { innerTextField ->
             Row(modifier = Modifier.fillMaxWidth()) {
                 if (state.value.isEmpty()) {
