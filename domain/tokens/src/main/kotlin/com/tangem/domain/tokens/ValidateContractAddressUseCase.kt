5824c74e104211dbfef10e6c3d5205e1064d9ee8
Tangem <git@tangem.com>
2024-02-09 23:26:37 +0100

    Updated on 2026-02-25

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/ValidateContractAddressUseCase.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/ValidateContractAddressUseCase.kt
@@ -3,26 +3,28 @@ package com.tangem.domain.tokens
 import arrow.core.Either
 import arrow.core.raise.catch
 import arrow.core.raise.either
-import com.tangem.domain.AddCustomTokenError
+import com.tangem.domain.tokens.error.AddCustomTokenError
 import com.tangem.domain.tokens.repository.TokensListRepository
 
-class ValidateContractAddressUseCase(private val tokensListRepository: TokensListRepository) {
+class ValidateContractAddressUseCase(
+    private val tokensListRepository: TokensListRepository,
+) {
     operator fun invoke(address: String, networkId: String): Either<AddCustomTokenError, Unit> {
         return either {
             catch(
                 block = {
-                    if (address.isEmpty()) raise(AddCustomTokenError.FieldIsEmpty)
+                    if (address.isEmpty()) raise(AddCustomTokenError.FIELD_IS_EMPTY)
 
                     if (!tokensListRepository.validateAddress(
                             contractAddress = address,
                             networkId = networkId,
                         )
                     ) {
-                        raise(AddCustomTokenError.InvalidContractAddress)
+                        raise(AddCustomTokenError.INVALID_CONTRACT_ADDRESS)
                     }
                 },
                 catch = {
-                    AddCustomTokenError.InvalidContractAddress
+                    AddCustomTokenError.INVALID_CONTRACT_ADDRESS
                 },
             )
         }
