a1d4a3af1e894e279cce383ae2adf34be9808aff
Ilya Shulgin <shulgin.ilya.i@gmail.com>
2024-02-13 16:13:21 +0100

    AND-5961 Fixed high impact logic.

--- features/swap/presentation/src/main/java/com/tangem/feature/swap/models/SwapStateHolder.kt
+++ features/swap/presentation/src/main/java/com/tangem/feature/swap/models/SwapStateHolder.kt
@@ -105,12 +105,12 @@ sealed interface SwapWarning {
     object InsufficientFunds : SwapWarning
     data class NoAvailableTokensToSwap(val notificationConfig: NotificationConfig) : SwapWarning
     data class GenericWarning(
+        val title: TextReference? = null,
         val message: TextReference? = null,
         val type: GenericWarningType = GenericWarningType.OTHER,
         val shouldWrapMessage: Boolean = false,
         val onClick: () -> Unit,
     ) : SwapWarning
-    // data class RateExpired(val onClick: () -> Unit) : SwapWarning
     data class GeneralError(val notificationConfig: NotificationConfig) : SwapWarning
     data class UnableToCoverFeeWarning(val notificationConfig: NotificationConfig) : SwapWarning
     data class GeneralWarning(val notificationConfig: NotificationConfig) : SwapWarning
