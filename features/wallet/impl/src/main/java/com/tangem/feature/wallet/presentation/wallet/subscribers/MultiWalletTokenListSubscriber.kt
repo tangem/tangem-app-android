c0bb4a4efe3b84657357ef65b16d5a95c4678d2a
Tangem <git@tangem.com>
2024-02-13 18:56:17 +0300

    Updated on 2026-09-23

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/subscribers/MultiWalletTokenListSubscriber.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/subscribers/MultiWalletTokenListSubscriber.kt
@@ -1,7 +1,9 @@
 package com.tangem.feature.wallet.presentation.wallet.subscribers
 
 import com.tangem.domain.appcurrency.GetSelectedAppCurrencyUseCase
+import com.tangem.domain.tokens.ApplyTokenListSortingUseCase
 import com.tangem.domain.tokens.GetTokenListUseCase
+import com.tangem.domain.tokens.model.TokenList
 import com.tangem.domain.wallets.models.UserWallet
 import com.tangem.feature.wallet.presentation.wallet.analytics.utils.TokenListAnalyticsSender
 import com.tangem.feature.wallet.presentation.wallet.domain.WalletWithFundsChecker
@@ -12,6 +14,7 @@ import com.tangem.feature.wallet.presentation.wallet.viewmodels.intents.WalletCl
 internal class MultiWalletTokenListSubscriber(
     private val userWallet: UserWallet,
     private val getTokenListUseCase: GetTokenListUseCase,
+    private val applyTokenListSortingUseCase: ApplyTokenListSortingUseCase,
     stateHolder: WalletStateController,
     clickIntents: WalletClickIntents,
     tokenListAnalyticsSender: TokenListAnalyticsSender,
@@ -27,4 +30,29 @@ internal class MultiWalletTokenListSubscriber(
 ) {
 
     override fun tokenListFlow(): MaybeTokenListFlow = getTokenListUseCase(userWallet.walletId)
+
+    override suspend fun onTokenListReceived(tokenList: TokenList) {
+        updateSortingIfNeeded(tokenList)
+    }
+
+    private suspend fun updateSortingIfNeeded(tokenList: TokenList) {
+        if (tokenList.totalFiatBalance is TokenList.FiatBalance.Loading ||
+            tokenList.sortedBy == TokenList.SortType.NONE
+        ) {
+            return
+        }
+
+        applyTokenListSortingUseCase(
+            userWalletId = userWallet.walletId,
+            sortedTokensIds = when (tokenList) {
+                is TokenList.GroupedByNetwork -> tokenList.groups.flatMap { group ->
+                    group.currencies.map { it.currency.id }
+                }
+                is TokenList.Ungrouped -> tokenList.currencies.map { it.currency.id }
+                is TokenList.Empty -> return
+            },
+            isGroupedByNetwork = tokenList is TokenList.GroupedByNetwork,
+            isSortedByBalance = true,
+        )
+    }
 }
