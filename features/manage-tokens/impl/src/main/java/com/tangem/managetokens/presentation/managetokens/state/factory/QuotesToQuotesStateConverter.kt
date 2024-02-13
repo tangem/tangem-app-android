0cbce620c371c4b7bce4a1e7e5deb7658e5118f1
Tangem <git@tangem.com>
2024-02-13 17:16:17 +0300

    Updated on 2026-09-14

--- features/manage-tokens/impl/src/main/java/com/tangem/managetokens/presentation/managetokens/state/factory/QuotesToQuotesStateConverter.kt
+++ features/manage-tokens/impl/src/main/java/com/tangem/managetokens/presentation/managetokens/state/factory/QuotesToQuotesStateConverter.kt
@@ -1,8 +1,9 @@
 package com.tangem.managetokens.presentation.managetokens.state.factory
 
+import com.tangem.core.ui.components.marketprice.PriceChangeType
+import com.tangem.core.ui.components.marketprice.utils.PriceChangeConverter
 import com.tangem.core.ui.utils.BigDecimalFormatter
 import com.tangem.domain.tokens.model.Quote
-import com.tangem.managetokens.presentation.managetokens.state.PriceChangeType
 import com.tangem.managetokens.presentation.managetokens.state.QuotesState
 import com.tangem.utils.converter.Converter
 import kotlinx.collections.immutable.persistentListOf
@@ -16,19 +17,18 @@ internal class QuotesToQuotesStateConverter : Converter<Quote, QuotesState> {
             priceChange = BigDecimalFormatter.formatPercent(
                 percent = priceChange.movePointLeft(2),
                 useAbsoluteValue = true,
-                maxFractionDigits = 1,
-                minFractionDigits = 1,
             ),
             changeType = priceChange.getPriceChangeType(),
             chartData = // TODO (in [REDACTED_TASK_KEY] when endpoint is ready)
             when (priceChange.getPriceChangeType()) {
                 PriceChangeType.UP -> persistentListOf(0f, 5f, 10f, 30f)
                 PriceChangeType.DOWN -> persistentListOf(15f, 12f, 13f, 18f, 10f, 3f)
+                PriceChangeType.NEUTRAL -> persistentListOf(0f, 0f, 0f, 0f)
             },
         )
     }
 
     private fun BigDecimal.getPriceChangeType(): PriceChangeType {
-        return if (this >= BigDecimal.ZERO) PriceChangeType.UP else PriceChangeType.DOWN
+        return PriceChangeConverter.fromBigDecimal(value = this)
     }
 }
