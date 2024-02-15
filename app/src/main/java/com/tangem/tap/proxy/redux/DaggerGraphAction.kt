cf7f410ade5b0844e63d6c83e45ba0afc361f9c6
Tangem <git@tangem.com>
2024-02-15 10:13:44 +0300

    Updated on 2026-08-19

--- app/src/main/java/com/tangem/tap/proxy/redux/DaggerGraphAction.kt
+++ app/src/main/java/com/tangem/tap/proxy/redux/DaggerGraphAction.kt
@@ -3,7 +3,7 @@ package com.tangem.tap.proxy.redux
 import com.tangem.feature.qrscanning.QrScanningRouter
 import com.tangem.domain.card.ScanCardUseCase
 import com.tangem.domain.card.repository.CardSdkConfigRepository
-import com.tangem.features.managetokens.navigation.ManageTokensRouter
+import com.tangem.features.managetokens.navigation.ManageTokensUi
 import com.tangem.features.send.api.navigation.SendRouter
 import com.tangem.features.tester.api.TesterRouter
 import com.tangem.features.tokendetails.navigation.TokenDetailsRouter
@@ -19,7 +19,7 @@ sealed interface DaggerGraphAction : Action {
         val walletRouter: WalletRouter,
         val walletConnectInteractor: WalletConnectInteractor,
         val tokenDetailsRouter: TokenDetailsRouter,
-        val manageTokensRouter: ManageTokensRouter,
+        val manageTokensUi: ManageTokensUi,
         val cardSdkConfigRepository: CardSdkConfigRepository,
         val sendRouter: SendRouter,
         val qrScanningRouter: QrScanningRouter,
