d711927b765474fc39b432958efd57d70ab0a135
Tangem <git@tangem.com>
2024-02-15 10:09:13 +0100

    Updated on 2026-09-21

--- features/swap/presentation/src/main/java/com/tangem/feature/swap/ui/StateBuilder.kt
+++ features/swap/presentation/src/main/java/com/tangem/feature/swap/ui/StateBuilder.kt
@@ -25,6 +25,7 @@ import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
 import java.math.BigDecimal
 import java.math.RoundingMode
+import java.util.Locale
 import kotlin.math.min
 
 /**
@@ -1090,7 +1091,10 @@ internal class StateBuilder(
                 }
                 actions.onSelectFeeType.invoke(selectedItem)
             },
+            readMoreUrl = buildReadMoreUrl(),
             feeItems = txFeeState.toFeeItemState(),
+            readMore = resourceReference(R.string.common_fee_selector_link_description),
+            onReadMoreClick = actions.onFeeReadMoreClick,
         )
         return uiState.copy(
             bottomSheetConfig = TangemBottomSheetConfig(
@@ -1099,6 +1103,14 @@ internal class StateBuilder(
                 content = config,
             ),
         )
+    }
+
+    private fun buildReadMoreUrl(): String {
+        return buildString {
+            append(FEE_READ_MORE_URL_FIRST_PART)
+            append(getLocaleName())
+            append(FEE_READ_MORE_URL_SECOND_PART)
+        }
     }
 
     fun updateSelectedFeeBottomSheet(uiState: SwapStateHolder, selectedFee: FeeType): SwapStateHolder {
@@ -1346,12 +1358,24 @@ internal class StateBuilder(
         return text.replace(",", ".").toBigDecimalOrNull()
     }
 
+    private fun getLocaleName(): String {
+        return if (Locale.getDefault().language == "ru") {
+            RU_LOCALE
+        } else {
+            EN_LOCALE
+        }
+    }
+
     private companion object {
+        private const val RU_LOCALE = "ru"
+        private const val EN_LOCALE = "en"
         const val ADDRESS_MIN_LENGTH = 11
         const val ADDRESS_FIRST_PART_LENGTH = 7
         const val ADDRESS_SECOND_PART_LENGTH = 4
         private const val PRICE_IMPACT_THRESHOLD = 0.1
         private const val UNKNOWN_AMOUNT_SIGN = "—"
         private const val MAX_DECIMALS_TO_SHOW = 8
+        private const val FEE_READ_MORE_URL_FIRST_PART = "https://tangem.com/"
+        private const val FEE_READ_MORE_URL_SECOND_PART = "/blog/post/what-is-a-transaction-fee-and-why-do-we-need-it/"
     }
 }
