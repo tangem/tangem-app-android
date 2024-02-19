aa571e9ecbc013bd573e7160440c91dbd0a89f10
Tangem <git@tangem.com>
2024-02-19 23:53:28 +0700

    Updated on 2026-04-15

--- app/src/main/java/com/tangem/tap/features/disclaimer/DisclaimerDataProvider.kt
+++ app/src/main/java/com/tangem/tap/features/disclaimer/DisclaimerDataProvider.kt
@@ -1,12 +1,11 @@
 package com.tangem.tap.features.disclaimer
 
-import com.tangem.data.source.preferences.storage.DisclaimerPrefStorage
-
 /**
 [REDACTED_AUTHOR]
  */
 interface DisclaimerDataProvider {
     fun getLanguage(): String
     fun getCardId(): String
-    fun storage(): DisclaimerPrefStorage
+    suspend fun accept()
+    suspend fun isAccepted(): Boolean
 }
