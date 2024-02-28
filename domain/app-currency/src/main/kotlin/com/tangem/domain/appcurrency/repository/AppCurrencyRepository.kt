415da98805ccc85cabbed714db51e7ab03121dd5
Tangem <git@tangem.com>
2024-02-28 16:31:12 +0500

    release/5.26.1

--- domain/app-currency/src/main/kotlin/com/tangem/domain/appcurrency/repository/AppCurrencyRepository.kt
+++ domain/app-currency/src/main/kotlin/com/tangem/domain/appcurrency/repository/AppCurrencyRepository.kt
@@ -5,9 +5,14 @@ import kotlinx.coroutines.flow.Flow
 
 interface AppCurrencyRepository {
 
+    /**
+     * Get Selected App Currency from cache or load from network.
+     */
     fun getSelectedAppCurrency(): Flow<AppCurrency>
 
     suspend fun getAvailableAppCurrencies(): List<AppCurrency>
 
     suspend fun changeAppCurrency(currencyCode: String)
+
+    suspend fun fetchDefaultAppCurrency(isRefresh: Boolean = false)
 }
