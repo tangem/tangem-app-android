0b56cb903b58b3bfd5bf77daad7c84bd5860ab7b
Tangem <git@tangem.com>
2024-02-28 16:41:28 +0000

    Updated on 2026-09-25

--- app/src/main/java/com/tangem/tap/features/send/ui/dialogs/RequestFeeErrorDialog.kt
+++ app/src/main/java/com/tangem/tap/features/send/ui/dialogs/RequestFeeErrorDialog.kt
@@ -2,6 +2,8 @@ package com.tangem.tap.features.send.ui.dialogs
 
 import android.content.Context
 import androidx.appcompat.app.AlertDialog
+import com.tangem.core.analytics.Analytics
+import com.tangem.core.analytics.models.Basic
 import com.tangem.tap.common.feedback.SendTransactionFailedEmail
 import com.tangem.tap.common.redux.global.GlobalAction
 import com.tangem.tap.features.send.redux.SendAction
@@ -19,6 +21,7 @@ object RequestFeeErrorDialog {
             setTitle(R.string.common_fee_error)
             setMessage(context.getString(R.string.alert_failed_to_send_transaction_message, errorMessage))
             setNegativeButton(R.string.details_row_title_contact_to_support) { _, _ ->
+                Analytics.send(Basic.ButtonSupport())
                 store.dispatch(GlobalAction.SendEmail(SendTransactionFailedEmail(errorMessage)))
             }
             setPositiveButton(R.string.common_retry) { _, _ -> dialog.onRetry() }
