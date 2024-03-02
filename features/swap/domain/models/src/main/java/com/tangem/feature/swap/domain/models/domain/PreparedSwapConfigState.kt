fc48636df040277525456dd035f2c26912424798
Tangem <git@tangem.com>
2024-03-02 15:36:13 +0500

    Updated on 2026-09-25

--- features/swap/domain/models/src/main/java/com/tangem/feature/swap/domain/models/domain/PreparedSwapConfigState.kt
+++ features/swap/domain/models/src/main/java/com/tangem/feature/swap/domain/models/domain/PreparedSwapConfigState.kt
@@ -7,19 +7,18 @@ import com.tangem.feature.swap.domain.models.SwapAmount
  *
  * @property isAllowedToSpend shows is token allowed to spend
  * @property isBalanceEnough shows is balance of token enough
- * @property isFeeEnough shows is amount of main coin enough for fee
  */
 // todo Refactor this state
 data class PreparedSwapConfigState(
     val isAllowedToSpend: Boolean,
     val isBalanceEnough: Boolean,
-    val isFeeEnough: Boolean,
+    val feeState: SwapFeeState,
     val hasOutgoingTransaction: Boolean,
     val includeFeeInAmount: IncludeFeeInAmount,
 )
 
 sealed class IncludeFeeInAmount {
     data class Included(val amountSubtractFee: SwapAmount) : IncludeFeeInAmount()
-    object Excluded : IncludeFeeInAmount()
-    object BalanceNotEnough : IncludeFeeInAmount()
+    data object Excluded : IncludeFeeInAmount()
+    data object BalanceNotEnough : IncludeFeeInAmount()
 }
