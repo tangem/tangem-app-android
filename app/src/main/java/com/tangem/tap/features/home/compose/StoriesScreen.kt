8cfc9bfea448ccfa8aa14f014fba51524ffcb77e
Tangem <git@tangem.com>
2024-03-04 08:15:00 +0600

    Updated on 2026-06-24

--- app/src/main/java/com/tangem/tap/features/home/compose/StoriesScreen.kt
+++ app/src/main/java/com/tangem/tap/features/home/compose/StoriesScreen.kt
@@ -15,11 +15,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.datasource.CollectionPreviewParameterProvider
 import com.tangem.core.ui.res.TangemTheme
+import com.tangem.tap.common.compose.resources.C
 import com.tangem.tap.features.home.compose.content.*
 import com.tangem.tap.features.home.compose.views.HomeButtons
 import com.tangem.tap.features.home.compose.views.SearchCurrenciesButton
@@ -55,7 +57,7 @@ fun StoriesScreen(
     }
 
     StoriesScreenContent(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize().testTag(C.Tag.STORIES_SCREEN),
         config = StoriesScreenContentConfig(
             storiesSize = state.stories.lastIndex,
             currentStoryIndex = currentStoryIndex,
