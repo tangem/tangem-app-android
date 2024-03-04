17d79fc8d888d1932fc1d235c4f0380f049f3a6c
Ilya Shulgin <shulgin.ilya.i@gmail.com>
2024-03-04 18:31:50 +0100

    AND-6184 Added comment.

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/RequiresHardenedDerivationOnlyUseCase.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/RequiresHardenedDerivationOnlyUseCase.kt
@@ -6,6 +6,9 @@ import arrow.core.raise.either
 import com.tangem.domain.tokens.repository.NetworksCompatibilityRepository
 import com.tangem.domain.wallets.models.UserWalletId
 
+/**
+ * Ed25519Slip0010 requires only hardened derivations on Wallet 2
+ */
 class RequiresHardenedDerivationOnlyUseCase(
     private val repository: NetworksCompatibilityRepository
 ) {
