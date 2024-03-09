94ead7cc27704514964cb6da7af41e1c83c39864
Tangem <git@tangem.com>
2024-03-09 09:51:23 +0800

    Updated on 2026-08-19

--- domain/feedback/src/main/java/com/tangem/domain/feedback/models/BlockchainInfo.kt
+++ domain/feedback/src/main/java/com/tangem/domain/feedback/models/BlockchainInfo.kt
@@ -5,10 +5,20 @@ data class BlockchainInfo(
     val derivationPath: String,
     val outputsCount: String?,
     val host: String,
-    val addresses: String,
-    val explorerLink: String,
+    val addresses: Addresses,
+    val explorerLinks: Addresses,
     val tokens: List<TokenInfo>,
 ) {
+
+    sealed class Addresses {
+
+        data class Single(val value: String) : Addresses()
+
+        data class Multiple(val values: List<AddressInfo>) : Addresses() {
+
+            data class AddressInfo(val type: String, val value: String)
+        }
+    }
 
     data class TokenInfo(
         val id: String?,
