feb9b22564f52b4f554630cd15617a3832a216b3
Tangem <git@tangem.com>
2024-03-09 09:51:23 +0800

    Updated on 2025-09-01

--- domain/settings/src/main/java/com/tangem/domain/settings/repositories/SettingsRepository.kt
+++ domain/settings/src/main/java/com/tangem/domain/settings/repositories/SettingsRepository.kt
@@ -1,7 +1,5 @@
 package com.tangem.domain.settings.repositories
 
-import com.tangem.domain.settings.models.AppLogsModel
-
 interface SettingsRepository {
 
     suspend fun shouldShowSaveUserWalletScreen(): Boolean
@@ -9,9 +7,6 @@ interface SettingsRepository {
     suspend fun isWalletScrollPreviewEnabled(): Boolean
 
     suspend fun setWalletScrollPreviewAvailability(isEnabled: Boolean)
-
-    @Throws
-    suspend fun getAppLogs(): List<AppLogsModel>
 
     @Throws
     suspend fun updateAppLogs(message: String)
