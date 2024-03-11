a81aa6f9fb3b90a11052baf9677cf9b8a3655f99
Tangem <git@tangem.com>
2024-03-11 17:50:56 +0100

    Updated on 2026-09-23

--- features/swap/presentation/src/main/java/com/tangem/feature/swap/ui/StateBuilder.kt
+++ features/swap/presentation/src/main/java/com/tangem/feature/swap/ui/StateBuilder.kt
@@ -344,8 +344,8 @@ internal class StateBuilder(
         if (quoteModel.permissionState is PermissionDataState.PermissionLoading) {
             warnings.add(
                 SwapWarning.TransactionInProgressWarning(
-                    title = resourceReference(R.string.swapping_pending_transaction_title),
-                    description = resourceReference(R.string.swapping_pending_transaction_subtitle),
+                    title = resourceReference(R.string.warning_express_approval_in_progress_title),
+                    description = resourceReference(R.string.warning_express_approval_in_progress_message),
                 ),
             )
         } else if (quoteModel.preparedSwapConfigState.hasOutgoingTransaction) {
@@ -821,8 +821,8 @@ internal class StateBuilder(
         warnings.add(
             0,
             SwapWarning.TransactionInProgressWarning(
-                title = resourceReference(R.string.swapping_pending_transaction_title),
-                description = resourceReference(R.string.swapping_pending_transaction_subtitle),
+                title = resourceReference(R.string.warning_express_approval_in_progress_title),
+                description = resourceReference(R.string.warning_express_approval_in_progress_message),
             ),
         )
         return uiState.copy(
@@ -926,17 +926,11 @@ internal class StateBuilder(
 
     fun clearAlert(uiState: SwapStateHolder): SwapStateHolder = uiState.copy(alert = null)
 
-    fun addWarning(
-        uiState: SwapStateHolder,
-        message: TextReference?,
-        shouldWrapMessage: Boolean = false,
-        onClick: () -> Unit,
-    ): SwapStateHolder {
+    fun addWarning(uiState: SwapStateHolder, message: TextReference?, onClick: () -> Unit): SwapStateHolder {
         val renewWarnings = uiState.warnings.filterNot { it is SwapWarning.GenericWarning }.toMutableList()
         renewWarnings.add(
             SwapWarning.GenericWarning(
                 message = message,
-                shouldWrapMessage = shouldWrapMessage,
                 onClick = onClick,
             ),
         )
