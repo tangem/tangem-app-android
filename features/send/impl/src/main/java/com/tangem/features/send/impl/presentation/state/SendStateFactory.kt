38786a377e65b0ce696ff5a411bf611fca2b4752
Tangem <git@tangem.com>
2024-03-13 13:35:47 +0500

    Updated on 2025-11-26

--- features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/state/SendStateFactory.kt
+++ features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/state/SendStateFactory.kt
@@ -5,6 +5,7 @@ import com.tangem.blockchain.common.TransactionData
 import com.tangem.core.ui.components.currency.tokenicon.converter.CryptoCurrencyToIconStateConverter
 import com.tangem.core.ui.event.consumedEvent
 import com.tangem.core.ui.extensions.resourceReference
+import com.tangem.core.ui.utils.parseBigDecimal
 import com.tangem.domain.appcurrency.model.AppCurrency
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.domain.txhistory.models.TxHistoryItem
@@ -215,9 +216,20 @@ internal class SendStateFactory(
     //endregion
 
     //region send
-    fun onSubtractSelect(isSubtract: Boolean): SendUiState {
+    fun onSubtractSelect(isSubtract: Boolean, isAmountSubtractAvailable: Boolean): SendUiState {
         val state = currentStateProvider()
+        val fee = state.feeState?.fee ?: return state
+        val amountState = state.amountState ?: return state
+        val amount = amountState.amountTextField.cryptoAmount
+        val amountValue = amount.value ?: return state
+        val amountToSend = if (isSubtract && isAmountSubtractAvailable) {
+            val feeValue = fee.amount.value ?: return state
+            amountValue.minus(feeValue)
+        } else {
+            amountValue
+        }
         return state.copy(
+            amountState = amountStateConverter.convert(amountToSend.parseBigDecimal(amount.decimals)),
             sendState = state.sendState.copy(isSubtract = isSubtract),
         )
     }
