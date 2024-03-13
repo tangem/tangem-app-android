258c66d0cbd62c17c76b2fe653bd755fbb64de23
Tangem <git@tangem.com>
2024-03-13 17:42:41 +0800

    Updated on 2026-05-13

--- features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/viewmodels/TokenDetailsViewModel.kt
+++ features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/viewmodels/TokenDetailsViewModel.kt
@@ -175,9 +175,10 @@ internal class TokenDetailsViewModel @Inject constructor(
             status = cryptoCurrencyStatus ?: return,
             transactionInfo = data.let {
                 TransactionInfo(
-                    amount = it.baseCurrencyAmount,
                     transactionId = it.transactionId,
                     destinationAddress = it.depositWalletAddress,
+                    amount = it.baseCurrencyAmount,
+                    tag = it.depositWalletAddressTag,
                 )
             },
         )
