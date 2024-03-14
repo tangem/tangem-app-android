19ae00750788f8f472e32f61c356b52e6e4dccb7
Tangem <git@tangem.com>
2024-03-14 23:32:11 +0500

    Updated on 2026-05-07

--- app/src/main/java/com/tangem/tap/di/domain/QrScanningDomainModule.kt
+++ app/src/main/java/com/tangem/tap/di/domain/QrScanningDomainModule.kt
@@ -3,6 +3,7 @@ package com.tangem.tap.di.domain
 import com.tangem.domain.qrscanning.repository.QrScanningEventsRepository
 import com.tangem.domain.qrscanning.usecases.EmitQrScannedEventUseCase
 import com.tangem.domain.qrscanning.usecases.ListenToQrScanningUseCase
+import com.tangem.domain.qrscanning.usecases.ParseQrCodeUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -23,5 +24,11 @@ internal object QrScanningDomainModule {
     @Singleton
     fun provideEmitQrScannedEventUseCase(repository: QrScanningEventsRepository): EmitQrScannedEventUseCase {
         return EmitQrScannedEventUseCase(repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideParseQrCodeUseCase(repository: QrScanningEventsRepository): ParseQrCodeUseCase {
+        return ParseQrCodeUseCase(repository)
     }
 }
