66870df5a406c311a006bbe9d66153c0ab9f26b6
Mama1emon <andrew.khokhlove@gmail.com>
2024-03-17 12:35:46 +0800

    AND-6317 Use new components for sending feedback

--- app/src/main/java/com/tangem/tap/features/details/ui/details/DetailsViewModel.kt
+++ app/src/main/java/com/tangem/tap/features/details/ui/details/DetailsViewModel.kt
@@ -6,11 +6,14 @@ import com.tangem.common.extensions.guard
 import com.tangem.core.analytics.Analytics
 import com.tangem.core.navigation.AppScreen
 import com.tangem.core.navigation.NavigationAction
+import com.tangem.core.navigation.email.EmailSender
 import com.tangem.core.ui.event.StateEvent
 import com.tangem.core.ui.event.consumedEvent
 import com.tangem.core.ui.event.triggeredEvent
 import com.tangem.core.ui.extensions.TextReference
 import com.tangem.domain.common.util.cardTypesResolver
+import com.tangem.domain.feedback.FeedbackManagerFeatureToggles
+import com.tangem.domain.feedback.GetSupportFeedbackEmailUseCase
 import com.tangem.domain.wallets.repository.WalletsRepository
 import com.tangem.tap.common.analytics.events.Settings
 import com.tangem.tap.common.extensions.addContext
@@ -24,6 +27,7 @@ import com.tangem.tap.features.details.redux.DetailsState
 import com.tangem.tap.features.disclaimer.redux.DisclaimerAction
 import com.tangem.tap.features.home.LocaleRegionProvider
 import com.tangem.tap.features.home.RUSSIA_COUNTRY_CODE
+import com.tangem.tap.mainScope
 import com.tangem.tap.scope
 import com.tangem.tap.userWalletsListManager
 import com.tangem.wallet.BuildConfig
@@ -35,6 +39,7 @@ import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import org.rekotlin.Store
 import timber.log.Timber
 
@@ -42,6 +47,9 @@ import timber.log.Timber
 internal class DetailsViewModel(
     private val store: Store<AppState>,
     private val walletsRepository: WalletsRepository,
+    private val feedbackManagerFeatureToggles: FeedbackManagerFeatureToggles,
+    private val getSupportFeedbackEmailUseCase: GetSupportFeedbackEmailUseCase,
+    private val emailSender: EmailSender,
 ) {
 
     var detailsScreenState: MutableState<DetailsScreenState> = mutableStateOf(updateState(store.state.detailsState))
@@ -138,7 +146,21 @@ internal class DetailsViewModel(
 
     private fun sendFeedback() {
         Analytics.send(Settings.ButtonSendFeedback())
-        store.dispatchOnMain(GlobalAction.SendEmail(FeedbackEmail()))
+        if (feedbackManagerFeatureToggles.isLocalLogsEnabled) {
+            mainScope.launch {
+                val email = getSupportFeedbackEmailUseCase()
+                emailSender.send(
+                    email = EmailSender.Email(
+                        address = email.address,
+                        subject = email.subject,
+                        message = email.message,
+                        attachment = email.file,
+                    ),
+                )
+            }
+        } else {
+            store.dispatchOnMain(GlobalAction.SendEmail(FeedbackEmail()))
+        }
     }
 
     private fun navigateToAppSettings() {
