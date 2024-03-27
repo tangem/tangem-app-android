a6ab6a6eaf0a22d048848bdf70d21a00210a42c7
Tangem <git@tangem.com>
2024-03-27 12:43:32 +0100

    Updated on 2026-08-14

--- features/swap/presentation/src/main/java/com/tangem/feature/swap/ui/StateBuilder.kt
+++ features/swap/presentation/src/main/java/com/tangem/feature/swap/ui/StateBuilder.kt
@@ -926,7 +926,7 @@ internal class StateBuilder(
     @Suppress("LongParameterList")
     fun createSuccessState(
         uiState: SwapStateHolder,
-        txState: TxState.TxSent,
+        swapTransactionState: SwapTransactionState.TxSent,
         dataState: SwapProcessDataState,
         onExploreClick: () -> Unit,
         onStatusClick: () -> Unit,
@@ -935,8 +935,8 @@ internal class StateBuilder(
         val fee = requireNotNull(dataState.selectedFee)
         val fromCryptoCurrency = requireNotNull(dataState.fromCryptoCurrency)
         val toCryptoCurrency = requireNotNull(dataState.toCryptoCurrency)
-        val fromAmount = txState.fromAmountValue ?: BigDecimal.ZERO
-        val toAmount = txState.toAmountValue ?: BigDecimal.ZERO
+        val fromAmount = swapTransactionState.fromAmountValue ?: BigDecimal.ZERO
+        val toAmount = swapTransactionState.toAmountValue ?: BigDecimal.ZERO
         val providerState = uiState.providerState as ProviderState.Content
 
         val fromFiatAmount = getFormattedFiatAmount(fromCryptoCurrency.value.fiatRate?.multiply(fromAmount))
@@ -944,7 +944,7 @@ internal class StateBuilder(
 
         return uiState.copy(
             successState = SwapSuccessStateHolder(
-                timestamp = txState.timestamp,
+                timestamp = swapTransactionState.timestamp,
                 txUrl = txUrl,
                 providerName = stringReference(providerState.name),
                 providerType = stringReference(providerState.type),
@@ -952,8 +952,8 @@ internal class StateBuilder(
                 providerIcon = providerState.iconUrl,
                 rate = providerState.subtitle,
                 fee = stringReference("${fee.feeCryptoFormatted} (${fee.feeFiatFormatted})"),
-                fromTokenAmount = stringReference(txState.fromAmount.orEmpty()),
-                toTokenAmount = stringReference(txState.toAmount.orEmpty()),
+                fromTokenAmount = stringReference(swapTransactionState.fromAmount.orEmpty()),
+                toTokenAmount = stringReference(swapTransactionState.toAmount.orEmpty()),
                 fromTokenFiatAmount = stringReference(fromFiatAmount),
                 toTokenFiatAmount = stringReference(toFiatAmount),
                 fromTokenIconState = iconStateConverter.convert(fromCryptoCurrency),
@@ -964,15 +964,48 @@ internal class StateBuilder(
         )
     }
 
-    fun createErrorTransaction(uiState: SwapStateHolder, txState: TxState, onAlertClick: () -> Unit): SwapStateHolder {
+    fun createErrorTransaction(
+        uiState: SwapStateHolder,
+        swapTransactionState: SwapTransactionState,
+        onAlertClick: () -> Unit,
+    ): SwapStateHolder {
         return uiState.copy(
             alert = SwapWarning.GenericWarning(
-                message = null,
+                message = if (swapTransactionState is SwapTransactionState.ExpressError) {
+                    getAlertErrorMessage(swapTransactionState.dataError)
+                } else {
+                    null
+                },
                 onClick = onAlertClick,
-                type = if (txState is TxState.NetworkError) GenericWarningType.NETWORK else GenericWarningType.OTHER,
+                type = if (swapTransactionState is SwapTransactionState.NetworkError) {
+                    GenericWarningType.NETWORK
+                } else {
+                    GenericWarningType.OTHER
+                },
             ),
             changeCardsButtonState = ChangeCardsButtonState.ENABLED,
         )
+    }
+
+    private fun getAlertErrorMessage(dataError: DataError): TextReference? {
+        return when (dataError) {
+            is DataError.SwapsAreUnavailableNowError -> resourceReference(
+                id = R.string.express_error_swap_unavailable,
+                formatArgs = wrappedList(dataError.code),
+            )
+            is DataError.ExchangeNotPossibleError -> resourceReference(
+                id = R.string.express_error_provider_unavailable,
+                formatArgs = wrappedList(dataError.code),
+            )
+            is DataError.ExchangeProviderNotActiveError,
+            is DataError.ExchangeProviderNotAvailableError,
+            is DataError.ExchangeProviderProviderInternalError,
+            -> resourceReference(
+                id = R.string.express_error_swap_pair_unavailable,
+                formatArgs = wrappedList(dataError.code),
+            )
+            else -> null
+        }
     }
 
     fun createAlert(
