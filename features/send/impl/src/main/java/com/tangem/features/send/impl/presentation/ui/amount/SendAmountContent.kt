ad46dfd63bdb9ed8650e8d4bfede9c96e4ef2df9
Tangem <git@tangem.com>
2024-03-29 16:46:48 +0500

    Updated on 2026-07-07

--- features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/ui/amount/SendAmountContent.kt
+++ features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/ui/amount/SendAmountContent.kt
@@ -1,19 +1,14 @@
 package com.tangem.features.send.impl.presentation.ui.amount
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListScope
-import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.tangem.core.ui.components.notifications.Notification
 import com.tangem.core.ui.res.TangemTheme
-import com.tangem.features.send.impl.presentation.state.SendNotification
 import com.tangem.features.send.impl.presentation.state.SendStates
+import com.tangem.features.send.impl.presentation.ui.common.notifications
 import com.tangem.features.send.impl.presentation.viewmodel.SendClickIntents
-import kotlinx.collections.immutable.ImmutableList
 
 @Composable
 internal fun SendAmountContent(
@@ -31,29 +26,4 @@ internal fun SendAmountContent(
         buttons(amountState.segmentedButtonConfig, clickIntents)
         notifications(amountState.notifications)
     }
-}
-
-@OptIn(ExperimentalFoundationApi::class)
-internal fun LazyListScope.notifications(configs: ImmutableList<SendNotification>, modifier: Modifier = Modifier) {
-    items(
-        items = configs,
-        key = { it::class.java },
-        contentType = { it::class.java },
-        itemContent = {
-            val bottomPadding = if (it == configs.last()) TangemTheme.dimens.spacing12 else TangemTheme.dimens.spacing0
-            Notification(
-                config = it.config,
-                modifier = modifier
-                    .animateItemPlacement()
-                    .padding(
-                        top = TangemTheme.dimens.spacing12,
-                        bottom = bottomPadding,
-                    ),
-                containerColor = when (it) {
-                    is SendNotification.Warning.NetworkFeeUnreachable -> TangemTheme.colors.background.action
-                    else -> TangemTheme.colors.button.disabled
-                },
-            )
-        },
-    )
 }
