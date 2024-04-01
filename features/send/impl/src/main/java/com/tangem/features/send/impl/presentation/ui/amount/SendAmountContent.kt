ef2570c8b5457c5439f8c5dd07c3464e53c9e413
Tangem <git@tangem.com>
2024-04-01 15:38:28 +0500

    Updated on 2026-09-21

--- features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/ui/amount/SendAmountContent.kt
+++ features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/ui/amount/SendAmountContent.kt
@@ -5,8 +5,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.features.send.impl.presentation.state.SendStates
+import com.tangem.features.send.impl.presentation.state.previewdata.AmountStatePreviewData
+import com.tangem.features.send.impl.presentation.state.previewdata.SendClickIntentsStub
 import com.tangem.features.send.impl.presentation.ui.common.notifications
 import com.tangem.features.send.impl.presentation.viewmodel.SendClickIntents
 
@@ -31,3 +36,40 @@ internal fun SendAmountContent(
         notifications(amountState.notifications)
     }
 }
+
+// region Preview
+@Preview
+@Composable
+private fun AmountFieldPreview_Light(
+    @PreviewParameter(AmountFieldPreviewProvider::class) amountState: SendStates.AmountState,
+) {
+    TangemTheme {
+        SendAmountContent(
+            amountState = amountState,
+            isBalanceHiding = false,
+            clickIntents = SendClickIntentsStub,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun AmountFieldPreview_Dark(
+    @PreviewParameter(AmountFieldPreviewProvider::class) amountState: SendStates.AmountState,
+) {
+    TangemTheme(isDark = true) {
+        SendAmountContent(
+            amountState = amountState,
+            isBalanceHiding = false,
+            clickIntents = SendClickIntentsStub,
+        )
+    }
+}
+
+private class AmountFieldPreviewProvider : PreviewParameterProvider<SendStates.AmountState> {
+    override val values: Sequence<SendStates.AmountState>
+        get() = sequenceOf(
+            AmountStatePreviewData.amountState,
+        )
+}
+// endregion
