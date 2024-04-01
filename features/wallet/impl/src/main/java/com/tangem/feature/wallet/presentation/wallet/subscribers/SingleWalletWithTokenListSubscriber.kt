41d818c7f83414c1cef236c09ef73a2f5496614a
Tangem <git@tangem.com>
2024-04-01 12:46:03 +0400

    Updated on 2026-07-23

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/subscribers/SingleWalletWithTokenListSubscriber.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/subscribers/SingleWalletWithTokenListSubscriber.kt
@@ -1,12 +1,17 @@
 package com.tangem.feature.wallet.presentation.wallet.subscribers
 
 import com.tangem.domain.appcurrency.GetSelectedAppCurrencyUseCase
+import com.tangem.domain.core.lce.LceFlow
+import com.tangem.domain.core.utils.toLce
 import com.tangem.domain.tokens.GetCardTokensListUseCase
+import com.tangem.domain.tokens.error.TokenListError
+import com.tangem.domain.tokens.model.TokenList
 import com.tangem.domain.wallets.models.UserWallet
 import com.tangem.feature.wallet.presentation.wallet.analytics.utils.TokenListAnalyticsSender
 import com.tangem.feature.wallet.presentation.wallet.domain.WalletWithFundsChecker
 import com.tangem.feature.wallet.presentation.wallet.state.WalletStateController
 import com.tangem.feature.wallet.presentation.wallet.viewmodels.intents.WalletClickIntents
+import kotlinx.coroutines.flow.map
 
 @Suppress("LongParameterList")
 internal class SingleWalletWithTokenListSubscriber(
@@ -26,5 +31,6 @@ internal class SingleWalletWithTokenListSubscriber(
     getSelectedAppCurrencyUseCase = getSelectedAppCurrencyUseCase,
 ) {
 
-    override fun tokenListFlow(): MaybeTokenListFlow = getCardTokensListUseCase(userWallet.walletId)
+    override fun tokenListFlow(): LceFlow<TokenListError, TokenList> = getCardTokensListUseCase(userWallet.walletId)
+        .map { it.toLce() }
 }
