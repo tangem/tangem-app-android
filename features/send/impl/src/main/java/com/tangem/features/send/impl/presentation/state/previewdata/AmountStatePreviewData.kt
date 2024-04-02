d0df16d0ea5020d8750e8b480a1868b0e60c9eec
Tangem <git@tangem.com>
2024-04-02 21:07:38 +0500

    release/5.26.1

--- features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/state/previewdata/AmountStatePreviewData.kt
+++ features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/state/previewdata/AmountStatePreviewData.kt
@@ -24,6 +24,7 @@ internal object AmountStatePreviewData {
         segmentedButtonConfig = persistentListOf(),
         notifications = persistentListOf(),
         isFeeLoading = false,
+        appCurrencyCode = "usd",
         amountTextField = SendTextField.AmountField(
             value = "123.123123123123123123",
             onValueChange = {},
