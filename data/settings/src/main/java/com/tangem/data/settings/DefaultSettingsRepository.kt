8de8f1596c553161015b66e795ada5bd37df0bee
Tangem <git@tangem.com>
2024-04-05 21:40:49 +0800

    release/5.26.1

--- data/settings/src/main/java/com/tangem/data/settings/DefaultSettingsRepository.kt
+++ data/settings/src/main/java/com/tangem/data/settings/DefaultSettingsRepository.kt
@@ -1,23 +1,25 @@
 package com.tangem.data.settings
 
-import com.tangem.data.source.preferences.PreferencesDataSource
 import com.tangem.datasource.local.preferences.AppPreferencesStore
 import com.tangem.datasource.local.preferences.PreferencesKeys
 import com.tangem.datasource.local.preferences.utils.getSyncOrDefault
 import com.tangem.datasource.local.preferences.utils.store
 import com.tangem.domain.settings.repositories.SettingsRepository
-import com.tangem.utils.coroutines.CoroutineDispatcherProvider
-import kotlinx.coroutines.withContext
 import org.joda.time.DateTime
 
 internal class DefaultSettingsRepository(
-    private val preferencesDataSource: PreferencesDataSource,
     private val appPreferencesStore: AppPreferencesStore,
-    private val dispatchers: CoroutineDispatcherProvider,
 ) : SettingsRepository {
 
     override suspend fun shouldShowSaveUserWalletScreen(): Boolean {
-        return withContext(dispatchers.io) { preferencesDataSource.shouldShowSaveUserWalletScreen }
+        return appPreferencesStore.getSyncOrDefault(
+            key = PreferencesKeys.SHOULD_SHOW_SAVE_USER_WALLET_SCREEN_KEY,
+            default = true,
+        )
+    }
+
+    override suspend fun setShouldShowSaveUserWalletScreen(value: Boolean) {
+        appPreferencesStore.store(key = PreferencesKeys.SHOULD_SHOW_SAVE_USER_WALLET_SCREEN_KEY, value = value)
     }
 
     override suspend fun isWalletScrollPreviewEnabled(): Boolean {
