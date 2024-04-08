90d59cadcbae293d22eeb4160d5d450c53cd87b0
Tangem <git@tangem.com>
2024-04-08 17:21:35 +0500

    release/5.27.0

--- features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/state/fee/SendFeeCustomFieldConverter.kt
+++ features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/state/fee/SendFeeCustomFieldConverter.kt
@@ -3,6 +3,7 @@ package com.tangem.features.send.impl.presentation.state.fee
 import com.tangem.blockchain.common.transaction.Fee
 import com.tangem.domain.appcurrency.model.AppCurrency
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
+import com.tangem.features.send.impl.presentation.state.fee.custom.BitcoinCustomFeeConverter
 import com.tangem.features.send.impl.presentation.state.fee.custom.EthereumCustomFeeConverter
 import com.tangem.features.send.impl.presentation.state.fields.SendTextField
 import com.tangem.features.send.impl.presentation.viewmodel.SendClickIntents
@@ -25,21 +26,34 @@ internal class SendFeeCustomFieldConverter(
         )
     }
 
+    private val bitcoinCustomFeeConverter by lazy(LazyThreadSafetyMode.NONE) {
+        BitcoinCustomFeeConverter(
+            clickIntents = clickIntents,
+            appCurrencyProvider = appCurrencyProvider,
+            feeCryptoCurrencyStatusProvider = feeCryptoCurrencyStatusProvider,
+        )
+    }
+
     override fun convert(value: Fee): ImmutableList<SendTextField.CustomFee> {
         return when (value) {
-            is Fee.Ethereum -> {
-                ethereumCustomFeeConverter.convert(value)
-            }
+            is Fee.Ethereum -> ethereumCustomFeeConverter.convert(value)
+            is Fee.Bitcoin -> bitcoinCustomFeeConverter.convert(value)
             else -> persistentListOf()
         }
     }
 
     fun onValueChange(feeSelectorState: FeeSelectorState.Content, index: Int, value: String) = feeSelectorState.copy(
-        customValues = when (feeSelectorState.fees.normal) {
+        customValues = when (val fee = feeSelectorState.fees.normal) {
             is Fee.Ethereum -> ethereumCustomFeeConverter.onValueChange(
                 customValues = feeSelectorState.customValues,
                 index = index,
                 value = value,
+            )
+            is Fee.Bitcoin -> bitcoinCustomFeeConverter.onValueChange(
+                customValues = feeSelectorState.customValues,
+                index = index,
+                value = value,
+                txSize = fee.txSize,
             )
             else -> feeSelectorState.customValues
         },
