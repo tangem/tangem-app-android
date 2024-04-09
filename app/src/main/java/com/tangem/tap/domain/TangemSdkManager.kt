806549d75a17c63d7b8d541a51ec1fed9dee4f17
Tangem <git@tangem.com>
2024-04-09 15:05:44 +0200

    Updated on 2026-09-01

--- app/src/main/java/com/tangem/tap/domain/TangemSdkManager.kt
+++ app/src/main/java/com/tangem/tap/domain/TangemSdkManager.kt
@@ -1,233 +1,84 @@
 package com.tangem.tap.domain
 
-import android.content.res.Resources
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import com.tangem.Message
-import com.tangem.TangemSdk
 import com.tangem.common.*
 import com.tangem.common.authentication.keystore.KeystoreManager
-import com.tangem.common.card.FirmwareVersion
 import com.tangem.common.core.*
 import com.tangem.common.extensions.ByteArrayKey
 import com.tangem.common.services.secure.SecureStorage
-import com.tangem.common.usersCode.UserCodeRepository
-import com.tangem.core.analytics.Analytics
-import com.tangem.core.analytics.models.Basic
-import com.tangem.crypto.bip39.DefaultMnemonic
 import com.tangem.crypto.hdWallet.DerivationPath
-import com.tangem.domain.card.repository.CardSdkConfigRepository
-import com.tangem.domain.common.util.cardTypesResolver
-import com.tangem.domain.common.util.derivationStyleProvider
 import com.tangem.domain.models.scan.CardDTO
 import com.tangem.domain.models.scan.ScanResponse
-import com.tangem.operations.ScanTask
 import com.tangem.operations.derivation.DerivationTaskResponse
-import com.tangem.operations.derivation.DeriveMultipleWalletPublicKeysTask
-import com.tangem.operations.pins.SetUserCodeCommand
-import com.tangem.operations.usersetttings.SetUserCodeRecoveryAllowedTask
 import com.tangem.crypto.hdWallet.bip32.ExtendedPublicKey
-import com.tangem.operations.derivation.DeriveWalletPublicKeyTask
-import com.tangem.tap.derivationsFinder
-import com.tangem.tap.domain.tasks.product.CreateProductWalletTask
 import com.tangem.tap.domain.tasks.product.CreateProductWalletTaskResponse
-import com.tangem.tap.domain.tasks.product.ResetToFactorySettingsTask
-import com.tangem.tap.domain.tasks.product.ScanProductTask
-import com.tangem.wallet.R
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.suspendCancellableCoroutine
-import kotlinx.coroutines.withContext
-import kotlin.coroutines.resume
 
-@Suppress("TooManyFunctions")
-class TangemSdkManager(
-    private val cardSdkConfigRepository: CardSdkConfigRepository,
-    private val resources: Resources,
-) {
-
-    private val tangemSdk: TangemSdk
-        get() = cardSdkConfigRepository.sdk
-
-    private val userCodeRepository by lazy {
-        UserCodeRepository(
-            keystoreManager = tangemSdk.keystoreManager,
-            secureStorage = tangemSdk.secureStorage,
-        )
-    }
+interface TangemSdkManager {
 
     val canUseBiometry: Boolean
-        get() = tangemSdk.authenticationManager.canAuthenticate || needEnrollBiometrics
 
     val needEnrollBiometrics: Boolean
-        get() = tangemSdk.authenticationManager.needEnrollBiometrics
 
     val keystoreManager: KeystoreManager
-        get() = tangemSdk.keystoreManager
 
     val secureStorage: SecureStorage
-        get() = tangemSdk.secureStorage
 
     val userCodeRequestPolicy: UserCodeRequestPolicy
-        get() = tangemSdk.config.userCodeRequestPolicy
 
     suspend fun scanProduct(
         cardId: String? = null,
         messageRes: Int? = null,
         allowsRequestAccessCodeFromRepository: Boolean = false,
-    ): CompletionResult<ScanResponse> {
-        val message = Message(resources.getString(messageRes ?: R.string.initial_message_scan_header))
-        return runTaskAsyncReturnOnMain(
-            runnable = ScanProductTask(
-                card = null,
-                derivationsFinder = derivationsFinder,
-                allowsRequestAccessCodeFromRepository = allowsRequestAccessCodeFromRepository,
-            ),
-            cardId = cardId,
-            initialMessage = message,
-        ).also { sendScanResultsToAnalytics(it) }
-    }
+    ): CompletionResult<ScanResponse>
 
     suspend fun createProductWallet(
         scanResponse: ScanResponse,
         shouldReset: Boolean = false,
-    ): CompletionResult<CreateProductWalletTaskResponse> {
-        return runTaskAsync(
-            runnable = CreateProductWalletTask(
-                cardTypesResolver = scanResponse.cardTypesResolver,
-                derivationStyleProvider = scanResponse.derivationStyleProvider,
-                shouldReset = shouldReset,
-            ),
-            cardId = scanResponse.card.cardId,
-            initialMessage = Message(resources.getString(R.string.initial_message_create_wallet_body)),
-            iconScanRes = if (scanResponse.cardTypesResolver.isRing()) R.drawable.img_hand_scan_ring else null,
-        )
-    }
+    ): CompletionResult<CreateProductWalletTaskResponse>
 
     suspend fun importWallet(
         scanResponse: ScanResponse,
         mnemonic: String,
         passphrase: String?,
         shouldReset: Boolean,
-    ): CompletionResult<CreateProductWalletTaskResponse> {
-        val defaultMnemonic = try {
-            DefaultMnemonic(mnemonic, tangemSdk.wordlist)
-        } catch (e: TangemSdkError.MnemonicException) {
-            return CompletionResult.Failure(e)
-        }
-        return runTaskAsync(
-            CreateProductWalletTask(
-                cardTypesResolver = scanResponse.cardTypesResolver,
-                derivationStyleProvider = scanResponse.derivationStyleProvider,
-                mnemonic = defaultMnemonic,
-                passphrase = passphrase,
-                shouldReset = shouldReset,
-            ),
-            scanResponse.card.cardId,
-            Message(resources.getString(R.string.initial_message_create_wallet_body)),
-        )
-    }
-
-    private fun sendScanResultsToAnalytics(result: CompletionResult<ScanResponse>) {
-        if (result is CompletionResult.Failure) {
-            (result.error as? TangemSdkError)?.let { error ->
-                Analytics.send(Basic.ScanError(error))
-            }
-        }
-    }
+    ): CompletionResult<CreateProductWalletTaskResponse>
 
     suspend fun derivePublicKeys(
         cardId: String?,
         derivations: Map<ByteArrayKey, List<DerivationPath>>,
-    ): CompletionResult<DerivationTaskResponse> {
-        return runTaskAsyncReturnOnMain(DeriveMultipleWalletPublicKeysTask(derivations), cardId)
-    }
+    ): CompletionResult<DerivationTaskResponse>
 
     suspend fun deriveExtendedPublicKey(
         cardId: String?,
         walletPublicKey: ByteArray,
         derivation: DerivationPath,
-    ): CompletionResult<ExtendedPublicKey> = withContext(Dispatchers.Main) {
-        runTaskAsyncReturnOnMain(
-            DeriveWalletPublicKeyTask(walletPublicKey, derivation),
-            cardId,
-        )
-    }
+    ): CompletionResult<ExtendedPublicKey>
 
     suspend fun resetToFactorySettings(
         cardId: String,
         allowsRequestAccessCodeFromRepository: Boolean,
-    ): CompletionResult<CardDTO> {
-        return runTaskAsyncReturnOnMain(
-            runnable = ResetToFactorySettingsTask(
-                allowsRequestAccessCodeFromRepository = allowsRequestAccessCodeFromRepository,
-            ),
-            cardId = cardId,
-            initialMessage = Message(resources.getString(R.string.card_settings_reset_card_to_factory)),
-        )
-            .map { CardDTO(it) }
-    }
+    ): CompletionResult<CardDTO>
 
-    suspend fun saveAccessCode(accessCode: String, cardsIds: Set<String>): CompletionResult<Unit> {
-        return userCodeRepository.save(
-            cardsIds = cardsIds,
-            userCode = UserCode(
-                type = UserCodeType.AccessCode,
-                stringValue = accessCode,
-            ),
-        )
-    }
+    suspend fun saveAccessCode(accessCode: String, cardsIds: Set<String>): CompletionResult<Unit>
 
-    suspend fun deleteSavedUserCodes(cardsIds: Set<String>): CompletionResult<Unit> {
-        return userCodeRepository.delete(cardsIds.toSet())
-    }
+    suspend fun deleteSavedUserCodes(cardsIds: Set<String>): CompletionResult<Unit>
 
-    suspend fun clearSavedUserCodes(): CompletionResult<Unit> {
-        return userCodeRepository.clear()
-    }
+    suspend fun clearSavedUserCodes(): CompletionResult<Unit>
 
-    suspend fun setPasscode(cardId: String?): CompletionResult<SuccessResponse> {
-        return runTaskAsyncReturnOnMain(
-            SetUserCodeCommand.changePasscode(null),
-            cardId,
-            initialMessage = Message(resources.getString(R.string.initial_message_change_passcode_body)),
-        )
-    }
+    suspend fun setPasscode(cardId: String?): CompletionResult<SuccessResponse>
 
-    suspend fun setAccessCode(cardId: String?): CompletionResult<SuccessResponse> {
-        return runTaskAsyncReturnOnMain(
-            SetUserCodeCommand.changeAccessCode(null),
-            cardId,
-            initialMessage = Message(resources.getString(R.string.initial_message_change_access_code_body)),
-        )
-    }
+    suspend fun setAccessCode(cardId: String?): CompletionResult<SuccessResponse>
 
-    suspend fun setLongTap(cardId: String?): CompletionResult<SuccessResponse> {
-        return runTaskAsyncReturnOnMain(
-            SetUserCodeCommand.resetUserCodes(),
-            cardId,
-            initialMessage = Message(resources.getString(R.string.initial_message_tap_header)),
-        )
-    }
+    suspend fun setLongTap(cardId: String?): CompletionResult<SuccessResponse>
 
-    suspend fun setAccessCodeRecoveryEnabled(cardId: String?, enabled: Boolean): CompletionResult<SuccessResponse> {
-        return runTaskAsyncReturnOnMain(
-            SetUserCodeRecoveryAllowedTask(enabled),
-            cardId,
-            initialMessage = Message(resources.getString(R.string.initial_message_tap_header)),
-        )
-    }
+    suspend fun setAccessCodeRecoveryEnabled(cardId: String?, enabled: Boolean): CompletionResult<SuccessResponse>
 
     suspend fun scanCard(
         cardId: String? = null,
         allowRequestAccessCodeFromRepository: Boolean = false,
-    ): CompletionResult<CardDTO> {
-        return runTaskAsyncReturnOnMain(
-            runnable = ScanTask(allowRequestAccessCodeFromRepository),
-            cardId = cardId,
-            initialMessage = Message(resources.getString(R.string.initial_message_tap_header)),
-        )
-            .map { CardDTO(it) }
-    }
+    ): CompletionResult<CardDTO>
 
     suspend fun <T> runTaskAsync(
         runnable: CardSessionRunnable<T>,
@@ -235,53 +86,13 @@ class TangemSdkManager(
         initialMessage: Message? = null,
         accessCode: String? = null,
         @DrawableRes iconScanRes: Int? = null,
-    ): CompletionResult<T> = withContext(Dispatchers.Main) {
-        suspendCancellableCoroutine { continuation ->
-            tangemSdk.startSessionWithRunnable(runnable, cardId, initialMessage, accessCode, iconScanRes) { result ->
-                if (continuation.isActive) continuation.resume(result)
-            }
-        }
-    }
-
-    private suspend fun <T> runTaskAsyncReturnOnMain(
-        runnable: CardSessionRunnable<T>,
-        cardId: String? = null,
-        initialMessage: Message? = null,
-    ): CompletionResult<T> {
-        val result = runTaskAsync(runnable, cardId, initialMessage)
-        return withContext(Dispatchers.Main) { result }
-    }
+    ): CompletionResult<T>
 
     @Suppress("MagicNumber")
-    fun changeDisplayedCardIdNumbersCount(scanResponse: ScanResponse?) {
-        tangemSdk.config.cardIdDisplayFormat = when {
-            scanResponse == null -> CardIdDisplayFormat.Full
-            scanResponse.cardTypesResolver.isTangemTwins() -> CardIdDisplayFormat.LastLuhn(4)
-            else -> CardIdDisplayFormat.Full
-        }
-    }
+    fun changeDisplayedCardIdNumbersCount(scanResponse: ScanResponse?)
 
     @Deprecated("TangemSdkManager shouldn't returns a string from resources")
-    fun getString(@StringRes stringResId: Int, vararg formatArgs: Any?): String {
-        return resources.getString(stringResId, *formatArgs)
-    }
+    fun getString(@StringRes stringResId: Int, vararg formatArgs: Any?): String
 
-    fun setUserCodeRequestPolicy(policy: UserCodeRequestPolicy) {
-        tangemSdk.config.userCodeRequestPolicy = policy
-    }
-
-    companion object {
-        @Deprecated("Use [DefaultCardSdkProvider] instead")
-        val config = Config(
-            linkedTerminal = true,
-            allowUntrustedCards = true,
-            filter = CardFilter(
-                allowedCardTypes = FirmwareVersion.FirmwareType.values().toList(),
-                maxFirmwareVersion = FirmwareVersion(major = 6, minor = 33),
-                batchIdFilter = CardFilter.Companion.ItemFilter.Deny(
-                    items = setOf("0027", "0030", "0031", "0035"),
-                ),
-            ),
-        )
-    }
+    fun setUserCodeRequestPolicy(policy: UserCodeRequestPolicy)
 }
