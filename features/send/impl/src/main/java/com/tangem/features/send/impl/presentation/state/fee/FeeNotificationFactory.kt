377466b0bb14f0cbe49192f9897257730802fa0d
Tangem <git@tangem.com>
2024-04-11 19:59:05 +0500

    release/5.26.1

--- features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/state/fee/FeeNotificationFactory.kt
+++ features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/state/fee/FeeNotificationFactory.kt
@@ -1,20 +1,15 @@
 package com.tangem.features.send.impl.presentation.state.fee
 
-import com.tangem.blockchain.common.transaction.TransactionFee
-import com.tangem.core.ui.utils.parseToBigDecimal
+import com.tangem.features.send.impl.presentation.state.SendNotification
 import com.tangem.features.send.impl.presentation.state.SendUiState
 import com.tangem.features.send.impl.presentation.state.SendUiStateType
 import com.tangem.features.send.impl.presentation.state.StateRouter
-import com.tangem.features.send.impl.presentation.state.fields.SendTextField
-import com.tangem.features.send.impl.presentation.state.SendNotification
 import com.tangem.features.send.impl.presentation.viewmodel.SendClickIntents
 import com.tangem.utils.Provider
-import com.tangem.utils.toFormattedString
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.toImmutableList
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.map
-import java.math.BigDecimal
 
 @Suppress("LongParameterList")
 internal class FeeNotificationFactory(
@@ -29,16 +24,7 @@ internal class FeeNotificationFactory(
             val state = currentStateProvider()
             val feeState = state.feeState ?: return@map persistentListOf()
             buildList {
-                when (val feeSelectorState = feeState.feeSelectorState) {
-                    FeeSelectorState.Error -> {
-                        addFeeUnreachableNotification(feeSelectorState)
-                    }
-                    is FeeSelectorState.Content -> {
-                        val customFee = feeSelectorState.customValues
-                        val selectedFee = feeSelectorState.selectedFee
-                        addTooHighNotification(feeSelectorState.fees, selectedFee, customFee)
-                    }
-                }
+                addFeeUnreachableNotification(feeState.feeSelectorState)
             }.toImmutableList()
         }
 
@@ -46,25 +32,5 @@ internal class FeeNotificationFactory(
         if (feeSelectorState is FeeSelectorState.Error) {
             add(SendNotification.Warning.NetworkFeeUnreachable(clickIntents::feeReload))
         }
-    }
-
-    private fun MutableList<SendNotification>.addTooHighNotification(
-        transactionFee: TransactionFee,
-        selectedFee: FeeType,
-        customFee: List<SendTextField.CustomFee>,
-    ) {
-        val multipleFees = transactionFee as? TransactionFee.Choosable ?: return
-        val highValue = multipleFees.priority.amount.value ?: return
-        val customAmount = customFee.firstOrNull() ?: return
-        val customValue = customAmount.value.parseToBigDecimal(customAmount.decimals)
-        val diff = customValue / highValue
-        if (selectedFee == FeeType.Custom && diff > FEE_MAX_DIFF) {
-            add(SendNotification.Warning.TooHigh(diff.toFormattedString(HIGH_FEE_DIFF_DECIMALS)))
-        }
-    }
-
-    companion object {
-        private val FEE_MAX_DIFF = BigDecimal(5)
-        private const val HIGH_FEE_DIFF_DECIMALS = 0
     }
 }
