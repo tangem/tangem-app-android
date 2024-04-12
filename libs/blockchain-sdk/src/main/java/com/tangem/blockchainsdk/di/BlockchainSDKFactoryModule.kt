4318d84c65f72c2e0af797e7232aab473204ba02
Tangem <git@tangem.com>
2024-04-12 20:29:29 +0800

    Updated on 2026-06-01

--- libs/blockchain-sdk/src/main/java/com/tangem/blockchainsdk/di/BlockchainSDKFactoryModule.kt
+++ libs/blockchain-sdk/src/main/java/com/tangem/blockchainsdk/di/BlockchainSDKFactoryModule.kt
@@ -1,21 +1,15 @@
 package com.tangem.blockchainsdk.di
 
-import com.squareup.moshi.Moshi
 import com.tangem.blockchain.common.logging.BlockchainSDKLogger
 import com.tangem.blockchainsdk.BlockchainSDKFactory
 import com.tangem.blockchainsdk.DefaultBlockchainSDKFactory
 import com.tangem.blockchainsdk.accountcreator.DefaultAccountCreator
 import com.tangem.blockchainsdk.config.RuntimeConfigStorage
 import com.tangem.blockchainsdk.datastorage.DefaultBlockchainDataStorage
-import com.tangem.blockchainsdk.loader.ConfigLoader
-import com.tangem.blockchainsdk.loader.LocalConfigLoader
 import com.tangem.datasource.api.common.AuthProvider
 import com.tangem.datasource.api.tangemTech.TangemTechApi
-import com.tangem.datasource.asset.reader.AssetReader
-import com.tangem.datasource.di.SdkMoshi
+import com.tangem.datasource.asset.loader.AssetLoader
 import com.tangem.datasource.local.preferences.AppPreferencesStore
-import com.tangem.libs.blockchain_sdk.BuildConfig
-import com.tangem.utils.coroutines.AppCoroutineDispatcherProvider
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -29,33 +23,18 @@ internal object BlockchainSDKFactoryModule {
     @Provides
     @Singleton
     fun provideBlockchainSDKFactory(
-        @SdkMoshi moshi: Moshi,
-        assetReader: AssetReader,
-        dispatchers: AppCoroutineDispatcherProvider,
+        assetLoader: AssetLoader,
         authProvider: AuthProvider,
         tangemTechApi: TangemTechApi,
         appPreferencesStore: AppPreferencesStore,
         blockchainSDKLogger: BlockchainSDKLogger,
     ): BlockchainSDKFactory {
         return DefaultBlockchainSDKFactory(
-            configLoader = createConfigLoader(moshi, assetReader, dispatchers),
+            assetLoader = assetLoader,
             configStorage = RuntimeConfigStorage(),
             accountCreator = DefaultAccountCreator(authProvider, tangemTechApi),
             blockchainDataStorage = DefaultBlockchainDataStorage(appPreferencesStore),
             blockchainSDKLogger = blockchainSDKLogger,
-        )
-    }
-
-    private fun createConfigLoader(
-        moshi: Moshi,
-        assetReader: AssetReader,
-        dispatchers: AppCoroutineDispatcherProvider,
-    ): ConfigLoader {
-        return LocalConfigLoader(
-            buildEnvironment = BuildConfig.ENVIRONMENT,
-            moshi = moshi,
-            assetReader = assetReader,
-            dispatchers = dispatchers,
         )
     }
 }
