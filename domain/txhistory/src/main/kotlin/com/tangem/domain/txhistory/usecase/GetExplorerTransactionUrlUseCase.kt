0e332251035b648edb9ea9d30ba0cf448fc3dc98
Tangem <git@tangem.com>
2024-04-12 17:20:20 +0500

    Updated on 2026-09-01

--- domain/txhistory/src/main/kotlin/com/tangem/domain/txhistory/usecase/GetExplorerTransactionUrlUseCase.kt
+++ domain/txhistory/src/main/kotlin/com/tangem/domain/txhistory/usecase/GetExplorerTransactionUrlUseCase.kt
@@ -6,15 +6,30 @@ import arrow.core.raise.either
 import com.tangem.domain.tokens.model.Network
 import com.tangem.domain.txhistory.models.TxStatusError
 import com.tangem.domain.txhistory.repository.TxHistoryRepository
+import com.tangem.domain.wallets.models.UserWalletId
 
 class GetExplorerTransactionUrlUseCase(
     private val repository: TxHistoryRepository,
 ) {
+    @Deprecated("Replace with invoke [UserWalletId, Network]")
     operator fun invoke(txHash: String, networkId: Network.ID): Either<TxStatusError, String> {
         return either {
             catch(
                 block = {
                     repository.getTxExploreUrl(txHash, networkId).ifEmpty {
+                        raise(TxStatusError.EmptyUrlError)
+                    }
+                },
+                catch = { raise(TxStatusError.DataError(it)) },
+            )
+        }
+    }
+
+    suspend operator fun invoke(userWalletId: UserWalletId, network: Network): Either<TxStatusError, String> {
+        return either {
+            catch(
+                block = {
+                    repository.getTxExploreUrl(userWalletId, network).ifEmpty {
                         raise(TxStatusError.EmptyUrlError)
                     }
                 },
