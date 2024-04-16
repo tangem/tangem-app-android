ce07fd41099846be07f22128168f141e9eada3fe
iMaks99 <maks.ismoilov0618@gmail.com>
2024-04-16 17:43:17 +0500

    AND-6804 Fixed Send: Explore and Share button are absent on the Transaction sent screen (BTC)

--- data/txhistory/src/main/kotlin/com/tangem/data/txhistory/repository/DefaultTxHistoryRepository.kt
+++ data/txhistory/src/main/kotlin/com/tangem/data/txhistory/repository/DefaultTxHistoryRepository.kt
@@ -9,7 +9,6 @@ import com.tangem.data.common.cache.CacheRegistry
 import com.tangem.data.txhistory.repository.paging.TxHistoryPagingSource
 import com.tangem.datasource.local.txhistory.TxHistoryItemsStore
 import com.tangem.datasource.local.userwallet.UserWalletsStore
-import com.tangem.domain.common.extensions.fromNetworkId
 import com.tangem.domain.tokens.model.CryptoCurrency
 import com.tangem.domain.tokens.model.Network
 import com.tangem.domain.txhistory.models.Page
@@ -80,7 +79,7 @@ class DefaultTxHistoryRepository(
     }
 
     override suspend fun getTxExploreUrl(userWalletId: UserWalletId, network: Network): String {
-        val blockchain = Blockchain.fromNetworkId(network.id.value)
+        val blockchain = Blockchain.fromId(network.id.value)
         val walletManager = walletManagersFacade.getOrCreateWalletManager(
             userWalletId = userWalletId,
             network = network,
