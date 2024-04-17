d3f02148ad07c274e20c2e6d3d1da7f89e7216c5
Tangem <git@tangem.com>
2024-04-17 09:49:55 +0800

    Updated on 2026-09-25

--- libs/blockchain-sdk/src/main/java/com/tangem/blockchainsdk/di/BlockchainSDKFactoryModule.kt
+++ libs/blockchain-sdk/src/main/java/com/tangem/blockchainsdk/di/BlockchainSDKFactoryModule.kt
@@ -4,13 +4,16 @@ import com.tangem.blockchain.common.BlockchainSdkConfig
 import com.tangem.blockchain.common.logging.BlockchainSDKLogger
 import com.tangem.blockchainsdk.BlockchainSDKFactory
 import com.tangem.blockchainsdk.DefaultBlockchainSDKFactory
+import com.tangem.blockchainsdk.WalletManagerFactoryCreator
 import com.tangem.blockchainsdk.accountcreator.DefaultAccountCreator
 import com.tangem.blockchainsdk.datastorage.DefaultBlockchainDataStorage
-import com.tangem.blockchainsdk.storage.DefaultRuntimeStore
+import com.tangem.blockchainsdk.loader.BlockchainProvidersResponseLoader
+import com.tangem.blockchainsdk.store.DefaultRuntimeStore
 import com.tangem.datasource.api.common.AuthProvider
 import com.tangem.datasource.api.tangemTech.TangemTechApi
 import com.tangem.datasource.asset.loader.AssetLoader
 import com.tangem.datasource.local.preferences.AppPreferencesStore
+import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -25,15 +28,29 @@ internal object BlockchainSDKFactoryModule {
     @Singleton
     fun provideBlockchainSDKFactory(
         assetLoader: AssetLoader,
+        blockchainProvidersResponseLoader: BlockchainProvidersResponseLoader,
+        walletManagerFactoryCreator: WalletManagerFactoryCreator,
+        dispatchers: CoroutineDispatcherProvider,
+    ): BlockchainSDKFactory {
+        return DefaultBlockchainSDKFactory(
+            assetLoader = assetLoader,
+            blockchainProvidersResponseLoader = blockchainProvidersResponseLoader,
+            configStore = DefaultRuntimeStore(defaultValue = BlockchainSdkConfig()),
+            blockchainProviderTypesStore = DefaultRuntimeStore(defaultValue = emptyMap()),
+            walletManagerFactoryCreator = walletManagerFactoryCreator,
+            dispatchers = dispatchers,
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideWalletManagerFactoryCreator(
         authProvider: AuthProvider,
         tangemTechApi: TangemTechApi,
         appPreferencesStore: AppPreferencesStore,
         blockchainSDKLogger: BlockchainSDKLogger,
-    ): BlockchainSDKFactory {
-        return DefaultBlockchainSDKFactory(
-            assetLoader = assetLoader,
-            configStore = DefaultRuntimeStore(defaultValue = BlockchainSdkConfig()),
-            blockchainProviderTypesStore = DefaultRuntimeStore(defaultValue = emptyMap()),
+    ): WalletManagerFactoryCreator {
+        return WalletManagerFactoryCreator(
             accountCreator = DefaultAccountCreator(authProvider, tangemTechApi),
             blockchainDataStorage = DefaultBlockchainDataStorage(appPreferencesStore),
             blockchainSDKLogger = blockchainSDKLogger,
