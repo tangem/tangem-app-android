0c3187c4173c7c67f4955846fc0b7a060f814645
Tangem <git@tangem.com>
2024-04-18 09:19:53 +0800

    Updated on 2026-09-25

--- app/src/main/java/com/tangem/tap/proxy/AppStateHolder.kt
+++ app/src/main/java/com/tangem/tap/proxy/AppStateHolder.kt
@@ -6,8 +6,6 @@ import com.tangem.core.navigation.ReduxNavController
 import com.tangem.domain.models.scan.CardDTO
 import com.tangem.domain.models.scan.ScanResponse
 import com.tangem.domain.redux.ReduxStateHolder
-import com.tangem.domain.wallets.legacy.UserWalletsListManager
-import com.tangem.domain.wallets.legacy.WalletsStateHolder
 import com.tangem.domain.wallets.models.UserWallet
 import com.tangem.tap.common.extensions.dispatchOnMain
 import com.tangem.tap.common.extensions.dispatchWithMain
@@ -15,8 +13,6 @@ import com.tangem.tap.common.extensions.onUserWalletSelected
 import com.tangem.tap.common.redux.AppState
 import com.tangem.tap.domain.sdk.TangemSdkManager
 import com.tangem.tap.network.exchangeServices.ExchangeService
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
 import org.rekotlin.Action
 import org.rekotlin.Store
 import javax.inject.Inject
@@ -25,18 +21,7 @@ import javax.inject.Inject
  * Holds objects from old modules, that missing in DI graph.
  * Object sets manually to use in new modules and [AppStateHolder] proxies its to DI.
  */
-class AppStateHolder @Inject constructor() : WalletsStateHolder, ReduxNavController, ReduxStateHolder {
-
-    override var userWalletsListManager: UserWalletsListManager? = null
-        set(value) {
-            field = value
-            _userWalletsListManagerFlow.value = value
-        }
-
-    override val userWalletListManagerFlow: Flow<UserWalletsListManager?>
-        get() = _userWalletsListManagerFlow
-
-    private val _userWalletsListManagerFlow = MutableStateFlow<UserWalletsListManager?>(null)
+class AppStateHolder @Inject constructor() : ReduxNavController, ReduxStateHolder {
 
     @Deprecated("Use scan response from selected user wallet")
     var scanResponse: ScanResponse? = null
