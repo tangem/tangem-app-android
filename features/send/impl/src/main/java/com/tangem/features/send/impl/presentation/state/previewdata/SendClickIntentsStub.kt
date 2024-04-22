595a4e471de7334a27acaa361300fbc465c270ec
Tangem <git@tangem.com>
2024-04-22 20:48:13 +0500

    Updated on 2026-07-07

--- features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/state/previewdata/SendClickIntentsStub.kt
+++ features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/state/previewdata/SendClickIntentsStub.kt
@@ -34,7 +34,7 @@ internal object SendClickIntentsStub : SendClickIntents {
 
     override fun onRecipientMemoValueChange(value: String) {}
 
-    override fun feeReload(isToNextState: Boolean) {}
+    override fun feeReload() {}
 
     override fun onFeeSelectorClick(feeType: FeeType) {}
 
