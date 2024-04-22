c878502e84d57ee34c9a9aaa4cf8a26906200e23
Tangem <git@tangem.com>
2024-04-22 20:48:13 +0500

    Updated on 2026-09-14

--- features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/state/amount/AmountStateFactory.kt
+++ features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/state/amount/AmountStateFactory.kt
@@ -1,12 +1,10 @@
 package com.tangem.features.send.impl.presentation.state.amount
 
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
-import com.tangem.features.send.impl.presentation.state.SendNotification
 import com.tangem.features.send.impl.presentation.state.SendUiState
 import com.tangem.features.send.impl.presentation.state.fields.SendAmountFieldChangeConverter
 import com.tangem.features.send.impl.presentation.state.fields.SendAmountFieldMaxAmountConverter
 import com.tangem.utils.Provider
-import kotlinx.collections.immutable.ImmutableList
 
 /**
  * Factory to produce amount state for [SendUiState]
