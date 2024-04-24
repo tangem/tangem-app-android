c1fc213d900cf08876d1eea6a2208a09f2416f81
Tangem <git@tangem.com>
2024-04-24 14:44:37 +0500

    release/5.26.1

--- features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/viewmodel/SendViewModel.kt
+++ features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/viewmodel/SendViewModel.kt
@@ -679,6 +679,7 @@ internal class SendViewModel @Inject constructor(
             val result = callFeeUseCase()?.fold(
                 ifRight = {
                     uiState = feeStateFactory.onFeeOnLoadedState(it)
+                    sendIdleTimer = SystemClock.elapsedRealtime()
                 },
                 ifLeft = {
                     onFeeLoadFailed(isShowStatus)
