3256362db46fa7b918d5d054dcf85964f259d0bb
Tangem <git@tangem.com>
2024-04-25 14:12:43 +0500

    Updated on 2026-09-23

--- features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/state/fields/SendAmountFieldMaxAmountConverter.kt
+++ features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/state/fields/SendAmountFieldMaxAmountConverter.kt
@@ -6,11 +6,13 @@ import androidx.compose.ui.text.input.KeyboardType
 import com.tangem.core.ui.utils.parseBigDecimal
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.features.send.impl.presentation.state.SendUiState
+import com.tangem.features.send.impl.presentation.state.StateRouter
 import com.tangem.utils.Provider
 import com.tangem.utils.converter.Converter
 import com.tangem.utils.isNullOrZero
 
 internal class SendAmountFieldMaxAmountConverter(
+    private val stateRouterProvider: Provider<StateRouter>,
     private val currentStateProvider: Provider<SendUiState>,
     private val cryptoCurrencyStatusProvider: Provider<CryptoCurrencyStatus>,
 ) : Converter<Unit, SendUiState> {
@@ -18,7 +20,8 @@ internal class SendAmountFieldMaxAmountConverter(
     override fun convert(value: Unit): SendUiState {
         val state = currentStateProvider()
         val cryptoCurrencyStatus = cryptoCurrencyStatusProvider()
-        val amountState = state.amountState ?: return state
+        val isEditState = stateRouterProvider().isEditState
+        val amountState = state.getAmountState(isEditState) ?: return state
         val amountTextField = amountState.amountTextField
 
         val cryptoDecimals = amountTextField.cryptoAmount.decimals
@@ -31,7 +34,8 @@ internal class SendAmountFieldMaxAmountConverter(
         val isDoneActionEnabled = !decimalCryptoValue.isNullOrZero()
         val cryptoValue = decimalCryptoValue?.parseBigDecimal(cryptoDecimals).orEmpty()
         val fiatValue = decimalFiatValue?.parseBigDecimal(fiatDecimals).orEmpty()
-        return state.copy(
+        return state.copyWrapped(
+            isEditState = isEditState,
             amountState = amountState.copy(
                 isPrimaryButtonEnabled = true,
                 amountTextField = amountTextField.copy(
