1af1c2d29f123e1ca2894fd1704fcf584a0c5791
Tangem <git@tangem.com>
2024-04-25 11:35:55 +0200

    Updated on 2025-12-18

--- app/src/main/java/com/tangem/tap/di/domain/TokensDomainModule.kt
+++ app/src/main/java/com/tangem/tap/di/domain/TokensDomainModule.kt
@@ -6,7 +6,6 @@ import com.tangem.domain.tokens.*
 import com.tangem.domain.tokens.repository.*
 import com.tangem.domain.walletmanager.WalletManagersFacade
 import com.tangem.feature.swap.domain.api.SwapRepository
-import com.tangem.features.send.api.featuretoggles.SendFeatureToggles
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import dagger.Module
 import dagger.Provides
@@ -190,7 +189,6 @@ internal object TokensDomainModule {
         currenciesRepository: CurrenciesRepository,
         quotesRepository: QuotesRepository,
         networksRepository: NetworksRepository,
-        sendFeatureToggles: SendFeatureToggles,
         dispatchers: CoroutineDispatcherProvider,
     ): GetCryptoCurrencyActionsUseCase {
         return GetCryptoCurrencyActionsUseCase(
@@ -199,7 +197,6 @@ internal object TokensDomainModule {
             currenciesRepository = currenciesRepository,
             quotesRepository = quotesRepository,
             networksRepository = networksRepository,
-            sendFeatureToggles = sendFeatureToggles,
             dispatchers = dispatchers,
         )
     }
