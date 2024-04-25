4371257bfe4ef10b5341bfef5e256653fb417319
Tangem <git@tangem.com>
2024-04-25 10:28:08 +0200

    Updated on 2026-08-27

--- app/src/main/java/com/tangem/tap/domain/sdk/mocks/MockProvider.kt
+++ app/src/main/java/com/tangem/tap/domain/sdk/mocks/MockProvider.kt
@@ -1,59 +1,81 @@
 package com.tangem.tap.domain.sdk.mocks
 
 import com.tangem.common.CompletionResult
+import com.tangem.common.core.TangemError
+import com.tangem.common.core.TangemSdkError
 import com.tangem.domain.models.scan.ProductType
-import com.tangem.tap.domain.sdk.mocks.wallet.WalletMocks
-import com.tangem.tap.domain.sdk.mocks.wallet2.Wallet2Mocks
+import com.tangem.tap.domain.sdk.mocks.content.WalletMockContent
+import com.tangem.tap.domain.sdk.mocks.content.Wallet2MockContent
 import com.tangem.tap.domain.tasks.product.CreateProductWalletTaskResponse
 
 object MockProvider {
 
-    private var mocks: Mocks = getMocks(ProductType.Wallet)
+    private var content: MockContent = getMockContent(ProductType.Wallet)
 
-    fun setMockConfig(mockConfig: MockConfig) {
-        mockConfig.content.fold(
-            ifLeft = {
-                mocks = getMocks(it)
-            },
-            ifRight = {
-                mocks = it
-            },
-        )
+    private var emulateError: Boolean = false
+
+    private var emulatedError: TangemError = TangemSdkError.TagLost()
+
+    fun setEmulateError(error: TangemError? = null) {
+        emulateError = true
+        error?.let {
+            emulatedError = it
+        }
     }
 
-    fun getSuccessResponse() = CompletionResult.Success(mocks.successResponse)
+    fun resetEmulateError() {
+        emulateError = false
+    }
 
-    fun getScanResponse() = CompletionResult.Success(mocks.scanResponse)
+    fun setMocks(productType: ProductType) {
+        content = getMockContent(productType)
+    }
 
-    fun getDerivationTaskResponse() = CompletionResult.Success(mocks.derivationTaskResponse)
+    fun setMocks(mockContent: MockContent) {
+        content = mockContent
+    }
 
-    fun getCardDto() = CompletionResult.Success(mocks.cardDto)
+    fun getSuccessResponse() = CompletionResult.Success(content.successResponse).orFailure()
 
-    fun getExtendedPublicKey() = CompletionResult.Success(mocks.extendedPublicKey)
+    fun getScanResponse() = CompletionResult.Success(content.scanResponse).orFailure()
+
+    fun getDerivationTaskResponse() = CompletionResult.Success(content.derivationTaskResponse).orFailure()
+
+    fun getCardDto() = CompletionResult.Success(content.cardDto).orFailure()
+
+    fun getExtendedPublicKey() = CompletionResult.Success(content.extendedPublicKey).orFailure()
 
     fun getCreateProductWalletResponse(): CompletionResult<CreateProductWalletTaskResponse> {
-        return CompletionResult.Success(mocks.createProductWalletTaskResponse)
+        return CompletionResult.Success(content.createProductWalletTaskResponse).orFailure()
     }
 
     fun getImportWalletResponse(): CompletionResult<CreateProductWalletTaskResponse> {
-        return CompletionResult.Success(mocks.importWalletResponse)
-    }
-
-    private fun getMocks(productType: ProductType): Mocks {
-        return when (productType) {
-            ProductType.Wallet -> WalletMocks
-            ProductType.Wallet2 -> Wallet2Mocks
-            else -> TODO()
-        }
+        return CompletionResult.Success(content.importWalletResponse).orFailure()
     }
 
     // region Twin-specific
 
-    fun finalizeTwin() = CompletionResult.Success(mocks.finalizeTwinResponse)
+    fun finalizeTwin() = CompletionResult.Success(content.finalizeTwinResponse).orFailure()
 
-    fun createFirstTwinWallet() = CompletionResult.Success(mocks.createFirstTwinResponse)
+    fun createFirstTwinWallet() = CompletionResult.Success(content.createFirstTwinResponse).orFailure()
 
-    fun createSecondTwinWallet() = CompletionResult.Success(mocks.createSecondTwinResponse)
+    fun createSecondTwinWallet() = CompletionResult.Success(content.createSecondTwinResponse).orFailure()
 
     // endregion
+
+    private fun getMockContent(productType: ProductType): MockContent {
+        return when (productType) {
+            ProductType.Wallet -> WalletMockContent
+            ProductType.Wallet2 -> Wallet2MockContent
+            else -> TODO()
+        }
+    }
+
+    private fun <T> CompletionResult.Success<T>.orFailure(): CompletionResult<T> {
+        return if (emulateError) {
+            CompletionResult.Failure(emulatedError)
+        } else {
+            this
+        }
+    }
 }
