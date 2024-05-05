5a223cbab5a082f6455316c05e152b468e51d734
Tangem <git@tangem.com>
2024-05-05 22:00:47 +0100

    Updated on 2026-09-23

--- features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/viewmodel/SendViewModel.kt
+++ features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/viewmodel/SendViewModel.kt
@@ -557,7 +557,6 @@ internal class SendViewModel @Inject constructor(
             )
             return true
         }
-        val feeSelectorState = uiState.feeState?.feeSelectorState as? FeeSelectorState.Content ?: return false
         return checkIfFeeTooHigh(
             feeSelectorState = feeSelectorState,
             onShow = { diff ->
