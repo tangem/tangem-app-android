c6bb73583911b98d7e933c575d0d08e5090b2185
Ilya Shulgin <shulgin.ilya.i@gmail.com>
2024-05-07 17:11:04 +0200

    AND-6891 Added hasVibrator() check.

--- app/src/main/java/com/tangem/tap/di/HapticModule.kt
+++ app/src/main/java/com/tangem/tap/di/HapticModule.kt
@@ -6,6 +6,7 @@ import android.os.Vibrator
 import android.os.VibratorManager
 import com.tangem.tap.common.haptic.DefaultHapticManager
 import com.tangem.core.ui.haptic.HapticManager
+import com.tangem.core.ui.haptic.MockHapticManager
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -20,13 +21,17 @@ class HapticModule {
     @Provides
     @Singleton
     fun provideHapticManager(@ApplicationContext context: Context): HapticManager {
-        return DefaultHapticManager(
-            vibrator = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                val vibratorManager = context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
-                vibratorManager.defaultVibrator
-            } else {
-                context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-            },
-        )
+        val vibrator = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val vibratorManager = context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+            vibratorManager.defaultVibrator
+        } else {
+            context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        }
+
+        return if (vibrator.hasVibrator()) {
+            DefaultHapticManager(vibrator = vibrator)
+        } else {
+            MockHapticManager
+        }
     }
 }
