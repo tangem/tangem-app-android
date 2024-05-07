cf8d32cea58895f97bcf9de17f84fc9fb50e708b
Ilya Shulgin <shulgin.ilya.i@gmail.com>
2024-05-07 11:55:44 +0200

    AND-6891 Fixed detekt.

--- core/haptic/src/main/java/com/tangem/core/haptic/di/HapticModule.kt
+++ core/haptic/src/main/java/com/tangem/core/haptic/di/HapticModule.kt
@@ -8,9 +8,7 @@ import com.tangem.core.haptic.HapticManager
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.android.scopes.ViewModelScoped
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
