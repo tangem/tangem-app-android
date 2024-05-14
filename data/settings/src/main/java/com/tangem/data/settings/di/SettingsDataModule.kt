4854caaded2287881fa0353667ddb57c78bc716c
Tangem <git@tangem.com>
2024-05-14 13:40:11 +0100

    Updated on 2026-09-25

--- data/settings/src/main/java/com/tangem/data/settings/di/SettingsDataModule.kt
+++ data/settings/src/main/java/com/tangem/data/settings/di/SettingsDataModule.kt
@@ -2,12 +2,12 @@ package com.tangem.data.settings.di
 
 import com.tangem.data.settings.DefaultAppRatingRepository
 import com.tangem.data.settings.DefaultSettingsRepository
-import com.tangem.data.settings.DefaultSwapPromoRepository
+import com.tangem.data.settings.DefaultPromoSettingsRepository
 import com.tangem.data.source.preferences.PreferencesDataSource
 import com.tangem.datasource.local.preferences.AppPreferencesStore
 import com.tangem.domain.settings.repositories.AppRatingRepository
 import com.tangem.domain.settings.repositories.SettingsRepository
-import com.tangem.domain.settings.repositories.SwapPromoRepository
+import com.tangem.domain.settings.repositories.PromoSettingsRepository
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import dagger.Module
 import dagger.Provides
@@ -41,7 +41,7 @@ internal object SettingsDataModule {
 
     @Provides
     @Singleton
-    fun provideSwapPromoRepository(appPreferencesStore: AppPreferencesStore): SwapPromoRepository {
-        return DefaultSwapPromoRepository(appPreferencesStore = appPreferencesStore)
+    fun providePromoSettingsSettingsRepository(appPreferencesStore: AppPreferencesStore): PromoSettingsRepository {
+        return DefaultPromoSettingsRepository(appPreferencesStore = appPreferencesStore)
     }
 }
