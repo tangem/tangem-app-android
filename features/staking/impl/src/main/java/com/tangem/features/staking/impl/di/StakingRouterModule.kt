6a9d3b7910d3dfe579a3a7b93ee8cbb3d712c719
Tangem <git@tangem.com>
2024-05-17 14:14:44 +0200

    Updated on 2025-10-09

---
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/di/StakingRouterModule.kt
@@ -0,0 +1,23 @@
+package com.tangem.features.staking.impl.di
+
+import com.tangem.features.staking.impl.navigation.DefaultStakingRouter
+import com.tangem.features.staking.api.navigation.StakingRouter
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.scopes.ActivityScoped
+
+/**
+ * DI module provides implementation of [StakingRouter]
+ */
+@Module
+@InstallIn(ActivityComponent::class)
+internal object StakingRouterModule {
+
+    @Provides
+    @ActivityScoped
+    fun provideStakingRouter(): StakingRouter {
+        return DefaultStakingRouter()
+    }
+}
