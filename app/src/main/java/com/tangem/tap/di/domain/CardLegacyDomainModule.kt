5e9f6915275434039c533083615e25ef387e5ef6
Tangem <git@tangem.com>
2024-05-21 12:02:55 +0200

    Updated on 2026-08-27

--- app/src/main/java/com/tangem/tap/di/domain/CardLegacyDomainModule.kt
+++ app/src/main/java/com/tangem/tap/di/domain/CardLegacyDomainModule.kt
@@ -1,6 +1,8 @@
 package com.tangem.tap.di.domain
 
 import com.tangem.domain.card.*
+import com.tangem.domain.wallets.legacy.UserWalletsListManager
+import com.tangem.domain.wallets.usecase.GenerateWalletNameUseCase
 import com.tangem.tap.domain.scanCard.DefaultScanCardProcessor
 import dagger.Module
 import dagger.Provides
@@ -14,5 +16,11 @@ internal object CardLegacyDomainModule {
 
     @Provides
     @Singleton
-    fun provideScanCardUseCase(): ScanCardProcessor = DefaultScanCardProcessor()
+    fun provideScanCardProcessor(): ScanCardProcessor = DefaultScanCardProcessor()
+
+    @Provides
+    @Singleton
+    fun providesWalletNameGenerateUseCase(userWalletsListManager: UserWalletsListManager): GenerateWalletNameUseCase {
+        return GenerateWalletNameUseCase(userWalletsListManager)
+    }
 }
