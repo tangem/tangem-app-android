f533004c05d36a6fc75882564e03d3d5f057586f
Tangem <git@tangem.com>
2024-05-21 12:02:55 +0200

    Updated on 2026-07-23

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/model/WalletAlertState.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/model/WalletAlertState.kt
@@ -25,6 +25,7 @@ internal sealed interface WalletAlertState {
         open val text: String = ""
         open val confirmButtonText: TextReference = resourceReference(id = R.string.common_ok)
         abstract val onConfirmClick: (String) -> Unit
+        abstract val errorTextProvider: (String) -> TextReference?
     }
 
     data class DefaultAlert(
@@ -36,6 +37,7 @@ internal sealed interface WalletAlertState {
     data class RenameWalletAlert(
         override val text: String,
         override val onConfirmClick: (String) -> Unit,
+        override val errorTextProvider: (String) -> TextReference?,
     ) : TextInput() {
         override val title: TextReference = resourceReference(id = R.string.user_wallet_list_rename_popup_title)
         override val label: TextReference = resourceReference(id = R.string.user_wallet_list_rename_popup_placeholder)
