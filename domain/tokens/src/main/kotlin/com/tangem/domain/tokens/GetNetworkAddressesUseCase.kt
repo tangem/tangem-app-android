3e9b7ca67c4ff292261dce0ec7528f6e45328f2b
Tangem <git@tangem.com>
2024-05-21 18:58:30 +0500

    Updated on 2026-09-14

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/GetNetworkAddressesUseCase.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/GetNetworkAddressesUseCase.kt
@@ -1,27 +1,14 @@
 package com.tangem.domain.tokens
 
 import com.tangem.domain.tokens.model.Network
-import com.tangem.domain.tokens.model.NetworkStatus
 import com.tangem.domain.tokens.repository.NetworksRepository
 import com.tangem.domain.wallets.models.UserWalletId
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 
 class GetNetworkAddressesUseCase(
     internal val networksRepository: NetworksRepository,
 ) {
 
-    operator fun invoke(userWalletId: UserWalletId, network: Network): Flow<List<String>> =
-        networksRepository.getNetworkStatusesUpdates(userWalletId, setOf(network))
-            .map { networkStatuses ->
-                networkStatuses.filter { it.network.id == network.id }
-                    .map { networkStatus ->
-                        when (val status = networkStatus.value) {
-                            is NetworkStatus.NoAccount -> status.address.defaultAddress.value
-                            is NetworkStatus.Unreachable -> status.address?.defaultAddress?.value.orEmpty()
-                            is NetworkStatus.Verified -> status.address.defaultAddress.value
-                            else -> ""
-                        }
-                    }
-            }
+    suspend fun invokeSync(userWalletId: UserWalletId, network: Network): List<String> {
+        return networksRepository.getNetworkAddresses(userWalletId, network)
+    }
 }
