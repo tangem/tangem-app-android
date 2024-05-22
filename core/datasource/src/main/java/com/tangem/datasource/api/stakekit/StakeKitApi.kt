bbf37571bd637e917633a5d789ff279125e0789f
Ilya Shulgin <shulgin.ilya.i@gmail.com>
2024-05-22 15:59:36 +0200

    AND-7132 Fixed name.

--- core/datasource/src/main/java/com/tangem/datasource/api/stakekit/StakeKitApi.kt
+++ core/datasource/src/main/java/com/tangem/datasource/api/stakekit/StakeKitApi.kt
@@ -1,16 +1,15 @@
 package com.tangem.datasource.api.stakekit
 
 import com.tangem.datasource.api.common.response.ApiResponse
-import com.tangem.datasource.api.express.models.response.Asset
 import com.tangem.datasource.api.stakekit.models.request.RevenueOption
 import com.tangem.datasource.api.stakekit.models.request.YieldType
-import com.tangem.datasource.api.stakekit.models.response.YieldsResponse
+import com.tangem.datasource.api.stakekit.models.response.EnabledYieldsResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface StakeKitApi {
 
-    @GET("v1/yields")
+    @GET("v1/yields/enabled")
     fun getAllYields(
         @Query("ledgerWalletAPICompatible") ledgerWalletAPICompatible: Boolean,
         @Query("type") type: YieldType,
@@ -18,6 +17,6 @@ interface StakeKitApi {
         @Query("page") page: Int,
         @Query("network") network: String,
         @Query("limit") limit: Int
-    ): ApiResponse<YieldsResponse>
+    ): ApiResponse<EnabledYieldsResponse>
 
 }
--- core/datasource/src/main/java/com/tangem/datasource/api/stakekit/models/response/EnabledYieldsResponse.kt
+++ core/datasource/src/main/java/com/tangem/datasource/api/stakekit/models/response/EnabledYieldsResponse.kt
@@ -4,7 +4,7 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class YieldsResponse(
+data class EnabledYieldsResponse(
     @Json(name = "data")
     val data: List<Yield>,
     @Json(name = "hasNextPage")
