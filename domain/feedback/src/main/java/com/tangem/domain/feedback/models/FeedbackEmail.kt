728a7f19e8903e7e590bd7eed905a6fcfb924bc4
Tangem <git@tangem.com>
2024-05-22 15:48:49 +0800

    Updated on 2026-06-17

--- domain/feedback/src/main/java/com/tangem/domain/feedback/models/FeedbackEmail.kt
+++ domain/feedback/src/main/java/com/tangem/domain/feedback/models/FeedbackEmail.kt
@@ -2,7 +2,7 @@ package com.tangem.domain.feedback.models
 
 import java.io.File
 
-data class SupportFeedbackEmail(
+data class FeedbackEmail(
     val address: String,
     val subject: String,
     val message: String,
