aa73195d5904dc76054313f3be8cdd1f7635b9fe
Tangem <git@tangem.com>
2024-05-22 15:48:49 +0800

    Updated on 2026-08-07

--- app/src/main/java/com/tangem/tap/proxy/redux/DaggerGraphState.kt
+++ app/src/main/java/com/tangem/tap/proxy/redux/DaggerGraphState.kt
@@ -2,6 +2,7 @@ package com.tangem.tap.proxy.redux
 
 import com.tangem.TangemSdkLogger
 import com.tangem.blockchainsdk.BlockchainSDKFactory
+import com.tangem.core.navigation.email.EmailSender
 import com.tangem.datasource.connection.NetworkConnectionManager
 import com.tangem.domain.appcurrency.repository.AppCurrencyRepository
 import com.tangem.domain.apptheme.repository.AppThemeModeRepository
@@ -11,15 +12,16 @@ import com.tangem.domain.card.ScanCardUseCase
 import com.tangem.domain.card.repository.CardRepository
 import com.tangem.domain.card.repository.CardSdkConfigRepository
 import com.tangem.domain.feedback.FeedbackManagerFeatureToggles
+import com.tangem.domain.feedback.SaveBlockchainErrorUseCase
 import com.tangem.domain.onboarding.SaveTwinsOnboardingShownUseCase
 import com.tangem.domain.onboarding.WasTwinsOnboardingShownUseCase
 import com.tangem.domain.settings.repositories.SettingsRepository
 import com.tangem.domain.tokens.repository.CurrenciesRepository
 import com.tangem.domain.tokens.repository.NetworksRepository
-import com.tangem.domain.wallets.usecase.GenerateWalletNameUseCase
 import com.tangem.domain.walletmanager.WalletManagersFacade
 import com.tangem.domain.wallets.legacy.UserWalletsListManager
 import com.tangem.domain.wallets.repository.WalletsRepository
+import com.tangem.domain.wallets.usecase.GenerateWalletNameUseCase
 import com.tangem.feature.qrscanning.QrScanningRouter
 import com.tangem.features.managetokens.featuretoggles.ManageTokensFeatureToggles
 import com.tangem.features.managetokens.navigation.ManageTokensUi
@@ -69,4 +71,6 @@ data class DaggerGraphState(
     val tangemSdkLogger: TangemSdkLogger? = null,
     val settingsRepository: SettingsRepository? = null,
     val blockchainSDKFactory: BlockchainSDKFactory? = null,
+    val emailSender: EmailSender? = null,
+    val saveBlockchainErrorUseCase: SaveBlockchainErrorUseCase? = null,
 ) : StateType
