37fa06556bce15433b81374f59ad062a91303e88
Tangem <git@tangem.com>
2024-05-28 19:20:56 +0400

    Updated on 2026-09-14

---
+++ app/src/main/java/com/tangem/tap/di/RootAppComponentContextModule.kt
@@ -0,0 +1,45 @@
+package com.tangem.tap.di
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import com.arkivanov.decompose.defaultComponentContext
+import com.tangem.core.decompose.context.AppComponentContext
+import com.tangem.core.decompose.context.DefaultAppComponentContext
+import com.tangem.core.decompose.di.DecomposeComponent
+import com.tangem.core.decompose.ui.UiMessage
+import com.tangem.core.decompose.ui.UiMessageHandler
+import com.tangem.utils.coroutines.CoroutineDispatcherProvider
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.qualifiers.ActivityContext
+import dagger.hilt.android.scopes.ActivityScoped
+import timber.log.Timber
+
+@Module
+@InstallIn(ActivityComponent::class)
+internal object RootAppComponentContextModule {
+
+    @Provides
+    @ActivityScoped
+    fun provideRootAppComponentContext(
+        @ActivityContext context: Context,
+        dispatchers: CoroutineDispatcherProvider,
+        componentBuilder: DecomposeComponent.Builder,
+    ): AppComponentContext {
+        // TODO: Implement message handler
+        val dummyMessageHandler = object : UiMessageHandler {
+            override fun handleMessage(message: UiMessage) {
+                Timber.w("Unable to handle message: $message")
+            }
+        }
+
+        return DefaultAppComponentContext(
+            componentContext = (context as AppCompatActivity).defaultComponentContext(),
+            messageHandler = dummyMessageHandler,
+            dispatchers = dispatchers,
+            hiltComponentBuilder = componentBuilder,
+        )
+    }
+}
