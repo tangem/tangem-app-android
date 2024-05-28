ed729f3d66ccee0df8a04f912b10229204a97052
Tangem <git@tangem.com>
2024-05-28 19:20:56 +0400

    Updated on 2025-09-01

---
+++ features/details/impl/src/main/kotlin/com/tangem/features/details/component/preview/PreviewWalletConnectComponent.kt
@@ -0,0 +1,16 @@
+package com.tangem.features.details.component.preview
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.tangem.features.details.component.WalletConnectComponent
+
+internal class PreviewWalletConnectComponent : WalletConnectComponent {
+
+    override suspend fun checkIsAvailable(): Boolean = true
+
+    @Composable
+    @Suppress("TopLevelComposableFunctions") // TODO: Remove this check
+    override fun View(modifier: Modifier) {
+        TODO("Will be implemented in [REDACTED_TASK_KEY]")
+    }
+}
