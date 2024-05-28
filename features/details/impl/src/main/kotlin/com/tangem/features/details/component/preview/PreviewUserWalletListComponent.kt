1b56c0ecbf9cf4dac1cca1d644e0a141f918c580
Tangem <git@tangem.com>
2024-05-28 19:20:56 +0400

    Updated on 2026-08-27

---
+++ features/details/impl/src/main/kotlin/com/tangem/features/details/component/preview/PreviewUserWalletListComponent.kt
@@ -0,0 +1,14 @@
+package com.tangem.features.details.component.preview
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.tangem.features.details.component.UserWalletListComponent
+
+internal class PreviewUserWalletListComponent : UserWalletListComponent {
+
+    @Composable
+    @Suppress("TopLevelComposableFunctions") // TODO: Remove this check
+    override fun View(modifier: Modifier) {
+        TODO("Will be implemented in [REDACTED_TASK_KEY]")
+    }
+}
---
+++ features/details/impl/src/main/kotlin/com/tangem/features/details/entity/DetailsItemUM.kt
@@ -0,0 +1,40 @@
+package com.tangem.features.details.entity
+
+import androidx.annotation.DrawableRes
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.Modifier
+import com.tangem.core.ui.extensions.TextReference
+import kotlinx.collections.immutable.ImmutableList
+
+@Immutable
+internal sealed class DetailsItemUM {
+
+    abstract val id: String
+
+    data class Basic(
+        override val id: String,
+        val items: ImmutableList<Item>,
+    ) : DetailsItemUM() {
+
+        data class Item(
+            val title: TextReference,
+            @DrawableRes
+            val iconRes: Int,
+            val onClick: () -> Unit,
+        )
+    }
+
+    data class Component(
+        override val id: String,
+        val content: Content,
+    ) : DetailsItemUM() {
+
+        fun interface Content {
+
+            @Composable
+            @Suppress("TopLevelComposableFunctions", "ComposableFunctionName")
+            operator fun invoke(modifier: Modifier)
+        }
+    }
+}
