d92661be3343217a154b809755134444bce62833
Tangem <git@tangem.com>
2024-05-29 15:56:22 +0400

    Updated on 2026-04-08

--- core/ui/src/main/java/com/tangem/core/ui/message/EventMessageHandler.kt
+++ core/ui/src/main/java/com/tangem/core/ui/message/EventMessageHandler.kt
@@ -12,7 +12,7 @@ import kotlinx.coroutines.flow.StateFlow
  * Message handler that is used to show or remove an [EventMessage] in the UI.
  */
 class EventMessageHandler(
-    private val events: MutableStateFlow<StateEvent<EventMessage>>,
+    private val events: MutableStateFlow<StateEvent<EventMessage>> = MutableStateFlow(consumedEvent()),
 ) : UiMessageHandler, StateFlow<StateEvent<EventMessage>> by events {
 
     override fun handleMessage(message: UiMessage) {
