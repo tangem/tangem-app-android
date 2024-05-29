ee1b477abf6e36fb7c0ace7a650d7bd1d140812d
Anton Tkachev <iiiburnyiii@gmail.com>
2024-05-29 17:52:43 +0400

    AND-7165 [Details] Improved Details feature launching

--- features/details/api/src/main/kotlin/com/tangem/features/details/DetailsEntryPoint.kt
+++ features/details/api/src/main/kotlin/com/tangem/features/details/DetailsEntryPoint.kt
@@ -5,4 +5,9 @@ import androidx.fragment.app.Fragment
 interface DetailsEntryPoint {
 
     fun entryFragment(): Fragment
+
+    companion object {
+
+        const val USER_WALLET_ID_KEY = "details_user_wallet_id"
+    }
 }
