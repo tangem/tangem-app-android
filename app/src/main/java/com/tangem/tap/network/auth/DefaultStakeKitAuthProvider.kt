62d3732a3ae25f33de3fa64bcc3d73a264ba6e4b
Tangem <git@tangem.com>
2024-05-29 12:37:11 +0200

    Updated on 2026-09-25

---
+++ app/src/main/java/com/tangem/tap/network/auth/DefaultStakeKitAuthProvider.kt
@@ -0,0 +1,13 @@
+package com.tangem.tap.network.auth
+
+import com.tangem.datasource.config.ConfigManager
+import com.tangem.lib.auth.StakeKitAuthProvider
+
+internal class DefaultStakeKitAuthProvider(
+    private val configManager: ConfigManager,
+) : StakeKitAuthProvider {
+
+    override fun getApiKey(): String {
+        return configManager.config.express?.apiKey ?: error("No StakeKit api key provided")
+    }
+}
