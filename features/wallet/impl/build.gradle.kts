eed0f2208ae89dfcf2c2e9093604260abff315a4
Anton Tkachev <iiiburnyiii@gmail.com>
2024-05-29 17:52:43 +0400

    AND-7165 [Details] Improved Details feature launching

--- features/wallet/impl/build.gradle.kts
+++ features/wallet/impl/build.gradle.kts
@@ -87,4 +87,5 @@ dependencies {
     implementation(projects.features.send.api)
     implementation(projects.features.tester.api)
     implementation(projects.features.manageTokens.api)
+    implementation(projects.features.details.api)
 }
