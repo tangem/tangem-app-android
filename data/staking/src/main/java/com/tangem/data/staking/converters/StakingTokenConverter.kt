f221ec8bcf6cf8c5a2cc5fd7e320dd664d190ea8
Tangem <git@tangem.com>
2024-06-05 17:23:23 +0200

    Updated on 2026-03-27

--- data/staking/src/main/java/com/tangem/data/staking/converters/StakingTokenConverter.kt
+++ data/staking/src/main/java/com/tangem/data/staking/converters/StakingTokenConverter.kt
@@ -1,13 +1,13 @@
 package com.tangem.data.staking.converters
 
-import com.tangem.datasource.api.stakekit.models.response.model.TokenWithYield
+import com.tangem.datasource.api.stakekit.models.response.model.TokenWithYieldDTO
 import com.tangem.domain.staking.model.StakingToken
 import com.tangem.domain.staking.model.StakingTokenWithYield
 import com.tangem.utils.converter.Converter
 
-class StakingTokenConverter : Converter<TokenWithYield, StakingTokenWithYield> {
+class StakingTokenConverter : Converter<TokenWithYieldDTO, StakingTokenWithYield> {
 
-    override fun convert(value: TokenWithYield): StakingTokenWithYield {
+    override fun convert(value: TokenWithYieldDTO): StakingTokenWithYield {
         return StakingTokenWithYield(
             token = StakingToken(
                 name = value.token.name,
