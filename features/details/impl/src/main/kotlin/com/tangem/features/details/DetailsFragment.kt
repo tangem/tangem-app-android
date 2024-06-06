ef4a17dcf82eb3474146a87a657bf2c6dacfabfa
Tangem <git@tangem.com>
2024-06-06 15:26:51 +0400

    Updated on 2025-09-25

--- features/details/impl/src/main/kotlin/com/tangem/features/details/DetailsFragment.kt
+++ features/details/impl/src/main/kotlin/com/tangem/features/details/DetailsFragment.kt
@@ -3,14 +3,16 @@ package com.tangem.features.details
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.fragment.app.Fragment
+import com.arkivanov.decompose.defaultComponentContext
 import com.tangem.core.decompose.context.AppComponentContext
+import com.tangem.core.decompose.context.childByContext
 import com.tangem.core.decompose.di.RootAppComponentContext
 import com.tangem.core.ui.UiDependencies
 import com.tangem.core.ui.message.EventMessageEffect
 import com.tangem.core.ui.message.EventMessageHandler
 import com.tangem.core.ui.screen.ComposeFragment
+import com.tangem.domain.wallets.models.UserWalletId
 import com.tangem.features.details.component.DetailsComponent
-import com.tangem.features.details.component.preview.PreviewDetailsComponent
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -21,8 +23,8 @@ internal class DetailsFragment : ComposeFragment() {
     @Inject
     override lateinit var uiDependencies: UiDependencies
 
-    // @Inject
-    // internal lateinit var componentFactory: DetailsComponent.Factory
+    @Inject
+    internal lateinit var componentFactory: DetailsComponent.Factory
 
     @Inject
     internal lateinit var detailsRouter: DetailsRouter
@@ -37,7 +39,7 @@ internal class DetailsFragment : ComposeFragment() {
 
     @Composable
     override fun ScreenContent(modifier: Modifier) {
-        component.View(modifier = modifier)
+        component.Content(modifier = modifier)
 
         EventMessageEffect(
             messageHandler = messageHandler,
@@ -46,27 +48,23 @@ internal class DetailsFragment : ComposeFragment() {
     }
 
     private fun initComponent(): DetailsComponent {
-        // TODO: Uncomment in [REDACTED_JIRA]
-        // val selectedUserWalletId = arguments?.getString(DetailsEntryPoint.USER_WALLET_ID_KEY)
-        //     ?.let(::UserWalletId)
-        //
-        //
-        // requireNotNull(selectedUserWalletId) { "UserWalletId must be provided" }
-        //
-        // val context = rootContext.childByContext(
-        //     componentContext = defaultComponentContext(requireActivity().onBackPressedDispatcher),
-        //     messageHandler = messageHandler,
-        //     router = detailsRouter,
-        // )
-        //
-        // return componentFactory.create(
-        //     context = context,
-        //     params = DetailsComponent.Params(
-        //         selectedUserWalletId = selectedUserWalletId,
-        //     ),
-        // )
+        val selectedUserWalletId = arguments?.getString(DetailsEntryPoint.USER_WALLET_ID_KEY)
+            ?.let(::UserWalletId)
 
-        return PreviewDetailsComponent()
+        requireNotNull(selectedUserWalletId) { "UserWalletId must be provided" }
+
+        val context = rootContext.childByContext(
+            componentContext = defaultComponentContext(requireActivity().onBackPressedDispatcher),
+            messageHandler = messageHandler,
+            router = detailsRouter,
+        )
+
+        return componentFactory.create(
+            context = context,
+            params = DetailsComponent.Params(
+                selectedUserWalletId = selectedUserWalletId,
+            ),
+        )
     }
 
     companion object : DetailsEntryPoint {
