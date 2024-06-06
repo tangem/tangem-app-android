03788af24dd460f6880c4583fde0e23875622091
Ilya Shulgin <shulgin.ilya.i@gmail.com>
2024-06-06 18:39:33 +0200

    AND-7219 Fixed CE.

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/transformers/HideBalanceStateTransformer.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/transformers/HideBalanceStateTransformer.kt
@@ -4,7 +4,7 @@ import com.tangem.features.staking.impl.presentation.state.StakingUiState
 
 internal class HideBalanceStateTransformer(
     private val isBalanceHidden: Boolean,
-) : StakingScreenStateTransformer() {
+) : StakingScreenStateTransformer {
 
     override fun transform(prevState: StakingUiState): StakingUiState {
         return prevState.copy(isBalanceHidden = isBalanceHidden)
