bf9ca5c9fb5cc05d209ca2e17050b94c6a1ba9ab
Tangem <git@tangem.com>
2024-06-10 18:03:44 +0300

    Updated on 2026-06-12

---
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/StakingStateController.kt
@@ -0,0 +1,47 @@
+package com.tangem.features.staking.impl.presentation.state
+
+import com.tangem.core.ui.event.consumedEvent
+import com.tangem.features.staking.impl.presentation.state.transformers.StakingScreenStateTransformer
+import com.tangem.features.staking.impl.presentation.viewmodel.StakingClickIntents
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+internal class StakingStateController @Inject constructor() {
+
+    val value: StakingUiState get() = uiState.value
+
+    private val mutableUiState: MutableStateFlow<StakingUiState> = MutableStateFlow(value = getInitialState())
+
+    val uiState: StateFlow<StakingUiState> get() = mutableUiState.asStateFlow()
+
+    fun update(function: (StakingUiState) -> StakingUiState) {
+        mutableUiState.update(function = function)
+    }
+
+    fun update(transformer: StakingScreenStateTransformer) {
+        mutableUiState.update(function = transformer::transform)
+    }
+
+    fun clear() {
+        mutableUiState.update { getInitialState() }
+    }
+
+    private fun getInitialState(): StakingUiState {
+        return StakingUiState(
+            clickIntents = object : StakingClickIntents {},
+            cryptoCurrencyName = "",
+            currentScreen = StakingUiStateType.InitialInfo,
+            initialInfoState = StakingStates.InitialInfoState.Empty(),
+            amountState = StakingStates.AmountState.Empty(),
+            validatorState = StakingStates.ValidatorState.Empty(),
+            confirmStakingState = StakingStates.ConfirmStakingState.Empty(),
+            isBalanceHidden = false,
+            event = consumedEvent(),
+        )
+    }
+}
