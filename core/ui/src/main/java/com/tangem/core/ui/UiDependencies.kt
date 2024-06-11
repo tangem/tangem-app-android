1414600a1052528219f83ca01001977798fbebed
Tangem <git@tangem.com>
2024-06-11 15:29:17 +0400

    Updated on 2026-09-21

--- core/ui/src/main/java/com/tangem/core/ui/UiDependencies.kt
+++ core/ui/src/main/java/com/tangem/core/ui/UiDependencies.kt
@@ -1,11 +1,19 @@
 package com.tangem.core.ui
 
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Stable
 import com.tangem.core.ui.haptic.HapticManager
+import com.tangem.core.ui.message.EventMessageHandler
 import com.tangem.core.ui.theme.AppThemeModeHolder
 
+@Stable
 interface UiDependencies {
 
     val hapticManager: HapticManager
 
     val appThemeModeHolder: AppThemeModeHolder
+
+    val globalSnackbarHostState: SnackbarHostState
+
+    val eventMessageHandler: EventMessageHandler
 }
