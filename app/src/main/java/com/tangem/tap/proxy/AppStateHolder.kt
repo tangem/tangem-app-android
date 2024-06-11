de1a23f0e3933b6ee51d49542e3b399e60130890
Tangem <git@tangem.com>
2024-06-11 14:49:38 +0400

    Updated on 2026-07-07

--- app/src/main/java/com/tangem/tap/proxy/AppStateHolder.kt
+++ app/src/main/java/com/tangem/tap/proxy/AppStateHolder.kt
@@ -10,9 +10,7 @@ import com.tangem.domain.wallets.models.UserWallet
 import com.tangem.tap.common.extensions.dispatchOnMain
 import com.tangem.tap.common.extensions.dispatchWithMain
 import com.tangem.tap.common.extensions.onUserWalletSelected
-import com.tangem.tap.common.feedback.FeedbackEmail
 import com.tangem.tap.common.redux.AppState
-import com.tangem.tap.common.redux.global.GlobalAction
 import com.tangem.tap.domain.sdk.TangemSdkManager
 import com.tangem.tap.network.exchangeServices.ExchangeService
 import org.rekotlin.Action
@@ -55,9 +53,5 @@ class AppStateHolder @Inject constructor() : ReduxNavController, ReduxStateHolde
 
     override suspend fun onUserWalletSelected(userWallet: UserWallet) {
         mainStore?.onUserWalletSelected(userWallet)
-    }
-
-    override fun sendFeedbackEmail() {
-        mainStore?.dispatch(GlobalAction.SendEmail(FeedbackEmail()))
     }
 }
