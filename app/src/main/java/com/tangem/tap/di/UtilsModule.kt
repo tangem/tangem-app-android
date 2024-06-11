d7169f84f3010f7ba4725984c718adad5b398a18
Tangem <git@tangem.com>
2024-06-11 15:00:57 +0400

    Updated on 2026-04-06

--- app/src/main/java/com/tangem/tap/di/UtilsModule.kt
+++ app/src/main/java/com/tangem/tap/di/UtilsModule.kt
@@ -1,8 +1,10 @@
 package com.tangem.tap.di
 
 import com.tangem.core.navigation.feedback.FeedbackManager
+import com.tangem.core.navigation.share.ShareManager
 import com.tangem.core.navigation.url.UrlOpener
 import com.tangem.tap.common.feedback.ProxyFeedbackManager
+import com.tangem.tap.common.share.IntentShareManager
 import com.tangem.tap.common.url.CustomTabsUrlOpener
 import dagger.Module
 import dagger.Provides
@@ -13,6 +15,10 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 internal object UtilsModule {
+
+    @Provides
+    @Singleton
+    fun provideShareManager(): ShareManager = IntentShareManager
 
     @Provides
     @Singleton
