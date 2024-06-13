24f387a62da6ba0f81897c0a7e596b9294aa593f
Tangem <git@tangem.com>
2024-06-13 11:22:32 +0400

    Updated on 2026-05-04

--- app/src/main/java/com/tangem/tap/common/ui/RussianCardholdersWarningBottomSheetDialog.kt
+++ app/src/main/java/com/tangem/tap/common/ui/RussianCardholdersWarningBottomSheetDialog.kt
@@ -9,7 +9,6 @@ import com.tangem.tap.common.analytics.events.Token
 import com.tangem.tap.common.extensions.dispatchDialogHide
 import com.tangem.tap.common.extensions.dispatchOpenUrl
 import com.tangem.tap.common.redux.AppDialog
-import com.tangem.tap.common.url.urlOpener
 import com.tangem.tap.store
 import com.tangem.wallet.databinding.DialogRussiansCardholdersWarningBinding
 
@@ -44,7 +43,7 @@ class RussianCardholdersWarningBottomSheetDialog(
             dismiss()
         }
         binding?.btnNo?.setOnClickListener {
-            urlOpener.openUrl(INSTRUCTION_URL)
+            store.dispatchOpenUrl(INSTRUCTION_URL)
             dismiss()
         }
     }
