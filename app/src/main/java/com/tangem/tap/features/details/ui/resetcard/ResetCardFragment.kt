7c95d207d1b63a7f40a8ea0ecd5992e05e800d80
Tangem <git@tangem.com>
2024-06-13 12:29:33 +0400

    Updated on 2026-05-13

--- app/src/main/java/com/tangem/tap/features/details/ui/resetcard/ResetCardFragment.kt
+++ app/src/main/java/com/tangem/tap/features/details/ui/resetcard/ResetCardFragment.kt
@@ -4,9 +4,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.tangem.core.navigation.NavigationAction
+import com.tangem.common.routing.AppRouter
 import com.tangem.core.ui.UiDependencies
 import com.tangem.core.ui.screen.ComposeFragment
+import com.tangem.tap.common.extensions.dispatchNavigationAction
 import com.tangem.tap.features.details.redux.DetailsState
 import com.tangem.tap.store
 import dagger.hilt.android.AndroidEntryPoint
@@ -30,7 +31,7 @@ internal class ResetCardFragment : ComposeFragment(), StoreSubscriber<DetailsSta
 
         ResetCardScreen(
             state = state,
-            onBackClick = { store.dispatch(NavigationAction.PopBackTo()) },
+            onBackClick = { store.dispatchNavigationAction(AppRouter::pop) },
             modifier = modifier,
         )
     }
