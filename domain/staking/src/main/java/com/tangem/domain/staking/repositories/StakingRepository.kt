aafe2504fc673caaa61a5af61fd4ca37ae66c456
Tangem <git@tangem.com>
2024-06-13 17:08:24 +0300

    Updated on 2026-09-21

--- domain/staking/src/main/java/com/tangem/domain/staking/repositories/StakingRepository.kt
+++ domain/staking/src/main/java/com/tangem/domain/staking/repositories/StakingRepository.kt
@@ -2,18 +2,21 @@ package com.tangem.domain.staking.repositories
 
 import com.tangem.domain.staking.model.StakingAvailability
 import com.tangem.domain.staking.model.StakingEntryInfo
+import com.tangem.domain.staking.model.Yield
 import com.tangem.domain.tokens.model.CryptoCurrency
 
 interface StakingRepository {
+
+    fun isStakingSupported(currencyId: String): Boolean
+
+    suspend fun fetchEnabledYields()
+
+    suspend fun getEntryInfo(integrationId: String): StakingEntryInfo
+
+    suspend fun getYield(cryptoCurrencyId: CryptoCurrency.ID, symbol: String): Yield
 
     suspend fun getStakingAvailabilityForActions(
         cryptoCurrencyId: CryptoCurrency.ID,
         symbol: String,
     ): StakingAvailability
-
-    suspend fun getEntryInfo(integrationId: String): StakingEntryInfo
-
-    suspend fun fetchEnabledYields()
-
-    fun isStakingSupported(currencyId: String): Boolean
 }
