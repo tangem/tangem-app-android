847658e6b5e8012d8cc8f84dc5e6d76e0b98f372
iMaks99 <maks.ismoilov0618@gmail.com>
2024-06-14 21:11:05 +0500

    AND-7136 Separated amount state to Data and Empty states

--- common/ui/src/main/java/com/tangem/common/ui/amountScreen/converters/AmountReduceByTransformer.kt
+++ common/ui/src/main/java/com/tangem/common/ui/amountScreen/converters/AmountReduceByTransformer.kt
@@ -8,7 +8,6 @@ import com.tangem.common.ui.amountScreen.utils.getFiatValue
 import com.tangem.common.ui.amountScreen.utils.getKeyboardAction
 import com.tangem.core.ui.utils.parseBigDecimal
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
-import com.tangem.utils.Provider
 import com.tangem.utils.isNullOrZero
 import com.tangem.utils.transformer.Transformer
 import java.math.BigDecimal
@@ -16,13 +15,16 @@ import java.math.BigDecimal
 /**
  * Reduces amount by specific value
  *
- * @property cryptoCurrencyStatusProvider current cryptocurrency status provider
+ * @property cryptoCurrencyStatus current cryptocurrency status
+ * @property value reduced by value
  */
 class AmountReduceByTransformer(
-    private val cryptoCurrencyStatusProvider: Provider<CryptoCurrencyStatus>,
-) : Transformer<AmountState, AmountReduceByTransformer.ReduceByData> {
-    override fun transform(prevState: AmountState, value: ReduceByData): AmountState {
-        val cryptoCurrencyStatus = cryptoCurrencyStatusProvider()
+    private val cryptoCurrencyStatus: CryptoCurrencyStatus,
+    private val value: ReduceByData,
+) : Transformer<AmountState> {
+    override fun transform(prevState: AmountState): AmountState {
+        if (prevState !is AmountState.Data) return prevState
+
         val amountTextField = prevState.amountTextField
         val cryptoDecimals = amountTextField.cryptoAmount.decimals
         val fiatDecimals = amountTextField.fiatAmount.decimals
