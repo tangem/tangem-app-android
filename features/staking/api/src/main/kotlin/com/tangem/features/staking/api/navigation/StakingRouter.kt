48402f057cc103078440202ed2d39430389c1447
Tangem <git@tangem.com>
2024-06-14 13:24:05 +0400

    Updated on 2026-07-23

--- features/staking/api/src/main/kotlin/com/tangem/features/staking/api/navigation/StakingRouter.kt
+++ features/staking/api/src/main/kotlin/com/tangem/features/staking/api/navigation/StakingRouter.kt
@@ -5,10 +5,4 @@ import androidx.fragment.app.Fragment
 interface StakingRouter {
 
     fun getEntryFragment(): Fragment
-
-    companion object {
-        const val CRYPTO_CURRENCY_ID_KEY = "CRYPTO_CURRENCY_ID_KEY"
-        const val USER_WALLET_ID_KEY = "USER_WALLET_ID_KEY"
-        const val YIELD_KEY = "YIELD_KEY"
-    }
 }
