02e45e9dc46713c53e009855f204ff041877bb44
Tangem <git@tangem.com>
2024-06-17 13:29:44 +0300

    Updated on 2026-01-24

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/transformers/StakingScreenStateTransformer.kt
+++
@@ -1,8 +0,0 @@
-package com.tangem.features.staking.impl.presentation.state.transformers
-
-import com.tangem.features.staking.impl.presentation.state.StakingUiState
-
-internal interface StakingScreenStateTransformer {
-
-    fun transform(prevState: StakingUiState): StakingUiState
-}
