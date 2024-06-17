4066c9a9bd6fabe8bbd835d19578b68960777e83
Tangem <git@tangem.com>
2024-06-17 13:17:08 +0500

    Updated on 2026-09-21

--- domain/settings/src/main/java/com/tangem/domain/settings/repositories/PermissionRepository.kt
+++ domain/settings/src/main/java/com/tangem/domain/settings/repositories/PermissionRepository.kt
@@ -6,34 +6,34 @@ interface PermissionRepository {
      * Return true if should display screen ONCE asking to allow [permission].
      * False otherwise or screen already was displayed
      */
-    suspend fun shouldInitiallyShowPermissionScreen(permission: Int): Boolean
+    suspend fun shouldInitiallyShowPermissionScreen(permission: String): Boolean
 
     /**
      * Indicates which time [permission] was asked via platform dialog.
      * NOTE: Use this method to indicate either reroute to settings or display platform dialog.
      */
-    suspend fun isFirstTimeAskingPermission(permission: Int): Boolean
+    suspend fun isFirstTimeAskingPermission(permission: String): Boolean
 
     /**
      * Sets value indicating that [permission] was asked via platform dialog.
      * NOTE: Use this method to indicate either reroute to settings or display platform dialog.
      */
-    suspend fun setFirstTimeAskingPermission(permission: Int, value: Boolean)
+    suspend fun setFirstTimeAskingPermission(permission: String, value: Boolean)
 
     /**
      * Is clear to ask [permission].
      * User could already granted or permanently denied permission
      * Or there is an active delay before next request
      */
-    suspend fun shouldAskPermission(permission: Int): Boolean
+    suspend fun shouldAskPermission(permission: String): Boolean
 
     /**
      * Permanently deny [permission] and never request again
      */
-    suspend fun neverAskPermission(permission: Int)
+    suspend fun neverAskPermission(permission: String)
 
     /**
      * Delay next [permission] request for some time or active sessions
      */
-    suspend fun delayPermissionAsking(permission: Int)
+    suspend fun delayPermissionAsking(permission: String)
 }
