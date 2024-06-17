79015d8e55f7435c05a6681d9b00289a155321cc
Tangem <git@tangem.com>
2024-06-17 16:42:57 +0300

    Updated on 2026-07-07

--- features/disclaimer/impl/src/main/java/com/tangem/features/disclaimer/impl/DisclaimerFragment.kt
+++ features/disclaimer/impl/src/main/java/com/tangem/features/disclaimer/impl/DisclaimerFragment.kt
@@ -7,8 +7,6 @@ import androidx.compose.ui.Modifier
 import androidx.fragment.app.viewModels
 import com.tangem.common.routing.AppRouter
 import com.tangem.core.ui.UiDependencies
-import com.tangem.core.ui.components.SystemBarsEffect
-import com.tangem.core.ui.res.TangemTheme
 import com.tangem.core.ui.screen.ComposeFragment
 import com.tangem.features.disclaimer.api.DisclaimerRouter
 import com.tangem.features.disclaimer.impl.presentation.ui.DisclaimerScreen
@@ -37,16 +35,12 @@ internal class DisclaimerFragment : ComposeFragment() {
 
     @Composable
     override fun ScreenContent(modifier: Modifier) {
-        val systemBarsColor = TangemTheme.colors.background.primary
         BackHandler {
             if (isTosAccepted) {
                 appRouter.pop()
             } else {
                 requireActivity().finish()
             }
-        }
-        SystemBarsEffect {
-            setSystemBarsColor(systemBarsColor)
         }
         DisclaimerScreen(viewModel.state, appRouter::pop)
     }
