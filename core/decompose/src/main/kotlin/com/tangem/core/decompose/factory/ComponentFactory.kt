c4b720d01a6a959a1c4aa30289009dacc5731f51
Tangem <git@tangem.com>
2024-06-17 13:26:18 +0400

    Updated on 2026-07-07

--- core/decompose/src/main/kotlin/com/tangem/core/decompose/factory/ComponentFactory.kt
+++ core/decompose/src/main/kotlin/com/tangem/core/decompose/factory/ComponentFactory.kt
@@ -2,7 +2,7 @@ package com.tangem.core.decompose.factory
 
 import com.tangem.core.decompose.context.AppComponentContext
 
-interface ComponentFactory<Component : Any, Params : Any> {
+interface ComponentFactory<P : Any, C : Any> {
 
-    fun create(context: AppComponentContext, params: Params): Component
+    fun create(context: AppComponentContext, params: P): C
 }
