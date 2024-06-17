f1264d8791f37199ca63496fbc228277d515bbd2
Tangem <git@tangem.com>
2024-06-17 13:29:44 +0300

    Updated on 2026-03-27

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/viewmodel/StakingClickIntents.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/viewmodel/StakingClickIntents.kt
@@ -1,10 +1,14 @@
 package com.tangem.features.staking.impl.presentation.viewmodel
 
-internal interface StakingClickIntents {
+import com.tangem.common.ui.amountScreen.AmountScreenClickIntents
+
+internal interface StakingClickIntents : AmountScreenClickIntents {
 
     fun onBackClick()
 
     fun onNextClick()
 
     fun onPrevClick()
+
+    override fun onAmountNext() = onNextClick()
 }
