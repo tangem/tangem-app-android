6f405dd1102b4a00c4051740a9520cb0cb3390e5
Tangem <git@tangem.com>
2024-06-17 13:29:44 +0300

    Updated on 2026-08-07

--- common/ui/src/main/java/com/tangem/common/ui/amountScreen/converters/AmountPastedTriggerDismissTransformer.kt
+++ common/ui/src/main/java/com/tangem/common/ui/amountScreen/converters/AmountPastedTriggerDismissTransformer.kt
@@ -6,8 +6,10 @@ import com.tangem.utils.transformer.Transformer
 /**
  * Dismisses indication on pasted value
  */
-class AmountPastedTriggerDismissTransformer : Transformer<AmountState, Boolean> {
-    override fun transform(prevState: AmountState, value: Boolean): AmountState {
+class AmountPastedTriggerDismissTransformer : Transformer<AmountState> {
+    override fun transform(prevState: AmountState): AmountState {
+        if (prevState !is AmountState.Data) return prevState
+
         return prevState.copy(
             amountTextField = prevState.amountTextField.copy(
                 isValuePasted = false,
