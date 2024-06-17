8f7cd26bea39ef12f5f5cc0acfd7d38657e91b01
Tangem <git@tangem.com>
2024-06-17 13:18:35 +0500

    Updated on 2026-09-21

--- app/src/main/java/com/tangem/tap/common/settings/IntentSettingsManager.kt
+++ app/src/main/java/com/tangem/tap/common/settings/IntentSettingsManager.kt
@@ -1,20 +1,17 @@
 package com.tangem.tap.common.settings
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.provider.Settings
 import com.tangem.core.navigation.settings.SettingsManager
-import com.tangem.tap.foregroundActivityObserver
-import com.tangem.tap.withForegroundActivity
 
-internal class IntentSettingsManager : SettingsManager {
+internal class IntentSettingsManager(val context: Context) : SettingsManager {
     override fun openSettings() {
-        foregroundActivityObserver.withForegroundActivity { activity ->
-            val openSettingsIntent = Intent(
-                Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-                Uri.fromParts("package", activity.packageName, null),
-            )
-            activity.startActivity(openSettingsIntent)
-        }
+        val openSettingsIntent = Intent(
+            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+            Uri.fromParts("package", context.packageName, null),
+        )
+        context.startActivity(openSettingsIntent)
     }
 }
