cb9132d1e6b535190e9628091887250792ef7792
Tangem <git@tangem.com>
2024-06-18 21:26:16 +0300

    Updated on 2026-07-23

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/ui/block/StakingFeeBlock.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/ui/block/StakingFeeBlock.kt
@@ -1,9 +1,8 @@
-package com.tangem.features.send.impl.presentation.ui.send
+package com.tangem.features.staking.impl.presentation.ui.block
 
 import android.content.res.Configuration
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,27 +13,30 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
-import com.tangem.common.ui.amountScreen.utils.getCryptoReference
-import com.tangem.common.ui.amountScreen.utils.getFiatReference
+import com.tangem.blockchain.common.Amount
+import com.tangem.blockchain.common.AmountType
+import com.tangem.blockchain.common.transaction.Fee
+import com.tangem.blockchain.common.transaction.TransactionFee
 import com.tangem.core.ui.components.RectangleShimmer
 import com.tangem.core.ui.components.rows.SelectorRowItem
 import com.tangem.core.ui.res.TangemThemePreview
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.core.ui.utils.BigDecimalFormatter
-import com.tangem.features.send.impl.R
-import com.tangem.features.send.impl.presentation.state.SendStates
-import com.tangem.features.send.impl.presentation.state.fee.FeeSelectorState
-import com.tangem.features.send.impl.presentation.state.fee.FeeType
-import com.tangem.features.send.impl.presentation.state.previewdata.FeeStatePreviewData
+import com.tangem.common.ui.R
+import com.tangem.common.ui.amountScreen.utils.getCryptoReference
+import com.tangem.common.ui.amountScreen.utils.getFiatReference
+import com.tangem.domain.appcurrency.model.AppCurrency
+import com.tangem.features.staking.impl.presentation.state.InnerFeeState
+import com.tangem.features.staking.impl.presentation.state.StakingStates
+import java.math.BigDecimal
 
 @Composable
-internal fun FeeBlock(feeState: SendStates.FeeState, isClickDisabled: Boolean, onClick: () -> Unit) {
+internal fun StakingFeeBlock(feeState: StakingStates.FeeState) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .clip(TangemTheme.shapes.roundedCornersXMedium)
             .background(TangemTheme.colors.background.action)
-            .clickable(enabled = !isClickDisabled, onClick = onClick)
             .padding(TangemTheme.dimens.spacing12),
     ) {
         Text(
@@ -46,18 +48,8 @@ internal fun FeeBlock(feeState: SendStates.FeeState, isClickDisabled: Boolean, o
         Box(
             modifier = Modifier.padding(top = TangemTheme.dimens.spacing8),
         ) {
-            val feeSelectorState = feeState.feeSelectorState
             val feeAmount = feeState.fee?.amount
-            val (title, icon) = if (feeSelectorState is FeeSelectorState.Content) {
-                when (feeSelectorState.selectedFee) {
-                    FeeType.Slow -> R.string.common_fee_selector_option_slow to R.drawable.ic_tortoise_24
-                    FeeType.Market -> R.string.common_fee_selector_option_market to R.drawable.ic_bird_24
-                    FeeType.Fast -> R.string.common_fee_selector_option_fast to R.drawable.ic_hare_24
-                    FeeType.Custom -> R.string.common_fee_selector_option_custom to R.drawable.ic_edit_24
-                }
-            } else {
-                R.string.common_fee_selector_option_market to R.drawable.ic_bird_24
-            }
+            val (title, icon) = R.string.common_fee_selector_option_market to R.drawable.ic_bird_24
             SelectorRowItem(
                 titleRes = title,
                 iconRes = icon,
@@ -73,20 +65,20 @@ internal fun FeeBlock(feeState: SendStates.FeeState, isClickDisabled: Boolean, o
                 showSelectedAppearance = false,
                 paddingValues = PaddingValues(),
             )
-            FeeLoading(feeSelectorState)
-            FeeError(feeSelectorState)
+            FeeLoading(feeState.innerFeeState)
+            FeeError(feeState.innerFeeState)
         }
     }
 }
 
 @Composable
-private fun BoxScope.FeeLoading(feeSelectorState: FeeSelectorState) {
+private fun BoxScope.FeeLoading(feeSelectorState: InnerFeeState) {
     AnimatedContent(
         targetState = feeSelectorState,
         label = "Fee Loading State Change",
         modifier = Modifier.align(Alignment.CenterEnd),
     ) {
-        if (it == FeeSelectorState.Loading) {
+        if (it == InnerFeeState.Loading) {
             RectangleShimmer(
                 radius = TangemTheme.dimens.radius3,
                 modifier = Modifier.size(
@@ -99,13 +91,13 @@ private fun BoxScope.FeeLoading(feeSelectorState: FeeSelectorState) {
 }
 
 @Composable
-private fun BoxScope.FeeError(feeSelectorState: FeeSelectorState) {
+private fun BoxScope.FeeError(feeSelectorState: InnerFeeState) {
     AnimatedContent(
         targetState = feeSelectorState,
         label = "Fee Error State Change",
         modifier = Modifier.align(Alignment.CenterEnd),
     ) {
-        if (it == FeeSelectorState.Error) {
+        if (it == InnerFeeState.Error) {
             Text(
                 text = BigDecimalFormatter.EMPTY_BALANCE_SIGN,
                 color = TangemTheme.colors.text.primary1,
@@ -119,21 +111,38 @@ private fun BoxScope.FeeError(feeSelectorState: FeeSelectorState) {
 @Preview
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
-private fun FeeBlockPreview(@PreviewParameter(FeeBlockPreviewProvider::class) value: SendStates.FeeState) {
+private fun FeeBlockPreview(@PreviewParameter(FeeBlockPreviewProvider::class) value: StakingStates.FeeState) {
     TangemThemePreview {
-        FeeBlock(
+        StakingFeeBlock(
             feeState = value,
-            isClickDisabled = true,
-            onClick = {},
         )
     }
 }
 
-private class FeeBlockPreviewProvider : PreviewParameterProvider<SendStates.FeeState> {
+private class FeeBlockPreviewProvider : PreviewParameterProvider<StakingStates.FeeState> {
 
-    override val values: Sequence<SendStates.FeeState>
+    override val values: Sequence<StakingStates.FeeState>
         get() = sequenceOf(
-            FeeStatePreviewData.feeState,
+            feeState,
         )
+
+    private val fee = Fee.Common(
+        amount = Amount(
+            currencySymbol = "MATIC",
+            value = BigDecimal(0.159806),
+            decimals = 18,
+            type = AmountType.Coin,
+        ),
+    )
+
+    private val feeState = StakingStates.FeeState(
+        innerFeeState = InnerFeeState.Content(TransactionFee.Single(normal = fee)),
+        fee = fee,
+        rate = BigDecimal.ONE,
+        appCurrency = AppCurrency.Default,
+        isFeeApproximate = false,
+        isFeeConvertibleToFiat = true,
+    )
 }
+
 // endregion
