93bee40a4a6a25c76a2255632c4888b1f350760d
Tangem <git@tangem.com>
2024-06-18 21:26:16 +0300

    Updated on 2025-12-30

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/StakingUiState.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/StakingUiState.kt
@@ -1,10 +1,15 @@
 package com.tangem.features.staking.impl.presentation.state
 
 import androidx.compose.runtime.Immutable
+import com.tangem.blockchain.common.transaction.Fee
 import com.tangem.common.ui.amountScreen.models.AmountState
 import com.tangem.core.ui.event.StateEvent
 import com.tangem.core.ui.extensions.TextReference
+import com.tangem.domain.appcurrency.model.AppCurrency
+import com.tangem.domain.staking.model.Yield
 import com.tangem.features.staking.impl.presentation.viewmodel.StakingClickIntents
+import kotlinx.collections.immutable.ImmutableList
+import java.math.BigDecimal
 
 /**
  * Ui states of the staking screen
@@ -16,7 +21,6 @@ internal data class StakingUiState(
     val currentStep: StakingStep,
     val initialInfoState: StakingStates.InitialInfoState,
     val amountState: AmountState,
-    val validatorState: StakingStates.ValidatorState,
     val confirmStakingState: StakingStates.ConfirmStakingState,
     val isBalanceHidden: Boolean,
     val event: StateEvent<StakingEvent>,
@@ -25,12 +29,10 @@ internal data class StakingUiState(
     fun copyWrapped(
         initialInfoState: StakingStates.InitialInfoState = this.initialInfoState,
         amountState: AmountState = this.amountState,
-        validatorState: StakingStates.ValidatorState = this.validatorState,
         confirmStakingState: StakingStates.ConfirmStakingState = this.confirmStakingState,
     ): StakingUiState = copy(
         initialInfoState = initialInfoState,
         amountState = amountState,
-        validatorState = validatorState,
         confirmStakingState = confirmStakingState,
     )
 }
@@ -58,32 +60,14 @@ internal sealed class StakingStates {
         ) : InitialInfoState()
     }
 
-    /** Validator state */
-    sealed class ValidatorState : StakingStates() {
-        data class Data(
-            override val isPrimaryButtonEnabled: Boolean,
-        ) : ValidatorState()
-
-        data class Empty(
-            override val isPrimaryButtonEnabled: Boolean = false,
-        ) : ValidatorState()
-    }
-
-    /** Fee state */
-    sealed class FeeState : StakingStates() {
-        data class Data(
-            override val isPrimaryButtonEnabled: Boolean,
-        ) : FeeState()
-
-        data class Empty(
-            override val isPrimaryButtonEnabled: Boolean = false,
-        ) : FeeState()
-    }
-
     /** Confirm state */
     sealed class ConfirmStakingState : StakingStates() {
         data class Data(
             override val isPrimaryButtonEnabled: Boolean,
+            val feeState: FeeState,
+            val validatorState: ValidatorState,
+            val notifications: ImmutableList<StakingNotification>,
+            val footerText: String,
             val isSuccess: Boolean,
             val isStaking: Boolean,
         ) : ConfirmStakingState()
@@ -92,11 +76,25 @@ internal sealed class StakingStates {
             override val isPrimaryButtonEnabled: Boolean = false,
         ) : ConfirmStakingState()
     }
+
+    data class FeeState(
+        val innerFeeState: InnerFeeState,
+        val fee: Fee?,
+        val rate: BigDecimal?,
+        val isFeeConvertibleToFiat: Boolean,
+        val appCurrency: AppCurrency,
+        val isFeeApproximate: Boolean,
+    )
+
+    data class ValidatorState(
+        val validatorState: InnerValidatorState,
+        val availableValidators: List<Yield.Validator>,
+    )
 }
 
 enum class StakingStep {
     InitialInfo,
     Amount,
-    ValidatorAndFee,
     Confirm,
+    Success,
 }
