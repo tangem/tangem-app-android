6723b352b6c75132f796ce00e572b824faaaed8e
Tangem <git@tangem.com>
2024-06-20 02:26:36 +0300

    Updated on 2025-12-30

---
+++ domain/staking/models/src/main/kotlin/com/tangem/domain/staking/model/transaction/StakingGasEstimate.kt
@@ -0,0 +1,10 @@
+package com.tangem.domain.staking.model.transaction
+
+import com.tangem.domain.staking.model.Token
+import java.math.BigDecimal
+
+data class StakingGasEstimate(
+    val amount: BigDecimal,
+    val token: Token,
+    val gasLimit: BigDecimal,
+)
