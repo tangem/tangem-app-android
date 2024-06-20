fa77b7aca44aae5a24219a90d7cf7ed111af1a58
Tangem <git@tangem.com>
2024-06-20 17:58:31 +0500

    Updated on 2026-09-21

--- features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/StakingBlockState.kt
+++ features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/StakingBlockState.kt
@@ -1,15 +1,20 @@
 package com.tangem.feature.tokendetails.presentation.tokendetails.state
 
 import androidx.compose.runtime.Immutable
+import com.tangem.core.ui.extensions.TextReference
+
+internal data class StakingBlocksState(
+    val stakingAvailable: StakingAvailable,
+    val stakingBalance: StakingBalance,
+)
 
 @Immutable
-internal sealed interface StakingBlockState {
-
+internal sealed interface StakingAvailable {
     val iconState: IconState
 
-    data class Error(override val iconState: IconState) : StakingBlockState
+    data class Error(override val iconState: IconState) : StakingAvailable
 
-    data class Loading(override val iconState: IconState) : StakingBlockState
+    data class Loading(override val iconState: IconState) : StakingAvailable
 
     data class Content(
         override val iconState: IconState,
@@ -17,5 +22,16 @@ internal sealed interface StakingBlockState {
         val periodInDays: Int,
         val tokenSymbol: String,
         val onStakeClicked: () -> Unit,
-    ) : StakingBlockState
+    ) : StakingAvailable
+}
+
+@Immutable
+sealed class StakingBalance {
+    data object Empty : StakingBalance()
+
+    data class Content(
+        val cryptoAmount: TextReference,
+        val fiatAmount: TextReference,
+        val rewardAmount: TextReference,
+    ) : StakingBalance()
 }
