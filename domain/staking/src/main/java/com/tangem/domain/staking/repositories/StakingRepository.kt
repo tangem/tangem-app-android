7594ee6c9bbbdbc07c899068735317ea11c344ac
Tangem <git@tangem.com>
2024-06-21 21:35:19 +0500

    release/5.26.1

--- domain/staking/src/main/java/com/tangem/domain/staking/repositories/StakingRepository.kt
+++ domain/staking/src/main/java/com/tangem/domain/staking/repositories/StakingRepository.kt
@@ -1,9 +1,10 @@
 package com.tangem.domain.staking.repositories
 
-import com.tangem.domain.staking.model.StakingAvailability
-import com.tangem.domain.staking.model.StakingEntryInfo
-import com.tangem.domain.staking.model.Yield
+import com.tangem.domain.staking.model.*
 import com.tangem.domain.tokens.model.CryptoCurrency
+import com.tangem.domain.tokens.model.NetworkStatus
+import com.tangem.domain.wallets.models.UserWalletId
+import kotlinx.coroutines.flow.Flow
 
 interface StakingRepository {
 
@@ -19,4 +20,30 @@ interface StakingRepository {
         cryptoCurrencyId: CryptoCurrency.ID,
         symbol: String,
     ): StakingAvailability
+
+    suspend fun fetchSingleYieldBalance(
+        userWalletId: UserWalletId,
+        networkStatus: NetworkStatus,
+        integrationId: String,
+        refresh: Boolean = false,
+    )
+
+    fun getSingleYieldBalanceFlow(
+        userWalletId: UserWalletId,
+        networkStatus: NetworkStatus,
+        integrationId: String,
+    ): Flow<List<YieldBalance>>
+
+    suspend fun fetchMultiYieldBalance(
+        userWalletId: UserWalletId,
+        networks: Set<NetworkStatus>,
+        integrationId: String,
+        refresh: Boolean = false,
+    )
+
+    fun getMultiYieldBalanceFlow(
+        userWalletId: UserWalletId,
+        networks: Set<NetworkStatus>,
+        integrationId: String,
+    ): Flow<List<YieldBalanceList>>
 }
