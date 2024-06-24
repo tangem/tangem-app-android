bc5c16b0b4f46924b41ccad58742f81466451113
Andrew Khokhlov <andrew.khokhlove@gmail.com>
2024-06-24 22:47:30 +0800

    [Feedback] Refactor flow of preparing FeedbackEmail using ScanResponse (#2876)
    * AND-7427 Refactor data layer for creating feedback for not logged-in users
    
    Signed-off-by: Mama1emon <andrew.khokhlove@gmail.com>
    
    * AND-7427 Refactor domain layer for creating feedback for not logged-in users
    
    Signed-off-by: Mama1emon <andrew.khokhlove@gmail.com>
    
    * AND-7427 Add use cases to DaggerGraphState
    
    Signed-off-by: Mama1emon <andrew.khokhlove@gmail.com>
    
    * AND-7427 Fix comments
    
    Signed-off-by: Mama1emon <andrew.khokhlove@gmail.com>
    
    ---------
    
    Signed-off-by: Mama1emon <andrew.khokhlove@gmail.com>

---
+++ domain/feedback/src/main/java/com/tangem/domain/feedback/GetCardInfoUseCase.kt
@@ -0,0 +1,23 @@
+package com.tangem.domain.feedback
+
+import arrow.core.Either
+import arrow.core.Either.Companion.catch
+import com.tangem.domain.feedback.models.CardInfo
+import com.tangem.domain.feedback.repository.FeedbackRepository
+import com.tangem.domain.models.scan.ScanResponse
+
+/**
+ * UseCase for creating 'CardInfo'
+ *
+ * @property feedbackRepository feedback repository
+ *
+ * @author Andrew Khokhlov on 19/06/2024
+ */
+class GetCardInfoUseCase(
+    private val feedbackRepository: FeedbackRepository,
+) {
+
+    suspend operator fun invoke(scanResponse: ScanResponse): Either<Throwable, CardInfo> {
+        return catch { feedbackRepository.getCardInfo(scanResponse) }
+    }
+}
