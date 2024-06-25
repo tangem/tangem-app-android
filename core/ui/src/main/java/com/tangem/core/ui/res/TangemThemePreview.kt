20c6306db2df904aaa82ffd2ad006f988605549c
Tangem <git@tangem.com>
2024-06-25 14:15:40 +0300

    Updated on 2026-05-19

--- core/ui/src/main/java/com/tangem/core/ui/res/TangemThemePreview.kt
+++ core/ui/src/main/java/com/tangem/core/ui/res/TangemThemePreview.kt
@@ -3,6 +3,8 @@ package com.tangem.core.ui.res
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalHapticFeedback
+import com.tangem.core.ui.haptic.MockHapticManager
 import com.tangem.core.ui.windowsize.rememberWindowSizePreview
 
 @Composable
@@ -20,6 +22,7 @@ fun TangemThemePreview(
             typography = typography,
             dimens = dimens,
             windowSize = rememberWindowSizePreview(maxWidth, maxHeight),
+            hapticManager = MockHapticManager(LocalHapticFeedback.current),
             content = content,
         )
     }
