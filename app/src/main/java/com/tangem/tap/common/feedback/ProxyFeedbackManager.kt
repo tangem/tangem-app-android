616c7d6552414bc7644000b311032d9a096ac85c
Tangem <git@tangem.com>
2024-06-25 20:58:35 +0800

    Updated on 2026-06-01

--- app/src/main/java/com/tangem/tap/common/feedback/ProxyFeedbackManager.kt
+++ app/src/main/java/com/tangem/tap/common/feedback/ProxyFeedbackManager.kt
@@ -1,13 +1,13 @@
 package com.tangem.tap.common.feedback
 
-import com.tangem.core.navigation.feedback.FeedbackManager
-import com.tangem.core.navigation.feedback.FeedbackType
+import com.tangem.domain.feedback.FeedbackManager
+import com.tangem.domain.feedback.models.FeedbackEmailType
 import com.tangem.tap.store
 import timber.log.Timber
 
 internal class ProxyFeedbackManager : FeedbackManager {
 
-    override fun sendEmail(type: FeedbackType) {
+    override fun sendEmail(type: FeedbackEmailType) {
         val manager = store.state.globalState.feedbackManager
 
         if (manager == null) {
@@ -15,14 +15,6 @@ internal class ProxyFeedbackManager : FeedbackManager {
             return
         }
 
-        val data = when (type) {
-            is FeedbackType.Feedback -> FeedbackEmail()
-            is FeedbackType.RateCanBeBetter -> RateCanBeBetterEmail()
-            is FeedbackType.ScanFails -> ScanFailsEmail()
-            is FeedbackType.SendTransactionFailed -> SendTransactionFailedEmail(type.error)
-            is FeedbackType.Support -> FeedbackEmail()
-        }
-
-        manager.sendEmail(data)
+        manager.sendEmail(type)
     }
 }
