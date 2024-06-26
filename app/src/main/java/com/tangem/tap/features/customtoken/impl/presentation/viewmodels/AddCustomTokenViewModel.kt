d69ccddb6c7193b00a8fb48dd1c6295e08112a22
Doston Kamalov <dkamalov@tangem.com>
2024-06-26 09:43:33 +0500

    Fixed comment

--- app/src/main/java/com/tangem/tap/features/customtoken/impl/presentation/viewmodels/AddCustomTokenViewModel.kt
+++ app/src/main/java/com/tangem/tap/features/customtoken/impl/presentation/viewmodels/AddCustomTokenViewModel.kt
@@ -595,16 +595,15 @@ internal class AddCustomTokenViewModel @Inject constructor(
     }
 
     private fun isTokenAlreadyAdded(): Boolean {
+        val networkSelectorValue = uiState.form.networkSelectorField.selectedItem.blockchain
+        val networkId = Blockchain.fromNetworkId(networkSelectorValue.toNetworkId())?.id
+        val contractAddress = convertTokenAddress(
+            blockchain = networkSelectorValue,
+            address = uiState.form.contractAddressInputField.value,
+        )
         return currentCryptoCurrencies
             .filterIsInstance<CryptoCurrency.Token>()
             .any { token ->
-                val networkSelectorValue = uiState.form.networkSelectorField.selectedItem.blockchain
-                val contractAddress = convertTokenAddress(
-                    blockchain = networkSelectorValue,
-                    address = uiState.form.contractAddressInputField.value,
-                )
-                val networkId = Blockchain.fromNetworkId(networkSelectorValue.toNetworkId())?.id
-
                 val sameId = if (!token.isCustom) {
                     // todo after move foundToken to CryptoCurrency model, use only id
                     foundToken?.id == token.id.rawCurrencyId
