b1f17a8adb0c349091890110cd3bb4064ef65ff4
Tangem <git@tangem.com>
2024-06-26 15:12:57 +0400

    Updated on 2026-06-27

--- plugins/configuration/src/main/kotlin/com/tangem/plugin/configuration/configurations/extension/BaseExtensionConfigurations.kt
+++ plugins/configuration/src/main/kotlin/com/tangem/plugin/configuration/configurations/extension/BaseExtensionConfigurations.kt
@@ -24,7 +24,6 @@ internal fun BaseExtension.configureCompose(project: Project) {
             contains(":features:onboarding") || // TODO: divide on api/impl after migrating all onboarding to module
             contains(Regex(pattern = ":presentation\$")) ||
             contains(Regex(pattern = ":app\$")) || // TODO: [REDACTED_JIRA]
-            contains(Regex(pattern = ":features:manage-tokens:api\$")) || // provides Composable function
             contains(Regex(pattern = ":features:markets:api\$")) || // provides Composable function
             contains(Regex(pattern = ":impl\$"))
     }
