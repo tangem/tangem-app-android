ccf4ace096235cc37986ed76aa0d59573e78124f
Tangem <git@tangem.com>
2024-06-27 20:32:12 +0300

    Updated on 2026-09-01

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/InnerFeeState.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/InnerFeeState.kt
@@ -1,9 +1,7 @@
 package com.tangem.features.staking.impl.presentation.state
 
-import androidx.compose.runtime.Immutable
 import com.tangem.blockchain.common.transaction.TransactionFee
 
-@Immutable
 internal sealed class InnerFeeState {
 
     data class Content(
