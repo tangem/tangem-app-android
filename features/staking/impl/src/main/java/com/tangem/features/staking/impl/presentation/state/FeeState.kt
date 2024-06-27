95c54d063908afbd13d02e686f7a3741e81f3990
Tangem <git@tangem.com>
2024-06-27 20:32:12 +0300

    Updated on 2026-05-14

---
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/FeeState.kt
@@ -0,0 +1,20 @@
+package com.tangem.features.staking.impl.presentation.state
+
+import com.tangem.blockchain.common.transaction.Fee
+import com.tangem.domain.appcurrency.model.AppCurrency
+import java.math.BigDecimal
+
+sealed class FeeState {
+
+    data class Content(
+        val fee: Fee?,
+        val rate: BigDecimal?,
+        val isFeeConvertibleToFiat: Boolean,
+        val appCurrency: AppCurrency,
+        val isFeeApproximate: Boolean,
+    ) : FeeState()
+
+    data object Loading : FeeState()
+
+    data object Error : FeeState()
+}
