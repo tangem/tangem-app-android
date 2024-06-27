6da964c04cf8254beb3470bb9530649f265e6c78
Tangem <git@tangem.com>
2024-06-27 11:30:53 +0300

    Updated on 2026-03-25

--- features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/viewmodel/SendViewModel.kt
+++ features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/viewmodel/SendViewModel.kt
@@ -870,7 +870,7 @@ internal class SendViewModel @Inject constructor(
         }
     }
 
-    private suspend fun sendTransaction(txData: TransactionData) {
+    private suspend fun sendTransaction(txData: TransactionData.Uncompiled) {
         sendTransactionUseCase(
             txData = txData,
             userWallet = userWallet,
@@ -911,7 +911,7 @@ internal class SendViewModel @Inject constructor(
         }
     }
 
-    private suspend fun updateTransactionStatus(txData: TransactionData) {
+    private suspend fun updateTransactionStatus(txData: TransactionData.Uncompiled) {
         val txUrl = getExplorerTransactionUrlUseCase(
             userWalletId = userWalletId,
             network = cryptoCurrency.network,
