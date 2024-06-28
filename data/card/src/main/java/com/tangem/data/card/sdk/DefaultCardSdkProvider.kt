e1eebb71a16835231bb41db90c9919b7ca2ee827
Anton Tkachev <iiiburnyiii@gmail.com>
2024-06-28 20:31:58 +0400

    AND-7484 Fixed user wallets deletion

--- data/card/src/main/java/com/tangem/data/card/sdk/DefaultCardSdkProvider.kt
+++ data/card/src/main/java/com/tangem/data/card/sdk/DefaultCardSdkProvider.kt
@@ -40,7 +40,7 @@ internal class DefaultCardSdkProvider @Inject constructor() : CardSdkProvider, C
             linkedTerminal = true,
             allowUntrustedCards = true,
             filter = CardFilter(
-                allowedCardTypes = FirmwareVersion.FirmwareType.values().toList(),
+                allowedCardTypes = FirmwareVersion.FirmwareType.entries.toList(),
                 maxFirmwareVersion = FirmwareVersion(major = 6, minor = 33),
                 batchIdFilter = CardFilter.Companion.ItemFilter.Deny(
                     items = setOf("0027", "0030", "0031", "0035"),
