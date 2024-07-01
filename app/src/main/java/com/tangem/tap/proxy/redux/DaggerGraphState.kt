330f3f0a84709a8abbe0cc03c4b006b15be65419
Tangem <git@tangem.com>
2024-07-01 18:46:08 +0500

    Updated on 2025-11-26

--- app/src/main/java/com/tangem/tap/proxy/redux/DaggerGraphState.kt
+++ app/src/main/java/com/tangem/tap/proxy/redux/DaggerGraphState.kt
@@ -30,7 +30,6 @@ import com.tangem.domain.wallets.repository.WalletsRepository
 import com.tangem.domain.wallets.usecase.GenerateWalletNameUseCase
 import com.tangem.feature.qrscanning.QrScanningRouter
 import com.tangem.features.details.DetailsFeatureToggles
-import com.tangem.features.disclaimer.api.DisclaimerRouter
 import com.tangem.features.pushnotifications.api.featuretoggles.PushNotificationsFeatureToggles
 import com.tangem.features.pushnotifications.api.navigation.PushNotificationsRouter
 import com.tangem.features.send.api.featuretoggles.SendFeatureToggles
@@ -88,7 +87,6 @@ data class DaggerGraphState(
     val urlOpener: UrlOpener? = null,
     val shareManager: ShareManager? = null,
     val appRouter: AppRouter? = null,
-    val disclaimerRouter: DisclaimerRouter? = null,
     val pushNotificationsFeatureToggles: PushNotificationsFeatureToggles? = null,
     val pushNotificationsRouter: PushNotificationsRouter? = null,
 ) : StateType
