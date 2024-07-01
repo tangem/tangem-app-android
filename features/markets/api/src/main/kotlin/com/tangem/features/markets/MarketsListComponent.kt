6e90a970e86b32f16ceabdf8a6d99bd3e2b08797
Tangem <git@tangem.com>
2024-07-01 14:07:11 +0300

    Updated on 2026-06-24

---
+++ features/markets/api/src/main/kotlin/com/tangem/features/markets/MarketsListComponent.kt
@@ -0,0 +1,12 @@
+package com.tangem.features.markets
+
+import androidx.compose.runtime.Stable
+
+@Stable
+interface MarketsListComponent {
+
+    // TODO
+
+    // @Composable
+    // fun BottomSheetContent(state: State<BottomSheetState>, onHeaderSizeChange: (Dp) -> Unit, modifier: Modifier)
+}
