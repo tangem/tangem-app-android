f01163ed7bc993721667900435ea9647ef9af3da
Tangem <git@tangem.com>
2024-07-01 13:21:26 +0500

    Updated on 2026-08-19

--- domain/staking/models/src/main/kotlin/com/tangem/domain/staking/model/YieldBalanceList.kt
+++ domain/staking/models/src/main/kotlin/com/tangem/domain/staking/model/YieldBalanceList.kt
@@ -5,13 +5,15 @@ sealed class YieldBalanceList {
     data class Data(
         val balances: List<YieldBalance>,
     ) : YieldBalanceList() {
-        fun getBalance(rawCurrencyId: String?): YieldBalance? {
+        fun getBalance(rawCurrencyId: String?): YieldBalance {
             return balances.firstOrNull { yield ->
                 (yield as? YieldBalance.Data)?.balance?.items
                     ?.any { it.rawCurrencyId == rawCurrencyId } == true
-            }
+            } ?: YieldBalance.Error
         }
     }
 
     data object Empty : YieldBalanceList()
+
+    data object Error : YieldBalanceList()
 }
