72f39e66fca995eee9687fc508075ee94634dff1
Tangem <git@tangem.com>
2024-07-03 14:33:31 +0300

    release/5.26.1

--- core/pagination/src/main/java/com/tangem/pagination/BatchListState.kt
+++ core/pagination/src/main/java/com/tangem/pagination/BatchListState.kt
@@ -3,16 +3,16 @@ package com.tangem.pagination
 /**
  * State that is used for listening the current state of a pagination.
  *
- * @param K type of the key of the batch.
- * @param T type of the data.
- * @param E type of the error.
+ * @param TKey type of the key of the batch.
+ * @param TData type of the data.
+ * @param TError type of the error.
  *
  * @property data list of loaded batches.
  * @property status current status of the pagination.
  *
  * @see BatchListSource
  */
-data class BatchListState<K, T, E>(
-    val data: List<Batch<K, T>>,
-    val status: PaginationStatus<T, E>,
+data class BatchListState<TKey, TData, TError>(
+    val data: List<Batch<TKey, TData>>,
+    val status: PaginationStatus<TData, TError>,
 )
