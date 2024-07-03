faa12d4860a32b78c8d61bb4e62a299c6e72d7f6
Tangem <git@tangem.com>
2024-07-03 14:33:31 +0300

    Updated on 2025-09-25

--- core/pagination/src/main/java/com/tangem/pagination/BatchingContext.kt
+++ core/pagination/src/main/java/com/tangem/pagination/BatchingContext.kt
@@ -6,9 +6,9 @@ import kotlinx.coroutines.flow.Flow
 /**
  * Context for working with [BatchListSource].
  *
- * @param R type of the request.
- * @param K type of the key.
- * @param U type of the update request.
+ * @param TRequest type of the request.
+ * @param TKey type of the key.
+ * @param TUpdate type of the update request.
  *
  * @property actionsFlow flow of [BatchAction]s that would be dispatched to [BatchListSource].
  * @property coroutineScope scope for the [BatchListSource] to launch coroutines. When it is cancelled,
@@ -16,7 +16,7 @@ import kotlinx.coroutines.flow.Flow
  *
  * @see BatchListSource
  */
-class BatchingContext<R, K, U>(
-    val actionsFlow: Flow<BatchAction<R, K, U>>,
+class BatchingContext<TRequest, TKey, TUpdate>(
+    val actionsFlow: Flow<BatchAction<TRequest, TKey, TUpdate>>,
     val coroutineScope: CoroutineScope,
 )
