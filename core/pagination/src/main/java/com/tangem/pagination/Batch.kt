ca1dff10ca83f2d72d5f9daf1ca122d7b5150598
Tangem <git@tangem.com>
2024-07-03 14:33:31 +0300

    Updated on 2026-02-11

--- core/pagination/src/main/java/com/tangem/pagination/Batch.kt
+++ core/pagination/src/main/java/com/tangem/pagination/Batch.kt
@@ -4,10 +4,10 @@ package com.tangem.pagination
  * Represents a batch of data with a key.
  * Used in [BatchListState].
  *
- * @param K type of the key.
- * @param T type of the data.
+ * @param TKey type of the key.
+ * @param TData type of the data.
  */
-data class Batch<K, T>(
-    val key: K,
-    val data: T,
+data class Batch<TKey, TData>(
+    val key: TKey,
+    val data: TData,
 )
