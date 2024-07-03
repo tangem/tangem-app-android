b9108a4a754756015c3a5c66f6ee7b17125e2e34
Mama1emon <andrew.khokhlove@gmail.com>
2024-07-03 21:03:27 +0800

    AND-7512 Shimmer loading is displayed if scan the card without tokens
    Signed-off-by: Mama1emon <andrew.khokhlove@gmail.com>

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/subscribers/BasicTokenListSubscriber.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/subscribers/BasicTokenListSubscriber.kt
@@ -61,6 +61,11 @@ internal abstract class BasicTokenListSubscriber(
                 },
             flow2 = getSelectedAppCurrencyUseCase().distinctUntilChanged(),
             transform = { maybeTokenList, maybeAppCurrency ->
+                val appCurrency = maybeAppCurrency.getOrElse { e ->
+                    Timber.e("Failed to load app currency: $e")
+                    AppCurrency.Default
+                }
+
                 val tokenList = maybeTokenList.getOrElse(
                     ifLoading = { maybeContent ->
                         val isRefreshing = stateHolder.getWalletState(userWallet.walletId)
@@ -75,16 +80,15 @@ internal abstract class BasicTokenListSubscriber(
                     ifError = { e ->
                         Timber.e("Failed to load token list: $e")
                         stateHolder.update(
-                            SetTokenListErrorTransformer(userWalletId = userWallet.walletId, error = e),
+                            SetTokenListErrorTransformer(
+                                selectedWallet = userWallet,
+                                error = e,
+                                appCurrency = appCurrency,
+                            ),
                         )
                         return@combine
                     },
                 )
-
-                val appCurrency = maybeAppCurrency.getOrElse { e ->
-                    Timber.e("Failed to load app currency: $e")
-                    AppCurrency.Default
-                }
 
                 updateContent(tokenList, appCurrency)
                 walletWithFundsChecker.check(tokenList)
