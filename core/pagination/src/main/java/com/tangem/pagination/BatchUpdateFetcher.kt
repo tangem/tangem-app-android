ccbf45b23e8ea6b1e75c2b951aa02f3ca5f41f0a
Tangem <git@tangem.com>
2024-07-04 14:39:28 +0300

    Updated on 2026-09-25

--- core/pagination/src/main/java/com/tangem/pagination/BatchUpdateFetcher.kt
+++ core/pagination/src/main/java/com/tangem/pagination/BatchUpdateFetcher.kt
@@ -6,20 +6,18 @@ package com.tangem.pagination
  *
  * @param TKey type of the key.
  * @param TData type of the data.
- * @param TError type of the error.
  * @param TUpdate type of the update request.
  */
-interface BatchUpdateFetcher<TKey, TData, TError, TUpdate> {
+fun interface BatchUpdateFetcher<TKey, TData, TUpdate> {
 
     /**
      * Fetches updates for a batch of data.
+     * Note that the result batch key as a result of executing the method must be presented in the [toUpdate] list,
+     * otherwise, updates will not be performed
      *
      * @param toUpdate list of batches to update.
      * @param updateRequest request to update the data.
      * @return result of the update operation.
      */
-    suspend fun fetchUpdate(
-        toUpdate: List<Batch<TKey, TData>>,
-        updateRequest: TUpdate,
-    ): FetchUpdateResult<TKey, TData, TError>
+    suspend fun fetchUpdate(toUpdate: List<Batch<TKey, TData>>, updateRequest: TUpdate): BatchUpdateResult<TKey, TData>
 }
