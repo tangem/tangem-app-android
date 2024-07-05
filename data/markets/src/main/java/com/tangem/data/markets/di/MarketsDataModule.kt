7f0ffda6d4ed511b63a84f043468c48f25d06084
Nikita Samoylov <nikit.samoylov@gmail.com>
2024-07-05 13:48:38 +0300

    AND-7448 [Markets] Markets Token List bottom sheet component (Data-Layer)

---
+++ data/markets/src/main/java/com/tangem/data/markets/di/MarketsDataModule.kt
@@ -0,0 +1,31 @@
+package com.tangem.data.markets.di
+
+import com.tangem.data.markets.DefaultMarketsTokenRepository
+import com.tangem.datasource.api.markets.TangemTechMarketsApi
+import com.tangem.datasource.api.tangemTech.TangemTechApi
+import com.tangem.domain.markets.repositories.MarketsTokenRepository
+import com.tangem.utils.coroutines.CoroutineDispatcherProvider
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object MarketsDataModule {
+
+    @Provides
+    @Singleton
+    fun provideMarketsRepository(
+        marketsApi: TangemTechMarketsApi,
+        tangemTechApi: TangemTechApi,
+        dispatchers: CoroutineDispatcherProvider,
+    ): MarketsTokenRepository {
+        return DefaultMarketsTokenRepository(
+            marketsApi = marketsApi,
+            tangemTechApi = tangemTechApi,
+            dispatcherProvider = dispatchers,
+        )
+    }
+}
