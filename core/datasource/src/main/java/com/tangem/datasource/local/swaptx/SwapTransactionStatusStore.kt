4b2dd55573dc3e78c890c76c1abfa20bb332429d
Vlad Kozarez <vladks32@gmail.com>
2024-07-09 14:39:17 +0100

    AND-7566 fixed status model required fields

--- core/datasource/src/main/java/com/tangem/datasource/local/swaptx/SwapTransactionStatusStore.kt
+++ core/datasource/src/main/java/com/tangem/datasource/local/swaptx/SwapTransactionStatusStore.kt
@@ -10,9 +10,12 @@ interface SwapTransactionStatusStore {
 }
 
 enum class ExchangeAnalyticsStatus(val value: String) {
+    WaitingTxHash("Waiting tx hash"),
     InProgress("In Progress"),
     Done("Done"),
     Fail("Fail"),
+    FailTx("Fail tx"),
+    Unknown("Unknown"),
     KYC("KYC"),
     Refunded("Refunded"),
     Cancelled("Canceled"),
