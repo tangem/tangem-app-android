895a09dc617a23f54b69a4c0e4eb1ba69dae3ad4
Tangem <git@tangem.com>
2024-07-09 15:05:17 +0100

    Updated on 2026-02-25

--- app/src/main/java/com/tangem/tap/domain/walletconnect2/data/DefaultLegacyWalletConnectRepository.kt
+++ app/src/main/java/com/tangem/tap/domain/walletconnect2/data/DefaultLegacyWalletConnectRepository.kt
@@ -494,6 +494,6 @@ internal class DefaultLegacyWalletConnectRepository(
 
     private companion object {
 
-        val unsupportedDApps = listOf("dYdX", "dYdX v4", "Apex Pro")
+        val unsupportedDApps = listOf("dYdX", "dYdX v4", "Apex Pro", "The Sandbox")
     }
 }
