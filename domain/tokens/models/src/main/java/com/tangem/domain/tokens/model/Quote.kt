b58d72f39678198de3f0de8f2156797e2b6544bd
Tangem <git@tangem.com>
2024-07-09 12:45:17 +0300

    Updated on 2026-05-07

--- domain/tokens/models/src/main/java/com/tangem/domain/tokens/model/Quote.kt
+++ domain/tokens/models/src/main/java/com/tangem/domain/tokens/model/Quote.kt
@@ -8,12 +8,9 @@ import java.math.BigDecimal
  * @property rawCurrencyId The unique identifier of the cryptocurrency for which the financial information is provided.
  * @property fiatRate The current fiat exchange rate for the cryptocurrency.
  * @property priceChange The price change for the cryptocurrency.
- * @property values The values representing the cryptocurrency's price changes over a 24-hour period,
- * suitable for chart plotting.
  */
 data class Quote(
     val rawCurrencyId: String,
     val fiatRate: BigDecimal,
     val priceChange: BigDecimal,
-    val values: List<Double>? = null,
 )
