482688a75180aa40103ceda45408c05528528c74
Tangem <git@tangem.com>
2024-07-10 10:59:08 +0200

    Updated on 2026-09-23

--- data/promo/src/main/java/com/tangem/data/promo/DefaultPromoRepository.kt
+++ data/promo/src/main/java/com/tangem/data/promo/DefaultPromoRepository.kt
@@ -33,17 +33,19 @@ internal class DefaultPromoRepository(
     }
 
     override suspend fun getOkxPromoBanner(): PromoBanner? {
-        return runCatching(dispatchers.io) {
-            promoResponseConverter.convert(
-                tangemApi.getPromotionInfo(OKX)
-                    .getOrThrow(),
-            )
-        }.getOrNull()
+        // TODO disabled for 5.12, enable for 5.12.1
+        return null
+        // return runCatching(dispatchers.io) {
+        //     promoResponseConverter.convert(
+        //         tangemApi.getPromotionInfo(OKX)
+        //             .getOrThrow(),
+        //     )
+        // }.getOrNull()
     }
 
     private companion object {
         private const val CHANGELLY_NAME = "changelly"
         private const val TRAVALA = "travala"
-        private const val OKX = "okx"
+        // private const val OKX = "okx"
     }
 }
