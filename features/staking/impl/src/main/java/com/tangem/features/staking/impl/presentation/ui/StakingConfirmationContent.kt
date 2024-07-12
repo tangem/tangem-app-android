d31085d30bb24100d9c1eaa47f33416b6450c418
Tangem <git@tangem.com>
2024-07-12 15:46:47 +0300

    Updated on 2026-06-27

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/ui/StakingConfirmationContent.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/ui/StakingConfirmationContent.kt
@@ -1,6 +1,7 @@
 package com.tangem.features.staking.impl.presentation.ui
 
 import android.content.res.Configuration
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
@@ -14,23 +15,26 @@ import com.tangem.common.ui.amountScreen.models.AmountState
 import com.tangem.common.ui.amountScreen.preview.AmountStatePreviewData
 import com.tangem.common.ui.amountScreen.ui.AmountBlock
 import com.tangem.core.ui.components.SpacerHMax
+import com.tangem.core.ui.components.transactions.TransactionDoneTitle
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.core.ui.res.TangemThemePreview
 import com.tangem.features.staking.impl.presentation.state.StakingStates
-import com.tangem.features.staking.impl.presentation.state.previewdata.ConfirmStakingStatePreviewData
+import com.tangem.features.staking.impl.presentation.state.previewdata.ConfirmationStatePreviewData
 import com.tangem.features.staking.impl.presentation.state.stub.StakingClickIntentsStub
 import com.tangem.features.staking.impl.presentation.ui.block.NotificationsBlock
 import com.tangem.features.staking.impl.presentation.ui.block.StakingFeeBlock
 import com.tangem.features.staking.impl.presentation.ui.block.ValidatorBlock
 import com.tangem.features.staking.impl.presentation.viewmodel.StakingClickIntents
+import com.tangem.features.staking.impl.R
+import com.tangem.features.staking.impl.presentation.state.TransactionDoneState
 
 @Composable
-internal fun StakingConfirmContent(
+internal fun StakingConfirmationContent(
     amountState: AmountState,
-    state: StakingStates.ConfirmStakingState,
+    state: StakingStates.ConfirmationState,
     clickIntents: StakingClickIntents,
 ) {
-    if (state !is StakingStates.ConfirmStakingState.Data) return
+    if (state !is StakingStates.ConfirmationState.Data) return
 
     Column(
         modifier = Modifier
@@ -40,6 +44,16 @@ internal fun StakingConfirmContent(
             .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.spacedBy(TangemTheme.dimens.spacing16),
     ) {
+        AnimatedVisibility(
+            visible = state.transactionDoneState is TransactionDoneState.Content,
+            modifier = Modifier.padding(vertical = TangemTheme.dimens.spacing12),
+        ) {
+            val transactionDoneStateContent = state.transactionDoneState as TransactionDoneState.Content
+            TransactionDoneTitle(
+                titleRes = R.string.sent_transaction_sent_title,
+                date = transactionDoneStateContent.timestamp,
+            )
+        }
         AmountBlock(
             amountState = amountState,
             isClickDisabled = true,
@@ -68,12 +82,12 @@ private fun FooterText(text: String) {
 @Preview(widthDp = 360, showBackground = true)
 @Preview(widthDp = 360, showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
-private fun Preview_StakingConfirmContent() {
+private fun Preview_StakingConfirmationContent() {
     TangemThemePreview {
         Column(Modifier.background(TangemTheme.colors.background.primary)) {
-            StakingConfirmContent(
+            StakingConfirmationContent(
                 amountState = AmountStatePreviewData.amountState,
-                state = ConfirmStakingStatePreviewData.confirmStakingState,
+                state = ConfirmationStatePreviewData.assentStakingState,
                 clickIntents = StakingClickIntentsStub,
             )
         }
