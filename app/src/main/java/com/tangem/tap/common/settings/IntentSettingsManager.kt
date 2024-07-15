37ebfcfbad0cac3c5a01b3b801571f438bf7e881
Tangem <git@tangem.com>
2024-07-15 20:08:03 +0500

    Updated on 2025-09-25

--- app/src/main/java/com/tangem/tap/common/settings/IntentSettingsManager.kt
+++ app/src/main/java/com/tangem/tap/common/settings/IntentSettingsManager.kt
@@ -2,6 +2,7 @@ package com.tangem.tap.common.settings
 
 import android.content.Context
 import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.net.Uri
 import android.provider.Settings
 import com.tangem.core.navigation.settings.SettingsManager
@@ -11,7 +12,9 @@ internal class IntentSettingsManager(val context: Context) : SettingsManager {
         val openSettingsIntent = Intent(
             Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
             Uri.fromParts("package", context.packageName, null),
-        )
+        ).apply {
+            flags = FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
         context.startActivity(openSettingsIntent)
     }
 }
