e4705ed7ca300c3c7c548eef4694353c0e13d0e2
Tangem <git@tangem.com>
2024-07-15 20:25:38 +0300

    Updated on 2026-06-05

--- features/referral/presentation/src/main/java/com/tangem/feature/referral/ui/AgreementBottomSheetContent.kt
+++ features/referral/presentation/src/main/java/com/tangem/feature/referral/ui/AgreementBottomSheetContent.kt
@@ -2,15 +2,19 @@ package com.tangem.feature.referral.ui
 
 import android.content.res.Configuration
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.google.accompanist.web.WebView
 import com.google.accompanist.web.rememberWebViewState
 import com.tangem.core.ui.components.appbar.TangemTopAppBar
@@ -22,11 +26,10 @@ import com.tangem.feature.referral.presentation.R
  * Bottom sheet content with referral program terms of use
  *
  * @param url link to the html page
+ * @param bottomBarHeight bottom insets
  */
 @Composable
-internal fun AgreementBottomSheetContent(url: String) {
-    val bottomBarHeight = with(LocalDensity.current) { WindowInsets.systemBars.getBottom(this).toDp() }
-
+internal fun AgreementBottomSheetContent(url: String, bottomBarHeight: Dp) {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -62,6 +65,6 @@ private fun AgreementHtmlView(url: String) {
 @Composable
 private fun Preview_AgreementBottomSheet() {
     TangemThemePreview {
-        AgreementBottomSheetContent(url = "https://tangem.com/en/")
+        AgreementBottomSheetContent(url = "https://tangem.com/en/", 50.dp)
     }
 }
