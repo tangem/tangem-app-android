4078dead1704cbdd08d07f0d403fc3b77196ff9e
Tangem <git@tangem.com>
2024-07-16 11:07:58 +0300

    Updated on 2026-03-27

--- core/pagination/src/main/java/com/tangem/pagination/BatchUpdateFetcher.kt
+++ core/pagination/src/main/java/com/tangem/pagination/BatchUpdateFetcher.kt
@@ -8,7 +8,7 @@ package com.tangem.pagination
  * @param TData type of the data.
  * @param TUpdate type of the update request.
  */
-fun interface BatchUpdateFetcher<TKey, TData, TUpdate> {
+interface BatchUpdateFetcher<TKey, TData, TUpdate> {
 
     /**
      * Fetches updates for a batch of data.
@@ -19,5 +19,45 @@ fun interface BatchUpdateFetcher<TKey, TData, TUpdate> {
      * @param updateRequest request to update the data.
      * @return result of the update operation.
      */
-    suspend fun fetchUpdate(toUpdate: List<Batch<TKey, TData>>, updateRequest: TUpdate): BatchUpdateResult<TKey, TData>
+    suspend fun fetchUpdate(
+        toUpdate: List<Batch<TKey, TData>>,
+        updateRequest: TUpdate,
+    ): BatchUpdateResult<TKey, TData> = BatchUpdateResult.Error(NotImplementedError())
+
+    /**
+     * Fetches updates for a batch of data asynchronously.
+     * To update the data, use the [UpdateContext.update] method.
+     * [UpdateContext.update] could be called as many times as you want.
+     *
+     * Note that the result batch key as a result of executing the method must be presented in the [toUpdate] list,
+     * otherwise, updates will not be performed.
+     *
+     * @param toUpdate list of batches to update. **Attention** Data may be outdated and should be used only to make
+     * a request for an update, not for the actual update operation. For the actual update operation, use the batches
+     * provided by [UpdateContext.update].
+     * @param updateRequest request to update the data.
+     */
+    suspend fun UpdateContext<TKey, TData>.fetchUpdateAsync(
+        toUpdate: List<Batch<TKey, TData>>,
+        updateRequest: TUpdate,
+    ) {
+    }
+
+    /**
+     * Context for updating the data.
+     * Used by [BatchListSource] to provide a way to update batches by [fetchUpdateAsync] method.
+     */
+    interface UpdateContext<TKey, TData> {
+
+        /**
+         * Updates the data of the batch.
+         * Could be called as many times as you want.
+         *
+         * Input batches keys and the data could not always be the same as the keys of the [toUpdate] list in
+         * [fetchUpdateAsync] method, but the provided set of keys will always be a subset of the [toUpdate] list.
+         *
+         * @param update lambda to update the data.
+         */
+        suspend fun update(update: List<Batch<TKey, TData>>.() -> BatchUpdateResult<TKey, TData>)
+    }
 }
