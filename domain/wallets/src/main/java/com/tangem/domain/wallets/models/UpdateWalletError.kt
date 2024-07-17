885dc15b28af1eaed50a867037309c9d12ee0148
Tangem <git@tangem.com>
2024-07-17 17:53:37 +0400

    Updated on 2025-10-01

--- domain/wallets/src/main/java/com/tangem/domain/wallets/models/UpdateWalletError.kt
+++ domain/wallets/src/main/java/com/tangem/domain/wallets/models/UpdateWalletError.kt
@@ -2,7 +2,7 @@ package com.tangem.domain.wallets.models
 
 sealed interface UpdateWalletError {
 
-    data object DataError : UpdateWalletError
-
     data object NameAlreadyExists : UpdateWalletError
+
+    data class DataError(val cause: Throwable) : UpdateWalletError
 }
