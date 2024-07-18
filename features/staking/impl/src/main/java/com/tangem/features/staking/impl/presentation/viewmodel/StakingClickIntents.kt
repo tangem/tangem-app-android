c4e77fa7f12240c1377b234c006b2de568bce589
Tangem <git@tangem.com>
2024-07-18 21:22:11 +0500

    Updated on 2026-05-04

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/viewmodel/StakingClickIntents.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/viewmodel/StakingClickIntents.kt
@@ -1,15 +1,18 @@
 package com.tangem.features.staking.impl.presentation.viewmodel
 
 import com.tangem.common.ui.amountScreen.AmountScreenClickIntents
+import com.tangem.domain.staking.model.stakekit.PendingAction
 import com.tangem.domain.staking.model.stakekit.Yield
 import com.tangem.features.staking.impl.presentation.state.BalanceState
 import com.tangem.features.staking.impl.presentation.state.transformers.InfoType
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
 
 internal interface StakingClickIntents : AmountScreenClickIntents {
 
     fun onBackClick()
 
-    fun onNextClick()
+    fun onNextClick(pendingActions: ImmutableList<PendingAction> = persistentListOf())
 
     fun onPrevClick()
 
