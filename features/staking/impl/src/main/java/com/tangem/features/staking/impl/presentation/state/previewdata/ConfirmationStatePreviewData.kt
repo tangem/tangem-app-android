9afcd4728c714208a68380a7fe4fceb03f763a66
Tangem <git@tangem.com>
2024-07-18 21:22:11 +0500

    Updated on 2025-12-22

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/previewdata/ConfirmationStatePreviewData.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/previewdata/ConfirmationStatePreviewData.kt
@@ -85,5 +85,6 @@ internal object ConfirmationStatePreviewData {
             ),
         ),
         transactionDoneState = TransactionDoneState.Empty,
+        pendingActions = persistentListOf(),
     )
 }
