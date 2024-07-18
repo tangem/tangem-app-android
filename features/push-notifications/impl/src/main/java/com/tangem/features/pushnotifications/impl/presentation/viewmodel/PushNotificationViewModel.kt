6c7c71b5ab910785d04fe279819e19efbc182f88
Tangem <git@tangem.com>
2024-07-18 20:58:30 +0500

    Updated on 2026-09-25

--- features/push-notifications/impl/src/main/java/com/tangem/features/pushnotifications/impl/presentation/viewmodel/PushNotificationViewModel.kt
+++ features/push-notifications/impl/src/main/java/com/tangem/features/pushnotifications/impl/presentation/viewmodel/PushNotificationViewModel.kt
@@ -2,11 +2,14 @@ package com.tangem.features.pushnotifications.impl.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.tangem.core.analytics.api.AnalyticsEventHandler
+import com.tangem.core.analytics.models.AnalyticsParam
 import com.tangem.core.navigation.settings.SettingsManager
 import com.tangem.domain.settings.DelayPermissionRequestUseCase
 import com.tangem.domain.settings.NeverRequestPermissionUseCase
 import com.tangem.domain.settings.NeverToInitiallyAskPermissionUseCase
 import com.tangem.domain.settings.SetFirstTimeAskingPermissionUseCase
+import com.tangem.features.pushnotifications.api.analytics.PushNotificationAnalyticEvents
 import com.tangem.features.pushnotifications.api.utils.PUSH_PERMISSION
 import com.tangem.features.pushnotifications.impl.navigation.DefaultPushNotificationsRouter
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -22,9 +25,13 @@ internal class PushNotificationViewModel @Inject constructor(
     private val neverToInitiallyAskPermissionUseCase: NeverToInitiallyAskPermissionUseCase,
     private val router: DefaultPushNotificationsRouter,
     private val settingsManager: SettingsManager,
+    private val analyticHandler: AnalyticsEventHandler,
 ) : ViewModel(), PushNotificationsClickIntents {
 
     override fun onAskLater() {
+        analyticHandler.send(
+            PushNotificationAnalyticEvents.ButtonLater(AnalyticsParam.ScreensSources.Stories),
+        )
         viewModelScope.launch {
             delayPermissionRequestUseCase(PUSH_PERMISSION)
             setFirstTimeAskingPermissionUseCase(PUSH_PERMISSION)
@@ -40,6 +47,9 @@ internal class PushNotificationViewModel @Inject constructor(
     }
 
     override fun onAllowedPermission() {
+        analyticHandler.send(
+            PushNotificationAnalyticEvents.ButtonAllow(AnalyticsParam.ScreensSources.Stories),
+        )
         viewModelScope.launch {
             neverRequestPermissionUseCase(PUSH_PERMISSION)
             neverToInitiallyAskPermissionUseCase(PUSH_PERMISSION)
