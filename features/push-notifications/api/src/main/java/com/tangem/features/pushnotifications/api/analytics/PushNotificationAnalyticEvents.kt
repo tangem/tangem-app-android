1387a195fc9e3348e0509800a271823d62e1013d
Tangem <git@tangem.com>
2024-07-18 20:58:30 +0500

    Updated on 2026-06-24

---
+++ features/push-notifications/api/src/main/java/com/tangem/features/pushnotifications/api/analytics/PushNotificationAnalyticEvents.kt
@@ -0,0 +1,32 @@
+package com.tangem.features.pushnotifications.api.analytics
+
+import com.tangem.core.analytics.models.AnalyticsEvent
+import com.tangem.core.analytics.models.AnalyticsParam
+
+sealed class PushNotificationAnalyticEvents(
+    event: String,
+    params: Map<String, String> = mapOf(),
+) : AnalyticsEvent(category = "Push", event = event, params = params) {
+
+    data class ButtonAllow(
+        val source: AnalyticsParam.ScreensSources,
+    ) : PushNotificationAnalyticEvents(
+        event = "Button - Allow",
+        params = mapOf(
+            AnalyticsParam.SOURCE to source.value,
+        ),
+    )
+
+    data class ButtonLater(
+        val source: AnalyticsParam.ScreensSources,
+    ) : PushNotificationAnalyticEvents(
+        event = "Button - Later",
+        params = mapOf(
+            AnalyticsParam.SOURCE to source.value,
+        ),
+    )
+
+    data object ButtonCancel : PushNotificationAnalyticEvents(
+        event = "Button - Cancel",
+    )
+}
