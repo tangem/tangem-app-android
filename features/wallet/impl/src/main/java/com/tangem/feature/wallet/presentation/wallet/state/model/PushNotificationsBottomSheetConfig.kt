ee3653f2954297389411a518307f5bc164b7b27f
Tangem <git@tangem.com>
2024-07-18 20:52:46 +0500

    Updated on 2026-09-21

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/model/PushNotificationsBottomSheetConfig.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/model/PushNotificationsBottomSheetConfig.kt
@@ -3,7 +3,8 @@ package com.tangem.feature.wallet.presentation.wallet.state.model
 import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfigContent
 
 data class PushNotificationsBottomSheetConfig(
-    val isFirstTimeAsking: Boolean,
+    val isFirstTimeRequested: Boolean,
+    val wasInitiallyAsk: Boolean,
     val onRequest: () -> Unit,
     val onAllow: () -> Unit,
     val onDeny: () -> Unit,
