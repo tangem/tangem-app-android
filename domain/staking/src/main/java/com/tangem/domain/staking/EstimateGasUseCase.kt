d5621f126ed1347ddbadab12844b91ad03fae68f
Tangem <git@tangem.com>
2024-07-19 12:10:24 +0300

    Updated on 2026-04-06

--- domain/staking/src/main/java/com/tangem/domain/staking/EstimateGasUseCase.kt
+++ domain/staking/src/main/java/com/tangem/domain/staking/EstimateGasUseCase.kt
@@ -1,9 +1,9 @@
 package com.tangem.domain.staking
 
 import arrow.core.Either
-import com.tangem.domain.staking.model.StakingError
-import com.tangem.domain.staking.model.transaction.ActionParams
-import com.tangem.domain.staking.model.transaction.StakingGasEstimate
+import com.tangem.domain.staking.model.stakekit.StakingError
+import com.tangem.domain.staking.model.stakekit.transaction.ActionParams
+import com.tangem.domain.staking.model.stakekit.transaction.StakingGasEstimate
 import com.tangem.domain.staking.repositories.StakingErrorResolver
 import com.tangem.domain.staking.repositories.StakingRepository
 
