998b469f1c65625f816897edc03da02637cec507
Tangem <git@tangem.com>
2024-07-23 14:43:09 +0300

    Updated on 2025-10-01

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/previewdata/InitialStakingStatePreview.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/previewdata/InitialStakingStatePreview.kt
@@ -1,21 +1,59 @@
 package com.tangem.features.staking.impl.presentation.state.previewdata
 
+import com.tangem.core.ui.components.list.RoundedListWithDividersItemData
+import com.tangem.core.ui.extensions.TextReference
 import com.tangem.core.ui.extensions.stringReference
 import com.tangem.domain.staking.model.stakekit.Yield
+import com.tangem.features.staking.impl.R
 import com.tangem.features.staking.impl.presentation.state.*
 import kotlinx.collections.immutable.persistentListOf
 
 internal object InitialStakingStatePreview {
     val defaultState = StakingStates.InitialInfoState.Data(
         isPrimaryButtonEnabled = true,
-        available = "15 SOL",
-        onStake = "0 SOL",
         aprRange = stringReference("2.54-5.12%"),
-        unbondingPeriod = "3d",
-        minimumRequirement = "12 SOL",
-        rewardClaiming = "Auto",
-        warmupPeriod = "Days",
-        rewardSchedule = "Block",
+        infoItems = persistentListOf(
+            RoundedListWithDividersItemData(
+                id = R.string.staking_details_available,
+                startText = TextReference.Res(R.string.staking_details_available),
+                endText = TextReference.Str("15 SOL"),
+            ),
+            RoundedListWithDividersItemData(
+                id = R.string.staking_details_apy,
+                startText = TextReference.Res(R.string.staking_details_apy),
+                endText = TextReference.Str("2.54-5.12%"),
+            ),
+            RoundedListWithDividersItemData(
+                id = R.string.staking_details_on_stake,
+                startText = TextReference.Res(R.string.staking_details_on_stake),
+                endText = TextReference.Str("0 SOL"),
+            ),
+            RoundedListWithDividersItemData(
+                id = R.string.staking_details_unbonding_period,
+                startText = TextReference.Res(R.string.staking_details_unbonding_period),
+                endText = TextReference.Str("3d"),
+            ),
+            RoundedListWithDividersItemData(
+                id = R.string.staking_details_minimum_requirement,
+                startText = TextReference.Res(R.string.staking_details_minimum_requirement),
+                endText = TextReference.Str("12 SOL"),
+            ),
+            RoundedListWithDividersItemData(
+                id = R.string.staking_details_reward_claiming,
+                startText = TextReference.Res(R.string.staking_details_reward_claiming),
+                endText = TextReference.Str("Auto"),
+            ),
+            RoundedListWithDividersItemData(
+                id = R.string.staking_details_warmup_period,
+                startText = TextReference.Res(R.string.staking_details_warmup_period),
+                endText = TextReference.Str("Days"),
+            ),
+            RoundedListWithDividersItemData(
+                id = R.string.staking_details_reward_schedule,
+                startText = TextReference.Res(R.string.staking_details_reward_schedule),
+                endText = TextReference.Str("Block"),
+            ),
+        ),
         onInfoClick = {},
         yieldBalance = InnerYieldBalanceState.Empty,
         isStakeMoreAvailable = true,
