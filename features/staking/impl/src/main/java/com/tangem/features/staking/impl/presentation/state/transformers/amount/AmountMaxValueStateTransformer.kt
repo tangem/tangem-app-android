76c385523edccb1ac4d96536c7979d1275e78982
Tangem <git@tangem.com>
2024-07-23 18:55:55 +0500

    Updated on 2026-04-08

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/transformers/amount/AmountMaxValueStateTransformer.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/transformers/amount/AmountMaxValueStateTransformer.kt
@@ -1,16 +1,32 @@
 package com.tangem.features.staking.impl.presentation.state.transformers.amount
 
 import com.tangem.common.ui.amountScreen.converters.field.AmountFieldMaxAmountTransformer
+import com.tangem.core.ui.utils.parseBigDecimal
+import com.tangem.domain.staking.model.stakekit.Yield
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.features.staking.impl.presentation.state.StakingUiState
 import com.tangem.utils.transformer.Transformer
 
 internal class AmountMaxValueStateTransformer(
     private val cryptoCurrencyStatus: CryptoCurrencyStatus,
+    private val yield: Yield,
 ) : Transformer<StakingUiState> {
+
+    private val amountRequirementStateTransformer by lazy(LazyThreadSafetyMode.NONE) {
+        val value = cryptoCurrencyStatus.value.amount
+            ?.parseBigDecimal(cryptoCurrencyStatus.currency.decimals)
+            .orEmpty()
+        AmountRequirementStateTransformer(
+            cryptoCurrencyStatus,
+            yield,
+            value,
+        )
+    }
+
     override fun transform(prevState: StakingUiState): StakingUiState {
+        val updatedAmountState = AmountFieldMaxAmountTransformer(cryptoCurrencyStatus).transform(prevState.amountState)
         return prevState.copy(
-            amountState = AmountFieldMaxAmountTransformer(cryptoCurrencyStatus).transform(prevState.amountState),
+            amountState = amountRequirementStateTransformer.transform(updatedAmountState),
         )
     }
 }
