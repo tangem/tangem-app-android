ddf93b3ffd83774ef3289977e77f4aff986de666
Tangem <git@tangem.com>
2024-07-26 12:45:54 +0300

    Updated on 2026-09-23

--- app/src/main/java/com/tangem/tap/features/details/ui/common/utils/ResetToFactory.kt
+++ app/src/main/java/com/tangem/tap/features/details/ui/common/utils/ResetToFactory.kt
@@ -1,15 +1,14 @@
 package com.tangem.tap.features.details.ui.common.utils
 
 import com.tangem.domain.common.CardTypesResolver
-import com.tangem.domain.models.scan.CardDTO
 import com.tangem.tap.features.details.ui.cardsettings.TextReference
 import com.tangem.wallet.R
 
 internal fun getResetToFactoryDescription(
-    backupStatus: CardDTO.BackupStatus?,
+    isActiveBackupStatus: Boolean,
     typesResolver: CardTypesResolver,
 ): TextReference {
-    return if (backupStatus?.isActive != true || typesResolver.isTangemTwins()) {
+    return if (!isActiveBackupStatus || typesResolver.isTangemTwins()) {
         TextReference.Res(R.string.reset_card_without_backup_to_factory_message)
     } else {
         TextReference.Res(R.string.reset_card_with_backup_to_factory_message)
