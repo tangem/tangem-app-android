5d1e5f792176e1129f042cd76d8d711b9deda3f6
Tangem <git@tangem.com>
2024-07-29 14:14:28 +0300

    Updated on 2025-12-18

---
+++ features/markets/impl/src/main/kotlin/com/tangem/features/markets/details/impl/ui/MarketsTokenDetailsContent.kt
@@ -0,0 +1,31 @@
+package com.tangem.features.markets.details.impl.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.tangem.core.ui.components.appbar.TangemTopAppBar
+import com.tangem.core.ui.components.appbar.models.TopAppBarButtonUM
+import com.tangem.features.markets.details.impl.ui.entity.MarketsTokenDetailsUM
+
+@Suppress("UnusedPrivateMember")
+@Composable
+internal fun MarketsTokenDetailsContent(
+    state: MarketsTokenDetailsUM,
+    onBackClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    // TODO
+}
+
+@Suppress("UnusedPrivateMember")
+@Composable
+private fun Content(state: MarketsTokenDetailsUM, onBackClick: () -> Unit, modifier: Modifier = Modifier) {
+    Column {
+        TangemTopAppBar(
+            title = state.tokenName,
+            startButton = TopAppBarButtonUM.Back(onBackClick),
+        )
+    }
+
+    // TODO
+}
