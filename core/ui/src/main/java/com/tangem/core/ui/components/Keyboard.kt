e15821e99cf4141bd178c297a5faf454c0a5ec0a
Tangem <git@tangem.com>
2024-08-01 15:21:44 +0400

    Updated on 2026-04-08

--- core/ui/src/main/java/com/tangem/core/ui/components/Keyboard.kt
+++ core/ui/src/main/java/com/tangem/core/ui/components/Keyboard.kt
@@ -3,6 +3,7 @@ package com.tangem.core.ui.components
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.ime
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.platform.LocalDensity
@@ -14,10 +15,14 @@ sealed interface Keyboard {
 
     data class Opened(override val height: Dp) : Keyboard
 
-    object Closed : Keyboard {
+    data object Closed : Keyboard {
         override val height: Dp = 0.dp
     }
 }
+
+val Keyboard.isOpened: Boolean
+    @Stable
+    get() = this is Keyboard.Opened
 
 /**
  * Allows to subscribe to a soft keyboard to detect when it's open/closed
