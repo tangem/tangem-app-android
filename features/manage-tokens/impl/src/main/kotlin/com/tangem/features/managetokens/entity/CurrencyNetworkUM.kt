4a52c22976a799f37881e4192a7c74b289744abd
Tangem <git@tangem.com>
2024-08-01 15:21:33 +0400

    Updated on 2026-09-25

--- features/manage-tokens/impl/src/main/kotlin/com/tangem/features/managetokens/entity/CurrencyNetworkUM.kt
+++ features/manage-tokens/impl/src/main/kotlin/com/tangem/features/managetokens/entity/CurrencyNetworkUM.kt
@@ -1,12 +1,15 @@
 package com.tangem.features.managetokens.entity
 
 import androidx.compose.runtime.Immutable
-import com.tangem.core.ui.components.rows.model.BlockchainRowUM
+import com.tangem.domain.tokens.model.Network
 
 @Immutable
 internal data class CurrencyNetworkUM(
-    val id: String,
-    val model: BlockchainRowUM,
+    val id: Network.ID,
+    val name: String,
+    val type: String,
+    val iconResId: Int,
+    val isMainNetwork: Boolean,
     val isSelected: Boolean,
     val onSelectedStateChange: (Boolean) -> Unit,
 )
