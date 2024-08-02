b40c520a2b6f947403dffeb1c8f054609196af03
Ilya Shulgin <shulgin.ilya.i@gmail.com>
2024-08-02 13:43:22 +0300

    AND-7711 Added comment.

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/InnerYieldBalanceState.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/InnerYieldBalanceState.kt
@@ -2,7 +2,6 @@ package com.tangem.features.staking.impl.presentation.state
 
 import androidx.compose.runtime.Immutable
 import com.tangem.core.ui.extensions.TextReference
-import com.tangem.domain.staking.model.stakekit.BalanceType
 import com.tangem.domain.staking.model.stakekit.PendingAction
 import com.tangem.domain.staking.model.stakekit.Yield
 import kotlinx.collections.immutable.ImmutableList
@@ -19,13 +18,13 @@ sealed class InnerYieldBalanceState {
     data object Empty : InnerYieldBalanceState()
 }
 
+// TODO staking get rid of unstable types
 @Immutable
 data class BalanceGroupedState(
     val items: ImmutableList<BalanceState>,
     val footer: TextReference?,
     val title: TextReference,
-    val type: BalanceType,
-    val isClickable: Boolean,
+    val type: BalanceGroupType,
 )
 
 @Immutable
@@ -39,3 +38,9 @@ data class BalanceState(
     val unbondingPeriod: TextReference,
     val pendingActions: ImmutableList<PendingAction>,
 )
+
+enum class BalanceGroupType {
+    ACTIVE,
+    UNSTAKED,
+    UNKNOWN,
+}
