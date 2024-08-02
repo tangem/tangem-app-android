eec2af568cde437e5d35c8876ada5b445ae8e5e1
Tangem <git@tangem.com>
2024-08-02 17:57:18 +0500

    Updated on 2025-12-05

--- features/manage-tokens/api/src/main/kotlin/com/tangem/features/managetokens/component/ManageTokensComponent.kt
+++ features/manage-tokens/api/src/main/kotlin/com/tangem/features/managetokens/component/ManageTokensComponent.kt
@@ -2,13 +2,10 @@ package com.tangem.features.managetokens.component
 
 import com.tangem.core.decompose.factory.ComponentFactory
 import com.tangem.core.ui.decompose.ComposableContentComponent
-import com.tangem.domain.wallets.models.UserWalletId
 
 interface ManageTokensComponent : ComposableContentComponent {
 
-    data class Params(
-        val userWalletId: UserWalletId,
-    )
+    class Params
 
     interface Factory : ComponentFactory<Params, ManageTokensComponent>
 }
