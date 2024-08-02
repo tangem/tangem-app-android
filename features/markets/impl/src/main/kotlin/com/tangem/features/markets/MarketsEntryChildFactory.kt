81241a968229b8b1e75658899dd7ab3e12191393
Tangem <git@tangem.com>
2024-08-02 11:24:49 +0300

    Updated on 2026-02-11

--- features/markets/impl/src/main/kotlin/com/tangem/features/markets/MarketsEntryChildFactory.kt
+++ features/markets/impl/src/main/kotlin/com/tangem/features/markets/MarketsEntryChildFactory.kt
@@ -1,6 +1,8 @@
 package com.tangem.features.markets
 
+import androidx.compose.runtime.Immutable
 import com.tangem.core.decompose.context.AppComponentContext
+import com.tangem.domain.appcurrency.model.AppCurrency
 import com.tangem.domain.markets.TokenMarket
 import com.tangem.features.markets.details.api.MarketsTokenDetailsComponent
 import com.tangem.features.markets.tokenlist.api.MarketsTokenListComponent
@@ -13,25 +15,30 @@ internal class MarketsEntryChildFactory @Inject constructor(
 ) {
 
     @Serializable
+    @Immutable
     sealed interface Child {
 
         @Serializable
+        @Immutable
         data object TokenList : Child
 
         @Serializable
+        @Immutable
         data class TokenDetails(val params: MarketsTokenDetailsComponent.Params) : Child
     }
 
     fun createChild(
         child: Child,
         appComponentContext: AppComponentContext,
-        onTokenSelected: (TokenMarket) -> Unit,
+        onTokenSelected: (TokenMarket, AppCurrency) -> Unit,
+        onDetailsBack: () -> Unit,
     ): Any {
         return when (child) {
             is Child.TokenDetails -> {
                 tokenDetailsComponentFactory.create(
                     context = appComponentContext,
                     params = child.params,
+                    onBack = onDetailsBack,
                 )
             }
             is Child.TokenList -> {
