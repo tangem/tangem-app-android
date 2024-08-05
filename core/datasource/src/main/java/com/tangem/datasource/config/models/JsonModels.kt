dde828d622bd163c1e47e8f5b3dad441b4b99a4e
Tangem <git@tangem.com>
2024-08-05 16:02:33 +0300

    Updated on 2026-05-04

--- core/datasource/src/main/java/com/tangem/datasource/config/models/JsonModels.kt
+++ core/datasource/src/main/java/com/tangem/datasource/config/models/JsonModels.kt
@@ -77,6 +77,7 @@ data class GetBlockAccessTokens(
     @Json(name = "zksync") val zksync: GetBlockToken?,
     @Json(name = "base") val base: GetBlockToken?,
     @Json(name = "blast") val blast: GetBlockToken?,
+    @Json(name = "filecoin") val filecoin: GetBlockToken?,
 )
 
 @JsonClass(generateAdapter = true)
