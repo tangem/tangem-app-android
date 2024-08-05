9e230dc07985199012e4148ae26d8d472f457be4
Tangem <git@tangem.com>
2024-08-05 21:40:29 +0400

    Updated on 2026-09-01

---
+++ domain/manage-tokens/src/main/kotlin/com/tangem/domain/managetokens/GetManagedTokensUseCase.kt
@@ -0,0 +1,14 @@
+package com.tangem.domain.managetokens
+
+import com.tangem.domain.managetokens.model.ManageTokensListBatchFlow
+import com.tangem.domain.managetokens.model.ManageTokensListBatchingContext
+import com.tangem.domain.managetokens.repository.ManageTokensRepository
+
+class GetManagedTokensUseCase(
+    private val repository: ManageTokensRepository,
+) {
+
+    operator fun invoke(context: ManageTokensListBatchingContext, batchSize: Int = 40): ManageTokensListBatchFlow {
+        return repository.tokensFlow(context, batchSize)
+    }
+}
