db3a093582bb403556b834ef7860994a186cc2ff
Tangem <git@tangem.com>
2024-08-06 16:14:34 +0400

    Updated on 2026-06-16

--- features/disclaimer/impl/src/main/java/com/tangem/features/disclaimer/impl/model/DisclaimerModel.kt
+++ features/disclaimer/impl/src/main/java/com/tangem/features/disclaimer/impl/model/DisclaimerModel.kt
@@ -5,6 +5,7 @@ import com.tangem.core.decompose.di.ComponentScoped
 import com.tangem.core.decompose.model.Model
 import com.tangem.core.decompose.model.ParamsContainer
 import com.tangem.core.decompose.navigation.Router
+import com.tangem.core.navigation.finisher.AppFinisher
 import com.tangem.domain.card.repository.CardRepository
 import com.tangem.domain.settings.NeverRequestPermissionUseCase
 import com.tangem.domain.settings.NeverToInitiallyAskPermissionUseCase
@@ -17,12 +18,14 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @ComponentScoped
+@Suppress("LongParameterList")
 internal class DisclaimerModel @Inject constructor(
+    override val dispatchers: CoroutineDispatcherProvider,
     private val cardRepository: CardRepository,
     private val router: Router,
-    override val dispatchers: CoroutineDispatcherProvider,
     private val neverToInitiallyAskPermissionUseCase: NeverToInitiallyAskPermissionUseCase,
     private val neverRequestPermissionUseCase: NeverRequestPermissionUseCase,
+    private val appFinisher: AppFinisher,
     paramsContainer: ParamsContainer,
 ) : Model() {
 
@@ -33,20 +36,31 @@ internal class DisclaimerModel @Inject constructor(
             onAccept = ::onAccept,
             url = DISCLAIMER_URL,
             isTosAccepted = params.isTosAccepted,
-            popBack = router::pop,
+            popBack = ::popBack,
         ),
     )
 
-    private fun onAccept(shouldAskPushPermission: Boolean) {
-        modelScope.launch {
+    private fun onAccept(shouldAskPushPermission: Boolean) = modelScope.launch {
+        if (params.isTosAccepted) {
+            router.pop()
+        } else {
             cardRepository.acceptTangemTOS()
+
             if (shouldAskPushPermission) {
                 router.push(AppRoute.PushNotification)
             } else {
                 neverToInitiallyAskPermissionUseCase(PUSH_PERMISSION)
                 neverRequestPermissionUseCase(PUSH_PERMISSION)
-                router.push(AppRoute.Home)
+                router.replaceAll(AppRoute.Home)
             }
+        }
+    }
+
+    private fun popBack() {
+        if (params.isTosAccepted) {
+            router.pop()
+        } else {
+            appFinisher.finish()
         }
     }
 
