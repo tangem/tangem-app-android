814ef660ae5038ef7a9c4cf0c2e09a070ee19d6a
Tangem <git@tangem.com>
2024-08-06 10:32:38 +0300

    Updated on 2026-06-24

--- app/src/main/java/com/tangem/tap/common/CustomTabsManager.kt
+++ app/src/main/java/com/tangem/tap/common/CustomTabsManager.kt
@@ -1,8 +1,6 @@
 package com.tangem.tap.common
 
 import android.content.Context
-import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
-import android.content.Intent.FLAG_ACTIVITY_NO_HISTORY
 import android.net.Uri
 import androidx.browser.customtabs.CustomTabColorSchemeParams
 import androidx.browser.customtabs.CustomTabsIntent
@@ -25,9 +23,6 @@ class CustomTabsManager {
                 if (MutableAppThemeModeHolder.isDarkThemeActive) COLOR_SCHEME_DARK else COLOR_SCHEME_LIGHT,
             )
             .build()
-
-        // Open CustomTabsActivity as new task without saving into the stack
-        customTabsIntent.intent.setFlags(FLAG_ACTIVITY_NEW_TASK or FLAG_ACTIVITY_NO_HISTORY)
 
         customTabsIntent.launchUrl(context, Uri.parse(url))
     }
