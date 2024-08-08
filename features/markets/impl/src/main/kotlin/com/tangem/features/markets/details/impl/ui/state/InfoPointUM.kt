0eeaaeaa1f2a5839720eb6a461bc1ef14afda898
Tangem <git@tangem.com>
2024-08-08 18:53:25 +0300

    Updated on 2026-09-01

--- features/markets/impl/src/main/kotlin/com/tangem/features/markets/details/impl/ui/state/InfoPointUM.kt
+++ features/markets/impl/src/main/kotlin/com/tangem/features/markets/details/impl/ui/state/InfoPointUM.kt
@@ -1,9 +1,7 @@
-package com.tangem.features.markets.details.impl.ui.entity
+package com.tangem.features.markets.details.impl.ui.state
 
-import androidx.compose.runtime.Immutable
 import com.tangem.core.ui.extensions.TextReference
 
-@Immutable
 internal data class InfoPointUM(
     val title: TextReference,
     val value: String,
