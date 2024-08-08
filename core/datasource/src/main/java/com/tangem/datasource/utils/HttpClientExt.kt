049c45b2c3bee4551781db27127fe0041ff8c59e
Tangem <git@tangem.com>
2024-08-08 19:22:21 +0300

    Updated on 2026-09-25

--- core/datasource/src/main/java/com/tangem/datasource/utils/HttpClientExt.kt
+++ core/datasource/src/main/java/com/tangem/datasource/utils/HttpClientExt.kt
@@ -3,6 +3,9 @@ package com.tangem.datasource.utils
 import android.content.Context
 import com.chuckerteam.chucker.api.ChuckerInterceptor
 import com.tangem.datasource.BuildConfig
+import com.tangem.datasource.api.common.SwitchBaseUrlInterceptor
+import com.tangem.datasource.api.common.config.ApiConfig
+import com.tangem.datasource.api.common.config.managers.ApiConfigsManager
 import com.tangem.datasource.api.common.createNetworkLoggingInterceptor
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
@@ -25,7 +28,7 @@ internal fun OkHttpClient.Builder.addHeaders(vararg requestHeaders: RequestHeade
 /**
  * Extension for logging each [OkHttpClient] request
  *
- * @param level logging level. By default, only the request body.
+ * @param context context
  */
 internal fun OkHttpClient.Builder.addLoggers(context: Context? = null): OkHttpClient.Builder {
     return if (BuildConfig.LOG_ENABLED) {
@@ -33,6 +36,28 @@ internal fun OkHttpClient.Builder.addLoggers(context: Context? = null): OkHttpCl
             addInterceptor(interceptor = ChuckerInterceptor(it))
         }
         addInterceptor(interceptor = createNetworkLoggingInterceptor())
+    } else {
+        this
+    }
+}
+
+/**
+ * Add environment switcher
+ *
+ * @param id                class of [ApiConfig]
+ * @param apiConfigsManager api configs manager
+ */
+internal fun OkHttpClient.Builder.addEnvironmentSwitcher(
+    id: ApiConfig.ID,
+    apiConfigsManager: ApiConfigsManager,
+): OkHttpClient.Builder {
+    return if (BuildConfig.TESTER_MENU_ENABLED) {
+        addInterceptor(
+            interceptor = SwitchBaseUrlInterceptor(
+                id = id,
+                apiConfigsManager = apiConfigsManager,
+            ),
+        )
     } else {
         this
     }
