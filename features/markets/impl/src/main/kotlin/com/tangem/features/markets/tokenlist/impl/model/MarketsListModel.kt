2318b8b2a932816ea7c6ee99caf6a5dcdc99674f
Nikita Samoylov <nikit.samoylov@gmail.com>
2024-08-08 10:05:18 +0300

    fix searchbar

--- features/markets/impl/src/main/kotlin/com/tangem/features/markets/tokenlist/impl/model/MarketsListModel.kt
+++ features/markets/impl/src/main/kotlin/com/tangem/features/markets/tokenlist/impl/model/MarketsListModel.kt
@@ -200,10 +200,12 @@ internal class MarketsListModel @Inject constructor(
 
         modelScope.launch {
             marketsListUMStateManager.searchQueryFlow
-                .filter { it.isNotEmpty() }
                 .debounce(timeoutMillis = SEARCH_QUERY_DEBOUNCE_MILLIS)
                 .distinctUntilChanged()
-                .filter { activeListManager == searchMarketsListManager }
+                .onEach {
+                    if (it.isEmpty()) searchMarketsListManager.clearStateAndStopAllActions()
+                }
+                .filter { it.isNotEmpty() && activeListManager == searchMarketsListManager }
                 .collectLatest {
                     searchMarketsListManager.reload(searchText = it)
                 }
@@ -229,6 +231,7 @@ internal class MarketsListModel @Inject constructor(
             }
         }
     }
+
     private fun CoroutineScope.loadQuotesWithTimer(timeMillis: Long) {
         launch {
             while (true) {
