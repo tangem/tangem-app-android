dd83c8e9d2b26560f6649ba91d6742b1e8e545bd
Tangem <git@tangem.com>
2024-08-08 18:53:25 +0300

    release/5.26.2

--- core/ui/src/main/java/com/tangem/core/ui/res/TangemAnimations.kt
+++ core/ui/src/main/java/com/tangem/core/ui/res/TangemAnimations.kt
@@ -1,10 +1,7 @@
 package com.tangem.core.ui.res
 
 import androidx.compose.animation.*
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.spring
-import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.*
 import androidx.compose.runtime.*
 
 @Immutable
@@ -24,10 +21,6 @@ object TangemAnimations {
 
     @Immutable
     object TransitionSpecs {
-
-        @Stable
-        val textChange: ContentTransform =
-            fadeIn(animationSpec = spring(dampingRatio = Spring.DampingRatioNoBouncy)) togetherWith
-                fadeOut(animationSpec = spring(dampingRatio = Spring.DampingRatioNoBouncy))
+        // TODO add more transition specs
     }
 }
