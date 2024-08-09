cf2adfc2c512939f3cd190eb98c41c47e84d7aec
Tangem <git@tangem.com>
2024-08-09 19:29:13 +0400

    Updated on 2025-10-09

--- domain/tokens/models/build.gradle.kts
+++ domain/tokens/models/build.gradle.kts
@@ -1,12 +1,7 @@
 plugins {
-    alias(deps.plugins.android.library)
-    alias(deps.plugins.kotlin.android)
+    alias(deps.plugins.kotlin.jvm)
     alias(deps.plugins.kotlin.serialization)
     id("configuration")
-}
-
-android {
-    namespace = "com.tangem.domain.tokens.model"
 }
 
 dependencies {
@@ -17,13 +12,7 @@ dependencies {
     implementation(projects.domain.txhistory.models)
     implementation(projects.domain.staking.models)
 
-    /** SDK dependencies */
-    implementation(deps.tangem.blockchain) {
-        exclude(module = "joda-time")
-    }
-
     /** Other dependencies */
     implementation(deps.kotlin.serialization)
     implementation(deps.jodatime)
-    implementation(deps.timber)
 }
