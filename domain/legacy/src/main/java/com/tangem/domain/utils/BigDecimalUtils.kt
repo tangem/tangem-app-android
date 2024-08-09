4b31c82dcf84d3e72e2114a5e85615c4561a41cc
Tangem <git@tangem.com>
2024-08-09 19:29:13 +0400

    Updated on 2026-09-01

--- domain/legacy/src/main/java/com/tangem/domain/utils/BigDecimalUtils.kt
+++ domain/legacy/src/main/java/com/tangem/domain/utils/BigDecimalUtils.kt
@@ -1,13 +1,13 @@
-package com.tangem.domain.tokens.utils
+package com.tangem.domain.utils
 
-import com.tangem.blockchain.common.Amount
 import com.tangem.blockchain.common.AmountType
 import com.tangem.blockchain.common.Token
 import com.tangem.domain.tokens.model.CryptoCurrency
 import java.math.BigDecimal
+import com.tangem.blockchain.common.Amount as SdkAmount
 
-/** Converts `BigDecimal` [cryptoCurrency] to [Amount] */
-fun BigDecimal.convertToAmount(cryptoCurrency: CryptoCurrency) = Amount(
+/** Converts `BigDecimal` [cryptoCurrency] to [SdkAmount] */
+fun BigDecimal.convertToSdkAmount(cryptoCurrency: CryptoCurrency): SdkAmount = SdkAmount(
     currencySymbol = cryptoCurrency.symbol,
     value = this,
     decimals = cryptoCurrency.decimals,
