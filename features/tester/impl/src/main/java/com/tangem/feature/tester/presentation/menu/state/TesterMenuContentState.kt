874f4980b04d8d9e1932ce9539b6664d1632a8c3
Tangem <git@tangem.com>
2024-08-09 12:25:31 +0300

    Updated on 2026-04-15

--- features/tester/impl/src/main/java/com/tangem/feature/tester/presentation/menu/state/TesterMenuContentState.kt
+++ features/tester/impl/src/main/java/com/tangem/feature/tester/presentation/menu/state/TesterMenuContentState.kt
@@ -3,12 +3,14 @@ package com.tangem.feature.tester.presentation.menu.state
 /**
  * Content state of tester menu screen
  *
- * @property onBackClick           the lambda to be invoked when back button is pressed
- * @property onFeatureTogglesClick the lambda to be invoked when feature toggles button is pressed
- * @property onTesterActionsClick  the lambda to be invoked when tester actions button is pressed
+ * @property onBackClick               the lambda to be invoked when back button is pressed
+ * @property onFeatureTogglesClick     the lambda to be invoked when feature toggles button is pressed
+ * @property onEnvironmentTogglesClick the lambda to be invoked when environment toggles button is pressed
+ * @property onTesterActionsClick      the lambda to be invoked when tester actions button is pressed
  */
 data class TesterMenuContentState(
     val onBackClick: () -> Unit,
     val onFeatureTogglesClick: () -> Unit,
+    val onEnvironmentTogglesClick: () -> Unit,
     val onTesterActionsClick: () -> Unit,
 )
