3efdf2e717ef888ebdf92defd09786c97133fc9d
Mama1emon <andrew.khokhlove@gmail.com>
2024-08-12 20:12:28 +0300

    AND-7888 Fix Redux logging
    Signed-off-by: Mama1emon <andrew.khokhlove@gmail.com>

--- app/src/main/java/com/tangem/tap/common/redux/LogMiddleware.kt
+++ app/src/main/java/com/tangem/tap/common/redux/LogMiddleware.kt
@@ -1,19 +1,15 @@
 package com.tangem.tap.common.redux
 
-import com.tangem.domain.common.LogConfig
 import org.rekotlin.Middleware
 import timber.log.Timber
 
 /**
  * Created by Anton Zhilenkov on 31/08/2020.
  */
-val logMiddleware: Middleware<AppState> = { dispatch, appState ->
+val logMiddleware: Middleware<AppState> = { _, _ ->
     { nextDispatch ->
         { action ->
-            if (LogConfig.storeAction) {
-                Timber.d("Dispatch action: $action")
-                // printOnboardingWalletState()
-            }
+            Timber.i("Dispatch action: $action")
             nextDispatch(action)
         }
     }
