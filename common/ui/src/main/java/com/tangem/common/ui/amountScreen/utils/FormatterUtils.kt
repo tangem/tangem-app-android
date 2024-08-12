21698e9c18fb900f932587b2826fed3f08cc792f
Tangem <git@tangem.com>
2024-08-12 20:36:12 +0500

    Updated on 2026-09-25

--- common/ui/src/main/java/com/tangem/common/ui/amountScreen/utils/FormatterUtils.kt
+++ common/ui/src/main/java/com/tangem/common/ui/amountScreen/utils/FormatterUtils.kt
@@ -1,29 +1,11 @@
 package com.tangem.common.ui.amountScreen.utils
 
-import com.tangem.blockchain.common.Amount
 import com.tangem.core.ui.extensions.TextReference
-import com.tangem.core.ui.extensions.combinedReference
 import com.tangem.core.ui.extensions.stringReference
 import com.tangem.core.ui.utils.BigDecimalFormatter
 import com.tangem.core.ui.utils.BigDecimalFormatter.EMPTY_BALANCE_SIGN
 import com.tangem.domain.appcurrency.model.AppCurrency
 import java.math.BigDecimal
-
-private const val CRYPTO_FEE_DECIMALS = 6
-
-fun getCryptoReference(amount: Amount?, isFeeApproximate: Boolean): TextReference? {
-    if (amount == null) return null
-    return combinedReference(
-        if (isFeeApproximate) stringReference("${BigDecimalFormatter.CAN_BE_LOWER_SIGN} ") else TextReference.EMPTY,
-        stringReference(
-            BigDecimalFormatter.formatCryptoAmount(
-                cryptoAmount = amount.value,
-                cryptoCurrency = amount.currencySymbol,
-                decimals = amount.decimals.coerceAtMost(CRYPTO_FEE_DECIMALS),
-            ),
-        ),
-    )
-}
 
 fun getFiatReference(value: BigDecimal?, rate: BigDecimal?, appCurrency: AppCurrency): TextReference? {
     if (value == null || rate == null) return null
