3acba9fbf3c9bd477736f2f7bb185460bf802c05
Mama1emon <andrew.khokhlove@gmail.com>
2024-08-12 17:30:18 +0300

    AND-7888 Fix Navigation's logging
    Signed-off-by: Mama1emon <andrew.khokhlove@gmail.com>

--- app/src/main/java/com/tangem/tap/routing/ProxyAppRouter.kt
+++ app/src/main/java/com/tangem/tap/routing/ProxyAppRouter.kt
@@ -7,6 +7,7 @@ import com.tangem.tap.routing.configurator.AppRouterConfig
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import kotlin.reflect.KClass
 
 internal class ProxyAppRouter(
@@ -31,30 +32,35 @@ internal class ProxyAppRouter(
 
     override fun push(route: AppRoute, onComplete: (isSuccess: Boolean) -> Unit) {
         routerScope.launch(dispatchers.mainImmediate) {
+            Timber.i("Push route: $route")
             innerRouter.push(route, onComplete)
         }
     }
 
     override fun replaceAll(vararg routes: AppRoute, onComplete: (isSuccess: Boolean) -> Unit) {
         routerScope.launch(dispatchers.mainImmediate) {
+            Timber.i("Replace all routes with $routes")
             innerRouter.replaceAll(*routes, onComplete = onComplete)
         }
     }
 
     override fun pop(onComplete: (isSuccess: Boolean) -> Unit) {
         routerScope.launch(dispatchers.mainImmediate) {
+            Timber.i("Pop route")
             innerRouter.pop(onComplete)
         }
     }
 
     override fun popTo(route: AppRoute, onComplete: (isSuccess: Boolean) -> Unit) {
         routerScope.launch(dispatchers.mainImmediate) {
+            Timber.i("Pop to route: $route")
             innerRouter.popTo(route, onComplete)
         }
     }
 
     override fun popTo(routeClass: KClass<out AppRoute>, onComplete: (isSuccess: Boolean) -> Unit) {
         routerScope.launch(dispatchers.mainImmediate) {
+            Timber.i("Pop to route class: $routeClass")
             innerRouter.popTo(routeClass, onComplete)
         }
     }
