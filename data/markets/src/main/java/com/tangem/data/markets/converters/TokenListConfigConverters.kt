2112b2776fe9788567211fb6124921ee7a235854
Tangem <git@tangem.com>
2024-08-12 14:38:46 +0300

    Updated on 2026-07-07

--- data/markets/src/main/java/com/tangem/data/markets/converters/TokenListConfigConverters.kt
+++ data/markets/src/main/java/com/tangem/data/markets/converters/TokenListConfigConverters.kt
@@ -3,13 +3,13 @@ package com.tangem.data.markets.converters
 import com.tangem.domain.markets.PriceChangeInterval
 import com.tangem.domain.markets.TokenMarketListConfig
 
-fun TokenMarketListConfig.Interval.toRequestParam(): String = when (this) {
+internal fun TokenMarketListConfig.Interval.toRequestParam(): String = when (this) {
     TokenMarketListConfig.Interval.H24 -> "24h"
     TokenMarketListConfig.Interval.WEEK -> "1w"
     TokenMarketListConfig.Interval.MONTH -> "30d"
 }
 
-fun TokenMarketListConfig.Order.toRequestParam(): String = when (this) {
+internal fun TokenMarketListConfig.Order.toRequestParam(): String = when (this) {
     TokenMarketListConfig.Order.ByRating -> "rating"
     TokenMarketListConfig.Order.Trending -> "trending"
     TokenMarketListConfig.Order.Buyers -> "buyers"
@@ -17,7 +17,7 @@ fun TokenMarketListConfig.Order.toRequestParam(): String = when (this) {
     TokenMarketListConfig.Order.TopLosers -> "losers"
 }
 
-fun PriceChangeInterval.toRequestParam(): String = when (this) {
+internal fun PriceChangeInterval.toRequestParam(): String = when (this) {
     PriceChangeInterval.H24 -> "24h"
     PriceChangeInterval.WEEK -> "1w"
     PriceChangeInterval.MONTH -> "1m"
