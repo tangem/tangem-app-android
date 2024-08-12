8dd572b82ac5aa8ad2e5b62f5bf5340c2426f52c
Tangem <git@tangem.com>
2024-08-12 22:01:26 +0500

    Updated on 2026-05-14

--- domain/staking/src/main/java/com/tangem/domain/staking/IsApproveNeededUseCase.kt
+++ domain/staking/src/main/java/com/tangem/domain/staking/IsApproveNeededUseCase.kt
@@ -1,6 +1,7 @@
 package com.tangem.domain.staking
 
 import arrow.core.Either
+import com.tangem.domain.staking.model.StakingApproval
 import com.tangem.domain.staking.model.stakekit.StakingError
 import com.tangem.domain.staking.repositories.StakingErrorResolver
 import com.tangem.domain.staking.repositories.StakingRepository
@@ -10,9 +11,9 @@ class IsApproveNeededUseCase(
     private val stakingRepository: StakingRepository,
     private val stakingErrorResolver: StakingErrorResolver,
 ) {
-    operator fun invoke(cryptoCurrency: CryptoCurrency): Either<StakingError, Boolean> {
+    operator fun invoke(cryptoCurrency: CryptoCurrency): Either<StakingError, StakingApproval> {
         return Either
-            .catch { stakingRepository.isApproveNeeded(cryptoCurrency) }
+            .catch { stakingRepository.getStakingApproval(cryptoCurrency) }
             .mapLeft { stakingErrorResolver.resolve(it) }
     }
 }
