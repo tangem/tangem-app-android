c7f702cc90e8167e2365ce9cc246c5abb719bd49
Tangem <git@tangem.com>
2024-08-13 15:31:16 +0300

    Updated on 2026-04-08

--- app/src/main/java/com/tangem/tap/data/TangemBlockchainSDKLogger.kt
+++ app/src/main/java/com/tangem/tap/data/TangemBlockchainSDKLogger.kt
@@ -2,33 +2,19 @@ package com.tangem.tap.data
 
 import com.tangem.blockchain.common.logging.BlockchainSDKLogger
 import com.tangem.domain.settings.repositories.SettingsRepository
-import com.tangem.utils.coroutines.CoroutineDispatcherProvider
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 
 /**
  * BlockchainSDK logger implementation
  *
  * @property settingsRepository settings repository
- * @property dispatchers        coroutine dispatchers provider
  *
 [REDACTED_AUTHOR]
  */
 internal class TangemBlockchainSDKLogger(
     private val settingsRepository: SettingsRepository,
-    private val dispatchers: CoroutineDispatcherProvider,
 ) : BlockchainSDKLogger {
 
-    private val scope = CoroutineScope(dispatchers.main)
-    private val mutex = Mutex()
-
     override fun log(level: BlockchainSDKLogger.Level, message: String) {
-        scope.launch(dispatchers.main) {
-            mutex.withLock {
-                settingsRepository.updateAppLogs(message)
-            }
-        }
+        settingsRepository.saveLogMessage(message)
     }
 }
