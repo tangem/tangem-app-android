01b2069584455a2b019b5e3cfdac38373bb27dd0
Tangem <git@tangem.com>
2024-08-13 15:31:16 +0300

    release/5.26.1

--- app/src/main/java/com/tangem/tap/ApplicationEntryPoint.kt
+++ app/src/main/java/com/tangem/tap/ApplicationEntryPoint.kt
@@ -32,6 +32,7 @@ import com.tangem.domain.wallets.repository.WalletsRepository
 import com.tangem.domain.wallets.usecase.GenerateWalletNameUseCase
 import com.tangem.features.details.DetailsFeatureToggles
 import com.tangem.features.pushnotifications.api.featuretoggles.PushNotificationsFeatureToggles
+import com.tangem.tap.common.log.TangemAppLoggerInitializer
 import com.tangem.tap.domain.walletconnect2.domain.WalletConnectSessionsRepository
 import com.tangem.tap.features.customtoken.api.featuretoggles.CustomTokenFeatureToggles
 import com.tangem.tap.proxy.AppStateHolder
@@ -116,4 +117,6 @@ interface ApplicationEntryPoint {
     fun getAppRouter(): AppRouter
 
     fun getPushNotificationsFeatureToggles(): PushNotificationsFeatureToggles
+
+    fun getTangemAppLogger(): TangemAppLoggerInitializer
 }
