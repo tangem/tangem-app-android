4fdd0264d77387f9b11070368b8438118b2ccd70
Tangem <git@tangem.com>
2024-08-13 20:00:57 +0300

    Updated on 2026-02-25

--- core/datasource/src/main/java/com/tangem/datasource/config/ConfigManagerImpl.kt
+++ core/datasource/src/main/java/com/tangem/datasource/config/ConfigManagerImpl.kt
@@ -1,7 +1,6 @@
 package com.tangem.datasource.config
 
 import com.tangem.blockchain.common.*
-import com.tangem.datasource.BuildConfig
 import com.tangem.datasource.config.ConfigManager.Companion.IS_CREATING_TWIN_CARDS_ALLOWED
 import com.tangem.datasource.config.ConfigManager.Companion.IS_TOP_UP_ENABLED
 import com.tangem.datasource.config.models.Config
@@ -109,7 +108,8 @@ internal class ConfigManagerImpl @Inject constructor() : ConfigManager {
             sprinklr = configValues.sprinklr,
             walletConnectProjectId = configValues.walletConnectProjectId,
             tangemComAuthorization = configValues.tangemComAuthorization,
-            express = if (BuildConfig.ENVIRONMENT == "dev") configValues.devExpress else configValues.express,
+            express = configValues.express,
+            devExpress = configValues.devExpress,
             stakeKitApiKey = configValues.stakeKitApiKey,
         )
     }
