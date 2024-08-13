35f4ab163280946bc9038346f8199f54f366f8ab
Tangem <git@tangem.com>
2024-08-13 15:57:36 +0300

    Updated on 2026-09-21

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/transformers/SetConfirmationStateLoadingTransformer.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/transformers/SetConfirmationStateLoadingTransformer.kt
@@ -1,8 +1,11 @@
 package com.tangem.features.staking.impl.presentation.state.transformers
 
 import com.tangem.domain.staking.model.stakekit.Yield
+import com.tangem.domain.staking.model.stakekit.action.StakingActionCommonType
+import com.tangem.features.staking.impl.R
 import com.tangem.features.staking.impl.presentation.state.*
 import com.tangem.utils.transformer.Transformer
+import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 
 internal class SetConfirmationStateLoadingTransformer(
@@ -24,12 +27,7 @@ internal class SetConfirmationStateLoadingTransformer(
                     chosenValidator = chosenValidator,
                     availableValidators = yield.validators,
                 ),
-                notifications = persistentListOf(
-                    StakingNotification.Warning.EarnRewards(
-                        currencyName = yield.token.name,
-                        days = yield.metadata.cooldownPeriod.days,
-                    ),
-                ),
+                notifications = getNotifications(prevState),
                 footerText = "",
                 transactionDoneState = TransactionDoneState.Empty,
                 pendingActions = persistentListOf(),
@@ -37,5 +35,42 @@ internal class SetConfirmationStateLoadingTransformer(
                 isApprovalNeeded = false,
             ),
         )
+    }
+
+    private fun getNotifications(prevState: StakingUiState): ImmutableList<StakingNotification> {
+        return persistentListOf(
+            if (prevState.actionType == StakingActionCommonType.EXIT) {
+                StakingNotification.Warning.Unstake(
+                    cooldownPeriodDays = yield.metadata.cooldownPeriod.days,
+                )
+            } else {
+                StakingNotification.Warning.EarnRewards(
+                    currencyName = yield.token.name,
+                    subtitleResourceId = getEarnRewardsPeriod(yield.metadata.rewardSchedule),
+                )
+            },
+        )
+    }
+
+    private fun getEarnRewardsPeriod(rewardSchedule: Yield.Metadata.RewardSchedule): Int {
+        return when (rewardSchedule) {
+            Yield.Metadata.RewardSchedule.BLOCK,
+            Yield.Metadata.RewardSchedule.DAY,
+            Yield.Metadata.RewardSchedule.ERA,
+            Yield.Metadata.RewardSchedule.EPOCH,
+            -> R.string.staking_notification_earn_rewards_text_period_day
+
+            Yield.Metadata.RewardSchedule.HOUR,
+            -> R.string.staking_notification_earn_rewards_text_period_hour
+
+            Yield.Metadata.RewardSchedule.WEEK,
+            -> R.string.staking_notification_earn_rewards_text_period_week
+
+            Yield.Metadata.RewardSchedule.MONTH,
+            -> R.string.staking_notification_earn_rewards_text_period_month
+
+            else
+            -> R.string.staking_notification_earn_rewards_text_period_day
+        }
     }
 }
