0108807098bf540870ac80313f98aee50be78fba
Tangem <git@tangem.com>
2024-08-13 15:57:36 +0300

    Updated on 2026-08-19

--- data/staking/src/main/java/com/tangem/data/staking/converters/YieldConverter.kt
+++ data/staking/src/main/java/com/tangem/data/staking/converters/YieldConverter.kt
@@ -2,8 +2,10 @@ package com.tangem.data.staking.converters
 
 import com.tangem.datasource.api.stakekit.models.response.model.AddressArgumentDTO
 import com.tangem.datasource.api.stakekit.models.response.model.YieldDTO
+import com.tangem.datasource.api.stakekit.models.response.model.YieldDTO.MetadataDTO.RewardScheduleDTO
 import com.tangem.domain.staking.model.stakekit.AddressArgument
 import com.tangem.domain.staking.model.stakekit.Yield
+import com.tangem.domain.staking.model.stakekit.Yield.Metadata.RewardSchedule
 import com.tangem.utils.converter.Converter
 
 class YieldConverter(
@@ -80,10 +82,10 @@ class YieldConverter(
             token = tokenConverter.convert(metadataDTO.tokenDTO),
             tokens = metadataDTO.tokensDTO.map { tokenConverter.convert(it) },
             type = metadataDTO.type,
-            rewardSchedule = metadataDTO.rewardSchedule,
+            rewardSchedule = convertRewardSchedule(metadataDTO.rewardSchedule),
             cooldownPeriod = convertPeriod(metadataDTO.cooldownPeriod),
             warmupPeriod = convertPeriod(metadataDTO.warmupPeriod),
-            rewardClaiming = metadataDTO.rewardClaiming,
+            rewardClaiming = convertRewardClaiming(metadataDTO.rewardClaiming),
             defaultValidator = metadataDTO.defaultValidator,
             minimumStake = metadataDTO.minimumStake,
             supportsMultipleValidators = metadataDTO.supportsMultipleValidators,
@@ -124,6 +126,29 @@ class YieldConverter(
             YieldDTO.RewardTypeDTO.APY -> Yield.RewardType.APY
             YieldDTO.RewardTypeDTO.APR -> Yield.RewardType.APR
             else -> Yield.RewardType.UNKNOWN
+        }
+    }
+
+    private fun convertRewardSchedule(rewardTypeDTO: RewardScheduleDTO): RewardSchedule {
+        return when (rewardTypeDTO) {
+            RewardScheduleDTO.BLOCK -> RewardSchedule.BLOCK
+            RewardScheduleDTO.WEEK -> RewardSchedule.WEEK
+            RewardScheduleDTO.HOUR -> RewardSchedule.HOUR
+            RewardScheduleDTO.DAY -> RewardSchedule.DAY
+            RewardScheduleDTO.MONTH -> RewardSchedule.MONTH
+            RewardScheduleDTO.ERA -> RewardSchedule.ERA
+            RewardScheduleDTO.EPOCH -> RewardSchedule.EPOCH
+            else -> RewardSchedule.UNKNOWN
+        }
+    }
+
+    private fun convertRewardClaiming(
+        rewardClaimingDTO: YieldDTO.MetadataDTO.RewardClaimingDTO,
+    ): Yield.Metadata.RewardClaiming {
+        return when (rewardClaimingDTO) {
+            YieldDTO.MetadataDTO.RewardClaimingDTO.AUTO -> Yield.Metadata.RewardClaiming.AUTO
+            YieldDTO.MetadataDTO.RewardClaimingDTO.MANUAL -> Yield.Metadata.RewardClaiming.MANUAL
+            else -> Yield.Metadata.RewardClaiming.UNKNOWN
         }
     }
 
