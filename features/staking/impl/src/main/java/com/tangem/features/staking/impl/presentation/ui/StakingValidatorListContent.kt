9119251db7e5b0d9e80827671a27ab6c4b5225ef
Tangem <git@tangem.com>
2024-08-14 18:23:39 +0500

    Updated on 2026-07-23

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/ui/StakingValidatorListContent.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/ui/StakingValidatorListContent.kt
@@ -1,25 +1,26 @@
 package com.tangem.features.staking.impl.presentation.ui
 
 import android.content.res.Configuration
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import com.tangem.core.ui.components.inputrow.InputRowImageSelector
-import com.tangem.core.ui.components.rows.CornersToRound
-import com.tangem.core.ui.extensions.annotatedReference
-import com.tangem.core.ui.extensions.combinedReference
-import com.tangem.core.ui.extensions.resourceReference
-import com.tangem.core.ui.extensions.stringReference
+import com.tangem.core.ui.decorations.roundedShapeItemDecoration
+import com.tangem.core.ui.extensions.*
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.core.ui.res.TangemThemePreview
 import com.tangem.core.ui.utils.BigDecimalFormatter
@@ -28,7 +29,7 @@ import com.tangem.features.staking.impl.presentation.state.ValidatorState
 import com.tangem.features.staking.impl.presentation.state.previewdata.ConfirmationStatePreviewData
 import com.tangem.features.staking.impl.presentation.state.stub.StakingClickIntentsStub
 import com.tangem.features.staking.impl.presentation.viewmodel.StakingClickIntents
-import java.math.BigDecimal
+import com.tangem.utils.extensions.orZero
 
 /**
  * Staking screen with validators
@@ -58,29 +59,40 @@ internal fun StakingValidatorListContent(
                     subtitle = stringReference(item.name),
                     caption = combinedReference(
                         resourceReference(R.string.staking_details_apr),
-                        annotatedReference(
-                            buildAnnotatedString {
-                                append(" ")
-                                withStyle(style = SpanStyle(color = TangemTheme.colors.text.accent)) {
-                                    append(
-                                        BigDecimalFormatter.formatPercent(item.apr ?: BigDecimal.ZERO, true),
-                                    )
-                                }
-                            },
-                        ),
+                        annotatedReference {
+                            appendSpace()
+                            appendColored(
+                                text = BigDecimalFormatter.formatPercent(item.apr.orZero(), true),
+                                color = TangemTheme.colors.text.accent,
+                            )
+                        },
                     ),
                     imageUrl = item.image.orEmpty(),
                     isSelected = item == state.chosenValidator,
                     onSelect = { clickIntents.onValidatorSelect(item) },
                     modifier = Modifier
-                        .clip(
-                            when (index) {
-                                0 -> CornersToRound.TOP_2
-                                validators.lastIndex -> CornersToRound.BOTTOM_2
-                                else -> CornersToRound.ZERO
-                            }.getShape(),
+                        .roundedShapeItemDecoration(
+                            currentIndex = index,
+                            lastIndex = validators.lastIndex,
+                            radius = TangemTheme.dimens.radius12,
+                            addDefaultPadding =
+                            false,
                         )
                         .background(TangemTheme.colors.background.action),
+                    selectorContent = { checked, _, _ ->
+                        AnimatedVisibility(
+                            modifier = Modifier,
+                            visible = checked,
+                        ) {
+                            Icon(
+                                painter = rememberVectorPainter(
+                                    image = ImageVector.vectorResource(id = R.drawable.ic_check_24),
+                                ),
+                                tint = TangemTheme.colors.icon.accent,
+                                contentDescription = null,
+                            )
+                        }
+                    },
                 )
             }
         }
