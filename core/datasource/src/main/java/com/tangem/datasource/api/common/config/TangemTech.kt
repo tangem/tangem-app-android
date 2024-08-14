9c6a1747895f9dac5bbd29dcb3a293162ee97826
Tangem <git@tangem.com>
2024-08-14 12:15:55 +0300

    Updated on 2026-02-02

--- core/datasource/src/main/java/com/tangem/datasource/api/common/config/TangemTech.kt
+++ core/datasource/src/main/java/com/tangem/datasource/api/common/config/TangemTech.kt
@@ -5,8 +5,20 @@ internal class TangemTech : ApiConfig() {
 
     override val defaultEnvironment: ApiEnvironment = ApiEnvironment.PROD
 
-    override val environments: Map<ApiEnvironment, String> = mapOf(
-        ApiEnvironment.DEV to "https://devapi.tangem-tech.com/v1/",
-        ApiEnvironment.PROD to "https://api.tangem-tech.com/v1/",
+    override val environmentConfigs = listOf(
+        createDevEnvironment(),
+        createProdEnvironment(),
+    )
+
+    private fun createProdEnvironment(): ApiEnvironmentConfig = ApiEnvironmentConfig(
+        environment = ApiEnvironment.PROD,
+        baseUrl = "https://api.tangem-tech.com/v1/",
+        headers = emptyMap(), // TODO: [REDACTED_JIRA]
+    )
+
+    private fun createDevEnvironment(): ApiEnvironmentConfig = ApiEnvironmentConfig(
+        environment = ApiEnvironment.DEV,
+        baseUrl = "https://devapi.tangem-tech.com/v1/",
+        headers = emptyMap(), // TODO: [REDACTED_JIRA]
     )
 }
