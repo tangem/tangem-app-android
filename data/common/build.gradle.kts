f3d5dd5e4ca49e2a796dd265db2b1a92ecdd9da6
Tangem <git@tangem.com>
2024-08-14 10:49:59 +0400

    Updated on 2026-09-01

--- data/common/build.gradle.kts
+++ data/common/build.gradle.kts
@@ -10,13 +10,26 @@ android {
 }
 
 dependencies {
+    /* Core */
     implementation(projects.core.datasource)
 
+    /* Domain */
+    implementation(projects.domain.models)
+    implementation(projects.domain.legacy)
+    implementation(projects.domain.tokens.models)
+
+    /* Libs - SDK */
+    implementation(deps.tangem.blockchain)
+    implementation(deps.tangem.card.core)
+    implementation(projects.libs.blockchainSdk)
+
+    /* DI */
+    implementation(deps.hilt.android)
+    kapt(deps.hilt.kapt)
+
+    /* Libs - Other */
     implementation(deps.kotlin.coroutines)
     implementation(deps.jodatime)
     implementation(deps.timber)
     implementation(deps.arrow.core)
-
-    implementation(deps.hilt.android)
-    kapt(deps.hilt.kapt)
 }
