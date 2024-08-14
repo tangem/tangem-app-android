1d7416c58f020401993aee9ec5f7891299ece3c7
Tangem <git@tangem.com>
2024-08-14 13:37:27 +0300

    Updated on 2026-08-14

--- core/datasource/src/main/java/com/tangem/datasource/api/common/config/TangemTech.kt
+++ core/datasource/src/main/java/com/tangem/datasource/api/common/config/TangemTech.kt
@@ -1,7 +1,12 @@
 package com.tangem.datasource.api.common.config
 
+import com.tangem.datasource.utils.RequestHeader
+import com.tangem.utils.version.AppVersionProvider
+
 /** TangemTech [ApiConfig] */
-internal class TangemTech : ApiConfig() {
+internal class TangemTech(
+    private val appVersionProvider: AppVersionProvider,
+) : ApiConfig() {
 
     override val defaultEnvironment: ApiEnvironment = ApiEnvironment.PROD
 
@@ -13,12 +18,16 @@ internal class TangemTech : ApiConfig() {
     private fun createProdEnvironment(): ApiEnvironmentConfig = ApiEnvironmentConfig(
         environment = ApiEnvironment.PROD,
         baseUrl = "https://api.tangem-tech.com/v1/",
-        headers = emptyMap(), // TODO: [REDACTED_JIRA]
+        headers = createHeaders(),
     )
 
     private fun createDevEnvironment(): ApiEnvironmentConfig = ApiEnvironmentConfig(
         environment = ApiEnvironment.DEV,
         baseUrl = "https://devapi.tangem-tech.com/v1/",
-        headers = emptyMap(), // TODO: [REDACTED_JIRA]
+        headers = createHeaders(),
     )
+
+    private fun createHeaders() = buildMap {
+        putAll(from = RequestHeader.AppVersionPlatformHeaders(appVersionProvider).values)
+    }
 }
