e2144f186e5929f1f357fcf76ec39bec2ff99fdf
Tangem <git@tangem.com>
2024-08-15 20:29:25 +0500

    Updated on 2026-09-25

--- core/datasource/src/main/java/com/tangem/datasource/local/token/StakingBalanceStore.kt
+++ core/datasource/src/main/java/com/tangem/datasource/local/token/StakingBalanceStore.kt
@@ -2,19 +2,20 @@ package com.tangem.datasource.local.token
 
 import com.tangem.datasource.api.stakekit.models.response.model.BalanceDTO
 import com.tangem.datasource.api.stakekit.models.response.model.YieldBalanceWrapperDTO
+import com.tangem.domain.wallets.models.UserWalletId
 import kotlinx.coroutines.flow.Flow
 
 interface StakingBalanceStore {
 
-    fun get(): Flow<List<YieldBalanceWrapperDTO>>
+    fun get(userWalletId: UserWalletId): Flow<Set<YieldBalanceWrapperDTO>>
 
-    suspend fun getSyncOrNull(): List<YieldBalanceWrapperDTO>?
+    suspend fun getSyncOrNull(userWalletId: UserWalletId): Set<YieldBalanceWrapperDTO>?
 
-    suspend fun store(items: List<YieldBalanceWrapperDTO>)
+    suspend fun store(userWalletId: UserWalletId, items: Set<YieldBalanceWrapperDTO>)
 
-    fun get(integrationId: String): Flow<List<BalanceDTO>>
+    fun get(userWalletId: UserWalletId, integrationId: String): Flow<List<BalanceDTO>>
 
-    suspend fun getSyncOrNull(integrationId: String): List<BalanceDTO>?
+    suspend fun getSyncOrNull(userWalletId: UserWalletId, integrationId: String): List<BalanceDTO>?
 
-    suspend fun store(integrationId: String, item: YieldBalanceWrapperDTO)
+    suspend fun store(userWalletId: UserWalletId, integrationId: String, item: YieldBalanceWrapperDTO)
 }
