0fdb8772b1b4bed348134474ab46793050cb9664
Tangem <git@tangem.com>
2024-08-15 11:02:29 +0500

    release/5.26.1

---
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/transformers/approval/SetApprovalInProgressTransformer.kt
@@ -0,0 +1,31 @@
+package com.tangem.features.staking.impl.presentation.state.transformers.approval
+
+import com.tangem.core.ui.extensions.resourceReference
+import com.tangem.features.staking.impl.R
+import com.tangem.features.staking.impl.presentation.state.StakingNotification
+import com.tangem.features.staking.impl.presentation.state.StakingStates
+import com.tangem.features.staking.impl.presentation.state.StakingUiState
+import com.tangem.utils.transformer.Transformer
+import kotlinx.collections.immutable.toPersistentList
+
+internal object SetApprovalInProgressTransformer : Transformer<StakingUiState> {
+    override fun transform(prevState: StakingUiState): StakingUiState {
+        val state = prevState.confirmationState as? StakingStates.ConfirmationState.Data
+        val notifications = state?.notifications?.toMutableList() ?: mutableListOf()
+// [REDACTED_TODO_COMMENT]
+        notifications.add(
+            StakingNotification.Warning.TransactionInProgress(
+                title = resourceReference(R.string.warning_express_approval_in_progress_title),
+                description = resourceReference(R.string.warning_express_approval_in_progress_message),
+            ),
+        )
+
+        val updatedConfirmationState = state?.copy(
+            notifications = notifications.toPersistentList(),
+        ) ?: prevState.confirmationState
+
+        return prevState.copy(
+            confirmationState = updatedConfirmationState,
+        )
+    }
+}
