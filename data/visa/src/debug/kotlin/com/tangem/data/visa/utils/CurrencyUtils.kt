d464ec29e8eca8e44bd5ddcaf81f7bcb742bd418
Tangem <git@tangem.com>
2024-08-15 15:06:34 +0300

    Updated on 2026-06-12

--- data/visa/src/debug/kotlin/com/tangem/data/visa/utils/CurrencyUtils.kt
+++ data/visa/src/debug/kotlin/com/tangem/data/visa/utils/CurrencyUtils.kt
@@ -1,15 +1,7 @@
 package com.tangem.data.visa.utils
 
-import android.os.Build
-import timber.log.Timber
 import java.util.Currency
 
-internal fun findCurrencyByNumericCode(code: Int): Currency {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-        Currency.getAvailableCurrencies().firstOrNull { it.numericCode == code }
-            ?: Currency.getInstance(VisaConstants.fiatCurrency.code)
-    } else {
-        Timber.w("Unable to get currency by numeric code on API level ${Build.VERSION.SDK_INT}")
-        Currency.getInstance(VisaConstants.fiatCurrency.code)
-    }
-}
+internal fun findCurrencyByNumericCode(code: Int) =
+    Currency.getAvailableCurrencies().firstOrNull { it.numericCode == code }
+        ?: Currency.getInstance(VisaConstants.fiatCurrency.code)
