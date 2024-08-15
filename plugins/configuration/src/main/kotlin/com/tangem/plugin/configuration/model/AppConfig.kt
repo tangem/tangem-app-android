08b1c4b0aa8bfb9acde10226e02ff7fecbdd567e
Tangem <git@tangem.com>
2024-08-15 15:06:34 +0300

    Updated on 2025-12-30

--- plugins/configuration/src/main/kotlin/com/tangem/plugin/configuration/model/AppConfig.kt
+++ plugins/configuration/src/main/kotlin/com/tangem/plugin/configuration/model/AppConfig.kt
@@ -4,7 +4,7 @@ internal object AppConfig {
     const val packageName = "com.tangem.wallet"
     const val versionCode = 1
     const val versionName = "1.0.0-SNAPSHOT"
-    const val minSdkVersion = 23
+    const val minSdkVersion = 24
     const val targetSdkVersion = 34
     const val compileSdkVersion = 34
 }
