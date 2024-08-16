579346a5b2fb6e73ceded134e977a7ceda902011
Tangem <git@tangem.com>
2024-08-16 18:00:50 +0400

    Updated on 2026-09-21

--- features/manage-tokens/impl/src/main/kotlin/com/tangem/features/managetokens/entity/CurrencyItemUM.kt
+++ features/manage-tokens/impl/src/main/kotlin/com/tangem/features/managetokens/entity/CurrencyItemUM.kt
@@ -1,18 +1,23 @@
 package com.tangem.features.managetokens.entity
 
 import androidx.compose.runtime.Immutable
-import com.tangem.core.ui.components.rows.model.ChainRowUM
+import com.tangem.core.ui.components.currency.icon.CurrencyIconState
+import com.tangem.domain.managetokens.model.ManagedCryptoCurrency
 import kotlinx.collections.immutable.ImmutableList
 
 @Immutable
 internal sealed class CurrencyItemUM {
 
-    abstract val id: String
-    abstract val model: ChainRowUM
+    abstract val id: ManagedCryptoCurrency.ID
+    abstract val name: String
+    abstract val symbol: String
+    abstract val icon: CurrencyIconState
 
     data class Basic(
-        override val id: String,
-        override val model: ChainRowUM,
+        override val id: ManagedCryptoCurrency.ID,
+        override val name: String,
+        override val symbol: String,
+        override val icon: CurrencyIconState,
         val networks: NetworksUM,
         val onExpandClick: () -> Unit,
     ) : CurrencyItemUM() {
@@ -29,8 +34,10 @@ internal sealed class CurrencyItemUM {
     }
 
     data class Custom(
-        override val id: String,
-        override val model: ChainRowUM,
+        override val id: ManagedCryptoCurrency.ID,
+        override val name: String,
+        override val symbol: String,
+        override val icon: CurrencyIconState,
         val onRemoveClick: () -> Unit,
     ) : CurrencyItemUM()
 }
