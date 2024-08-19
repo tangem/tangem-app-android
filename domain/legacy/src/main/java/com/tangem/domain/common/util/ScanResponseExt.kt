e87561cfc4f69152c5b280aae7c92042a9cf18aa
Tangem <git@tangem.com>
2024-08-19 18:09:38 +0400

    release/5.26.1

--- domain/legacy/src/main/java/com/tangem/domain/common/util/ScanResponseExt.kt
+++ domain/legacy/src/main/java/com/tangem/domain/common/util/ScanResponseExt.kt
@@ -12,6 +12,7 @@ import com.tangem.domain.common.TapWorkarounds.isTangemTwins
 import com.tangem.domain.common.TapWorkarounds.isTestCard
 import com.tangem.domain.common.configs.CardConfig
 import com.tangem.domain.common.configs.Wallet2CardConfig
+import com.tangem.domain.models.scan.CardDTO
 import com.tangem.domain.models.scan.ScanResponse
 import com.tangem.domain.wallets.models.UserWallet
 
@@ -32,8 +33,6 @@ val UserWallet.cardTypesResolver: CardTypesResolver
     get() = scanResponse.cardTypesResolver
 
 fun ScanResponse.twinsIsTwinned(): Boolean = card.isTangemTwins && walletData != null && secondTwinPublicKey != null
-fun ScanResponse.supportsHdWallet(): Boolean = card.settings.isHDWalletAllowed
-fun ScanResponse.supportsBackup(): Boolean = card.settings.isBackupAllowed
 
 fun ScanResponse.hasDerivation(blockchain: Blockchain, rawDerivationPath: String): Boolean {
     return hasDerivation(blockchain, DerivationPath(rawDerivationPath))
@@ -66,4 +65,37 @@ private fun ScanResponse.hasDerivation(curve: EllipticCurve, derivationPath: Der
     val extendedPublicKeysMap = derivedKeys[foundWallet.publicKey.toMapKey()] ?: return false
     val extendedPublicKey = extendedPublicKeysMap[derivationPath]
     return extendedPublicKey != null
+}
+
+/**
+ * Get total cards count in wallets set for this [ScanResponse] card
+ *
+ * @return null if wallet is not multi-currency or total cards count
+ */
+fun ScanResponse.getCardsCount(): Int? {
+    if (!cardTypesResolver.isMultiwalletAllowed()) return null
+
+    return when (val status = card.backupStatus) {
+        is CardDTO.BackupStatus.Active -> status.cardCount + 1
+        is CardDTO.BackupStatus.NoBackup,
+        is CardDTO.BackupStatus.CardLinked,
+        null, // Multi-currency wallet without backup function. Example, 4.12
+        -> 1
+    }
+}
+
+/**
+ * Get backup cards count for this [ScanResponse] card
+ *
+ * @return null if wallet is not multi-currency or total cards count
+ */
+fun ScanResponse.getBackupCardsCount(): Int? {
+    return if (cardTypesResolver.isMultiwalletAllowed()) {
+        when (val status = card.backupStatus) {
+            is CardDTO.BackupStatus.Active -> status.cardCount
+            else -> 0
+        }
+    } else {
+        null
+    }
 }
