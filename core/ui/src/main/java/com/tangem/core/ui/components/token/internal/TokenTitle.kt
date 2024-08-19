62f567a28ff20c06b328cfdc812ca612af6d9a0f
Tangem <git@tangem.com>
2024-08-19 22:22:48 +0400

    Updated on 2026-06-12

--- core/ui/src/main/java/com/tangem/core/ui/components/token/internal/TokenTitle.kt
+++ core/ui/src/main/java/com/tangem/core/ui/components/token/internal/TokenTitle.kt
@@ -1,4 +1,4 @@
-package com.tangem.feature.wallet.presentation.common.component.token
+package com.tangem.core.ui.components.token.internal
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
@@ -13,10 +13,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
+import com.tangem.core.ui.R
 import com.tangem.core.ui.components.RectangleShimmer
 import com.tangem.core.ui.res.TangemTheme
-import com.tangem.feature.wallet.impl.R
-import com.tangem.feature.wallet.presentation.common.state.TokenItemState.TitleState as TokenTitleState
+import com.tangem.core.ui.components.token.state.TokenItemState.TitleState as TokenTitleState
 
 @Composable
 internal fun TokenTitle(state: TokenTitleState?, modifier: Modifier = Modifier) {
