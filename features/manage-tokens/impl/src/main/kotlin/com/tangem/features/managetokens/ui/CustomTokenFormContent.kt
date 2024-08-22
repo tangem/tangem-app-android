092972a321b8b1757715f8681e816bb3df1b8b2c
Tangem <git@tangem.com>
2024-08-22 16:01:36 +0400

    Updated on 2025-09-25

--- features/manage-tokens/impl/src/main/kotlin/com/tangem/features/managetokens/ui/CustomTokenFormContent.kt
+++ features/manage-tokens/impl/src/main/kotlin/com/tangem/features/managetokens/ui/CustomTokenFormContent.kt
@@ -2,97 +2,152 @@ package com.tangem.features.managetokens.ui
 
 import android.content.res.Configuration
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListScope
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.util.fastForEach
+import com.tangem.core.ui.components.PrimaryButton
 import com.tangem.core.ui.components.block.information.InformationBlock
 import com.tangem.core.ui.components.fields.SimpleTextField
+import com.tangem.core.ui.components.isOpened
+import com.tangem.core.ui.components.keyboardAsState
 import com.tangem.core.ui.components.notifications.Notification
 import com.tangem.core.ui.extensions.resolveReference
 import com.tangem.core.ui.extensions.stringReference
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.core.ui.res.TangemThemePreview
 import com.tangem.features.managetokens.component.preview.PreviewCustomTokenFormComponent
-import com.tangem.features.managetokens.entity.ClickableFieldUM
-import com.tangem.features.managetokens.entity.CustomTokenFormUM
-import com.tangem.features.managetokens.entity.TextInputFieldUM
+import com.tangem.features.managetokens.entity.customtoken.ClickableFieldUM
+import com.tangem.features.managetokens.entity.customtoken.CustomTokenFormUM
+import com.tangem.features.managetokens.entity.customtoken.TextInputFieldUM
+import com.tangem.features.managetokens.impl.R
+import com.tangem.features.managetokens.ui.component.AddCustomTokenDescription
 
-internal fun LazyListScope.customTokenFormContent(model: CustomTokenFormUM) {
-    item {
-        ClickableField(
-            modifier = Modifier.padding(bottom = TangemTheme.dimens.spacing12),
-            model = model.networkName,
-        )
-    }
+@Composable
+internal fun CustomTokenFormContent(model: CustomTokenFormUM, modifier: Modifier = Modifier) {
+    val keyboard by keyboardAsState()
+    val bottomBarHeight by animateDpAsState(
+        label = "Bottom bar height",
+        targetValue = if (keyboard.isOpened) {
+            TangemTheme.dimens.spacing0
+        } else {
+            with(LocalDensity.current) {
+                WindowInsets.systemBars.getBottom(density = this).toDp()
+            }
+        },
+    )
 
-    item {
+    Box(
+        modifier = modifier
+            .imePadding()
+            .fillMaxSize()
+            .background(color = TangemTheme.colors.background.secondary),
+    ) {
+        val scrollState = rememberScrollState()
+
         Column(
             modifier = Modifier
-                .padding(bottom = TangemTheme.dimens.spacing12)
-                .background(
-                    color = TangemTheme.colors.background.action,
-                    shape = TangemTheme.shapes.roundedCornersXMedium,
-                ),
+                .verticalScroll(scrollState)
+                .fillMaxSize()
+                .padding(bottom = TangemTheme.dimens.spacing76),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(TangemTheme.dimens.spacing16),
         ) {
-            TextField(
-                model = model.contractAddress,
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    imeAction = ImeAction.Next,
-                ),
-            )
-            TextField(
-                model = model.tokenName,
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    imeAction = ImeAction.Next,
-                ),
-            )
-            TextField(
-                model = model.tokenSymbol,
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    imeAction = ImeAction.Next,
-                ),
-            )
-            TextField(
-                model = model.tokenDecimals,
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    keyboardType = KeyboardType.Decimal,
-                    imeAction = ImeAction.Next,
-                ),
+            AddCustomTokenDescription()
+            FormContent(model)
+        }
+
+        PrimaryButton(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = TangemTheme.dimens.spacing16 + bottomBarHeight)
+                .fillMaxWidth(),
+            text = stringResource(id = R.string.custom_token_add_token),
+            enabled = model.canAddToken,
+            onClick = model.saveToken,
+        )
+    }
+}
+
+@Composable
+private fun FormContent(model: CustomTokenFormUM, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(TangemTheme.dimens.spacing12),
+    ) {
+        ClickableField(
+            model = model.networkName,
+        )
+
+        val tokenForm = model.tokenForm
+        if (tokenForm != null) {
+            TokenForm(tokenForm)
+        }
+
+        ClickableField(
+            model = model.derivationPath,
+        )
+
+        model.notifications.fastForEach { notification ->
+            Notification(
+                config = notification.config,
+                containerColor = TangemTheme.colors.button.disabled,
             )
         }
     }
+}
 
-    item {
-        ClickableField(
-            modifier = Modifier.padding(bottom = TangemTheme.dimens.spacing12),
-            model = model.derivationPath,
+@Composable
+private fun TokenForm(tokenForm: CustomTokenFormUM.TokenFormUM, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .background(
+                color = TangemTheme.colors.background.action,
+                shape = TangemTheme.shapes.roundedCornersXMedium,
+            ),
+    ) {
+        TextField(
+            model = tokenForm.contractAddress,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Next,
+            ),
         )
-    }
-
-    items(
-        items = model.notifications,
-        key = { it.id },
-    ) { notification ->
-        Notification(
-            modifier = Modifier.padding(bottom = TangemTheme.dimens.spacing12),
-            config = notification.config,
-            containerColor = TangemTheme.colors.button.disabled,
+        TextField(
+            model = tokenForm.name,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Next,
+            ),
+        )
+        TextField(
+            model = tokenForm.symbol,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Next,
+            ),
+        )
+        TextField(
+            model = tokenForm.decimals,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Decimal,
+                imeAction = ImeAction.Next,
+            ),
         )
     }
 }
@@ -124,7 +179,9 @@ private fun TextField(
         },
         content = {
             SimpleTextField(
-                modifier = Modifier.padding(bottom = TangemTheme.dimens.spacing12),
+                modifier = Modifier
+                    .padding(bottom = TangemTheme.dimens.spacing12)
+                    .fillMaxWidth(),
                 value = model.value,
                 onValueChange = model.onValueChange,
                 readOnly = false,
@@ -170,9 +227,7 @@ private fun Preview_CustomTokenFormContent(
     component: PreviewCustomTokenFormComponent,
 ) {
     TangemThemePreview {
-        LazyColumn(
-            modifier = Modifier.background(color = TangemTheme.colors.background.secondary),
-        ) { component.content(scope = this) }
+        component.Content(modifier = Modifier)
     }
 }
 
@@ -183,13 +238,18 @@ private class PreviewCustomTokenFormComponentProvider :
         get() = sequenceOf(
             PreviewCustomTokenFormComponent(),
             PreviewCustomTokenFormComponent(
-                contractAddress = TextInputFieldUM(
-                    label = stringReference("Contract address"),
-                    value = "0x1234567890",
-                    error = stringReference("Contract address is invalid"),
-                    placeholder = stringReference("0x1234567890"),
-                    onValueChange = {},
+                tokenForm = PreviewCustomTokenFormComponent.tokenForm.copy(
+                    contractAddress = TextInputFieldUM(
+                        label = stringReference("Contract address"),
+                        value = "0x1234567890",
+                        error = stringReference("Contract address is invalid"),
+                        placeholder = stringReference("0x1234567890"),
+                        onValueChange = {},
+                    ),
                 ),
+            ),
+            PreviewCustomTokenFormComponent(
+                tokenForm = null,
             ),
         )
 }
