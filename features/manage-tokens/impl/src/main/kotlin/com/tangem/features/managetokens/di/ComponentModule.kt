641272c3b7c5a2be8dc76b7d4e65821c82e14479
Tangem <git@tangem.com>
2024-08-22 17:31:08 +0400

    Updated on 2026-09-25

--- features/manage-tokens/impl/src/main/kotlin/com/tangem/features/managetokens/di/ComponentModule.kt
+++ features/manage-tokens/impl/src/main/kotlin/com/tangem/features/managetokens/di/ComponentModule.kt
@@ -1,6 +1,12 @@
 package com.tangem.features.managetokens.di
 
+import com.tangem.features.managetokens.component.AddCustomTokenComponent
+import com.tangem.features.managetokens.component.CustomTokenFormComponent
+import com.tangem.features.managetokens.component.CustomTokenSelectorComponent
 import com.tangem.features.managetokens.component.ManageTokensComponent
+import com.tangem.features.managetokens.component.impl.DefaultAddCustomTokenComponent
+import com.tangem.features.managetokens.component.impl.DefaultCustomTokenFormComponent
+import com.tangem.features.managetokens.component.impl.DefaultCustomTokenSelectorComponent
 import com.tangem.features.managetokens.component.impl.DefaultManageTokensComponent
 import dagger.Binds
 import dagger.Module
@@ -15,4 +21,22 @@ internal interface ComponentModule {
     @Binds
     @Singleton
     fun bindManageTokensComponentFactory(factory: DefaultManageTokensComponent.Factory): ManageTokensComponent.Factory
+
+    @Binds
+    @Singleton
+    fun bindAddCustomTokenComponentFactory(
+        factory: DefaultAddCustomTokenComponent.Factory,
+    ): AddCustomTokenComponent.Factory
+
+    @Binds
+    @Singleton
+    fun bindCustomTokenSelectorComponentFactory(
+        factory: DefaultCustomTokenSelectorComponent.Factory,
+    ): CustomTokenSelectorComponent.Factory
+
+    @Binds
+    @Singleton
+    fun bindCustomTokenFormComponentFactory(
+        factory: DefaultCustomTokenFormComponent.Factory,
+    ): CustomTokenFormComponent.Factory
 }
