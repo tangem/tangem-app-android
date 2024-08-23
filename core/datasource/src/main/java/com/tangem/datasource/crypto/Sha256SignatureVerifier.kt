cb9c30565f7a8bcd8618a0ac390c25b8ccf047bb
Vlad Kozarez <vladks32@gmail.com>
2024-08-23 10:59:48 +0300

    AND-8053 fixed express signature verification

--- core/datasource/src/main/java/com/tangem/datasource/crypto/Sha256SignatureVerifier.kt
+++ core/datasource/src/main/java/com/tangem/datasource/crypto/Sha256SignatureVerifier.kt
@@ -2,16 +2,30 @@ package com.tangem.datasource.crypto
 
 import com.tangem.common.extensions.hexToBytes
 import com.tangem.crypto.CryptoUtils
+import com.tangem.datasource.api.common.config.ApiConfig
+import com.tangem.datasource.api.common.config.ApiEnvironment
+import com.tangem.datasource.api.common.config.managers.ApiConfigsManager
 import com.tangem.datasource.config.ConfigManager
 
-internal class Sha256SignatureVerifier(private val configManager: ConfigManager) : DataSignatureVerifier {
+internal class Sha256SignatureVerifier(
+    private val configManager: ConfigManager,
+    private val apiConfigsManager: ApiConfigsManager,
+) : DataSignatureVerifier {
 
     override fun verifySignature(signature: String, data: String): Boolean {
-        val pubKey = configManager.config.express?.signVerifierPublicKey ?: return false
+        val pubKey = getPubKey() ?: return false
         return CryptoUtils.verify(
             publicKey = pubKey.hexToBytes().takeLast(n = 65).toByteArray(),
             message = data.toByteArray(),
             signature = signature.hexToBytes(),
         )
+    }
+
+    private fun getPubKey(): String? {
+        val expressConfig = apiConfigsManager.getEnvironmentConfig(ApiConfig.ID.Express)
+        return when (expressConfig.environment) {
+            ApiEnvironment.PROD -> configManager.config.express?.signVerifierPublicKey
+            else -> configManager.config.devExpress?.signVerifierPublicKey
+        }
     }
 }
