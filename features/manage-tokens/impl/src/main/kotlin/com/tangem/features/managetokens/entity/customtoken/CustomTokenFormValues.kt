164190ae9438aeb29fdbc87aa3abc5e803277a7e
Tangem <git@tangem.com>
2024-08-23 18:57:51 +0400

    Updated on 2025-11-26

--- features/manage-tokens/impl/src/main/kotlin/com/tangem/features/managetokens/entity/customtoken/CustomTokenFormValues.kt
+++ features/manage-tokens/impl/src/main/kotlin/com/tangem/features/managetokens/entity/customtoken/CustomTokenFormValues.kt
@@ -1,5 +1,6 @@
 package com.tangem.features.managetokens.entity.customtoken
 
+import com.tangem.domain.managetokens.model.AddCustomTokenForm
 import com.tangem.features.managetokens.entity.customtoken.CustomTokenFormUM.TokenFormUM
 import kotlinx.serialization.Serializable
 
@@ -28,4 +29,17 @@ internal value class CustomTokenFormValues private constructor(private val value
         symbol = to.symbol.copy(value = values.getOrElse(index = 2) { "" }),
         decimals = to.decimals.copy(value = values.getOrElse(index = 3) { "" }),
     )
+
+    fun toDomainModel(): AddCustomTokenForm.Raw? {
+        return if (values.isEmpty()) {
+            null
+        } else {
+            AddCustomTokenForm.Raw(
+                contractAddress = values.getOrElse(index = 0) { "" },
+                name = values.getOrElse(index = 1) { "" },
+                symbol = values.getOrElse(index = 2) { "" },
+                decimals = values.getOrElse(index = 3) { "" },
+            )
+        }
+    }
 }
