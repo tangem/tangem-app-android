c72a5dd29227b42fd8a0360b7988103a7b6cba52
Tangem <git@tangem.com>
2024-08-26 22:26:45 +0300

    Updated on 2026-05-04

--- features/markets/impl/src/main/kotlin/com/tangem/features/markets/portfolio/impl/ui/preview/PreviewAddToPortfolioBSContentProvider.kt
+++ features/markets/impl/src/main/kotlin/com/tangem/features/markets/portfolio/impl/ui/preview/PreviewAddToPortfolioBSContentProvider.kt
@@ -2,6 +2,8 @@ package com.tangem.features.markets.portfolio.impl.ui.preview
 
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import com.tangem.common.ui.userwallet.state.UserWalletItemUM
+import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfig
+import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfigContent
 import com.tangem.core.ui.components.rows.model.BlockchainRowUM
 import com.tangem.core.ui.extensions.stringReference
 import com.tangem.domain.wallets.models.UserWalletId
@@ -20,18 +22,20 @@ internal class PreviewAddToPortfolioBSContentProvider : PreviewParameterProvider
         isSelected = false,
     )
 
+    val userWallet = UserWalletItemUM(
+        id = UserWalletId("1"),
+        name = stringReference("Wallet 1"),
+        information = stringReference("3 cards, 10,123$"),
+        imageUrl = "",
+        isEnabled = true,
+        endIcon = UserWalletItemUM.EndIcon.Arrow,
+        onClick = {},
+    )
+
     override val values: Sequence<AddToPortfolioBSContentUM>
         get() = sequenceOf(
             AddToPortfolioBSContentUM(
-                selectedWallet = UserWalletItemUM(
-                    id = UserWalletId("1"),
-                    name = stringReference("Wallet 1"),
-                    information = stringReference("3 cards, 10,123$"),
-                    imageUrl = "",
-                    isEnabled = true,
-                    endIcon = UserWalletItemUM.EndIcon.Arrow,
-                    onClick = {},
-                ),
+                selectedWallet = userWallet,
                 selectNetworkUM = SelectNetworkUM(
                     tokenId = "etherium",
                     tokenName = "Etherium",
@@ -51,17 +55,15 @@ internal class PreviewAddToPortfolioBSContentProvider : PreviewParameterProvider
                 isScanCardNotificationVisible = true,
                 continueButtonEnabled = true,
                 onContinueButtonClick = {},
+                walletSelectorConfig = TangemBottomSheetConfig(
+                    isShow = false,
+                    onDismissRequest = {},
+                    content = TangemBottomSheetConfigContent.Empty,
+                ),
+                onWalletClick = {},
             ),
             AddToPortfolioBSContentUM(
-                selectedWallet = UserWalletItemUM(
-                    id = UserWalletId("1"),
-                    name = stringReference("Wallet 1"),
-                    information = stringReference("3 cards, 10,123$"),
-                    imageUrl = "",
-                    isEnabled = true,
-                    endIcon = UserWalletItemUM.EndIcon.Arrow,
-                    onClick = {},
-                ),
+                selectedWallet = userWallet,
                 selectNetworkUM = SelectNetworkUM(
                     tokenId = "etherium",
                     tokenName = "Etherium",
@@ -81,6 +83,12 @@ internal class PreviewAddToPortfolioBSContentProvider : PreviewParameterProvider
                 isScanCardNotificationVisible = true,
                 continueButtonEnabled = false,
                 onContinueButtonClick = {},
+                walletSelectorConfig = TangemBottomSheetConfig(
+                    isShow = false,
+                    onDismissRequest = {},
+                    content = TangemBottomSheetConfigContent.Empty,
+                ),
+                onWalletClick = {},
             ),
         )
 }
