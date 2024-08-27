e967bdda93a39e0db21aa1e91113a20b0ac0b967
Tangem <git@tangem.com>
2024-08-27 10:06:13 +0400

    Updated on 2026-07-07

--- features/markets/impl/src/main/kotlin/com/tangem/features/markets/portfolio/impl/ui/MyPortfolio.kt
+++ features/markets/impl/src/main/kotlin/com/tangem/features/markets/portfolio/impl/ui/MyPortfolio.kt
@@ -11,7 +11,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.fastForEachIndexed
 import com.tangem.core.ui.components.PrimaryButton
 import com.tangem.core.ui.components.SmallButtonShimmer
@@ -26,51 +25,68 @@ import com.tangem.core.ui.res.TangemThemePreview
 import com.tangem.features.markets.impl.R
 import com.tangem.features.markets.portfolio.impl.ui.preview.PreviewMyPortfolioUMProvider
 import com.tangem.features.markets.portfolio.impl.ui.state.MyPortfolioUM
+import com.tangem.features.markets.portfolio.impl.ui.state.MyPortfolioUM.Tokens.AddButtonState
 
 @Composable
 internal fun MyPortfolio(state: MyPortfolioUM, modifier: Modifier = Modifier) {
     InformationBlock(
         modifier = modifier,
-        contentHorizontalPadding = 0.dp,
-        title = {
-            Text(
-                text = stringResource(R.string.markets_common_my_portfolio),
-                style = TangemTheme.typography.subtitle2,
-                color = TangemTheme.colors.text.tertiary,
-            )
-        },
+        contentHorizontalPadding = TangemTheme.dimens.spacing0,
+        title = { Title() },
         action = {
             if (state !is MyPortfolioUM.Tokens) return@InformationBlock
 
-            when (state.buttonState) {
-                MyPortfolioUM.Tokens.AddButtonState.Loading -> {
-                    SmallButtonShimmer(
-                        modifier = Modifier.size(width = 63.dp, height = TangemTheme.dimens.size18),
-                        shape = RoundedCornerShape(TangemTheme.dimens.radius3),
-                    )
-                }
-                else -> {
-                    SecondarySmallButton(
-                        config = SmallButtonConfig(
-                            text = resourceReference(R.string.markets_add_token),
-                            icon = TangemButtonIconPosition.Start(R.drawable.ic_plus_24),
-                            onClick = state.onAddClick,
-                            enabled = state.buttonState == MyPortfolioUM.Tokens.AddButtonState.Available,
-                        ),
-                    )
-                }
-            }
+            AddButton(state = state.buttonState, onClick = state.onAddClick)
         },
     ) {
+        val contentModifier = Modifier.padding(
+            start = TangemTheme.dimens.spacing12,
+            end = TangemTheme.dimens.spacing12,
+            bottom = TangemTheme.dimens.spacing12,
+        )
+
         when (state) {
             is MyPortfolioUM.Tokens -> TokenList(state = state)
-            is MyPortfolioUM.AddFirstToken -> AddFirstTokenContent(state = state)
-            MyPortfolioUM.Loading -> LoadingPlaceholder()
-            MyPortfolioUM.Unavailable -> UnavailableContent()
+            is MyPortfolioUM.AddFirstToken -> AddFirstTokenContent(state = state, modifier = contentModifier)
+            MyPortfolioUM.Loading -> LoadingPlaceholder(modifier = contentModifier)
+            MyPortfolioUM.Unavailable -> UnavailableContent(modifier = contentModifier)
         }
     }
 
-    AddToPortfolioBottomSheet(state.addToPortfolioBS)
+    AddToPortfolioBottomSheet(config = state.addToPortfolioBS)
+}
+
+@Composable
+private fun Title() {
+    Text(
+        text = stringResource(R.string.markets_common_my_portfolio),
+        style = TangemTheme.typography.subtitle2,
+        color = TangemTheme.colors.text.tertiary,
+    )
+}
+
+@Composable
+private fun AddButton(state: AddButtonState, onClick: () -> Unit) {
+    when (state) {
+        AddButtonState.Loading -> {
+            SmallButtonShimmer(
+                modifier = Modifier.size(width = TangemTheme.dimens.size63, height = TangemTheme.dimens.size18),
+                shape = RoundedCornerShape(TangemTheme.dimens.radius3),
+            )
+        }
+        AddButtonState.Available,
+        AddButtonState.Unavailable,
+        -> {
+            SecondarySmallButton(
+                config = SmallButtonConfig(
+                    text = resourceReference(R.string.markets_add_token),
+                    icon = TangemButtonIconPosition.Start(R.drawable.ic_plus_24),
+                    onClick = onClick,
+                    enabled = state == AddButtonState.Available,
+                ),
+            )
+        }
+    }
 }
 
 @Composable
@@ -90,12 +106,7 @@ private fun TokenList(state: MyPortfolioUM.Tokens, modifier: Modifier = Modifier
 @Composable
 private fun UnavailableContent(modifier: Modifier = Modifier) {
     Text(
-        modifier = modifier
-            .padding(
-                start = TangemTheme.dimens.spacing12,
-                end = TangemTheme.dimens.spacing12,
-                bottom = TangemTheme.dimens.spacing12,
-            ),
+        modifier = modifier,
         text = stringResource(R.string.markets_add_to_my_portfolio_unavailable_description),
         style = TangemTheme.typography.body2,
         color = TangemTheme.colors.text.tertiary,
@@ -105,12 +116,7 @@ private fun UnavailableContent(modifier: Modifier = Modifier) {
 @Composable
 private fun AddFirstTokenContent(state: MyPortfolioUM.AddFirstToken, modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier
-            .padding(
-                start = TangemTheme.dimens.spacing12,
-                end = TangemTheme.dimens.spacing12,
-                bottom = TangemTheme.dimens.spacing12,
-            ),
+        modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(TangemTheme.dimens.spacing12),
     ) {
         Text(
@@ -118,6 +124,7 @@ private fun AddFirstTokenContent(state: MyPortfolioUM.AddFirstToken, modifier: M
             style = TangemTheme.typography.body2,
             color = TangemTheme.colors.text.tertiary,
         )
+
         PrimaryButton(
             modifier = Modifier.fillMaxWidth(),
             text = stringResource(R.string.markets_add_to_portfolio_button),
@@ -128,19 +135,13 @@ private fun AddFirstTokenContent(state: MyPortfolioUM.AddFirstToken, modifier: M
 
 @Composable
 private fun LoadingPlaceholder(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier
-            .padding(
-                start = TangemTheme.dimens.spacing12,
-                end = TangemTheme.dimens.spacing12,
-                bottom = TangemTheme.dimens.spacing12,
-            ),
-    ) {
+    Column(modifier = modifier) {
         TextShimmer(
             modifier = Modifier.fillMaxWidth(),
             style = TangemTheme.typography.body2,
             textSizeHeight = true,
         )
+
         TextShimmer(
             modifier = Modifier.fillMaxWidth(fraction = 0.7f),
             style = TangemTheme.typography.body2,
