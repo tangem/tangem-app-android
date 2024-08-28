d1720f52d5b2d4570c94b27360bb58468c78be24
Tangem <git@tangem.com>
2024-08-28 16:04:23 +0300

    Updated on 2026-01-24

--- features/markets/impl/src/main/kotlin/com/tangem/features/markets/tokenlist/impl/model/converters/MarketsTokenItemConverter.kt
+++ features/markets/impl/src/main/kotlin/com/tangem/features/markets/tokenlist/impl/model/converters/MarketsTokenItemConverter.kt
@@ -3,6 +3,7 @@ package com.tangem.features.markets.tokenlist.impl.model.converters
 import com.tangem.common.ui.charts.state.converter.PriceAndTimePointValuesConverter
 import com.tangem.common.ui.charts.state.MarketChartData
 import com.tangem.common.ui.charts.state.MarketChartRawData
+import com.tangem.common.ui.charts.state.sorted
 import com.tangem.core.ui.components.marketprice.PriceChangeType
 import com.tangem.core.ui.utils.BigDecimalFormatter
 import com.tangem.domain.appcurrency.model.AppCurrency
@@ -115,7 +116,7 @@ internal class MarketsTokenItemConverter(
                 MarketChartData.Data(
                     y = ct.priceY.toImmutableList(),
                     x = ct.timeStamps.map { it.toBigDecimal() }.toImmutableList(),
-                ),
+                ).sorted(),
             )
         }
     }
