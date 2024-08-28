b93e6a9943a4122bb9fc6d74cc4462012e83b69d
Tangem <git@tangem.com>
2024-08-28 18:08:43 +0500

    Updated on 2026-09-01

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/di/StakingRouterModule.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/di/StakingRouterModule.kt
@@ -1,8 +1,9 @@
 package com.tangem.features.staking.impl.di
 
+import com.tangem.common.routing.AppRouter
 import com.tangem.core.navigation.url.UrlOpener
-import com.tangem.features.staking.impl.navigation.DefaultStakingRouter
 import com.tangem.features.staking.api.navigation.StakingRouter
+import com.tangem.features.staking.impl.navigation.DefaultStakingRouter
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -18,9 +19,10 @@ internal object StakingRouterModule {
 
     @Provides
     @ActivityScoped
-    fun provideStakingRouter(urlOpener: UrlOpener): StakingRouter {
+    fun provideStakingRouter(urlOpener: UrlOpener, router: AppRouter): StakingRouter {
         return DefaultStakingRouter(
             urlOpener = urlOpener,
+            router = router,
         )
     }
 }
