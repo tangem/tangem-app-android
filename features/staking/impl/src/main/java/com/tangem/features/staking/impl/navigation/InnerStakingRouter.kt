daa5c1786b433a554bba497ddc269b60805d0d98
Tangem <git@tangem.com>
2024-08-28 18:08:43 +0500

    Updated on 2026-09-14

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/navigation/InnerStakingRouter.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/navigation/InnerStakingRouter.kt
@@ -1,8 +1,12 @@
 package com.tangem.features.staking.impl.navigation
 
+import com.tangem.domain.tokens.model.CryptoCurrency
+import com.tangem.domain.wallets.models.UserWalletId
 import com.tangem.features.staking.api.navigation.StakingRouter
 
 interface InnerStakingRouter : StakingRouter {
 
     fun openUrl(url: String)
+
+    fun openTokenDetails(userWalletId: UserWalletId, currency: CryptoCurrency)
 }
