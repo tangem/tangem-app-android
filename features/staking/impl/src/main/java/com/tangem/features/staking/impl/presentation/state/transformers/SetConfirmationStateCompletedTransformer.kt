9aac146b843ebe7ab34c65db65a8ba6f752387e3
Tangem <git@tangem.com>
2024-08-29 12:24:26 +0300

    Updated on 2026-08-07

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/transformers/SetConfirmationStateCompletedTransformer.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/transformers/SetConfirmationStateCompletedTransformer.kt
@@ -1,9 +1,7 @@
 package com.tangem.features.staking.impl.presentation.state.transformers
 
-import com.tangem.blockchain.common.Amount
 import com.tangem.blockchain.common.transaction.Fee
 import com.tangem.domain.appcurrency.model.AppCurrency
-import com.tangem.domain.staking.model.stakekit.transaction.StakingGasEstimate
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.features.staking.impl.presentation.state.*
 import com.tangem.features.staking.impl.presentation.state.StakingStates
@@ -15,32 +13,24 @@ import com.tangem.utils.transformer.Transformer
 internal class SetConfirmationStateCompletedTransformer(
     private val appCurrencyProvider: Provider<AppCurrency>,
     private val feeCryptoCurrencyStatus: CryptoCurrencyStatus?,
-    private val stakingGasEstimate: StakingGasEstimate,
+    private val fee: Fee,
     private val txUrl: String,
 ) : Transformer<StakingUiState> {
 
     override fun transform(prevState: StakingUiState): StakingUiState {
         return prevState.copy(
-            confirmationState = prevState.confirmationState.copyWrapped(stakingGasEstimate),
+            confirmationState = prevState.confirmationState.copyWrapped(fee),
         )
     }
 
-    private fun StakingStates.ConfirmationState.copyWrapped(
-        gasEstimate: StakingGasEstimate,
-    ): StakingStates.ConfirmationState {
+    private fun StakingStates.ConfirmationState.copyWrapped(fee: Fee): StakingStates.ConfirmationState {
         if (this is StakingStates.ConfirmationState.Data) {
             val isFeeConvertibleToFiat = feeCryptoCurrencyStatus?.currency?.network?.hasFiatFeeRate == true
             return copy(
                 isPrimaryButtonEnabled = true,
                 innerState = InnerConfirmationStakingState.COMPLETED,
                 feeState = FeeState.Content(
-                    fee = Fee.Common(
-                        Amount(
-                            currencySymbol = gasEstimate.token.symbol,
-                            value = gasEstimate.amount,
-                            decimals = gasEstimate.token.decimals,
-                        ),
-                    ),
+                    fee = fee,
                     rate = feeCryptoCurrencyStatus?.value?.fiatRate,
                     isFeeConvertibleToFiat = isFeeConvertibleToFiat,
                     appCurrency = appCurrencyProvider(),
