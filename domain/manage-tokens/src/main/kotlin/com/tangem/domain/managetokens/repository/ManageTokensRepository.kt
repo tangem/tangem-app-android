5a673713f88224751c0d2e3777454e3255e0ed63
Tangem <git@tangem.com>
2024-08-29 15:32:52 +0500

    Updated on 2026-08-19

--- domain/manage-tokens/src/main/kotlin/com/tangem/domain/managetokens/repository/ManageTokensRepository.kt
+++ domain/manage-tokens/src/main/kotlin/com/tangem/domain/managetokens/repository/ManageTokensRepository.kt
@@ -2,8 +2,17 @@ package com.tangem.domain.managetokens.repository
 
 import com.tangem.domain.managetokens.model.ManageTokensListBatchFlow
 import com.tangem.domain.managetokens.model.ManageTokensListBatchingContext
+import com.tangem.domain.managetokens.model.ManagedCryptoCurrency
+import com.tangem.domain.tokens.model.Network
+import com.tangem.domain.wallets.models.UserWalletId
 
 interface ManageTokensRepository {
 
     fun getTokenListBatchFlow(context: ManageTokensListBatchingContext, batchSize: Int): ManageTokensListBatchFlow
+
+    suspend fun saveManagedCurrencies(
+        userWalletId: UserWalletId,
+        currenciesToAdd: Map<ManagedCryptoCurrency.Token, Set<Network>>,
+        currenciesToRemove: Map<ManagedCryptoCurrency.Token, Set<Network>>,
+    )
 }
