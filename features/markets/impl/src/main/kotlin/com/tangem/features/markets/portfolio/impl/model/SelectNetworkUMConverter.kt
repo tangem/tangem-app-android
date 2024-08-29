195c9bf3b310584a8b3486035e2ed06a0fff7021
Mama1emon <andrew.khokhlove@gmail.com>
2024-08-29 15:44:47 +0400

    AND-8157 Add portfolio factories and converters for UI
    Signed-off-by: Mama1emon <andrew.khokhlove@gmail.com>

---
+++ features/markets/impl/src/main/kotlin/com/tangem/features/markets/portfolio/impl/model/SelectNetworkUMConverter.kt
@@ -0,0 +1,32 @@
+package com.tangem.features.markets.portfolio.impl.model
+
+import com.tangem.domain.markets.TokenMarketInfo
+import com.tangem.domain.markets.TokenMarketParams
+import com.tangem.features.markets.portfolio.impl.ui.state.SelectNetworkUM
+import com.tangem.utils.converter.Converter
+import kotlinx.collections.immutable.toImmutableList
+
+/**
+ * Converter from [TokenMarketParams] to [SelectNetworkUM]
+ *
+ * @property networksWithToggle   map of networks with toggles
+ * @property onNetworkSwitchClick callback is called when network switch is clicked
+ *
+ * @author Andrew Khokhlov on 28/08/2024
+ */
+internal class SelectNetworkUMConverter(
+    private val networksWithToggle: Map<TokenMarketInfo.Network, Boolean>,
+    private val onNetworkSwitchClick: (String, Boolean) -> Unit,
+) : Converter<TokenMarketParams, SelectNetworkUM> {
+
+    override fun convert(value: TokenMarketParams): SelectNetworkUM {
+        return SelectNetworkUM(
+            tokenId = value.id,
+            iconUrl = value.imageUrl,
+            tokenName = value.name,
+            tokenCurrencySymbol = value.symbol,
+            networks = BlockchainRowUMConverter.convertList(networksWithToggle.toList()).toImmutableList(),
+            onNetworkSwitchClick = { um, isChecked -> onNetworkSwitchClick(um.id, isChecked) },
+        )
+    }
+}
