bf13cd02d12d847d5574e9929a2f743044a74c2f
Tangem <git@tangem.com>
2024-08-29 18:44:22 +0500

    Updated on 2025-11-26

--- data/manage-tokens/src/main/kotlin/com/tangem/data/managetokens/utils/ManagedCryptoCurrencyFactory.kt
+++ data/manage-tokens/src/main/kotlin/com/tangem/data/managetokens/utils/ManagedCryptoCurrencyFactory.kt
@@ -77,6 +77,7 @@ internal class ManagedCryptoCurrencyFactory {
                 iconUrl = token.id?.let { getIconUrl(it, imageHost) },
                 contractAddress = contractAddress,
                 network = network,
+                decimals = token.decimals,
             )
         }
     }
