c341d94156b49cbb62af1b8503985fa7e5fdfcce
Tangem <git@tangem.com>
2024-08-30 15:24:18 +0500

    Updated on 2026-05-14

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/previewdata/InitialStakingStatePreview.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/previewdata/InitialStakingStatePreview.kt
@@ -6,7 +6,10 @@ import com.tangem.core.ui.extensions.stringReference
 import com.tangem.domain.staking.model.stakekit.BalanceType
 import com.tangem.domain.staking.model.stakekit.Yield
 import com.tangem.features.staking.impl.R
-import com.tangem.features.staking.impl.presentation.state.*
+import com.tangem.features.staking.impl.presentation.state.BalanceGroupedState
+import com.tangem.features.staking.impl.presentation.state.BalanceState
+import com.tangem.features.staking.impl.presentation.state.InnerYieldBalanceState
+import com.tangem.features.staking.impl.presentation.state.StakingStates
 import kotlinx.collections.immutable.persistentListOf
 
 internal object InitialStakingStatePreview {
@@ -87,6 +90,7 @@ internal object InitialStakingStatePreview {
                             ),
                             unbondingPeriod = stringReference("3 days"),
                             pendingActions = persistentListOf(),
+                            warmupPeriod = stringReference("3 days"),
                         ),
                     ),
                 ),
