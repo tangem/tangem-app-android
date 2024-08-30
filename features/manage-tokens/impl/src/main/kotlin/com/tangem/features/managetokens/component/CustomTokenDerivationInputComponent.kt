1d54b260d9fd8d1a6884db7d8fab8572838abe27
Tangem <git@tangem.com>
2024-08-30 12:53:14 +0400

    Updated on 2026-06-05

--- features/manage-tokens/impl/src/main/kotlin/com/tangem/features/managetokens/component/CustomTokenDerivationInputComponent.kt
+++ features/manage-tokens/impl/src/main/kotlin/com/tangem/features/managetokens/component/CustomTokenDerivationInputComponent.kt
@@ -3,12 +3,13 @@ package com.tangem.features.managetokens.component
 import com.tangem.core.decompose.factory.ComponentFactory
 import com.tangem.core.ui.decompose.ComposableDialogComponent
 import com.tangem.domain.wallets.models.UserWalletId
+import com.tangem.features.managetokens.entity.customtoken.SelectedDerivationPath
 
 internal interface CustomTokenDerivationInputComponent : ComposableDialogComponent {
 
     data class Params(
         val userWalletId: UserWalletId,
-        val onConfirm: (String) -> Unit,
+        val onConfirm: (SelectedDerivationPath) -> Unit,
         val onDismiss: () -> Unit,
     )
 
