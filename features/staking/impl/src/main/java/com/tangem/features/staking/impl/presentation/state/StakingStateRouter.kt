23371f6abc2f5c0c85686897d6422d6f17d5bfbb
Tangem <git@tangem.com>
2024-08-30 15:23:48 +0500

    release/5.26.1

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/StakingStateRouter.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/StakingStateRouter.kt
@@ -26,9 +26,7 @@ internal class StakingStateRouter(
             StakingStep.Validators,
             StakingStep.Amount,
             -> showConfirmation()
-            StakingStep.Confirmation -> {
-// [REDACTED_TODO_COMMENT]
-            }
+            StakingStep.Confirmation -> showInitial()
         }
     }
 
