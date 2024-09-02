15cffa736b1f6fad1de617f51eb4cfe619ac4a88
Tangem <git@tangem.com>
2024-09-02 13:28:13 +0500

    release/5.26.1

--- core/datasource/src/main/java/com/tangem/datasource/local/token/StakingBalanceStore.kt
+++ core/datasource/src/main/java/com/tangem/datasource/local/token/StakingBalanceStore.kt
@@ -1,6 +1,5 @@
 package com.tangem.datasource.local.token
 
-import com.tangem.datasource.api.stakekit.models.response.model.BalanceDTO
 import com.tangem.datasource.api.stakekit.models.response.model.YieldBalanceWrapperDTO
 import com.tangem.domain.wallets.models.UserWalletId
 import kotlinx.coroutines.flow.Flow
@@ -13,9 +12,9 @@ interface StakingBalanceStore {
 
     suspend fun store(userWalletId: UserWalletId, items: Set<YieldBalanceWrapperDTO>)
 
-    fun get(userWalletId: UserWalletId, integrationId: String): Flow<List<BalanceDTO>>
+    fun get(userWalletId: UserWalletId, integrationId: String): Flow<YieldBalanceWrapperDTO>
 
-    suspend fun getSyncOrNull(userWalletId: UserWalletId, integrationId: String): List<BalanceDTO>?
+    suspend fun getSyncOrNull(userWalletId: UserWalletId, integrationId: String): YieldBalanceWrapperDTO?
 
     suspend fun store(userWalletId: UserWalletId, integrationId: String, item: YieldBalanceWrapperDTO)
 }
