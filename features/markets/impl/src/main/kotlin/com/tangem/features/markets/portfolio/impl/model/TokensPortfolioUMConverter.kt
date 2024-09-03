43055cc0a74fc78b53358ef09726e347ad7ad7b7
Tangem <git@tangem.com>
2024-09-03 11:05:02 +0300

    Updated on 2025-09-17

--- features/markets/impl/src/main/kotlin/com/tangem/features/markets/portfolio/impl/model/TokensPortfolioUMConverter.kt
+++ features/markets/impl/src/main/kotlin/com/tangem/features/markets/portfolio/impl/model/TokensPortfolioUMConverter.kt
@@ -5,6 +5,7 @@ import com.tangem.domain.appcurrency.model.AppCurrency
 import com.tangem.domain.tokens.model.CryptoCurrency
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.domain.wallets.models.UserWallet
+import com.tangem.features.markets.portfolio.impl.loader.PortfolioData
 import com.tangem.features.markets.portfolio.impl.ui.state.MyPortfolioUM
 import com.tangem.features.markets.portfolio.impl.ui.state.MyPortfolioUM.Tokens.AddButtonState
 import com.tangem.utils.converter.Converter
@@ -15,6 +16,7 @@ import kotlinx.collections.immutable.toImmutableList
  *
 [REDACTED_AUTHOR]
  */
+@Suppress("LongParameterList")
 internal class TokensPortfolioUMConverter(
     private val appCurrency: AppCurrency,
     private val isBalanceHidden: Boolean,
@@ -22,25 +24,27 @@ internal class TokensPortfolioUMConverter(
     private val bsConfig: TangemBottomSheetConfig,
     private val onAddClick: () -> Unit,
     private val onTokenItemClick: (index: Int, id: CryptoCurrency.ID) -> Unit,
-) : Converter<Map<UserWallet, List<CryptoCurrencyStatus>>, MyPortfolioUM.Tokens> {
+    private val quickActionsIntents: TokenActionsHandler,
+) : Converter<Map<UserWallet, List<PortfolioData.CryptoCurrencyData>>, MyPortfolioUM.Tokens> {
 
-    override fun convert(value: Map<UserWallet, List<CryptoCurrencyStatus>>): MyPortfolioUM.Tokens {
+    override fun convert(value: Map<UserWallet, List<PortfolioData.CryptoCurrencyData>>): MyPortfolioUM.Tokens {
         return MyPortfolioUM.Tokens(
             tokens = value
-                .flatMap { entry ->
-                    entry.value.map { entry.key to it }
-                }
-                .mapIndexed { index, status ->
+                .flatMap { entry -> entry.value }
+                .mapIndexed { index, cryptoData ->
                     PortfolioTokenUMConverter(
                         appCurrency = appCurrency,
                         isBalanceHidden = isBalanceHidden,
                         onTokenItemClick = { onTokenItemClick(index, it.currency.id) },
-                    ).convert(status)
+                        tokenActionsHandler = quickActionsIntents,
+                    ).convert(cryptoData)
                 }
                 .toImmutableList(),
             buttonState = if (isAllAvailableNetworksAdded) AddButtonState.Unavailable else AddButtonState.Available,
-            bsConfig = bsConfig,
+            addToPortfolioBSConfig = bsConfig,
             onAddClick = onAddClick,
+            tokenReceiveBSConfig = TangemBottomSheetConfig.Empty,
+            tokenActionsBSConfig = TangemBottomSheetConfig.Empty,
         )
     }
 }
