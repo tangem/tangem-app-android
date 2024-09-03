f4089c8482bd91dac6ed9285f414a6c1ebaa9700
Tangem <git@tangem.com>
2024-09-03 11:05:02 +0300

    Updated on 2026-06-24

--- features/markets/impl/src/main/kotlin/com/tangem/features/markets/portfolio/impl/loader/PortfolioDataLoader.kt
+++ features/markets/impl/src/main/kotlin/com/tangem/features/markets/portfolio/impl/loader/PortfolioDataLoader.kt
@@ -6,9 +6,9 @@ import com.tangem.domain.appcurrency.model.AppCurrency
 import com.tangem.domain.balancehiding.GetBalanceHidingSettingsUseCase
 import com.tangem.domain.core.lce.Lce
 import com.tangem.domain.tokens.GetAllWalletsCryptoCurrencyStatusesUseCase
+import com.tangem.domain.tokens.GetCryptoCurrencyActionsUseCase
 import com.tangem.domain.tokens.GetWalletTotalBalanceUseCase
 import com.tangem.domain.tokens.error.TokenListError
-import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.domain.tokens.model.TotalFiatBalance
 import com.tangem.domain.wallets.models.UserWallet
 import com.tangem.domain.wallets.models.UserWalletId
@@ -32,18 +32,19 @@ internal class PortfolioDataLoader @Inject constructor(
     private val getSelectedAppCurrencyUseCase: GetSelectedAppCurrencyUseCase,
     private val getBalanceHidingSettingsUseCase: GetBalanceHidingSettingsUseCase,
     private val getWalletTotalBalanceUseCase: GetWalletTotalBalanceUseCase,
+    private val getCryptoCurrencyActionsUseCase: GetCryptoCurrencyActionsUseCase,
 ) {
 
     /** Load data by [currencyRawId] */
     @OptIn(ExperimentalCoroutinesApi::class)
     fun load(currencyRawId: String): Flow<PortfolioData> {
         return combine(
-            flow = getAllWalletsCryptoCurrencyStatusesFlow(currencyRawId = currencyRawId),
+            flow = getAllWalletsCryptoCurrenciesData(currencyRawId = currencyRawId),
             flow2 = getSelectedAppCurrencyFlow(),
             flow3 = getBalanceHidingSettingsFlow(),
-        ) { walletsWithCurrencyStatuses, appCurrency, isBalanceHidden ->
+        ) { walletsWithCurrencies, appCurrency, isBalanceHidden ->
             PortfolioData(
-                walletsWithCurrencyStatuses = walletsWithCurrencyStatuses,
+                walletsWithCurrencies = walletsWithCurrencies,
                 appCurrency = appCurrency,
                 isBalanceHidden = isBalanceHidden,
                 walletsWithBalance = emptyMap(),
@@ -52,20 +53,45 @@ internal class PortfolioDataLoader @Inject constructor(
             // setup balances for wallets from walletsWithCurrencyStatuses
             .flatMapLatest { portfolioData ->
                 getWalletsWithTotalBalanceFlow(
-                    ids = portfolioData.walletsWithCurrencyStatuses.keys.map(UserWallet::walletId),
+                    ids = portfolioData.walletsWithCurrencies.keys.map(UserWallet::walletId),
                 )
                     .map { portfolioData.copy(walletsWithBalance = it) }
             }
     }
 
-    private fun getAllWalletsCryptoCurrencyStatusesFlow(
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private fun getAllWalletsCryptoCurrenciesData(
         currencyRawId: String,
-    ): Flow<Map<UserWallet, List<CryptoCurrencyStatus>>> {
+    ): Flow<Map<UserWallet, List<PortfolioData.CryptoCurrencyData>>> {
         return getAllWalletsCryptoCurrencyStatusesUseCase(currencyRawId)
             .distinctUntilChanged()
             .map { walletsWithMaybeStatuses ->
                 walletsWithMaybeStatuses.mapValues { entry ->
                     entry.value.mapNotNull { it.getOrNull() }
+                }
+            }
+            .flatMapLatest { walletsWithStatuses ->
+                val actionsFlows = walletsWithStatuses.flatMap { (wallet, statuses) ->
+                    statuses.map { status ->
+                        getCryptoCurrencyActionsUseCase(wallet, status)
+                            .map {
+                                PortfolioData.CryptoCurrencyData(
+                                    userWallet = wallet,
+                                    status = status,
+                                    actions = it.states,
+                                )
+                            }
+                    }
+                }
+
+                combine(actionsFlows) { actions ->
+                    walletsWithStatuses.mapValues { entry ->
+                        entry.value.mapNotNull { status ->
+                            actions.firstOrNull {
+                                it.userWallet == entry.key && it.status == status
+                            }
+                        }
+                    }
                 }
             }
     }
