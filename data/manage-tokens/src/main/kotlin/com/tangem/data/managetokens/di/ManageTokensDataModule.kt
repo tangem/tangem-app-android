0fdcdda8d52027cc51359d693881209cc0fc138f
Tangem <git@tangem.com>
2024-09-04 14:24:34 +0500

    Updated on 2026-08-14

--- data/manage-tokens/src/main/kotlin/com/tangem/data/managetokens/di/ManageTokensDataModule.kt
+++ data/manage-tokens/src/main/kotlin/com/tangem/data/managetokens/di/ManageTokensDataModule.kt
@@ -3,10 +3,8 @@ package com.tangem.data.managetokens.di
 import com.tangem.data.managetokens.DefaultCustomTokensRepository
 import com.tangem.data.managetokens.DefaultManageTokensRepository
 import com.tangem.data.managetokens.utils.ManageTokensUpdateFetcher
-import com.tangem.datasource.api.express.TangemExpressApi
 import com.tangem.datasource.api.tangemTech.TangemTechApi
 import com.tangem.datasource.local.preferences.AppPreferencesStore
-import com.tangem.datasource.local.token.ExpressAssetsStore
 import com.tangem.datasource.local.userwallet.UserWalletsStore
 import com.tangem.domain.managetokens.repository.CustomTokensRepository
 import com.tangem.domain.managetokens.repository.ManageTokensRepository
@@ -30,9 +28,6 @@ internal object ManageTokensDataModule {
         manageTokensUpdateFetcher: ManageTokensUpdateFetcher,
         appPreferencesStore: AppPreferencesStore,
         dispatchers: CoroutineDispatcherProvider,
-        walletManagersFacade: WalletManagersFacade,
-        tangemExpressApi: TangemExpressApi,
-        expressAssetsStore: ExpressAssetsStore,
     ): ManageTokensRepository {
         return DefaultManageTokensRepository(
             tangemTechApi,
@@ -40,9 +35,6 @@ internal object ManageTokensDataModule {
             manageTokensUpdateFetcher,
             appPreferencesStore,
             dispatchers,
-            walletManagersFacade,
-            tangemExpressApi,
-            expressAssetsStore,
         )
     }
 
