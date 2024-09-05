aea5e8414aa29e6d751051d4cf7ce669f0d3ed0a
Tangem <git@tangem.com>
2024-09-05 18:33:40 +0300

    Updated on 2026-09-01

--- features/wallet/impl/build.gradle.kts
+++ features/wallet/impl/build.gradle.kts
@@ -79,6 +79,7 @@ dependencies {
     implementation(projects.domain.analytics)
     implementation(projects.domain.visa)
     implementation(projects.domain.staking.models)
+    implementation(projects.domain.markets.models)
 
     //TODO: Create api/impl modules for onboarding [REDACTED_JIRA]
     implementation(projects.features.onboarding)
