cb67fee9bbbe7191c1404028ad2ef6aae26434fe
Tangem <git@tangem.com>
2024-09-05 09:18:09 +0300

    Updated on 2026-07-07

--- core/ui/src/main/java/com/tangem/core/ui/components/rows/BlockchainRow.kt
+++ core/ui/src/main/java/com/tangem/core/ui/components/rows/BlockchainRow.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
@@ -19,6 +20,8 @@ import com.tangem.core.ui.components.TangemSwitch
 import com.tangem.core.ui.components.rows.model.BlockchainRowUM
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.core.ui.res.TangemThemePreview
+
+private const val DISABLED_ICON_ALPHA = 0.4f
 
 /**
  * [Figma Component](https://www.figma.com/design/14ISV23YB1yVW1uNVwqrKv/Android?node-id=2737-2800&t=ewlXfWwbDnRhjw4B-4)
@@ -36,8 +39,9 @@ fun BlockchainRow(model: BlockchainRowUM, action: @Composable BoxScope.() -> Uni
         icon = {
             RowIcon(
                 resId = model.iconResId,
-                isColored = model.isSelected,
+                isColored = model.isSelected && model.isEnabled,
                 showAccentBadge = model.isMainNetwork,
+                isEnabled = model.isEnabled,
             )
         },
         text = {
@@ -46,6 +50,7 @@ fun BlockchainRow(model: BlockchainRowUM, action: @Composable BoxScope.() -> Uni
                 secondText = model.type,
                 accentMainText = model.isSelected,
                 accentSecondText = model.isMainNetwork,
+                isEnabled = model.isEnabled,
             )
         },
         action = action,
@@ -58,10 +63,9 @@ private fun RowIcon(
     isColored: Boolean,
     showAccentBadge: Boolean,
     modifier: Modifier = Modifier,
+    isEnabled: Boolean = true,
 ) {
-    Box(
-        modifier = modifier.size(TangemTheme.dimens.size24),
-    ) {
+    Box(modifier = modifier.size(TangemTheme.dimens.size24)) {
         if (isColored) {
             Image(
                 modifier = Modifier
@@ -78,6 +82,7 @@ private fun RowIcon(
                         color = TangemTheme.colors.button.secondary,
                         shape = CircleShape,
                     )
+                    .alpha(if (isEnabled) 1f else DISABLED_ICON_ALPHA)
                     .size(TangemTheme.dimens.size22),
                 painter = painterResource(id = resId),
                 tint = TangemTheme.colors.icon.informative,
@@ -126,7 +131,7 @@ private fun Preview_BlockchainRow(@PreviewParameter(BlockchainRowParameterProvid
                 BlockchainRow(
                     model = state,
                     action = {
-                        TangemSwitch(onCheckedChange = { }, checked = true)
+                        TangemSwitch(onCheckedChange = { }, checked = true, enabled = state.isEnabled)
                     },
                 )
             },
@@ -159,6 +164,15 @@ private class BlockchainRowParameterProvider : CollectionPreviewParameterProvide
             iconResId = R.drawable.ic_bsc_16,
             isMainNetwork = false,
             isSelected = false,
+        ),
+        BlockchainRowUM(
+            id = "2",
+            name = "BNB BEACON CHAIN",
+            type = "1234567890111213141516171819",
+            iconResId = R.drawable.ic_bsc_16,
+            isMainNetwork = false,
+            isSelected = false,
+            isEnabled = false,
         ),
     ),
 )
