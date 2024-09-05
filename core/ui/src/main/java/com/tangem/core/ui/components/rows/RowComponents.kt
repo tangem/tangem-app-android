1eef84b5a1aac2e9a27414f24a569ee21a209fc1
Tangem <git@tangem.com>
2024-09-05 09:18:09 +0300

    release/5.26.1

--- core/ui/src/main/java/com/tangem/core/ui/components/rows/RowComponents.kt
+++ core/ui/src/main/java/com/tangem/core/ui/components/rows/RowComponents.kt
@@ -53,6 +53,7 @@ internal fun RowText(
     accentSecondText: Boolean,
     modifier: Modifier = Modifier,
     subtitle: TextReference? = null,
+    isEnabled: Boolean = true,
 ) {
     Column(
         modifier = modifier,
@@ -67,7 +68,11 @@ internal fun RowText(
                 modifier = Modifier.weight(weight = 10f, fill = false),
                 text = mainText,
                 style = TangemTheme.typography.subtitle2,
-                color = if (accentMainText) TangemTheme.colors.text.primary1 else TangemTheme.colors.text.secondary,
+                color = if (isEnabled) {
+                    if (accentMainText) TangemTheme.colors.text.primary1 else TangemTheme.colors.text.secondary
+                } else {
+                    TangemTheme.colors.text.disabled
+                },
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis,
             )
@@ -76,7 +81,11 @@ internal fun RowText(
                 modifier = Modifier.weight(weight = 4f, fill = false),
                 text = secondText,
                 style = TangemTheme.typography.body2,
-                color = if (accentSecondText) TangemTheme.colors.text.accent else TangemTheme.colors.text.secondary,
+                color = if (isEnabled) {
+                    if (accentSecondText) TangemTheme.colors.text.accent else TangemTheme.colors.text.secondary
+                } else {
+                    TangemTheme.colors.text.disabled
+                },
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis,
             )
