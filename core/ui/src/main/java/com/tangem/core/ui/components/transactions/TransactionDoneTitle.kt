8cc909a88d37bb1ca9e8227f0b79769c6687b737
Tangem <git@tangem.com>
2024-09-09 17:16:46 +0500

    Updated on 2026-09-01

--- core/ui/src/main/java/com/tangem/core/ui/components/transactions/TransactionDoneTitle.kt
+++ core/ui/src/main/java/com/tangem/core/ui/components/transactions/TransactionDoneTitle.kt
@@ -1,7 +1,6 @@
 package com.tangem.core.ui.components.transactions
 
 import android.content.res.Configuration
-import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -13,22 +12,24 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.tangem.core.ui.R
-import com.tangem.core.ui.res.TangemThemePreview
+import com.tangem.core.ui.extensions.TextReference
+import com.tangem.core.ui.extensions.resolveReference
+import com.tangem.core.ui.extensions.resourceReference
+import com.tangem.core.ui.extensions.stringReference
 import com.tangem.core.ui.res.TangemTheme
-import com.tangem.core.ui.utils.DateTimeFormatters
-import com.tangem.core.ui.utils.toTimeFormat
+import com.tangem.core.ui.res.TangemThemePreview
 
 /**
  * Common transaction done screen title
  *
- * @param titleRes title resource
- * @param date transaction timestamp in millis
+ * @param title title resource
+ * @param subtitle subtitle text
  */
 @Composable
-fun TransactionDoneTitle(@StringRes titleRes: Int, date: Long, modifier: Modifier = Modifier) {
+fun TransactionDoneTitle(title: TextReference, subtitle: TextReference, modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
             .fillMaxWidth(),
@@ -41,20 +42,17 @@ fun TransactionDoneTitle(@StringRes titleRes: Int, date: Long, modifier: Modifie
                 .size(TangemTheme.dimens.size64),
         )
         Text(
-            text = stringResource(id = titleRes),
+            text = title.resolveReference(),
             style = TangemTheme.typography.h3,
             color = TangemTheme.colors.text.primary1,
             modifier = Modifier
                 .padding(top = TangemTheme.dimens.spacing16),
         )
         Text(
-            text = stringResource(
-                id = R.string.send_date_format,
-                date.toTimeFormat(DateTimeFormatters.dateFormatter),
-                date.toTimeFormat(),
-            ),
+            text = subtitle.resolveReference(),
             style = TangemTheme.typography.body2,
             color = TangemTheme.colors.text.tertiary,
+            textAlign = TextAlign.Center,
             modifier = Modifier
                 .padding(top = TangemTheme.dimens.spacing4),
         )
@@ -68,8 +66,8 @@ fun TransactionDoneTitle(@StringRes titleRes: Int, date: Long, modifier: Modifie
 private fun TransactionDoneTitlePreview() {
     TangemThemePreview {
         TransactionDoneTitle(
-            titleRes = R.string.sent_transaction_sent_title,
-            date = 0,
+            title = resourceReference(R.string.sent_transaction_sent_title),
+            subtitle = stringReference("0"),
             modifier = Modifier
                 .background(TangemTheme.colors.background.tertiary)
                 .padding(TangemTheme.dimens.spacing16),
