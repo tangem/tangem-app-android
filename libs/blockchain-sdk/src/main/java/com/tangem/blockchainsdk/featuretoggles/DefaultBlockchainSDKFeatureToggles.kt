140e380b7c9e9cbe55cdd0e7c7e27e327172d715
Mama1emon <andrew.khokhlove@gmail.com>
2024-09-09 14:46:54 +0300

    AND-8274 [App] Support a new Ethereum fee contract
    Signed-off-by: Mama1emon <andrew.khokhlove@gmail.com>

--- libs/blockchain-sdk/src/main/java/com/tangem/blockchainsdk/featuretoggles/DefaultBlockchainSDKFeatureToggles.kt
+++ libs/blockchain-sdk/src/main/java/com/tangem/blockchainsdk/featuretoggles/DefaultBlockchainSDKFeatureToggles.kt
@@ -3,5 +3,9 @@ package com.tangem.blockchainsdk.featuretoggles
 import com.tangem.core.featuretoggle.manager.FeatureTogglesManager
 
 internal class DefaultBlockchainSDKFeatureToggles(
-    @Suppress("UnusedPrivateMember") private val featureTogglesManager: FeatureTogglesManager,
-) : BlockchainSDKFeatureToggles
+    private val featureTogglesManager: FeatureTogglesManager,
+) : BlockchainSDKFeatureToggles {
+
+    override val isEthereumEIP1559Enabled: Boolean
+        get() = featureTogglesManager.isFeatureEnabled(name = "IS_ETHEREUM_EIP_1559_ENABLED")
+}
