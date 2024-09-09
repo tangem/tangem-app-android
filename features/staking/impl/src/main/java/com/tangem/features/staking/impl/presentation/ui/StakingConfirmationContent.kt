a16a9e80bac77cc419437b5de9edc2b8bd8af809
Tangem <git@tangem.com>
2024-09-09 17:16:46 +0500

    Updated on 2026-08-14

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/ui/StakingConfirmationContent.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/ui/StakingConfirmationContent.kt
@@ -9,13 +9,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.tangem.common.ui.amountScreen.models.AmountState
 import com.tangem.common.ui.amountScreen.preview.AmountStatePreviewData
 import com.tangem.common.ui.amountScreen.ui.AmountBlock
 import com.tangem.core.ui.components.transactions.TransactionDoneTitle
+import com.tangem.core.ui.extensions.resourceReference
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.core.ui.res.TangemThemePreview
 import com.tangem.domain.staking.model.stakekit.action.StakingActionCommonType
@@ -52,10 +52,9 @@ internal fun StakingConfirmationContent(
             visible = doneState is TransactionDoneState.Content,
             modifier = Modifier.padding(vertical = TangemTheme.dimens.spacing12),
         ) {
-            val transactionDoneStateContent = remember(this) { doneState as TransactionDoneState.Content }
             TransactionDoneTitle(
-                titleRes = R.string.sent_transaction_sent_title,
-                date = transactionDoneStateContent.timestamp,
+                title = resourceReference(R.string.common_in_progress),
+                subtitle = resourceReference(R.string.staking_transaction_in_progress_text),
             )
         }
         AmountBlock(
