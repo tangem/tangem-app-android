d59cd8812c24655389e33c11041a0493664d6871
Tangem <git@tangem.com>
2024-09-09 10:10:35 +0300

    Updated on 2026-04-06

--- domain/staking/src/main/java/com/tangem/domain/staking/SubmitHashUseCase.kt
+++ domain/staking/src/main/java/com/tangem/domain/staking/SubmitHashUseCase.kt
@@ -3,20 +3,20 @@ package com.tangem.domain.staking
 import arrow.core.Either
 import com.tangem.domain.staking.model.stakekit.StakingError
 import com.tangem.domain.staking.repositories.StakingErrorResolver
-import com.tangem.domain.staking.repositories.StakingRepository
+import com.tangem.domain.staking.repositories.StakingTransactionHashRepository
 
 /**
  * Use case for submitting transaction hash to stakekit
  */
 class SubmitHashUseCase(
-    private val stakingRepository: StakingRepository,
+    private val stakingTransactionHashRepository: StakingTransactionHashRepository,
     private val stakingErrorResolver: StakingErrorResolver,
 ) {
 
     suspend fun submitHash(transactionId: String, transactionHash: String): Either<StakingError, Unit> {
         return Either
             .catch {
-                stakingRepository.submitHash(
+                stakingTransactionHashRepository.submitHash(
                     transactionId = transactionId,
                     transactionHash = transactionHash,
                 )
