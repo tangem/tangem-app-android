fb942174f1db3f7311c2bdca04e002f931e67653
Tangem <git@tangem.com>
2024-09-09 19:54:56 +0500

    Updated on 2026-09-14

--- app/src/androidTest/kotlin/com/tangem/tests/MainScreenTest.kt
+++ app/src/androidTest/kotlin/com/tangem/tests/MainScreenTest.kt
@@ -1,11 +1,8 @@
 package com.tangem.tests
 
 import com.tangem.common.BaseTestCase
-import com.tangem.screens.DisclaimerScreen
-import com.tangem.screens.StoriesScreen
-import com.tangem.screens.WalletScreen
+import com.tangem.scenarios.OpenMainScreenScenario
 import dagger.hilt.android.testing.HiltAndroidTest
-import io.github.kakaocup.compose.node.element.ComposeScreen
 import org.junit.Test
 
 @HiltAndroidTest
@@ -14,26 +11,6 @@ class MainScreenTest : BaseTestCase() {
     @Test
     fun goToMain() =
         setupHooks().run {
-            ComposeScreen.onComposeScreen<StoriesScreen>(composeTestRule) {
-                step("Click on \"Scan\" button") {
-                    scanButton {
-                        assertIsDisplayed()
-                        performClick()
-                    }
-                }
-            }
-            DisclaimerScreen {
-                step("Click on \"Accept\" button") {
-                    acceptButton {
-                        isVisible()
-                        click()
-                    }
-                }
-            }
-            ComposeScreen.onComposeScreen<WalletScreen>(composeTestRule) {
-                step("Make sure wallet screen is visible") {
-                    assertIsDisplayed()
-                }
-            }
+            scenario(OpenMainScreenScenario(composeTestRule))
         }
 }
