2df7780df6f2fac5a55bdf99d2a82d88af432f2b
Tangem <git@tangem.com>
2024-09-11 18:53:55 +0300

    Updated on 2026-04-06

--- features/markets/impl/src/main/kotlin/com/tangem/features/markets/portfolio/impl/model/TokensPortfolioUMConverter.kt
+++ features/markets/impl/src/main/kotlin/com/tangem/features/markets/portfolio/impl/model/TokensPortfolioUMConverter.kt
@@ -23,7 +23,7 @@ import kotlinx.collections.immutable.toImmutableList
 internal class TokensPortfolioUMConverter(
     private val appCurrency: AppCurrency,
     private val isBalanceHidden: Boolean,
-    private val isAllAvailableNetworksAdded: Boolean,
+    private val addButtonState: AddButtonState,
     private val bsConfig: TangemBottomSheetConfig,
     private val onAddClick: () -> Unit,
     private val quickActionsIntents: TokenActionsHandler,
@@ -47,7 +47,7 @@ internal class TokensPortfolioUMConverter(
                 }
                 .setQuickActionsVisibility(currentState = currentTokensState)
                 .toImmutableList(),
-            buttonState = if (isAllAvailableNetworksAdded) AddButtonState.Unavailable else AddButtonState.Available,
+            buttonState = addButtonState,
             addToPortfolioBSConfig = bsConfig,
             onAddClick = onAddClick,
             tokenReceiveBSConfig = (currentState() as? MyPortfolioUM.Tokens)
