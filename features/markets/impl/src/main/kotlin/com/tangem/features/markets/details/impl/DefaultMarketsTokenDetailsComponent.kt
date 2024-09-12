58a15bfa6573974d5d2c3580d13eedbde9f5485e
Tangem <git@tangem.com>
2024-09-12 12:04:51 +0300

    Updated on 2026-07-07

--- features/markets/impl/src/main/kotlin/com/tangem/features/markets/details/impl/DefaultMarketsTokenDetailsComponent.kt
+++ features/markets/impl/src/main/kotlin/com/tangem/features/markets/details/impl/DefaultMarketsTokenDetailsComponent.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
 import androidx.lifecycle.compose.LifecycleStartEffect
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.tangem.blockchainsdk.compatibility.getTokenIdIfL2Network
 import com.tangem.core.decompose.context.AppComponentContext
 import com.tangem.core.decompose.context.child
 import com.tangem.core.decompose.model.getOrCreateModel
@@ -31,12 +32,18 @@ internal class DefaultMarketsTokenDetailsComponent @AssistedInject constructor(
     portfolioComponentFactory: MarketsPortfolioComponent.Factory,
 ) : AppComponentContext by appComponentContext, MarketsTokenDetailsComponent {
 
-    private val model: MarketsTokenDetailsModel = getOrCreateModel(params)
+    // applying l2 compatibility
+    private val updatedParams = params.copy(
+        token = params.token.copy(
+            id = getTokenIdIfL2Network(params.token.id),
+        ),
+    )
+    private val model: MarketsTokenDetailsModel = getOrCreateModel(updatedParams)
 
-    private val portfolioComponent: MarketsPortfolioComponent? = if (params.showPortfolio) {
+    private val portfolioComponent: MarketsPortfolioComponent? = if (updatedParams.showPortfolio) {
         portfolioComponentFactory.create(
             context = child("my_portfolio"),
-            params = MarketsPortfolioComponent.Params(params.token),
+            params = MarketsPortfolioComponent.Params(updatedParams.token),
         )
     } else {
         null
