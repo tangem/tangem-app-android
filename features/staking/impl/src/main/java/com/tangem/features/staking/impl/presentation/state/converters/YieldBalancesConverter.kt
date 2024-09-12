4dc94e3e89cf1a8c79ea1c74b97e217e55d7a1ef
Tangem <git@tangem.com>
2024-09-12 20:52:37 +0300

    release/5.26.1

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/converters/YieldBalancesConverter.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/converters/YieldBalancesConverter.kt
@@ -50,9 +50,11 @@ internal class YieldBalancesConverter(
         }
     }
 
-    private fun List<BalanceItem>.mapBalances() = sortedBy { it.type.order }
+    private fun List<BalanceItem>.mapBalances() = asSequence()
         .filterNot { it.amount.isZero() || it.type == BalanceType.REWARDS }
         .mapNotNull(balanceItemConverter::convert)
+        .sortedByDescending { it.cryptoDecimal }
+        .sortedBy { it.type.order }
         .toPersistentList()
 
     private fun getRewardBlockType(): RewardBlockType {
