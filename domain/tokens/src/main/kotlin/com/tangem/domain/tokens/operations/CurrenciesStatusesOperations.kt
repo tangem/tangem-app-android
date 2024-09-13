3f902f2521386d0929bfef9ae8332d5aada99b73
Mama1emon <andrew.khokhlove@gmail.com>
2024-09-13 14:11:40 +0300

    AND-8283 Nodl: there is en 'endless' loader for nodl card on the main screen
    Signed-off-by: Mama1emon <andrew.khokhlove@gmail.com>

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/operations/CurrenciesStatusesOperations.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/operations/CurrenciesStatusesOperations.kt
@@ -146,9 +146,13 @@ internal class CurrenciesStatusesOperations(
             val currenciesFlow = combine(
                 getQuotes(currenciesIds),
                 getNetworksStatuses(networks),
-                getYieldBalances(nonEmptyCurrencies),
-            ) { maybeQuotes, maybeNetworksStatuses, maybeYieldBalances ->
-                createCurrenciesStatuses(nonEmptyCurrencies, maybeQuotes, maybeNetworksStatuses, maybeYieldBalances)
+            ) { maybeQuotes, maybeNetworksStatuses ->
+                createCurrenciesStatuses(
+                    currencies = nonEmptyCurrencies,
+                    maybeQuotes = maybeQuotes,
+                    maybeNetworkStatuses = maybeNetworksStatuses,
+                    maybeYieldBalances = null,
+                )
             }
 
             emitAll(currenciesFlow)
@@ -386,15 +390,6 @@ internal class CurrenciesStatusesOperations(
             .map<Set<NetworkStatus>, Either<Error, Set<NetworkStatus>>> { it.right() }
             .catch { emit(Error.DataError(it).left()) }
             .onEmpty { emit(Error.EmptyNetworksStatuses.left()) }
-    }
-
-    private fun getYieldBalances(cryptoCurrencies: List<CryptoCurrency>): Flow<Either<Error, YieldBalanceList>> {
-        return stakingRepository.getMultiYieldBalanceFlow(
-            userWalletId = userWalletId,
-            cryptoCurrencies = cryptoCurrencies,
-        ).map<YieldBalanceList, Either<Error, YieldBalanceList>> { it.right() }
-            .catch { emit(Error.DataError(it).left()) }
-            .onEmpty { emit(Error.EmptyYieldBalances.left()) }
     }
 
     private suspend fun getYieldBalancesSync(
