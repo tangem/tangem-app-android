d72593ac532da113803b247f6820186e32017a7a
Tangem <git@tangem.com>
2024-09-16 22:34:07 +0300

    Updated on 2025-12-30

--- domain/markets/models/src/main/kotlin/com/tangem/domain/markets/TokenQuotes.kt
+++ domain/markets/models/src/main/kotlin/com/tangem/domain/markets/TokenQuotes.kt
@@ -12,3 +12,16 @@ data class TokenQuotes(
     val yearChangePercent: BigDecimal?,
     val allTimeChangePercent: BigDecimal?,
 )
+
+fun TokenQuotes.populateWith(quotes: TokenQuotes): TokenQuotes {
+    return TokenQuotes(
+        currentPrice = quotes.currentPrice,
+        h24ChangePercent = quotes.h24ChangePercent ?: this.h24ChangePercent,
+        weekChangePercent = quotes.weekChangePercent ?: this.weekChangePercent,
+        monthChangePercent = quotes.monthChangePercent ?: this.monthChangePercent,
+        m3ChangePercent = quotes.m3ChangePercent ?: this.m3ChangePercent,
+        m6ChangePercent = quotes.m6ChangePercent ?: this.m6ChangePercent,
+        yearChangePercent = quotes.yearChangePercent ?: this.yearChangePercent,
+        allTimeChangePercent = quotes.allTimeChangePercent ?: this.allTimeChangePercent,
+    )
+}
