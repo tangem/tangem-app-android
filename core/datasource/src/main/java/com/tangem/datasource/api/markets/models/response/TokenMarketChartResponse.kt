63cdc41289924a0a0874d338ac8db52cfef85aa4
Tangem <git@tangem.com>
2024-09-16 18:24:55 +0300

    release/5.26.2

--- core/datasource/src/main/java/com/tangem/datasource/api/markets/models/response/TokenMarketChartResponse.kt
+++ core/datasource/src/main/java/com/tangem/datasource/api/markets/models/response/TokenMarketChartResponse.kt
@@ -1,8 +1,10 @@
 package com.tangem.datasource.api.markets.models.response
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 import java.math.BigDecimal
 
+@JsonClass(generateAdapter = true)
 data class TokenMarketChartResponse(
     @Json(name = "prices")
     val prices: Map<Long, BigDecimal>,
