963554651e997a823f3dfce1dc650a266bacc1bb
Tangem <git@tangem.com>
2024-09-16 17:48:58 +0300

    Updated on 2025-09-15

--- domain/legacy/src/main/java/com/tangem/domain/walletmanager/utils/SdkTransactionTypeConverter.kt
+++ domain/legacy/src/main/java/com/tangem/domain/walletmanager/utils/SdkTransactionTypeConverter.kt
@@ -1,29 +1,37 @@
 package com.tangem.domain.walletmanager.utils
 
-import com.tangem.blockchain.transactionhistory.models.TransactionHistoryItem
+import com.tangem.blockchain.transactionhistory.models.TransactionHistoryItem.TransactionType
 import com.tangem.domain.txhistory.models.TxHistoryItem
 import com.tangem.domain.walletmanager.model.SmartContractMethod
 import com.tangem.utils.converter.Converter
 
 internal class SdkTransactionTypeConverter(
     private val smartContractMethods: Map<String, SmartContractMethod>,
-) : Converter<TransactionHistoryItem.TransactionType, TxHistoryItem.TransactionType> {
+) : Converter<TransactionType, TxHistoryItem.TransactionType> {
 
-    override fun convert(value: TransactionHistoryItem.TransactionType): TxHistoryItem.TransactionType {
+    override fun convert(value: TransactionType): TxHistoryItem.TransactionType {
         return when (value) {
-            is TransactionHistoryItem.TransactionType.ContractMethod ->
+            is TransactionType.ContractMethod -> {
                 getTransactionType(methodName = smartContractMethods[value.id]?.name)
-            is TransactionHistoryItem.TransactionType.ContractMethodName -> getTransactionType(methodName = value.name)
-            is TransactionHistoryItem.TransactionType.Transfer -> TxHistoryItem.TransactionType.Transfer
-            TransactionHistoryItem.TransactionType.TronStakingTransactionType.FreezeBalanceV2Contract ->
-                TxHistoryItem.TransactionType.Transfer // TODO map correct [REDACTED_JIRA]
-            TransactionHistoryItem.TransactionType.TronStakingTransactionType.UnfreezeBalanceV2Contract ->
-                TxHistoryItem.TransactionType.Transfer // TODO map correct [REDACTED_JIRA]
-            TransactionHistoryItem.TransactionType.TronStakingTransactionType.VoteWitnessContract ->
-                TxHistoryItem
-                    .TransactionType.Transfer // TODO map correct [REDACTED_JIRA]
-            TransactionHistoryItem.TransactionType.TronStakingTransactionType.WithdrawBalanceContract ->
-                TxHistoryItem.TransactionType.Transfer // TODO map correct [REDACTED_JIRA]
+            }
+            is TransactionType.ContractMethodName -> {
+                getTransactionType(methodName = value.name)
+            }
+            is TransactionType.Transfer -> {
+                TxHistoryItem.TransactionType.Transfer
+            }
+            is TransactionType.TronStakingTransactionType.FreezeBalanceV2Contract -> {
+                TxHistoryItem.TransactionType.TronStakingTransactionType.Stake
+            }
+            is TransactionType.TronStakingTransactionType.UnfreezeBalanceV2Contract -> {
+                TxHistoryItem.TransactionType.TronStakingTransactionType.Unstake
+            }
+            is TransactionType.TronStakingTransactionType.VoteWitnessContract -> {
+                TxHistoryItem.TransactionType.TronStakingTransactionType.Vote
+            }
+            is TransactionType.TronStakingTransactionType.WithdrawBalanceContract -> {
+                TxHistoryItem.TransactionType.TronStakingTransactionType.Withdraw
+            }
         }
     }
 
