bb65260c530898901393cd29b0d9684bfaf16e50
Tangem <git@tangem.com>
2024-09-16 12:09:24 +0300

    Updated on 2026-05-19

---
+++ core/ui/src/main/java/com/tangem/core/ui/components/progressbar/LinearProgressIndicator.kt
@@ -0,0 +1,126 @@
+@file:Suppress("UnnecessaryParentheses")
+
+package com.tangem.core.ui.components.progressbar
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.semantics.ProgressBarRangeInfo
+import androidx.compose.ui.semantics.progressBarRangeInfo
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.offset
+import com.tangem.core.ui.res.TangemTheme
+import kotlin.math.abs
+
+/**
+ * Progress indicators express an unspecified wait time or display the length of a process.
+ *
+ * By default there is no animation between [progress] values. You can use
+ *
+ * @param progress The progress of this progress indicator, where 0.0 represents no progress and 1.0
+ * represents full progress. Values outside of this range are coerced into the range.
+ * @param modifier the [Modifier] to be applied to this progress indicator
+ * @param color The color of the progress indicator.
+ * @param backgroundColor The color of the background behind the indicator, visible when the
+ * progress has not reached that area of the overall indicator yet.
+ * @param strokeCap stroke cap to use for the ends of this progress indicator
+ */
+@Composable
+fun LinearProgressIndicator(
+    progress: () -> Float,
+    modifier: Modifier = Modifier,
+    color: Color = TangemTheme.colors.text.accent,
+    backgroundColor: Color = TangemTheme.colors.background.tertiary,
+    strokeCap: StrokeCap = StrokeCap.Round,
+) {
+    val coercedProgress = { progress().coerceIn(0f, 1f) }
+    Canvas(
+        modifier
+            .increaseSemanticsBounds()
+            .semantics(mergeDescendants = true) {
+                progressBarRangeInfo = ProgressBarRangeInfo(coercedProgress(), 0f..1f)
+            }
+            .size(height = 4.dp, width = 24.dp),
+    ) {
+        val strokeWidth = size.height
+        drawLinearIndicatorBackground(backgroundColor, strokeWidth, strokeCap)
+        drawLinearIndicator(0f, coercedProgress(), color, strokeWidth, strokeCap)
+    }
+}
+
+internal fun Modifier.increaseSemanticsBounds(): Modifier {
+    val padding = 10.dp
+    return this
+        .layout { measurable, constraints ->
+            val paddingPx = padding.roundToPx()
+            // We need to add vertical padding to the semantics bounds in other to meet
+            // screenreader green box minimum size, but we also want to
+            // preserve a visual appearance and layout size below that minimum
+            // in order to maintain backwards compatibility. This custom
+            // layout effectively implements "negative padding".
+            val newConstraint = constraints.offset(0, paddingPx * 2)
+            val placeable = measurable.measure(newConstraint)
+
+            // But when actually placing the placeable, create the layout without additional
+            // space. Place the placeable where it would've been without any extra padding.
+            val height = placeable.height - paddingPx * 2
+            val width = placeable.width
+            layout(width, height) {
+                placeable.place(0, -paddingPx)
+            }
+        }
+        .semantics(mergeDescendants = true) {}
+        .padding(vertical = padding)
+}
+
+private fun DrawScope.drawLinearIndicator(
+    startFraction: Float,
+    endFraction: Float,
+    color: Color,
+    strokeWidth: Float,
+    strokeCap: StrokeCap,
+) {
+    val width = size.width
+    val height = size.height
+    // Start drawing from the vertical center of the stroke
+    val yOffset = height / 2
+
+    val isLtr = layoutDirection == LayoutDirection.Ltr
+    val barStart = (if (isLtr) startFraction else 1f - endFraction) * width
+    val barEnd = (if (isLtr) endFraction else 1f - startFraction) * width
+
+    // if there isn't enough space to draw the stroke caps, fall back to StrokeCap.Butt
+    if (strokeCap == StrokeCap.Butt || height > width) {
+        // Progress line
+        drawLine(color, Offset(barStart, yOffset), Offset(barEnd, yOffset), strokeWidth)
+    } else {
+        // need to adjust barStart and barEnd for the stroke caps
+        val strokeCapOffset = strokeWidth / 2
+        val coerceRange = strokeCapOffset..(width - strokeCapOffset)
+        val adjustedBarStart = barStart.coerceIn(coerceRange)
+        val adjustedBarEnd = barEnd.coerceIn(coerceRange)
+
+        if (abs(endFraction - startFraction) > 0) {
+            // Progress line
+            drawLine(
+                color,
+                Offset(adjustedBarStart, yOffset),
+                Offset(adjustedBarEnd, yOffset),
+                strokeWidth,
+                strokeCap,
+            )
+        }
+    }
+}
+
+private fun DrawScope.drawLinearIndicatorBackground(color: Color, strokeWidth: Float, strokeCap: StrokeCap) =
+    drawLinearIndicator(0f, 1f, color, strokeWidth, strokeCap)
