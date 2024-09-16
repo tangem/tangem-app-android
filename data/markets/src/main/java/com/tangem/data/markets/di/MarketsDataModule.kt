cd473ee02ebd62c8d730c13a9e0171e158bfe6ce
Tangem <git@tangem.com>
2024-09-16 18:24:55 +0300

    Updated on 2026-06-10

--- data/markets/src/main/java/com/tangem/data/markets/di/MarketsDataModule.kt
+++ data/markets/src/main/java/com/tangem/data/markets/di/MarketsDataModule.kt
@@ -1,5 +1,6 @@
 package com.tangem.data.markets.di
 
+import com.tangem.core.analytics.api.AnalyticsEventHandler
 import com.tangem.data.markets.DefaultMarketsTokenRepository
 import com.tangem.datasource.api.markets.TangemTechMarketsApi
 import com.tangem.datasource.api.tangemTech.TangemTechApi
@@ -24,12 +25,14 @@ internal object MarketsDataModule {
         @DevTangemApi tangemTechApi: TangemTechApi,
         userWalletsStore: UserWalletsStore,
         dispatchers: CoroutineDispatcherProvider,
+        analyticsEventHandler: AnalyticsEventHandler,
     ): MarketsTokenRepository {
         return DefaultMarketsTokenRepository(
             marketsApi = marketsApi,
             tangemTechApi = tangemTechApi,
             dispatcherProvider = dispatchers,
             userWalletsStore = userWalletsStore,
+            analyticsEventHandler = analyticsEventHandler,
         )
     }
 }
