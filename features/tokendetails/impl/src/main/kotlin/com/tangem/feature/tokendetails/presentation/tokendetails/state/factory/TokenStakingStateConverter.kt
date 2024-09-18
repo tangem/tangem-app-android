07525c759276a4d269918e34eb30b528d3dc1090
Tangem <git@tangem.com>
2024-09-18 20:07:33 +0500

    Updated on 2026-08-27

--- features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/factory/TokenStakingStateConverter.kt
+++ features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/factory/TokenStakingStateConverter.kt
@@ -8,7 +8,6 @@ import com.tangem.domain.staking.model.StakingEntryInfo
 import com.tangem.domain.staking.model.stakekit.StakingError
 import com.tangem.feature.tokendetails.presentation.tokendetails.state.StakingBlockUM
 import com.tangem.feature.tokendetails.presentation.tokendetails.state.TokenDetailsState
-import com.tangem.feature.tokendetails.presentation.tokendetails.state.utils.getStringResourceId
 import com.tangem.feature.tokendetails.presentation.tokendetails.viewmodels.TokenDetailsClickIntents
 import com.tangem.features.tokendetails.impl.R
 import com.tangem.utils.Provider
@@ -39,7 +38,7 @@ internal class TokenStakingStateConverter(
                         formatArgs = wrappedList(apr),
                     ),
                     subtitleText = resourceReference(
-                        id = stakingEntryInfo.rewardSchedule.getStringResourceId(),
+                        id = R.string.staking_notification_earn_rewards_text,
                         formatArgs = wrappedList(stakingEntryInfo.tokenSymbol),
                     ),
                     iconState = iconState,
