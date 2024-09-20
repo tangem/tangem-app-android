1bf432dc7cdbb78baa799d041d2b02bea358d082
Tangem <git@tangem.com>
2024-09-20 16:40:25 +0300

    Updated on 2026-09-25

---
+++ domain/staking/models/src/main/kotlin/com/tangem/domain/staking/model/PendingTransaction.kt
@@ -0,0 +1,13 @@
+package com.tangem.domain.staking.model
+
+import com.tangem.domain.staking.model.stakekit.BalanceType
+import com.tangem.domain.staking.model.stakekit.Yield
+import java.math.BigDecimal
+
+data class PendingTransaction(
+    val groupId: String?,
+    val type: BalanceType?,
+    val amount: BigDecimal?,
+    val rawCurrencyId: String?,
+    val validator: Yield.Validator?,
+)
