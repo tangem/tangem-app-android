a4a07e6f3be43ce3d3b50e1cf9f984a04d4c6a7b
Tangem <git@tangem.com>
2024-09-20 16:40:25 +0300

    Updated on 2026-09-21

---
+++ domain/staking/src/main/java/com/tangem/domain/staking/GetPendingTransactionsUseCase.kt
@@ -0,0 +1,24 @@
+package com.tangem.domain.staking
+
+import arrow.core.Either
+import com.tangem.domain.staking.model.stakekit.BalanceItem
+import com.tangem.domain.staking.model.stakekit.StakingError
+import com.tangem.domain.staking.repositories.StakingErrorResolver
+import com.tangem.domain.staking.repositories.StakingPendingTransactionRepository
+
+/**
+ * Use case for getting saved staking pending transactions.
+ */
+class GetPendingTransactionsUseCase(
+    private val stakingPendingTransactionRepository: StakingPendingTransactionRepository,
+    private val stakingErrorResolver: StakingErrorResolver,
+) {
+
+    operator fun invoke(): Either<StakingError, List<BalanceItem>> {
+        return Either.catch {
+            stakingPendingTransactionRepository.getTransactionsWithBalanceItems().map { it.second }
+        }.mapLeft {
+            stakingErrorResolver.resolve(it)
+        }
+    }
+}
