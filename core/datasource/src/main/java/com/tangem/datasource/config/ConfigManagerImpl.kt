dc6597b8c7641ad744b2398ca87e26e4a8313692
Mama1emon <andrew.khokhlove@gmail.com>
2024-09-24 10:58:42 +0300

    AND-8507 Remove unused properties from Config
    Signed-off-by: Mama1emon <andrew.khokhlove@gmail.com>

--- core/datasource/src/main/java/com/tangem/datasource/config/ConfigManagerImpl.kt
+++ core/datasource/src/main/java/com/tangem/datasource/config/ConfigManagerImpl.kt
@@ -1,12 +1,9 @@
 package com.tangem.datasource.config
 
 import com.tangem.blockchain.common.*
-import com.tangem.datasource.config.ConfigManager.Companion.IS_CREATING_TWIN_CARDS_ALLOWED
-import com.tangem.datasource.config.ConfigManager.Companion.IS_TOP_UP_ENABLED
 import com.tangem.datasource.config.models.Config
 import com.tangem.datasource.config.models.ConfigModel
 import com.tangem.datasource.config.models.ConfigValueModel
-import com.tangem.datasource.config.models.FeatureModel
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -16,59 +13,21 @@ internal class ConfigManagerImpl @Inject constructor() : ConfigManager {
     override var config: Config = Config()
         private set
 
-    private var defaultConfig = Config()
-
     override suspend fun load(configLoader: Loader<ConfigModel>, onComplete: ((config: Config) -> Unit)?) {
         configLoader.load { configModel ->
-            setupFeature(configModel.features)
             setupConfigValues(configModel.configValues)
             onComplete?.invoke(config)
         }
-    }
-
-    override fun turnOff(name: String) {
-        when (name) {
-            IS_TOP_UP_ENABLED -> config = config.copy(isTopUpEnabled = false)
-            IS_CREATING_TWIN_CARDS_ALLOWED -> config = config.copy(isCreatingTwinCardsAllowed = false)
-        }
-    }
-
-    override fun resetToDefault(name: String) {
-        when (name) {
-            IS_TOP_UP_ENABLED -> {
-                config = config.copy(isTopUpEnabled = defaultConfig.isTopUpEnabled)
-            }
-            IS_CREATING_TWIN_CARDS_ALLOWED -> {
-                config = config.copy(isCreatingTwinCardsAllowed = defaultConfig.isCreatingTwinCardsAllowed)
-            }
-            else -> Unit
-        }
-    }
-
-    private fun setupFeature(featureModel: FeatureModel?) {
-        val model = featureModel ?: return
-
-        config = config.copy(
-            isTopUpEnabled = model.isTopUpEnabled,
-            isCreatingTwinCardsAllowed = model.isCreatingTwinCardsAllowed,
-        )
-
-        defaultConfig = defaultConfig.copy(
-            isTopUpEnabled = model.isTopUpEnabled,
-            isCreatingTwinCardsAllowed = model.isCreatingTwinCardsAllowed,
-        )
     }
 
     private fun setupConfigValues(configValues: ConfigValueModel?) {
         val values = configValues ?: return
 
         config = createConfig(config, values)
-        defaultConfig = config.copy()
     }
 
     private fun createConfig(config: Config, configValues: ConfigValueModel): Config {
         return config.copy(
-            coinMarketCapKey = configValues.coinMarketCapKey,
             moonPayApiKey = configValues.moonPayApiKey,
             moonPayApiSecretKey = configValues.moonPayApiSecretKey,
             mercuryoWidgetId = configValues.mercuryoWidgetId,
@@ -105,9 +64,7 @@ internal class ConfigManagerImpl @Inject constructor() : ConfigManager {
                 koinosProApiKey = configValues.koinosProApiKey,
             ),
             amplitudeApiKey = configValues.amplitudeApiKey,
-            sprinklr = configValues.sprinklr,
             walletConnectProjectId = configValues.walletConnectProjectId,
-            tangemComAuthorization = configValues.tangemComAuthorization,
             express = configValues.express,
             devExpress = configValues.devExpress,
             stakeKitApiKey = configValues.stakeKitApiKey,
