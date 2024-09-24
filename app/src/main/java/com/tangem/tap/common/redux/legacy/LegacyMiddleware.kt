6c31f5477210c15ded1f67a04bb6ae86c131f97e
Tangem <git@tangem.com>
2024-09-24 10:58:42 +0300

    release/5.26.1

--- app/src/main/java/com/tangem/tap/common/redux/legacy/LegacyMiddleware.kt
+++ app/src/main/java/com/tangem/tap/common/redux/legacy/LegacyMiddleware.kt
@@ -2,14 +2,14 @@ package com.tangem.tap.common.redux.legacy
 
 import com.tangem.blockchain.common.AmountType
 import com.tangem.domain.feedback.models.BlockchainErrorInfo
+import com.tangem.domain.feedback.models.CardInfo
+import com.tangem.domain.feedback.models.FeedbackEmailType
+import com.tangem.domain.models.scan.ScanResponse
 import com.tangem.domain.redux.LegacyAction
 import com.tangem.domain.utils.convertToSdkAmount
 import com.tangem.tap.common.extensions.dispatchWithMain
 import com.tangem.tap.common.extensions.inject
 import com.tangem.tap.common.extensions.stripZeroPlainString
-import com.tangem.tap.common.feedback.FeedbackEmail
-import com.tangem.tap.common.feedback.RateCanBeBetterEmail
-import com.tangem.tap.common.feedback.SendTransactionFailedEmail
 import com.tangem.tap.common.redux.AppState
 import com.tangem.tap.common.redux.global.GlobalAction
 import com.tangem.tap.features.details.redux.DetailsAction
@@ -23,7 +23,6 @@ import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
 import org.rekotlin.Middleware
 
 internal object LegacyMiddleware {
@@ -33,68 +32,49 @@ internal object LegacyMiddleware {
         { next ->
             { action ->
                 when (action) {
+                    is LegacyAction.StartOnboardingProcess -> {
+                        store.dispatch(
+                            GlobalAction.Onboarding.Start(
+                                scanResponse = action.scanResponse,
+                                canSkipBackup = action.canSkipBackup,
+                            ),
+                        )
+                    }
                     is LegacyAction.SendEmailRateCanBeBetter -> {
                         store.state.globalState.feedbackManager?.sendEmail(
-                            feedbackData = RateCanBeBetterEmail(),
-                            scanResponse = action.scanResponse,
+                            type = FeedbackEmailType.RateCanBeBetter(cardInfo = getCardInfo(action.scanResponse)),
                         )
                     }
                     is LegacyAction.SendEmailSupport -> {
                         store.state.globalState.feedbackManager?.sendEmail(
-                            feedbackData = FeedbackEmail(),
-                            scanResponse = action.scanResponse,
-                        )
-                    }
-                    is LegacyAction.StartOnboardingProcess -> {
-                        store.dispatch(
-                            GlobalAction.Onboarding.Start(action.scanResponse, canSkipBackup = action.canSkipBackup),
+                            type = FeedbackEmailType.DirectUserRequest(cardInfo = getCardInfo(action.scanResponse)),
                         )
                     }
                     is LegacyAction.SendEmailTransactionFailed -> {
-                        if (store.inject(DaggerGraphState::feedbackManagerFeatureToggles).isLocalLogsEnabled) {
+                        val amount = action.amount?.convertToSdkAmount(action.cryptoCurrency)
 
-                            val amount = action.amount?.convertToSdkAmount(action.cryptoCurrency)
-                            store.inject(DaggerGraphState::saveBlockchainErrorUseCase).invoke(
-                                error = BlockchainErrorInfo(
-                                    errorMessage = action.errorMessage,
-                                    blockchainId = action.cryptoCurrency.network.id.value,
-                                    derivationPath = action.cryptoCurrency.network.derivationPath.value,
-                                    destinationAddress = action.destinationAddress.orEmpty(),
-                                    tokenSymbol = if (amount?.type is AmountType.Token) {
-                                        amount.currencySymbol
-                                    } else {
-                                        ""
-                                    },
-                                    amount = amount?.value?.stripZeroPlainString() ?: "unknown",
-                                    fee = action.fee?.convertToSdkAmount(action.cryptoCurrency)
-                                        ?.value?.stripZeroPlainString() ?: "unknown",
-                                ),
-                            )
+                        store.inject(DaggerGraphState::saveBlockchainErrorUseCase).invoke(
+                            error = BlockchainErrorInfo(
+                                errorMessage = action.errorMessage,
+                                blockchainId = action.cryptoCurrency.network.id.value,
+                                derivationPath = action.cryptoCurrency.network.derivationPath.value,
+                                destinationAddress = action.destinationAddress.orEmpty(),
+                                tokenSymbol = if (amount?.type is AmountType.Token) {
+                                    amount.currencySymbol
+                                } else {
+                                    ""
+                                },
+                                amount = amount?.value?.stripZeroPlainString() ?: "unknown",
+                                fee = action.fee?.convertToSdkAmount(action.cryptoCurrency)
+                                    ?.value?.stripZeroPlainString() ?: "unknown",
+                            ),
+                        )
 
-                            store.state.globalState.feedbackManager?.sendEmail(
-                                feedbackData = SendTransactionFailedEmail(action.errorMessage),
-                                scanResponse = action.scanResponse,
-                            )
-                        } else {
-                            scope.launch {
-                                store.inject(DaggerGraphState::walletManagersFacade)
-                                    .getOrCreateWalletManager(
-                                        userWalletId = action.userWalletId,
-                                        network = action.cryptoCurrency.network,
-                                    )?.let { walletManager ->
-                                        store.state.globalState.feedbackManager?.infoHolder?.updateOnSendError(
-                                            walletManager = walletManager,
-                                            amountToSend = action.amount?.convertToSdkAmount(action.cryptoCurrency),
-                                            feeAmount = action.fee?.convertToSdkAmount(action.cryptoCurrency),
-                                            destinationAddress = action.destinationAddress,
-                                        )
-                                    }
-                                store.state.globalState.feedbackManager?.sendEmail(
-                                    feedbackData = SendTransactionFailedEmail(action.errorMessage),
-                                    scanResponse = null,
-                                )
-                            }
-                        }
+                        store.state.globalState.feedbackManager?.sendEmail(
+                            type = FeedbackEmailType.TransactionSendingProblem(
+                                cardInfo = getCardInfo(action.scanResponse),
+                            ),
+                        )
                     }
                     is LegacyAction.PrepareDetailsScreen -> {
                         val userWalletsListManager = store.inject(DaggerGraphState::generalUserWalletsListManager)
@@ -118,5 +98,11 @@ internal object LegacyMiddleware {
                 next(action)
             }
         }
+    }
+
+    private fun getCardInfo(scanResponse: ScanResponse): CardInfo {
+        return store.inject(DaggerGraphState::getCardInfoUseCase).invoke(scanResponse)
+            .getOrNull()
+            ?: error("Card info not found")
     }
 }
