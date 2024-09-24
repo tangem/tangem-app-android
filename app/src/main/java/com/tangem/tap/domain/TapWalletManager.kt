4fff9591103fb774c7699a62edea7f314db6562e
Tangem <git@tangem.com>
2024-09-24 10:58:42 +0300

    Updated on 2026-09-21

--- app/src/main/java/com/tangem/tap/domain/TapWalletManager.kt
+++ app/src/main/java/com/tangem/tap/domain/TapWalletManager.kt
@@ -10,11 +10,9 @@ import com.tangem.domain.common.util.cardTypesResolver
 import com.tangem.domain.models.scan.ScanResponse
 import com.tangem.domain.wallets.models.UserWallet
 import com.tangem.operations.attestation.Attestation
-import com.tangem.tap.common.extensions.inject
 import com.tangem.tap.common.extensions.setContext
 import com.tangem.tap.common.redux.global.GlobalAction
 import com.tangem.tap.features.onboarding.products.twins.redux.TwinCardsAction
-import com.tangem.tap.proxy.redux.DaggerGraphState
 import com.tangem.tap.store
 import com.tangem.tap.tangemSdkManager
 import com.tangem.utils.coroutines.AppCoroutineDispatcherProvider
@@ -51,11 +49,6 @@ class TapWalletManager(
         val attestationFailed = card.attestation.status == Attestation.Status.Failed
 
         tangemSdkManager.changeDisplayedCardIdNumbersCount(scanResponse)
-
-        val featureToggles = store.inject(DaggerGraphState::feedbackManagerFeatureToggles)
-        if (!featureToggles.isLocalLogsEnabled) {
-            store.state.globalState.feedbackManager?.infoHolder?.setCardInfo(scanResponse)
-        }
 
         updateConfigManager(scanResponse)
         withMainContext {
