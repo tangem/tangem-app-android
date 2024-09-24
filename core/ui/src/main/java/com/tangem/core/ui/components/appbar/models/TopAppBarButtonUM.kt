57488eeafd6938152e690db6903e570959474d31
Tangem <git@tangem.com>
2024-09-24 14:46:53 +0300

    Updated on 2026-08-07

--- core/ui/src/main/java/com/tangem/core/ui/components/appbar/models/TopAppBarButtonUM.kt
+++ core/ui/src/main/java/com/tangem/core/ui/components/appbar/models/TopAppBarButtonUM.kt
@@ -6,14 +6,18 @@ import com.tangem.core.ui.R
 data class TopAppBarButtonUM(
     @DrawableRes val iconRes: Int,
     val onIconClicked: () -> Unit,
+    val enabled: Boolean = true,
 ) {
 
     @Suppress("FunctionName")
     companion object {
 
-        fun Back(onBackClicked: () -> Unit) = TopAppBarButtonUM(
+        fun Back(onBackClicked: () -> Unit) = Back(true, onBackClicked)
+
+        fun Back(enabled: Boolean = true, onBackClicked: () -> Unit) = TopAppBarButtonUM(
             iconRes = R.drawable.ic_back_24,
             onIconClicked = onBackClicked,
+            enabled = enabled,
         )
     }
 }
