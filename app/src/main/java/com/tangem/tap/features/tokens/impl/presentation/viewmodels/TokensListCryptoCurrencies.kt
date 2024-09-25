ba506fcf7ae80cff7ed0fa18dd1b8b6478ddf520
Tangem <git@tangem.com>
2024-09-25 18:34:28 +0400

    Updated on 2026-09-21

--- app/src/main/java/com/tangem/tap/features/tokens/impl/presentation/viewmodels/TokensListCryptoCurrencies.kt
+++
@@ -1,9 +0,0 @@
-package com.tangem.tap.features.tokens.impl.presentation.viewmodels
-
-import com.tangem.blockchain.common.Blockchain
-import com.tangem.domain.tokens.TokenWithBlockchain
-
-internal data class TokensListCryptoCurrencies(
-    val coins: List<Blockchain>,
-    val tokens: List<TokenWithBlockchain>,
-)
