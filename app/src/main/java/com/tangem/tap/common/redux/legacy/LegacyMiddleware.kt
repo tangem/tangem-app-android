e643e7aa9abe03022554410dd483b11dcd8945df
Tangem <git@tangem.com>
2024-09-25 22:01:06 +0300

    Updated on 2025-09-01

--- app/src/main/java/com/tangem/tap/common/redux/legacy/LegacyMiddleware.kt
+++ app/src/main/java/com/tangem/tap/common/redux/legacy/LegacyMiddleware.kt
@@ -1,12 +1,6 @@
 package com.tangem.tap.common.redux.legacy
 
-import com.tangem.blockchain.common.AmountType
-import com.tangem.domain.feedback.models.BlockchainErrorInfo
-import com.tangem.domain.feedback.models.CardInfo
-import com.tangem.domain.feedback.models.FeedbackEmailType
-import com.tangem.domain.models.scan.ScanResponse
 import com.tangem.domain.redux.LegacyAction
-import com.tangem.domain.utils.convertToSdkAmount
 import com.tangem.tap.common.extensions.dispatchWithMain
 import com.tangem.tap.common.extensions.inject
 import com.tangem.tap.common.redux.AppState
@@ -17,7 +11,6 @@ import com.tangem.tap.scope
 import com.tangem.tap.store
 import com.tangem.utils.coroutines.JobHolder
 import com.tangem.utils.coroutines.saveIn
-import com.tangem.utils.extensions.stripZeroPlainString
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flowOn
@@ -37,42 +30,6 @@ internal object LegacyMiddleware {
                             GlobalAction.Onboarding.Start(
                                 scanResponse = action.scanResponse,
                                 canSkipBackup = action.canSkipBackup,
-                            ),
-                        )
-                    }
-                    is LegacyAction.SendEmailRateCanBeBetter -> {
-                        store.state.globalState.feedbackManager?.sendEmail(
-                            type = FeedbackEmailType.RateCanBeBetter(cardInfo = getCardInfo(action.scanResponse)),
-                        )
-                    }
-                    is LegacyAction.SendEmailSupport -> {
-                        store.state.globalState.feedbackManager?.sendEmail(
-                            type = FeedbackEmailType.DirectUserRequest(cardInfo = getCardInfo(action.scanResponse)),
-                        )
-                    }
-                    is LegacyAction.SendEmailTransactionFailed -> {
-                        val amount = action.amount?.convertToSdkAmount(action.cryptoCurrency)
-
-                        store.inject(DaggerGraphState::saveBlockchainErrorUseCase).invoke(
-                            error = BlockchainErrorInfo(
-                                errorMessage = action.errorMessage,
-                                blockchainId = action.cryptoCurrency.network.id.value,
-                                derivationPath = action.cryptoCurrency.network.derivationPath.value,
-                                destinationAddress = action.destinationAddress.orEmpty(),
-                                tokenSymbol = if (amount?.type is AmountType.Token) {
-                                    amount.currencySymbol
-                                } else {
-                                    ""
-                                },
-                                amount = amount?.value?.stripZeroPlainString() ?: "unknown",
-                                fee = action.fee?.convertToSdkAmount(action.cryptoCurrency)
-                                    ?.value?.stripZeroPlainString() ?: "unknown",
-                            ),
-                        )
-
-                        store.state.globalState.feedbackManager?.sendEmail(
-                            type = FeedbackEmailType.TransactionSendingProblem(
-                                cardInfo = getCardInfo(action.scanResponse),
                             ),
                         )
                     }
@@ -98,11 +55,5 @@ internal object LegacyMiddleware {
                 next(action)
             }
         }
-    }
-
-    private fun getCardInfo(scanResponse: ScanResponse): CardInfo {
-        return store.inject(DaggerGraphState::getCardInfoUseCase).invoke(scanResponse)
-            .getOrNull()
-            ?: error("Card info not found")
     }
 }
