3eca68ed5c5305ce9b088c780b6dd309406b519a
Tangem <git@tangem.com>
2024-09-25 17:00:13 +0300

    release/5.26.1

--- app/src/main/java/com/tangem/tap/common/feedback/LegacyFeedbackManager.kt
+++ app/src/main/java/com/tangem/tap/common/feedback/LegacyFeedbackManager.kt
@@ -1,33 +1,20 @@
 package com.tangem.tap.common.feedback
 
-import com.tangem.core.navigation.email.EmailSender
-import com.tangem.domain.feedback.GetFeedbackEmailUseCase
+import com.tangem.domain.feedback.SendFeedbackEmailUseCase
 import com.tangem.domain.feedback.models.FeedbackEmailType
-import com.tangem.tap.common.extensions.inject
-import com.tangem.tap.proxy.redux.DaggerGraphState
 import com.tangem.tap.scope
-import com.tangem.tap.store
 import kotlinx.coroutines.launch
 
 /**
 * [REDACTED_AUTHOR]
  */
 class LegacyFeedbackManager(
-    private val getFeedbackEmailUseCase: GetFeedbackEmailUseCase,
+    private val sendFeedbackEmailUseCase: SendFeedbackEmailUseCase,
 ) {
-// [REDACTED_TODO_COMMENT]
+
     fun sendEmail(type: FeedbackEmailType) {
         scope.launch {
-            val email = getFeedbackEmailUseCase(type = type)
-
-            store.inject(DaggerGraphState::emailSender).send(
-                email = EmailSender.Email(
-                    address = email.address,
-                    subject = email.subject,
-                    message = email.message,
-                    attachment = email.file,
-                ),
-            )
+            sendFeedbackEmailUseCase(type = type)
         }
     }
 }
