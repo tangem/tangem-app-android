1deb66310d81c74d96f99eba59cd4f7905e1441e
Tangem <git@tangem.com>
2024-09-27 16:34:57 +0500

    Updated on 2026-06-10

--- data/staking/src/main/java/com/tangem/data/staking/DefaultStakingRepository.kt
+++ data/staking/src/main/java/com/tangem/data/staking/DefaultStakingRepository.kt
@@ -121,7 +121,7 @@ internal class DefaultStakingRepository(
                 key = YIELDS_STORE_KEY,
                 skipCache = refresh,
                 block = {
-                    val stakingTokensWithYields = stakeKitApi.getMultipleYields(preferredValidatorsOnly = true)
+                    val stakingTokensWithYields = stakeKitApi.getMultipleYields(preferredValidatorsOnly = false)
                         .getOrThrow()
 
                     stakingYieldsStore.store(stakingTokensWithYields.data)
