4a536b7ffc1303c48f08d9e74b484a343238cba5
Nikita Samoylov <nikit.samoylov@gmail.com>
2024-09-29 19:57:56 +0300

    fix tests

--- core/ui/src/main/java/com/tangem/core/ui/format/bigdecimal/BigDecimalFormatConstants.kt
+++ core/ui/src/main/java/com/tangem/core/ui/format/bigdecimal/BigDecimalFormatConstants.kt
@@ -14,11 +14,13 @@ object BigDecimalFormatConstants {
     val FORMAT_THRESHOLD = BigDecimal("0.01")
 
     val CURRENCY_SPACE by lazy {
-        val formatter = NumberFormat.getCurrencyInstance(Locale.GERMAN)
+        val formatter = NumberFormat.getCurrencyInstance(Locale.GERMAN).apply {
+            currency = Currency.getInstance("USD")
+        }
         formatter.format(BigDecimal.TEN).firstOrNull { it.isWhitespace() } ?: '\u00a0'
     }
 
     val CRYPTO_FEE_FORMAT_THRESHOLD = BigDecimal("0.000001")
 
-    val usdCurrency = Currency.getInstance("USD")
+    val usdCurrency: Currency = Currency.getInstance("USD")
 }
--- core/ui/src/test/kotlin/com/tangem/core/ui/format/bigdecimal/Utils.kt
+++ core/ui/src/test/kotlin/com/tangem/core/ui/format/bigdecimal/Utils.kt
@@ -2,10 +2,13 @@ package com.tangem.core.ui.format.bigdecimal
 
 import java.math.BigDecimal
 import java.text.NumberFormat
+import java.util.Currency
 import java.util.Locale
 
 internal val CURRENCY_SPACE_FOR_TESTS by lazy {
-    val formatter = NumberFormat.getCurrencyInstance(Locale.GERMAN)
+    val formatter = NumberFormat.getCurrencyInstance(Locale.GERMAN).apply {
+        currency = Currency.getInstance("USD")
+    }
     formatter.format(BigDecimal.TEN).firstOrNull { it.isWhitespace() } ?: '\u00a0'
 }
 
