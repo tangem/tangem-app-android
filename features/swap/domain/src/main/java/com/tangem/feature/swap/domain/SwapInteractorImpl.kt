8384e07a95b4ea9d7576eb620e0f792b9ce20427
iMaks99 <maks.ismoilov0618@gmail.com>
2024-09-30 17:19:39 +0500

    AND-8544 [Swap] Refactored notifications using common NotificationUM

--- features/swap/domain/src/main/java/com/tangem/feature/swap/domain/SwapInteractorImpl.kt
+++ features/swap/domain/src/main/java/com/tangem/feature/swap/domain/SwapInteractorImpl.kt
@@ -1078,7 +1078,7 @@ internal class SwapInteractorImpl @AssistedInject constructor(
                 isAllowedToSpend = isAllowedToSpend,
                 isBalanceWithoutFeeEnough = isBalanceWithoutFeeEnough,
                 txFee = txFee,
-                transactionFee = txFeeResult?.getOrNull(),
+                transactionFee = txFeeResult.getOrNull(),
                 includeFeeInAmount = includeFeeInAmount,
             )
         }
