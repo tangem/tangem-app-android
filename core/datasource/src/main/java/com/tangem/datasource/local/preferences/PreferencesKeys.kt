2a5d18d70307a66bba04fd9955f2e8a15685e4f3
Tangem <git@tangem.com>
2024-10-06 12:16:42 +0400

    Updated on 2026-06-02

--- core/datasource/src/main/java/com/tangem/datasource/local/preferences/PreferencesKeys.kt
+++ core/datasource/src/main/java/com/tangem/datasource/local/preferences/PreferencesKeys.kt
@@ -99,18 +99,13 @@ object PreferencesKeys {
 
     val apiConfigsEnvironmentKey by lazy { stringPreferencesKey(name = "apiConfigsEnvironment") }
 
+    val IS_RING_ADDED_KEY by lazy { booleanPreferencesKey(name = "isRingAdded") }
+
     // region Permission
     fun getShouldShowPermission(permission: String) = booleanPreferencesKey("shouldShowPushPermission_$permission")
 
     fun getShouldShowInitialPermissionScreen(permission: String) =
         booleanPreferencesKey("shouldShowInitialPushPermissionScreen_$permission")
-
-    fun getIsFirstTimeAskingPermission(permission: String) =
-        booleanPreferencesKey("shouldAskInitialPushPermission_$permission")
-
-    fun getPermissionLaunchCount(permission: String) = intPreferencesKey("pushPermissionLaunchCount_$permission")
-
-    fun getPermissionDaysCount(permission: String) = longPreferencesKey("pushPermissionDaysCount_$permission")
     // endregion
 
     fun getUserTokensKey(userWalletId: String) = stringPreferencesKey(name = "user_tokens_$userWalletId")
