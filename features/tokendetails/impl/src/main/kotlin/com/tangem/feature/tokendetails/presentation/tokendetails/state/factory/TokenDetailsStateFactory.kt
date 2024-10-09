e3a458b3b4e592ea5ea038cafa3789649abe0ea7
Tangem <git@tangem.com>
2024-10-09 12:10:19 +0300

    Updated on 2026-09-01

--- features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/factory/TokenDetailsStateFactory.kt
+++ features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/factory/TokenDetailsStateFactory.kt
@@ -17,6 +17,7 @@ import com.tangem.core.ui.res.TangemTheme
 import com.tangem.domain.appcurrency.model.AppCurrency
 import com.tangem.domain.card.NetworkHasDerivationUseCase
 import com.tangem.domain.common.CardTypesResolver
+import com.tangem.domain.staking.GetStakingAvailabilityUseCase
 import com.tangem.domain.staking.GetStakingIntegrationIdUseCase
 import com.tangem.domain.staking.model.StakingAvailability
 import com.tangem.domain.staking.model.StakingEntryInfo
@@ -60,6 +61,7 @@ internal class TokenDetailsStateFactory(
     private val userWalletId: UserWalletId,
     stakingFeatureToggles: StakingFeatureToggles,
     getStakingIntegrationIdUseCase: GetStakingIntegrationIdUseCase,
+    getStakingAvailabilityUseCase: GetStakingAvailabilityUseCase,
     symbol: String,
     decimals: Int,
 ) {
@@ -69,6 +71,7 @@ internal class TokenDetailsStateFactory(
             clickIntents = clickIntents,
             networkHasDerivationUseCase = networkHasDerivationUseCase,
             getStakingIntegrationIdUseCase = getStakingIntegrationIdUseCase,
+            getStakingAvailabilityUseCase = getStakingAvailabilityUseCase,
             getUserWalletUseCase = getUserWalletUseCase,
             userWalletId = userWalletId,
         )
