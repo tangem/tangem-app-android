be791d91e7916a6285447193b14b23be095e5b90
Tangem <git@tangem.com>
2024-10-10 14:39:13 +0300

    Updated on 2026-09-21

--- domain/staking/models/src/main/kotlin/com/tangem/domain/staking/model/StakingToken.kt
+++
@@ -1,9 +0,0 @@
-package com.tangem.domain.staking.model
-
-data class StakingToken(
-    val name: String,
-    val symbol: String,
-    val decimals: Int,
-    val contractAddress: String?,
-    val coinGeckoId: String?,
-)
