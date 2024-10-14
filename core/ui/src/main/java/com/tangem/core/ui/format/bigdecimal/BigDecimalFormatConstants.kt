2e68510c2a36d53d03785f047c9cfb821cde8418
Nikita Samoylov <nikit.samoylov@gmail.com>
2024-10-14 13:40:28 +0300

    test

--- core/ui/src/main/java/com/tangem/core/ui/format/bigdecimal/BigDecimalFormatConstants.kt
+++ core/ui/src/main/java/com/tangem/core/ui/format/bigdecimal/BigDecimalFormatConstants.kt
@@ -3,9 +3,7 @@ package com.tangem.core.ui.format.bigdecimal
 import com.tangem.utils.StringsSigns.DASH_SIGN
 import com.tangem.utils.StringsSigns.LOWER_SIGN
 import java.math.BigDecimal
-import java.text.NumberFormat
 import java.util.Currency
-import java.util.Locale
 
 object BigDecimalFormatConstants {
 
@@ -13,14 +11,9 @@ object BigDecimalFormatConstants {
     const val CAN_BE_LOWER_SIGN = LOWER_SIGN
     val FORMAT_THRESHOLD = BigDecimal("0.01")
 
-    val CURRENCY_SPACE by lazy {
-        val formatter = NumberFormat.getCurrencyInstance(Locale.GERMAN).apply {
-            currency = Currency.getInstance("USD")
-        }
-        formatter.format(BigDecimal.TEN).firstOrNull { it.isWhitespace() } ?: '\u00a0'
-    }
+    const val CURRENCY_SPACE = '\u00a0'
 
     val CRYPTO_FEE_FORMAT_THRESHOLD = BigDecimal("0.000001")
 
-    val usdCurrency: Currency = Currency.getInstance("USD")
+    val usdCurrency: Currency by lazy { Currency.getInstance("USD") }
 }
--- core/ui/src/test/kotlin/com/tangem/core/ui/format/bigdecimal/Utils.kt
+++ core/ui/src/test/kotlin/com/tangem/core/ui/format/bigdecimal/Utils.kt
@@ -1,16 +1,6 @@
 package com.tangem.core.ui.format.bigdecimal
 
-import java.math.BigDecimal
-import java.text.NumberFormat
-import java.util.Currency
-import java.util.Locale
-
-internal val CURRENCY_SPACE_FOR_TESTS by lazy {
-    val formatter = NumberFormat.getCurrencyInstance(Locale.GERMAN).apply {
-        currency = Currency.getInstance("USD")
-    }
-    formatter.format(BigDecimal.TEN).firstOrNull { it.isWhitespace() } ?: '\u00a0'
-}
+internal const val CURRENCY_SPACE_FOR_TESTS = '\u00a0'
 
 internal fun String.addSymbolWithSpaceRight(symbol: String): String = "$this$CURRENCY_SPACE_FOR_TESTS$symbol"
 
