af5a1d8751ada4e520ea8145c3615dcfe815f3f9
Tangem <git@tangem.com>
2024-10-24 12:18:09 +0400

    Updated on 2026-09-14

---
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/tokenlist/utils/SearchTokensManager.kt
@@ -0,0 +1,29 @@
+package com.tangem.feature.wallet.presentation.tokenlist.utils
+
+import com.tangem.utils.coroutines.JobHolder
+import com.tangem.utils.coroutines.withDebounce
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import javax.inject.Inject
+
+/**
+ * Search tokens manager
+ *
+[REDACTED_AUTHOR]
+ */
+internal class SearchTokensManager @Inject constructor() {
+
+    val query: Flow<String>
+        get() = _query
+
+    private val _query = MutableStateFlow(value = "")
+
+    private val jobHolder = JobHolder()
+
+    suspend fun update(value: String) {
+        coroutineScope {
+            withDebounce(jobHolder) { _query.value = value }
+        }
+    }
+}
