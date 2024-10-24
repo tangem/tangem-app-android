0d459e33049e1fe1befbd1fedc739ff5bf0b72d2
Mama1emon <andrew.khokhlove@gmail.com>
2024-10-24 12:18:09 +0400

    AND-8840 [Actions] Add TokenListComponent
    Signed-off-by: Mama1emon <andrew.khokhlove@gmail.com>

---
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/tokenlist/utils/SearchTokensManager.kt
@@ -0,0 +1,29 @@
+package com.tangem.feature.wallet.presentation.tokenlist.utils
+
+import com.tangem.utils.coroutines.JobHolder
+import com.tangem.utils.coroutines.withDebounce
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import javax.inject.Inject
+
+/**
+ * Search tokens manager
+ *
+ * @author Andrew Khokhlov on 23/10/2024
+ */
+internal class SearchTokensManager @Inject constructor() {
+
+    val query: Flow<String>
+        get() = _query
+
+    private val _query = MutableStateFlow(value = "")
+
+    private val jobHolder = JobHolder()
+
+    suspend fun update(value: String) {
+        coroutineScope {
+            withDebounce(jobHolder) { _query.value = value }
+        }
+    }
+}
