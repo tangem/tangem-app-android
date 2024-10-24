17b9ca4af42f2afd636356760f8b60cdfb7c9365
Tangem <git@tangem.com>
2024-10-24 12:18:09 +0400

    Updated on 2026-05-14

--- core/utils/src/main/java/com/tangem/utils/coroutines/JobHolder.kt
+++ core/utils/src/main/java/com/tangem/utils/coroutines/JobHolder.kt
@@ -1,6 +1,9 @@
 package com.tangem.utils.coroutines
 
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 /**
  * Job holder. It is automatically finished old job if new one is started
@@ -27,3 +30,12 @@ class JobHolder {
 fun Job.saveIn(jobHolder: JobHolder): Job = jobHolder.update(job = this)
 
 suspend fun Job.saveInAndJoin(jobHolder: JobHolder) = saveIn(jobHolder).join()
+
+fun CoroutineScope.withDebounce(jobHolder: JobHolder, timeMillis: Long = 800L, function: () -> Unit) {
+    launch {
+        delay(timeMillis = timeMillis)
+
+        function()
+    }
+        .saveIn(jobHolder)
+}
