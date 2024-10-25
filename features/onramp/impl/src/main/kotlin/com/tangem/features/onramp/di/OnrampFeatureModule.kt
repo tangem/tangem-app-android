0311a16ea9be5bfb89d7667e14b5ff7171fdb181
Tangem <git@tangem.com>
2024-10-25 14:18:36 +0500

    Updated on 2026-08-27

--- features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/di/OnrampFeatureModule.kt
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/di/OnrampFeatureModule.kt
@@ -11,7 +11,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-internal object FeatureModule {
+internal object OnrampFeatureModule {
 
     @Provides
     @Singleton
