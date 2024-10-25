a960513030d5e2a257ad36cc4aa1c8a8d6d49476
Tangem <git@tangem.com>
2024-10-25 14:57:41 +0300

    release/5.26.1

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/GetCurrencyCheckUseCase.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/GetCurrencyCheckUseCase.kt
@@ -4,10 +4,13 @@ import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.domain.tokens.model.warnings.CryptoCurrencyCheck
 import com.tangem.domain.tokens.repository.CurrencyChecksRepository
 import com.tangem.domain.wallets.models.UserWalletId
+import com.tangem.utils.coroutines.CoroutineDispatcherProvider
+import kotlinx.coroutines.withContext
 import java.math.BigDecimal
 
 class GetCurrencyCheckUseCase(
     private val currencyChecksRepository: CurrencyChecksRepository,
+    private val dispatchers: CoroutineDispatcherProvider,
 ) {
 
     suspend operator fun invoke(
@@ -17,34 +20,36 @@ class GetCurrencyCheckUseCase(
         fee: BigDecimal?,
         recipientAddress: String? = null,
     ): CryptoCurrencyCheck {
-        val network = currencyStatus.currency.network
-        val dustValue = currencyChecksRepository.getDustValue(userWalletId, network)
-        val reserveAmount = currencyChecksRepository.getReserveAmount(userWalletId, network)
-        val existentialDeposit = currencyChecksRepository.getExistentialDeposit(userWalletId, network)
-        val isAccountFunded = recipientAddress?.let {
-            currencyChecksRepository.checkIfAccountFunded(
-                userWalletId,
-                network,
-                recipientAddress,
-            )
-        } ?: false
-        val utxoAmountLimit = if (amount != null && fee != null) {
-            currencyChecksRepository.checkUtxoAmountLimit(
-                userWalletId = userWalletId,
-                network = network,
-                amount = amount,
-                fee = fee,
-            )
-        } else {
-            null
-        }
+        return withContext(dispatchers.io) {
+            val network = currencyStatus.currency.network
+            val dustValue = currencyChecksRepository.getDustValue(userWalletId, network)
+            val reserveAmount = currencyChecksRepository.getReserveAmount(userWalletId, network)
+            val existentialDeposit = currencyChecksRepository.getExistentialDeposit(userWalletId, network)
+            val isAccountFunded = recipientAddress?.let {
+                currencyChecksRepository.checkIfAccountFunded(
+                    userWalletId,
+                    network,
+                    recipientAddress,
+                )
+            } ?: false
+            val utxoAmountLimit = if (amount != null && fee != null) {
+                currencyChecksRepository.checkUtxoAmountLimit(
+                    userWalletId = userWalletId,
+                    network = network,
+                    amount = amount,
+                    fee = fee,
+                )
+            } else {
+                null
+            }
 
-        return CryptoCurrencyCheck(
-            dustValue = dustValue,
-            reserveAmount = reserveAmount,
-            existentialDeposit = existentialDeposit,
-            utxoAmountLimit = utxoAmountLimit,
-            isAccountFunded = isAccountFunded,
-        )
+            CryptoCurrencyCheck(
+                dustValue = dustValue,
+                reserveAmount = reserveAmount,
+                existentialDeposit = existentialDeposit,
+                utxoAmountLimit = utxoAmountLimit,
+                isAccountFunded = isAccountFunded,
+            )
+        }
     }
 }
