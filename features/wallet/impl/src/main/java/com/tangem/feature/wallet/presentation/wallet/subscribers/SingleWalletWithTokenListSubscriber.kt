c6baf159a99e2b3fb38766423939ab8521cefb81
Tangem <git@tangem.com>
2024-10-28 17:27:49 +0400

    Updated on 2026-08-19

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/subscribers/SingleWalletWithTokenListSubscriber.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/subscribers/SingleWalletWithTokenListSubscriber.kt
@@ -2,23 +2,21 @@ package com.tangem.feature.wallet.presentation.wallet.subscribers
 
 import com.tangem.domain.appcurrency.GetSelectedAppCurrencyUseCase
 import com.tangem.domain.core.lce.LceFlow
-import com.tangem.domain.core.utils.toLce
-import com.tangem.domain.tokens.GetNodlTokenListUseCase
 import com.tangem.domain.tokens.RunPolkadotAccountHealthCheckUseCase
 import com.tangem.domain.tokens.error.TokenListError
 import com.tangem.domain.tokens.model.TokenList
 import com.tangem.domain.wallets.models.UserWallet
 import com.tangem.feature.wallet.presentation.wallet.analytics.utils.TokenListAnalyticsSender
+import com.tangem.feature.wallet.presentation.wallet.domain.MultiWalletTokenListStore
 import com.tangem.feature.wallet.presentation.wallet.domain.WalletWithFundsChecker
 import com.tangem.feature.wallet.presentation.wallet.state.WalletStateController
 import com.tangem.feature.wallet.presentation.wallet.viewmodels.intents.WalletClickIntents
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.map
 
 @Suppress("LongParameterList")
 internal class SingleWalletWithTokenListSubscriber(
     private val userWallet: UserWallet,
-    private val getNodlTokenListUseCase: GetNodlTokenListUseCase,
+    private val tokenListStore: MultiWalletTokenListStore,
     stateHolder: WalletStateController,
     clickIntents: WalletClickIntents,
     tokenListAnalyticsSender: TokenListAnalyticsSender,
@@ -35,9 +33,9 @@ internal class SingleWalletWithTokenListSubscriber(
     runPolkadotAccountHealthCheckUseCase = runPolkadotAccountHealthCheckUseCase,
 ) {
 
-    override fun tokenListFlow(coroutineScope: CoroutineScope): LceFlow<TokenListError, TokenList> =
-        getNodlTokenListUseCase(
-            userWallet.walletId,
-        )
-            .map { it.toLce() }
+    override fun tokenListFlow(coroutineScope: CoroutineScope): LceFlow<TokenListError, TokenList> {
+        tokenListStore.addIfNot(userWallet.walletId, coroutineScope)
+
+        return tokenListStore.getOrThrow(userWallet.walletId)
+    }
 }
