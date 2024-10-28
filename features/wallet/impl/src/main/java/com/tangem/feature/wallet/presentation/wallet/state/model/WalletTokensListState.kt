e81ea4066833c28d30b97dbb25e4b2a88e94acd6
Tangem <git@tangem.com>
2024-10-28 15:36:35 +0400

    Updated on 2025-09-01

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/model/WalletTokensListState.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/model/WalletTokensListState.kt
@@ -1,8 +1,8 @@
 package com.tangem.feature.wallet.presentation.wallet.state.model
 
 import androidx.compose.runtime.Immutable
-import com.tangem.core.ui.components.fields.entity.SearchBarUM
 import com.tangem.core.ui.components.token.state.TokenItemState
+import com.tangem.core.ui.components.tokenlist.state.TokensListItemUM
 import com.tangem.core.ui.extensions.TextReference
 import com.tangem.feature.wallet.impl.R
 import kotlinx.collections.immutable.ImmutableList
@@ -15,44 +15,27 @@ internal sealed class WalletTokensListState {
 
     sealed class ContentState : WalletTokensListState() {
 
-        abstract val items: ImmutableList<TokensListItemState>
+        abstract val items: ImmutableList<TokensListItemUM>
         abstract val organizeTokensButtonConfig: OrganizeTokensButtonConfig?
 
         data object Loading : ContentState() {
-            override val items = persistentListOf<TokensListItemState>()
+            override val items = persistentListOf<TokensListItemUM>()
             override val organizeTokensButtonConfig = null
         }
 
         data class Content(
-            override val items: ImmutableList<TokensListItemState>,
+            override val items: ImmutableList<TokensListItemUM>,
             override val organizeTokensButtonConfig: OrganizeTokensButtonConfig?,
         ) : ContentState()
 
         data object Locked : ContentState() {
             override val items = persistentListOf(
-                TokensListItemState.NetworkGroupTitle(id = 42, name = TextReference.Res(id = R.string.main_tokens)),
-                TokensListItemState.Token(state = TokenItemState.Locked(id = "Locked#1")),
+                TokensListItemUM.NetworkGroupTitle(id = 42, name = TextReference.Res(id = R.string.main_tokens)),
+                TokensListItemUM.Token(state = TokenItemState.Locked(id = "Locked#1")),
             )
             override val organizeTokensButtonConfig = null
         }
     }
 
     data class OrganizeTokensButtonConfig(val isEnabled: Boolean, val onClick: () -> Unit)
-
-    @Immutable
-    sealed class TokensListItemState {
-
-        abstract val id: Any
-
-        data class SearchBar(
-            override val id: Any = "search_bar",
-            val searchBarUM: SearchBarUM,
-        ) : TokensListItemState()
-
-        data class NetworkGroupTitle(override val id: Int, val name: TextReference) : TokensListItemState()
-
-        data class Token(val state: TokenItemState) : TokensListItemState() {
-            override val id: String = state.id
-        }
-    }
 }
