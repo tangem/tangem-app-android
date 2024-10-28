672e978fd0f5236a809d6304cf55fbb331eec25b
Tangem <git@tangem.com>
2024-10-28 13:22:03 +0400

    Updated on 2026-06-02

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/tokenlist/TokenListComponent.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/tokenlist/TokenListComponent.kt
@@ -1,14 +1,19 @@
 package com.tangem.feature.wallet.presentation.tokenlist
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
+import androidx.compose.ui.Modifier
 import com.tangem.core.decompose.factory.ComponentFactory
-import com.tangem.core.ui.decompose.ComposableContentComponent
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.domain.wallets.models.UserWalletId
 
 /** Token list component that present list of token for multi-currency wallet */
 @Stable
-internal interface TokenListComponent : ComposableContentComponent {
+internal interface TokenListComponent {
+
+    @Composable
+    fun Content(contentPadding: PaddingValues, modifier: Modifier)
 
     /** Component factory */
     interface Factory : ComponentFactory<Params, TokenListComponent>
@@ -16,9 +21,9 @@ internal interface TokenListComponent : ComposableContentComponent {
     /**
      * Params
      *
-     * @property hasSearchBar flag that indicates if search bar should be shown
-     * @property userWalletId id of multi-currency wallet
-     * @property onTokenClick callback for token click
+     * @property hasSearchBar   flag that indicates if search bar should be shown
+     * @property userWalletId   id of multi-currency wallet
+     * @property onTokenClick   callback for token click
      */
     data class Params(
         val hasSearchBar: Boolean,
