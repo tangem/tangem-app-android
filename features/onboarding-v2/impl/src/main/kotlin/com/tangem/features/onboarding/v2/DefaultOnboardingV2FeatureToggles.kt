8f3e7184365bb0d66d54f44697fb65557b945072
Tangem <git@tangem.com>
2024-10-28 17:46:06 +0300

    Updated on 2026-09-23

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/DefaultOnboardingV2FeatureToggles.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/DefaultOnboardingV2FeatureToggles.kt
@@ -1,11 +1,11 @@
-package com.tangem.feature.onboarding.legacy.featuretoggle
+package com.tangem.features.onboarding.v2
 
 import com.tangem.core.featuretoggle.manager.FeatureTogglesManager
 import javax.inject.Inject
 
-internal class DefaultOnboardingLegacyFeatureToggles @Inject constructor(
+internal class DefaultOnboardingV2FeatureToggles @Inject constructor(
     private val featureTogglesManager: FeatureTogglesManager,
-) : OnboardingLegacyFeatureToggles {
+) : OnboardingV2FeatureToggles {
     override val isRewrittenOnboardingCodeInUse: Boolean
         get() = featureTogglesManager.isFeatureEnabled("ONBOARDING_CODE_REFACTORING_ENABLED")
 }
