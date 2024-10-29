bdbe73a089e089d8d60c37b2a10e4abec8ee8024
Tangem <git@tangem.com>
2024-10-29 09:18:22 +0200

    Updated on 2026-06-05

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/helpers/StakingTransactionSender.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/helpers/StakingTransactionSender.kt
@@ -227,7 +227,7 @@ internal class StakingTransactionSender @AssistedInject constructor(
                     networkId = cryptoCurrencyStatus.currency.network.id,
                 ).getOrElse { "" }
 
-                balanceUpdater.scheduleUpdates()
+                balanceUpdater.fullUpdate()
                 onSendSuccess(txUrl)
             },
         )
