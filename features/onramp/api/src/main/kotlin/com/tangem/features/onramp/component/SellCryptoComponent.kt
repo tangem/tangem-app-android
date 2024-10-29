7f1739dadf383e28d9990adda41810d4c1e1b234
Tangem <git@tangem.com>
2024-10-29 14:20:14 +0400

    Updated on 2026-06-10

---
+++ features/onramp/api/src/main/kotlin/com/tangem/features/onramp/component/SellCryptoComponent.kt
@@ -0,0 +1,22 @@
+package com.tangem.features.onramp.component
+
+import com.tangem.core.decompose.factory.ComponentFactory
+import com.tangem.core.ui.decompose.ComposableContentComponent
+import com.tangem.domain.wallets.models.UserWalletId
+
+/**
+ * Sell crypto component
+ *
+[REDACTED_AUTHOR]
+ */
+interface SellCryptoComponent : ComposableContentComponent {
+
+    interface Factory : ComponentFactory<Params, SellCryptoComponent>
+
+    /**
+     * Params
+     *
+     * @property userWalletId user wallet id
+     */
+    data class Params(val userWalletId: UserWalletId)
+}
