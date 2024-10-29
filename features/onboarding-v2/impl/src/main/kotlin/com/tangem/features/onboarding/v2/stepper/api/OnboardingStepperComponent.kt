0fcb1b7a0ca62d72afecb2e3c0f3b089a9f6ce85
Tangem <git@tangem.com>
2024-10-29 13:12:57 +0300

    Updated on 2026-05-07

---
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/stepper/api/OnboardingStepperComponent.kt
@@ -0,0 +1,31 @@
+package com.tangem.features.onboarding.v2.stepper.api
+
+import androidx.annotation.IntRange
+import com.tangem.core.decompose.factory.ComponentFactory
+import com.tangem.core.ui.decompose.ComposableContentComponent
+import com.tangem.core.ui.extensions.TextReference
+import com.tangem.domain.models.scan.ScanResponse
+import kotlinx.coroutines.flow.StateFlow
+
+interface OnboardingStepperComponent : ComposableContentComponent {
+
+    data class StepperState(
+        @IntRange(from = 0) val currentStep: Int,
+        @IntRange(from = 0) val steps: Int,
+        val title: TextReference,
+    )
+
+    class Params(
+        val initState: StepperState,
+        val popBack: () -> Unit,
+        val scanResponse: ScanResponse,
+    )
+
+    val state: StateFlow<StepperState>
+
+    fun changeStep(step: Int)
+    fun nextStep()
+    fun changeTitle(title: TextReference)
+
+    interface Factory : ComponentFactory<Params, OnboardingStepperComponent>
+}
