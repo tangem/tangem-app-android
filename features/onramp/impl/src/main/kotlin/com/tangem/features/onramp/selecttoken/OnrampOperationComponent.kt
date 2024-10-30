6d6cc892e7c959b275653c6732377e36ea85db48
Tangem <git@tangem.com>
2024-10-30 13:13:53 +0400

    Updated on 2026-02-02

--- features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/selecttoken/OnrampOperationComponent.kt
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/selecttoken/OnrampOperationComponent.kt
@@ -1,32 +1,29 @@
 package com.tangem.features.onramp.selecttoken
 
-import androidx.annotation.StringRes
 import com.tangem.core.decompose.factory.ComponentFactory
 import com.tangem.core.ui.decompose.ComposableContentComponent
-import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.domain.wallets.models.UserWalletId
+import com.tangem.features.onramp.entity.OnrampOperation
 
 /**
- * Select token component
+ * Base operation component
  *
 [REDACTED_AUTHOR]
  */
-interface OnrampSelectTokenComponent : ComposableContentComponent {
+internal interface OnrampOperationComponent : ComposableContentComponent {
 
-    interface Factory : ComponentFactory<Params, OnrampSelectTokenComponent>
+    interface Factory : ComponentFactory<Params, OnrampOperationComponent>
 
     /**
      * Params
      *
+     * @property operation    operation
      * @property hasSearchBar flag that indicates if search bar should be shown
      * @property userWalletId id of multi-currency wallet
-     * @property titleResId   resource id of title
-     * @property onTokenClick callback for token click
      */
     data class Params(
+        val operation: OnrampOperation,
         val hasSearchBar: Boolean,
         val userWalletId: UserWalletId,
-        @StringRes val titleResId: Int,
-        val onTokenClick: (CryptoCurrencyStatus) -> Unit,
     )
 }
