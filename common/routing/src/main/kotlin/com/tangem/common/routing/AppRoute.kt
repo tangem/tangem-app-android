77255cc88a1557f67d3f52bcfbdbbe7d030f3ab4
Tangem <git@tangem.com>
2024-10-30 10:56:02 +0300

    Updated on 2026-09-23

--- common/routing/src/main/kotlin/com/tangem/common/routing/AppRoute.kt
+++ common/routing/src/main/kotlin/com/tangem/common/routing/AppRoute.kt
@@ -7,6 +7,7 @@ import com.tangem.common.routing.entity.SerializableIntent
 import com.tangem.core.decompose.navigation.Route
 import com.tangem.domain.appcurrency.model.AppCurrency
 import com.tangem.domain.markets.TokenMarketParams
+import com.tangem.domain.models.scan.ScanResponse
 import com.tangem.domain.qrscanning.models.SourceType
 import com.tangem.domain.staking.model.stakekit.Yield
 import com.tangem.domain.tokens.model.CryptoCurrency
@@ -297,4 +298,7 @@ sealed class AppRoute(val path: String) : Route {
     data class SellCrypto(
         val userWalletId: UserWalletId,
     ) : AppRoute(path = "/sell_crypto/${userWalletId.stringValue}")
+
+    // Onboarding V2
+    data class Onboarding(val scanResponse: ScanResponse) : AppRoute(path = "/onboarding_v2")
 }
