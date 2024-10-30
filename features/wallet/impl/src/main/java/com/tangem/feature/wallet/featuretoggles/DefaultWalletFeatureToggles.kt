abb5e356565bff117484fd76ee2698a5b7d6391a
Tangem <git@tangem.com>
2024-10-30 15:07:29 +0400

    Updated on 2026-09-21

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/featuretoggles/DefaultWalletFeatureToggles.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/featuretoggles/DefaultWalletFeatureToggles.kt
@@ -1,6 +1,6 @@
 package com.tangem.feature.wallet.featuretoggles
 
-import com.tangem.core.toggle.feature.FeatureTogglesManager
+import com.tangem.core.configtoggle.feature.FeatureTogglesManager
 import com.tangem.features.wallet.featuretoggles.WalletFeatureToggles
 import javax.inject.Inject
 
