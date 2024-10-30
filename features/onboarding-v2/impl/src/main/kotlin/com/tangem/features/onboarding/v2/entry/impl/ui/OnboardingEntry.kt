ca004e05619b4aa56fd2c3f836826a0ee3c0e035
Tangem <git@tangem.com>
2024-10-30 15:41:46 +0300

    Updated on 2026-06-05

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/entry/impl/ui/OnboardingEntry.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/entry/impl/ui/OnboardingEntry.kt
@@ -1,12 +1,10 @@
 package com.tangem.features.onboarding.v2.entry.impl.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.tangem.core.ui.components.appbar.TangemTopAppBar
 import com.tangem.core.ui.components.appbar.models.TopAppBarButtonUM
@@ -15,7 +13,11 @@ import com.tangem.core.ui.res.TangemThemePreview
 import com.tangem.features.onboarding.v2.impl.R
 
 @Composable
-internal inline fun OnboardingEntry(modifier: Modifier = Modifier, stepperContent: @Composable (Modifier) -> Unit) {
+internal inline fun OnboardingEntry(
+    modifier: Modifier = Modifier,
+    stepperContent: @Composable (Modifier) -> Unit,
+    content: @Composable (Modifier) -> Unit,
+) {
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -23,6 +25,7 @@ internal inline fun OnboardingEntry(modifier: Modifier = Modifier, stepperConten
             .background(TangemTheme.colors.background.primary),
     ) {
         stepperContent(Modifier.fillMaxWidth())
+        content(Modifier.fillMaxSize())
     }
 }
 
@@ -30,15 +33,20 @@ internal inline fun OnboardingEntry(modifier: Modifier = Modifier, stepperConten
 @Composable
 private fun Preview() {
     TangemThemePreview {
-        OnboardingEntry {
-            TangemTopAppBar(
-                title = "Onboarding",
-                startButton = TopAppBarButtonUM(
-                    iconRes = R.drawable.ic_back_24,
-                    enabled = true,
-                    onIconClicked = {},
-                ),
-            )
-        }
+        OnboardingEntry(
+            stepperContent = {
+                TangemTopAppBar(
+                    title = "Onboarding",
+                    startButton = TopAppBarButtonUM(
+                        iconRes = R.drawable.ic_back_24,
+                        enabled = true,
+                        onIconClicked = {},
+                    ),
+                )
+            },
+            content = { modifier ->
+                Box(modifier.background(Color.Cyan.copy(alpha = 0.3f)))
+            },
+        )
     }
 }
