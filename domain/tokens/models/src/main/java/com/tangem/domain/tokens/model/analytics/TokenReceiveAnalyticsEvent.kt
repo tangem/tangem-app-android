c156653d61c45cb832050cccfd09b111d4546cdc
Tangem <git@tangem.com>
2024-10-31 13:32:27 +0500

    release/5.26.1

--- domain/tokens/models/src/main/java/com/tangem/domain/tokens/model/analytics/TokenReceiveAnalyticsEvent.kt
+++ domain/tokens/models/src/main/java/com/tangem/domain/tokens/model/analytics/TokenReceiveAnalyticsEvent.kt
@@ -1,13 +1,17 @@
 package com.tangem.domain.tokens.model.analytics
 
 import com.tangem.core.analytics.models.AnalyticsEvent
+import com.tangem.core.analytics.models.AnalyticsParam.Key.TOKEN_PARAM
 
 sealed class TokenReceiveAnalyticsEvent(
     event: String,
     params: Map<String, String> = mapOf(),
 ) : AnalyticsEvent("Token / Receive", event, params, null) {
 
-    object ReceiveScreenOpened : TokenReceiveAnalyticsEvent(event = "Receive Screen Opened")
+    class ReceiveScreenOpened(token: String) : TokenReceiveAnalyticsEvent(
+        event = "Receive Screen Opened",
+        params = mapOf(TOKEN_PARAM to token),
+    )
 
     class ButtonCopyAddress(token: String) : TokenReceiveAnalyticsEvent(
         event = "Button - Copy Address",
