0f21cab81709dfd38d7050b62ed23d7ad53d49d5
Tangem <git@tangem.com>
2024-11-01 17:47:18 +0200

    release/5.26.2

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/transformers/amount/AmountMaxValueStateTransformer.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/transformers/amount/AmountMaxValueStateTransformer.kt
@@ -1,18 +1,35 @@
 package com.tangem.features.staking.impl.presentation.state.transformers.amount
 
-import com.tangem.common.ui.amountScreen.converters.field.AmountFieldMaxAmountTransformer
+import com.tangem.common.ui.amountScreen.converters.MaxEnterAmountConverter
+import com.tangem.common.ui.amountScreen.converters.field.AmountFieldSetMaxAmountTransformer
+import com.tangem.common.ui.amountScreen.models.MaxEnterAmount
 import com.tangem.domain.staking.model.stakekit.Yield
+import com.tangem.domain.staking.model.stakekit.action.StakingActionCommonType
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.features.staking.impl.presentation.state.StakingUiState
 import com.tangem.utils.transformer.Transformer
 
 internal class AmountMaxValueStateTransformer(
     private val cryptoCurrencyStatus: CryptoCurrencyStatus,
+    private val actionType: StakingActionCommonType,
     private val yield: Yield,
 ) : Transformer<StakingUiState> {
 
+    private val maxEnterAmountConverter = MaxEnterAmountConverter()
+
     override fun transform(prevState: StakingUiState): StakingUiState {
-        val updatedAmountState = AmountFieldMaxAmountTransformer(cryptoCurrencyStatus).transform(prevState.amountState)
+        val maxEnterAmount = if (actionType == StakingActionCommonType.Exit) {
+            MaxEnterAmount(
+                amount = prevState.balanceState?.cryptoAmount,
+                fiatAmount = prevState.balanceState?.fiatAmount,
+                fiatRate = cryptoCurrencyStatus.value.fiatRate,
+            )
+        } else {
+            maxEnterAmountConverter.convert(cryptoCurrencyStatus)
+        }
+
+        val updatedAmountState = AmountFieldSetMaxAmountTransformer(maxEnterAmount)
+            .transform(prevState.amountState)
         return prevState.copy(
             amountState = AmountRequirementStateTransformer(
                 cryptoCurrencyStatus = cryptoCurrencyStatus,
