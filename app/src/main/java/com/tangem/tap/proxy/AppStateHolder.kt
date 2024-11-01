1509986389162bbbacf9303b0facc4b39b3d1c34
Tangem <git@tangem.com>
2024-11-01 08:49:59 +0300

    Updated on 2026-08-19

--- app/src/main/java/com/tangem/tap/proxy/AppStateHolder.kt
+++ app/src/main/java/com/tangem/tap/proxy/AppStateHolder.kt
@@ -5,11 +5,11 @@ import com.tangem.domain.models.scan.ScanResponse
 import com.tangem.domain.redux.ReduxStateHolder
 import com.tangem.domain.redux.StateDialog
 import com.tangem.domain.wallets.models.UserWallet
+import com.tangem.sdk.api.TangemSdkManager
 import com.tangem.tap.common.extensions.dispatchDialogShow
 import com.tangem.tap.common.extensions.dispatchWithMain
 import com.tangem.tap.common.extensions.onUserWalletSelected
 import com.tangem.tap.common.redux.AppState
-import com.tangem.tap.domain.sdk.TangemSdkManager
 import com.tangem.tap.network.exchangeServices.ExchangeService
 import org.rekotlin.Action
 import org.rekotlin.Store
--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/stepper/impl/DefaultOnboardingStepperComponent.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/stepper/impl/DefaultOnboardingStepperComponent.kt
@@ -7,7 +7,6 @@ import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.arkivanov.essenty.instancekeeper.getOrCreateSimple
 import com.tangem.core.decompose.context.AppComponentContext
-import com.tangem.core.ui.extensions.TextReference
 import com.tangem.domain.feedback.GetCardInfoUseCase
 import com.tangem.domain.feedback.SendFeedbackEmailUseCase
 import com.tangem.domain.feedback.models.FeedbackEmailType
@@ -17,7 +16,6 @@ import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 internal class DefaultOnboardingStepperComponent @AssistedInject constructor(
@@ -28,24 +26,6 @@ internal class DefaultOnboardingStepperComponent @AssistedInject constructor(
 ) : OnboardingStepperComponent, AppComponentContext by context {
 
     override val state = instanceKeeper.getOrCreateSimple { MutableStateFlow(params.initState) }
-
-    override fun changeStep(step: Int) {
-        state.update { it.copy(currentStep = step.coerceAtLeast(minimumValue = 0)) }
-    }
-
-    override fun nextStep() {
-        state.update { state ->
-            if (state.currentStep < state.steps - 1) {
-                state.copy(currentStep = state.currentStep + 1)
-            } else {
-                state
-            }
-        }
-    }
-
-    override fun changeTitle(title: TextReference) {
-        state.update { it.copy(title = title) }
-    }
 
     private fun openSupport() {
         componentScope.launch {
