b6d968841530194a736f480e24c67649a4963e89
Tangem <git@tangem.com>
2024-11-01 17:47:18 +0200

    Updated on 2025-11-26

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/transformers/amount/AmountChangeStateTransformer.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/transformers/amount/AmountChangeStateTransformer.kt
@@ -1,7 +1,10 @@
 package com.tangem.features.staking.impl.presentation.state.transformers.amount
 
+import com.tangem.common.ui.amountScreen.converters.MaxEnterAmountConverter
 import com.tangem.common.ui.amountScreen.converters.field.AmountFieldChangeTransformer
+import com.tangem.common.ui.amountScreen.models.MaxEnterAmount
 import com.tangem.domain.staking.model.stakekit.Yield
+import com.tangem.domain.staking.model.stakekit.action.StakingActionCommonType
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.features.staking.impl.presentation.state.StakingUiState
 import com.tangem.utils.transformer.Transformer
@@ -12,11 +15,21 @@ internal class AmountChangeStateTransformer(
     private val yield: Yield,
 ) : Transformer<StakingUiState> {
 
+    private val maxEnterAmountConverter = MaxEnterAmountConverter()
+
     override fun transform(prevState: StakingUiState): StakingUiState {
-        val updatedAmountState = AmountFieldChangeTransformer(
-            cryptoCurrencyStatus,
-            value,
-        ).transform(prevState.amountState)
+        val actionType = prevState.actionType
+        val maxEnterAmount = if (actionType == StakingActionCommonType.Exit) {
+            MaxEnterAmount(
+                amount = prevState.balanceState?.cryptoAmount,
+                fiatAmount = prevState.balanceState?.fiatAmount,
+                fiatRate = cryptoCurrencyStatus.value.fiatRate,
+            )
+        } else {
+            maxEnterAmountConverter.convert(cryptoCurrencyStatus)
+        }
+
+        val updatedAmountState = AmountFieldChangeTransformer(maxEnterAmount, value).transform(prevState.amountState)
 
         return prevState.copy(
             amountState = AmountRequirementStateTransformer(
