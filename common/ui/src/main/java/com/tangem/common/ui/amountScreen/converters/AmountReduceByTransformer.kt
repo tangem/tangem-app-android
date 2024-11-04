e7f6c6ff40c9eff72dca48c6a7a7d7d1e6ef5b07
Tangem <git@tangem.com>
2024-11-05 00:41:17 +0500

    Updated on 2026-07-01

--- common/ui/src/main/java/com/tangem/common/ui/amountScreen/converters/AmountReduceByTransformer.kt
+++ common/ui/src/main/java/com/tangem/common/ui/amountScreen/converters/AmountReduceByTransformer.kt
@@ -4,10 +4,15 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
 import com.tangem.common.ui.R
 import com.tangem.common.ui.amountScreen.models.AmountState
+import com.tangem.common.ui.amountScreen.models.EnterAmountBoundary
 import com.tangem.common.ui.amountScreen.utils.checkExceedBalance
 import com.tangem.common.ui.amountScreen.utils.getFiatValue
 import com.tangem.common.ui.amountScreen.utils.getKeyboardAction
+import com.tangem.core.ui.extensions.TextReference
 import com.tangem.core.ui.extensions.resourceReference
+import com.tangem.core.ui.extensions.wrappedList
+import com.tangem.core.ui.format.bigdecimal.crypto
+import com.tangem.core.ui.format.bigdecimal.format
 import com.tangem.core.ui.utils.parseBigDecimal
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.utils.isNullOrZero
@@ -22,6 +27,7 @@ import java.math.BigDecimal
  */
 class AmountReduceByTransformer(
     private val cryptoCurrencyStatus: CryptoCurrencyStatus,
+    private val minimumTransactionAmount: EnterAmountBoundary?,
     private val value: ReduceByData,
 ) : Transformer<AmountState> {
 
@@ -47,22 +53,37 @@ class AmountReduceByTransformer(
 
         val checkValue = if (amountTextField.isFiatValue) fiatValue else cryptoValue
         val isExceedBalance = checkValue.checkExceedBalance(maxEnterAmount, amountTextField)
+        val isLessThanMinimumIfProvided = minimumTransactionAmount?.amount?.let { decimalCryptoValue < it } ?: false
         val isZero = if (amountTextField.isFiatValue) {
             decimalFiatValue.isNullOrZero()
         } else {
             decimalCryptoValue.isNullOrZero()
         }
+        val isCheckFailed = isExceedBalance || isLessThanMinimumIfProvided
         return prevState.copy(
-            isPrimaryButtonEnabled = !isExceedBalance && !isZero,
+            isPrimaryButtonEnabled = !isZero && !isCheckFailed,
             amountTextField = amountTextField.copy(
                 value = cryptoValue,
                 fiatValue = fiatValue,
-                isError = isExceedBalance,
-                error = resourceReference(R.string.send_validation_amount_exceeds_balance),
+                isError = isCheckFailed,
+                error = when {
+                    isExceedBalance -> resourceReference(R.string.send_validation_amount_exceeds_balance)
+                    isLessThanMinimumIfProvided -> {
+                        val minimumAmount = minimumTransactionAmount
+                            ?.amount
+                            ?.format { crypto(cryptoCurrencyStatus.currency) }
+                            .orEmpty()
+                        resourceReference(
+                            R.string.transfer_notification_invalid_minimum_transaction_amount_text,
+                            wrappedList(minimumAmount, minimumAmount),
+                        )
+                    }
+                    else -> TextReference.EMPTY
+                },
                 cryptoAmount = amountTextField.cryptoAmount.copy(value = decimalCryptoValue),
                 fiatAmount = amountTextField.fiatAmount.copy(value = decimalFiatValue),
                 keyboardOptions = KeyboardOptions(
-                    imeAction = getKeyboardAction(isExceedBalance, decimalCryptoValue),
+                    imeAction = getKeyboardAction(isCheckFailed, decimalCryptoValue),
                     keyboardType = KeyboardType.Number,
                 ),
             ),
