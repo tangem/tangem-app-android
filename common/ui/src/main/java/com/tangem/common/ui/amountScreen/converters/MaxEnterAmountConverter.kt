9e54f40696df3b8ca289e456754c51785e33dd56
Tangem <git@tangem.com>
2024-11-05 00:41:17 +0500

    Updated on 2026-02-25

--- common/ui/src/main/java/com/tangem/common/ui/amountScreen/converters/MaxEnterAmountConverter.kt
+++ common/ui/src/main/java/com/tangem/common/ui/amountScreen/converters/MaxEnterAmountConverter.kt
@@ -1,16 +1,16 @@
 package com.tangem.common.ui.amountScreen.converters
 
-import com.tangem.common.ui.amountScreen.models.MaxEnterAmount
+import com.tangem.common.ui.amountScreen.models.EnterAmountBoundary
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.utils.converter.Converter
 
 /**
- * Converts [CryptoCurrencyStatus] to [MaxEnterAmount]
+ * Converts [CryptoCurrencyStatus] to [EnterAmountBoundary]
  */
-class MaxEnterAmountConverter : Converter<CryptoCurrencyStatus, MaxEnterAmount> {
+class MaxEnterAmountConverter : Converter<CryptoCurrencyStatus, EnterAmountBoundary> {
 
-    override fun convert(value: CryptoCurrencyStatus): MaxEnterAmount {
-        return MaxEnterAmount(
+    override fun convert(value: CryptoCurrencyStatus): EnterAmountBoundary {
+        return EnterAmountBoundary(
             amount = value.value.amount,
             fiatAmount = value.value.fiatAmount,
             fiatRate = value.value.fiatRate,
