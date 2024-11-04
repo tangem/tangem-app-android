5125d340d7953820687d022753570b00ccf269d6
Evgenii Kuzovkin <nzeeei@gmail.com>
2024-11-05 00:41:17 +0500

    AND-8946 Add min transaction amount support

--- features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/state/fields/SendAmountFieldChangeConverter.kt
+++ features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/state/fields/SendAmountFieldChangeConverter.kt
@@ -2,6 +2,7 @@ package com.tangem.features.send.impl.presentation.state.fields
 
 import com.tangem.common.ui.amountScreen.converters.MaxEnterAmountConverter
 import com.tangem.common.ui.amountScreen.converters.field.AmountFieldChangeTransformer
+import com.tangem.common.ui.amountScreen.models.EnterAmountBoundary
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.features.send.impl.presentation.state.SendUiState
 import com.tangem.features.send.impl.presentation.state.StateRouter
@@ -12,21 +13,29 @@ internal class SendAmountFieldChangeConverter(
     private val stateRouterProvider: Provider<StateRouter>,
     private val currentStateProvider: Provider<SendUiState>,
     private val cryptoCurrencyStatusProvider: Provider<CryptoCurrencyStatus>,
+    private val minimumTransactionAmountProvider: Provider<EnterAmountBoundary?>,
 ) : Converter<String, SendUiState> {
 
     private val maxEnterAmountConverter = MaxEnterAmountConverter()
 
     override fun convert(value: String): SendUiState {
         val state = currentStateProvider()
+        val cryptoCurrencyStatus = cryptoCurrencyStatusProvider()
         val isEditState = stateRouterProvider().isEditState
         val amountState = state.getAmountState(isEditState)
 
-        val maxEnterAmount = maxEnterAmountConverter.convert(cryptoCurrencyStatusProvider())
+        val maxEnterAmount = maxEnterAmountConverter.convert(cryptoCurrencyStatus)
+        val minimumTransactionAmount = minimumTransactionAmountProvider()
 
         return state.copyWrapped(
             isEditState = isEditState,
             sendState = state.sendState?.copy(reduceAmountBy = null),
-            amountState = AmountFieldChangeTransformer(maxEnterAmount, value).transform(amountState),
+            amountState = AmountFieldChangeTransformer(
+                cryptoCurrencyStatus = cryptoCurrencyStatus,
+                maxEnterAmount = maxEnterAmount,
+                minimumTransactionAmount = minimumTransactionAmount,
+                value = value,
+            ).transform(amountState),
         )
     }
 }
