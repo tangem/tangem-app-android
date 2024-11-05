21858b3978aff3612a9316712367d94e51c08b96
Tangem <git@tangem.com>
2024-11-05 16:29:41 +0400

    Updated on 2026-09-23

--- app/src/main/java/com/tangem/tap/network/exchangeServices/DefaultRampManager.kt
+++ app/src/main/java/com/tangem/tap/network/exchangeServices/DefaultRampManager.kt
@@ -5,13 +5,18 @@ import com.tangem.domain.models.scan.ScanResponse
 import com.tangem.domain.tokens.model.CryptoCurrency
 import com.tangem.domain.tokens.repository.MarketCryptoCurrencyRepository
 import com.tangem.domain.wallets.models.UserWalletId
+import com.tangem.utils.Provider
+import kotlinx.coroutines.flow.Flow
 
 class DefaultRampManager(
     private val exchangeService: ExchangeService?,
+    private val buyService: Provider<ExchangeService>,
+    private val sellService: Provider<ExchangeService>,
     private val marketsCryptoCurrencyRepository: MarketCryptoCurrencyRepository,
 ) : RampStateManager {
 
     private val cryptoCurrencyConverter = CryptoCurrencyConverter()
+
     override fun availableForBuy(scanResponse: ScanResponse, cryptoCurrency: CryptoCurrency): Boolean {
         return exchangeService?.availableForBuy(
             scanResponse,
@@ -27,5 +32,13 @@ class DefaultRampManager(
 
     override suspend fun availableForSwap(userWalletId: UserWalletId, cryptoCurrency: CryptoCurrency): Boolean {
         return marketsCryptoCurrencyRepository.isExchangeable(userWalletId, cryptoCurrency)
+    }
+
+    override fun getBuyInitializationStatus(): Flow<ExchangeServiceInitializationStatus> {
+        return buyService.invoke().initializationStatus
+    }
+
+    override fun getSellInitializationStatus(): Flow<ExchangeServiceInitializationStatus> {
+        return sellService.invoke().initializationStatus
     }
 }
