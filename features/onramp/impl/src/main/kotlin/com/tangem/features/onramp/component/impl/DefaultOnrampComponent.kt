f23de0300e96f14d3e885e488b9ebaf39a325930
Tangem <git@tangem.com>
2024-11-05 20:07:00 +0500

    release/5.26.1

--- features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/component/impl/DefaultOnrampComponent.kt
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/component/impl/DefaultOnrampComponent.kt
@@ -1,61 +1,62 @@
 package com.tangem.features.onramp.component.impl
 
-import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.decompose.extensions.compose.jetpack.stack.Children
+import com.arkivanov.decompose.extensions.compose.jetpack.stack.animation.stackAnimation
 import com.arkivanov.decompose.extensions.compose.jetpack.subscribeAsState
-import com.arkivanov.decompose.router.slot.childSlot
-import com.arkivanov.decompose.router.slot.dismiss
+import com.arkivanov.decompose.router.stack.StackNavigation
+import com.arkivanov.decompose.router.stack.childStack
 import com.tangem.core.decompose.context.AppComponentContext
 import com.tangem.core.decompose.context.childByContext
-import com.tangem.core.decompose.model.getOrCreateModel
-import com.tangem.core.ui.decompose.ComposableBottomSheetComponent
+import com.tangem.core.ui.decompose.ComposableContentComponent
 import com.tangem.features.onramp.component.OnrampComponent
-import com.tangem.features.onramp.component.ConfirmResidencyComponent
-import com.tangem.features.onramp.entity.OnrampBottomSheetConfig
-import com.tangem.features.onramp.model.OnrampModel
+import com.tangem.features.onramp.entity.OnrampChild
+import com.tangem.features.onramp.settings.OnrampSettingsComponent
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 
+@Suppress("UnusedPrivateMember")
 internal class DefaultOnrampComponent @AssistedInject constructor(
     @Assisted context: AppComponentContext,
-    @Assisted private val params: OnrampComponent.Params,
-    private val confirmResidencyComponentFactory: ConfirmResidencyComponent.Factory,
+    @Assisted params: OnrampComponent.Params,
+    private val settingsComponentFactory: OnrampSettingsComponent.Factory,
 ) : OnrampComponent, AppComponentContext by context {
 
-    private val model: OnrampModel = getOrCreateModel(params)
-    private val confirmResidencyBottomSheetSlot = childSlot(
-        source = model.bottomSheetNavigation,
-        serializer = null,
+    private val navigation = StackNavigation<OnrampChild>()
+    private val contentStack = childStack(
+        key = "onramp_content_stack",
+        source = navigation,
+        serializer = OnrampChild.serializer(),
+        initialConfiguration = OnrampChild.Settings,
         handleBackButton = false,
-        childFactory = ::bottomSheetChild,
+        childFactory = ::screenChild,
     )
 
     @Composable
     override fun Content(modifier: Modifier) {
-        val bottomSheet by confirmResidencyBottomSheetSlot.subscribeAsState()
-        BackHandler(onBack = router::pop)
+        val childStack by contentStack.subscribeAsState()
 
-        bottomSheet.child?.instance?.BottomSheet()
+        Children(
+            stack = childStack,
+            animation = stackAnimation(),
+        ) { child ->
+            child.instance.Content(modifier = modifier)
+        }
     }
 
-    private fun bottomSheetChild(
-        config: OnrampBottomSheetConfig,
-        componentContext: ComponentContext,
-    ): ComposableBottomSheetComponent = when (config) {
-        OnrampBottomSheetConfig.ConfirmResidency -> confirmResidencyComponentFactory.create(
-            context = childByContext(componentContext),
-            params = ConfirmResidencyComponent.Params(
-                countryName = "United States",
-                isOnrampSupported = true,
-                countryFlagUrl = "https://hatscripts.github.io/circle-flags/flags/us.svg",
-                onDismiss = model.bottomSheetNavigation::dismiss,
-            ),
-        )
-    }
+    private fun screenChild(config: OnrampChild, componentContext: ComponentContext): ComposableContentComponent =
+        when (config) {
+            OnrampChild.Settings -> settingsComponentFactory.create(
+                context = childByContext(componentContext),
+                // params = OnrampSettingsComponent.Params(navigation::pop) // todo uncomment after https://tangem.atlassian.net/browse/AND-8407
+                params = OnrampSettingsComponent.Params(router::pop),
+            )
+            OnrampChild.Main -> TODO("https://tangem.atlassian.net/browse/AND-8407")
+        }
 
     @AssistedFactory
     interface Factory : OnrampComponent.Factory {
---
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/settings/entity/OnrampSettingsItemUM.kt
@@ -0,0 +1,5 @@
+package com.tangem.features.onramp.settings.entity
+
+sealed class OnrampSettingsItemUM {
+    data class Residence(val countryName: String, val flagUrl: String, val onClick: () -> Unit) : OnrampSettingsItemUM()
+}
