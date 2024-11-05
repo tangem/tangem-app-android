1582a40fc2414c106cda639fe0c275a55f06537f
Tangem <git@tangem.com>
2024-11-05 15:32:00 +0500

    release/5.26.1

---
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/selectcountry/entity/CountryListUM.kt
@@ -0,0 +1,23 @@
+package com.tangem.features.onramp.selectcountry.entity
+
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
+
+internal data class CountryListUM(val items: ImmutableList<CountriesListItemUM>) {
+
+    /** Get search bar if it exists */
+    fun getSearchBar(): CountriesListItemUM.SearchBar? {
+        return items.firstOrNull() as? CountriesListItemUM.SearchBar
+    }
+
+    /** Get tokens */
+    fun getTokens(): ImmutableList<CountriesListItemUM> {
+        if (getSearchBar() == null) return items
+
+        return if (items.size > 1) {
+            items.subList(fromIndex = 1, toIndex = items.size)
+        } else {
+            persistentListOf()
+        }
+    }
+}
