ba29a7c09a9984ddfdb33065e3b5b83a81ef3e73
Tangem <git@tangem.com>
2024-11-05 10:23:39 +0300

    Updated on 2026-05-19

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/model/OnboardingMultiWalletModel.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/model/OnboardingMultiWalletModel.kt
@@ -1,22 +1,14 @@
 package com.tangem.features.onboarding.v2.multiwallet.impl.model
 
-import com.tangem.common.CompletionResult
-import com.tangem.common.core.TangemSdkError
 import com.tangem.core.decompose.di.ComponentScoped
 import com.tangem.core.decompose.model.Model
 import com.tangem.core.decompose.model.ParamsContainer
-import com.tangem.domain.feedback.GetCardInfoUseCase
-import com.tangem.domain.feedback.SendFeedbackEmailUseCase
-import com.tangem.domain.feedback.models.FeedbackEmailType
 import com.tangem.domain.models.scan.CardDTO
 import com.tangem.features.onboarding.v2.multiwallet.api.OnboardingMultiWalletComponent
-import com.tangem.features.onboarding.v2.multiwallet.impl.ui.state.OnboardingMultiWalletUM
-import com.tangem.sdk.api.TangemSdkManager
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -24,27 +16,14 @@ import javax.inject.Inject
 internal class OnboardingMultiWalletModel @Inject constructor(
     paramsContainer: ParamsContainer,
     override val dispatchers: CoroutineDispatcherProvider,
-    private val tangemSdkManager: TangemSdkManager,
-    private val sendFeedbackEmailUseCase: SendFeedbackEmailUseCase,
-    private val getCardInfoUseCase: GetCardInfoUseCase,
 ) : Model() {
 
     private val params = paramsContainer.require<OnboardingMultiWalletComponent.Params>()
-    private val currentScanResponse = MutableStateFlow(params.scanResponse)
 
     val state = MutableStateFlow(
         OnboardingMultiWalletState(
             currentStep = getInitialStep(),
-        ),
-    )
-
-    val uiState = MutableStateFlow(
-        OnboardingMultiWalletUM(
-            onCreateWalletClick = { createWallet(false) },
-            showSeedPhraseOption = params.withSeedPhraseFlow,
-            onOtherOptionsClick = { /* navigate */ },
-            onBack = { },
-            dialog = null,
+            currentScanResponse = params.scanResponse,
         ),
     )
 
@@ -53,7 +32,7 @@ internal class OnboardingMultiWalletModel @Inject constructor(
     }
 
     private fun getInitialStep(): OnboardingMultiWalletState.Step {
-        val card = currentScanResponse.value.card
+        val card = params.scanResponse.card
 
         return when {
             card.wallets.isNotEmpty() && card.backupStatus == CardDTO.BackupStatus.NoBackup ->
@@ -76,65 +55,6 @@ internal class OnboardingMultiWalletModel @Inject constructor(
                     val title = screenTitleByStep(step)
                     params.titleProvider.changeTitle(title)
                 }
-        }
-    }
-
-    private fun createWallet(shouldReset: Boolean) {
-        modelScope.launch {
-            val result = tangemSdkManager.createProductWallet(
-                scanResponse = currentScanResponse.value,
-                shouldReset = shouldReset,
-            )
-
-            when (result) {
-                is CompletionResult.Success -> {
-                    currentScanResponse.update {
-                        it.copy(
-                            card = result.data.card,
-                            derivedKeys = result.data.derivedKeys,
-                            primaryCard = result.data.primaryCard,
-                        )
-                    }
-
-                    // TODO
-                    // Analytics.send(Onboarding.CreateWallet.WalletCreatedSuccessfully())
-                }
-
-                is CompletionResult.Failure -> {
-                    if (result.error is TangemSdkError.WalletAlreadyCreated) {
-                        // show should reset dialog
-                        handleActivationError()
-                    }
-                }
-            }
-        }
-    }
-
-    private fun handleActivationError() {
-        uiState.update {
-            it.copy(
-                dialog = resetCardDialog(
-                    onConfirm = {
-                        uiState.update { it.copy(dialog = null) }
-                        resetCard()
-                    },
-                    onDismiss = {
-                        uiState.update { it.copy(dialog = null) }
-                    },
-                    onDismissButtonClick = ::navigateToSupportScreen,
-                ),
-            )
-        }
-    }
-
-    private fun resetCard() {
-        createWallet(true)
-    }
-
-    fun navigateToSupportScreen() {
-        modelScope.launch {
-            val cardInfo = getCardInfoUseCase(currentScanResponse.value).getOrNull() ?: return@launch
-            sendFeedbackEmailUseCase(FeedbackEmailType.DirectUserRequest(cardInfo))
         }
     }
 }
