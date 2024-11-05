a5e552c257f18aa1a92995283547d5e2bbfb06ff
Tangem <git@tangem.com>
2024-11-05 10:23:39 +0300

    Updated on 2026-09-23

---
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/createwallet/ui/state/MultiWalletCreateWalletUM.kt
@@ -0,0 +1,20 @@
+package com.tangem.features.onboarding.v2.multiwallet.impl.child.createwallet.ui.state
+
+import com.tangem.core.ui.extensions.TextReference
+
+data class MultiWalletCreateWalletUM(
+    val onCreateWalletClick: () -> Unit,
+    val showSeedPhraseOption: Boolean,
+    val onOtherOptionsClick: () -> Unit,
+    val dialog: Dialog?,
+) {
+    data class Dialog(
+        val title: TextReference,
+        val description: TextReference,
+        val dismissButtonText: TextReference,
+        val confirmButtonText: TextReference,
+        val onConfirm: () -> Unit,
+        val onDismissButtonClick: () -> Unit,
+        val onDismiss: () -> Unit,
+    )
+}
