2d11ae5a1d185dfa935c1959365ef2df14f7e3a4
Tangem <git@tangem.com>
2024-11-05 10:23:39 +0300

    release/5.26.1

---
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/createwallet/ui/MultiWalletCreateWallet.kt
@@ -0,0 +1,92 @@
+package com.tangem.features.onboarding.v2.multiwallet.impl.child.createwallet.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.tangem.core.ui.components.BasicDialog
+import com.tangem.core.ui.components.DialogButtonUM
+import com.tangem.core.ui.components.PrimaryButtonIconEnd
+import com.tangem.core.ui.components.SecondaryButton
+import com.tangem.core.ui.extensions.resolveReference
+import com.tangem.core.ui.res.TangemThemePreview
+import com.tangem.features.onboarding.v2.impl.R
+import com.tangem.features.onboarding.v2.multiwallet.impl.child.createwallet.ui.state.MultiWalletCreateWalletUM
+
+@Composable
+fun MultiWalletCreateWallet(state: MultiWalletCreateWalletUM, modifier: Modifier = Modifier) {
+    if (state.dialog != null) {
+        BasicDialog(
+            title = state.dialog.title.resolveReference(),
+            message = state.dialog.description.resolveReference(),
+            confirmButton = DialogButtonUM(
+                title = state.dialog.confirmButtonText.resolveReference(),
+                onClick = state.dialog.onConfirm,
+            ),
+            dismissButton = DialogButtonUM(
+                title = state.dialog.dismissButtonText.resolveReference(),
+                onClick = state.dialog.onDismissButtonClick,
+            ),
+            onDismissDialog = state.dialog.onDismiss,
+        )
+    }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .navigationBarsPadding()
+            .verticalScroll(rememberScrollState()),
+    ) {
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .weight(1f),
+        ) {
+            Box(
+                Modifier
+                    .align(Alignment.Center)
+                    .size(100.dp)
+                    .background(Color.Black),
+            )
+        }
+
+        PrimaryButtonIconEnd(
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp, bottom = 12.dp)
+                .fillMaxWidth(),
+            iconResId = R.drawable.ic_tangem_24,
+            text = stringResource(R.string.onboarding_create_wallet_button_create_wallet),
+            onClick = state.onCreateWalletClick,
+        )
+
+        SecondaryButton(
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+                .fillMaxWidth(),
+            text = stringResource(R.string.onboarding_create_wallet_options_button_options),
+            onClick = state.onOtherOptionsClick,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    TangemThemePreview {
+        MultiWalletCreateWallet(
+            state = MultiWalletCreateWalletUM(
+                onCreateWalletClick = {},
+                showSeedPhraseOption = true,
+                onOtherOptionsClick = {},
+                dialog = null,
+            ),
+        )
+    }
+}
