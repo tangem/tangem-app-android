5107221b4888c99476bef8a6fa8bc2cacf2989e6
Tangem <git@tangem.com>
2024-11-06 17:20:12 +0500

    Updated on 2026-05-07

--- data/onramp/src/main/java/com/tangem/data/onramp/DefaultOnrampRepository.kt
+++ data/onramp/src/main/java/com/tangem/data/onramp/DefaultOnrampRepository.kt
@@ -4,15 +4,24 @@ import com.tangem.data.onramp.converters.CountryConverter
 import com.tangem.data.onramp.converters.CurrencyConverter
 import com.tangem.datasource.api.common.response.getOrThrow
 import com.tangem.datasource.api.onramp.OnrampApi
+import com.tangem.datasource.api.onramp.models.response.model.OnrampCurrencyDTO
+import com.tangem.datasource.local.preferences.AppPreferencesStore
+import com.tangem.datasource.local.preferences.PreferencesKeys
+import com.tangem.datasource.local.preferences.utils.getObject
+import com.tangem.datasource.local.preferences.utils.getObjectSyncOrNull
+import com.tangem.datasource.local.preferences.utils.storeObject
 import com.tangem.domain.onramp.model.OnrampCountry
 import com.tangem.domain.onramp.model.OnrampCurrency
 import com.tangem.domain.onramp.repositories.OnrampRepository
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 
 internal class DefaultOnrampRepository(
     private val onrampApi: OnrampApi,
     private val dispatchers: CoroutineDispatcherProvider,
+    private val appPreferencesStore: AppPreferencesStore,
 ) : OnrampRepository {
 
     private val currencyConverter = CurrencyConverter()
@@ -28,5 +37,24 @@ internal class DefaultOnrampRepository(
         onrampApi.getCountries()
             .getOrThrow()
             .map(countryConverter::convert)
+    }
+
+    override suspend fun saveDefaultCurrency(currency: OnrampCurrency) {
+        appPreferencesStore.storeObject<OnrampCurrencyDTO>(
+            key = PreferencesKeys.ONRAMP_DEFAULT_CURRENCY,
+            value = currencyConverter.convertBack(currency),
+        )
+    }
+
+    override suspend fun getDefaultCurrencySync(): OnrampCurrency? {
+        return appPreferencesStore
+            .getObjectSyncOrNull<OnrampCurrencyDTO>(PreferencesKeys.ONRAMP_DEFAULT_CURRENCY)
+            ?.let(currencyConverter::convert)
+    }
+
+    override fun getDefaultCurrency(): Flow<OnrampCurrency?> {
+        return appPreferencesStore
+            .getObject<OnrampCurrencyDTO>(PreferencesKeys.ONRAMP_DEFAULT_CURRENCY)
+            .map { it?.let(currencyConverter::convert) }
     }
 }
