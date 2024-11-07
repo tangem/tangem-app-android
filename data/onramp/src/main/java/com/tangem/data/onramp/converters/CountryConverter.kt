2df5aec9bf0fe1aa14fd652eabbf2ad25b7aa144
Tangem <git@tangem.com>
2024-11-07 12:53:30 +0500

    Updated on 2026-02-11

--- data/onramp/src/main/java/com/tangem/data/onramp/converters/CountryConverter.kt
+++ data/onramp/src/main/java/com/tangem/data/onramp/converters/CountryConverter.kt
@@ -2,19 +2,30 @@ package com.tangem.data.onramp.converters
 
 import com.tangem.datasource.api.onramp.models.response.model.OnrampCountryDTO
 import com.tangem.domain.onramp.model.OnrampCountry
-import com.tangem.utils.converter.Converter
+import com.tangem.utils.converter.TwoWayConverter
 
-internal class CountryConverter : Converter<OnrampCountryDTO, OnrampCountry> {
-
-    private val currencyConverter = CurrencyConverter()
+internal class CountryConverter(
+    private val currencyConverter: CurrencyConverter,
+) : TwoWayConverter<OnrampCountryDTO, OnrampCountry> {
 
     override fun convert(value: OnrampCountryDTO) = OnrampCountry(
+        id = "${value.alpha3}-${value.name}",
         name = value.name,
         code = value.code,
         image = value.image,
         alpha3 = value.alpha3,
         continent = value.continent,
         defaultCurrency = currencyConverter.convert(value.defaultCurrency),
+        onrampAvailable = value.onrampAvailable,
+    )
+
+    override fun convertBack(value: OnrampCountry): OnrampCountryDTO = OnrampCountryDTO(
+        name = value.name,
+        code = value.code,
+        image = value.image,
+        alpha3 = value.alpha3,
+        continent = value.continent,
+        defaultCurrency = currencyConverter.convertBack(value.defaultCurrency),
         onrampAvailable = value.onrampAvailable,
     )
 }
