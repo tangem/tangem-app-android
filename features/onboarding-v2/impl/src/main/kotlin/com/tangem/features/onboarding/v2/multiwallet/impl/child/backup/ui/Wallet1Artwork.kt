12b62734c6f63a58f26bf80bfefcf6eef95d8a50
Tangem <git@tangem.com>
2024-11-07 11:56:45 +0300

    Updated on 2026-07-07

---
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/backup/ui/Wallet1Artwork.kt
@@ -0,0 +1,21 @@
+package com.tangem.features.onboarding.v2.multiwallet.impl.child.backup.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tangem.core.ui.res.TangemThemePreview
+
+@Composable
+fun Wallet1Artwork(modifier: Modifier = Modifier) {
+    Box(modifier) {
+    }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    TangemThemePreview {
+        Wallet1Artwork()
+    }
+}
