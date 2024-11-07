7b1b97a5d4492678a05d10d658580ddcdca99c40
Tangem <git@tangem.com>
2024-11-07 12:53:30 +0500

    Updated on 2026-09-01

--- domain/onramp/src/main/java/com/tangem/domain/onramp/repositories/OnrampRepository.kt
+++ domain/onramp/src/main/java/com/tangem/domain/onramp/repositories/OnrampRepository.kt
@@ -7,7 +7,11 @@ import kotlinx.coroutines.flow.Flow
 interface OnrampRepository {
     suspend fun getCurrencies(): List<OnrampCurrency>
     suspend fun getCountries(): List<OnrampCountry>
+    suspend fun getCountryByIp(): OnrampCountry
     suspend fun saveDefaultCurrency(currency: OnrampCurrency)
     suspend fun getDefaultCurrencySync(): OnrampCurrency?
     fun getDefaultCurrency(): Flow<OnrampCurrency?>
+    suspend fun saveDefaultCountry(country: OnrampCountry)
+    suspend fun getDefaultCountrySync(): OnrampCountry?
+    fun getDefaultCountry(): Flow<OnrampCountry?>
 }
