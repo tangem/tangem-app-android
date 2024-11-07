4d8df1c138fc114328f2efd733e734f61a0e8204
Tangem <git@tangem.com>
2024-11-07 12:53:30 +0500

    Updated on 2025-09-25

--- domain/onramp/src/main/java/com/tangem/domain/onramp/OnrampSaveDefaultCurrencyUseCase.kt
+++ domain/onramp/src/main/java/com/tangem/domain/onramp/OnrampSaveDefaultCurrencyUseCase.kt
@@ -3,9 +3,9 @@ package com.tangem.domain.onramp
 import com.tangem.domain.onramp.model.OnrampCurrency
 import com.tangem.domain.onramp.repositories.OnrampRepository
 
-class OnrampSaveDefaultCurrencyUseCase(private val onrampRepository: OnrampRepository) {
+class OnrampSaveDefaultCurrencyUseCase(private val repository: OnrampRepository) {
 
     suspend operator fun invoke(currency: OnrampCurrency) {
-        onrampRepository.saveDefaultCurrency(currency)
+        repository.saveDefaultCurrency(currency)
     }
 }
