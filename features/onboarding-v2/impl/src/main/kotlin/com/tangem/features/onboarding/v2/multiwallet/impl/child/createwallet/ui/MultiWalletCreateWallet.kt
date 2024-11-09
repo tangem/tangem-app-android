e2b5e5b07b107e18c853bb7f0f99f95cd4035150
Tangem <git@tangem.com>
2024-11-09 08:43:41 +0300

    Updated on 2026-08-14

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/createwallet/ui/MultiWalletCreateWallet.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/createwallet/ui/MultiWalletCreateWallet.kt
@@ -1,13 +1,8 @@
 package com.tangem.features.onboarding.v2.multiwallet.impl.child.createwallet.ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,22 +37,9 @@ fun MultiWalletCreateWallet(state: MultiWalletCreateWalletUM, modifier: Modifier
     Column(
         modifier = modifier
             .fillMaxSize()
-            .navigationBarsPadding()
-            .verticalScroll(rememberScrollState()),
+            .navigationBarsPadding(),
+        verticalArrangement = Arrangement.Bottom,
     ) {
-        Box(
-            Modifier
-                .fillMaxWidth()
-                .weight(1f),
-        ) {
-            Box(
-                Modifier
-                    .align(Alignment.Center)
-                    .size(100.dp)
-                    .background(Color.Black),
-            )
-        }
-
         PrimaryButtonIconEnd(
             modifier = Modifier
                 .padding(start = 16.dp, end = 16.dp, bottom = 12.dp)
