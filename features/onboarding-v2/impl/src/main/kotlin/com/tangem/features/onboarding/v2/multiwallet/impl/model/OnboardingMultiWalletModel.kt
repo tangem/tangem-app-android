2d85995552aaaf6d8a87b90be20087a80bda374e
Tangem <git@tangem.com>
2024-11-09 08:43:41 +0300

    Updated on 2025-10-13

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/model/OnboardingMultiWalletModel.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/model/OnboardingMultiWalletModel.kt
@@ -4,11 +4,11 @@ import com.tangem.core.decompose.di.ComponentScoped
 import com.tangem.core.decompose.model.Model
 import com.tangem.core.decompose.model.ParamsContainer
 import com.tangem.domain.models.scan.CardDTO
+import com.tangem.domain.wallets.usecase.GetCardImageUseCase
 import com.tangem.features.onboarding.v2.multiwallet.api.OnboardingMultiWalletComponent
+import com.tangem.features.onboarding.v2.multiwallet.impl.ui.state.OnboardingMultiWalletUM
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -17,8 +17,9 @@ internal class OnboardingMultiWalletModel @Inject constructor(
     paramsContainer: ParamsContainer,
     override val dispatchers: CoroutineDispatcherProvider,
 ) : Model() {
-
     private val params = paramsContainer.require<OnboardingMultiWalletComponent.Params>()
+    private val getCardImageUseCase = GetCardImageUseCase()
+    private val _uiState = MutableStateFlow(OnboardingMultiWalletUM())
 
     val state = MutableStateFlow(
         OnboardingMultiWalletState(
@@ -27,12 +28,15 @@ internal class OnboardingMultiWalletModel @Inject constructor(
         ),
     )
 
+    val uiState = _uiState.asStateFlow()
+
     init {
         // TODO add analytics
         // if (!manager.isActivationStarted(notNullCard.cardId)) {
         //     Analytics.send(Onboarding.Started())
         // }
         initScreenTitleSub()
+        loadCardArtwork()
     }
 
     private fun getInitialStep(): OnboardingMultiWalletState.Step {
@@ -58,6 +62,19 @@ internal class OnboardingMultiWalletModel @Inject constructor(
                     val stepTitle = screenTitleByStep(step)
                     params.titleProvider.changeTitle(stepTitle)
                 }
+        }
+    }
+
+    private fun loadCardArtwork() {
+        modelScope.launch {
+            val artwork =
+                getCardImageUseCase.invoke(params.scanResponse.card.cardId, params.scanResponse.card.cardPublicKey)
+
+            if (artwork != getCardImageUseCase.getDefaultFallbackUrl()) {
+                _uiState.update {
+                    it.copy(artworkUrl = artwork)
+                }
+            }
         }
     }
 }
