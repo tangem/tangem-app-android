cb283ba9057ca6e0719ec840b5470754fa8d2925
Tangem <git@tangem.com>
2024-11-10 18:25:23 +0400

    Updated on 2026-07-01

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/viewmodels/intents/WalletCurrencyActionsClickIntents.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/viewmodels/intents/WalletCurrencyActionsClickIntents.kt
@@ -445,7 +445,7 @@ internal class WalletCurrencyActionsClickIntentsImplementor @Inject constructor(
 
     override fun onMultiWalletSwapClick(userWalletId: UserWalletId) {
         // TODO [REDACTED_JIRA]
-        // appRouter.push(route = AppRoute.SwapCrypto(userWalletId = userWalletId))
+        appRouter.push(route = AppRoute.SwapCrypto(userWalletId = userWalletId))
     }
 
     override fun onMultiWalletBuyClick(userWalletId: UserWalletId) {
