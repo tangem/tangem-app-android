f2f0eb7057cf1fb34d3eb031a6d9f3a9deaea152
Tangem <git@tangem.com>
2024-11-11 12:42:36 +0300

    Updated on 2026-08-07

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/GetMinimumTransactionAmountSyncUseCase.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/GetMinimumTransactionAmountSyncUseCase.kt
@@ -5,22 +5,17 @@ import arrow.core.raise.either
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.domain.tokens.repository.CurrencyChecksRepository
 import com.tangem.domain.wallets.models.UserWalletId
-import com.tangem.utils.coroutines.CoroutineDispatcherProvider
-import kotlinx.coroutines.withContext
 import java.math.BigDecimal
 
 class GetMinimumTransactionAmountSyncUseCase(
     private val currencyChecksRepository: CurrencyChecksRepository,
-    private val dispatchers: CoroutineDispatcherProvider,
 ) {
 
     suspend operator fun invoke(
         userWalletId: UserWalletId,
         cryptoCurrencyStatus: CryptoCurrencyStatus,
-    ): Either<Throwable, BigDecimal?> = withContext(dispatchers.io) {
-        either {
-            val cryptoCurrency = cryptoCurrencyStatus.currency
-            currencyChecksRepository.getMinimumSendAmount(userWalletId, cryptoCurrency.network)
-        }
+    ): Either<Throwable, BigDecimal?> = either {
+        val cryptoCurrency = cryptoCurrencyStatus.currency
+        currencyChecksRepository.getMinimumSendAmount(userWalletId, cryptoCurrency.network)
     }
 }
