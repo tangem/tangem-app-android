bb3a1744034a4472656b0372996adcfe4d7e2040
Tangem <git@tangem.com>
2024-11-11 13:33:40 +0500

    Updated on 2026-09-21

--- features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/selectcurrency/entity/CurrencyListController.kt
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/selectcurrency/entity/CurrencyListController.kt
@@ -1,21 +1,27 @@
 package com.tangem.features.onramp.selectcurrency.entity
 
 import com.tangem.core.ui.components.fields.entity.SearchBarUM
-import com.tangem.core.ui.extensions.stringReference
-import com.tangem.features.onramp.selectcurrency.entity.transformer.UpdateCurrencyItemsTransformer
 import com.tangem.features.onramp.utils.SearchBarUMTransformer
+import com.tangem.utils.transformer.Transformer
 import kotlinx.collections.immutable.ImmutableList
-import kotlinx.collections.immutable.toImmutableList
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
-internal class CurrencyListController(private val currencySearchBarUM: SearchBarUM) {
+internal class CurrencyListController(
+    currencySearchBarUM: SearchBarUM,
+    loadingSections: ImmutableList<CurrenciesSection<CurrencyItemState.Loading>>,
+) {
     val state: StateFlow<CurrenciesListUM> get() = _state.asStateFlow()
-    private val _state: MutableStateFlow<CurrenciesListUM> = MutableStateFlow(value = getInitialState())
+    private val _state: MutableStateFlow<CurrenciesListUM> = MutableStateFlow(
+        value = CurrenciesListUM.Loading(
+            searchBarUM = currencySearchBarUM,
+            sections = loadingSections,
+        ),
+    )
 
-    fun update(transformer: UpdateCurrencyItemsTransformer) {
+    fun update(transformer: Transformer<CurrenciesListUM>) {
         _state.update(transformer::transform)
     }
 
@@ -25,17 +31,4 @@ internal class CurrencyListController(private val currencySearchBarUM: SearchBar
             prevState.copySealed(searchBarUM)
         }
     }
-
-    private fun getInitialState(): CurrenciesListUM {
-        return CurrenciesListUM.Loading(
-            searchBarUM = currencySearchBarUM,
-            sections = listOf(
-                CurrenciesSection(stringReference("Popular fiats"), items = createLoadingItems("popular")),
-                CurrenciesSection(stringReference("Other currencies"), items = createLoadingItems("other")),
-            ).toImmutableList(),
-        )
-    }
-
-    private fun createLoadingItems(prefix: String, size: Int = 5): ImmutableList<CurrencyItemState.Loading> =
-        MutableList(size) { index -> CurrencyItemState.Loading("$prefix#$index") }.toImmutableList()
 }
