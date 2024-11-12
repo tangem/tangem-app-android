6b6dde9dab764621eeddf0a12b637c588bb55586
Tangem <git@tangem.com>
2024-11-12 14:42:17 +0500

    Updated on 2026-07-23

---
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/main/entity/OnrampMainBottomSheetConfig.kt
@@ -0,0 +1,10 @@
+package com.tangem.features.onramp.main.entity
+
+import com.tangem.domain.onramp.model.OnrampCountry
+import kotlinx.serialization.Serializable
+
+@Serializable
+internal sealed interface OnrampMainBottomSheetConfig {
+    @Serializable
+    data class ConfirmResidency(val country: OnrampCountry) : OnrampMainBottomSheetConfig
+}
