88bdc12e4ddf58043bfe5d31c88515c6d8c2af8c
Tangem <git@tangem.com>
2024-11-14 16:48:28 +0300

    Updated on 2026-09-14

--- core/datasource/src/main/java/com/tangem/datasource/api/tangemTech/TangemTechApiV2.kt
+++ core/datasource/src/main/java/com/tangem/datasource/api/tangemTech/TangemTechApiV2.kt
@@ -7,16 +7,10 @@ import retrofit2.http.*
 interface TangemTechApiV2 {
 
     @GET("user-tokens/{wallet_id}")
-    suspend fun getUserTokens(
-        @Header("card_public_key") cardPublicKey: String,
-        @Header("card_id") cardId: String,
-        @Path("wallet_id") walletId: String,
-    ): ApiResponse<UserTokensResponseV2>
+    suspend fun getUserTokens(@Path("wallet_id") walletId: String): ApiResponse<UserTokensResponseV2>
 
     @PUT("user-tokens/{wallet_id}")
     suspend fun saveUserTokens(
-        @Header("card_public_key") cardPublicKey: String,
-        @Header("card_id") cardId: String,
         @Path("wallet_id") walletId: String,
         @Body userTokens: UserTokensResponseV2,
     ): ApiResponse<Unit>
