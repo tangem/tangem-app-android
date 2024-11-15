3695cd7b6d7146dc5b3b83604bbfa2e1e1edb473
Tangem <git@tangem.com>
2024-11-15 17:12:02 +0500

    Updated on 2025-11-18

--- features/onramp/api/src/main/kotlin/com/tangem/features/onramp/component/OnrampComponent.kt
+++ features/onramp/api/src/main/kotlin/com/tangem/features/onramp/component/OnrampComponent.kt
@@ -2,10 +2,11 @@ package com.tangem.features.onramp.component
 
 import com.tangem.core.decompose.factory.ComponentFactory
 import com.tangem.core.ui.decompose.ComposableContentComponent
+import com.tangem.domain.tokens.model.CryptoCurrency
 
 interface OnrampComponent : ComposableContentComponent {
 
-    data class Params(val currency: String)
+    data class Params(val cryptoCurrency: CryptoCurrency)
 
     interface Factory : ComponentFactory<Params, OnrampComponent>
 }
