530f44078d38bb734cf84447244a8a99b9524eb5
Tangem <git@tangem.com>
2024-11-15 17:12:02 +0500

    Updated on 2025-09-15

--- domain/onramp/src/main/java/com/tangem/domain/onramp/CheckOnrampAvailabilityUseCase.kt
+++ domain/onramp/src/main/java/com/tangem/domain/onramp/CheckOnrampAvailabilityUseCase.kt
@@ -8,10 +8,13 @@ class CheckOnrampAvailabilityUseCase(private val repository: OnrampRepository) {
 
     suspend operator fun invoke(): Either<Throwable, OnrampAvailability> {
         return Either.catch {
-            return@catch OnrampAvailability.Available
             repository.getDefaultCountrySync()?.let { savedCountry ->
                 return@catch if (savedCountry.onrampAvailable) {
-                    OnrampAvailability.Available
+                    val currency = repository.getDefaultCurrencySync() ?: run {
+                        repository.saveDefaultCurrency(savedCountry.defaultCurrency)
+                        savedCountry.defaultCurrency
+                    }
+                    OnrampAvailability.Available(country = savedCountry, currency = currency)
                 } else {
                     OnrampAvailability.NotSupported(savedCountry)
                 }
