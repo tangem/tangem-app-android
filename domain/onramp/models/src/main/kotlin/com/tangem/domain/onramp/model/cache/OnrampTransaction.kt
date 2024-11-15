0aa917afd8eba80aeee1cac3927f5ef6120cc0ce
Tangem <git@tangem.com>
2024-11-15 13:41:23 +0500

    Updated on 2026-09-23

---
+++ domain/onramp/models/src/main/kotlin/com/tangem/domain/onramp/model/cache/OnrampTransaction.kt
@@ -0,0 +1,20 @@
+package com.tangem.domain.onramp.model.cache
+
+import com.tangem.domain.core.serialization.SerializedBigDecimal
+import com.tangem.domain.onramp.model.OnrampCurrency
+import com.tangem.domain.tokens.model.CryptoCurrency
+import com.tangem.domain.wallets.models.UserWalletId
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class OnrampTransaction(
+    val txId: String,
+    val userWalletId: UserWalletId,
+    val fromAmount: SerializedBigDecimal,
+    val fromCurrency: OnrampCurrency,
+    val toAmount: SerializedBigDecimal,
+    val toCurrency: CryptoCurrency,
+    val timestamp: Long,
+    val providerName: String,
+    val providerImageUrl: String,
+)
