a9be1035f6676b1a2c3b964ac6a1ae30e575ce9d
Tangem <git@tangem.com>
2024-11-18 14:40:19 +0200

    Updated on 2026-09-01

--- features/markets/impl/src/main/kotlin/com/tangem/features/markets/details/impl/model/MarketsTokenDetailsModel.kt
+++ features/markets/impl/src/main/kotlin/com/tangem/features/markets/details/impl/model/MarketsTokenDetailsModel.kt
@@ -88,8 +88,19 @@ internal class MarketsTokenDetailsModel @Inject constructor(
             // === Analytics ===
             analyticsEventHandler.send(analyticsEventBuilder.linkClicked(linkTitle = link.title))
         },
+        onSecurityScoreInfoClick = {
+            showBottomSheet(it)
+
+            // === Analytics ===
+            analyticsEventHandler.send(analyticsEventBuilder.securityScoreOpened())
+        },
         onSecurityScoreProviderLinkClick = {
-            urlOpener.openUrl(it)
+            it.urlData?.fullUrl?.let { url ->
+                urlOpener.openUrl(url)
+            }
+
+            // === Analytics ===
+            analyticsEventHandler.send(analyticsEventBuilder.securityScoreProviderClicked(it.name))
         },
         // === Analytics ===
         onPricePerformanceIntervalChanged = {
