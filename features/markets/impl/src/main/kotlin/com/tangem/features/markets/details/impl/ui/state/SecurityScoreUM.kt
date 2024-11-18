ca41a99df6572e008a0f121ac461ce307ae1af74
Tangem <git@tangem.com>
2024-11-18 10:44:43 +0200

    Updated on 2026-05-04

--- features/markets/impl/src/main/kotlin/com/tangem/features/markets/details/impl/ui/state/SecurityScoreUM.kt
+++ features/markets/impl/src/main/kotlin/com/tangem/features/markets/details/impl/ui/state/SecurityScoreUM.kt
@@ -1,9 +1,10 @@
 package com.tangem.features.markets.details.impl.ui.state
 
 import androidx.annotation.FloatRange
+import com.tangem.core.ui.extensions.TextReference
 
 internal data class SecurityScoreUM(
     @FloatRange(from = 0.0, to = 5.0) val score: Float,
-    val description: String,
+    val description: TextReference,
     val onInfoClick: () -> Unit,
 )
