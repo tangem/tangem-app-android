12da9a77576d9793a69b0c08baaf30372e10c56d
Tangem <git@tangem.com>
2024-11-18 16:30:50 +0300

    Updated on 2026-06-17

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/model/OnboardingMultiWalletState.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/model/OnboardingMultiWalletState.kt
@@ -6,7 +6,18 @@ data class OnboardingMultiWalletState(
     val currentStep: Step,
     val currentScanResponse: ScanResponse,
 ) {
+    /**
+     * Wallet1
+     * CreateWallet -> ChooseBackupOption -> AddBackupDevice -> FinishBackup -> [Done]
+     *                                    |
+     *                                    -> [Done]
+     *
+     * Wallet2/Ring
+     * CreateWallet -> SeedPhrase -> AddBackupDevice -> FinishBackup -> [Done]
+     *              |
+     *              -> AddBackupDevice -> FinishBackup -> [Done]
+     */
     enum class Step {
-        CreateWallet, AddBackupDevice, FinishBackup, Done
+        CreateWallet, ChooseBackupOption, SeedPhrase, AddBackupDevice, FinishBackup, Done
     }
 }
