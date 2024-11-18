2ae25447e685f0cd020aaf0b96be3354d38dc70e
Tangem <git@tangem.com>
2024-11-18 17:01:35 +0300

    Updated on 2026-08-27

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/GetCurrencyWarningsUseCase.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/GetCurrencyWarningsUseCase.kt
@@ -58,7 +58,7 @@ class GetCurrencyWarningsUseCase(
                 derivationPath = derivationPath,
                 isSingleWalletWithTokens = isSingleWalletWithTokens,
             ),
-            flowOf(walletManagersFacade.getRentInfo(userWalletId, currency.network)),
+            flowOf(currencyChecksRepository.getRentInfoWarning(userWalletId, currencyStatus, null)),
             flowOf(currencyChecksRepository.getExistentialDeposit(userWalletId, currency.network)),
             flowOf(currencyChecksRepository.getFeeResourceAmount(userWalletId, currency.network)),
             getSwapPromoNotificationWarning(
