91fabe08c1c34cb069969aaad4dffc516a61fb71
Tangem <git@tangem.com>
2024-11-19 14:32:47 +0300

    Updated on 2026-07-07

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/seedphrase/model/MultiWalletSeedPhraseModel.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/seedphrase/model/MultiWalletSeedPhraseModel.kt
@@ -2,9 +2,11 @@ package com.tangem.features.onboarding.v2.multiwallet.impl.child.seedphrase.mode
 
 import com.tangem.core.decompose.di.ComponentScoped
 import com.tangem.core.decompose.model.Model
+import com.tangem.core.navigation.url.UrlOpener
+import com.tangem.features.onboarding.v2.multiwallet.impl.child.seedphrase.model.builder.GenerateSeedPhraseUiStateBuilder
+import com.tangem.features.onboarding.v2.multiwallet.impl.child.seedphrase.model.builder.SeedPhraseCheckUiStateBuilder
 import com.tangem.features.onboarding.v2.multiwallet.impl.child.seedphrase.ui.state.MultiWalletSeedPhraseUM
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
-import kotlinx.collections.immutable.toImmutableList
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -13,44 +15,92 @@ import javax.inject.Inject
 @ComponentScoped
 class MultiWalletSeedPhraseModel @Inject constructor(
     override val dispatchers: CoroutineDispatcherProvider,
+    private val mnemonicRepository: MnemonicRepository,
+    private val urlOpener: UrlOpener,
 ) : Model() {
 
     private val state = MutableStateFlow(SeedPhraseState())
+    private val _uiState = MutableStateFlow(getStartState())
 
-    private val _uiState = MutableStateFlow<MultiWalletSeedPhraseUM>(
-        MultiWalletSeedPhraseUM.Start(
-            onImportSeedPhraseClicked = {},
-            onGenerateSeedPhraseClicked = {
-                openGeneratedSeedPhrase()
-            },
-            onLearnMoreClicked = {},
-        ),
+    private val generateSeedPhraseUiStateBuilder = GenerateSeedPhraseUiStateBuilder(
+        state = state,
+        updateUiState = { block -> updateUiStateSpecific(block) },
+        onContinue = { openWordsCheck() },
+    )
+
+    private val seedPhraseCheckUiStateBuilder = SeedPhraseCheckUiStateBuilder(
+        state = state,
+        currentUiState = { _uiState.value },
+        updateUiState = { block -> updateUiStateSpecific(block) },
+        importWallet = { },
     )
 
     val uiState = _uiState.asStateFlow()
 
     fun onBack() {
-        TODO("Not yet implemented")
+        when (_uiState.value) {
+            is MultiWalletSeedPhraseUM.Start -> {
+                // TODO navigate back
+            }
+            is MultiWalletSeedPhraseUM.Import,
+            is MultiWalletSeedPhraseUM.GenerateSeedPhrase,
+            -> {
+                _uiState.value = getStartState()
+                state.value = SeedPhraseState()
+            }
+            is MultiWalletSeedPhraseUM.GeneratedWordsCheck -> {
+                openGeneratedSeedPhrase()
+            }
+        }
+    }
+
+    private fun getStartState(): MultiWalletSeedPhraseUM {
+        return MultiWalletSeedPhraseUM.Start(
+            onImportSeedPhraseClicked = {},
+            onGenerateSeedPhraseClicked = {
+                generateSeedPhrase()
+                openGeneratedSeedPhrase()
+            },
+            onLearnMoreClicked = {
+                urlOpener.openUrl(seedPhraseLearnMoreUrl())
+            },
+        )
     }
 
     private fun openGeneratedSeedPhrase() {
-        // TODO state.value.generatedWords
-        val words = listOf("word, word")
+        // TODO analytics SeedPhraseEvents.ButtonGenerateSeedPhrase
 
-        _uiState.value = MultiWalletSeedPhraseUM.GenerateSeedPhrase(
-            words24OptionSelected = false,
-            words = words.mapIndexed { index, s ->
-                MultiWalletSeedPhraseUM.GenerateSeedPhrase.MnemonicGridItem(index, s)
-            }.toImmutableList(),
-            onWords24OptionSwitch = {
-                _uiState.update {
-                    if (it !is MultiWalletSeedPhraseUM.GenerateSeedPhrase) return@update it
-                    it.copy(words24OptionSelected = it.words24OptionSelected.not())
-                }
-                state.update { it.copy(words24Option = it.words24Option.not()) }
-            },
-            onContinueClick = {
-            },
+        val words12 = state.value.generatedWords12 ?: return
+        val words24 = state.value.generatedWords24 ?: return
+
+        // TODO disallow screenshots
+
+        _uiState.value = generateSeedPhraseUiStateBuilder.getState(
+            generatedWords12 = words12,
+            generatedWords24 = words24,
         )
+    }
+
+    private fun openWordsCheck() {
+        _uiState.value = seedPhraseCheckUiStateBuilder.getState()
+    }
+
+    private fun generateSeedPhrase() {
+        val words12 = mnemonicRepository.generateMnemonic(MnemonicRepository.MnemonicType.Words12)
+        val words24 = mnemonicRepository.generateMnemonic(MnemonicRepository.MnemonicType.Words24)
+
+        state.update {
+            it.copy(
+                generatedWords12 = words12,
+                generatedWords24 = words24,
+            )
+        }
+    }
+
+    private inline fun <reified T : MultiWalletSeedPhraseUM> updateUiStateSpecific(block: (T) -> T) {
+        _uiState.update {
+            if (it !is T) return@update it
+            block(it)
+        }
     }
 }
