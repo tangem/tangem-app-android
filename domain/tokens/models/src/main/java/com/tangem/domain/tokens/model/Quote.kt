148f2fd505ba8fcdf442b257a87d96af0909dd05
Tangem <git@tangem.com>
2024-11-21 12:12:33 +0700

    Updated on 2025-11-18

--- domain/tokens/models/src/main/java/com/tangem/domain/tokens/model/Quote.kt
+++ domain/tokens/models/src/main/java/com/tangem/domain/tokens/model/Quote.kt
@@ -2,15 +2,27 @@ package com.tangem.domain.tokens.model
 
 import java.math.BigDecimal
 
-/**
- * Represents financial information for a specific cryptocurrency, including its fiat exchange rate and price change.
- *
- * @property rawCurrencyId The unique identifier of the cryptocurrency for which the financial information is provided.
- * @property fiatRate The current fiat exchange rate for the cryptocurrency.
- * @property priceChange The price change for the cryptocurrency.
- */
-data class Quote(
-    val rawCurrencyId: String,
-    val fiatRate: BigDecimal,
-    val priceChange: BigDecimal,
-)
+sealed interface Quote {
+
+    val rawCurrencyId: String?
+
+    /**
+     * Represents unknown financial information for a specific cryptocurrency.
+     *
+     * @property rawCurrencyId The raw cryptocurrency ID. If it is a custom token, the value will be `null`.
+     */
+    data class Empty(override val rawCurrencyId: String?) : Quote
+
+    /**
+     * Represents financial information for a specific cryptocurrency, including its fiat exchange rate and price change.
+     *
+     * @property rawCurrencyId The unique identifier of the cryptocurrency for which the financial information is provided.
+     * @property fiatRate The current fiat exchange rate for the cryptocurrency.
+     * @property priceChange The price change for the cryptocurrency.
+     */
+    data class Value(
+        override val rawCurrencyId: String,
+        val fiatRate: BigDecimal,
+        val priceChange: BigDecimal,
+    ) : Quote
+}
--- domain/tokens/src/test/kotlin/com/tangem/domain/tokens/mock/MockQuotes.kt
+++ domain/tokens/src/test/kotlin/com/tangem/domain/tokens/mock/MockQuotes.kt
@@ -7,65 +7,71 @@ import java.math.BigDecimal
 @Suppress("MemberVisibilityCanBePrivate")
 internal object MockQuotes {
 
-    val quote1 = Quote(
+    val quote1 = Quote.Value(
         rawCurrencyId = MockTokens.token1.id.rawCurrencyId!!,
         fiatRate = BigDecimal("1.23"),
         priceChange = BigDecimal("0.01"),
     )
 
-    val quote2 = Quote(
+    val quote2 = Quote.Value(
         rawCurrencyId = MockTokens.token2.id.rawCurrencyId!!,
         fiatRate = BigDecimal("2.34"),
         priceChange = BigDecimal("-0.02"),
     )
 
-    val quote3 = Quote(
+    val quote3 = Quote.Value(
         rawCurrencyId = MockTokens.token3.id.rawCurrencyId!!,
         fiatRate = BigDecimal("3.45"),
         priceChange = BigDecimal("0.03"),
     )
 
-    val quote4 = Quote(
+    val quote4 = Quote.Value(
         rawCurrencyId = MockTokens.token4.id.rawCurrencyId!!,
         fiatRate = BigDecimal("4.56"),
         priceChange = BigDecimal("-0.04"),
     )
 
-    val quote5 = Quote(
+    val quote5 = Quote.Value(
         rawCurrencyId = MockTokens.token5.id.rawCurrencyId!!,
         fiatRate = BigDecimal("5.67"),
         priceChange = BigDecimal("0.05"),
     )
 
-    val quote6 = Quote(
+    val quote6 = Quote.Value(
         rawCurrencyId = MockTokens.token6.id.rawCurrencyId!!,
         fiatRate = BigDecimal("6.78"),
         priceChange = BigDecimal("-0.06"),
     )
 
-    val quote7 = Quote(
+    val quote7 = Quote.Value(
         rawCurrencyId = MockTokens.token7.id.rawCurrencyId!!,
         fiatRate = BigDecimal("7.89"),
         priceChange = BigDecimal("0.07"),
     )
 
-    val quote8 = Quote(
+    val quote8 = Quote.Value(
         rawCurrencyId = MockTokens.token8.id.rawCurrencyId!!,
         fiatRate = BigDecimal("8.90"),
         priceChange = BigDecimal("-0.08"),
     )
 
-    val quote9 = Quote(
+    val quote9 = Quote.Value(
         rawCurrencyId = MockTokens.token9.id.rawCurrencyId!!,
         fiatRate = BigDecimal("9.01"),
         priceChange = BigDecimal("0.09"),
     )
 
-    val quote10 = Quote(
+    val quote10 = Quote.Value(
         rawCurrencyId = MockTokens.token10.id.rawCurrencyId!!,
         fiatRate = BigDecimal("10.12"),
         priceChange = BigDecimal("-0.10"),
     )
 
-    val quotes = nonEmptySetOf(quote1, quote2, quote3, quote4, quote5, quote6, quote7, quote8, quote9, quote10)
+    val quote11 = Quote.Empty(null)
+    val quote12 = Quote.Empty("null")
+
+    val quotes = nonEmptySetOf(
+        quote1, quote2, quote3, quote4, quote5, quote6, quote7, quote8, quote9, quote10,
+        quote11, quote12,
+    )
 }
