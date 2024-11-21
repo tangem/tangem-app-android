6cda73a51807dafd63b2f934997fd92617f02b06
Tangem <git@tangem.com>
2024-11-21 17:49:53 +0300

    Updated on 2026-09-14

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/createwallet/ui/state/MultiWalletCreateWalletUM.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/createwallet/ui/state/MultiWalletCreateWalletUM.kt
@@ -1,23 +1,13 @@
 package com.tangem.features.onboarding.v2.multiwallet.impl.child.createwallet.ui.state
 
 import com.tangem.core.ui.extensions.TextReference
+import com.tangem.features.onboarding.v2.multiwallet.impl.common.ui.OnboardingDialogUM
 
-data class MultiWalletCreateWalletUM(
+internal data class MultiWalletCreateWalletUM(
     val title: TextReference,
     val bodyText: TextReference,
     val showOtherOptionsButton: Boolean,
     val onCreateWalletClick: () -> Unit,
     val onOtherOptionsClick: () -> Unit,
-    val dialog: Dialog?,
-) {
-    data class Dialog(
-        val title: TextReference,
-        val description: TextReference,
-        val dismissButtonText: TextReference,
-        val confirmButtonText: TextReference,
-        val dismissWarningColor: Boolean = false,
-        val onConfirm: () -> Unit,
-        val onDismissButtonClick: () -> Unit,
-        val onDismiss: () -> Unit,
-    )
-}
+    val dialog: OnboardingDialogUM?,
+)
