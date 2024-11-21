574f4879802d17e47f9dd5e6eebbc7584880b687
Tangem <git@tangem.com>
2024-11-21 17:49:53 +0300

    Updated on 2026-08-27

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/seedphrase/MultiWalletSeedPhraseComponent.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/seedphrase/MultiWalletSeedPhraseComponent.kt
@@ -1,9 +1,15 @@
 package com.tangem.features.onboarding.v2.multiwallet.impl.child.seedphrase
 
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+import android.view.WindowManager
 import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.tangem.core.decompose.context.AppComponentContext
 import com.tangem.core.decompose.model.getOrCreateModel
@@ -16,20 +22,24 @@ import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.launch
 
 @Suppress("UnusedPrivateMember")
-class MultiWalletSeedPhraseComponent(
+internal class MultiWalletSeedPhraseComponent(
     context: AppComponentContext,
     params: MultiWalletChildParams,
     onNextStep: (OnboardingMultiWalletState.Step) -> Unit,
     backButtonClickFlow: SharedFlow<Unit>,
 ) : AppComponentContext by context, MultiWalletChildComponent {
 
-    private val model: MultiWalletSeedPhraseModel = getOrCreateModel()
+    private val model: MultiWalletSeedPhraseModel = getOrCreateModel(params)
 
     init {
         componentScope.launch {
-            backButtonClickFlow.collect {
-                model.onBack()
-            }
+            backButtonClickFlow.collect { model.onBack() }
+        }
+        componentScope.launch {
+            model.onDone.collect { onNextStep(OnboardingMultiWalletState.Step.AddBackupDevice) }
+        }
+        componentScope.launch {
+            model.navigateBack.collect { onNextStep(OnboardingMultiWalletState.Step.CreateWallet) }
         }
     }
 
@@ -41,9 +51,30 @@ class MultiWalletSeedPhraseComponent(
             model.onBack()
         }
 
+        // disable screenshots
+        val activity = LocalContext.current.findActivity()
+        DisposableEffect(activity) {
+            activity.window.setFlags(
+                WindowManager.LayoutParams.FLAG_SECURE,
+                WindowManager.LayoutParams.FLAG_SECURE,
+            )
+            onDispose {
+                activity.window.clearFlags(WindowManager.LayoutParams.FLAG_SECURE)
+            }
+        }
+
         MultiWalletSeedPhrase(
             modifier = modifier,
             state = state,
         )
+    }
+
+    private fun Context.findActivity(): Activity {
+        var context = this
+        while (context is ContextWrapper) {
+            if (context is Activity) return context
+            context = context.baseContext
+        }
+        error("")
     }
 }
--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/common/ui/ResetCardDialog.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/common/ui/ResetCardDialog.kt
@@ -1,18 +1,17 @@
-package com.tangem.features.onboarding.v2.multiwallet.impl.child.createwallet.model
+package com.tangem.features.onboarding.v2.multiwallet.impl.common.ui
 
 import com.tangem.core.ui.extensions.resourceReference
 import com.tangem.features.onboarding.v2.impl.R
-import com.tangem.features.onboarding.v2.multiwallet.impl.child.createwallet.ui.state.MultiWalletCreateWalletUM
 
 internal fun resetCardDialog(onConfirm: () -> Unit, dismiss: () -> Unit, onDismissButtonClick: () -> Unit) =
-    MultiWalletCreateWalletUM.Dialog(
+    OnboardingDialogUM(
         title = resourceReference(R.string.onboarding_activation_error_title),
-        description = resourceReference(R.string.onboarding_activation_error_message),
+        message = resourceReference(R.string.onboarding_activation_error_message),
         confirmButtonText = resourceReference(R.string.common_support),
         dismissButtonText = resourceReference(R.string.common_ok),
         dismissWarningColor = true,
         onDismiss = dismiss,
-        onConfirm = {
+        onConfirmClick = {
             onConfirm()
             dismiss()
         },
