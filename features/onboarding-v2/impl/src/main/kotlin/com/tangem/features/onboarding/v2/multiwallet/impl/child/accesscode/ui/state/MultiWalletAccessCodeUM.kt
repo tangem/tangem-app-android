5b345270927a7107c83e249635a730426a9a0296
Tangem <git@tangem.com>
2024-11-22 14:51:20 +0300

    Updated on 2026-07-07

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/accesscode/ui/state/MultiWalletAccessCodeUM.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/accesscode/ui/state/MultiWalletAccessCodeUM.kt
@@ -7,9 +7,12 @@ internal data class MultiWalletAccessCodeUM(
     val accessCodeFirst: TextFieldValue = TextFieldValue(""),
     val accessCodeSecond: TextFieldValue = TextFieldValue(""),
     val codesNotMatchError: Boolean = false,
+    val atLeast4CharError: Boolean = false,
+    val accessCodeHidden: Boolean = true,
     val onAccessCodeFirstChange: (TextFieldValue) -> Unit = {},
     val onAccessCodeSecondChange: (TextFieldValue) -> Unit = {},
     val onContinue: () -> Unit = {},
+    val onAccessCodeHideClick: () -> Unit = {},
 ) {
     enum class Step {
         Intro,
