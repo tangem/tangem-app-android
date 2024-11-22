6dce243004acfce8a1d9d9692eb9a0fd4e426db3
Tangem <git@tangem.com>
2024-11-22 14:51:20 +0300

    Updated on 2026-08-27

---
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/finalize/ui/MultiWalletFinalize.kt
@@ -0,0 +1,22 @@
+package com.tangem.features.onboarding.v2.multiwallet.impl.child.finalize.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.tangem.features.onboarding.v2.multiwallet.impl.child.finalize.state.MultiWalletFinalizeUM
+
+@Suppress("UnusedPrivateMember")
+@Composable
+fun MultiWalletFinalize(state: MultiWalletFinalizeUM, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .navigationBarsPadding(),
+        verticalArrangement = Arrangement.Bottom,
+    ) {
+        // TODO
+    }
+}
