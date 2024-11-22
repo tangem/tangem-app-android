de6622ed1e2b15eb67f08bc154ab6e6eb4988612
Tangem <git@tangem.com>
2024-11-22 14:51:20 +0300

    Updated on 2026-09-01

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/accesscode/ui/MultiWalletAccessCodeEnter.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/accesscode/ui/MultiWalletAccessCodeEnter.kt
@@ -3,16 +3,23 @@ package com.tangem.features.onboarding.v2.multiwallet.impl.child.accesscode.ui
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.tangem.core.ui.components.OutlineTextField
+import com.tangem.core.ui.components.OutlineTextFieldWithIcon
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.core.ui.res.TangemThemePreview
 import com.tangem.features.onboarding.v2.impl.R
 import com.tangem.features.onboarding.v2.multiwallet.impl.child.accesscode.ui.state.MultiWalletAccessCodeUM
+import kotlinx.coroutines.delay
 
 @Composable
 internal fun MultiWalletAccessCodeEnter(
@@ -43,8 +50,19 @@ internal fun MultiWalletAccessCodeEnter(
             style = TangemTheme.typography.body1,
         )
 
-        OutlineTextField(
-            modifier = modifier.fillMaxWidth(),
+        val focusRequester = remember { FocusRequester() }
+
+        OutlineTextFieldWithIcon(
+            modifier = modifier
+                .focusRequester(focusRequester)
+                .fillMaxWidth(),
+            iconResId = if (state.accessCodeHidden) {
+                R.drawable.ic_eye_outline_24
+            } else {
+                R.drawable.ic_eye_off_outline_24
+            },
+            iconColor = TangemTheme.colors.icon.primary1,
+            onIconClick = state.onAccessCodeHideClick,
             value = if (reEnterAccessCodeState) {
                 state.accessCodeSecond
             } else {
@@ -56,13 +74,25 @@ internal fun MultiWalletAccessCodeEnter(
                 state.onAccessCodeFirstChange
             },
             label = stringResource(id = R.string.onboarding_wallet_info_title_third),
-            isError = state.codesNotMatchError,
-            caption = if (state.codesNotMatchError && reEnterAccessCodeState) {
-                stringResource(R.string.onboarding_access_codes_doesnt_match)
+            isError = state.codesNotMatchError || state.atLeast4CharError,
+            visualTransformation = if (state.accessCodeHidden) {
+                PasswordVisualTransformation()
             } else {
-                null
+                VisualTransformation.None
+            },
+            caption = when {
+                state.codesNotMatchError && reEnterAccessCodeState ->
+                    stringResource(R.string.onboarding_access_codes_doesnt_match)
+                state.atLeast4CharError && !reEnterAccessCodeState ->
+                    stringResource(R.string.onboarding_access_code_too_short)
+                else -> null
             },
         )
+
+        LaunchedEffect(Unit) {
+            delay(timeMillis = 200)
+            focusRequester.requestFocus()
+        }
     }
 }
 
