b05d7a0d2494d5b848b91a4e4fe7499aaa1ad59d
Tangem <git@tangem.com>
2024-11-26 09:21:35 +0300

    Updated on 2026-09-14

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/entry/impl/routing/OnboardingChildFactory.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/entry/impl/routing/OnboardingChildFactory.kt
@@ -1,11 +1,14 @@
 package com.tangem.features.onboarding.v2.entry.impl.routing
 
 import com.tangem.core.decompose.context.AppComponentContext
+import com.tangem.features.managetokens.component.ManageTokensComponent
+import com.tangem.features.managetokens.component.ManageTokensSource
 import com.tangem.features.onboarding.v2.multiwallet.api.OnboardingMultiWalletComponent
 import javax.inject.Inject
 
 internal class OnboardingChildFactory @Inject constructor(
     private val multiWalletComponentFactory: OnboardingMultiWalletComponent.Factory,
+    private val manageTokensComponentFactory: ManageTokensComponent.Factory,
 ) {
 
     fun createChild(route: OnboardingRoute, childContext: AppComponentContext): Any {
@@ -16,6 +19,14 @@ internal class OnboardingChildFactory @Inject constructor(
                     scanResponse = route.scanResponse,
                     withSeedPhraseFlow = route.withSeedPhraseFlow,
                     titleProvider = route.titleProvider,
+                    onDone = route.onDone,
+                ),
+            )
+            is OnboardingRoute.ManageTokens -> manageTokensComponentFactory.create( // TODO replace with wrapper
+                context = childContext,
+                params = ManageTokensComponent.Params(
+                    userWalletId = route.userWallet.walletId,
+                    source = ManageTokensSource.ONBOARDING,
                 ),
             )
             else -> Unit
