3154623fda7d93d47bb316d9e1f81a4673faa670
Tangem <git@tangem.com>
2024-11-26 17:39:58 +0300

    Updated on 2026-09-01

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/model/Utils.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/model/Utils.kt
@@ -7,10 +7,9 @@ import com.tangem.features.onboarding.v2.impl.R
 fun screenTitleByStep(step: OnboardingMultiWalletState.Step): TextReference = when (step) {
     OnboardingMultiWalletState.Step.CreateWallet ->
         resourceReference(R.string.onboarding_create_wallet_header)
-    OnboardingMultiWalletState.Step.ChooseBackupOption -> TODO()
-    OnboardingMultiWalletState.Step.SeedPhrase -> TODO()
     OnboardingMultiWalletState.Step.AddBackupDevice ->
         resourceReference(R.string.onboarding_navbar_title_creating_backup)
-    OnboardingMultiWalletState.Step.Finalize -> TODO()
-    OnboardingMultiWalletState.Step.Done -> error("No title for Done step")
+    OnboardingMultiWalletState.Step.Finalize ->
+        resourceReference(R.string.onboarding_button_finalize_backup)
+    else -> error("No title for the step")
 }
