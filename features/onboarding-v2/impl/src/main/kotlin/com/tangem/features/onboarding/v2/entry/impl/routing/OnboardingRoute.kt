f8a513bb1f365ec08f177eefb5b1c7e19e08dd22
Tangem <git@tangem.com>
2024-11-26 09:21:35 +0300

    Updated on 2026-05-19

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/entry/impl/routing/OnboardingRoute.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/entry/impl/routing/OnboardingRoute.kt
@@ -2,6 +2,7 @@ package com.tangem.features.onboarding.v2.entry.impl.routing
 
 import com.tangem.core.decompose.navigation.Route
 import com.tangem.domain.models.scan.ScanResponse
+import com.tangem.domain.wallets.models.UserWallet
 import com.tangem.features.onboarding.v2.TitleProvider
 
 sealed class OnboardingRoute : Route {
@@ -12,5 +13,10 @@ sealed class OnboardingRoute : Route {
         val titleProvider: TitleProvider,
         val scanResponse: ScanResponse,
         val withSeedPhraseFlow: Boolean,
+        val onDone: (UserWallet) -> Unit,
+    ) : OnboardingRoute()
+
+    data class ManageTokens(
+        val userWallet: UserWallet,
     ) : OnboardingRoute()
 }
