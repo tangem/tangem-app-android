72ed889dcc3ee2ad57b36becf84f40f4d90fb801
Tangem <git@tangem.com>
2024-11-26 11:06:15 +0300

    Updated on 2025-09-15

--- features/manage-tokens/api/src/main/kotlin/com/tangem/features/managetokens/component/OnboardingManageTokensComponent.kt
+++ features/manage-tokens/api/src/main/kotlin/com/tangem/features/managetokens/component/OnboardingManageTokensComponent.kt
@@ -1,6 +1,6 @@
 package com.tangem.features.managetokens.component
 
-import com.tangem.core.decompose.factory.ComponentFactory
+import com.tangem.core.decompose.context.AppComponentContext
 import com.tangem.core.ui.decompose.ComposableContentComponent
 import com.tangem.domain.wallets.models.UserWalletId
 
@@ -8,5 +8,7 @@ interface OnboardingManageTokensComponent : ComposableContentComponent {
 
     data class Params(val userWalletId: UserWalletId)
 
-    interface Factory : ComponentFactory<Params, OnboardingManageTokensComponent>
+    interface Factory {
+        fun create(context: AppComponentContext, params: Params, onDone: () -> Unit): OnboardingManageTokensComponent
+    }
 }
