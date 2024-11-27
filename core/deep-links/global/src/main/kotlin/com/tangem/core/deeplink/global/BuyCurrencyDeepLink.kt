3e7432ebc7c9886e7778c722238ed9f10f591bc5
Tangem <git@tangem.com>
2024-11-27 14:04:49 +0500

    Updated on 2026-06-24

--- core/deep-links/global/src/main/kotlin/com/tangem/core/deeplink/global/BuyCurrencyDeepLink.kt
+++ core/deep-links/global/src/main/kotlin/com/tangem/core/deeplink/global/BuyCurrencyDeepLink.kt
@@ -2,11 +2,13 @@ package com.tangem.core.deeplink.global
 
 import com.tangem.core.deeplink.DeepLink
 
-class BuyCurrencyDeepLink(val onReceive: () -> Unit) : DeepLink {
+class BuyCurrencyDeepLink(val onReceive: (txId: String) -> Unit) : DeepLink {
 
     override val uri: String = "tangem://redirect?action=dismissBrowser"
 
     override fun onReceive(params: Map<String, String>) {
-        onReceive()
+        onReceive(
+            params["txId"] ?: return,
+        )
     }
 }
