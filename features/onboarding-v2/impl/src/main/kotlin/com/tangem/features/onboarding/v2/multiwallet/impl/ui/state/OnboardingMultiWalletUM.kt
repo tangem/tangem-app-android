088a86a08cdb3871da29217f28375c241cfb6f4f
Tangem <git@tangem.com>
2024-11-27 10:42:19 +0300

    Updated on 2026-08-27

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/ui/state/OnboardingMultiWalletUM.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/ui/state/OnboardingMultiWalletUM.kt
@@ -1,5 +1,7 @@
 package com.tangem.features.onboarding.v2.multiwallet.impl.ui.state
 
 internal data class OnboardingMultiWalletUM(
-    val artworkUrl: String? = null,
+    val artwork1Url: String? = null,
+    val artwork2Url: String? = null,
+    val artwork3Url: String? = null,
 )
