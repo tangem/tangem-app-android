f4dfe1fdf0a5215384557de318f1bf01bca81140
Tangem <git@tangem.com>
2024-11-28 18:35:01 +0500

    Updated on 2026-08-27

--- features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/main/entity/OnrampProviderBlockUM.kt
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/main/entity/OnrampProviderBlockUM.kt
@@ -1,11 +1,12 @@
 package com.tangem.features.onramp.main.entity
 
+import com.tangem.domain.onramp.model.OnrampPaymentMethod
+
 sealed class OnrampProviderBlockUM {
     data object Empty : OnrampProviderBlockUM()
     data object Loading : OnrampProviderBlockUM()
     data class Content(
-        val paymentMethodIconUrl: String,
-        val paymentMethodName: String,
+        val paymentMethod: OnrampPaymentMethod,
         val providerName: String,
         val isBestRate: Boolean,
         val onClick: () -> Unit,
