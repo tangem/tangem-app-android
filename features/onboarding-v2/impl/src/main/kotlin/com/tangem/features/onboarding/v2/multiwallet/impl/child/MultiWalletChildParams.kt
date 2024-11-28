7965084e247a7bbe194f005c352142e57cc5c368
Tangem <git@tangem.com>
2024-11-28 12:52:09 +0300

    Updated on 2026-08-07

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/MultiWalletChildParams.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/MultiWalletChildParams.kt
@@ -1,10 +1,12 @@
 package com.tangem.features.onboarding.v2.multiwallet.impl.child
 
 import com.tangem.features.onboarding.v2.multiwallet.api.OnboardingMultiWalletComponent
+import com.tangem.features.onboarding.v2.multiwallet.impl.MultiWalletInnerNavigationState
 import com.tangem.features.onboarding.v2.multiwallet.impl.model.OnboardingMultiWalletState
 import kotlinx.coroutines.flow.MutableStateFlow
 
 class MultiWalletChildParams(
     val multiWalletState: MutableStateFlow<OnboardingMultiWalletState>,
+    val innerNavigation: MutableStateFlow<MultiWalletInnerNavigationState>,
     val parentParams: OnboardingMultiWalletComponent.Params,
 )
--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/chooseoption/Wallet1ChooseOptionComponent.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/chooseoption/Wallet1ChooseOptionComponent.kt
@@ -3,14 +3,32 @@ package com.tangem.features.onboarding.v2.multiwallet.impl.child.chooseoption
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.tangem.core.decompose.context.AppComponentContext
+import com.tangem.core.ui.extensions.resourceReference
+import com.tangem.features.onboarding.v2.impl.R
 import com.tangem.features.onboarding.v2.multiwallet.impl.child.MultiWalletChildComponent
+import com.tangem.features.onboarding.v2.multiwallet.impl.child.MultiWalletChildParams
 import com.tangem.features.onboarding.v2.multiwallet.impl.child.chooseoption.ui.Wallet1ChooseOption
 import com.tangem.features.onboarding.v2.multiwallet.impl.model.OnboardingMultiWalletState
+import kotlinx.coroutines.flow.update
 
 class Wallet1ChooseOptionComponent(
     context: AppComponentContext,
+    params: MultiWalletChildParams,
     private val onNextStep: (OnboardingMultiWalletState.Step) -> Unit,
 ) : AppComponentContext by context, MultiWalletChildComponent {
+
+    init {
+        params.innerNavigation.update {
+            it.copy(
+                stackSize = 3,
+                stackMaxSize = 8,
+            )
+        }
+
+        params.parentParams.titleProvider.changeTitle(
+            text = resourceReference(R.string.onboarding_create_wallet_header),
+        )
+    }
 
     @Composable
     override fun Content(modifier: Modifier) {
