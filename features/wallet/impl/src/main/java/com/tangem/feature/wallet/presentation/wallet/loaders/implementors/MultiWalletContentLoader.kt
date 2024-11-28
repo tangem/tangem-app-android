a32c459d1c22d27007225eb0f5ea115781bb1718
Tangem <git@tangem.com>
2024-11-28 20:08:28 +0400

    release/5.26.1

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/loaders/implementors/MultiWalletContentLoader.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/loaders/implementors/MultiWalletContentLoader.kt
@@ -1,7 +1,6 @@
 package com.tangem.feature.wallet.presentation.wallet.loaders.implementors
 
 import com.tangem.domain.appcurrency.GetSelectedAppCurrencyUseCase
-import com.tangem.domain.exchange.RampStateManager
 import com.tangem.domain.tokens.ApplyTokenListSortingUseCase
 import com.tangem.domain.tokens.RunPolkadotAccountHealthCheckUseCase
 import com.tangem.domain.wallets.models.UserWallet
@@ -11,12 +10,10 @@ import com.tangem.feature.wallet.presentation.wallet.domain.GetMultiWalletWarnin
 import com.tangem.feature.wallet.presentation.wallet.domain.MultiWalletTokenListStore
 import com.tangem.feature.wallet.presentation.wallet.domain.WalletWithFundsChecker
 import com.tangem.feature.wallet.presentation.wallet.state.WalletStateController
-import com.tangem.feature.wallet.presentation.wallet.subscribers.MultiWalletActionsSubscriber
 import com.tangem.feature.wallet.presentation.wallet.subscribers.MultiWalletTokenListSubscriber
 import com.tangem.feature.wallet.presentation.wallet.subscribers.MultiWalletWarningsSubscriber
 import com.tangem.feature.wallet.presentation.wallet.subscribers.WalletSubscriber
 import com.tangem.feature.wallet.presentation.wallet.viewmodels.intents.WalletClickIntents
-import com.tangem.features.wallet.featuretoggles.WalletFeatureToggles
 
 @Suppress("LongParameterList")
 internal class MultiWalletContentLoader(
@@ -31,8 +28,6 @@ internal class MultiWalletContentLoader(
     private val applyTokenListSortingUseCase: ApplyTokenListSortingUseCase,
     private val getMultiWalletWarningsFactory: GetMultiWalletWarningsFactory,
     private val runPolkadotAccountHealthCheckUseCase: RunPolkadotAccountHealthCheckUseCase,
-    private val rampStateManager: RampStateManager,
-    private val walletFeatureToggles: WalletFeatureToggles,
 ) : WalletContentLoader(id = userWallet.walletId) {
 
     override fun create(): List<WalletSubscriber> {
@@ -55,16 +50,6 @@ internal class MultiWalletContentLoader(
                 getMultiWalletWarningsFactory = getMultiWalletWarningsFactory,
                 walletWarningsAnalyticsSender = walletWarningsAnalyticsSender,
             ),
-            createMultiWalletButtonsSubscriber(),
         )
-    }
-
-    private fun createMultiWalletButtonsSubscriber(): MultiWalletActionsSubscriber? {
-        return MultiWalletActionsSubscriber(
-            userWallet = userWallet,
-            rampStateManager = rampStateManager,
-            stateController = stateHolder,
-        )
-            .takeIf { walletFeatureToggles.isMainActionButtonsEnabled }
     }
 }
