f11084b33381f5a175312a98ad6f1d00b298db2e
Tangem <git@tangem.com>
2024-11-28 12:52:09 +0300

    release/5.27.0

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/seedphrase/MultiWalletSeedPhraseComponent.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/seedphrase/MultiWalletSeedPhraseComponent.kt
@@ -13,12 +13,16 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.tangem.core.decompose.context.AppComponentContext
 import com.tangem.core.decompose.model.getOrCreateModel
+import com.tangem.core.ui.extensions.resourceReference
+import com.tangem.features.onboarding.v2.impl.R
 import com.tangem.features.onboarding.v2.multiwallet.impl.child.MultiWalletChildComponent
 import com.tangem.features.onboarding.v2.multiwallet.impl.child.MultiWalletChildParams
 import com.tangem.features.onboarding.v2.multiwallet.impl.child.seedphrase.model.MultiWalletSeedPhraseModel
 import com.tangem.features.onboarding.v2.multiwallet.impl.child.seedphrase.ui.MultiWalletSeedPhrase
+import com.tangem.features.onboarding.v2.multiwallet.impl.child.seedphrase.ui.state.MultiWalletSeedPhraseUM
 import com.tangem.features.onboarding.v2.multiwallet.impl.model.OnboardingMultiWalletState
 import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 @Suppress("UnusedPrivateMember")
@@ -26,6 +30,7 @@ internal class MultiWalletSeedPhraseComponent(
     context: AppComponentContext,
     params: MultiWalletChildParams,
     onNextStep: (OnboardingMultiWalletState.Step) -> Unit,
+    onBack: () -> Unit,
     backButtonClickFlow: SharedFlow<Unit>,
 ) : AppComponentContext by context, MultiWalletChildComponent {
 
@@ -33,13 +38,36 @@ internal class MultiWalletSeedPhraseComponent(
 
     init {
         componentScope.launch {
+            model.uiState.collect {
+                // change stepper state based on the stack of the current step
+                @Suppress("MagicNumber")
+                params.innerNavigation.update { st ->
+                    st.copy(
+                        stackSize = 3 + it.order,
+                        stackMaxSize = 10,
+                    )
+                }
+
+                val title = when (it) {
+                    is MultiWalletSeedPhraseUM.Import -> R.string.onboarding_seed_intro_button_import
+                    is MultiWalletSeedPhraseUM.GenerateSeedPhrase,
+                    is MultiWalletSeedPhraseUM.GeneratedWordsCheck,
+                    is MultiWalletSeedPhraseUM.Start,
+                    -> R.string.onboarding_create_wallet_header
+                }
+
+                params.parentParams.titleProvider.changeTitle(text = resourceReference(title))
+            }
+        }
+
+        componentScope.launch {
             backButtonClickFlow.collect { model.onBack() }
         }
         componentScope.launch {
             model.onDone.collect { onNextStep(OnboardingMultiWalletState.Step.AddBackupDevice) }
         }
         componentScope.launch {
-            model.navigateBack.collect { onNextStep(OnboardingMultiWalletState.Step.CreateWallet) }
+            model.navigateBack.collect { onBack() }
         }
     }
 
