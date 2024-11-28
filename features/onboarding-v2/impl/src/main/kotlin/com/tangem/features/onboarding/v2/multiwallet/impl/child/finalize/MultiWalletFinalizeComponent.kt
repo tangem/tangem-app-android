463d9dc1a835f8313cf63fc419b79d9dde3de612
Tangem <git@tangem.com>
2024-11-28 12:52:09 +0300

    Updated on 2025-10-01

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/finalize/MultiWalletFinalizeComponent.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/finalize/MultiWalletFinalizeComponent.kt
@@ -6,10 +6,13 @@ import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.tangem.core.decompose.context.AppComponentContext
 import com.tangem.core.decompose.model.getOrCreateModel
+import com.tangem.core.ui.extensions.resourceReference
+import com.tangem.features.onboarding.v2.impl.R
 import com.tangem.features.onboarding.v2.multiwallet.impl.child.MultiWalletChildComponent
 import com.tangem.features.onboarding.v2.multiwallet.impl.child.MultiWalletChildParams
 import com.tangem.features.onboarding.v2.multiwallet.impl.child.finalize.model.MultiWalletFinalizeModel
 import com.tangem.features.onboarding.v2.multiwallet.impl.child.finalize.ui.MultiWalletFinalize
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 @Suppress("UnusedPrivateMember")
@@ -22,6 +25,16 @@ internal class MultiWalletFinalizeComponent(
     private val model: MultiWalletFinalizeModel = getOrCreateModel(params)
 
     init {
+        params.innerNavigation.update {
+            it.copy(
+                stackSize = 7,
+                stackMaxSize = 8,
+            )
+        }
+        params.parentParams.titleProvider.changeTitle(
+            resourceReference(R.string.onboarding_button_finalize_backup),
+        )
+
         componentScope.launch {
             model.onEvent.collect {
                 onEvent(it)
