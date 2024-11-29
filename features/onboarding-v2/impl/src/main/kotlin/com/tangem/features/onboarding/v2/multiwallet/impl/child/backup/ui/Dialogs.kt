ddc105d3ab729eddb25827d37d40d8eec6758c3b
Tangem <git@tangem.com>
2024-11-29 16:39:09 +0300

    Updated on 2026-04-15

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/backup/ui/Dialogs.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/backup/ui/Dialogs.kt
@@ -4,16 +4,20 @@ import com.tangem.core.ui.extensions.resourceReference
 import com.tangem.features.onboarding.v2.impl.R
 import com.tangem.features.onboarding.v2.multiwallet.impl.child.backup.ui.state.MultiWalletBackupUMDialog
 
-internal fun resetBackupCardDialog(onReset: () -> Unit, onDismiss: () -> Unit) = MultiWalletBackupUMDialog(
-    title = resourceReference(R.string.common_attention),
-    bodyText = resourceReference(R.string.onboarding_linking_error_card_with_wallets),
-    confirmText = resourceReference(R.string.common_cancel),
-    cancelText = resourceReference(R.string.card_settings_action_sheet_reset),
-    warningCancelColor = true,
-    onDismiss = onDismiss,
-    onConfirm = onDismiss,
-    onCancel = { onReset(); onDismiss() },
-)
+internal fun resetBackupCardDialog(onReset: () -> Unit, onDismiss: () -> Unit, onDismissClick: () -> Unit) =
+    MultiWalletBackupUMDialog(
+        title = resourceReference(R.string.common_attention),
+        bodyText = resourceReference(R.string.onboarding_linking_error_card_with_wallets),
+        confirmText = resourceReference(R.string.common_cancel),
+        cancelText = resourceReference(R.string.card_settings_action_sheet_reset),
+        warningCancelColor = true,
+        onDismiss = {
+            onDismiss()
+            onDismissClick()
+        },
+        onConfirm = onDismiss,
+        onCancel = { onReset(); onDismiss() },
+    )
 
 internal fun backupCardAttestationFailedDialog(onDismiss: () -> Unit) = MultiWalletBackupUMDialog(
     title = resourceReference(R.string.common_error),
