82a09cb0b17a5b881cfef9d44db78cda56075ce4
Tangem <git@tangem.com>
2024-11-29 11:05:00 +0300

    release/5.26.1

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/chooseoption/Wallet1ChooseOptionComponent.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/chooseoption/Wallet1ChooseOptionComponent.kt
@@ -1,21 +1,27 @@
 package com.tangem.features.onboarding.v2.multiwallet.impl.child.chooseoption
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.tangem.core.decompose.context.AppComponentContext
+import com.tangem.core.decompose.model.getOrCreateModel
 import com.tangem.core.ui.extensions.resourceReference
 import com.tangem.features.onboarding.v2.impl.R
 import com.tangem.features.onboarding.v2.multiwallet.impl.child.MultiWalletChildComponent
 import com.tangem.features.onboarding.v2.multiwallet.impl.child.MultiWalletChildParams
+import com.tangem.features.onboarding.v2.multiwallet.impl.child.chooseoption.model.Wallet1ChooseOptionModel
 import com.tangem.features.onboarding.v2.multiwallet.impl.child.chooseoption.ui.Wallet1ChooseOption
 import com.tangem.features.onboarding.v2.multiwallet.impl.model.OnboardingMultiWalletState
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class Wallet1ChooseOptionComponent(
     context: AppComponentContext,
     params: MultiWalletChildParams,
     private val onNextStep: (OnboardingMultiWalletState.Step) -> Unit,
 ) : AppComponentContext by context, MultiWalletChildComponent {
+
+    private val model: Wallet1ChooseOptionModel = getOrCreateModel(params)
 
     init {
         params.innerNavigation.update {
@@ -28,6 +34,12 @@ class Wallet1ChooseOptionComponent(
         params.parentParams.titleProvider.changeTitle(
             text = resourceReference(R.string.onboarding_create_wallet_header),
         )
+
+        componentScope.launch {
+            model.returnToParentFlow.collect {
+                onNextStep(OnboardingMultiWalletState.Step.Done)
+            }
+        }
     }
 
     @Composable
@@ -36,8 +48,10 @@ class Wallet1ChooseOptionComponent(
             onBackupClick = {
                 onNextStep(OnboardingMultiWalletState.Step.AddBackupDevice)
             },
-            onScipClick = {
-                onNextStep(OnboardingMultiWalletState.Step.Done)
+            onSkipClick = remember(model) {
+                {
+                    model.onSkipClick()
+                }
             },
         )
     }
