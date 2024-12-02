98539c200bc9bc7969d236768b425f98a451961a
Tangem <git@tangem.com>
2024-12-02 17:37:19 +0400

    Updated on 2026-08-07

--- features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/selecttoken/ui/OnrampSelectToken.kt
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/selecttoken/ui/OnrampSelectToken.kt
@@ -9,10 +9,12 @@ import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.tangem.core.ui.components.appbar.AppBarWithBackButton
 import com.tangem.core.ui.res.TangemTheme
+import com.tangem.core.ui.utils.rememberHideKeyboardNestedScrollConnection
 import com.tangem.features.onramp.impl.R
 import com.tangem.features.onramp.selecttoken.entity.OnrampOperationUM
 import com.tangem.features.onramp.tokenlist.OnrampTokenListComponent
@@ -26,8 +28,10 @@ internal fun OnrampSelectToken(
 ) {
     BackHandler(onBack = state.onBackClick)
 
+    val nestedScrollConnection = rememberHideKeyboardNestedScrollConnection()
     LazyColumn(
         modifier = modifier
+            .nestedScroll(nestedScrollConnection)
             .background(TangemTheme.colors.background.secondary)
             .imePadding()
             .systemBarsPadding(),
