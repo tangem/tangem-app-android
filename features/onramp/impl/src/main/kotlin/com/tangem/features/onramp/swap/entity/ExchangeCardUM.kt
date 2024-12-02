35d9d053d4460cfd8e45e483bc58b5be7287cf6c
Tangem <git@tangem.com>
2024-12-02 18:56:22 +0400

    Updated on 2026-08-19

--- features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/swap/entity/ExchangeCardUM.kt
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/swap/entity/ExchangeCardUM.kt
@@ -1,10 +1,7 @@
 package com.tangem.features.onramp.swap.entity
 
-import com.tangem.core.ui.components.currency.icon.CurrencyIconState
 import com.tangem.core.ui.components.token.state.TokenItemState
 import com.tangem.core.ui.extensions.TextReference
-import com.tangem.core.ui.extensions.resourceReference
-import com.tangem.features.onramp.impl.R
 
 /**
  * Exchange card UI model
@@ -19,9 +16,6 @@ internal sealed interface ExchangeCardUM {
     /** Remove button UI model */
     val removeButtonUM: RemoveButtonUM?
 
-    /** Token item state */
-    val tokenItemState: TokenItemState
-
     /**
      * Empty state
      *
@@ -34,19 +28,6 @@ internal sealed interface ExchangeCardUM {
     ) : ExchangeCardUM {
 
         override val removeButtonUM: RemoveButtonUM? = null
-
-        override val tokenItemState: TokenItemState = TokenItemState.Content(
-            id = "empty",
-            iconState = CurrencyIconState.Empty(R.drawable.ic_empty_64),
-            titleState = TokenItemState.TitleState.Content(
-                text = resourceReference(id = R.string.action_buttons_swap_choose_token),
-            ),
-            subtitleState = TokenItemState.SubtitleState.TextContent(value = subtitleReference),
-            fiatAmountState = TokenItemState.FiatAmountState.Content(text = ""),
-            subtitle2State = TokenItemState.Subtitle2State.TextContent(text = ""),
-            onItemClick = null,
-            onItemLongClick = null,
-        )
     }
 
     /**
@@ -59,7 +40,7 @@ internal sealed interface ExchangeCardUM {
     data class Filled(
         override val titleReference: TextReference,
         override val removeButtonUM: RemoveButtonUM?,
-        override val tokenItemState: TokenItemState,
+        val tokenItemState: TokenItemState,
     ) : ExchangeCardUM
 
     data class RemoveButtonUM(val onClick: () -> Unit)
