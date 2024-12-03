81a108a57021149334df7bb3c4c037c7168e8e4b
Doston Kamalov <dkamalov@tangem.com>
2024-12-03 19:22:46 +0500

    AND-9286 Fixed data loading after residence confirmed

--- features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/main/model/OnrampMainComponentModel.kt
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/main/model/OnrampMainComponentModel.kt
@@ -75,6 +75,12 @@ internal class OnrampMainComponentModel @Inject constructor(
         )
     }
 
+    fun handleOnrampAvailable(currency: OnrampCurrency) {
+        _state.update { stateFactory.getReadyState(currency) }
+        subscribeToCurrencyUpdates()
+        subscribeToQuotesUpdate()
+    }
+
     fun onProviderSelected(providerWithQuote: OnrampProviderWithQuote.Data) {
         _state.update { amountStateFactory.getAmountSecondaryUpdatedState(providerWithQuote) }
     }
@@ -94,12 +100,6 @@ internal class OnrampMainComponentModel @Inject constructor(
             is OnrampAvailability.NotSupported,
             -> bottomSheetNavigation.activate(OnrampMainBottomSheetConfig.ConfirmResidency(availability.country))
         }
-    }
-
-    private fun handleOnrampAvailable(currency: OnrampCurrency) {
-        _state.update { stateFactory.getReadyState(currency) }
-        subscribeToCurrencyUpdates()
-        subscribeToQuotesUpdate()
     }
 
     private fun subscribeToCurrencyUpdates() {
