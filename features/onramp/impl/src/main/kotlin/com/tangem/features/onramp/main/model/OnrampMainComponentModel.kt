26024f3176bf2a636ce2c1f113cb93f00cd538af
Tangem <git@tangem.com>
2024-12-03 20:17:14 +0500

    Updated on 2026-06-27

--- features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/main/model/OnrampMainComponentModel.kt
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/main/model/OnrampMainComponentModel.kt
@@ -1,16 +1,26 @@
 package com.tangem.features.onramp.main.model
 
+import androidx.compose.ui.res.stringResource
 import com.arkivanov.decompose.router.slot.SlotNavigation
 import com.arkivanov.decompose.router.slot.activate
+import com.tangem.common.ui.alerts.models.AlertDemoModeUM
 import com.tangem.core.analytics.api.AnalyticsEventHandler
 import com.tangem.core.decompose.model.Model
 import com.tangem.core.decompose.model.ParamsContainer
 import com.tangem.core.decompose.navigation.Router
+import com.tangem.core.decompose.ui.UiMessageSender
+import com.tangem.core.ui.components.BasicDialog
+import com.tangem.core.ui.components.DialogButtonUM
+import com.tangem.core.ui.extensions.resolveReference
+import com.tangem.core.ui.message.ContentMessage
+import com.tangem.domain.demo.IsDemoCardUseCase
 import com.tangem.domain.onramp.*
 import com.tangem.domain.onramp.analytics.OnrampAnalyticsEvent
 import com.tangem.domain.onramp.model.OnrampAvailability
 import com.tangem.domain.onramp.model.OnrampCurrency
 import com.tangem.domain.onramp.model.OnrampProviderWithQuote
+import com.tangem.domain.wallets.usecase.GetWalletsUseCase
+import com.tangem.features.onramp.impl.R
 import com.tangem.features.onramp.main.OnrampMainComponent
 import com.tangem.features.onramp.main.entity.OnrampIntents
 import com.tangem.features.onramp.main.entity.OnrampMainBottomSheetConfig
@@ -31,12 +41,15 @@ internal class OnrampMainComponentModel @Inject constructor(
     override val dispatchers: CoroutineDispatcherProvider,
     private val analyticsEventHandler: AnalyticsEventHandler,
     private val router: Router,
+    private val isDemoCardUseCase: IsDemoCardUseCase,
     private val checkOnrampAvailabilityUseCase: CheckOnrampAvailabilityUseCase,
     private val getOnrampCurrencyUseCase: GetOnrampCurrencyUseCase,
     private val clearOnrampCacheUseCase: ClearOnrampCacheUseCase,
     private val fetchQuotesUseCase: OnrampFetchQuotesUseCase,
     private val getOnrampQuotesUseCase: GetOnrampQuotesUseCase,
     private val amountInputManager: InputManager,
+    private val messageSender: UiMessageSender,
+    getWalletsUseCase: GetWalletsUseCase,
     paramsContainer: ParamsContainer,
 ) : Model(), OnrampIntents {
 
@@ -50,7 +63,7 @@ internal class OnrampMainComponentModel @Inject constructor(
         currentStateProvider = Provider { _state.value },
         onrampIntents = this,
     )
-
+    private val selectedUserWallet = getWalletsUseCase.invokeSync().first { it.walletId == params.userWalletId }
     private val _state: MutableStateFlow<OnrampMainComponentUM> = MutableStateFlow(
         value = stateFactory.getInitialState(
             currency = params.cryptoCurrency.name,
@@ -145,15 +158,19 @@ internal class OnrampMainComponentModel @Inject constructor(
     }
 
     override fun onBuyClick(quote: OnrampProviderWithQuote.Data) {
-        val currentContentState = state.value as? OnrampMainComponentUM.Content ?: return
-        analyticsEventHandler.send(
-            OnrampAnalyticsEvent.OnBuyClick(
-                providerName = quote.provider.info.name,
-                currency = currentContentState.amountBlockState.currencyUM.code,
-                cryptoCurrency = params.cryptoCurrency.name,
-            ),
-        )
-        params.openRedirectPage(quote)
+        if (isDemoCardUseCase.invoke(selectedUserWallet.cardId)) {
+            showDemoWarning()
+        } else {
+            val currentContentState = state.value as? OnrampMainComponentUM.Content ?: return
+            analyticsEventHandler.send(
+                OnrampAnalyticsEvent.OnBuyClick(
+                    providerName = quote.provider.info.name,
+                    currency = currentContentState.amountBlockState.currencyUM.code,
+                    cryptoCurrency = params.cryptoCurrency.name,
+                ),
+            )
+            params.openRedirectPage(quote)
+        }
     }
 
     override fun openCurrenciesList() {
@@ -174,5 +191,34 @@ internal class OnrampMainComponentModel @Inject constructor(
     override fun onDestroy() {
         modelScope.launch { clearOnrampCacheUseCase.invoke() }
         super.onDestroy()
+    }
+
+    private fun showDemoWarning() {
+        val alertUM = AlertDemoModeUM(onConfirmClick = {})
+
+        messageSender.send(
+            message = ContentMessage { onDismiss ->
+                val confirmButton = DialogButtonUM(
+                    title = alertUM.confirmButtonText.resolveReference(),
+                    onClick = {
+                        alertUM.onConfirmClick()
+                        onDismiss()
+                    },
+                )
+
+                val dismissButton = DialogButtonUM(
+                    title = stringResource(id = R.string.common_cancel),
+                    onClick = onDismiss,
+                )
+
+                BasicDialog(
+                    message = alertUM.message.resolveReference(),
+                    confirmButton = confirmButton,
+                    onDismissDialog = onDismiss,
+                    title = alertUM.title.resolveReference(),
+                    dismissButton = dismissButton,
+                )
+            },
+        )
     }
 }
