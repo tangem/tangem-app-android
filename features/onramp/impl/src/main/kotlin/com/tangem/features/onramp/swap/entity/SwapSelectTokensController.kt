38ebdf396f70c764f882885a4d207ae735f5fc2e
Tangem <git@tangem.com>
2024-12-03 15:44:56 +0400

    Updated on 2026-06-17

--- features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/swap/entity/SwapSelectTokensController.kt
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/swap/entity/SwapSelectTokensController.kt
@@ -22,6 +22,7 @@ internal class SwapSelectTokensController @Inject constructor() {
             onBackClick = {},
             exchangeFrom = createEmptyExchangeFrom(),
             exchangeTo = createEmptyExchangeTo(),
+            isBalanceHidden = false,
         ),
     )
 
