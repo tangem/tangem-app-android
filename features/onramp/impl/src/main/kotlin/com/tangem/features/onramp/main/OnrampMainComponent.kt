95331b4fa7cf8a6824a28fc6dc5baaff006775ca
Tangem <git@tangem.com>
2024-12-03 13:45:26 +0500

    Updated on 2026-07-07

--- features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/main/OnrampMainComponent.kt
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/main/OnrampMainComponent.kt
@@ -3,12 +3,14 @@ package com.tangem.features.onramp.main
 import com.tangem.core.decompose.factory.ComponentFactory
 import com.tangem.core.ui.decompose.ComposableContentComponent
 import com.tangem.domain.onramp.model.OnrampProviderWithQuote
+import com.tangem.domain.onramp.model.OnrampSource
 import com.tangem.domain.tokens.model.CryptoCurrency
 
 internal interface OnrampMainComponent : ComposableContentComponent {
 
     data class Params(
         val cryptoCurrency: CryptoCurrency,
+        val source: OnrampSource,
         val openSettings: () -> Unit,
         val openRedirectPage: (quote: OnrampProviderWithQuote.Data) -> Unit,
     )
