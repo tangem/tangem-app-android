4c0be7e6ad913e147cace1928402724daead5d30
Tangem <git@tangem.com>
2024-12-03 20:17:14 +0500

    Updated on 2025-10-13

--- features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/redirect/model/OnrampRedirectModel.kt
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/redirect/model/OnrampRedirectModel.kt
@@ -1,14 +1,19 @@
 package com.tangem.features.onramp.redirect.model
 
+import androidx.compose.ui.res.stringResource
 import com.tangem.core.decompose.model.Model
 import com.tangem.core.decompose.model.ParamsContainer
 import com.tangem.core.decompose.navigation.Router
+import com.tangem.core.decompose.ui.UiMessageSender
 import com.tangem.core.navigation.url.UrlOpener
+import com.tangem.core.ui.components.BasicDialog
+import com.tangem.core.ui.components.DialogButtonUM
 import com.tangem.core.ui.components.appbar.models.TopAppBarButtonUM
 import com.tangem.core.ui.extensions.combinedReference
 import com.tangem.core.ui.extensions.resourceReference
 import com.tangem.core.ui.extensions.stringReference
 import com.tangem.core.ui.extensions.wrappedList
+import com.tangem.core.ui.message.ContentMessage
 import com.tangem.domain.onramp.GetOnrampRedirectUrlUseCase
 import com.tangem.features.onramp.impl.R
 import com.tangem.features.onramp.redirect.OnrampRedirectComponent
@@ -23,6 +28,7 @@ internal class OnrampRedirectModel @Inject constructor(
     override val dispatchers: CoroutineDispatcherProvider,
     private val urlOpener: UrlOpener,
     private val getOnrampRedirectUrlUseCase: GetOnrampRedirectUrlUseCase,
+    private val messageSender: UiMessageSender,
     router: Router,
     paramsContainer: ParamsContainer,
 ) : Model() {
@@ -62,8 +68,33 @@ internal class OnrampRedirectModel @Inject constructor(
                 quote = params.onrampProviderWithQuote,
                 cryptoCurrency = params.cryptoCurrency,
             )
-                .onLeft { Timber.e(it) }
-                .onRight { urlOpener.openUrl(it) }
+                .onLeft(::handleError)
+                .onRight {
+                    params.onBack()
+                    urlOpener.openUrl(it)
+                }
         }
+    }
+
+    private fun handleError(throwable: Throwable) {
+        Timber.e(throwable)
+
+        val contentMessage = ContentMessage { onDismiss ->
+            BasicDialog(
+                message = stringResource(id = R.string.common_unknown_error),
+                confirmButton = DialogButtonUM(
+                    title = stringResource(id = R.string.common_ok),
+                    onClick = {
+                        params.onBack()
+                        onDismiss()
+                    },
+                ),
+                onDismissDialog = {
+                    params.onBack()
+                    onDismiss()
+                },
+            )
+        }
+        messageSender.send(contentMessage)
     }
 }
