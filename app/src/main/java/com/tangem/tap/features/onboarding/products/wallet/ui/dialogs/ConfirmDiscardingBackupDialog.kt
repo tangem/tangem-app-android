7952a7f0eecb1a98e98efb3ddd3e85592b30682a
Tangem <git@tangem.com>
2024-12-05 10:35:27 +0300

    Updated on 2026-04-06

--- app/src/main/java/com/tangem/tap/features/onboarding/products/wallet/ui/dialogs/ConfirmDiscardingBackupDialog.kt
+++ app/src/main/java/com/tangem/tap/features/onboarding/products/wallet/ui/dialogs/ConfirmDiscardingBackupDialog.kt
@@ -3,18 +3,19 @@ package com.tangem.tap.features.onboarding.products.wallet.ui.dialogs
 import android.content.Context
 import androidx.appcompat.app.AlertDialog
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.tangem.domain.models.scan.ScanResponse
 import com.tangem.tap.common.redux.global.GlobalAction
 import com.tangem.tap.features.onboarding.products.wallet.redux.BackupAction
 import com.tangem.tap.store
 import com.tangem.wallet.R
 
 object ConfirmDiscardingBackupDialog {
-    fun create(context: Context): AlertDialog {
+    fun create(context: Context, unfinishedBackupScanResponse: ScanResponse? = null): AlertDialog {
         return MaterialAlertDialogBuilder(context, R.style.CustomMaterialDialog).apply {
             setTitle(R.string.welcome_interrupted_backup_discard_title)
             setMessage(R.string.welcome_interrupted_backup_discard_message)
             setPositiveButton(R.string.welcome_interrupted_backup_discard_resume) { _, _ ->
-                store.dispatch(BackupAction.ResumeFoundUnfinishedBackup)
+                store.dispatch(BackupAction.ResumeFoundUnfinishedBackup(unfinishedBackupScanResponse))
             }
             setNegativeButton(R.string.welcome_interrupted_backup_discard_discard) { _, _ ->
                 store.dispatch(BackupAction.DiscardSavedBackup)
--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/backup/model/BackupState.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/backup/model/BackupState.kt
@@ -1,6 +1,5 @@
 package com.tangem.features.onboarding.v2.multiwallet.impl.child.backup.model
 
-// TODO maybe delete some fields in the future
 data class BackupState(
-    val backupCardsNumber: Int = 0,
+    val numberOfBackupCards: Int = 0,
 )
