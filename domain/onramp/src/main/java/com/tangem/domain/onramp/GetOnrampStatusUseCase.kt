6f1141a956f0a0da6d479cd6a5948c8602226de8
Tangem <git@tangem.com>
2024-12-05 12:45:38 +0500

    Updated on 2025-10-01

--- domain/onramp/src/main/java/com/tangem/domain/onramp/GetOnrampStatusUseCase.kt
+++ domain/onramp/src/main/java/com/tangem/domain/onramp/GetOnrampStatusUseCase.kt
@@ -5,19 +5,15 @@ import com.tangem.domain.onramp.model.OnrampError
 import com.tangem.domain.onramp.model.OnrampStatus
 import com.tangem.domain.onramp.repositories.OnrampErrorResolver
 import com.tangem.domain.onramp.repositories.OnrampRepository
-import com.tangem.domain.onramp.repositories.OnrampTransactionRepository
 
 class GetOnrampStatusUseCase(
     private val onrampRepository: OnrampRepository,
-    private val onrampTransactionRepository: OnrampTransactionRepository,
     private val errorResolver: OnrampErrorResolver,
 ) {
 
     suspend operator fun invoke(txId: String): Either<OnrampError, OnrampStatus> {
         return Either.catch {
-            val status = onrampRepository.getStatus(txId)
-            onrampTransactionRepository.updateTransactionStatus(txId = txId, status = status.status)
-            status
+            onrampRepository.getStatus(txId)
         }.mapLeft {
             errorResolver.resolve(it)
         }
