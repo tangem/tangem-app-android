4bab9e8c257d68494915fd9755f45f650193b101
Tangem <git@tangem.com>
2024-12-06 17:49:11 +0500

    Updated on 2026-05-07

--- domain/onramp/src/main/java/com/tangem/domain/onramp/GetOnrampCurrencyUseCase.kt
+++ domain/onramp/src/main/java/com/tangem/domain/onramp/GetOnrampCurrencyUseCase.kt
@@ -4,16 +4,23 @@ import arrow.core.Either
 import arrow.core.left
 import arrow.core.right
 import com.tangem.domain.onramp.model.OnrampCurrency
+import com.tangem.domain.onramp.model.error.OnrampError
+import com.tangem.domain.onramp.repositories.OnrampErrorResolver
 import com.tangem.domain.onramp.repositories.OnrampRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 
-class GetOnrampCurrencyUseCase(private val repository: OnrampRepository) {
+class GetOnrampCurrencyUseCase(
+    private val repository: OnrampRepository,
+    private val errorResolver: OnrampErrorResolver,
+) {
 
-    operator fun invoke(): Flow<Either<Throwable, OnrampCurrency?>> {
+    operator fun invoke(): Flow<Either<OnrampError, OnrampCurrency?>> {
         return repository.getDefaultCurrency()
-            .map<OnrampCurrency?, Either<Throwable, OnrampCurrency?>> { it.right() }
-            .catch { emit(it.left()) }
+            .map<OnrampCurrency?, Either<OnrampError, OnrampCurrency?>> { it.right() }
+            .catch {
+                emit(errorResolver.resolve(it).left())
+            }
     }
 }
