36ea8591816f3fa7907b66b61b5283921224da2a
Tangem <git@tangem.com>
2024-12-09 11:02:33 +0500

    Updated on 2026-06-27

--- data/onramp/src/main/java/com/tangem/data/onramp/DefaultOnrampErrorResolver.kt
+++ data/onramp/src/main/java/com/tangem/data/onramp/DefaultOnrampErrorResolver.kt
@@ -1,20 +1,17 @@
 package com.tangem.data.onramp
 
-import com.tangem.core.analytics.api.AnalyticsEventHandler
 import com.tangem.data.onramp.converters.error.OnrampErrorConverter
 import com.tangem.datasource.api.common.response.ApiResponseError
-import com.tangem.domain.onramp.analytics.OnrampAnalyticsEvent
 import com.tangem.domain.onramp.model.error.OnrampError
 import com.tangem.domain.onramp.model.error.OnrampRedirectError
 import com.tangem.domain.onramp.repositories.OnrampErrorResolver
 
 internal class DefaultOnrampErrorResolver(
-    private val analyticsEventHandler: AnalyticsEventHandler,
     private val onrampErrorConverter: OnrampErrorConverter,
 ) : OnrampErrorResolver {
 
     override fun resolve(throwable: Throwable): OnrampError {
-        val error = when (throwable) {
+        return when (throwable) {
             is ApiResponseError.HttpException -> {
                 onrampErrorConverter.convert(throwable.errorBody.orEmpty())
             }
@@ -24,20 +21,5 @@ internal class DefaultOnrampErrorResolver(
                 OnrampError.DomainError(throwable.message)
             }
         }
-
-        when (error) {
-            is OnrampError.AmountError.TooBigError -> analyticsEventHandler.send(OnrampAnalyticsEvent.MaxAmountError)
-            is OnrampError.AmountError.TooSmallError -> analyticsEventHandler.send(OnrampAnalyticsEvent.MinAmountError)
-            is OnrampError.DataError -> analyticsEventHandler.send(
-                OnrampAnalyticsEvent.Errors(
-                    tokenSymbol = "tokenSymbol",
-                    providerName = "providerName",
-                    errorCode = error.code,
-                ),
-            )
-            else -> { /* no-op */
-            }
-        }
-        return error
     }
 }
