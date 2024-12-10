8cc64f99c66858751e0a415ae99e5d8819b301ce
Tangem <git@tangem.com>
2024-12-10 17:30:15 +0300

    Updated on 2026-08-27

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/visa/impl/child/welcome/OnboardingVisaWelcomeComponent.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/visa/impl/child/welcome/OnboardingVisaWelcomeComponent.kt
@@ -6,8 +6,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.tangem.core.decompose.context.AppComponentContext
 import com.tangem.core.ui.decompose.ComposableContentComponent
+import com.tangem.features.onboarding.v2.visa.impl.child.welcome.ui.OnboardingVisaWelcome
+import com.tangem.features.onboarding.v2.visa.impl.child.welcome.ui.state.OnboardingVisaWelcomeUM
 
-class OnboardingVisaWelcomeComponent(
+internal class OnboardingVisaWelcomeComponent(
     appComponentContext: AppComponentContext,
     private val params: Params,
 ) : ComposableContentComponent, AppComponentContext by appComponentContext {
@@ -15,9 +17,15 @@ class OnboardingVisaWelcomeComponent(
     @Composable
     override fun Content(modifier: Modifier) {
         BackHandler(onBack = remember(this) { { params.onBack() } })
+
+        OnboardingVisaWelcome(
+            modifier = modifier,
+            state = OnboardingVisaWelcomeUM(),
+        )
     }
 
     data class Params(
         val onBack: () -> Unit,
+        val startActivation: () -> Unit,
     )
 }
