5a11989101bb10c36ad9d74189403f7850b8dc38
Tangem <git@tangem.com>
2024-12-11 21:41:39 +0500

    Updated on 2026-09-21

--- core/datasource/src/main/java/com/tangem/datasource/exchangeservice/swap/ExpressServiceLoader.kt
+++ core/datasource/src/main/java/com/tangem/datasource/exchangeservice/swap/ExpressServiceLoader.kt
@@ -1,20 +1,20 @@
 package com.tangem.datasource.exchangeservice.swap
 
+import com.tangem.datasource.api.express.models.request.LeastTokenInfo
 import com.tangem.datasource.api.express.models.response.Asset
-import com.tangem.datasource.api.tangemTech.models.UserTokensResponse
 import com.tangem.domain.core.lce.Lce
 import com.tangem.domain.wallets.models.UserWalletId
 import kotlinx.coroutines.flow.StateFlow
 
 /**
- * Swap service loader
+ * Express service loader
  *
 [REDACTED_AUTHOR]
  */
-interface SwapServiceLoader {
+interface ExpressServiceLoader {
 
     /** Update service using [userWalletId] and [userTokens] */
-    suspend fun update(userWalletId: UserWalletId, userTokens: UserTokensResponse)
+    suspend fun update(userWalletId: UserWalletId, userTokens: List<LeastTokenInfo>)
 
     /** Get initialization status by [userWalletId] */
     fun getInitializationStatus(userWalletId: UserWalletId): StateFlow<Lce<Throwable, List<Asset>>>
