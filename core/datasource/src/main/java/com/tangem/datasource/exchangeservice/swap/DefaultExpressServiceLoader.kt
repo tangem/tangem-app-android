8d7bb70873f34f3e67caf04e8c46eeeae2eedcf7
Tangem <git@tangem.com>
2024-12-11 21:41:39 +0500

    Updated on 2026-08-19

--- core/datasource/src/main/java/com/tangem/datasource/exchangeservice/swap/DefaultExpressServiceLoader.kt
+++ core/datasource/src/main/java/com/tangem/datasource/exchangeservice/swap/DefaultExpressServiceLoader.kt
@@ -2,11 +2,9 @@ package com.tangem.datasource.exchangeservice.swap
 
 import com.tangem.datasource.api.common.response.getOrThrow
 import com.tangem.datasource.api.express.TangemExpressApi
-import com.tangem.datasource.api.express.models.TangemExpressValues.EMPTY_CONTRACT_ADDRESS_VALUE
 import com.tangem.datasource.api.express.models.request.AssetsRequestBody
 import com.tangem.datasource.api.express.models.request.LeastTokenInfo
 import com.tangem.datasource.api.express.models.response.Asset
-import com.tangem.datasource.api.tangemTech.models.UserTokensResponse
 import com.tangem.datasource.local.token.ExpressAssetsStore
 import com.tangem.domain.core.lce.Lce
 import com.tangem.domain.core.utils.lceContent
@@ -23,39 +21,32 @@ import javax.inject.Inject
 typealias InitializationStatusFlow = MutableStateFlow<Lce<Throwable, List<Asset>>>
 
 /**
- * Default implementation of [SwapServiceLoader]
+ * Default implementation of [ExpressServiceLoader]
  *
  * @property tangemExpressApi   express api
  * @property expressAssetsStore local storage
  *
 [REDACTED_AUTHOR]
  */
-internal class DefaultSwapServiceLoader @Inject constructor(
+internal class DefaultExpressServiceLoader @Inject constructor(
     private val tangemExpressApi: TangemExpressApi,
     private val expressAssetsStore: ExpressAssetsStore,
     private val dispatchers: CoroutineDispatcherProvider,
-) : SwapServiceLoader {
+) : ExpressServiceLoader {
 
     private val initializationStatuses =
         MutableStateFlow<Map<UserWalletId, InitializationStatusFlow>>(value = emptyMap())
 
-    override suspend fun update(userWalletId: UserWalletId, userTokens: UserTokensResponse) {
+    override suspend fun update(userWalletId: UserWalletId, userTokens: List<LeastTokenInfo>) {
         withContext(dispatchers.io) {
             val initializationStatus = getInitializationStatusInternal(userWalletId)
 
             initializationStatus.update { lceLoading() }
 
             try {
-                val tokensList = userTokens.tokens.map {
-                    LeastTokenInfo(
-                        contractAddress = it.contractAddress ?: EMPTY_CONTRACT_ADDRESS_VALUE,
-                        network = it.networkId,
-                    )
-                }
-
-                if (tokensList.isNotEmpty()) {
+                if (userTokens.isNotEmpty()) {
                     val response = tangemExpressApi.getAssets(
-                        body = AssetsRequestBody(tokensList = tokensList),
+                        body = AssetsRequestBody(tokensList = userTokens),
                     ).getOrThrow()
 
                     expressAssetsStore.store(userWalletId, response)
