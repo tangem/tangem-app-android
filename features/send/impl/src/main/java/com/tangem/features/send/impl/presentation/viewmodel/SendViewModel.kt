5bb1f54604a77e3be0bb99fd8831096a34c6aed4
Tangem <git@tangem.com>
2024-12-12 02:39:36 +0500

    Updated on 2026-09-25

--- features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/viewmodel/SendViewModel.kt
+++ features/send/impl/src/main/java/com/tangem/features/send/impl/presentation/viewmodel/SendViewModel.kt
@@ -513,13 +513,12 @@ internal class SendViewModel @Inject constructor(
         when (currentState.type) {
             SendUiStateType.Fee,
             SendUiStateType.EditFee,
-            -> if (onFeeNext()) return
+            -> if (onFeeNextIntercept(isFromEdit)) return
             SendUiStateType.Amount,
             SendUiStateType.EditAmount,
             -> loadFee()
             else -> Unit
         }
-
         stateRouter.onNextClick()
     }
 
@@ -580,9 +579,23 @@ internal class SendViewModel @Inject constructor(
 
     override fun onTokenDetailsClick(currency: CryptoCurrency) = innerRouter.openTokenDetails(userWalletId, currency)
 
-    private fun onFeeNext(): Boolean {
+    private fun onFeeNextIntercept(isFromEdit: Boolean): Boolean {
         val feeState = uiState.value.getFeeState(stateRouter.isEditState)
         val feeSelectorState = feeState?.feeSelectorState as? FeeSelectorState.Content ?: return false
+
+        if (isFromEdit) {
+            // in some cases, if it's possible to fix incorrect fee automatically,
+            // do it, update current state and go let continue the flow
+            val fixedState = feeStateFactory.tryAutoFixCustomFeeValue()
+            if (fixedState.editFeeState != feeState) {
+                uiState.value = fixedState
+                val currentState = stateRouter.currentState.value
+                uiState.value = stateFactory.syncEditStates(isFromEdit = isFromEdit)
+                sendScreenAnalyticSender.send(currentState.type, uiState.value)
+                return false
+            }
+        }
+
         if (checkIfFeeTooLow(feeSelectorState)) {
             uiState.value = eventStateFactory.getFeeTooLowAlert(
                 onConsume = { uiState.value = eventStateFactory.onConsumeEventState() },
