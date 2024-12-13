59e65598698ce9260277cca759c25d2c639a08cb
Mama1emon <andrew.khokhlove@gmail.com>
2024-12-13 13:20:19 +0400

    AND-9477 Delete redux DomainStore
    Signed-off-by: Mama1emon <andrew.khokhlove@gmail.com>

--- domain/legacy/src/main/java/com/tangem/domain/redux/DomainState.kt
+++
@@ -1,9 +0,0 @@
-package com.tangem.domain.redux
-
-import com.tangem.domain.redux.global.DomainGlobalState
-import org.rekotlin.StateType
-
-/**
- * Created by Anton Zhilenkov on 07/04/2022.
- */
-data class DomainState(val globalState: DomainGlobalState = DomainGlobalState()) : StateType
