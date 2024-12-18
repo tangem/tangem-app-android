de5de09663df0157025408f435601d5a5ea68b15
Tangem <git@tangem.com>
2024-12-18 22:57:32 +0500

    Updated on 2026-06-01

--- features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/providers/model/previewData/SelectProviderPreviewData.kt
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/providers/model/previewData/SelectProviderPreviewData.kt
@@ -2,59 +2,83 @@ package com.tangem.features.onramp.providers.model.previewData
 
 import com.tangem.core.ui.extensions.TextReference
 import com.tangem.core.ui.extensions.stringReference
-import com.tangem.domain.onramp.model.OnrampPaymentMethod
-import com.tangem.domain.onramp.model.PaymentMethodType
+import com.tangem.domain.onramp.model.*
+import com.tangem.features.onramp.providers.entity.PaymentProviderUM
 import com.tangem.features.onramp.providers.entity.ProviderListItemUM
 import com.tangem.features.onramp.providers.entity.SelectPaymentAndProviderUM
-import com.tangem.features.onramp.providers.entity.SelectProviderUM
+import com.tangem.features.onramp.providers.entity.SelectProviderResult
 import kotlinx.collections.immutable.persistentListOf
+import java.math.BigDecimal
 
 internal object SelectProviderPreviewData {
 
-    private val paymentMethods = persistentListOf(
-        OnrampPaymentMethod(
-            id = "card",
-            name = "Card",
-            imageUrl = "https://s3.eu-central-1.amazonaws.com/tangem.api/express/PaymentMethods/visa-mc.png",
-            type = PaymentMethodType.CARD,
-        ),
+    private val paymentMethod = OnrampPaymentMethod(
+        id = "card",
+        name = "Card",
+        imageUrl = "https://s3.eu-central-1.amazonaws.com/tangem.api/express/PaymentMethods/visa-mc.png",
+        type = PaymentMethodType.CARD,
     )
 
-    private val providerListUM = SelectProviderUM(
-        paymentMethod = paymentMethods.first(),
+    private val provider = OnrampProvider(
+        id = "mercuryo",
+        info = OnrampProviderInfo(
+            name = "Mercuryo",
+            imageLarge = "https://s3.eu-central-1.amazonaws.com/tangem.api/express/mercuryo1024.png",
+            termsOfUseLink = null,
+            privacyPolicyLink = null,
+        ),
+        paymentMethods = listOf(),
+    )
+
+    private val providerListUM = PaymentProviderUM(
+        paymentMethod = paymentMethod,
         providers = persistentListOf(
-            ProviderListItemUM.Available(
-                providerId = "mercuryo",
-                imageUrl = "https://s3.eu-central-1.amazonaws.com/tangem.api/express/mercuryo1024.png",
-                name = "Mercuryo",
+            ProviderListItemUM.Available.Content(
+                providerId = provider.id,
+                imageUrl = provider.info.imageLarge,
+                name = provider.info.name,
                 rate = "1 BTC",
                 isSelected = true,
                 isBestRate = true,
                 diffRate = TextReference.EMPTY,
+                providerResult = SelectProviderResult.ProviderWithQuote(
+                    paymentMethod = paymentMethod,
+                    provider = provider,
+                    fromAmount = OnrampAmount(symbol = "$", value = BigDecimal.ONE, decimals = 2),
+                    toAmount = OnrampAmount(symbol = "BTC", value = BigDecimal.ONE, decimals = 18),
+
+                ),
                 onClick = {},
             ),
-            ProviderListItemUM.Available(
-                providerId = "mercuryo",
-                imageUrl = "https://s3.eu-central-1.amazonaws.com/tangem.api/express/mercuryo1024.png",
-                name = "Mercuryo",
+            ProviderListItemUM.Available.Content(
+                providerId = provider.id,
+                imageUrl = provider.info.imageLarge,
+                name = provider.info.name,
                 rate = "1 BTC",
                 isSelected = false,
                 isBestRate = false,
                 diffRate = stringReference("-0.03%"),
+                providerResult = SelectProviderResult.ProviderWithQuote(
+                    paymentMethod = paymentMethod,
+                    provider = provider,
+                    fromAmount = OnrampAmount(symbol = "$", value = BigDecimal.ONE, decimals = 2),
+                    toAmount = OnrampAmount(symbol = "BTC", value = BigDecimal.ONE, decimals = 18),
+                ),
                 onClick = {},
             ),
             ProviderListItemUM.Unavailable(
-                providerId = "mercuryo",
-                imageUrl = "https://s3.eu-central-1.amazonaws.com/tangem.api/express/mercuryo1024.png",
-                name = "Mercuryo",
+                providerId = provider.id,
+                imageUrl = provider.info.imageLarge,
+                name = provider.info.name,
                 subtitle = stringReference("Avaiable from 0,00413 BTC"),
             ),
         ),
     )
 
     val state = SelectPaymentAndProviderUM(
-        paymentMethods = paymentMethods,
+        paymentMethods = persistentListOf(providerListUM),
         selectedPaymentMethod = providerListUM,
+        selectedProviderId = provider.id,
         isPaymentMethodClickEnabled = false,
         onPaymentMethodClick = {},
     )
