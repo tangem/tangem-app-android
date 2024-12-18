65d0fe58dcab74018392924fbecce3c62f54b237
Tangem <git@tangem.com>
2024-12-18 14:57:19 +0400

    release/5.26.1

--- app/src/main/java/com/tangem/tap/common/clipboard/MockClipboardManager.kt
+++ app/src/main/java/com/tangem/tap/common/clipboard/MockClipboardManager.kt
@@ -8,4 +8,6 @@ internal object MockClipboardManager : ClipboardManager {
     override fun setText(text: String, isSensitive: Boolean, label: String) {
         Timber.w("Clipboard Manager not available")
     }
+
+    override fun getText(default: String?): String? = null
 }
