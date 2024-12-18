be93d56a70d7004f8890d070d5523721e1e62c3d
Tangem <git@tangem.com>
2024-12-18 13:05:33 +0400

    Updated on 2025-11-18

---
+++ app/src/main/java/com/tangem/tap/features/welcome/component/WelcomeComponent.kt
@@ -0,0 +1,14 @@
+package com.tangem.tap.features.welcome.component
+
+import com.tangem.common.routing.entity.SerializableIntent
+import com.tangem.core.decompose.factory.ComponentFactory
+import com.tangem.core.ui.decompose.ComposableContentComponent
+
+interface WelcomeComponent : ComposableContentComponent {
+
+    data class Params(
+        val intent: SerializableIntent?,
+    )
+
+    interface Factory : ComponentFactory<Params, WelcomeComponent>
+}
