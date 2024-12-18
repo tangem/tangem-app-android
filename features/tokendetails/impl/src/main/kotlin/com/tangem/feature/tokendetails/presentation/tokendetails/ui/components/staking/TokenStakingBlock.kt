cea07e957271d1c24327eff00bb287e0eac26267
Tangem <git@tangem.com>
2024-12-18 13:16:08 +0300

    Updated on 2026-02-02

--- features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/ui/components/staking/TokenStakingBlock.kt
+++ features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/ui/components/staking/TokenStakingBlock.kt
@@ -112,6 +112,7 @@ private fun StakingAvailableContent(state: StakingBlockUM.StakeAvailable, modifi
         SecondaryButton(
             modifier = Modifier.fillMaxWidth(),
             text = stringResource(id = R.string.common_stake),
+            enabled = state.isEnabled,
             onClick = state.onStakeClicked,
         )
     }
