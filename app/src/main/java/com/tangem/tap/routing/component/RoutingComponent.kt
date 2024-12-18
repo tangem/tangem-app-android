e1ff9a5d4f0630b376dcfbd3dd4e40e4e19db3c7
Tangem <git@tangem.com>
2024-12-18 13:06:49 +0400

    release/5.26.1

--- app/src/main/java/com/tangem/tap/routing/component/RoutingComponent.kt
+++ app/src/main/java/com/tangem/tap/routing/component/RoutingComponent.kt
@@ -1,22 +1,25 @@
-package com.tangem.tap.routing
+package com.tangem.tap.routing.component
 
 import android.content.Intent
+import androidx.compose.runtime.Immutable
 import androidx.fragment.app.Fragment
 import com.arkivanov.decompose.router.stack.ChildStack
 import com.arkivanov.decompose.value.Value
 import com.tangem.common.routing.AppRoute
 import com.tangem.core.decompose.context.AppComponentContext
 import com.tangem.core.decompose.navigation.Router
+import com.tangem.core.ui.decompose.ComposableContentComponent
 import com.tangem.utils.Provider
 
-internal interface RoutingComponent {
-
+internal interface RoutingComponent : ComposableContentComponent {
+// [REDACTED_TODO_COMMENT]
     val router: Router
-
+// [REDACTED_TODO_COMMENT]
     val stack: Value<ChildStack<AppRoute, Child>>
 
+    @Immutable
     sealed class Child {
-
+// [REDACTED_TODO_COMMENT]
         data object Initial : Child()
 
         data class LegacyFragment(
@@ -25,6 +28,10 @@ internal interface RoutingComponent {
         ) : Child()
 
         data class LegacyIntent(val intent: Intent) : Child()
+
+        data class ComposableComponent(
+            val component: ComposableContentComponent,
+        ) : Child()
     }
 
     interface Factory {
