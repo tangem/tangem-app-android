8c80b76482fe0f92470ef94dc6c976f1237e950d
Tangem <git@tangem.com>
2024-12-23 14:19:34 +0400

    Updated on 2026-09-23

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,4 +1,26 @@
 plugins {
-    alias(deps.plugins.kotlin.jvm)
+    alias(deps.plugins.android.library)
+    alias(deps.plugins.kotlin.android)
     id("configuration")
+}
+
+android {
+    namespace = "com.tangem.common"
+}
+
+dependencies {
+
+    // region Firebase libraries
+    implementation(platform(deps.firebase.bom))
+    implementation(deps.firebase.analytics)
+    implementation(deps.firebase.crashlytics)
+    implementation(deps.firebase.messaging)
+    // end
+
+    implementation(deps.timber)
+
+    implementation(deps.arrow.core)
+
+    implementation(deps.test.junit)
+    implementation(deps.test.truth)
 }
