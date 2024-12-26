cca016d415dbc6d91176951bf7ebc9e87a057c70
Tangem <git@tangem.com>
2024-12-26 15:11:55 +0400

    Updated on 2026-09-23

--- app/src/main/java/com/tangem/tap/features/main/model/ActionConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.tangem.tap.features.main.model
-
-import com.tangem.core.ui.extensions.TextReference
-
-internal data class ActionConfig(
-    val text: TextReference,
-    val onClick: () -> Unit,
-)
