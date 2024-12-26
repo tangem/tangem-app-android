60fb139f00aa8e4df659e903184e921df4cf7180
Tangem <git@tangem.com>
2024-12-26 15:11:55 +0400

    Updated on 2026-06-27

--- app/src/main/java/com/tangem/tap/features/main/model/MainScreenState.kt
+++
@@ -1,9 +0,0 @@
-package com.tangem.tap.features.main.model
-
-import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfig
-import com.tangem.core.ui.event.StateEvent
-
-internal data class MainScreenState(
-    val toast: StateEvent<Toast>,
-    val modalNotification: TangemBottomSheetConfig?,
-)
