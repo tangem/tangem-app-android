a0ad3f53135dca48e5e6fc519b2cab8bb2b8e39b
Tangem <git@tangem.com>
2024-12-26 14:15:12 +0500

    Updated on 2025-09-01

---
+++ features/stories/impl/src/main/java/com/tangem/features/stories/impl/ui/StoriesComponentContent.kt
@@ -0,0 +1,9 @@
+package com.tangem.features.stories.impl.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun StoriesComponentContent() {
+    Text("Stories!")
+}
