db185fe3b2cd81927ce431cb1951b1b16e559521
Tangem <git@tangem.com>
2024-12-29 12:08:14 +0300

    Updated on 2026-02-11

--- features/tester/impl/src/main/java/com/tangem/feature/tester/presentation/menu/ui/TesterMenuScreen.kt
+++ features/tester/impl/src/main/java/com/tangem/feature/tester/presentation/menu/ui/TesterMenuScreen.kt
@@ -2,71 +2,62 @@ package com.tangem.feature.tester.presentation.menu.ui
 
 import android.content.res.Configuration
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.tangem.core.ui.components.PrimaryButton
 import com.tangem.core.ui.components.appbar.AppBarWithBackButton
 import com.tangem.core.ui.extensions.stringResourceSafe
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.core.ui.res.TangemThemePreview
 import com.tangem.feature.tester.impl.R
-import com.tangem.feature.tester.presentation.menu.state.TesterMenuContentState
+import com.tangem.feature.tester.presentation.menu.state.TesterMenuUM
+import com.tangem.feature.tester.presentation.menu.state.TesterMenuUM.ButtonUM
+import kotlinx.collections.immutable.persistentSetOf
+import kotlinx.collections.immutable.toImmutableList
 
 /**
  * Screen with functionality for testers
  *
  * @param state screen state
  */
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-internal fun TesterMenuScreen(state: TesterMenuContentState) {
+internal fun TesterMenuScreen(state: TesterMenuUM) {
     BackHandler(onBack = state.onBackClick)
 
-    Column(
+    LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .background(TangemTheme.colors.background.secondary),
+            .background(TangemTheme.colors.background.primary),
     ) {
-        AppBarWithBackButton(
-            onBackClick = state.onBackClick,
-            text = stringResourceSafe(id = R.string.tester_menu),
-        )
-
-        Column(
-            modifier = Modifier
-                .padding(
-                    horizontal = TangemTheme.dimens.spacing18,
-                    vertical = TangemTheme.dimens.spacing8,
-                )
-                .verticalScroll(rememberScrollState()),
-            verticalArrangement = Arrangement.spacedBy(TangemTheme.dimens.spacing16),
-        ) {
-            PrimaryButton(
-                text = stringResourceSafe(R.string.feature_toggles),
-                onClick = state.onFeatureTogglesClick,
-                modifier = Modifier.fillMaxWidth(),
+        stickyHeader {
+            AppBarWithBackButton(
+                onBackClick = state.onBackClick,
+                text = stringResourceSafe(id = R.string.tester_menu),
+                containerColor = TangemTheme.colors.background.primary,
             )
+        }
 
+        items(
+            items = state.buttons.toImmutableList(),
+            key = ButtonUM::textResId,
+            contentType = { "button" },
+        ) { button ->
             PrimaryButton(
-                modifier = Modifier.fillMaxWidth(),
-                text = stringResourceSafe(R.string.excluded_blockchains),
-                onClick = state.onExcludedBlockchainsClick,
-            )
-
-            PrimaryButton(
-                text = stringResourceSafe(R.string.environment_toggles),
-                onClick = state.onEnvironmentTogglesClick,
-                modifier = Modifier.fillMaxWidth(),
-            )
-
-            PrimaryButton(
-                modifier = Modifier.fillMaxWidth(),
-                text = stringResourceSafe(id = R.string.tester_actions),
-                onClick = state.onTesterActionsClick,
+                text = stringResourceSafe(button.textResId),
+                onClick = { state.onButtonClick(button) },
+                modifier = Modifier
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+                    .fillMaxWidth(),
             )
         }
     }
@@ -78,12 +69,16 @@ internal fun TesterMenuScreen(state: TesterMenuContentState) {
 private fun PreviewTesterMenuScreen() {
     TangemThemePreview {
         TesterMenuScreen(
-            state = TesterMenuContentState(
+            state = TesterMenuUM(
                 onBackClick = {},
-                onFeatureTogglesClick = {},
-                onEnvironmentTogglesClick = {},
-                onExcludedBlockchainsClick = {},
-                onTesterActionsClick = {},
+                buttons = persistentSetOf(
+                    ButtonUM.FEATURE_TOGGLES,
+                    ButtonUM.EXCLUDED_BLOCKCHAINS,
+                    ButtonUM.ENVIRONMENT_TOGGLES,
+                    ButtonUM.BLOCKCHAIN_PROVIDERS,
+                    ButtonUM.TESTER_ACTIONS,
+                ),
+                onButtonClick = {},
             ),
         )
     }
