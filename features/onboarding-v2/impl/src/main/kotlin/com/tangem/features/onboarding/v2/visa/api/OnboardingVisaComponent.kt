f03eb74113d5d3c5cfbbd1050134f2c1365eb911
Tangem <git@tangem.com>
2025-01-14 13:45:57 +0300

    Updated on 2025-12-05

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/visa/api/OnboardingVisaComponent.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/visa/api/OnboardingVisaComponent.kt
@@ -3,11 +3,15 @@ package com.tangem.features.onboarding.v2.visa.api
 import com.tangem.core.decompose.factory.ComponentFactory
 import com.tangem.core.decompose.navigation.inner.InnerNavigationHolder
 import com.tangem.core.ui.decompose.ComposableContentComponent
+import com.tangem.domain.models.scan.ScanResponse
 import com.tangem.features.onboarding.v2.TitleProvider
 
 interface OnboardingVisaComponent : ComposableContentComponent, InnerNavigationHolder {
 
-    data class Params(val titleProvider: TitleProvider)
+    data class Params(
+        val scanResponse: ScanResponse,
+        val titleProvider: TitleProvider,
+    )
 
     interface Factory : ComponentFactory<Params, OnboardingVisaComponent>
 }
--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/visa/impl/child/accesscode/model/OnboardingVisaAccessCodeModel.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/visa/impl/child/accesscode/model/OnboardingVisaAccessCodeModel.kt
@@ -3,22 +3,75 @@ package com.tangem.features.onboarding.v2.visa.impl.child.accesscode.model
 import androidx.compose.runtime.Stable
 import com.tangem.core.decompose.di.ComponentScoped
 import com.tangem.core.decompose.model.Model
-import com.tangem.core.decompose.model.ParamsContainer
-import com.tangem.features.onboarding.v2.visa.impl.child.accesscode.OnboardingVisaAccessCodeComponent
+import com.tangem.features.onboarding.v2.visa.impl.child.accesscode.ui.state.OnboardingVisaAccessCodeUM
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @Stable
 @ComponentScoped
 internal class OnboardingVisaAccessCodeModel @Inject constructor(
     override val dispatchers: CoroutineDispatcherProvider,
-    private val paramsContainer: ParamsContainer,
 ) : Model() {
 
-    @Suppress("UnusedPrivateMember")
-    private val params = paramsContainer.require<OnboardingVisaAccessCodeComponent.Params>()
+    private val _uiState = MutableStateFlow(getInitialState())
+    val uiState = _uiState.asStateFlow()
+    val onBack = MutableSharedFlow<Unit>()
 
     fun onBack() {
+        when (uiState.value.step) {
+            OnboardingVisaAccessCodeUM.Step.Enter -> modelScope.launch { onBack.emit(Unit) }
+            OnboardingVisaAccessCodeUM.Step.ReEnter ->
+                _uiState.update { it.copy(step = OnboardingVisaAccessCodeUM.Step.Enter) }
+        }
+    }
+
+    private fun getInitialState(): OnboardingVisaAccessCodeUM {
+        return OnboardingVisaAccessCodeUM(
+            onAccessCodeFirstChange = { _uiState.update { it.copy(accessCodeFirst = it.accessCodeFirst) } },
+            onAccessCodeSecondChange = { _uiState.update { it.copy(accessCodeSecond = it.accessCodeSecond) } },
+            onAccessCodeHideClick = { _uiState.update { it.copy(accessCodeHidden = !it.accessCodeHidden) } },
+            onContinue = { onContinue() },
+        )
+    }
+
+    private fun onContinue() {
+        if (checkAccessCode().not()) return
+
+        when (uiState.value.step) {
+            OnboardingVisaAccessCodeUM.Step.Enter -> {
+                _uiState.update { it.copy(step = OnboardingVisaAccessCodeUM.Step.ReEnter) }
+            }
+            OnboardingVisaAccessCodeUM.Step.ReEnter -> {
+                startActivationProcess()
+            }
+        }
+    }
+
+    private fun checkAccessCode(): Boolean {
+        val first = uiState.value.accessCodeFirst.text
+        val second = uiState.value.accessCodeSecond.text
+
+        when {
+            first.length < ACCESS_CODE_MIN_LENGTH -> {
+                _uiState.update { it.copy(atLeastMinCharsError = true) }
+                return false
+            }
+            first != second -> {
+                _uiState.update { it.copy(codesNotMatchError = true) }
+                return false
+            }
+        }
+
+        return true
+    }
+
+    private fun startActivationProcess() {
         TODO()
+    }
+
+    private companion object {
+        const val ACCESS_CODE_MIN_LENGTH = 4
     }
 }
