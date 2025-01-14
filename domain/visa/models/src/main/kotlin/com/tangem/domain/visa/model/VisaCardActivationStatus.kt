23178965a24bb7ddd240f58cddd64cf1c7dd9bcc
Tangem <git@tangem.com>
2025-01-14 11:04:06 +0300

    Updated on 2026-06-05

---
+++ domain/visa/models/src/main/kotlin/com/tangem/domain/visa/model/VisaCardActivationStatus.kt
@@ -0,0 +1,38 @@
+package com.tangem.domain.visa.model
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.adapters.PolymorphicJsonAdapterFactory
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed class VisaCardActivationStatus {
+
+    @Serializable
+    data class Activated(
+        @Json(name = "visaAuthTokens") val visaAuthTokens: VisaAuthTokens,
+    ) : VisaCardActivationStatus()
+
+    @Serializable
+    data class ActivationStarted(
+        @Json(name = "activationInput") val activationInput: VisaActivationInput,
+        @Json(name = "authTokens") val authTokens: VisaAuthTokens,
+        @Json(name = "remoteState") val remoteState: VisaActivationRemoteState,
+    ) : VisaCardActivationStatus()
+
+    @Serializable
+    data class NotStartedActivation(
+        @Json(name = "activationInput") val activationInput: VisaActivationInput,
+    ) : VisaCardActivationStatus()
+
+    @Serializable
+    data object Blocked : VisaCardActivationStatus()
+
+    companion object {
+        val serializer: PolymorphicJsonAdapterFactory<VisaCardActivationStatus>
+            get() = PolymorphicJsonAdapterFactory.of(VisaCardActivationStatus::class.java, "type")
+                .withSubtype(Activated::class.java, "Activated")
+                .withSubtype(ActivationStarted::class.java, "ActivationStarted")
+                .withSubtype(NotStartedActivation::class.java, "NotStartedActivation")
+                .withDefaultValue(Blocked)
+    }
+}
