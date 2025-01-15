d96916ced88e897e916df3cffc238625fce2ceaa
Tangem <git@tangem.com>
2025-01-15 12:05:53 +0500

    Updated on 2026-09-01

--- data/onramp/build.gradle.kts
+++ data/onramp/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.tangem.plugin.configuration.configurations.extension.kaptForObfuscatingVariants
+
 plugins {
     alias(deps.plugins.android.library)
     alias(deps.plugins.kotlin.android)
@@ -41,6 +43,10 @@ dependencies {
     implementation(deps.moshi)
     implementation(deps.moshi.kotlin)
     implementation(deps.timber)
+    kaptForObfuscatingVariants(deps.moshi.kotlin.codegen)
+    kaptForObfuscatingVariants(deps.retrofit.response.type.keeper)
+    implementation(deps.kotlin.serialization)
+
 
     implementation(projects.libs.blockchainSdk)
     implementation(projects.libs.crypto)
