a6ba1731daed3ad4380e515fbe0f391b970f4aba
Tangem <git@tangem.com>
2025-01-15 12:05:53 +0500

    Updated on 2025-09-15

--- features/swap/domain/models/src/main/java/com/tangem/feature/swap/domain/models/domain/ExchangeStatus.kt
+++ features/swap/domain/models/src/main/java/com/tangem/feature/swap/domain/models/domain/ExchangeStatus.kt
@@ -1,28 +1,67 @@
 package com.tangem.feature.swap.domain.models.domain
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class ExchangeStatusModel(
+    @Json(name = "providerId")
     val providerId: String,
+    @Json(name = "status")
     val status: ExchangeStatus? = null,
+    @Json(name = "txId")
     val txId: String? = null,
+    @Json(name = "txExternalUrl")
     val txExternalUrl: String? = null,
+    @Json(name = "txExternalId")
     val txExternalId: String? = null,
+    @Json(name = "refundNetwork")
     val refundNetwork: String? = null,
+    @Json(name = "refundContractAddress")
     val refundContractAddress: String? = null,
 )
 
+@JsonClass(generateAdapter = false)
 enum class ExchangeStatus {
+    @Json(name = "New")
     New,
+
+    @Json(name = "Waiting")
     Waiting,
+
+    @Json(name = "WaitingTxHash")
     WaitingTxHash,
+
+    @Json(name = "Confirming")
     Confirming,
+
+    @Json(name = "Verifying")
     Verifying,
+
+    @Json(name = "Exchanging")
     Exchanging,
+
+    @Json(name = "Failed")
     Failed,
+
+    @Json(name = "Sending")
     Sending,
+
+    @Json(name = "Finished")
     Finished,
+
+    @Json(name = "Refunded")
     Refunded,
+
+    @Json(name = "Cancelled")
     Cancelled,
+
+    @Json(name = "TxFailed")
     TxFailed,
+
+    @Json(name = "Unknown")
     Unknown,
+
+    @Json(name = "Paused")
     Paused,
 }
