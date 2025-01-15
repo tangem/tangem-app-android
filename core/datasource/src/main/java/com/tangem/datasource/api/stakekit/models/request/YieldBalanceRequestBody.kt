f92d110418333ffb034ae8b0e24f35fb9bd21d8f
Tangem <git@tangem.com>
2025-01-15 12:05:53 +0500

    Updated on 2026-07-07

--- core/datasource/src/main/java/com/tangem/datasource/api/stakekit/models/request/YieldBalanceRequestBody.kt
+++ core/datasource/src/main/java/com/tangem/datasource/api/stakekit/models/request/YieldBalanceRequestBody.kt
@@ -1,13 +1,16 @@
 package com.tangem.datasource.api.stakekit.models.request
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 data class YieldBalanceRequestBody(
     @Json(name = "addresses") val addresses: Address,
     @Json(name = "args") val args: YieldBalanceRequestArgs,
     @Json(name = "integrationId") val integrationId: String,
 ) {
 
+    @JsonClass(generateAdapter = true)
     data class YieldBalanceRequestArgs(
         @Json(name = "validatorAddresses") val validatorAddresses: List<String>,
     )
