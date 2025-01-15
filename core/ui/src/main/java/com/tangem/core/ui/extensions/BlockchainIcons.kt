9e36ab65120347d22eeeec8242c036c8fc689601
Tangem <git@tangem.com>
2025-01-15 17:23:49 +0700

    Updated on 2026-02-06

--- core/ui/src/main/java/com/tangem/core/ui/extensions/BlockchainIcons.kt
+++ core/ui/src/main/java/com/tangem/core/ui/extensions/BlockchainIcons.kt
@@ -86,6 +86,7 @@ fun getActiveIconRes(blockchainId: String): Int {
         "clore-ai" -> R.drawable.img_clore_22
         "fact0rn" -> R.drawable.img_fact0rn_22
         "odyssey", "odyssey/test" -> R.drawable.img_odyssey_chain_22
+        "bitrock", "bitrock/test" -> R.drawable.ic_alert_24 // todo
         else -> R.drawable.ic_alert_24
     }
 }
@@ -170,6 +171,7 @@ fun getActiveIconResByCoinId(coinId: String): Int {
         "clore-ai" -> R.drawable.img_clore_22
         "fact0rn" -> R.drawable.img_fact0rn_22
         "odyssey", "odyssey/test" -> R.drawable.img_odyssey_chain_22
+        "bitrock", "bitrock/test" -> R.drawable.ic_alert_24 // todo
         else -> R.drawable.ic_alert_24
     }
 }
@@ -257,6 +259,7 @@ fun getGreyedOutIconRes(blockchainId: String): Int {
         "clore-ai" -> R.drawable.ic_clore_22
         "fact0rn" -> R.drawable.ic_fact0rn_22
         "odyssey", "odyssey/test" -> R.drawable.ic_odyssey_chain_22
+        "bitrock", "bitrock/test" -> R.drawable.ic_alert_24 // todo
         else -> R.drawable.ic_alert_24
     }
 }
