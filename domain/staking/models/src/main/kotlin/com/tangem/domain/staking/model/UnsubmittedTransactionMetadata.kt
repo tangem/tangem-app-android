ea7ff663e5dcfdd60a7dcad9d75281501e9a27e7
Tangem <git@tangem.com>
2025-01-15 12:05:53 +0500

    Updated on 2025-10-01

--- domain/staking/models/src/main/kotlin/com/tangem/domain/staking/model/UnsubmittedTransactionMetadata.kt
+++ domain/staking/models/src/main/kotlin/com/tangem/domain/staking/model/UnsubmittedTransactionMetadata.kt
@@ -1,6 +1,12 @@
 package com.tangem.domain.staking.model
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class UnsubmittedTransactionMetadata(
+    @Json(name = "transactionHash")
     val transactionHash: String,
+    @Json(name = "transactionId")
     val transactionId: String,
 )
