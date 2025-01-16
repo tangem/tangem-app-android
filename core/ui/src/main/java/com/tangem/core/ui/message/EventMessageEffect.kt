ecc1c9c1bd5c7beb3ffd6f787ff3045017cca3d8
Tangem <git@tangem.com>
2025-01-16 10:35:06 +0300

    Updated on 2026-08-19

--- core/ui/src/main/java/com/tangem/core/ui/message/EventMessageEffect.kt
+++ core/ui/src/main/java/com/tangem/core/ui/message/EventMessageEffect.kt
@@ -131,6 +131,7 @@ private fun MessageDialog(message: DialogMessage, onDismissRequest: () -> Unit) 
             )
         },
         onDismissDialog = onDismissRequest,
+        isDismissable = message.isDismissable,
     )
 }
 
