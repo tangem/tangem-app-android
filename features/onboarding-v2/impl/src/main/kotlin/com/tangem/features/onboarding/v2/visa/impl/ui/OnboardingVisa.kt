ac94acfd41a10f46cf58f6ba700035060807b183
Tangem <git@tangem.com>
2025-01-19 21:50:00 +0300

    Updated on 2026-09-25

---
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/visa/impl/ui/OnboardingVisa.kt
@@ -0,0 +1,9 @@
+package com.tangem.features.onboarding.v2.visa.impl.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+internal fun OnboardingVisa(modifier: Modifier = Modifier, childContent: @Composable (Modifier) -> Unit) {
+    childContent(modifier)
+}
