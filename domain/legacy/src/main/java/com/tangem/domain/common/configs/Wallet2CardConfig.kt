b034a10eca67d7740b015f13014f28ccec26ad00
Tangem <git@tangem.com>
2025-01-21 19:25:56 +0500

    release/5.26.1

--- domain/legacy/src/main/java/com/tangem/domain/common/configs/Wallet2CardConfig.kt
+++ domain/legacy/src/main/java/com/tangem/domain/common/configs/Wallet2CardConfig.kt
@@ -194,6 +194,8 @@ data object Wallet2CardConfig : CardConfig {
             Blockchain.OdysseyChainTestnet -> EllipticCurve.Secp256k1
             Blockchain.Bitrock -> EllipticCurve.Secp256k1
             Blockchain.BitrockTestnet -> EllipticCurve.Secp256k1
+            Blockchain.Sonic -> EllipticCurve.Secp256k1
+            Blockchain.SonicTestnet -> EllipticCurve.Secp256k1
         }
     }
 }
