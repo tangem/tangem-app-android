58bbbabaa84fe998135d05e8a84fa1ba9aaebee8
Tangem <git@tangem.com>
2025-01-22 20:41:34 +0300

    Updated on 2026-03-25

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/visa/impl/child/approve/model/OnboardingVisaApproveModel.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/visa/impl/child/approve/model/OnboardingVisaApproveModel.kt
@@ -1,22 +1,30 @@
 package com.tangem.features.onboarding.v2.visa.impl.child.approve.model
 
 import androidx.compose.runtime.Stable
+import com.tangem.common.CompletionResult
 import com.tangem.core.decompose.di.ComponentScoped
 import com.tangem.core.decompose.model.Model
+import com.tangem.core.decompose.model.ParamsContainer
+import com.tangem.features.onboarding.v2.visa.impl.child.approve.OnboardingVisaApproveComponent
 import com.tangem.features.onboarding.v2.visa.impl.child.approve.ui.state.OnboardingVisaApproveUM
+import com.tangem.sdk.api.TangemSdkManager
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @Stable
 @ComponentScoped
 internal class OnboardingVisaApproveModel @Inject constructor(
+    paramsContainer: ParamsContainer,
     override val dispatchers: CoroutineDispatcherProvider,
+    private val tangemSdkManager: TangemSdkManager,
 ) : Model() {
 
+    private val params = paramsContainer.require<OnboardingVisaApproveComponent.Config>()
     private val _uiState = MutableStateFlow(getInitialState())
 
     val uiState = _uiState.asStateFlow()
@@ -29,7 +37,29 @@ internal class OnboardingVisaApproveModel @Inject constructor(
     }
 
     private fun onApproveClick() {
-        // TODO
-        modelScope.launch { onDone.emit(Unit) }
+        loading(true)
+
+        modelScope.launch {
+            val result = tangemSdkManager.visaCustomerWalletApprove(
+                visaDataForApprove = params.visaDataForApprove,
+            )
+
+            when (result) {
+                is CompletionResult.Success -> {
+                    // TODO make backend call
+
+                    onDone.emit(Unit)
+                }
+                is CompletionResult.Failure -> {
+                    loading(false)
+                    // TODO show dialog
+                    return@launch
+                }
+            }
+        }
+    }
+
+    private fun loading(state: Boolean) {
+        _uiState.update { it.copy(approveButtonLoading = state) }
     }
 }
