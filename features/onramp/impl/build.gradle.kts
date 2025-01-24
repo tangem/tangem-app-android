950dc416626023c741a3e5eb2650b0d57ab2ce2a
Tangem <git@tangem.com>
2025-01-24 13:07:37 +0300

    Updated on 2026-08-14

--- features/onramp/impl/build.gradle.kts
+++ features/onramp/impl/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     implementation(projects.domain.appCurrency.models)
     implementation(projects.domain.balanceHiding)
     implementation(projects.domain.balanceHiding.models)
+    implementation(projects.domain.card)
     implementation(projects.domain.demo)
     implementation(projects.domain.legacy)
     implementation(projects.domain.models)
