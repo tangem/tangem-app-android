064ff078554eb50c4c47a22146ae153d8d6f2895
Tangem <git@tangem.com>
2025-01-24 13:07:37 +0300

    Updated on 2026-08-19

---
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/hottokens/portfolio/ui/OnrampAddToPortfolioBottomSheet.kt
@@ -0,0 +1,17 @@
+package com.tangem.features.onramp.hottokens.portfolio.ui
+
+import androidx.compose.runtime.Composable
+import com.tangem.core.ui.components.bottomsheets.TangemBottomSheet
+import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfig
+import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfigContent
+import com.tangem.core.ui.res.TangemTheme
+
+@Composable
+internal fun OnrampAddToPortfolioBottomSheet(config: TangemBottomSheetConfig, content: @Composable () -> Unit) {
+    TangemBottomSheet<TangemBottomSheetConfigContent.Empty>(
+        config = config,
+        addBottomInsets = true,
+        containerColor = TangemTheme.colors.background.tertiary,
+        content = { content() },
+    )
+}
