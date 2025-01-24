1f122b2c250066b696523a3a8cb2436a68ae44d4
Tangem <git@tangem.com>
2025-01-24 13:07:37 +0300

    Updated on 2026-07-23

---
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/hottokens/portfolio/entity/OnrampAddToPortfolioUM.kt
@@ -0,0 +1,34 @@
+package com.tangem.features.onramp.hottokens.portfolio.entity
+
+import com.tangem.core.ui.components.currency.icon.CurrencyIconState
+import com.tangem.core.ui.extensions.TextReference
+import com.tangem.core.ui.extensions.resourceReference
+import com.tangem.core.ui.extensions.wrappedList
+import com.tangem.features.onramp.impl.R
+
+/**
+ * Onramp add to portfolio UM
+ *
+ * @property currencyName      currency name
+ * @property networkName       network name
+ * @property currencyIconState currency icon state
+ * @property onAddClick        lambda be invoked when add button is clicked
+ *
+[REDACTED_AUTHOR]
+ */
+data class OnrampAddToPortfolioUM(
+    val currencyName: String,
+    val networkName: String,
+    val currencyIconState: CurrencyIconState,
+    val onAddClick: () -> Unit,
+) {
+
+    val bsTitle: TextReference = resourceReference(id = R.string.common_add_token)
+
+    val subtitle: TextReference = resourceReference(
+        id = R.string.hot_crypto_token_network,
+        formatArgs = wrappedList(networkName),
+    )
+
+    val addButtonText: TextReference = resourceReference(R.string.common_add_to_portfolio)
+}
