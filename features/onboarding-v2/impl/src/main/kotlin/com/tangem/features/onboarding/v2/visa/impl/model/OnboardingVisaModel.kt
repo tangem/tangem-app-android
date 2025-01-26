89b90528e48b02b674d4b0fd97f12b00701a54b1
Tangem <git@tangem.com>
2025-01-26 12:50:53 +0300

    Updated on 2025-09-17

---
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/visa/impl/model/OnboardingVisaModel.kt
@@ -0,0 +1,23 @@
+package com.tangem.features.onboarding.v2.visa.impl.model
+
+import androidx.compose.runtime.Stable
+import com.tangem.core.decompose.di.ComponentScoped
+import com.tangem.core.decompose.model.Model
+import com.tangem.core.decompose.model.ParamsContainer
+import com.tangem.features.onboarding.v2.visa.api.OnboardingVisaComponent
+import com.tangem.features.onboarding.v2.visa.impl.route.OnboardingVisaRoute
+import com.tangem.utils.coroutines.CoroutineDispatcherProvider
+import javax.inject.Inject
+
+@Stable
+@ComponentScoped
+internal class OnboardingVisaModel @Inject constructor(
+    paramsContainer: ParamsContainer,
+    override val dispatchers: CoroutineDispatcherProvider,
+) : Model() {
+
+    @Suppress("UnusedPrivateMember")
+    private val params = paramsContainer.require<OnboardingVisaComponent.Params>()
+
+    val initialRoute = OnboardingVisaRoute.Welcome(isWelcomeBack = false)
+}
