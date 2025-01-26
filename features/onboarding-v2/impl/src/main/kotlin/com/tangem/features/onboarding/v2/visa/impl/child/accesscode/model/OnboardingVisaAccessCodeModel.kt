cae0cd6e2b5ad0897c16f6c136c3e3b6664e28d8
Tangem <git@tangem.com>
2025-01-26 12:50:53 +0300

    Updated on 2026-09-25

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/visa/impl/child/accesscode/model/OnboardingVisaAccessCodeModel.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/visa/impl/child/accesscode/model/OnboardingVisaAccessCodeModel.kt
@@ -1,6 +1,7 @@
 package com.tangem.features.onboarding.v2.visa.impl.child.accesscode.model
 
 import androidx.compose.runtime.Stable
+import androidx.compose.ui.text.input.TextFieldValue
 import com.tangem.core.decompose.di.ComponentScoped
 import com.tangem.core.decompose.model.Model
 import com.tangem.features.onboarding.v2.visa.impl.child.accesscode.ui.state.OnboardingVisaAccessCodeUM
@@ -14,12 +15,14 @@ import javax.inject.Inject
 @ComponentScoped
 internal class OnboardingVisaAccessCodeModel @Inject constructor(
     override val dispatchers: CoroutineDispatcherProvider,
+    @Suppress("UnusedPrivateMember")
     private val tangemSdkManager: TangemSdkManager,
 ) : Model() {
 
     private val _uiState = MutableStateFlow(getInitialState())
     val uiState = _uiState.asStateFlow()
     val onBack = MutableSharedFlow<Unit>()
+    val onDone = MutableSharedFlow<Unit>()
 
     fun onBack() {
         when (uiState.value.step) {
@@ -31,64 +34,90 @@ internal class OnboardingVisaAccessCodeModel @Inject constructor(
 
     private fun getInitialState(): OnboardingVisaAccessCodeUM {
         return OnboardingVisaAccessCodeUM(
-            onAccessCodeFirstChange = { _uiState.update { it.copy(accessCodeFirst = it.accessCodeFirst) } },
-            onAccessCodeSecondChange = { _uiState.update { it.copy(accessCodeSecond = it.accessCodeSecond) } },
+            onAccessCodeFirstChange = ::onAccessCodeFirstChange,
+            onAccessCodeSecondChange = ::onAccessCodeSecondChange,
             onAccessCodeHideClick = { _uiState.update { it.copy(accessCodeHidden = !it.accessCodeHidden) } },
             onContinue = { onContinue() },
         )
     }
 
-    private fun onContinue() {
-        if (checkAccessCode().not()) return
+    private fun onAccessCodeFirstChange(textFieldValue: TextFieldValue) {
+        _uiState.update {
+            it.copy(
+                accessCodeFirst = textFieldValue,
+                atLeastMinCharsError = false,
+            )
+        }
+    }
 
+    private fun onAccessCodeSecondChange(textFieldValue: TextFieldValue) {
+        _uiState.update {
+            it.copy(
+                accessCodeSecond = textFieldValue,
+                codesNotMatchError = false,
+            )
+        }
+    }
+
+    private fun onContinue() {
         when (uiState.value.step) {
             OnboardingVisaAccessCodeUM.Step.Enter -> {
+                if (checkAccessCodeMinChars().not()) return
                 _uiState.update { it.copy(step = OnboardingVisaAccessCodeUM.Step.ReEnter) }
             }
             OnboardingVisaAccessCodeUM.Step.ReEnter -> {
+                if (checkAccessCodesMatch().not()) return
                 startActivationProcess(accessCode = uiState.value.accessCodeFirst.text)
             }
         }
     }
 
-    private fun checkAccessCode(): Boolean {
+    private fun checkAccessCodeMinChars(): Boolean {
         val first = uiState.value.accessCodeFirst.text
-        val second = uiState.value.accessCodeSecond.text
 
-        when {
-            first.length < ACCESS_CODE_MIN_LENGTH -> {
-                _uiState.update { it.copy(atLeastMinCharsError = true) }
-                return false
-            }
-            first != second -> {
-                _uiState.update { it.copy(codesNotMatchError = true) }
-                return false
-            }
+        if (first.length < ACCESS_CODE_MIN_LENGTH) {
+            _uiState.update { it.copy(atLeastMinCharsError = true) }
+            return false
         }
 
         return true
     }
 
-    private fun startActivationProcess(accessCode: String) {
-        modelScope.launch {
-            @Suppress("UnusedPrivateMember")
-            val result = tangemSdkManager.activateVisaCard(
-                accessCode = accessCode,
-                challengeToSign = null,
-                activationInput = TODO(),
-            )
+    private fun checkAccessCodesMatch(): Boolean {
+        val first = uiState.value.accessCodeFirst.text
+        val second = uiState.value.accessCodeSecond.text
 
-            // when (result) {
-            //     is CompletionResult.Success -> {
-            //         val response = result.data
-            //         TODO()
-            //     }
-            //     is CompletionResult.Failure -> {
-            //         // show alert
-            //         TODO()
-            //     }
-            // }
+        if (first != second) {
+            _uiState.update { it.copy(codesNotMatchError = true) }
+            return false
         }
+
+        return true
+    }
+
+    @Suppress("UnusedPrivateMember")
+    private fun startActivationProcess(accessCode: String) {
+        modelScope.launch { onDone.emit(Unit) }
+
+        // modelScope.launch {
+        //     @Suppress("UnusedPrivateMember")
+        //     val result = tangemSdkManager.activateVisaCard(
+        //         accessCode = accessCode,
+        //         challengeToSign = null,
+        //         activationInput = TODO(),
+        //     )
+        //
+        // when (result) {
+        //     is CompletionResult.Success -> {
+        //         val response = result.data
+        //         TODO()
+        //     }
+        //     is CompletionResult.Failure -> {
+        //         // show alert
+        //         TODO()
+        //     }
+        // }
+        // }
     }
 
     private companion object {
