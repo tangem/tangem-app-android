2f2248dd6165e911bd2514304b5026f4274c0189
Tangem <git@tangem.com>
2025-01-27 12:45:57 +0300

    Updated on 2026-08-27

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/visa/impl/route/Step.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/visa/impl/route/Step.kt
@@ -4,9 +4,9 @@ package com.tangem.features.onboarding.v2.visa.impl.route
 internal fun OnboardingVisaRoute.stepNum() = when (this) {
     is OnboardingVisaRoute.Welcome -> 1
     OnboardingVisaRoute.AccessCode -> 2
-    OnboardingVisaRoute.ChooseWallet -> 3
-    OnboardingVisaRoute.OtherWalletApproveOption -> 4
-    OnboardingVisaRoute.TangemWalletApproveOption -> 4
+    is OnboardingVisaRoute.ChooseWallet -> 3
+    is OnboardingVisaRoute.OtherWalletApproveOption -> 4
+    is OnboardingVisaRoute.TangemWalletApproveOption -> 4
     OnboardingVisaRoute.InProgress -> 5
     OnboardingVisaRoute.PinCode -> 6
 }
