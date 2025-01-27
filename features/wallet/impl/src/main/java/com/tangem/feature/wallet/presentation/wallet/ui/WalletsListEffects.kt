5c39cb11ca2d035e3c0fe9df8573c43849a2b5b1
Tangem <git@tangem.com>
2025-01-27 15:02:37 +0300

    Updated on 2026-03-25

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/WalletsListEffects.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/WalletsListEffects.kt
@@ -3,48 +3,44 @@ package com.tangem.feature.wallet.presentation.wallet.ui
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.State
 import androidx.compose.runtime.snapshotFlow
+import com.tangem.feature.wallet.presentation.wallet.ui.utils.CenterOfItemScrollingDetector
 import com.tangem.feature.wallet.presentation.wallet.ui.utils.LazyListItemData
-import com.tangem.feature.wallet.presentation.wallet.ui.utils.ScrollOffsetCollector
 import com.tangem.feature.wallet.presentation.wallet.ui.utils.WalletsListInteractionsCollector
 
-@Suppress("LongParameterList")
+/**
+ * Wallets list effects
+ *
+ * @param lazyListState       wallets list [LazyListState]
+ * @param selectedWalletIndex selected wallet index
+ * @param onUserScroll        lambda be invoked when user scrolls the list
+ * @param onIndexChange       lambda be invoked when index is changed
+ */
 @Composable
 internal fun WalletsListEffects(
     lazyListState: LazyListState,
     selectedWalletIndex: Int,
-    onWalletChange: (Int) -> Unit,
-    onSelectedWalletIndexSet: (Int) -> Unit,
-    isAutoScroll: State<Boolean>,
-    onAutoScrollReset: () -> Unit,
+    onUserScroll: () -> Unit,
+    onIndexChange: (Int) -> Unit,
 ) {
-    LaunchedEffect(key1 = lazyListState, key2 = onWalletChange) {
+    LaunchedEffect(key1 = lazyListState) {
         snapshotFlow {
             lazyListState.layoutInfo.visibleItemsInfo.map {
                 LazyListItemData(it.index, it.size, it.offset)
             }
         }
             .collect(
-                collector = ScrollOffsetCollector(
-                    selectedWalletIndex = selectedWalletIndex,
+                collector = CenterOfItemScrollingDetector(
+                    initialIndex = selectedWalletIndex,
                     lazyListState = lazyListState,
-                    onWalletChange = { newIndex ->
-                        // Auto scroll must not change wallet
-                        if (isAutoScroll.value) {
-                            onSelectedWalletIndexSet(newIndex)
-                        } else {
-                            onSelectedWalletIndexSet(newIndex)
-                            onWalletChange(newIndex)
-                        }
-                    },
+                    onIndexChange = { newIndex -> onIndexChange(newIndex) },
                 ),
             )
     }
 
     LaunchedEffect(Unit) {
         lazyListState.interactionSource.interactions.collect(
-            collector = WalletsListInteractionsCollector(onDragStart = onAutoScrollReset),
+            collector = WalletsListInteractionsCollector(onDragStart = onUserScroll),
         )
     }
 }
