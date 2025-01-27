4b96e78dc2a428c3bde1caba96b38fcac91cf7a7
Tangem <git@tangem.com>
2025-01-27 15:02:37 +0300

    Updated on 2025-12-22

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/utils/CenterOfItemScrollingDetector.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/utils/CenterOfItemScrollingDetector.kt
@@ -5,26 +5,26 @@ import kotlinx.coroutines.flow.FlowCollector
 import kotlin.math.abs
 
 /**
- * Flow collector for scroll items tracking.
+ * Scrolling detector that changes the current element when scrolling the center of the element.
  * If first visible item offset is greater than half item size, then change selected wallet index.
  * If last visible item offset is greater than half item size, then change selected wallet index.
  *
- * @param selectedWalletIndex selected wallet index
- * @property lazyListState     lazy list state
- * @property onWalletChange    callback that will be invoked on wallet change
+ * @param initialIndex     initial index
+ * @property lazyListState lazy list state
+ * @property onIndexChange lambda be invoked when index is changed
  *
 [REDACTED_AUTHOR]
  */
-internal class ScrollOffsetCollector(
-    selectedWalletIndex: Int,
+internal class CenterOfItemScrollingDetector(
+    initialIndex: Int,
     private val lazyListState: LazyListState,
-    private val onWalletChange: (Int) -> Unit,
+    private val onIndexChange: (Int) -> Unit,
 ) : FlowCollector<List<LazyListItemData>> {
 
     private val LazyListItemData.halfItemSize
         get() = size.div(other = 2)
 
-    private var currentIndex = selectedWalletIndex
+    private var currentIndex = initialIndex
 
     override suspend fun emit(value: List<LazyListItemData>) {
         if (!lazyListState.isScrollInProgress || value.size <= 1) return
@@ -33,22 +33,18 @@ internal class ScrollOffsetCollector(
         val lastItem = value.lastOrNull() ?: return
 
         if (abs(firstItem.offset) > firstItem.halfItemSize) {
-            selectIndex(newIndex = firstItem.index + 1)
+            changeIndex(newIndex = firstItem.index + 1)
         } else if (abs(lastItem.offset) > lastItem.halfItemSize) {
-            selectIndex(newIndex = lastItem.index - 1)
+            changeIndex(newIndex = lastItem.index - 1)
         }
     }
 
-    private fun selectIndex(newIndex: Int) {
+    private fun changeIndex(newIndex: Int) {
         if (currentIndex != newIndex) {
             currentIndex = newIndex
-            onWalletChange(newIndex)
+            onIndexChange(newIndex)
         }
     }
 }
 
-internal data class LazyListItemData(
-    val index: Int,
-    val size: Int,
-    val offset: Int,
-)
+internal data class LazyListItemData(val index: Int, val size: Int, val offset: Int)
