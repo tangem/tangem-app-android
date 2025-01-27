d27c57cd844749389c5c2a542fc491fcf5f719b2
Tangem <git@tangem.com>
2025-01-27 12:23:42 +0300

    Updated on 2026-08-14

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/visa/impl/child/inprogress/model/OnboardingVisaInProgressModel.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/visa/impl/child/inprogress/model/OnboardingVisaInProgressModel.kt
@@ -3,6 +3,10 @@ package com.tangem.features.onboarding.v2.visa.impl.child.inprogress.model
 import androidx.compose.runtime.Stable
 import com.tangem.core.decompose.di.ComponentScoped
 import com.tangem.core.decompose.model.Model
+import com.tangem.core.decompose.model.ParamsContainer
+import com.tangem.domain.visa.model.VisaActivationRemoteState
+import com.tangem.domain.visa.repository.VisaActivationRepository
+import com.tangem.features.onboarding.v2.visa.impl.child.inprogress.OnboardingVisaInProgressComponent
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -12,16 +16,29 @@ import javax.inject.Inject
 @Stable
 @ComponentScoped
 internal class OnboardingVisaInProgressModel @Inject constructor(
+    paramsContainer: ParamsContainer,
     override val dispatchers: CoroutineDispatcherProvider,
+    private val visaActivationRepositoryFactory: VisaActivationRepository.Factory,
 ) : Model() {
 
+    private val params = paramsContainer.require<OnboardingVisaInProgressComponent.Config>()
+    private val visaActivationRepository = visaActivationRepositoryFactory.create(params.scanResponse.card.cardId)
     val onDone = MutableSharedFlow<Unit>()
 
     init {
         modelScope.launch {
-            // TODO check state
-            delay(timeMillis = 2000)
-            onDone.emit(Unit)
+            while (true) {
+                val result = runCatching {
+                    visaActivationRepository.getActivationRemoteStateLongPoll()
+                }.getOrNull()
+
+                if (result == VisaActivationRemoteState.WaitingPinCode) {
+                    onDone.emit(Unit)
+                    break
+                }
+
+                delay(timeMillis = 1000)
+            }
         }
     }
 }
