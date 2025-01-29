8e34968bc4f6eaabd6fe8ecbf86bed035935aa88
Tangem <git@tangem.com>
2025-01-29 16:56:12 +0300

    Updated on 2026-08-19

--- domain/visa/models/src/main/kotlin/com/tangem/domain/visa/model/VisaDataForApprove.kt
+++ domain/visa/models/src/main/kotlin/com/tangem/domain/visa/model/VisaDataForApprove.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class VisaDataForApprove(
+    @SerialName("customerWalletCardId") val customerWalletCardId: String?,
     @SerialName("targetAddress") val targetAddress: String,
     @SerialName("approveHash") val approveHash: String,
 )
