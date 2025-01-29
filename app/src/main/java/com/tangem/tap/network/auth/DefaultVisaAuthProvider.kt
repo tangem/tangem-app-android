7f2f1d2b5a4daef40101f333ddecb1145e67349e
Tangem <git@tangem.com>
2025-01-29 16:55:54 +0300

    Updated on 2026-09-25

--- app/src/main/java/com/tangem/tap/network/auth/DefaultVisaAuthProvider.kt
+++ app/src/main/java/com/tangem/tap/network/auth/DefaultVisaAuthProvider.kt
@@ -1,14 +1,20 @@
 package com.tangem.tap.network.auth
 
 import com.tangem.datasource.api.common.visa.TangemVisaAuthProvider
-import com.tangem.datasource.local.visa.VisaAuthTokenStorage
+import com.tangem.domain.visa.model.VisaCardActivationStatus
+import com.tangem.domain.wallets.legacy.UserWalletsListManager
 import javax.inject.Inject
 
 internal class DefaultVisaAuthProvider @Inject constructor(
-    private val authStorage: VisaAuthTokenStorage,
+    private val userWalletsListManager: UserWalletsListManager,
 ) : TangemVisaAuthProvider {
 
     override suspend fun getAuthHeader(cardId: String): String {
-        return authStorage.get(cardId)?.accessToken?.let { "Bearer $it" } ?: "Error in the app!"
+        val card = userWalletsListManager.userWalletsSync.firstOrNull { it.cardId == cardId }
+        val status = card?.scanResponse?.visaCardActivationStatus as? VisaCardActivationStatus.Activated
+            ?: return "Error in the app!"
+        val accessToken = status.visaAuthTokens.accessToken
+
+        return "Bearer $accessToken"
     }
 }
