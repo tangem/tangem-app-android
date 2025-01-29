3010468a06fbf402df6f8ffbfeb840141978961b
Tangem <git@tangem.com>
2025-01-29 16:55:54 +0300

    Updated on 2026-04-15

--- data/visa/src/debug/kotlin/com/tangem/data/visa/DefaultVisaRepository.kt
+++ data/visa/src/debug/kotlin/com/tangem/data/visa/DefaultVisaRepository.kt
@@ -6,22 +6,25 @@ import androidx.paging.PagingData
 import arrow.fx.coroutines.parZip
 import com.tangem.blockchain.common.address.Address
 import com.tangem.blockchain.common.address.AddressType
-import com.tangem.common.card.EllipticCurve
 import com.tangem.common.extensions.hexToBytes
 import com.tangem.common.extensions.toHexString
 import com.tangem.data.common.cache.CacheRegistry
 import com.tangem.data.visa.config.VisaLibLoader
 import com.tangem.data.visa.utils.*
+import com.tangem.datasource.api.common.response.ApiResponseError
 import com.tangem.datasource.api.common.response.getOrThrow
+import com.tangem.datasource.api.common.visa.TangemVisaAuthProvider
 import com.tangem.datasource.api.tangemTech.TangemTechApi
 import com.tangem.datasource.local.userwallet.UserWalletsStore
 import com.tangem.domain.common.util.cardTypesResolver
-import com.tangem.domain.visa.model.VisaCurrency
-import com.tangem.domain.visa.model.VisaTxDetails
-import com.tangem.domain.visa.model.VisaTxHistoryItem
+import com.tangem.domain.common.visa.VisaUtilities
+import com.tangem.domain.visa.exception.RefreshTokenExpiredException
+import com.tangem.domain.visa.model.*
+import com.tangem.domain.visa.repository.VisaAuthRepository
 import com.tangem.domain.visa.repository.VisaRepository
 import com.tangem.domain.wallets.models.UserWallet
 import com.tangem.domain.wallets.models.UserWalletId
+import com.tangem.lib.visa.api.VisaApi
 import com.tangem.lib.visa.model.VisaTxHistoryResponse
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import kotlinx.coroutines.flow.Flow
@@ -29,13 +32,19 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.withContext
 import java.math.BigDecimal
+import javax.inject.Inject
+import javax.inject.Singleton
+import kotlin.jvm.Throws
 
-internal class DefaultVisaRepository(
+@Singleton
+internal class DefaultVisaRepository @Inject constructor(
     private val visaLibLoader: VisaLibLoader,
     private val tangemTechApi: TangemTechApi,
     private val cacheRegistry: CacheRegistry,
     private val userWalletsStore: UserWalletsStore,
     private val dispatchers: CoroutineDispatcherProvider,
+    private val visaAuthProvider: TangemVisaAuthProvider,
+    private val visaAuthRepository: VisaAuthRepository,
 ) : VisaRepository {
 
     private val currencyFactory by lazy(mode = LazyThreadSafetyMode.NONE) {
@@ -106,11 +115,12 @@ internal class DefaultVisaRepository(
                         cardPublicKey = cardPubKey,
                         pageSize = pageSize,
                         isRefresh = isRefresh,
+                        userWallet = userWallet,
                     ),
                     cacheRegistry = cacheRegistry,
-                    visaApi = api,
                     fetchedItems = fetchedHistoryItems,
                     dispatchers = dispatchers,
+                    requestTxHistory = { offset, pageSize -> getTxHistory(api, userWalletId, offset, pageSize) },
                 )
             },
         )
@@ -131,6 +141,25 @@ internal class DefaultVisaRepository(
                 transaction = transaction,
                 walletBlockchain = userWallet.scanResponse.cardTypesResolver.getBlockchain(),
             )
+        }
+    }
+
+    private suspend fun getTxHistory(
+        api: VisaApi,
+        userWalletId: UserWalletId,
+        offset: Int,
+        pageSize: Int,
+    ): VisaTxHistoryResponse = withContext(dispatchers.io) {
+        val userWallet = findVisaUserWallet(userWalletId)
+        val cardPubKey = getCardPubKey(userWallet)
+
+        request(userWalletId = userWalletId) {
+            api.getTxHistory(
+                authorizationHeader = visaAuthProvider.getAuthHeader(userWallet.cardId),
+                cardPublicKey = cardPubKey,
+                limit = pageSize,
+                offset = offset,
+            ).getOrThrow()
         }
     }
 
@@ -166,9 +195,9 @@ internal class DefaultVisaRepository(
         if (VisaConstants.IS_DEMO_MODE_ENABLED) return getDemoPublicKey()
 
         val cardWallet = userWallet.scanResponse.card.wallets.firstOrNull {
-            it.curve == EllipticCurve.Secp256k1
+            it.curve == VisaUtilities.mandatoryCurve
         }
-        requireNotNull(cardWallet) { "Secp256k1 card wallet not found" }
+        requireNotNull(cardWallet) { "Visa card wallet not found" }
 
         return cardWallet.publicKey.toHexString()
     }
@@ -186,5 +215,57 @@ internal class DefaultVisaRepository(
 
     private fun getVisaCurrencyKey(address: String): String {
         return "visa_currency_$address"
+    }
+
+    private suspend fun <T : Any> request(
+        userWalletId: UserWalletId,
+        requestBlock: suspend () -> T,
+    ): T {
+        return runCatching {
+            requestBlock()
+        }.getOrElse { responseError ->
+            if (responseError !is ApiResponseError.HttpException ||
+                responseError.code != ApiResponseError.HttpException.Code.UNAUTHORIZED
+            ) {
+                throw responseError
+            }
+
+            val authTokens = getAuthTokens(userWalletId)
+            val newTokens = runCatching {
+                visaAuthRepository.refreshAccessTokens(authTokens.refreshToken)
+            }.getOrElse {
+                if (it is ApiResponseError.HttpException &&
+                    it.code == ApiResponseError.HttpException.Code.UNAUTHORIZED
+                ) {
+                    userWalletsStore.update(userWalletId) { userWallet ->
+                        userWallet.copy(
+                            scanResponse = userWallet.scanResponse.copy(
+                                visaCardActivationStatus = VisaCardActivationStatus.RefreshTokenExpired
+                            )
+                        )
+                    }
+                }
+                throw RefreshTokenExpiredException()
+            }
+
+            userWalletsStore.update(userWalletId) { userWallet ->
+                userWallet.copy(
+                    scanResponse = userWallet.scanResponse.copy(
+                        visaCardActivationStatus = VisaCardActivationStatus.Activated(
+                            visaAuthTokens = newTokens
+                        )
+                    )
+                )
+            }
+
+            requestBlock()
+        }
+    }
+
+    @Throws
+    private suspend fun getAuthTokens(userWalletId: UserWalletId): VisaAuthTokens {
+        val userWallet = findVisaUserWallet(userWalletId)
+        val status = userWallet.scanResponse.visaCardActivationStatus ?: error("Visa card activation status not found")
+        return (status as? VisaCardActivationStatus.Activated)?.visaAuthTokens ?: error("Visa card is not activated")
     }
 }
