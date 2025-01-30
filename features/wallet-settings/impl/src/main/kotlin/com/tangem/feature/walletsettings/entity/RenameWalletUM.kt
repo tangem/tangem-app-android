ee6eb5a80f73428490e57b43472e8dbcb72f83a5
Tangem <git@tangem.com>
2025-01-30 16:14:52 +0300

    Updated on 2026-09-01

--- features/wallet-settings/impl/src/main/kotlin/com/tangem/feature/walletsettings/entity/RenameWalletUM.kt
+++ features/wallet-settings/impl/src/main/kotlin/com/tangem/feature/walletsettings/entity/RenameWalletUM.kt
@@ -2,11 +2,13 @@ package com.tangem.feature.walletsettings.entity
 
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.text.input.TextFieldValue
+import com.tangem.core.ui.extensions.TextReference
 
 @Immutable
 internal data class RenameWalletUM(
     val walletNameValue: TextFieldValue,
-    val updateValue: (value: TextFieldValue) -> Unit,
+    val onValueChange: (value: TextFieldValue) -> Unit,
     val isConfirmEnabled: Boolean,
     val onConfirm: () -> Unit,
+    val error: TextReference?,
 )
