9d7e8048bcb6fca738ef549571597c2eeacc23de
Tangem <git@tangem.com>
2025-01-30 13:30:51 +0300

    Updated on 2026-08-07

---
+++ domain/transaction/src/main/java/com/tangem/domain/transaction/error/ErrorsMapper.kt
@@ -0,0 +1,50 @@
+package com.tangem.domain.transaction.error
+
+import com.tangem.blockchain.common.BlockchainSdkError
+import com.tangem.blockchain.extensions.Result
+import com.tangem.common.core.TangemSdkError
+import com.tangem.core.ui.extensions.wrappedList
+import com.tangem.domain.transaction.R
+import com.tangem.domain.transaction.error.SendTransactionError.Companion.USER_CANCELLED_ERROR_CODE
+import com.tangem.sdk.extensions.localizedDescriptionRes
+
+fun Result.Failure.mapToFeeError(): GetFeeError {
+    return when (this.error) {
+        is BlockchainSdkError.Tron.AccountActivationError -> {
+            GetFeeError.BlockchainErrors.TronActivationError
+        }
+        is BlockchainSdkError.Kaspa.ZeroUtxoError -> {
+            GetFeeError.BlockchainErrors.KaspaZeroUtxo
+        }
+        else -> GetFeeError.DataError(this.error)
+    }
+}
+
+fun parseWrappedError(error: BlockchainSdkError.WrappedTangemError): SendTransactionError {
+    return if (error.code == USER_CANCELLED_ERROR_CODE) {
+        SendTransactionError.UserCancelledError
+    } else {
+        when (val tangemError = error.tangemError) {
+            is TangemSdkError -> {
+                val resource = tangemError.localizedDescriptionRes()
+                val resId = resource.resId ?: R.string.common_unknown_error
+                val resArgs = resource.args.map { it.value }
+                SendTransactionError.TangemSdkError(tangemError.code, resId, wrappedList(resArgs))
+            }
+            is BlockchainSdkError.WrappedTangemError -> {
+                parseWrappedError(tangemError) // todo remove when sdk errors are revised
+            }
+            is BlockchainSdkError.WrappedThrowable -> {
+                val causeError = tangemError.cause
+                if (causeError is BlockchainSdkError) {
+                    SendTransactionError.BlockchainSdkError(causeError.code, causeError.customMessage)
+                } else {
+                    SendTransactionError.BlockchainSdkError(tangemError.code, tangemError.customMessage)
+                }
+            }
+            else -> {
+                SendTransactionError.BlockchainSdkError(error.code, tangemError.customMessage)
+            }
+        }
+    }
+}
