fecf5e460e9890319b86109360163e75f0051c25
Tangem <git@tangem.com>
2025-01-31 13:31:12 +0400

    Updated on 2026-08-19

--- domain/settings/src/main/java/com/tangem/domain/settings/ShouldSaveAccessCodesUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.tangem.domain.settings
-
-import com.tangem.domain.settings.repositories.SettingsRepository
-
-class ShouldSaveAccessCodesUseCase(private val settingsRepository: SettingsRepository) {
-
-    suspend operator fun invoke(): Boolean = settingsRepository.shouldSaveAccessCodes()
-}
