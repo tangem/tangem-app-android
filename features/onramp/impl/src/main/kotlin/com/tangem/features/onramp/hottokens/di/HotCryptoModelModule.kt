4d677925d2f196db428c61e28b105c01d853da47
Tangem <git@tangem.com>
2025-02-04 19:32:36 +0400

    Updated on 2026-08-19

---
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/hottokens/di/HotCryptoModelModule.kt
@@ -0,0 +1,20 @@
+package com.tangem.features.onramp.hottokens.di
+
+import com.tangem.core.decompose.di.DecomposeComponent
+import com.tangem.core.decompose.model.Model
+import com.tangem.features.onramp.hottokens.model.HotCryptoModel
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.multibindings.ClassKey
+import dagger.multibindings.IntoMap
+
+@Module
+@InstallIn(DecomposeComponent::class)
+internal interface HotCryptoModelModule {
+
+    @Binds
+    @IntoMap
+    @ClassKey(HotCryptoModel::class)
+    fun bindHotCryptoModel(model: HotCryptoModel): Model
+}
