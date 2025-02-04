ae408f6bb9815b4364b8d7935f70158121db42fa
Tangem <git@tangem.com>
2025-02-04 19:32:36 +0400

    Updated on 2025-11-26

---
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/hottokens/entity/HotCryptoUM.kt
@@ -0,0 +1,13 @@
+package com.tangem.features.onramp.hottokens.entity
+
+import com.tangem.core.ui.components.tokenlist.state.TokensListItemUM
+import kotlinx.collections.immutable.ImmutableList
+
+/**
+ * Hot crypto UM
+ *
+ * @property items items
+ *
+[REDACTED_AUTHOR]
+ */
+internal data class HotCryptoUM(val items: ImmutableList<TokensListItemUM>)
