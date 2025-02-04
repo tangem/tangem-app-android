7f323bab98602c0ca583d32bf9493eabb5f7ec70
Tangem <git@tangem.com>
2025-02-04 19:32:36 +0400

    Updated on 2025-12-05

---
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/hottokens/HotCryptoComponent.kt
@@ -0,0 +1,27 @@
+package com.tangem.features.onramp.hottokens
+
+import com.tangem.core.decompose.factory.ComponentFactory
+import com.tangem.core.ui.decompose.ComposableContentComponent
+import com.tangem.domain.tokens.model.CryptoCurrencyStatus
+import com.tangem.domain.wallets.models.UserWalletId
+
+/**
+ * Hot crypto component
+ *
+[REDACTED_AUTHOR]
+ */
+internal interface HotCryptoComponent : ComposableContentComponent {
+
+    /**
+     * Params
+     *
+     * @property userWalletId user wallet id
+     * @property onTokenClick lambda be invoked when token item is clicked
+     */
+    data class Params(
+        val userWalletId: UserWalletId,
+        val onTokenClick: (CryptoCurrencyStatus) -> Unit,
+    )
+
+    interface Factory : ComponentFactory<Params, HotCryptoComponent>
+}
