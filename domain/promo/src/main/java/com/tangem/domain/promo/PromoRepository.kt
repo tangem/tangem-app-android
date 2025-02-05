616fa9444fd129ba6a304fff3c72e08926e48226
Tangem <git@tangem.com>
2025-02-05 11:55:12 +0500

    Updated on 2026-09-21

--- domain/promo/src/main/java/com/tangem/domain/promo/PromoRepository.kt
+++ domain/promo/src/main/java/com/tangem/domain/promo/PromoRepository.kt
@@ -1,9 +1,11 @@
 package com.tangem.domain.promo
 
+import com.tangem.domain.promo.models.StoryContent
 import kotlinx.coroutines.flow.Flow
 
 interface PromoRepository {
 
+    // region Promo
     fun isReadyToShowWalletSwapPromo(): Flow<Boolean>
 
     fun isReadyToShowTokenSwapPromo(): Flow<Boolean>
@@ -11,10 +13,15 @@ interface PromoRepository {
     suspend fun setNeverToShowWalletSwapPromo()
 
     suspend fun setNeverToShowTokenSwapPromo()
+    // endregion
+
+    // region Stories
+    suspend fun getStoryById(id: String): StoryContent
 
     fun isReadyToShowSwapStories(): Flow<Boolean>
 
     suspend fun isReadyToShowSwapStoriesSync(): Boolean
 
     suspend fun setNeverToShowSwapStories()
+    // endregion
 }
