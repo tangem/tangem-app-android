35fb67e87af919ba6a754009f01116f54bdf43f8
Tangem <git@tangem.com>
2025-02-05 19:08:37 +0300

    Updated on 2026-08-19

--- app/src/main/java/com/tangem/tap/common/libs/blockchainsdk/DefaultTransactionSignerFactory.kt
+++ app/src/main/java/com/tangem/tap/common/libs/blockchainsdk/DefaultTransactionSignerFactory.kt
@@ -3,18 +3,20 @@ package com.tangem.tap.common.libs.blockchainsdk
 import com.tangem.Message
 import com.tangem.TangemSdk
 import com.tangem.blockchain.common.TransactionSigner
-import com.tangem.blockchainsdk.signer.TransactionSignerFactory
+import com.tangem.data.card.TransactionSignerFactory
+import com.tangem.domain.card.models.TwinKey
 import com.tangem.tap.common.redux.global.GlobalAction
 import com.tangem.tap.domain.TangemSigner
 import com.tangem.tap.store
 
 internal class DefaultTransactionSignerFactory : TransactionSignerFactory {
 
-    override fun createTransactionSigner(cardId: String?, sdk: TangemSdk): TransactionSigner {
+    override fun createTransactionSigner(cardId: String?, sdk: TangemSdk, twinKey: TwinKey?): TransactionSigner {
         return TangemSigner(
             cardId = cardId,
             tangemSdk = sdk,
             initialMessage = Message(),
+            twinKey = twinKey,
         ) { signResponse ->
             store.dispatch(action = GlobalAction.IsSignWithRing(signResponse.isRing))
         }
