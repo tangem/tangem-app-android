47ca165087760b3ffa397409bcd97aace5aa614a
Tangem <git@tangem.com>
2025-02-06 18:53:34 +0500

    Updated on 2026-07-23

--- data/manage-tokens/src/main/kotlin/com/tangem/data/managetokens/utils/ManagedCryptoCurrencyFactory.kt
+++ data/manage-tokens/src/main/kotlin/com/tangem/data/managetokens/utils/ManagedCryptoCurrencyFactory.kt
@@ -66,7 +66,7 @@ internal class ManagedCryptoCurrencyFactory(
                     createSource(
                         networkId = network.id,
                         contractAddress = network.address,
-                        decimals = network.decimalCount ?: return@mapNotNull null,
+                        decimals = network.decimalCount,
                         scanResponse = scanResponse,
                     )
                 } ?: emptyList(),
