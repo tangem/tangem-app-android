5d421022a6c3dea24f59542becc6fe9ca7340477
Tangem <git@tangem.com>
2025-02-07 18:14:09 +0400

    Updated on 2026-04-15

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/viewmodels/intents/WalletCardClickIntents.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/viewmodels/intents/WalletCardClickIntents.kt
@@ -1,21 +1,22 @@
 package com.tangem.feature.wallet.presentation.wallet.viewmodels.intents
 
 import arrow.core.getOrElse
+import com.arkivanov.decompose.router.slot.activate
 import com.tangem.common.routing.AppRoute
 import com.tangem.common.routing.AppRouter
 import com.tangem.core.analytics.api.AnalyticsEventHandler
-import com.tangem.core.ui.extensions.resourceReference
-import com.tangem.core.ui.extensions.wrappedList
 import com.tangem.domain.card.DeleteSavedAccessCodesUseCase
 import com.tangem.domain.redux.ReduxStateHolder
 import com.tangem.domain.wallets.models.UserWalletId
-import com.tangem.domain.wallets.usecase.*
-import com.tangem.feature.wallet.impl.R
+import com.tangem.domain.wallets.usecase.DeleteWalletUseCase
+import com.tangem.domain.wallets.usecase.GetSelectedWalletSyncUseCase
+import com.tangem.domain.wallets.usecase.GetUserWalletUseCase
 import com.tangem.feature.wallet.presentation.wallet.analytics.WalletScreenAnalyticsEvent.MainScreen
 import com.tangem.feature.wallet.presentation.wallet.domain.MultiWalletTokenListStore
 import com.tangem.feature.wallet.presentation.wallet.loaders.WalletScreenContentLoader
 import com.tangem.feature.wallet.presentation.wallet.state.WalletStateController
 import com.tangem.feature.wallet.presentation.wallet.state.model.WalletAlertState
+import com.tangem.feature.wallet.presentation.wallet.state.model.WalletDialogConfig
 import com.tangem.feature.wallet.presentation.wallet.state.model.WalletEvent
 import com.tangem.feature.wallet.presentation.wallet.state.utils.WalletEventSender
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
@@ -26,8 +27,6 @@ import javax.inject.Inject
 internal interface WalletCardClickIntents {
 
     fun onRenameBeforeConfirmationClick(userWalletId: UserWalletId)
-
-    fun onRenameAfterConfirmationClick(userWalletId: UserWalletId, name: String)
 
     fun onDeleteBeforeConfirmationClick(userWalletId: UserWalletId)
 
@@ -41,8 +40,6 @@ internal class WalletCardClickIntentsImplementor @Inject constructor(
     private val tokenListStore: MultiWalletTokenListStore,
     private val walletEventSender: WalletEventSender,
     private val walletScreenContentLoader: WalletScreenContentLoader,
-    private val renameWalletUseCase: RenameWalletUseCase,
-    private val getWalletNamesUseCase: GetWalletNamesUseCase,
     private val getUserWalletUseCase: GetUserWalletUseCase,
     private val getSelectedWalletSyncUseCase: GetSelectedWalletSyncUseCase,
     private val deleteWalletUseCase: DeleteWalletUseCase,
@@ -56,34 +53,12 @@ internal class WalletCardClickIntentsImplementor @Inject constructor(
     override fun onRenameBeforeConfirmationClick(userWalletId: UserWalletId) {
         analyticsEventHandler.send(MainScreen.EditWalletTapped)
 
-        viewModelScope.launch(dispatchers.main) {
-            val walletNames = getWalletNamesUseCase()
-            val currentWalletName = stateHolder.getSelectedWallet().walletCardState.title
-            walletEventSender.send(
-                event = WalletEvent.ShowAlert(
-                    state = WalletAlertState.RenameWalletAlert(
-                        text = currentWalletName,
-                        onConfirmClick = { onRenameAfterConfirmationClick(userWalletId, it) },
-                        errorTextProvider = { enteredName ->
-                            if (walletNames.contains(enteredName) && enteredName != currentWalletName) {
-                                resourceReference(
-                                    R.string.user_wallet_list_rename_popup_error_already_exists,
-                                    wrappedList(enteredName),
-                                )
-                            } else {
-                                null
-                            }
-                        },
-                    ),
-                ),
-            )
-        }
-    }
-
-    override fun onRenameAfterConfirmationClick(userWalletId: UserWalletId, name: String) {
-        viewModelScope.launch(dispatchers.main) {
-            renameWalletUseCase(userWalletId = userWalletId, name)
-        }
+        router.dialogNavigation.activate(
+            configuration = WalletDialogConfig.RenameWallet(
+                userWalletId = userWalletId,
+                currentName = stateHolder.getSelectedWallet().walletCardState.title,
+            ),
+        )
     }
 
     override fun onDeleteBeforeConfirmationClick(userWalletId: UserWalletId) {
