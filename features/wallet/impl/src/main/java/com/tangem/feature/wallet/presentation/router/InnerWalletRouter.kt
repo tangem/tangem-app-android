2d568fc78709756f3b62e50f6069ec526d9c2c74
Tangem <git@tangem.com>
2025-02-07 18:14:09 +0400

    Updated on 2026-06-27

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/router/InnerWalletRouter.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/router/InnerWalletRouter.kt
@@ -2,10 +2,12 @@ package com.tangem.feature.wallet.presentation.router
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
+import com.arkivanov.decompose.router.slot.SlotNavigation
+import com.tangem.core.decompose.context.AppComponentContext
 import com.tangem.domain.models.scan.ScanResponse
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.domain.wallets.models.UserWalletId
-import com.tangem.features.markets.entry.MarketsEntryComponent
+import com.tangem.feature.wallet.presentation.wallet.state.model.WalletDialogConfig
 import com.tangem.features.wallet.navigation.WalletRouter
 
 /**
@@ -19,13 +21,17 @@ import com.tangem.features.wallet.navigation.WalletRouter
 @Stable
 internal interface InnerWalletRouter : WalletRouter {
 
+    val dialogNavigation: SlotNavigation<WalletDialogConfig>
+
+    fun initializeResources(appComponentContext: AppComponentContext)
+
     /**
      * Initialize router
      *
      * @param onFinish finish activity callback
      */
     @Composable
-    fun Initialize(onFinish: () -> Unit, marketsEntryComponent: MarketsEntryComponent)
+    fun Initialize(onFinish: () -> Unit)
 
     /** Pop back stack */
     fun popBackStack()
