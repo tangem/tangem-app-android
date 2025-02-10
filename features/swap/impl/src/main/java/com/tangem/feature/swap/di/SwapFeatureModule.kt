23a5302e609790162d5ce775699d6dd2e3ccb01e
Tangem <git@tangem.com>
2025-02-10 21:38:58 +0300

    Updated on 2026-07-07

--- features/swap/impl/src/main/java/com/tangem/feature/swap/di/SwapFeatureModule.kt
+++ features/swap/impl/src/main/java/com/tangem/feature/swap/di/SwapFeatureModule.kt
@@ -1,8 +1,11 @@
 package com.tangem.feature.swap.di
 
 import com.tangem.core.configtoggle.feature.FeatureTogglesManager
+import com.tangem.feature.swap.DefaultSwapComponent
 import com.tangem.feature.swap.DefaultSwapFeatureToggles
+import com.tangem.features.swap.SwapComponent
 import com.tangem.features.swap.SwapFeatureToggles
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -18,4 +21,13 @@ internal object SwapFeatureModule {
     fun provideSwapFeatureToggles(featureToggles: FeatureTogglesManager): SwapFeatureToggles {
         return DefaultSwapFeatureToggles(featureToggles)
     }
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal interface SwapFeatureModuleBinds {
+
+    @Binds
+    @Singleton
+    fun provideSwapComponentFactory(impl: DefaultSwapComponent.Factory): SwapComponent.Factory
 }
