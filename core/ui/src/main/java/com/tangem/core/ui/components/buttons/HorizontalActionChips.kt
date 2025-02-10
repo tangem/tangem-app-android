cf85f768a0b4d3a9ab0712d4850b0c422f267e2e
Tangem <git@tangem.com>
2025-02-10 11:01:12 +0500

    Updated on 2026-07-23

--- core/ui/src/main/java/com/tangem/core/ui/components/buttons/HorizontalActionChips.kt
+++ core/ui/src/main/java/com/tangem/core/ui/components/buttons/HorizontalActionChips.kt
@@ -4,20 +4,23 @@ import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.datasource.CollectionPreviewParameterProvider
+import androidx.compose.ui.unit.dp
 import com.tangem.core.ui.R
 import com.tangem.core.ui.components.buttons.actions.ActionButton
 import com.tangem.core.ui.components.buttons.actions.ActionButtonConfig
 import com.tangem.core.ui.extensions.TextReference
-import com.tangem.core.ui.res.TangemThemePreview
 import com.tangem.core.ui.res.TangemTheme
+import com.tangem.core.ui.res.TangemThemePreview
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 
@@ -25,6 +28,7 @@ import kotlinx.collections.immutable.persistentListOf
 fun HorizontalActionChips(
     buttons: ImmutableList<ActionButtonConfig>,
     modifier: Modifier = Modifier,
+    containerColor: Color = TangemTheme.colors.background.secondary,
     contentPadding: PaddingValues = PaddingValues(TangemTheme.dimens.spacing0),
 ) {
     LazyRow(
@@ -36,7 +40,7 @@ fun HorizontalActionChips(
         // do not use key cause when change items order, list is scrolled
         items(
             items = buttons,
-            itemContent = { ActionButton(config = it) },
+            itemContent = { ActionButton(config = it, containerColor = containerColor) },
         )
     }
 }
@@ -46,21 +50,30 @@ fun HorizontalActionChips(
 @Preview(widthDp = 360, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 private fun Preview_HorizontalActionChips(
-    @PreviewParameter(ActionButtonConfigProvider::class) buttons: HorizontalActionChips,
+    @PreviewParameter(ActionButtonConfigProvider::class) buttons: HorizontalActionChipsData,
 ) {
     TangemThemePreview {
-        HorizontalActionChips(buttons = buttons.buttons)
+        HorizontalActionChips(
+            buttons = buttons.buttons,
+            modifier = Modifier.padding(4.dp),
+        )
     }
 }
 
-private class ActionButtonConfigProvider : CollectionPreviewParameterProvider<HorizontalActionChips>(
+private class ActionButtonConfigProvider : CollectionPreviewParameterProvider<HorizontalActionChipsData>(
     collection = listOf(
-        HorizontalActionChips(
+        HorizontalActionChipsData(
             buttons = persistentListOf(
                 ActionButtonConfig(
                     text = TextReference.Str(value = "Buy"),
                     iconResId = R.drawable.ic_plus_24,
                     onClick = {},
+                ),
+                ActionButtonConfig(
+                    text = TextReference.Str(value = "Exchange"),
+                    iconResId = R.drawable.ic_exchange_vertical_24,
+                    onClick = {},
+                    showBadge = true,
                 ),
                 ActionButtonConfig(
                     text = TextReference.Str(value = "Send"),
@@ -72,17 +85,12 @@ private class ActionButtonConfigProvider : CollectionPreviewParameterProvider<Ho
                     iconResId = R.drawable.ic_arrow_down_24,
                     onClick = {},
                 ),
-                ActionButtonConfig(
-                    text = TextReference.Str(value = "Exchange"),
-                    iconResId = R.drawable.ic_exchange_vertical_24,
-                    onClick = {},
-                ),
             ),
         ),
     ),
 )
 
-private data class HorizontalActionChips(
+private data class HorizontalActionChipsData(
     val buttons: ImmutableList<ActionButtonConfig>,
 )
 // endregion Preview
