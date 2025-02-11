22429bd9fac6add84084ddb4e4d7f72b17918111
Tangem <git@tangem.com>
2025-02-11 13:35:31 +0300

    Updated on 2025-10-01

--- app/src/main/java/com/tangem/tap/data/DefaultVisaOTPStorage.kt
+++ app/src/main/java/com/tangem/tap/data/DefaultVisaOTPStorage.kt
@@ -1,8 +1,11 @@
 package com.tangem.tap.data
 
 import android.content.Context
+import com.tangem.common.extensions.toByteArray
+import com.tangem.common.extensions.toInt
 import com.tangem.common.services.secure.SecureStorage
 import com.tangem.datasource.local.visa.VisaOTPStorage
+import com.tangem.datasource.local.visa.VisaOtpData
 import com.tangem.sdk.storage.AndroidSecureStorage
 import com.tangem.sdk.storage.createEncryptedSharedPreferences
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
@@ -24,19 +27,27 @@ class DefaultVisaOTPStorage @Inject constructor(
         ),
     )
 
-    override suspend fun saveOTP(cardId: String, otp: ByteArray) = withContext(dispatcherProvider.io) {
-        secureStorage.store(otp, VISA_OTP_KEY_PREFIX + cardId)
+    override suspend fun saveOTP(cardId: String, data: VisaOtpData) = withContext(dispatcherProvider.io) {
+        secureStorage.store(data.rootOTP, VISA_ROOT_OTP_KEY_PREFIX + cardId)
+        secureStorage.store(data.counter.toByteArray(), VISA_OTP_COUNTER_KEY_PREFIX + cardId)
     }
 
-    override suspend fun getOTP(cardId: String): ByteArray? = withContext(dispatcherProvider.io) {
-        secureStorage.get(VISA_OTP_KEY_PREFIX + cardId)
+    override suspend fun getOTP(cardId: String): VisaOtpData? = withContext(dispatcherProvider.io) {
+        val rootOTP = secureStorage.get(VISA_ROOT_OTP_KEY_PREFIX + cardId) ?: return@withContext null
+        val counter = secureStorage.get(VISA_OTP_COUNTER_KEY_PREFIX + cardId)?.toInt() ?: return@withContext null
+        VisaOtpData(
+            rootOTP = rootOTP,
+            counter = counter,
+        )
     }
 
     override suspend fun removeOTP(cardId: String) = withContext(dispatcherProvider.io) {
-        secureStorage.delete(VISA_OTP_KEY_PREFIX + cardId)
+        secureStorage.delete(VISA_ROOT_OTP_KEY_PREFIX + cardId)
+        secureStorage.delete(VISA_OTP_COUNTER_KEY_PREFIX + cardId)
     }
 
     private companion object {
-        const val VISA_OTP_KEY_PREFIX = "visa_otp_"
+        const val VISA_ROOT_OTP_KEY_PREFIX = "visa_root_otp_"
+        const val VISA_OTP_COUNTER_KEY_PREFIX = "visa_otp_counter_"
     }
 }
