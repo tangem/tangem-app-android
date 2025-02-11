ecdb72e55ccdba4dd95db6d0b932a02553f5e09c
Tangem <git@tangem.com>
2025-02-11 13:35:31 +0300

    Updated on 2026-08-14

--- data/visa/src/main/kotlin/com/tangem/data/visa/DefaultVisaActivationRepository.kt
+++ data/visa/src/main/kotlin/com/tangem/data/visa/DefaultVisaActivationRepository.kt
@@ -1,49 +1,195 @@
 package com.tangem.data.visa
 
-import com.tangem.common.extensions.toHexString
-import com.tangem.crypto.CryptoUtils
+import com.tangem.data.visa.converter.AccessCodeDataConverter
 import com.tangem.data.visa.converter.VisaActivationStatusConverter
-import com.tangem.datasource.api.common.visa.TangemVisaAuthProvider
+import com.tangem.datasource.api.common.response.ApiResponseError
+import com.tangem.datasource.api.common.response.getOrThrow
 import com.tangem.datasource.api.visa.TangemVisaApi
-import com.tangem.domain.visa.model.ActivationOrder
-import com.tangem.domain.visa.model.VisaActivationRemoteState
+import com.tangem.datasource.api.visa.models.request.ActivationByCardWalletRequest
+import com.tangem.datasource.api.visa.models.request.ActivationByCustomerWalletRequest
+import com.tangem.datasource.local.visa.VisaAuthTokenStorage
+import com.tangem.domain.visa.exception.RefreshTokenExpiredException
+import com.tangem.domain.visa.model.*
 import com.tangem.domain.visa.repository.VisaActivationRepository
+import com.tangem.domain.visa.repository.VisaAuthRepository
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import kotlinx.coroutines.withContext
 
-@Suppress("UnusedPrivateMember")
+@Suppress("LongParameterList")
 internal class DefaultVisaActivationRepository @AssistedInject constructor(
-    @Assisted private val cardId: String,
+    @Assisted private val visaCardId: VisaCardId,
     private val visaApi: TangemVisaApi,
     private val dispatcherProvider: CoroutineDispatcherProvider,
     private val visaActivationStatusConverter: VisaActivationStatusConverter,
-    private val visaAuthProvider: TangemVisaAuthProvider,
+    private val visaAuthTokenStorage: VisaAuthTokenStorage,
+    private val accessCodeDataConverter: AccessCodeDataConverter,
+    private val visaAuthRepository: VisaAuthRepository,
 ) : VisaActivationRepository {
 
     override suspend fun getActivationRemoteState(): VisaActivationRemoteState = withContext(dispatcherProvider.io) {
-        // visaActivationStatusConverter.convert(visaApi.getRemoteActivationStatus(visaAuthProvider.getAuthHeader(cardId)))
-        VisaActivationRemoteState.CardWalletSignatureRequired // mock
-        // TODO implement refreshing access token if it's expired
+        val result = request {
+            val authTokens =
+                checkNotNull(visaAuthTokenStorage.get(visaCardId.cardId)) { "Visa auth tokens are not stored" }
+            val accessCodeData = accessCodeDataConverter.convert(authTokens)
+
+            visaApi.getRemoteActivationStatus(
+                authHeader = authTokens.getAuthHeader(),
+                customerId = accessCodeData.customerId,
+                productInstanceId = accessCodeData.productInstanceId,
+                cardId = visaCardId.cardId,
+                cardPublicKey = visaCardId.cardPublicKey,
+            ).getOrThrow()
+        }
+
+        visaActivationStatusConverter.convert(result)
     }
 
     override suspend fun getActivationRemoteStateLongPoll(): VisaActivationRemoteState =
         withContext(dispatcherProvider.io) {
-            // visaActivationStatusConverter.convert(
-            //     visaApi.getRemoteActivationStatusLongPoll(visaAuthProvider.getAuthHeader(cardId)),
-            // )
+            val result = request {
+                val authTokens =
+                    checkNotNull(visaAuthTokenStorage.get(visaCardId.cardId)) { "Visa auth tokens are not stored" }
+                val accessCodeData = accessCodeDataConverter.convert(authTokens)
 
-            VisaActivationRemoteState.WaitingPinCode
+                visaApi.getRemoteActivationStatusLongPoll(
+                    authHeader = authTokens.getAuthHeader(),
+                    customerId = accessCodeData.customerId,
+                    productInstanceId = accessCodeData.productInstanceId,
+                    cardId = visaCardId.cardId,
+                    cardPublicKey = visaCardId.cardPublicKey,
+                ).getOrThrow()
+            }
+
+            visaActivationStatusConverter.convert(result)
         }
 
-    override suspend fun getActivationOrderToSign(): ActivationOrder = withContext(dispatcherProvider.io) {
-        ActivationOrder(CryptoUtils.generateRandomBytes(length = 32).toHexString())
+    override suspend fun getCardWalletAcceptanceData(
+        request: VisaCardWalletDataToSignRequest,
+    ): VisaDataToSignByCardWallet = withContext(dispatcherProvider.io) {
+        val result = request {
+            val authTokens =
+                checkNotNull(visaAuthTokenStorage.get(visaCardId.cardId)) { "Visa auth tokens are not stored" }
+            val accessCodeData = accessCodeDataConverter.convert(authTokens)
+
+            visaApi.getCardWalletAcceptance(
+                authHeader = authTokens.getAuthHeader(),
+                customerId = accessCodeData.customerId,
+                productInstanceId = accessCodeData.productInstanceId,
+                activationOrderId = request.orderId,
+                customerWalletAddress = request.customerWalletAddress,
+            ).getOrThrow()
+        }
+
+        VisaDataToSignByCardWallet(
+            request = request,
+            hashToSign = result.dataForCardWallet.hash,
+        )
+    }
+
+    override suspend fun getCustomerWalletAcceptanceData(
+        request: VisaCustomerWalletDataToSignRequest,
+    ): VisaDataToSignByCustomerWallet = withContext(dispatcherProvider.io) {
+        val result = request {
+            val authTokens =
+                checkNotNull(visaAuthTokenStorage.get(visaCardId.cardId)) { "Visa auth tokens are not stored" }
+            val accessCodeData = accessCodeDataConverter.convert(authTokens)
+
+            visaApi.getCustomerWalletAcceptance(
+                authHeader = authTokens.getAuthHeader(),
+                customerId = accessCodeData.customerId,
+                productInstanceId = accessCodeData.productInstanceId,
+                activationOrderId = request.orderId,
+                cardWalletAddress = request.cardWalletAddress,
+            ).getOrThrow()
+        }
+
+        VisaDataToSignByCustomerWallet(
+            request = request,
+            hashToSign = result.dataForCardWallet.hash,
+        )
+    }
+
+    override suspend fun activateCard(signedData: VisaSignedActivationDataByCardWallet) {
+        withContext(dispatcherProvider.io) {
+            request {
+                val authTokens =
+                    checkNotNull(visaAuthTokenStorage.get(visaCardId.cardId)) { "Visa auth tokens are not stored" }
+                val accessCodeData = accessCodeDataConverter.convert(authTokens)
+
+                visaApi.activateByCardWallet(
+                    authHeader = authTokens.getAuthHeader(),
+                    body = ActivationByCardWalletRequest(
+                        customerId = accessCodeData.customerId,
+                        productInstanceId = accessCodeData.productInstanceId,
+                        activationOrderId = signedData.dataToSign.request.orderId,
+                        data = ActivationByCardWalletRequest.Data(
+                            cardWallet = ActivationByCardWalletRequest.CardWallet(
+                                address = signedData.cardWalletAddress,
+                                cardWalletConfirmation = null, // for second iteration
+                                deployAcceptanceSignature = signedData.signature,
+                            ),
+                            otp = ActivationByCardWalletRequest.Otp(
+                                rootOtp = signedData.rootOTP,
+                                counter = signedData.otpCounter,
+                            ),
+                        ),
+                    ),
+                )
+            }
+        }
+    }
+
+    override suspend fun approveByCustomerWallet(signedData: VisaSignedDataByCustomerWallet) {
+        withContext(dispatcherProvider.io) {
+            request {
+                val authTokens =
+                    checkNotNull(visaAuthTokenStorage.get(visaCardId.cardId)) { "Visa auth tokens are not stored" }
+                val accessCodeData = accessCodeDataConverter.convert(authTokens)
+
+                visaApi.activateByCustomerWallet(
+                    authHeader = authTokens.getAuthHeader(),
+                    body = ActivationByCustomerWalletRequest(
+                        customerId = accessCodeData.customerId,
+                        productInstanceId = accessCodeData.productInstanceId,
+                        activationOrderId = signedData.dataToSign.request.orderId,
+                        data = ActivationByCustomerWalletRequest.Data(
+                            customerWallet = ActivationByCustomerWalletRequest.CustomerWallet(
+                                address = signedData.customerWalletAddress,
+                                deployAcceptanceSignature = signedData.signature,
+                            ),
+                        ),
+                    ),
+                )
+            }
+        }
+    }
+
+    private suspend fun <T : Any> request(requestBlock: suspend () -> T): T {
+        return runCatching {
+            requestBlock()
+        }.getOrElse { responseError ->
+            if (responseError !is ApiResponseError.HttpException ||
+                responseError.code != ApiResponseError.HttpException.Code.UNAUTHORIZED
+            ) {
+                throw responseError
+            }
+
+            val authTokens = visaAuthTokenStorage.get(visaCardId.cardId) ?: error("Auth tokens are not stored")
+            val newTokens = runCatching {
+                visaAuthRepository.refreshAccessTokens(authTokens.refreshToken)
+            }.getOrElse { throw RefreshTokenExpiredException() }
+
+            visaAuthTokenStorage.store(visaCardId.cardId, newTokens)
+
+            requestBlock()
+        }
     }
 
     @AssistedFactory
     interface Factory : VisaActivationRepository.Factory {
-        override fun create(cardId: String): DefaultVisaActivationRepository
+        override fun create(cardId: VisaCardId): DefaultVisaActivationRepository
     }
 }
