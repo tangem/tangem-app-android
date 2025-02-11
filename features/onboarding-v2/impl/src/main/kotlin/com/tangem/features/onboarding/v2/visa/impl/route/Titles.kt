1ff7b52f866dc7f051736d6d9570496b98e8c5ed
Tangem <git@tangem.com>
2025-02-11 14:22:10 +0300

    Updated on 2025-12-30

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/visa/impl/route/Titles.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/visa/impl/route/Titles.kt
@@ -8,7 +8,7 @@ internal fun OnboardingVisaRoute.screenTitle(): TextReference = when (this) {
     is OnboardingVisaRoute.ChooseWallet -> TextReference.Str("Account activation")
     OnboardingVisaRoute.InProgress -> TextReference.Str("In progress")
     is OnboardingVisaRoute.OtherWalletApproveOption -> TextReference.Str("Wallet connection")
-    OnboardingVisaRoute.PinCode -> TextReference.Str("PIN code")
+    is OnboardingVisaRoute.PinCode -> TextReference.Str("PIN code")
     is OnboardingVisaRoute.TangemWalletApproveOption -> TextReference.Str("Wallet connection")
     is OnboardingVisaRoute.Welcome -> TextReference.Str("Getting started")
 }
