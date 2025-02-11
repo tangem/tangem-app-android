7c31f7d870018806a47552230afb46b690392363
Tangem <git@tangem.com>
2025-02-11 13:35:31 +0300

    Updated on 2026-05-04

--- libs/tangem-sdk-api/src/main/kotlin/com/tangem/sdk/api/visa/VisaCardActivationResponse.kt
+++ libs/tangem-sdk-api/src/main/kotlin/com/tangem/sdk/api/visa/VisaCardActivationResponse.kt
@@ -1,11 +1,9 @@
 package com.tangem.sdk.api.visa
 
 import com.tangem.domain.models.scan.CardDTO
-import com.tangem.domain.visa.model.SignedActivationOrder
-import com.tangem.domain.visa.model.VisaRootOTP
+import com.tangem.domain.visa.model.VisaSignedActivationDataByCardWallet
 
 data class VisaCardActivationResponse(
-    val signedActivationOrder: SignedActivationOrder,
-    val rootOTP: VisaRootOTP,
+    val signedActivationData: VisaSignedActivationDataByCardWallet,
     val newCardDTO: CardDTO,
 )
