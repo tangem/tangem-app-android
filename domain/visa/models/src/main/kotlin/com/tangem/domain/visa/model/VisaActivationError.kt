ae74bb0bb7818089897ec828ac578236dc142c78
Tangem <git@tangem.com>
2025-02-11 13:35:31 +0300

    Updated on 2026-02-06

--- domain/visa/models/src/main/kotlin/com/tangem/domain/visa/model/VisaActivationError.kt
+++ domain/visa/models/src/main/kotlin/com/tangem/domain/visa/model/VisaActivationError.kt
@@ -4,6 +4,7 @@ enum class VisaActivationError(val message: String) {
     BlockedForActivation("Card is blocked for activation"),
     InvalidActivationState("Invalid activation state"),
     WrongCard("Wrong card tapped"),
+    WrongRemoteState("Wrong remote state"),
     MissingWallet("Missing wallet"),
     MissingRootOTP("Missing root OTP"),
 }
