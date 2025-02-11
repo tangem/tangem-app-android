c47cc1aacd3ac24e4c85b4ef8c4d800a6cd7e13b
Tangem <git@tangem.com>
2025-02-11 13:35:31 +0300

    Updated on 2026-01-20

--- core/datasource/src/main/java/com/tangem/datasource/local/visa/VisaOTPStorage.kt
+++ core/datasource/src/main/java/com/tangem/datasource/local/visa/VisaOTPStorage.kt
@@ -2,11 +2,16 @@ package com.tangem.datasource.local.visa
 
 interface VisaOTPStorage {
 
-    suspend fun saveOTP(cardId: String, otp: ByteArray)
+    suspend fun saveOTP(cardId: String, data: VisaOtpData)
 
-    suspend fun getOTP(cardId: String): ByteArray?
+    suspend fun getOTP(cardId: String): VisaOtpData?
 
     suspend fun removeOTP(cardId: String)
 }
+
+class VisaOtpData(
+    val rootOTP: ByteArray,
+    val counter: Int,
+)
 
 suspend fun VisaOTPStorage.hasSavedOTP(cardId: String): Boolean = getOTP(cardId) != null
