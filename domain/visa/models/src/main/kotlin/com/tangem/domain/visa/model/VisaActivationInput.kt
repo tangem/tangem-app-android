bbe21932fc23a27fd46b54b6c3b939c1204d7eac
Tangem <git@tangem.com>
2025-02-11 13:35:31 +0300

    Updated on 2025-11-03

--- domain/visa/models/src/main/kotlin/com/tangem/domain/visa/model/VisaActivationInput.kt
+++ domain/visa/models/src/main/kotlin/com/tangem/domain/visa/model/VisaActivationInput.kt
@@ -8,26 +8,6 @@ import kotlinx.serialization.Serializable
 @JsonClass(generateAdapter = true)
 data class VisaActivationInput(
     @Json(name = "cardId") val cardId: String,
-    @Json(name = "cardPublicKey") val cardPublicKey: ByteArray,
+    @Json(name = "cardPublicKey") val cardPublicKey: String,
     @Json(name = "isAccessCodeSet") val isAccessCodeSet: Boolean,
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as VisaActivationInput
-
-        if (cardId != other.cardId) return false
-        if (!cardPublicKey.contentEquals(other.cardPublicKey)) return false
-        if (isAccessCodeSet != other.isAccessCodeSet) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = cardId.hashCode()
-        result = 31 * result + cardPublicKey.contentHashCode()
-        result = 31 * result + isAccessCodeSet.hashCode()
-        return result
-    }
-}
+)
