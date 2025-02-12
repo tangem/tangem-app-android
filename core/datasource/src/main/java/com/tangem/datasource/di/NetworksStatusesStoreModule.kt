a6bafadf596137f4f064ac48cd52736eaffaa186
Tangem <git@tangem.com>
2025-02-12 23:04:32 +0400

    Updated on 2026-08-27

--- core/datasource/src/main/java/com/tangem/datasource/di/NetworksStatusesStoreModule.kt
+++ core/datasource/src/main/java/com/tangem/datasource/di/NetworksStatusesStoreModule.kt
@@ -1,21 +1,39 @@
 package com.tangem.datasource.di
 
+import android.content.Context
+import androidx.datastore.core.DataStoreFactory
+import androidx.datastore.dataStoreFile
+import com.squareup.moshi.Moshi
 import com.tangem.datasource.local.datastore.RuntimeDataStore
 import com.tangem.datasource.local.network.DefaultNetworksStatusesStore
 import com.tangem.datasource.local.network.NetworksStatusesStore
+import com.tangem.datasource.local.network.utils.NetworkStatusesSerializer
+import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal object NetworksStatusesStoreModule {
 
     @Provides
-    fun provideNetworksStatusesStore(): NetworksStatusesStore {
+    fun provideNetworksStatusesStore(
+        @NetworkMoshi moshi: Moshi,
+        @ApplicationContext context: Context,
+        dispatchers: CoroutineDispatcherProvider,
+    ): NetworksStatusesStore {
         return DefaultNetworksStatusesStore(
-            dataStore = RuntimeDataStore(),
+            runtimeDataStore = RuntimeDataStore(),
+            persistneceDataStore = DataStoreFactory.create(
+                serializer = NetworkStatusesSerializer(moshi),
+                produceFile = { context.dataStoreFile(fileName = "networks_statuses") },
+                scope = CoroutineScope(context = dispatchers.io + SupervisorJob()),
+            ),
         )
     }
 }
