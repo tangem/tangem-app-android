5b3c248cedb1ee8fa51d1e38938c85e5517c7290
Tangem <git@tangem.com>
2025-02-12 23:05:18 +0400

    Updated on 2025-12-30

--- core/datasource/src/main/java/com/tangem/datasource/di/StakingStoreModule.kt
+++ core/datasource/src/main/java/com/tangem/datasource/di/StakingStoreModule.kt
@@ -1,13 +1,20 @@
 package com.tangem.datasource.di
 
+import android.content.Context
+import androidx.datastore.core.DataStoreFactory
+import androidx.datastore.dataStoreFile
+import com.squareup.moshi.Moshi
 import com.tangem.datasource.local.datastore.RuntimeDataStore
 import com.tangem.datasource.local.token.*
-import com.tangem.datasource.local.token.DefaultStakingBalanceStore
-import com.tangem.datasource.local.token.DefaultStakingYieldsStore
+import com.tangem.datasource.local.token.utils.YieldBalancesSerializer
+import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 import javax.inject.Singleton
 
 @Module
@@ -22,8 +29,18 @@ internal object StakingStoreModule {
 
     @Provides
     @Singleton
-    fun provideStakingBalanceStore(): StakingBalanceStore {
-        return DefaultStakingBalanceStore(dataStore = RuntimeDataStore())
+    fun provideStakingBalanceStore(
+        @NetworkMoshi moshi: Moshi,
+        @ApplicationContext context: Context,
+        dispatchers: CoroutineDispatcherProvider,
+    ): StakingBalanceStore {
+        return DefaultStakingBalanceStore(
+            dataStore = DataStoreFactory.create(
+                serializer = YieldBalancesSerializer(moshi),
+                produceFile = { context.dataStoreFile(fileName = "yield_balances") },
+                scope = CoroutineScope(context = dispatchers.io + SupervisorJob()),
+            ),
+        )
     }
 
     @Provides
