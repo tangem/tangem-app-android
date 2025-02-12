fbb5b30d1c5997501377d1ab9d495fa526c9f71e
Tangem <git@tangem.com>
2025-02-12 15:33:35 +0400

    Updated on 2026-03-27

--- core/datasource/src/main/java/com/tangem/datasource/exchangeservice/hotcrypto/HotCryptoResponseStore.kt
+++ core/datasource/src/main/java/com/tangem/datasource/exchangeservice/hotcrypto/HotCryptoResponseStore.kt
@@ -2,15 +2,15 @@ package com.tangem.datasource.exchangeservice.hotcrypto
 
 import com.tangem.datasource.api.tangemTech.models.HotCryptoResponse
 import com.tangem.datasource.local.datastore.RuntimeStateStore
+import com.tangem.domain.wallets.models.UserWalletId
 import javax.inject.Inject
 import javax.inject.Singleton
 
 /**
- * Store of [HotCryptoResponse]
+ * Store of pair of [UserWalletId] and [HotCryptoResponse]
  *
 [REDACTED_AUTHOR]
  */
 @Singleton
-class HotCryptoResponseStore @Inject constructor() : RuntimeStateStore<HotCryptoResponse> by RuntimeStateStore(
-    defaultValue = HotCryptoResponse(imageHost = null, tokens = emptyList()),
-)
+class HotCryptoResponseStore @Inject constructor() :
+    RuntimeStateStore<Map<UserWalletId, HotCryptoResponse>> by RuntimeStateStore(defaultValue = emptyMap())
