5ae5b86980fb716a546eb5ffd0d6bdd23ac6d0c7
Tangem <git@tangem.com>
2025-02-12 11:46:40 +0400

    Updated on 2026-08-14

--- domain/onramp/models/src/main/kotlin/com/tangem/domain/onramp/model/HotCryptoCurrency.kt
+++ domain/onramp/models/src/main/kotlin/com/tangem/domain/onramp/model/HotCryptoCurrency.kt
@@ -1,19 +1,17 @@
 package com.tangem.domain.onramp.model
 
 import com.tangem.domain.tokens.model.CryptoCurrency
-import java.math.BigDecimal
+import com.tangem.domain.tokens.model.Quote
 
 /**
  * Hot crypto currency
  *
  * @property cryptoCurrency crypto currency
- * @property fiatRate       fiat rate
- * @property priceChange    price change
+ * @property quote          quote
  *
 [REDACTED_AUTHOR]
  */
 data class HotCryptoCurrency(
     val cryptoCurrency: CryptoCurrency,
-    val fiatRate: BigDecimal,
-    val priceChange: BigDecimal,
+    val quote: Quote,
 )
