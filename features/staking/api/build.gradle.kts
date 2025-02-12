83db31a661e715025dc8c73016bcf18d54b9d6eb
Tangem <git@tangem.com>
2025-02-12 18:03:25 +0300

    Updated on 2026-05-04

--- features/staking/api/build.gradle.kts
+++ features/staking/api/build.gradle.kts
@@ -10,6 +10,14 @@ android {
 }
 
 dependencies {
+    /** Core */
+    implementation(projects.core.decompose)
+    implementation(projects.core.ui)
+
+    /** Domain models */
+    implementation(projects.domain.staking.models)
+    implementation(projects.domain.tokens.models)
+    implementation(projects.domain.wallets.models)
 
     /** AndroidX */
     implementation(deps.androidx.fragment.ktx)
