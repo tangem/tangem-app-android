78f10fb2325e334dc866f999f861b3ba0d68ff50
Tangem <git@tangem.com>
2025-02-12 17:35:12 +0500

    Updated on 2026-07-01

--- domain/promo/src/main/java/com/tangem/domain/promo/GetStoryContentUseCase.kt
+++ domain/promo/src/main/java/com/tangem/domain/promo/GetStoryContentUseCase.kt
@@ -1,13 +1,24 @@
 package com.tangem.domain.promo
 
 import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
 import com.tangem.domain.promo.models.StoryContent
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEmpty
 
 class GetStoryContentUseCase(
     private val promoRepository: PromoRepository,
 ) {
 
-    suspend operator fun invoke(id: String): Either<Throwable, StoryContent> = Either.catch {
-        promoRepository.getStoryById(id)
+    operator fun invoke(id: String): Flow<Either<Throwable, StoryContent?>> = promoRepository.getStoryById(id)
+        .map<StoryContent?, Either<Throwable, StoryContent?>> { it.right() }
+        .catch { emit(it.left()) }
+        .onEmpty { emit(null.right()) }
+
+    suspend fun invokeSync(id: String): Either<Throwable, StoryContent?> = Either.catch {
+        promoRepository.getStoryByIdSync(id)
     }
 }
