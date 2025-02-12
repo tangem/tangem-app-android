de45e4463b1221ca8bb817a69da111378c1544e3
Tangem <git@tangem.com>
2025-02-12 11:46:40 +0400

    Updated on 2025-10-13

--- core/datasource/src/main/java/com/tangem/datasource/api/tangemTech/models/HotCryptoResponse.kt
+++ core/datasource/src/main/java/com/tangem/datasource/api/tangemTech/models/HotCryptoResponse.kt
@@ -12,12 +12,12 @@ data class HotCryptoResponse(
 
     @JsonClass(generateAdapter = true)
     data class Token(
-        @Json(name = "id") val id: String,
-        @Json(name = "symbol") val symbol: String,
-        @Json(name = "name") val name: String,
-        @Json(name = "network_id") val networkId: String,
-        @Json(name = "current_price") val currentPrice: BigDecimal,
-        @Json(name = "price_change_percentage_24h") val priceChangePercentage: BigDecimal,
+        @Json(name = "id") val id: String? = null,
+        @Json(name = "symbol") val symbol: String? = null,
+        @Json(name = "name") val name: String? = null,
+        @Json(name = "network_id") val networkId: String? = null,
+        @Json(name = "current_price") val currentPrice: BigDecimal? = null,
+        @Json(name = "price_change_percentage_24h") val priceChangePercentage: BigDecimal? = null,
         @Json(name = "contract_address") val contractAddress: String? = null, // specific for token
         @Json(name = "decimal_count") val decimalCount: Int? = null, // specific for token
     )
