ab86077b7f546996f170cf665a0ed571d0b43a1c
Tangem <git@tangem.com>
2025-02-12 17:35:12 +0500

    Updated on 2025-09-15

--- core/datasource/src/main/java/com/tangem/datasource/local/promo/PromoStoriesStore.kt
+++ core/datasource/src/main/java/com/tangem/datasource/local/promo/PromoStoriesStore.kt
@@ -1,9 +1,12 @@
 package com.tangem.datasource.local.promo
 
 import com.tangem.datasource.api.promotion.models.StoryContentResponse
+import kotlinx.coroutines.flow.Flow
 
 interface PromoStoriesStore {
     suspend fun getSyncOrNull(storyId: String): StoryContentResponse?
+
+    fun get(storyId: String): Flow<StoryContentResponse?>
 
     suspend fun store(storyId: String, item: StoryContentResponse)
 }
