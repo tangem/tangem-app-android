13d0b574eb702a2b173a6a998778709d1f82147e
Tangem <git@tangem.com>
2025-02-12 18:03:25 +0300

    Updated on 2026-06-24

--- app/src/main/java/com/tangem/tap/di/domain/TxHistoryDomainModule.kt
+++ app/src/main/java/com/tangem/tap/di/domain/TxHistoryDomainModule.kt
@@ -1,6 +1,5 @@
 package com.tangem.tap.di.domain
 
-import com.tangem.domain.tokens.*
 import com.tangem.domain.txhistory.repository.TxHistoryRepository
 import com.tangem.domain.txhistory.usecase.GetExplorerTransactionUrlUseCase
 import com.tangem.domain.txhistory.usecase.GetFixedTxHistoryItemsUseCase
@@ -9,27 +8,23 @@ import com.tangem.domain.txhistory.usecase.GetTxHistoryItemsUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
-import dagger.hilt.android.scopes.ViewModelScoped
+import dagger.hilt.components.SingletonComponent
 
 @Module
-@InstallIn(ViewModelComponent::class)
+@InstallIn(SingletonComponent::class)
 internal object TxHistoryDomainModule {
 
     @Provides
-    @ViewModelScoped
     fun provideGetTxHistoryItemsCountUseCase(txHistoryRepository: TxHistoryRepository): GetTxHistoryItemsCountUseCase {
         return GetTxHistoryItemsCountUseCase(repository = txHistoryRepository)
     }
 
     @Provides
-    @ViewModelScoped
     fun provideGetTxHistoryItemsUseCase(txHistoryRepository: TxHistoryRepository): GetTxHistoryItemsUseCase {
         return GetTxHistoryItemsUseCase(repository = txHistoryRepository)
     }
 
     @Provides
-    @ViewModelScoped
     fun providesGetExplorerTransactionUrlUseCase(
         txHistoryRepository: TxHistoryRepository,
     ): GetExplorerTransactionUrlUseCase {
@@ -37,7 +32,6 @@ internal object TxHistoryDomainModule {
     }
 
     @Provides
-    @ViewModelScoped
     fun providesGetFixedTxHistoryItemsUseCase(txHistoryRepository: TxHistoryRepository): GetFixedTxHistoryItemsUseCase {
         return GetFixedTxHistoryItemsUseCase(repository = txHistoryRepository)
     }
