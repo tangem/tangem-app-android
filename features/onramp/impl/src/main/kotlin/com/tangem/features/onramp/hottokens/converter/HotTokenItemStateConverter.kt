ba2f6641c894985484e6061897d0e42a2c48a791
Tangem <git@tangem.com>
2025-02-12 11:46:40 +0400

    Updated on 2026-05-13

--- features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/hottokens/converter/HotTokenItemStateConverter.kt
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/hottokens/converter/HotTokenItemStateConverter.kt
@@ -10,6 +10,7 @@ import com.tangem.core.ui.format.bigdecimal.format
 import com.tangem.core.ui.format.bigdecimal.percent
 import com.tangem.domain.appcurrency.model.AppCurrency
 import com.tangem.domain.onramp.model.HotCryptoCurrency
+import com.tangem.domain.tokens.model.Quote
 import com.tangem.utils.converter.Converter
 import java.math.BigDecimal
 
@@ -29,7 +30,7 @@ internal class HotTokenItemStateConverter(
             id = value.cryptoCurrency.id.value,
             iconState = CryptoCurrencyToIconStateConverter().convert(value.cryptoCurrency),
             titleState = TokenItemState.TitleState.Content(text = stringReference(value.cryptoCurrency.name)),
-            subtitleState = value.getCryptoPriceState(appCurrency),
+            subtitleState = value.quote.getCryptoPriceState(appCurrency),
             fiatAmountState = null,
             subtitle2State = null,
             onItemClick = onItemClick.let { onItemClick -> { onItemClick(it, value) } },
@@ -37,12 +38,17 @@ internal class HotTokenItemStateConverter(
         )
     }
 
-    private fun HotCryptoCurrency.getCryptoPriceState(appCurrency: AppCurrency): TokenItemState.SubtitleState {
-        return TokenItemState.SubtitleState.CryptoPriceContent(
-            price = fiatRate.getFormattedCryptoPrice(appCurrency),
-            priceChangePercent = priceChange.format { percent() },
-            type = priceChange.getPriceChangeType(),
-        )
+    private fun Quote.getCryptoPriceState(appCurrency: AppCurrency): TokenItemState.SubtitleState {
+        return when (this) {
+            is Quote.Empty -> TokenItemState.SubtitleState.Unknown
+            is Quote.Value -> {
+                TokenItemState.SubtitleState.CryptoPriceContent(
+                    price = fiatRate.getFormattedCryptoPrice(appCurrency),
+                    priceChangePercent = priceChange.format { percent() },
+                    type = priceChange.getPriceChangeType(),
+                )
+            }
+        }
     }
 
     private fun BigDecimal.getFormattedCryptoPrice(appCurrency: AppCurrency): String {
