917f53a432689c4789d1faa878122cc252ea2c00
Tangem <git@tangem.com>
2025-02-13 17:13:08 +0700

    Updated on 2026-09-25

--- libs/blockchain-sdk/src/main/java/com/tangem/blockchainsdk/providers/ProviderTypeIdMapping.kt
+++ libs/blockchain-sdk/src/main/java/com/tangem/blockchainsdk/providers/ProviderTypeIdMapping.kt
@@ -23,5 +23,6 @@ internal enum class ProviderTypeIdMapping(val id: String, val providerType: Prov
     BittensorDwellir(id = "dwellirBittensor", providerType = ProviderType.Bittensor.Dwellir),
     BittensorOnfinality(id = "onfinalityBittensor", providerType = ProviderType.Bittensor.Onfinality),
     KoinosPro(id = "koinospro", providerType = ProviderType.Koinos.KoinosPro),
+    AlephiumTangem(id = "tangemAlephium", providerType = ProviderType.Alephium.Tangem),
     ;
 }
