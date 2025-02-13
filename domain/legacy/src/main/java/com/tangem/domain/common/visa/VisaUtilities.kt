05c97460684ac4cd42841c1187ab3978782a8876
Tangem <git@tangem.com>
2025-02-13 19:01:17 +0300

    release/5.26.1

--- domain/legacy/src/main/java/com/tangem/domain/common/visa/VisaUtilities.kt
+++ domain/legacy/src/main/java/com/tangem/domain/common/visa/VisaUtilities.kt
@@ -2,15 +2,12 @@ package com.tangem.domain.common.visa
 
 import com.tangem.blockchain.common.Blockchain
 import com.tangem.blockchain.common.derivation.DerivationStyle
-import com.tangem.common.card.EllipticCurve
 import com.tangem.common.card.FirmwareVersion
 import com.tangem.domain.models.scan.CardDTO
 
 private const val VISA_BATCH_START = "AE"
 
 object VisaUtilities {
-
-    val mandatoryCurve = EllipticCurve.Secp256k1
 
     const val tokenId = "tether"
 
