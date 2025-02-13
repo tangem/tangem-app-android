b04c2cf3c53f4f2a12386aa31b14a51881a271da
Tangem <git@tangem.com>
2025-02-13 15:11:36 +0300

    Updated on 2026-09-01

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/seedphrase/model/builder/GenerateSeedPhraseUiStateBuilder.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/seedphrase/model/builder/GenerateSeedPhraseUiStateBuilder.kt
@@ -1,19 +1,24 @@
 package com.tangem.features.onboarding.v2.multiwallet.impl.child.seedphrase.model.builder
 
 import com.tangem.crypto.bip39.Mnemonic
+import com.tangem.features.onboarding.v2.multiwallet.impl.child.seedphrase.model.GeneratedWordsType
 import com.tangem.features.onboarding.v2.multiwallet.impl.child.seedphrase.ui.state.MultiWalletSeedPhraseUM
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
 
 internal class GenerateSeedPhraseUiStateBuilder(
-    private val changeWords24Option: (Boolean) -> Unit,
+    private val changeGeneratedWordsType: (GeneratedWordsType) -> Unit,
     private val updateUiState: (
         (MultiWalletSeedPhraseUM.GenerateSeedPhrase) -> MultiWalletSeedPhraseUM.GenerateSeedPhrase,
     ) -> Unit,
     private val onContinue: () -> Unit,
 ) {
 
-    fun getState(generatedWords12: Mnemonic, generatedWords24: Mnemonic): MultiWalletSeedPhraseUM.GenerateSeedPhrase {
+    fun getState(
+        generatedWords12: Mnemonic,
+        generatedWords24: Mnemonic,
+        option: GeneratedWordsType,
+    ): MultiWalletSeedPhraseUM.GenerateSeedPhrase {
         val words12 = generatedWords12.mnemonicComponents.mapIndexed { index, s ->
             MultiWalletSeedPhraseUM.GenerateSeedPhrase.MnemonicGridItem(index + 1, s)
         }.toImmutableList()
@@ -22,26 +27,35 @@ internal class GenerateSeedPhraseUiStateBuilder(
         }.toImmutableList()
 
         return MultiWalletSeedPhraseUM.GenerateSeedPhrase(
-            words24OptionSelected = false,
-            words = words12,
-            onWords24OptionSwitch = { switchType(words12, words24) },
+            option = option,
+            words = when (option) {
+                GeneratedWordsType.Words12 -> words12
+                GeneratedWordsType.Words24 -> words24
+            },
+            onOptionChange = {
+                switchType(
+                    newType = it,
+                    generatedWords12 = words12,
+                    generatedWords24 = words24,
+                )
+            },
             onContinueClick = onContinue,
         )
     }
 
     private fun switchType(
+        newType: GeneratedWordsType,
         generatedWords12: ImmutableList<MultiWalletSeedPhraseUM.GenerateSeedPhrase.MnemonicGridItem>,
         generatedWords24: ImmutableList<MultiWalletSeedPhraseUM.GenerateSeedPhrase.MnemonicGridItem>,
     ) {
         updateUiState { uiSt ->
-            changeWords24Option(uiSt.words24OptionSelected.not())
+            changeGeneratedWordsType(newType)
 
             uiSt.copy(
-                words24OptionSelected = uiSt.words24OptionSelected.not(),
-                words = if (uiSt.words24OptionSelected) {
-                    generatedWords12
-                } else {
-                    generatedWords24
+                option = newType,
+                words = when (newType) {
+                    GeneratedWordsType.Words12 -> generatedWords12
+                    GeneratedWordsType.Words24 -> generatedWords24
                 },
             )
         }
