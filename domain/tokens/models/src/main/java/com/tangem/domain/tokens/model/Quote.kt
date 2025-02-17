59f2a79bd0138030fd8f09548138d80e0925088f
Tangem <git@tangem.com>
2025-02-17 12:43:00 +0400

    Updated on 2026-03-25

--- domain/tokens/models/src/main/java/com/tangem/domain/tokens/model/Quote.kt
+++ domain/tokens/models/src/main/java/com/tangem/domain/tokens/model/Quote.kt
@@ -19,10 +19,12 @@ sealed interface Quote {
      * @property rawCurrencyId The unique identifier of the cryptocurrency for which the financial information is provided.
      * @property fiatRate The current fiat exchange rate for the cryptocurrency.
      * @property priceChange The price change for the cryptocurrency.
+     * @property isCached flag that determines whether the quote is a cache
      */
     data class Value(
         override val rawCurrencyId: CryptoCurrency.RawID,
         val fiatRate: BigDecimal,
         val priceChange: BigDecimal,
+        val isCached: Boolean,
     ) : Quote
 }
