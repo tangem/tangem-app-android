86ed69102b70f7d3978460699b9381685e2f1b46
Tangem <git@tangem.com>
2025-02-17 12:43:00 +0400

    Updated on 2026-05-19

--- core/datasource/src/main/java/com/tangem/datasource/local/quote/QuotesStore.kt
+++ core/datasource/src/main/java/com/tangem/datasource/local/quote/QuotesStore.kt
@@ -5,11 +5,15 @@ import com.tangem.domain.tokens.model.CryptoCurrency
 import com.tangem.domain.tokens.model.Quote
 import kotlinx.coroutines.flow.Flow
 
+/** Quotes store */
 interface QuotesStore {
 
+    /** Get flow of quotes for [currenciesIds] */
     fun get(currenciesIds: Set<CryptoCurrency.RawID>): Flow<Set<Quote>>
 
+    /** Get quotes for [currenciesIds] synchronously */
     suspend fun getSync(currenciesIds: Set<CryptoCurrency.RawID>): Set<Quote>
 
+    /** Store [response] from remote */
     suspend fun store(response: QuotesResponse)
 }
