a1f19393f66dcad4e2a62cbe020b713aaffbc347
Tangem <git@tangem.com>
2025-02-17 14:50:26 +0400

    Updated on 2026-04-06

--- core/datasource/src/main/java/com/tangem/datasource/local/network/entity/NetworkStatusDM.kt
+++ core/datasource/src/main/java/com/tangem/datasource/local/network/entity/NetworkStatusDM.kt
@@ -1,27 +1,34 @@
 package com.tangem.datasource.local.network.entity
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import com.tangem.domain.tokens.model.Network
 import java.math.BigDecimal
 
-internal typealias NetworkStatusesDM = Map<String, Set<NetworkStatusDM>>
+internal typealias NetworkStatusesByWalletId = Map<String, Set<NetworkStatusDM>>
 
 @JsonClass(generateAdapter = true)
 internal data class NetworkStatusDM(
-    val networkId: Network.ID,
-    val selectedAddress: String,
-    val availableAddresses: Set<Address>,
-    val amounts: Map<String, BigDecimal>,
+    @Json(name = "network_id") val networkId: Network.ID,
+    @Json(name = "selected_address") val selectedAddress: String,
+    @Json(name = "available_addresses") val availableAddresses: Set<Address>,
+    @Json(name = "amounts") val amounts: Map<String, BigDecimal>,
 ) {
 
     @JsonClass(generateAdapter = true)
     data class Address(
-        val value: String,
-        val type: Type,
+        @Json(name = "value") val value: String,
+        @Json(name = "type") val type: Type,
     ) {
 
+        @JsonClass(generateAdapter = false)
         enum class Type {
-            Primary, Secondary,
+
+            @Json(name = "primary")
+            Primary,
+
+            @Json(name = "secondary")
+            Secondary,
         }
     }
 }
