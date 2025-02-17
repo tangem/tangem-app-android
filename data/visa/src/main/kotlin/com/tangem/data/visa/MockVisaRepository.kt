94580b0120a91284ff98d9387828e4b4416b76b3
Tangem <git@tangem.com>
2025-02-17 21:44:49 +0300

    Updated on 2026-06-27

--- data/visa/src/main/kotlin/com/tangem/data/visa/MockVisaRepository.kt
+++ data/visa/src/main/kotlin/com/tangem/data/visa/MockVisaRepository.kt
@@ -7,8 +7,9 @@ import com.tangem.domain.visa.model.VisaTxHistoryItem
 import com.tangem.domain.visa.repository.VisaRepository
 import com.tangem.domain.wallets.models.UserWalletId
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-internal class DummyVisaRepository : VisaRepository {
+internal class MockVisaRepository @Inject constructor() : VisaRepository {
 
     override suspend fun getVisaCurrency(userWalletId: UserWalletId, isRefresh: Boolean): VisaCurrency {
         TODO("Not implemented for this build type")
