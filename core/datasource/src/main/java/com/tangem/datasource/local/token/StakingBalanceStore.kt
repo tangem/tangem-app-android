c0772262b0af26a4c3a8a7e540af2c33a82af670
Tangem <git@tangem.com>
2025-02-18 20:34:37 +0400

    Updated on 2026-09-14

--- core/datasource/src/main/java/com/tangem/datasource/local/token/StakingBalanceStore.kt
+++ core/datasource/src/main/java/com/tangem/datasource/local/token/StakingBalanceStore.kt
@@ -1,24 +1,29 @@
 package com.tangem.datasource.local.token
 
 import com.tangem.datasource.api.stakekit.models.response.model.YieldBalanceWrapperDTO
+import com.tangem.domain.staking.model.stakekit.YieldBalance
+import com.tangem.domain.staking.model.stakekit.YieldBalanceList
 import com.tangem.domain.wallets.models.UserWalletId
 import kotlinx.coroutines.flow.Flow
 
+/** Staking balance store */
 interface StakingBalanceStore {
 
-    fun get(userWalletId: UserWalletId): Flow<Set<YieldBalanceWrapperDTO>>
+    /** Get flow of [YieldBalanceList] by [userWalletId] */
+    fun get(userWalletId: UserWalletId): Flow<Set<YieldBalance>>
 
-    suspend fun getSyncOrNull(userWalletId: UserWalletId): Set<YieldBalanceWrapperDTO>?
+    /** Get flow of [YieldBalance] by [userWalletId], [address] and [integrationId] */
+    fun get(userWalletId: UserWalletId, address: String, integrationId: String): Flow<YieldBalance?>
 
+    /** Get [YieldBalanceList] synchronously or null by [userWalletId] */
+    suspend fun getSyncOrNull(userWalletId: UserWalletId): Set<YieldBalance>?
+
+    /** Get [YieldBalance] synchronously or null by [userWalletId], [address] and [integrationId] */
+    suspend fun getSyncOrNull(userWalletId: UserWalletId, address: String, integrationId: String): YieldBalance?
+
+    /** Store [items] by [userWalletId] */
     suspend fun store(userWalletId: UserWalletId, items: Set<YieldBalanceWrapperDTO>)
 
-    fun get(userWalletId: UserWalletId, address: String, integrationId: String): Flow<YieldBalanceWrapperDTO?>
-
-    suspend fun getSyncOrNull(
-        userWalletId: UserWalletId,
-        address: String,
-        integrationId: String,
-    ): YieldBalanceWrapperDTO?
-
+    /** Store [item] by [userWalletId], [integrationId] and [address] */
     suspend fun store(userWalletId: UserWalletId, integrationId: String, address: String, item: YieldBalanceWrapperDTO)
 }
