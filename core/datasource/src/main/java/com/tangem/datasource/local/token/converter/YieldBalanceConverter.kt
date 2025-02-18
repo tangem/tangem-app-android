a385dc1885842c6363591426a99410f5756b50ed
Tangem <git@tangem.com>
2025-02-18 20:34:37 +0400

    Updated on 2026-09-01

--- core/datasource/src/main/java/com/tangem/datasource/local/token/converter/YieldBalanceConverter.kt
+++ core/datasource/src/main/java/com/tangem/datasource/local/token/converter/YieldBalanceConverter.kt
@@ -1,36 +1,36 @@
-package com.tangem.data.staking.converters
+package com.tangem.datasource.local.token.converter
 
-import com.tangem.data.staking.converters.action.PendingActionConverter
 import com.tangem.datasource.api.stakekit.models.response.model.YieldBalanceWrapperDTO
 import com.tangem.domain.staking.model.stakekit.BalanceItem
 import com.tangem.domain.staking.model.stakekit.YieldBalance
 import com.tangem.domain.staking.model.stakekit.YieldBalanceItem
 import com.tangem.utils.converter.Converter
 
-internal class YieldBalanceConverter : Converter<YieldBalanceWrapperDTO, YieldBalance> {
+internal class YieldBalanceConverter(private val isCached: Boolean) : Converter<YieldBalanceWrapperDTO, YieldBalance> {
 
-    private val pendingActionConverter by lazy(LazyThreadSafetyMode.NONE) { PendingActionConverter() }
-    private val networkTypeConverter by lazy(LazyThreadSafetyMode.NONE) { StakingNetworkTypeConverter() }
-    private val tokenConverter by lazy(LazyThreadSafetyMode.NONE) { TokenConverter(networkTypeConverter) }
-    private val balanceTypeConverter by lazy(LazyThreadSafetyMode.NONE) { BalanceTypeConverter() }
     override fun convert(value: YieldBalanceWrapperDTO): YieldBalance {
         return if (value.balances.isEmpty()) {
-            YieldBalance.Empty
+            YieldBalance.Empty(
+                integrationId = value.integrationId,
+                address = value.addresses.address,
+                isCached = isCached,
+            )
         } else {
             YieldBalance.Data(
+                integrationId = value.integrationId,
                 address = value.addresses.address,
                 balance = YieldBalanceItem(
                     items = value.balances.map { item ->
                         BalanceItem(
                             groupId = item.groupId,
-                            token = tokenConverter.convert(item.tokenDTO),
-                            type = balanceTypeConverter.convert(item.type),
+                            token = TokenConverter.convert(item.tokenDTO),
+                            type = BalanceTypeConverter.convert(item.type),
                             amount = item.amount,
                             rawCurrencyId = item.tokenDTO.coinGeckoId,
                             // tron-specific. operates validatorAddresses instead of validatorAddress
                             validatorAddress = item.validatorAddress ?: item.validatorAddresses?.get(0),
                             date = item.date?.toDateTime(),
-                            pendingActions = pendingActionConverter
+                            pendingActions = PendingActionConverter
                                 .convertList(item.pendingActions)
                                 .sortedBy { it.passthrough },
                             isPending = false,
@@ -39,6 +39,7 @@ internal class YieldBalanceConverter : Converter<YieldBalanceWrapperDTO, YieldBa
                         .sortedWith(compareBy({ it.type }, { it.amount })),
                     integrationId = value.integrationId,
                 ),
+                isCached = isCached,
             )
         }
     }
