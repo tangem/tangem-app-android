708fd802da34e981cc32073a96eb8688545367f2
Tangem <git@tangem.com>
2025-02-18 20:43:38 +0400

    Updated on 2026-07-23

--- core/datasource/src/main/java/com/tangem/datasource/local/network/entity/NetworkStatusDM.kt
+++ core/datasource/src/main/java/com/tangem/datasource/local/network/entity/NetworkStatusDM.kt
@@ -3,17 +3,31 @@ package com.tangem.datasource.local.network.entity
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import com.tangem.domain.tokens.model.Network
+import dev.onenowy.moshipolymorphicadapter.annotations.NameLabel
 import java.math.BigDecimal
 
-internal typealias NetworkStatusesByWalletId = Map<String, Set<NetworkStatusDM>>
+internal sealed interface NetworkStatusDM {
 
-@JsonClass(generateAdapter = true)
-internal data class NetworkStatusDM(
-    @Json(name = "network_id") val networkId: Network.ID,
-    @Json(name = "selected_address") val selectedAddress: String,
-    @Json(name = "available_addresses") val availableAddresses: Set<Address>,
-    @Json(name = "amounts") val amounts: Map<String, BigDecimal>,
-) {
+    val networkId: Network.ID
+    val selectedAddress: String
+    val availableAddresses: Set<Address>
+
+    @NameLabel("amounts")
+    data class Verified(
+        @Json(name = "network_id") override val networkId: Network.ID,
+        @Json(name = "selected_address") override val selectedAddress: String,
+        @Json(name = "available_addresses") override val availableAddresses: Set<Address>,
+        @Json(name = "amounts") val amounts: Map<String, BigDecimal>,
+    ) : NetworkStatusDM
+
+    @NameLabel("amount_to_create_account")
+    data class NoAccount(
+        @Json(name = "network_id") override val networkId: Network.ID,
+        @Json(name = "selected_address") override val selectedAddress: String,
+        @Json(name = "available_addresses") override val availableAddresses: Set<Address>,
+        @Json(name = "amount_to_create_account") val amountToCreateAccount: BigDecimal,
+        @Json(name = "error_message") val errorMessage: String,
+    ) : NetworkStatusDM
 
     @JsonClass(generateAdapter = true)
     data class Address(
