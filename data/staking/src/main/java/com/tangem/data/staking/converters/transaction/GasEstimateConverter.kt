6282af3ee235101a90b2d1072217e05ca803fff0
Tangem <git@tangem.com>
2025-02-18 20:34:37 +0400

    Updated on 2026-06-16

--- data/staking/src/main/java/com/tangem/data/staking/converters/transaction/GasEstimateConverter.kt
+++ data/staking/src/main/java/com/tangem/data/staking/converters/transaction/GasEstimateConverter.kt
@@ -1,18 +1,16 @@
 package com.tangem.data.staking.converters.transaction
 
-import com.tangem.data.staking.converters.TokenConverter
 import com.tangem.datasource.api.stakekit.models.response.model.transaction.StakingGasEstimateDTO
+import com.tangem.datasource.local.token.converter.TokenConverter
 import com.tangem.domain.staking.model.stakekit.transaction.StakingGasEstimate
 import com.tangem.utils.converter.Converter
 
-class GasEstimateConverter(
-    private val tokenConverter: TokenConverter,
-) : Converter<StakingGasEstimateDTO, StakingGasEstimate> {
+internal object GasEstimateConverter : Converter<StakingGasEstimateDTO, StakingGasEstimate> {
 
     override fun convert(value: StakingGasEstimateDTO): StakingGasEstimate {
         return StakingGasEstimate(
             amount = value.amount,
-            token = tokenConverter.convert(value.token),
+            token = TokenConverter.convert(value.token),
             gasLimit = value.gasLimit,
         )
     }
