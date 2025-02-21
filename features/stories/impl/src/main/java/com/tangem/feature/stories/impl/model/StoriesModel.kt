1dd055bd0e6624ff92e8527603083c244b522c80
Tangem <git@tangem.com>
2025-02-21 16:03:19 +0500

    Updated on 2026-09-14

--- features/stories/impl/src/main/java/com/tangem/feature/stories/impl/model/StoriesModel.kt
+++ features/stories/impl/src/main/java/com/tangem/feature/stories/impl/model/StoriesModel.kt
@@ -2,6 +2,7 @@ package com.tangem.feature.stories.impl.model
 
 import com.tangem.common.ui.swapStoriesScreen.SwapStoriesFactory
 import com.tangem.common.ui.swapStoriesScreen.SwapStoriesUM
+import com.tangem.core.analytics.api.AnalyticsEventHandler
 import com.tangem.core.decompose.model.Model
 import com.tangem.core.decompose.model.ParamsContainer
 import com.tangem.core.decompose.navigation.Router
@@ -9,6 +10,7 @@ import com.tangem.domain.promo.GetStoryContentUseCase
 import com.tangem.domain.promo.ShouldShowStoriesUseCase
 import com.tangem.domain.promo.models.StoryContentIds
 import com.tangem.feature.stories.api.StoriesComponent
+import com.tangem.feature.stories.impl.analytics.StoriesEvents
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -23,6 +25,7 @@ internal class StoriesModel @Inject constructor(
     override val dispatchers: CoroutineDispatcherProvider,
     private val getStoryContentUseCase: GetStoryContentUseCase,
     private val shouldShowStoriesUseCase: ShouldShowStoriesUseCase,
+    private val analyticsEventHandler: AnalyticsEventHandler,
 ) : Model() {
     val state: StateFlow<SwapStoriesUM> get() = _state
 
@@ -50,16 +53,36 @@ internal class StoriesModel @Inject constructor(
             getStoryContentUseCase.invokeSync(params.storyId).fold(
                 ifLeft = {
                     Timber.e("Unable to load stories for ${StoryContentIds.STORY_FIRST_TIME_SWAP.id}")
+                    analyticsEventHandler.send(
+                        StoriesEvents.Error(
+                            source = params.screenSource,
+                            type = StoryContentIds.STORY_FIRST_TIME_SWAP.analyticType,
+                        ),
+                    )
                     openScreen(hideStories = false) // Fallback to target screen
                 },
                 ifRight = { swapStory ->
                     if (swapStory == null) {
+                        analyticsEventHandler.send(
+                            StoriesEvents.Error(
+                                source = params.screenSource,
+                                type = StoryContentIds.STORY_FIRST_TIME_SWAP.analyticType,
+                            ),
+                        )
                         openScreen(hideStories = false) // Fallback to target screen
                     } else {
                         _state.update {
                             SwapStoriesFactory.createStoriesState(
                                 swapStory = swapStory,
-                                onStoriesClose = ::openScreen,
+                                onStoriesClose = { watchCount ->
+                                    analyticsEventHandler.send(
+                                        StoriesEvents.SwapStories(
+                                            source = params.screenSource,
+                                            watchCount = watchCount.toString(),
+                                        ),
+                                    )
+                                    openScreen()
+                                },
                             )
                         }
                     }
