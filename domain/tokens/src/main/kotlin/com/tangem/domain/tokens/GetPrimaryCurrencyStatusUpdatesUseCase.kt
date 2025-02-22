56664cce2cb6e12ebc0613270532d1eea21f515e
Tangem <git@tangem.com>
2025-02-22 21:33:23 +0400

    Updated on 2025-09-25

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/GetPrimaryCurrencyStatusUpdatesUseCase.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/GetPrimaryCurrencyStatusUpdatesUseCase.kt
@@ -1,14 +1,11 @@
 package com.tangem.domain.tokens
 
 import arrow.core.Either
-import com.tangem.domain.staking.repositories.StakingRepository
 import com.tangem.domain.tokens.error.CurrencyStatusError
 import com.tangem.domain.tokens.error.mapper.mapToCurrencyError
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
+import com.tangem.domain.tokens.operations.BaseCurrencyStatusOperations
 import com.tangem.domain.tokens.operations.CurrenciesStatusesOperations
-import com.tangem.domain.tokens.repository.CurrenciesRepository
-import com.tangem.domain.tokens.repository.NetworksRepository
-import com.tangem.domain.tokens.repository.QuotesRepository
 import com.tangem.domain.wallets.models.UserWalletId
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import kotlinx.coroutines.flow.*
@@ -16,17 +13,11 @@ import kotlinx.coroutines.flow.*
 /**
  * Use case for fetching the status of the primary cryptocurrency associated with a user wallet.
  *
- * @property currenciesRepository Repository for managing and fetching cryptocurrencies.
- * @property quotesRepository Repository for managing and fetching cryptocurrency quotes.
- * @property networksRepository Repository for managing and fetching information related to blockchain networks.
  * @property dispatchers Provides coroutine dispatchers.
  */
 class GetPrimaryCurrencyStatusUpdatesUseCase(
-    private val currenciesRepository: CurrenciesRepository,
-    private val quotesRepository: QuotesRepository,
-    private val networksRepository: NetworksRepository,
-    private val stakingRepository: StakingRepository,
     private val dispatchers: CoroutineDispatcherProvider,
+    private val currencyStatusOperations: BaseCurrencyStatusOperations,
 ) {
 
     /**
@@ -44,15 +35,7 @@ class GetPrimaryCurrencyStatusUpdatesUseCase(
     private suspend fun getPrimaryCurrency(
         userWalletId: UserWalletId,
     ): Flow<Either<CurrencyStatusError, CryptoCurrencyStatus>> {
-        val operations = CurrenciesStatusesOperations(
-            currenciesRepository = currenciesRepository,
-            quotesRepository = quotesRepository,
-            networksRepository = networksRepository,
-            stakingRepository = stakingRepository,
-            userWalletId = userWalletId,
-        )
-
-        return operations.getPrimaryCurrencyStatusFlow().map { maybeCurrency ->
+        return currencyStatusOperations.getPrimaryCurrencyStatusFlow(userWalletId).map { maybeCurrency ->
             maybeCurrency.mapLeft(CurrenciesStatusesOperations.Error::mapToCurrencyError)
         }
     }
