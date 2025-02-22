69805cd7b01dcbe51cf16bbad8e887fbe2388324
Tangem <git@tangem.com>
2025-02-22 21:33:23 +0400

    Updated on 2026-06-10

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/GetCryptoCurrencyStatusesSyncUseCase.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/GetCryptoCurrencyStatusesSyncUseCase.kt
@@ -1,35 +1,18 @@
 package com.tangem.domain.tokens
 
 import arrow.core.Either
-import com.tangem.domain.staking.repositories.StakingRepository
 import com.tangem.domain.tokens.error.TokenListError
 import com.tangem.domain.tokens.error.mapper.mapToTokenListError
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
-import com.tangem.domain.tokens.operations.CurrenciesStatusesOperations
-import com.tangem.domain.tokens.repository.CurrenciesRepository
-import com.tangem.domain.tokens.repository.NetworksRepository
-import com.tangem.domain.tokens.repository.QuotesRepository
+import com.tangem.domain.tokens.operations.BaseCurrencyStatusOperations
 import com.tangem.domain.wallets.models.UserWalletId
-import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 
 class GetCryptoCurrencyStatusesSyncUseCase(
-    internal val currenciesRepository: CurrenciesRepository,
-    internal val quotesRepository: QuotesRepository,
-    internal val networksRepository: NetworksRepository,
-    internal val stakingRepository: StakingRepository,
-    internal val dispatchers: CoroutineDispatcherProvider,
+    private val currencyStatusOperations: BaseCurrencyStatusOperations,
 ) {
 
     suspend operator fun invoke(userWalletId: UserWalletId): Either<TokenListError, List<CryptoCurrencyStatus>> {
-        val operations = CurrenciesStatusesOperations(
-            userWalletId = userWalletId,
-            currenciesRepository = currenciesRepository,
-            quotesRepository = quotesRepository,
-            networksRepository = networksRepository,
-            stakingRepository = stakingRepository,
-        )
-
-        return operations.getCurrenciesStatusesSync()
+        return currencyStatusOperations.getCurrenciesStatusesSync(userWalletId)
             .mapLeft { error -> error.mapToTokenListError() }
     }
 }
