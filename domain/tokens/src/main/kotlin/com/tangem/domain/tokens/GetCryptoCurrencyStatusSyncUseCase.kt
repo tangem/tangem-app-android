840ef262a77db4b7dc20618aeb0caafa40eae1e1
Tangem <git@tangem.com>
2025-02-22 21:33:23 +0400

    release/5.26.1

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/GetCryptoCurrencyStatusSyncUseCase.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/GetCryptoCurrencyStatusSyncUseCase.kt
@@ -1,24 +1,15 @@
 package com.tangem.domain.tokens
 
 import arrow.core.Either
-import com.tangem.domain.staking.repositories.StakingRepository
 import com.tangem.domain.tokens.error.CurrencyStatusError
 import com.tangem.domain.tokens.error.mapper.mapToCurrencyError
 import com.tangem.domain.tokens.model.CryptoCurrency
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
-import com.tangem.domain.tokens.operations.CurrenciesStatusesOperations
-import com.tangem.domain.tokens.repository.CurrenciesRepository
-import com.tangem.domain.tokens.repository.NetworksRepository
-import com.tangem.domain.tokens.repository.QuotesRepository
+import com.tangem.domain.tokens.operations.BaseCurrencyStatusOperations
 import com.tangem.domain.wallets.models.UserWalletId
-import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 
 class GetCryptoCurrencyStatusSyncUseCase(
-    internal val currenciesRepository: CurrenciesRepository,
-    internal val quotesRepository: QuotesRepository,
-    internal val networksRepository: NetworksRepository,
-    internal val stakingRepository: StakingRepository,
-    internal val dispatchers: CoroutineDispatcherProvider,
+    private val currencyStatusOperations: BaseCurrencyStatusOperations,
 ) {
 
     // multi-currency
@@ -27,29 +18,13 @@ class GetCryptoCurrencyStatusSyncUseCase(
         cryptoCurrencyId: CryptoCurrency.ID,
         isSingleWalletWithTokens: Boolean = false,
     ): Either<CurrencyStatusError, CryptoCurrencyStatus> {
-        val operations = CurrenciesStatusesOperations(
-            userWalletId = userWalletId,
-            currenciesRepository = currenciesRepository,
-            quotesRepository = quotesRepository,
-            networksRepository = networksRepository,
-            stakingRepository = stakingRepository,
-        )
-
-        return operations.getCurrencyStatusSync(cryptoCurrencyId, isSingleWalletWithTokens)
+        return currencyStatusOperations.getCurrencyStatusSync(userWalletId, cryptoCurrencyId, isSingleWalletWithTokens)
             .mapLeft { error -> error.mapToCurrencyError() }
     }
 
     // single-currency
     suspend operator fun invoke(userWalletId: UserWalletId): Either<CurrencyStatusError, CryptoCurrencyStatus> {
-        val operations = CurrenciesStatusesOperations(
-            userWalletId = userWalletId,
-            currenciesRepository = currenciesRepository,
-            quotesRepository = quotesRepository,
-            networksRepository = networksRepository,
-            stakingRepository = stakingRepository,
-        )
-
-        return operations.getPrimaryCurrencyStatusSync()
+        return currencyStatusOperations.getPrimaryCurrencyStatusSync(userWalletId)
             .mapLeft { error -> error.mapToCurrencyError() }
     }
 }
