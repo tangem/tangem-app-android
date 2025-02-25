d16d62bfb81715677573b1a449dd476b3f98298a
Tangem <git@tangem.com>
2025-02-25 13:21:07 +0300

    Updated on 2025-09-25

--- features/push-notifications/impl/src/main/java/com/tangem/features/pushnotifications/impl/navigation/InnerPushNotificationsRouter.kt
+++
@@ -1,8 +0,0 @@
-package com.tangem.features.pushnotifications.impl.navigation
-
-import com.tangem.features.pushnotifications.api.navigation.PushNotificationsRouter
-
-interface InnerPushNotificationsRouter : PushNotificationsRouter {
-
-    fun openHome()
-}
