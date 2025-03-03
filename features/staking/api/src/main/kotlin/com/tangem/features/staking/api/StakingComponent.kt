30d26454def230048659f45ddfc0aa2cc20f7127
Tangem <git@tangem.com>
2025-03-03 15:30:31 +0200

    Updated on 2026-06-01

--- features/staking/api/src/main/kotlin/com/tangem/features/staking/api/StakingComponent.kt
+++ features/staking/api/src/main/kotlin/com/tangem/features/staking/api/StakingComponent.kt
@@ -2,7 +2,6 @@ package com.tangem.features.staking.api
 
 import com.tangem.core.decompose.factory.ComponentFactory
 import com.tangem.core.ui.decompose.ComposableContentComponent
-import com.tangem.domain.staking.model.stakekit.Yield
 import com.tangem.domain.tokens.model.CryptoCurrency
 import com.tangem.domain.wallets.models.UserWalletId
 
@@ -11,7 +10,7 @@ interface StakingComponent : ComposableContentComponent {
     data class Params(
         val userWalletId: UserWalletId,
         val cryptoCurrencyId: CryptoCurrency.ID,
-        val yield: Yield,
+        val yieldId: String,
     )
 
     interface Factory : ComponentFactory<Params, StakingComponent>
