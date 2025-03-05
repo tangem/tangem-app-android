1977d0c7e81f398f3a9b64b3cb0d7dcbd629c1cf
Tangem <git@tangem.com>
2025-03-05 10:04:58 +0300

    Updated on 2026-09-01

--- app/src/main/java/com/tangem/tap/features/details/ui/cardsettings/CardSettingsFragment.kt
+++ app/src/main/java/com/tangem/tap/features/details/ui/cardsettings/CardSettingsFragment.kt
@@ -1,12 +1,16 @@
 package com.tangem.tap.features.details.ui.cardsettings
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.tangem.common.routing.AppRouter
 import com.tangem.core.ui.UiDependencies
 import com.tangem.core.ui.screen.ComposeFragment
+import com.tangem.tap.common.extensions.dispatchNavigationAction
+import com.tangem.tap.store
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -20,6 +24,8 @@ internal class CardSettingsFragment : ComposeFragment() {
 
     @Composable
     override fun ScreenContent(modifier: Modifier) {
+        BackHandler { store.dispatchNavigationAction(AppRouter::pop) }
+
         val state by viewModel.screenState.collectAsStateWithLifecycle()
 
         CardSettingsScreen(modifier = modifier, state = state)
