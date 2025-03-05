696b605eb6e9c9ab7a47a99f0e28bfa3a2d1f31b
Tangem <git@tangem.com>
2025-03-05 10:04:58 +0300

    Updated on 2026-08-19

--- app/src/main/java/com/tangem/tap/features/details/ui/appcurrency/AppCurrencySelectorFragment.kt
+++ app/src/main/java/com/tangem/tap/features/details/ui/appcurrency/AppCurrencySelectorFragment.kt
@@ -1,13 +1,17 @@
 package com.tangem.tap.features.details.ui.appcurrency
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.tangem.common.routing.AppRouter
 import com.tangem.core.ui.UiDependencies
 import com.tangem.core.ui.components.NavigationBar3ButtonsScrim
 import com.tangem.core.ui.screen.ComposeFragment
+import com.tangem.tap.common.extensions.dispatchNavigationAction
+import com.tangem.tap.store
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -21,6 +25,8 @@ internal class AppCurrencySelectorFragment : ComposeFragment() {
 
     @Composable
     override fun ScreenContent(modifier: Modifier) {
+        BackHandler { store.dispatchNavigationAction(AppRouter::pop) }
+
         val uiState by viewModel.uiState.collectAsStateWithLifecycle()
         NavigationBar3ButtonsScrim()
         AppCurrencySelectorScreen(
