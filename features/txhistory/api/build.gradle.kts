61ea435127991ff5bd4d1878543f48851019a3dc
Tangem <git@tangem.com>
2025-03-05 13:16:45 +0500

    Updated on 2025-11-26

---
+++ features/txhistory/api/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    alias(deps.plugins.android.library)
+    alias(deps.plugins.kotlin.android)
+    id("configuration")
+}
+
+android {
+    namespace = "com.tangem.features.txhistory.api"
+}
+
+dependencies {
+    /** Project - Core */
+    implementation(projects.core.ui)
+    implementation(projects.core.decompose)
+
+    /** Domain models */
+    implementation(projects.domain.tokens.models)
+    implementation(projects.domain.wallets.models)
+
+    /** Compose */
+    implementation(deps.compose.runtime)
+    implementation(deps.compose.foundation)
+
+    /** Other */
+    implementation(deps.kotlin.immutable.collections)
+}
