9f4b1c33c0a5c8d0743692212dccf0541cc5375f
Tangem <git@tangem.com>
2025-03-07 13:44:37 +0300

    Updated on 2026-09-25

---
+++ features/ask-biometry/impl/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    alias(deps.plugins.android.library)
+    alias(deps.plugins.kotlin.android)
+    alias(deps.plugins.kotlin.kapt)
+    alias(deps.plugins.kotlin.serialization)
+    alias(deps.plugins.hilt.android)
+    id("configuration")
+}
+
+android {
+    namespace = "com.tangem.features.askbiometry.impl"
+}
+
+dependencies {
+    api(projects.features.askBiometry.api)
+
+    /** Core modules */
+    implementation(projects.core.ui)
+    implementation(projects.core.res)
+    implementation(projects.core.decompose)
+    implementation(projects.core.analytics)
+    implementation(projects.core.analytics.models)
+    implementation(projects.core.configToggles)
+    implementation(projects.core.navigation)
+
+    /** Domain */
+    implementation(projects.domain.wallets)
+    implementation(projects.domain.wallets.models)
+    implementation(projects.domain.settings)
+    implementation(projects.domain.card)
+
+    /** Compose libraries */
+    implementation(deps.compose.material3)
+    implementation(deps.compose.animation)
+    implementation(deps.compose.foundation)
+    implementation(deps.compose.ui)
+    implementation(deps.compose.ui.tooling)
+
+    /** Tangem libraries */
+    implementation(projects.libs.tangemSdkApi)
+    implementation(tangemDeps.card.core)
+    implementation(tangemDeps.card.android) {
+        exclude(module = "joda-time")
+    }
+
+    /** Other */
+    implementation(deps.timber)
+
+    /** DI */
+    implementation(deps.hilt.android)
+    kapt(deps.hilt.kapt)
+}
