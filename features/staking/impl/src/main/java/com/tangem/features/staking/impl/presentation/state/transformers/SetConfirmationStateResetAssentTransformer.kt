d5172d5a269f945581afc151140490a512d829f6
Tangem <git@tangem.com>
2025-03-11 14:03:37 +0400

    Updated on 2026-09-21

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/transformers/SetConfirmationStateResetAssentTransformer.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/state/transformers/SetConfirmationStateResetAssentTransformer.kt
@@ -1,17 +1,22 @@
 package com.tangem.features.staking.impl.presentation.state.transformers
 
+import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.features.staking.impl.presentation.state.InnerConfirmationStakingState
 import com.tangem.features.staking.impl.presentation.state.StakingStates
 import com.tangem.features.staking.impl.presentation.state.StakingUiState
 import com.tangem.utils.transformer.Transformer
 
-internal object SetConfirmationStateResetAssentTransformer : Transformer<StakingUiState> {
+internal class SetConfirmationStateResetAssentTransformer(
+    private val cryptoCurrencyStatus: CryptoCurrencyStatus,
+) : Transformer<StakingUiState> {
     override fun transform(prevState: StakingUiState): StakingUiState {
         val confirmationState = prevState.confirmationState
         return prevState.copy(
             confirmationState = if (confirmationState is StakingStates.ConfirmationState.Data) {
                 confirmationState.copy(
-                    isPrimaryButtonEnabled = true,
+                    isPrimaryButtonEnabled = with(cryptoCurrencyStatus.value) {
+                        sources.yieldBalanceSource.isActual() && sources.networkSource.isActual()
+                    },
                     innerState = InnerConfirmationStakingState.ASSENT,
                 )
             } else {
