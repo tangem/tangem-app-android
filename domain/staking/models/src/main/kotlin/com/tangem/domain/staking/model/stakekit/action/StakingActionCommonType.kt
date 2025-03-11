68db72fc03caca508fe99c1c3cddabea9fc8a88c
Tangem <git@tangem.com>
2025-03-11 12:31:07 +0500

    Updated on 2026-05-13

--- domain/staking/models/src/main/kotlin/com/tangem/domain/staking/model/stakekit/action/StakingActionCommonType.kt
+++ domain/staking/models/src/main/kotlin/com/tangem/domain/staking/model/stakekit/action/StakingActionCommonType.kt
@@ -5,6 +5,7 @@ sealed class StakingActionCommonType {
     data class Enter(val skipEnterAmount: Boolean) : StakingActionCommonType()
     data class Exit(val partiallyUnstakeDisabled: Boolean) : StakingActionCommonType()
     sealed class Pending : StakingActionCommonType() {
+        data class Stake(val skipEnterAmount: Boolean) : Pending()
         data object Restake : Pending()
         data object Rewards : Pending()
         data object Other : Pending()
