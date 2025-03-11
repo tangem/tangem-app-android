5fcbc3c31e5ad2563ba01d066aea30da3dc3b7af
Tangem <git@tangem.com>
2025-03-11 15:39:32 +0500

    Updated on 2026-07-23

--- common/ui/src/main/java/com/tangem/common/ui/navigationButtons/NavigationButtonsState.kt
+++ common/ui/src/main/java/com/tangem/common/ui/navigationButtons/NavigationButtonsState.kt
@@ -16,6 +16,17 @@ sealed class NavigationButtonsState {
     ) : NavigationButtonsState()
 }
 
+/**
+ * @property textReference      text
+ * @property iconRes            icon resource id
+ * @property isSecondary        should set secondary color scheme
+ * @property isIconVisible      determines whether icon is visible
+ * @property showProgress       indicates progress state of button
+ * @property isEnabled          enabled
+ * @property isDimmed           determines whether the button content will be dimmed.
+ * This property will be ignored if [isEnabled] is `false`.
+ * @property onClick            lambda be invoked when action component is clicked
+ */
 data class NavigationButton(
     val textReference: TextReference,
     @DrawableRes val iconRes: Int? = null,
@@ -23,5 +34,6 @@ data class NavigationButton(
     val isIconVisible: Boolean = false,
     val showProgress: Boolean = false,
     val isEnabled: Boolean = true,
+    val isDimmed: Boolean = false,
     val onClick: () -> Unit,
 )
