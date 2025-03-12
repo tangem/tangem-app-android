d73d4ac1d62df94597156d8c3246c91cc6782f57
Tangem <git@tangem.com>
2025-03-12 13:20:39 +0500

    release/5.26.1

---
+++ core/datasource/src/main/java/com/tangem/datasource/local/nft/NFTRuntimeStoreFactory.kt
@@ -0,0 +1,16 @@
+package com.tangem.datasource.local.nft
+
+import com.tangem.datasource.local.datastore.RuntimeSharedStore
+import com.tangem.domain.tokens.model.Network
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class NFTRuntimeStoreFactory @Inject constructor() {
+
+    fun provide(network: Network): NFTRuntimeStore = DefaultNFTRuntimeStore(
+        network = network,
+        collectionsRuntimeStore = RuntimeSharedStore(),
+        pricesRuntimeStore = RuntimeSharedStore(),
+    )
+}
