5da1092b113098416f2963ec9c2f1be5a473bbcd
Tangem <git@tangem.com>
2025-03-13 14:37:46 +0500

    Updated on 2025-12-22

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/note/api/OnboardingNoteComponent.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/note/api/OnboardingNoteComponent.kt
@@ -1,11 +1,12 @@
 package com.tangem.features.onboarding.v2.note.api
 
 import com.tangem.core.decompose.factory.ComponentFactory
+import com.tangem.core.decompose.navigation.inner.InnerNavigationHolder
 import com.tangem.core.ui.decompose.ComposableContentComponent
 import com.tangem.domain.models.scan.ScanResponse
 import com.tangem.features.onboarding.v2.TitleProvider
 
-interface OnboardingNoteComponent : ComposableContentComponent {
+interface OnboardingNoteComponent : ComposableContentComponent, InnerNavigationHolder {
 
     data class Params(
         val titleProvider: TitleProvider,
