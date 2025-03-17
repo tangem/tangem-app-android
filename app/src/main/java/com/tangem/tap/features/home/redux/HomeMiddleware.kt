7f3ac376865172c7314b1c336028cb74ef2e327d
Tangem <git@tangem.com>
2025-03-17 13:06:30 +0400

    Updated on 2026-09-21

--- app/src/main/java/com/tangem/tap/features/home/redux/HomeMiddleware.kt
+++ app/src/main/java/com/tangem/tap/features/home/redux/HomeMiddleware.kt
@@ -10,7 +10,6 @@ import com.tangem.core.analytics.models.AnalyticsParam
 import com.tangem.core.analytics.models.Basic
 import com.tangem.domain.common.util.cardTypesResolver
 import com.tangem.domain.models.scan.ScanResponse
-import com.tangem.domain.wallets.builder.UserWalletBuilder
 import com.tangem.tap.common.analytics.converters.ParamCardCurrencyConverter
 import com.tangem.tap.common.analytics.events.IntroductionProcess
 import com.tangem.tap.common.extensions.dispatchNavigationAction
@@ -91,8 +90,9 @@ private suspend fun readCard() {
 }
 
 private fun proceedWithScanResponse(scanResponse: ScanResponse) = scope.launch {
-    val walletNameGenerateUseCase = store.inject(DaggerGraphState::generateWalletNameUseCase)
-    val userWallet = UserWalletBuilder(scanResponse, walletNameGenerateUseCase).build().guard {
+    val userWalletBuilder = store.inject(DaggerGraphState::userWalletBuilderFactory).create(scanResponse)
+
+    val userWallet = userWalletBuilder.build().guard {
         Timber.e("User wallet not created")
         return@launch
     }
