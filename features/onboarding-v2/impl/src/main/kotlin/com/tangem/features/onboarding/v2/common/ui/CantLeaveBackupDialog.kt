0fefdf06fd49ad5d0a0f1af0a69ea712b8a6cdbd
Tangem <git@tangem.com>
2025-03-18 13:42:10 +0300

    Updated on 2026-07-23

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/common/ui/CantLeaveBackupDialog.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/common/ui/CantLeaveBackupDialog.kt
@@ -1,11 +1,11 @@
-package com.tangem.features.onboarding.v2.multiwallet.impl.child.finalize.model
+package com.tangem.features.onboarding.v2.common.ui
 
 import com.tangem.core.ui.extensions.resourceReference
 import com.tangem.core.ui.message.DialogMessage
 import com.tangem.core.ui.message.EventMessageAction
 import com.tangem.features.onboarding.v2.impl.R
 
-internal val CantLeaveFinalizeDialog: DialogMessage = DialogMessage(
+internal val CantLeaveBackupDialog: DialogMessage = DialogMessage(
     message = resourceReference(R.string.onboarding_backup_exit_warning),
     firstAction = EventMessageAction(
         title = resourceReference(R.string.warning_button_ok),
