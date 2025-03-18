749105165b477f67053263c35d55d042477b6fc7
Tangem <git@tangem.com>
2025-03-18 15:14:57 +0500

    Updated on 2025-12-05

--- features/send-v2/impl/src/main/java/com/tangem/features/send/v2/send/model/SendModel.kt
+++ features/send-v2/impl/src/main/java/com/tangem/features/send/v2/send/model/SendModel.kt
@@ -4,9 +4,11 @@ import androidx.compose.runtime.Stable
 import com.tangem.common.ui.amountScreen.models.AmountState
 import com.tangem.core.decompose.di.ModelScoped
 import com.tangem.core.decompose.model.Model
+import com.tangem.domain.appcurrency.model.AppCurrency
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.domain.wallets.models.UserWallet
 import com.tangem.features.send.v2.send.ui.state.SendUM
+import com.tangem.features.send.v2.subcomponents.amount.SendAmountComponent
 import com.tangem.features.send.v2.subcomponents.destination.SendDestinationComponent
 import com.tangem.features.send.v2.subcomponents.destination.ui.state.DestinationUM
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
@@ -21,20 +23,28 @@ import kotlin.properties.Delegates
 @Suppress("LongParameterList")
 internal class SendModel @Inject constructor(
     override val dispatchers: CoroutineDispatcherProvider,
-) : Model(), SendDestinationComponent.ModelCallback {
+) : Model(),
+    SendDestinationComponent.ModelCallback,
+    SendAmountComponent.ModelCallback {
 
     private val _uiState = MutableStateFlow(initialState())
     val uiState = _uiState.asStateFlow()
 
     var userWallet: UserWallet by Delegates.notNull()
     var cryptoCurrencyStatus: CryptoCurrencyStatus by Delegates.notNull()
-
-    private fun initialState(): SendUM = SendUM(
-        amountState = AmountState.Empty(),
-        destinationUM = DestinationUM.Empty(),
-    )
+    var appCurrency: AppCurrency = AppCurrency.Default
+    var predefinedAmountValue: String? = null
 
     override fun onDestinationResult(destinationUM: DestinationUM) {
         _uiState.update { it.copy(destinationUM = destinationUM) }
     }
+
+    override fun onAmountResult(state: AmountState) {
+        _uiState.update { it.copy(amountUM = state) }
+    }
+
+    private fun initialState(): SendUM = SendUM(
+        amountUM = AmountState.Empty(),
+        destinationUM = DestinationUM.Empty(),
+    )
 }
