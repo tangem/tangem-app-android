1e92a962a7032a010f08088cb4d1bc2f58849417
Tangem <git@tangem.com>
2025-03-18 13:37:16 +0500

    release/5.26.1

--- features/send-v2/impl/src/main/java/com/tangem/features/send/v2/DefaultSendFeatureToggles.kt
+++ features/send-v2/impl/src/main/java/com/tangem/features/send/v2/DefaultSendFeatureToggles.kt
@@ -3,7 +3,7 @@ package com.tangem.features.send.v2
 import com.tangem.core.configtoggle.feature.FeatureTogglesManager
 import com.tangem.features.send.v2.api.SendFeatureToggles
 
-class DefaultSendFeatureToggles(
+internal class DefaultSendFeatureToggles(
     private val featureToggles: FeatureTogglesManager,
 ) : SendFeatureToggles {
     override val isSendV2Enabled: Boolean
---
+++ features/send-v2/impl/src/main/java/com/tangem/features/send/v2/send/model/SendModel.kt
@@ -0,0 +1,40 @@
+package com.tangem.features.send.v2.send.model
+
+import androidx.compose.runtime.Stable
+import com.tangem.common.ui.amountScreen.models.AmountState
+import com.tangem.core.decompose.di.ModelScoped
+import com.tangem.core.decompose.model.Model
+import com.tangem.domain.tokens.model.CryptoCurrencyStatus
+import com.tangem.domain.wallets.models.UserWallet
+import com.tangem.features.send.v2.send.ui.state.SendUM
+import com.tangem.features.send.v2.subcomponents.destination.SendDestinationComponent
+import com.tangem.features.send.v2.subcomponents.destination.ui.state.DestinationUM
+import com.tangem.utils.coroutines.CoroutineDispatcherProvider
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+import kotlin.properties.Delegates
+
+@Stable
+@ModelScoped
+@Suppress("LongParameterList")
+internal class SendModel @Inject constructor(
+    override val dispatchers: CoroutineDispatcherProvider,
+) : Model(), SendDestinationComponent.ModelCallback {
+
+    private val _uiState = MutableStateFlow(initialState())
+    val uiState = _uiState.asStateFlow()
+
+    var userWallet: UserWallet by Delegates.notNull()
+    var cryptoCurrencyStatus: CryptoCurrencyStatus by Delegates.notNull()
+
+    private fun initialState(): SendUM = SendUM(
+        amountState = AmountState.Empty(),
+        destinationUM = DestinationUM.Empty(),
+    )
+
+    override fun onDestinationResult(destinationUM: DestinationUM) {
+        _uiState.update { it.copy(destinationUM = destinationUM) }
+    }
+}
