8f321989032a173a53ca9c7d07ab178b06177ee4
Tangem <git@tangem.com>
2025-03-18 15:22:15 +0300

    release/5.26.1

---
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/twin/impl/model/OnboardingTwinModel.kt
@@ -0,0 +1,13 @@
+package com.tangem.features.onboarding.v2.twin.impl.model
+
+import com.tangem.core.decompose.di.ModelScoped
+import com.tangem.core.decompose.model.Model
+import com.tangem.utils.coroutines.CoroutineDispatcherProvider
+import javax.inject.Inject
+
+@ModelScoped
+class OnboardingTwinModel @Inject constructor(
+    override val dispatchers: CoroutineDispatcherProvider,
+) : Model() {
+// [REDACTED_TODO_COMMENT]
+}
