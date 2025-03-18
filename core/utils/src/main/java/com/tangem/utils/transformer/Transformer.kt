c2c00e19ea4b126c4d0e638e913e569bc80dcc54
Tangem <git@tangem.com>
2025-03-18 13:35:51 +0500

    Updated on 2026-08-07

--- core/utils/src/main/java/com/tangem/utils/transformer/Transformer.kt
+++ core/utils/src/main/java/com/tangem/utils/transformer/Transformer.kt
@@ -1,8 +1,19 @@
 package com.tangem.utils.transformer
 
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+
 /**
  * Transforms state to updated state.
  */
 interface Transformer<S> {
     fun transform(prevState: S): S
+}
+
+fun <T> MutableStateFlow<T>.update(transformer: Transformer<T>) {
+    update { transformer.transform(this.value) }
+}
+
+fun <T> MutableStateFlow<T>.updateAll(vararg transformers: Transformer<T>) {
+    transformers.forEach { transformer -> update { transformer.transform(this.value) } }
 }
