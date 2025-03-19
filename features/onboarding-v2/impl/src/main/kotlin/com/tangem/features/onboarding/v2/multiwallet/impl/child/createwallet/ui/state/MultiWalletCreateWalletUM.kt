56a84f9cca134ae3917caa02ddc64bfab708d4ec
Tangem <git@tangem.com>
2025-03-19 15:01:00 +0500

    Updated on 2025-10-09

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/createwallet/ui/state/MultiWalletCreateWalletUM.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/child/createwallet/ui/state/MultiWalletCreateWalletUM.kt
@@ -1,7 +1,7 @@
 package com.tangem.features.onboarding.v2.multiwallet.impl.child.createwallet.ui.state
 
 import com.tangem.core.ui.extensions.TextReference
-import com.tangem.features.onboarding.v2.multiwallet.impl.common.ui.OnboardingDialogUM
+import com.tangem.features.onboarding.v2.common.ui.OnboardingDialogUM
 
 internal data class MultiWalletCreateWalletUM(
     val title: TextReference,
