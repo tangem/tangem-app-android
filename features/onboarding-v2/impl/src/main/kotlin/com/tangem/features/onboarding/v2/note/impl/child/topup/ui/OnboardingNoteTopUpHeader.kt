4e462dba9ad80bc4be10bc029ee55b053ec2192d
Tangem <git@tangem.com>
2025-03-19 12:33:00 +0300

    Updated on 2025-12-22

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/note/impl/child/topup/ui/OnboardingNoteTopUpHeader.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/note/impl/child/topup/ui/OnboardingNoteTopUpHeader.kt
@@ -1,20 +1,11 @@
 package com.tangem.features.onboarding.v2.note.impl.child.topup.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,11 +14,12 @@ import com.tangem.core.ui.components.SpacerHMax
 import com.tangem.core.ui.extensions.stringResourceSafe
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.core.ui.res.TangemThemePreview
+import com.tangem.features.onboarding.v2.common.ui.RefreshButton
 import com.tangem.features.onboarding.v2.common.ui.WalletCard
 import com.tangem.features.onboarding.v2.impl.R
 
 @Composable
-fun OnboardinNoteTopUpHeader(
+fun OnboardingNoteTopUpHeader(
     balance: String?,
     cardArtworkUrl: String?,
     isRefreshing: Boolean,
@@ -78,44 +70,11 @@ fun OnboardinNoteTopUpHeader(
     }
 }
 
-@Composable
-private fun RefreshButton(isRefreshing: Boolean, onRefreshBalanceClick: () -> Unit, modifier: Modifier = Modifier) {
-    Box(
-        modifier = modifier
-            .size(TangemTheme.dimens.size48)
-            .shadow(elevation = 2.dp, shape = CircleShape)
-            .background(TangemTheme.colors.background.action)
-            .clickable(
-                enabled = !isRefreshing,
-                onClick = onRefreshBalanceClick,
-                indication = ripple(),
-                interactionSource = remember { MutableInteractionSource() },
-            ),
-    ) {
-        if (isRefreshing) {
-            Box {
-                CircularProgressIndicator(
-                    modifier = Modifier.padding(TangemTheme.dimens.spacing12),
-                    color = TangemTheme.colors.icon.primary1,
-                    strokeWidth = TangemTheme.dimens.size2,
-                )
-            }
-        } else {
-            Icon(
-                painter = painterResource(id = R.drawable.ic_refresh_24),
-                contentDescription = null,
-                tint = TangemTheme.colors.text.disabled,
-                modifier = Modifier.padding(TangemTheme.dimens.spacing12),
-            )
-        }
-    }
-}
-
 @Preview(showBackground = true)
 @Composable
 private fun OnboardinNoteTopUpHeaderPreview() {
     TangemThemePreview {
-        OnboardinNoteTopUpHeader(
+        OnboardingNoteTopUpHeader(
             balance = "0.00000001 BTC",
             cardArtworkUrl = "",
             onRefreshBalanceClick = {},
