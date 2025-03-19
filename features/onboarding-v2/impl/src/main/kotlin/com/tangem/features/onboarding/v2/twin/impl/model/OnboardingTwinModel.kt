10f760a6b764e00c2e68cd88cca9b976cb655e86
Tangem <git@tangem.com>
2025-03-19 12:33:00 +0300

    Updated on 2026-07-01

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/twin/impl/model/OnboardingTwinModel.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/twin/impl/model/OnboardingTwinModel.kt
@@ -2,12 +2,18 @@ package com.tangem.features.onboarding.v2.twin.impl.model
 
 import com.tangem.core.decompose.di.ModelScoped
 import com.tangem.core.decompose.model.Model
+import com.tangem.core.decompose.model.ParamsContainer
+import com.tangem.features.onboarding.v2.twin.api.OnboardingTwinComponent
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import javax.inject.Inject
 
 @ModelScoped
-class OnboardingTwinModel @Inject constructor(
+internal class OnboardingTwinModel @Inject constructor(
     override val dispatchers: CoroutineDispatcherProvider,
+    paramsContainer: ParamsContainer,
 ) : Model() {
+
+    val params = paramsContainer.require<OnboardingTwinComponent.Params>()
+
     // TODO
 }
