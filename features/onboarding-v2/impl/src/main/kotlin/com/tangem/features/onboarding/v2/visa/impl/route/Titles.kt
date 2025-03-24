508fcf4c7c02e57429241b7f5f1297b727500ae0
Tangem <git@tangem.com>
2025-03-24 11:20:28 +0300

    Updated on 2026-08-07

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/visa/impl/route/Titles.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/visa/impl/route/Titles.kt
@@ -1,16 +1,22 @@
 package com.tangem.features.onboarding.v2.visa.impl.route
 
 import com.tangem.core.ui.extensions.TextReference
+import com.tangem.core.ui.extensions.resourceReference
+import com.tangem.features.onboarding.v2.impl.R
 
-// TODO add translations [REDACTED_TASK_KEY]
 internal fun OnboardingVisaRoute.screenTitle(): TextReference = when (this) {
-    OnboardingVisaRoute.AccessCode -> TextReference.Str("Access code")
-    is OnboardingVisaRoute.ChooseWallet -> TextReference.Str("Account activation")
-    is OnboardingVisaRoute.InProgress -> TextReference.Str("In progress")
-    is OnboardingVisaRoute.OtherWalletApproveOption -> TextReference.Str("Wallet connection")
-    is OnboardingVisaRoute.PinCode -> TextReference.Str("PIN code")
-    is OnboardingVisaRoute.TangemWalletApproveOption -> TextReference.Str("Wallet connection")
+    OnboardingVisaRoute.AccessCode ->
+        resourceReference(R.string.visa_onboarding_access_code_navigation_title)
+    is OnboardingVisaRoute.ChooseWallet ->
+        resourceReference(R.string.visa_onboarding_account_activation_navigation_title)
+    is OnboardingVisaRoute.InProgress ->
+        resourceReference(R.string.common_in_progress)
+    is OnboardingVisaRoute.OtherWalletApproveOption,
+    is OnboardingVisaRoute.TangemWalletApproveOption,
+    -> resourceReference(R.string.visa_onboarding_wallet_connection_navigation_title)
+    is OnboardingVisaRoute.PinCode ->
+        resourceReference(R.string.visa_onboarding_pin_code_navigation_title)
     is OnboardingVisaRoute.Welcome,
     is OnboardingVisaRoute.WelcomeBack,
-    -> TextReference.Str("Getting started")
+    -> resourceReference(R.string.onboarding_getting_started)
 }
