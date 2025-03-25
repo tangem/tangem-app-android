e809fbb688a3a5d3965f51ef48261d8f36676e46
Tangem <git@tangem.com>
2025-03-25 19:46:33 +0200

    Updated on 2026-04-08

--- core/analytics/models/src/main/java/com/tangem/core/analytics/models/ExceptionAnalyticsEvent.kt
+++ core/analytics/models/src/main/java/com/tangem/core/analytics/models/ExceptionAnalyticsEvent.kt
@@ -2,7 +2,5 @@ package com.tangem.core.analytics.models
 
 class ExceptionAnalyticsEvent(
     val exception: Throwable,
-    // val category: String,
-    // val event: String,
-    var params: Map<String, String> = mapOf(),
+    val params: Map<String, String> = mapOf(),
 )
