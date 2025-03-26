80d00dfb6bd38a4188f6307422367c2440f41986
Tangem <git@tangem.com>
2025-03-26 22:17:53 +0500

    Updated on 2026-08-14

--- core/ui/src/main/java/com/tangem/core/ui/components/UnableToLoadData.kt
+++ core/ui/src/main/java/com/tangem/core/ui/components/UnableToLoadData.kt
@@ -1,4 +1,4 @@
-package com.tangem.features.markets.tokenlist.impl.ui.components
+package com.tangem.core.ui.components
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
@@ -8,16 +8,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.tangem.core.ui.R
 import com.tangem.core.ui.components.buttons.SecondarySmallButton
 import com.tangem.core.ui.components.buttons.SmallButtonConfig
 import com.tangem.core.ui.extensions.resourceReference
 import com.tangem.core.ui.extensions.stringResourceSafe
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.core.ui.res.TangemThemePreview
-import com.tangem.features.markets.impl.R
 
 @Composable
-internal fun UnableToLoadData(onRetryClick: () -> Unit, modifier: Modifier = Modifier) {
+fun UnableToLoadData(onRetryClick: () -> Unit, modifier: Modifier = Modifier) {
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(TangemTheme.dimens.spacing12),
