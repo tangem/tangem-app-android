9792a1d24f543c3dac2b7bdac277c3a718de51be
Tangem <git@tangem.com>
2025-03-27 11:08:57 +0500

    Updated on 2026-02-02

---
+++ features/send-v2/impl/src/main/java/com/tangem/features/send/v2/subcomponents/fee/model/converters/custom/CustomFeeConverter.kt
@@ -0,0 +1,14 @@
+package com.tangem.features.send.v2.subcomponents.fee.model.converters.custom
+
+import com.tangem.blockchain.common.transaction.Fee
+import com.tangem.features.send.v2.subcomponents.fee.ui.state.CustomFeeFieldUM
+import com.tangem.utils.converter.Converter
+import kotlinx.collections.immutable.ImmutableList
+
+internal interface CustomFeeConverter<T : Fee> : Converter<T, ImmutableList<CustomFeeFieldUM>> {
+    fun convertBack(normalFee: T, value: ImmutableList<CustomFeeFieldUM>): T
+}
+
+internal fun MutableList<CustomFeeFieldUM>.setEmpty(index: Int) {
+    set(index, this[index].copy(value = ""))
+}
