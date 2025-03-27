e9fac0e50eb8816cc24e00dffa94d7baf2ca18c8
Tangem <git@tangem.com>
2025-03-27 21:21:17 +0400

    Updated on 2026-08-14

---
+++ common/test/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    alias(deps.plugins.android.library)
+    alias(deps.plugins.kotlin.android)
+    id("configuration")
+}
+
+android {
+    namespace = "com.tangem.common.test"
+}
+
+dependencies {
+    implementation(projects.data.common)
+
+    implementation(projects.domain.legacy)
+    implementation(projects.domain.models)
+    implementation(projects.domain.tokens.models)
+
+    implementation(projects.libs.blockchainSdk)
+
+    implementation(deps.androidx.datastore)
+    implementation(deps.test.coroutine)
+
+    implementation(tangemDeps.blockchain)
+    implementation(tangemDeps.card.core)
+}
