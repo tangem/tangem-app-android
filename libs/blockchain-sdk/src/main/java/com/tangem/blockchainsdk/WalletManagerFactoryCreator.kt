755ffc6007f97acd7def4cd4dc3c4de60d106b43
Tangem <git@tangem.com>
2025-03-27 07:24:44 -0700

    Updated on 2026-06-24

--- libs/blockchain-sdk/src/main/java/com/tangem/blockchainsdk/WalletManagerFactoryCreator.kt
+++ libs/blockchain-sdk/src/main/java/com/tangem/blockchainsdk/WalletManagerFactoryCreator.kt
@@ -6,7 +6,6 @@ import com.tangem.blockchain.common.BlockchainSdkConfig
 import com.tangem.blockchain.common.WalletManagerFactory
 import com.tangem.blockchain.common.datastorage.BlockchainDataStorage
 import com.tangem.blockchain.common.logging.BlockchainSDKLogger
-import com.tangem.blockchainsdk.featuretoggles.BlockchainSDKFeatureToggles
 import com.tangem.blockchainsdk.providers.BlockchainProviderTypes
 import timber.log.Timber
 import javax.inject.Inject
@@ -24,7 +23,6 @@ internal class WalletManagerFactoryCreator @Inject constructor(
     private val accountCreator: AccountCreator,
     private val blockchainDataStorage: BlockchainDataStorage,
     private val blockchainSDKLogger: BlockchainSDKLogger,
-    private val blockchainSDKFeatureToggles: BlockchainSDKFeatureToggles,
 ) {
 
     fun create(config: BlockchainSdkConfig, blockchainProviderTypes: BlockchainProviderTypes): WalletManagerFactory {
@@ -35,7 +33,7 @@ internal class WalletManagerFactoryCreator @Inject constructor(
             blockchainProviderTypes = blockchainProviderTypes,
             accountCreator = accountCreator,
             featureToggles = BlockchainFeatureToggles(
-                isEthereumEIP1559Enabled = blockchainSDKFeatureToggles.isEthereumEIP1559Enabled,
+                isEthereumEIP1559Enabled = true,
             ),
             blockchainDataStorage = blockchainDataStorage,
             loggers = listOf(blockchainSDKLogger),
