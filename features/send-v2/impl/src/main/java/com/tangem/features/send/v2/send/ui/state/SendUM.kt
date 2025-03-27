e2d0b71f2c27acf87fbf824aacd5832faf2f3e1c
Tangem <git@tangem.com>
2025-03-27 11:08:57 +0500

    Updated on 2026-02-11

--- features/send-v2/impl/src/main/java/com/tangem/features/send/v2/send/ui/state/SendUM.kt
+++ features/send-v2/impl/src/main/java/com/tangem/features/send/v2/send/ui/state/SendUM.kt
@@ -2,8 +2,10 @@ package com.tangem.features.send.v2.send.ui.state
 
 import com.tangem.common.ui.amountScreen.models.AmountState
 import com.tangem.features.send.v2.subcomponents.destination.ui.state.DestinationUM
+import com.tangem.features.send.v2.subcomponents.fee.ui.state.FeeUM
 
 internal data class SendUM(
     val amountUM: AmountState,
     val destinationUM: DestinationUM,
+    val feeUM: FeeUM,
 )
