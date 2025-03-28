1d80da67f83d36b7ff6b30c1e26d05cbc71976a7
Tangem <git@tangem.com>
2025-03-28 18:17:56 +0500

    Updated on 2026-09-25

--- common/ui/src/main/java/com/tangem/common/ui/notifications/NotificationsFactory.kt
+++ common/ui/src/main/java/com/tangem/common/ui/notifications/NotificationsFactory.kt
@@ -16,6 +16,8 @@ import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.domain.tokens.model.warnings.CryptoCurrencyCheck
 import com.tangem.domain.tokens.model.warnings.CryptoCurrencyWarning
 import com.tangem.domain.transaction.error.GetFeeError
+import com.tangem.lib.crypto.BlockchainUtils.getTezosThreshold
+import com.tangem.lib.crypto.BlockchainUtils.isTezos
 import com.tangem.utils.extensions.isZero
 import com.tangem.utils.extensions.orZero
 import java.math.BigDecimal
@@ -240,6 +242,31 @@ object NotificationsFactory {
         }
     }
 
+    fun MutableList<NotificationUM>.addFeeCoverageNotification(
+        isFeeCoverage: Boolean,
+        enteredAmountValue: BigDecimal,
+        sendingValue: BigDecimal,
+        appCurrency: AppCurrency,
+        cryptoCurrencyStatus: CryptoCurrencyStatus,
+    ) {
+        val cryptoCurrency = cryptoCurrencyStatus.currency
+        val fiatRate = cryptoCurrencyStatus.value.fiatRate
+
+        val cryptoDiff = enteredAmountValue.minus(sendingValue)
+        if (isFeeCoverage) {
+            add(
+                NotificationUM.Warning.FeeCoverageNotification(
+                    cryptoAmount = cryptoDiff.format { crypto(cryptoCurrency).uncapped() },
+                    fiatAmount = getFiatString(
+                        value = cryptoDiff,
+                        rate = fiatRate,
+                        appCurrency = appCurrency,
+                    ),
+                ),
+            )
+        }
+    }
+
     fun MutableList<NotificationUM>.addDustWarningNotification(
         dustValue: BigDecimal?,
         feeValue: BigDecimal,
@@ -423,6 +450,41 @@ object NotificationsFactory {
     fun MutableList<NotificationUM>.addRentExemptionNotification(rentWarning: CryptoCurrencyWarning.Rent?) {
         if (rentWarning == null) return
         add(NotificationUM.Solana.RentInfo(rentWarning))
+    }
+
+    fun MutableList<NotificationUM>.addHighFeeWarningNotification(
+        enteredAmountValue: BigDecimal,
+        cryptoCurrencyStatus: CryptoCurrencyStatus,
+        ignoreAmountReduce: Boolean,
+        onReduceClick: (
+            reduceAmountBy: BigDecimal,
+            reduceAmountByDiff: BigDecimal,
+            notification: Class<out NotificationUM>,
+        ) -> Unit,
+        onCloseClick: (Class<out NotificationUM>) -> Unit,
+    ) {
+        val balance = cryptoCurrencyStatus.value.amount ?: BigDecimal.ZERO
+        val isTezos = isTezos(cryptoCurrencyStatus.currency.network.id.value)
+        val threshold = getTezosThreshold()
+        val isTotalBalance = enteredAmountValue >= balance && balance > threshold
+        if (!ignoreAmountReduce && isTotalBalance && isTezos) {
+            add(
+                NotificationUM.Warning.HighFeeError(
+                    currencyName = cryptoCurrencyStatus.currency.name,
+                    amount = threshold.toPlainString(),
+                    onConfirmClick = {
+                        onReduceClick(
+                            threshold,
+                            threshold,
+                            NotificationUM.Warning.HighFeeError::class.java,
+                        )
+                    },
+                    onCloseClick = {
+                        onCloseClick(NotificationUM.Warning.HighFeeError::class.java)
+                    },
+                ),
+            )
+        }
     }
 
     private fun checkDustLimits(
