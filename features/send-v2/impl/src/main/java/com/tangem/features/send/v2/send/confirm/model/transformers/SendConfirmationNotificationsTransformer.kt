1e0b441434db536ff6eaf4dda0d88670eb470421
Tangem <git@tangem.com>
2025-03-28 18:40:31 +0500

    Updated on 2026-05-19

---
+++ features/send-v2/impl/src/main/java/com/tangem/features/send/v2/send/confirm/model/transformers/SendConfirmationNotificationsTransformer.kt
@@ -0,0 +1,55 @@
+package com.tangem.features.send.v2.send.confirm.model.transformers
+
+import com.tangem.blockchain.common.transaction.TransactionFee
+import com.tangem.common.ui.notifications.NotificationUM
+import com.tangem.core.analytics.api.AnalyticsEventHandler
+import com.tangem.core.ui.utils.parseToBigDecimal
+import com.tangem.domain.tokens.model.CryptoCurrency
+import com.tangem.features.send.v2.send.analytics.SendAnalyticEvents
+import com.tangem.features.send.v2.send.confirm.ui.state.ConfirmUM
+import com.tangem.features.send.v2.subcomponents.fee.model.checkIfFeeTooHigh
+import com.tangem.features.send.v2.subcomponents.fee.ui.state.FeeSelectorUM
+import com.tangem.features.send.v2.subcomponents.fee.ui.state.FeeType
+import com.tangem.features.send.v2.subcomponents.fee.ui.state.FeeUM
+import com.tangem.utils.transformer.Transformer
+import kotlinx.collections.immutable.toPersistentList
+
+internal class SendConfirmationNotificationsTransformer(
+    private val feeUM: FeeUM,
+    private val analyticsEventHandler: AnalyticsEventHandler,
+    private val cryptoCurrency: CryptoCurrency,
+) : Transformer<ConfirmUM> {
+    override fun transform(prevState: ConfirmUM): ConfirmUM {
+        val state = prevState as? ConfirmUM.Content ?: return prevState
+        val feeUM = feeUM as? FeeUM.Content ?: return prevState
+        return state.copy(
+            notifications = buildList {
+                addTooHighNotification(feeUM.feeSelectorUM)
+                addTooLowNotification(feeUM)
+            }.toPersistentList(),
+        )
+    }
+
+    private fun MutableList<NotificationUM>.addTooLowNotification(feeUM: FeeUM.Content) {
+        val feeSelectorUM = feeUM.feeSelectorUM as? FeeSelectorUM.Content ?: return
+        val multipleFees = feeSelectorUM.fees as? TransactionFee.Choosable ?: return
+        val minimumValue = multipleFees.minimum.amount.value ?: return
+        val customAmount = feeSelectorUM.customValues.firstOrNull() ?: return
+        val customValue = customAmount.value.parseToBigDecimal(customAmount.decimals)
+        if (feeSelectorUM.selectedType == FeeType.Custom && minimumValue > customValue) {
+            add(NotificationUM.Warning.FeeTooLow)
+            analyticsEventHandler.send(
+                SendAnalyticEvents.NoticeTransactionDelays(cryptoCurrency.symbol),
+            )
+        }
+    }
+
+    private fun MutableList<NotificationUM>.addTooHighNotification(feeSelectorUM: FeeSelectorUM) {
+        if (feeSelectorUM !is FeeSelectorUM.Content) return
+
+        val (isFeeTooHigh, diff) = checkIfFeeTooHigh(feeSelectorUM)
+        if (isFeeTooHigh) {
+            add(NotificationUM.Warning.TooHigh(diff))
+        }
+    }
+}
