8190754124ecee4628808b2a68eb43f7632307cd
Tangem <git@tangem.com>
2025-03-31 15:10:05 +0400

    Updated on 2026-09-14

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/operations/CachedCurrenciesStatusesOperations.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/operations/CachedCurrenciesStatusesOperations.kt
@@ -1,6 +1,7 @@
 package com.tangem.domain.tokens.operations
 
 import arrow.core.*
+import arrow.core.raise.either
 import arrow.core.raise.recover
 import com.tangem.domain.core.lce.Lce
 import com.tangem.domain.core.lce.LceFlow
@@ -9,6 +10,9 @@ import com.tangem.domain.core.lce.lceFlow
 import com.tangem.domain.core.utils.EitherFlow
 import com.tangem.domain.core.utils.lceContent
 import com.tangem.domain.core.utils.lceError
+import com.tangem.domain.networks.multi.MultiNetworkStatusFetcher
+import com.tangem.domain.networks.single.SingleNetworkStatusProducer
+import com.tangem.domain.networks.single.SingleNetworkStatusSupplier
 import com.tangem.domain.staking.model.stakekit.YieldBalance
 import com.tangem.domain.staking.model.stakekit.YieldBalanceList
 import com.tangem.domain.staking.repositories.StakingRepository
@@ -21,15 +25,19 @@ import com.tangem.domain.tokens.repository.NetworksRepository
 import com.tangem.domain.tokens.repository.QuotesRepository
 import com.tangem.domain.tokens.utils.extractAddress
 import com.tangem.domain.wallets.models.UserWalletId
+import com.tangem.utils.extensions.addOrReplace
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 
+@Suppress("LongParameterList")
 class CachedCurrenciesStatusesOperations(
     private val currenciesRepository: CurrenciesRepository,
     private val quotesRepository: QuotesRepository,
     private val networksRepository: NetworksRepository,
     private val stakingRepository: StakingRepository,
-    @Suppress("UnusedPrivateMember") private val tokensFeatureToggles: TokensFeatureToggles,
+    private val singleNetworkStatusSupplier: SingleNetworkStatusSupplier,
+    private val multiNetworkStatusFetcher: MultiNetworkStatusFetcher,
+    private val tokensFeatureToggles: TokensFeatureToggles,
 ) : BaseCurrenciesStatusesOperations,
     BaseCurrencyStatusOperations(currenciesRepository, quotesRepository, networksRepository, stakingRepository) {
 
@@ -145,20 +153,26 @@ class CachedCurrenciesStatusesOperations(
         networks: Set<Network>,
         currenciesIds: Set<CryptoCurrency.ID>,
         currencies: List<CryptoCurrency>,
-    ): Either<Throwable, Unit> {
-        return coroutineScope {
-            Either.catch {
-                awaitAll(
-                    async { networksRepository.fetchNetworkStatuses(userWalletId, networks) },
-                    async {
-                        val rawCurrenciesIds = currenciesIds.mapNotNullTo(mutableSetOf()) { it.rawCurrencyId }
-                        quotesRepository.fetchQuotes(rawCurrenciesIds)
-                    },
-                    async { stakingRepository.fetchMultiYieldBalance(userWalletId, currencies) },
-                )
-            }
-                .map { }
+    ): Either<Throwable, Unit> = either {
+        coroutineScope {
+            awaitAll(
+                async {
+                    if (tokensFeatureToggles.isNetworksLoadingRefactoringEnabled) {
+                        multiNetworkStatusFetcher(
+                            params = MultiNetworkStatusFetcher.Params(userWalletId, networks),
+                        )
+                    } else {
+                        networksRepository.fetchNetworkStatuses(userWalletId, networks)
+                    }
+                },
+                async {
+                    val rawCurrenciesIds = currenciesIds.mapNotNullTo(mutableSetOf()) { it.rawCurrencyId }
+                    quotesRepository.fetchQuotes(rawCurrenciesIds)
+                },
+                async { stakingRepository.fetchMultiYieldBalance(userWalletId, currencies) },
+            )
         }
+            .map { }
     }
 
     private fun createCurrenciesStatuses(
@@ -251,31 +265,44 @@ class CachedCurrenciesStatusesOperations(
         userWalletId: UserWalletId,
         network: Network,
     ): EitherFlow<Error, Set<NetworkStatus>> {
-        return networksRepository.getNetworkStatusesUpdates(userWalletId, setOf(network))
-            .map<Set<NetworkStatus>, Either<Error, Set<NetworkStatus>>> { it.right() }
-            .retryWhen { cause, _ ->
-                emit(Error.DataError(cause).left())
-                // adding delay before retry to avoid spam when flow restarted
-                delay(RETRY_DELAY)
-                true
-            }
-            .distinctUntilChanged()
-            .onEmpty { emit(Error.EmptyNetworksStatuses.left()) }
+        return if (tokensFeatureToggles.isNetworksLoadingRefactoringEnabled) {
+            singleNetworkStatusSupplier(
+                params = SingleNetworkStatusProducer.Params(userWalletId = userWalletId, network = network),
+            )
+                .map<NetworkStatus, Either<Error, Set<NetworkStatus>>> { setOf(it).right() }
+                .distinctUntilChanged()
+                .onEmpty { emit(Error.EmptyNetworksStatuses.left()) }
+        } else {
+            networksRepository.getNetworkStatusesUpdates(userWalletId, setOf(network))
+                .map<Set<NetworkStatus>, Either<Error, Set<NetworkStatus>>> { it.right() }
+                .retryWhen { cause, _ ->
+                    emit(Error.DataError(cause).left())
+                    // adding delay before retry to avoid spam when flow restarted
+                    delay(RETRY_DELAY)
+                    true
+                }
+                .distinctUntilChanged()
+                .onEmpty { emit(Error.EmptyNetworksStatuses.left()) }
+        }
     }
 
     private fun getNetworksStatuses(
         userWalletId: UserWalletId,
         networks: NonEmptySet<Network>,
     ): EitherFlow<TokenListError, Set<NetworkStatus>> {
-        return networksRepository.getNetworkStatusesUpdates(userWalletId, networks)
-            .map<Set<NetworkStatus>, Either<TokenListError, Set<NetworkStatus>>> { it.right() }
-            .retryWhen { cause, _ ->
-                emit(TokenListError.DataError(cause).left())
-                // adding delay before retry to avoid spam when flow restarted
-                delay(RETRY_DELAY)
-                true
-            }
-            .distinctUntilChanged()
+        return if (tokensFeatureToggles.isNetworksLoadingRefactoringEnabled) {
+            getNetworkStatusesUpdates(userWalletId, networks)
+        } else {
+            networksRepository.getNetworkStatusesUpdates(userWalletId, networks)
+                .map<Set<NetworkStatus>, Either<TokenListError, Set<NetworkStatus>>> { it.right() }
+                .retryWhen { cause, _ ->
+                    emit(TokenListError.DataError(cause).left())
+                    // adding delay before retry to avoid spam when flow restarted
+                    delay(RETRY_DELAY)
+                    true
+                }
+                .distinctUntilChanged()
+        }
     }
 
     private fun getYieldBalances(
@@ -290,6 +317,36 @@ class CachedCurrenciesStatusesOperations(
                 delay(RETRY_DELAY)
                 true
             }
+            .distinctUntilChanged()
+    }
+
+    // temporary code because token list is built using networks list
+    private fun getNetworkStatusesUpdates(
+        userWalletId: UserWalletId,
+        networks: NonEmptySet<Network>,
+    ): EitherFlow<TokenListError, Set<NetworkStatus>> {
+        return channelFlow {
+            val state = MutableStateFlow(emptySet<NetworkStatus>())
+
+            networks.onEach {
+                launch {
+                    singleNetworkStatusSupplier(
+                        params = SingleNetworkStatusProducer.Params(userWalletId = userWalletId, network = it),
+                    )
+                        .onEach { status ->
+                            state.update { loadedStatuses ->
+                                loadedStatuses.addOrReplace(status) { it.network == status.network }
+                            }
+                        }
+                        .launchIn(scope = this)
+                }
+            }
+
+            state
+                .onEach(::send)
+                .launchIn(scope = this)
+        }
+            .map<Set<NetworkStatus>, Either<TokenListError, Set<NetworkStatus>>> { it.right() }
             .distinctUntilChanged()
     }
 
