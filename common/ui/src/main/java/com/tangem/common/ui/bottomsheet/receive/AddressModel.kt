61cfd7b89f2e4399834642fe668e89b134decbb0
Tangem <git@tangem.com>
2025-04-02 01:41:05 +0500

    Updated on 2026-09-23

--- common/ui/src/main/java/com/tangem/common/ui/bottomsheet/receive/AddressModel.kt
+++ common/ui/src/main/java/com/tangem/common/ui/bottomsheet/receive/AddressModel.kt
@@ -4,6 +4,7 @@ import com.tangem.core.ui.extensions.TextReference
 
 data class AddressModel(
     val displayName: TextReference,
+    val fullName: TextReference,
     val value: String,
     val type: Type,
 ) {
