7fb4d9ee864d6fd6a61f0f827b09b868726168df
Tangem <git@tangem.com>
2025-04-01 16:21:50 +0500

    Updated on 2026-09-21

--- features/nft/impl/src/main/kotlin/com/tangem/features/nft/receive/entity/NFTReceiveNetworksUM.kt
+++
@@ -1,9 +0,0 @@
-package com.tangem.features.nft.receive.entity
-
-import com.tangem.core.ui.components.fields.entity.SearchBarUM
-import kotlinx.collections.immutable.ImmutableList
-
-internal data class NFTReceiveNetworksUM(
-    val searchBar: SearchBarUM,
-    val networks: ImmutableList<NFTNetworkUM>,
-)
