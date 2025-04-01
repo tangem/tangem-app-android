b206b631e3935263a19d4414c42168403e287c5e
Tangem <git@tangem.com>
2025-04-01 14:42:30 +0400

    Updated on 2026-08-27

--- domain/manage-tokens/build.gradle.kts
+++ domain/manage-tokens/build.gradle.kts
@@ -11,8 +11,9 @@ android {
 dependencies {
 
     /* Domain */
-    api(projects.domain.manageTokens.models)
     api(projects.domain.core)
+    api(projects.domain.manageTokens.models)
+    api(projects.domain.networks)
     implementation(projects.domain.wallets.models)
     implementation(projects.domain.tokens.models)
     implementation(projects.domain.staking)
