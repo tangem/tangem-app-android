ab634b25225b39efe5c50f7d329fdf8cf6d4954b
Tangem <git@tangem.com>
2025-04-02 01:41:05 +0500

    Updated on 2026-07-07

--- common/ui/src/main/java/com/tangem/common/ui/bottomsheet/receive/AddressMappers.kt
+++ common/ui/src/main/java/com/tangem/common/ui/bottomsheet/receive/AddressMappers.kt
@@ -1,6 +1,9 @@
 package com.tangem.common.ui.bottomsheet.receive
 
+import com.tangem.core.ui.R
 import com.tangem.core.ui.extensions.TextReference
+import com.tangem.core.ui.extensions.combinedReference
+import com.tangem.core.ui.extensions.resourceReference
 import com.tangem.core.ui.extensions.stringReference
 import com.tangem.domain.tokens.model.CryptoCurrency
 import com.tangem.domain.tokens.model.Network
@@ -12,18 +15,48 @@ private const val DEL_ADDRESS_NAME = "Main"
 private const val LEGACY_ADDRESS_NAME = "Legacy"
 private const val DEFAULT_ADDRESS_NAME = "Default"
 
-fun Set<NetworkAddress.Address>.mapToAddressModels(currency: CryptoCurrency): List<AddressModel> = this
-    .sortedBy { it.type.ordinal }
-    .map { address ->
-        AddressModel(
-            displayName = currency.network.getAddressDisplayName(address.type),
-            value = address.value,
-            type = when (address.type) {
-                NetworkAddress.Address.Type.Primary -> AddressModel.Type.Default
-                NetworkAddress.Address.Type.Secondary -> AddressModel.Type.Legacy
-            },
-        )
-    }
+fun Set<NetworkAddress.Address>.mapToAddressModels(cryptoCurrency: CryptoCurrency) = mapToAddressModels(
+    stringReference("${cryptoCurrency.name} (${cryptoCurrency.symbol})"),
+    cryptoCurrency.network,
+)
+
+fun Set<NetworkAddress.Address>.mapToAddressModels(
+    asset: TokenReceiveBottomSheetConfig.Asset,
+    network: Network,
+): List<AddressModel> = when (asset) {
+    is TokenReceiveBottomSheetConfig.Asset.Currency -> mapToAddressModels(
+        stringReference("${asset.name} (${asset.symbol})"),
+        network,
+    )
+    is TokenReceiveBottomSheetConfig.Asset.NFT -> mapToAddressModels(
+        resourceReference(R.string.common_nft),
+        network,
+    )
+}
+
+private fun Set<NetworkAddress.Address>.mapToAddressModels(name: TextReference, network: Network): List<AddressModel> =
+    this
+        .sortedBy { it.type.ordinal }
+        .map { address ->
+            val displayName = network.getAddressDisplayName(address.type)
+            AddressModel(
+                displayName = displayName,
+                fullName = if (this.size < 2) {
+                    name
+                } else {
+                    combinedReference(
+                        displayName,
+                        stringReference(" "),
+                        name,
+                    )
+                },
+                value = address.value,
+                type = when (address.type) {
+                    NetworkAddress.Address.Type.Primary -> AddressModel.Type.Default
+                    NetworkAddress.Address.Type.Secondary -> AddressModel.Type.Legacy
+                },
+            )
+        }
 
 private fun Network.getAddressDisplayName(addressType: NetworkAddress.Address.Type): TextReference {
     return when (id.value) {
