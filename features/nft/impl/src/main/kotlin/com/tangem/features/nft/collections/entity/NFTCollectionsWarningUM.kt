e0ed8185e82e17f4d9baf1ca107f5436b93902a8
Tangem <git@tangem.com>
2025-04-01 13:10:07 +0500

    Updated on 2025-12-30

--- features/nft/impl/src/main/kotlin/com/tangem/features/nft/collections/entity/NFTCollectionsWarningUM.kt
+++ features/nft/impl/src/main/kotlin/com/tangem/features/nft/collections/entity/NFTCollectionsWarningUM.kt
@@ -2,4 +2,7 @@ package com.tangem.features.nft.collections.entity
 
 import com.tangem.core.ui.components.notifications.NotificationConfig
 
-internal data class NFTCollectionsWarningUM(val config: NotificationConfig)
+internal data class NFTCollectionsWarningUM(
+    val id: String,
+    val config: NotificationConfig,
+)
