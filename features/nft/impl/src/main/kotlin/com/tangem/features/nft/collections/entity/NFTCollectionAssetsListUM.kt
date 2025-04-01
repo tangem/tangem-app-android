82f7e1cd21c068a59568748152d821d89178aaba
Tangem <git@tangem.com>
2025-04-01 13:10:07 +0500

    Updated on 2026-05-04

--- features/nft/impl/src/main/kotlin/com/tangem/features/nft/collections/entity/NFTCollectionAssetsListUM.kt
+++ features/nft/impl/src/main/kotlin/com/tangem/features/nft/collections/entity/NFTCollectionAssetsListUM.kt
@@ -5,12 +5,8 @@ import kotlinx.collections.immutable.ImmutableList
 
 @Immutable
 internal sealed class NFTCollectionAssetsListUM {
-    data object Collapsed : NFTCollectionAssetsListUM()
-
-    @Immutable
-    sealed class Expanded : NFTCollectionAssetsListUM() {
-        data class Loading(val itemsCount: Int) : Expanded()
-        data class Failed(val onRetryClick: () -> Unit) : Expanded()
-        data class Content(val items: ImmutableList<NFTCollectionAssetUM>) : Expanded()
-    }
+    data object Init : NFTCollectionAssetsListUM()
+    data class Loading(val itemsCount: Int) : NFTCollectionAssetsListUM()
+    data class Failed(val onRetryClick: () -> Unit) : NFTCollectionAssetsListUM()
+    data class Content(val items: ImmutableList<NFTCollectionAssetUM>) : NFTCollectionAssetsListUM()
 }
