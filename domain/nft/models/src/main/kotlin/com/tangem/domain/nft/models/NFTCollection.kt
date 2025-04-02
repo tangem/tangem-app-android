23d526389f361ecc9062d59f90c0101c3eb65be7
Tangem <git@tangem.com>
2025-04-02 14:20:37 +0500

    release/5.27.0

--- domain/nft/models/src/main/kotlin/com/tangem/domain/nft/models/NFTCollection.kt
+++ domain/nft/models/src/main/kotlin/com/tangem/domain/nft/models/NFTCollection.kt
@@ -2,7 +2,9 @@ package com.tangem.domain.nft.models
 
 import com.tangem.domain.models.StatusSource
 import com.tangem.domain.tokens.model.Network
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class NFTCollection(
     val id: Identifier,
     val network: Network,
@@ -12,19 +14,33 @@ data class NFTCollection(
     val count: Int,
     val assets: Assets,
 ) {
+    @Serializable
     sealed class Assets {
+        @Serializable
         data object Empty : Assets()
+
+        @Serializable
         data object Loading : Assets()
+
+        @Serializable
         data object Failed : Assets()
+
+        @Serializable
         data class Value(
             val items: List<NFTAsset>,
             val source: StatusSource,
         ) : Assets()
     }
 
+    @Serializable
     sealed class Identifier {
+        @Serializable
         data class EVM(val tokenAddress: String) : Identifier()
+
+        @Serializable
         data class TON(val contractAddress: String?) : Identifier()
+
+        @Serializable
         data object Unknown : Identifier()
     }
 }
