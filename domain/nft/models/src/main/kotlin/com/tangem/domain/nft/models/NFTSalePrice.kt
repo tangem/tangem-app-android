3c779df9b9c9ded121b3856f22c4732b0c44075d
Tangem <git@tangem.com>
2025-04-02 14:20:37 +0500

    Updated on 2026-09-25

--- domain/nft/models/src/main/kotlin/com/tangem/domain/nft/models/NFTSalePrice.kt
+++ domain/nft/models/src/main/kotlin/com/tangem/domain/nft/models/NFTSalePrice.kt
@@ -1,25 +1,31 @@
 package com.tangem.domain.nft.models
 
-import java.math.BigDecimal
+import com.tangem.domain.core.serialization.SerializedBigDecimal
+import kotlinx.serialization.Serializable
 
+@Serializable
 sealed class NFTSalePrice {
     abstract val assetId: NFTAsset.Identifier
 
+    @Serializable
     data class Empty(
         override val assetId: NFTAsset.Identifier,
     ) : NFTSalePrice()
 
+    @Serializable
     data class Loading(
         override val assetId: NFTAsset.Identifier,
     ) : NFTSalePrice()
 
+    @Serializable
     data class Error(
         override val assetId: NFTAsset.Identifier,
     ) : NFTSalePrice()
 
+    @Serializable
     data class Value(
         override val assetId: NFTAsset.Identifier,
-        val value: BigDecimal,
+        val value: SerializedBigDecimal,
         val symbol: String,
     ) : NFTSalePrice()
 }
