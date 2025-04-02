b1f7516e42f90f17f5d402dde6eaf02b851f01a1
Tangem <git@tangem.com>
2025-04-02 17:54:38 +0500

    release/5.26.1

--- core/datasource/src/main/java/com/tangem/datasource/di/StakingStoreModule.kt
+++ core/datasource/src/main/java/com/tangem/datasource/di/StakingStoreModule.kt
@@ -5,10 +5,12 @@ import androidx.datastore.core.DataStoreFactory
 import androidx.datastore.dataStoreFile
 import com.squareup.moshi.Moshi
 import com.tangem.datasource.api.stakekit.models.response.model.YieldBalanceWrapperDTO
+import com.tangem.datasource.api.stakekit.models.response.model.YieldDTO
 import com.tangem.datasource.local.datastore.RuntimeDataStore
 import com.tangem.datasource.local.datastore.RuntimeSharedStore
 import com.tangem.datasource.local.token.*
 import com.tangem.datasource.utils.MoshiDataStoreSerializer
+import com.tangem.datasource.utils.listTypes
 import com.tangem.datasource.utils.mapWithStringKeyTypes
 import com.tangem.datasource.utils.setTypes
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
@@ -27,8 +29,22 @@ internal object StakingStoreModule {
 
     @Provides
     @Singleton
-    fun provideStakingTokensStore(): StakingYieldsStore {
-        return DefaultStakingYieldsStore(dataStore = RuntimeDataStore())
+    fun provideStakingTokensStore(
+        @NetworkMoshi moshi: Moshi,
+        @ApplicationContext context: Context,
+        dispatchers: CoroutineDispatcherProvider,
+    ): StakingYieldsStore {
+        return DefaultStakingYieldsStore(
+            dataStore = DataStoreFactory.create(
+                serializer = MoshiDataStoreSerializer(
+                    moshi = moshi,
+                    types = listTypes<YieldDTO>(),
+                    defaultValue = emptyList(),
+                ),
+                produceFile = { context.dataStoreFile(fileName = "yields_cache") },
+                scope = CoroutineScope(context = dispatchers.io + SupervisorJob()),
+            ),
+        )
     }
 
     @Provides
