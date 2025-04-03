de6683b57ac873fac53677a2b7e34286718827d4
Tangem <git@tangem.com>
2025-04-03 15:51:03 +0700

    Updated on 2026-08-14

--- domain/wallet-connect/src/main/kotlin/com/tangem/domain/walletconnect/usecase/pair/WcPairUseCase.kt
+++ domain/wallet-connect/src/main/kotlin/com/tangem/domain/walletconnect/usecase/pair/WcPairUseCase.kt
@@ -1,15 +1,14 @@
 package com.tangem.domain.walletconnect.usecase.pair
 
 import com.tangem.domain.walletconnect.model.WcSessionApprove
-import com.tangem.domain.wallets.models.UserWallet
 import kotlinx.coroutines.flow.Flow
 
 interface WcPairUseCase {
 
-    fun pairFlow(uri: String, source: Source, selectedWallet: UserWallet): Flow<WcPairState>
+    fun pairFlow(uri: String, source: Source): Flow<WcPairState>
 
     fun approve(sessionForApprove: WcSessionApprove)
     fun reject()
 
-    enum class Source { QR, DEEPLINK, ETC }
+    enum class Source { QR, DEEPLINK, CLIPBOARD, ETC }
 }
