3d66d5cd03e9736993fb7fe5f0fa1b05572276ce
Tangem <git@tangem.com>
2025-04-04 19:31:21 +0500

    Updated on 2026-02-11

---
+++ features/send-v2/impl/src/main/java/com/tangem/features/send/v2/send/di/SendModelModule.kt
@@ -0,0 +1,26 @@
+package com.tangem.features.send.v2.send.di
+
+import com.tangem.core.decompose.di.ModelComponent
+import com.tangem.core.decompose.model.Model
+import com.tangem.features.send.v2.send.confirm.model.SendConfirmModel
+import com.tangem.features.send.v2.send.model.SendModel
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.multibindings.ClassKey
+import dagger.multibindings.IntoMap
+
+@Module
+@InstallIn(ModelComponent::class)
+internal interface SendModelModule {
+
+    @Binds
+    @IntoMap
+    @ClassKey(SendModel::class)
+    fun provideSendModel(model: SendModel): Model
+
+    @Binds
+    @IntoMap
+    @ClassKey(SendConfirmModel::class)
+    fun provideSendConfirmModel(model: SendConfirmModel): Model
+}
