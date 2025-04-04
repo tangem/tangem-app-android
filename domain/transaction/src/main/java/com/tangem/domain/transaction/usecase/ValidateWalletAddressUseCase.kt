4080b4acf33302383bfd861d4b915171561ae244
Tangem <git@tangem.com>
2025-04-04 16:20:13 +0500

    Updated on 2026-09-14

--- domain/transaction/src/main/java/com/tangem/domain/transaction/usecase/ValidateWalletAddressUseCase.kt
+++ domain/transaction/src/main/java/com/tangem/domain/transaction/usecase/ValidateWalletAddressUseCase.kt
@@ -2,6 +2,7 @@ package com.tangem.domain.transaction.usecase
 
 import arrow.core.left
 import arrow.core.right
+import com.tangem.domain.tokens.model.CryptoCurrencyAddress
 import com.tangem.domain.tokens.model.Network
 import com.tangem.domain.tokens.model.NetworkAddress
 import com.tangem.domain.transaction.WalletAddressServiceRepository
@@ -28,6 +29,31 @@ class ValidateWalletAddressUseCase(
         val isUtxoConsolidationAvailable =
             walletManagersFacade.checkUtxoConsolidationAvailability(userWalletId, network)
         val isCurrentAddress = currencyAddress?.any { it.value == address } ?: true
+
+        val isForbidSelfSend = isCurrentAddress && !isUtxoConsolidationAvailable
+
+        val decodedXAddress = BlockchainUtils.decodeRippleXAddress(address, network.id.value)
+        val addressToValidate = decodedXAddress?.address ?: address
+
+        val isValidAddress = walletAddressServiceRepository.validateAddress(userWalletId, network, addressToValidate)
+
+        return when {
+            !isValidAddress -> AddressValidation.Error.InvalidAddress.left()
+            isForbidSelfSend -> AddressValidation.Error.AddressInWallet.left()
+            decodedXAddress != null -> AddressValidation.Success.ValidXAddress.right()
+            else -> AddressValidation.Success.Valid.right()
+        }
+    }
+
+    suspend operator fun invoke(
+        userWalletId: UserWalletId,
+        network: Network,
+        address: String,
+        senderAddresses: List<CryptoCurrencyAddress>,
+    ): AddressValidationResult {
+        val isUtxoConsolidationAvailable =
+            walletManagersFacade.checkUtxoConsolidationAvailability(userWalletId, network)
+        val isCurrentAddress = senderAddresses.any { it.address == address }
 
         val isForbidSelfSend = isCurrentAddress && !isUtxoConsolidationAvailable
 
