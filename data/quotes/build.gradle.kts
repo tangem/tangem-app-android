b750e72f747a9f525e1b15027a583c0c72c89d37
Tangem <git@tangem.com>
2025-04-05 12:19:37 +0400

    Updated on 2026-06-24

--- data/quotes/build.gradle.kts
+++ data/quotes/build.gradle.kts
@@ -1,30 +1,30 @@
 plugins {
     alias(deps.plugins.android.library)
     alias(deps.plugins.kotlin.android)
+    alias(deps.plugins.kotlin.kapt)
     id("configuration")
 }
 
 android {
-    namespace = "com.tangem.common.test"
+    namespace = "com.tangem.data.quotes"
 }
 
 dependencies {
     implementation(projects.core.datasource)
     implementation(projects.core.utils)
 
-    implementation(projects.data.common)
-
-    implementation(projects.domain.legacy)
     implementation(projects.domain.models)
     implementation(projects.domain.tokens.models)
-    implementation(projects.domain.wallets)
     implementation(projects.domain.wallets.models)
 
-    implementation(projects.libs.blockchainSdk)
-
     implementation(deps.androidx.datastore)
-    implementation(deps.test.coroutine)
 
-    implementation(tangemDeps.blockchain)
-    implementation(tangemDeps.card.core)
+    implementation(deps.hilt.android)
+    kapt(deps.hilt.kapt)
+
+    testImplementation(deps.test.coroutine)
+    testImplementation(deps.test.junit)
+    testImplementation(deps.test.mockk)
+    testImplementation(deps.test.truth)
+    testImplementation(projects.common.test)
 }
