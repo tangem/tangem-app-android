bdaae58336caf6bc1d23c3ee2ce2f5ee472cbeb1
Tangem <git@tangem.com>
2025-04-07 12:53:01 +0300

    Updated on 2025-12-18

--- features/details/impl/src/main/kotlin/com/tangem/features/details/entity/DetailsUM.kt
+++ features/details/impl/src/main/kotlin/com/tangem/features/details/entity/DetailsUM.kt
@@ -1,9 +1,11 @@
 package com.tangem.features.details.entity
 
+import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfig
 import kotlinx.collections.immutable.ImmutableList
 
 internal data class DetailsUM(
     val items: ImmutableList<DetailsItemUM>,
     val footer: DetailsFooterUM,
+    val selectFeedbackEmailTypeBSConfig: TangemBottomSheetConfig,
     val popBack: () -> Unit,
 )
