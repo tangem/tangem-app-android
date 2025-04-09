93bd7c96a19e4d0e12fae6a40ea06dcd6fac00f3
Tangem <git@tangem.com>
2025-04-09 13:24:16 +0700

    Updated on 2026-04-15

--- data/wallet-connect/src/main/kotlin/com/tangem/data/walletconnect/pair/DefaultWcPairUseCase.kt
+++ data/wallet-connect/src/main/kotlin/com/tangem/data/walletconnect/pair/DefaultWcPairUseCase.kt
@@ -4,8 +4,6 @@ import arrow.core.Either
 import arrow.core.left
 import arrow.core.right
 import com.reown.walletkit.client.Wallet
-import com.reown.walletkit.client.WalletKit
-import com.tangem.data.walletconnect.utils.WcSdkObserver
 import com.tangem.data.walletconnect.utils.WcSdkSessionConverter
 import com.tangem.domain.walletconnect.model.WcPairError
 import com.tangem.domain.walletconnect.model.WcSession
@@ -21,9 +19,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.receiveAsFlow
-import kotlinx.coroutines.suspendCancellableCoroutine
 import timber.log.Timber
-import kotlin.coroutines.resume
 
 val unsupportedDApps = listOf("dYdX", "dYdX v4", "Apex Pro", "The Sandbox")
 
@@ -31,27 +27,18 @@ internal class DefaultWcPairUseCase(
     private val sessionsManager: WcSessionsManager,
     private val associateNetworksDelegate: AssociateNetworksDelegate,
     private val caipNamespaceDelegate: CaipNamespaceDelegate,
-) : WcPairUseCase, WcSdkObserver {
+    private val sdkDelegate: WcPairSdkDelegate,
+) : WcPairUseCase {
 
     private val onCallTerminalAction = Channel<TerminalAction>()
-    private val onSessionProposal =
-        Channel<Pair<Wallet.Model.SessionProposal, Wallet.Model.VerifyContext>>(Channel.BUFFERED)
-    private val onSessionSettleResponse = Channel<Wallet.Model.SettledSessionResponse>(Channel.BUFFERED)
 
     override fun pairFlow(uri: String, source: WcPairUseCase.Source): Flow<WcPairState> {
         return flow {
             emit(WcPairState.Loading)
 
-            // call sdk.pair and wait result, finish flow on error
-            walletKitPair(uri).onLeft { throwable ->
-                emit(WcPairState.Error(WcPairError.Unknown(throwable.localizedMessage.orEmpty())))
-                return@flow
-            }
-            // wait for sdk onSessionProposal callback
-            val (sdkSessionProposal, verifyContext) = onSessionProposal.receiveAsFlow()
-                .first { (sessionProposal, verifyContext) ->
-                    true // todo(wc) check verifyContext? compare uri?
-                }
+            val sdkSessionProposal = sdkDelegate.pair(uri)
+                .onLeft { emit(WcPairState.Error(it)) }
+                .getOrNull() ?: return@flow
 
             // check unsupported dApps, just local constant for now, finish if unsupported
             if (sdkSessionProposal.name in unsupportedDApps) {
@@ -62,44 +49,32 @@ internal class DefaultWcPairUseCase(
             }
 
             val proposalState = buildProposalState(sdkSessionProposal)
-                .fold(ifLeft = { WcPairState.Error(it) }, ifRight = { it })
+                .onLeft { emit(WcPairState.Error(it)) }
+                .getOrNull() ?: return@flow
             emit(proposalState)
 
             // wait first terminal action and continue WC pair flow
             val terminalAction = onCallTerminalAction.receiveAsFlow().first()
             val sessionForApprove: WcSessionApprove? = when (terminalAction) {
                 is TerminalAction.Approve -> terminalAction.sessionForApprove
-                TerminalAction.Reject -> {
-                    // non suspending WalletKit.rejectSession call
-                    rejectSession(sdkSessionProposal.proposerPublicKey)
-                    null
-                }
+                TerminalAction.Reject -> null
             }
             // finish flow if rejected above
-            sessionForApprove ?: return@flow
+            if (sessionForApprove == null) {
+                sdkDelegate.rejectSession(sdkSessionProposal.proposerPublicKey)
+                return@flow
+            }
 
             // start flow of approving in wc sdk
             emit(WcPairState.Approving.Loading(sessionForApprove))
-            // call sdk approve and wait result
             val either = walletKitApproveSession(
                 sessionForApprove = sessionForApprove,
                 sdkSessionProposal = sdkSessionProposal,
-            ).fold(
-                ifLeft = { WcPairError.ExternalApprovalError(it.localizedMessage.orEmpty()).left() },
-                ifRight = {
-                    when (val settledSession = onSessionSettleResponse.receiveAsFlow().first()) {
-                        is Wallet.Model.SettledSessionResponse.Error -> WcPairError.ExternalApprovalError(
-                            settledSession.errorMessage,
-                        ).left()
-
-                        is Wallet.Model.SettledSessionResponse.Result -> {
-                            val newSession = settledSession.session.toDomain(sessionForApprove.wallet)
-                            sessionsManager.saveSession(newSession)
-                            newSession.right()
-                        }
-                    }
-                },
-            )
+            ).map { settledSession ->
+                val newSession = settledSession.session.toDomain(sessionForApprove.wallet)
+                sessionsManager.saveSession(newSession)
+                newSession
+            }
             emit(WcPairState.Approving.Result(sessionForApprove, either))
         }
     }
@@ -112,77 +87,20 @@ internal class DefaultWcPairUseCase(
         onCallTerminalAction.trySend(TerminalAction.Reject)
     }
 
-    override fun onSessionProposal(
-        sessionProposal: Wallet.Model.SessionProposal,
-        verifyContext: Wallet.Model.VerifyContext,
-    ) {
-        // Triggered when wallet receives the session proposal sent by a Dapp
-        Timber.i("sessionProposal: $sessionProposal")
-        onSessionProposal.trySend(sessionProposal to verifyContext)
-    }
-
-    override fun onSessionSettleResponse(settleSessionResponse: Wallet.Model.SettledSessionResponse) {
-        // Triggered when wallet receives the session settlement response from Dapp
-        Timber.i("onSessionSettleResponse: $settleSessionResponse")
-        onSessionSettleResponse.trySend(settleSessionResponse)
-    }
-
-    private suspend fun walletKitPair(uri: String): Either<Throwable, Unit> =
-        suspendCancellableCoroutine { continuation ->
-            WalletKit.pair(
-                params = Wallet.Params.Pair(uri),
-                onSuccess = {
-                    Timber.i("Paired successfully: $it")
-                    continuation.resume(Unit.right())
-                },
-                onError = {
-                    Timber.e("Error while pairing: $it")
-                    continuation.resume(it.throwable.left())
-                },
-            )
-        }
-
     private suspend fun walletKitApproveSession(
         sessionForApprove: WcSessionApprove,
         sdkSessionProposal: Wallet.Model.SessionProposal,
-    ): Either<Throwable, Unit> {
+    ): Either<WcPairError, Wallet.Model.SettledSessionResponse.Result> {
         val namespaces = caipNamespaceDelegate.associate(
             sdkSessionProposal,
             sessionForApprove.wallet,
-            sessionForApprove.network.map { it.network },
+            sessionForApprove.network,
         )
         val sessionApprove = Wallet.Params.SessionApprove(
             proposerPublicKey = sdkSessionProposal.proposerPublicKey,
             namespaces = namespaces,
         )
-        return suspendCancellableCoroutine { continuation ->
-            WalletKit.approveSession(
-                params = sessionApprove,
-                onSuccess = {
-                    Timber.i("Approved successfully: $it")
-                    continuation.resume(Unit.right())
-                },
-                onError = {
-                    Timber.e("Error while approving: $it")
-                    continuation.resume(it.throwable.left())
-                },
-            )
-        }
-    }
-
-    private fun rejectSession(proposerPublicKey: String) {
-        WalletKit.rejectSession(
-            params = Wallet.Params.SessionReject(
-                proposerPublicKey = proposerPublicKey,
-                reason = "",
-            ),
-            onSuccess = {
-                Timber.i("Rejected successfully: $it")
-            },
-            onError = {
-                Timber.e("Error while rejecting: $it")
-            },
-        )
+        return sdkDelegate.approve(sessionApprove)
     }
 
     private suspend fun buildProposalState(
