891a980120dd5e536a1eaf923f7bffef293170d2
Tangem <git@tangem.com>
2025-04-09 13:24:16 +0700

    Updated on 2025-09-15

--- domain/wallet-connect/models/src/main/java/com/tangem/domain/walletconnect/model/WcSessionApprove.kt
+++ domain/wallet-connect/models/src/main/java/com/tangem/domain/walletconnect/model/WcSessionApprove.kt
@@ -1,8 +1,9 @@
 package com.tangem.domain.walletconnect.model
 
+import com.tangem.domain.tokens.model.Network
 import com.tangem.domain.wallets.models.UserWallet
 
 data class WcSessionApprove(
     val wallet: UserWallet,
-    val network: List<WcNetwork.Supported>,
+    val network: List<Network>,
 )
