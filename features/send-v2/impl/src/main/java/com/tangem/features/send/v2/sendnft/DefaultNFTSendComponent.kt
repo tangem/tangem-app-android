336f4ba544ffa22cd200872673f4bafc3fdf7578
Tangem <git@tangem.com>
2025-04-09 20:54:55 +0500

    Updated on 2026-07-23

--- features/send-v2/impl/src/main/java/com/tangem/features/send/v2/sendnft/DefaultNFTSendComponent.kt
+++ features/send-v2/impl/src/main/java/com/tangem/features/send/v2/sendnft/DefaultNFTSendComponent.kt
@@ -2,84 +2,195 @@ package com.tangem.features.send.v2.sendnft
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.arkivanov.decompose.extensions.compose.subscribeAsState
+import com.arkivanov.decompose.router.stack.StackNavigation
+import com.arkivanov.decompose.router.stack.childStack
+import com.arkivanov.decompose.router.stack.pop
+import com.arkivanov.decompose.value.ObserveLifecycleMode
+import com.arkivanov.decompose.value.subscribe
 import com.tangem.core.decompose.context.AppComponentContext
+import com.tangem.core.decompose.context.childByContext
 import com.tangem.core.decompose.model.getOrCreateModel
+import com.tangem.core.decompose.navigation.inner.InnerRouter
+import com.tangem.core.ui.decompose.ComposableContentComponent
+import com.tangem.features.nft.component.NFTDetailsBlockComponent
 import com.tangem.features.send.v2.api.NFTSendComponent
+import com.tangem.features.send.v2.common.CommonSendRoute
+import com.tangem.features.send.v2.common.ui.SendContent
+import com.tangem.features.send.v2.common.ui.state.ConfirmUM
+import com.tangem.features.send.v2.sendnft.confirm.NFTSendConfirmComponent
 import com.tangem.features.send.v2.sendnft.model.NFTSendModel
+import com.tangem.features.send.v2.subcomponents.destination.SendDestinationComponent
+import com.tangem.features.send.v2.subcomponents.destination.SendDestinationComponentParams
+import com.tangem.features.send.v2.subcomponents.destination.ui.state.DestinationUM
+import com.tangem.features.send.v2.subcomponents.fee.SendFeeComponent
+import com.tangem.features.send.v2.subcomponents.fee.SendFeeComponentParams
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.filterIsInstance
+import kotlinx.coroutines.launch
+import java.math.BigDecimal
 
 internal class DefaultNFTSendComponent @AssistedInject constructor(
     @Assisted appComponentContext: AppComponentContext,
     @Assisted private val params: NFTSendComponent.Params,
+    private val nftDetailsBlockComponentFactory: NFTDetailsBlockComponent.Factory,
 ) : NFTSendComponent, AppComponentContext by appComponentContext {
 
-    // private val stackNavigation = StackNavigation<NFTSendRoute>()
-    //
-    // private val innerRouter = InnerRouter<NFTSendRoute>(
-    //     stackNavigation = stackNavigation,
-    //     popCallback = { onChildBack() },
-    // )
+    private val stackNavigation = StackNavigation<CommonSendRoute>()
 
-    // private val initialRoute = NFTSendRoute.Empty
-    // private val currentRouteFlow = MutableStateFlow<NFTSendRoute>(initialRoute)
+    private val innerRouter = InnerRouter<CommonSendRoute>(
+        stackNavigation = stackNavigation,
+        popCallback = { onChildBack() },
+    )
 
-    private val model: NFTSendModel = getOrCreateModel(params = params/*, router = innerRouter*/)
+    private val initialRoute = CommonSendRoute.Empty
+    private val currentRouteFlow = MutableStateFlow<CommonSendRoute>(initialRoute)
 
-    // private val childStack = childStack(
-    //     key = "NFTSendInnerStack",
-    //     source = stackNavigation,
-    //     serializer = null,
-    //     initialConfiguration = initialRoute,
-    //     handleBackButton = true,
-    //     childFactory = { configuration, factoryContext ->
-    //         // todo
-    //         ComposableContentComponent { }
-    //     },
-    // )
+    private val model: NFTSendModel = getOrCreateModel(params = params, router = innerRouter)
+
+    private val childStack = childStack(
+        key = "NFTSendInnerStack",
+        source = stackNavigation,
+        serializer = null,
+        initialConfiguration = initialRoute,
+        handleBackButton = true,
+        childFactory = { configuration, factoryContext ->
+            createChild(
+                configuration,
+                childByContext(
+                    componentContext = factoryContext,
+                    router = innerRouter,
+                ),
+            )
+        },
+    )
 
     init {
-        // childStack.subscribe(
-        //     lifecycle = lifecycle,
-        //     mode = ObserveLifecycleMode.CREATE_DESTROY,
-        // ) { stack ->
-        //     componentScope.launch {
-        //         when (val activeComponent = stack.active.instance) {
-        //             is SendDestinationComponent -> {
-        //                 // analyticsEventHandler.send(SendAnalyticEvents.AddressScreenOpened)
-        //                 activeComponent.updateState(model.uiState.value.destinationUM)
-        //             }
-        //             is SendFeeComponent -> {
-        //                 // analyticsEventHandler.send(SendAnalyticEvents.FeeScreenOpened)
-        //             }
-        //         }
-        //         currentRouteFlow.emit(stack.active.configuration)
-        //     }
-        // }
+        childStack.subscribe(
+            lifecycle = lifecycle,
+            mode = ObserveLifecycleMode.CREATE_DESTROY,
+        ) { stack ->
+            componentScope.launch {
+                when (val activeComponent = stack.active.instance) {
+                    is NFTSendConfirmComponent -> if (currentRouteFlow.value.isEditMode) {
+                        // analyticsEventHandler.send(SendAnalyticEvents.ConfirmationScreenOpened)
+                        activeComponent.updateState(model.uiState.value)
+                    }
+                    is SendDestinationComponent -> {
+                        // analyticsEventHandler.send(SendAnalyticEvents.AddressScreenOpened)
+                        activeComponent.updateState(model.uiState.value.destinationUM)
+                    }
+                    is SendFeeComponent -> {
+                        // analyticsEventHandler.send(SendAnalyticEvents.FeeScreenOpened)
+                    }
+                }
+                currentRouteFlow.emit(stack.active.configuration)
+            }
+        }
     }
 
     @Composable
     override fun Content(modifier: Modifier) {
-        // val stackState by childStack.subscribeAsState()
-        // val state by model.uiState.collectAsStateWithLifecycle()
+        val stackState by childStack.subscribeAsState()
+        val state by model.uiState.collectAsStateWithLifecycle()
 
         BackHandler(onBack = ::onChildBack)
-        // TODO
+        SendContent(
+            navigationUM = state.navigationUM,
+            stackState = stackState,
+        )
     }
 
+    private fun createChild(route: CommonSendRoute, factoryContext: AppComponentContext) = when (route) {
+        is CommonSendRoute.Destination -> getDestinationComponent(factoryContext, route)
+        is CommonSendRoute.Fee -> getFeeComponent(factoryContext)
+        CommonSendRoute.Confirm -> getConfirmComponent(factoryContext)
+        else -> getStubComponent()
+    }
+
+    private fun getDestinationComponent(factoryContext: AppComponentContext, route: CommonSendRoute) =
+        SendDestinationComponent(
+            appComponentContext = factoryContext,
+            params = SendDestinationComponentParams.DestinationParams(
+                state = model.uiState.value.destinationUM,
+                currentRoute = currentRouteFlow.filterIsInstance<CommonSendRoute.Destination>(),
+                isBalanceHidingFlow = model.isBalanceHiddenFlow,
+                analyticsCategoryName = "", // todo
+                userWalletId = params.userWalletId,
+                cryptoCurrency = model.cryptoCurrency,
+                callback = model,
+                onBackClick = ::onChildBack,
+                onNextClick = {
+                    if (route.isEditMode) {
+                        onChildBack()
+                    } else {
+                        innerRouter.push(CommonSendRoute.Confirm)
+                    }
+                },
+            ),
+        )
+
+    private fun getFeeComponent(factoryContext: AppComponentContext): ComposableContentComponent {
+        val state = model.uiState.value
+        val destinationAddress = (state.destinationUM as? DestinationUM.Content)?.addressTextField?.value
+        return if (destinationAddress != null) {
+            SendFeeComponent(
+                appComponentContext = factoryContext,
+                params = SendFeeComponentParams.FeeParams(
+                    state = model.uiState.value.feeUM,
+                    currentRoute = currentRouteFlow.filterIsInstance<CommonSendRoute.Fee>(),
+                    analyticsCategoryName = "", // todo
+                    userWallet = model.userWallet,
+                    cryptoCurrencyStatus = model.cryptoCurrencyStatus,
+                    feeCryptoCurrencyStatus = model.feeCryptoCurrencyStatus,
+                    appCurrency = model.appCurrency,
+                    sendAmount = BigDecimal.ZERO,
+                    destinationAddress = destinationAddress,
+                    callback = model,
+                    onNextClick = ::onChildBack,
+                ),
+            )
+        } else {
+            getStubComponent()
+        }
+    }
+
+    private fun getConfirmComponent(factoryContext: AppComponentContext) = NFTSendConfirmComponent(
+        appComponentContext = factoryContext,
+        nftDetailsBlockComponentFactory = nftDetailsBlockComponentFactory,
+        params = NFTSendConfirmComponent.Params(
+            state = model.uiState.value,
+            analyticsCategoryName = "", // todo
+            userWallet = model.userWallet,
+            nftAsset = params.nftAsset,
+            nftCollectionName = params.nftCollectionName,
+            cryptoCurrencyStatus = model.cryptoCurrencyStatus,
+            feeCryptoCurrencyStatus = model.feeCryptoCurrencyStatus,
+            appCurrency = model.appCurrency,
+            callback = model,
+            currentRoute = currentRouteFlow.filterIsInstance<CommonSendRoute.Confirm>(),
+            isBalanceHidingFlow = model.isBalanceHiddenFlow,
+        ),
+    )
+
+    private fun getStubComponent() = ComposableContentComponent { }
+
     private fun onChildBack() {
-        // TODO
-        // val isEmptyRoute = childStack.value.active.configuration == NFTSendRoute.Empty
-        // val isEmptyStack = childStack.value.backStack.isEmpty()
-        // val isSuccess = model.uiState.value.confirmUM is ConfirmUM.Success
-        //
-        // if (isEmptyRoute || isEmptyStack || isSuccess) {
-        //     router.pop()
-        // } else {
-        //     stackNavigation.pop()
-        // }
+        val isEmptyRoute = childStack.value.active.configuration == CommonSendRoute.Empty
+        val isEmptyStack = childStack.value.backStack.isEmpty()
+        val isSuccess = model.uiState.value.confirmUM is ConfirmUM.Success
+
+        if (isEmptyRoute || isEmptyStack || isSuccess) {
+            router.pop()
+        } else {
+            stackNavigation.pop()
+        }
     }
 
     @AssistedFactory
