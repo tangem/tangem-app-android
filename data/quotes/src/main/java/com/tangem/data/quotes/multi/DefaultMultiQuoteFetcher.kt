84f6b15dd8131938516f6c91192f7806f993dbfa
Tangem <git@tangem.com>
2025-04-10 11:51:40 +0400

    Updated on 2026-09-25

--- data/quotes/src/main/java/com/tangem/data/quotes/multi/DefaultMultiQuoteFetcher.kt
+++ data/quotes/src/main/java/com/tangem/data/quotes/multi/DefaultMultiQuoteFetcher.kt
@@ -6,9 +6,13 @@ import com.tangem.data.quotes.store.QuotesStoreV2
 import com.tangem.data.tokens.utils.QuotesUnsupportedCurrenciesIdAdapter
 import com.tangem.datasource.api.tangemTech.TangemTechApi
 import com.tangem.datasource.appcurrency.AppCurrencyResponseStore
+import com.tangem.domain.core.utils.catchOn
 import com.tangem.domain.quotes.multi.MultiQuoteFetcher
 import com.tangem.domain.tokens.model.CryptoCurrency
+import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import timber.log.Timber
+import javax.inject.Inject
+import javax.inject.Singleton
 
 /**
  * Default implementation of [MultiQuoteFetcher]
@@ -16,18 +20,26 @@ import timber.log.Timber
  * @property tangemTechApi            tangemTech api
  * @property appCurrencyResponseStore app currency response store
  * @property quotesStore              quotes store
+ * @property dispatchers              dispatchers
  *
 [REDACTED_AUTHOR]
  */
-internal class DefaultMultiQuoteFetcher(
+@Singleton
+internal class DefaultMultiQuoteFetcher @Inject constructor(
     private val tangemTechApi: TangemTechApi,
     private val appCurrencyResponseStore: AppCurrencyResponseStore,
     private val quotesStore: QuotesStoreV2,
+    private val dispatchers: CoroutineDispatcherProvider,
 ) : MultiQuoteFetcher {
 
     private val quotesUnsupportedCurrenciesAdapter = QuotesUnsupportedCurrenciesIdAdapter()
 
-    override suspend fun invoke(params: MultiQuoteFetcher.Params): Either<Throwable, Unit> = Either.catch {
+    override suspend fun invoke(params: MultiQuoteFetcher.Params) = Either.catchOn(dispatchers.default) {
+        if (params.currenciesIds.isEmpty()) {
+            Timber.d("No currencies to fetch quotes for")
+            return@catchOn
+        }
+
         quotesStore.refresh(currenciesIds = params.currenciesIds)
 
         val replacementIdsResult = quotesUnsupportedCurrenciesAdapter.replaceUnsupportedCurrencies(
@@ -37,26 +49,37 @@ internal class DefaultMultiQuoteFetcher(
             ),
         )
 
-        val appCurrency = appCurrencyResponseStore.getSyncOrNull()
-            ?: error(message = "Unable to get AppCurrency for updating quotes")
+        val appCurrencyId = getAppCurrencyId(params = params)
+        val coinIds = replacementIdsResult.idsForRequest.joinToString(separator = ",")
 
-        safeApiCallWithTimeout(
-            call = {
-                val coinIds = replacementIdsResult.idsForRequest.joinToString(separator = ",")
-                val response = tangemTechApi.getQuotes(currencyId = appCurrency.id, coinIds = coinIds).bind()
-
-                val updatedResponse = quotesUnsupportedCurrenciesAdapter.getResponseWithUnsupportedCurrencies(
-                    response = response,
-                    filteredIds = replacementIdsResult.idsFiltered,
-                )
-
-                quotesStore.storeActual(values = updatedResponse.quotes)
-            },
+        val response = safeApiCallWithTimeout(
+            call = { tangemTechApi.getQuotes(currencyId = appCurrencyId, coinIds = coinIds).bind() },
             onError = { error -> throw error },
         )
+
+        val updatedResponse = quotesUnsupportedCurrenciesAdapter.getResponseWithUnsupportedCurrencies(
+            response = response,
+            filteredIds = replacementIdsResult.idsFiltered,
+        )
+
+        quotesStore.storeActual(values = updatedResponse.quotes)
     }
         .onLeft {
             Timber.e(it)
             quotesStore.storeError(currenciesIds = params.currenciesIds)
         }
+
+    private suspend fun getAppCurrencyId(params: MultiQuoteFetcher.Params): String {
+        val appCurrencyId = params.appCurrencyId
+            ?: appCurrencyResponseStore.getSyncOrNull()?.id
+
+        if (appCurrencyId.isNullOrBlank()) {
+            val exception = IllegalStateException("Unable to get AppCurrency for updating quotes")
+            Timber.e(exception)
+
+            throw exception
+        }
+
+        return appCurrencyId
+    }
 }
