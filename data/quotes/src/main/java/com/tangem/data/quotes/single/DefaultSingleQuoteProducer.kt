ea7e614cb408e7c0c642ba7b10586fa3feca9d72
Tangem <git@tangem.com>
2025-04-10 11:25:34 +0400

    Updated on 2026-09-01

--- data/quotes/src/main/java/com/tangem/data/quotes/single/DefaultSingleQuoteProducer.kt
+++ data/quotes/src/main/java/com/tangem/data/quotes/single/DefaultSingleQuoteProducer.kt
@@ -3,11 +3,13 @@ package com.tangem.data.quotes.single
 import com.tangem.data.quotes.store.QuotesStoreV2
 import com.tangem.domain.quotes.single.SingleQuoteProducer
 import com.tangem.domain.tokens.model.Quote
+import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.mapNotNull
 
 /**
@@ -19,6 +21,7 @@ import kotlinx.coroutines.flow.mapNotNull
 internal class DefaultSingleQuoteProducer @AssistedInject constructor(
     @Assisted val params: SingleQuoteProducer.Params,
     private val quotesStore: QuotesStoreV2,
+    private val dispatchers: CoroutineDispatcherProvider,
 ) : SingleQuoteProducer {
 
     override val fallback: Quote = Quote.Empty(rawCurrencyId = params.rawCurrencyId)
@@ -27,6 +30,7 @@ internal class DefaultSingleQuoteProducer @AssistedInject constructor(
         return quotesStore.get()
             .mapNotNull { quotes -> quotes.firstOrNull { it.rawCurrencyId == params.rawCurrencyId } }
             .distinctUntilChanged()
+            .flowOn(dispatchers.default)
     }
 
     @AssistedFactory
