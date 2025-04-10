94865ce2506033a73f330037c0a5357732f02f8b
Tangem <git@tangem.com>
2025-04-10 11:25:34 +0400

    Updated on 2026-08-27

--- core/datasource/src/main/java/com/tangem/datasource/appcurrency/DefaultAppCurrencyResponseStore.kt
+++ core/datasource/src/main/java/com/tangem/datasource/appcurrency/DefaultAppCurrencyResponseStore.kt
@@ -3,7 +3,9 @@ package com.tangem.datasource.appcurrency
 import com.tangem.datasource.api.tangemTech.models.CurrenciesResponse
 import com.tangem.datasource.local.preferences.AppPreferencesStore
 import com.tangem.datasource.local.preferences.PreferencesKeys
+import com.tangem.datasource.local.preferences.utils.getObject
 import com.tangem.datasource.local.preferences.utils.getObjectSyncOrNull
+import kotlinx.coroutines.flow.Flow
 
 /**
  * Default implementation of [AppCurrencyResponseStore]
@@ -13,6 +15,10 @@ import com.tangem.datasource.local.preferences.utils.getObjectSyncOrNull
 internal class DefaultAppCurrencyResponseStore(
     private val appPreferencesStore: AppPreferencesStore,
 ) : AppCurrencyResponseStore {
+
+    override fun get(): Flow<CurrenciesResponse.Currency?> {
+        return appPreferencesStore.getObject(PreferencesKeys.SELECTED_APP_CURRENCY_KEY)
+    }
 
     override suspend fun getSyncOrNull(): CurrenciesResponse.Currency? {
         return appPreferencesStore.getObjectSyncOrNull<CurrenciesResponse.Currency>(
