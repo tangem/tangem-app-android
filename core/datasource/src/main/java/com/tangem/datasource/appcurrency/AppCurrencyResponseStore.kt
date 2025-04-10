2b823ed4a72cd65a053ede5455c4df6283421d5d
Tangem <git@tangem.com>
2025-04-10 11:25:34 +0400

    Updated on 2026-08-07

--- core/datasource/src/main/java/com/tangem/datasource/appcurrency/AppCurrencyResponseStore.kt
+++ core/datasource/src/main/java/com/tangem/datasource/appcurrency/AppCurrencyResponseStore.kt
@@ -1,6 +1,7 @@
 package com.tangem.datasource.appcurrency
 
 import com.tangem.datasource.api.tangemTech.models.CurrenciesResponse
+import kotlinx.coroutines.flow.Flow
 
 /**
  * Store of app currency data model [CurrenciesResponse.Currency]
@@ -9,5 +10,9 @@ import com.tangem.datasource.api.tangemTech.models.CurrenciesResponse
  */
 interface AppCurrencyResponseStore {
 
+    /** Get flow of [CurrenciesResponse.Currency] */
+    fun get(): Flow<CurrenciesResponse.Currency?>
+
+    /** Get [CurrenciesResponse.Currency] synchronously or null */
     suspend fun getSyncOrNull(): CurrenciesResponse.Currency?
 }
