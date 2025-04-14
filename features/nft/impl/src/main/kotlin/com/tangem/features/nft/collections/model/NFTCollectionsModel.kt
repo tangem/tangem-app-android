02b1d36c8916e6f2dd4e0678c807262be897c05c
Tangem <git@tangem.com>
2025-04-14 12:12:25 +0500

    Updated on 2025-12-18

--- features/nft/impl/src/main/kotlin/com/tangem/features/nft/collections/model/NFTCollectionsModel.kt
+++ features/nft/impl/src/main/kotlin/com/tangem/features/nft/collections/model/NFTCollectionsModel.kt
@@ -1,24 +1,21 @@
 package com.tangem.features.nft.collections.model
 
-import com.tangem.common.routing.AppRoute
 import com.tangem.core.decompose.di.ModelScoped
 import com.tangem.core.decompose.model.Model
 import com.tangem.core.decompose.model.ParamsContainer
-import com.tangem.core.decompose.navigation.Router
 import com.tangem.core.ui.components.fields.InputManager
 import com.tangem.core.ui.components.fields.entity.SearchBarUM
 import com.tangem.core.ui.extensions.resourceReference
 import com.tangem.domain.nft.FetchNFTCollectionAssetsUseCase
 import com.tangem.domain.nft.GetNFTCollectionsUseCase
-import com.tangem.domain.nft.models.NFTAsset
 import com.tangem.domain.nft.models.NFTCollection
+import com.tangem.features.nft.collections.NFTCollectionsComponent
 import com.tangem.features.nft.collections.entity.NFTCollectionsStateUM
 import com.tangem.features.nft.collections.entity.NFTCollectionsUM
 import com.tangem.features.nft.collections.entity.transformer.ChangeCollectionExpandedStateTransformer
 import com.tangem.features.nft.collections.entity.transformer.ToggleSearchBarTransformer
 import com.tangem.features.nft.collections.entity.transformer.UpdateDataStateTransformer
 import com.tangem.features.nft.collections.entity.transformer.UpdateSearchQueryTransformer
-import com.tangem.features.nft.component.NFTCollectionsComponent
 import com.tangem.features.nft.impl.R
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import kotlinx.coroutines.flow.*
@@ -29,23 +26,22 @@ import javax.inject.Inject
 @ModelScoped
 internal class NFTCollectionsModel @Inject constructor(
     override val dispatchers: CoroutineDispatcherProvider,
-    private val router: Router,
     private val searchManager: InputManager,
     private val getNFTCollectionsUseCase: GetNFTCollectionsUseCase,
     private val fetchNFTCollectionAssetsUseCase: FetchNFTCollectionAssetsUseCase,
     paramsContainer: ParamsContainer,
 ) : Model() {
 
-    val state: StateFlow<NFTCollectionsStateUM> get() = _state
+    private val params: NFTCollectionsComponent.Params = paramsContainer.require()
 
     private val _state = MutableStateFlow(
         value = NFTCollectionsStateUM(
-            onBackClick = ::navigateBack,
-            content = NFTCollectionsUM.Loading(::onReceiveClick),
+            onBackClick = params.onBackClick,
+            content = NFTCollectionsUM.Loading(params.onBackClick),
         ),
     )
 
-    private val params: NFTCollectionsComponent.Params = paramsContainer.require()
+    val state: StateFlow<NFTCollectionsStateUM> get() = _state
 
     init {
         subscribeToNFTCollections()
@@ -60,11 +56,15 @@ internal class NFTCollectionsModel @Inject constructor(
                 UpdateDataStateTransformer(
                     nftCollections = nftCollections,
                     searchQuery = query,
-                    onReceiveClick = ::onReceiveClick,
+                    onReceiveClick = {
+                        params.onReceiveClick()
+                    },
                     onRetryClick = ::onRetryClick,
                     onExpandCollectionClick = ::onExpandCollectionClick,
                     onRetryAssetsClick = ::onRetryAssetsClick,
-                    onAssetClick = ::onAssetClick,
+                    onAssetClick = { asset, collectionName ->
+                        params.onAssetClick(asset, collectionName)
+                    },
                     initialSearchBarFactory = ::getInitialSearchBar,
                 ).transform(it)
             }
@@ -113,28 +113,6 @@ internal class NFTCollectionsModel @Inject constructor(
 
     private fun onRetryClick() {
         // TODO refresh all
-    }
-
-    private fun onAssetClick(asset: NFTAsset, collectionName: String) {
-        router.push(
-            AppRoute.NFTDetails(
-                userWalletId = params.userWalletId,
-                nftAsset = asset,
-                collectionName = collectionName,
-            ),
-        )
-    }
-
-    private fun onReceiveClick() {
-        router.push(
-            AppRoute.NFTReceive(
-                userWalletId = params.userWalletId,
-            ),
-        )
-    }
-
-    private fun navigateBack() {
-        router.pop()
     }
 
     private fun loadCollectionAssets(collection: NFTCollection) {
