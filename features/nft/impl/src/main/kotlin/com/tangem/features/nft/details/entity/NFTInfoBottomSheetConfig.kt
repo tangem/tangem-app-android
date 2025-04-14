d0b078a793469bc9a0b6169951b4f2be8fa8d171
Tangem <git@tangem.com>
2025-04-14 12:24:12 +0500

    Updated on 2026-08-14

--- features/nft/impl/src/main/kotlin/com/tangem/features/nft/details/entity/NFTInfoBottomSheetConfig.kt
+++
@@ -1,9 +0,0 @@
-package com.tangem.features.nft.details.entity
-
-import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfigContent
-import com.tangem.core.ui.extensions.TextReference
-
-internal data class NFTInfoBottomSheetConfig(
-    val title: TextReference,
-    val text: TextReference,
-) : TangemBottomSheetConfigContent
