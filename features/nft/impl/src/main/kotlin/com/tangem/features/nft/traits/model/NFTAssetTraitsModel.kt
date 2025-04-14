95c9cd484de1f4eeb50fed34e5b7675fc7d4f529
Tangem <git@tangem.com>
2025-04-14 12:12:25 +0500

    Updated on 2026-05-19

--- features/nft/impl/src/main/kotlin/com/tangem/features/nft/traits/model/NFTAssetTraitsModel.kt
+++ features/nft/impl/src/main/kotlin/com/tangem/features/nft/traits/model/NFTAssetTraitsModel.kt
@@ -3,9 +3,8 @@ package com.tangem.features.nft.traits.model
 import com.tangem.core.decompose.di.ModelScoped
 import com.tangem.core.decompose.model.Model
 import com.tangem.core.decompose.model.ParamsContainer
-import com.tangem.core.decompose.navigation.Router
 import com.tangem.domain.nft.models.NFTAsset
-import com.tangem.features.nft.component.NFTAssetTraitsComponent
+import com.tangem.features.nft.traits.NFTAssetTraitsComponent
 import com.tangem.features.nft.traits.entity.NFTAssetTraitUM
 import com.tangem.features.nft.traits.entity.NFTAssetTraitsUM
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
@@ -17,20 +16,19 @@ import javax.inject.Inject
 @ModelScoped
 internal class NFTAssetTraitsModel @Inject constructor(
     override val dispatchers: CoroutineDispatcherProvider,
-    private val router: Router,
     paramsContainer: ParamsContainer,
 ) : Model() {
 
     private val params: NFTAssetTraitsComponent.Params = paramsContainer.require()
 
-    val state: StateFlow<NFTAssetTraitsUM> get() = _state
-
     private val _state = MutableStateFlow(
         value = NFTAssetTraitsUM(
             traits = params.nftAsset.transform(),
-            onBackClick = ::navigateBack,
+            onBackClick = { params.onBackClick() },
         ),
     )
+
+    val state: StateFlow<NFTAssetTraitsUM> get() = _state
 
     private fun NFTAsset.transform(): ImmutableList<NFTAssetTraitUM> = this
         .traits
@@ -41,8 +39,4 @@ internal class NFTAssetTraitsModel @Inject constructor(
                 value = trait.value,
             )
         }.toPersistentList()
-
-    private fun navigateBack() {
-        router.pop()
-    }
 }
