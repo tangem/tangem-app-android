ffff3efdefd41f6e34574846c8a0a32487114f0c
Tangem <git@tangem.com>
2025-04-14 12:12:25 +0500

    Updated on 2026-06-10

--- features/nft/impl/src/main/kotlin/com/tangem/features/nft/receive/NFTReceiveComponent.kt
+++ features/nft/impl/src/main/kotlin/com/tangem/features/nft/receive/NFTReceiveComponent.kt
@@ -6,17 +6,17 @@ import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.tangem.core.decompose.context.AppComponentContext
 import com.tangem.core.decompose.model.getOrCreateModel
-import com.tangem.features.nft.component.NFTReceiveComponent
+import com.tangem.core.ui.decompose.ComposableContentComponent
+import com.tangem.domain.wallets.models.UserWalletId
 import com.tangem.features.nft.receive.model.NFTReceiveModel
 import com.tangem.features.nft.receive.ui.NFTReceive
 import dagger.assisted.Assisted
-import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 
-internal class DefaultNFTReceiveComponent @AssistedInject constructor(
+internal class NFTReceiveComponent @AssistedInject constructor(
     @Assisted context: AppComponentContext,
-    @Assisted private val params: NFTReceiveComponent.Params,
-) : NFTReceiveComponent, AppComponentContext by context {
+    @Assisted private val params: Params,
+) : ComposableContentComponent, AppComponentContext by context {
 
     private val model: NFTReceiveModel = getOrCreateModel(params)
 
@@ -27,11 +27,8 @@ internal class DefaultNFTReceiveComponent @AssistedInject constructor(
         NFTReceive(state, modifier)
     }
 
-    @AssistedFactory
-    interface Factory : NFTReceiveComponent.Factory {
-        override fun create(
-            context: AppComponentContext,
-            params: NFTReceiveComponent.Params,
-        ): DefaultNFTReceiveComponent
-    }
+    data class Params(
+        val userWalletId: UserWalletId,
+        val onBackClick: () -> Unit,
+    )
 }
