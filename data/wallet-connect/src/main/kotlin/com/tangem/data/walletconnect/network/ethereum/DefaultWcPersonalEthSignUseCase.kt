a480f97d43a5192ff02c62382e060c9235c1e599
Tangem <git@tangem.com>
2025-04-15 16:40:35 +0700

    Updated on 2026-09-14

--- data/wallet-connect/src/main/kotlin/com/tangem/data/walletconnect/network/ethereum/DefaultWcPersonalEthSignUseCase.kt
+++ data/wallet-connect/src/main/kotlin/com/tangem/data/walletconnect/network/ethereum/DefaultWcPersonalEthSignUseCase.kt
@@ -1,12 +1,10 @@
 package com.tangem.data.walletconnect.network.ethereum
 
 import arrow.core.left
-import arrow.core.right
 import com.tangem.blockchain.blockchains.ethereum.EthereumUtils
 import com.tangem.blockchain.blockchains.ethereum.EthereumUtils.toKeccak
 import com.tangem.blockchain.common.HEX_PREFIX
 import com.tangem.blockchain.extensions.isAscii
-import com.tangem.common.CompletionResult
 import com.tangem.common.extensions.hexToBytes
 import com.tangem.common.extensions.toDecompressedPublicKey
 import com.tangem.data.walletconnect.respond.WcRespondService
@@ -14,9 +12,7 @@ import com.tangem.data.walletconnect.sign.BaseWcSignUseCase
 import com.tangem.data.walletconnect.sign.OnSign
 import com.tangem.data.walletconnect.sign.SignStateConverter.toResult
 import com.tangem.data.walletconnect.sign.WcMethodUseCaseContext
-import com.tangem.domain.card.models.TwinKey
-import com.tangem.domain.card.repository.CardSdkConfigRepository
-import com.tangem.domain.common.TapWorkarounds.isTangemTwins
+import com.tangem.domain.transaction.usecase.SignUseCase
 import com.tangem.domain.walletconnect.model.WcEthMethod
 import com.tangem.domain.walletconnect.usecase.ethereum.WcPersonalEthSignUseCase
 import com.tangem.domain.walletconnect.usecase.sign.WcSignState
@@ -30,29 +26,17 @@ internal class DefaultWcPersonalEthSignUseCase(
     override val context: WcMethodUseCaseContext,
     private val method: WcEthMethod.PersonalEthSign,
     private val walletManagersFacade: WalletManagersFacade,
-    private val cardRepository: CardSdkConfigRepository,
+    private val signUseCase: SignUseCase,
 ) : BaseWcSignUseCase<Nothing, WcPersonalEthSignUseCase.SignModel>(),
     WcPersonalEthSignUseCase {
 
     override val onSign: OnSign<WcPersonalEthSignUseCase.SignModel> = collector@{ state ->
         val hashToSign = LegacySdkHelper.createMessageData(state.signModel.rawMsg)
         val userWallet = session.wallet
-        val card = userWallet.scanResponse.card
-        val isCardNotBackedUp = card.backupStatus?.isActive != true && !card.isTangemTwins
         val walletManager = walletManagersFacade.getOrCreateWalletManager(userWallet.walletId, network)
             ?: return@collector
 
-        val signer = cardRepository.getCommonSigner(
-            cardId = card.cardId.takeIf { isCardNotBackedUp },
-            twinKey = TwinKey.getOrNull(scanResponse = userWallet.scanResponse),
-        )
-
-        val signResult = when (val signResult = signer.sign(hashToSign, walletManager.wallet.publicKey)) {
-            is CompletionResult.Failure -> signResult.error.left()
-            is CompletionResult.Success -> signResult.data.right()
-        }
-
-        val signedHash = signResult
+        val signedHash = signUseCase(hashToSign, userWallet, network)
             .onLeft { emit(state.toResult(it.left())) }
             .getOrNull() ?: return@collector
 
