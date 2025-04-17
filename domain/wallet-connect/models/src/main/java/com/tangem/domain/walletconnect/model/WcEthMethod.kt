5243d86676bfb26c42a6c5af1af5786f7fd5b9e6
Tangem <git@tangem.com>
2025-04-17 21:50:36 +0700

    Updated on 2026-09-23

--- domain/wallet-connect/models/src/main/java/com/tangem/domain/walletconnect/model/WcEthMethod.kt
+++ domain/wallet-connect/models/src/main/java/com/tangem/domain/walletconnect/model/WcEthMethod.kt
@@ -2,8 +2,16 @@ package com.tangem.domain.walletconnect.model
 
 sealed interface WcEthMethod : WcMethod {
 
-    data class PersonalEthSign(
+    data class MessageSign(
         val message: String,
         val account: String,
+    ) : WcEthMethod
+
+    data class SendTransaction(
+        val transaction: WcEthTransactionParams,
+    ) : WcEthMethod
+
+    data class SignTransaction(
+        val transaction: WcEthTransactionParams,
     ) : WcEthMethod
 }
