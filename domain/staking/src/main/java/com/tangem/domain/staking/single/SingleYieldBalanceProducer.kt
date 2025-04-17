fdc2e87793688ccf7dc691092d269d4b1ad1c133
Tangem <git@tangem.com>
2025-04-17 18:00:29 +0300

    Updated on 2026-02-11

---
+++ domain/staking/src/main/java/com/tangem/domain/staking/single/SingleYieldBalanceProducer.kt
@@ -0,0 +1,23 @@
+package com.tangem.domain.staking.single
+
+import com.tangem.domain.core.flow.FlowProducer
+import com.tangem.domain.staking.model.stakekit.YieldBalance
+import com.tangem.domain.tokens.model.CryptoCurrency
+import com.tangem.domain.tokens.model.Network
+import com.tangem.domain.wallets.models.UserWalletId
+
+/**
+ * Producer of yield balance for selected wallet [UserWalletId]
+ *
+[REDACTED_AUTHOR]
+ */
+interface SingleYieldBalanceProducer : FlowProducer<YieldBalance> {
+
+    data class Params(
+        val userWalletId: UserWalletId,
+        val currencyId: CryptoCurrency.ID,
+        val network: Network,
+    )
+
+    interface Factory : FlowProducer.Factory<Params, SingleYieldBalanceProducer>
+}
