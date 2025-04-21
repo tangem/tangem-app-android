9cf48fb6607311b1ee064555d0fdbc84dce2f647
Tangem <git@tangem.com>
2025-04-21 14:32:52 +0400

    Updated on 2026-04-08

--- data/blockaid/src/main/kotlin/com/tangem/data/blockaid/di/BlockAidDataInternalModule.kt
+++ data/blockaid/src/main/kotlin/com/tangem/data/blockaid/di/BlockAidDataInternalModule.kt
@@ -1,18 +1,27 @@
 package com.tangem.data.blockaid.di
 
+import com.tangem.data.blockaid.BlockAidMapper
 import com.tangem.data.blockaid.BlockAidRepository
 import com.tangem.data.blockaid.DefaultBlockAidRepository
-import dagger.Binds
+import com.tangem.datasource.api.common.blockaid.BlockAidApi
+import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-internal interface BlockAidDataInternalModule {
+internal object BlockAidDataInternalModule {
 
-    @Binds
+    @Provides
     @Singleton
-    fun bindRepository(repository: DefaultBlockAidRepository): BlockAidRepository
+    fun provideRepository(api: BlockAidApi, dispatcherProvider: CoroutineDispatcherProvider): BlockAidRepository {
+        return DefaultBlockAidRepository(
+            api = api,
+            dispatcherProvider = dispatcherProvider,
+            mapper = BlockAidMapper(),
+        )
+    }
 }
