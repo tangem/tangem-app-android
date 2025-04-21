801f988634732dea8fcf46a4e7e76e4121516c34
Tangem <git@tangem.com>
2025-04-21 15:03:17 +0300

    Updated on 2025-11-03

--- common/test/build.gradle.kts
+++ common/test/build.gradle.kts
@@ -17,6 +17,7 @@ dependencies {
 
     implementation(projects.domain.legacy)
     implementation(projects.domain.models)
+    implementation(projects.domain.staking.models)
     implementation(projects.domain.tokens.models)
     implementation(projects.domain.wallets)
     implementation(projects.domain.wallets.models)
