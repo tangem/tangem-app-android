b58d15c6cf925dcd1554819b145822f192b3630e
Tangem <git@tangem.com>
2025-04-21 14:32:52 +0400

    Updated on 2026-09-21

--- domain/wallet-connect/models/src/main/java/com/tangem/domain/walletconnect/model/WcSession.kt
+++ domain/wallet-connect/models/src/main/java/com/tangem/domain/walletconnect/model/WcSession.kt
@@ -1,9 +1,11 @@
 package com.tangem.domain.walletconnect.model
 
+import com.domain.blockaid.models.dapp.CheckDAppResult
 import com.tangem.domain.walletconnect.model.sdkcopy.WcSdkSession
 import com.tangem.domain.wallets.models.UserWallet
 
 data class WcSession(
     val wallet: UserWallet,
     val sdkModel: WcSdkSession,
+    val securityStatus: CheckDAppResult,
 )
