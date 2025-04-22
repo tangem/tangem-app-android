d861634865493d8c2f700ffe0d2cfe42df347a43
Tangem <git@tangem.com>
2025-04-22 16:28:48 +0700

    Updated on 2025-12-18

--- domain/wallet-connect/src/main/kotlin/com/tangem/domain/walletconnect/usecase/WcUseCase.kt
+++ domain/wallet-connect/src/main/kotlin/com/tangem/domain/walletconnect/usecase/WcUseCase.kt
@@ -1,5 +1,6 @@
 package com.tangem.domain.walletconnect.usecase
 
+import com.tangem.blockchain.common.transaction.Fee
 import com.tangem.domain.tokens.model.Network
 import com.tangem.domain.walletconnect.model.WcSession
 import com.tangem.domain.walletconnect.model.sdkcopy.WcSdkSessionRequest
@@ -10,4 +11,8 @@ interface WcMethodUseCase : WcUseCase {
     val session: WcSession
     val rawSdkRequest: WcSdkSessionRequest
     val network: Network
+}
+
+interface WcMutableFee {
+    fun updateFee(fee: Fee)
 }
