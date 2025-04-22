76c9af3bc4982b9f4c11a0a897dd16f8616991c2
Tangem <git@tangem.com>
2025-04-22 17:19:42 +0500

    Updated on 2026-08-19

---
+++ data/notifications/src/main/java/com/tangem/data/notifications/di/NotificationsModule.kt
@@ -0,0 +1,18 @@
+package com.tangem.data.notifications.di
+
+import com.tangem.data.notifications.DefaultNotificationsRepository
+import com.tangem.domain.notifications.NotificationsRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal interface NotificationsModule {
+
+    @Binds
+    @Singleton
+    fun bindNotificationsRepository(repository: DefaultNotificationsRepository): NotificationsRepository
+}
