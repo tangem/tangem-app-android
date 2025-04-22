2145c174eac3e1514ef4dd337a89d0dfc68df464
Tangem <git@tangem.com>
2025-04-22 14:06:01 +0300

    Updated on 2026-05-04

---
+++ domain/staking/src/main/java/com/tangem/domain/staking/multi/MultiYieldBalanceFetcher.kt
@@ -0,0 +1,11 @@
+package com.tangem.domain.staking.multi
+
+import com.tangem.domain.core.flow.FlowFetcher
+import com.tangem.domain.staking.fetcher.YieldBalanceFetcherParams
+
+/**
+ * Fetcher of yields balances
+ *
+[REDACTED_AUTHOR]
+ */
+interface MultiYieldBalanceFetcher : FlowFetcher<YieldBalanceFetcherParams.Multi>
