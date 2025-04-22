a7a87e1523d4590c2b86782ee39f19d2cbad0e34
Tangem <git@tangem.com>
2025-04-22 17:19:42 +0500

    Updated on 2026-09-25

--- core/datasource/src/main/java/com/tangem/datasource/api/tangemTech/models/WalletResponse.kt
+++ core/datasource/src/main/java/com/tangem/datasource/api/tangemTech/models/WalletResponse.kt
@@ -5,7 +5,7 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class WalletResponse(
-    @Json(name = "notifyStatus") val notifyStatus: String? = null,
-    @Json(name = "name") val name: String? = null,
+    @Json(name = "notifyStatus") val notifyStatus: Boolean,
     @Json(name = "id") val id: String,
+    @Json(name = "name") val name: String? = null,
 )
