90b03aa3f9202de892ed886e3571af18e2e90bcc
Tangem <git@tangem.com>
2025-04-22 17:19:42 +0500

    Updated on 2026-09-14

---
+++ domain/notifications/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    alias(deps.plugins.kotlin.jvm)
+    alias(deps.plugins.kotlin.serialization)
+    id("configuration")
+}
+
+dependencies {
+    implementation(deps.arrow.core)
+    implementation(deps.kotlin.coroutines)
+
+    implementation(projects.domain.notifications.models)
+}
