1292bb2e4807aad1213ef3459c12fb1c3cda9a8c
Tangem <git@tangem.com>
2025-04-22 11:52:10 +0300

    Updated on 2025-09-17

--- domain/visa/models/src/main/kotlin/com/tangem/domain/visa/model/VisaCardWalletDataToSignRequest.kt
+++ domain/visa/models/src/main/kotlin/com/tangem/domain/visa/model/VisaCardWalletDataToSignRequest.kt
@@ -1,3 +1,12 @@
 package com.tangem.domain.visa.model
 
-typealias VisaCardWalletDataToSignRequest = VisaActivationOrderInfo
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import kotlinx.serialization.Serializable
+
+@Serializable
+@JsonClass(generateAdapter = true)
+data class VisaCardWalletDataToSignRequest(
+    @Json(name = "activationOrderInfo") val activationOrderInfo: VisaActivationOrderInfo,
+    @Json(name = "cardWalletAddress") val cardWalletAddress: String,
+)
