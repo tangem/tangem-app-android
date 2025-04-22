899d1fc787563f5da08ceff2b3b566b794f96624
Tangem <git@tangem.com>
2025-04-22 16:28:48 +0700

    release/5.26.1

--- data/wallet-connect/src/main/kotlin/com/tangem/data/walletconnect/network/ethereum/DefaultWcEthSendTransactionUseCase.kt
+++ data/wallet-connect/src/main/kotlin/com/tangem/data/walletconnect/network/ethereum/DefaultWcEthSendTransactionUseCase.kt
@@ -1,50 +1,53 @@
 package com.tangem.data.walletconnect.network.ethereum
 
 import arrow.core.left
-import com.tangem.blockchain.common.HEX_PREFIX
-import com.tangem.blockchain.common.TransactionData
-import com.tangem.blockchain.common.transaction.TransactionFee
+import com.tangem.blockchain.common.transaction.Fee
+import com.tangem.blockchain.extensions.formatHex
 import com.tangem.data.walletconnect.respond.WcRespondService
 import com.tangem.data.walletconnect.sign.BaseWcSignUseCase
-import com.tangem.data.walletconnect.sign.OnSign
+import com.tangem.data.walletconnect.sign.SignCollector
 import com.tangem.data.walletconnect.sign.SignStateConverter.toResult
 import com.tangem.data.walletconnect.sign.WcMethodUseCaseContext
 import com.tangem.domain.transaction.usecase.SendTransactionUseCase
 import com.tangem.domain.walletconnect.model.WcEthMethod
 import com.tangem.domain.walletconnect.usecase.ethereum.WcEthSendTransactionUseCase
+import com.tangem.domain.walletconnect.usecase.ethereum.WcEthTransaction
 import com.tangem.domain.walletconnect.usecase.sign.WcSignState
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
 
 internal class DefaultWcEthSendTransactionUseCase @AssistedInject constructor(
     @Assisted override val context: WcMethodUseCaseContext,
     @Assisted private val method: WcEthMethod.SendTransaction,
     override val respondService: WcRespondService,
     private val sendTransaction: SendTransactionUseCase,
-) : BaseWcSignUseCase<Nothing, TransactionData>(),
+) : BaseWcSignUseCase<Fee, WcEthTransaction>(),
     WcEthSendTransactionUseCase {
 
-    override val onSign: OnSign<TransactionData> = collector@{ state ->
-        val hash = sendTransaction(state.signModel, wallet, network)
+    private val converter = EthTransactionParamsConverter(context)
+
+    override suspend fun SignCollector<WcEthTransaction>.onSign(state: WcSignState<WcEthTransaction>) {
+        val hash = sendTransaction(state.signModel.transactionData, wallet, network)
             .onLeft { error ->
                 val sendError = IllegalArgumentException(error.toString()) // todo(wc) use domain error
                 emit(state.toResult(sendError.left()))
             }
-            .getOrNull() ?: return@collector
-        val respondHash = if (hash.startsWith(HEX_PREFIX)) hash else HEX_PREFIX + hash
-        val respondResult = respondService.respond(rawSdkRequest, respondHash)
+            .getOrNull() ?: return
+        val respondResult = respondService.respond(rawSdkRequest, hash.formatHex())
         emit(state.toResult(respondResult))
     }
 
-    override fun updateFee(fee: TransactionFee) {
-        TODO("Not yet implemented")
+    override fun updateFee(fee: Fee) {
+        middleAction(fee)
     }
 
-    override fun invoke(): Flow<WcSignState<TransactionData>> {
-        method
-        TODO("Not yet implemented")
+    override fun invoke(): Flow<WcSignState<WcEthTransaction>> = flow {
+        val ethTransaction = converter.convert(method.transaction) ?: return@flow
+        emitAll(delegate.invoke(ethTransaction))
     }
 
     @AssistedFactory
--- domain/wallet-connect/src/main/kotlin/com/tangem/domain/walletconnect/usecase/ethereum/WcEthTransactionUseCase.kt
+++ domain/wallet-connect/src/main/kotlin/com/tangem/domain/walletconnect/usecase/ethereum/WcEthTransactionUseCase.kt
@@ -1,24 +1,42 @@
 package com.tangem.domain.walletconnect.usecase.ethereum
 
 import com.tangem.blockchain.common.TransactionData
-import com.tangem.blockchain.common.transaction.TransactionFee
+import com.tangem.blockchain.common.transaction.Fee
+import com.tangem.domain.tokens.model.Network
+import com.tangem.domain.walletconnect.model.WcSession
+import com.tangem.domain.walletconnect.model.sdkcopy.WcSdkSessionRequest
+import com.tangem.domain.walletconnect.usecase.WcMutableFee
 import com.tangem.domain.walletconnect.usecase.sign.WcSignUseCase
 
 interface WcEthSendTransactionUseCase :
     WcSignUseCase,
-    WcSignUseCase.SimpleRun<TransactionData> {
+    WcSignUseCase.SimpleRun<WcEthTransaction>,
+    WcMutableFee {
 
-    fun updateFee(fee: TransactionFee)
+    override val session: WcSession
+    override val rawSdkRequest: WcSdkSessionRequest
+    override val network: Network
+
+    override fun sign()
+    override fun cancel()
+    override fun updateFee(fee: Fee)
 }
 
 interface WcEthSignTransactionUseCase :
     WcSignUseCase,
-    WcSignUseCase.SimpleRun<TransactionData> {
+    WcSignUseCase.SimpleRun<WcEthTransaction>,
+    WcMutableFee {
 
-    fun updateFee(fee: TransactionFee)
+    override val session: WcSession
+    override val rawSdkRequest: WcSdkSessionRequest
+    override val network: Network
+
+    override fun sign()
+    override fun cancel()
+    override fun updateFee(fee: Fee)
 }
 
 data class WcEthTransaction(
-    val isFeeByDap: Boolean,
-    val fee: TransactionFee,
+    val dAppFee: Fee.Ethereum.Legacy?,
+    val transactionData: TransactionData.Uncompiled,
 )
