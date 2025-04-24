6a96486cb359b84043064d1e83e8a94de0c92b0d
Tangem <git@tangem.com>
2025-04-24 17:40:50 +0300

    Updated on 2026-07-07

--- core/datasource/src/main/java/com/tangem/datasource/api/common/config/TangemVisa.kt
+++ core/datasource/src/main/java/com/tangem/datasource/api/common/config/TangemVisa.kt
@@ -7,14 +7,21 @@ internal class TangemVisa(
     private val appVersionProvider: AppVersionProvider,
 ) : ApiConfig() {
 
-    override val defaultEnvironment: ApiEnvironment = ApiEnvironment.PROD
+    override val defaultEnvironment: ApiEnvironment = ApiEnvironment.DEV
 
     override val environmentConfigs = listOf(
         createProdEnvironment(),
+        createDevEnvironment(),
     )
 
     private fun createProdEnvironment(): ApiEnvironmentConfig = ApiEnvironmentConfig(
         environment = ApiEnvironment.PROD,
+        baseUrl = "https://api.paera.com/bff/",
+        headers = createHeaders(),
+    )
+
+    private fun createDevEnvironment(): ApiEnvironmentConfig = ApiEnvironmentConfig(
+        environment = ApiEnvironment.DEV,
         baseUrl = "[REDACTED_ENV_URL]",
         headers = createHeaders(),
     )
