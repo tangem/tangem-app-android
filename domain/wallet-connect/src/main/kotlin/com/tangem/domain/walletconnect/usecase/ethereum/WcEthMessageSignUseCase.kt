4b69cc1f7f6982ebe7497d405ba34273d1ea951e
Tangem <git@tangem.com>
2025-04-24 13:02:08 +0400

    Updated on 2026-09-21

--- domain/wallet-connect/src/main/kotlin/com/tangem/domain/walletconnect/usecase/ethereum/WcEthMessageSignUseCase.kt
+++ domain/wallet-connect/src/main/kotlin/com/tangem/domain/walletconnect/usecase/ethereum/WcEthMessageSignUseCase.kt
@@ -1,10 +1,12 @@
 package com.tangem.domain.walletconnect.usecase.ethereum
 
+import com.tangem.domain.walletconnect.usecase.blockaid.WcBlockAidEligibleTransactionUseCase
 import com.tangem.domain.walletconnect.usecase.sign.WcSignUseCase
 
 interface WcEthMessageSignUseCase :
     WcSignUseCase,
-    WcSignUseCase.SimpleRun<WcEthMessageSignUseCase.SignModel> {
+    WcSignUseCase.SimpleRun<WcEthMessageSignUseCase.SignModel>,
+    WcBlockAidEligibleTransactionUseCase {
 
     data class SignModel(
         val humanMsg: String,
