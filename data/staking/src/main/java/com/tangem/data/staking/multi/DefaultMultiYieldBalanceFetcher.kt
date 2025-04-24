2b89649660a7c6b4581eb7629c0a40c8f14140cd
Tangem <git@tangem.com>
2025-04-24 11:23:21 +0300

    Updated on 2026-09-25

--- data/staking/src/main/java/com/tangem/data/staking/multi/DefaultMultiYieldBalanceFetcher.kt
+++ data/staking/src/main/java/com/tangem/data/staking/multi/DefaultMultiYieldBalanceFetcher.kt
@@ -6,6 +6,7 @@ import com.tangem.data.staking.store.YieldsBalancesStore
 import com.tangem.data.staking.utils.StakingIdFactory
 import com.tangem.datasource.api.stakekit.StakeKitApi
 import com.tangem.datasource.local.token.StakingYieldsStore
+import com.tangem.datasource.local.userwallet.UserWalletsStore
 import com.tangem.domain.core.flow.FlowFetcher
 import com.tangem.domain.staking.fetcher.YieldBalanceFetcherParams
 import com.tangem.domain.staking.multi.MultiYieldBalanceFetcher
@@ -15,6 +16,7 @@ import javax.inject.Inject
 /**
  * Default implementation of [MultiYieldBalanceFetcher]
  *
+ * @property userWalletsStore    user wallets store
  * @property stakingYieldsStore  staking yields store
  * @property yieldsBalancesStore yields balances store
  * @property stakingIdFactory    factory for creating StakingID
@@ -24,6 +26,7 @@ import javax.inject.Inject
 [REDACTED_AUTHOR]
  */
 internal class DefaultMultiYieldBalanceFetcher @Inject constructor(
+    private val userWalletsStore: UserWalletsStore,
     private val stakingYieldsStore: StakingYieldsStore,
     private val yieldsBalancesStore: YieldsBalancesStore,
     private val stakingIdFactory: StakingIdFactory,
@@ -32,6 +35,7 @@ internal class DefaultMultiYieldBalanceFetcher @Inject constructor(
 ) : MultiYieldBalanceFetcher,
     FlowFetcher<YieldBalanceFetcherParams.Multi> by commonFetcher(
         implementor = createMultiFetcherImplementor(yieldsBalancesStore, stakingIdFactory, stakeKitApi, dispatchers),
+        userWalletsStore = userWalletsStore,
         stakingYieldsStore = stakingYieldsStore,
         yieldsBalancesStore = yieldsBalancesStore,
         dispatchers = dispatchers,
