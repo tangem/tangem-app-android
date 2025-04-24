a789e9538722bde3bc7eeea648f5f014d1c1f8ee
Tangem <git@tangem.com>
2025-04-24 18:33:04 +0400

    release/5.27.0

--- features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/model/WcEthereumMessageSignRequestModel.kt
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/model/WcEthereumMessageSignRequestModel.kt
@@ -5,21 +5,21 @@ import com.domain.blockaid.models.dapp.CheckDAppResult
 import com.tangem.core.decompose.di.ModelScoped
 import com.tangem.core.decompose.model.Model
 import com.tangem.core.decompose.model.ParamsContainer
+import com.tangem.core.decompose.navigation.Router
 import com.tangem.core.ui.clipboard.ClipboardManager
 import com.tangem.core.ui.extensions.getActiveIconRes
 import com.tangem.core.ui.extensions.resourceReference
 import com.tangem.domain.walletconnect.WcRequestUseCaseFactory
 import com.tangem.domain.walletconnect.usecase.ethereum.WcEthMessageSignUseCase
+import com.tangem.domain.walletconnect.usecase.sign.WcSignState
+import com.tangem.domain.walletconnect.usecase.sign.WcSignStep
 import com.tangem.features.walletconnect.impl.R
 import com.tangem.features.walletconnect.transaction.components.WcEthereumMessageSignRequestComponent
 import com.tangem.features.walletconnect.transaction.entity.*
 import com.tangem.features.walletconnect.transaction.entity.WcEthereumMessageSignRequestUM.State
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import kotlinx.collections.immutable.persistentListOf
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -28,6 +28,7 @@ import javax.inject.Inject
 internal class WcEthereumMessageSignRequestModel @Inject constructor(
     paramsContainer: ParamsContainer,
     override val dispatchers: CoroutineDispatcherProvider,
+    private val router: Router,
     private val clipboardManager: ClipboardManager,
     private val useCaseFactory: WcRequestUseCaseFactory,
 ) : Model() {
@@ -41,24 +42,41 @@ internal class WcEthereumMessageSignRequestModel @Inject constructor(
         modelScope.launch(dispatchers.io) {
             val useCase: WcEthMessageSignUseCase = useCaseFactory.createUseCase(params.rawRequest)
             useCase.invoke()
-                .onEach { domainToUM(useCase, it.signModel) }
+                .onEach { signState ->
+                    if (signingIsDone(useCase, signState)) return@onEach
+                    _uiState.emit(domainToUM(useCase, signState))
+                }
                 .launchIn(this)
         }
     }
 
+    private fun signingIsDone(
+        useCase: WcEthMessageSignUseCase,
+        signState: WcSignState<WcEthMessageSignUseCase.SignModel>,
+    ): Boolean {
+        (signState.domainStep as? WcSignStep.Result)?.result?.fold(
+            ifLeft = { /* [REDACTED_TODO_COMMENT]*/ },
+            ifRight = {
+                cancel(useCase)
+                return true
+            },
+        )
+        return false
+    }
+
     private fun domainToUM(
         useCase: WcEthMessageSignUseCase,
-        signModel: WcEthMessageSignUseCase.SignModel,
+        signState: WcSignState<WcEthMessageSignUseCase.SignModel>,
     ): WcEthereumMessageSignRequestUM {
         return WcEthereumMessageSignRequestUM(
             startIconRes = R.drawable.ic_back_24,
             endIconRes = R.drawable.ic_close_24,
             state = State.TRANSACTION,
             actions = WcTransactionActionsUM(
-                onDismiss = {}, // wcEthMessageSignUseCase::cancel,
+                onDismiss = { cancel(useCase) },
                 onBack = ::showTransactionState,
-                onSign = {}, // wcEthMessageSignUseCase::sign,
-                onCopy = { onCopy(signModel.rawMsg) },
+                onSign = useCase::sign,
+                onCopy = { onCopy(signState.signModel.rawMsg) },
                 transactionRequestOnClick = ::showTransactionRequestState,
             ),
             transaction = WcTransactionUM(
@@ -71,6 +89,7 @@ internal class WcEthereumMessageSignRequestModel @Inject constructor(
                     name = useCase.network.name,
                     iconRes = getActiveIconRes(useCase.network.id.value),
                 ),
+                isLoading = signState.domainStep == WcSignStep.Signing,
             ),
             transactionRequestInfo = WcTransactionRequestInfoUM(
                 persistentListOf(
@@ -80,11 +99,16 @@ internal class WcEthereumMessageSignRequestModel @Inject constructor(
                     ),
                     WcTransactionRequestInfoItemUM(
                         title = resourceReference(R.string.wc_contents),
-                        description = signModel.humanMsg,
+                        description = signState.signModel.humanMsg,
                     ),
                 ),
             ),
         )
+    }
+
+    private fun cancel(useCase: WcEthMessageSignUseCase) {
+        useCase.cancel()
+        router.pop()
     }
 
     private fun onCopy(text: String) {
