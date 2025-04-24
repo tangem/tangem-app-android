61d0a160aaa3f49191f653e3d2e38eb76564849a
Tangem <git@tangem.com>
2025-04-24 13:02:08 +0400

    Updated on 2026-02-11

--- data/blockaid/src/main/kotlin/com/tangem/data/blockaid/DefaultBlockAidRepository.kt
+++ data/blockaid/src/main/kotlin/com/tangem/data/blockaid/DefaultBlockAidRepository.kt
@@ -12,19 +12,19 @@ import kotlinx.coroutines.withContext
 
 internal class DefaultBlockAidRepository(
     private val api: BlockAidApi,
-    private val dispatcherProvider: CoroutineDispatcherProvider,
+    private val dispatchers: CoroutineDispatcherProvider,
     private val mapper: BlockAidMapper,
 ) : BlockAidRepository {
 
     override suspend fun verifyDAppDomain(data: DAppData): CheckDAppResult {
-        val response = withContext(dispatcherProvider.io) {
+        val response = withContext(dispatchers.io) {
             api.scanDomain(DomainScanRequest(data.url))
         }
         return mapper.mapToDomain(response)
     }
 
     override suspend fun verifyTransaction(data: TransactionData): CheckTransactionResult {
-        val response = withContext(dispatcherProvider.io) {
+        val response = withContext(dispatchers.io) {
             when (data.params) {
                 is TransactionParams.Evm -> {
                     api.scanJsonRpc(mapper.mapToEvmRequest(data))
