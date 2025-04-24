1c1ddb0ccd3b72720816d274091c5b84719ce149
Tangem <git@tangem.com>
2025-04-24 18:06:06 +0700

    Updated on 2025-09-01

--- features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/components/WcEthereumMessageSignRequestComponent.kt
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/components/WcEthereumMessageSignRequestComponent.kt
@@ -2,38 +2,36 @@ package com.tangem.features.walletconnect.transaction.components
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.tangem.core.decompose.context.AppComponentContext
 import com.tangem.core.decompose.model.getOrCreateModel
 import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfig
-import com.tangem.core.ui.decompose.ComposableBottomSheetComponent
+import com.tangem.core.ui.decompose.ComposableContentComponent
+import com.tangem.domain.walletconnect.model.sdkcopy.WcSdkSessionRequest
 import com.tangem.features.walletconnect.transaction.model.WcEthereumMessageSignRequestModel
 import com.tangem.features.walletconnect.transaction.ui.sign.WcEthereumMessageSignRequestModalBottomSheet
 
 internal class WcEthereumMessageSignRequestComponent(
     appComponentContext: AppComponentContext,
     private val params: Params,
-) : AppComponentContext by appComponentContext, ComposableBottomSheetComponent {
+) : AppComponentContext by appComponentContext, ComposableContentComponent {
 
-    private val model: WcEthereumMessageSignRequestModel = getOrCreateModel()
-
-    override fun dismiss() {
-        params.onDismiss()
-    }
+    private val model: WcEthereumMessageSignRequestModel = getOrCreateModel(params)
 
     @Composable
-    override fun BottomSheet() {
+    override fun Content(modifier: Modifier) {
         val content by model.uiState.collectAsStateWithLifecycle()
         content?.let {
             WcEthereumMessageSignRequestModalBottomSheet(
                 config = TangemBottomSheetConfig(
                     isShown = true,
-                    onDismissRequest = ::dismiss,
+                    onDismissRequest = {},
                     content = it,
                 ),
             )
         }
     }
 
-    data class Params(val onDismiss: () -> Unit)
+    data class Params(val rawRequest: WcSdkSessionRequest)
 }
