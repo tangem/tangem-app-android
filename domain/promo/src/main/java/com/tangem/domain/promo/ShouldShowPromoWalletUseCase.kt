1bc8d7e86f9fdf65110d75d6a97bbb63f077d893
Tangem <git@tangem.com>
2025-04-25 18:57:37 +0500

    Updated on 2026-08-14

--- domain/promo/src/main/java/com/tangem/domain/promo/ShouldShowPromoWalletUseCase.kt
+++ domain/promo/src/main/java/com/tangem/domain/promo/ShouldShowPromoWalletUseCase.kt
@@ -3,11 +3,16 @@ package com.tangem.domain.promo
 import com.tangem.domain.promo.models.PromoId
 import com.tangem.domain.wallets.models.UserWalletId
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
 
 class ShouldShowPromoWalletUseCase(private val promoRepository: PromoRepository) {
 
     operator fun invoke(userWalletId: UserWalletId, promoId: PromoId): Flow<Boolean> {
-        return promoRepository.isReadyToShowWalletPromo(userWalletId, promoId)
+        return flow {
+            emit(false)
+            emitAll(promoRepository.isReadyToShowWalletPromo(userWalletId, promoId))
+        }
     }
 
     suspend fun neverToShow(promoId: PromoId) = promoRepository.setNeverToShowWalletPromo(promoId)
