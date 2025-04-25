b9b67f8b02cdd51b20925c85043bbc6782550a55
Tangem <git@tangem.com>
2025-04-25 13:54:11 +0300

    Updated on 2025-12-22

--- features/biometry/impl/src/main/kotlin/com/tangem/features/biometry/impl/di/FeatureModule.kt
+++ features/biometry/impl/src/main/kotlin/com/tangem/features/biometry/impl/di/FeatureModule.kt
@@ -3,9 +3,7 @@ package com.tangem.features.biometry.impl.di
 import com.tangem.core.decompose.di.ModelComponent
 import com.tangem.core.decompose.model.Model
 import com.tangem.features.biometry.AskBiometryComponent
-import com.tangem.features.biometry.BiometryFeatureToggles
 import com.tangem.features.biometry.impl.DefaultAskBiometryComponent
-import com.tangem.features.biometry.impl.DefaultBiometryFeatureToggles
 import com.tangem.features.biometry.impl.model.AskBiometryModel
 import dagger.Binds
 import dagger.Module
@@ -17,9 +15,6 @@ import dagger.multibindings.IntoMap
 @Module
 @InstallIn(SingletonComponent::class)
 internal interface FeatureModule {
-
-    @Binds
-    fun bindFeatureToggles(impl: DefaultBiometryFeatureToggles): BiometryFeatureToggles
 
     @Binds
     fun bindComponentFactory(impl: DefaultAskBiometryComponent.Factory): AskBiometryComponent.Factory
