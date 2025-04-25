7bcde1a51b15d77fb53663561727d14e8115f7b2
Tangem <git@tangem.com>
2025-04-25 19:13:37 +0300

    Updated on 2025-12-05

--- data/staking/src/main/java/com/tangem/data/staking/utils/YieldBalanceRequestBodyFactory.kt
+++ data/staking/src/main/java/com/tangem/data/staking/utils/YieldBalanceRequestBodyFactory.kt
@@ -1,6 +1,5 @@
 package com.tangem.data.staking.utils
 
-import com.tangem.datasource.api.stakekit.models.request.Address
 import com.tangem.datasource.api.stakekit.models.request.YieldBalanceRequestBody
 import com.tangem.domain.staking.model.StakingID
 
@@ -13,11 +12,7 @@ internal object YieldBalanceRequestBodyFactory {
 
     fun create(stakingID: StakingID): YieldBalanceRequestBody {
         return YieldBalanceRequestBody(
-            addresses = Address(
-                address = stakingID.address,
-                additionalAddresses = null, // todo fill additional addresses metadata if needed
-                explorerUrl = "", // todo fill exporer url [REDACTED_JIRA]
-            ),
+            addresses = YieldBalanceRequestBodyAddressFactory.create(stakingID),
             args = YieldBalanceRequestBody.YieldBalanceRequestArgs(
                 validatorAddresses = listOf(), // todo add validators [REDACTED_JIRA]
             ),
