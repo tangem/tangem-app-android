d2f3ea70c9e56ca39a1388b559da4222daaaa5e8
Tangem <git@tangem.com>
2025-04-25 12:15:06 +0500

    Updated on 2026-08-07

--- features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/connections/entity/WcAppInfoUM.kt
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/connections/entity/WcAppInfoUM.kt
@@ -6,20 +6,31 @@ import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfigContent
 import kotlinx.collections.immutable.ImmutableList
 
 @Immutable
-internal data class WcAppInfoUM(
-    val appName: String,
-    val appIcon: String,
-    val isVerified: Boolean,
-    val appSubtitle: String,
-    val notificationUM: NotificationUM?,
-    val walletName: String,
-    val networksInfo: WcNetworksInfo,
-    val onDismiss: () -> Unit,
-    val onConnect: () -> Unit,
-) : TangemBottomSheetConfigContent
+internal sealed class WcAppInfoUM : TangemBottomSheetConfigContent {
+
+    abstract val onDismiss: () -> Unit
+    abstract val connectButtonConfig: WcPrimaryButtonConfig
+
+    data class Loading(
+        override val onDismiss: () -> Unit,
+        override val connectButtonConfig: WcPrimaryButtonConfig,
+    ) : WcAppInfoUM()
+
+    data class Content(
+        val appName: String,
+        val appIcon: String,
+        val isVerified: Boolean,
+        val appSubtitle: String,
+        val notificationUM: NotificationUM?,
+        val walletName: String,
+        val networksInfo: WcNetworksInfo,
+        override val connectButtonConfig: WcPrimaryButtonConfig,
+        override val onDismiss: () -> Unit,
+    ) : WcAppInfoUM()
+}
 
 @Immutable
 internal sealed class WcNetworksInfo {
-    data class MissingRequiredNetworkInfo(val network: String) : WcNetworksInfo()
+    data class MissingRequiredNetworkInfo(val networks: String) : WcNetworksInfo()
     data class ContainsAllRequiredNetworks(val icons: ImmutableList<Int>) : WcNetworksInfo()
 }
