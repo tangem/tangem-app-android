cb11b6775a2e4c0b79e7ad7c3aa6fb0aba056595
Tangem <git@tangem.com>
2025-04-25 15:44:00 +0700

    Updated on 2026-06-02

--- domain/wallet-connect/models/src/main/java/com/tangem/domain/walletconnect/model/sdkcopy/WcSdkSessionRequest.kt
+++ domain/wallet-connect/models/src/main/java/com/tangem/domain/walletconnect/model/sdkcopy/WcSdkSessionRequest.kt
@@ -1,14 +1,18 @@
 package com.tangem.domain.walletconnect.model.sdkcopy
 
+import kotlinx.serialization.Serializable
+
 /**
  * copy of [com.reown.walletkit.client.Wallet.Model.SessionRequest]
  */
+@Serializable
 data class WcSdkSessionRequest(
     val topic: String,
     val chainId: String?,
     val request: JSONRPCRequest,
 ) {
 
+    @Serializable
     data class JSONRPCRequest(
         val id: Long,
         val method: String,
