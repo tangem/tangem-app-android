55db91f3ca817c2e1429b27798e76559339dca16
Tangem <git@tangem.com>
2025-04-28 16:08:31 +0500

    Updated on 2026-06-10

--- app/src/main/java/com/tangem/tap/data/FirebasePushNotificationsTokenProvider.kt
+++ app/src/main/java/com/tangem/tap/data/FirebasePushNotificationsTokenProvider.kt
@@ -4,7 +4,7 @@ import com.google.firebase.messaging.FirebaseMessaging
 import com.tangem.utils.notifications.PushNotificationsTokenProvider
 import kotlinx.coroutines.tasks.await
 
-class FirebasePushNotificationsTokenProvider : PushNotificationsTokenProvider {
+internal class FirebasePushNotificationsTokenProvider : PushNotificationsTokenProvider {
     override suspend fun getToken(): String {
         return FirebaseMessaging.getInstance().token.await()
     }
