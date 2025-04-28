5ef32c2c0cfcd12bb53809c3e9fab53a3af150e4
Tangem <git@tangem.com>
2025-04-28 16:08:31 +0500

    Updated on 2026-08-19

--- domain/notifications/build.gradle.kts
+++ domain/notifications/build.gradle.kts
@@ -1,17 +1,31 @@
 plugins {
-    alias(deps.plugins.kotlin.jvm)
-    alias(deps.plugins.kotlin.serialization)
+    alias(deps.plugins.android.library)
+    alias(deps.plugins.kotlin.android)
+    alias(deps.plugins.kotlin.kapt)
+    alias(deps.plugins.hilt.android)
     id("configuration")
+}
+
+android {
+    namespace = "com.tangem.domain.notifications"
 }
 
 dependencies {
     implementation(projects.core.utils)
     implementation(projects.domain.core)
+    implementation(projects.domain.wallets.models)
     implementation(projects.domain.notifications.models)
+    implementation(projects.domain.wallets)
 
-    /* Tests */
+    // region DI
+    implementation(deps.hilt.android)
+    kapt(deps.hilt.kapt)
+    // end
+
+    // region Tests
     testImplementation(deps.test.junit)
     testImplementation(deps.test.coroutine)
     testImplementation(deps.test.truth)
     testImplementation(deps.test.mockk)
+    // end
 }
