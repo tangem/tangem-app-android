1b05158b0208042028aadfc095ab14c2e6b6de10
Tangem <git@tangem.com>
2025-04-28 09:56:00 +0300

    Updated on 2026-07-23

--- app/src/main/java/com/tangem/tap/routing/component/RoutingComponent.kt
+++ app/src/main/java/com/tangem/tap/routing/component/RoutingComponent.kt
@@ -3,21 +3,12 @@ package com.tangem.tap.routing.component
 import android.content.Intent
 import androidx.compose.runtime.Immutable
 import androidx.fragment.app.Fragment
-import com.arkivanov.decompose.router.stack.ChildStack
-import com.arkivanov.decompose.value.Value
 import com.tangem.common.routing.AppRoute
 import com.tangem.core.decompose.context.AppComponentContext
-import com.tangem.core.decompose.navigation.Router
 import com.tangem.core.ui.decompose.ComposableContentComponent
 import com.tangem.utils.Provider
 
 internal interface RoutingComponent : ComposableContentComponent {
-
-    // TODO: Remove after full navigation refactoring
-    val router: Router
-
-    // TODO: Remove after full navigation refactoring
-    val stack: Value<ChildStack<AppRoute, Child>>
 
     @Immutable
     sealed class Child {
