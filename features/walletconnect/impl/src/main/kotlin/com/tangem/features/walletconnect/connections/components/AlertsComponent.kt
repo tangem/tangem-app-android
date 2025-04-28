aa83541ed24fba348a2589b58e8d1deaa62a5d22
Tangem <git@tangem.com>
2025-04-28 14:02:26 +0500

    Updated on 2026-09-23

--- features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/connections/components/AlertsComponent.kt
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/connections/components/AlertsComponent.kt
@@ -7,6 +7,7 @@ import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfig
 import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfigContent
 import com.tangem.core.ui.decompose.ComposableBottomSheetComponent
 import com.tangem.features.walletconnect.connections.ui.AlertsModalBottomSheet
+import kotlinx.serialization.Serializable
 
 internal class AlertsComponent(
     appComponentContext: AppComponentContext,
@@ -31,17 +32,35 @@ internal class AlertsComponent(
     internal data class Params(val alertType: AlertType)
 
     @Immutable
+    @Serializable
     internal sealed class AlertType : TangemBottomSheetConfigContent {
         abstract val onDismiss: () -> Unit
 
+        @Serializable
         data class VerifiedDomain(val appName: String, override val onDismiss: () -> Unit) : AlertType()
+
+        @Serializable
         data class UnknownDomain(val onConnect: () -> Unit, override val onDismiss: () -> Unit) : AlertType()
+
+        @Serializable
         data class UnsafeDomain(val onConnect: () -> Unit, override val onDismiss: () -> Unit) : AlertType()
+
+        @Serializable
         data class UnsupportedNetworks(val appName: String, override val onDismiss: () -> Unit) : AlertType()
+
+        @Serializable
         data class WcDisconnected(override val onDismiss: () -> Unit) : AlertType()
+
+        @Serializable
         data class UnknownError(val errorCode: Int, override val onDismiss: () -> Unit) : AlertType()
+
+        @Serializable
         data class WrongCardSelected(override val onDismiss: () -> Unit) : AlertType()
+
+        @Serializable
         data class ConnectionTimeout(val onTryAgain: () -> Unit, override val onDismiss: () -> Unit) : AlertType()
+
+        @Serializable
         data class MaliciousTransaction(
             val descriptionMessage: String,
             val onConnect: () -> Unit,
