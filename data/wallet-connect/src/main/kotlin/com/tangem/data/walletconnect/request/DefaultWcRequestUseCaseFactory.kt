b051e46be4a0d6d20de264b81901e04d60b60b4b
Tangem <git@tangem.com>
2025-04-29 16:40:19 +0700

    Updated on 2026-04-15

--- data/wallet-connect/src/main/kotlin/com/tangem/data/walletconnect/request/DefaultWcRequestUseCaseFactory.kt
+++ data/wallet-connect/src/main/kotlin/com/tangem/data/walletconnect/request/DefaultWcRequestUseCaseFactory.kt
@@ -2,7 +2,7 @@ package com.tangem.data.walletconnect.request
 
 import com.tangem.domain.walletconnect.WcRequestUseCaseFactory
 import com.tangem.domain.walletconnect.model.sdkcopy.WcSdkSessionRequest
-import com.tangem.domain.walletconnect.usecase.WcMethodUseCase
+import com.tangem.domain.walletconnect.usecase.method.WcMethodUseCase
 import javax.inject.Inject
 
 internal class DefaultWcRequestUseCaseFactory @Inject constructor(
