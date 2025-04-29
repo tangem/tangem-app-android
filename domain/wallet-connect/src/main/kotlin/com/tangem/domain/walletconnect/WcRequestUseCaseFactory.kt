ba12a65fc839ef2b84017a8fdc23ebd9affe3c6a
Tangem <git@tangem.com>
2025-04-29 16:40:19 +0700

    Updated on 2026-08-14

--- domain/wallet-connect/src/main/kotlin/com/tangem/domain/walletconnect/WcRequestUseCaseFactory.kt
+++ domain/wallet-connect/src/main/kotlin/com/tangem/domain/walletconnect/WcRequestUseCaseFactory.kt
@@ -1,7 +1,7 @@
 package com.tangem.domain.walletconnect
 
 import com.tangem.domain.walletconnect.model.sdkcopy.WcSdkSessionRequest
-import com.tangem.domain.walletconnect.usecase.WcMethodUseCase
+import com.tangem.domain.walletconnect.usecase.method.WcMethodUseCase
 
 interface WcRequestUseCaseFactory {
 
