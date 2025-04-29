8c9b3c06af30fa154340369872a29bdecd2c4969
Tangem <git@tangem.com>
2025-04-29 18:34:21 +0500

    Updated on 2026-09-25

--- features/onramp/api/build.gradle.kts
+++ features/onramp/api/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     /* Project - Core */
     implementation(projects.core.decompose)
     implementation(projects.core.ui)
+    implementation(projects.core.deepLinks)
 
     /* Project - Domain */
     implementation(projects.domain.onramp.models)
