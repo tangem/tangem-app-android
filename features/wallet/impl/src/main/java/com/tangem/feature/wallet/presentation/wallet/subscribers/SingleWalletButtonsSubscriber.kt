8bb3b3596d282df502c8a2ad370a32e16fc0b7f9
Tangem <git@tangem.com>
2025-04-30 19:53:58 +0500

    Updated on 2026-09-01

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/subscribers/SingleWalletButtonsSubscriber.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/subscribers/SingleWalletButtonsSubscriber.kt
@@ -1,7 +1,7 @@
 package com.tangem.feature.wallet.presentation.wallet.subscribers
 
 import com.tangem.domain.tokens.GetCryptoCurrencyActionsUseCase
-import com.tangem.domain.tokens.GetPrimaryCurrencyStatusUpdatesUseCase
+import com.tangem.domain.tokens.GetSingleCryptoCurrencyStatusUseCase
 import com.tangem.domain.tokens.model.TokenActionsState
 import com.tangem.domain.wallets.models.UserWallet
 import com.tangem.feature.wallet.child.wallet.model.intents.WalletClickIntents
@@ -15,13 +15,13 @@ internal class SingleWalletButtonsSubscriber(
     private val userWallet: UserWallet,
     private val stateHolder: WalletStateController,
     private val clickIntents: WalletClickIntents,
-    private val getPrimaryCurrencyStatusUpdatesUseCase: GetPrimaryCurrencyStatusUpdatesUseCase,
+    private val getSingleCryptoCurrencyStatusUseCase: GetSingleCryptoCurrencyStatusUseCase,
     private val getCryptoCurrencyActionsUseCase: GetCryptoCurrencyActionsUseCase,
 ) : WalletSubscriber() {
 
     override fun create(coroutineScope: CoroutineScope): Flow<TokenActionsState> {
         return channelFlow {
-            getPrimaryCurrencyStatusUpdatesUseCase.collectLatest(userWalletId = userWallet.walletId) { status ->
+            getSingleCryptoCurrencyStatusUseCase.collectLatest(userWalletId = userWallet.walletId) { status ->
                 getCryptoCurrencyActionsUseCase(userWallet = userWallet, cryptoCurrencyStatus = status)
                     .conflate()
                     .distinctUntilChanged()
