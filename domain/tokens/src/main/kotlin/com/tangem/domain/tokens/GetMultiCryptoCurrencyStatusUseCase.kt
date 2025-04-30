7ae8882cd29c6b4f0a758ce7feb5d353ac4fcf57
Tangem <git@tangem.com>
2025-04-30 19:53:58 +0500

    Updated on 2026-08-19

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/GetMultiCryptoCurrencyStatusUseCase.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/GetMultiCryptoCurrencyStatusUseCase.kt
@@ -1,17 +1,25 @@
 package com.tangem.domain.tokens
 
 import arrow.core.Either
+import com.tangem.domain.tokens.error.CurrencyStatusError
 import com.tangem.domain.tokens.error.TokenListError
 import com.tangem.domain.tokens.error.mapper.mapToTokenListError
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.domain.tokens.operations.BaseCurrencyStatusOperations
 import com.tangem.domain.wallets.models.UserWalletId
+import kotlinx.coroutines.flow.Flow
 
-class GetCryptoCurrencyStatusesSyncUseCase(
+class GetMultiCryptoCurrencyStatusUseCase(
     private val currencyStatusOperations: BaseCurrencyStatusOperations,
 ) {
 
-    suspend operator fun invoke(userWalletId: UserWalletId): Either<TokenListError, List<CryptoCurrencyStatus>> {
+    /**
+     * Returns synchronously list of cryptocurrency statuses for Multi-Currency wallet
+     *
+     * @param userWalletId The unique identifier of the user's wallet.
+     * @return A [Flow] emitting either a [CurrencyStatusError] or a [CryptoCurrencyStatus], indicating the result of the fetch operation.
+     */
+    suspend fun invokeMultiWalletSync(userWalletId: UserWalletId): Either<TokenListError, List<CryptoCurrencyStatus>> {
         return currencyStatusOperations.getCurrenciesStatusesSync(userWalletId)
             .mapLeft { error -> error.mapToTokenListError() }
     }
