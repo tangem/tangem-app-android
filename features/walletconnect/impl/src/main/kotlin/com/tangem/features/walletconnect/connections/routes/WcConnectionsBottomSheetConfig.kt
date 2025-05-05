7841b8e09faf6bb9e42581f48e17081a22557ccd
Tangem <git@tangem.com>
2025-05-05 16:58:27 +0700

    Updated on 2026-08-07

--- features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/connections/routes/WcConnectionsBottomSheetConfig.kt
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/connections/routes/WcConnectionsBottomSheetConfig.kt
@@ -5,8 +5,5 @@ import kotlinx.serialization.Serializable
 @Serializable
 internal sealed class WcConnectionsBottomSheetConfig {
     @Serializable
-    data class AppInfo(val wcUrl: String) : WcConnectionsBottomSheetConfig()
-
-    @Serializable
     data class ConnectedApp(val topic: String) : WcConnectionsBottomSheetConfig()
 }
