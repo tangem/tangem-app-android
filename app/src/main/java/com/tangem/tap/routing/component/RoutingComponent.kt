fa489d586afb25a013d8b9ab3c6f0a6f4598b8c8
Tangem <git@tangem.com>
2025-05-05 11:47:30 +0300

    Updated on 2025-10-09

--- app/src/main/java/com/tangem/tap/routing/component/RoutingComponent.kt
+++ app/src/main/java/com/tangem/tap/routing/component/RoutingComponent.kt
@@ -2,11 +2,9 @@ package com.tangem.tap.routing.component
 
 import android.content.Intent
 import androidx.compose.runtime.Immutable
-import androidx.fragment.app.Fragment
 import com.tangem.common.routing.AppRoute
 import com.tangem.core.decompose.context.AppComponentContext
 import com.tangem.core.ui.decompose.ComposableContentComponent
-import com.tangem.utils.Provider
 
 internal interface RoutingComponent : ComposableContentComponent {
 
@@ -15,11 +13,6 @@ internal interface RoutingComponent : ComposableContentComponent {
 
         // TODO: Remove and find initial intent in RoutingComponent: [REDACTED_JIRA]
         data object Initial : Child()
-
-        data class LegacyFragment(
-            val name: String,
-            val fragmentProvider: Provider<Fragment>,
-        ) : Child()
 
         data class LegacyIntent(val intent: Intent) : Child()
 
