b267ed7522035f47ac4ecb8a533b2b8809967d62
Tangem <git@tangem.com>
2025-05-06 12:51:54 +0400

    Updated on 2026-08-07

--- features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/utils/WcSignTransactionUtils.kt
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/utils/WcSignTransactionUtils.kt
@@ -3,40 +3,50 @@ package com.tangem.features.walletconnect.transaction.utils
 import com.domain.blockaid.models.dapp.CheckDAppResult
 import com.tangem.core.ui.extensions.getActiveIconRes
 import com.tangem.core.ui.extensions.resourceReference
+import com.tangem.domain.walletconnect.model.WcEthMethod
+import com.tangem.domain.walletconnect.model.WcSolanaMethod
+import com.tangem.domain.walletconnect.model.sdkcopy.WcSdkSessionRequest
 import com.tangem.domain.walletconnect.usecase.method.WcMessageSignUseCase
 import com.tangem.domain.walletconnect.usecase.method.WcSignState
 import com.tangem.domain.walletconnect.usecase.method.WcSignStep
 import com.tangem.domain.walletconnect.usecase.method.WcSignUseCase
 import com.tangem.features.walletconnect.impl.R
-import com.tangem.features.walletconnect.transaction.entity.WcNetworkInfoUM
-import com.tangem.features.walletconnect.transaction.entity.WcSignTransactionUM
-import com.tangem.features.walletconnect.transaction.entity.WcTransactionActionsUM
-import com.tangem.features.walletconnect.transaction.entity.WcTransactionRequestInfoItemUM
-import com.tangem.features.walletconnect.transaction.entity.WcTransactionRequestInfoUM
-import com.tangem.features.walletconnect.transaction.entity.WcTransactionUM
+import com.tangem.features.walletconnect.transaction.entity.common.WcNetworkInfoUM
+import com.tangem.features.walletconnect.transaction.entity.sign.WcSignTransactionUM
+import com.tangem.features.walletconnect.transaction.entity.common.WcTransactionActionsUM
+import com.tangem.features.walletconnect.transaction.entity.common.WcTransactionRequestBlockUM
+import com.tangem.features.walletconnect.transaction.entity.common.WcTransactionRequestInfoItemUM
+import com.tangem.features.walletconnect.transaction.entity.common.WcTransactionRequestInfoUM
+import com.tangem.features.walletconnect.transaction.entity.common.WcTransactionUM
 import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.toImmutableList
 
 internal fun WcSignUseCase.toUM(signState: WcSignState<*>, actions: WcTransactionActionsUM): WcSignTransactionUM? {
     return when (this) {
         is WcMessageSignUseCase -> {
-            ethMessageSignToUM(
-                signState = signState,
-                signModel = signState.signModel as WcMessageSignUseCase.SignModel,
-                actions = actions,
-            )
+            when (method) {
+                is WcEthMethod.SignTypedData -> signTypedDataToUM(
+                    signState = signState,
+                    signModel = signState.signModel as WcMessageSignUseCase.SignModel,
+                    actions = actions,
+                )
+                is WcEthMethod.MessageSign, is WcSolanaMethod.SignMessage -> messageSignToUM(
+                    signState = signState,
+                    signModel = signState.signModel as WcMessageSignUseCase.SignModel,
+                    actions = actions,
+                )
+                else -> null
+            }
         }
         else -> null
     }
 }
 
-private fun WcMessageSignUseCase.ethMessageSignToUM(
+private fun WcMessageSignUseCase.signTypedDataToUM(
     signState: WcSignState<*>,
     signModel: WcMessageSignUseCase.SignModel,
     actions: WcTransactionActionsUM,
 ) = WcSignTransactionUM(
-    startIconRes = R.drawable.ic_back_24,
-    endIconRes = R.drawable.ic_close_24,
-    transactionIconRes = R.drawable.ic_doc_new_24,
     state = WcSignTransactionUM.State.TRANSACTION,
     actions = actions,
     transaction = WcTransactionUM(
@@ -49,9 +59,66 @@ private fun WcMessageSignUseCase.ethMessageSignToUM(
             name = network.name,
             iconRes = getActiveIconRes(network.id.value),
         ),
+        addressText = walletAddress.toShortAddressText(),
+        activeButtonText = resourceReference(R.string.common_sign),
         isLoading = signState.domainStep == WcSignStep.Signing,
     ),
     transactionRequestInfo = WcTransactionRequestInfoUM(
+        buildList {
+            add(createInfoBlockUM(rawSdkRequest, signModel))
+            (method as? WcEthMethod.SignTypedData)?.params?.message?.to?.let { to ->
+                add(
+                    WcTransactionRequestBlockUM(
+                        persistentListOf(
+                            WcTransactionRequestInfoItemUM(
+                                title = resourceReference(R.string.wc_transaction_info_to_title),
+                            ),
+                            WcTransactionRequestInfoItemUM(
+                                title = resourceReference(R.string.settings_wallet_name_title),
+                                description = to.name,
+                            ),
+                            WcTransactionRequestInfoItemUM(
+                                title = resourceReference(R.string.wc_common_wallet),
+                                description = to.wallet,
+                            ),
+                        ),
+                    ),
+                )
+            }
+        }.toImmutableList(),
+    ),
+)
+
+private fun WcMessageSignUseCase.messageSignToUM(
+    signState: WcSignState<*>,
+    signModel: WcMessageSignUseCase.SignModel,
+    actions: WcTransactionActionsUM,
+) = WcSignTransactionUM(
+    state = WcSignTransactionUM.State.TRANSACTION,
+    actions = actions,
+    transaction = WcTransactionUM(
+        appName = session.sdkModel.appMetaData.name,
+        appIcon = session.sdkModel.appMetaData.url,
+        isVerified = session.securityStatus == CheckDAppResult.SAFE,
+        appSubtitle = session.sdkModel.appMetaData.description,
+        walletName = session.wallet.name,
+        networkInfo = WcNetworkInfoUM(
+            name = network.name,
+            iconRes = getActiveIconRes(network.id.value),
+        ),
+        activeButtonText = resourceReference(R.string.common_sign),
+        isLoading = signState.domainStep == WcSignStep.Signing,
+    ),
+    transactionRequestInfo = WcTransactionRequestInfoUM(
+        persistentListOf(createInfoBlockUM(rawSdkRequest, signModel)),
+    ),
+)
+
+private fun createInfoBlockUM(
+    rawSdkRequest: WcSdkSessionRequest,
+    signModel: WcMessageSignUseCase.SignModel,
+): WcTransactionRequestBlockUM {
+    return WcTransactionRequestBlockUM(
         persistentListOf(
             WcTransactionRequestInfoItemUM(
                 title = resourceReference(R.string.wc_signature_type),
@@ -62,5 +129,5 @@ private fun WcMessageSignUseCase.ethMessageSignToUM(
                 description = signModel.humanMsg,
             ),
         ),
-    ),
-)
+    )
+}
