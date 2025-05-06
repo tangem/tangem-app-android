fc81ca0fdd213e833e078740938eb9cc6b9f9735
Tangem <git@tangem.com>
2025-05-06 14:11:51 +0500

    Updated on 2026-08-07

---
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/connections/model/transformers/WcNetworksInfoConverter.kt
@@ -0,0 +1,32 @@
+package com.tangem.features.walletconnect.connections.model.transformers
+
+import com.tangem.core.ui.extensions.iconResId
+import com.tangem.domain.walletconnect.model.WcSessionProposal
+import com.tangem.features.walletconnect.connections.entity.WcNetworkInfoItem
+import com.tangem.features.walletconnect.connections.entity.WcNetworksInfo
+import com.tangem.utils.converter.Converter
+import kotlinx.collections.immutable.toImmutableList
+
+internal object WcNetworksInfoConverter : Converter<WcSessionProposal.ProposalNetwork, WcNetworksInfo> {
+    override fun convert(value: WcSessionProposal.ProposalNetwork): WcNetworksInfo {
+        return if (value.missingRequired.isNotEmpty()) {
+            WcNetworksInfo.MissingRequiredNetworkInfo(
+                networks = value.missingRequired
+                    .joinToString { it.name },
+            )
+        } else {
+            WcNetworksInfo.ContainsAllRequiredNetworks(
+                items = (value.required + value.available)
+                    .map {
+                        WcNetworkInfoItem(
+                            id = it.id.value,
+                            icon = it.iconResId,
+                            name = it.name,
+                            symbol = it.currencySymbol,
+                        )
+                    }
+                    .toImmutableList(),
+            )
+        }
+    }
+}
