754c4bc1eba902d16252987302a02207e4857cf3
Tangem <git@tangem.com>
2025-05-07 15:44:38 +0500

    Updated on 2026-02-11

--- domain/nft/src/main/kotlin/com/tangem/domain/nft/repository/NFTRepository.kt
+++ domain/nft/src/main/kotlin/com/tangem/domain/nft/repository/NFTRepository.kt
@@ -3,12 +3,23 @@ package com.tangem.domain.nft.repository
 import com.tangem.domain.nft.models.NFTAsset
 import com.tangem.domain.nft.models.NFTCollection
 import com.tangem.domain.nft.models.NFTCollections
+import com.tangem.domain.nft.models.NFTSalePrice
+import com.tangem.domain.tokens.model.CryptoCurrency
 import com.tangem.domain.tokens.model.Network
 import com.tangem.domain.wallets.models.UserWalletId
 import kotlinx.coroutines.flow.Flow
 
 interface NFTRepository {
     fun observeCollections(userWalletId: UserWalletId, networks: List<Network>): Flow<List<NFTCollections>>
+
+    fun getNFTCurrency(network: Network): CryptoCurrency
+
+    suspend fun getNFTSalePrice(
+        userWalletId: UserWalletId,
+        network: Network,
+        collectionId: NFTCollection.Identifier,
+        assetId: NFTAsset.Identifier,
+    ): NFTSalePrice
 
     suspend fun refreshCollections(userWalletId: UserWalletId, networks: List<Network>)
 
