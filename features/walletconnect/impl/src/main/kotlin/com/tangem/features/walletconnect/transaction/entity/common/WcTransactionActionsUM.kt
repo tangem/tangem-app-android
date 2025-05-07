58a6d7e61eea344f552ecdf86c3cd165aea47ec0
Tangem <git@tangem.com>
2025-05-07 22:47:07 +0400

    Updated on 2026-06-02

---
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/entity/common/WcTransactionActionsUM.kt
@@ -0,0 +1,10 @@
+package com.tangem.features.walletconnect.transaction.entity.common
+
+import androidx.compose.runtime.Immutable
+
+@Immutable
+internal data class WcTransactionActionsUM(
+    val onDismiss: () -> Unit,
+    val onSign: () -> Unit,
+    val onCopy: () -> Unit,
+)
