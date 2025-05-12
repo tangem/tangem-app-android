a9990643ff98e2e55f9970f0d52ced52bb8028f7
Tangem <git@tangem.com>
2025-05-12 15:07:38 +0400

    Updated on 2026-07-07

--- features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/entity/sign/WcSignTransactionUM.kt
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/entity/sign/WcSignTransactionUM.kt
@@ -1,18 +1,15 @@
 package com.tangem.features.walletconnect.transaction.entity.sign
 
-import androidx.compose.runtime.Immutable
 import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfigContent
 import com.tangem.features.walletconnect.transaction.entity.common.WcNetworkInfoUM
 import com.tangem.features.walletconnect.transaction.entity.common.WcTransactionAppInfoContentUM
 import com.tangem.features.walletconnect.transaction.entity.common.WcTransactionRequestInfoUM
 
-@Immutable
 internal data class WcSignTransactionUM(
     val transaction: WcSignTransactionItemUM,
     val transactionRequestInfo: WcTransactionRequestInfoUM,
 )
 
-@Immutable
 internal data class WcSignTransactionItemUM(
     val onDismiss: () -> Unit,
     val onSign: () -> Unit,
