5418d5b4750ebd8cf3cca2f41945ffefb2aebd6c
Tangem <git@tangem.com>
2025-05-12 15:07:38 +0400

    Updated on 2026-09-23

--- features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/entity/common/WcNetworkInfoUM.kt
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/entity/common/WcNetworkInfoUM.kt
@@ -1,9 +1,7 @@
 package com.tangem.features.walletconnect.transaction.entity.common
 
 import androidx.annotation.DrawableRes
-import androidx.compose.runtime.Immutable
 
-@Immutable
 internal data class WcNetworkInfoUM(
     val name: String,
     @DrawableRes val iconRes: Int,
