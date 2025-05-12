ea7ac174e6a2cb30245d64800f95bab1e27c8605
Tangem <git@tangem.com>
2025-05-12 17:44:40 +0400

    Updated on 2026-08-07

--- data/networks/build.gradle.kts
+++ data/networks/build.gradle.kts
@@ -10,36 +10,52 @@ android {
 }
 
 dependencies {
-    implementation(projects.libs.blockchainSdk)
-
+    // region Project - Core
     implementation(projects.core.datasource)
     implementation(projects.core.utils)
+    // endregion
 
+    // region Project - Data
     implementation(projects.data.common)
-    implementation(projects.data.tokens)
+    // endregion
 
+    // region Project - Domain
     implementation(projects.domain.core)
     implementation(projects.domain.demo)
     implementation(projects.domain.legacy)
     implementation(projects.domain.models)
     implementation(projects.domain.networks)
     implementation(projects.domain.tokens.models)
+    implementation(projects.domain.txhistory.models)
     implementation(projects.domain.wallets)
     implementation(projects.domain.wallets.models)
+    // endregion
 
+    // region Project - Libs
+    implementation(projects.libs.blockchainSdk)
+    // endregion
+
+    // region Tangem libraries
+    implementation(tangemDeps.blockchain)
+    // endregion
+
+    // region DI
     implementation(deps.hilt.android)
     kapt(deps.hilt.kapt)
+    // endregion
 
+    // region Other libraries
     implementation(deps.androidx.datastore)
     implementation(deps.moshi)
     implementation(deps.timber)
+    // endregion
 
-    implementation(tangemDeps.blockchain)
-
+    // region Tests
     testImplementation(deps.test.coroutine)
     testImplementation(deps.test.junit)
     testImplementation(deps.test.mockk)
     testImplementation(deps.test.truth)
-    testImplementation(tangemDeps.card.core)
     testImplementation(projects.common.test)
+    testImplementation(tangemDeps.card.core)
+    // endregion
 }
