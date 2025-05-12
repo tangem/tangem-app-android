49c79ff46918065ae012f2aee86f1a4d174e11eb
Tangem <git@tangem.com>
2025-05-12 17:44:40 +0400

    Updated on 2026-05-07

--- domain/tokens/models/src/main/java/com/tangem/domain/tokens/model/NetworkStatus.kt
+++ domain/tokens/models/src/main/java/com/tangem/domain/tokens/model/NetworkStatus.kt
@@ -5,23 +5,18 @@ import com.tangem.domain.txhistory.models.TxHistoryItem
 import java.math.BigDecimal
 
 /**
- * Represents the status of a specific blockchain network.
+ * Represents the status of a specific blockchain network
  *
- * @property network The network for which the status is provided.
- * @property value The specific status value, represented as a sealed class to encapsulate the various possible states of the network.
+ * @property network the network for which the status is provided
+ * @property value   the specific status value, represented as a sealed class to encapsulate the various possible
+ * states of the network
  */
-data class NetworkStatus(
-    val network: Network,
-    val value: Value,
-) {
+data class NetworkStatus(val network: Network, val value: Value) {
 
-    /**
-     * Represents the various possible statuses of a network.
-     *
-     * This sealed class includes different states like unreachable, missed derivation, verified, and no account.
-     */
+    /** Represents the various possible statuses of a network */
     sealed class Value {
 
+        /** Status source */
         abstract val source: StatusSource
 
         fun copySealed(source: StatusSource): Value {
@@ -36,30 +31,28 @@ data class NetworkStatus(
     }
 
     /**
-     * Represents the state where the network is unreachable.
+     * Represents the state where the network is unreachable
      *
-     * @property address Network addresses.
+     * @property address network address
      */
     data class Unreachable(val address: NetworkAddress?) : Value() {
         override val source: StatusSource = StatusSource.ACTUAL
     }
 
-    /**
-     * Represents the state where a derivation has been missed.
-     */
+    /** Represents the state where a derivation has been missed */
     data object MissedDerivation : Value() {
         override val source: StatusSource = StatusSource.ACTUAL
     }
 
     /**
      * Represents the verified state of the network, including the amounts associated with different cryptocurrencies
-     * and whether there are transactions in progress.
+     * and whether there are transactions in progress
      *
-     * @property address Network addresses.
-     * @property amounts A map containing the amounts associated with different cryptocurrencies within the network.
-     * @property pendingTransactions A map containing pending transactions associated with different cryptocurrencies
-     * @property source source of data
-     * within the network.
+     * @property address             network address
+     * @property amounts             a map containing the amounts associated with different cryptocurrencies within the
+     * network
+     * @property pendingTransactions a map containing pending transactions associated with different cryptocurrencies
+     * @property source              source of data
      */
     data class Verified(
         val address: NetworkAddress,
@@ -69,12 +62,12 @@ data class NetworkStatus(
     ) : Value()
 
     /**
-     * Represents the state where there is no account, and an amount is required to create one.
+     * Represents the state where there is no account, and an amount is required to create one
      *
-     * @property address Network addresses.
-     * @property amountToCreateAccount The amount required to create an account within the network.
-     * @property errorMessage error message
-     * @property source source of data
+     * @property address               network address
+     * @property amountToCreateAccount the amount required to create an account within the network
+     * @property errorMessage          error message
+     * @property source                source of data
      */
     data class NoAccount(
         val address: NetworkAddress,
