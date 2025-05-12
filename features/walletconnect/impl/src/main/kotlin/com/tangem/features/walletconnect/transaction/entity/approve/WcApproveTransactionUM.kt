56ca12b6413d62c4a99a1d52cdffe57fd9eadc7e
Tangem <git@tangem.com>
2025-05-12 15:07:38 +0400

    release/5.27.0

--- features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/entity/approve/WcApproveTransactionUM.kt
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/entity/approve/WcApproveTransactionUM.kt
@@ -1,20 +1,17 @@
 package com.tangem.features.walletconnect.transaction.entity.approve
 
 import androidx.annotation.DrawableRes
-import androidx.compose.runtime.Immutable
 import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfigContent
 import com.tangem.features.walletconnect.transaction.entity.common.WcNetworkInfoUM
 import com.tangem.features.walletconnect.transaction.entity.common.WcTransactionAppInfoContentUM
 import com.tangem.features.walletconnect.transaction.entity.common.WcTransactionRequestInfoUM
 
-@Immutable
 internal data class WcApproveTransactionUM(
     val transaction: WcApproveTransactionItemUM,
     val transactionRequestInfo: WcTransactionRequestInfoUM,
     val customAllowance: WcCustomAllowanceUM,
 )
 
-@Immutable
 internal data class WcApproveTransactionItemUM(
     val onDismiss: () -> Unit,
     val onSend: () -> Unit,
@@ -26,7 +23,6 @@ internal data class WcApproveTransactionItemUM(
     val isLoading: Boolean = false,
 ) : TangemBottomSheetConfigContent
 
-@Immutable
 internal data class WcCustomAllowanceUM(
     @DrawableRes val networkIconRes: Int,
     val tokenIconUrl: String,
