334d4c3ea4f1f63213d665452e465fc48cbe596d
Tangem <git@tangem.com>
2025-05-12 15:07:38 +0400

    Updated on 2026-06-27

--- features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/entity/common/WcTransactionRequestInfoUM.kt
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/entity/common/WcTransactionRequestInfoUM.kt
@@ -1,22 +1,18 @@
 package com.tangem.features.walletconnect.transaction.entity.common
 
-import androidx.compose.runtime.Immutable
 import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfigContent
 import com.tangem.core.ui.extensions.TextReference
 import kotlinx.collections.immutable.ImmutableList
 
-@Immutable
 internal data class WcTransactionRequestInfoUM(
     val blocks: ImmutableList<WcTransactionRequestBlockUM>,
     val onCopy: () -> Unit,
 ) : TangemBottomSheetConfigContent
 
-@Immutable
 internal data class WcTransactionRequestBlockUM(
     val info: ImmutableList<WcTransactionRequestInfoItemUM>,
 )
 
-@Immutable
 internal data class WcTransactionRequestInfoItemUM(
     val title: TextReference,
     val description: String = "",
