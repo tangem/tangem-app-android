86bf5906828d730d89cabd74640476966c08ee33
Tangem <git@tangem.com>
2025-05-13 16:29:46 +0700

    Updated on 2026-09-25

--- data/wallet-connect/src/main/kotlin/com/tangem/data/walletconnect/sign/BaseWcSignUseCase.kt
+++ data/wallet-connect/src/main/kotlin/com/tangem/data/walletconnect/sign/BaseWcSignUseCase.kt
@@ -1,7 +1,9 @@
 package com.tangem.data.walletconnect.sign
 
+import com.tangem.core.analytics.api.AnalyticsEventHandler
 import com.tangem.data.walletconnect.respond.WcRespondService
 import com.tangem.domain.tokens.model.Network
+import com.tangem.domain.walletconnect.WcAnalyticEvents
 import com.tangem.domain.walletconnect.model.WcSession
 import com.tangem.domain.walletconnect.model.sdkcopy.WcSdkSessionRequest
 import com.tangem.domain.walletconnect.usecase.method.WcMethodUseCase
@@ -16,6 +18,7 @@ internal abstract class BaseWcSignUseCase<MiddleAction, SignModel> :
     MiddleActionCollector<MiddleAction, SignModel> {
 
     abstract val respondService: WcRespondService
+    abstract val analytics: AnalyticsEventHandler
 
     abstract val context: WcMethodUseCaseContext
     override val network: Network get() = context.network
@@ -25,8 +28,20 @@ internal abstract class BaseWcSignUseCase<MiddleAction, SignModel> :
 
     protected val delegate by lazy {
         WcSignUseCaseDelegate(
+            analytics = analytics,
+            context = context,
             finalActionCollector = this,
             middleActionCollector = this,
+        )
+    }
+
+    init {
+        analytics.send(
+            WcAnalyticEvents.SignatureRequestReceived(
+                session = context.session,
+                rawRequest = context.rawSdkRequest,
+                network = context.network,
+            ),
         )
     }
 
@@ -34,8 +49,15 @@ internal abstract class BaseWcSignUseCase<MiddleAction, SignModel> :
         defaultReject()
     }
 
-    override fun sign() = delegate.sign()
-    override fun cancel() = delegate.cancel()
+    override fun sign() {
+        analytics.send(WcAnalyticEvents.ButtonSign(context.rawSdkRequest))
+        delegate.sign()
+    }
+
+    override fun cancel() {
+        analytics.send(WcAnalyticEvents.ButtonCancel(WcAnalyticEvents.ButtonCancel.Type.Sign))
+        delegate.cancel()
+    }
     protected fun middleAction(action: MiddleAction) = delegate.middleAction(action)
 
     protected fun defaultReject() {
