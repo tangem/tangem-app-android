1825074702c4559f9fe379ac5baed23a6d299176
Tangem <git@tangem.com>
2025-05-14 19:54:34 +0400

    Updated on 2026-04-15

--- data/networks/src/main/java/com/tangem/data/networks/di/NetworkStatusSupplierModule.kt
+++ data/networks/src/main/java/com/tangem/data/networks/di/NetworkStatusSupplierModule.kt
@@ -1,15 +1,9 @@
 package com.tangem.data.networks.di
 
-import androidx.datastore.core.DataStore
-import com.tangem.data.networks.store.DefaultNetworksStatusesStoreV2
-import com.tangem.data.networks.store.NetworksStatusesStoreV2
-import com.tangem.datasource.local.datastore.RuntimeSharedStore
-import com.tangem.datasource.local.network.entity.NetworkStatusDM
 import com.tangem.domain.networks.multi.MultiNetworkStatusProducer
 import com.tangem.domain.networks.multi.MultiNetworkStatusSupplier
 import com.tangem.domain.networks.single.SingleNetworkStatusProducer
 import com.tangem.domain.networks.single.SingleNetworkStatusSupplier
-import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -19,19 +13,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 internal object NetworkStatusSupplierModule {
-
-    @Provides
-    @Singleton
-    fun provideNetworksStatusesStoreV2(
-        persistenceNetworksStatusesStore: DataStore<Map<String, Set<NetworkStatusDM>>>,
-        dispatchers: CoroutineDispatcherProvider,
-    ): NetworksStatusesStoreV2 {
-        return DefaultNetworksStatusesStoreV2(
-            runtimeStore = RuntimeSharedStore(),
-            persistenceDataStore = persistenceNetworksStatusesStore,
-            dispatchers = dispatchers,
-        )
-    }
 
     @Provides
     @Singleton
