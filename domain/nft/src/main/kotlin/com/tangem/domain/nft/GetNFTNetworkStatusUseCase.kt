dfb8e3924d12299e0540788ba7a7018757ad8d52
Tangem <git@tangem.com>
2025-05-14 19:54:34 +0400

    Updated on 2026-06-05

--- domain/nft/src/main/kotlin/com/tangem/domain/nft/GetNFTNetworkStatusUseCase.kt
+++ domain/nft/src/main/kotlin/com/tangem/domain/nft/GetNFTNetworkStatusUseCase.kt
@@ -1,15 +1,20 @@
 package com.tangem.domain.nft
 
+import com.tangem.domain.networks.single.SingleNetworkStatusProducer
+import com.tangem.domain.networks.single.SingleNetworkStatusSupplier
 import com.tangem.domain.tokens.model.Network
 import com.tangem.domain.tokens.model.NetworkStatus
-import com.tangem.domain.tokens.repository.NetworksRepository
 import com.tangem.domain.wallets.models.UserWalletId
+import kotlinx.coroutines.flow.firstOrNull
 
 class GetNFTNetworkStatusUseCase(
-    private val networksRepository: NetworksRepository,
+    private val singleNetworkStatusSupplier: SingleNetworkStatusSupplier,
 ) {
 
-    suspend operator fun invoke(userWalletId: UserWalletId, network: Network): NetworkStatus? = networksRepository
-        .getNetworkStatusesSync(userWalletId, setOf(network), false)
-        .firstOrNull { it.network == network }
+    suspend operator fun invoke(userWalletId: UserWalletId, network: Network): NetworkStatus? {
+        return singleNetworkStatusSupplier(
+            params = SingleNetworkStatusProducer.Params(userWalletId = userWalletId, network = network),
+        )
+            .firstOrNull()
+    }
 }
