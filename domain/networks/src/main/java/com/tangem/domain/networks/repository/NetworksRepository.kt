9e8d0f4c33858577c3f565e11ec4496b7c1a5f6e
Tangem <git@tangem.com>
2025-05-14 15:33:20 +0400

    Updated on 2026-03-27

---
+++ domain/networks/src/main/java/com/tangem/domain/networks/repository/NetworksRepository.kt
@@ -0,0 +1,24 @@
+package com.tangem.domain.networks.repository
+
+import com.tangem.domain.tokens.model.CryptoCurrencyAddress
+import com.tangem.domain.tokens.model.Network
+import com.tangem.domain.wallets.models.UserWalletId
+
+/**
+ * Repository for working with pending transactions
+ *
+[REDACTED_AUTHOR]
+ */
+interface NetworksRepository {
+
+    /** Fetches pending transactions for given [network] in selected [userWalletId] */
+    suspend fun fetchPendingTransactions(userWalletId: UserWalletId, network: Network)
+
+    /**
+     * Returns addresses and crypto currency
+     *
+     * @param userWalletId the unique identifier of the user wallet
+     * @param network      network
+     */
+    suspend fun getNetworkAddresses(userWalletId: UserWalletId, network: Network): List<CryptoCurrencyAddress>
+}
