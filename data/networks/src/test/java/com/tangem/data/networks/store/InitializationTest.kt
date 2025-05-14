055ec81416623e3c8fe3c83332178f73761c8b9e
Tangem <git@tangem.com>
2025-05-14 17:13:57 +0400

    Updated on 2026-08-27

--- data/networks/src/test/java/com/tangem/data/networks/store/InitializationTest.kt
+++ data/networks/src/test/java/com/tangem/data/networks/store/InitializationTest.kt
@@ -20,7 +20,7 @@ import org.junit.Test
 /**
 [REDACTED_AUTHOR]
  */
-internal class NetworksStatusesStoreInitializationTest {
+internal class InitializationTest {
 
     @Test
     fun `test initialization if cache store is empty`() = runTest {
