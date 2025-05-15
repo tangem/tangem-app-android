1841d77c4cacd62d8e362a3aed4df4ce4277dff8
Tangem <git@tangem.com>
2025-05-15 08:39:07 +0400

    Updated on 2026-08-14

--- data/networks/src/main/java/com/tangem/data/networks/di/NetworkDataModule.kt
+++ data/networks/src/main/java/com/tangem/data/networks/di/NetworkDataModule.kt
@@ -1,24 +1,53 @@
 package com.tangem.data.networks.di
 
+import android.content.Context
 import androidx.datastore.core.DataStore
+import androidx.datastore.core.DataStoreFactory
+import androidx.datastore.dataStoreFile
+import com.squareup.moshi.Moshi
 import com.tangem.data.common.currency.CardCryptoCurrencyFactory
 import com.tangem.data.networks.repository.DefaultNetworksRepository
 import com.tangem.data.networks.store.DefaultNetworksStatusesStoreV2
 import com.tangem.data.networks.store.NetworksStatusesStoreV2
+import com.tangem.datasource.di.NetworkMoshi
 import com.tangem.datasource.local.datastore.RuntimeSharedStore
 import com.tangem.datasource.local.network.entity.NetworkStatusDM
+import com.tangem.datasource.utils.MoshiDataStoreSerializer
+import com.tangem.datasource.utils.mapWithStringKeyTypes
+import com.tangem.datasource.utils.setTypes
 import com.tangem.domain.networks.repository.NetworksRepository
 import com.tangem.domain.walletmanager.WalletManagersFacade
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal object NetworkDataModule {
+
+    @Singleton
+    @Provides
+    fun providePersistenceNetworksStatusesStore(
+        @NetworkMoshi moshi: Moshi,
+        @ApplicationContext context: Context,
+        dispatchers: CoroutineDispatcherProvider,
+    ): DataStore<Map<String, Set<NetworkStatusDM>>> {
+        return DataStoreFactory.create(
+            serializer = MoshiDataStoreSerializer(
+                moshi = moshi,
+                types = mapWithStringKeyTypes(valueTypes = setTypes<NetworkStatusDM>()),
+                defaultValue = emptyMap(),
+            ),
+            produceFile = { context.dataStoreFile(fileName = "networks_statuses") },
+            scope = CoroutineScope(context = dispatchers.io + SupervisorJob()),
+        )
+    }
 
     @Provides
     @Singleton
