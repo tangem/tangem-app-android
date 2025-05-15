99d2f782f397518963fe221bd7f969cefaeb2d10
Tangem <git@tangem.com>
2025-05-15 18:35:01 +0400

    Updated on 2026-08-19

---
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/entity/blockaid/WcEstimatedWalletChangesUM.kt
@@ -0,0 +1,16 @@
+package com.tangem.features.walletconnect.transaction.entity.blockaid
+
+import androidx.annotation.DrawableRes
+import com.tangem.core.ui.extensions.TextReference
+import kotlinx.collections.immutable.ImmutableList
+
+internal data class WcEstimatedWalletChangesUM(
+    val items: ImmutableList<WcEstimatedWalletChangeUM>,
+)
+
+internal data class WcEstimatedWalletChangeUM(
+    @DrawableRes val iconRes: Int,
+    val title: TextReference,
+    val description: String,
+    val tokenIconUrl: String,
+)
