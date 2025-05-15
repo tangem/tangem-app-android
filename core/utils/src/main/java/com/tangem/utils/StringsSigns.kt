2029662eb1e6a0934f34c68f074b673c34e5dca7
Tangem <git@tangem.com>
2025-05-15 18:03:07 +0500

    release/5.26.1

--- core/utils/src/main/java/com/tangem/utils/StringsSigns.kt
+++ core/utils/src/main/java/com/tangem/utils/StringsSigns.kt
@@ -8,6 +8,7 @@ object StringsSigns {
     const val DASH_SIGN = "—"
     const val LOWER_SIGN = "<"
     const val TILDE_SIGN = "~"
+    const val COMA_SIGN = ","
     const val INFINITY_SIGN = "∞"
     const val NON_BREAKING_SPACE = '\u00A0'
     const val PERCENT = "%"
