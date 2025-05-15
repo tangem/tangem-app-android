12df546e468214a9be6c22641df05452c05583d7
Tangem <git@tangem.com>
2025-05-15 23:47:29 +0500

    Updated on 2026-08-19

--- features/nft/impl/src/main/kotlin/com/tangem/features/nft/collections/model/NFTCollectionsModel.kt
+++ features/nft/impl/src/main/kotlin/com/tangem/features/nft/collections/model/NFTCollectionsModel.kt
@@ -11,6 +11,7 @@ import com.tangem.domain.nft.FetchNFTCollectionAssetsUseCase
 import com.tangem.domain.nft.GetNFTCollectionsUseCase
 import com.tangem.domain.nft.RefreshAllNFTUseCase
 import com.tangem.domain.nft.models.NFTCollection
+import com.tangem.domain.nft.models.NFTCollections
 import com.tangem.features.nft.collections.NFTCollectionsComponent
 import com.tangem.features.nft.collections.entity.NFTCollectionsStateUM
 import com.tangem.features.nft.collections.entity.NFTCollectionsUM
@@ -71,8 +72,7 @@ internal class NFTCollectionsModel @Inject constructor(
         ) { nftCollections, query ->
             _state.update {
                 UpdateDataStateTransformer(
-                    nftCollections = nftCollections,
-                    searchQuery = query,
+                    nftCollections = nftCollections.filter(query),
                     onReceiveClick = {
                         params.onReceiveClick()
                     },
@@ -89,6 +89,38 @@ internal class NFTCollectionsModel @Inject constructor(
         }
             .onStart { onRefresh() }
             .launchIn(modelScope)
+    }
+
+    private fun List<NFTCollections>.filter(query: String): List<NFTCollections> = map {
+        it.copy(
+            content = when (val content = it.content) {
+                is NFTCollections.Content.Collections -> content.copy(
+                    collections = content.collections.orEmpty().filter {
+                        val assetsFulfillQuery = if (query.isEmpty()) {
+                            true
+                        } else {
+                            when (val assets = it.assets) {
+                                is NFTCollection.Assets.Empty,
+                                is NFTCollection.Assets.Failed,
+                                is NFTCollection.Assets.Loading,
+                                -> false
+                                is NFTCollection.Assets.Value -> {
+                                    assets.items.any { asset ->
+                                        asset.name?.lowercase()?.contains(query.lowercase()) == true
+                                    }
+                                }
+                            }
+                        }
+
+                        val collectionFulfillQuery =
+                            query.isEmpty() || it.name?.lowercase()?.contains(query.lowercase()) == true
+
+                        collectionFulfillQuery || assetsFulfillQuery
+                    },
+                )
+                is NFTCollections.Content.Error -> it.content
+            },
+        )
     }
 
     private fun onRefresh() {
