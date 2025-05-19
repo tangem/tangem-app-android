068628dc16b2a6a2d235547f13eebe69ccc481d1
Tangem <git@tangem.com>
2025-05-19 14:10:44 +0500

    Updated on 2026-06-01

--- domain/wallets/src/main/java/com/tangem/domain/wallets/usecase/AssociateWalletsWithApplicationIdUseCase.kt
+++ domain/wallets/src/main/java/com/tangem/domain/wallets/usecase/AssociateWalletsWithApplicationIdUseCase.kt
@@ -2,16 +2,15 @@ package com.tangem.domain.wallets.usecase
 
 import arrow.core.Either
 import com.tangem.domain.notifications.models.ApplicationId
-import com.tangem.domain.wallets.legacy.UserWalletsListManager
+import com.tangem.domain.wallets.models.UserWallet
 import com.tangem.domain.wallets.repository.WalletsRepository
 
 class AssociateWalletsWithApplicationIdUseCase(
-    private val userWalletsListManager: UserWalletsListManager,
     private val walletsRepository: WalletsRepository,
 ) {
 
-    suspend operator fun invoke(applicationId: ApplicationId): Either<Throwable, Unit> = Either.catch {
-        val wallets = userWalletsListManager.userWalletsSync
-        walletsRepository.associateWallets(applicationId.value, wallets)
-    }
+    suspend operator fun invoke(applicationId: ApplicationId, wallets: List<UserWallet>): Either<Throwable, Unit> =
+        Either.catch {
+            walletsRepository.associateWallets(applicationId.value, wallets)
+        }
 }
