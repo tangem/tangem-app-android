8c6ec222d9dbfbadf7aa3e27c9eb6a5ae8e8a64e
Tangem <git@tangem.com>
2025-05-19 16:17:51 +0400

    Updated on 2026-06-12

--- domain/legacy/src/main/java/com/tangem/domain/walletmanager/model/CryptoCurrencyTransaction.kt
+++ domain/legacy/src/main/java/com/tangem/domain/walletmanager/model/CryptoCurrencyTransaction.kt
@@ -1,17 +1,17 @@
 package com.tangem.domain.walletmanager.model
 
-import com.tangem.domain.txhistory.models.TxHistoryItem
+import com.tangem.domain.models.network.TxInfo
 
 // TODO: [REDACTED_JIRA] move to txhistory module
 sealed class CryptoCurrencyTransaction {
 
-    abstract val txHistoryItem: TxHistoryItem
+    abstract val txInfo: TxInfo
 
-    data class Coin(override val txHistoryItem: TxHistoryItem) : CryptoCurrencyTransaction()
+    data class Coin(override val txInfo: TxInfo) : CryptoCurrencyTransaction()
 
     data class Token(
         val tokenId: String?,
         val tokenContractAddress: String,
-        override val txHistoryItem: TxHistoryItem,
+        override val txInfo: TxInfo,
     ) : CryptoCurrencyTransaction()
 }
