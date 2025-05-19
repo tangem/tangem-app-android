6fb937b7388e488759218af372953f31351bfde4
Tangem <git@tangem.com>
2025-05-19 13:50:46 +0400

    Updated on 2026-02-11

--- domain/nft/src/main/kotlin/com/tangem/domain/nft/GetNFTExploreUrlUseCase.kt
+++ domain/nft/src/main/kotlin/com/tangem/domain/nft/GetNFTExploreUrlUseCase.kt
@@ -1,9 +1,9 @@
 package com.tangem.domain.nft
 
 import arrow.core.raise.catch
+import com.tangem.domain.models.network.Network
 import com.tangem.domain.nft.models.NFTAsset
 import com.tangem.domain.nft.repository.NFTRepository
-import com.tangem.domain.tokens.model.Network
 
 class GetNFTExploreUrlUseCase(
     private val nftRepository: NFTRepository,
