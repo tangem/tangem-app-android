92408da7b4f6eb72418c1517350d6cc1f0c98d38
Tangem <git@tangem.com>
2025-05-19 22:49:30 +0700

    Updated on 2026-08-27

---
+++ data/wallet-connect/src/main/kotlin/com/tangem/data/walletconnect/network/ethereum/WcEthAddNetworkUseCase.kt
@@ -0,0 +1,42 @@
+package com.tangem.data.walletconnect.network.ethereum
+
+import arrow.core.Either
+import com.tangem.data.walletconnect.respond.WcRespondService
+import com.tangem.data.walletconnect.sign.WcMethodUseCaseContext
+import com.tangem.domain.tokens.model.Network
+import com.tangem.domain.walletconnect.model.WcEthMethod
+import com.tangem.domain.walletconnect.model.WcSession
+import com.tangem.domain.walletconnect.model.sdkcopy.WcSdkSessionRequest
+import com.tangem.domain.walletconnect.usecase.method.WcAddNetworkUseCase
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+
+internal class WcEthAddNetworkUseCase @AssistedInject constructor(
+    private val respondService: WcRespondService,
+    @Assisted val context: WcMethodUseCaseContext,
+    @Assisted override val method: WcEthMethod.AddEthereumChain,
+) : WcAddNetworkUseCase {
+
+    override val session: WcSession
+        get() = context.session
+    override val rawSdkRequest: WcSdkSessionRequest
+        get() = context.rawSdkRequest
+    override val network: Network
+        get() = context.network
+    override val walletAddress: String
+        get() = context.accountAddress
+
+    override suspend fun approve(): Either<Throwable, Unit> {
+        return respondService.respond(rawSdkRequest, "")
+    }
+
+    override fun reject() {
+        respondService.rejectRequestNonBlock(rawSdkRequest)
+    }
+
+    @AssistedFactory
+    interface Factory {
+        fun create(context: WcMethodUseCaseContext, method: WcEthMethod.AddEthereumChain): WcEthAddNetworkUseCase
+    }
+}
