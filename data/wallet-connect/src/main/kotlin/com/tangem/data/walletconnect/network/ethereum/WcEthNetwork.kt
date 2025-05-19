f1496a7d75e4e8d894bc3871c254518ea6c363d2
Tangem <git@tangem.com>
2025-05-19 22:49:30 +0700

    Updated on 2025-12-05

--- data/wallet-connect/src/main/kotlin/com/tangem/data/walletconnect/network/ethereum/WcEthNetwork.kt
+++ data/wallet-connect/src/main/kotlin/com/tangem/data/walletconnect/network/ethereum/WcEthNetwork.kt
@@ -10,6 +10,7 @@ import com.tangem.data.walletconnect.request.WcRequestToUseCaseConverter.Compani
 import com.tangem.data.walletconnect.sign.WcMethodUseCaseContext
 import com.tangem.data.walletconnect.utils.WcNamespaceConverter
 import com.tangem.domain.tokens.model.Network
+import com.tangem.domain.walletconnect.model.WcEthAddChain
 import com.tangem.domain.walletconnect.model.WcEthMethod
 import com.tangem.domain.walletconnect.model.WcEthMethodName
 import com.tangem.domain.walletconnect.model.WcEthSignTypedDataParams
@@ -17,6 +18,7 @@ import com.tangem.domain.walletconnect.model.WcEthTransactionParams
 import com.tangem.domain.walletconnect.model.sdkcopy.WcSdkSessionRequest
 import com.tangem.domain.walletconnect.repository.WcSessionsManager
 import com.tangem.domain.walletconnect.usecase.method.WcMethodUseCase
+import com.tangem.domain.walletmanager.WalletManagersFacade
 import com.tangem.domain.wallets.models.UserWallet
 import jakarta.inject.Inject
 
@@ -25,6 +27,7 @@ internal class WcEthNetwork(
     private val sessionsManager: WcSessionsManager,
     private val factories: Factories,
     private val namespaceConverter: NamespaceConverter,
+    private val walletManagersFacade: WalletManagersFacade,
 ) : WcRequestToUseCaseConverter {
 
     override fun toWcMethodName(request: WcSdkSessionRequest): WcEthMethodName? {
@@ -33,16 +36,19 @@ internal class WcEthNetwork(
         return name
     }
 
+    @Suppress("CyclomaticComplexMethod")
     override suspend fun toUseCase(request: WcSdkSessionRequest): WcMethodUseCase? {
         val name = toWcMethodName(request) ?: return null
-        val method: WcEthMethod = name.toMethod(request) ?: return null
         val session = sessionsManager.findSessionByTopic(request.topic) ?: return null
+        val method: WcEthMethod = name.toMethod(request, session.wallet) ?: return null
         val network = namespaceConverter.toNetwork(request.chainId.orEmpty(), session.wallet) ?: return null
+        val walletManagerAddress = walletManagersFacade.getDefaultAddress(session.wallet.walletId, network).orEmpty()
         val accountAddress = when (method) {
             is WcEthMethod.MessageSign -> method.account
             is WcEthMethod.SendTransaction -> method.transaction.from
             is WcEthMethod.SignTransaction -> method.transaction.from
             is WcEthMethod.SignTypedData -> method.account
+            is WcEthMethod.AddEthereumChain -> walletManagerAddress
         }
         val context = WcMethodUseCaseContext(
             session = session,
@@ -55,10 +61,11 @@ internal class WcEthNetwork(
             is WcEthMethod.SendTransaction -> factories.sendTransaction.create(context, method)
             is WcEthMethod.SignTransaction -> factories.signTransaction.create(context, method)
             is WcEthMethod.SignTypedData -> factories.signTypedData.create(context, method)
+            is WcEthMethod.AddEthereumChain -> factories.addNetwork.create(context, method)
         }
     }
 
-    private fun WcEthMethodName.toMethod(request: WcSdkSessionRequest): WcEthMethod? {
+    private fun WcEthMethodName.toMethod(request: WcSdkSessionRequest, wallet: UserWallet): WcEthMethod? {
         val rawParams = request.request.params
         return when (this) {
             WcEthMethodName.EthSign,
@@ -77,6 +84,13 @@ internal class WcEthNetwork(
                     } else {
                         WcEthMethod.SendTransaction(transaction = it)
                     }
+                }
+            WcEthMethodName.AddEthereumChain -> moshi.fromJson<List<WcEthAddChain>>(rawParams)
+                ?.firstOrNull()
+                ?.let {
+                    val newNetwork = namespaceConverter
+                        .toNetwork(it.chainId, wallet) ?: return null
+                    WcEthMethod.AddEthereumChain(rawChain = it, network = newNetwork)
                 }
         }
     }
@@ -130,5 +144,6 @@ internal class WcEthNetwork(
         val signTypedData: WcEthSignTypedDataUseCase.Factory,
         val sendTransaction: WcEthSendTransactionUseCase.Factory,
         val signTransaction: WcEthSignTransactionUseCase.Factory,
+        val addNetwork: WcEthAddNetworkUseCase.Factory,
     )
 }
