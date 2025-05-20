19b20c32b1ad88656c27ff6ea82b69626a01a325
Tangem <git@tangem.com>
2025-05-20 16:37:36 +0400

    Updated on 2026-06-02

--- data/networks/src/test/java/com/tangem/data/networks/NetworkStatusExt.kt
+++ data/networks/src/test/java/com/tangem/data/networks/NetworkStatusExt.kt
@@ -5,12 +5,7 @@ import com.tangem.data.networks.models.SimpleNetworkStatus
 import com.tangem.datasource.local.network.entity.NetworkStatusDM
 import com.tangem.domain.tokens.model.NetworkStatus
 
-internal fun NetworkStatus.toSimple(): SimpleNetworkStatus {
-    return SimpleNetworkStatus(
-        id = SimpleNetworkStatus.Id(network = network),
-        value = value,
-    )
-}
+internal fun NetworkStatus.toSimple() = SimpleNetworkStatus(status = this)
 
 internal fun NetworkStatus.toDataModel(): NetworkStatusDM? {
     return NetworkStatusDataModelConverter.convert(value = this)
