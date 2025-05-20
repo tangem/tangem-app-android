508248dfcce34f45f380a47e47093863497da960
Tangem <git@tangem.com>
2025-05-20 23:37:58 +0700

    Updated on 2026-09-21

--- data/wallet-connect/src/main/kotlin/com/tangem/data/walletconnect/network/solana/WcSolanaSignTransactionUseCase.kt
+++ data/wallet-connect/src/main/kotlin/com/tangem/data/walletconnect/network/solana/WcSolanaSignTransactionUseCase.kt
@@ -2,7 +2,7 @@ package com.tangem.data.walletconnect.network.solana
 
 import arrow.core.left
 import com.tangem.blockchain.common.TransactionData
-import com.tangem.blockchain.extensions.encodeBase64
+import com.tangem.blockchain.extensions.encodeBase58
 import com.tangem.core.analytics.api.AnalyticsEventHandler
 import com.tangem.data.walletconnect.respond.WcRespondService
 import com.tangem.data.walletconnect.sign.BaseWcSignUseCase
@@ -13,24 +13,22 @@ import com.tangem.data.walletconnect.utils.BlockAidVerificationDelegate
 import com.tangem.domain.transaction.usecase.PrepareForSendUseCase
 import com.tangem.domain.walletconnect.model.WcSolanaMethod
 import com.tangem.domain.walletconnect.usecase.method.WcSignState
-import com.tangem.domain.walletconnect.usecase.solana.WcSolanaSignAllTransactionUseCase
+import com.tangem.domain.walletconnect.usecase.method.WcTransactionUseCase
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import kotlinx.coroutines.flow.Flow
 import okio.ByteString.Companion.decodeBase64
-import org.json.JSONArray
-import org.json.JSONObject
 
-internal class DefaultWcSolanaSignAllTransactionUseCase @AssistedInject constructor(
+internal class WcSolanaSignTransactionUseCase @AssistedInject constructor(
     override val respondService: WcRespondService,
     override val analytics: AnalyticsEventHandler,
     private val prepareForSend: PrepareForSendUseCase,
     @Assisted override val context: WcMethodUseCaseContext,
-    @Assisted override val method: WcSolanaMethod.SignAllTransaction,
+    @Assisted override val method: WcSolanaMethod.SignTransaction,
     blockAidDelegate: BlockAidVerificationDelegate,
-) : BaseWcSignUseCase<Nothing, List<TransactionData.Compiled>>(),
-    WcSolanaSignAllTransactionUseCase {
+) : BaseWcSignUseCase<Nothing, TransactionData>(),
+    WcTransactionUseCase {
 
     override val securityStatus = blockAidDelegate.getSecurityStatus(
         network = network,
@@ -40,51 +38,32 @@ internal class DefaultWcSolanaSignAllTransactionUseCase @AssistedInject construc
         accountAddress = context.accountAddress,
     )
 
-    override suspend fun SignCollector<List<TransactionData.Compiled>>.onSign(
-        state: WcSignState<List<TransactionData.Compiled>>,
-    ) {
+    override suspend fun SignCollector<TransactionData>.onSign(state: WcSignState<TransactionData>) {
         val hash = prepareForSend.invoke(transactionData = state.signModel, userWallet = wallet, network = network)
             .onLeft { error ->
                 emit(state.toResult(error.left()))
             }
             .getOrNull()
             ?: return
-        val respond = getSolanaResultTxHashesString(hash)
+        val respond = "{ signature: \"${hash.encodeBase58()}\" }"
         val respondResult = respondService.respond(rawSdkRequest, respond)
         emit(state.toResult(respondResult))
     }
 
-    override fun invoke(): Flow<WcSignState<List<TransactionData.Compiled>>> {
-        val transactionData = method.transaction
-            .map { it.decodeBase64()?.toByteArray() ?: ByteArray(0) }
-            .map { TransactionData.Compiled(value = TransactionData.Compiled.Data.Bytes(it)) }
+    override fun invoke(): Flow<WcSignState<TransactionData>> {
+        val data = method.transaction.decodeBase64()?.toByteArray() ?: ByteArray(0)
 
+        val transactionData = TransactionData.Compiled(
+            value = TransactionData.Compiled.Data.Bytes(data),
+        )
         return delegate.invoke(transactionData)
-    }
-
-    /**
-     * Build json object
-     * {
-     *   "transactions": [
-     *     "signed_tx_hash"
-     *   ]
-     * }
-     */
-    private fun getSolanaResultTxHashesString(signedHashes: List<ByteArray>): String {
-        val result = JSONObject()
-        val transactions = JSONArray()
-        signedHashes.forEach {
-            transactions.put(it.encodeBase64())
-        }
-        result.put("transactions", transactions)
-        return result.toString()
     }
 
     @AssistedFactory
     interface Factory {
         fun create(
             context: WcMethodUseCaseContext,
-            method: WcSolanaMethod.SignAllTransaction,
-        ): DefaultWcSolanaSignAllTransactionUseCase
+            method: WcSolanaMethod.SignTransaction,
+        ): WcSolanaSignTransactionUseCase
     }
 }
