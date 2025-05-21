601da2ff092512551e56655a51649e5a9e19c30f
Tangem <git@tangem.com>
2025-05-21 17:02:18 +0400

    Updated on 2026-09-01

--- data/networks/src/main/java/com/tangem/data/networks/repository/DefaultNetworksRepository.kt
+++ data/networks/src/main/java/com/tangem/data/networks/repository/DefaultNetworksRepository.kt
@@ -5,9 +5,9 @@ import com.tangem.data.networks.store.NetworksStatusesStoreV2
 import com.tangem.data.networks.store.storeStatus
 import com.tangem.data.networks.utils.NetworkStatusFactory
 import com.tangem.domain.models.currency.CryptoCurrency
+import com.tangem.domain.models.network.CryptoCurrencyAddress
 import com.tangem.domain.models.network.Network
 import com.tangem.domain.networks.repository.NetworksRepository
-import com.tangem.domain.tokens.model.CryptoCurrencyAddress
 import com.tangem.domain.walletmanager.WalletManagersFacade
 import com.tangem.domain.wallets.models.UserWalletId
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
