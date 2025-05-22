4a8bb30982b8fb47ec70a36af658461ac534b2b0
Tangem <git@tangem.com>
2025-05-22 17:41:04 +0400

    Updated on 2026-06-10

--- domain/card/src/main/kotlin/com/tangem/domain/card/NetworkHasDerivationUseCase.kt
+++ domain/card/src/main/kotlin/com/tangem/domain/card/NetworkHasDerivationUseCase.kt
@@ -1,7 +1,7 @@
 package com.tangem.domain.card
 
 import arrow.core.Either
-import com.tangem.blockchain.common.Blockchain
+import com.tangem.blockchainsdk.utils.toBlockchain
 import com.tangem.domain.common.util.hasDerivation
 import com.tangem.domain.models.network.Network
 import com.tangem.domain.models.scan.ScanResponse
@@ -9,7 +9,7 @@ import com.tangem.domain.models.scan.ScanResponse
 class NetworkHasDerivationUseCase {
 
     operator fun invoke(scanResponse: ScanResponse, network: Network): Either<Throwable, Boolean> {
-        val blockchain = Blockchain.fromId(network.rawId)
+        val blockchain = network.toBlockchain()
         val derivationPath = network.derivationPath.value
         return Either.catch { derivationPath != null && scanResponse.hasDerivation(blockchain, derivationPath) }
     }
