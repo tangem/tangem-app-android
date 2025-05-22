46a2721c5528c764bfec8fa677fcb879a86b30f7
Tangem <git@tangem.com>
2025-05-22 15:25:03 +0500

    Updated on 2026-09-23

---
+++ features/nft/impl/src/main/kotlin/com/tangem/features/nft/entity/DefaultNFTSendSuccessTrigger.kt
@@ -0,0 +1,25 @@
+package com.tangem.features.nft.entity
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * Listens to success nft send trigger
+ */
+interface NFTSendSuccessListener {
+    val nftSendSuccessFlow: Flow<Unit>
+}
+
+@Singleton
+internal class DefaultNFTSendSuccessTrigger @Inject constructor() : NFTSendSuccessTrigger, NFTSendSuccessListener {
+
+    override val nftSendSuccessFlow: SharedFlow<Unit>
+    field = MutableSharedFlow<Unit>()
+
+    override suspend fun triggerSuccessNFTSend() {
+        nftSendSuccessFlow.emit(Unit)
+    }
+}
