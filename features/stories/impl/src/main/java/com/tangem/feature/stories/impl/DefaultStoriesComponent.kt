8e2710a18be7a4cf2e32622039772eea358ebd15
Tangem <git@tangem.com>
2025-05-22 10:43:14 +0300

    Updated on 2026-09-14

--- features/stories/impl/src/main/java/com/tangem/feature/stories/impl/DefaultStoriesComponent.kt
+++ features/stories/impl/src/main/java/com/tangem/feature/stories/impl/DefaultStoriesComponent.kt
@@ -1,16 +1,14 @@
 package com.tangem.feature.stories.impl
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.Stable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.tangem.common.ui.swapStoriesScreen.SwapStoriesScreen
 import com.tangem.core.decompose.context.AppComponentContext
 import com.tangem.core.decompose.model.getOrCreateModel
-import com.tangem.core.ui.res.LocalRootBackgroundColor
 import com.tangem.core.ui.res.TangemColorPalette
+import com.tangem.core.ui.utils.ChangeRootBackgroundColorEffect
 import com.tangem.feature.stories.api.StoriesComponent
 import com.tangem.feature.stories.impl.model.StoriesModel
 import dagger.assisted.Assisted
@@ -29,16 +27,7 @@ internal class DefaultStoriesComponent @AssistedInject constructor(
     override fun Content(modifier: Modifier) {
         val state = model.state.collectAsStateWithLifecycle()
         SwapStoriesScreen(state.value)
-
-        val rootBackgroundColor = LocalRootBackgroundColor.current
-        val previousColor = remember { rootBackgroundColor.value }
-
-        DisposableEffect(Unit) {
-            rootBackgroundColor.value = TangemColorPalette.Black
-            onDispose {
-                rootBackgroundColor.value = previousColor
-            }
-        }
+        ChangeRootBackgroundColorEffect(TangemColorPalette.Black)
     }
 
     @AssistedFactory
