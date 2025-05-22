4b6a093ba91edca0bc137cbcde50a56850b191a6
Tangem <git@tangem.com>
2025-05-22 14:13:05 +0500

    Updated on 2026-08-14

--- features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/connections/entity/WcAppInfoWalletUM.kt
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/connections/entity/WcAppInfoWalletUM.kt
@@ -1,10 +1,10 @@
 package com.tangem.features.walletconnect.connections.entity
 
 import com.tangem.common.ui.userwallet.state.UserWalletItemUM
-import com.tangem.domain.wallets.models.UserWallet
+import com.tangem.domain.wallets.models.UserWalletId
 import kotlinx.collections.immutable.ImmutableList
 
 internal data class WcAppInfoWalletUM(
     val wallets: ImmutableList<UserWalletItemUM>,
-    val selectedUserWallet: UserWallet,
+    val selectedUserWalletId: UserWalletId,
 )
