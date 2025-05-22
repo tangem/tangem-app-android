3f24ced7ab6e9941c0ac2020511fc63f95d51bb9
Tangem <git@tangem.com>
2025-05-22 14:13:05 +0500

    Updated on 2026-08-19

--- features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/connections/model/transformers/WcSelectNetworksTransformer.kt
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/connections/model/transformers/WcSelectNetworksTransformer.kt
@@ -2,19 +2,23 @@ package com.tangem.features.walletconnect.connections.model.transformers
 
 import com.tangem.core.ui.extensions.getGreyedOutIconRes
 import com.tangem.core.ui.extensions.iconResId
-import com.tangem.domain.walletconnect.model.WcSessionProposal
+import com.tangem.domain.models.network.Network
 import com.tangem.features.walletconnect.connections.entity.WcNetworkInfoItem
 import com.tangem.features.walletconnect.connections.entity.WcSelectNetworksUM
 import com.tangem.utils.transformer.Transformer
 import kotlinx.collections.immutable.toImmutableList
 
 internal class WcSelectNetworksTransformer(
-    private val proposalNetwork: WcSessionProposal.ProposalNetwork,
+    private val missingRequiredNetworks: Set<Network>,
+    private val requiredNetworks: Set<Network>,
+    private val availableNetworks: Set<Network>,
+    private val notAddedNetworks: Set<Network>,
+    private val enabledNetworks: Set<Network.RawID>,
     private val onCheckedChange: (Boolean, String) -> Unit,
 ) : Transformer<WcSelectNetworksUM> {
     override fun transform(prevState: WcSelectNetworksUM): WcSelectNetworksUM {
         return prevState.copy(
-            missing = proposalNetwork.missingRequired.map { network ->
+            missing = missingRequiredNetworks.map { network ->
                 WcNetworkInfoItem.Required(
                     id = network.rawId,
                     icon = network.iconResId,
@@ -22,7 +26,7 @@ internal class WcSelectNetworksTransformer(
                     symbol = network.currencySymbol,
                 )
             }.toImmutableList(),
-            required = proposalNetwork.required.map { network ->
+            required = requiredNetworks.map { network ->
                 WcNetworkInfoItem.Checked(
                     id = network.rawId,
                     icon = network.iconResId,
@@ -30,17 +34,17 @@ internal class WcSelectNetworksTransformer(
                     symbol = network.currencySymbol,
                 )
             }.toImmutableList(),
-            available = proposalNetwork.available.map { network ->
+            available = availableNetworks.map { network ->
                 WcNetworkInfoItem.Checkable(
                     id = network.rawId,
                     icon = network.iconResId,
                     name = network.name,
                     symbol = network.currencySymbol,
-                    checked = false,
+                    checked = network.id.rawId in enabledNetworks,
                     onCheckedChange = { onCheckedChange(it, network.rawId) },
                 )
             }.toImmutableList(),
-            notAdded = proposalNetwork.notAdded.map { network ->
+            notAdded = notAddedNetworks.map { network ->
                 WcNetworkInfoItem.ReadOnly(
                     id = network.rawId,
                     icon = getGreyedOutIconRes(network.rawId),
