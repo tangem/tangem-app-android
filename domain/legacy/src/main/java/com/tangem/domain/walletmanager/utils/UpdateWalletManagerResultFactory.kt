e5d0c7a689c126c248989b63c53dd9916f6f0f57
Tangem <git@tangem.com>
2025-05-26 17:42:58 +0400

    Updated on 2026-08-07

--- domain/legacy/src/main/java/com/tangem/domain/walletmanager/utils/UpdateWalletManagerResultFactory.kt
+++ domain/legacy/src/main/java/com/tangem/domain/walletmanager/utils/UpdateWalletManagerResultFactory.kt
@@ -9,8 +9,10 @@ import timber.log.Timber
 import java.math.BigDecimal
 import com.tangem.blockchain.common.address.Address as SdkAddress
 
+/** Factory for creating [UpdateWalletManagerResult] */
 internal class UpdateWalletManagerResultFactory {
 
+    /** Get [Verified] result for [walletManager] */
     fun getResult(walletManager: WalletManager): Verified {
         val wallet = walletManager.wallet
         val addresses = getAvailableAddresses(wallet.addresses)
@@ -25,6 +27,12 @@ internal class UpdateWalletManagerResultFactory {
         )
     }
 
+    /**
+     * Get demo [Verified] result
+     *
+     * @param walletManager wallet manager
+     * @param demoAmount    amount that will be used for demo result
+     */
     fun getDemoResult(walletManager: WalletManager, demoAmount: Amount): Verified {
         val wallet = walletManager.wallet
         val addresses = getAvailableAddresses(wallet.addresses)
@@ -39,6 +47,14 @@ internal class UpdateWalletManagerResultFactory {
         )
     }
 
+    /**
+     * Get [NoAccount] result.
+     * If unable to get required amount for creating account, [Unreachable] result will be returned.
+     *
+     * @param walletManager         wallet manager
+     * @param customMessage         custom error message
+     * @param amountToCreateAccount amount to create account
+     */
     fun getNoAccountResult(
         walletManager: WalletManager,
         customMessage: String,
@@ -65,7 +81,8 @@ internal class UpdateWalletManagerResultFactory {
         }
     }
 
-    fun getUnreachableResult(walletManager: WalletManager): UpdateWalletManagerResult {
+    /** Get [Unreachable] result for [walletManager] */
+    fun getUnreachableResult(walletManager: WalletManager): Unreachable {
         val wallet = walletManager.wallet
 
         return Unreachable(
@@ -74,8 +91,44 @@ internal class UpdateWalletManagerResultFactory {
         )
     }
 
+    private fun getAvailableAddresses(addresses: Set<SdkAddress>): Set<Address> {
+        return SdkAddressToAddressConverter.convertList(addresses).toSet()
+    }
+
     private fun getTokensAmounts(amounts: Set<Amount>): Set<CryptoCurrencyAmount> {
         return amounts.mapNotNullTo(hashSetOf(), ::createCurrencyAmount)
+    }
+
+    private fun createCurrencyAmount(amount: Amount): CryptoCurrencyAmount? {
+        return when (val type = amount.type) {
+            is AmountType.Token -> {
+                val value = getCurrencyAmountValue(amount) ?: return null
+
+                CryptoCurrencyAmount.Token(
+                    currencyRawId = type.token.id?.let(CryptoCurrency::RawID),
+                    contractAddress = type.token.contractAddress,
+                    value = value,
+                )
+            }
+            is AmountType.Coin -> {
+                val value = getCurrencyAmountValue(amount) ?: return null
+
+                CryptoCurrencyAmount.Coin(value = value)
+            }
+            is AmountType.FeeResource,
+            is AmountType.Reserve,
+            -> null
+        }
+    }
+
+    private fun getCurrencyAmountValue(amount: Amount): BigDecimal? {
+        val value = amount.value
+
+        if (value == null) {
+            Timber.w("Currency amount must not be null: ${amount.currencySymbol}")
+        }
+
+        return value
     }
 
     private fun getDemoTokensAmounts(demoAmount: Amount, tokens: Set<Token>): Set<CryptoCurrencyAmount> {
@@ -95,31 +148,13 @@ internal class UpdateWalletManagerResultFactory {
         txHistoryItemConverter: TransactionDataToTxHistoryItemConverter,
         recentTransactions: Set<TransactionData.Uncompiled>,
     ): Set<CryptoCurrencyTransaction> {
-        val unconfirmedTransactions = recentTransactions.filter {
-            it.status == TransactionStatus.Unconfirmed
-        }
+        val unconfirmedTransactions = recentTransactions.filter { it.status == TransactionStatus.Unconfirmed }
 
         return unconfirmedTransactions.mapNotNullTo(hashSetOf()) {
             createCurrencyTransaction(
                 txHistoryItemConverter = txHistoryItemConverter,
                 data = it,
             )
-        }
-    }
-
-    private fun createCurrencyAmount(amount: Amount): CryptoCurrencyAmount? {
-        return when (val type = amount.type) {
-            is AmountType.Token -> CryptoCurrencyAmount.Token(
-                currencyRawId = type.token.id?.let(CryptoCurrency::RawID),
-                contractAddress = type.token.contractAddress,
-                value = getCurrencyAmountValue(amount) ?: return null,
-            )
-            is AmountType.Coin -> CryptoCurrencyAmount.Coin(
-                value = getCurrencyAmountValue(amount) ?: return null,
-            )
-            is AmountType.FeeResource,
-            AmountType.Reserve,
-            -> null
         }
     }
 
@@ -130,10 +165,12 @@ internal class UpdateWalletManagerResultFactory {
         return when (val type = data.amount.type) {
             is AmountType.Coin -> {
                 val txHistoryItem = txHistoryItemConverter.convert(data) ?: return null
-                CryptoCurrencyTransaction.Coin(txHistoryItem)
+
+                CryptoCurrencyTransaction.Coin(txInfo = txHistoryItem)
             }
             is AmountType.Token -> {
                 val txHistoryItem = txHistoryItemConverter.convert(data) ?: return null
+
                 CryptoCurrencyTransaction.Token(
                     tokenId = type.token.id,
                     contractAddress = type.token.contractAddress,
@@ -141,22 +178,8 @@ internal class UpdateWalletManagerResultFactory {
                 )
             }
             is AmountType.FeeResource,
-            AmountType.Reserve,
+            is AmountType.Reserve,
             -> null
         }
-    }
-
-    private fun getAvailableAddresses(addresses: Set<SdkAddress>): Set<Address> {
-        return SdkAddressToAddressConverter.convertList(addresses).toSet()
-    }
-
-    private fun getCurrencyAmountValue(amount: Amount): BigDecimal? {
-        val value = amount.value
-
-        if (value == null) {
-            Timber.w("Currency amount must not be null: ${amount.currencySymbol}")
-        }
-
-        return value
     }
 }
