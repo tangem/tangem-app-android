1ac41b7be32f4b67211667243df13cd178748902
Tangem <git@tangem.com>
2025-05-27 19:20:31 +0500

    Updated on 2026-09-25

--- core/utils/src/main/java/com/tangem/utils/extensions/StringExt.kt
+++ core/utils/src/main/java/com/tangem/utils/extensions/StringExt.kt
@@ -5,7 +5,7 @@ private const val DEEPLINK_VALIDATION_REGEX = "['\";<>()+\\\\]"
 /**
  * Check for malicious symbol in uri part
  */
-fun String.validate(): Boolean {
+fun String.uriValidate(): Boolean {
     val regex = DEEPLINK_VALIDATION_REGEX.toRegex()
 
     return !regex.containsMatchIn(this)
