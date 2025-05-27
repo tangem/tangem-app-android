096f4710a91b4669804c379b408c00b9c0db35a6
Tangem <git@tangem.com>
2025-05-27 20:22:58 +0400

    Updated on 2026-07-23

--- features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/components/WcTransactionRequestInfoComponent.kt
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/components/WcTransactionRequestInfoComponent.kt
@@ -2,23 +2,20 @@ package com.tangem.features.walletconnect.transaction.components
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.tangem.core.decompose.context.AppComponentContext
 import com.tangem.core.ui.decompose.ComposableContentComponent
-import com.tangem.features.walletconnect.transaction.model.WcSignTransactionModel
+import com.tangem.features.walletconnect.transaction.entity.common.WcTransactionRequestInfoUM
 import com.tangem.features.walletconnect.transaction.ui.common.TransactionRequestInfoContent
 
-internal class WcSignTransactionRequestInfoComponent(
+internal class WcTransactionRequestInfoComponent(
     private val appComponentContext: AppComponentContext,
-    private val model: WcSignTransactionModel,
+    private val model: WcTransactionRequestInfoUM?,
 ) : AppComponentContext by appComponentContext, ComposableContentComponent {
 
     @Composable
     override fun Content(modifier: Modifier) {
-        val content = model.uiState.collectAsStateWithLifecycle().value
-
-        if (content != null) {
-            TransactionRequestInfoContent(content.transactionRequestInfo)
+        if (model != null) {
+            TransactionRequestInfoContent(model)
         }
     }
 }
