9b81553c9b9804b62dfe69512970a003bcfe08be
Tangem <git@tangem.com>
2025-05-28 22:51:59 +0400

    Updated on 2026-03-25

--- features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/di/WalletConnectModelModule.kt
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/di/WalletConnectModelModule.kt
@@ -4,6 +4,8 @@ import com.tangem.core.decompose.di.ModelComponent
 import com.tangem.core.decompose.model.Model
 import com.tangem.features.walletconnect.connections.model.*
 import com.tangem.features.walletconnect.connections.routing.WcRoutingModel
+import com.tangem.features.walletconnect.transaction.model.WcAddNetworkModel
+import com.tangem.features.walletconnect.transaction.model.WcSendTransactionModel
 import com.tangem.features.walletconnect.transaction.model.WcSignTransactionModel
 import dagger.Binds
 import dagger.Module
@@ -49,4 +51,14 @@ internal interface WalletConnectModelModule {
     @IntoMap
     @ClassKey(WcSignTransactionModel::class)
     fun bindWcSignTransactionModel(model: WcSignTransactionModel): Model
+
+    @Binds
+    @IntoMap
+    @ClassKey(WcAddNetworkModel::class)
+    fun bindWcAddNetworkModel(model: WcAddNetworkModel): Model
+
+    @Binds
+    @IntoMap
+    @ClassKey(WcSendTransactionModel::class)
+    fun bindWcSendTransactionModel(model: WcSendTransactionModel): Model
 }
