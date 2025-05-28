3a9d6f9085cedc20840804f95f77b43845a685c4
Tangem <git@tangem.com>
2025-05-28 22:45:52 +0400

    Updated on 2026-05-13

--- features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/routes/WcTransactionRoutes.kt
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/routes/WcTransactionRoutes.kt
@@ -1,12 +1,13 @@
 package com.tangem.features.walletconnect.transaction.routes
 
 import androidx.compose.runtime.Immutable
+import com.tangem.core.decompose.navigation.Route
 import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfigContent
 import kotlinx.serialization.Serializable
 
 @Serializable
 @Immutable
-internal sealed class WcTransactionRoutes : TangemBottomSheetConfigContent {
+internal sealed class WcTransactionRoutes : TangemBottomSheetConfigContent, Route {
     @Serializable
     data object Transaction : WcTransactionRoutes()
 
