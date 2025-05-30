c6ab7ca068797380fb86a22bc8d9f640cd1a66d3
Tangem <git@tangem.com>
2025-05-30 17:36:17 +0400

    Updated on 2026-03-25

--- domain/quotes/src/main/java/com/tangem/domain/quotes/single/SingleQuoteProducer.kt
+++ domain/quotes/src/main/java/com/tangem/domain/quotes/single/SingleQuoteProducer.kt
@@ -2,14 +2,14 @@ package com.tangem.domain.quotes.single
 
 import com.tangem.domain.core.flow.FlowProducer
 import com.tangem.domain.models.currency.CryptoCurrency
-import com.tangem.domain.tokens.model.Quote
+import com.tangem.domain.tokens.model.QuoteStatus
 
 /**
  * Producer of quote [CryptoCurrency.RawID]
  *
 [REDACTED_AUTHOR]
  */
-interface SingleQuoteProducer : FlowProducer<Quote> {
+interface SingleQuoteProducer : FlowProducer<QuoteStatus> {
 
     data class Params(val rawCurrencyId: CryptoCurrency.RawID)
 
