f99d2b532ee0e3db882baed62fa4acc16af496f2
Tangem <git@tangem.com>
2025-05-30 17:36:17 +0400

    Updated on 2026-08-19

--- domain/quotes/src/main/java/com/tangem/domain/quotes/QuotesRepositoryV2.kt
+++ domain/quotes/src/main/java/com/tangem/domain/quotes/QuotesRepositoryV2.kt
@@ -1,7 +1,7 @@
 package com.tangem.domain.quotes
 
 import com.tangem.domain.models.currency.CryptoCurrency
-import com.tangem.domain.tokens.model.Quote
+import com.tangem.domain.tokens.model.QuoteStatus
 
 /**
  * Quotes repository
@@ -11,5 +11,5 @@ import com.tangem.domain.tokens.model.Quote
 interface QuotesRepositoryV2 {
 
     /** Get quotes by [currenciesIds] synchronously or null */
-    suspend fun getMultiQuoteSyncOrNull(currenciesIds: Set<CryptoCurrency.RawID>): Set<Quote>?
+    suspend fun getMultiQuoteSyncOrNull(currenciesIds: Set<CryptoCurrency.RawID>): Set<QuoteStatus>?
 }
