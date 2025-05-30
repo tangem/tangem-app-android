23a6802d8e75d737ca15a49bc6922b8873de8003
Tangem <git@tangem.com>
2025-05-30 19:48:30 +0500

    Updated on 2026-09-23

--- features/tester/impl/src/main/java/com/tangem/feature/tester/presentation/testpush/entity/TestPushClickIntents.kt
+++ features/tester/impl/src/main/java/com/tangem/feature/tester/presentation/testpush/entity/TestPushClickIntents.kt
@@ -5,9 +5,14 @@ import androidx.compose.ui.text.input.TextFieldValue
 interface TestPushClickIntents {
     fun onTitleChange(value: TextFieldValue)
     fun onMessageChange(value: TextFieldValue)
+
     fun onDataKeyChange(index: Int, value: TextFieldValue)
     fun onDataValueChange(index: Int, value: TextFieldValue)
     fun onDataAdd()
     fun onDataRemove(index: Int)
+
+    fun onDeeplinkParamMenu()
+
     fun onBackClick()
+    fun onSendPush()
 }
