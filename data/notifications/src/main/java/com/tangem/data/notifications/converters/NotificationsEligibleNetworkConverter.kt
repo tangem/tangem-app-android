219067fa9a021695dd9cbcb50b84ca56342470ac
Tangem <git@tangem.com>
2025-05-30 18:22:42 +0500

    Updated on 2026-09-21

--- data/notifications/src/main/java/com/tangem/data/notifications/converters/NotificationsEligibleNetworkConverter.kt
+++ data/notifications/src/main/java/com/tangem/data/notifications/converters/NotificationsEligibleNetworkConverter.kt
@@ -1,15 +1,19 @@
 package com.tangem.data.notifications.converters
 
+import com.tangem.blockchain.common.Blockchain
+import com.tangem.blockchainsdk.utils.fromNetworkId
+import com.tangem.core.ui.extensions.getActiveIconRes
 import com.tangem.datasource.api.tangemTech.models.CryptoNetworkResponse
 import com.tangem.domain.notifications.models.NotificationsEligibleNetwork
-import com.tangem.utils.converter.Converter
 
-internal object NotificationsEligibleNetworkConverter : Converter<CryptoNetworkResponse, NotificationsEligibleNetwork> {
-    override fun convert(value: CryptoNetworkResponse): NotificationsEligibleNetwork {
+internal object NotificationsEligibleNetworkConverter {
+    fun convert(value: CryptoNetworkResponse): NotificationsEligibleNetwork? {
+        val blockchain: Blockchain = Blockchain.fromNetworkId(value.networkId) ?: return null
         return NotificationsEligibleNetwork(
-            id = value.id.toString(),
-            networkId = value.networkId,
-            name = value.name,
+            id = value.networkId,
+            name = blockchain.fullName,
+            symbol = blockchain.currency,
+            icon = getActiveIconRes(blockchain.id),
         )
     }
 }
