34a787d9f69369976f0816bcabfd47efc5821d5c
Tangem <git@tangem.com>
2025-05-30 17:36:17 +0400

    Updated on 2026-06-05

--- data/quotes/src/main/java/com/tangem/data/quotes/DefaultQuotesRepositoryV2.kt
+++ data/quotes/src/main/java/com/tangem/data/quotes/DefaultQuotesRepositoryV2.kt
@@ -3,7 +3,7 @@ package com.tangem.data.quotes
 import com.tangem.data.quotes.store.QuotesStoreV2
 import com.tangem.domain.models.currency.CryptoCurrency
 import com.tangem.domain.quotes.QuotesRepositoryV2
-import com.tangem.domain.tokens.model.Quote
+import com.tangem.domain.tokens.model.QuoteStatus
 import javax.inject.Inject
 
 /**
@@ -17,9 +17,10 @@ internal class DefaultQuotesRepositoryV2 @Inject constructor(
     private val quotesStore: QuotesStoreV2,
 ) : QuotesRepositoryV2 {
 
-    override suspend fun getMultiQuoteSyncOrNull(currenciesIds: Set<CryptoCurrency.RawID>): Set<Quote>? {
+    override suspend fun getMultiQuoteSyncOrNull(currenciesIds: Set<CryptoCurrency.RawID>): Set<QuoteStatus>? {
         return quotesStore.getAllSyncOrNull()?.mapTo(hashSetOf()) {
-            it.takeIf { it.rawCurrencyId in currenciesIds } ?: Quote.Empty(it.rawCurrencyId)
+            it.takeIf { it.rawCurrencyId in currenciesIds }
+                ?: QuoteStatus(rawCurrencyId = it.rawCurrencyId)
         }
     }
 }
