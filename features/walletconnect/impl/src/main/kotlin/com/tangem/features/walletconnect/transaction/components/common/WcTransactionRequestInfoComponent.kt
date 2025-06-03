a632af9a7f188196caf5b977ab2bd67a534bf723
Tangem <git@tangem.com>
2025-06-03 19:35:11 +0400

    Updated on 2026-06-16

--- features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/components/common/WcTransactionRequestInfoComponent.kt
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/components/common/WcTransactionRequestInfoComponent.kt
@@ -1,25 +1,31 @@
 package com.tangem.features.walletconnect.transaction.components.common
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.tangem.core.decompose.context.AppComponentContext
 import com.tangem.core.ui.decompose.ComposableBottomSheetComponent
-import com.tangem.features.walletconnect.transaction.entity.common.WcTransactionRequestInfoUM
+import com.tangem.features.walletconnect.transaction.entity.common.WcCommonTransactionModel
 import com.tangem.features.walletconnect.transaction.ui.common.TransactionRequestInfoContent
 
 internal class WcTransactionRequestInfoComponent(
     private val appComponentContext: AppComponentContext,
-    private val model: WcTransactionRequestInfoUM?,
-    private val onDismiss: () -> Unit,
+    private val model: WcCommonTransactionModel,
 ) : AppComponentContext by appComponentContext, ComposableBottomSheetComponent {
 
     override fun dismiss() {
-        onDismiss()
+        model.dismiss()
     }
 
     @Composable
     override fun BottomSheet() {
-        if (model != null) {
-            TransactionRequestInfoContent(model, onBack = router::pop, onDismiss = ::dismiss)
+        val content by model.uiState.collectAsStateWithLifecycle()
+        if (content?.transactionRequestInfo != null) {
+            TransactionRequestInfoContent(
+                state = content!!.transactionRequestInfo,
+                onBack = router::pop,
+                onDismiss = ::dismiss,
+            )
         }
     }
 }
