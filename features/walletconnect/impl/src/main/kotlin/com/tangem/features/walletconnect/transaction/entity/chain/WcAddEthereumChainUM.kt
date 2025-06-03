51b7e6f78b4f334dd6d145b0bd8b48f927900fd2
Tangem <git@tangem.com>
2025-06-03 19:35:11 +0400

    Updated on 2026-02-25

--- features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/entity/chain/WcAddEthereumChainUM.kt
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/entity/chain/WcAddEthereumChainUM.kt
@@ -1,13 +1,14 @@
 package com.tangem.features.walletconnect.transaction.entity.chain
 
 import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfigContent
+import com.tangem.features.walletconnect.transaction.entity.common.WcCommonTransactionUM
 import com.tangem.features.walletconnect.transaction.entity.common.WcTransactionAppInfoContentUM
 import com.tangem.features.walletconnect.transaction.entity.common.WcTransactionRequestInfoUM
 
 internal data class WcAddEthereumChainUM(
     val transaction: WcAddEthereumChainItemUM,
-    val transactionRequestInfo: WcTransactionRequestInfoUM,
-)
+    override val transactionRequestInfo: WcTransactionRequestInfoUM,
+) : WcCommonTransactionUM
 
 internal data class WcAddEthereumChainItemUM(
     val onDismiss: () -> Unit,
