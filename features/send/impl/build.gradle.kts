fc4caf9bfb313743f72426d2feac113fee2693d9
Tangem <git@tangem.com>
2025-06-09 12:42:30 +0300

    Updated on 2026-05-14

--- features/send/impl/build.gradle.kts
+++ features/send/impl/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.tangem.features.staking.impl"
+    namespace = "com.tangem.features.send.impl"
 }
 
 dependencies {
@@ -23,6 +23,8 @@ dependencies {
     implementation(deps.lifecycle.compose)
     implementation(deps.jodatime)
     implementation(deps.timber)
+    implementation(deps.reKotlin)
+    implementation(deps.kotlin.serialization)
 
     /** Compose */
     implementation(deps.compose.accompanist.systemUiController)
@@ -33,6 +35,7 @@ dependencies {
     implementation(deps.compose.ui.tooling)
     implementation(deps.compose.navigation)
     implementation(deps.compose.navigation.hilt)
+    implementation(deps.compose.paging)
     implementation(deps.compose.constraintLayout)
 
     /** Tangem SDKs */
@@ -46,26 +49,8 @@ dependencies {
     implementation(projects.core.navigation)
     implementation(projects.core.analytics)
     implementation(projects.core.analytics.models)
+    implementation(projects.core.datasource)
     implementation(projects.core.decompose)
-
-
-    /** Domain */
-    implementation(projects.domain.tokens)
-    implementation(projects.domain.tokens.models)
-    implementation(projects.domain.wallets)
-    implementation(projects.domain.wallets.models)
-    implementation(projects.domain.staking)
-    implementation(projects.domain.balanceHiding)
-    implementation(projects.domain.balanceHiding.models)
-    implementation(projects.domain.appCurrency)
-    implementation(projects.domain.appCurrency.models)
-    implementation(projects.domain.legacy)
-    implementation(projects.domain.models)
-    implementation(projects.domain.transaction)
-    implementation(projects.domain.transaction.models)
-    implementation(projects.domain.txhistory)
-    implementation(projects.domain.feedback)
-    implementation(projects.domain.feedback.models)
 
     /** Common */
     implementation(projects.common.ui)
@@ -74,9 +59,35 @@ dependencies {
     /** Libs */
     implementation(projects.libs.crypto)
 
+    /** Domain modules */
+    implementation(projects.domain.models)
+    implementation(projects.domain.legacy)
+    implementation(projects.libs.blockchainSdk)
+    implementation(projects.domain.tokens)
+    implementation(projects.domain.tokens.models)
+    implementation(projects.domain.wallets)
+    implementation(projects.domain.wallets.models)
+    implementation(projects.domain.appCurrency)
+    implementation(projects.domain.appCurrency.models)
+    implementation(projects.domain.txhistory)
+    implementation(projects.domain.txhistory.models)
+    implementation(projects.domain.transaction)
+    implementation(projects.domain.transaction.models)
+    implementation(projects.domain.card)
+    implementation(projects.domain.balanceHiding)
+    implementation(projects.domain.balanceHiding.models)
+    implementation(projects.domain.feedback)
+    implementation(projects.domain.feedback.models)
+    implementation(projects.domain.qrScanning)
+    implementation(projects.domain.qrScanning.models)
+    implementation(projects.domain.settings)
+    implementation(projects.domain.notifications)
+
     /** Feature modules */
-    implementation(projects.features.staking.api)
+    implementation(projects.features.send.api)
+    implementation(projects.features.tokendetails.api)
     implementation(projects.features.txhistory.api)
+    implementation(projects.features.qrScanning.api)
 
     /** DI */
     implementation(deps.hilt.android)
