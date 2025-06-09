e8456932864e8d5df444cc15a6b66cae31d401bc
Tangem <git@tangem.com>
2025-06-09 13:11:59 +0400

    release/5.26.1

--- app/src/main/java/com/tangem/tap/common/log/TangemCardSDKLogger.kt
+++ app/src/main/java/com/tangem/tap/common/log/TangemCardSDKLogger.kt
@@ -22,9 +22,11 @@ internal class TangemCardSDKLogger(
 ) : TangemSdkLogger {
 
     override fun log(message: () -> String, level: Log.Level) {
-        // Disabled for now in https://tangem.atlassian.net/browse/AND-9614
-        // if (!levels.contains(level)) return
-        //
-        // appLogsStore.saveLogMessage(message = messageFormatter.format(message, level))
+        if (!levels.contains(level)) return
+
+        appLogsStore.saveLogMessage(
+            tag = "CardSDK_${level.name}",
+            message = messageFormatter.format(message = message, level = level),
+        )
     }
 }
