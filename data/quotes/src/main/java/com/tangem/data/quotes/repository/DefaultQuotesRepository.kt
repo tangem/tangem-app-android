9874f286d35c5a6d26ddd83f839df3717f8ef2f2
Tangem <git@tangem.com>
2025-06-10 12:10:53 +0400

    Updated on 2025-09-17

---
+++ data/quotes/src/main/java/com/tangem/data/quotes/repository/DefaultQuotesRepository.kt
@@ -0,0 +1,35 @@
+package com.tangem.data.quotes.repository
+
+import com.tangem.data.quotes.store.QuotesStatusesStore
+import com.tangem.domain.models.currency.CryptoCurrency
+import com.tangem.domain.quotes.QuotesRepository
+import com.tangem.domain.tokens.model.QuoteStatus
+import timber.log.Timber
+import javax.inject.Inject
+
+/**
+ * Default implementation of [QuotesRepository]
+ *
+ * @property quotesStatusesStore quotes statuses store
+ *
+[REDACTED_AUTHOR]
+ */
+internal class DefaultQuotesRepository @Inject constructor(
+    private val quotesStatusesStore: QuotesStatusesStore,
+) : QuotesRepository {
+
+    override suspend fun getMultiQuoteSyncOrNull(currenciesIds: Set<CryptoCurrency.RawID>): Set<QuoteStatus> {
+        if (currenciesIds.isEmpty()) {
+            Timber.e("currenciesIds are empty")
+            return emptySet()
+        }
+
+        val storedQuotes = quotesStatusesStore.getAllSyncOrNull()
+            ?.filter { it.rawCurrencyId in currenciesIds }
+
+        return currenciesIds.mapTo(hashSetOf()) { currencyId ->
+            storedQuotes?.firstOrNull { it.rawCurrencyId == currencyId }
+                ?: QuoteStatus(rawCurrencyId = currencyId)
+        }
+    }
+}
