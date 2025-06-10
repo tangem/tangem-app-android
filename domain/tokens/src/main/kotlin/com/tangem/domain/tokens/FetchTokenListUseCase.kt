8487afe49dd4a7bbb1bb1812c143778938897cde
Tangem <git@tangem.com>
2025-06-10 19:16:32 +0400

    Updated on 2026-09-21

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/FetchTokenListUseCase.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/FetchTokenListUseCase.kt
@@ -9,7 +9,7 @@ import arrow.core.toNonEmptyListOrNull
 import com.tangem.domain.models.currency.CryptoCurrency
 import com.tangem.domain.models.network.Network
 import com.tangem.domain.networks.multi.MultiNetworkStatusFetcher
-import com.tangem.domain.quotes.multi.MultiQuoteFetcher
+import com.tangem.domain.quotes.multi.MultiQuoteStatusFetcher
 import com.tangem.domain.staking.fetcher.YieldBalanceFetcherParams
 import com.tangem.domain.staking.multi.MultiYieldBalanceFetcher
 import com.tangem.domain.staking.repositories.StakingRepository
@@ -33,7 +33,7 @@ class FetchTokenListUseCase(
     private val currenciesRepository: CurrenciesRepository,
     private val stakingRepository: StakingRepository,
     private val multiNetworkStatusFetcher: MultiNetworkStatusFetcher,
-    private val multiQuoteFetcher: MultiQuoteFetcher,
+    private val multiQuoteStatusFetcher: MultiQuoteStatusFetcher,
     private val multiYieldBalanceFetcher: MultiYieldBalanceFetcher,
     private val tokensFeatureToggles: TokensFeatureToggles,
 ) {
@@ -112,8 +112,8 @@ class FetchTokenListUseCase(
     }
 
     private suspend fun Raise<TokenListError>.fetchQuotes(currenciesIds: Set<CryptoCurrency.ID>) {
-        multiQuoteFetcher(
-            params = MultiQuoteFetcher.Params(
+        multiQuoteStatusFetcher(
+            params = MultiQuoteStatusFetcher.Params(
                 currenciesIds = currenciesIds.mapNotNull { it.rawCurrencyId }.toSet(),
                 appCurrencyId = null,
             ),
