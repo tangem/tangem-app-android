86ca05d7c863021b2fa4756c9efc861bd344a02e
Tangem <git@tangem.com>
2025-06-11 17:09:20 +0500

    Updated on 2026-02-06

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/deeplink/DefaultStakingDeepLinkHandler.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/deeplink/DefaultStakingDeepLinkHandler.kt
@@ -3,7 +3,12 @@ package com.tangem.features.staking.impl.deeplink
 import arrow.core.getOrElse
 import com.tangem.common.routing.AppRoute
 import com.tangem.common.routing.AppRouter
+import com.tangem.core.deeplink.DeeplinkConst.NETWORK_ID_KEY
+import com.tangem.core.deeplink.DeeplinkConst.TOKEN_ID_KEY
+import com.tangem.core.deeplink.DeeplinkConst.WALLET_ID_KEY
+import com.tangem.domain.staking.GetStakingAvailabilityUseCase
 import com.tangem.domain.staking.GetYieldUseCase
+import com.tangem.domain.staking.model.StakingAvailability
 import com.tangem.domain.tokens.GetCryptoCurrenciesUseCase
 import com.tangem.domain.wallets.models.UserWalletId
 import com.tangem.domain.wallets.usecase.GetSelectedWalletSyncUseCase
@@ -15,6 +20,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
+@Suppress("LongParameterList")
 internal class DefaultStakingDeepLinkHandler @AssistedInject constructor(
     @Assisted private val scope: CoroutineScope,
     @Assisted private val queryParams: Map<String, String>,
@@ -22,6 +28,7 @@ internal class DefaultStakingDeepLinkHandler @AssistedInject constructor(
     private val getSelectedWalletSyncUseCase: GetSelectedWalletSyncUseCase,
     private val getCryptoCurrenciesUseCase: GetCryptoCurrenciesUseCase,
     private val getYieldUseCase: GetYieldUseCase,
+    private val getStakingAvailabilityUseCase: GetStakingAvailabilityUseCase,
 ) : StakingDeepLinkHandler {
 
     init {
@@ -29,24 +36,28 @@ internal class DefaultStakingDeepLinkHandler @AssistedInject constructor(
     }
 
     private fun handleDeepLink() {
-        // It is okay here, we are navigating from outside, and there is no other way to getting UserWallet
-        val userWalletId = queryParams[WALLET_ID_KEY]?.let(::UserWalletId)
-            ?: getSelectedWalletSyncUseCase().getOrNull()?.walletId
+        val networkId = queryParams[NETWORK_ID_KEY]
+        val tokenId = queryParams[TOKEN_ID_KEY]
 
-        if (userWalletId == null) {
+        // It is okay here, we are navigating from outside, and there is no other way to getting UserWallet
+        val selectedUserWalletId = getSelectedWalletSyncUseCase().getOrNull()?.walletId
+        val walletId = queryParams[WALLET_ID_KEY]?.let(::UserWalletId) ?: selectedUserWalletId
+
+        // If selected user wallet is different than from deeplink - ignore deeplink
+        // If selected user wallet is null - ignore deeplink
+        if (walletId != selectedUserWalletId || selectedUserWalletId == null) {
             Timber.e("Error on getting user wallet")
             return
         }
 
-        val networkId = queryParams[NETWORK_ID_KEY]
-        val tokenId = queryParams[TOKEN_ID_KEY]
-
         scope.launch {
-            val cryptoCurrency = getCryptoCurrenciesUseCase(userWalletId = userWalletId).getOrElse {
+            val cryptoCurrency = getCryptoCurrenciesUseCase(userWalletId = selectedUserWalletId).getOrElse {
                 Timber.e("Error on getting crypto currency list")
                 return@launch
             }.firstOrNull {
-                it.network.backendId == networkId && it.id.rawCurrencyId?.value == tokenId
+                val isNetwork = it.network.backendId.equals(networkId, ignoreCase = true)
+                val isCurrency = it.id.rawCurrencyId?.value?.equals(tokenId, ignoreCase = true) == true
+                isNetwork && isCurrency
             }
 
             if (cryptoCurrency == null) {
@@ -60,6 +71,15 @@ internal class DefaultStakingDeepLinkHandler @AssistedInject constructor(
                 return@launch
             }
 
+            val isStakingEnabled = getStakingAvailabilityUseCase.invokeSync(
+                userWalletId = selectedUserWalletId,
+                cryptoCurrency = cryptoCurrency,
+            ).getOrNull()
+
+            if (isStakingEnabled !is StakingAvailability.Available) {
+                return@launch
+            }
+
             val yield = getYieldUseCase.invoke(
                 cryptoCurrencyId = cryptoCurrency.id,
                 symbol = cryptoCurrency.symbol,
@@ -70,7 +90,7 @@ internal class DefaultStakingDeepLinkHandler @AssistedInject constructor(
 
             appRouter.push(
                 AppRoute.Staking(
-                    userWalletId = userWalletId,
+                    userWalletId = selectedUserWalletId,
                     cryptoCurrencyId = cryptoCurrency.id,
                     yieldId = yield.id,
                 ),
@@ -84,11 +104,5 @@ internal class DefaultStakingDeepLinkHandler @AssistedInject constructor(
             coroutineScope: CoroutineScope,
             queryParams: Map<String, String>,
         ): DefaultStakingDeepLinkHandler
-    }
-
-    private companion object {
-        const val WALLET_ID_KEY = "walletId"
-        const val NETWORK_ID_KEY = "network_id"
-        const val TOKEN_ID_KEY = "token_id"
     }
 }
