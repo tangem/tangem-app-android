e3b1480e329ba20343a8729cba10288c2c772737
Tangem <git@tangem.com>
2025-06-16 16:23:56 +0400

    Updated on 2026-08-14

--- domain/staking/src/main/java/com/tangem/domain/staking/multi/MultiYieldBalanceFetcher.kt
+++ domain/staking/src/main/java/com/tangem/domain/staking/multi/MultiYieldBalanceFetcher.kt
@@ -1,11 +1,26 @@
 package com.tangem.domain.staking.multi
 
 import com.tangem.domain.core.flow.FlowFetcher
+import com.tangem.domain.models.currency.CryptoCurrency
+import com.tangem.domain.models.network.Network
 import com.tangem.domain.staking.fetcher.YieldBalanceFetcherParams
+import com.tangem.domain.wallets.models.UserWalletId
 
 /**
  * Fetcher of yields balances
  *
 [REDACTED_AUTHOR]
  */
-interface MultiYieldBalanceFetcher : FlowFetcher<YieldBalanceFetcherParams.Multi>
+interface MultiYieldBalanceFetcher : FlowFetcher<YieldBalanceFetcherParams.Multi> {
+
+    /**
+     * Params for fetching multiple yield balances
+     *
+     * @property userWalletId             user wallet ID
+     * @property currencyIdWithNetworkMap map of currency ID to network
+     */
+    data class Params(
+        val userWalletId: UserWalletId,
+        val currencyIdWithNetworkMap: Map<CryptoCurrency.ID, Network>,
+    )
+}
