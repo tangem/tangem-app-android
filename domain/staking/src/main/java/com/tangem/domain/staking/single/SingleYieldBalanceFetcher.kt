29584a7ce668c2f4f20c736422d14c4ff141dcc7
Tangem <git@tangem.com>
2025-06-16 16:23:56 +0400

    Updated on 2026-05-19

--- domain/staking/src/main/java/com/tangem/domain/staking/single/SingleYieldBalanceFetcher.kt
+++ domain/staking/src/main/java/com/tangem/domain/staking/single/SingleYieldBalanceFetcher.kt
@@ -1,11 +1,28 @@
 package com.tangem.domain.staking.single
 
 import com.tangem.domain.core.flow.FlowFetcher
+import com.tangem.domain.models.currency.CryptoCurrency
+import com.tangem.domain.models.network.Network
 import com.tangem.domain.staking.fetcher.YieldBalanceFetcherParams
+import com.tangem.domain.wallets.models.UserWalletId
 
 /**
  * Fetcher of yield balance
  *
 [REDACTED_AUTHOR]
  */
-interface SingleYieldBalanceFetcher : FlowFetcher<YieldBalanceFetcherParams.Single>
+interface SingleYieldBalanceFetcher : FlowFetcher<YieldBalanceFetcherParams.Single> {
+
+    /**
+     * Params for fetching single yield balance
+     *
+     * @property userWalletId user wallet ID
+     * @property currencyId   currency ID
+     * @property network      network
+     */
+    data class Params(
+        val userWalletId: UserWalletId,
+        val currencyId: CryptoCurrency.ID,
+        val network: Network,
+    )
+}
