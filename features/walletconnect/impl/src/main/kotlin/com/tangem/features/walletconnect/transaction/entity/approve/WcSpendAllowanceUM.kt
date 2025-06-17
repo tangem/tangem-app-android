cb84530204ce131402aa2d4425f5a4923730dc1d
Tangem <git@tangem.com>
2025-06-17 12:17:29 +0400

    Updated on 2025-09-15

--- features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/entity/approve/WcSpendAllowanceUM.kt
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/entity/approve/WcSpendAllowanceUM.kt
@@ -1,9 +1,14 @@
 package com.tangem.features.walletconnect.transaction.entity.approve
 
+import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfigContent
 import com.tangem.core.ui.extensions.TextReference
+import java.math.BigDecimal
 
 internal data class WcSpendAllowanceUM(
+    val amountValue: BigDecimal,
+    val isUnlimited: Boolean,
     val amountText: TextReference,
     val tokenSymbol: String,
     val tokenImageUrl: String?,
-)
+    val networkIconRes: Int?,
+) : TangemBottomSheetConfigContent
