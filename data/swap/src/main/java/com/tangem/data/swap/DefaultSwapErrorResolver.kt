553f47e9ee5433e7bcfbedb1053e26bdfe84945f
Tangem <git@tangem.com>
2025-06-18 18:38:24 +0500

    Updated on 2026-02-11

---
+++ data/swap/src/main/java/com/tangem/data/swap/DefaultSwapErrorResolver.kt
@@ -0,0 +1,19 @@
+package com.tangem.data.swap
+
+import com.tangem.data.express.converter.ExpressErrorConverter
+import com.tangem.datasource.api.common.response.ApiResponseError
+import com.tangem.domain.express.models.ExpressError
+import com.tangem.domain.swap.SwapErrorResolver
+
+internal class DefaultSwapErrorResolver(
+    private val expressErrorConverter: ExpressErrorConverter,
+) : SwapErrorResolver {
+    override fun resolve(throwable: Throwable): ExpressError {
+        return when (throwable) {
+            is ApiResponseError.HttpException -> {
+                expressErrorConverter.convert(throwable.errorBody.orEmpty())
+            }
+            else -> ExpressError.UnknownError
+        }
+    }
+}
