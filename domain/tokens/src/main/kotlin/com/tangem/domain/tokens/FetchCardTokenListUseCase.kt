56bf46086272291cf66e8341cbfb0b0445692f04
Tangem <git@tangem.com>
2025-06-18 16:03:55 +0400

    Updated on 2025-11-18

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/FetchCardTokenListUseCase.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/FetchCardTokenListUseCase.kt
@@ -8,7 +8,6 @@ import com.tangem.domain.models.currency.CryptoCurrency
 import com.tangem.domain.models.network.Network
 import com.tangem.domain.networks.multi.MultiNetworkStatusFetcher
 import com.tangem.domain.quotes.multi.MultiQuoteFetcher
-import com.tangem.domain.staking.fetcher.YieldBalanceFetcherParams
 import com.tangem.domain.staking.multi.MultiYieldBalanceFetcher
 import com.tangem.domain.staking.repositories.StakingRepository
 import com.tangem.domain.tokens.error.TokenListError
@@ -95,7 +94,7 @@ class FetchCardTokenListUseCase(
     ) {
         if (tokensFeatureToggles.isStakingLoadingRefactoringEnabled) {
             multiYieldBalanceFetcher(
-                params = YieldBalanceFetcherParams.Multi(
+                params = MultiYieldBalanceFetcher.Params(
                     userWalletId = userWalletId,
                     currencyIdWithNetworkMap = currencies.associateTo(hashMapOf()) { it.id to it.network },
                 ),
