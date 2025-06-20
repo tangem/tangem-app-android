2bfd6244efcf74327b6bfb08c9d1efb500d69d78
Tangem <git@tangem.com>
2025-06-20 14:19:58 +0500

    Updated on 2026-07-01

--- features/send-v2/impl/src/main/java/com/tangem/features/send/v2/feeselector/di/FeeSelectorFeatureModule.kt
+++ features/send-v2/impl/src/main/java/com/tangem/features/send/v2/feeselector/di/FeeSelectorFeatureModule.kt
@@ -1,6 +1,8 @@
 package com.tangem.features.send.v2.feeselector.di
 
+import com.tangem.features.send.v2.api.FeeSelectorBlockComponent
 import com.tangem.features.send.v2.api.FeeSelectorComponent
+import com.tangem.features.send.v2.feeselector.DefaultFeeSelectorBlockComponent
 import com.tangem.features.send.v2.feeselector.DefaultFeeSelectorComponent
 import dagger.Binds
 import dagger.Module
@@ -15,4 +17,8 @@ internal interface FeeSelectorFeatureModule {
     @Binds
     @Singleton
     fun bindComponentFactory(factory: DefaultFeeSelectorComponent.Factory): FeeSelectorComponent.Factory
+
+    @Binds
+    @Singleton
+    fun bindBlockComponentFactory(factory: DefaultFeeSelectorBlockComponent.Factory): FeeSelectorBlockComponent.Factory
 }
