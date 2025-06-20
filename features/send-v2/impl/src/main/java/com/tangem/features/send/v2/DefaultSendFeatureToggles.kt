d88a7ec0600ae39c036c3d410b9925870601ac5c
Tangem <git@tangem.com>
2025-06-20 16:50:40 +0500

    Updated on 2026-08-19

--- features/send-v2/impl/src/main/java/com/tangem/features/send/v2/DefaultSendFeatureToggles.kt
+++ features/send-v2/impl/src/main/java/com/tangem/features/send/v2/DefaultSendFeatureToggles.kt
@@ -4,5 +4,8 @@ import com.tangem.core.configtoggle.feature.FeatureTogglesManager
 import com.tangem.features.send.v2.api.SendFeatureToggles
 
 internal class DefaultSendFeatureToggles(
-    @Suppress("UnusedPrivateMember") private val featureToggles: FeatureTogglesManager,
-) : SendFeatureToggles
+    private val featureToggles: FeatureTogglesManager,
+) : SendFeatureToggles {
+    override val isSendRedesignEnabled: Boolean
+        get() = featureToggles.isFeatureEnabled("SEND_REDESIGN_ENABLED")
+}
