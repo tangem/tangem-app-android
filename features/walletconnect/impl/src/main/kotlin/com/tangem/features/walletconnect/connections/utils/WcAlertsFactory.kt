507ed0b9ebbd22b5f7d171a9d95486eb6930f10f
Tangem <git@tangem.com>
2025-06-23 13:58:27 +0400

    Updated on 2026-01-20

--- features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/connections/utils/WcAlertsFactory.kt
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/connections/utils/WcAlertsFactory.kt
@@ -10,11 +10,16 @@ import com.tangem.features.walletconnect.transaction.routes.WcTransactionRoutes
 internal object WcAlertsFactory {
 
     fun createCommonTransactionAppInfoAlertUM(alertType: WcTransactionRoutes.Alert.Type) = when (alertType) {
-        is WcTransactionRoutes.Alert.Type.Verified -> createVerifiedDomainAlert(alertType.appName)
-        is WcTransactionRoutes.Alert.Type.UnknownDomain -> createUnknownDomainAlert()
-        is WcTransactionRoutes.Alert.Type.UnsafeDomain -> createUnsafeDomainAlert()
-        is WcTransactionRoutes.Alert.Type.MaliciousDApp ->
+        is WcTransactionRoutes.Alert.Type.Verified ->
+            createVerifiedDomainAlert(alertType.appName)
+        is WcTransactionRoutes.Alert.Type.UnknownDomain ->
+            createUnknownDomainAlert()
+        is WcTransactionRoutes.Alert.Type.UnsafeDomain ->
+            createUnsafeDomainAlert()
+        is WcTransactionRoutes.Alert.Type.MaliciousInfo ->
             createMaliciousDAppAlert(alertType.description, alertType.onClick)
+        is WcTransactionRoutes.Alert.Type.UnknownError ->
+            createUnknownErrorAlert(alertType.errorMessage, alertType.onDismiss)
     }
 
     fun createUnknownDomainAlert(activeButtonOnClick: (() -> Unit)? = null): MessageBottomSheetUMV2 {
@@ -69,8 +74,32 @@ internal object WcAlertsFactory {
         }
     }
 
+    private fun createUnknownErrorAlert(errorMessage: String?, onDismiss: () -> Unit): MessageBottomSheetUMV2 {
+        return messageBottomSheetUM {
+            infoBlock {
+                icon(R.drawable.img_attention_20) {
+                    type = MessageBottomSheetUMV2.Icon.Type.Warning
+                    backgroundType = MessageBottomSheetUMV2.Icon.BackgroundType.SameAsTint
+                }
+                title = resourceReference(R.string.wc_alert_unknown_error_title)
+                body = if (errorMessage.isNullOrEmpty()) {
+                    resourceReference(R.string.wc_alert_unknown_error_description_no_error_code)
+                } else {
+                    resourceReference(
+                        R.string.wc_alert_unknown_error_description,
+                        wrappedList(errorMessage),
+                    )
+                }
+            }
+            secondaryButton {
+                text = resourceReference(R.string.balance_hidden_got_it_button)
+                onClick { onDismiss() }
+            }
+        }
+    }
+
     private fun createMaliciousDAppAlert(
-        description: String,
+        description: String?,
         activeButtonOnClick: (() -> Unit),
     ): MessageBottomSheetUMV2 {
         return messageBottomSheetUM {
@@ -80,7 +109,9 @@ internal object WcAlertsFactory {
                     backgroundType = MessageBottomSheetUMV2.Icon.BackgroundType.SameAsTint
                 }
                 title = resourceReference(R.string.security_alert_title)
-                body = TextReference.Str(description)
+                if (!description.isNullOrEmpty()) {
+                    body = TextReference.Str(description)
+                }
             }
             primaryButton {
                 text = resourceReference(R.string.common_cancel)
