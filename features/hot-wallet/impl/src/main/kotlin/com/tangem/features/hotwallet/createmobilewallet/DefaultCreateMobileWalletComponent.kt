3814fe9c7a7ec8e5a3f4da054d66da395a00fee3
Tangem <git@tangem.com>
2025-06-24 22:39:17 +0500

    Updated on 2026-09-01

--- features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/createmobilewallet/DefaultCreateMobileWalletComponent.kt
+++ features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/createmobilewallet/DefaultCreateMobileWalletComponent.kt
@@ -1,4 +1,4 @@
-package com.tangem.features.createwalletselection
+package com.tangem.features.hotwallet.createmobilewallet
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -6,29 +6,29 @@ import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.tangem.core.decompose.context.AppComponentContext
 import com.tangem.core.decompose.model.getOrCreateModel
-import com.tangem.features.createwalletselection.ui.CreateWalletSelectionContent
+import com.tangem.features.hotwallet.CreateMobileWalletComponent
+import com.tangem.features.hotwallet.createmobilewallet.ui.CreateMobileWalletContent
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 
-internal class DefaultCreateWalletSelectionComponent @AssistedInject constructor(
+internal class DefaultCreateMobileWalletComponent @AssistedInject constructor(
     @Assisted private val context: AppComponentContext,
     @Assisted private val params: Unit,
-) : CreateWalletSelectionComponent, AppComponentContext by context {
-
-    private val model: CreateWalletSelectionModel = getOrCreateModel(params)
+) : CreateMobileWalletComponent, AppComponentContext by context {
+    private val model: CreateMobileWalletModel = getOrCreateModel(params)
 
     @Composable
     override fun Content(modifier: Modifier) {
         val state by model.uiState.collectAsStateWithLifecycle()
-        CreateWalletSelectionContent(
+        CreateMobileWalletContent(
             state = state,
             modifier = modifier,
         )
     }
 
     @AssistedFactory
-    interface Factory : CreateWalletSelectionComponent.Factory {
-        override fun create(context: AppComponentContext, params: Unit): DefaultCreateWalletSelectionComponent
+    interface Factory : CreateMobileWalletComponent.Factory {
+        override fun create(context: AppComponentContext, params: Unit): DefaultCreateMobileWalletComponent
     }
 }
