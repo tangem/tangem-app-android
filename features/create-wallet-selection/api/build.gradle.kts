16b470ffc69f04e6f72c8fb9a2fa89337b2afd27
Tangem <git@tangem.com>
2025-06-24 15:44:11 +0500

    Updated on 2026-09-25

---
+++ features/create-wallet-selection/api/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    alias(deps.plugins.android.library)
+    alias(deps.plugins.kotlin.android)
+    id("configuration")
+}
+
+android {
+    namespace = "com.tangem.features.createwalletselection.api"
+}
+
+dependencies {
+
+    /* Project - Domain */
+    implementation(projects.domain.models)
+
+    /* Project - Core */
+    implementation(projects.core.decompose)
+    implementation(projects.core.ui)
+
+    /* Compose */
+    implementation(deps.compose.runtime)
+} 
