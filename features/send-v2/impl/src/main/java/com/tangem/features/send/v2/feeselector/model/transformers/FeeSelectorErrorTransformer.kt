32a6cce35a8d7eb582da10e06f1c0ca62d2f49ed
Tangem <git@tangem.com>
2025-06-25 17:19:42 +0500

    Updated on 2026-06-02

---
+++ features/send-v2/impl/src/main/java/com/tangem/features/send/v2/feeselector/model/transformers/FeeSelectorErrorTransformer.kt
@@ -0,0 +1,12 @@
+package com.tangem.features.send.v2.feeselector.model.transformers
+
+import com.tangem.domain.transaction.error.GetFeeError
+import com.tangem.features.send.v2.feeselector.entity.FeeSelectorUM
+import com.tangem.utils.transformer.Transformer
+
+internal class FeeSelectorErrorTransformer(private val error: GetFeeError) : Transformer<FeeSelectorUM> {
+
+    override fun transform(prevState: FeeSelectorUM): FeeSelectorUM {
+        return FeeSelectorUM.Error(error = error, onDone = prevState.doneButtonConfig.onClick)
+    }
+}
