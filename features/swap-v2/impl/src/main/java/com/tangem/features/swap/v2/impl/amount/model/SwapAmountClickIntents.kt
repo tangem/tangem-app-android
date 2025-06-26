649da7701eaf235d47457ab579438a8dafc9b73f
Tangem <git@tangem.com>
2025-06-26 12:57:08 +0500

    Updated on 2026-05-07

---
+++ features/swap-v2/impl/src/main/java/com/tangem/features/swap/v2/impl/amount/model/SwapAmountClickIntents.kt
@@ -0,0 +1,10 @@
+package com.tangem.features.swap.v2.impl.amount.model
+
+import com.tangem.common.ui.amountScreen.AmountScreenClickIntents
+import com.tangem.features.swap.v2.impl.amount.entity.SwapAmountType
+
+internal interface SwapAmountClickIntents : AmountScreenClickIntents {
+
+    fun onExpandEditField(selectedAmountType: SwapAmountType)
+    fun onSelectTokenClick()
+}
