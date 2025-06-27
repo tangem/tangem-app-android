ba0b6f31e7b139c586158f052f79f5d5dd10ad11
Tangem <git@tangem.com>
2025-06-27 18:08:39 +0500

    Updated on 2026-05-19

---
+++ features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/addexistingwallet/root/routing/AddExistingWalletRoute.kt
@@ -0,0 +1,10 @@
+package com.tangem.features.hotwallet.addexistingwallet.root.routing
+
+import com.tangem.core.decompose.navigation.Route
+import kotlinx.serialization.Serializable
+
+internal sealed class AddExistingWalletRoute : Route {
+
+    @Serializable
+    object Start : AddExistingWalletRoute()
+}
