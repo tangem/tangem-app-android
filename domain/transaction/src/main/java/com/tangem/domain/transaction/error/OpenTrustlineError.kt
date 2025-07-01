e6d0db2a0964c3f4546cfe864b6df5eb3a18b3cd
Tangem <git@tangem.com>
2025-07-01 23:03:36 +0700

    Updated on 2025-12-05

--- domain/transaction/src/main/java/com/tangem/domain/transaction/error/OpenTrustlineError.kt
+++ domain/transaction/src/main/java/com/tangem/domain/transaction/error/OpenTrustlineError.kt
@@ -3,8 +3,8 @@ package com.tangem.domain.transaction.error
 import java.math.BigDecimal
 
 sealed interface OpenTrustlineError {
-    val message: String?
 
-    data class SomeError(override val message: String?) : OpenTrustlineError
-    data class NotEnoughCoin(val amount: BigDecimal, override val message: String?) : OpenTrustlineError
+    data class UnknownError(val message: String?) : OpenTrustlineError
+    data class SendError(val error: SendTransactionError) : OpenTrustlineError
+    data class NotEnoughCoin(val amount: BigDecimal, val symbol: String, val message: String?) : OpenTrustlineError
 }
