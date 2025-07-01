fc0c9b4721529a7a241d6a24637150d3c2d0f7ad
Tangem <git@tangem.com>
2025-07-01 14:35:52 +0500

    Updated on 2026-08-19

--- common/routing/src/main/kotlin/com/tangem/common/routing/AppRoute.kt
+++ common/routing/src/main/kotlin/com/tangem/common/routing/AppRoute.kt
@@ -286,4 +286,13 @@ sealed class AppRoute(val path: String) : Route {
         val nftAsset: NFTAsset,
         val nftCollectionName: String,
     ) : AppRoute(path = "/send/nft/${userWalletId.stringValue}/$nftCollectionName/${nftAsset.id}")
+
+    @Serializable
+    object CreateWalletSelection : AppRoute(path = "/create_wallet_selection")
+
+    @Serializable
+    object CreateMobileWallet : AppRoute(path = "/create_mobile_wallet")
+
+    @Serializable
+    object AddExistingWallet : AppRoute(path = "/add_existing_wallet")
 }
