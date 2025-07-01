f59f183fb3bdbe43e5200824f0e910a9e2e3dd11
Tangem <git@tangem.com>
2025-07-01 20:34:06 +0500

    Updated on 2026-06-17

--- features/send-v2/impl/src/main/java/com/tangem/features/send/v2/send/ui/state/SendUM.kt
+++ features/send-v2/impl/src/main/java/com/tangem/features/send/v2/send/ui/state/SendUM.kt
@@ -1,9 +1,9 @@
 package com.tangem.features.send.v2.send.ui.state
 
 import com.tangem.common.ui.amountScreen.models.AmountState
-import com.tangem.features.send.v2.common.ui.state.NavigationUM
+import com.tangem.common.ui.navigationButtons.NavigationUM
+import com.tangem.features.send.v2.api.subcomponents.destination.entity.DestinationUM
 import com.tangem.features.send.v2.common.ui.state.ConfirmUM
-import com.tangem.features.send.v2.subcomponents.destination.ui.state.DestinationUM
 import com.tangem.features.send.v2.subcomponents.fee.ui.state.FeeUM
 
 internal data class SendUM(
