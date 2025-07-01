04917c93a7f404397d5519d1d52b6feb9485642c
Tangem <git@tangem.com>
2025-07-01 12:07:19 +0300

    Updated on 2026-09-25

--- domain/transaction/src/main/java/com/tangem/domain/transaction/usecase/ValidateWalletAddressUseCase.kt
+++ domain/transaction/src/main/java/com/tangem/domain/transaction/usecase/ValidateWalletAddressUseCase.kt
@@ -2,6 +2,7 @@ package com.tangem.domain.transaction.usecase
 
 import arrow.core.left
 import arrow.core.right
+import com.tangem.blockchain.common.ResolveAddressResult
 import com.tangem.domain.models.network.CryptoCurrencyAddress
 import com.tangem.domain.models.network.Network
 import com.tangem.domain.models.network.NetworkAddress
@@ -24,44 +25,59 @@ class ValidateWalletAddressUseCase(
         userWalletId: UserWalletId,
         network: Network,
         address: String,
-        currencyAddress: Set<NetworkAddress.Address>?,
-    ): AddressValidationResult {
-        val decodedXAddress = BlockchainUtils.decodeRippleXAddress(address, network.rawId)
-        val isUtxoConsolidationAvailable =
-            walletManagersFacade.checkUtxoConsolidationAvailability(userWalletId, network)
-
-        val addressToValidate = decodedXAddress?.address ?: address
-        val isCurrentAddress = currencyAddress?.any { it.value == addressToValidate } ?: true
-
-        val isForbidSelfSend = isCurrentAddress && !isUtxoConsolidationAvailable
-        val isValidAddress = walletAddressServiceRepository.validateAddress(userWalletId, network, addressToValidate)
-
-        return when {
-            !isValidAddress -> AddressValidation.Error.InvalidAddress.left()
-            isForbidSelfSend -> AddressValidation.Error.AddressInWallet.left()
-            decodedXAddress != null -> AddressValidation.Success.ValidXAddress.right()
-            else -> AddressValidation.Success.Valid.right()
-        }
-    }
+        currencyAddresses: Set<NetworkAddress.Address>?,
+    ): AddressValidationResult = validateAddressInternal(
+        userWalletId,
+        network,
+        address,
+        isCurrentAddress = { toValidate ->
+            currencyAddresses?.any { it.value == toValidate } ?: true
+        },
+    )
 
     suspend operator fun invoke(
         userWalletId: UserWalletId,
         network: Network,
         address: String,
         senderAddresses: List<CryptoCurrencyAddress>,
+    ): AddressValidationResult = validateAddressInternal(
+        userWalletId,
+        network,
+        address,
+        isCurrentAddress = { toValidate ->
+            senderAddresses.any { it.address == toValidate }
+        },
+    )
+
+    private suspend fun validateAddressInternal(
+        userWalletId: UserWalletId,
+        network: Network,
+        address: String,
+        isCurrentAddress: (String) -> Boolean,
     ): AddressValidationResult {
         val decodedXAddress = BlockchainUtils.decodeRippleXAddress(address, network.rawId)
         val isUtxoConsolidationAvailable =
             walletManagersFacade.checkUtxoConsolidationAvailability(userWalletId, network)
 
         val addressToValidate = decodedXAddress?.address ?: address
-        val isCurrentAddress = senderAddresses.any { it.address == addressToValidate }
-
-        val isForbidSelfSend = isCurrentAddress && !isUtxoConsolidationAvailable
+        val current = isCurrentAddress(addressToValidate)
+        val isForbidSelfSend = current && !isUtxoConsolidationAvailable
         val isValidAddress = walletAddressServiceRepository.validateAddress(userWalletId, network, addressToValidate)
 
         return when {
-            !isValidAddress -> AddressValidation.Error.InvalidAddress.left()
+            !isValidAddress -> {
+                val resolveAddressResult = walletAddressServiceRepository.resolveAddress(
+                    userWalletId = userWalletId,
+                    network = network,
+                    address = addressToValidate,
+                )
+
+                if (resolveAddressResult is ResolveAddressResult.Resolved) {
+                    AddressValidation.Success.ValidNamedAddress(resolveAddressResult.address).right()
+                } else {
+                    AddressValidation.Error.InvalidAddress.left()
+                }
+            }
             isForbidSelfSend -> AddressValidation.Error.AddressInWallet.left()
             decodedXAddress != null -> AddressValidation.Success.ValidXAddress.right()
             else -> AddressValidation.Success.Valid.right()
