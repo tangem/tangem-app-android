6b1aa94cd68bbe9e576e425d2a2e34965eb3ea69
Tangem <git@tangem.com>
2025-07-01 20:34:06 +0500

    Updated on 2025-09-01

--- features/send-v2/impl/src/main/java/com/tangem/features/send/v2/subcomponents/destination/model/transformers/SendDestinationValidationStartedTransformer.kt
+++ features/send-v2/impl/src/main/java/com/tangem/features/send/v2/subcomponents/destination/model/transformers/SendDestinationValidationStartedTransformer.kt
@@ -1,6 +1,6 @@
 package com.tangem.features.send.v2.subcomponents.destination.model.transformers
 
-import com.tangem.features.send.v2.subcomponents.destination.ui.state.DestinationUM
+import com.tangem.features.send.v2.api.subcomponents.destination.entity.DestinationUM
 import com.tangem.utils.transformer.Transformer
 
 internal object SendDestinationValidationStartedTransformer : Transformer<DestinationUM> {
