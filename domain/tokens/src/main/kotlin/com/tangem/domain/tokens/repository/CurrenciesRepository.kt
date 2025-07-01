52dffd1eda987575d3ec200d64cac616066d01f0
Tangem <git@tangem.com>
2025-07-01 16:10:30 +0400

    release/5.26.1

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/repository/CurrenciesRepository.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/repository/CurrenciesRepository.kt
@@ -1,5 +1,6 @@
 package com.tangem.domain.tokens.repository
 
+import com.tangem.domain.common.CardTypesResolver
 import com.tangem.domain.core.error.DataError
 import com.tangem.domain.models.currency.CryptoCurrency
 import com.tangem.domain.models.network.Network
@@ -8,7 +9,6 @@ import com.tangem.domain.tokens.model.FeePaidCurrency
 import com.tangem.domain.wallets.models.UserWallet
 import com.tangem.domain.wallets.models.UserWalletId
 import kotlinx.coroutines.flow.Flow
-import kotlin.jvm.Throws
 
 /**
  * Repository for everything related to the tokens of user wallet
@@ -263,4 +263,7 @@ interface CurrenciesRepository {
      */
     @Throws
     suspend fun syncTokens(userWalletId: UserWalletId)
+
+    @Throws
+    fun getCardTypesResolver(userWalletId: UserWalletId): CardTypesResolver
 }
