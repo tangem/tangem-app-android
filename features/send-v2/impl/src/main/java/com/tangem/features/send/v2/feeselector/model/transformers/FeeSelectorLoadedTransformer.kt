28c5eb6192c5dc4ecd4313e2ec7e7fdbcf48544e
Tangem <git@tangem.com>
2025-07-01 13:39:08 +0500

    Updated on 2026-09-23

--- features/send-v2/impl/src/main/java/com/tangem/features/send/v2/feeselector/model/transformers/FeeSelectorLoadedTransformer.kt
+++ features/send-v2/impl/src/main/java/com/tangem/features/send/v2/feeselector/model/transformers/FeeSelectorLoadedTransformer.kt
@@ -3,10 +3,11 @@ package com.tangem.features.send.v2.feeselector.model.transformers
 import com.tangem.blockchain.common.transaction.TransactionFee
 import com.tangem.domain.appcurrency.model.AppCurrency
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
+import com.tangem.features.send.v2.api.entity.FeeFiatRateUM
+import com.tangem.features.send.v2.api.entity.FeeItem
+import com.tangem.features.send.v2.api.entity.FeeSelectorUM
 import com.tangem.features.send.v2.api.params.FeeSelectorParams
-import com.tangem.features.send.v2.feeselector.entity.FeeFiatRateUM
-import com.tangem.features.send.v2.feeselector.entity.FeeItem
-import com.tangem.features.send.v2.feeselector.entity.FeeSelectorUM
+import com.tangem.features.send.v2.feeselector.model.FeeSelectorIntents
 import com.tangem.utils.transformer.Transformer
 import kotlinx.collections.immutable.ImmutableList
 
@@ -17,29 +18,31 @@ internal class FeeSelectorLoadedTransformer(
     private val fees: TransactionFee,
     private val suggestedFeeState: FeeSelectorParams.SuggestedFeeState,
     private val isFeeApproximate: Boolean,
-    private val onFeeSelected: (FeeItem) -> Unit,
-    private val onCustomFeeValueChange: (Int, String) -> Unit,
-    private val onNextClick: () -> Unit,
+    private val feeSelectorIntents: FeeSelectorIntents,
 ) : Transformer<FeeSelectorUM> {
 
     private val feeItemsConverter = FeeItemConverter(
         suggestedFeeState = suggestedFeeState,
         normalFee = fees.normal,
-        onCustomFeeValueChange = onCustomFeeValueChange,
-        onNextClick = onNextClick,
+        feeSelectorIntents = feeSelectorIntents,
         appCurrency = appCurrency,
         cryptoCurrencyStatus = cryptoCurrencyStatus,
     )
 
     override fun transform(prevState: FeeSelectorUM): FeeSelectorUM {
         val feeItems: ImmutableList<FeeItem> = feeItemsConverter.convert(fees)
-        val selectedFee = feeItems.find { it is FeeItem.Suggested } ?: feeItems.first { it is FeeItem.Market }
+
+        val selectedFee = when (prevState) {
+            is FeeSelectorUM.Content -> feeItems.first { it.isSame(prevState.selectedFeeItem) }
+            is FeeSelectorUM.Error,
+            FeeSelectorUM.Loading,
+            -> feeItems.find { it is FeeItem.Suggested } ?: feeItems.first { it is FeeItem.Market }
+        }
+
         return FeeSelectorUM.Content(
-            doneButtonConfig = prevState.doneButtonConfig.copy(enabled = true),
             feeItems = feeItems,
             selectedFeeItem = selectedFee,
             isFeeApproximate = isFeeApproximate,
-            onFeeSelected = onFeeSelected,
             feeFiatRateUM = cryptoCurrencyStatus.value.fiatRate?.let { rate ->
                 FeeFiatRateUM(
                     rate = rate,
