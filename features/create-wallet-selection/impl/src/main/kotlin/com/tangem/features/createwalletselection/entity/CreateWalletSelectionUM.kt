586e62028bd7eec85b99203fee7bbbc511f9b278
Tangem <git@tangem.com>
2025-07-01 14:35:52 +0500

    Updated on 2026-08-07

--- features/create-wallet-selection/impl/src/main/kotlin/com/tangem/features/createwalletselection/entity/CreateWalletSelectionUM.kt
+++ features/create-wallet-selection/impl/src/main/kotlin/com/tangem/features/createwalletselection/entity/CreateWalletSelectionUM.kt
@@ -2,6 +2,7 @@ package com.tangem.features.createwalletselection.entity
 
 internal data class CreateWalletSelectionUM(
     val isScanInProgress: Boolean = false,
+    val hardwareWalletPrice: String = "$54.90",
     val onBackClick: () -> Unit,
     val onMobileWalletClick: () -> Unit,
     val onHardwareWalletClick: () -> Unit,
