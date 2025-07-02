7aa76b344a4ce895180cc5a72093e0148b58557b
Tangem <git@tangem.com>
2025-07-02 11:53:48 +0400

    Updated on 2026-06-02

---
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/wallet/FetchingSource.kt
@@ -0,0 +1,12 @@
+package com.tangem.domain.tokens.wallet
+
+/**
+ * Source type that is necessary to load cryptocurrency data
+ *
+[REDACTED_AUTHOR]
+ */
+internal enum class FetchingSource {
+    NETWORK,
+    QUOTE,
+    STAKING,
+}
