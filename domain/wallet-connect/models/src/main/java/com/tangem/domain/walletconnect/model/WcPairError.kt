48900cf98f37754bd91172d02be173e03da2b356
Tangem <git@tangem.com>
2025-07-07 16:29:50 +0700

    Updated on 2026-01-20

--- domain/wallet-connect/models/src/main/java/com/tangem/domain/walletconnect/model/WcPairError.kt
+++ domain/wallet-connect/models/src/main/java/com/tangem/domain/walletconnect/model/WcPairError.kt
@@ -8,7 +8,7 @@ sealed class WcPairError(
     data class UriAlreadyUsed(override val message: String) : WcPairError("107 001 001")
     data class PairingFailed(override val message: String) : WcPairError("107 001 002")
     data object InvalidDomainURL : WcPairError("107 001 003")
-    data object UnsupportedDomain : WcPairError("107 001 004")
+    data object UnsupportedDApp : WcPairError("107 001 004")
     data class UnsupportedBlockchains(val chains: Set<String>) : WcPairError("107 001 005")
 
     data object InvalidConnectionRequest : WcPairError("107 002 001")
