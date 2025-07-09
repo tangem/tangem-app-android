a70b79a6f66ec7ecf7a8209dcd2a8ef378419ef1
Tangem <git@tangem.com>
2025-07-09 11:08:16 +0300

    Updated on 2026-04-06

--- core/datasource/src/main/java/com/tangem/datasource/api/common/config/TangemTech.kt
+++ core/datasource/src/main/java/com/tangem/datasource/api/common/config/TangemTech.kt
@@ -1,5 +1,6 @@
 package com.tangem.datasource.api.common.config
 
+import com.tangem.datasource.BuildConfig
 import com.tangem.datasource.api.common.AuthProvider
 import com.tangem.datasource.utils.RequestHeader
 import com.tangem.utils.info.AppInfoProvider
@@ -12,13 +13,27 @@ internal class TangemTech(
     private val appInfoProvider: AppInfoProvider,
 ) : ApiConfig() {
 
-    override val defaultEnvironment: ApiEnvironment = ApiEnvironment.PROD
+    override val defaultEnvironment: ApiEnvironment = getInitialEnvironment()
 
     override val environmentConfigs = listOf(
         createDevEnvironment(),
         createStageEnvironment(),
+        createMockedEnvironment(),
         createProdEnvironment(),
     )
+
+    private fun getInitialEnvironment(): ApiEnvironment {
+        return when (BuildConfig.BUILD_TYPE) {
+            MOCKED_BUILD_TYPE,
+            -> ApiEnvironment.MOCK
+            DEBUG_BUILD_TYPE,
+            INTERNAL_BUILD_TYPE,
+            EXTERNAL_BUILD_TYPE,
+            RELEASE_BUILD_TYPE,
+            -> ApiEnvironment.PROD
+            else -> error("Unknown build type [${BuildConfig.BUILD_TYPE}]")
+        }
+    }
 
     private fun createDevEnvironment(): ApiEnvironmentConfig = ApiEnvironmentConfig(
         environment = ApiEnvironment.DEV,
@@ -28,6 +43,12 @@ internal class TangemTech(
 
     private fun createStageEnvironment(): ApiEnvironmentConfig = ApiEnvironmentConfig(
         environment = ApiEnvironment.STAGE,
+        baseUrl = "[REDACTED_ENV_URL]",
+        headers = createHeaders(),
+    )
+
+    private fun createMockedEnvironment(): ApiEnvironmentConfig = ApiEnvironmentConfig(
+        environment = ApiEnvironment.MOCK,
         baseUrl = "[REDACTED_ENV_URL]",
         headers = createHeaders(),
     )
