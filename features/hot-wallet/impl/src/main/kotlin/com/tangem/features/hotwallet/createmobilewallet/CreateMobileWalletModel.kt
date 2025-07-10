42024b03638097741e54015ebe07fe248dd11d4f
Tangem <git@tangem.com>
2025-07-10 10:33:13 +0300

    Updated on 2026-09-14

--- features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/createmobilewallet/CreateMobileWalletModel.kt
+++ features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/createmobilewallet/CreateMobileWalletModel.kt
@@ -2,18 +2,23 @@ package com.tangem.features.hotwallet.createmobilewallet
 
 import com.tangem.core.decompose.di.ModelScoped
 import com.tangem.core.decompose.model.Model
+import com.tangem.core.decompose.model.ParamsContainer
 import com.tangem.core.decompose.navigation.Router
 import com.tangem.features.hotwallet.createmobilewallet.entity.CreateMobileWalletUM
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @ModelScoped
 internal class CreateMobileWalletModel @Inject constructor(
     override val dispatchers: CoroutineDispatcherProvider,
     private val router: Router,
+    private val paramsContainer: ParamsContainer,
 ) : Model() {
+
+    private val params = paramsContainer.require<DefaultCreateMobileWalletComponent.Params>()
 
     internal val uiState: StateFlow<CreateMobileWalletUM>
     field = MutableStateFlow(
@@ -24,6 +29,16 @@ internal class CreateMobileWalletModel @Inject constructor(
     )
 
     private fun onCreateClick() {
-        // TODO create a wallet
+        modelScope.launch {
+            val tangemHotSdk = params.tangemHotSdk.instanceAccess.get() ?: return@launch
+            // TODO
+            // val hotWalletId = tangemHotSdk.generateWallet(HotAuth.NoAuth, mnemonicType = MnemonicType.Words12)
+            // val hotUserWalletBuilder = hotUserWalletBuilderFactory.create(hotWalletId, tangemHotSdk)
+            // saveUserWalletUseCase(
+            //     hotUserWalletBuilder.build(),
+            // )
+
+            // router.push(AppRoute.Wallet)
+        }
     }
 }
