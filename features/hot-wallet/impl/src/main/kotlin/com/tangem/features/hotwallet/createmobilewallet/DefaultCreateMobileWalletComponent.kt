bff7d9c8ef798acbf71f77d218bd449c40d2c59f
Tangem <git@tangem.com>
2025-07-10 10:33:13 +0300

    Updated on 2026-06-12

--- features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/createmobilewallet/DefaultCreateMobileWalletComponent.kt
+++ features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/createmobilewallet/DefaultCreateMobileWalletComponent.kt
@@ -6,17 +6,28 @@ import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.tangem.core.decompose.context.AppComponentContext
 import com.tangem.core.decompose.model.getOrCreateModel
+import com.tangem.core.decompose.utils.ActivityInstanceHolder
+import com.tangem.core.decompose.utils.getOrCreateActivityInstanceHolder
 import com.tangem.features.hotwallet.CreateMobileWalletComponent
 import com.tangem.features.hotwallet.createmobilewallet.ui.CreateMobileWalletContent
+import com.tangem.hot.sdk.TangemHotSdk
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 
+@Suppress("UnusedPrivateMember")
 internal class DefaultCreateMobileWalletComponent @AssistedInject constructor(
     @Assisted private val context: AppComponentContext,
     @Assisted private val params: Unit,
+    private val tangemHotSdk: TangemHotSdk,
 ) : CreateMobileWalletComponent, AppComponentContext by context {
-    private val model: CreateMobileWalletModel = getOrCreateModel(params)
+
+    private val modelParams: Params =
+        Params(
+            tangemHotSdk = getOrCreateActivityInstanceHolder { tangemHotSdk },
+        )
+
+    private val model: CreateMobileWalletModel = getOrCreateModel(modelParams)
 
     @Composable
     override fun Content(modifier: Modifier) {
@@ -26,6 +37,10 @@ internal class DefaultCreateMobileWalletComponent @AssistedInject constructor(
             modifier = modifier,
         )
     }
+
+    class Params(
+        val tangemHotSdk: ActivityInstanceHolder<TangemHotSdk>,
+    )
 
     @AssistedFactory
     interface Factory : CreateMobileWalletComponent.Factory {
