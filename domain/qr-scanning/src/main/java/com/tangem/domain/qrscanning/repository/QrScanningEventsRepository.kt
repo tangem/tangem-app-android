60a4fe2dd286317594c614c39b62e08c3d45588a
Tangem <git@tangem.com>
2025-07-11 16:24:00 +0700

    Updated on 2026-05-13

--- domain/qr-scanning/src/main/java/com/tangem/domain/qrscanning/repository/QrScanningEventsRepository.kt
+++ domain/qr-scanning/src/main/java/com/tangem/domain/qrscanning/repository/QrScanningEventsRepository.kt
@@ -2,14 +2,15 @@ package com.tangem.domain.qrscanning.repository
 
 import com.tangem.domain.models.currency.CryptoCurrency
 import com.tangem.domain.qrscanning.models.QrResult
+import com.tangem.domain.qrscanning.models.RawQrResult
 import com.tangem.domain.qrscanning.models.SourceType
 import kotlinx.coroutines.flow.Flow
 
 interface QrScanningEventsRepository {
 
-    suspend fun emitResult(type: SourceType, qrCode: String)
+    suspend fun emitResult(qrCode: RawQrResult)
 
-    fun subscribeToScanningResults(type: SourceType): Flow<String>
+    fun subscribeToScanningResults(type: SourceType): Flow<RawQrResult>
 
     fun parseQrCode(qrCode: String, cryptoCurrency: CryptoCurrency): QrResult
 }
