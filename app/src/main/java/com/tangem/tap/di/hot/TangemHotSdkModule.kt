01d8744d5d3795449ce78f2d67cfc834a502a2a8
Tangem <git@tangem.com>
2025-07-12 17:37:51 +0300

    Updated on 2025-10-01

--- app/src/main/java/com/tangem/tap/di/hot/TangemHotSdkModule.kt
+++ app/src/main/java/com/tangem/tap/di/hot/TangemHotSdkModule.kt
@@ -1,23 +1,18 @@
 package com.tangem.tap.di.hot
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import com.tangem.hot.sdk.TangemHotSdk
-import com.tangem.hot.sdk.android.create
+import com.tangem.tap.features.hot.TangemHotSDKProxy
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
-import dagger.hilt.android.qualifiers.ActivityContext
-import dagger.hilt.android.scopes.ActivityScoped
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
-@InstallIn(ActivityComponent::class)
-internal object TangemHotSdkModule {
+@InstallIn(SingletonComponent::class)
+internal interface TangemHotSdkModule {
 
-    @Provides
-    @ActivityScoped
-    fun provideRootAppComponentContext(@ActivityContext context: Context): TangemHotSdk {
-        return TangemHotSdk.create(context as AppCompatActivity)
-    }
+    @Binds
+    @Singleton
+    fun bindTangemHotSdk(proxy: TangemHotSDKProxy): TangemHotSdk
 }
