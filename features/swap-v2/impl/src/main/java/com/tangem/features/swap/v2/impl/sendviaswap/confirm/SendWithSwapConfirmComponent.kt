86db53cc92f99785219de770d8a750a1094b79ed
Tangem <git@tangem.com>
2025-07-14 18:01:49 +0500

    Updated on 2026-02-11

---
+++ features/swap-v2/impl/src/main/java/com/tangem/features/swap/v2/impl/sendviaswap/confirm/SendWithSwapConfirmComponent.kt
@@ -0,0 +1,162 @@
+package com.tangem.features.swap.v2.impl.sendviaswap.confirm
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.tangem.core.decompose.context.AppComponentContext
+import com.tangem.core.decompose.context.child
+import com.tangem.core.decompose.context.childByContext
+import com.tangem.core.decompose.model.getOrCreateModel
+import com.tangem.core.ui.decompose.ComposableContentComponent
+import com.tangem.domain.appcurrency.model.AppCurrency
+import com.tangem.domain.swap.models.SwapDirection
+import com.tangem.domain.tokens.model.CryptoCurrencyStatus
+import com.tangem.domain.wallets.models.UserWallet
+import com.tangem.features.send.v2.api.FeeSelectorBlockComponent
+import com.tangem.features.send.v2.api.SendNotificationsComponent
+import com.tangem.features.send.v2.api.entity.PredefinedValues
+import com.tangem.features.send.v2.api.params.FeeSelectorParams
+import com.tangem.features.send.v2.api.subcomponents.destination.SendDestinationBlockComponent
+import com.tangem.features.send.v2.api.subcomponents.destination.SendDestinationComponentParams
+import com.tangem.features.swap.v2.impl.amount.SwapAmountBlockComponent
+import com.tangem.features.swap.v2.impl.amount.SwapAmountComponentParams
+import com.tangem.features.swap.v2.impl.common.entity.ConfirmUM
+import com.tangem.features.swap.v2.impl.sendviaswap.SendWithSwapRoute
+import com.tangem.features.swap.v2.impl.sendviaswap.confirm.model.SendWithSwapConfirmModel
+import com.tangem.features.swap.v2.impl.sendviaswap.confirm.ui.SendWithSwapConfirmContent
+import com.tangem.features.swap.v2.impl.sendviaswap.entity.SendWithSwapUM
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import kotlinx.coroutines.flow.*
+import java.math.BigDecimal
+
+internal class SendWithSwapConfirmComponent @AssistedInject constructor(
+    @Assisted private val appComponentContext: AppComponentContext,
+    @Assisted private val params: Params,
+    sendDestinationBlockComponent: SendDestinationBlockComponent.Factory,
+    feeSelectorBlockComponentFactory: FeeSelectorBlockComponent.Factory,
+    sendNotificationsComponentFactory: SendNotificationsComponent.Factory,
+) : ComposableContentComponent, AppComponentContext by appComponentContext {
+
+    private val model: SendWithSwapConfirmModel = getOrCreateModel(params = params)
+
+    private val blockClickEnableFlow = MutableStateFlow(false)
+
+    private val amountBlockComponent = SwapAmountBlockComponent(
+        appComponentContext = child("sendWithSwapConfirmAmountBlock"),
+        params = SwapAmountComponentParams.AmountBlockParams(
+            amountUM = model.uiState.value.amountUM,
+            analyticsCategoryName = params.analyticsCategoryName,
+            userWallet = params.userWallet,
+            blockClickEnableFlow = blockClickEnableFlow.asStateFlow(),
+            primaryCryptoCurrencyStatusFlow = params.primaryCryptoCurrencyStatusFlow,
+            secondaryCryptoCurrency = model.secondaryCurrency,
+            isBalanceHidingFlow = params.isBalanceHidingFlow,
+            swapDirection = params.swapDirection,
+        ),
+        onResult = model::onAmountResult,
+        onClick = model::showEditAmount,
+    )
+
+    private val sendDestinationBlockComponent = sendDestinationBlockComponent.create(
+        context = child("sendWithSwapConfirmDestinationBlock"),
+        params = SendDestinationComponentParams.DestinationBlockParams(
+            state = model.uiState.value.destinationUM,
+            analyticsCategoryName = params.analyticsCategoryName,
+            userWalletId = params.userWallet.walletId,
+            blockClickEnableFlow = blockClickEnableFlow.asStateFlow(),
+            cryptoCurrency = model.secondaryCurrency,
+            predefinedValues = PredefinedValues.Empty,
+        ),
+        onResult = model::onDestinationResult,
+        onClick = model::showEditDestination,
+    )
+
+    private val feeSelectorBlockComponent = feeSelectorBlockComponentFactory.create(
+        context = child("sendWithSwapConfirmFeeBlock"),
+        params = FeeSelectorParams.FeeSelectorBlockParams(
+            state = model.uiState.value.feeSelectorUM,
+            onLoadFee = model::loadFee,
+            feeCryptoCurrencyStatus = model.primaryFeePaidCurrencyStatus,
+            cryptoCurrencyStatus = model.primaryCurrencyStatus,
+            suggestedFeeState = FeeSelectorParams.SuggestedFeeState.None,
+            feeDisplaySource = FeeSelectorParams.FeeDisplaySource.Screen,
+        ),
+        onResult = model::onFeeResult,
+    )
+
+    private val sendNotificationsComponent = sendNotificationsComponentFactory.create(
+        context = appComponentContext.childByContext(child("sendWithSwapConfirmNotifications")),
+        params = SendNotificationsComponent.Params(
+            analyticsCategoryName = params.analyticsCategoryName,
+            userWalletId = params.userWallet.walletId,
+            cryptoCurrencyStatus = model.primaryCurrencyStatus,
+            feeCryptoCurrencyStatus = model.primaryFeePaidCurrencyStatus,
+            appCurrency = params.appCurrency,
+            notificationData = SendNotificationsComponent.Params.NotificationData(
+                // todo fill with data
+                destinationAddress = "",
+                memo = "",
+                amountValue = BigDecimal.ZERO,
+                reduceAmountBy = BigDecimal.ZERO,
+                isIgnoreReduce = false,
+                fee = null,
+                feeError = null,
+            ),
+        ),
+    )
+
+    init {
+        model.uiState.onEach { state ->
+            val confirmUM = state.confirmUM as? ConfirmUM.Content
+            blockClickEnableFlow.value = confirmUM?.isTransactionInProcess == false
+        }.launchIn(componentScope)
+    }
+
+    fun updateState(sendWithSwapUM: SendWithSwapUM) {
+        amountBlockComponent.updateState(sendWithSwapUM.amountUM)
+        sendDestinationBlockComponent.updateState(sendWithSwapUM.destinationUM)
+        feeSelectorBlockComponent.updateState(sendWithSwapUM.feeSelectorUM)
+        model.updateState(sendWithSwapUM)
+    }
+
+    @Composable
+    override fun Content(modifier: Modifier) {
+        val sendWithSwapUM by model.uiState.collectAsStateWithLifecycle()
+        val sendNotificationsUM by sendNotificationsComponent.state.collectAsStateWithLifecycle()
+
+        SendWithSwapConfirmContent(
+            sendWithSwapUM = sendWithSwapUM,
+            amountBlockComponent = amountBlockComponent,
+            sendDestinationBlockComponent = sendDestinationBlockComponent,
+            feeSelectorBlockComponent = feeSelectorBlockComponent,
+            sendNotificationsComponent = sendNotificationsComponent,
+            sendNotificationsUM = sendNotificationsUM,
+            modifier = modifier,
+        )
+    }
+
+    data class Params(
+        val sendWithSwapUM: SendWithSwapUM,
+        val analyticsCategoryName: String,
+        val userWallet: UserWallet,
+        val appCurrency: AppCurrency,
+        val currentRoute: Flow<SendWithSwapRoute>,
+        val swapDirection: SwapDirection,
+        val isBalanceHidingFlow: StateFlow<Boolean>,
+        val primaryCryptoCurrencyStatusFlow: StateFlow<CryptoCurrencyStatus>,
+        val primaryFeePaidCurrencyStatusFlow: StateFlow<CryptoCurrencyStatus>,
+        val callback: ModelCallback,
+    )
+
+    @AssistedFactory
+    interface Factory {
+        fun create(appComponentContext: AppComponentContext, params: Params): SendWithSwapConfirmComponent
+    }
+
+    interface ModelCallback {
+        fun onResult(sendWithSwapUM: SendWithSwapUM)
+    }
+}
