42b60804170c579955012ec43c4a872e7628cca8
Tangem <git@tangem.com>
2025-07-14 20:45:28 +0500

    Updated on 2026-09-21

--- data/swap/src/main/java/com/tangem/data/swap/DefaultSwapRepositoryV2.kt
+++ data/swap/src/main/java/com/tangem/data/swap/DefaultSwapRepositoryV2.kt
@@ -1,19 +1,28 @@
 package com.tangem.data.swap
 
+import com.squareup.moshi.Moshi
 import com.tangem.data.common.api.safeApiCall
+import com.tangem.data.swap.converter.SwapDataConverter
+import com.tangem.data.swap.converter.SwapStatusConverter
 import com.tangem.data.swap.converter.TokenInfoConverter
 import com.tangem.datasource.api.common.response.getOrThrow
 import com.tangem.datasource.api.express.TangemExpressApi
+import com.tangem.datasource.api.express.models.request.ExchangeSentRequestBody
 import com.tangem.datasource.api.express.models.request.PairsRequestBody
+import com.tangem.datasource.api.express.models.response.ExchangeDataResponseWithTxDetails
+import com.tangem.datasource.api.express.models.response.TxDetails
+import com.tangem.datasource.crypto.DataSignatureVerifier
+import com.tangem.datasource.di.NetworkMoshi
 import com.tangem.datasource.exchangeservice.swap.ExpressUtils
 import com.tangem.datasource.local.preferences.AppPreferencesStore
 import com.tangem.domain.express.ExpressRepository
+import com.tangem.domain.express.models.ExpressError
 import com.tangem.domain.express.models.ExpressProvider
+import com.tangem.domain.express.models.ExpressProviderType
 import com.tangem.domain.express.models.ExpressRateType
 import com.tangem.domain.models.currency.CryptoCurrency
 import com.tangem.domain.swap.SwapRepositoryV2
-import com.tangem.domain.swap.models.SwapPairModel
-import com.tangem.domain.swap.models.SwapQuoteModel
+import com.tangem.domain.swap.models.*
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.domain.tokens.operations.BaseCurrencyStatusOperations
 import com.tangem.domain.wallets.models.UserWallet
@@ -24,19 +33,26 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.withContext
 import timber.log.Timber
+import java.io.IOException
 import java.math.BigDecimal
+import java.util.UUID
 import javax.inject.Inject
 
-@Suppress("LongParameterList")
+@Suppress("LongParameterList", "LargeClass")
 internal class DefaultSwapRepositoryV2 @Inject constructor(
     private val tangemExpressApi: TangemExpressApi,
     private val expressRepository: ExpressRepository,
     private val coroutineDispatcher: CoroutineDispatcherProvider,
     private val appPreferencesStore: AppPreferencesStore,
     private val currencyStatusOperations: BaseCurrencyStatusOperations,
+    private val dataSignatureVerifier: DataSignatureVerifier,
+    @NetworkMoshi moshi: Moshi,
 ) : SwapRepositoryV2 {
 
+    private val swapDataConverter = SwapDataConverter()
     private val tokenInfoConverter = TokenInfoConverter()
+    private val exchangeStatusConverter = SwapStatusConverter()
+    private val txDetailsMoshiAdapter = moshi.adapter(TxDetails::class.java)
 
     override suspend fun getPairs(
         userWallet: UserWallet,
@@ -153,6 +169,115 @@ internal class DefaultSwapRepositoryV2 @Inject constructor(
         )
     }
 
+    override suspend fun getSwapData(
+        userWallet: UserWallet,
+        fromCryptoCurrencyStatus: CryptoCurrencyStatus,
+        toCryptoCurrencyStatus: CryptoCurrencyStatus,
+        fromAmount: String,
+        toAddress: String?,
+        expressProvider: ExpressProvider,
+        rateType: ExpressRateType,
+    ): SwapDataModel = withContext(coroutineDispatcher.io) {
+        val requestId = UUID.randomUUID().toString()
+        val fromCryptoCurrency = fromCryptoCurrencyStatus.currency
+        val toCryptoCurrency = toCryptoCurrencyStatus.currency
+
+        val refundData = when (expressProvider.type) {
+            ExpressProviderType.CEX,
+            ExpressProviderType.DEX_BRIDGE,
+            ExpressProviderType.DEX,
+            -> SwapRefundData(
+                refundAddress = fromCryptoCurrencyStatus.value.networkAddress?.defaultAddress?.value,
+                refundExtraId = null, // currently always null
+            )
+            else -> null
+        }
+
+        val response = tangemExpressApi.getExchangeData(
+            fromContractAddress = fromCryptoCurrency.getContractAddress(),
+            toContractAddress = toCryptoCurrency.getContractAddress(),
+            fromNetwork = fromCryptoCurrency.network.backendId,
+            toNetwork = toCryptoCurrency.network.backendId,
+            fromAddress = fromCryptoCurrencyStatus.value.networkAddress?.defaultAddress?.value.orEmpty(),
+            toAddress = toAddress ?: toCryptoCurrencyStatus.value.networkAddress?.defaultAddress?.value.orEmpty(),
+            fromDecimals = fromCryptoCurrency.decimals,
+            toDecimals = toCryptoCurrency.decimals,
+            fromAmount = fromAmount,
+            providerId = expressProvider.name,
+            rateType = rateType.name.lowercase(),
+            requestId = requestId,
+            refundAddress = refundData?.refundAddress,
+            refundExtraId = refundData?.refundExtraId,
+            userWalletId = userWallet.walletId.stringValue,
+            refCode = ExpressUtils.getRefCode(
+                userWallet = userWallet,
+                appPreferencesStore = appPreferencesStore,
+            ),
+        ).getOrThrow()
+
+        if (dataSignatureVerifier.verifySignature(response.signature, response.txDetailsJson)) {
+            val txDetails = parseTxDetails(response.txDetailsJson)
+                ?: throw ExpressError.UnknownError
+            if (txDetails.requestId != requestId) {
+                throw ExpressError.InvalidRequestIdError()
+            }
+            if (!toAddress.equals(txDetails.payoutAddress, ignoreCase = true)) {
+                throw ExpressError.InvalidPayoutAddressError()
+            }
+            swapDataConverter.convert(
+                ExchangeDataResponseWithTxDetails(
+                    dataResponse = response,
+                    txDetails = txDetails,
+                ),
+            )
+        } else {
+            throw ExpressError.InvalidSignatureError()
+        }
+    }
+
+    override suspend fun swapTransactionSent(
+        userWallet: UserWallet,
+        fromCryptoCurrencyStatus: CryptoCurrencyStatus,
+        toAddress: String,
+        txId: String,
+        txHash: String,
+        txExtraId: String?,
+    ) {
+        withContext(coroutineDispatcher.io) {
+            tangemExpressApi.exchangeSent(
+                userWalletId = userWallet.walletId.stringValue,
+                refCode = ExpressUtils.getRefCode(
+                    userWallet = userWallet,
+                    appPreferencesStore = appPreferencesStore,
+                ),
+                body = ExchangeSentRequestBody(
+                    txId = txId,
+                    fromNetwork = fromCryptoCurrencyStatus.currency.network.backendId,
+                    fromAddress = fromCryptoCurrencyStatus.value.networkAddress?.defaultAddress?.value.orEmpty(),
+                    payinAddress = toAddress,
+                    payinExtraId = txExtraId,
+                    txHash = txHash,
+                ),
+            ).getOrThrow()
+        }
+    }
+
+    override suspend fun getExchangeStatus(userWallet: UserWallet, txId: String): SwapStatusModel =
+        withContext(coroutineDispatcher.io) {
+            exchangeStatusConverter.convert(
+                tangemExpressApi
+                    .getExchangeStatus(
+                        userWalletId = userWallet.walletId.stringValue,
+                        refCode = ExpressUtils.getRefCode(
+                            userWallet = userWallet,
+                            appPreferencesStore = appPreferencesStore,
+                        ),
+                        txId = txId,
+                    )
+                    .getOrThrow(),
+            )
+        }
+
     private suspend fun CoroutineScope.getPairsInternal(
         userWallet: UserWallet,
         initialCurrency: CryptoCurrency,
@@ -200,7 +325,7 @@ internal class DefaultSwapRepositoryV2 @Inject constructor(
         },
     )
 
-    private suspend fun CoroutineScope.createPairModelOnly(
+    private suspend fun createPairModelOnly(
         currencyFrom: CryptoCurrency?,
         currencyTo: CryptoCurrency?,
         userWalletId: UserWalletId,
@@ -225,6 +350,15 @@ internal class DefaultSwapRepositoryV2 @Inject constructor(
                 null
             }
         } else {
+            null
+        }
+    }
+
+    private fun parseTxDetails(txDetailsJson: String): TxDetails? {
+        return try {
+            txDetailsMoshiAdapter.fromJson(txDetailsJson)
+        } catch (e: IOException) {
+            Timber.e(e, "error parsing txDetailsJson")
             null
         }
     }
