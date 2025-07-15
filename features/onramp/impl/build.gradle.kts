1852dae685f0329d949367f64a4ebcb4062982b3
Tangem <git@tangem.com>
2025-07-15 18:55:31 +0400

    Updated on 2026-06-05

--- features/onramp/impl/build.gradle.kts
+++ features/onramp/impl/build.gradle.kts
@@ -22,7 +22,6 @@ dependencies {
     implementation(projects.core.analytics)
     implementation(projects.core.configToggles)
     implementation(projects.core.decompose)
-    implementation(projects.core.deepLinks)
     implementation(projects.core.navigation)
     implementation(projects.core.ui)
     implementation(projects.core.utils)
