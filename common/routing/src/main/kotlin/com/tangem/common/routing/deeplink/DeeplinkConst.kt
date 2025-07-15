634f26c4914d76d6a8b17282306a2c653ab4de4a
Tangem <git@tangem.com>
2025-07-15 18:55:31 +0400

    Updated on 2025-11-26

--- common/routing/src/main/kotlin/com/tangem/common/routing/deeplink/DeeplinkConst.kt
+++ common/routing/src/main/kotlin/com/tangem/common/routing/deeplink/DeeplinkConst.kt
@@ -1,6 +1,9 @@
-package com.tangem.core.deeplink
+package com.tangem.common.routing.deeplink
 
 object DeeplinkConst {
+    const val DEEPLINK_KEY = "deeplink"
+    const val WEBLINK_KEY = "link"
+
     const val TANGEM_SCHEME = "tangem"
     const val WALLET_ID_KEY = "user_wallet_id"
     const val NETWORK_ID_KEY = "network_id"
