9d96e2cada4da9c732c1e6f13294b64e5d943987
Tangem <git@tangem.com>
2025-07-16 20:11:00 +0700

    Updated on 2026-09-25

--- data/wallet-connect/src/main/kotlin/com/tangem/data/walletconnect/pair/CaipNamespaceDelegate.kt
+++ data/wallet-connect/src/main/kotlin/com/tangem/data/walletconnect/pair/CaipNamespaceDelegate.kt
@@ -1,48 +1,47 @@
 package com.tangem.data.walletconnect.pair
 
 import com.reown.walletkit.client.Wallet
-import com.tangem.data.walletconnect.model.CAIP10
 import com.tangem.data.walletconnect.utils.WcNamespaceConverter
+import com.tangem.data.walletconnect.utils.WcNetworksConverter
 import com.tangem.domain.models.network.Network
+import com.tangem.data.walletconnect.model.CAIP10
+import com.tangem.domain.walletconnect.model.WcSessionApprove
 import com.tangem.domain.walletmanager.WalletManagersFacade
-import com.tangem.domain.wallets.models.UserWallet
+import com.tangem.domain.wallets.models.UserWalletId
 
 internal class CaipNamespaceDelegate(
     private val namespaceConverters: Set<WcNamespaceConverter>,
     private val walletManagersFacade: WalletManagersFacade,
+    private val wcNetworksConverter: WcNetworksConverter,
 ) {
 
     suspend fun associate(
         sessionProposal: Wallet.Model.SessionProposal,
-        userWallet: UserWallet,
-        networks: List<Network>,
+        sessionForApprove: WcSessionApprove,
     ): Map<String, Wallet.Model.Namespace.Session> {
+        val userWallet = sessionForApprove.wallet
         val result = mutableMapOf<String, Session>()
 
-        networks.map { network ->
-            val address = walletManagersFacade.getDefaultAddress(userWallet.walletId, network)
-            val chainId = namespaceConverters.firstNotNullOfOrNull { it.toCAIP2(network) }
-            requireNotNull(chainId)
-            requireNotNull(address)
-            CAIP10(chainId = chainId, accountAddress = address)
-        }.forEach { account ->
-            val namespaceKey = account.chainId.namespace
-            val session = result.getOrPut(namespaceKey) { Session() }
-            val requiredNamespaces = sessionProposal.requiredNamespaces
-            val optionalNamespaces = sessionProposal.optionalNamespaces
-            val methods = buildSet {
-                requiredNamespaces[namespaceKey]?.methods?.let { addAll(it) }
-                optionalNamespaces[namespaceKey]?.methods?.let { addAll(it) }
+        wcNetworksConverter.convertNetworksForApprove(sessionForApprove)
+            .mapNotNull { createCAIP10(userWallet.walletId, it) }
+            .forEach { account ->
+                val namespaceKey = account.chainId.namespace
+                val session = result.getOrPut(namespaceKey) { Session() }
+                val requiredNamespaces = sessionProposal.requiredNamespaces
+                val optionalNamespaces = sessionProposal.optionalNamespaces
+                val methods = buildSet {
+                    requiredNamespaces[namespaceKey]?.methods?.let { addAll(it) }
+                    optionalNamespaces[namespaceKey]?.methods?.let { addAll(it) }
+                }
+                val events = buildSet {
+                    requiredNamespaces[namespaceKey]?.events?.let { addAll(it) }
+                    optionalNamespaces[namespaceKey]?.events?.let { addAll(it) }
+                }
+                session.chains.add(account.chainId.raw)
+                session.accounts.add(account.raw)
+                session.methods.addAll(methods)
+                session.events.addAll(events)
             }
-            val events = buildSet {
-                requiredNamespaces[namespaceKey]?.events?.let { addAll(it) }
-                optionalNamespaces[namespaceKey]?.events?.let { addAll(it) }
-            }
-            session.chains.add(account.chainId.raw)
-            session.accounts.add(account.raw)
-            session.methods.addAll(methods)
-            session.events.addAll(events)
-        }
         return result.mapValues { (_, session) ->
             Wallet.Model.Namespace.Session(
                 chains = session.chains.toList(),
@@ -51,6 +50,13 @@ internal class CaipNamespaceDelegate(
                 events = session.events.toList(),
             )
         }
+    }
+
+    private suspend fun createCAIP10(userWalletId: UserWalletId, network: Network): CAIP10? {
+        val address = walletManagersFacade.getDefaultAddress(userWalletId, network)
+        val chainId = namespaceConverters.firstNotNullOfOrNull { it.toCAIP2(network) }
+        if (chainId == null || address == null) return null
+        return CAIP10(chainId = chainId, accountAddress = address)
     }
 
     private data class Session(
