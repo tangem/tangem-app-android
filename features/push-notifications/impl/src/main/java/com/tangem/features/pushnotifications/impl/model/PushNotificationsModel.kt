8dad34463ec1d878846d64bd1bcd16e408b2cf8a
Tangem <git@tangem.com>
2025-07-16 18:02:31 +0500

    Updated on 2026-09-25

--- features/push-notifications/impl/src/main/java/com/tangem/features/pushnotifications/impl/model/PushNotificationsModel.kt
+++ features/push-notifications/impl/src/main/java/com/tangem/features/pushnotifications/impl/model/PushNotificationsModel.kt
@@ -1,15 +1,16 @@
 package com.tangem.features.pushnotifications.impl.model
 
 import androidx.compose.runtime.Stable
-import com.tangem.common.routing.AppRoute
 import com.tangem.common.routing.AppRouter
 import com.tangem.core.analytics.api.AnalyticsEventHandler
 import com.tangem.core.analytics.models.AnalyticsParam
 import com.tangem.core.decompose.di.ModelScoped
 import com.tangem.core.decompose.model.Model
+import com.tangem.core.decompose.model.ParamsContainer
 import com.tangem.domain.notifications.toggles.NotificationsFeatureToggles
 import com.tangem.domain.settings.NeverRequestPermissionUseCase
 import com.tangem.domain.settings.NeverToInitiallyAskPermissionUseCase
+import com.tangem.features.pushnotifications.api.PushNotificationsComponent
 import com.tangem.features.pushnotifications.api.analytics.PushNotificationAnalyticEvents
 import com.tangem.features.pushnotifications.api.utils.PUSH_PERMISSION
 import com.tangem.features.pushnotifications.impl.presentation.state.PushNotificationsUM
@@ -21,14 +22,18 @@ import javax.inject.Inject
 
 @Stable
 @ModelScoped
+@Suppress("LongParameterList")
 internal class PushNotificationsModel @Inject constructor(
+    paramsContainer: ParamsContainer,
     override val dispatchers: CoroutineDispatcherProvider,
     private val neverRequestPermissionUseCase: NeverRequestPermissionUseCase,
     private val neverToInitiallyAskPermissionUseCase: NeverToInitiallyAskPermissionUseCase,
     private val appRouter: AppRouter,
     private val analyticHandler: AnalyticsEventHandler,
-    private val notificationsFeatureToggles: NotificationsFeatureToggles,
+    notificationsFeatureToggles: NotificationsFeatureToggles,
 ) : Model(), PushNotificationsClickIntents {
+
+    private val params: PushNotificationsComponent.Params = paramsContainer.require()
 
     private val _state = MutableStateFlow(
         PushNotificationsUM(
@@ -51,7 +56,7 @@ internal class PushNotificationsModel @Inject constructor(
         modelScope.launch {
             neverRequestPermissionUseCase(PUSH_PERMISSION)
             neverToInitiallyAskPermissionUseCase(PUSH_PERMISSION)
-            appRouter.push(AppRoute.Home)
+            onResult()
         }
     }
 
@@ -62,7 +67,7 @@ internal class PushNotificationsModel @Inject constructor(
         modelScope.launch {
             neverRequestPermissionUseCase(PUSH_PERMISSION)
             neverToInitiallyAskPermissionUseCase(PUSH_PERMISSION)
-            appRouter.push(AppRoute.Home)
+            onResult()
         }
     }
 
@@ -73,7 +78,18 @@ internal class PushNotificationsModel @Inject constructor(
         modelScope.launch {
             neverRequestPermissionUseCase(PUSH_PERMISSION)
             neverToInitiallyAskPermissionUseCase(PUSH_PERMISSION)
-            appRouter.push(AppRoute.Home)
+            onResult()
+        }
+    }
+
+    private fun onResult() {
+        when (params) {
+            is PushNotificationsComponent.Params.Callbacks -> {
+                params.callbacks.onResult()
+            }
+            is PushNotificationsComponent.Params.Route -> {
+                appRouter.push(params.route)
+            }
         }
     }
 }
