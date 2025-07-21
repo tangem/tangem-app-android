bea4b7d4ac9af44952a75821b1b7387f331b25c8
Tangem <git@tangem.com>
2025-07-21 18:04:33 +0500

    Updated on 2026-06-05

--- features/send-v2/api/src/main/java/com/tangem/features/send/v2/api/subcomponents/feeSelector/FeeSelectorReloadTrigger.kt
+++ features/send-v2/api/src/main/java/com/tangem/features/send/v2/api/subcomponents/feeSelector/FeeSelectorReloadTrigger.kt
@@ -7,9 +7,15 @@ interface FeeSelectorReloadTrigger {
 
     /** Trigger fee update */
     suspend fun triggerUpdate(feeData: FeeSelectorData = FeeSelectorData())
+
+    /** Triggers fee loading status. Used in cases where fee loading state is extended due to business logic */
+    suspend fun triggerLoadingState()
 }
 
 interface FeeSelectorReloadListener {
     /** Flow triggers fee reload */
     val reloadTriggerFlow: Flow<FeeSelectorData>
+
+    /** Triggers fee loading status. Used in cases where fee loading state is extended due to business logic */
+    val loadingStateTriggerFlow: Flow<Unit>
 }
