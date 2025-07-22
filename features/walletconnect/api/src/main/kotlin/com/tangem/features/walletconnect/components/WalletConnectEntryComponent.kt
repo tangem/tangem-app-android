9e6ea8acf7ed1052cf152c1112e6ee8f0a3c9808
Tangem <git@tangem.com>
2025-07-22 11:19:30 +0400

    Updated on 2025-10-01

--- features/walletconnect/api/src/main/kotlin/com/tangem/features/walletconnect/components/WalletConnectEntryComponent.kt
+++ features/walletconnect/api/src/main/kotlin/com/tangem/features/walletconnect/components/WalletConnectEntryComponent.kt
@@ -2,9 +2,11 @@ package com.tangem.features.walletconnect.components
 
 import com.tangem.core.decompose.factory.ComponentFactory
 import com.tangem.core.ui.decompose.ComposableContentComponent
-import com.tangem.domain.wallets.models.UserWalletId
+import com.tangem.domain.models.wallet.UserWalletId
 
 interface WalletConnectEntryComponent : ComposableContentComponent {
+
     data class Params(val userWalletId: UserWalletId)
+
     interface Factory : ComponentFactory<Params, WalletConnectEntryComponent>
 }
