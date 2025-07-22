7253cad8356c5527acb281bddaff070f8e1555a5
Tangem <git@tangem.com>
2025-07-22 11:19:30 +0400

    Updated on 2026-06-10

--- domain/wallets/src/main/java/com/tangem/domain/wallets/usecase/IsNeedToBackupUseCase.kt
+++ domain/wallets/src/main/java/com/tangem/domain/wallets/usecase/IsNeedToBackupUseCase.kt
@@ -3,7 +3,7 @@ package com.tangem.domain.wallets.usecase
 import com.tangem.domain.models.scan.CardDTO
 import com.tangem.domain.wallets.legacy.UserWalletsListManager
 import com.tangem.domain.wallets.models.UserWallet
-import com.tangem.domain.wallets.models.UserWalletId
+import com.tangem.domain.models.wallet.UserWalletId
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
