c0b674f3bd865ad408fc7b2d1893b4521b3606d5
Tangem <git@tangem.com>
2025-07-22 20:26:37 +0300

    Updated on 2026-09-23

--- app/src/main/java/com/tangem/tap/domain/userWalletList/repository/SelectedUserWalletRepository.kt
+++ app/src/main/java/com/tangem/tap/domain/userWalletList/repository/SelectedUserWalletRepository.kt
@@ -3,6 +3,6 @@ package com.tangem.tap.domain.userWalletList.repository
 import com.tangem.domain.wallets.models.UserWalletId
 
 internal interface SelectedUserWalletRepository {
-    fun get(): UserWalletId?
-    fun set(walletId: UserWalletId?)
+    suspend fun get(): UserWalletId?
+    suspend fun set(walletId: UserWalletId?)
 }
