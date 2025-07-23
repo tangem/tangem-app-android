e622a4f80005b5351f9416734294d276d255746a
Tangem <git@tangem.com>
2025-07-23 16:53:37 +0700

    Updated on 2025-12-22

--- data/wallet-connect/src/main/kotlin/com/tangem/data/walletconnect/request/WcRequestToUseCaseConverter.kt
+++ data/wallet-connect/src/main/kotlin/com/tangem/data/walletconnect/request/WcRequestToUseCaseConverter.kt
@@ -5,14 +5,14 @@ import arrow.core.left
 import arrow.core.right
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.adapter
+import com.tangem.domain.walletconnect.model.HandleMethodError
 import com.tangem.domain.walletconnect.model.WcMethodName
-import com.tangem.domain.walletconnect.model.WcRequestError
 import com.tangem.domain.walletconnect.model.sdkcopy.WcSdkSessionRequest
 import com.tangem.domain.walletconnect.usecase.method.WcMethodUseCase
 
 interface WcRequestToUseCaseConverter {
     fun toWcMethodName(request: WcSdkSessionRequest): WcMethodName?
-    suspend fun toUseCase(request: WcSdkSessionRequest): Either<WcRequestError.HandleMethodError, WcMethodUseCase>
+    suspend fun toUseCase(request: WcSdkSessionRequest): Either<HandleMethodError, WcMethodUseCase>
 
     companion object {
         @OptIn(ExperimentalStdlibApi::class)
