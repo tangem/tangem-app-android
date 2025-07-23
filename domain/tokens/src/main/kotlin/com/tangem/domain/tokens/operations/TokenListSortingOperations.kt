7f8feafc30ad2fb77c6850972f5f2fbaa7465ac3
Tangem <git@tangem.com>
2025-07-23 15:04:55 +0400

    Updated on 2026-04-06

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/operations/TokenListSortingOperations.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/operations/TokenListSortingOperations.kt
@@ -7,6 +7,7 @@ import arrow.core.raise.either
 import arrow.core.raise.ensure
 import arrow.core.raise.ensureNotNull
 import arrow.core.toNonEmptyListOrNull
+import com.tangem.domain.models.TokensSortType
 import com.tangem.domain.models.network.Network
 import com.tangem.domain.staking.model.stakekit.YieldBalance
 import com.tangem.domain.staking.utils.getTotalWithRewardsStakingBalance
@@ -25,7 +26,7 @@ internal class TokenListSortingOperations(
 
     constructor(
         tokenList: TokenList,
-        sortByBalance: Boolean = tokenList.sortedBy == TokenList.SortType.BALANCE,
+        sortByBalance: Boolean = tokenList.sortedBy == TokensSortType.BALANCE,
         isAnyTokenLoading: Boolean = tokenList.totalFiatBalance is TotalFiatBalance.Loading,
     ) : this(
         currencies = tokenList.flattenCurrencies(),
@@ -51,7 +52,7 @@ internal class TokenListSortingOperations(
         if (sortByBalance) sortTokensByBalance(nonEmptyCurrencies) else nonEmptyCurrencies
     }
 
-    fun getSortType(): TokenList.SortType = if (sortByBalance) TokenList.SortType.BALANCE else TokenList.SortType.NONE
+    fun getSortType(): TokensSortType = if (sortByBalance) TokensSortType.BALANCE else TokensSortType.NONE
 
     private fun Raise<Error>.groupTokens(): NonEmptyList<NetworkGroup> {
         val groupedTokens = currencies
