21ba2e4cae757a164085581136f91aba069f5647
Tangem <git@tangem.com>
2025-07-23 10:37:38 +0300

    Updated on 2025-09-15

---
+++ features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/accesscoderequest/ui/HotAccessCodeRequestFullScreenContent.kt
@@ -0,0 +1,137 @@
+package com.tangem.features.hotwallet.accesscoderequest.ui
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.tangem.core.ui.components.SecondaryButton
+import com.tangem.core.ui.components.SpacerH
+import com.tangem.core.ui.components.SpacerH24
+import com.tangem.core.ui.components.appbar.TangemTopAppBar
+import com.tangem.core.ui.components.appbar.models.TopAppBarButtonUM
+import com.tangem.core.ui.components.fields.PinTextField
+import com.tangem.core.ui.extensions.stringResourceSafe
+import com.tangem.core.ui.haptic.TangemHapticEffect
+import com.tangem.core.ui.res.LocalHapticManager
+import com.tangem.core.ui.res.TangemTheme
+import com.tangem.core.ui.res.TangemThemePreview
+import com.tangem.features.hotwallet.accesscoderequest.entity.HotAccessCodeRequestUM
+import com.tangem.features.hotwallet.impl.R
+
+@Suppress("MagicNumber", "LongMethod")
+@Composable
+internal fun HotAccessCodeRequestFullScreenContent(state: HotAccessCodeRequestUM, modifier: Modifier = Modifier) {
+    AnimatedVisibility(
+        modifier = modifier,
+        visible = state.isShown,
+        enter = fadeIn(),
+        exit = fadeOut(),
+    ) {
+        Column(
+            Modifier
+                .fillMaxSize()
+                .background(TangemTheme.colors.background.primary),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            TangemTopAppBar(
+                modifier = Modifier
+                    .statusBarsPadding(),
+                startButton = TopAppBarButtonUM.Back(state.onDismiss),
+            )
+
+            SpacerH(68.dp)
+
+            Column(
+                Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    modifier = Modifier.animateEnterExit(
+                        enter = slideInVertically(
+                            tween(),
+                            initialOffsetY = { it + 200 },
+                        ) + fadeIn(tween()),
+                        exit = slideOutVertically(tween(300)) { it - 200 } + fadeOut(tween()),
+                    ),
+                    text = stringResourceSafe(R.string.access_code_check_title),
+                    style = TangemTheme.typography.h2,
+                    color = TangemTheme.colors.text.primary1,
+                )
+
+                SpacerH24()
+
+                PinTextField(
+                    modifier = Modifier.animateEnterExit(
+                        enter = slideInVertically(
+                            tween(),
+                            initialOffsetY = { it + 200 },
+                        ) + fadeIn(tween()),
+                        exit = slideOutVertically(tween(300)) { it - 200 } + fadeOut(tween()),
+                    ),
+                    length = 6,
+                    isPasswordVisual = true,
+                    value = state.accessCode,
+                    wrongCode = state.wrongAccessCode,
+                    onValueChange = state.onAccessCodeChange,
+                )
+            }
+
+            if (state.useBiometricVisible) {
+                SecondaryButton(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth()
+                        .navigationBarsPadding()
+                        .imePadding(),
+                    text = "Use biometric",
+                    onClick = state.useBiometricClick,
+                )
+            }
+        }
+    }
+
+    val hapticManager = LocalHapticManager.current
+
+    LaunchedEffect(state.wrongAccessCode) {
+        if (state.wrongAccessCode) {
+            hapticManager.perform(TangemHapticEffect.View.Reject)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview() {
+    TangemThemePreview {
+        Box(
+            Modifier
+                .fillMaxSize()
+                .background(TangemTheme.colors.background.secondary),
+        ) {
+            var isShown by remember { mutableStateOf(true) }
+
+            HotAccessCodeRequestFullScreenContent(
+                state = HotAccessCodeRequestUM(isShown = isShown),
+                modifier = Modifier,
+            )
+
+            Button(onClick = { isShown = !isShown }) {
+                Text("TOggle")
+            }
+        }
+    }
+}
