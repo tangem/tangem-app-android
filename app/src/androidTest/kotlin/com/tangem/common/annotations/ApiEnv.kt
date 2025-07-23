5d248c425e17a6d1ddf036f11a1958fad02aa16b
Tangem <git@tangem.com>
2025-07-23 14:53:14 +0300

    release/5.27.0

--- app/src/androidTest/kotlin/com/tangem/common/annotations/ApiEnv.kt
+++ app/src/androidTest/kotlin/com/tangem/common/annotations/ApiEnv.kt
@@ -1,14 +1,26 @@
 package com.tangem.common.annotations
 
+import com.tangem.datasource.api.common.config.ApiConfig
 import com.tangem.datasource.api.common.config.ApiEnvironment
 
 /**
- * Annotation to specify the API environment for a class or function.
+ * Single API environment configuration.
  *
+ * @property apiConfigId the ID of the API configuration
  * @property environment the API environment to be used (defaults to [ApiEnvironment.MOCK])
+ */
+annotation class ApiEnvConfig(
+    val apiConfigId: ApiConfig.ID,
+    val environment: ApiEnvironment = ApiEnvironment.MOCK,
+)
+
+/**
+ * Annotation to specify the API environment configurations for a class or function.
+ *
+ * @property value array of API environment configurations
  */
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class ApiEnv(
-    val environment: ApiEnvironment = ApiEnvironment.MOCK,
+    vararg val value: ApiEnvConfig,
 )
