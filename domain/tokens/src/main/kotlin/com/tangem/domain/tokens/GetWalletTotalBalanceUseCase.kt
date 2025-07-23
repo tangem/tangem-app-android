8284ff88d05becbd63a7d46258bdbd786a01ca4d
Tangem <git@tangem.com>
2025-07-23 16:07:06 +0400

    Updated on 2026-09-23

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/GetWalletTotalBalanceUseCase.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/GetWalletTotalBalanceUseCase.kt
@@ -8,12 +8,12 @@ import com.tangem.domain.core.lce.LceFlow
 import com.tangem.domain.core.lce.lce
 import com.tangem.domain.core.utils.lceContent
 import com.tangem.domain.core.utils.lceLoading
+import com.tangem.domain.models.TotalFiatBalance
+import com.tangem.domain.models.wallet.UserWalletId
 import com.tangem.domain.tokens.error.TokenListError
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
-import com.tangem.domain.tokens.model.TotalFiatBalance
 import com.tangem.domain.tokens.operations.BaseCurrenciesStatusesOperations
 import com.tangem.domain.tokens.operations.TokenListFiatBalanceOperations
-import com.tangem.domain.models.wallet.UserWalletId
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
 import timber.log.Timber
