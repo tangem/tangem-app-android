88a55f89672d501a74b93a00793addfe3761fea8
Tangem <git@tangem.com>
2025-07-23 10:37:38 +0300

    Updated on 2026-07-23

--- app/src/main/java/com/tangem/tap/di/domain/TransactionDomainModule.kt
+++ app/src/main/java/com/tangem/tap/di/domain/TransactionDomainModule.kt
@@ -11,7 +11,7 @@ import com.tangem.domain.transaction.FeeRepository
 import com.tangem.domain.transaction.TransactionRepository
 import com.tangem.domain.transaction.usecase.*
 import com.tangem.domain.walletmanager.WalletManagersFacade
-import com.tangem.tap.domain.hot.TangemHotSigner
+import com.tangem.tap.domain.hot.TangemHotWalletSigner
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -44,7 +44,7 @@ internal object TransactionDomainModule {
         transactionRepository: TransactionRepository,
         walletManagersFacade: WalletManagersFacade,
         singleNetworkStatusFetcher: SingleNetworkStatusFetcher,
-        tangemHotSignerFactory: TangemHotSigner.Factory,
+        tangemHotWalletSignerFactory: TangemHotWalletSigner.Factory,
     ): SendTransactionUseCase {
         return SendTransactionUseCase(
             demoConfig = DemoConfig(),
@@ -52,7 +52,7 @@ internal object TransactionDomainModule {
             transactionRepository = transactionRepository,
             walletManagersFacade = walletManagersFacade,
             singleNetworkStatusFetcher = singleNetworkStatusFetcher,
-            getHotSigner = tangemHotSignerFactory::create,
+            getHotWalletSigner = tangemHotWalletSignerFactory::create,
         )
     }
 
