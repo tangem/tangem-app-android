81ca7a95f05ac513673686b1cf91651cc062941b
Tangem <git@tangem.com>
2025-07-23 16:07:06 +0400

    Updated on 2025-09-01

--- common/ui/src/main/java/com/tangem/common/ui/userwallet/converter/UserWalletItemUMConverter.kt
+++ common/ui/src/main/java/com/tangem/common/ui/userwallet/converter/UserWalletItemUMConverter.kt
@@ -11,10 +11,10 @@ import com.tangem.domain.appcurrency.model.AppCurrency
 import com.tangem.domain.common.util.getCardsCount
 import com.tangem.domain.models.ArtworkModel
 import com.tangem.domain.models.StatusSource
+import com.tangem.domain.models.TotalFiatBalance
 import com.tangem.domain.models.wallet.UserWallet
 import com.tangem.domain.models.wallet.UserWalletId
 import com.tangem.domain.models.wallet.isLocked
-import com.tangem.domain.tokens.model.TotalFiatBalance
 import com.tangem.utils.converter.Converter
 
 /**
