5f331ad398d3c838bb88b794d3893af99c95e6f7
Tangem <git@tangem.com>
2025-07-29 11:50:44 +0500

    Updated on 2026-09-25

--- features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/ui/common/WcWalletItem.kt
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/ui/common/WcWalletItem.kt
@@ -5,11 +5,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import com.tangem.core.ui.components.SpacerWMax
+import com.tangem.core.ui.components.tooltip.TangemTooltip
 import com.tangem.core.ui.extensions.stringResourceSafe
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.features.walletconnect.impl.R
@@ -17,6 +19,7 @@ import com.tangem.features.walletconnect.impl.R
 @Composable
 internal fun WcWalletItem(walletName: String, modifier: Modifier = Modifier) {
     Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
+        var isTooltipEnabled by remember { mutableStateOf(false) }
         Icon(
             modifier = Modifier.size(TangemTheme.dimens.size24),
             painter = painterResource(R.drawable.ic_wallet_new_24),
@@ -24,21 +27,28 @@ internal fun WcWalletItem(walletName: String, modifier: Modifier = Modifier) {
             tint = TangemTheme.colors.icon.accent,
         )
         Text(
-            modifier = Modifier
-                .padding(start = TangemTheme.dimens.spacing8)
-                .weight(1f),
+            modifier = Modifier.padding(start = TangemTheme.dimens.spacing8),
             text = stringResourceSafe(R.string.manage_tokens_network_selector_wallet),
             style = TangemTheme.typography.body1,
             color = TangemTheme.colors.text.primary1,
+            maxLines = 1,
         )
-        Text(
-            modifier = Modifier
-                .padding(start = TangemTheme.dimens.spacing16)
-                .weight(1f),
+        SpacerWMax()
+        TangemTooltip(
+            modifier = Modifier.padding(start = TangemTheme.dimens.spacing16),
             text = walletName,
-            textAlign = TextAlign.End,
-            style = TangemTheme.typography.body1,
-            color = TangemTheme.colors.text.tertiary,
+            enabled = isTooltipEnabled,
+            content = { contentModifier ->
+                Text(
+                    modifier = contentModifier,
+                    text = walletName,
+                    style = TangemTheme.typography.body1,
+                    color = TangemTheme.colors.text.tertiary,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    onTextLayout = { isTooltipEnabled = it.hasVisualOverflow },
+                )
+            },
         )
     }
 }
