e2f96976bdbc8396318404f12ba5bd5e7035b9a0
Tangem <git@tangem.com>
2025-07-29 20:53:48 +0400

    Updated on 2026-04-06

--- features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/entity/blockaid/WcSendReceiveTransactionCheckResultsUM.kt
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/entity/blockaid/WcSendReceiveTransactionCheckResultsUM.kt
@@ -6,6 +6,16 @@ import com.tangem.features.walletconnect.transaction.entity.approve.WcSpendAllow
 internal data class WcSendReceiveTransactionCheckResultsUM(
     val estimatedWalletChanges: WcEstimatedWalletChangesUM? = null,
     val spendAllowance: WcSpendAllowanceUM? = null,
-    val notificationText: TextReference? = null,
+    val notification: BlockAidNotificationUM? = null,
     val isLoading: Boolean = true,
 )
+
+internal data class BlockAidNotificationUM(
+    val type: Type,
+    val title: TextReference,
+    val text: TextReference? = null,
+) {
+    internal enum class Type {
+        ERROR, WARNING
+    }
+}
