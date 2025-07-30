c9bfdaeaff5efabb787ade61041669630839163a
Tangem <git@tangem.com>
2025-07-30 17:42:54 +0400

    Updated on 2026-09-21

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/GetTokenListUseCase.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/GetTokenListUseCase.kt
@@ -5,10 +5,10 @@ import com.tangem.domain.core.utils.lceError
 import com.tangem.domain.core.utils.lceLoading
 import com.tangem.domain.core.utils.toLce
 import com.tangem.domain.models.currency.CryptoCurrencyStatus
+import com.tangem.domain.models.tokenlist.TokenList
 import com.tangem.domain.models.wallet.UserWalletId
 import com.tangem.domain.tokens.error.TokenListError
 import com.tangem.domain.tokens.error.mapper.mapToTokenListError
-import com.tangem.domain.tokens.model.TokenList
 import com.tangem.domain.tokens.operations.BaseCurrenciesStatusesOperations
 import com.tangem.domain.tokens.operations.TokenListOperations
 import com.tangem.domain.tokens.repository.CurrenciesRepository
