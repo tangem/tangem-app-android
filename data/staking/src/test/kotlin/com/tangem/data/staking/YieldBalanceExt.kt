5f7e6f04e930862c0173d20698b35c31ed0704f4
Tangem <git@tangem.com>
2025-07-30 13:45:45 +0400

    Updated on 2026-04-06

--- data/staking/src/test/kotlin/com/tangem/data/staking/YieldBalanceExt.kt
+++ data/staking/src/test/kotlin/com/tangem/data/staking/YieldBalanceExt.kt
@@ -3,7 +3,7 @@ package com.tangem.data.staking
 import com.tangem.datasource.api.stakekit.models.response.model.YieldBalanceWrapperDTO
 import com.tangem.datasource.local.token.converter.YieldBalanceConverter
 import com.tangem.domain.models.StatusSource
-import com.tangem.domain.staking.model.stakekit.YieldBalance
+import com.tangem.domain.models.staking.YieldBalance
 
 internal fun YieldBalanceWrapperDTO.toDomain(source: StatusSource = StatusSource.CACHE): YieldBalance {
     return YieldBalanceConverter(source = source).convert(this)!!
