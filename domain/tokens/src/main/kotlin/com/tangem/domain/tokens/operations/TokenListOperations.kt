8ded79ccd7f1ed3aad2996b474cb3a0bfdeaf15d
Tangem <git@tangem.com>
2025-07-30 17:42:54 +0400

    Updated on 2026-09-25

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/operations/TokenListOperations.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/operations/TokenListOperations.kt
@@ -7,8 +7,8 @@ import arrow.core.raise.withError
 import com.tangem.domain.models.TokensSortType
 import com.tangem.domain.models.TotalFiatBalance
 import com.tangem.domain.models.currency.CryptoCurrencyStatus
+import com.tangem.domain.models.tokenlist.TokenList
 import com.tangem.domain.models.wallet.UserWalletId
-import com.tangem.domain.tokens.model.TokenList
 import com.tangem.domain.tokens.repository.CurrenciesRepository
 import kotlinx.coroutines.flow.*
 
