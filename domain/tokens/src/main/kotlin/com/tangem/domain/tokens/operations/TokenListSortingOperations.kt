dcbf68d3f1647d7eda94bf6570139df18ea5c213
Tangem <git@tangem.com>
2025-07-30 17:42:54 +0400

    Updated on 2026-09-23

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/operations/TokenListSortingOperations.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/operations/TokenListSortingOperations.kt
@@ -12,9 +12,9 @@ import com.tangem.domain.models.TotalFiatBalance
 import com.tangem.domain.models.currency.CryptoCurrencyStatus
 import com.tangem.domain.models.network.Network
 import com.tangem.domain.models.staking.YieldBalance
+import com.tangem.domain.models.tokenlist.TokenList
+import com.tangem.domain.models.tokenlist.TokenList.GroupedByNetwork.NetworkGroup
 import com.tangem.domain.staking.utils.getTotalWithRewardsStakingBalance
-import com.tangem.domain.tokens.model.NetworkGroup
-import com.tangem.domain.tokens.model.TokenList
 import com.tangem.utils.extensions.orZero
 import java.math.BigDecimal
 
