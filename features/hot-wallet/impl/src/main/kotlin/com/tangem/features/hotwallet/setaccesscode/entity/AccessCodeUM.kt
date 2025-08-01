45be455405a754e83989484222baa1a34360d3e1
Tangem <git@tangem.com>
2025-08-01 09:53:17 +0500

    Updated on 2026-05-13

--- features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/setaccesscode/entity/AccessCodeUM.kt
+++ features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/setaccesscode/entity/AccessCodeUM.kt
@@ -5,7 +5,9 @@ import com.tangem.features.hotwallet.setaccesscode.ACCESS_CODE_LENGTH
 internal data class AccessCodeUM(
     val accessCode: String,
     val onAccessCodeChange: (String) -> Unit,
+    val isConfirmMode: Boolean,
     val buttonEnabled: Boolean,
+    val buttonInProgress: Boolean,
     val onButtonClick: () -> Unit,
 ) {
     val accessCodeLength: Int = ACCESS_CODE_LENGTH
