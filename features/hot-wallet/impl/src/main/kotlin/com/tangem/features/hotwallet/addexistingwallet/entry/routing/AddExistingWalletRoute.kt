4908285e710b27ca8c7222d423fe0ff438e6979f
Tangem <git@tangem.com>
2025-08-01 09:53:17 +0500

    Updated on 2026-02-02

--- features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/addexistingwallet/entry/routing/AddExistingWalletRoute.kt
+++ features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/addexistingwallet/entry/routing/AddExistingWalletRoute.kt
@@ -1,6 +1,7 @@
 package com.tangem.features.hotwallet.addexistingwallet.entry.routing
 
 import com.tangem.core.decompose.navigation.Route
+import com.tangem.domain.models.wallet.UserWalletId
 import kotlinx.serialization.Serializable
 
 internal sealed class AddExistingWalletRoute : Route {
@@ -12,13 +13,13 @@ internal sealed class AddExistingWalletRoute : Route {
     object Import : AddExistingWalletRoute()
 
     @Serializable
-    object BackupCompleted : AddExistingWalletRoute()
+    data class BackupCompleted(val userWalletId: UserWalletId) : AddExistingWalletRoute()
 
     @Serializable
-    object SetAccessCode : AddExistingWalletRoute()
+    data class SetAccessCode(val userWalletId: UserWalletId) : AddExistingWalletRoute()
 
     @Serializable
-    data class ConfirmAccessCode(val accessCode: String) : AddExistingWalletRoute()
+    data class ConfirmAccessCode(val userWalletId: UserWalletId, val accessCode: String) : AddExistingWalletRoute()
 
     @Serializable
     object PushNotifications : AddExistingWalletRoute()
