5ebed36ed8e419bca544cf207805945c72714311
Tangem <git@tangem.com>
2025-08-04 13:58:50 +0400

    Updated on 2025-12-05

--- domain/models/src/main/kotlin/com/tangem/domain/models/account/AccountId.kt
+++ domain/models/src/main/kotlin/com/tangem/domain/models/account/AccountId.kt
@@ -1,6 +1,7 @@
 package com.tangem.domain.models.account
 
 import com.tangem.domain.models.wallet.UserWalletId
+import kotlinx.serialization.Serializable
 
 /**
  * Represents a unique identifier for an account
@@ -8,6 +9,7 @@ import com.tangem.domain.models.wallet.UserWalletId
  * @property value        a unique string value that distinguishes this account
  * @property userWalletId the identifier of the user wallet associated with the account
  */
+@Serializable
 data class AccountId(
     val value: String,
     val userWalletId: UserWalletId,
