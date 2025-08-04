37c098fbcc72706efb04a5a8fa930f1f4d547e36
Tangem <git@tangem.com>
2025-08-04 13:42:29 +0500

    Updated on 2026-04-08

--- features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/utils/WcNotificationsFactory.kt
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/utils/WcNotificationsFactory.kt
@@ -2,15 +2,30 @@ package com.tangem.features.walletconnect.utils
 
 import com.tangem.blockchain.common.transaction.TransactionFee
 import com.tangem.common.ui.notifications.NotificationUM
+import com.tangem.core.ui.components.notifications.NotificationConfig
+import com.tangem.core.ui.extensions.resourceReference
 import com.tangem.core.ui.extensions.stringReference
 import com.tangem.domain.tokens.model.CryptoCurrencyStatus
 import com.tangem.features.send.v2.api.entity.FeeSelectorUM
 import com.tangem.features.send.v2.api.subcomponents.feeSelector.utils.FeeCalculationUtils
+import com.tangem.features.walletconnect.impl.R
 import javax.inject.Inject
 
 internal class WcNotificationsFactory @Inject constructor() {
 
-    fun createFeeExceedsBalance(
+    fun createFeeNotifications(
+        cryptoCurrencyStatus: CryptoCurrencyStatus,
+        feeSelectorUM: FeeSelectorUM?,
+        onFeeReload: () -> Unit,
+    ): NotificationUM.Info? {
+        return when (feeSelectorUM) {
+            is FeeSelectorUM.Content -> createFeeExceedsBalance(cryptoCurrencyStatus, feeSelectorUM)
+            is FeeSelectorUM.Error -> createFeeErrorNotification(onFeeReload)
+            FeeSelectorUM.Loading, null -> null
+        }
+    }
+
+    private fun createFeeExceedsBalance(
         cryptoCurrencyStatus: CryptoCurrencyStatus,
         feeSelectorUM: FeeSelectorUM?,
     ): NotificationUM.Info? {
@@ -19,6 +34,17 @@ internal class WcNotificationsFactory @Inject constructor() {
             title = stringReference("Insufficient ${cryptoCurrencyStatus.currency.name}"),
             subtitle = stringReference("Top up your balance to cover the network fee"),
         ).takeIf { isFeeExceedsBalance(cryptoCurrencyStatus = cryptoCurrencyStatus, feeSelectorUM = feeSelectorUM) }
+    }
+
+    private fun createFeeErrorNotification(onFeeReload: () -> Unit): NotificationUM.Info {
+        return NotificationUM.Info(
+            title = resourceReference(R.string.send_fee_unreachable_error_title),
+            subtitle = resourceReference(R.string.send_fee_unreachable_error_text),
+            buttonsState = NotificationConfig.ButtonsState.SecondaryButtonConfig(
+                text = resourceReference(R.string.warning_button_refresh),
+                onClick = onFeeReload,
+            ),
+        )
     }
 
     private fun isFeeExceedsBalance(
