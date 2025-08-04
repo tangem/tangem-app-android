e8e9eabc047a3411502520a41698ba82e110e50f
Tangem <git@tangem.com>
2025-08-04 11:40:43 +0500

    Updated on 2026-06-10

--- features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/connections/model/transformers/WcNetworksSelectedTransformer.kt
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/connections/model/transformers/WcNetworksSelectedTransformer.kt
@@ -7,19 +7,29 @@ import com.tangem.utils.transformer.Transformer
 internal class WcNetworksSelectedTransformer(
     private val missingNetworks: Set<Network>,
     private val requiredNetworks: Set<Network>,
+    private val availableNetworks: Set<Network>,
+    private val notAddedNetworks: Set<Network>,
     private val additionallyEnabledNetworks: Set<Network>,
 ) : Transformer<WcAppInfoUM> {
     override fun transform(prevState: WcAppInfoUM): WcAppInfoUM {
         val contentState = prevState as? WcAppInfoUM.Content ?: return prevState
         return contentState.copy(
             connectButtonConfig = contentState.connectButtonConfig.copy(
-                enabled = missingNetworks.isEmpty() &&
-                    (requiredNetworks.isNotEmpty() || additionallyEnabledNetworks.isNotEmpty()),
+                enabled = WcConnectButtonAvailabilityConverter.convert(
+                    WcConnectButtonAvailabilityConverter.Input(
+                        missingNetworks = missingNetworks,
+                        requiredNetworks = requiredNetworks,
+                        availableNetworks = availableNetworks,
+                        selectedNetworks = additionallyEnabledNetworks,
+                    ),
+                ),
             ),
             networksInfo = WcNetworksInfoConverter.convert(
                 value = WcNetworksInfoConverter.Input(
                     missingNetworks = missingNetworks,
                     requiredNetworks = requiredNetworks,
+                    availableNetworks = availableNetworks,
+                    notAddedNetworks = notAddedNetworks,
                     additionallyEnabledNetworks = additionallyEnabledNetworks,
                 ),
             ),
