97d402adcdcba3e69b8da42f6605b1bb82ec6ccf
Tangem <git@tangem.com>
2025-08-05 15:45:35 +0400

    Updated on 2026-07-23

---
+++ domain/account/src/main/java/com/tangem/domain/account/repository/AccountsCRUDRepository.kt
@@ -0,0 +1,49 @@
+package com.tangem.domain.account.repository
+
+import arrow.core.Option
+import com.tangem.domain.account.models.AccountList
+import com.tangem.domain.models.account.Account
+import com.tangem.domain.models.account.AccountId
+import com.tangem.domain.models.wallet.UserWallet
+import com.tangem.domain.models.wallet.UserWalletId
+
+/**
+ * Repository interface for performing CRUD operations on accounts
+ *
+[REDACTED_AUTHOR]
+ */
+interface AccountsCRUDRepository {
+
+    /**
+     * Retrieves a list of accounts associated with a specific user wallet
+     *
+     * @param userWalletId the unique identifier of the user wallet
+     * @return an [Option] containing the [AccountList] if found, or `Option.None` if not
+     */
+    suspend fun getAccounts(userWalletId: UserWalletId): Option<AccountList>
+
+    /**
+     * Retrieves a specific account by its unique identifier
+     *
+     * @param accountId the unique identifier of the account
+     * @return an [Option] containing the [Account.CryptoPortfolio] if found, or `Option.None` if not
+     */
+    suspend fun getAccount(accountId: AccountId): Option<Account.CryptoPortfolio>
+
+    /**
+     * Saves a list of accounts to the repository
+     *
+     * @param accountList the list of accounts to be saved.
+     */
+    @Throws
+    suspend fun saveAccounts(accountList: AccountList)
+
+    /**
+     * Retrieves a user wallet by its unique identifier
+     *
+     * @param userWalletId the unique identifier of the user wallet
+     * @return the [UserWallet] associated with the given identifier
+     */
+    @Throws
+    fun getUserWallet(userWalletId: UserWalletId): UserWallet
+}
