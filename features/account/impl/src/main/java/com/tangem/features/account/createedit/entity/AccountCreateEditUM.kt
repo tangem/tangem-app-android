b257b27177be574fab14a35452043ffe44e0b4e9
Tangem <git@tangem.com>
2025-08-06 16:08:52 +0700

    Updated on 2026-03-25

--- features/account/impl/src/main/java/com/tangem/features/account/createedit/entity/AccountCreateEditUM.kt
+++ features/account/impl/src/main/java/com/tangem/features/account/createedit/entity/AccountCreateEditUM.kt
@@ -3,40 +3,39 @@ package com.tangem.features.account.createedit.entity
 import com.tangem.core.ui.extensions.TextReference
 import com.tangem.domain.models.account.CryptoPortfolioIcon
 import kotlinx.collections.immutable.ImmutableList
-import kotlinx.collections.immutable.persistentListOf
 
 data class AccountCreateEditUM(
-    val title: TextReference = TextReference.EMPTY,
-    val account: Account = Account(),
+    val title: TextReference,
+    val account: Account,
     val colorsState: Colors,
     val iconsState: Icons,
-    val buttonState: Button = Button(),
-    val onCloseClick: () -> Unit = {},
+    val buttonState: Button,
+    val onCloseClick: () -> Unit,
 ) {
 
     data class Account(
-        val name: TextReference = TextReference.EMPTY,
-        val portfolioIcon: CryptoPortfolioIcon = CryptoPortfolioIcon.ofDefaultCustomAccount(),
-        val derivationInfo: TextReference = TextReference.EMPTY,
-        val inputPlaceholder: TextReference = TextReference.EMPTY,
-        val onNameChange: (String) -> Unit = {},
+        val name: String,
+        val portfolioIcon: CryptoPortfolioIcon,
+        val derivationInfo: TextReference,
+        val inputPlaceholder: TextReference,
+        val onNameChange: (String) -> Unit,
     )
 
     data class Colors(
         val selected: CryptoPortfolioIcon.Color,
-        val list: ImmutableList<CryptoPortfolioIcon.Color> = persistentListOf(),
-        val onColorSelect: (CryptoPortfolioIcon.Color) -> Unit = {},
+        val list: ImmutableList<CryptoPortfolioIcon.Color>,
+        val onColorSelect: (CryptoPortfolioIcon.Color) -> Unit,
     )
 
     data class Icons(
         val selected: CryptoPortfolioIcon.Icon,
-        val list: ImmutableList<CryptoPortfolioIcon.Icon> = persistentListOf(),
-        val onIconSelect: (CryptoPortfolioIcon.Icon) -> Unit = {},
+        val list: ImmutableList<CryptoPortfolioIcon.Icon>,
+        val onIconSelect: (CryptoPortfolioIcon.Icon) -> Unit,
     )
 
     data class Button(
-        val isButtonEnabled: Boolean = false,
-        val onConfirmClick: () -> Unit = {},
-        val text: TextReference = TextReference.EMPTY,
+        val isButtonEnabled: Boolean,
+        val onConfirmClick: () -> Unit,
+        val text: TextReference,
     )
 }
