f6fad0e6d18011e7f5b573f8978381b29094cbd5
Tangem <git@tangem.com>
2025-08-06 17:09:04 +0700

    Updated on 2026-08-14

---
+++ features/account/impl/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    alias(deps.plugins.android.library)
+    alias(deps.plugins.kotlin.android)
+    alias(deps.plugins.kotlin.kapt)
+    alias(deps.plugins.kotlin.serialization)
+    alias(deps.plugins.hilt.android)
+    id("configuration")
+}
+
+android {
+    namespace = "com.tangem.features.account.impl"
+}
+
+dependencies {
+    /** Api */
+    implementation(projects.features.account.api)
+
+    /** Core modules */
+    implementation(projects.core.analytics)
+    implementation(projects.core.analytics.models)
+    implementation(projects.core.utils)
+    implementation(projects.core.ui)
+    implementation(projects.core.res)
+    implementation(projects.core.decompose)
+    implementation(projects.core.navigation)
+    implementation(projects.core.datasource)
+
+    /** Domain */
+    implementation(projects.domain.models)
+
+    /** Common */
+    implementation(projects.common.ui)
+    implementation(projects.common.routing)
+
+    /** AndroidX libraries */
+    implementation(deps.androidx.core.ktx)
+    implementation(deps.lifecycle.runtime.ktx)
+
+    /** Compose libraries */
+    implementation(deps.compose.material3)
+    implementation(deps.compose.animation)
+    implementation(deps.compose.foundation)
+    implementation(deps.compose.ui)
+    implementation(deps.compose.ui.tooling)
+    implementation(deps.compose.coil)
+    implementation(deps.decompose.ext.compose)
+    implementation(deps.androidx.activity.compose)
+
+    /** Other libraries */
+    implementation(deps.arrow.core)
+    implementation(deps.kotlin.immutable.collections)
+    implementation(deps.kotlin.serialization)
+    implementation(deps.timber)
+    implementation(deps.firebase.crashlytics)
+
+    /** DI */
+    implementation(deps.hilt.android)
+    kapt(deps.hilt.kapt)
+}
