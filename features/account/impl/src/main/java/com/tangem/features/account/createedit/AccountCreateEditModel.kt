2e1a595b549ca431857fa864f9722b31809bc803
Tangem <git@tangem.com>
2025-08-06 16:08:52 +0700

    Updated on 2026-02-06

--- features/account/impl/src/main/java/com/tangem/features/account/createedit/AccountCreateEditModel.kt
+++ features/account/impl/src/main/java/com/tangem/features/account/createedit/AccountCreateEditModel.kt
@@ -9,11 +9,22 @@ import com.tangem.core.res.R
 import com.tangem.core.ui.extensions.resourceReference
 import com.tangem.core.ui.message.DialogMessage
 import com.tangem.core.ui.message.EventMessageAction
+import com.tangem.domain.account.usecase.AddCryptoPortfolioUseCase
+import com.tangem.domain.account.usecase.UpdateCryptoPortfolioUseCase
+import com.tangem.domain.models.account.AccountName
+import com.tangem.domain.models.account.CryptoPortfolioIcon
 import com.tangem.features.account.AccountCreateEditComponent
 import com.tangem.features.account.createedit.entity.AccountCreateEditUM
+import com.tangem.features.account.createedit.entity.AccountCreateEditUMBuilder
+import com.tangem.features.account.createedit.entity.AccountCreateEditUMBuilder.Companion.portfolioIcon
+import com.tangem.features.account.createedit.entity.AccountCreateEditUMBuilder.Companion.updateButton
+import com.tangem.features.account.createedit.entity.AccountCreateEditUMBuilder.Companion.updateColorSelect
+import com.tangem.features.account.createedit.entity.AccountCreateEditUMBuilder.Companion.updateIconSelect
+import com.tangem.features.account.createedit.entity.AccountCreateEditUMBuilder.Companion.updateName
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @ModelScoped
@@ -22,24 +33,24 @@ internal class AccountCreateEditModel @Inject constructor(
     private val messageSender: UiMessageSender,
     private val router: Router,
     override val dispatchers: CoroutineDispatcherProvider,
+    private val updateCryptoPortfolio: UpdateCryptoPortfolioUseCase,
+    private val addCryptoPortfolio: AddCryptoPortfolioUseCase,
 ) : Model() {
 
     private val params = paramsContainer.require<AccountCreateEditComponent.Params>()
+    private val umBuilder = AccountCreateEditUMBuilder(params)
 
-    val uiState: StateFlow<AccountCreateEditUM>
-    field = MutableStateFlow(TODO())
+    val uiState: StateFlow<AccountCreateEditUM> get() = _uiState
+    private val _uiState = MutableStateFlow(value = getInitialState())
 
-    init {
-        params
-    }
-
-    fun unsaveChangeDialog() {
-        val firstAction = EventMessageAction(
+    private fun unsaveChangeDialog() {
+        val secondAction = EventMessageAction(
             title = resourceReference(R.string.account_unsaved_dialog_action_first),
             onClick = {},
         )
-        val secondAction = EventMessageAction(
+        val firstAction = EventMessageAction(
             title = resourceReference(R.string.account_unsaved_dialog_action_second),
+            warning = true,
             onClick = { router.pop() },
         )
         messageSender.send(
@@ -49,6 +60,82 @@ internal class AccountCreateEditModel @Inject constructor(
                 firstActionBuilder = { firstAction },
                 secondActionBuilder = { secondAction },
             ),
+        )
+    }
+
+    private fun onConfirmClick() = modelScope.launch {
+        when (params) {
+            is AccountCreateEditComponent.Params.Create -> createNewCryptoPortfolio(params)
+            is AccountCreateEditComponent.Params.Edit -> editCryptoPortfolio(params)
+        }
+    }
+
+    private suspend fun createNewCryptoPortfolio(params: AccountCreateEditComponent.Params.Create) {
+        val state = uiState.value
+        val name = AccountName(state.account.name).getOrNull() ?: return
+        val icon = state.account.portfolioIcon
+        addCryptoPortfolio(
+            userWalletId = params.userWalletId,
+            accountName = name,
+            icon = icon,
+            derivationIndex = 0, // todo account
+        )
+    }
+
+    private suspend fun editCryptoPortfolio(params: AccountCreateEditComponent.Params.Edit) {
+        val state = uiState.value
+        val name = AccountName(state.account.name).getOrNull() ?: return
+        val icon = state.account.portfolioIcon
+        val isNewName = name != params.account.name
+        val isNewIcon = icon != params.account.portfolioIcon
+        updateCryptoPortfolio(
+            icon = if (isNewIcon) icon else null,
+            name = if (isNewName) name else null,
+            accountId = params.account.accountId,
+        )
+    }
+
+    private fun onCloseClick() = unsaveChangeDialog()
+
+    private fun onIconSelect(icon: CryptoPortfolioIcon.Icon) {
+        _uiState.value = uiState.value
+            .updateIconSelect(icon)
+            .validateNewState()
+    }
+
+    private fun onColorSelect(color: CryptoPortfolioIcon.Color) {
+        _uiState.value = uiState.value
+            .updateColorSelect(color)
+            .validateNewState()
+    }
+
+    private fun onNameChange(name: String) {
+        _uiState.value = uiState.value
+            .updateName(name)
+            .validateNewState()
+    }
+
+    private fun AccountCreateEditUM.validateNewState(): AccountCreateEditUM {
+        val isValidName = AccountName(this.account.name).isRight()
+        val isAvailableForConfirm = when (params) {
+            is AccountCreateEditComponent.Params.Create -> isValidName
+            is AccountCreateEditComponent.Params.Edit -> {
+                val isNewName = this.account.name != params.account.name.value
+                val isNewIcon = this.account.portfolioIcon != params.account.portfolioIcon
+                isValidName && isNewName && isNewIcon
+            }
+        }
+        return this.updateButton(isButtonEnabled = isAvailableForConfirm)
+    }
+
+    private fun getInitialState(): AccountCreateEditUM {
+        return AccountCreateEditUM(
+            title = umBuilder.toolbarTitle,
+            account = umBuilder.initAccountUM(::onNameChange),
+            colorsState = umBuilder.initColorsUM(::onColorSelect),
+            iconsState = umBuilder.initIconsUM(::onIconSelect),
+            buttonState = umBuilder.initButtonUM(::onConfirmClick),
+            onCloseClick = ::onCloseClick,
         )
     }
 }
