4aee911fc5f4609e5b1ca56e1d0176278b989664
Tangem <git@tangem.com>
2025-08-06 18:05:37 +0400

    Updated on 2026-06-27

--- domain/account/src/main/java/com/tangem/domain/account/usecase/AddCryptoPortfolioUseCase.kt
+++ domain/account/src/main/java/com/tangem/domain/account/usecase/AddCryptoPortfolioUseCase.kt
@@ -1,9 +1,13 @@
 package com.tangem.domain.account.usecase
 
 import arrow.core.Either
+import arrow.core.Option
+import arrow.core.getOrElse
+import arrow.core.raise.Raise
+import arrow.core.raise.catch
 import arrow.core.raise.either
 import com.tangem.domain.account.models.AccountList
-import com.tangem.domain.account.usecase.AddCryptoPortfolioUseCase.Error.AccountCreation
+import com.tangem.domain.account.repository.AccountsCRUDRepository
 import com.tangem.domain.models.TokensGroupType
 import com.tangem.domain.models.TokensSortType
 import com.tangem.domain.models.account.Account
@@ -14,19 +18,56 @@ import com.tangem.domain.models.wallet.UserWalletId
 import java.util.UUID
 
 /**
+ * Use case for adding a new crypto portfolio account
+ *
+ * @property crudRepository the repository used for performing CRUD operations on accounts
+ *
 [REDACTED_AUTHOR]
  */
-class AddCryptoPortfolioUseCase {
+class AddCryptoPortfolioUseCase(
+    private val crudRepository: AccountsCRUDRepository,
+) {
 
+    /**
+     * Adds a new crypto portfolio account to the repository
+     *
+     * @param userWalletId    the unique identifier of the user wallet
+     * @param accountName     the name of the new account
+     * @param icon            the icon representing the new account
+     * @param derivationIndex the derivation index for the account
+     *
+
+     */
     suspend operator fun invoke(
         userWalletId: UserWalletId,
         accountName: AccountName,
         icon: CryptoPortfolioIcon,
         derivationIndex: Int,
     ): Either<Error, Account.CryptoPortfolio> = either {
-        Account.CryptoPortfolio(
+        val newAccount = createAccount(userWalletId, accountName, icon, derivationIndex)
+
+        val accountList = getAccountList(userWalletId = userWalletId).getOrElse {
+            createNewAccountList(userWalletId = userWalletId)
+        }
+
+        val updatedAccounts = (accountList + newAccount)
+            .getOrElse { raise(Error.AccountListRequirementsNotMet(it)) }
+
+        saveAccounts(updatedAccounts)
+
+        newAccount
+    }
+
+    private fun Raise<Error>.createAccount(
+        userWalletId: UserWalletId,
+        accountName: AccountName,
+        icon: CryptoPortfolioIcon,
+        derivationIndex: Int,
+    ): Account.CryptoPortfolio {
+        // TODO: [REDACTED_JIRA]
+        return Account.CryptoPortfolio(
             accountId = AccountId(userWalletId = userWalletId, value = UUID.randomUUID().toString()),
-            name = accountName.value,
+            accountName = accountName,
             accountIcon = icon,
             derivationIndex = derivationIndex,
             isArchived = false,
@@ -36,20 +77,56 @@ class AddCryptoPortfolioUseCase {
                 groupType = TokensGroupType.NONE,
             ),
         )
-            .mapLeft(::AccountCreation)
-            .bind()
-
-        // TODO: [REDACTED_JIRA]
-        // Save to local store
-        // Save to backend (tokens migration) – asynchronously
+            .getOrElse { raise(Error.AccountCreation(it)) }
     }
 
+    private suspend fun Raise<Error>.getAccountList(userWalletId: UserWalletId): Option<AccountList> {
+        return catch(
+            block = { crudRepository.getAccounts(userWalletId = userWalletId) },
+            catch = { raise(Error.DataOperationFailed(cause = it)) },
+        )
+    }
+
+    private fun Raise<Error>.createNewAccountList(userWalletId: UserWalletId): AccountList {
+        val userWallet = catch(
+            block = { crudRepository.getUserWallet(userWalletId = userWalletId) },
+            catch = { raise(Error.DataOperationFailed(cause = it)) },
+        )
+
+        return AccountList.createEmpty(userWallet = userWallet)
+    }
+
+    private suspend fun Raise<Error>.saveAccounts(accountList: AccountList) {
+        catch(
+            block = { crudRepository.saveAccounts(accountList) },
+            catch = { raise(Error.DataOperationFailed(cause = it)) },
+        )
+    }
+
+    /**
+     * Represents possible errors that can occur during the add operation
+     */
     sealed interface Error {
 
+        /**
+         * Error indicating that the account creation failed
+         *
+         * @property cause the underlying cause of the failure
+         */
         data class AccountCreation(val cause: Account.CryptoPortfolio.Error) : Error
 
-        data class AccountListRequirementsNotMet(val cause: AccountList.Error) : Error
+        /**
+         * Error indicating that the account list requirements were not met.
+         *
+         * @property cause the underlying cause of the error
+         */
+        data class AccountListRequirementsNotMet(val cause: AccountList.Error) : Error {
+            override fun toString(): String = "Account list requirements not met: $cause"
+        }
 
-        data object DataOperationFailed : Error
+        /** Error indicating that a data operation failed */
+        data class DataOperationFailed(val cause: Throwable) : Error {
+            override fun toString(): String = "Data operation failed: ${cause.message ?: "Unknown error"}"
+        }
     }
 }
