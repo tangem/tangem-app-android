6178034e714a6730329fa34b83ea8d99a853a45e
Tangem <git@tangem.com>
2025-08-07 20:04:35 +0500

    Updated on 2026-08-14

--- features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/walletbackup/model/WalletBackupModel.kt
+++ features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/walletbackup/model/WalletBackupModel.kt
@@ -4,9 +4,13 @@ import com.tangem.core.decompose.di.ModelScoped
 import com.tangem.core.decompose.model.Model
 import com.tangem.core.decompose.model.ParamsContainer
 import com.tangem.core.decompose.navigation.Router
+import com.tangem.core.ui.R
+import com.tangem.core.ui.components.label.entity.LabelStyle
+import com.tangem.core.ui.components.label.entity.LabelUM
+import com.tangem.core.ui.extensions.resourceReference
+import com.tangem.domain.models.wallet.UserWallet
 import com.tangem.domain.wallets.usecase.GetUserWalletUseCase
 import com.tangem.features.hotwallet.WalletBackupComponent
-import com.tangem.features.hotwallet.walletbackup.entity.BackupStatus
 import com.tangem.features.hotwallet.walletbackup.entity.WalletBackupUM
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import kotlinx.coroutines.flow.*
@@ -26,8 +30,14 @@ internal class WalletBackupModel @Inject constructor(
     field = MutableStateFlow(
         WalletBackupUM(
             onBackClick = { router.pop() },
-            recoveryPhraseStatus = BackupStatus.NoBackup,
-            googleDriveStatus = BackupStatus.ComingSoon,
+            recoveryPhraseStatus = LabelUM(
+                text = resourceReference(R.string.hw_backup_no_backup),
+                style = LabelStyle.WARNING,
+            ),
+            googleDriveStatus = LabelUM(
+                text = resourceReference(R.string.common_coming_soon),
+                style = LabelStyle.REGULAR,
+            ),
             onRecoveryPhraseClick = { },
             onGoogleDriveClick = { },
         ),
@@ -37,18 +47,38 @@ internal class WalletBackupModel @Inject constructor(
         getWalletUseCase.invokeFlow(params.userWalletId)
             .map { it.getOrNull() }
             .distinctUntilChanged()
+            .filterNotNull()
             .onEach {
-                updateBackupStatuses()
+                updateBackupStatuses(it)
             }
             .launchIn(modelScope)
     }
 
-    private fun updateBackupStatuses() {
+    private fun updateBackupStatuses(userWallet: UserWallet) {
         uiState.update { currentState ->
-            currentState.copy(
-                recoveryPhraseStatus = BackupStatus.NoBackup,
-                googleDriveStatus = BackupStatus.ComingSoon,
-            )
+            if (userWallet is UserWallet.Hot) {
+                currentState.updateBackupStatusesHotWallet(userWallet)
+            } else {
+                currentState
+            }
         }
     }
+
+    private fun WalletBackupUM.updateBackupStatusesHotWallet(userWallet: UserWallet.Hot): WalletBackupUM = copy(
+        recoveryPhraseStatus = if (userWallet.backedUp) {
+            LabelUM(
+                text = resourceReference(R.string.common_done),
+                style = LabelStyle.ACCENT,
+            )
+        } else {
+            LabelUM(
+                text = resourceReference(R.string.hw_backup_no_backup),
+                style = LabelStyle.WARNING,
+            )
+        },
+        googleDriveStatus = LabelUM(
+            text = resourceReference(R.string.common_coming_soon),
+            style = LabelStyle.REGULAR,
+        ),
+    )
 }
