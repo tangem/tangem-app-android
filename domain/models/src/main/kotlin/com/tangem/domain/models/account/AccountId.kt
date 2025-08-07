125b6803ea6a8e90872b183a8af73cded9b325a6
Tangem <git@tangem.com>
2025-08-07 18:58:42 +0400

    Updated on 2026-06-02

--- domain/models/src/main/kotlin/com/tangem/domain/models/account/AccountId.kt
+++ domain/models/src/main/kotlin/com/tangem/domain/models/account/AccountId.kt
@@ -1,7 +1,10 @@
 package com.tangem.domain.models.account
 
+import com.tangem.common.extensions.toByteArray
 import com.tangem.domain.models.wallet.UserWalletId
+import com.tangem.utils.extensions.toHexString
 import kotlinx.serialization.Serializable
+import java.security.MessageDigest
 
 /**
  * Represents a unique identifier for an account
@@ -10,7 +13,26 @@ import kotlinx.serialization.Serializable
  * @property userWalletId the identifier of the user wallet associated with the account
  */
 @Serializable
-data class AccountId(
+data class AccountId private constructor(
     val value: String,
     val userWalletId: UserWalletId,
-)
+) {
+
+    companion object {
+
+        private val sha256Digest: MessageDigest by lazy { MessageDigest.getInstance("SHA-256") }
+
+        /**
+         * Creates a unique account identifier for a crypto portfolio
+         *
+         * @param userWalletId    the identifier of the user wallet
+         * @param derivationIndex the derivation index used to generate the identifier
+         */
+        fun forCryptoPortfolio(userWalletId: UserWalletId, derivationIndex: DerivationIndex): AccountId {
+            val input = userWalletId.value + derivationIndex.value.toByteArray()
+            val value = sha256Digest.digest(input).toHexString()
+
+            return AccountId(value = value, userWalletId = userWalletId)
+        }
+    }
+}
