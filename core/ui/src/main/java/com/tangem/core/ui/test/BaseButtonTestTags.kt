564c3d3dd474a698b195914f4b2eab2985357f42
Tangem <git@tangem.com>
2025-08-08 12:36:38 +0300

    release/5.27.0

--- core/ui/src/main/java/com/tangem/core/ui/test/BaseButtonTestTags.kt
+++ core/ui/src/main/java/com/tangem/core/ui/test/BaseButtonTestTags.kt
@@ -2,4 +2,6 @@ package com.tangem.core.ui.test
 
 object BaseButtonTestTags {
     const val BUTTON = "BASE_BUTTON"
+    const val ICON = "BASE_BUTTON_ICON"
+    const val TEXT = "BASE_BUTTON_TEXT"
 }
