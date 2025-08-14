a89c33ed43e6b3e4ec0a5865d0b4896536a97e65
Tangem <git@tangem.com>
2025-08-14 14:47:48 +0300

    Updated on 2025-12-30

--- app/src/main/java/com/tangem/tap/di/data/UserWalletsStoreModule.kt
+++ app/src/main/java/com/tangem/tap/di/data/UserWalletsStoreModule.kt
@@ -2,7 +2,10 @@ package com.tangem.tap.di.data
 
 import com.tangem.datasource.local.userwallet.UserWalletsStore
 import com.tangem.domain.wallets.legacy.UserWalletsListManager
+import com.tangem.domain.core.wallets.UserWalletsListRepository
+import com.tangem.features.hotwallet.HotWalletFeatureToggles
 import com.tangem.tap.data.RuntimeUserWalletsStore
+import com.tangem.tap.data.UserWalletsStoreRepositoryProxy
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -15,7 +18,15 @@ internal object UserWalletsStoreModule {
 
     @Provides
     @Singleton
-    fun provideUserWalletsStore(userWalletsListManager: UserWalletsListManager): UserWalletsStore {
-        return RuntimeUserWalletsStore(userWalletsListManager = userWalletsListManager)
+    fun provideUserWalletsStore(
+        userWalletsListManager: UserWalletsListManager,
+        userWalletsListRepository: UserWalletsListRepository,
+        hotWalletFeatureToggles: HotWalletFeatureToggles,
+    ): UserWalletsStore {
+        return if (hotWalletFeatureToggles.isHotWalletEnabled) {
+            UserWalletsStoreRepositoryProxy(userWalletsListRepository)
+        } else {
+            RuntimeUserWalletsStore(userWalletsListManager = userWalletsListManager)
+        }
     }
 }
