9cc60bcc6813c2079eb7ebfc93edbf48dfa0e964
Tangem <git@tangem.com>
2025-08-14 14:47:48 +0300

    Updated on 2026-09-25

--- domain/wallets/src/main/java/com/tangem/domain/wallets/usecase/GetSelectedWalletUseCase.kt
+++ domain/wallets/src/main/java/com/tangem/domain/wallets/usecase/GetSelectedWalletUseCase.kt
@@ -5,7 +5,9 @@ import arrow.core.raise.either
 import com.tangem.domain.wallets.legacy.UserWalletsListManager
 import com.tangem.domain.wallets.models.GetUserWalletError
 import com.tangem.domain.models.wallet.UserWallet
+import com.tangem.domain.core.wallets.UserWalletsListRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filterNotNull
 
 /**
  * Use case for getting flow of selected wallet.
@@ -14,12 +16,32 @@ import kotlinx.coroutines.flow.Flow
  *
 [REDACTED_AUTHOR]
  */
-class GetSelectedWalletUseCase(private val userWalletsListManager: UserWalletsListManager) {
+@Deprecated("You should provide the selected wallet via routing parameters due to the scalability of the features")
+class GetSelectedWalletUseCase(
+    private val userWalletsListManager: UserWalletsListManager,
+    private val userWalletsListRepository: UserWalletsListRepository,
+    private val useNewRepository: Boolean = false,
+) {
 
     @Deprecated("You should provide the selected wallet via routing parameters due to the scalability of the features")
     operator fun invoke(): Either<GetUserWalletError, Flow<UserWallet>> {
         return either {
-            userWalletsListManager.selectedUserWallet
+            if (useNewRepository) {
+                userWalletsListRepository.selectedUserWallet.filterNotNull()
+            } else {
+                userWalletsListManager.selectedUserWallet
+            }
+        }
+    }
+
+    @Deprecated("You should provide the selected wallet via routing parameters due to the scalability of the features")
+    fun sync(): Either<GetUserWalletError, UserWallet?> {
+        return either {
+            if (useNewRepository) {
+                userWalletsListRepository.selectedUserWallet.value
+            } else {
+                userWalletsListManager.selectedUserWalletSync
+            }
         }
     }
 }
