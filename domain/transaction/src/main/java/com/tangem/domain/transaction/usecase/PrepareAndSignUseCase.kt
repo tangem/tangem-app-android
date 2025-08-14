f94ea9a6f4773f9e0d3c179935eecef4f1cf9ea9
Tangem <git@tangem.com>
2025-08-14 16:31:49 +0500

    Updated on 2026-07-23

---
+++ domain/transaction/src/main/java/com/tangem/domain/transaction/usecase/PrepareAndSignUseCase.kt
@@ -0,0 +1,70 @@
+package com.tangem.domain.transaction.usecase
+
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
+import com.tangem.blockchain.common.TransactionData
+import com.tangem.blockchain.common.TransactionSigner
+import com.tangem.blockchain.extensions.Result
+import com.tangem.domain.card.common.TapWorkarounds.isTangemTwins
+import com.tangem.domain.card.models.TwinKey
+import com.tangem.domain.card.repository.CardSdkConfigRepository
+import com.tangem.domain.models.network.Network
+import com.tangem.domain.models.wallet.UserWallet
+import com.tangem.domain.models.wallet.requireColdWallet
+import com.tangem.domain.transaction.TransactionRepository
+import com.tangem.domain.transaction.error.SendTransactionError
+
+class PrepareAndSignUseCase(
+    private val transactionRepository: TransactionRepository,
+    private val cardSdkConfigRepository: CardSdkConfigRepository,
+) {
+
+    suspend operator fun invoke(
+        transactionData: TransactionData,
+        userWallet: UserWallet,
+        network: Network,
+    ): Either<SendTransactionError, ByteArray> {
+        val signer = createSigner(userWallet)
+        val result = transactionRepository.prepareAndSign(
+            transactionData = transactionData,
+            userWalletId = userWallet.walletId,
+            network = network,
+            signer = signer,
+        )
+        return when (result) {
+            is Result.Failure -> SendTransactionUseCase.handleError(result).left()
+            is Result.Success -> result.data.right()
+        }
+    }
+
+    suspend operator fun invoke(
+        transactionData: List<TransactionData>,
+        userWallet: UserWallet,
+        network: Network,
+    ): Either<SendTransactionError, List<ByteArray>> {
+        val signer = createSigner(userWallet)
+        val result = transactionRepository.prepareAndSignMultiple(
+            transactionData = transactionData,
+            userWalletId = userWallet.walletId,
+            network = network,
+            signer = signer,
+        )
+        return when (result) {
+            is Result.Failure -> SendTransactionUseCase.handleError(result).left()
+            is Result.Success -> result.data.right()
+        }
+    }
+
+    private fun createSigner(userWallet: UserWallet): TransactionSigner {
+        userWallet.requireColdWallet() // TODO [REDACTED_TASK_KEY]
+        val card = userWallet.scanResponse.card
+        val isCardNotBackedUp = card.backupStatus?.isActive != true && !card.isTangemTwins
+
+        val signer = cardSdkConfigRepository.getCommonSigner(
+            cardId = card.cardId.takeIf { isCardNotBackedUp },
+            twinKey = TwinKey.getOrNull(scanResponse = userWallet.scanResponse),
+        )
+        return signer
+    }
+}
