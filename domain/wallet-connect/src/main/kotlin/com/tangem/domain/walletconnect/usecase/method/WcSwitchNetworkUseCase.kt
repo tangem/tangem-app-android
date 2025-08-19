1317e793dd89f54e49430c35b333cbd250c9a5cd
Tangem <git@tangem.com>
2025-08-19 14:15:30 +0700

    Updated on 2025-10-13

--- domain/wallet-connect/src/main/kotlin/com/tangem/domain/walletconnect/usecase/method/WcSwitchNetworkUseCase.kt
+++ domain/wallet-connect/src/main/kotlin/com/tangem/domain/walletconnect/usecase/method/WcSwitchNetworkUseCase.kt
@@ -3,17 +3,15 @@ package com.tangem.domain.walletconnect.usecase.method
 import arrow.core.Either
 import com.tangem.domain.models.network.Network
 import com.tangem.domain.walletconnect.model.HandleMethodError
-import com.tangem.domain.walletconnect.model.WcRequestError
 
-interface WcAddNetworkUseCase :
+interface WcSwitchNetworkUseCase :
     WcMethodUseCase,
     WcMethodContext {
 
-    suspend operator fun invoke(): Either<HandleMethodError, AddNetwork>
-    suspend fun approve(): Either<WcRequestError, String>
+    suspend operator fun invoke(): Either<HandleMethodError, SwitchNetwork>
     fun reject()
 
-    data class AddNetwork(
+    data class SwitchNetwork(
         val network: Network,
     )
 }
