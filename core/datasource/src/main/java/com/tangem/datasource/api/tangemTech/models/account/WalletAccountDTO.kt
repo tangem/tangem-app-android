d29d5d97586f04ffd8c0816acda38923bf3805e1
Tangem <git@tangem.com>
2025-08-19 11:40:46 +0400

    Updated on 2025-09-15

---
+++ core/datasource/src/main/java/com/tangem/datasource/api/tangemTech/models/account/WalletAccountDTO.kt
@@ -0,0 +1,17 @@
+package com.tangem.datasource.api.tangemTech.models.account
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import com.tangem.datasource.api.tangemTech.models.UserTokensResponse
+
+@JsonClass(generateAdapter = true)
+data class WalletAccountDTO(
+    @Json(name = "id") val id: String,
+    @Json(name = "name") val name: String,
+    @Json(name = "derivation") val derivationIndex: Int,
+    @Json(name = "icon") val icon: String,
+    @Json(name = "iconColor") val iconColor: String,
+    @Json(name = "tokens") val tokens: List<UserTokensResponse.Token>? = null,
+    @Json(name = "totalTokens") val totalTokens: Int? = null,
+    @Json(name = "totalNetworks") val totalNetworks: Int? = null,
+)
