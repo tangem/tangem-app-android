a23bca27138db284f9fb94e45effea755d07de6f
Tangem <git@tangem.com>
2025-08-19 18:01:55 +0700

    Updated on 2026-02-06

--- domain/wallet-connect/models/src/main/java/com/tangem/domain/walletconnect/model/sdkcopy/WcSdkSession.kt
+++ domain/wallet-connect/models/src/main/java/com/tangem/domain/walletconnect/model/sdkcopy/WcSdkSession.kt
@@ -6,4 +6,12 @@ package com.tangem.domain.walletconnect.model.sdkcopy
 data class WcSdkSession(
     val topic: String,
     val appMetaData: WcAppMetaData,
-)
+    val namespaces: Map<String, Session>,
+) {
+    data class Session(
+        val chains: List<String>,
+        val accounts: List<String>,
+        val methods: List<String>,
+        val events: List<String>,
+    )
+}
