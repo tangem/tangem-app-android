0e7f4e1f92dca659ca006790a2f8ba7271c5d527
Tangem <git@tangem.com>
2025-08-20 15:25:19 +0300

    Updated on 2026-07-01

--- app/src/androidTest/kotlin/com/tangem/screens/TokenDetailsPageObject.kt
+++ app/src/androidTest/kotlin/com/tangem/screens/TokenDetailsPageObject.kt
@@ -4,21 +4,77 @@ import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.SemanticsMatcher
 import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
 import com.tangem.common.BaseTestCase
+import com.tangem.core.ui.test.BaseButtonTestTags
 import com.tangem.common.utils.LazyListItemNode
 import com.tangem.core.ui.test.TokenDetailsScreenTestTags
-import com.tangem.core.ui.utils.LazyListItemPositionSemantics
 import com.tangem.features.tokendetails.impl.R
+import com.tangem.core.ui.utils.LazyListItemPositionSemantics
 import io.github.kakaocup.compose.node.element.ComposeScreen
 import io.github.kakaocup.compose.node.element.ComposeScreen.Companion.onComposeScreen
 import io.github.kakaocup.compose.node.element.KNode
-import io.github.kakaocup.compose.node.element.lazylist.KLazyListNode
 import io.github.kakaocup.kakao.common.utilities.getResourceString
+import io.github.kakaocup.compose.node.element.lazylist.KLazyListNode
 
 class TokenDetailsPageObject(semanticsProvider: SemanticsNodeInteractionsProvider) :
     ComposeScreen<TokenDetailsPageObject>(semanticsProvider = semanticsProvider) {
 
     val screenContainer: KNode = child {
         hasTestTag(TokenDetailsScreenTestTags.SCREEN_CONTAINER)
+    }
+
+    val availableStakingBlock: KNode = child {
+        hasTestTag(TokenDetailsScreenTestTags.STAKING_AVAILABLE_BLOCK)
+        useUnmergedTree = true
+    }
+
+    val stakingBlock: KNode = child {
+        hasTestTag(TokenDetailsScreenTestTags.STAKING_BLOCK)
+        useUnmergedTree = true
+    }
+
+    val availableStakingBlockTitle: KNode = child {
+        hasTestTag(TokenDetailsScreenTestTags.STAKING_SERVICE_TITLE)
+        useUnmergedTree = true
+    }
+
+    val availableStakingBlockText: KNode = child {
+        hasTestTag(TokenDetailsScreenTestTags.STAKING_SERVICE_TEXT)
+        useUnmergedTree = true
+    }
+
+    val availableStakingBlockCurrencyIcon: KNode = child {
+        hasTestTag(TokenDetailsScreenTestTags.STAKING_CURRENCY_ICON)
+        useUnmergedTree = true
+    }
+
+    val stakeButton: KNode = child {
+        hasTestTag(BaseButtonTestTags.TEXT)
+        hasText(getResourceString(R.string.common_stake))
+        useUnmergedTree = true
+    }
+
+    val stakingFiatAmount: KNode = child {
+        hasTestTag(TokenDetailsScreenTestTags.STAKING_FIAT_AMOUNT)
+        useUnmergedTree = true
+    }
+
+    val stakingDot: KNode = child {
+        hasTestTag(TokenDetailsScreenTestTags.STAKING_DOT)
+        useUnmergedTree = true
+    }
+
+    val stakingTokenAmount: KNode = child {
+        hasTestTag(TokenDetailsScreenTestTags.STAKING_TOKEN_AMOUNT)
+        useUnmergedTree = true
+    }
+
+    val stakingChevronIcon: KNode = child {
+        hasTestTag(TokenDetailsScreenTestTags.STAKING_CHEVRON_ICON)
+        useUnmergedTree = true
+    }
+
+    val stakingTitle: KNode = child {
+        hasText(getResourceString(R.string.staking_native))
     }
 
     val title: KNode = child {
