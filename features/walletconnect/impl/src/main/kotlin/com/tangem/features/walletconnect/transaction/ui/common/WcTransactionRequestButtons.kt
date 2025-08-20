df564957066d8b21c4e06c330d1ac694162825e9
Tangem <git@tangem.com>
2025-08-20 14:20:25 +0700

    Updated on 2026-04-15

--- features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/ui/common/WcTransactionRequestButtons.kt
+++ features/walletconnect/impl/src/main/kotlin/com/tangem/features/walletconnect/transaction/ui/common/WcTransactionRequestButtons.kt
@@ -2,6 +2,7 @@ package com.tangem.features.walletconnect.transaction.ui.common
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -25,6 +26,73 @@ internal fun WcTransactionRequestButtons(
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
 ) {
+    WcCommonButtons(
+        onDismiss = onDismiss,
+        modifier = modifier,
+        primaryButton = {
+            // Before change, make sure you are align with WcSendTransactionModel::onSign
+            when (validationResult) {
+                ValidationResult.UNSAFE,
+                ValidationResult.WARNING,
+                -> PrimaryButton(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f),
+                    text = stringResourceSafe(R.string.common_continue),
+                    onClick = onClickActiveButton,
+                    showProgress = isLoading,
+                    enabled = enabled,
+                )
+                ValidationResult.SAFE,
+                ValidationResult.FAILED_TO_VALIDATE,
+                null,
+                -> PrimaryButtonIconEnd(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f),
+                    text = activeButtonText.resolveReference(),
+                    onClick = onClickActiveButton,
+                    iconResId = R.drawable.ic_tangem_24,
+                    showProgress = isLoading,
+                    enabled = enabled,
+                )
+            }
+        },
+    )
+}
+
+@Composable
+internal fun WcSimpleConfirmButtons(
+    activeButtonText: TextReference,
+    isLoading: Boolean,
+    onDismiss: () -> Unit,
+    onClickActiveButton: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+) {
+    WcCommonButtons(
+        onDismiss = onDismiss,
+        modifier = modifier,
+        primaryButton = {
+            PrimaryButton(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                text = activeButtonText.resolveReference(),
+                onClick = onClickActiveButton,
+                showProgress = isLoading,
+                enabled = enabled,
+            )
+        },
+    )
+}
+
+@Composable
+internal fun WcCommonButtons(
+    onDismiss: () -> Unit,
+    modifier: Modifier = Modifier,
+    primaryButton: @Composable RowScope.() -> Unit,
+) {
     Row(modifier = modifier, horizontalArrangement = Arrangement.spacedBy(TangemTheme.dimens.spacing8)) {
         SecondaryButton(
             modifier = Modifier
@@ -33,32 +101,6 @@ internal fun WcTransactionRequestButtons(
             text = stringResourceSafe(R.string.common_cancel),
             onClick = onDismiss,
         )
-        // Before change, make sure you are align with WcSendTransactionModel::onSign
-        when (validationResult) {
-            ValidationResult.UNSAFE,
-            ValidationResult.WARNING,
-            -> PrimaryButton(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f),
-                text = stringResourceSafe(R.string.common_continue),
-                onClick = onClickActiveButton,
-                showProgress = isLoading,
-                enabled = enabled,
-            )
-            ValidationResult.SAFE,
-            ValidationResult.FAILED_TO_VALIDATE,
-            null,
-            -> PrimaryButtonIconEnd(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f),
-                text = activeButtonText.resolveReference(),
-                onClick = onClickActiveButton,
-                iconResId = R.drawable.ic_tangem_24,
-                showProgress = isLoading,
-                enabled = enabled,
-            )
-        }
+        primaryButton()
     }
 }
