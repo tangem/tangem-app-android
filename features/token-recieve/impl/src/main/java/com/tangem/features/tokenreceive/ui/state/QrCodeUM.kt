2c9b4133d00eb6440d8dff537b6f7e61fe6d37ca
Tangem <git@tangem.com>
2025-08-20 12:45:49 +0200

    Updated on 2026-08-19

---
+++ features/token-recieve/impl/src/main/java/com/tangem/features/tokenreceive/ui/state/QrCodeUM.kt
@@ -0,0 +1,11 @@
+package com.tangem.features.tokenreceive.ui.state
+
+import com.tangem.core.ui.extensions.TextReference
+
+internal data class QrCodeUM(
+    val onCopyClick: (String) -> Unit,
+    val onShareClick: (String) -> Unit,
+    val addressName: TextReference,
+    val addressValue: String,
+    val network: String,
+)
