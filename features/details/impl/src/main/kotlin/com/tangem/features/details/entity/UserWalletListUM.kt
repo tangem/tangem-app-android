90b9941d419b8d5b8aaea9da5639794a38107f9d
Tangem <git@tangem.com>
2025-08-21 17:09:31 +0500

    Updated on 2026-09-01

--- features/details/impl/src/main/kotlin/com/tangem/features/details/entity/UserWalletListUM.kt
+++ features/details/impl/src/main/kotlin/com/tangem/features/details/entity/UserWalletListUM.kt
@@ -2,6 +2,7 @@ package com.tangem.features.details.entity
 
 import androidx.compose.runtime.Immutable
 import com.tangem.common.ui.userwallet.state.UserWalletItemUM
+import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfig
 import com.tangem.core.ui.extensions.TextReference
 import kotlinx.collections.immutable.ImmutableList
 
@@ -11,4 +12,5 @@ internal data class UserWalletListUM(
     val isWalletSavingInProgress: Boolean,
     val addNewWalletText: TextReference,
     val onAddNewWalletClick: () -> Unit,
+    val addWalletBottomSheet: TangemBottomSheetConfig = TangemBottomSheetConfig.Empty,
 )
