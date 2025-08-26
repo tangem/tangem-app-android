bc019672faee55478dac072b2d9ad3fe373b46f5
Tangem <git@tangem.com>
2025-08-26 21:50:18 +0700

    Updated on 2026-09-25

--- domain/wallet-connect/models/src/main/java/com/tangem/domain/walletconnect/model/WcEthSignTypedDataParams.kt
+++ domain/wallet-connect/models/src/main/java/com/tangem/domain/walletconnect/model/WcEthSignTypedDataParams.kt
@@ -5,60 +5,6 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class WcEthSignTypedDataParams(
-    @Json(name = "domain")
-    val domain: Domain?,
     @Json(name = "message")
-    val message: Message?,
-    @Json(name = "primaryType")
-    val primaryType: String?,
-    @Json(name = "types")
-    val types: Map<String, List<Types.Type>>,
-) {
-    @JsonClass(generateAdapter = true)
-    data class Domain(
-        @Json(name = "chainId")
-        val chainId: Int?,
-        @Json(name = "name")
-        val name: String?,
-        @Json(name = "verifyingContract")
-        val verifyingContract: String?,
-        @Json(name = "version")
-        val version: String?,
-    )
-
-    @JsonClass(generateAdapter = true)
-    data class Message(
-        @Json(name = "contents")
-        val contents: String?,
-        @Json(name = "from")
-        val from: Address?,
-        @Json(name = "to")
-        val to: Address?,
-    ) {
-        @JsonClass(generateAdapter = true)
-        data class Address(
-            @Json(name = "name")
-            val name: String,
-            @Json(name = "wallet")
-            val wallet: String,
-        )
-    }
-
-    @JsonClass(generateAdapter = true)
-    data class Types(
-        @Json(name = "EIP712Domain")
-        val eIP712Domain: List<Type> = listOf(),
-        @Json(name = "Mail")
-        val mail: List<Type> = listOf(),
-        @Json(name = "Person")
-        val person: List<Type> = listOf(),
-    ) {
-        @JsonClass(generateAdapter = true)
-        data class Type(
-            @Json(name = "name")
-            val name: String,
-            @Json(name = "type")
-            val type: String,
-        )
-    }
-}
+    val message: String?,
+)
