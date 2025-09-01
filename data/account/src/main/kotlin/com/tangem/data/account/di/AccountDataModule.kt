099d947588ea2013bcf62c1255c59bdc94f2404e
Tangem <git@tangem.com>
2025-09-01 20:22:30 +0400

    Updated on 2025-09-17

--- data/account/src/main/kotlin/com/tangem/data/account/di/AccountDataModule.kt
+++ data/account/src/main/kotlin/com/tangem/data/account/di/AccountDataModule.kt
@@ -1,11 +1,14 @@
 package com.tangem.data.account.di
 
+import com.tangem.core.configtoggle.feature.FeatureTogglesManager
 import com.tangem.data.account.converter.AccountConverterFactoryContainer
+import com.tangem.data.account.featuretoggle.DefaultAccountsFeatureToggles
 import com.tangem.data.account.repository.DefaultAccountsCRUDRepository
 import com.tangem.data.account.store.AccountsResponseStoreFactory
 import com.tangem.data.account.store.ArchivedAccountsStoreFactory
 import com.tangem.datasource.api.tangemTech.TangemTechApi
 import com.tangem.datasource.local.userwallet.UserWalletsStore
+import com.tangem.domain.account.featuretoggle.AccountsFeatureToggles
 import com.tangem.domain.account.repository.AccountsCRUDRepository
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import dagger.Module
@@ -17,6 +20,12 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 internal object AccountDataModule {
+
+    @Provides
+    @Singleton
+    fun provideAccountFeatureToggle(featureTogglesManager: FeatureTogglesManager): AccountsFeatureToggles {
+        return DefaultAccountsFeatureToggles(featureTogglesManager = featureTogglesManager)
+    }
 
     @Provides
     @Singleton
