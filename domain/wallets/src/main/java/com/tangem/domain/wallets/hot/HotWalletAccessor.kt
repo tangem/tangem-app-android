ffe72d321de1e874419510e3b11a8c08fd219945
Tangem <git@tangem.com>
2025-09-01 23:22:34 +0500

    Updated on 2026-09-23

--- domain/wallets/src/main/java/com/tangem/domain/wallets/hot/HotWalletAccessor.kt
+++ domain/wallets/src/main/java/com/tangem/domain/wallets/hot/HotWalletAccessor.kt
@@ -7,4 +7,10 @@ interface HotWalletAccessor {
     suspend fun signHashes(hotWalletId: HotWalletId, dataToSign: List<DataToSign>): List<SignedData>
 
     suspend fun derivePublicKeys(hotWalletId: HotWalletId, request: DeriveWalletRequest): DerivedPublicKeyResponse
+
+    suspend fun exportSeedPhrase(hotWalletId: HotWalletId): SeedPhrasePrivateInfo
+
+    suspend fun unlockContextual(hotWalletId: HotWalletId): UnlockHotWallet
+
+    fun clearContextualUnlock(hotWalletId: HotWalletId)
 }
