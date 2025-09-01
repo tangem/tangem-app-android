702c87ea289645a0508bdabff53734224f1a8a74
Tangem <git@tangem.com>
2025-09-01 23:22:34 +0500

    Updated on 2025-12-18

--- features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/walletbackup/model/WalletBackupModel.kt
+++ features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/walletbackup/model/WalletBackupModel.kt
@@ -1,38 +1,33 @@
 package com.tangem.features.hotwallet.walletbackup.model
 
-import com.tangem.core.decompose.di.GlobalUiMessageSender
 import com.tangem.core.decompose.di.ModelScoped
 import com.tangem.core.decompose.model.Model
 import com.tangem.core.decompose.model.ParamsContainer
 import com.tangem.core.decompose.navigation.Router
-import com.tangem.core.decompose.ui.UiMessageSender
 import com.tangem.core.ui.R
-import com.tangem.core.ui.components.bottomsheets.message.MessageBottomSheetUMV2
-import com.tangem.core.ui.components.bottomsheets.message.icon
-import com.tangem.core.ui.components.bottomsheets.message.infoBlock
-import com.tangem.core.ui.components.bottomsheets.message.onClick
-import com.tangem.core.ui.components.bottomsheets.message.secondaryButton
 import com.tangem.core.ui.components.label.entity.LabelStyle
 import com.tangem.core.ui.components.label.entity.LabelUM
 import com.tangem.core.ui.extensions.resourceReference
-import com.tangem.core.ui.message.bottomSheetMessage
 import com.tangem.domain.models.wallet.UserWallet
 import com.tangem.domain.wallets.usecase.GetUserWalletUseCase
 import com.tangem.common.routing.AppRoute
+import com.tangem.domain.wallets.usecase.UnlockHotWalletContextualUseCase
 import com.tangem.features.hotwallet.WalletBackupComponent
 import com.tangem.features.hotwallet.walletbackup.entity.BackupStatus
 import com.tangem.features.hotwallet.walletbackup.entity.WalletBackupUM
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @ModelScoped
 internal class WalletBackupModel @Inject constructor(
     paramsContainer: ParamsContainer,
-    getWalletUseCase: GetUserWalletUseCase,
-    private val router: Router,
+    private val getWalletUseCase: GetUserWalletUseCase,
+    private val unlockHotWalletContextualUseCase: UnlockHotWalletContextualUseCase,
     override val dispatchers: CoroutineDispatcherProvider,
-    @GlobalUiMessageSender private val uiMessageSender: UiMessageSender,
+    private val router: Router,
 ) : Model() {
 
     private val params: WalletBackupComponent.Params = paramsContainer.require()
@@ -57,34 +52,16 @@ internal class WalletBackupModel @Inject constructor(
         ),
     )
 
-    private val makeBackupAtFirstAlertBS
-        get() = bottomSheetMessage {
-            infoBlock {
-                icon(R.drawable.ic_passcode_lock_32) {
-                    type = MessageBottomSheetUMV2.Icon.Type.Accent
-                    backgroundType = MessageBottomSheetUMV2.Icon.BackgroundType.SameAsTint
-                }
-                title = resourceReference(R.string.hw_backup_need_title)
-                body = resourceReference(R.string.hw_backup_need_description)
-            }
-            secondaryButton {
-                text = resourceReference(R.string.hw_backup_need_action)
-                onClick {
-                    router.push(AppRoute.CreateWalletBackup(params.userWalletId))
-                    closeBs()
-                }
-            }
-        }
-
     init {
-        getWalletUseCase.invokeFlow(params.userWalletId)
-            .map { it.getOrNull() }
-            .distinctUntilChanged()
-            .filterNotNull()
-            .onEach {
-                updateBackupStatuses(it)
-            }
-            .launchIn(modelScope)
+        getWalletUseCase.invoke(params.userWalletId)
+            .fold(
+                ifLeft = {
+                    Timber.e("Error on getting user wallet: $it")
+                },
+                ifRight = {
+                    updateBackupStatuses(it)
+                },
+            )
     }
 
     private fun updateBackupStatuses(userWallet: UserWallet) {
@@ -118,9 +95,37 @@ internal class WalletBackupModel @Inject constructor(
 
     private fun onRecoveryPhraseClick() {
         if (uiState.value.backedUp) {
-            // TODO [REDACTED_TASK_KEY]
+            getWalletUseCase.invoke(params.userWalletId)
+                .fold(
+                    ifLeft = {
+                        Timber.e("Error on getting user wallet: $it")
+                    },
+                    ifRight = { userWallet ->
+                        when (userWallet) {
+                            is UserWallet.Cold -> {
+                                val userWalletId = userWallet.walletId
+                                Timber.e("Unexpected cold wallet when request seed phrase: $userWalletId")
+                            }
+                            is UserWallet.Hot -> showSeedPhrase(userWallet)
+                        }
+                    },
+                )
         } else {
-            uiMessageSender.send(makeBackupAtFirstAlertBS)
+            router.push(AppRoute.CreateWalletBackup(params.userWalletId))
+        }
+    }
+
+    private fun showSeedPhrase(hotWallet: UserWallet.Hot) {
+        modelScope.launch {
+            unlockHotWalletContextualUseCase.invoke(hotWallet.hotWalletId)
+                .fold(
+                    ifLeft = {
+                        Timber.e("Error while export seed phrase: $it")
+                    },
+                    ifRight = { seedPhrasePrivateInfo ->
+                        router.push(AppRoute.ViewPhrase(params.userWalletId))
+                    },
+                )
         }
     }
 
