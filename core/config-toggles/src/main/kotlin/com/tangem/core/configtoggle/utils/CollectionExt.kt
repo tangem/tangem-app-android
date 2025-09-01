16c30f111c1c942782f142a24ee7090d7c631d1d
Tangem <git@tangem.com>
2025-09-01 17:39:17 +0400

    Updated on 2025-09-17

--- core/config-toggles/src/main/kotlin/com/tangem/core/configtoggle/utils/CollectionExt.kt
+++ core/config-toggles/src/main/kotlin/com/tangem/core/configtoggle/utils/CollectionExt.kt
@@ -1,16 +1,6 @@
 package com.tangem.core.configtoggle.utils
 
-import com.tangem.core.configtoggle.storage.ConfigToggle
 import com.tangem.core.configtoggle.version.VersionAvailabilityContract
-
-internal fun List<ConfigToggle>.associateToggles(currentVersion: String): Map<String, Boolean> {
-    return associate { localToggle ->
-        Pair(
-            first = localToggle.name,
-            second = VersionAvailabilityContract(currentVersion, localToggle.version),
-        )
-    }
-}
 
 internal fun Map<String, String>.defineTogglesAvailability(appVersion: String?): Map<String, Boolean> {
     return if (appVersion == null) {
