30fc502107850bdc161ef5fe4c40f10a40952729
Tangem <git@tangem.com>
2025-09-03 09:55:24 +0400

    Updated on 2026-06-16

--- domain/nft/src/main/kotlin/com/tangem/domain/nft/FetchNFTCollectionsUseCase.kt
+++ domain/nft/src/main/kotlin/com/tangem/domain/nft/FetchNFTCollectionsUseCase.kt
@@ -1,28 +1,20 @@
 package com.tangem.domain.nft
 
+import com.tangem.domain.models.wallet.UserWalletId
 import com.tangem.domain.nft.repository.NFTRepository
 import com.tangem.domain.tokens.MultiWalletCryptoCurrenciesProducer
 import com.tangem.domain.tokens.MultiWalletCryptoCurrenciesSupplier
-import com.tangem.domain.tokens.TokensFeatureToggles
-import com.tangem.domain.tokens.repository.CurrenciesRepository
-import com.tangem.domain.models.wallet.UserWalletId
 
 class FetchNFTCollectionsUseCase(
-    private val currenciesRepository: CurrenciesRepository,
     private val multiWalletCryptoCurrenciesSupplier: MultiWalletCryptoCurrenciesSupplier,
-    private val tokensFeatureToggles: TokensFeatureToggles,
     private val nftRepository: NFTRepository,
 ) {
 
     suspend operator fun invoke(userWalletId: UserWalletId) {
-        val currencies = if (tokensFeatureToggles.isWalletBalanceFetcherEnabled) {
-            multiWalletCryptoCurrenciesSupplier.getSyncOrNull(
-                params = MultiWalletCryptoCurrenciesProducer.Params(userWalletId),
-            )
-                .orEmpty()
-        } else {
-            currenciesRepository.getMultiCurrencyWalletCurrenciesSync(userWalletId)
-        }
+        val currencies = multiWalletCryptoCurrenciesSupplier.getSyncOrNull(
+            params = MultiWalletCryptoCurrenciesProducer.Params(userWalletId),
+        )
+            .orEmpty()
 
         nftRepository.refreshCollections(userWalletId, currencies.map { it.network }.distinct())
     }
