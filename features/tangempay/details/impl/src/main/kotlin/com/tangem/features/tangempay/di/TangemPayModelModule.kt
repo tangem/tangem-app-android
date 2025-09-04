d5003a9b8c001e5a7e88c5e6b9841a5dc6200e52
Tangem <git@tangem.com>
2025-09-04 17:10:01 +0500

    Updated on 2026-09-23

--- features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/di/TangemPayModelModule.kt
+++ features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/di/TangemPayModelModule.kt
@@ -3,6 +3,7 @@ package com.tangem.features.tangempay.di
 import com.tangem.core.decompose.di.ModelComponent
 import com.tangem.core.decompose.model.Model
 import com.tangem.features.tangempay.model.TangemPayDetailsModel
+import com.tangem.features.tangempay.model.TangemPayTxHistoryModel
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -16,5 +17,10 @@ internal interface TangemPayModelModule {
     @Binds
     @IntoMap
     @ClassKey(TangemPayDetailsModel::class)
-    fun bindWcConnectionsModel(model: TangemPayDetailsModel): Model
+    fun bindTangemPayDetailsModel(model: TangemPayDetailsModel): Model
+
+    @Binds
+    @IntoMap
+    @ClassKey(TangemPayTxHistoryModel::class)
+    fun bindTangemPayTxHistoryModel(model: TangemPayTxHistoryModel): Model
 }
