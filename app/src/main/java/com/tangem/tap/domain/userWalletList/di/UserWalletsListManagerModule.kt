c5a9788893f626fbc4117ddd6a88e9e405133edc
Tangem <git@tangem.com>
2025-09-05 13:19:12 +0300

    Updated on 2026-08-19

--- app/src/main/java/com/tangem/tap/domain/userWalletList/di/UserWalletsListManagerModule.kt
+++ app/src/main/java/com/tangem/tap/domain/userWalletList/di/UserWalletsListManagerModule.kt
@@ -8,6 +8,7 @@ import com.tangem.common.json.TangemSdkAdapter
 import com.tangem.common.services.secure.SecureStorage
 import com.tangem.core.analytics.api.AnalyticsEventHandler
 import com.tangem.datasource.local.preferences.AppPreferencesStore
+import com.tangem.domain.card.ScanCardProcessor
 import com.tangem.domain.models.scan.serialization.*
 import com.tangem.domain.visa.model.VisaActivationRemoteState
 import com.tangem.domain.visa.model.VisaCardActivationStatus
@@ -122,6 +123,7 @@ internal object UserWalletsListManagerModule {
         passwordRequester: HotWalletPasswordRequester,
         appPreferencesStore: AppPreferencesStore,
         hotWalletAccessCodeAttemptsRepository: HotWalletAccessCodeAttemptsRepository,
+        scanCardProcessor: ScanCardProcessor,
     ): UserWalletsListRepository {
         val moshi = buildMoshi()
         val secureStorage = buildSecureStorage(applicationContext = applicationContext)
@@ -168,6 +170,7 @@ internal object UserWalletsListManagerModule {
             appPreferencesStore = appPreferencesStore,
             savePersistentInformation = ProviderSuspend { true }, // Always save persistent information for now
             hotWalletAccessCodeAttemptsRepository = hotWalletAccessCodeAttemptsRepository,
+            scanCardProcessor = scanCardProcessor,
         )
     }
 
