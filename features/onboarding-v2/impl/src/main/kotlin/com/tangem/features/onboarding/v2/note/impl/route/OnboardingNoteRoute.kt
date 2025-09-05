588c5306424c7532eec48f8689c76b82e5a0c54c
Tangem <git@tangem.com>
2025-09-05 16:23:03 +0300

    Updated on 2025-12-22

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/note/impl/route/OnboardingNoteRoute.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/note/impl/route/OnboardingNoteRoute.kt
@@ -7,9 +7,6 @@ internal sealed class OnboardingNoteRoute {
 
     @Serializable
     data object CreateWallet : OnboardingNoteRoute()
-
-    @Serializable
-    data object Done : OnboardingNoteRoute()
 }
 
-internal const val ONBOARDING_NOTE_STEPS_COUNT = 2
+internal const val ONBOARDING_NOTE_STEPS_COUNT = 1
