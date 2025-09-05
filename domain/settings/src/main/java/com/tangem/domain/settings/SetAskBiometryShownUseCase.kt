2f0b0549cae77f45a9bc0615b66a3cdd9aa93af4
Tangem <git@tangem.com>
2025-09-05 13:19:12 +0300

    Updated on 2026-09-23

--- domain/settings/src/main/java/com/tangem/domain/settings/SetAskBiometryShownUseCase.kt
+++ domain/settings/src/main/java/com/tangem/domain/settings/SetAskBiometryShownUseCase.kt
@@ -3,13 +3,13 @@ package com.tangem.domain.settings
 import arrow.core.Either
 import com.tangem.domain.settings.repositories.SettingsRepository
 
-class SetSaveWalletScreenShownUseCase(
+class SetAskBiometryShownUseCase(
     private val settingsRepository: SettingsRepository,
 ) {
 
     suspend operator fun invoke(): Either<Throwable, Unit> {
         return Either.catch {
-            settingsRepository.setShouldShowSaveUserWalletScreen(value = false)
+            settingsRepository.setShouldShowAskBiometry(value = false)
         }
     }
 }
