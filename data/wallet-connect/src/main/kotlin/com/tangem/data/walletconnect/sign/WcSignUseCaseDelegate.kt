1af08b456821c2e2047f41aee0bb4254f670acb4
Tangem <git@tangem.com>
2025-09-05 16:01:22 +0300

    Updated on 2025-12-05

--- data/wallet-connect/src/main/kotlin/com/tangem/data/walletconnect/sign/WcSignUseCaseDelegate.kt
+++ data/wallet-connect/src/main/kotlin/com/tangem/data/walletconnect/sign/WcSignUseCaseDelegate.kt
@@ -8,6 +8,7 @@ import com.tangem.data.walletconnect.sign.SignStateConverter.toSigning
 import com.tangem.domain.walletconnect.WcAnalyticEvents
 import com.tangem.domain.walletconnect.model.WcRequestError
 import com.tangem.domain.walletconnect.model.WcRequestError.Companion.code
+import com.tangem.domain.walletconnect.model.WcRequestError.Companion.message
 import com.tangem.domain.walletconnect.usecase.method.WcSignState
 import com.tangem.domain.walletconnect.usecase.method.WcSignStep
 import kotlinx.coroutines.Job
@@ -65,10 +66,12 @@ internal class WcSignUseCaseDelegate<MiddleAction, SignModel>(
                 val step = state.domainStep as? WcSignStep.Result ?: return@onEach
                 val event = step.result.fold(
                     ifLeft = { error ->
+                        val errorMessage = error.message() ?: ""
                         WcAnalyticEvents.SignatureRequestFailed(
                             rawRequest = context.rawSdkRequest,
                             network = context.network,
                             errorCode = error.code() ?: error::class.simpleName.orEmpty(),
+                            errorMessage = errorMessage,
                         )
                     },
                     ifRight = {
