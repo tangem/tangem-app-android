0ddaa164d3909cbf9c3922d5ff462d82925ebb46
Tangem <git@tangem.com>
2025-09-09 10:45:42 +0200

    Updated on 2026-02-02

--- features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/alloffers/entity/AllOffersIntents.kt
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/alloffers/entity/AllOffersIntents.kt
@@ -1,12 +1,13 @@
 package com.tangem.features.onramp.alloffers.entity
 
 import com.tangem.domain.onramp.model.OnrampProviderWithQuote
+import com.tangem.features.onramp.mainv2.entity.OnrampOfferAdvantagesUM
 
-interface AllOffersIntents {
+internal interface AllOffersIntents {
 
     fun onPaymentMethodClicked(paymentMethodId: String)
 
-    fun onBuyClick(quote: OnrampProviderWithQuote.Data)
+    fun onBuyClick(quote: OnrampProviderWithQuote.Data, onrampOfferAdvantagesUM: OnrampOfferAdvantagesUM)
 
     fun onBackClicked()
 
