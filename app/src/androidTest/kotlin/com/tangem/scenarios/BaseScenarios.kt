cd8e97d7c01b1d2ebe95dafdcb20335dfbd70921
Tangem <git@tangem.com>
2025-09-09 15:46:25 +0300

    Updated on 2026-01-24

--- app/src/androidTest/kotlin/com/tangem/scenarios/BaseScenarios.kt
+++ app/src/androidTest/kotlin/com/tangem/scenarios/BaseScenarios.kt
@@ -1,29 +1,31 @@
-package com.tangem.steps
+package com.tangem.scenarios
 
 import com.tangem.common.BaseTestCase
 import com.tangem.common.extensions.clickWithAssertion
 import com.tangem.domain.models.scan.ProductType
-import com.tangem.screens.onDisclaimerScreen
-import com.tangem.screens.onMainScreen
-import com.tangem.screens.onMarketsTooltipScreen
-import com.tangem.screens.onStoriesScreen
+import com.tangem.screens.*
 import com.tangem.tap.domain.sdk.mocks.MockProvider
 import io.qameta.allure.kotlin.Allure.step
 
-fun BaseTestCase.openMainScreen(productType: ProductType? = null) {
+fun BaseTestCase.openMainScreen(productType: ProductType? = null, alreadyActivatedDialogIsShown : Boolean = false) {
     if (productType != null) {
         MockProvider.setMocks(productType)
     }
     step("Click on 'Accept' button") {
         onDisclaimerScreen { acceptButton.clickWithAssertion() }
     }
-    step("Click on 'Accept' button") {
+    step("Click on 'Scan' button") {
         onStoriesScreen { scanButton.clickWithAssertion() }
     }
-    step("Assert main screen is displayed") {
+    if (alreadyActivatedDialogIsShown) {
+        step("Click on 'This is my wallet' button") {
+            AlreadyUsedWalletDialogPageObject { thisIsMyWalletButton.click() }
+        }
+    }
+    step("Assert 'Main' screen is displayed") {
         onMainScreen { screenContainer.assertIsDisplayed() }
     }
-    step("Assert main screen is displayed") {
+    step("Click on 'Market Tooltip' screen") {
         onMarketsTooltipScreen { contentContainer.clickWithAssertion() }
     }
 }
