0605f28499b8675ef9b10f41cbcfb449cb3cb6cd
Tangem <git@tangem.com>
2025-09-09 10:04:56 +0200

    Updated on 2026-06-01

--- features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/alloffers/AllOffersComponent.kt
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/alloffers/AllOffersComponent.kt
@@ -4,6 +4,7 @@ import com.tangem.core.decompose.factory.ComponentFactory
 import com.tangem.core.ui.decompose.ComposableBottomSheetComponent
 import com.tangem.domain.models.currency.CryptoCurrency
 import com.tangem.domain.models.wallet.UserWallet
+import com.tangem.domain.onramp.model.OnrampProviderWithQuote
 
 internal interface AllOffersComponent : ComposableBottomSheetComponent {
 
@@ -11,6 +12,8 @@ internal interface AllOffersComponent : ComposableBottomSheetComponent {
         val userWallet: UserWallet,
         val cryptoCurrency: CryptoCurrency,
         val onDismiss: () -> Unit,
+        val openRedirectPage: (quote: OnrampProviderWithQuote.Data) -> Unit,
+        val amountCurrencyCode: String,
     )
 
     interface Factory : ComponentFactory<Params, AllOffersComponent>
