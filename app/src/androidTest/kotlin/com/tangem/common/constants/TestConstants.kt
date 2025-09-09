9ee81d6598eeca0420331aede0ac383e1f24b3b6
Tangem <git@tangem.com>
2025-09-09 15:46:25 +0300

    Updated on 2025-12-22

--- app/src/androidTest/kotlin/com/tangem/common/constants/TestConstants.kt
+++ app/src/androidTest/kotlin/com/tangem/common/constants/TestConstants.kt
@@ -3,5 +3,7 @@ package com.tangem.common.constants
 object TestConstants {
     const val TOTAL_BALANCE = "$3,299.18"
 
+    const val RECIPIENT_ADDRESS = "0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0"
+
     const val WAIT_UNTIL_TIMEOUT = 20_000L
 }
