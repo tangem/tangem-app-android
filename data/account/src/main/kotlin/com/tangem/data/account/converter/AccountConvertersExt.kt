27904e053aff81a30b688abd9c6654c3df604a95
Tangem <git@tangem.com>
2025-09-10 16:15:18 +0400

    Updated on 2026-06-16

--- data/account/src/main/kotlin/com/tangem/data/account/converter/AccountConvertersExt.kt
+++ data/account/src/main/kotlin/com/tangem/data/account/converter/AccountConvertersExt.kt
@@ -3,7 +3,6 @@ package com.tangem.data.account.converter
 import arrow.core.getOrElse
 import com.tangem.datasource.api.tangemTech.models.account.WalletAccountDTO
 import com.tangem.domain.models.account.AccountId
-import com.tangem.domain.models.account.AccountName
 import com.tangem.domain.models.account.CryptoPortfolioIcon
 import com.tangem.domain.models.account.DerivationIndex
 import com.tangem.domain.models.wallet.UserWalletId
@@ -11,12 +10,6 @@ import com.tangem.domain.models.wallet.UserWalletId
 internal fun String.toAccountId(userWalletId: UserWalletId): AccountId {
     return AccountId.forCryptoPortfolio(value = this, userWalletId = userWalletId).getOrElse {
         error("Unable to create AccountId from value: $this. Cause: $it")
-    }
-}
-
-internal fun String.toAccountName(): AccountName {
-    return AccountName(value = this).getOrElse {
-        error("Unable to create AccountName from value: $this. Cause: $it")
     }
 }
 
