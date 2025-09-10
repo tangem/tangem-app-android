586a614b0e8eb76783bea8af8def1250c29120bd
Tangem <git@tangem.com>
2025-09-10 10:51:33 +0400

    Updated on 2026-09-21

--- data/account/src/main/kotlin/com/tangem/data/account/di/AccountDataModule.kt
+++ data/account/src/main/kotlin/com/tangem/data/account/di/AccountDataModule.kt
@@ -3,9 +3,13 @@ package com.tangem.data.account.di
 import com.tangem.core.configtoggle.feature.FeatureTogglesManager
 import com.tangem.data.account.converter.AccountConverterFactoryContainer
 import com.tangem.data.account.featuretoggle.DefaultAccountsFeatureToggles
+import com.tangem.data.account.fetcher.DefaultWalletAccountsFetcher
 import com.tangem.data.account.repository.DefaultAccountsCRUDRepository
 import com.tangem.data.account.store.AccountsResponseStoreFactory
 import com.tangem.data.account.store.ArchivedAccountsStoreFactory
+import com.tangem.data.common.account.WalletAccountsFetcher
+import com.tangem.data.common.account.WalletAccountsSaver
+import com.tangem.data.common.cache.etag.ETagsStore
 import com.tangem.datasource.api.tangemTech.TangemTechApi
 import com.tangem.datasource.local.userwallet.UserWalletsStore
 import com.tangem.domain.account.featuretoggle.AccountsFeatureToggles
@@ -31,18 +35,30 @@ internal object AccountDataModule {
     @Singleton
     fun provideAccountsCRUDRepository(
         tangemTechApi: TangemTechApi,
+        walletAccountsSaver: WalletAccountsSaver,
         accountsResponseStoreFactory: AccountsResponseStoreFactory,
         userWalletsStore: UserWalletsStore,
+        eTagsStore: ETagsStore,
         accountConverterFactoryContainer: AccountConverterFactoryContainer,
         dispatchers: CoroutineDispatcherProvider,
     ): AccountsCRUDRepository {
         return DefaultAccountsCRUDRepository(
             tangemTechApi = tangemTechApi,
+            walletAccountsSaver = walletAccountsSaver,
             accountsResponseStoreFactory = accountsResponseStoreFactory,
             archivedAccountsStoreFactory = ArchivedAccountsStoreFactory,
             userWalletsStore = userWalletsStore,
+            eTagsStore = eTagsStore,
             convertersContainer = accountConverterFactoryContainer,
             dispatchers = dispatchers,
         )
     }
+
+    @Provides
+    @Singleton
+    fun provideWalletAccountsFetcher(impl: DefaultWalletAccountsFetcher): WalletAccountsFetcher = impl
+
+    @Provides
+    @Singleton
+    fun provideWalletAccountsSaver(impl: DefaultWalletAccountsFetcher): WalletAccountsSaver = impl
 }
