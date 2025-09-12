6e1cb840665fcb5552565d03c236910ad101cd64
Tangem <git@tangem.com>
2025-09-12 16:32:29 +0500

    Updated on 2026-06-17

--- core/ui/src/main/java/com/tangem/core/ui/components/label/entity/LabelUM.kt
+++ core/ui/src/main/java/com/tangem/core/ui/components/label/entity/LabelUM.kt
@@ -1,10 +1,13 @@
 package com.tangem.core.ui.components.label.entity
 
+import androidx.annotation.DrawableRes
 import com.tangem.core.ui.extensions.TextReference
 
 data class LabelUM(
     val text: TextReference,
     val style: LabelStyle,
+    @DrawableRes val icon: Int? = null,
+    val onIconClick: (() -> Unit)? = null,
 )
 
 enum class LabelStyle {
