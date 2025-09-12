533ecbed3e11fafcfb8bd36810f9ee1e95fe32cb
Tangem <git@tangem.com>
2025-09-12 11:44:13 +0400

    Updated on 2025-09-17

--- features/tangempay/onboarding/impl/src/main/kotlin/com/tangem/features/tangempay/deeplink/DefaultOnboardVisaDeepLinkHandler.kt
+++ features/tangempay/onboarding/impl/src/main/kotlin/com/tangem/features/tangempay/deeplink/DefaultOnboardVisaDeepLinkHandler.kt
@@ -3,6 +3,7 @@ package com.tangem.features.tangempay.deeplink
 import android.net.Uri
 import dagger.assisted.Assisted
 import com.tangem.common.routing.AppRoute
+import com.tangem.features.tangempay.TangemPayFeatureToggles
 import com.tangem.common.routing.AppRouter
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
@@ -10,10 +11,15 @@ import dagger.assisted.AssistedInject
 internal class DefaultOnboardVisaDeepLinkHandler @AssistedInject constructor(
     @Assisted uri: Uri,
     appRouter: AppRouter,
+    tangemPayFeatureToggles: TangemPayFeatureToggles,
 ) : OnboardVisaDeepLinkHandler {
 
     init {
-        appRouter.push(AppRoute.TangemPayOnboarding(uri.toString()))
+        if (tangemPayFeatureToggles.isTangemPayEnabled) {
+            appRouter.push(AppRoute.TangemPayOnboarding(uri.toString()))
+        } else {
+            appRouter.push(AppRoute.Home())
+        }
     }
 
     @AssistedFactory
