e8345a12789386afc281c96fa58f50a82e68b9ff
Tangem <git@tangem.com>
2025-09-12 16:32:45 +0500

    Updated on 2026-09-25

---
+++ features/yield-supply/impl/src/main/java/com/tangem/features/yield/supply/impl/promo/entity/YieldSupplyPromoUM.kt
@@ -0,0 +1,9 @@
+package com.tangem.features.yield.supply.impl.promo.entity
+
+import com.tangem.core.ui.extensions.TextReference
+
+data class YieldSupplyPromoUM(
+    val tosLink: String,
+    val policyLink: String,
+    val title: TextReference,
+)
