15f1d97baa86d3283acad7d812cae4498948e518
Tangem <git@tangem.com>
2025-09-12 11:44:13 +0400

    Updated on 2026-09-21

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -263,8 +263,8 @@ include(":features:hot-wallet:api")
 include(":features:hot-wallet:impl")
 
 include(":features:kyc:api")
-//TODO disable for release because of the permissions
-// include(":features:kyc:impl")
+include(":features:kyc:impl")
+include(":features:kyc:mock")
 
 include(":features:tangempay:main:api")
 include(":features:tangempay:main:impl")
