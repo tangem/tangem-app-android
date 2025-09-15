d647df6a158e97d8cef4e132b3626684c10db8a0
Tangem <git@tangem.com>
2025-09-15 19:30:17 +0500

    Updated on 2025-10-13

--- domain/transaction/src/main/java/com/tangem/domain/transaction/FeeRepository.kt
+++ domain/transaction/src/main/java/com/tangem/domain/transaction/FeeRepository.kt
@@ -1,10 +1,21 @@
 package com.tangem.domain.transaction
 
 import com.tangem.blockchain.common.AmountType
+import com.tangem.blockchain.common.TransactionData
+import com.tangem.blockchain.common.transaction.TransactionFee
+import com.tangem.domain.models.currency.CryptoCurrency
 import com.tangem.domain.models.network.Network
+import com.tangem.domain.models.wallet.UserWallet
 
 interface FeeRepository {
 
     /** Returns if fee is approximate for current [networkId] */
     fun isFeeApproximate(networkId: Network.ID, amountType: AmountType): Boolean
+
+    /** Returns fee calculated for the transaction [transactionData] */
+    suspend fun calculateFee(
+        userWallet: UserWallet,
+        cryptoCurrency: CryptoCurrency,
+        transactionData: TransactionData,
+    ): TransactionFee
 }
