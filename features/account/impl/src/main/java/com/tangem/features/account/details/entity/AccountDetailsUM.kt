25bf09e730f7842085a7b575a805dafafdf78eda
Tangem <git@tangem.com>
2025-09-15 21:10:37 +0700

    Updated on 2025-10-13

--- features/account/impl/src/main/java/com/tangem/features/account/details/entity/AccountDetailsUM.kt
+++ features/account/impl/src/main/java/com/tangem/features/account/details/entity/AccountDetailsUM.kt
@@ -6,8 +6,16 @@ import com.tangem.core.ui.extensions.TextReference
 internal data class AccountDetailsUM(
     val accountName: TextReference,
     val accountIcon: CryptoPortfolioIconUM,
+    val archiveMode: ArchiveMode,
     val onCloseClick: () -> Unit,
     val onAccountEditClick: () -> Unit,
     val onManageTokensClick: () -> Unit,
-    val onArchiveAccountClick: () -> Unit,
-)
+) {
+
+    sealed interface ArchiveMode {
+        data object None : ArchiveMode
+        data class Available(
+            val onArchiveAccountClick: () -> Unit,
+        ) : ArchiveMode
+    }
+}
