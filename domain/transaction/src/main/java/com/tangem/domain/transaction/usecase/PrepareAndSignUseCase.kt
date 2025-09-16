6c32e4a3d2d7cc0320d258036d21569db0c93c11
Tangem <git@tangem.com>
2025-09-16 14:10:26 +0300

    Updated on 2025-09-17

--- domain/transaction/src/main/java/com/tangem/domain/transaction/usecase/PrepareAndSignUseCase.kt
+++ domain/transaction/src/main/java/com/tangem/domain/transaction/usecase/PrepareAndSignUseCase.kt
@@ -18,6 +18,7 @@ import com.tangem.domain.transaction.error.SendTransactionError
 class PrepareAndSignUseCase(
     private val transactionRepository: TransactionRepository,
     private val cardSdkConfigRepository: CardSdkConfigRepository,
+    private val getHotTransactionSigner: (UserWallet.Hot) -> TransactionSigner,
 ) {
 
     suspend operator fun invoke(
@@ -56,8 +57,14 @@ class PrepareAndSignUseCase(
         }
     }
 
-    private fun createSigner(userWallet: UserWallet): TransactionSigner {
-        userWallet.requireColdWallet() // TODO [REDACTED_TASK_KEY]
+    private fun createSigner(userWallet: UserWallet) : TransactionSigner {
+        return when(userWallet) {
+            is UserWallet.Hot -> getHotTransactionSigner(userWallet)
+            is UserWallet.Cold -> createColdSigner(userWallet)
+        }
+    }
+
+    private fun createColdSigner(userWallet: UserWallet.Cold): TransactionSigner {
         val card = userWallet.scanResponse.card
         val isCardNotBackedUp = card.backupStatus?.isActive != true && !card.isTangemTwins
 
