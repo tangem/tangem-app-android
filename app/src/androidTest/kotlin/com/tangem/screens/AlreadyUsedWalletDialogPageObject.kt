376a1d249ae36af25e28b290f888bc61a6a7b0b3
Tangem <git@tangem.com>
2025-09-16 12:28:45 +0300

    Updated on 2025-11-18

--- app/src/androidTest/kotlin/com/tangem/screens/AlreadyUsedWalletDialogPageObject.kt
+++ app/src/androidTest/kotlin/com/tangem/screens/AlreadyUsedWalletDialogPageObject.kt
@@ -1,32 +1,29 @@
 package com.tangem.screens
 
-import com.kaspersky.kaspresso.screens.KScreen
+import com.tangem.common.extensions.withDialogRoot
 import com.tangem.wallet.R
-import io.github.kakaocup.kakao.text.KTextView
 import io.github.kakaocup.kakao.text.KButton
+import io.github.kakaocup.kakao.text.KTextView
 
-object AlreadyUsedWalletDialogPageObject : KScreen<AlreadyUsedWalletDialogPageObject>() {
-
-    override val layoutId: Int? = null
-    override val viewClass: Class<*>? = null
+object AlreadyUsedWalletDialogPageObject : BaseDialog() {
 
     val title = KTextView {
         withText(R.string.security_alert_title)
-    }
+    }.withDialogRoot()
 
     val message = KTextView {
         withText(R.string.wallet_been_activated_message)
-    }
+    }.withDialogRoot()
 
     val cancelButton = KButton {
         withText(R.string.common_cancel)
-    }
+    }.withDialogRoot()
 
     val requestSupportButton = KButton {
         withText(R.string.alert_button_request_support)
-    }
+    }.withDialogRoot()
 
     val thisIsMyWalletButton = KButton {
         withText(R.string.this_is_my_wallet_title)
-    }
+    }.withDialogRoot()
 }
