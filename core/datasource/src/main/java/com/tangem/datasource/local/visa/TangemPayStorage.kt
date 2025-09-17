49cba03333a8f115603b7ab33ebb1a69df189673
Tangem <git@tangem.com>
2025-09-17 11:39:17 +0400

    Updated on 2025-12-05

--- core/datasource/src/main/java/com/tangem/datasource/local/visa/TangemPayStorage.kt
+++ core/datasource/src/main/java/com/tangem/datasource/local/visa/TangemPayStorage.kt
@@ -1,10 +1,16 @@
 package com.tangem.datasource.local.visa
 
+import com.tangem.domain.visa.model.VisaAuthTokens
+
 interface TangemPayStorage {
 
-    suspend fun store(authHeader: String)
+    suspend fun storeAuthTokens(customerWalletAddress: String, tokens: VisaAuthTokens)
 
-    suspend fun get(): String?
+    suspend fun getAuthTokens(customerWalletAddress: String): VisaAuthTokens?
 
-    suspend fun clear()
+    suspend fun storeCustomerWalletAddress(customerWalletAddress: String)
+
+    suspend fun getCustomerWalletAddress(): String?
+
+    suspend fun clear(customerWalletAddress: String)
 }
