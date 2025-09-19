c73bb8ce97d0072dc0e51cc770d7587e65601b9a
Tangem <git@tangem.com>
2025-09-19 21:05:18 +0500

    Updated on 2026-09-21

--- features/yield-supply/impl/src/main/java/com/tangem/features/yield/supply/impl/subcomponents/startearning/YieldSupplyStartEarningComponent.kt
+++ features/yield-supply/impl/src/main/java/com/tangem/features/yield/supply/impl/subcomponents/startearning/YieldSupplyStartEarningComponent.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.tangem.core.decompose.context.AppComponentContext
+import com.tangem.core.decompose.context.child
 import com.tangem.core.decompose.model.getOrCreateModel
 import com.tangem.core.ui.components.PrimaryButtonIconEnd
 import com.tangem.core.ui.components.bottomsheets.modal.TangemModalBottomSheetTitle
@@ -25,7 +26,9 @@ import com.tangem.domain.models.currency.CryptoCurrency
 import com.tangem.domain.models.wallet.UserWalletId
 import com.tangem.features.yield.supply.impl.R
 import com.tangem.features.yield.supply.impl.common.entity.YieldSupplyActionUM
+import com.tangem.features.yield.supply.impl.common.entity.YieldSupplyFeeUM
 import com.tangem.features.yield.supply.impl.common.ui.YieldSupplyActionContent
+import com.tangem.features.yield.supply.impl.subcomponents.notifications.YieldSupplyNotificationsComponent
 import com.tangem.features.yield.supply.impl.subcomponents.startearning.model.YieldSupplyStartEarningModel
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
@@ -36,6 +39,16 @@ internal class YieldSupplyStartEarningComponent(
 ) : ComposableModularContentComponent, AppComponentContext by appComponentContext {
 
     private val model: YieldSupplyStartEarningModel = getOrCreateModel(params = params)
+
+    private val yieldSupplyNotificationsComponent = YieldSupplyNotificationsComponent(
+        appComponentContext = child("yieldSupplyEnterNotifications"),
+        params = YieldSupplyNotificationsComponent.Params(
+            userWalletId = params.userWalletId,
+            cryptoCurrencyStatusFlow = model.cryptoCurrencyStatusFlow,
+            feeCryptoCurrencyStatusFlow = model.feeCryptoCurrencyStatusFlow,
+            callback = model,
+        ),
+    )
 
     init {
         model.uiState.onEach(params.callback::onResult).launchIn(componentScope)
@@ -57,6 +70,8 @@ internal class YieldSupplyStartEarningComponent(
         YieldSupplyActionContent(
             yieldSupplyActionUM = state,
             onFooterClick = params.callback::onFeePolicyClick,
+            enableFooterClick = state.yieldSupplyFeeUM is YieldSupplyFeeUM.Content,
+            yieldSupplyNotificationsComponent = yieldSupplyNotificationsComponent,
             modifier = modifier,
         ) {
             Box(
