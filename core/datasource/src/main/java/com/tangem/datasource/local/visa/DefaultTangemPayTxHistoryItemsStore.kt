59697e38f3a209e39efa077ac727bd16041eca46
Tangem <git@tangem.com>
2025-09-21 15:32:47 +0400

    Updated on 2026-06-02

--- core/datasource/src/main/java/com/tangem/datasource/local/visa/DefaultTangemPayTxHistoryItemsStore.kt
+++ core/datasource/src/main/java/com/tangem/datasource/local/visa/DefaultTangemPayTxHistoryItemsStore.kt
@@ -3,22 +3,22 @@ package com.tangem.datasource.local.visa
 import com.tangem.datasource.local.datastore.core.StringKeyDataStore
 import com.tangem.datasource.local.datastore.core.StringKeyDataStoreDecorator
 import com.tangem.domain.models.wallet.UserWalletId
-import com.tangem.domain.visa.model.VisaTxHistoryItem
+import com.tangem.domain.visa.model.TangemPayTxHistoryItem
 
 internal class DefaultTangemPayTxHistoryItemsStore(
-    dataStore: StringKeyDataStore<Map<Int, List<VisaTxHistoryItem>>>,
+    dataStore: StringKeyDataStore<Map<String, List<TangemPayTxHistoryItem>>>,
 ) : TangemPayTxHistoryItemsStore,
-    StringKeyDataStoreDecorator<UserWalletId, Map<Int, List<VisaTxHistoryItem>>>(dataStore) {
+    StringKeyDataStoreDecorator<UserWalletId, Map<String, List<TangemPayTxHistoryItem>>>(dataStore) {
     override fun provideStringKey(key: UserWalletId): String = key.stringValue
 
-    override suspend fun getSyncOrNull(key: UserWalletId, offset: Int): List<VisaTxHistoryItem>? {
+    override suspend fun getSyncOrNull(key: UserWalletId, cursor: String): List<TangemPayTxHistoryItem>? {
         val storedValue = getSyncOrNull(key)
-        return storedValue?.get(offset)
+        return storedValue?.get(cursor)
     }
 
-    override suspend fun store(key: UserWalletId, offset: Int, value: List<VisaTxHistoryItem>) {
+    override suspend fun store(key: UserWalletId, cursor: String, value: List<TangemPayTxHistoryItem>) {
         val oldValue = getSyncOrNull(key).orEmpty()
-        val newValue = oldValue.toMutableMap().apply { put(offset, value) }
+        val newValue = oldValue.toMutableMap().apply { put(cursor, value) }
         store(key, newValue)
     }
 }
