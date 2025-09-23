beee7b48db557cdeacab311d34365bc5d7acc365
Tangem <git@tangem.com>
2025-09-23 16:59:15 +0400

    Updated on 2026-06-16

--- domain/visa/src/main/kotlin/com/tangem/domain/pay/model/CustomerInfo.kt
+++ domain/visa/src/main/kotlin/com/tangem/domain/pay/model/CustomerInfo.kt
@@ -1,19 +1,28 @@
 package com.tangem.domain.pay.model
 
+import java.math.BigDecimal
+
 private const val APPROVED_KYC_STATUS = "APPROVED"
 private const val ACTIVE_PI_STATUS = "active"
 
 data class CustomerInfo(
     val productInstance: ProductInstance?,
     val kycStatus: String?,
+    val cardInfo: CardInfo?,
 ) {
+
+    data class ProductInstance(
+        val id: String,
+        val status: String,
+    )
+
+    data class CardInfo(
+        val lastFourDigits: String,
+        val balance: BigDecimal,
+        val currencyCode: String,
+    )
 
     fun isKycApproved() = kycStatus == APPROVED_KYC_STATUS
 
     fun isProductInstanceActive() = productInstance?.status == ACTIVE_PI_STATUS
 }
-
-data class ProductInstance(
-    val id: String,
-    val status: String,
-)
