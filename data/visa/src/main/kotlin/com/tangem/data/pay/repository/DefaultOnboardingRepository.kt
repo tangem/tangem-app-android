c0e2e3d62b22ada7ab2f784c7bcd968ed8035698
Tangem <git@tangem.com>
2025-09-23 16:59:15 +0400

    Updated on 2025-11-26

--- data/visa/src/main/kotlin/com/tangem/data/pay/repository/DefaultOnboardingRepository.kt
+++ data/visa/src/main/kotlin/com/tangem/data/pay/repository/DefaultOnboardingRepository.kt
@@ -5,8 +5,10 @@ import arrow.core.raise.either
 import com.tangem.core.error.UniversalError
 import com.tangem.datasource.api.pay.TangemPayApi
 import com.tangem.datasource.api.pay.models.request.DeeplinkValidityRequest
+import com.tangem.datasource.api.pay.models.response.CustomerMeResponse
+import com.tangem.domain.pay.model.CustomerInfo.CardInfo
 import com.tangem.domain.pay.model.CustomerInfo
-import com.tangem.domain.pay.model.ProductInstance
+import com.tangem.domain.pay.model.CustomerInfo.ProductInstance
 import com.tangem.domain.pay.repository.OnboardingRepository
 import javax.inject.Inject
 
@@ -26,11 +28,31 @@ internal class DefaultOnboardingRepository @Inject constructor(
     override suspend fun getCustomerInfo(): Either<UniversalError, CustomerInfo> = either {
         return requestHelper.request { authHeader ->
             tangemPayApi.getCustomerMe(authHeader)
-        }.map {
-            CustomerInfo(
-                productInstance = it.result?.productInstance?.let { ProductInstance(id = it.id, status = it.status) },
-                kycStatus = it.result?.kyc?.status,
+        }.map { getCustomerInfo(it.result) }
+    }
+
+    override suspend fun getMainScreenCustomerInfo(): Either<UniversalError, CustomerInfo> = either {
+        return requestHelper.requestWithPersistedToken { authHeader ->
+            tangemPayApi.getCustomerMe(authHeader)
+        }.map { getCustomerInfo(it.result) }
+    }
+
+    private fun getCustomerInfo(response: CustomerMeResponse.Result?): CustomerInfo {
+        val card = response?.card
+        val balance = response?.balance
+        val cardInfo = if (card != null && balance != null) {
+            CardInfo(
+                lastFourDigits = card.cardNumberEnd,
+                balance = balance.availableBalance,
+                currencyCode = balance.currency,
             )
+        } else {
+            null
         }
+        return CustomerInfo(
+            productInstance = response?.productInstance?.let { ProductInstance(id = it.id, status = it.status) },
+            kycStatus = response?.kyc?.status,
+            cardInfo = cardInfo,
+        )
     }
 }
