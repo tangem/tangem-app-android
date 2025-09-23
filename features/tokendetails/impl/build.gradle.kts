9c64d3d28e8ea53715c0d452498ccf1ff69633d5
Tangem <git@tangem.com>
2025-09-23 18:02:14 +0500

    Updated on 2025-11-26

--- features/tokendetails/impl/build.gradle.kts
+++ features/tokendetails/impl/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(deps.plugins.android.library)
+    alias(deps.plugins.kotlin.serialization)
     alias(deps.plugins.kotlin.android)
     alias(deps.plugins.kotlin.kapt)
     alias(deps.plugins.hilt.android)
