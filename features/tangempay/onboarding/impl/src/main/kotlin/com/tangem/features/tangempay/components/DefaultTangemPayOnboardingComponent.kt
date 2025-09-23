9f5fbb1c6b8c6a365d72bd543542880caf5da5a7
Tangem <git@tangem.com>
2025-09-23 16:59:15 +0400

    Updated on 2026-07-07

--- features/tangempay/onboarding/impl/src/main/kotlin/com/tangem/features/tangempay/components/DefaultTangemPayOnboardingComponent.kt
+++ features/tangempay/onboarding/impl/src/main/kotlin/com/tangem/features/tangempay/components/DefaultTangemPayOnboardingComponent.kt
@@ -5,33 +5,29 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.tangem.core.decompose.context.AppComponentContext
-import com.tangem.core.decompose.context.child
-import com.tangem.features.kyc.KycComponent
 import com.tangem.core.decompose.model.getOrCreateModel
 import com.tangem.features.tangempay.model.TangemPayOnboardingModel
+import com.tangem.features.tangempay.ui.TandemPayOnboardingScreen
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
-import com.tangem.features.tangempay.ui.TandemPayOnboardingScreen
 
 internal class DefaultTangemPayOnboardingComponent @AssistedInject constructor(
     @Assisted appComponentContext: AppComponentContext,
     @Assisted params: TangemPayOnboardingComponent.Params,
-    kycComponentFactory: KycComponent.Factory,
 ) : TangemPayOnboardingComponent, AppComponentContext by appComponentContext {
-
-    private val kycComponent = kycComponentFactory.create(child("kycComponent"))
 
     private val model: TangemPayOnboardingModel = getOrCreateModel(params)
 
     @Composable
     override fun Content(modifier: Modifier) {
         val state by model.screenState.collectAsStateWithLifecycle()
-        TandemPayOnboardingScreen(modifier = modifier, state = state, onButtonClick = ::onButtonClick)
-    }
-
-    private fun onButtonClick() {
-        kycComponent.launch()
+        TandemPayOnboardingScreen(
+            modifier = modifier,
+            state = state,
+            onBackClick = model::back,
+            onOpenKycClick = model::openKyc,
+        )
     }
 
     @AssistedFactory
