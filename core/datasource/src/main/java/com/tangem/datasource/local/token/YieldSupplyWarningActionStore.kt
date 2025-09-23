4d2dfb197b455cded1234db60b3bb2cfb20ed8ce
Tangem <git@tangem.com>
2025-09-23 11:00:57 +0500

    Updated on 2025-11-18

--- core/datasource/src/main/java/com/tangem/datasource/local/token/YieldSupplyWarningActionStore.kt
+++
@@ -1,8 +0,0 @@
-package com.tangem.datasource.local.token
-
-interface YieldSupplyWarningActionStore {
-
-    suspend fun getSync(): Set<String>
-
-    suspend fun store(symbol: String)
-}
--- data/tokens/src/main/kotlin/com/tangem/data/tokens/repository/DefaultYieldSupplyWarningsViewedRepository.kt
+++ data/tokens/src/main/kotlin/com/tangem/data/tokens/repository/DefaultYieldSupplyWarningsViewedRepository.kt
@@ -1,17 +1,36 @@
 package com.tangem.data.tokens.repository
 
-import com.tangem.datasource.local.token.YieldSupplyWarningActionStore
+import com.tangem.datasource.local.preferences.AppPreferencesStore
+import com.tangem.datasource.local.preferences.PreferencesKeys
+import com.tangem.datasource.local.preferences.utils.getObjectSet
 import com.tangem.domain.tokens.repository.YieldSupplyWarningsViewedRepository
+import com.tangem.utils.coroutines.CoroutineDispatcherProvider
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.withContext
 
 internal class DefaultYieldSupplyWarningsViewedRepository(
-    private val yieldSupplyWarningActionStore: YieldSupplyWarningActionStore,
+    private val appPreferencesStore: AppPreferencesStore,
+    private val dispatchers: CoroutineDispatcherProvider,
 ) : YieldSupplyWarningsViewedRepository {
 
-    override suspend fun getViewedWarnings(): Set<String> {
-        return yieldSupplyWarningActionStore.getSync()
+    override suspend fun getViewedWarnings(): Set<String> = withContext(dispatchers.io) {
+        appPreferencesStore.getObjectSet<String>(PreferencesKeys.YIELD_SUPPLY_WARNINGS_STATES_KEY).firstOrNull()
+            ?: emptySet()
     }
 
-    override suspend fun view(symbol: String) {
-        yieldSupplyWarningActionStore.store(symbol)
+    override suspend fun view(symbol: String) = withContext(dispatchers.io) {
+        appPreferencesStore.editData { mutablePreferences ->
+            val stored = mutablePreferences.getObjectSet<String>(
+                PreferencesKeys.YIELD_SUPPLY_WARNINGS_STATES_KEY,
+            ) ?: mutableSetOf()
+
+            val updated = stored + symbol
+
+            mutablePreferences.setObjectSet<String>(
+                key = PreferencesKeys.YIELD_SUPPLY_WARNINGS_STATES_KEY,
+                value = updated,
+            )
+        }
+        return@withContext
     }
 }
