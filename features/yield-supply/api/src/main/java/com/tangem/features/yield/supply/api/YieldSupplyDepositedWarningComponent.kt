74fb3926487599e76580a0af15e84ca1871718cc
Tangem <git@tangem.com>
2025-09-23 18:02:14 +0500

    Updated on 2026-06-02

--- features/yield-supply/api/src/main/java/com/tangem/features/yield/supply/api/YieldSupplyDepositedWarningComponent.kt
+++ features/yield-supply/api/src/main/java/com/tangem/features/yield/supply/api/YieldSupplyDepositedWarningComponent.kt
@@ -4,17 +4,19 @@ import com.tangem.core.decompose.context.AppComponentContext
 import com.tangem.core.decompose.factory.ComponentFactory
 import com.tangem.core.ui.decompose.ComposableBottomSheetComponent
 import com.tangem.domain.models.currency.CryptoCurrency
+import com.tangem.domain.tokens.model.details.TokenAction
 
 interface YieldSupplyDepositedWarningComponent : ComposableBottomSheetComponent {
 
     data class Params(
         val cryptoCurrency: CryptoCurrency,
+        val tokenAction: TokenAction,
         val modelCallback: ModelCallback,
         val onDismiss: () -> Unit,
     )
 
     interface ModelCallback {
-        fun onYieldSupplyWarningAcknowledged()
+        fun onYieldSupplyWarningAcknowledged(tokenAction: TokenAction)
     }
 
     interface Factory : ComponentFactory<Params, YieldSupplyDepositedWarningComponent> {
