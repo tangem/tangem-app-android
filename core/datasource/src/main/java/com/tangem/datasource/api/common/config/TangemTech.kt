7f8aa23ba7f9725cb8cc2818cc38ba4f7d20ee7b
Tangem <git@tangem.com>
2025-09-24 12:20:23 +0500

    Updated on 2025-12-05

--- core/datasource/src/main/java/com/tangem/datasource/api/common/config/TangemTech.kt
+++ core/datasource/src/main/java/com/tangem/datasource/api/common/config/TangemTech.kt
@@ -2,12 +2,15 @@ package com.tangem.datasource.api.common.config
 
 import com.tangem.datasource.BuildConfig
 import com.tangem.datasource.api.common.AuthProvider
+import com.tangem.datasource.local.config.environment.EnvironmentConfigStorage
 import com.tangem.datasource.utils.RequestHeader
+import com.tangem.utils.ProviderSuspend
 import com.tangem.utils.info.AppInfoProvider
 import com.tangem.utils.version.AppVersionProvider
 
 /** TangemTech [ApiConfig] */
 internal class TangemTech(
+    private val environmentConfigStorage: EnvironmentConfigStorage,
     private val appVersionProvider: AppVersionProvider,
     private val authProvider: AuthProvider,
     private val appInfoProvider: AppInfoProvider,
@@ -38,29 +41,41 @@ internal class TangemTech(
     private fun createDevEnvironment(): ApiEnvironmentConfig = ApiEnvironmentConfig(
         environment = ApiEnvironment.DEV,
         baseUrl = "[REDACTED_ENV_URL]",
-        headers = createHeaders(),
+        headers = createHeaders(ApiEnvironment.DEV),
     )
 
     private fun createStageEnvironment(): ApiEnvironmentConfig = ApiEnvironmentConfig(
         environment = ApiEnvironment.STAGE,
         baseUrl = "[REDACTED_ENV_URL]",
-        headers = createHeaders(),
+        headers = createHeaders(ApiEnvironment.STAGE),
     )
 
     private fun createMockedEnvironment(): ApiEnvironmentConfig = ApiEnvironmentConfig(
         environment = ApiEnvironment.MOCK,
         baseUrl = "[REDACTED_ENV_URL]",
-        headers = createHeaders(),
+        headers = createHeaders(ApiEnvironment.MOCK),
     )
 
     private fun createProdEnvironment(): ApiEnvironmentConfig = ApiEnvironmentConfig(
         environment = ApiEnvironment.PROD,
         baseUrl = "https://api.tangem.org/",
-        headers = createHeaders(),
+        headers = createHeaders(ApiEnvironment.PROD),
     )
 
-    private fun createHeaders() = buildMap {
+    private fun createHeaders(apiEnvironment: ApiEnvironment) = buildMap {
+        put(key = "api-key", value = ProviderSuspend { getApiKey(apiEnvironment) })
         putAll(from = RequestHeader.AppVersionPlatformHeaders(appVersionProvider, appInfoProvider).values)
         putAll(from = RequestHeader.AuthenticationHeader(authProvider).values)
+    }
+
+    private fun getApiKey(apiEnvironment: ApiEnvironment): String {
+        return when (apiEnvironment) {
+            ApiEnvironment.MOCK -> null
+            ApiEnvironment.DEV,
+            ApiEnvironment.DEV_2,
+            -> environmentConfigStorage.getConfigSync().tangemApiKeyDev
+            ApiEnvironment.STAGE -> environmentConfigStorage.getConfigSync().tangemApiKeyStage
+            ApiEnvironment.PROD -> environmentConfigStorage.getConfigSync().tangemApiKey
+        } ?: error("No tangem tech api config provided")
     }
 }
