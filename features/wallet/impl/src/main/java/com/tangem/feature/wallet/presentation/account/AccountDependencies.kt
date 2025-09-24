e34cda50e728e05fd50350105dad3ad0b34ced3a
Tangem <git@tangem.com>
2025-09-24 16:32:09 +0700

    Updated on 2026-03-27

---
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/account/AccountDependencies.kt
@@ -0,0 +1,15 @@
+package com.tangem.feature.wallet.presentation.account
+
+import com.tangem.core.decompose.di.ModelScoped
+import com.tangem.domain.account.featuretoggle.AccountsFeatureToggles
+import com.tangem.domain.account.status.supplier.SingleAccountStatusListSupplier
+import com.tangem.domain.account.usecase.IsAccountsModeEnabledUseCase
+import javax.inject.Inject
+
+@ModelScoped
+internal class AccountDependencies @Inject constructor(
+    val accountsFeatureToggles: AccountsFeatureToggles,
+    val isAccountsModeEnabledUseCase: IsAccountsModeEnabledUseCase,
+    val expandedAccountsHolder: ExpandedAccountsHolder,
+    val singleAccountStatusListSupplier: SingleAccountStatusListSupplier,
+)
