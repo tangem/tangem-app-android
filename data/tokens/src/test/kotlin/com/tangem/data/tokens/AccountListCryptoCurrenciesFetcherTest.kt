10287ae1eaaec5c85f8050b174af6d77db47ad15
Tangem <git@tangem.com>
2025-09-25 16:06:15 +0400

    Updated on 2026-09-25

---
+++ data/tokens/src/test/kotlin/com/tangem/data/tokens/AccountListCryptoCurrenciesFetcherTest.kt
@@ -0,0 +1,103 @@
+package com.tangem.data.tokens
+
+import arrow.core.left
+import com.tangem.common.test.utils.assertEither
+import com.tangem.common.test.utils.assertEitherRight
+import com.tangem.data.common.account.WalletAccountsFetcher
+import com.tangem.datasource.local.userwallet.UserWalletsStore
+import com.tangem.domain.models.wallet.UserWallet
+import com.tangem.domain.models.wallet.UserWalletId
+import com.tangem.domain.models.wallet.isMultiCurrency
+import com.tangem.domain.tokens.MultiWalletCryptoCurrenciesFetcher
+import com.tangem.utils.coroutines.TestingCoroutineDispatcherProvider
+import io.mockk.*
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class AccountListCryptoCurrenciesFetcherTest {
+
+    private val userWalletsStore: UserWalletsStore = mockk(relaxUnitFun = true)
+    private val walletAccountsFetcher: WalletAccountsFetcher = mockk(relaxUnitFun = true)
+    private val dispatchers = TestingCoroutineDispatcherProvider()
+
+    private val fetcher = AccountListCryptoCurrenciesFetcher(
+        userWalletsStore = userWalletsStore,
+        walletAccountsFetcher = walletAccountsFetcher,
+        dispatchers = dispatchers,
+    )
+
+    @BeforeEach
+    fun resetMocks() {
+        clearMocks(userWalletsStore, walletAccountsFetcher)
+    }
+
+    @Test
+    fun `returns failure if wallet is not multi-currency`() = runTest {
+        // Arrange
+        val params = MultiWalletCryptoCurrenciesFetcher.Params(userWalletId = userWalletId)
+        val mockUserWallet = mockk<UserWallet> { every { isMultiCurrency } returns false }
+        every { userWalletsStore.getSyncStrict(key = params.userWalletId) } returns mockUserWallet
+
+        // Act
+        val actual = fetcher(params)
+
+        // Assert
+        val expected = IllegalStateException(
+            "${AccountListCryptoCurrenciesFetcher::class.simpleName} supports only multi-currency wallet",
+        ).left()
+        assertEither(actual, expected)
+
+        verify { userWalletsStore.getSyncStrict(key = params.userWalletId) }
+        coVerify(inverse = true) { walletAccountsFetcher.fetch(any()) }
+    }
+
+    @Test
+    fun `returns accounts if wallet is multi-currency`() = runTest {
+        // Arrange
+        val params = MultiWalletCryptoCurrenciesFetcher.Params(userWalletId = userWalletId)
+        val mockUserWallet = mockk<UserWallet> { every { isMultiCurrency } returns true }
+
+        every { userWalletsStore.getSyncStrict(key = params.userWalletId) } returns mockUserWallet
+
+        // Act
+        val actual = fetcher(params)
+
+        // Assert
+        assertEitherRight(actual)
+
+        coVerify(ordering = Ordering.SEQUENCE) {
+            userWalletsStore.getSyncStrict(key = params.userWalletId)
+            walletAccountsFetcher.fetch(userWalletId = params.userWalletId)
+        }
+    }
+
+    @Test
+    fun `returns error if walletAccountsFetcher returns error`() = runTest {
+        // Arrange
+        val params = MultiWalletCryptoCurrenciesFetcher.Params(userWalletId = userWalletId)
+        val mockUserWallet = mockk<UserWallet> { every { isMultiCurrency } returns true }
+        val error = RuntimeException("fetch error")
+
+        every { userWalletsStore.getSyncStrict(key = params.userWalletId) } returns mockUserWallet
+        coEvery { walletAccountsFetcher.fetch(userWalletId = params.userWalletId) } throws error
+
+        // Act
+        val actual = fetcher(params)
+
+        // Assert
+        val expected = error.left()
+        assertEither(actual, expected)
+
+        coVerify(ordering = Ordering.SEQUENCE) {
+            userWalletsStore.getSyncStrict(key = params.userWalletId)
+            walletAccountsFetcher.fetch(userWalletId = params.userWalletId)
+        }
+    }
+
+    private companion object {
+        val userWalletId = UserWalletId("012")
+    }
+}
