f7cc8fb3e881dc097a07f88989a96c53a7b32349
Tangem <git@tangem.com>
2025-09-25 13:35:18 +0400

    Updated on 2026-05-14

--- domain/account/src/main/java/com/tangem/domain/account/models/AccountStatusList.kt
+++ domain/account/src/main/java/com/tangem/domain/account/models/AccountStatusList.kt
@@ -8,9 +8,10 @@ import kotlinx.serialization.Serializable
 /**
  * Represents a list of account statuses associated with a user wallet
  *
- * @property userWallet      the user wallet to which the account statuses belong
- * @property accountStatuses a set of account statuses associated with the user wallet
- * @property totalAccounts   the total number of accounts
+ * @property userWallet       the user wallet to which the account statuses belong
+ * @property accountStatuses  a set of account statuses associated with the user wallet
+ * @property totalAccounts    the total number of accounts (including archived ones)
+ * @property totalFiatBalance the total fiat balance across all accounts
  *
 [REDACTED_AUTHOR]
  */
@@ -19,7 +20,7 @@ data class AccountStatusList(
     val userWallet: UserWallet,
     val accountStatuses: Set<AccountStatus>,
     val totalAccounts: Int,
-    val totalFiatBalance: TotalFiatBalance = TotalFiatBalance.Failed,
+    val totalFiatBalance: TotalFiatBalance,
 ) {
 
     val mainAccount: AccountStatus
