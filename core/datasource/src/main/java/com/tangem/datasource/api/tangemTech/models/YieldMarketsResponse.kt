7884e3b0bf4a5387dabdb4a76c9d9a26922514b2
Tangem <git@tangem.com>
2025-09-25 18:27:06 +0500

    Updated on 2026-06-17

---
+++ core/datasource/src/main/java/com/tangem/datasource/api/tangemTech/models/YieldMarketsResponse.kt
@@ -0,0 +1,28 @@
+package com.tangem.datasource.api.tangemTech.models
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import java.math.BigDecimal
+
+@JsonClass(generateAdapter = true)
+data class YieldMarketsResponse(
+    @Json(name = "markets") val marketDtos: List<MarketDto>,
+    @Json(name = "lastUpdated") val lastUpdated: String,
+) {
+
+    @JsonClass(generateAdapter = true)
+    data class MarketDto(
+        @Json(name = "tokenAddress") val tokenAddress: String,
+        @Json(name = "tokenSymbol") val tokenSymbol: String,
+        @Json(name = "tokenName") val tokenName: String,
+        @Json(name = "apy") val apy: BigDecimal,
+        @Json(name = "totalSupplied") val totalSupplied: String,
+        @Json(name = "totalBorrowed") val totalBorrowed: String,
+        @Json(name = "liquidityRate") val liquidityRate: String,
+        @Json(name = "borrowRate") val borrowRate: String,
+        @Json(name = "utilizationRate") val utilizationRate: BigDecimal,
+        @Json(name = "isActive") val isActive: Boolean,
+        @Json(name = "ltv") val ltv: BigDecimal,
+        @Json(name = "liquidationThreshold") val liquidationThreshold: BigDecimal,
+    )
+}
