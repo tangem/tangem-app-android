0b6d998910775fe95554142b57b20c2a394e208f
Tangem <git@tangem.com>
2025-09-26 20:48:51 +0500

    Updated on 2025-12-22

--- domain/yield-supply/src/main/java/com/tangem/domain/yield/supply/usecase/YieldSupplyStartEarningUseCase.kt
+++ domain/yield-supply/src/main/java/com/tangem/domain/yield/supply/usecase/YieldSupplyStartEarningUseCase.kt
@@ -4,22 +4,25 @@ import arrow.core.Either
 import com.tangem.blockchain.common.TransactionData
 import com.tangem.domain.models.currency.CryptoCurrencyStatus
 import com.tangem.domain.models.wallet.UserWalletId
+import com.tangem.domain.yield.supply.YieldSupplyError
+import com.tangem.domain.yield.supply.YieldSupplyErrorResolver
 import com.tangem.domain.yield.supply.YieldSupplyTransactionRepository
 import java.math.BigDecimal
 
 class YieldSupplyStartEarningUseCase(
     private val yieldSupplyTransactionRepository: YieldSupplyTransactionRepository,
+    private val yieldSupplyErrorResolver: YieldSupplyErrorResolver,
 ) {
 
     suspend operator fun invoke(
         userWalletId: UserWalletId,
         cryptoCurrencyStatus: CryptoCurrencyStatus,
         maxNetworkFee: BigDecimal,
-    ): Either<Throwable, List<TransactionData.Uncompiled>> = Either.catch {
+    ): Either<YieldSupplyError, List<TransactionData.Uncompiled>> = Either.catch {
         yieldSupplyTransactionRepository.createEnterTransactions(
             userWalletId = userWalletId,
             cryptoCurrencyStatus = cryptoCurrencyStatus,
             maxNetworkFee = maxNetworkFee,
         )
-    }
+    }.mapLeft(yieldSupplyErrorResolver::resolve)
 }
