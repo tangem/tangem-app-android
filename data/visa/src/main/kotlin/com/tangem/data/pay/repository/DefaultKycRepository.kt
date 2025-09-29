0b12dce037e6ff9b51421bf273e13cf7187ddce0
Tangem <git@tangem.com>
2025-09-29 12:29:17 +0400

    Updated on 2026-05-13

--- data/visa/src/main/kotlin/com/tangem/data/pay/repository/DefaultKycRepository.kt
+++ data/visa/src/main/kotlin/com/tangem/data/pay/repository/DefaultKycRepository.kt
@@ -1,23 +1,26 @@
 package com.tangem.data.pay.repository
 
+import arrow.core.Either
+import com.tangem.core.error.UniversalError
 import com.tangem.datasource.api.pay.TangemPayApi
 import com.tangem.domain.pay.KycStartInfo
 import com.tangem.domain.pay.repository.KycRepository
-import com.tangem.utils.coroutines.CoroutineDispatcherProvider
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
+
+private const val TAG = "TangemPay: KycRepository"
 
 internal class DefaultKycRepository @Inject constructor(
     private val tangemPayApi: TangemPayApi,
-    private val dispatchers: CoroutineDispatcherProvider,
     private val requestHelper: TangemPayRequestPerformer,
 ) : KycRepository {
 
-    override suspend fun getKycStartInfo() = withContext(dispatchers.io) {
-        requestHelper.request { authHeader ->
-            tangemPayApi.getKycAccess(authHeader = authHeader)
-        }.map {
-            KycStartInfo(token = it.result.token, locale = it.result.locale)
+    override suspend fun getKycStartInfo(): Either<UniversalError, KycStartInfo> {
+        return requestHelper.runWithErrorLogs(TAG) {
+            val result = requestHelper.request { authHeader ->
+                tangemPayApi.getKycAccess(authHeader = authHeader)
+            }.result
+
+            KycStartInfo(token = result.token, locale = result.locale)
         }
     }
 }
