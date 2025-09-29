983a82754e89bfba1ca2dd0c7cb60a1142d83e67
Tangem <git@tangem.com>
2025-09-29 15:59:01 +0700

    Updated on 2026-08-19

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/transformers/TokenConverterParams.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/transformers/TokenConverterParams.kt
@@ -1,11 +1,13 @@
 package com.tangem.feature.wallet.presentation.wallet.state.transformers
 
 import com.tangem.domain.account.models.AccountStatusList
+import com.tangem.domain.models.PortfolioId
 import com.tangem.domain.models.account.AccountId
 import com.tangem.domain.models.tokenlist.TokenList
 
 sealed interface TokenConverterParams {
     data class Wallet(
+        val portfolioId: PortfolioId,
         val tokenList: TokenList,
     ) : TokenConverterParams
 
