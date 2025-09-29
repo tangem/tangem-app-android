15d473c13cc4a009b1f3463aa419a3b27613a232
Tangem <git@tangem.com>
2025-09-29 16:40:30 +0400

    Updated on 2026-04-06

---
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/transformers/TangemPayStateTransformer.kt
@@ -0,0 +1,74 @@
+package com.tangem.feature.wallet.presentation.wallet.state.transformers
+
+import com.tangem.common.ui.R
+import com.tangem.core.ui.extensions.TextReference
+import com.tangem.core.ui.format.bigdecimal.fiat
+import com.tangem.core.ui.format.bigdecimal.format
+import com.tangem.domain.pay.model.CustomerInfo.CardInfo
+import com.tangem.domain.pay.model.MainScreenCustomerInfo
+import com.tangem.domain.pay.model.OrderStatus.NOT_ISSUED
+import com.tangem.domain.pay.model.OrderStatus.CANCELED
+import com.tangem.feature.wallet.presentation.wallet.state.model.TangemPayState
+import com.tangem.feature.wallet.presentation.wallet.state.model.TangemPayState.Progress
+import com.tangem.utils.transformer.Transformer
+import java.util.Currency
+
+internal class TangemPayStateTransformer(
+    private val value: MainScreenCustomerInfo? = null,
+    private val onIssueOrderClick: () -> Unit = {},
+    private val onContinueKycClick: () -> Unit = {},
+    private val issueProgressState: Boolean = false,
+    private val issueState: Boolean = false,
+) : Transformer<TangemPayState> {
+
+    override fun transform(prevState: TangemPayState): TangemPayState = when {
+        issueProgressState -> createIssueProgressState()
+        issueState -> createIssueState()
+        else -> createInitialState()
+    }
+
+    private fun createInitialState(): TangemPayState {
+        val cardInfo = value?.info?.cardInfo
+        return when {
+            value == null -> TangemPayState.Empty
+            !value.info.isKycApproved() -> createKycInProgressState(onContinueKycClick)
+            value.orderStatus == NOT_ISSUED || value.orderStatus == CANCELED -> createIssueState()
+            cardInfo != null -> getCardInfoState(cardInfo)
+            else -> createIssueProgressState()
+        }
+    }
+
+    private fun createIssueProgressState(): TangemPayState = Progress(
+        title = TextReference.Res(R.string.tangempay_issue_card_notification_title),
+        buttonText = TextReference.EMPTY,
+        iconRes = R.drawable.ic_tangem_pay_promo_card_36,
+        onButtonClick = {},
+        showProgress = true,
+    )
+
+    private fun createIssueState() = Progress(
+        title = TextReference.Res(R.string.tangempay_issue_card_notification_title),
+        buttonText = TextReference.Res(R.string.common_continue),
+        iconRes = R.drawable.ic_tangem_pay_promo_card_36,
+        onButtonClick = onIssueOrderClick,
+    )
+
+    private fun createKycInProgressState(onContinueKycClick: () -> Unit): TangemPayState = Progress(
+        title = TextReference.Res(R.string.tangempay_kyc_in_progress_notification_title),
+        buttonText = TextReference.Res(R.string.tangempay_kyc_in_progress_notification_button),
+        iconRes = R.drawable.ic_promo_kyc_36,
+        onButtonClick = onContinueKycClick,
+    )
+
+    private fun getCardInfoState(cardInfo: CardInfo): TangemPayState = TangemPayState.Card(
+        lastFourDigits = TextReference.Str("*${cardInfo.lastFourDigits}"),
+        balanceText = TextReference.Str(getBalanceText(cardInfo)),
+    )
+
+    private fun getBalanceText(cardInfo: CardInfo): String {
+        val currency = Currency.getInstance(cardInfo.currencyCode)
+        return cardInfo.balance.format {
+            fiat(fiatCurrencyCode = currency.currencyCode, fiatCurrencySymbol = currency.symbol)
+        }
+    }
+}
