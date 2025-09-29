6777ff835aca416999b00209b4ddf8cdd397ff54
Tangem <git@tangem.com>
2025-09-29 15:58:48 +0700

    Updated on 2026-01-24

---
+++ features/account/impl/src/main/java/com/tangem/features/account/selector/entity/PortfolioSelectorUM.kt
@@ -0,0 +1,27 @@
+package com.tangem.features.account.selector.entity
+
+import androidx.compose.runtime.Immutable
+import com.tangem.common.ui.userwallet.state.UserWalletItemUM
+import com.tangem.core.ui.extensions.TextReference
+import kotlinx.collections.immutable.ImmutableList
+
+data class PortfolioSelectorUM(
+    val title: TextReference,
+    val items: ImmutableList<PortfolioSelectorItemUM>,
+)
+
+@Immutable
+sealed interface PortfolioSelectorItemUM {
+    val id: String
+
+    data class GroupTitle(
+        override val id: String,
+        val name: TextReference,
+    ) : PortfolioSelectorItemUM
+
+    data class Portfolio(
+        val item: UserWalletItemUM,
+    ) : PortfolioSelectorItemUM {
+        override val id: String = item.id.stringValue
+    }
+}
