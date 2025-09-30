86cc20319fe3a637214aad5809fbc4ee2dab5d81
Tangem <git@tangem.com>
2025-09-30 11:59:23 +0400

    Updated on 2026-06-12

--- common/routing/src/main/kotlin/com/tangem/common/routing/AppRoute.kt
+++ common/routing/src/main/kotlin/com/tangem/common/routing/AppRoute.kt
@@ -439,7 +439,7 @@ sealed class AppRoute(val path: String) : Route {
     ) : AppRoute(path = "/archived_account/${userWalletId.stringValue}")
 
     @Serializable
-    data class TangemPayDetails(val userWalletId: UserWalletId) : AppRoute(path = "/tangem_pay_details")
+    data class TangemPayDetails(val customerWalletAddress: String) : AppRoute(path = "/tangem_pay_details")
 
     @Serializable
     data class TangemPayOnboarding(
