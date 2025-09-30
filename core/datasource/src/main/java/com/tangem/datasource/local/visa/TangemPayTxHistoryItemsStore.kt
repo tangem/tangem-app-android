631ba350d3958a8652fac56c26e350976c31bec3
Tangem <git@tangem.com>
2025-09-30 11:59:23 +0400

    Updated on 2026-09-01

--- core/datasource/src/main/java/com/tangem/datasource/local/visa/TangemPayTxHistoryItemsStore.kt
+++ core/datasource/src/main/java/com/tangem/datasource/local/visa/TangemPayTxHistoryItemsStore.kt
@@ -1,13 +1,12 @@
 package com.tangem.datasource.local.visa
 
-import com.tangem.domain.models.wallet.UserWalletId
 import com.tangem.domain.visa.model.TangemPayTxHistoryItem
 
 interface TangemPayTxHistoryItemsStore {
 
-    suspend fun getSyncOrNull(key: UserWalletId, cursor: String): List<TangemPayTxHistoryItem>?
+    suspend fun getSyncOrNull(key: String, cursor: String): List<TangemPayTxHistoryItem>?
 
-    suspend fun remove(key: UserWalletId)
+    suspend fun remove(key: String)
 
-    suspend fun store(key: UserWalletId, cursor: String, value: List<TangemPayTxHistoryItem>)
+    suspend fun store(key: String, cursor: String, value: List<TangemPayTxHistoryItem>)
 }
