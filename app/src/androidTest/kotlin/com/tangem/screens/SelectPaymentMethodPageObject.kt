1c88ef675005a8a321ad0b4289da81d9d243ed0b
Tangem <git@tangem.com>
2025-09-30 11:39:17 +0300

    Updated on 2026-09-14

--- app/src/androidTest/kotlin/com/tangem/screens/SelectPaymentMethodPageObject.kt
+++ app/src/androidTest/kotlin/com/tangem/screens/SelectPaymentMethodPageObject.kt
@@ -1,17 +1,14 @@
 package com.tangem.screens
 
-import androidx.compose.ui.test.SemanticsMatcher
 import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
 import com.tangem.common.BaseTestCase
-import com.tangem.common.utils.LazyListItemNode
+import com.tangem.core.ui.test.BaseBottomSheetTestTags
+import com.tangem.core.ui.test.OnrampOffersBlockTestTags
 import com.tangem.core.ui.test.SelectPaymentMethodBottomSheetTestTags
-import com.tangem.core.ui.test.TopAppBarTestTags
-import com.tangem.core.ui.utils.LazyListItemPositionSemantics
 import com.tangem.features.onramp.impl.R
 import io.github.kakaocup.compose.node.element.ComposeScreen
 import io.github.kakaocup.compose.node.element.ComposeScreen.Companion.onComposeScreen
 import io.github.kakaocup.compose.node.element.KNode
-import io.github.kakaocup.compose.node.element.lazylist.KLazyListNode
 import io.github.kakaocup.kakao.common.utilities.getResourceString
 import androidx.compose.ui.test.hasTestTag as withTestTag
 import androidx.compose.ui.test.hasText as withText
@@ -19,30 +16,31 @@ import androidx.compose.ui.test.hasText as withText
 class SelectPaymentMethodPageObject(semanticsProvider: SemanticsNodeInteractionsProvider) :
     ComposeScreen<SelectPaymentMethodPageObject>(semanticsProvider = semanticsProvider) {
 
-
-    private val lazyList = KLazyListNode(
-        semanticsProvider = semanticsProvider,
-        viewBuilderAction = { hasTestTag(SelectPaymentMethodBottomSheetTestTags.LAZY_LIST) },
-        itemTypeBuilder = { itemType(::LazyListItemNode) },
-        positionMatcher = { position ->
-            SemanticsMatcher.expectValue(
-                LazyListItemPositionSemantics,
-                position
-            )
-        }
-    )
-
     val title: KNode = child {
-        hasTestTag(TopAppBarTestTags.TITLE)
-        hasText(getResourceString(R.string.onramp_pay_with))
+        hasTestTag(BaseBottomSheetTestTags.TITLE)
+        hasText(getResourceString(R.string.onramp_all_offers_button_title))
     }
 
-    fun paymentMethodWithNameAndIcon(name: String): KNode {
-        return lazyList.child<KNode> {
-            hasAnyDescendant(withText(name))
-            hasAnyDescendant(withTestTag(SelectPaymentMethodBottomSheetTestTags.PAYMENT_METHOD_ICON))
-            useUnmergedTree = true
-        }
+    val subtitle: KNode = child {
+        hasTestTag(BaseBottomSheetTestTags.SUBTITLE)
+        hasText(getResourceString(R.string.onramp_payment_method_subtitle))
+    }
+
+    val closeButton: KNode = child {
+        hasTestTag(BaseBottomSheetTestTags.CLOSE_BUTTON)
+    }
+
+    fun paymentMethodWithName(name: String): KNode = child {
+        hasTestTag(SelectPaymentMethodBottomSheetTestTags.PAYMENT_METHOD)
+        hasAnyDescendant(withText(name))
+        hasAnyDescendant(withTestTag(SelectPaymentMethodBottomSheetTestTags.PAYMENT_METHOD_ICON))
+        hasAnyDescendant(withTestTag(SelectPaymentMethodBottomSheetTestTags.TIMING_ICON))
+        hasAnyDescendant(withTestTag(OnrampOffersBlockTestTags.TIMING_TEXT))
+        hasAnyDescendant(withTestTag(SelectPaymentMethodBottomSheetTestTags.TOKEN_AMOUNT))
+        hasAnyDescendant(withTestTag(SelectPaymentMethodBottomSheetTestTags.PROVIDER_COUNT_ICON))
+        hasAnyDescendant(withTestTag(SelectPaymentMethodBottomSheetTestTags.PROVIDER_COUNT_TEXT))
+        hasAnyDescendant(withTestTag(SelectPaymentMethodBottomSheetTestTags.UP_TO_TEXT))
+        useUnmergedTree = true
     }
 }
 
