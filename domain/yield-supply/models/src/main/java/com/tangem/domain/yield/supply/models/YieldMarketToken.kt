bf6f75ca4d57bf0a2fbe08465f77b6a805847081
Tangem <git@tangem.com>
2025-10-02 14:06:30 +0500

    Updated on 2026-09-21

--- domain/yield-supply/models/src/main/java/com/tangem/domain/yield/supply/models/YieldMarketToken.kt
+++ domain/yield-supply/models/src/main/java/com/tangem/domain/yield/supply/models/YieldMarketToken.kt
@@ -12,12 +12,5 @@ data class YieldMarketToken(
     val tokenSymbol: String,
     val tokenName: String,
     val apy: SerializedBigDecimal,
-    val totalSupplied: String,
-    val totalBorrowed: String,
-    val liquidityRate: String,
-    val borrowRate: String,
-    val utilizationRate: SerializedBigDecimal,
     val isActive: Boolean,
-    val ltv: SerializedBigDecimal,
-    val liquidationThreshold: SerializedBigDecimal,
 )
