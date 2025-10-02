2c4c7576cc57ea042de45f946666059bb87f1724
Tangem <git@tangem.com>
2025-10-02 14:06:30 +0500

    Updated on 2026-09-25

--- domain/yield-supply/src/main/java/com/tangem/domain/yield/supply/usecase/YieldSupplyGetTokenStatusUseCase.kt
+++ domain/yield-supply/src/main/java/com/tangem/domain/yield/supply/usecase/YieldSupplyGetTokenStatusUseCase.kt
@@ -3,13 +3,13 @@ package com.tangem.domain.yield.supply.usecase
 import arrow.core.Either
 import com.tangem.domain.models.currency.CryptoCurrency
 import com.tangem.domain.yield.supply.YieldSupplyMarketRepository
-import com.tangem.domain.yield.supply.models.YieldTokenStatus
+import com.tangem.domain.yield.supply.models.YieldMarketToken
 
 class YieldSupplyGetTokenStatusUseCase(
     private val yieldSupplyMarketRepository: YieldSupplyMarketRepository,
 ) {
 
-    suspend operator fun invoke(token: CryptoCurrency.Token): Either<Throwable, YieldTokenStatus> = Either.catch {
+    suspend operator fun invoke(token: CryptoCurrency.Token): Either<Throwable, YieldMarketToken> = Either.catch {
         yieldSupplyMarketRepository.getTokenStatus(token.contractAddress)
     }
 }
