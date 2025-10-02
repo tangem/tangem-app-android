adac5c4a82b11601c129f360787077e0a09c4a64
Tangem <git@tangem.com>
2025-10-02 14:06:30 +0500

    Updated on 2026-07-07

--- data/yield-supply/src/main/java/com/tangem/data/yield/supply/converters/YieldTokenStatusConverter.kt
+++ data/yield-supply/src/main/java/com/tangem/data/yield/supply/converters/YieldTokenStatusConverter.kt
@@ -1,29 +1,17 @@
 package com.tangem.data.yield.supply.converters
 
 import com.tangem.datasource.api.tangemTech.models.YieldTokenStatusResponse
-import com.tangem.domain.yield.supply.models.YieldTokenStatus
+import com.tangem.domain.yield.supply.models.YieldMarketToken
 import com.tangem.utils.converter.Converter
 
-internal object YieldTokenStatusConverter : Converter<YieldTokenStatusResponse, YieldTokenStatus> {
-    override fun convert(value: YieldTokenStatusResponse): YieldTokenStatus {
-        return YieldTokenStatus(
+internal object YieldTokenStatusConverter : Converter<YieldTokenStatusResponse, YieldMarketToken> {
+    override fun convert(value: YieldTokenStatusResponse): YieldMarketToken {
+        return YieldMarketToken(
             tokenAddress = value.tokenAddress,
             tokenSymbol = value.tokenSymbol,
             tokenName = value.tokenName,
             apy = value.apy,
-            totalSupplied = value.totalSupplied,
-            totalBorrowed = value.totalBorrowed,
-            liquidityRate = value.liquidityRate,
-            borrowRate = value.borrowRate,
-            utilizationRate = value.utilizationRate,
             isActive = value.isActive,
-            ltv = value.ltv,
-            liquidationThreshold = value.liquidationThreshold,
-            decimals = value.decimals,
-            chainId = value.chainId,
-            priority = value.priority,
-            isEnabled = value.isEnabled,
-            lastUpdatedAt = value.lastUpdatedAt,
         )
     }
 }
