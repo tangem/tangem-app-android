f713e8d8f6b7cba8d26ca9479ba9356258c391c3
Tangem <git@tangem.com>
2025-10-02 13:30:24 +0500

    Updated on 2026-08-19

---
+++ features/yield-supply/impl/src/main/java/com/tangem/features/yield/supply/impl/chart/model/YieldSupplyChartModel.kt
@@ -0,0 +1,36 @@
+package com.tangem.features.yield.supply.impl.chart.model
+
+import com.tangem.core.decompose.di.ModelScoped
+import com.tangem.core.decompose.model.Model
+import com.tangem.core.decompose.model.ParamsContainer
+import com.tangem.features.yield.supply.impl.chart.entity.YieldSupplyChartUM
+import com.tangem.features.yield.supply.impl.chart.entity.YieldSupplyMarketChartData
+import com.tangem.utils.coroutines.CoroutineDispatcherProvider
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@Suppress("UnusedPrivateProperty")
+@ModelScoped
+internal class YieldSupplyChartModel @Inject constructor(
+    paramsContainer: ParamsContainer,
+    override val dispatchers: CoroutineDispatcherProvider,
+) : Model() {
+
+    data class Params(
+        val tokenContractAddress: String,
+        val onRetry: () -> Unit = {},
+    )
+
+    private val params: Params = paramsContainer.require()
+
+    val uiState: StateFlow<YieldSupplyChartUM>
+        field = MutableStateFlow<YieldSupplyChartUM>(YieldSupplyChartUM.Loading)
+
+    init {
+        // TODO [REDACTED_TASK_KEY]
+        val chartData = YieldSupplyMarketChartData.mock()
+        uiState.update { YieldSupplyChartUM.Data(chartData = chartData) }
+    }
+}
