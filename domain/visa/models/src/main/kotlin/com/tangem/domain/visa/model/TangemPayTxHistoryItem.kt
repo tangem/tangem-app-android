6a61569032a956089461c352dc437b23ab5178ab
Tangem <git@tangem.com>
2025-10-03 17:05:44 +0500

    Updated on 2026-02-06

--- domain/visa/models/src/main/kotlin/com/tangem/domain/visa/model/TangemPayTxHistoryItem.kt
+++ domain/visa/models/src/main/kotlin/com/tangem/domain/visa/model/TangemPayTxHistoryItem.kt
@@ -2,14 +2,38 @@ package com.tangem.domain.visa.model
 
 import org.joda.time.DateTime
 import java.math.BigDecimal
+import java.util.Currency
 
-data class TangemPayTxHistoryItem(
-    val id: String,
-    val date: DateTime?,
-    val amount: BigDecimal?,
-    val merchantName: String?,
-    val status: String?,
-    val currency: String?,
-) {
-    val timeStampInMillis: Long = date?.millis ?: 0
+sealed class TangemPayTxHistoryItem {
+    abstract val id: String
+    abstract val date: DateTime
+    abstract val amount: BigDecimal
+    abstract val currency: Currency
+
+    data class Spend(
+        override val id: String,
+        override val date: DateTime,
+        override val amount: BigDecimal,
+        override val currency: Currency,
+        val enrichedMerchantName: String?,
+        val merchantName: String,
+        val enrichedMerchantCategory: String?,
+        val merchantCategory: String,
+        val status: String,
+        val enrichedMerchantIconUrl: String?,
+    ) : TangemPayTxHistoryItem()
+
+    data class Payment(
+        override val id: String,
+        override val date: DateTime,
+        override val amount: BigDecimal,
+        override val currency: Currency,
+    ) : TangemPayTxHistoryItem()
+
+    data class Fee(
+        override val id: String,
+        override val date: DateTime,
+        override val amount: BigDecimal,
+        override val currency: Currency,
+    ) : TangemPayTxHistoryItem()
 }
