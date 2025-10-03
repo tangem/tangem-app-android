876569ab83c8e1e4aa7d06d0c8f5281260addc06
Tangem <git@tangem.com>
2025-10-03 17:05:44 +0500

    Updated on 2026-04-06

--- data/visa/src/main/kotlin/com/tangem/data/visa/utils/TangemPayTxHistoryItemConverter.kt
+++ data/visa/src/main/kotlin/com/tangem/data/visa/utils/TangemPayTxHistoryItemConverter.kt
@@ -3,49 +3,55 @@ package com.tangem.data.visa.utils
 import com.tangem.datasource.api.pay.models.response.TangemPayTxHistoryResponse
 import com.tangem.domain.visa.model.TangemPayTxHistoryItem
 import com.tangem.utils.converter.Converter
+import timber.log.Timber
+import java.util.Currency
 
 internal object TangemPayTxHistoryItemConverter :
-    Converter<TangemPayTxHistoryResponse.Transaction, TangemPayTxHistoryItem> {
+    Converter<TangemPayTxHistoryResponse.Transaction, TangemPayTxHistoryItem?> {
 
-    @Suppress("CyclomaticComplexMethod")
-    override fun convert(value: TangemPayTxHistoryResponse.Transaction): TangemPayTxHistoryItem {
-        val spend = value.spend
-        val collateral = value.collateral
-        val payment = value.payment
-        val fee = value.fee
+    override fun convert(value: TangemPayTxHistoryResponse.Transaction): TangemPayTxHistoryItem? {
+        return value.spend?.let { convertSpend(id = value.id, spend = it) }
+            ?: value.payment?.let { convertPayment(id = value.id, payment = it) }
+            ?: value.fee?.let { convertFee(id = value.id, fee = it) }
+            ?: run {
+                Timber.wtf("unknown type of transaction: $value")
+                null
+            }
+    }
 
-        return TangemPayTxHistoryItem(
-            id = value.id,
-            date = when {
-                spend != null -> spend.postedAt
-                collateral != null -> collateral.postedAt
-                payment != null -> payment.postedAt
-                fee != null -> fee.postedAt
-                else -> null
-            },
-            amount = when {
-                spend != null -> spend.amount
-                collateral != null -> collateral.amount
-                payment != null -> payment.amount
-                fee != null -> fee.amount
-                else -> null
-            },
-            merchantName = when {
-                spend != null -> spend.merchantName
-                else -> null
-            },
-            status = when {
-                spend != null -> spend.status
-                payment != null -> payment.status
-                else -> null
-            },
-            currency = when {
-                spend != null -> spend.currency
-                collateral != null -> collateral.currency
-                payment != null -> payment.currency
-                fee != null -> fee.currency
-                else -> null
-            },
+    private fun convertSpend(id: String, spend: TangemPayTxHistoryResponse.Spend): TangemPayTxHistoryItem.Spend {
+        return TangemPayTxHistoryItem.Spend(
+            id = id,
+            date = spend.postedAt,
+            amount = spend.amount,
+            currency = Currency.getInstance(spend.currency),
+            enrichedMerchantName = spend.enrichedMerchantName,
+            merchantName = spend.merchantName,
+            enrichedMerchantCategory = spend.enrichedMerchantCategory,
+            merchantCategory = spend.merchantCategory,
+            status = spend.status,
+            enrichedMerchantIconUrl = spend.enrichedMerchantIcon,
+        )
+    }
+
+    private fun convertPayment(
+        id: String,
+        payment: TangemPayTxHistoryResponse.Payment,
+    ): TangemPayTxHistoryItem.Payment {
+        return TangemPayTxHistoryItem.Payment(
+            id = id,
+            date = payment.postedAt,
+            currency = Currency.getInstance(payment.currency),
+            amount = payment.amount,
+        )
+    }
+
+    private fun convertFee(id: String, fee: TangemPayTxHistoryResponse.Fee): TangemPayTxHistoryItem.Fee {
+        return TangemPayTxHistoryItem.Fee(
+            id = id,
+            date = fee.postedAt,
+            currency = Currency.getInstance(fee.currency),
+            amount = fee.amount,
         )
     }
 }
