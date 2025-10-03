80f829a2aa6c7900ba25df001eb3c9a2ee117e18
Tangem <git@tangem.com>
2025-10-03 17:05:44 +0500

    Updated on 2026-08-14

--- core/datasource/src/main/java/com/tangem/datasource/api/pay/models/response/TangemPayTxHistoryResponse.kt
+++ core/datasource/src/main/java/com/tangem/datasource/api/pay/models/response/TangemPayTxHistoryResponse.kt
@@ -35,20 +35,20 @@ data class TangemPayTxHistoryResponse(
         @Json(name = "authorized_amount") val authorizedAmount: BigDecimal? = null,
         @Json(name = "authorization_method") val authorizationMethod: String? = null,
         @Json(name = "memo") val memo: String? = null,
-        @Json(name = "receipt") val receipt: Boolean? = null,
-        @Json(name = "merchant_name") val merchantName: String? = null,
-        @Json(name = "merchant_category") val merchantCategory: String? = null,
-        @Json(name = "merchant_category_code") val merchantCategoryCode: String? = null,
+        @Json(name = "receipt") val receipt: Boolean,
+        @Json(name = "merchant_name") val merchantName: String,
+        @Json(name = "merchant_category") val merchantCategory: String,
+        @Json(name = "merchant_category_code") val merchantCategoryCode: String,
         @Json(name = "merchant_id") val merchantId: String? = null,
         @Json(name = "enriched_merchant_icon") val enrichedMerchantIcon: String? = null,
         @Json(name = "enriched_merchant_name") val enrichedMerchantName: String? = null,
         @Json(name = "enriched_merchant_category") val enrichedMerchantCategory: String? = null,
         @Json(name = "card_id") val cardId: String? = null,
         @Json(name = "card_type") val cardType: String? = null,
-        @Json(name = "status") val status: String? = null,
+        @Json(name = "status") val status: String,
         @Json(name = "declined_reason") val declinedReason: String? = null,
-        @Json(name = "authorized_at") val authorizedAt: DateTime? = null,
-        @Json(name = "posted_at") val postedAt: DateTime? = null,
+        @Json(name = "authorized_at") val authorizedAt: DateTime,
+        @Json(name = "posted_at") val postedAt: DateTime,
     )
 
     @JsonClass(generateAdapter = true)
@@ -56,10 +56,10 @@ data class TangemPayTxHistoryResponse(
         @Json(name = "amount") val amount: BigDecimal,
         @Json(name = "currency") val currency: String,
         @Json(name = "memo") val memo: String? = null,
-        @Json(name = "chain_id") val chainId: Long? = null,
-        @Json(name = "wallet_address") val walletAddress: String? = null,
-        @Json(name = "transaction_hash") val transactionHash: String? = null,
-        @Json(name = "posted_at") val postedAt: DateTime? = null,
+        @Json(name = "chain_id") val chainId: Long,
+        @Json(name = "wallet_address") val walletAddress: String,
+        @Json(name = "transaction_hash") val transactionHash: String,
+        @Json(name = "posted_at") val postedAt: DateTime?,
     )
 
     @JsonClass(generateAdapter = true)
@@ -70,8 +70,8 @@ data class TangemPayTxHistoryResponse(
         @Json(name = "chain_id") val chainId: Long? = null,
         @Json(name = "wallet_address") val walletAddress: String? = null,
         @Json(name = "transaction_hash") val transactionHash: String? = null,
-        @Json(name = "status") val status: String? = null,
-        @Json(name = "posted_at") val postedAt: DateTime? = null,
+        @Json(name = "status") val status: String,
+        @Json(name = "posted_at") val postedAt: DateTime,
     )
 
     @JsonClass(generateAdapter = true)
@@ -79,6 +79,6 @@ data class TangemPayTxHistoryResponse(
         @Json(name = "amount") val amount: BigDecimal,
         @Json(name = "currency") val currency: String,
         @Json(name = "description") val description: String? = null,
-        @Json(name = "posted_at") val postedAt: DateTime? = null,
+        @Json(name = "posted_at") val postedAt: DateTime,
     )
 }
