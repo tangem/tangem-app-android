6586883d805dd6f91d9790a26a6f192de22115b3
Tangem <git@tangem.com>
2025-10-06 11:22:18 +0500

    Updated on 2026-05-14

--- domain/yield-supply/src/main/java/com/tangem/domain/yield/supply/YieldSupplyMarketRepository.kt
+++ domain/yield-supply/src/main/java/com/tangem/domain/yield/supply/YieldSupplyMarketRepository.kt
@@ -1,6 +1,8 @@
 package com.tangem.domain.yield.supply
 
+import com.tangem.domain.models.currency.CryptoCurrency
 import com.tangem.domain.yield.supply.models.YieldMarketToken
+import com.tangem.domain.yield.supply.models.YieldMarketTokenStatus
 import com.tangem.domain.yield.supply.models.YieldSupplyMarketChartData
 import kotlinx.coroutines.flow.Flow
 
@@ -26,11 +28,11 @@ interface YieldSupplyMarketRepository {
      * Get yield token status by contract address.
      */
     @Throws
-    suspend fun getTokenStatus(contractAddress: String): YieldMarketToken
+    suspend fun getTokenStatus(cryptoCurrencyToken: CryptoCurrency.Token): YieldMarketTokenStatus
 
     /**
      * Get yield token APY chart by contract address.
      */
     @Throws
-    suspend fun getTokenChart(contractAddress: String): YieldSupplyMarketChartData
+    suspend fun getTokenChart(cryptoCurrencyToken: CryptoCurrency.Token): YieldSupplyMarketChartData
 }
