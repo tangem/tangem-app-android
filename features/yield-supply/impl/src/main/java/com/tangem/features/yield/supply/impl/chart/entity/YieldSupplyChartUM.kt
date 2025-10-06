f6dc34a1a693e73980e3810b74f68228213cabb8
Tangem <git@tangem.com>
2025-10-06 14:30:09 +0500

    Updated on 2026-08-27

--- features/yield-supply/impl/src/main/java/com/tangem/features/yield/supply/impl/chart/entity/YieldSupplyChartUM.kt
+++ features/yield-supply/impl/src/main/java/com/tangem/features/yield/supply/impl/chart/entity/YieldSupplyChartUM.kt
@@ -14,7 +14,10 @@ internal sealed class YieldSupplyChartUM {
     data class Error(val onRetry: () -> Unit) : YieldSupplyChartUM()
 
     @Immutable
-    data class Data(val chartData: YieldSupplyMarketChartDataUM) : YieldSupplyChartUM()
+    data class Data(
+        val chartData: YieldSupplyMarketChartDataUM,
+        val monthLables: ImmutableList<String>,
+    ) : YieldSupplyChartUM()
 }
 
 @Immutable
@@ -22,16 +25,18 @@ internal data class YieldSupplyMarketChartDataUM(
     val y: ImmutableList<Double>,
     val x: ImmutableList<Double>,
     val avr: Double,
+    val percentFormat: String,
 ) {
     companion object {
         @Suppress("MagicNumber")
         fun mock(): YieldSupplyMarketChartDataUM {
-            // TODO [REDACTED_TASK_KEY] replace mock values with real APY series
             val y = listOf(
                 4.6, 4.0, 4.2, 3.3, 2.5, 5.6, 4.2, 6.7, 3.5, 2.5, 4.5, 3.4,
+                4.6, 4.0, 4.2, 3.3, 2.5, 5.6, 4.2, 6.7, 3.5, 2.5, 4.5, 3.4,
+                4.2, 6.7, 3.5, 2.5, 4.5, 3.4,
             ).toImmutableList()
             val x = List(y.size) { 1.0 }.toImmutableList()
-            return YieldSupplyMarketChartDataUM(y = y, x = x, avr = 5.15)
+            return YieldSupplyMarketChartDataUM(y = y, x = x, avr = 5.15, "%.1f")
         }
     }
 }
