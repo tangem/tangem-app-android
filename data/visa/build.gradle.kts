9c6bf5f94b649e441ea0ffa3d8223e4bf34ccb9a
Tangem <git@tangem.com>
2025-10-06 12:52:29 +0400

    Updated on 2026-09-21

--- data/visa/build.gradle.kts
+++ data/visa/build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     implementation(projects.domain.tokens.models)
     implementation(projects.domain.tokens)
     implementation(projects.domain.networks)
+    implementation(projects.domain.walletManager)
 
     /** Feature API - remove after removing [HotWalletFeatureToggles] */
     implementation(projects.features.hotWallet.api)
