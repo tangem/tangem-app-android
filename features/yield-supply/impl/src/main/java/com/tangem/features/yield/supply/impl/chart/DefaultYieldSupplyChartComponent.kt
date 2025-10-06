7c2bc4d329ffa8fcdfc39568f97021026c9e06ad
Tangem <git@tangem.com>
2025-10-06 14:30:09 +0500

    Updated on 2026-05-04

--- features/yield-supply/impl/src/main/java/com/tangem/features/yield/supply/impl/chart/DefaultYieldSupplyChartComponent.kt
+++ features/yield-supply/impl/src/main/java/com/tangem/features/yield/supply/impl/chart/DefaultYieldSupplyChartComponent.kt
@@ -6,6 +6,8 @@ import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.tangem.core.decompose.context.AppComponentContext
 import com.tangem.core.decompose.model.getOrCreateModel
+import com.tangem.core.ui.decompose.ComposableContentComponent
+import com.tangem.domain.models.currency.CryptoCurrency
 import com.tangem.features.yield.supply.impl.chart.model.YieldSupplyChartModel
 import com.tangem.features.yield.supply.impl.chart.ui.YieldSupplyChartContent
 import dagger.assisted.Assisted
@@ -14,13 +16,13 @@ import dagger.assisted.AssistedInject
 
 internal class DefaultYieldSupplyChartComponent @AssistedInject constructor(
     @Assisted private val appComponentContext: AppComponentContext,
-    @Assisted private val params: YieldSupplyChartModel.Params,
-) : AppComponentContext by appComponentContext {
+    @Assisted private val params: Params,
+) : ComposableContentComponent, AppComponentContext by appComponentContext {
 
     private val model: YieldSupplyChartModel = getOrCreateModel(params = params)
 
     @Composable
-    fun Content(modifier: Modifier = Modifier) {
+    override fun Content(modifier: Modifier) {
         val state by model.uiState.collectAsStateWithLifecycle()
         YieldSupplyChartContent(
             state = state,
@@ -30,6 +32,17 @@ internal class DefaultYieldSupplyChartComponent @AssistedInject constructor(
 
     @AssistedFactory
     internal interface Factory {
-        fun create(context: AppComponentContext, params: YieldSupplyChartModel.Params): DefaultYieldSupplyChartComponent
+        fun create(context: AppComponentContext, params: Params): DefaultYieldSupplyChartComponent
+    }
+
+    internal data class Params(
+        val cryptoCurrency: CryptoCurrency.Token,
+        val callback: ModelCallback? = null,
+    )
+
+    internal interface ModelCallback {
+        fun onStartLoading()
+        fun onSuccessLoad()
+        fun onLoadFail()
     }
 }
