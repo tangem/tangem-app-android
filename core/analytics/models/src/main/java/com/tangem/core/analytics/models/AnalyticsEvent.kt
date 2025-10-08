be2e56f108c5d4d3d13006a232e3061a5432126e
Tangem <git@tangem.com>
2025-10-08 17:49:34 +0300

    Updated on 2025-11-18

--- core/analytics/models/src/main/java/com/tangem/core/analytics/models/AnalyticsEvent.kt
+++ core/analytics/models/src/main/java/com/tangem/core/analytics/models/AnalyticsEvent.kt
@@ -2,11 +2,19 @@ package com.tangem.core.analytics.models
 
 /**
 [REDACTED_AUTHOR]
+ *
+ * Base analytics event
+ * @param category event category (e.g. "Wallet", "Settings")
+ * @param event event name (e.g. "Create", "Backup")
+ * @param params event parameters
+ * @param isSpecific if true, event will be sent only to ***specific*** analytics handlers
+ * @see [com.tangem.core.analytics.api.AnalyticsHandler.specific]
  */
 open class AnalyticsEvent(
     val category: String,
     val event: String,
     var params: Map<String, String> = mapOf(),
+    val isSpecific: Boolean = false,
 ) {
 
     val id: String = "[$category] $event"
