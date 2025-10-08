f2b9d3f91c9a58094f27a2893790137e3503b388
Tangem <git@tangem.com>
2025-10-08 20:03:08 +0500

    Updated on 2026-06-27

--- common/ui/src/main/java/com/tangem/common/ui/amountScreen/preview/AmountStatePreviewData.kt
+++ common/ui/src/main/java/com/tangem/common/ui/amountScreen/preview/AmountStatePreviewData.kt
@@ -3,7 +3,6 @@ package com.tangem.common.ui.amountScreen.preview
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import com.tangem.common.ui.amountScreen.models.AmountFieldModel
-import com.tangem.common.ui.amountScreen.models.AmountSegmentedButtonsConfig
 import com.tangem.common.ui.amountScreen.models.AmountState
 import com.tangem.core.ui.components.currency.icon.CurrencyIconState
 import com.tangem.core.ui.extensions.TextReference
@@ -12,31 +11,18 @@ import com.tangem.domain.appcurrency.model.AppCurrency
 import com.tangem.domain.tokens.model.Amount
 import com.tangem.domain.tokens.model.AmountType
 import com.tangem.utils.StringsSigns
-import kotlinx.collections.immutable.persistentListOf
 import java.math.BigDecimal
 
 object AmountStatePreviewData {
 
-    val emptyState = AmountState.Empty(isRedesignEnabled = true)
+    val emptyState = AmountState.Empty
 
     val amountState = AmountState.Data(
         isPrimaryButtonEnabled = false,
         title = stringReference("Family Wallet"),
-        availableBalance = stringReference("2 130,81231238 USDT • 2 129,12 \$)"),
         availableBalanceCrypto = stringReference("2 130,81231238 USDT"),
         availableBalanceFiat = stringReference("1 232 129,12 \$"),
         tokenIconState = CurrencyIconState.Loading,
-        segmentedButtonConfig = persistentListOf(
-            AmountSegmentedButtonsConfig(
-                title = stringReference("USDT"),
-                iconState = CurrencyIconState.Locked,
-                isFiat = false,
-            ),
-            AmountSegmentedButtonsConfig(
-                title = stringReference("USD"),
-                isFiat = true,
-            ),
-        ),
         appCurrency = AppCurrency.Default,
         tokenName = stringReference("Tether"),
         amountTextField = AmountFieldModel(
@@ -65,12 +51,9 @@ object AmountStatePreviewData {
             isValuePasted = false,
             onValuePastedTriggerDismiss = {},
         ),
-        isSegmentedButtonsEnabled = true,
-        selectedButton = 0,
-        isRedesignEnabled = false,
     )
 
-    val amountWithValueState = amountState.copy(
+    private val amountWithValueState = amountState.copy(
         amountTextField = amountState.amountTextField.copy(
             value = "100.00",
             cryptoAmount = amountState.amountTextField.cryptoAmount.copy(
@@ -84,14 +67,8 @@ object AmountStatePreviewData {
     )
 
     val amountStateV2 = amountState.copy(
-        isRedesignEnabled = true,
-        availableBalance = stringReference("2 130,81231238 USDT • 2 129,12 \$)"),
         availableBalanceCrypto = stringReference("2 130,81231238 USDT"),
         availableBalanceFiat = stringReference(" ${StringsSigns.DOT} 1 232 129,12 $"),
-    )
-
-    val amountWithValueFiatState = amountWithValueState.copy(
-        amountTextField = amountWithValueState.amountTextField.copy(isFiatValue = false),
     )
 
     val amountStateV2WithoutRates = amountState.copy(
