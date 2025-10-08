19081d7ae02c668dec61555c6ce6776d8a0c74db
Tangem <git@tangem.com>
2025-10-08 11:49:42 +0500

    Updated on 2025-12-18

--- features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/tokenlist/entity/transformer/SetNothingToFoundStateTransformerV2.kt
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/tokenlist/entity/transformer/SetNothingToFoundStateTransformerV2.kt
@@ -2,39 +2,28 @@ package com.tangem.features.onramp.tokenlist.entity.transformer
 
 import com.tangem.core.ui.components.tokenlist.state.TokensListItemUM
 import com.tangem.core.ui.extensions.TextReference
-import com.tangem.core.ui.extensions.resourceReference
-import com.tangem.features.onramp.impl.R
 import com.tangem.features.onramp.tokenlist.entity.TokenListUM
 import com.tangem.features.onramp.tokenlist.entity.TokenListUMData
 import com.tangem.features.onramp.tokenlist.entity.TokenListUMTransformer
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.toImmutableList
 
-internal class SetNothingToFoundStateTransformer(
+internal class SetNothingToFoundStateTransformerV2(
     private val isBalanceHidden: Boolean,
     private val emptySearchMessageReference: TextReference,
 ) : TokenListUMTransformer {
 
     override fun transform(prevState: TokenListUM): TokenListUM {
         return prevState.copy(
-            availableItems = buildList {
-                createGroupTitle(
-                    textReference = resourceReference(id = R.string.exchange_tokens_available_tokens_header),
-                )
-                    .let(::add)
-
+            availableItems = persistentListOf(),
+            unavailableItems = persistentListOf(),
+            tokensListData = TokenListUMData.TokenList(tokensList = buildList {
                 TokensListItemUM.Text(
                     id = emptySearchMessageReference.hashCode(),
                     text = emptySearchMessageReference,
                 ).let(::add)
-            }.toImmutableList(),
-            unavailableItems = persistentListOf(),
-            tokensListData = TokenListUMData.EmptyList,
+            }.toImmutableList()),
             isBalanceHidden = isBalanceHidden,
         )
-    }
-
-    private fun createGroupTitle(textReference: TextReference): TokensListItemUM.GroupTitle {
-        return TokensListItemUM.GroupTitle(id = textReference.hashCode(), text = textReference)
     }
 }
