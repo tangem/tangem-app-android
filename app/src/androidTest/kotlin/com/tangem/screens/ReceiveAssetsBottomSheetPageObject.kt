bb2b10f6a01eae33ff7ae8f2e81ee2b220fe3c3b
Tangem <git@tangem.com>
2025-10-09 16:31:02 +0300

    Updated on 2025-11-18

--- app/src/androidTest/kotlin/com/tangem/screens/ReceiveAssetsBottomSheetPageObject.kt
+++ app/src/androidTest/kotlin/com/tangem/screens/ReceiveAssetsBottomSheetPageObject.kt
@@ -3,20 +3,19 @@ package com.tangem.screens
 import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
 import com.tangem.common.BaseTestCase
 import com.tangem.core.ui.R
-import com.tangem.core.ui.test.BaseBottomSheetTestTags
 import io.github.kakaocup.compose.node.element.ComposeScreen
 import io.github.kakaocup.compose.node.element.ComposeScreen.Companion.onComposeScreen
 import io.github.kakaocup.compose.node.element.KNode
 import io.github.kakaocup.kakao.common.utilities.getResourceString
 
-class BaseBottomSheetPageObject(semanticsProvider: SemanticsNodeInteractionsProvider) :
-    ComposeScreen<BaseBottomSheetPageObject>(semanticsProvider = semanticsProvider) {
+class ReceiveAssetsBottomSheetPageObject(semanticsProvider: SemanticsNodeInteractionsProvider) :
+    ComposeScreen<ReceiveAssetsBottomSheetPageObject>(semanticsProvider = semanticsProvider) {
 
-    val hideButton: KNode = child {
-        hasText(getResourceString(R.string.token_details_hide_token))
-        hasTestTag(BaseBottomSheetTestTags.ACTION_TITLE)
+    val showQrCodeButton: KNode = child {
+        hasText(getResourceString(R.string.token_receive_show_qr_code_title))
+        useUnmergedTree = true
     }
 }
 
-internal fun BaseTestCase.onBottomSheet(function: BaseBottomSheetPageObject.() -> Unit) =
+internal fun BaseTestCase.onReceiveAssetsBottomSheet(function: ReceiveAssetsBottomSheetPageObject.() -> Unit) =
     onComposeScreen(composeTestRule, function)
