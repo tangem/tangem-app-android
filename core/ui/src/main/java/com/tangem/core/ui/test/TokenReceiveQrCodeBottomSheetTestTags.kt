54a5d5ba08892e22760527c949f7d3172fb1bd3c
Tangem <git@tangem.com>
2025-10-09 16:31:02 +0300

    Updated on 2026-06-12

---
+++ core/ui/src/main/java/com/tangem/core/ui/test/TokenReceiveQrCodeBottomSheetTestTags.kt
@@ -0,0 +1,7 @@
+package com.tangem.core.ui.test
+
+object TokenReceiveQrCodeBottomSheetTestTags {
+    const val TITLE = "TOKEN_RECEIVE_QR_CODE_BOTTOM_SHEET_TITLE"
+    const val QR_CODE = "TOKEN_RECEIVE_QR_CODE_BOTTOM_SHEET_QR_CODE"
+    const val ADDRESS = "TOKEN_RECEIVE_QR_CODE_BOTTOM_SHEET_ADDRESS"
+}
