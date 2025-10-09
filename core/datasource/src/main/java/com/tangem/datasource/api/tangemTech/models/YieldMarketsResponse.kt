0b5ff67ead696db2efe5ba557aca627a084cda71
Tangem <git@tangem.com>
2025-10-09 17:48:02 +0500

    Updated on 2026-07-23

--- core/datasource/src/main/java/com/tangem/datasource/api/tangemTech/models/YieldMarketsResponse.kt
+++ core/datasource/src/main/java/com/tangem/datasource/api/tangemTech/models/YieldMarketsResponse.kt
@@ -2,21 +2,9 @@ package com.tangem.datasource.api.tangemTech.models
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import java.math.BigDecimal
 
 @JsonClass(generateAdapter = true)
 data class YieldMarketsResponse(
-    @Json(name = "tokens") val marketDtos: List<MarketDto>,
+    @Json(name = "tokens") val marketDtos: List<YieldSupplyMarketTokenDto>,
     @Json(name = "lastUpdatedAt") val lastUpdated: String,
-) {
-
-    @JsonClass(generateAdapter = true)
-    data class MarketDto(
-        @Json(name = "tokenAddress") val tokenAddress: String? = null,
-        @Json(name = "tokenSymbol") val tokenSymbol: String? = null,
-        @Json(name = "tokenName") val tokenName: String? = null,
-        @Json(name = "apy") val apy: BigDecimal,
-        @Json(name = "isActive") val isActive: Boolean,
-        @Json(name = "chainId") val chainId: Int? = null,
-    )
-}
+)
--- core/datasource/src/main/java/com/tangem/datasource/local/yieldsupply/YieldMarketsStore.kt
+++ core/datasource/src/main/java/com/tangem/datasource/local/yieldsupply/YieldMarketsStore.kt
@@ -1,13 +1,13 @@
 package com.tangem.datasource.local.yieldsupply
 
-import com.tangem.datasource.api.tangemTech.models.YieldMarketsResponse
+import com.tangem.datasource.api.tangemTech.models.YieldSupplyMarketTokenDto
 import kotlinx.coroutines.flow.Flow
 
 interface YieldMarketsStore {
 
-    fun get(): Flow<List<YieldMarketsResponse.MarketDto>>
+    fun get(): Flow<List<YieldSupplyMarketTokenDto>>
 
-    suspend fun getSyncOrNull(): List<YieldMarketsResponse.MarketDto>?
+    suspend fun getSyncOrNull(): List<YieldSupplyMarketTokenDto>?
 
-    suspend fun store(items: List<YieldMarketsResponse.MarketDto>)
+    suspend fun store(items: List<YieldSupplyMarketTokenDto>)
 }
