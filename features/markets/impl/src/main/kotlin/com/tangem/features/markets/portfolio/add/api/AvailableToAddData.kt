7ee5b10bb84bdcde82736b99c65aaf1e237e5d8d
Tangem <git@tangem.com>
2025-10-10 16:59:23 +0700

    Updated on 2025-11-18

--- features/markets/impl/src/main/kotlin/com/tangem/features/markets/portfolio/add/api/AvailableToAddData.kt
+++ features/markets/impl/src/main/kotlin/com/tangem/features/markets/portfolio/add/api/AvailableToAddData.kt
@@ -4,40 +4,51 @@ import com.tangem.domain.markets.TokenMarketInfo
 import com.tangem.domain.models.account.AccountId
 import com.tangem.domain.models.account.AccountStatus
 import com.tangem.domain.models.currency.CryptoCurrency
+import com.tangem.domain.models.network.Network
 import com.tangem.domain.models.wallet.UserWallet
 import com.tangem.domain.models.wallet.UserWalletId
 
-data class AvailableToAddData(
+internal data class AvailableToAddData(
     val availableToAddWallets: Map<UserWalletId, AvailableToAddWallet>,
 ) {
+    val availableToAdd: Boolean
+        get() = availableToAddWallets.isNotEmpty()
     val isSinglePortfolio: Boolean
         get() = availableToAddWallets.size == 1 && availableToAddWallets.values.first().accounts.size == 1
 }
 
-data class AvailableToAddWallet(
+internal data class AvailableToAddWallet(
     val userWallet: UserWallet,
     val accounts: Set<AccountStatus>,
     val availableNetworks: Set<TokenMarketInfo.Network>,
     val availableToAddAccounts: Map<AccountId, AvailableToAddAccount>,
-) {
-    val isSingleAvailableNetworks: Boolean get() = availableNetworks.size == 1
-    val singleAvailableNetworks: TokenMarketInfo.Network get() = availableNetworks.first()
-}
-
-data class AvailableToAddAccount(
-    val account: AccountStatus,
-    val availableNetworks: Set<TokenMarketInfo.Network>,
-    val availableToAddNetworks: Set<TokenMarketInfo.Network>,
 )
 
-data class SelectedPortfolio(
-    val userWallet: UserWallet,
+internal data class AvailableToAddAccount(
     val account: AccountStatus,
+    val availableNetworks: Set<TokenMarketInfo.Network>,
+    val addedNetworks: Set<Network>,
+) {
+    val isSingleNetwork: Boolean
+        get() = availableNetworks.size == 1
+
+    val availableToAddNetworks: Set<TokenMarketInfo.Network> = availableNetworks
+        .filter { available -> addedNetworks.none { added -> added.backendId == available.networkId } }
+        .toSet()
+
+    val addedMarketNetworks: Set<TokenMarketInfo.Network> = availableNetworks
+        .filter { available -> addedNetworks.any { added -> added.backendId == available.networkId } }
+        .toSet()
+}
+
+internal data class SelectedPortfolio(
+    val userWallet: UserWallet,
+    val account: AvailableToAddAccount,
     val isAccountMode: Boolean,
     val availableMorePortfolio: Boolean,
 )
 
-data class SelectedNetwork(
+internal data class SelectedNetwork(
     val selectedNetwork: TokenMarketInfo.Network,
     val cryptoCurrency: CryptoCurrency,
     val availableMoreNetwork: Boolean,
