f90efd5d6179f4a3c8ee0df3bd600e0a0ba75988
Tangem <git@tangem.com>
2025-10-15 22:15:31 +0500

    Updated on 2026-08-07

--- features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/createhardwarewallet/ui/CreateHardwareWalletContent.kt
+++ features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/createhardwarewallet/ui/CreateHardwareWalletContent.kt
@@ -1,4 +1,4 @@
-package com.tangem.features.hotwallet.createmobilewallet.ui
+package com.tangem.features.hotwallet.createhardwarewallet.ui
 
 import android.content.res.Configuration
 import androidx.compose.foundation.background
@@ -6,12 +6,13 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.tangem.core.ui.R
-import com.tangem.core.ui.components.PrimaryButton
+import com.tangem.core.ui.components.PrimaryButtonIconEnd
 import com.tangem.core.ui.components.SecondaryButton
 import com.tangem.core.ui.components.appbar.TangemTopAppBar
 import com.tangem.core.ui.components.appbar.models.TopAppBarButtonUM
@@ -20,12 +21,12 @@ import com.tangem.core.ui.extensions.TextReference
 import com.tangem.core.ui.extensions.stringResourceSafe
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.core.ui.res.TangemThemePreview
-import com.tangem.features.hotwallet.createmobilewallet.entity.CreateMobileWalletUM
+import com.tangem.features.hotwallet.createhardwarewallet.entity.CreateHardwareWalletUM
 
 @Suppress("LongMethod")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-internal fun CreateMobileWalletContent(state: CreateMobileWalletUM, modifier: Modifier = Modifier) {
+internal fun CreateHardwareWalletContent(state: CreateHardwareWalletUM, modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
             .background(TangemTheme.colors.background.primary)
@@ -50,9 +51,9 @@ internal fun CreateMobileWalletContent(state: CreateMobileWalletUM, modifier: Mo
             Icon(
                 modifier = Modifier
                     .fillMaxWidth(),
-                painter = painterResource(R.drawable.ic_create_mobile_wallet_56),
+                painter = painterResource(R.drawable.ic_tangem_64),
                 contentDescription = null,
-                tint = TangemTheme.colors.icon.informative,
+                tint = Color.Unspecified,
             )
             Text(
                 modifier = Modifier
@@ -62,7 +63,7 @@ internal fun CreateMobileWalletContent(state: CreateMobileWalletUM, modifier: Mo
                         top = 20.dp,
                         end = 16.dp,
                     ),
-                text = stringResourceSafe(R.string.hw_create_title),
+                text = stringResourceSafe(R.string.wallet_create_common_title),
                 style = TangemTheme.typography.h2,
                 color = TangemTheme.colors.text.primary1,
                 textAlign = TextAlign.Center,
@@ -70,51 +71,59 @@ internal fun CreateMobileWalletContent(state: CreateMobileWalletUM, modifier: Mo
             FeatureBlock(
                 modifier = Modifier
                     .padding(top = 32.dp),
-                title = stringResourceSafe(R.string.hw_create_keys_title),
-                description = stringResourceSafe(R.string.hw_create_keys_description),
-                iconRes = R.drawable.ic_lock_24,
+                title = stringResourceSafe(R.string.hw_upgrade_key_migration_title),
+                description = stringResourceSafe(R.string.hw_upgrade_key_migration_description),
+                iconRes = R.drawable.ic_mobile_security_24,
             )
             FeatureBlock(
                 modifier = Modifier
                     .padding(top = 24.dp),
-                title = stringResourceSafe(R.string.hw_create_seed_title),
-                description = stringResourceSafe(R.string.hw_create_seed_description),
-                iconRes = R.drawable.ic_settings_24,
+                title = stringResourceSafe(R.string.hw_upgrade_funds_access_title),
+                description = stringResourceSafe(R.string.hw_upgrade_funds_access_description),
+                iconRes = R.drawable.ic_knight_shield_24,
+            )
+            FeatureBlock(
+                modifier = Modifier
+                    .padding(top = 24.dp),
+                title = stringResourceSafe(R.string.hw_upgrade_general_security_title),
+                description = stringResourceSafe(R.string.hw_upgrade_general_security_description),
+                iconRes = R.drawable.ic_protect_24,
             )
         }
-        SecondaryButton(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(
-                    start = 16.dp,
-                    top = 16.dp,
-                    end = 16.dp,
-                ),
-            text = stringResourceSafe(R.string.hw_import_existing_wallet),
-            onClick = state.onImportClick,
-        )
-        PrimaryButton(
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp),
-            text = stringResourceSafe(R.string.onboarding_create_wallet_button_create_wallet),
-            showProgress = state.createButtonLoading,
-            onClick = state.onCreateClick,
-        )
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+        ) {
+            SecondaryButton(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                text = stringResourceSafe(R.string.details_buy_wallet),
+                onClick = state.onBuyTangemWalletClick,
+            )
+            PrimaryButtonIconEnd(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                text = stringResourceSafe(R.string.home_button_scan),
+                onClick = state.onScanDeviceClick,
+                iconResId = R.drawable.ic_tangem_24,
+                showProgress = state.isScanInProgress,
+            )
+        }
     }
 }
 
 @Preview(showBackground = true, widthDp = 360)
 @Preview(showBackground = true, widthDp = 360, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
-private fun PreviewCreateWalletContent() {
+private fun PreviewCreateHardwareWalletContent() {
     TangemThemePreview {
-        CreateMobileWalletContent(
-            state = CreateMobileWalletUM(
+        CreateHardwareWalletContent(
+            state = CreateHardwareWalletUM(
                 onBackClick = {},
-                createButtonLoading = false,
-                onImportClick = {},
-                onCreateClick = {},
+                onBuyTangemWalletClick = {},
+                onScanDeviceClick = {},
             ),
         )
     }
