b478c0f3de7dbccbd50c34d68f5d71ad72912677
Tangem <git@tangem.com>
2025-10-15 15:59:43 +0700

    Updated on 2026-03-27

--- features/account/api/src/main/java/com/tangem/features/account/PortfolioSelectorComponent.kt
+++ features/account/api/src/main/java/com/tangem/features/account/PortfolioSelectorComponent.kt
@@ -8,27 +8,55 @@ import com.tangem.domain.models.account.AccountId
 import com.tangem.domain.models.account.AccountStatus
 import com.tangem.domain.models.wallet.UserWallet
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
+/**
+ * How to use
+ * 1) Create and keep instance of [PortfolioFetcher] and [PortfolioSelectorController] in your Feature Model
+ * 2) Provide them via [Params]
+ * 3) Now you have a bridge between your Feature and PortfolioSelector
+ * 4) Initial state is unselected. Select yourself [PortfolioSelectorController.selectAccount]
+ * or offer users to select
+ *
+ * 5) Listen [PortfolioSelectorController.selectedAccount] or [PortfolioSelectorController.selectedAccountWithData]
+ *
+ * Note:
+ *  - Supports [PortfolioSelectorComponent.BottomSheet] and [PortfolioSelectorComponent.Content] modes
+ */
 interface PortfolioSelectorComponent : ComposableBottomSheetComponent, ComposableContentComponent {
 
     val title: StateFlow<TextReference>
 
     data class Params(
-        val onDismiss: () -> Unit,
         val portfolioFetcher: PortfolioFetcher,
         val controller: PortfolioSelectorController,
+        val bsCallback: BottomSheetCallback? = null,
     )
+
+    interface BottomSheetCallback {
+        val onDismiss: () -> Unit
+        val onBack: () -> Unit
+    }
 
     interface Factory : ComponentFactory<Params, PortfolioSelectorComponent>
 }
 
 /**
+ * How to use see [PortfolioSelectorComponent]
+ *
  * if [isAccountMode] is false it's mean [selectedAccount] emit [AccountId] for Main account
  */
 interface PortfolioSelectorController {
     val isAccountMode: Flow<Boolean>
-    val selectedAccount: StateFlow<AccountId?>
+    val selectedAccount: Flow<AccountId?>
+    val selectedAccountSync: AccountId?
+
+    /**
+     * for some Feature specific filtering
+     * combine and update with your Feature data and [PortfolioFetcher.data]
+     */
+    val isEnabled: MutableStateFlow<(UserWallet, AccountStatus) -> Boolean>
 
     fun selectAccount(accountId: AccountId?)
     fun selectedAccountWithData(portfolioFetcher: PortfolioFetcher): Flow<Pair<UserWallet, AccountStatus>?>
