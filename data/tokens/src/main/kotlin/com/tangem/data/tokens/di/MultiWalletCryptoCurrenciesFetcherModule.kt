dee67987034a4634987628bdce7f790d979ffbc7
Tangem <git@tangem.com>
2025-10-17 10:37:51 +0400

    Updated on 2026-07-07

--- data/tokens/src/main/kotlin/com/tangem/data/tokens/di/MultiWalletCryptoCurrenciesFetcherModule.kt
+++ data/tokens/src/main/kotlin/com/tangem/data/tokens/di/MultiWalletCryptoCurrenciesFetcherModule.kt
@@ -7,11 +7,11 @@ import com.tangem.data.tokens.AccountListCryptoCurrenciesFetcher
 import com.tangem.data.tokens.DefaultMultiWalletCryptoCurrenciesFetcher
 import com.tangem.data.tokens.utils.CustomTokensMerger
 import com.tangem.datasource.api.tangemTech.TangemTechApi
-import com.tangem.datasource.exchangeservice.swap.ExpressServiceLoader
 import com.tangem.datasource.local.token.UserTokensResponseStore
 import com.tangem.datasource.local.userwallet.UserWalletsStore
 import com.tangem.domain.account.featuretoggle.AccountsFeatureToggles
 import com.tangem.domain.demo.models.DemoConfig
+import com.tangem.domain.express.ExpressServiceFetcher
 import com.tangem.domain.tokens.MultiWalletCryptoCurrenciesFetcher
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import dagger.Module
@@ -33,7 +33,7 @@ internal class MultiWalletCryptoCurrenciesFetcherModule {
         userTokensResponseStore: UserTokensResponseStore,
         userTokensSaver: UserTokensSaver,
         cardCryptoCurrencyFactory: CardCryptoCurrencyFactory,
-        expressServiceLoader: ExpressServiceLoader,
+        expressServiceFetcher: ExpressServiceFetcher,
         walletAccountsFetcher: WalletAccountsFetcher,
         dispatchers: CoroutineDispatcherProvider,
     ): MultiWalletCryptoCurrenciesFetcher {
@@ -56,7 +56,7 @@ internal class MultiWalletCryptoCurrenciesFetcherModule {
                 userTokensResponseStore = userTokensResponseStore,
                 userTokensSaver = userTokensSaver,
                 cardCryptoCurrencyFactory = cardCryptoCurrencyFactory,
-                expressServiceLoader = expressServiceLoader,
+                expressServiceFetcher = expressServiceFetcher,
                 dispatchers = dispatchers,
             )
         }
