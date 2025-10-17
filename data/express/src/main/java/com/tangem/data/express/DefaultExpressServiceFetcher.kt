1550ec9c7d3ef5f9b64bef1baf05ae240335c5dc
Tangem <git@tangem.com>
2025-10-17 10:37:51 +0400

    Updated on 2026-09-23

--- data/express/src/main/java/com/tangem/data/express/DefaultExpressServiceFetcher.kt
+++ data/express/src/main/java/com/tangem/data/express/DefaultExpressServiceFetcher.kt
@@ -1,17 +1,23 @@
-package com.tangem.datasource.exchangeservice.swap
+package com.tangem.data.express
 
+import arrow.core.Either
+import arrow.core.raise.either
+import com.tangem.data.express.converter.ExpressAssetConverter
 import com.tangem.datasource.api.common.response.getOrThrow
 import com.tangem.datasource.api.express.TangemExpressApi
 import com.tangem.datasource.api.express.models.request.AssetsRequestBody
 import com.tangem.datasource.api.express.models.request.LeastTokenInfo
-import com.tangem.datasource.api.express.models.response.Asset
 import com.tangem.datasource.exchangeservice.swap.ExpressUtils.getRefCode
 import com.tangem.datasource.local.preferences.AppPreferencesStore
 import com.tangem.datasource.local.token.ExpressAssetsStore
+import com.tangem.datasource.local.userwallet.UserWalletsStore
 import com.tangem.domain.core.lce.Lce
+import com.tangem.domain.core.utils.catchOn
 import com.tangem.domain.core.utils.lceContent
 import com.tangem.domain.core.utils.lceError
 import com.tangem.domain.core.utils.lceLoading
+import com.tangem.domain.express.ExpressServiceFetcher
+import com.tangem.domain.express.models.ExpressAsset
 import com.tangem.domain.models.wallet.UserWallet
 import com.tangem.domain.models.wallet.UserWalletId
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
@@ -19,56 +25,72 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.withContext
 import timber.log.Timber
 import javax.inject.Inject
 
-typealias InitializationStatusFlow = MutableStateFlow<Lce<Throwable, List<Asset>>>
+typealias InitializationStatusFlow = MutableStateFlow<Lce<Throwable, List<ExpressAsset>>>
 
 /**
- * Default implementation of [ExpressServiceLoader]
+ * Default implementation of [ExpressServiceFetcher]
  *
  * @property tangemExpressApi   express api
  * @property expressAssetsStore local storage
  *
 [REDACTED_AUTHOR]
  */
-internal class DefaultExpressServiceLoader @Inject constructor(
+internal class DefaultExpressServiceFetcher @Inject constructor(
     private val tangemExpressApi: TangemExpressApi,
     private val expressAssetsStore: ExpressAssetsStore,
     private val appPreferencesStore: AppPreferencesStore,
+    private val userWalletsStore: UserWalletsStore,
     private val dispatchers: CoroutineDispatcherProvider,
-) : ExpressServiceLoader {
+) : ExpressServiceFetcher {
 
     private val initializationStatuses =
         MutableStateFlow<Map<UserWalletId, InitializationStatusFlow>>(value = emptyMap())
 
-    override suspend fun update(userWallet: UserWallet, userTokens: List<LeastTokenInfo>) {
-        withContext(dispatchers.io) {
+    override suspend fun fetch(userWalletId: UserWalletId, assetIds: Set<ExpressAsset.ID>): Either<Throwable, Unit> =
+        either {
+            val userWallet = arrow.core.raise.catch(
+                block = { userWalletsStore.getSyncStrict(userWalletId) },
+                catch = ::raise,
+            )
+
+            fetch(userWallet = userWallet, assetIds = assetIds).bind()
+        }
+
+    override suspend fun fetch(userWallet: UserWallet, assetIds: Set<ExpressAsset.ID>): Either<Throwable, Unit> {
+        return Either.catchOn(dispatchers.io) {
             val initializationStatus = getInitializationStatusInternal(userWallet.walletId)
 
             try {
-                if (userTokens.isNotEmpty()) {
+                if (assetIds.isNotEmpty()) {
+                    val tokenList = assetIds.map {
+                        LeastTokenInfo(contractAddress = it.contractAddress, network = it.networkId)
+                    }
+
                     val response = tangemExpressApi.getAssets(
                         userWalletId = userWallet.walletId.stringValue,
                         refCode = getRefCode(userWallet, appPreferencesStore),
-                        body = AssetsRequestBody(tokensList = userTokens),
+                        body = AssetsRequestBody(tokensList = tokenList),
                     ).getOrThrow()
 
                     expressAssetsStore.store(userWallet.walletId, response)
 
-                    initializationStatus.update { response.lceContent() }
+                    val expressAssets = ExpressAssetConverter.convertList(response)
+                    initializationStatus.update { expressAssets.lceContent() }
                 }
             } catch (e: Throwable) {
                 if (expressAssetsStore.getSyncOrNull(userWallet.walletId) == null) {
                     initializationStatus.update { e.lceError() }
                 }
                 Timber.e(e, "Unable to fetch assets for: ${userWallet.walletId.stringValue}")
+                throw e
             }
         }
     }
 
-    override fun getInitializationStatus(userWalletId: UserWalletId): Flow<Lce<Throwable, List<Asset>>> {
+    override fun getInitializationStatus(userWalletId: UserWalletId): Flow<Lce<Throwable, List<ExpressAsset>>> {
         return flow { getInitializationStatusInternal(userWalletId).collect { emit(it) } }
     }
 
@@ -77,7 +99,7 @@ internal class DefaultExpressServiceLoader @Inject constructor(
         val initializationStatus = initializationStatuses.value[userWalletId]
         if (initializationStatus != null) return initializationStatus
 
-        val cached = expressAssetsStore.getSyncOrNull(userWalletId)
+        val cached = expressAssetsStore.getSyncOrNull(userWalletId)?.let(ExpressAssetConverter::convertList)
         val default: InitializationStatusFlow = MutableStateFlow(value = cached?.lceContent() ?: lceLoading())
 
         initializationStatuses.update { statuses ->
