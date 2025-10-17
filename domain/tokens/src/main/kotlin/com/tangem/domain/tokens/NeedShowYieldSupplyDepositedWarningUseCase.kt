e91929e2aa1c75d2eedfc1c91dfb35e4db1dc5a5
Tangem <git@tangem.com>
2025-10-17 23:45:25 +0500

    Updated on 2026-07-01

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/NeedShowYieldSupplyDepositedWarningUseCase.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/NeedShowYieldSupplyDepositedWarningUseCase.kt
@@ -5,15 +5,18 @@ import com.tangem.domain.tokens.repository.YieldSupplyWarningsViewedRepository
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import kotlinx.coroutines.withContext
 
+@Suppress("UnusedPrivateProperty")
 class NeedShowYieldSupplyDepositedWarningUseCase(
     private val yieldSupplyWarningsViewedRepository: YieldSupplyWarningsViewedRepository,
     private val dispatchers: CoroutineDispatcherProvider,
 ) {
 
     suspend operator fun invoke(cryptoCurrencyStatus: CryptoCurrencyStatus?): Boolean = withContext(dispatchers.io) {
-        val hasActiveLending = cryptoCurrencyStatus?.value?.yieldSupplyStatus?.isActive == true
-        if (!hasActiveLending) return@withContext false
-        val showedWarnings = yieldSupplyWarningsViewedRepository.getViewedWarnings()
-        return@withContext !showedWarnings.contains(cryptoCurrencyStatus?.currency?.name)
+        // TEMPORARY REQUIREMENTS
+        return@withContext false
+        // val hasActiveLending = cryptoCurrencyStatus?.value?.yieldSupplyStatus?.isActive == true
+        // if (!hasActiveLending) return@withContext false
+        // val showedWarnings = yieldSupplyWarningsViewedRepository.getViewedWarnings()
+        // return@withContext !showedWarnings.contains(cryptoCurrencyStatus?.currency?.name)
     }
 }
