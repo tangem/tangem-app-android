cc9cf90098b0913365f7dbd4468e975ee388e51c
Tangem <git@tangem.com>
2025-10-17 21:29:09 +0400

    Updated on 2026-04-06

--- features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/entity/TangemPayDetailsStateFactory.kt
+++ features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/entity/TangemPayDetailsStateFactory.kt
@@ -3,8 +3,10 @@ package com.tangem.features.tangempay.entity
 import com.tangem.core.ui.components.buttons.actions.ActionButtonConfig
 import com.tangem.core.ui.components.containers.pullToRefresh.PullToRefreshConfig
 import com.tangem.core.ui.components.containers.pullToRefresh.PullToRefreshConfig.ShowRefreshState
+import com.tangem.core.ui.components.dropdownmenu.TangemDropdownMenuItem
 import com.tangem.core.ui.extensions.TextReference
 import com.tangem.core.ui.extensions.resourceReference
+import com.tangem.core.ui.res.TangemTheme
 import com.tangem.features.tangempay.details.impl.R
 import com.tangem.features.tangempay.utils.CardDetailsFormatUtil
 import com.tangem.utils.StringsSigns
@@ -14,6 +16,7 @@ private const val CARD_NUMBER_SART_DIGITS_COUNT = 12
 private const val DATE_PART_LENGTH = 2
 private const val CVV_LENGTH = 3
 
+@Suppress("LongParameterList")
 internal class TangemPayDetailsStateFactory(
     private val cardNumberEnd: String,
     private val onBack: () -> Unit,
@@ -21,6 +24,8 @@ internal class TangemPayDetailsStateFactory(
     private val onReceive: () -> Unit,
     private val onReveal: () -> Unit,
     private val onCopy: (String) -> Unit,
+    private val onClickChangePin: () -> Unit,
+    private val onClickFreezeCard: () -> Unit,
 ) {
 
     private val cardStartMasked = maskedBlock(CARD_NUMBER_SART_DIGITS_COUNT)
@@ -28,7 +33,21 @@ internal class TangemPayDetailsStateFactory(
     private val cvvMasked = maskedBlock(CVV_LENGTH)
 
     fun getInitialState() = TangemPayDetailsUM(
-        topBarConfig = TangemPayDetailsTopBarConfig(onBackClick = onBack, items = null),
+        topBarConfig = TangemPayDetailsTopBarConfig(
+            onBackClick = onBack,
+            items = persistentListOf(
+                TangemDropdownMenuItem(
+                    title = TextReference.Res(R.string.tangempay_card_details_change_pin),
+                    textColorProvider = { TangemTheme.colors.text.primary1 },
+                    onClick = onClickChangePin,
+                ),
+                TangemDropdownMenuItem(
+                    title = TextReference.Res(R.string.tangempay_card_details_freeze_card),
+                    textColorProvider = { TangemTheme.colors.text.primary1 },
+                    onClick = onClickFreezeCard,
+                ),
+            ),
+        ),
         pullToRefreshConfig = PullToRefreshConfig(isRefreshing = false, onRefresh = onRefresh),
         balanceBlockState = TangemPayDetailsBalanceBlockState.Loading(
             actionButtons = persistentListOf(
