309c704afffe54eb2475c63405ba7140eb9426da
Tangem <git@tangem.com>
2025-10-17 13:50:20 +0500

    Updated on 2025-12-18

--- data/visa/src/main/kotlin/com/tangem/data/visa/utils/TangemPayTxHistoryItemConverter.kt
+++ data/visa/src/main/kotlin/com/tangem/data/visa/utils/TangemPayTxHistoryItemConverter.kt
@@ -1,13 +1,18 @@
 package com.tangem.data.visa.utils
 
+import com.squareup.moshi.Moshi
 import com.tangem.datasource.api.pay.models.response.TangemPayTxHistoryResponse
 import com.tangem.domain.visa.model.TangemPayTxHistoryItem
 import com.tangem.utils.converter.Converter
 import timber.log.Timber
 import java.util.Currency
 
-internal object TangemPayTxHistoryItemConverter :
+internal class TangemPayTxHistoryItemConverter(moshi: Moshi) :
     Converter<TangemPayTxHistoryResponse.Transaction, TangemPayTxHistoryItem?> {
+
+    private val spendAdapter = moshi.adapter(TangemPayTxHistoryResponse.Spend::class.java)
+    private val paymentAdapter = moshi.adapter(TangemPayTxHistoryResponse.Payment::class.java)
+    private val feeAdapter = moshi.adapter(TangemPayTxHistoryResponse.Fee::class.java)
 
     override fun convert(value: TangemPayTxHistoryResponse.Transaction): TangemPayTxHistoryItem? {
         return value.spend?.let { convertSpend(id = value.id, spend = it) }
@@ -22,6 +27,7 @@ internal object TangemPayTxHistoryItemConverter :
     private fun convertSpend(id: String, spend: TangemPayTxHistoryResponse.Spend): TangemPayTxHistoryItem.Spend {
         return TangemPayTxHistoryItem.Spend(
             id = id,
+            jsonRepresentation = spendAdapter.toJson(spend),
             // If postedAt is null, it means transaction wasn't posted and was likely declined. Use authorizedAt
             date = spend.postedAt ?: spend.authorizedAt,
             amount = spend.amount,
@@ -41,15 +47,18 @@ internal object TangemPayTxHistoryItemConverter :
     ): TangemPayTxHistoryItem.Payment {
         return TangemPayTxHistoryItem.Payment(
             id = id,
+            jsonRepresentation = paymentAdapter.toJson(payment),
             date = payment.postedAt,
             currency = Currency.getInstance(payment.currency),
             amount = payment.amount,
+            transactionHash = payment.transactionHash,
         )
     }
 
     private fun convertFee(id: String, fee: TangemPayTxHistoryResponse.Fee): TangemPayTxHistoryItem.Fee {
         return TangemPayTxHistoryItem.Fee(
             id = id,
+            jsonRepresentation = feeAdapter.toJson(fee),
             date = fee.postedAt,
             currency = Currency.getInstance(fee.currency),
             amount = fee.amount,
