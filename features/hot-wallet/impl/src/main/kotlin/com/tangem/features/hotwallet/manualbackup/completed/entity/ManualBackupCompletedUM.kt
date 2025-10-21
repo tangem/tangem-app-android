05b919c394061c49e6585442f6d9105b1db83e51
Tangem <git@tangem.com>
2025-10-21 14:35:01 +0500

    Updated on 2026-05-07

--- features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/manualbackup/completed/entity/ManualBackupCompletedUM.kt
+++ features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/manualbackup/completed/entity/ManualBackupCompletedUM.kt
@@ -2,4 +2,5 @@ package com.tangem.features.hotwallet.manualbackup.completed.entity
 
 internal data class ManualBackupCompletedUM(
     val onContinueClick: () -> Unit,
+    val isLoading: Boolean,
 )
