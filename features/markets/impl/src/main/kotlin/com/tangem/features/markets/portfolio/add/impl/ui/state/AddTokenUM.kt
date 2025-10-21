7a3305c88d1a609dad5de8021a8433a641d8a272
Tangem <git@tangem.com>
2025-10-21 14:44:33 +0700

    Updated on 2026-08-19

--- features/markets/impl/src/main/kotlin/com/tangem/features/markets/portfolio/add/impl/ui/state/AddTokenUM.kt
+++ features/markets/impl/src/main/kotlin/com/tangem/features/markets/portfolio/add/impl/ui/state/AddTokenUM.kt
@@ -1,24 +1,16 @@
 package com.tangem.features.markets.portfolio.add.impl.ui.state
 
 import androidx.annotation.DrawableRes
-import com.tangem.common.ui.account.CryptoPortfolioIconUM
+import com.tangem.common.ui.account.PortfolioSelectUM
 import com.tangem.core.ui.components.token.state.TokenItemState
 import com.tangem.core.ui.extensions.TextReference
 
 data class AddTokenUM(
     val tokenToAdd: TokenItemState,
     val network: Network,
-    val portfolio: Portfolio,
+    val portfolio: PortfolioSelectUM,
     val button: Button,
 ) {
-    data class Portfolio(
-        val accountIconUM: CryptoPortfolioIconUM?,
-        val name: TextReference,
-        val editable: Boolean,
-        val onClick: () -> Unit,
-    ) {
-        val isAccountMode get() = accountIconUM != null
-    }
 
     data class Network(
         @DrawableRes val icon: Int,
