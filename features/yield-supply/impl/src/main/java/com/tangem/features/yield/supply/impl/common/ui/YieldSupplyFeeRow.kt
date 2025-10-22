efd0026b2db107bd87bff46679e408d4efe4e36c
Tangem <git@tangem.com>
2025-10-22 22:05:13 +0500

    Updated on 2026-08-07

--- features/yield-supply/impl/src/main/java/com/tangem/features/yield/supply/impl/common/ui/YieldSupplyFeeRow.kt
+++ features/yield-supply/impl/src/main/java/com/tangem/features/yield/supply/impl/common/ui/YieldSupplyFeeRow.kt
@@ -1,17 +1,12 @@
 package com.tangem.features.yield.supply.impl.common.ui
 
 import androidx.compose.animation.AnimatedContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.tangem.core.ui.components.SpacerWMax
@@ -23,15 +18,11 @@ import com.tangem.core.ui.res.TangemTheme
 import com.tangem.features.yield.supply.impl.R
 
 @Composable
-internal fun YieldSupplyFeeRow(title: TextReference, value: TextReference?) {
+internal fun YieldSupplyFeeRow(title: TextReference, value: TextReference?, modifier: Modifier = Modifier) {
     Row(
         horizontalArrangement = Arrangement.spacedBy(12.dp),
         verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .fillMaxWidth()
-            .clip(RoundedCornerShape(14.dp))
-            .background(TangemTheme.colors.background.action)
-            .padding(horizontal = 16.dp, vertical = 12.dp),
+        modifier = modifier,
     ) {
         Text(
             text = title.resolveReference(),
