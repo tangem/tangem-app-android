19473b9fec853d22423e4de6f30ec8c6dc8c7b31
Tangem <git@tangem.com>
2025-10-22 16:12:30 +0500

    Updated on 2026-06-24

--- features/tangempay/onboarding/impl/src/main/kotlin/com/tangem/features/tangempay/ui/TandemPayOnboardingScreen.kt
+++ features/tangempay/onboarding/impl/src/main/kotlin/com/tangem/features/tangempay/ui/TandemPayOnboardingScreen.kt
@@ -9,24 +9,21 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.datasource.CollectionPreviewParameterProvider
 import com.tangem.core.ui.R
 import com.tangem.core.ui.components.appbar.AppBarWithBackButton
 import com.tangem.core.ui.res.TangemThemePreview
 import com.tangem.core.ui.utils.WindowInsetsZero
 
 @Composable
-internal fun TandemPayOnboardingScreen(
-    state: TangemPayOnboardingScreenState,
-    onOpenKycClick: () -> Unit,
-    onBackClick: () -> Unit,
-    modifier: Modifier = Modifier,
-) {
+internal fun TandemPayOnboardingScreen(state: TangemPayOnboardingScreenState, modifier: Modifier = Modifier) {
     Scaffold(
         modifier = modifier.systemBarsPadding(),
         topBar = {
             AppBarWithBackButton(
                 modifier = Modifier.statusBarsPadding(),
-                onBackClick = onBackClick,
+                onBackClick = state.onBackClick,
                 iconRes = R.drawable.ic_back_24,
             )
         },
@@ -37,7 +34,7 @@ internal fun TandemPayOnboardingScreen(
                     .padding(paddingValues)
                     .fillMaxSize(),
                 state = state,
-                onButtonClick = onOpenKycClick,
+                onButtonClick = state.onGetCardClick,
             )
         },
     )
@@ -46,8 +43,35 @@ internal fun TandemPayOnboardingScreen(
 @Preview(showBackground = true)
 @Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
-private fun PreviewDarkTheme() {
+private fun PreviewDarkTheme(
+    @PreviewParameter(TangemPayOnboardingScreenStateProvider::class)
+    state: TangemPayOnboardingScreenState,
+) {
     TangemThemePreview {
-        TandemPayOnboardingScreen(state = TangemPayOnboardingScreenState(), {}, {})
+        TandemPayOnboardingScreen(state = state, modifier = Modifier.fillMaxSize())
     }
 }
+
+private class TangemPayOnboardingScreenStateProvider :
+    CollectionPreviewParameterProvider<TangemPayOnboardingScreenState>(
+        listOf(
+            TangemPayOnboardingScreenState(
+                fullScreenLoading = true,
+                buttonLoading = false,
+                onGetCardClick = {},
+                onBackClick = {},
+            ),
+            TangemPayOnboardingScreenState(
+                fullScreenLoading = false,
+                buttonLoading = false,
+                onGetCardClick = {},
+                onBackClick = {},
+            ),
+            TangemPayOnboardingScreenState(
+                fullScreenLoading = false,
+                buttonLoading = true,
+                onGetCardClick = {},
+                onBackClick = {},
+            ),
+        ),
+    )
