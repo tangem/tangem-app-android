cf4bd826bc118f5b8e3cf73579ebf008ed3293b3
Tangem <git@tangem.com>
2025-10-27 12:15:41 +0100

    Updated on 2026-02-25

--- domain/staking/src/main/java/com/tangem/domain/staking/usecase/StakingApyFlowUseCase.kt
+++ domain/staking/src/main/java/com/tangem/domain/staking/usecase/StakingApyFlowUseCase.kt
@@ -4,34 +4,24 @@ import com.tangem.domain.staking.model.stakekit.Yield
 import com.tangem.domain.staking.repositories.StakingRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
-import java.math.BigDecimal
 
 /**
- * Emits a map of APY values per currency for staking.
+ * Emits a map of Validators values per currency for staking.
  *
  * Return map:
  * - key: currency staking key (network.backendId + "_" + symbol)
- * - value: APY as string
+ * - value: validators
  */
 class StakingApyFlowUseCase(private val stakingRepository: StakingRepository) {
 
-    operator fun invoke(): Flow<Map<String, BigDecimal>> {
+    operator fun invoke(): Flow<Map<String, List<Yield.Validator>>> {
         return stakingRepository.getEnabledYields()
             .map { yields ->
                 yields.associate { yield ->
                     val key = "${yield.token.coinGeckoId}_${yield.token.symbol}"
-                    val apy = calculateApy(yield)
+                    val apy = yield.validators
                     key to apy
                 }
             }
-    }
-
-    private fun calculateApy(yield: Yield): BigDecimal {
-        val rates = yield.validators.mapNotNull { it.rewardInfo?.rate }
-        return if (rates.isNotEmpty()) {
-            rates.maxOf { it }
-        } else {
-            yield.apy
-        }
     }
 }
