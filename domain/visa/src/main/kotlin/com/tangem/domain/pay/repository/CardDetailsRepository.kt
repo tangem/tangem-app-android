bb195b2c6c175f8f17ba10a9e0ea5bf8990d1a30
Tangem <git@tangem.com>
2025-10-27 13:07:28 +0400

    Updated on 2026-09-23

--- domain/visa/src/main/kotlin/com/tangem/domain/pay/repository/CardDetailsRepository.kt
+++ domain/visa/src/main/kotlin/com/tangem/domain/pay/repository/CardDetailsRepository.kt
@@ -2,6 +2,7 @@ package com.tangem.domain.pay.repository
 
 import arrow.core.Either
 import com.tangem.core.error.UniversalError
+import com.tangem.domain.pay.model.SetPinResult
 import com.tangem.domain.pay.model.TangemPayCardBalance
 import com.tangem.domain.pay.model.TangemPayCardDetails
 
@@ -10,4 +11,6 @@ interface CardDetailsRepository {
     suspend fun getCardBalance(): Either<UniversalError, TangemPayCardBalance>
 
     suspend fun revealCardDetails(): Either<UniversalError, TangemPayCardDetails>
+
+    suspend fun setPin(pin: String): Either<UniversalError, SetPinResult>
 }
