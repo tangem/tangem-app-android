9845bc9ae67403380ac8e96f2ad148cccd54aa84
Tangem <git@tangem.com>
2025-10-28 15:12:55 +0400

    Updated on 2026-08-27

--- features/yield-supply/impl/src/main/java/com/tangem/features/yield/supply/impl/main/model/YieldSupplyModel.kt
+++ features/yield-supply/impl/src/main/java/com/tangem/features/yield/supply/impl/main/model/YieldSupplyModel.kt
@@ -13,6 +13,7 @@ import com.tangem.core.ui.extensions.combinedReference
 import com.tangem.core.ui.extensions.resourceReference
 import com.tangem.core.ui.extensions.stringReference
 import com.tangem.domain.balancehiding.GetBalanceHidingSettingsUseCase
+import com.tangem.domain.models.StatusSource
 import com.tangem.domain.models.currency.CryptoCurrency
 import com.tangem.domain.models.currency.CryptoCurrencyStatus
 import com.tangem.domain.models.currency.yieldSupplyNotAllAmountSupplied
@@ -35,6 +36,8 @@ import com.tangem.features.yield.supply.impl.main.model.transformers.YieldSupply
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import com.tangem.utils.coroutines.DelayedWork
 import com.tangem.utils.transformer.update
+import com.tangem.utils.coroutines.JobHolder
+import com.tangem.utils.coroutines.saveIn
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
@@ -84,6 +87,7 @@ internal class YieldSupplyModel @Inject constructor(
         field = MutableStateFlow(false)
 
     private var lastYieldSupplyStatus: YieldSupplyStatus? = null
+    private val fetchCurrencyJobHolder = JobHolder()
 
     init {
         checkIfYieldSupplyIsAvailable()
@@ -183,6 +187,19 @@ internal class YieldSupplyModel @Inject constructor(
         val yieldSupplyStatus = cryptoCurrencyStatus.value.yieldSupplyStatus
         val tokenProtocolStatus = yieldSupplyRepository.getTokenProtocolStatus(cryptoCurrency)
         val isActive = yieldSupplyStatus?.isActive == true
+        val isCryptoCurrencyStatusFromCache = cryptoCurrencyStatus.value.sources.networkSource != StatusSource.ACTUAL
+        val processing = uiState.value is YieldSupplyUM.Processing
+        Timber.d(
+            "currentUiState ${uiState.value.javaClass} \n" +
+                "yieldSupplyStatus $yieldSupplyStatus" +
+                "tokenProtocolStatus $tokenProtocolStatus " +
+                "isActive $isActive " +
+                "processing $processing " +
+                "isCryptoCurrencyStatusFromCache $isCryptoCurrencyStatusFromCache",
+        )
+        if (isCryptoCurrencyStatusFromCache && processing) {
+            return
+        }
 
         when {
             !isActive && tokenProtocolStatus == YieldSupplyEnterStatus.Enter -> {
@@ -215,8 +232,10 @@ internal class YieldSupplyModel @Inject constructor(
                     userWalletId = userWallet.walletId,
                     network = cryptoCurrency.network,
                 ),
-            )
-        }
+            ).onLeft {
+                fetchCurrencyWithDelay()
+            }
+        }.saveIn(fetchCurrencyJobHolder)
     }
 
     private fun loadActiveState(cryptoCurrencyStatus: CryptoCurrencyStatus, yieldSupplyStatus: YieldSupplyStatus) {
