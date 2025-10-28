8cd1af8af3548d9716a9e46c0e73aad608d81cb1
Tangem <git@tangem.com>
2025-10-28 11:52:48 +0400

    Updated on 2026-06-05

--- features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/di/TangemPayDetailsFeatureModule.kt
+++ features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/di/TangemPayDetailsFeatureModule.kt
@@ -2,6 +2,8 @@ package com.tangem.features.tangempay.di
 
 import com.tangem.features.tangempay.components.DefaultTangemPayDetailsContainerComponent
 import com.tangem.features.tangempay.components.TangemPayDetailsContainerComponent
+import com.tangem.features.tangempay.model.listener.CardDetailsEventListener
+import com.tangem.features.tangempay.model.listener.DefaultCardDetailsEventListener
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -17,4 +19,8 @@ internal interface TangemPayDetailsFeatureModule {
     fun bindTangemPayDetailsContainerComponentFactory(
         factory: DefaultTangemPayDetailsContainerComponent.Factory,
     ): TangemPayDetailsContainerComponent.Factory
+
+    @Binds
+    @Singleton
+    fun bindCardDetailsEventListener(impl: DefaultCardDetailsEventListener): CardDetailsEventListener
 }
