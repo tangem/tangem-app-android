7f8ff3cf1ef4a0d7574cdbf2b2906e7540a4f78f
Tangem <git@tangem.com>
2025-10-28 11:52:48 +0400

    Updated on 2026-05-04

--- features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/entity/TangemPayDetailsStateFactory.kt
+++ features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/entity/TangemPayDetailsStateFactory.kt
@@ -8,29 +8,15 @@ import com.tangem.core.ui.extensions.TextReference
 import com.tangem.core.ui.extensions.resourceReference
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.features.tangempay.details.impl.R
-import com.tangem.features.tangempay.utils.CardDetailsFormatUtil
-import com.tangem.utils.StringsSigns
 import kotlinx.collections.immutable.persistentListOf
 
-private const val CARD_NUMBER_SART_DIGITS_COUNT = 12
-private const val DATE_PART_LENGTH = 2
-private const val CVV_LENGTH = 3
-
-@Suppress("LongParameterList")
 internal class TangemPayDetailsStateFactory(
-    private val cardNumberEnd: String,
     private val onBack: () -> Unit,
     private val onRefresh: (ShowRefreshState) -> Unit,
     private val onReceive: () -> Unit,
-    private val onReveal: () -> Unit,
-    private val onCopy: (String) -> Unit,
     private val onClickChangePin: () -> Unit,
     private val onClickFreezeCard: () -> Unit,
 ) {
-
-    private val cardStartMasked = maskedBlock(CARD_NUMBER_SART_DIGITS_COUNT)
-    private val dateMasked = maskedBlock(DATE_PART_LENGTH)
-    private val cvvMasked = maskedBlock(CVV_LENGTH)
 
     fun getInitialState() = TangemPayDetailsUM(
         topBarConfig = TangemPayDetailsTopBarConfig(
@@ -58,18 +44,6 @@ internal class TangemPayDetailsStateFactory(
                 ),
             ),
         ),
-        cardDetailsUM = TangemPayCardDetailsUM(
-            number = CardDetailsFormatUtil.formatCardNumber(cardNumber = "$cardStartMasked$cardNumberEnd"),
-            expiry = CardDetailsFormatUtil.formatDate(month = dateMasked, year = dateMasked),
-            cvv = cvvMasked,
-            buttonText = TextReference.Res(R.string.tangempay_card_details_reveal_text),
-            onClick = onReveal,
-            onCopy = onCopy,
-            isHidden = true,
-        ),
         isBalanceHidden = false,
-
     )
-
-    private fun maskedBlock(count: Int) = StringsSigns.DOT.repeat(count)
 }
