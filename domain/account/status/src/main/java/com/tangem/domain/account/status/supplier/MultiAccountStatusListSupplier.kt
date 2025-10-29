edac48fef5dc31a79fe7419d194f2b67e332b348
Tangem <git@tangem.com>
2025-10-29 16:09:39 +0400

    Updated on 2026-02-25

--- domain/account/status/src/main/java/com/tangem/domain/account/status/supplier/MultiAccountStatusListSupplier.kt
+++ domain/account/status/src/main/java/com/tangem/domain/account/status/supplier/MultiAccountStatusListSupplier.kt
@@ -3,6 +3,7 @@ package com.tangem.domain.account.status.supplier
 import com.tangem.domain.account.models.AccountStatusList
 import com.tangem.domain.account.status.producer.MultiAccountStatusListProducer
 import com.tangem.domain.core.flow.FlowCachingSupplier
+import kotlinx.coroutines.flow.Flow
 
 /**
  * Supplier that provides a list of [AccountStatusList]s for all user wallets.
@@ -12,4 +13,9 @@ import com.tangem.domain.core.flow.FlowCachingSupplier
 abstract class MultiAccountStatusListSupplier(
     override val factory: MultiAccountStatusListProducer.Factory,
     override val keyCreator: (Unit) -> String,
-) : FlowCachingSupplier<MultiAccountStatusListProducer, Unit, List<AccountStatusList>>()
+) : FlowCachingSupplier<MultiAccountStatusListProducer, Unit, List<AccountStatusList>>() {
+
+    operator fun invoke(): Flow<List<AccountStatusList>> {
+        return super.invoke(params = Unit)
+    }
+}
