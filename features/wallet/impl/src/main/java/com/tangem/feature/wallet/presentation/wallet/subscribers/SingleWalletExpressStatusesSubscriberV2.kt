de9b0d59506a385a3a5e32fbf8f687dc5a1e28c6
Tangem <git@tangem.com>
2025-10-30 12:35:06 +0400

    Updated on 2026-09-23

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/subscribers/SingleWalletExpressStatusesSubscriberV2.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/subscribers/SingleWalletExpressStatusesSubscriberV2.kt
@@ -1,93 +1,84 @@
 package com.tangem.feature.wallet.presentation.wallet.subscribers
 
-import arrow.core.Either
-import arrow.core.getOrElse
 import com.tangem.core.analytics.api.AnalyticsEventHandler
+import com.tangem.domain.account.status.supplier.SingleAccountStatusListSupplier
 import com.tangem.domain.appcurrency.GetSelectedAppCurrencyUseCase
-import com.tangem.domain.appcurrency.model.AppCurrency
-import com.tangem.domain.models.currency.CryptoCurrencyStatus
 import com.tangem.domain.models.wallet.UserWallet
 import com.tangem.domain.onramp.GetOnrampTransactionsUseCase
 import com.tangem.domain.onramp.OnrampRemoveTransactionUseCase
 import com.tangem.domain.onramp.model.cache.OnrampTransaction
-import com.tangem.domain.tokens.GetSingleCryptoCurrencyStatusUseCase
-import com.tangem.domain.tokens.error.CurrencyStatusError
 import com.tangem.feature.wallet.child.wallet.model.intents.WalletClickIntents
 import com.tangem.feature.wallet.presentation.wallet.state.WalletStateController
 import com.tangem.feature.wallet.presentation.wallet.state.transformers.SetExpressStatusesTransformer
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
-import timber.log.Timber
 
 @Suppress("LongParameterList")
-@Deprecated("Use SingleWalletExpressStatusesSubscriberV2 instead")
-internal class SingleWalletExpressStatusesSubscriber(
-    private val userWallet: UserWallet,
-    private val stateHolder: WalletStateController,
+internal class SingleWalletExpressStatusesSubscriberV2(
+    override val userWallet: UserWallet,
+    override val singleAccountStatusListSupplier: SingleAccountStatusListSupplier,
+    private val getOnrampTransactionsUseCase: GetOnrampTransactionsUseCase,
+    private val getSelectedAppCurrencyUseCase: GetSelectedAppCurrencyUseCase,
+    private val onrampRemoveTransactionUseCase: OnrampRemoveTransactionUseCase,
+    private val stateController: WalletStateController,
     private val clickIntents: WalletClickIntents,
     private val analyticsEventHandler: AnalyticsEventHandler,
-    private val getSelectedAppCurrencyUseCase: GetSelectedAppCurrencyUseCase,
-    private val getSingleCryptoCurrencyStatusUseCase: GetSingleCryptoCurrencyStatusUseCase,
-    private val getOnrampTransactionsUseCase: GetOnrampTransactionsUseCase,
-    private val onrampRemoveTransactionUseCase: OnrampRemoveTransactionUseCase,
-) : WalletSubscriber() {
+) : BasicSingleWalletSubscriber() {
 
-    override fun create(
-        coroutineScope: CoroutineScope,
-    ): Flow<Pair<Either<CurrencyStatusError, CryptoCurrencyStatus>, AppCurrency>> {
-        return combine(
-            flow = getSingleCryptoCurrencyStatusUseCase.invokeSingleWallet(userWalletId = userWallet.walletId)
-                .conflate()
-                .distinctUntilChanged(),
-            flow2 = getSelectedAppCurrencyUseCase()
-                .conflate()
-                .distinctUntilChanged()
-                .map { maybeAppCurrency -> maybeAppCurrency.getOrElse { AppCurrency.Default } },
-            transform = { maybeCurrencyStatus, appCurrency -> maybeCurrencyStatus to appCurrency },
-        ).onEach { maybeCurrencyStatusAndAppCurrency ->
-            val status = maybeCurrencyStatusAndAppCurrency.first.getOrElse {
-                Timber.e("Unable to get primary currency status: $it")
-                return@onEach
-            }
-
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override fun create(coroutineScope: CoroutineScope): Flow<*> {
+        val getOnrampTransactionsFlow = getPrimaryCurrencyStatusFlow().flatMapLatest { currencyStatus ->
             getOnrampTransactionsUseCase(
                 userWalletId = userWallet.walletId,
-                cryptoCurrencyId = status.currency.id,
-            ).onEach { maybeTransaction ->
+                cryptoCurrencyId = currencyStatus.currency.id,
+            )
+                .map { currencyStatus to it }
+        }
+
+        return combine(
+            flow = getOnrampTransactionsFlow,
+            flow2 = getSelectedAppCurrencyUseCase.invokeOrDefault(),
+            transform = ::toTriple,
+        )
+            .onEach { (status, maybeTransaction, appCurrency) ->
                 maybeTransaction.fold(
                     ifRight = { onrampTxs ->
                         onrampTxs.clearHiddenTerminal()
-                        stateHolder.update(
+                        stateController.update(
                             SetExpressStatusesTransformer(
                                 userWalletId = userWallet.walletId,
                                 onrampTxs = onrampTxs,
                                 clickIntents = clickIntents,
                                 cryptoCurrencyStatus = status,
-                                appCurrency = maybeCurrencyStatusAndAppCurrency.second,
+                                appCurrency = appCurrency,
                                 analyticsEventHandler = analyticsEventHandler,
                             ),
                         )
                     },
                     ifLeft = {
-                        stateHolder.update(
+                        stateController.update(
                             SetExpressStatusesTransformer(
                                 userWalletId = userWallet.walletId,
                                 onrampTxs = listOf(),
                                 clickIntents = clickIntents,
                                 cryptoCurrencyStatus = status,
-                                appCurrency = maybeCurrencyStatusAndAppCurrency.second,
+                                appCurrency = appCurrency,
                                 analyticsEventHandler = analyticsEventHandler,
                             ),
                         )
                     },
                 )
             }
-                .launchIn(coroutineScope)
-        }
+    }
+
+    private fun <A, B, C> toTriple(firstPair: Pair<A, B>, second: C): Triple<A, B, C> {
+        return Triple(firstPair.first, firstPair.second, second)
     }
 
     private suspend fun List<OnrampTransaction>.clearHiddenTerminal() {
-        this.filter { it.status.isHidden && it.status.isTerminal }
+        this
+            .filter { it.status.isHidden && it.status.isTerminal }
             .forEach { onrampRemoveTransactionUseCase(txId = it.txId) }
     }
 }
