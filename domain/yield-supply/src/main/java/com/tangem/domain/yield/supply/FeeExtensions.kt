0bf768d2ea5f432fd8c0a189f937723b22c43ab9
Tangem <git@tangem.com>
2025-11-04 17:25:11 +0500

    Updated on 2026-09-25

--- domain/yield-supply/src/main/java/com/tangem/domain/yield/supply/FeeExtensions.kt
+++ domain/yield-supply/src/main/java/com/tangem/domain/yield/supply/FeeExtensions.kt
@@ -3,6 +3,10 @@ package com.tangem.domain.yield.supply
 import com.tangem.blockchain.common.transaction.Fee
 import com.tangem.domain.models.currency.CryptoCurrency
 import java.math.BigInteger
+import java.math.RoundingMode
+
+private val HUNDRED_PERCENT = 100.toBigInteger() // base 100%
+val INCREASE_GAS_LIMIT_FOR_SUPPLY = 112.toBigInteger() // 12% increase
 
 fun Fee.fixFee(cryptoCurrency: CryptoCurrency, gasLimit: BigInteger): Fee = when (this) {
     is Fee.Ethereum.Legacy -> copy(
@@ -20,4 +24,24 @@ fun Fee.fixFee(cryptoCurrency: CryptoCurrency, gasLimit: BigInteger): Fee = when
         ),
     )
     else -> this
+}
+
+/**
+ * Increase gasLimit for Fee.Ethereum
+ */
+fun Fee.increaseGasLimitBy(percent: BigInteger): Fee {
+    if (this !is Fee.Ethereum) return this
+    val gasLimit = this.gasLimit
+    val increasedGasPrice = this.amount.value?.movePointRight(this.amount.decimals)
+        ?.divide(gasLimit.toBigDecimal(), RoundingMode.HALF_UP)
+    val increasedGasLimit = gasLimit
+        .multiply(percent)
+        .divide(HUNDRED_PERCENT)
+    val increasedAmount = this.amount.copy(
+        value = increasedGasLimit.toBigDecimal().multiply(increasedGasPrice).movePointLeft(this.amount.decimals),
+    )
+    return when (this) {
+        is Fee.Ethereum.EIP1559 -> copy(amount = increasedAmount, gasLimit = increasedGasLimit)
+        is Fee.Ethereum.Legacy -> copy(amount = increasedAmount, gasLimit = increasedGasLimit)
+    }
 }
