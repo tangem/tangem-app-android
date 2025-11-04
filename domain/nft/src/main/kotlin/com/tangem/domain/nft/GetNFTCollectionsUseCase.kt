a5cd9f4ae52a589e6bad30b05e71feb21ff04935
Tangem <git@tangem.com>
2025-11-05 03:49:15 +0700

    Updated on 2026-09-14

--- domain/nft/src/main/kotlin/com/tangem/domain/nft/GetNFTCollectionsUseCase.kt
+++ domain/nft/src/main/kotlin/com/tangem/domain/nft/GetNFTCollectionsUseCase.kt
@@ -1,25 +1,54 @@
 package com.tangem.domain.nft
 
+import com.tangem.domain.account.featuretoggle.AccountsFeatureToggles
+import com.tangem.domain.account.status.supplier.SingleAccountStatusListSupplier
+import com.tangem.domain.models.account.Account
+import com.tangem.domain.models.account.AccountStatus
+import com.tangem.domain.models.currency.CryptoCurrency
+import com.tangem.domain.models.wallet.UserWalletId
 import com.tangem.domain.nft.models.NFTCollections
+import com.tangem.domain.nft.models.WalletNFTCollections
 import com.tangem.domain.nft.repository.NFTRepository
 import com.tangem.domain.tokens.repository.CurrenciesRepository
-import com.tangem.domain.models.wallet.UserWalletId
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.*
 
 class GetNFTCollectionsUseCase(
     private val currenciesRepository: CurrenciesRepository,
     private val nftRepository: NFTRepository,
+    private val singleAccountStatusListSupplier: SingleAccountStatusListSupplier,
+    private val accountsFeatureToggles: AccountsFeatureToggles,
 ) {
 
     @OptIn(ExperimentalCoroutinesApi::class)
-    operator fun invoke(userWalletId: UserWalletId): Flow<List<NFTCollections>> = currenciesRepository
-        .getWalletCurrenciesUpdates(userWalletId)
-        .flatMapLatest {
-            val networks = it
-                .map { cryptoCurrency -> cryptoCurrency.network }
-                .distinct()
-            nftRepository.observeCollections(userWalletId, networks)
+    operator fun invoke(userWalletId: UserWalletId): Flow<List<NFTCollections>> =
+        if (accountsFeatureToggles.isFeatureEnabled) {
+            invokeForAccounts(userWalletId).map { it.flattenCollections }
+        } else {
+            currenciesRepository
+                .getWalletCurrenciesUpdates(userWalletId)
+                .flatMapLatest {
+                    nftCollections(userWalletId, it)
+                }
         }
+
+    fun invokeForAccounts(userWalletId: UserWalletId): Flow<WalletNFTCollections> {
+        fun AccountStatus.flowOfNFTCollections(): Flow<Pair<Account, List<NFTCollections>>> =
+            nftCollections(userWalletId, this.flattenCurrencies().map { it.currency })
+                .map { nfts -> this.account to nfts }
+
+        return singleAccountStatusListSupplier(userWalletId)
+            .mapLatest { statusList -> statusList.accountStatuses.map { it.flowOfNFTCollections() } }
+            .flatMapLatest { flows -> combine(flows) { WalletNFTCollections(it.toMap()) } }
+    }
+
+    private fun nftCollections(
+        userWalletId: UserWalletId,
+        cryptoCurrencies: List<CryptoCurrency>,
+    ): Flow<List<NFTCollections>> {
+        val networks = cryptoCurrencies
+            .map { cryptoCurrency -> cryptoCurrency.network }
+            .distinct()
+        return nftRepository.observeCollections(userWalletId, networks)
+    }
 }
