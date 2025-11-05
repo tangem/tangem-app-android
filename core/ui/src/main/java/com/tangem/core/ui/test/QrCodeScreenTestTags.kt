07bb37c1ae2eafa6ccdc166a732099249321359a
Tangem <git@tangem.com>
2025-11-05 14:51:28 +0300

    Updated on 2026-07-07

---
+++ core/ui/src/main/java/com/tangem/core/ui/test/QrCodeScreenTestTags.kt
@@ -0,0 +1,6 @@
+package com.tangem.core.ui.test
+
+object QrCodeScreenTestTags {
+    const val FLASHLIGHT_BUTTON = "QR_CODE_SCREEN_FLASHLIGHT_BUTTON"
+    const val GALLERY_BUTTON = "QR_CODE_SCREEN_GALLERY_BUTTON"
+}
