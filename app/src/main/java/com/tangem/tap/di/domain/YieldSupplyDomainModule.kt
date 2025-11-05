afe3065de3b52b456ace53694fd0491f436e64eb
Tangem <git@tangem.com>
2025-11-05 20:11:39 +0500

    Updated on 2026-09-01

--- app/src/main/java/com/tangem/tap/di/domain/YieldSupplyDomainModule.kt
+++ app/src/main/java/com/tangem/tap/di/domain/YieldSupplyDomainModule.kt
@@ -169,8 +169,8 @@ internal object YieldSupplyDomainModule {
         yieldSupplyRepository: YieldSupplyRepository,
         quotesRepository: QuotesRepository,
         currenciesRepository: CurrenciesRepository,
-    ): YieldSupplyGetTokenMaxFeeUseCase {
-        return YieldSupplyGetTokenMaxFeeUseCase(
+    ): YieldSupplyGetMaxFeeUseCase {
+        return YieldSupplyGetMaxFeeUseCase(
             yieldSupplyRepository = yieldSupplyRepository,
             quotesRepository = quotesRepository,
             currenciesRepository = currenciesRepository,
