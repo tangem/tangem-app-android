eb2ac4ad29d56fbc9a278649fcefdb3064900b07
Tangem <git@tangem.com>
2025-11-06 15:31:30 +0500

    Updated on 2026-06-27

--- domain/visa/models/src/main/kotlin/com/tangem/domain/pay/TangemPayDetailsConfig.kt
+++ domain/visa/models/src/main/kotlin/com/tangem/domain/pay/TangemPayDetailsConfig.kt
@@ -4,6 +4,8 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class TangemPayDetailsConfig(
+    val cardId: String,
+    val isCardFrozen: Boolean,
     val customerWalletAddress: String,
     val cardNumberEnd: String,
     val chainId: Int,
