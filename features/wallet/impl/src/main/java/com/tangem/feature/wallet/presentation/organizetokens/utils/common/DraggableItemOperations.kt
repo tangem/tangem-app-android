76dfd82df46cd4c574b5e2d9497ec3d06088dddb
Tangem <git@tangem.com>
2025-11-06 15:59:41 +0500

    Updated on 2026-02-06

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/organizetokens/utils/common/DraggableItemOperations.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/organizetokens/utils/common/DraggableItemOperations.kt
@@ -2,6 +2,9 @@ package com.tangem.feature.wallet.presentation.organizetokens.utils.common
 
 import com.tangem.feature.wallet.presentation.organizetokens.model.DraggableItem
 
-internal fun getGroupPlaceholder(index: Int): DraggableItem.Placeholder {
-    return DraggableItem.Placeholder(id = "placeholder_${index.inc()}")
+internal fun getGroupPlaceholder(index: Int, accountId: String = ""): DraggableItem.Placeholder {
+    return DraggableItem.Placeholder(
+        id = "placeholder_${accountId}_${index.inc()}",
+        accountId = accountId,
+    )
 }
