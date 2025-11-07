15d6b8dffebd099e82a5709098963dc24fda9ca9
Tangem <git@tangem.com>
2025-11-07 10:41:10 +0500

    Updated on 2026-06-01

--- features/wallet-settings/impl/build.gradle.kts
+++ features/wallet-settings/impl/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(deps.compose.material3)
     implementation(deps.compose.shimmer)
     implementation(deps.decompose.ext.compose)
+    implementation(deps.compose.reorderableV2)
 
     /* DI */
     implementation(deps.hilt.android)
