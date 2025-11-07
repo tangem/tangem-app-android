01ce2ffe73fb4d1fe7c8354ad64853c7a0bbc991
Tangem <git@tangem.com>
2025-11-07 10:41:10 +0500

    Updated on 2025-12-30

--- features/wallet-settings/impl/src/main/kotlin/com/tangem/feature/walletsettings/entity/WalletSettingsUM.kt
+++ features/wallet-settings/impl/src/main/kotlin/com/tangem/feature/walletsettings/entity/WalletSettingsUM.kt
@@ -9,6 +9,13 @@ internal data class WalletSettingsUM(
     val items: PersistentList<WalletSettingsItemUM>,
     val requestPushNotificationsPermission: Boolean = false,
     val onPushNotificationPermissionGranted: (Boolean) -> Unit,
+    val accountReorderUM: AccountReorderUM,
     val isWalletBackedUp: Boolean = true,
     val walletUpgradeDismissed: Boolean = false,
+)
+
+internal data class AccountReorderUM(
+    val isDragEnabled: Boolean,
+    val onMove: (fromIndex: Int, toIndex: Int) -> Unit,
+    val onDragStopped: () -> Unit,
 )
