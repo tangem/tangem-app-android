f652ca6aeeb97e38cac681d53620a9a2f8f15587
Tangem <git@tangem.com>
2025-11-12 13:27:24 +0200

    Updated on 2026-09-14

--- domain/legacy/src/main/java/com/tangem/domain/analytics/ChangeCardAnalyticsContextUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.tangem.domain.analytics
-
-import com.tangem.domain.models.scan.ScanResponse
-
-interface ChangeCardAnalyticsContextUseCase {
-
-    operator fun invoke(scanResponse: ScanResponse)
-}
