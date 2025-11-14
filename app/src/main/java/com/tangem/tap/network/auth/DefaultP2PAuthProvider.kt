aa126393a5babd5f2b2c99605b16246fe0b8a5a4
Tangem <git@tangem.com>
2025-11-14 15:45:02 +0200

    Updated on 2025-11-18

--- app/src/main/java/com/tangem/tap/network/auth/DefaultP2PAuthProvider.kt
+++ app/src/main/java/com/tangem/tap/network/auth/DefaultP2PAuthProvider.kt
@@ -8,9 +8,13 @@ internal class DefaultP2PAuthProvider(
 ) : P2PAuthProvider {
 
     override fun getApiKey(): String {
-        val keys = environmentConfigStorage.getConfigSync().p2pApiKey
-            ?: error("No P2P api keys provided")
+        // val keys = environmentConfigStorage.getConfigSync().p2pApiKey
+        //     ?: error("No P2P api keys provided")
+        //
+        // return keys.mainnet
 
-        return keys.mainnet
+        environmentConfigStorage
+
+        return "TODO restore p2p config call after release 5.30"
     }
 }
