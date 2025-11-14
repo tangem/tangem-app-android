f331804a1f7191696d539ea3a2eee19478acf619
Tangem <git@tangem.com>
2025-11-14 18:54:33 +0500

    Updated on 2026-07-23

--- features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/entity/TangemPayDetailsStateFactory.kt
+++ features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/entity/TangemPayDetailsStateFactory.kt
@@ -2,23 +2,19 @@ package com.tangem.features.tangempay.entity
 
 import com.tangem.core.ui.components.buttons.actions.ActionButtonConfig
 import com.tangem.core.ui.components.containers.pullToRefresh.PullToRefreshConfig
-import com.tangem.core.ui.components.containers.pullToRefresh.PullToRefreshConfig.ShowRefreshState
 import com.tangem.core.ui.components.dropdownmenu.TangemDropdownMenuItem
 import com.tangem.core.ui.extensions.resourceReference
 import com.tangem.core.ui.extensions.themedColor
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.features.tangempay.details.impl.R
+import com.tangem.features.tangempay.utils.TangemPayDetailIntents
 import kotlinx.collections.immutable.persistentListOf
 
 @Suppress("LongParameterList")
 internal class TangemPayDetailsStateFactory(
     private val onBack: () -> Unit,
-    private val onRefresh: (ShowRefreshState) -> Unit,
-    private val onAddFunds: () -> Unit,
-    private val onClickChangePin: () -> Unit,
+    private val intents: TangemPayDetailIntents,
     private val isCardFrozen: Boolean,
-    private val onClickFreezeCard: () -> Unit,
-    private val onClickUnfreezeCard: () -> Unit,
 ) {
 
     fun getInitialState(): TangemPayDetailsUM {
@@ -28,7 +24,7 @@ internal class TangemPayDetailsStateFactory(
                 dropdownItem = TangemDropdownMenuItem(
                     title = resourceReference(R.string.tangempay_card_details_unfreeze_card),
                     textColor = themedColor { TangemTheme.colors.text.primary1 },
-                    onClick = onClickUnfreezeCard,
+                    onClick = intents::onClickUnfreezeCard,
                 ),
             )
         } else {
@@ -37,7 +33,7 @@ internal class TangemPayDetailsStateFactory(
                 dropdownItem = TangemDropdownMenuItem(
                     title = resourceReference(R.string.tangempay_card_details_freeze_card),
                     textColor = themedColor { TangemTheme.colors.text.primary1 },
-                    onClick = onClickFreezeCard,
+                    onClick = intents::onClickFreezeCard,
                 ),
             )
         }
@@ -51,7 +47,15 @@ internal class TangemPayDetailsStateFactory(
                         dropdownItem = TangemDropdownMenuItem(
                             title = resourceReference(R.string.tangempay_card_details_change_pin),
                             textColor = themedColor { TangemTheme.colors.text.primary1 },
-                            onClick = onClickChangePin,
+                            onClick = intents::onClickChangePin,
+                        ),
+                    ),
+                    TangemPayDetailsTopBarMenuItem(
+                        type = TangemPayDetailsTopBarMenuItemType.TermsAndLimits,
+                        dropdownItem = TangemDropdownMenuItem(
+                            title = resourceReference(R.string.tangem_pay_terms_limits),
+                            textColor = themedColor { TangemTheme.colors.text.primary1 },
+                            onClick = intents::onClickTermsAndLimits,
                         ),
                     ),
                     cardFrozenStateItem,
@@ -59,18 +63,18 @@ internal class TangemPayDetailsStateFactory(
             ),
             pullToRefreshConfig = PullToRefreshConfig(
                 isRefreshing = false,
-                onRefresh = onRefresh,
+                onRefresh = intents::onRefreshSwipe,
             ),
             balanceBlockState = TangemPayDetailsBalanceBlockState.Loading(
                 actionButtons = persistentListOf(
                     ActionButtonConfig(
                         text = resourceReference(id = R.string.tangempay_card_details_add_funds),
                         iconResId = R.drawable.ic_arrow_down_24,
-                        onClick = onAddFunds,
+                        onClick = intents::onClickAddFunds,
                     ),
                 ),
                 frozenState = if (isCardFrozen) {
-                    CardFrozenState.Frozen(onClickUnfreezeCard)
+                    CardFrozenState.Frozen(intents::onClickUnfreezeCard)
                 } else {
                     CardFrozenState.Unfrozen
                 },
