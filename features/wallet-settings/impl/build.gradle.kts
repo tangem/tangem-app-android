ec3e6d1d82c8afa2c795b6bfc07adad40e3bc380
Tangem <git@tangem.com>
2025-11-14 18:49:07 +0500

    Updated on 2026-06-24

--- features/wallet-settings/impl/build.gradle.kts
+++ features/wallet-settings/impl/build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     implementation(projects.core.navigation)
     implementation(projects.core.analytics)
     implementation(projects.core.analytics.models)
+    implementation(projects.core.datasource)
     implementation(projects.common.routing)
     implementation(projects.common.ui)
 
