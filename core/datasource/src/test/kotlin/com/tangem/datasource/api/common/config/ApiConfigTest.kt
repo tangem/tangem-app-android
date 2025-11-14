64247b2ff5b592000a3d2a7b0ab615ecfadfe23a
Tangem <git@tangem.com>
2025-11-15 01:20:55 +0200

    Updated on 2025-11-18

--- core/datasource/src/test/kotlin/com/tangem/datasource/api/common/config/ApiConfigTest.kt
+++ core/datasource/src/test/kotlin/com/tangem/datasource/api/common/config/ApiConfigTest.kt
@@ -72,6 +72,7 @@ class ApiConfigTest {
                 ApiConfig.ID.TangemPay -> TangemPay(appVersionProvider = mockk())
                 ApiConfig.ID.BlockAid -> BlockAid(configStorage = mockk())
                 ApiConfig.ID.MoonPay -> MoonPay()
+                ApiConfig.ID.P2P -> P2P(p2pAuthProvider = mockk())
             }
         }
     }
