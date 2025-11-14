54f415cff2b37886f589951ed1319959b663ac37
Tangem <git@tangem.com>
2025-11-14 10:03:02 +0100

    Updated on 2025-11-18

---
+++ data/news/src/main/java/com/tangem/data/news/repository/DefaultNewsRepository.kt
@@ -0,0 +1,10 @@
+package com.tangem.data.news.repository
+
+import com.tangem.domain.news.repository.NewsRepository
+
+/**
+ * Default implementation of [NewsRepository]
+ *
+[REDACTED_AUTHOR]
+ */
+internal class DefaultNewsRepository : NewsRepository
