6741e840d08e48669020e18025cd1023f102af5e
Tangem <git@tangem.com>
2025-11-17 10:45:20 +0300

    Updated on 2026-08-14

--- data/wallets/src/main/java/com/tangem/data/wallets/DefaultWalletsRepository.kt
+++ data/wallets/src/main/java/com/tangem/data/wallets/DefaultWalletsRepository.kt
@@ -391,10 +391,16 @@ internal class DefaultWalletsRepository(
     override suspend fun associateWallets(applicationId: String, wallets: List<UserWallet>) =
         withContext(dispatchers.io) {
             val publicKeys = authProvider.getCardsPublicKeys()
-            val walletsBody = wallets.filterIsInstance<UserWallet.Cold>().map { userWallet ->
+            val walletsBody = wallets.map { userWallet ->
                 WalletIdBodyConverter.convert(
                     userWallet = userWallet,
-                    publicKeys = publicKeys.filterKeys { userWallet.cardsInWallet.contains(it) },
+                    publicKeys = if (userWallet is UserWallet.Cold) {
+                        publicKeys.filterKeys {
+                            userWallet.cardsInWallet.contains(it)
+                        }
+                    } else {
+                        emptyMap()
+                    },
                 )
             }
 
