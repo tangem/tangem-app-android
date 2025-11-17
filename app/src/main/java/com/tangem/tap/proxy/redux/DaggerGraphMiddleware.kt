404fbd853c423a1366b2299fe1b603e9c03c9a6d
Tangem <git@tangem.com>
2025-11-17 11:20:04 +0300

    Updated on 2025-12-18

--- app/src/main/java/com/tangem/tap/proxy/redux/DaggerGraphMiddleware.kt
+++ app/src/main/java/com/tangem/tap/proxy/redux/DaggerGraphMiddleware.kt
@@ -3,6 +3,7 @@ package com.tangem.tap.proxy.redux
 import com.tangem.tap.common.redux.AppState
 import org.rekotlin.Middleware
 
+@Suppress("MemberNameEqualsClassName")
 object DaggerGraphMiddleware {
     val daggerGraphMiddleware: Middleware<AppState> = { _, _ ->
         { next ->
