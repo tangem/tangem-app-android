ffce4a1fb66c33d6dee4c1efc3fa9fb364e3daa8
Tangem <git@tangem.com>
2025-11-19 11:04:47 +0400

    Updated on 2025-12-22

--- domain/manage-tokens/src/main/kotlin/com/tangem/domain/managetokens/RemoveCustomManagedCryptoCurrencyUseCase.kt
+++ domain/manage-tokens/src/main/kotlin/com/tangem/domain/managetokens/RemoveCustomManagedCryptoCurrencyUseCase.kt
@@ -5,7 +5,7 @@ import com.tangem.domain.managetokens.model.ManagedCryptoCurrency
 import com.tangem.domain.managetokens.repository.CustomTokensRepository
 import com.tangem.domain.models.wallet.UserWalletId
 
-@Deprecated("Use SaveCryptoCurrenciesUseCase")
+@Deprecated("Use ManageCryptoCurrenciesUseCase")
 class RemoveCustomManagedCryptoCurrencyUseCase(private val repository: CustomTokensRepository) {
 
     suspend operator fun invoke(
