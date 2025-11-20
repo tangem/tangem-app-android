cc4fa67509d189ca07367e5ea36e9a1132bdc34a
Tangem <git@tangem.com>
2025-11-20 14:05:32 +0500

    Updated on 2026-02-25

--- features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/components/cardDetails/TangemPayCardDetailsBlockComponent.kt
+++ features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/components/cardDetails/TangemPayCardDetailsBlockComponent.kt
@@ -1,11 +1,13 @@
 package com.tangem.features.tangempay.components.cardDetails
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.Modifier
 import com.tangem.features.tangempay.components.TangemPayDetailsContainerComponent
 import com.tangem.features.tangempay.entity.TangemPayCardDetailsUM
 import kotlinx.coroutines.flow.StateFlow
 
+@Stable
 internal interface TangemPayCardDetailsBlockComponent {
     val state: StateFlow<TangemPayCardDetailsUM>
 
