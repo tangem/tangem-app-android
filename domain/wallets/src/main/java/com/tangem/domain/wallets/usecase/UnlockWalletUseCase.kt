364c5f5df84d4a4ab9bb6613fc6c187878ececa9
Tangem <git@tangem.com>
2025-11-20 17:20:38 +0300

    Updated on 2026-05-14

--- domain/wallets/src/main/java/com/tangem/domain/wallets/usecase/UnlockWalletUseCase.kt
+++ domain/wallets/src/main/java/com/tangem/domain/wallets/usecase/UnlockWalletUseCase.kt
@@ -6,13 +6,26 @@ import com.tangem.domain.common.wallets.UserWalletsListRepository
 import com.tangem.domain.common.wallets.error.UnlockWalletError
 import com.tangem.domain.models.wallet.UserWalletId
 
+/**
+ * Use case for unlocking a wallet using biometric authentication.
+ * If biometric unlock fails, it falls back to non-biometric unlock methods.
+ *
+ * ** Does not** return [UnlockWalletError.AlreadyUnlocked] as an error, since the wallet is already unlocked.
+ *
+ * @see NonBiometricUnlockWalletUseCase
+ */
 class UnlockWalletUseCase(
     private val userWalletsListRepository: UserWalletsListRepository,
     private val nonBiometricUnlockWalletUseCase: NonBiometricUnlockWalletUseCase,
 ) {
 
     suspend operator fun invoke(userWalletId: UserWalletId): Either<UnlockWalletError, Unit> = either {
-        userWalletsListRepository.unlockAllWallets()
-            .mapLeft { nonBiometricUnlockWalletUseCase(userWalletId).bind() }
+        userWalletsListRepository.unlock(userWalletId, UserWalletsListRepository.UnlockMethod.Biometric)
+            .mapLeft { error ->
+                when (error) {
+                    UnlockWalletError.AlreadyUnlocked -> Unit
+                    else -> nonBiometricUnlockWalletUseCase(userWalletId).bind()
+                }
+            }
     }
 }
