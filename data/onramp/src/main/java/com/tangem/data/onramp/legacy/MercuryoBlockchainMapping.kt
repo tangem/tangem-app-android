e319ea728eba28086797a00ffb515eebc41db33e
Tangem <git@tangem.com>
2025-11-21 16:39:34 +0400

    Updated on 2026-04-06

--- data/onramp/src/main/java/com/tangem/data/onramp/legacy/MercuryoBlockchainMapping.kt
+++ data/onramp/src/main/java/com/tangem/data/onramp/legacy/MercuryoBlockchainMapping.kt
@@ -162,5 +162,6 @@ public val Blockchain.mercuryoNetwork: String?
             Blockchain.Quai, Blockchain.QuaiTestnet -> null
             Blockchain.Linea, Blockchain.LineaTestnet -> null
             Blockchain.ArbitrumNova -> null
+            Blockchain.Plasma, Blockchain.PlasmaTestnet -> null
         }
     }
