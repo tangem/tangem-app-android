da5b015098a6f7d1bf1346b79a2592d19305807e
Tangem <git@tangem.com>
2025-11-21 16:39:34 +0400

    Updated on 2026-08-14

--- domain/card/src/test/java/com/tangem/domain/card/configs/Wallet2CardConfigTest.kt
+++ domain/card/src/test/java/com/tangem/domain/card/configs/Wallet2CardConfigTest.kt
@@ -171,6 +171,8 @@ class Wallet2CardConfigTest {
         Blockchain.Linea to EllipticCurve.Secp256k1,
         Blockchain.LineaTestnet to EllipticCurve.Secp256k1,
         Blockchain.ArbitrumNova to EllipticCurve.Secp256k1,
+        Blockchain.Plasma to EllipticCurve.Secp256k1,
+        Blockchain.PlasmaTestnet to EllipticCurve.Secp256k1,
     )
 
     @Test
