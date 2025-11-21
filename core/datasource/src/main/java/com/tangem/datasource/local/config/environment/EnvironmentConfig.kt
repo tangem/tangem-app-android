4eb4dc699c558d6dc7c9ecf4b0b9650e10abfa4b
Tangem <git@tangem.com>
2025-11-21 11:38:00 +0200

    Updated on 2026-06-17

--- core/datasource/src/main/java/com/tangem/datasource/local/config/environment/EnvironmentConfig.kt
+++ core/datasource/src/main/java/com/tangem/datasource/local/config/environment/EnvironmentConfig.kt
@@ -2,6 +2,7 @@ package com.tangem.datasource.local.config.environment
 
 import com.tangem.blockchain.common.BlockchainSdkConfig
 import com.tangem.datasource.local.config.environment.models.ExpressModel
+import com.tangem.datasource.local.config.environment.models.P2PKeys
 
 data class EnvironmentConfig(
     val moonPayApiKey: String = "",
@@ -16,7 +17,7 @@ data class EnvironmentConfig(
     val express: ExpressModel? = null,
     val devExpress: ExpressModel? = null,
     val stakeKitApiKey: String? = null,
-    // val p2pApiKey: P2PKeys? = null, TODO p2p after release 5.30
+    val p2pApiKey: P2PKeys? = null,
     val blockAidApiKey: String? = null,
     val tangemApiKey: String? = null,
     val tangemApiKeyDev: String? = null,
