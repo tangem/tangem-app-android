c26657678f846baf36b16c206db355104634e823
Tangem <git@tangem.com>
2025-11-22 15:33:50 +0500

    Updated on 2025-12-22

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/subscribers/WalletDropDownItemsSubscriber.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/subscribers/WalletDropDownItemsSubscriber.kt
@@ -4,26 +4,42 @@ import com.tangem.domain.wallets.usecase.ShouldSaveUserWalletsUseCase
 import com.tangem.feature.wallet.child.wallet.model.intents.WalletClickIntents
 import com.tangem.feature.wallet.presentation.wallet.state.WalletStateController
 import com.tangem.feature.wallet.presentation.wallet.state.transformers.SetWalletCardDropDownItemsTransformer
+import com.tangem.features.hotwallet.HotWalletFeatureToggles
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.onEach
 
 internal class WalletDropDownItemsSubscriber(
     private val stateHolder: WalletStateController,
     private val shouldSaveUserWalletsUseCase: ShouldSaveUserWalletsUseCase,
     private val clickIntents: WalletClickIntents,
+    private val hotWalletFeatureToggles: HotWalletFeatureToggles,
 ) : WalletSubscriber() {
     override fun create(coroutineScope: CoroutineScope): Flow<*> {
-        return shouldSaveUserWalletsUseCase.invoke()
-            .distinctUntilChanged()
-            .onEach {
+        if (!hotWalletFeatureToggles.isHotWalletEnabled) {
+            return shouldSaveUserWalletsUseCase.invoke()
+                .distinctUntilChanged()
+                .onEach { shouldSaveUserWallets ->
+                    stateHolder.update(
+                        SetWalletCardDropDownItemsTransformer(
+                            dropdownEnabled = shouldSaveUserWallets,
+                            clickIntents = clickIntents,
+                            hotWalletFeatureToggles = hotWalletFeatureToggles,
+                        ),
+                    )
+                }
+        } else {
+            return flow<Unit> {
                 stateHolder.update(
                     SetWalletCardDropDownItemsTransformer(
-                        dropdownEnabled = it,
+                        dropdownEnabled = true,
                         clickIntents = clickIntents,
+                        hotWalletFeatureToggles = hotWalletFeatureToggles,
                     ),
                 )
             }
+        }
     }
 }
