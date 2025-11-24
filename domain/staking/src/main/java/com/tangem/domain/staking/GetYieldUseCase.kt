393f379f99fff17d8ff9750d86be7becdac0fdca
Tangem <git@tangem.com>
2025-11-24 13:48:52 +0200

    Updated on 2026-09-21

--- domain/staking/src/main/java/com/tangem/domain/staking/GetYieldUseCase.kt
+++ domain/staking/src/main/java/com/tangem/domain/staking/GetYieldUseCase.kt
@@ -5,25 +5,25 @@ import com.tangem.domain.models.currency.CryptoCurrency
 import com.tangem.domain.staking.model.stakekit.StakingError
 import com.tangem.domain.staking.model.stakekit.Yield
 import com.tangem.domain.staking.repositories.StakingErrorResolver
-import com.tangem.domain.staking.repositories.StakingRepository
+import com.tangem.domain.staking.repositories.StakeKitRepository
 
 /**
  * Use case for getting staking yield for staking scenario start.
  */
 class GetYieldUseCase(
-    private val stakingRepository: StakingRepository,
+    private val stakeKitRepository: StakeKitRepository,
     private val stakingErrorResolver: StakingErrorResolver,
 ) {
 
     suspend operator fun invoke(cryptoCurrencyId: CryptoCurrency.ID, symbol: String): Either<StakingError, Yield> {
         return Either
-            .catch { stakingRepository.getYield(cryptoCurrencyId, symbol) }
+            .catch { stakeKitRepository.getYield(cryptoCurrencyId, symbol) }
             .mapLeft { stakingErrorResolver.resolve(it) }
     }
 
     suspend operator fun invoke(yieldId: String): Either<StakingError, Yield> {
         return Either
-            .catch { stakingRepository.getYield(yieldId) }
+            .catch { stakeKitRepository.getYield(yieldId) }
             .mapLeft { stakingErrorResolver.resolve(it) }
     }
 }
