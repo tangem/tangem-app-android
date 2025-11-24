d3a06328febfdb616f60cda4dc75c97e7ed72dc8
Tangem <git@tangem.com>
2025-11-24 21:29:11 +0400

    Updated on 2026-09-21

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/child/wallet/model/intents/TangemPayClickIntents.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/child/wallet/model/intents/TangemPayClickIntents.kt
@@ -3,15 +3,11 @@ package com.tangem.feature.wallet.child.wallet.model.intents
 import com.tangem.core.decompose.di.ModelScoped
 import com.tangem.domain.pay.repository.OnboardingRepository
 import com.tangem.domain.pay.repository.TangemPayCardDetailsRepository
-import com.tangem.domain.pay.usecase.TangemPayIssueOrderUseCase
 import com.tangem.domain.pay.usecase.TangemPayMainScreenCustomerInfoUseCase
 import com.tangem.domain.visa.model.TangemPayCardFrozenState
 import com.tangem.feature.wallet.presentation.wallet.state.WalletStateController
 import com.tangem.feature.wallet.presentation.wallet.state.transformers.TangemPayInitialStateTransformer
-import com.tangem.feature.wallet.presentation.wallet.state.transformers.TangemPayIssueAvailableStateTransformer
-import com.tangem.feature.wallet.presentation.wallet.state.transformers.TangemPayIssueProgressStateTransformer
 import com.tangem.features.tangempay.TangemPayFeatureToggles
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 internal interface TangemPayIntents {
@@ -23,7 +19,6 @@ internal interface TangemPayIntents {
 internal class TangemPayClickIntentsImplementor @Inject constructor(
     private val stateHolder: WalletStateController,
     private val mainScreenCustomerInfoUseCase: TangemPayMainScreenCustomerInfoUseCase,
-    private val issueOrderUseCase: TangemPayIssueOrderUseCase,
     private val featureToggles: TangemPayFeatureToggles,
     private val onboardingRepository: OnboardingRepository,
     private val cardDetailsRepository: TangemPayCardDetailsRepository,
@@ -40,21 +35,11 @@ internal class TangemPayClickIntentsImplementor @Inject constructor(
             stateHolder.update(
                 transformer = TangemPayInitialStateTransformer(
                     value = info,
-                    onClickIssue = ::issueOrder,
                     onClickKyc = router::openTangemPayOnboarding,
                     openDetails = { config -> router.openTangemPayDetails(userWalletId, config) },
                     cardFrozenState = cardFrozenState,
                 ),
             )
-        }
-    }
-
-    private fun issueOrder() {
-        modelScope.launch {
-            stateHolder.update(TangemPayIssueProgressStateTransformer())
-            issueOrderUseCase().onLeft {
-                stateHolder.update(TangemPayIssueAvailableStateTransformer(onClickIssue = ::issueOrder))
-            }
         }
     }
 }
