9872f716f93fa1d33b51406125c99ec27aec8436
Tangem <git@tangem.com>
2025-11-24 13:48:52 +0200

    Updated on 2026-08-27

--- domain/staking/src/main/java/com/tangem/domain/staking/repositories/StakingRepository.kt
+++ domain/staking/src/main/java/com/tangem/domain/staking/repositories/StakingRepository.kt
@@ -1,35 +1,11 @@
 package com.tangem.domain.staking.repositories
 
-import com.tangem.blockchain.common.Amount
-import com.tangem.blockchain.common.TransactionData
-import com.tangem.blockchain.common.transaction.Fee
 import com.tangem.domain.models.currency.CryptoCurrency
-import com.tangem.domain.models.network.Network
 import com.tangem.domain.models.wallet.UserWalletId
 import com.tangem.domain.staking.model.StakingAvailability
-import com.tangem.domain.staking.model.StakingEntryInfo
-import com.tangem.domain.models.staking.NetworkType
-import com.tangem.domain.staking.model.stakekit.Yield
-import com.tangem.domain.staking.model.stakekit.action.StakingAction
-import com.tangem.domain.staking.model.stakekit.action.StakingActionStatus
-import com.tangem.domain.staking.model.stakekit.transaction.ActionParams
-import com.tangem.domain.staking.model.stakekit.transaction.StakingGasEstimate
-import com.tangem.domain.staking.model.stakekit.transaction.StakingTransaction
 import kotlinx.coroutines.flow.Flow
 
-// TODO p2p make 3 repos: stakekit, p2p, common staking
-@Suppress("TooManyFunctions")
 interface StakingRepository {
-
-    suspend fun fetchYields()
-
-    fun getEnabledYields(): Flow<List<Yield>>
-
-    suspend fun getEntryInfo(cryptoCurrencyId: CryptoCurrency.ID, symbol: String): StakingEntryInfo
-
-    suspend fun getYield(cryptoCurrencyId: CryptoCurrency.ID, symbol: String): Yield
-
-    suspend fun getYield(yieldId: String): Yield
 
     fun getStakingAvailability(userWalletId: UserWalletId, cryptoCurrency: CryptoCurrency): Flow<StakingAvailability>
 
@@ -37,24 +13,6 @@ interface StakingRepository {
         userWalletId: UserWalletId,
         cryptoCurrency: CryptoCurrency,
     ): StakingAvailability
-
-    suspend fun getActions(
-        userWalletId: UserWalletId,
-        cryptoCurrency: CryptoCurrency,
-        networkType: NetworkType,
-        stakingActionStatus: StakingActionStatus,
-    ): List<StakingAction>
-
-    suspend fun createAction(userWalletId: UserWalletId, network: Network, params: ActionParams): StakingAction
-
-    suspend fun estimateGas(userWalletId: UserWalletId, network: Network, params: ActionParams): StakingGasEstimate
-
-    suspend fun constructTransaction(
-        networkId: String,
-        fee: Fee,
-        amount: Amount,
-        transactionId: String,
-    ): Pair<StakingTransaction, TransactionData.Compiled>
 
     suspend fun isAnyTokenStaked(userWalletId: UserWalletId): Boolean
 }
