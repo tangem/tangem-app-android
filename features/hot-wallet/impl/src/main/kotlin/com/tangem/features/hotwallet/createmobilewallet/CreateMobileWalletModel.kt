5886374b872c83b523a6b67e3d7255134f8c134c
Tangem <git@tangem.com>
2025-11-24 17:01:39 +0400

    Updated on 2026-07-23

--- features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/createmobilewallet/CreateMobileWalletModel.kt
+++ features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/createmobilewallet/CreateMobileWalletModel.kt
@@ -7,11 +7,14 @@ import com.tangem.core.decompose.model.Model
 import com.tangem.core.decompose.navigation.Router
 import com.tangem.domain.wallets.builder.HotUserWalletBuilder
 import com.tangem.domain.wallets.usecase.SaveWalletUseCase
+import com.tangem.domain.wallets.usecase.SyncWalletWithRemoteUseCase
 import com.tangem.features.hotwallet.createmobilewallet.entity.CreateMobileWalletUM
 import com.tangem.hot.sdk.TangemHotSdk
 import com.tangem.hot.sdk.model.HotAuth
 import com.tangem.hot.sdk.model.MnemonicType
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
+import com.tangem.utils.coroutines.runSuspendCatching
+import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
@@ -19,11 +22,13 @@ import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
 
+@Suppress("LongParameterList")
 @ModelScoped
 internal class CreateMobileWalletModel @Inject constructor(
     override val dispatchers: CoroutineDispatcherProvider,
     private val hotUserWalletBuilderFactory: HotUserWalletBuilder.Factory,
     private val saveUserWalletUseCase: SaveWalletUseCase,
+    private val syncWalletWithRemoteUseCase: SyncWalletWithRemoteUseCase,
     private val router: Router,
     private val tangemHotSdk: TangemHotSdk,
     private val trackingContextProxy: TrackingContextProxy,
@@ -58,18 +63,21 @@ internal class CreateMobileWalletModel @Inject constructor(
                 it.copy(createButtonLoading = true)
             }
 
-            runCatching {
+            runSuspendCatching {
                 val hotWalletId = tangemHotSdk.generateWallet(HotAuth.NoAuth, mnemonicType = MnemonicType.Words12)
                 val hotUserWalletBuilder = hotUserWalletBuilderFactory.create(hotWalletId)
                 val userWallet = hotUserWalletBuilder.build()
-                saveUserWalletUseCase(userWallet)
-                router.replaceAll(AppRoute.Wallet)
-            }.onFailure {
-                Timber.e(it)
 
-                uiState.update {
-                    it.copy(createButtonLoading = false)
+                saveUserWalletUseCase(userWallet)
+
+                launch(NonCancellable) {
+                    syncWalletWithRemoteUseCase(userWalletId = userWallet.walletId)
                 }
+                router.replaceAll(AppRoute.Wallet)
+            }.onFailure { throwable ->
+                Timber.e(throwable)
+
+                uiState.update { it.copy(createButtonLoading = false) }
             }
         }
     }
