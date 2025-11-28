fa176ac1f9ea1c883f85f83729d8b8071c89d577
Tangem <git@tangem.com>
2025-11-28 14:12:42 +0500

    Updated on 2026-04-08

--- core/datasource/src/main/java/com/tangem/datasource/local/visa/TangemPayStorage.kt
+++ core/datasource/src/main/java/com/tangem/datasource/local/visa/TangemPayStorage.kt
@@ -1,16 +1,16 @@
 package com.tangem.datasource.local.visa
 
 import com.tangem.domain.models.wallet.UserWalletId
-import com.tangem.domain.visa.model.VisaAuthTokens
+import com.tangem.domain.visa.model.TangemPayAuthTokens
 
 interface TangemPayStorage {
 
     suspend fun storeCustomerWalletAddress(userWalletId: UserWalletId, customerWalletAddress: String)
     suspend fun getCustomerWalletAddress(userWalletId: UserWalletId): String?
 
-    suspend fun storeAuthTokens(customerWalletAddress: String, tokens: VisaAuthTokens)
+    suspend fun storeAuthTokens(customerWalletAddress: String, tokens: TangemPayAuthTokens)
 
-    suspend fun getAuthTokens(customerWalletAddress: String): VisaAuthTokens?
+    suspend fun getAuthTokens(customerWalletAddress: String): TangemPayAuthTokens?
 
     suspend fun storeOrderId(customerWalletAddress: String, orderId: String)
 
@@ -21,6 +21,8 @@ interface TangemPayStorage {
     suspend fun getAddToWalletDone(customerWalletAddress: String): Boolean
 
     suspend fun storeAddToWalletDone(customerWalletAddress: String, isDone: Boolean)
+    suspend fun storeCheckCustomerWalletResult(userWalletId: UserWalletId)
+    suspend fun checkCustomerWalletResult(userWalletId: UserWalletId): Boolean?
 
     suspend fun clearAll(userWalletId: UserWalletId, customerWalletAddress: String)
 }
