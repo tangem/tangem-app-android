d9455004ff129f87ce7992eef94c479399732eac
Tangem <git@tangem.com>
2025-11-28 18:13:46 +0400

    Updated on 2026-05-07

--- domain/visa/src/main/kotlin/com/tangem/domain/pay/datasource/TangemPayAuthDataSource.kt
+++ domain/visa/src/main/kotlin/com/tangem/domain/pay/datasource/TangemPayAuthDataSource.kt
@@ -9,4 +9,6 @@ interface TangemPayAuthDataSource {
     suspend fun produceInitialCredentials(cardId: String): Either<Throwable, TangemPayInitialCredentials>
 
     suspend fun refreshAuthTokens(refreshToken: String): Either<Throwable, TangemPayAuthTokens>
+
+    suspend fun getWithdrawalSignature(cardId: String, hash: String): Either<Throwable, String>
 }
