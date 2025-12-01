e142a304f84723c6e9eff713cc36d897ffb314cd
Tangem <git@tangem.com>
2025-12-01 16:29:40 +0300

    Updated on 2026-07-07

--- features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/manualbackup/start/ui/ManualBackupStartContent.kt
+++ features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/manualbackup/start/ui/ManualBackupStartContent.kt
@@ -3,9 +3,12 @@ package com.tangem.features.hotwallet.manualbackup.start.ui
 import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,8 +25,8 @@ import com.tangem.features.hotwallet.manualbackup.start.entity.ManualBackupStart
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun ManualBackupStartContent(state: ManualBackupStartUM, modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier
+    Box(
+        modifier
             .background(TangemTheme.colors.background.primary)
             .fillMaxSize()
             .padding(
@@ -33,53 +36,57 @@ internal fun ManualBackupStartContent(state: ManualBackupStartUM, modifier: Modi
                 bottom = 16.dp,
             ),
     ) {
-        Text(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(
-                    horizontal = 16.dp,
-                    vertical = 8.dp,
+        Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        horizontal = 16.dp,
+                        vertical = 8.dp,
+                    ),
+                text = stringResourceSafe(R.string.backup_info_title),
+                style = TangemTheme.typography.h2,
+                color = TangemTheme.colors.text.primary1,
+                textAlign = TextAlign.Center,
+            )
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        horizontal = 16.dp,
+                        vertical = 8.dp,
+                    ),
+                text = stringResourceSafe(
+                    R.string.backup_info_description,
+                    state.seepPhraseLength.toString(),
                 ),
-            text = stringResourceSafe(R.string.backup_info_title),
-            style = TangemTheme.typography.h2,
-            color = TangemTheme.colors.text.primary1,
-            textAlign = TextAlign.Center,
-        )
-        Text(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(
-                    horizontal = 16.dp,
-                    vertical = 8.dp,
+                style = TangemTheme.typography.body1,
+                color = TangemTheme.colors.text.secondary,
+                textAlign = TextAlign.Center,
+            )
+            FeatureBlock(
+                modifier = Modifier
+                    .padding(top = 24.dp),
+                title = stringResourceSafe(R.string.backup_info_save_title),
+                description = stringResourceSafe(
+                    R.string.backup_info_save_description,
+                    state.seepPhraseLength.toString(),
                 ),
-            text = stringResourceSafe(
-                R.string.backup_info_description,
-                state.seepPhraseLength.toString(),
-            ),
-            style = TangemTheme.typography.body1,
-            color = TangemTheme.colors.text.secondary,
-            textAlign = TextAlign.Center,
-        )
-        FeatureBlock(
-            modifier = Modifier
-                .padding(top = 24.dp),
-            title = stringResourceSafe(R.string.backup_info_save_title),
-            description = stringResourceSafe(
-                R.string.backup_info_save_description,
-                state.seepPhraseLength.toString(),
-            ),
-            iconRes = R.drawable.ic_lock_24,
-        )
-        FeatureBlock(
-            modifier = Modifier
-                .padding(top = 24.dp),
-            title = stringResourceSafe(R.string.backup_info_keep_title),
-            description = stringResourceSafe(R.string.backup_info_keep_description),
-            iconRes = R.drawable.ic_settings_24,
-        )
-        Spacer(modifier = Modifier.weight(1f))
+                iconRes = R.drawable.ic_lock_24,
+            )
+            FeatureBlock(
+                modifier = Modifier
+                    .padding(top = 24.dp),
+                title = stringResourceSafe(R.string.backup_info_keep_title),
+                description = stringResourceSafe(R.string.backup_info_keep_description),
+                iconRes = R.drawable.ic_settings_24,
+            )
+            Spacer(modifier = Modifier.weight(1f))
+        }
+
         PrimaryButton(
             modifier = Modifier
+                .align(Alignment.BottomCenter)
                 .fillMaxWidth()
                 .padding(top = 16.dp),
             text = stringResourceSafe(R.string.common_continue),
