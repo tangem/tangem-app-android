9c3df1ccb07cec4b0a3dc7dc8399626c7fd9e22f
Tangem <git@tangem.com>
2025-12-04 16:42:58 +0700

    Updated on 2026-08-07

--- data/wallet-connect/src/main/kotlin/com/tangem/data/walletconnect/di/WalletConnectDataModule.kt
+++ data/wallet-connect/src/main/kotlin/com/tangem/data/walletconnect/di/WalletConnectDataModule.kt
@@ -124,7 +124,10 @@ internal object WalletConnectDataModule {
 
     @Provides
     @Singleton
-    fun defaultWcRequestService(diHelperBox: DiHelperBox, respondService: WcRespondService): DefaultWcRequestService {
+    fun defaultWcRequestService(
+        diHelperBox: DiHelperBox,
+        respondService: DefaultWcRespondService,
+    ): DefaultWcRequestService {
         return DefaultWcRequestService(
             requestConverters = diHelperBox.handlers,
             respondService = respondService,
@@ -133,7 +136,11 @@ internal object WalletConnectDataModule {
 
     @Provides
     @Singleton
-    fun wcRespondService(): WcRespondService = DefaultWcRespondService()
+    fun wcDefaultWcRespondService(): DefaultWcRespondService = DefaultWcRespondService()
+
+    @Provides
+    @Singleton
+    fun wcRespondService(default: DefaultWcRespondService): WcRespondService = default
 
     @Provides
     @Singleton
