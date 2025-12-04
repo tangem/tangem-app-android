b529b84636d3a4142bc46c843a2be9b2527a7bda
Tangem <git@tangem.com>
2025-12-04 22:06:09 +0500

    Updated on 2026-07-23

--- data/visa/src/main/kotlin/com/tangem/data/pay/datasource/DefaultTangemPayAuthDataSource.kt
+++ data/visa/src/main/kotlin/com/tangem/data/pay/datasource/DefaultTangemPayAuthDataSource.kt
@@ -2,33 +2,24 @@ package com.tangem.data.pay.datasource
 
 import arrow.core.Either
 import arrow.core.left
-import arrow.core.raise.either
+import arrow.core.right
 import com.tangem.common.CompletionResult
 import com.tangem.common.core.TangemSdkError
 import com.tangem.domain.pay.datasource.TangemPayAuthDataSource
 import com.tangem.domain.pay.model.WithdrawalSignatureResult
-import com.tangem.domain.visa.datasource.VisaAuthRemoteDataSource
-import com.tangem.domain.visa.model.TangemPayAuthTokens
 import com.tangem.domain.visa.model.TangemPayInitialCredentials
 import com.tangem.sdk.api.TangemSdkManager
 import javax.inject.Inject
 
 internal class DefaultTangemPayAuthDataSource @Inject constructor(
-    private val visaAuthRemoteDataSource: VisaAuthRemoteDataSource,
     private val tangemSdkManager: TangemSdkManager,
 ) : TangemPayAuthDataSource {
 
     override suspend fun produceInitialCredentials(cardId: String): Either<Throwable, TangemPayInitialCredentials> {
         return when (val initialCredentials = tangemSdkManager.tangemPayProduceInitialCredentials(cardId = cardId)) {
-            is CompletionResult.Failure<*> -> Either.Left(initialCredentials.error)
-            is CompletionResult.Success<TangemPayInitialCredentials> -> Either.Right(initialCredentials.data)
+            is CompletionResult.Failure<*> -> initialCredentials.error.left()
+            is CompletionResult.Success<TangemPayInitialCredentials> -> initialCredentials.data.right()
         }
-    }
-
-    override suspend fun refreshAuthTokens(refreshToken: String): Either<Throwable, TangemPayAuthTokens> = either {
-        visaAuthRemoteDataSource.refreshCustomerWalletAuthTokens(refreshToken = refreshToken)
-            .mapLeft { IllegalStateException("TangemPay token refresh failed. Error code: ${it.errorCode}") }
-            .bind()
     }
 
     override suspend fun getWithdrawalSignature(
@@ -38,13 +29,13 @@ internal class DefaultTangemPayAuthDataSource @Inject constructor(
         return when (val signResult = tangemSdkManager.getWithdrawalSignature(cardId, hash)) {
             is CompletionResult.Failure<*> -> {
                 if (signResult.error is TangemSdkError.UserCancelled) {
-                    Either.Right(WithdrawalSignatureResult.Cancelled)
+                    WithdrawalSignatureResult.Cancelled.right()
                 } else {
                     signResult.error.left()
                 }
             }
             is CompletionResult.Success<String> -> {
-                Either.Right(WithdrawalSignatureResult.Success(signResult.data))
+                WithdrawalSignatureResult.Success(signResult.data).right()
             }
         }
     }
