eef94027011cd5dc40fa00f1e1dcab6389701da2
Tangem <git@tangem.com>
2025-12-08 10:46:19 +0200

    Updated on 2026-09-21

--- domain/models/src/main/kotlin/com/tangem/domain/models/staking/StakingID.kt
+++ domain/models/src/main/kotlin/com/tangem/domain/models/staking/StakingID.kt
@@ -2,11 +2,6 @@ package com.tangem.domain.models.staking
 
 import kotlinx.serialization.Serializable
 
-enum class StakingProvider {
-    STAKEKIT,
-    P2P,
-}
-
 @Serializable
 data class StakingID(
     val integrationId: String,
