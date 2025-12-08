f3504444f7ba3d7fc3e3d880cf4ef5377c92eaa7
Tangem <git@tangem.com>
2025-12-08 22:25:17 +0500

    Updated on 2026-09-21

--- features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/model/TangemPayTxHistoryDetailsModel.kt
+++ features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/model/TangemPayTxHistoryDetailsModel.kt
@@ -5,6 +5,7 @@ import com.tangem.core.decompose.di.ModelScoped
 import com.tangem.core.decompose.model.Model
 import com.tangem.core.decompose.model.ParamsContainer
 import com.tangem.core.navigation.url.UrlOpener
+import com.tangem.domain.balancehiding.GetBalanceHidingSettingsUseCase
 import com.tangem.domain.feedback.GetWalletMetaInfoUseCase
 import com.tangem.domain.feedback.SendFeedbackEmailUseCase
 import com.tangem.domain.feedback.models.FeedbackEmailType
@@ -14,39 +15,56 @@ import com.tangem.features.tangempay.components.txHistory.TangemPayTxHistoryDeta
 import com.tangem.features.tangempay.entity.TangemPayTxHistoryDetailsUM
 import com.tangem.features.tangempay.model.transformers.TangemPayTxHistoryDetailsConverter
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @Stable
 @ModelScoped
+@Suppress("LongParameterList")
 internal class TangemPayTxHistoryDetailsModel @Inject constructor(
     override val dispatchers: CoroutineDispatcherProvider,
     private val getUserWalletsUseCase: GetWalletsUseCase,
     private val getWalletMetaInfoUseCase: GetWalletMetaInfoUseCase,
     private val sendFeedbackEmailUseCase: SendFeedbackEmailUseCase,
     private val urlOpener: UrlOpener,
+    private val balanceHidingSettings: GetBalanceHidingSettingsUseCase,
     paramsContainer: ParamsContainer,
 ) : Model() {
 
     private val params = paramsContainer.require<TangemPayTxHistoryDetailsComponent.Params>()
-    val uiState: TangemPayTxHistoryDetailsUM = TangemPayTxHistoryDetailsConverter.convert(
-        TangemPayTxHistoryDetailsConverter.Input(
-            item = params.transaction,
-            onExplorerClick = ::openExplorer,
-            onDisputeClick = ::dispute,
-            onDismiss = ::dismiss,
-        ),
-    )
+    val uiState: StateFlow<TangemPayTxHistoryDetailsUM>
+        field = MutableStateFlow(
+            value = TangemPayTxHistoryDetailsConverter.convert(
+                value = TangemPayTxHistoryDetailsConverter.Input(
+                    item = params.transaction,
+                    isBalanceHidden = params.isBalanceHidden,
+                    onExplorerClick = ::openExplorer,
+                    onDisputeClick = ::dispute,
+                    onDismiss = ::dismiss,
+                ),
+            ),
+        )
+
+    init {
+        subscribeToBalanceHiding()
+    }
 
     fun dismiss() {
         params.onDismiss()
     }
 
-    fun openExplorer(txHash: String?) {
+    private fun subscribeToBalanceHiding() {
+        balanceHidingSettings.isBalanceHidden()
+            .onEach { isBalanceHidden -> uiState.update { it.copy(isBalanceHidden = isBalanceHidden) } }
+            .launchIn(modelScope)
+    }
+
+    private fun openExplorer(txHash: String?) {
         txHash?.let(urlOpener::openUrlExternalBrowser)
     }
 
-    fun dispute() {
+    private fun dispute() {
         modelScope.launch {
             val userWalletId = params.userWalletId
             val userWallet = getUserWalletsUseCase.invokeSync()
