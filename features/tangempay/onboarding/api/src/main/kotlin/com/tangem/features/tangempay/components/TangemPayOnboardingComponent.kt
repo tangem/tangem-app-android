1852606c603e05f1280b806d9828356e02273ba2
Tangem <git@tangem.com>
2025-12-08 19:26:33 +0300

    Updated on 2026-01-24

--- features/tangempay/onboarding/api/src/main/kotlin/com/tangem/features/tangempay/components/TangemPayOnboardingComponent.kt
+++ features/tangempay/onboarding/api/src/main/kotlin/com/tangem/features/tangempay/components/TangemPayOnboardingComponent.kt
@@ -2,15 +2,22 @@ package com.tangem.features.tangempay.components
 
 import com.tangem.core.decompose.factory.ComponentFactory
 import com.tangem.core.ui.decompose.ComposableContentComponent
+import com.tangem.domain.models.wallet.UserWalletId
 
 interface TangemPayOnboardingComponent : ComposableContentComponent {
 
     sealed class Params {
+
+        abstract val userWalletId: UserWalletId?
+
         data class Deeplink(
             val deeplink: String,
+            override val userWalletId: UserWalletId?,
         ) : Params()
 
-        object ContinueOnboarding : Params()
+        data class ContinueOnboarding(
+            override val userWalletId: UserWalletId?,
+        ) : Params()
     }
 
     interface Factory : ComponentFactory<Params, TangemPayOnboardingComponent>
