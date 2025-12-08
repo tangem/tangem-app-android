dd558f63f64f90ba562e9c29f1ad7a4238798e6d
Tangem <git@tangem.com>
2025-12-08 19:20:12 +0300

    Updated on 2026-07-01

--- app/src/main/java/com/tangem/tap/di/domain/HotWalletDomainModule.kt
+++ app/src/main/java/com/tangem/tap/di/domain/HotWalletDomainModule.kt
@@ -2,6 +2,7 @@ package com.tangem.tap.di.domain
 
 import com.tangem.domain.hotwallet.GetAccessCodeSkippedUseCase
 import com.tangem.domain.hotwallet.IsHotWalletCreationSupported
+import com.tangem.domain.hotwallet.IsAccessCodeSimpleUseCase
 import com.tangem.domain.hotwallet.SetAccessCodeSkippedUseCase
 import com.tangem.domain.hotwallet.repository.HotWalletRepository
 import dagger.Module
@@ -24,6 +25,12 @@ internal object HotWalletDomainModule {
     @Singleton
     fun provideSetAccessCodeSkippedUseCase(hotWalletRepository: HotWalletRepository): SetAccessCodeSkippedUseCase {
         return SetAccessCodeSkippedUseCase(hotWalletRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideIsAccessCodeSimpleUseCase(): IsAccessCodeSimpleUseCase {
+        return IsAccessCodeSimpleUseCase()
     }
 
     @Provides
