f96efb23bc190ea91c30883526097f7f99d335c6
Tangem <git@tangem.com>
2025-12-09 18:17:49 +0300

    Updated on 2026-07-01

--- app/src/main/java/com/tangem/tap/common/analytics/handlers/appsflyer/AppsFlyerAnalyticsClient.kt
+++ app/src/main/java/com/tangem/tap/common/analytics/handlers/appsflyer/AppsFlyerAnalyticsClient.kt
@@ -2,9 +2,11 @@ package com.tangem.tap.common.analytics.handlers.appsflyer
 
 import android.content.Context
 import com.appsflyer.AppsFlyerLib
+import com.tangem.core.analytics.api.AnalyticsUserIdHandler
 import com.tangem.core.analytics.api.EventLogger
+import com.tangem.core.analytics.api.UserIdHolder
 
-interface AppsFlyerAnalyticsClient : EventLogger
+interface AppsFlyerAnalyticsClient : EventLogger, UserIdHolder
 
 internal class AppsFlyerClient(
     private val context: Context,
@@ -18,6 +20,14 @@ internal class AppsFlyerClient(
         appsFlyerLib.init(key, null, context)
         appsFlyerLib.setAppId(appId)
         appsFlyerLib.start(context)
+    }
+
+    override fun setUserId(userId: String) {
+        appsFlyerLib.setCustomerUserId(userId)
+    }
+
+    override fun clearUserId() {
+        appsFlyerLib.setCustomerUserId(null)
     }
 
     override fun logEvent(event: String, params: Map<String, String>) {
