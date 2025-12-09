f708fb50d8e54ad4c6ebe56fe8e8acb6a92927e5
Tangem <git@tangem.com>
2025-12-09 17:38:17 +0500

    Updated on 2026-07-07

---
+++ features/tester/impl/src/main/java/com/tangem/feature/tester/presentation/news/viewmodel/NewsViewModel.kt
@@ -0,0 +1,44 @@
+package com.tangem.feature.tester.presentation.news.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.tangem.feature.tester.presentation.navigation.InnerTesterRouter
+import com.tangem.feature.tester.presentation.navigation.TesterScreen
+import com.tangem.feature.tester.presentation.news.state.NewsUM
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.collections.immutable.persistentSetOf
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+internal class NewsViewModel @Inject constructor() : ViewModel() {
+
+    private val _uiState = MutableStateFlow(createInitialState())
+    val uiState: StateFlow<NewsUM> = _uiState
+
+    private var router: InnerTesterRouter? = null
+
+    fun setupNavigation(router: InnerTesterRouter) {
+        this.router = router
+    }
+
+    private fun createInitialState(): NewsUM {
+        return NewsUM(
+            onBackClick = ::onBackClick,
+            buttons = persistentSetOf(
+                NewsUM.ButtonUM.NEWS_DETAILS,
+            ),
+            onButtonClick = ::onButtonClick,
+        )
+    }
+
+    private fun onBackClick() {
+        router?.back()
+    }
+
+    private fun onButtonClick(button: NewsUM.ButtonUM) {
+        when (button) {
+            NewsUM.ButtonUM.NEWS_DETAILS -> router?.open(TesterScreen.NEWS_DETAILS)
+        }
+    }
+}
