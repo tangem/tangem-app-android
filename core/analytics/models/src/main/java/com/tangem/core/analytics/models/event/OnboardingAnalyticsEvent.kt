99801fe5c0eaa971a2e45e253da309da63c434da
Tangem <git@tangem.com>
2025-12-09 17:22:28 +0500

    Updated on 2026-09-14

--- core/analytics/models/src/main/java/com/tangem/core/analytics/models/event/OnboardingAnalyticsEvent.kt
+++ core/analytics/models/src/main/java/com/tangem/core/analytics/models/event/OnboardingAnalyticsEvent.kt
@@ -12,11 +12,85 @@ sealed class OnboardingAnalyticsEvent(
     sealed class Onboarding(
         event: String,
         params: Map<String, String> = mapOf(),
+    ) : OnboardingAnalyticsEvent(category = "Onboarding", event = event, params = params) {
+
+        class Started(
+            source: String,
+        ) : Onboarding(
+            event = "Onboarding Started",
+            params = mapOf(
+                AnalyticsParam.SOURCE to source,
+            ),
+        )
+
+        class Finished(
+            source: String,
+        ) : Onboarding(
+            event = "Onboarding Finished",
+            params = mapOf(
+                AnalyticsParam.SOURCE to source,
+            ),
+        )
+
+        class ButtonMobileWallet(
+            source: String,
+        ) : Onboarding(
+            event = "Button - Mobile Wallet",
+            params = mapOf(
+                AnalyticsParam.SOURCE to source,
+            ),
+        )
+    }
+
+    sealed class CreateWallet(
+        event: String,
+        params: Map<String, String> = mapOf(),
+    ) : OnboardingAnalyticsEvent(category = "Onboarding / Create Wallet", event = event, params = params) {
+
+        data object ButtonCreateWallet : CreateWallet("Button - Create Wallet")
+
+        class WalletCreatedSuccessfully(
+            source: String,
+            creationType: WalletCreationType = WalletCreationType.NewSeed,
+            seedPhraseLength: Int? = null,
+            passPhraseState: AnalyticsParam.EmptyFull,
+        ) : CreateWallet(
+            event = "Wallet Created Successfully",
+            params = buildMap {
+                put(AnalyticsParam.SOURCE, source)
+                put("Creation Type", creationType.value)
+                put("Passphrase", passPhraseState.value)
+                if (seedPhraseLength != null) {
+                    put("Seed Phrase Length", seedPhraseLength.toString())
+                }
+            },
+        )
+
+        sealed class WalletCreationType(val value: String) {
+            data object NewSeed : WalletCreationType(value = "New Seed")
+            data object SeedImport : WalletCreationType(value = "Seed Import")
+        }
+    }
+
+    sealed class SeedPhrase(
+        event: String,
+        params: Map<String, String> = mapOf(),
+    ) : OnboardingAnalyticsEvent(category = "Onboarding / Seed Phrase", event = event, params = params) {
+
+        data object CreateMobileScreenOpened : SeedPhrase("Create Mobile Screen Opened")
+        data object ButtonImportWallet : SeedPhrase("Button - Import Wallet")
+        data object ImportSeedPhraseScreenOpened : SeedPhrase("Import Seed Phrase Screen Opened")
+        data object ButtonImport : SeedPhrase("Button - Import")
+    }
+
+    sealed class Error(
+        event: String,
+        params: Map<String, String> = mapOf(),
     ) : OnboardingAnalyticsEvent(category = "Error", event = event, params = params) {
 
         data class OfflineAttestationFailed(
             val source: AnalyticsParam.ScreensSources,
-        ) : Onboarding(
+        ) : Error(
             event = "Offline Attestation Failed",
             params = mapOf(AnalyticsParam.SOURCE to source.value),
         )
