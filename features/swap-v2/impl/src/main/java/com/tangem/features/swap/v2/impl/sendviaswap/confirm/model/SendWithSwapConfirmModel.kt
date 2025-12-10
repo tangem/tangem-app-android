7883f0a5984c4fa1073f17c39c2c1b7fdade868d
Tangem <git@tangem.com>
2025-12-10 15:46:19 +0500

    Updated on 2026-07-23

--- features/swap-v2/impl/src/main/java/com/tangem/features/swap/v2/impl/sendviaswap/confirm/model/SendWithSwapConfirmModel.kt
+++ features/swap-v2/impl/src/main/java/com/tangem/features/swap/v2/impl/sendviaswap/confirm/model/SendWithSwapConfirmModel.kt
@@ -428,6 +428,7 @@ internal class SendWithSwapConfirmModel @Inject constructor(
                 route = SendWithSwapRoute.Confirm,
                 sendWithSwapUM = state.copy(
                     navigationUM = NavigationUM.Content(
+                        source = SendWithSwapRoute.Confirm.javaClass.simpleName,
                         title = resourceReference(id = R.string.send_with_swap_confirm_title),
                         subtitle = null,
                         backIconRes = R.drawable.ic_back_24,
