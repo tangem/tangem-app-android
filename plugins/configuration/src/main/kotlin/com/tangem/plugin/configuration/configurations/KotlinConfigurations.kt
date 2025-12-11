43f7295ef003fa2a450c5e139d694c5dc49be5e6
Tangem <git@tangem.com>
2025-12-11 12:14:20 +0400

    Updated on 2026-07-01

--- plugins/configuration/src/main/kotlin/com/tangem/plugin/configuration/configurations/KotlinConfigurations.kt
+++ plugins/configuration/src/main/kotlin/com/tangem/plugin/configuration/configurations/KotlinConfigurations.kt
@@ -1,21 +1,31 @@
 package com.tangem.plugin.configuration.configurations
 
 import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.jvm.toolchain.JavaLanguageVersion
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 internal fun Project.configureKotlinCompilerOptions() {
+    extensions.configure<JavaPluginExtension> {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(17))
+        }
+    }
+
     kotlinExtension.sourceSets.all {
         // https://github.com/Kotlin/KEEP/blob/explicit-backing-fields-re/proposals/explicit-backing-fields.md
         languageSettings.enableLanguageFeature("ExplicitBackingFields")
     }
     project.tasks.withType<KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = "17"
-            allWarningsAsErrors = false
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+            allWarningsAsErrors.set(false)
             // this is required to produce a unique META-INF/*.kotlin_module files
-            moduleName = project.path.removePrefix(":").replace(':', '-')
+            freeCompilerArgs.add("-module-name=${project.path.removePrefix(":").replace(':', '-')}")
         }
     }
 }
