7f95b8cedb46e9ce0852104e7f4d3102fb959960
Tangem <git@tangem.com>
2025-12-15 10:48:01 +0100

    Updated on 2026-02-11

--- features/feed/impl/src/main/kotlin/com/tangem/features/feed/di/FeedComponentModule.kt
+++ features/feed/impl/src/main/kotlin/com/tangem/features/feed/di/FeedComponentModule.kt
@@ -10,7 +10,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-internal interface ComponentModule {
+internal interface FeedComponentModule {
 
     @Binds
     @Singleton
