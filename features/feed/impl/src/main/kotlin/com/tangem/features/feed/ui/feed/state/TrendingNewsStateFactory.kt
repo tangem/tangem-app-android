eae1fdf254c05200e51c357e27cf0b9da5361188
Tangem <git@tangem.com>
2025-12-15 10:48:01 +0100

    Updated on 2026-09-21

--- features/feed/impl/src/main/kotlin/com/tangem/features/feed/ui/feed/state/TrendingNewsStateFactory.kt
+++ features/feed/impl/src/main/kotlin/com/tangem/features/feed/ui/feed/state/TrendingNewsStateFactory.kt
@@ -4,12 +4,19 @@ import com.tangem.common.ui.news.ArticleConfigUM
 import com.tangem.core.ui.components.label.entity.LabelLeadingContentUM
 import com.tangem.core.ui.components.label.entity.LabelUM
 import com.tangem.core.ui.extensions.TextReference
+import com.tangem.core.ui.extensions.WrappedList
+import com.tangem.core.ui.extensions.wrappedList
+import com.tangem.core.ui.utils.FormattedDate
+import com.tangem.core.ui.utils.getFormattedDate
 import com.tangem.data.common.currency.getTokenIconUrlFromDefaultHost
 import com.tangem.domain.models.currency.CryptoCurrency
 import com.tangem.domain.models.news.ShortArticle
+import com.tangem.features.feed.impl.R
 import com.tangem.utils.Provider
+import com.tangem.utils.StringsSigns
 import kotlinx.collections.immutable.toPersistentList
 import kotlinx.collections.immutable.toPersistentSet
+import org.joda.time.DateTime
 
 internal class TrendingNewsStateFactory(
     private val currentStateProvider: Provider<FeedListUM>,
@@ -47,7 +54,7 @@ internal class TrendingNewsStateFactory(
                                 )
                             },
                         ).toPersistentSet(),
-                        createdAt = "1 min ago", // TODO in [REDACTED_TASK_KEY]
+                        createdAt = mapFormattedDate(article.createdAt),
                         isViewed = article.viewed,
                     )
                 },
@@ -72,12 +79,42 @@ internal class TrendingNewsStateFactory(
                                     )
                                 },
                             ).toPersistentSet(),
-                            createdAt = "1 min ago", // TODO in [REDACTED_TASK_KEY]
+                            createdAt = mapFormattedDate(article.createdAt),
                             isViewed = article.viewed,
                         )
                     }.toPersistentList(),
                 ),
             ),
         )
+    }
+
+    private fun mapFormattedDate(createdAt: String): TextReference {
+        val formattedDate = getFormattedDate(
+            createdAt = createdAt,
+            now = DateTime.now(),
+        )
+        return when (formattedDate) {
+            is FormattedDate.FullDate -> TextReference.Str(value = formattedDate.date)
+            is FormattedDate.HoursAgo -> TextReference.PluralRes(
+                id = R.plurals.news_published_hours_ago,
+                count = formattedDate.hours,
+                formatArgs = wrappedList(formattedDate.hours),
+            )
+            is FormattedDate.MinutesAgo -> TextReference.PluralRes(
+                id = R.plurals.news_published_minutes_ago,
+                count = formattedDate.minutes,
+                formatArgs = wrappedList(formattedDate.minutes),
+            )
+            is FormattedDate.Today -> TextReference.Combined(
+                refs = WrappedList(
+                    data = listOf(
+                        TextReference.Res(R.string.common_today),
+                        TextReference.Str(StringsSigns.COMA_SIGN),
+                        TextReference.Str(StringsSigns.WHITE_SPACE),
+                        TextReference.Str(formattedDate.time),
+                    ),
+                ),
+            )
+        }
     }
 }
