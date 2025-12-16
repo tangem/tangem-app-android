7d1fd4b95dc326ea659aa77e849a0a097009c072
Tangem <git@tangem.com>
2025-12-16 13:11:59 +0100

    Updated on 2026-09-23

--- features/feed/impl/src/main/kotlin/com/tangem/features/feed/components/feed/DefaultFeedComponent.kt
+++ features/feed/impl/src/main/kotlin/com/tangem/features/feed/components/feed/DefaultFeedComponent.kt
@@ -3,19 +3,22 @@ package com.tangem.features.feed.components.feed
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.LifecycleStartEffect
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.tangem.core.decompose.context.AppComponentContext
 import com.tangem.core.decompose.model.getOrCreateModel
 import com.tangem.core.ui.decompose.ComposableModularContentComponent
 import com.tangem.features.feed.model.feed.FeedComponentModel
-import com.tangem.features.feed.ui.feed.FeedListContent
+import com.tangem.features.feed.model.feed.FeedModelClickIntents
+import com.tangem.features.feed.ui.feed.FeedList
 import com.tangem.features.feed.ui.feed.FeedListHeader
 
 internal class DefaultFeedComponent(
     appComponentContext: AppComponentContext,
+    private val params: FeedParams,
 ) : ComposableModularContentComponent, AppComponentContext by appComponentContext {
 
-    private val feedComponentModel = getOrCreateModel<FeedComponentModel>()
+    private val feedComponentModel = getOrCreateModel<FeedComponentModel, FeedParams>(params = params)
 
     @Composable
     override fun Title() {
@@ -25,8 +28,15 @@ internal class DefaultFeedComponent(
 
     @Composable
     override fun Content(modifier: Modifier) {
+        LifecycleStartEffect(Unit) {
+            feedComponentModel.isVisibleOnScreen.value = true
+            onStopOrDispose {
+                feedComponentModel.isVisibleOnScreen.value = false
+            }
+        }
+
         val state by feedComponentModel.state.collectAsStateWithLifecycle()
-        FeedListContent(
+        FeedList(
             modifier = modifier,
             state = state,
         )
@@ -34,4 +44,6 @@ internal class DefaultFeedComponent(
 
     @Composable
     override fun Footer() = Unit
+
+    data class FeedParams(val feedClickIntents: FeedModelClickIntents)
 }
