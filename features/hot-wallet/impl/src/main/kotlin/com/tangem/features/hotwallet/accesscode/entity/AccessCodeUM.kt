7e02e72a40a654c2dd40b4106d907559c09bcc7c
Tangem <git@tangem.com>
2025-12-16 16:26:46 +0500

    Updated on 2026-08-14

--- features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/accesscode/entity/AccessCodeUM.kt
+++ features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/accesscode/entity/AccessCodeUM.kt
@@ -1,6 +1,8 @@
 package com.tangem.features.hotwallet.accesscode.entity
 
 import com.tangem.core.ui.components.fields.PinTextColor
+import com.tangem.core.ui.event.StateEvent
+import com.tangem.core.ui.event.consumedEvent
 import com.tangem.features.hotwallet.accesscode.ACCESS_CODE_LENGTH
 
 internal data class AccessCodeUM(
@@ -8,6 +10,7 @@ internal data class AccessCodeUM(
     val accessCodeColor: PinTextColor,
     val onAccessCodeChange: (String) -> Unit,
     val isConfirmMode: Boolean,
+    val requestFocus: StateEvent<Unit> = consumedEvent(),
 ) {
     val accessCodeLength: Int = ACCESS_CODE_LENGTH
 }
