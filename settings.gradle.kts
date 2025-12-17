a48cde7898218cf224edf65618c72f5345cdf839
Tangem <git@tangem.com>
2025-12-17 15:20:04 +0500

    Updated on 2026-07-07

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -153,19 +153,20 @@ include(":test:core")
 include(":test:mock")
 
 // region Core modules
+include(":core:ab-tests")
 include(":core:analytics")
 include(":core:analytics:models")
-include(":core:datasource")
 include(":core:config-toggles")
-include(":core:ab-tests")
-include(":core:navigation")
-include(":core:res")
-include(":core:ui")
-include(":core:utils")
+include(":core:datasource")
 include(":core:decompose")
-include(":core:pagination")
 include(":core:error")
 include(":core:error:ext")
+include(":core:navigation")
+include(":core:pagination")
+include(":core:res")
+include(":core:security")
+include(":core:ui")
+include(":core:utils")
 // endregion Core modules
 
 // region Common modules
