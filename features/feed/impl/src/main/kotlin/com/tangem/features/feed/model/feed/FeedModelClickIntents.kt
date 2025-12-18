36c0547d963593b4513e04b809ef07ab7a45ba9c
Tangem <git@tangem.com>
2025-12-18 12:40:50 +0100

    Updated on 2026-07-07

--- features/feed/impl/src/main/kotlin/com/tangem/features/feed/model/feed/FeedModelClickIntents.kt
+++ features/feed/impl/src/main/kotlin/com/tangem/features/feed/model/feed/FeedModelClickIntents.kt
@@ -2,7 +2,7 @@ package com.tangem.features.feed.model.feed
 
 import com.tangem.domain.appcurrency.model.AppCurrency
 import com.tangem.domain.markets.TokenMarketParams
-import com.tangem.features.feed.ui.market.state.SortByTypeUM
+import com.tangem.features.feed.ui.market.list.state.SortByTypeUM
 
 /**
  * Callback interface for feed model navigation actions.
