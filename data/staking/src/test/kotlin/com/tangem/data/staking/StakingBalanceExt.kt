87fc29130d0e4bb8178fa802703c9d98f32fa1e1
Tangem <git@tangem.com>
2025-12-18 10:31:03 +0200

    Updated on 2026-06-12

--- data/staking/src/test/kotlin/com/tangem/data/staking/StakingBalanceExt.kt
+++ data/staking/src/test/kotlin/com/tangem/data/staking/StakingBalanceExt.kt
@@ -1,6 +1,6 @@
 package com.tangem.data.staking
 
-import com.tangem.data.staking.converters.ethpool.P2PStakingBalanceConverter
+import com.tangem.data.staking.converters.ethpool.P2PEthPoolStakingBalanceConverter
 import com.tangem.datasource.api.ethpool.models.response.P2PEthPoolAccountResponse
 import com.tangem.datasource.api.stakekit.models.response.model.YieldBalanceWrapperDTO
 import com.tangem.datasource.local.token.converter.StakingBalanceConverter
@@ -11,8 +11,10 @@ internal fun YieldBalanceWrapperDTO.toDomain(source: StatusSource = StatusSource
     return StakingBalanceConverter(isCached = source == StatusSource.CACHE).convert(this)!!
 }
 
-internal fun P2PEthPoolAccountResponse.toDomain(source: StatusSource = StatusSource.CACHE): StakingBalance.Data.P2P {
-    return P2PStakingBalanceConverter.convert(
+internal fun P2PEthPoolAccountResponse.toDomain(
+    source: StatusSource = StatusSource.CACHE,
+): StakingBalance.Data.P2PEthPool {
+    return P2PEthPoolStakingBalanceConverter.convert(
         response = this,
         source = source,
     )
--- domain/staking/src/main/java/com/tangem/domain/staking/usecase/StakingApyFlowUseCase.kt
+++ domain/staking/src/main/java/com/tangem/domain/staking/usecase/StakingApyFlowUseCase.kt
@@ -2,35 +2,50 @@ package com.tangem.domain.staking.usecase
 
 import com.tangem.blockchain.common.Blockchain
 import com.tangem.blockchainsdk.utils.toCoinId
-import com.tangem.domain.staking.model.stakekit.Yield
+import com.tangem.domain.staking.model.StakingTarget
+import com.tangem.domain.staking.model.toStakingTarget
+import com.tangem.domain.staking.repositories.P2PEthPoolRepository
 import com.tangem.domain.staking.repositories.StakeKitRepository
 import com.tangem.domain.staking.toggles.StakingFeatureToggles
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.combine
 
 /**
- * Emits a map of Validators values per currency for staking.
+ * Emits a map of StakingTarget values per currency for staking.
  *
  * Return map:
- * - key: currency staking key (network.backendId + "_" + symbol)
- * - value: validators
+ * - key: currency staking key (coinGeckoId + "_" + symbol)
+ * - value: list of staking targets (validators or vaults)
  */
 class StakingApyFlowUseCase(
     private val stakeKitRepository: StakeKitRepository,
+    private val p2pEthPoolRepository: P2PEthPoolRepository,
     private val stakingFeatureToggles: StakingFeatureToggles,
 ) {
 
-    operator fun invoke(): Flow<Map<String, List<Yield.Validator>>> {
-        return stakeKitRepository.getEnabledYields()
-            .map { yields ->
-                yields.filterNot { yield ->
-                    val isCardanoYield = yield.token.coinGeckoId == Blockchain.Cardano.toCoinId()
-                    isCardanoYield && !stakingFeatureToggles.isCardanoStakingEnabled
-                }.associate { yield ->
-                    val key = "${yield.token.coinGeckoId}_${yield.token.symbol}"
-                    val apy = yield.validators
-                    key to apy
-                }
+    operator fun invoke(): Flow<Map<String, List<StakingTarget>>> {
+        return combine(
+            stakeKitRepository.getEnabledYields(),
+            p2pEthPoolRepository.getVaultsFlow(),
+        ) { yields, p2pVaults ->
+            val stakeKitMap = yields.filterNot { yield ->
+                val isCardanoYield = yield.token.coinGeckoId == Blockchain.Cardano.toCoinId()
+                isCardanoYield && !stakingFeatureToggles.isCardanoStakingEnabled
+            }.associate { yield ->
+                val key = "${yield.token.coinGeckoId}_${yield.token.symbol}"
+                val targets = yield.validators.map { it.toStakingTarget() }
+                key to targets
             }
+
+            val p2pMap = if (p2pVaults.isNotEmpty()) {
+                val ethKey = "${Blockchain.Ethereum.toCoinId()}_${Blockchain.Ethereum.currency}"
+                val targets = p2pVaults.map { it.toStakingTarget() }
+                mapOf(ethKey to targets)
+            } else {
+                emptyMap()
+            }
+
+            stakeKitMap + p2pMap
+        }
     }
 }
