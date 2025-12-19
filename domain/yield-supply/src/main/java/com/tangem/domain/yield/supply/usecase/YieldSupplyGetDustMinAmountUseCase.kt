58a1b3ec202beabd7e42f6625747277bc08cd006
Tangem <git@tangem.com>
2025-12-19 15:29:07 +0400

    Updated on 2026-01-20

--- domain/yield-supply/src/main/java/com/tangem/domain/yield/supply/usecase/YieldSupplyGetDustMinAmountUseCase.kt
+++ domain/yield-supply/src/main/java/com/tangem/domain/yield/supply/usecase/YieldSupplyGetDustMinAmountUseCase.kt
@@ -3,6 +3,13 @@ package com.tangem.domain.yield.supply.usecase
 import com.tangem.domain.appcurrency.model.AppCurrency
 import java.math.BigDecimal
 
+/**
+ * Use case for getting dust minimum amount for yield supply.
+ *
+ * Returns the minimum amount based on the selected [AppCurrency].
+ * If [AppCurrency] is in [SUPPORTED_DUST_CURRENCIES], returns [DUST_MIN_AMOUNT],
+ * otherwise returns the original [minAmount] with trailing zeros stripped.
+ */
 class YieldSupplyGetDustMinAmountUseCase {
 
     operator fun invoke(minAmount: BigDecimal, appCurrency: AppCurrency): BigDecimal {
