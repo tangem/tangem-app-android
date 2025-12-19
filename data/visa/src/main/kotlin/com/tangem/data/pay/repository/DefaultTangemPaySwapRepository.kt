c7b47a6b7ea77bf59e1ab4836c168b812b262273
Tangem <git@tangem.com>
2025-12-19 17:25:04 +0500

    Updated on 2026-02-06

--- data/visa/src/main/kotlin/com/tangem/data/pay/repository/DefaultTangemPaySwapRepository.kt
+++ data/visa/src/main/kotlin/com/tangem/data/pay/repository/DefaultTangemPaySwapRepository.kt
@@ -1,23 +1,20 @@
 package com.tangem.data.pay.repository
 
 import arrow.core.Either
+import arrow.core.left
 import com.tangem.core.error.UniversalError
 import com.tangem.data.common.quote.QuotesFetcher
 import com.tangem.datasource.api.pay.TangemPayApi
 import com.tangem.datasource.api.pay.models.request.WithdrawDataRequest
 import com.tangem.datasource.api.pay.models.request.WithdrawRequest
 import com.tangem.datasource.local.visa.TangemPayStorage
-import com.tangem.domain.common.wallets.UserWalletsListRepository
 import com.tangem.domain.models.currency.CryptoCurrency
 import com.tangem.domain.models.wallet.UserWallet
-import com.tangem.domain.models.wallet.UserWalletId
 import com.tangem.domain.pay.WithdrawalResult
+import com.tangem.domain.pay.WithdrawalSignatureResult
 import com.tangem.domain.pay.datasource.TangemPayAuthDataSource
-import com.tangem.domain.pay.model.WithdrawalSignatureResult
 import com.tangem.domain.pay.repository.TangemPaySwapRepository
 import com.tangem.domain.visa.error.VisaApiError
-import com.tangem.domain.wallets.legacy.UserWalletsListManager
-import com.tangem.features.hotwallet.HotWalletFeatureToggles
 import com.tangem.utils.extensions.addHexPrefix
 import java.math.BigDecimal
 import java.math.RoundingMode
@@ -30,29 +27,25 @@ internal class DefaultTangemPaySwapRepository @Inject constructor(
     private val tangemPayApi: TangemPayApi,
     private val requestHelper: TangemPayRequestPerformer,
     private val authDataSource: TangemPayAuthDataSource,
-    private val userWalletsListManager: UserWalletsListManager,
-    private val userWalletsListRepository: UserWalletsListRepository,
-    private val hotWalletFeatureToggles: HotWalletFeatureToggles,
     private val quotesFetcher: QuotesFetcher,
     private val tangemPayStorage: TangemPayStorage,
 ) : TangemPaySwapRepository {
 
     override suspend fun withdraw(
-        userWalletId: UserWalletId,
+        userWallet: UserWallet,
         receiverAddress: String,
         cryptoAmount: BigDecimal,
         cryptoCurrencyId: CryptoCurrency.RawID,
     ): Either<UniversalError, WithdrawalResult> {
         val amountInCents = getAmountInCents(cryptoAmount, cryptoCurrencyId)
         if (amountInCents.isNullOrEmpty()) return Either.Left(VisaApiError.WithdrawalDataError)
-        return requestHelper.performRequest(userWalletId) { authHeader ->
+        return requestHelper.performRequest(userWallet.walletId) { authHeader ->
             val request = WithdrawDataRequest(amountInCents = amountInCents, recipientAddress = receiverAddress)
             tangemPayApi.getWithdrawData(authHeader = authHeader, body = request)
         }.map { data ->
-            val result = data.result
-            if (result == null) return Either.Left(VisaApiError.WithdrawalDataError)
+            val result = data.result ?: return VisaApiError.WithdrawalDataError.left()
             val signatureResult = authDataSource.getWithdrawalSignature(
-                cardId = getCardId(userWalletId),
+                userWallet = userWallet,
                 hash = result.hash,
             ).getOrNull()
 
@@ -61,7 +54,7 @@ internal class DefaultTangemPaySwapRepository @Inject constructor(
                     Either.Right(WithdrawalResult.Cancelled)
                 }
                 is WithdrawalSignatureResult.Success -> {
-                    requestHelper.performRequest(userWalletId) { authHeader ->
+                    requestHelper.performRequest(userWallet.walletId) { authHeader ->
                         val request = WithdrawRequest(
                             amountInCents = amountInCents,
                             recipientAddress = receiverAddress,
@@ -74,7 +67,7 @@ internal class DefaultTangemPaySwapRepository @Inject constructor(
                         .mapLeft { return Either.Left(VisaApiError.WithdrawError) }
                         .map { response ->
                             val orderId = response.result?.orderId
-                            if (orderId != null) tangemPayStorage.storeWithdrawOrder(userWalletId, orderId)
+                            if (orderId != null) tangemPayStorage.storeWithdrawOrder(userWallet.walletId, orderId)
                             WithdrawalResult.Success
                         }
                 }
@@ -102,18 +95,5 @@ internal class DefaultTangemPaySwapRepository @Inject constructor(
         ).getOrNull()
 
         return quotes?.quotes[cryptoCurrencyId.value]?.price
-    }
-
-    private fun getCardId(userWalletId: UserWalletId): String {
-        val userWallet = if (hotWalletFeatureToggles.isHotWalletEnabled) {
-            userWalletsListRepository.userWallets.value?.firstOrNull { it.walletId == userWalletId }
-        } else {
-            userWalletsListManager.userWalletsSync.firstOrNull { it.walletId == userWalletId }
-        } ?: error("No User Wallet found")
-        return if (userWallet is UserWallet.Cold) {
-            userWallet.cardId
-        } else {
-            TODO("[REDACTED_JIRA]")
-        }
     }
 }
