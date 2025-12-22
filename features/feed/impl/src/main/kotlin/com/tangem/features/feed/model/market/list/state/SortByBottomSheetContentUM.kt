daaae403a81f37627dad5623dba2c975f2467bb9
Tangem <git@tangem.com>
2025-12-22 09:03:01 +0100

    Updated on 2026-03-27

--- features/feed/impl/src/main/kotlin/com/tangem/features/feed/model/market/list/state/SortByBottomSheetContentUM.kt
+++ features/feed/impl/src/main/kotlin/com/tangem/features/feed/model/market/list/state/SortByBottomSheetContentUM.kt
@@ -1,4 +1,4 @@
-package com.tangem.features.feed.ui.market.list.state
+package com.tangem.features.feed.model.market.list.state
 
 import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfigContent
 
