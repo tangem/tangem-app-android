77702334bb629d6df8bf7ca4f6ac1627ac5d8d94
Tangem <git@tangem.com>
2025-12-23 15:21:07 +0400

    Updated on 2026-02-06

--- app/src/main/java/com/tangem/tap/di/domain/WalletConnectDomainModule.kt
+++ app/src/main/java/com/tangem/tap/di/domain/WalletConnectDomainModule.kt
@@ -1,6 +1,8 @@
 package com.tangem.tap.di.domain
 
 import com.tangem.blockchain.common.TransactionSigner
+import com.tangem.data.wallets.hot.TangemHotWalletSigner
+import com.tangem.domain.card.common.TapWorkarounds.isTangemTwins
 import com.tangem.domain.card.models.TwinKey
 import com.tangem.domain.card.repository.CardSdkConfigRepository
 import com.tangem.domain.models.wallet.UserWallet
@@ -37,15 +39,21 @@ internal object WalletConnectDomainModule {
     @Singleton
     fun providesWcTransactionSignerProvider(
         cardSdkConfigRepository: CardSdkConfigRepository,
+        tangemHotWalletSignerFactory: TangemHotWalletSigner.Factory,
     ): WcTransactionSignerProvider {
         return object : WcTransactionSignerProvider {
             override fun createSigner(wallet: UserWallet): TransactionSigner {
-                val coldWallet = wallet as? UserWallet.Cold
-                val card = coldWallet?.scanResponse?.card
-                return cardSdkConfigRepository.getCommonSigner(
-                    cardId = card?.cardId,
-                    twinKey = coldWallet?.scanResponse?.let { TwinKey.getOrNull(it) },
-                )
+                return when (wallet) {
+                    is UserWallet.Hot -> tangemHotWalletSignerFactory.create(wallet)
+                    is UserWallet.Cold -> {
+                        val card = wallet.scanResponse.card
+                        val isCardNotBackedUp = card.backupStatus?.isActive != true && !card.isTangemTwins
+                        cardSdkConfigRepository.getCommonSigner(
+                            cardId = card.cardId.takeIf { isCardNotBackedUp },
+                            twinKey = TwinKey.getOrNull(scanResponse = wallet.scanResponse),
+                        )
+                    }
+                }
             }
         }
     }
