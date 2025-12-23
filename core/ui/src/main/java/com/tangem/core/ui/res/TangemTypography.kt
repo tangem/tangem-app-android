37bb1fac727eaf7d88374e3ffb5cb45910b8d2d6
Tangem <git@tangem.com>
2025-12-23 15:12:48 +0500

    Updated on 2026-06-24

--- core/ui/src/main/java/com/tangem/core/ui/res/TangemTypography.kt
+++ core/ui/src/main/java/com/tangem/core/ui/res/TangemTypography.kt
@@ -4,7 +4,6 @@ import androidx.compose.runtime.Immutable
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.TextUnit
@@ -15,11 +14,6 @@ import com.tangem.core.ui.R
 internal val RobotoFamily = FontFamily(
     Font(R.font.roboto_regular, FontWeight.Normal),
     Font(R.font.roboto_medium, FontWeight.Medium),
-)
-
-internal val InterFamily = FontFamily(
-    Font(R.font.inter_regular),
-    Font(R.font.inter_italic, style = FontStyle.Italic),
 )
 
 @Immutable
