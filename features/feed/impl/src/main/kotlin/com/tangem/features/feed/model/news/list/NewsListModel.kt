fc4826b40c8efe476889b7052506df9ddedbab5e
Tangem <git@tangem.com>
2025-12-26 13:06:23 +0100

    Updated on 2026-06-05

---
+++ features/feed/impl/src/main/kotlin/com/tangem/features/feed/model/news/list/NewsListModel.kt
@@ -0,0 +1,82 @@
+package com.tangem.features.feed.model.news.list
+
+import com.tangem.core.decompose.di.ModelScoped
+import com.tangem.core.decompose.model.Model
+import com.tangem.core.decompose.model.ParamsContainer
+import com.tangem.core.ui.components.chip.entity.ChipUM
+import com.tangem.core.ui.extensions.TextReference
+import com.tangem.domain.news.usecase.GetNewsCategoriesUseCase
+import com.tangem.features.feed.components.news.list.DefaultNewsListComponent
+import com.tangem.features.feed.ui.news.list.state.NewsListUM
+import com.tangem.utils.coroutines.CoroutineDispatcherProvider
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.toImmutableList
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@ModelScoped
+internal class NewsListModel @Inject constructor(
+    override val dispatchers: CoroutineDispatcherProvider,
+    private val getNewsCategoriesUseCase: GetNewsCategoriesUseCase,
+    paramsContainer: ParamsContainer,
+) : Model() {
+
+    private val params = paramsContainer.require<DefaultNewsListComponent.Params>()
+
+    private val _state = MutableStateFlow(
+        NewsListUM(
+            selectedCategoryId = 0,
+            filters = persistentListOf(),
+            articles = persistentListOf(),
+            onArticleClick = params.onArticleClicked,
+            onBackClick = params.onBackClick,
+        ),
+    )
+    val state = _state.asStateFlow()
+
+    init {
+        modelScope.launch(dispatchers.default) {
+            val filterChips = getNewsCategoriesUseCase
+                .invoke()
+                .fold(
+                    ifLeft = {
+                        persistentListOf()
+                    },
+                    ifRight = { categories ->
+                        categories.map { articleCategory ->
+                            ChipUM(
+                                id = articleCategory.id,
+                                text = TextReference.Str(articleCategory.name),
+                                isSelected = false,
+                                onClick = {
+                                    onCategoryClick(articleCategory.id)
+                                },
+                            )
+                        }.toImmutableList()
+                    },
+                )
+            _state.update { currentState ->
+                currentState.copy(filters = filterChips)
+            }
+        }
+    }
+
+    private fun onCategoryClick(categoryId: Int) {
+        _state.update { currentState ->
+            currentState.copy(
+                selectedCategoryId = categoryId,
+                filters = updateFilterChips(categoryId),
+            )
+        }
+    }
+
+    private fun updateFilterChips(categoryId: Int): ImmutableList<ChipUM> {
+        return state.value.filters.map { chip ->
+            chip.copy(isSelected = chip.id == categoryId)
+        }.toImmutableList()
+    }
+}
