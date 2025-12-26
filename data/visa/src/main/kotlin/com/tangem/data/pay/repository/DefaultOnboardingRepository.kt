b9299319770cbb8a5369040df0d1813fefe3f265
Tangem <git@tangem.com>
2025-12-26 15:29:35 +0500

    Updated on 2026-06-02

--- data/visa/src/main/kotlin/com/tangem/data/pay/repository/DefaultOnboardingRepository.kt
+++ data/visa/src/main/kotlin/com/tangem/data/pay/repository/DefaultOnboardingRepository.kt
@@ -1,6 +1,7 @@
 package com.tangem.data.pay.repository
 
 import arrow.core.Either
+import arrow.core.raise.catch
 import com.tangem.datasource.api.pay.TangemPayApi
 import com.tangem.datasource.api.pay.models.request.DeeplinkValidityRequest
 import com.tangem.datasource.api.pay.models.request.OrderRequest
@@ -23,6 +24,7 @@ import com.tangem.features.hotwallet.HotWalletFeatureToggles
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import timber.log.Timber
 import java.util.concurrent.ConcurrentHashMap
 import javax.inject.Inject
 
@@ -110,15 +112,21 @@ internal class DefaultOnboardingRepository @Inject constructor(
 
     override suspend fun createOrder(userWalletId: UserWalletId) = withContext(dispatcherProvider.io) {
         launch {
-            requestHelper.runWithErrorLogs(TAG) {
-                val walletAddress = requestHelper.getCustomerWalletAddress(userWalletId)
-                val result = requestHelper.request(userWalletId) { authHeader ->
-                    tangemPayApi.createOrder(authHeader, body = OrderRequest(walletAddress))
-                }.result ?: error("Create order result is null")
+            catch(
+                block = {
+                    val walletAddress = requestHelper.getCustomerWalletAddress(userWalletId)
+                    val response = requestHelper.performRequest(userWalletId) { authHeader ->
+                        tangemPayApi.createOrder(authHeader, body = OrderRequest(walletAddress))
+                    }.getOrNull()
 
-                val customerWalletAddress = requireNotNull(result.data.customerWalletAddress)
-                tangemPayStorage.storeOrderId(customerWalletAddress, result.id)
-            }
+                    val result = requireNotNull(response?.result)
+                    val customerWalletAddress = requireNotNull(result.data.customerWalletAddress)
+                    tangemPayStorage.storeOrderId(customerWalletAddress, result.id)
+                },
+                catch = {
+                    Timber.tag(TAG).e("createOrder: $it")
+                },
+            )
         }
     }
 
