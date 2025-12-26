e6ab1412e5f18debc7de51e28b9aee2c718d3227
Tangem <git@tangem.com>
2025-12-26 11:05:04 +0100

    Updated on 2026-06-12

--- features/feed/impl/src/main/kotlin/com/tangem/features/feed/ui/feed/state/TrendingNewsStateFactory.kt
+++ features/feed/impl/src/main/kotlin/com/tangem/features/feed/ui/feed/state/TrendingNewsStateFactory.kt
@@ -4,22 +4,16 @@ import com.tangem.common.ui.news.ArticleConfigUM
 import com.tangem.core.ui.components.label.entity.LabelLeadingContentUM
 import com.tangem.core.ui.components.label.entity.LabelUM
 import com.tangem.core.ui.extensions.TextReference
-import com.tangem.core.ui.extensions.WrappedList
-import com.tangem.core.ui.extensions.wrappedList
-import com.tangem.core.ui.utils.FormattedDate
-import com.tangem.core.ui.utils.getFormattedDate
 import com.tangem.data.common.currency.getTokenIconUrlFromDefaultHost
 import com.tangem.domain.models.currency.CryptoCurrency
 import com.tangem.domain.models.news.ShortArticle
 import com.tangem.domain.models.news.TrendingNews
-import com.tangem.features.feed.impl.R
+import com.tangem.features.feed.ui.utils.mapFormattedDate
 import com.tangem.utils.Provider
-import com.tangem.utils.StringsSigns
 import kotlinx.collections.immutable.ImmutableSet
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.toPersistentList
 import kotlinx.collections.immutable.toPersistentSet
-import org.joda.time.DateTime
 
 internal class TrendingNewsStateFactory(
     private val currentStateProvider: Provider<FeedListUM>,
@@ -110,35 +104,5 @@ internal class TrendingNewsStateFactory(
             )
         }
         return (categoryLabels + tokenLabels).toPersistentSet()
-    }
-
-    private fun mapFormattedDate(createdAt: String): TextReference {
-        val formattedDate = getFormattedDate(
-            createdAt = createdAt,
-            now = DateTime.now(),
-        )
-        return when (formattedDate) {
-            is FormattedDate.FullDate -> TextReference.Str(value = formattedDate.date)
-            is FormattedDate.HoursAgo -> TextReference.PluralRes(
-                id = R.plurals.news_published_hours_ago,
-                count = formattedDate.hours,
-                formatArgs = wrappedList(formattedDate.hours),
-            )
-            is FormattedDate.MinutesAgo -> TextReference.PluralRes(
-                id = R.plurals.news_published_minutes_ago,
-                count = formattedDate.minutes,
-                formatArgs = wrappedList(formattedDate.minutes),
-            )
-            is FormattedDate.Today -> TextReference.Combined(
-                refs = WrappedList(
-                    data = listOf(
-                        TextReference.Res(R.string.common_today),
-                        TextReference.Str(StringsSigns.COMA_SIGN),
-                        TextReference.Str(StringsSigns.WHITE_SPACE),
-                        TextReference.Str(formattedDate.time),
-                    ),
-                ),
-            )
-        }
     }
 }
