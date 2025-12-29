884b04579f31111b7088aaa2b3b037a7b6ffc285
Tangem <git@tangem.com>
2025-12-29 15:05:35 +0200

    Updated on 2026-07-23

--- data/staking/src/main/java/com/tangem/data/staking/converters/ethpool/P2PEthPoolStakingAccountConverter.kt
+++ data/staking/src/main/java/com/tangem/data/staking/converters/ethpool/P2PEthPoolStakingAccountConverter.kt
@@ -4,17 +4,20 @@ import com.tangem.datasource.api.ethpool.models.response.P2PEthPoolAccountRespon
 import com.tangem.datasource.api.ethpool.models.response.P2PEthPoolExitQueueDTO
 import com.tangem.datasource.api.ethpool.models.response.P2PEthPoolExitRequestDTO
 import com.tangem.datasource.api.ethpool.models.response.P2PEthPoolStakeDTO
-import com.tangem.domain.staking.model.ethpool.*
+import com.tangem.domain.models.staking.P2PEthPoolExitQueue
+import com.tangem.domain.models.staking.P2PEthPoolExitRequest
+import com.tangem.domain.models.staking.P2PEthPoolStake
+import com.tangem.domain.models.staking.P2PEthPoolStakingAccount
 import com.tangem.utils.converter.Converter
-import org.joda.time.Instant
+import kotlinx.datetime.Instant
 
 /**
- * Converter from P2PEthPool Account Info Response to Domain model
+ * Converts P2PEthPool Account API response to domain [P2PEthPoolStakingAccount].
  */
-internal object P2PEthPoolAccountConverter : Converter<P2PEthPoolAccountResponse, P2PEthPoolAccount> {
+internal object P2PEthPoolStakingAccountConverter : Converter<P2PEthPoolAccountResponse, P2PEthPoolStakingAccount> {
 
-    override fun convert(value: P2PEthPoolAccountResponse): P2PEthPoolAccount {
-        return P2PEthPoolAccount(
+    override fun convert(value: P2PEthPoolAccountResponse): P2PEthPoolStakingAccount {
+        return P2PEthPoolStakingAccount(
             delegatorAddress = value.delegatorAddress,
             vaultAddress = value.vaultAddress,
             stake = convertStake(value.stake),
@@ -24,26 +27,26 @@ internal object P2PEthPoolAccountConverter : Converter<P2PEthPoolAccountResponse
         )
     }
 
-    private fun convertStake(dto: P2PEthPoolStakeDTO): P2PEthPoolStake {
+    fun convertStake(dto: P2PEthPoolStakeDTO): P2PEthPoolStake {
         return P2PEthPoolStake(
             assets = dto.assets,
             totalEarnedAssets = dto.totalEarnedAssets,
         )
     }
 
-    private fun convertExitQueue(dto: P2PEthPoolExitQueueDTO): P2PEthPoolExitQueue {
+    fun convertExitQueue(dto: P2PEthPoolExitQueueDTO): P2PEthPoolExitQueue {
         return P2PEthPoolExitQueue(
-            total = dto.total.toBigDecimal(),
+            total = dto.total,
             requests = dto.requests.map(::convertExitRequest),
         )
     }
 
-    private fun convertExitRequest(dto: P2PEthPoolExitRequestDTO): P2PEthPoolExitRequest {
+    fun convertExitRequest(dto: P2PEthPoolExitRequestDTO): P2PEthPoolExitRequest {
         return P2PEthPoolExitRequest(
             ticket = dto.ticket,
-            totalAssets = dto.totalAssets.toBigDecimal(),
-            timestamp = Instant.ofEpochSecond(dto.timestamp),
-            withdrawalTimestamp = Instant.ofEpochSecond(dto.withdrawalTimestamp),
+            totalAssets = dto.totalAssets,
+            timestamp = Instant.fromEpochMilliseconds(dto.timestamp),
+            withdrawalTimestamp = dto.withdrawalTimestamp?.let { Instant.fromEpochMilliseconds(it) },
             isClaimable = dto.isClaimable,
         )
     }
