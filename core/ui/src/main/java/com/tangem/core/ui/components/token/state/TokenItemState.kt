a3fa78a5d14869210defcfb80108d1793ae385f4
Tangem <git@tangem.com>
2026-01-05 18:11:33 +0300

    Updated on 2026-06-27

--- core/ui/src/main/java/com/tangem/core/ui/components/token/state/TokenItemState.kt
+++ core/ui/src/main/java/com/tangem/core/ui/components/token/state/TokenItemState.kt
@@ -235,6 +235,11 @@ sealed class TokenItemState {
             val isFlickering: Boolean = false,
         ) : FiatAmountState()
 
+        data class Icon(
+            val iconRes: Int,
+            val tint: IconTint = IconTint.Inactive,
+        ) : FiatAmountState()
+
         data object Loading : FiatAmountState()
 
         data object Locked : FiatAmountState()
