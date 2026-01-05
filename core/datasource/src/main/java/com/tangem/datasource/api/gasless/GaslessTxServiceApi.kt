ed366851ee063887d89b6178673b42e50f93a042
Tangem <git@tangem.com>
2026-01-05 17:52:37 +0300

    Updated on 2026-09-25

--- core/datasource/src/main/java/com/tangem/datasource/api/gasless/GaslessTxServiceApi.kt
+++ core/datasource/src/main/java/com/tangem/datasource/api/gasless/GaslessTxServiceApi.kt
@@ -2,11 +2,20 @@ package com.tangem.datasource.api.gasless
 
 import com.tangem.datasource.api.common.response.ApiResponse
 import com.tangem.datasource.api.gasless.models.GaslessServiceResponse
+import com.tangem.datasource.api.gasless.models.GaslessSignedTransactionResult
 import com.tangem.datasource.api.gasless.models.GaslessSupportedTokens
+import com.tangem.datasource.api.gasless.models.GaslessTransactionRequest
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface GaslessTxServiceApi {
 
     @GET("api/v1/tokens")
     suspend fun getSupportedTokens(): ApiResponse<GaslessServiceResponse<GaslessSupportedTokens>>
+
+    @POST("api/v1/sign")
+    suspend fun signGaslessTransaction(
+        @Body transaction: GaslessTransactionRequest,
+    ): ApiResponse<GaslessServiceResponse<GaslessSignedTransactionResult>>
 }
