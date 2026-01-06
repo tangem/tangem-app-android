680794d447e6de5bc9cd3f541d65e9fcaec6d920
Tangem <git@tangem.com>
2026-01-06 13:15:40 +0300

    Updated on 2026-08-14

---
+++ data/transaction/src/main/java/com/tangem/data/transaction/convertes/GaslessTxDataToGaslessRequestConverter.kt
@@ -0,0 +1,43 @@
+package com.tangem.data.transaction.convertes
+
+import com.tangem.datasource.api.gasless.models.FeeData
+import com.tangem.datasource.api.gasless.models.TransactionData
+import com.tangem.domain.transaction.models.GaslessTransactionData
+import com.tangem.utils.converter.Converter
+import com.tangem.utils.extensions.toHexString
+import com.tangem.datasource.api.gasless.models.GaslessTransactionData as GaslessTransactionDataDTO
+
+/**
+ * Converts domain GaslessTransactionData to DTO for API requests.
+ * Note: This converter only handles the transaction data conversion.
+ * Additional fields (signature, userAddress, chainId) must be added separately
+ * to create complete GaslessTransactionRequest.
+ */
+class GaslessTxDataToGaslessRequestConverter : Converter<GaslessTransactionData, GaslessTransactionDataDTO> {
+
+    override fun convert(value: GaslessTransactionData): GaslessTransactionDataDTO {
+        return GaslessTransactionDataDTO(
+            transaction = convertTransaction(value.transaction),
+            fee = convertFee(value.fee),
+            nonce = value.nonce.toString(),
+        )
+    }
+
+    private fun convertTransaction(transaction: com.tangem.domain.transaction.models.Transaction): TransactionData {
+        return TransactionData(
+            to = transaction.to,
+            value = transaction.value.toString(),
+            data = transaction.data.toHexString(),
+        )
+    }
+
+    private fun convertFee(fee: com.tangem.domain.transaction.models.Fee): FeeData {
+        return FeeData(
+            feeToken = fee.feeToken,
+            maxTokenFee = fee.maxTokenFee.toString(),
+            coinPriceInToken = fee.coinPriceInToken.toString(),
+            feeTransferGasLimit = fee.feeTransferGasLimit.toString(),
+            baseGas = fee.baseGas.toString(),
+        )
+    }
+}
