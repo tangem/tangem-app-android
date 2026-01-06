2f67a55c7d9976c6fc2611bf59687abaec55c6ad
Tangem <git@tangem.com>
2026-01-06 17:19:24 +0300

    Updated on 2026-05-14

---
+++ features/send-v2/impl/src/main/java/com/tangem/features/send/v2/feeselector/component/extended/model/SelectedTokenItemConverter.kt
@@ -0,0 +1,59 @@
+package com.tangem.features.send.v2.feeselector.component.extended.model
+
+import com.tangem.common.ui.tokens.TokenItemStateConverter
+import com.tangem.core.ui.components.icons.IconTint
+import com.tangem.core.ui.components.token.state.TokenItemState
+import com.tangem.core.ui.extensions.resourceReference
+import com.tangem.core.ui.extensions.wrappedList
+import com.tangem.core.ui.format.bigdecimal.fiat
+import com.tangem.core.ui.format.bigdecimal.format
+import com.tangem.domain.appcurrency.model.AppCurrency
+import com.tangem.domain.models.currency.CryptoCurrencyStatus
+import com.tangem.features.send.v2.impl.R
+import com.tangem.utils.Provider
+import com.tangem.utils.converter.Converter
+
+internal class SelectedTokenItemConverter(
+    private val appCurrencyProvider: Provider<AppCurrency>,
+    private val onTokenClick: () -> Unit,
+) : Converter<CryptoCurrencyStatus, TokenItemState> {
+
+    override fun convert(value: CryptoCurrencyStatus): TokenItemState {
+        if (value.value !is CryptoCurrencyStatus.Loaded) {
+            return TokenItemState.Loading(id = value.currency.id.value)
+        }
+
+        val appCurrency = appCurrencyProvider()
+        val tokenItemConverter = TokenItemStateConverter(
+            appCurrency = appCurrencyProvider(),
+            onItemClick = { _, _ -> onTokenClick() },
+        )
+
+        val state = tokenItemConverter.convert(value)
+
+        return when (state) {
+            is TokenItemState.Content -> {
+                val balance = value.value.fiatAmount.format {
+                    fiat(fiatCurrencyCode = appCurrency.code, fiatCurrencySymbol = appCurrency.symbol)
+                }
+
+                state.copy(
+                    fiatAmountState = TokenItemState.FiatAmountState.Icon(
+                        iconRes = R.drawable.ic_select_18_24,
+                        tint = IconTint.Informative,
+                    ),
+                    subtitleState = TokenItemState.SubtitleState.TextContent(
+                        value = resourceReference(
+                            R.string.common_balance,
+                            wrappedList(balance),
+                        ),
+                        isAvailable = false,
+                    ),
+                    subtitle2State = null,
+
+                )
+            }
+            else -> state
+        }
+    }
+}
