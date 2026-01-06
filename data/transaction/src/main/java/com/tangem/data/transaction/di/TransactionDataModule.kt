2f6c2232f08741c0726ea04dad54151ac04f6731
Tangem <git@tangem.com>
2026-01-06 15:07:38 +0300

    Updated on 2026-09-21

--- data/transaction/src/main/java/com/tangem/data/transaction/di/TransactionDataModule.kt
+++ data/transaction/src/main/java/com/tangem/data/transaction/di/TransactionDataModule.kt
@@ -1,13 +1,17 @@
 package com.tangem.data.transaction.di
 
+import com.tangem.data.common.currency.ResponseCryptoCurrenciesFactory
 import com.tangem.data.transaction.DefaultFeeRepository
+import com.tangem.data.transaction.DefaultGaslessTransactionRepository
 import com.tangem.data.transaction.DefaultTransactionRepository
 import com.tangem.data.transaction.DefaultWalletAddressServiceRepository
 import com.tangem.data.transaction.error.DefaultFeeErrorResolver
+import com.tangem.datasource.api.gasless.GaslessTxServiceApi
 import com.tangem.datasource.api.tangemTech.TangemTechApi
 import com.tangem.datasource.local.walletmanager.WalletManagersStore
 import com.tangem.domain.demo.models.DemoConfig
 import com.tangem.domain.transaction.FeeRepository
+import com.tangem.domain.transaction.GaslessTransactionRepository
 import com.tangem.domain.transaction.TransactionRepository
 import com.tangem.domain.transaction.WalletAddressServiceRepository
 import com.tangem.domain.transaction.error.FeeErrorResolver
@@ -64,5 +68,19 @@ internal object TransactionDataModule {
     @Singleton
     fun providerFeeErrorResolver(): FeeErrorResolver {
         return DefaultFeeErrorResolver()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGaslessTransactionRepository(
+        gaslessTxServiceApi: GaslessTxServiceApi,
+        coroutineDispatcherProvider: CoroutineDispatcherProvider,
+        responseCryptoCurrenciesFactory: ResponseCryptoCurrenciesFactory,
+    ): GaslessTransactionRepository {
+        return DefaultGaslessTransactionRepository(
+            gaslessTxServiceApi,
+            coroutineDispatcherProvider,
+            responseCryptoCurrenciesFactory,
+        )
     }
 }
