c14fa82ab6340ee8ef6aa32529fd13367f9ab0b4
Tangem <git@tangem.com>
2026-01-06 13:15:40 +0300

    Updated on 2026-06-10

---
+++ domain/transaction/src/main/java/com/tangem/domain/transaction/usecase/gasless/CreateAndSendGaslessTransactionUseCase.kt
@@ -0,0 +1,11 @@
+package com.tangem.domain.transaction.usecase.gasless
+
+import com.tangem.blockchain.common.TransactionData
+import com.tangem.domain.transaction.models.TransactionFeeExtended
+
+class CreateAndSendGaslessTransactionUseCase {
+
+    operator fun invoke(transactionData: TransactionData, fee: TransactionFeeExtended) {
+        // Implementation goes here
+    }
+}
