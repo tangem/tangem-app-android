bd4a76717ab303d98a2bf8ea883bcdef334a4289
Tangem <git@tangem.com>
2026-01-07 15:45:42 +0300

    Updated on 2026-09-01

---
+++ features/send-v2/impl/src/main/java/com/tangem/features/send/v2/feeselector/model/transformers/FeeSelectorTokenSelectedTransformer.kt
@@ -0,0 +1,26 @@
+package com.tangem.features.send.v2.feeselector.model.transformers
+
+import com.tangem.domain.models.currency.CryptoCurrencyStatus
+import com.tangem.features.send.v2.api.entity.FeeItem
+import com.tangem.features.send.v2.api.entity.FeeSelectorUM
+import com.tangem.utils.transformer.Transformer
+import kotlinx.collections.immutable.persistentListOf
+
+class FeeSelectorTokenSelectedTransformer(
+    private val selectedToken: CryptoCurrencyStatus,
+) : Transformer<FeeSelectorUM> {
+
+    override fun transform(prevState: FeeSelectorUM): FeeSelectorUM {
+        return if (prevState is FeeSelectorUM.Content) {
+            prevState.copy(
+                selectedFeeItem = FeeItem.Loading,
+                feeItems = persistentListOf(FeeItem.Loading),
+                feeExtraInfo = prevState.feeExtraInfo.copy(
+                    feeCryptoCurrencyStatus = selectedToken,
+                ),
+            )
+        } else {
+            prevState
+        }
+    }
+}
