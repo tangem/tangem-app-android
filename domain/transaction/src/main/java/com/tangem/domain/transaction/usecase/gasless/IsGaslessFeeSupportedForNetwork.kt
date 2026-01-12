bd4b7d6b946dd78f4634077795fcd98763905cdc
Tangem <git@tangem.com>
2026-01-12 10:20:23 +0300

    Updated on 2026-03-27

--- domain/transaction/src/main/java/com/tangem/domain/transaction/usecase/gasless/IsGaslessFeeSupportedForNetwork.kt
+++ domain/transaction/src/main/java/com/tangem/domain/transaction/usecase/gasless/IsGaslessFeeSupportedForNetwork.kt
@@ -1,16 +1,16 @@
 package com.tangem.domain.transaction.usecase.gasless
 
 import com.tangem.domain.models.network.Network
-import com.tangem.domain.transaction.GaslessTransactionRepository
+import com.tangem.domain.tokens.repository.CurrencyChecksRepository
 
 /**
  * Use case to check if gasless fee is supported for a given network.
  */
 class IsGaslessFeeSupportedForNetwork(
-    private val gaslessTransactionRepository: GaslessTransactionRepository,
+    private val currencyChecksRepository: CurrencyChecksRepository,
 ) {
 
     operator fun invoke(network: Network): Boolean {
-        return gaslessTransactionRepository.isNetworkSupported(network)
+        return currencyChecksRepository.isNetworkSupportedForGaslessTx(network)
     }
 }
