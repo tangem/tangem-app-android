28d5213f963ffbda4d9b56e3d22fe6a675c3b28e
Tangem <git@tangem.com>
2026-01-13 07:31:15 +0300

    Updated on 2026-06-10

--- app/src/main/java/com/tangem/tap/common/analytics/handlers/appsflyer/AppsFlyerAnalyticsHandler.kt
+++ app/src/main/java/com/tangem/tap/common/analytics/handlers/appsflyer/AppsFlyerAnalyticsHandler.kt
@@ -6,19 +6,26 @@ import com.tangem.core.analytics.models.AnalyticsEvent
 import com.tangem.core.analytics.models.AppsFlyerIncludedEvent
 import com.tangem.core.analytics.models.AppsFlyerOnlyEvent
 import com.tangem.tap.common.analytics.api.AnalyticsHandlerBuilder
+import timber.log.Timber
 
 class AppsFlyerAnalyticsHandler(
     private val client: AppsFlyerAnalyticsClient,
 ) : AnalyticsHandler, AnalyticsUserIdHandler {
+
+    init {
+        Timber.tag("AppsFlyer").i("AppsFlyer Analytics Handler created")
+    }
 
     override fun id(): String = ID
 
     override fun send(event: AnalyticsEvent) {
         when (event) {
             is AppsFlyerOnlyEvent -> {
+                Timber.tag("AppsFlyer").i("Sending event to AppsFlyer: ${event.id} with params: ${event.params}")
                 client.logEvent(event.id, event.params)
             }
             is AppsFlyerIncludedEvent -> {
+                Timber.tag("AppsFlyer").i("Sending event to AppsFlyer: ${event.id} with params: ${event.params}")
                 client.logEvent(
                     event = AnalyticsEvent(category = event.category, event = event.appsFlyerReplacedEvent).id,
                     params = event.params,
@@ -40,10 +47,16 @@ class AppsFlyerAnalyticsHandler(
     }
 
     class Builder : AnalyticsHandlerBuilder {
+        init {
+            Timber.tag("AppsFlyer").i("AppsFlyer Analytics Handler Builder created")
+        }
+
         override fun build(data: AnalyticsHandlerBuilder.Data): AnalyticsHandler = AppsFlyerAnalyticsHandler(
             client = if (data.logConfig.isAppsflyerLogEnabled) {
+                Timber.tag("AppsFlyer").i("AppsFlyer log enabled, mock client created")
                 AppsFlyerLogClient(data.jsonConverter)
             } else {
+                Timber.tag("AppsFlyer").i("AppsFlyer log disabled, real client created")
                 AppsFlyerClient(data.application, data.config.appsFlyerApiKey)
             },
         )
