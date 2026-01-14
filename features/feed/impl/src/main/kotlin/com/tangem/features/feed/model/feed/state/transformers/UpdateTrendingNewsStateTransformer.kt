8f213e6694df84bfccf989a22d1631332b52b522
Tangem <git@tangem.com>
2026-01-14 13:29:32 +0100

    Updated on 2026-07-07

--- features/feed/impl/src/main/kotlin/com/tangem/features/feed/model/feed/state/transformers/UpdateTrendingNewsStateTransformer.kt
+++ features/feed/impl/src/main/kotlin/com/tangem/features/feed/model/feed/state/transformers/UpdateTrendingNewsStateTransformer.kt
@@ -1,8 +1,11 @@
-package com.tangem.features.feed.model.feed.state
+package com.tangem.features.feed.model.feed.state.transformers
 
+import com.tangem.core.analytics.api.AnalyticsEventHandler
+import com.tangem.domain.models.news.NewsError
 import com.tangem.domain.models.news.ShortArticle
 import com.tangem.domain.models.news.TrendingNews
 import com.tangem.features.feed.model.converter.ShortArticleToArticleConfigUMConverter
+import com.tangem.features.feed.model.feed.analytics.FeedAnalyticsEvent
 import com.tangem.features.feed.ui.feed.state.FeedListUM
 import com.tangem.features.feed.ui.feed.state.NewsUM
 import com.tangem.features.feed.ui.feed.state.NewsUMState
@@ -10,21 +13,32 @@ import com.tangem.utils.Provider
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.toPersistentList
 
-internal class TrendingNewsStateFactory(
-    private val currentStateProvider: Provider<FeedListUM>,
-    private val onStateUpdate: (FeedListUM) -> Unit,
+internal class UpdateTrendingNewsStateTransformer(
+    private val result: TrendingNews,
     private val onRetryClicked: () -> Unit,
-) {
+    private val analyticsEventHandler: AnalyticsEventHandler,
+) : FeedListUMTransformer {
 
-    fun updateTrendingNewsState(result: TrendingNews) {
-        val currentState = currentStateProvider()
-        when (result) {
-            is TrendingNews.Data -> handleDataState(currentState, result.articles)
-            is TrendingNews.Error -> handleErrorState(currentState)
+    override fun transform(prevState: FeedListUM): FeedListUM {
+        return when (result) {
+            is TrendingNews.Data -> handleDataState(prevState, result.articles)
+            is TrendingNews.Error -> {
+                val (code, message) = when (val error = result.error) {
+                    is NewsError.HttpError -> error.code to error.message
+                    NewsError.NotHttpError -> null to ""
+                }
+                analyticsEventHandler.send(
+                    FeedAnalyticsEvent.NewsLoadError(
+                        code = code,
+                        message = message,
+                    ),
+                )
+                handleErrorState(prevState)
+            }
         }
     }
 
-    private fun handleDataState(currentState: FeedListUM, articles: List<ShortArticle>) {
+    private fun handleDataState(currentState: FeedListUM, articles: List<ShortArticle>): FeedListUM {
         val (trendingArticle, commonArticles) = separateTrendingAndCommonArticles(articles)
         val commonArticlesUM = getShortArticleConfigConverter(isTrending = false)
             .convert(commonArticles)
@@ -40,26 +54,22 @@ internal class TrendingNewsStateFactory(
             )
         }
 
-        onStateUpdate(
-            currentState.copy(
-                trendingArticle = trendingArticle?.let { article ->
-                    getShortArticleConfigConverter(isTrending = true)
-                        .convert(listOf(article))
-                }?.firstOrNull(),
-                news = updatedNews,
-            ),
+        return currentState.copy(
+            trendingArticle = trendingArticle?.let { article ->
+                getShortArticleConfigConverter(isTrending = true)
+                    .convert(listOf(article))
+            }?.firstOrNull(),
+            news = updatedNews,
         )
     }
 
-    private fun handleErrorState(currentState: FeedListUM) {
-        onStateUpdate(
-            currentState.copy(
-                trendingArticle = null,
-                news = NewsUM(
-                    content = persistentListOf(),
-                    onRetryClicked = onRetryClicked,
-                    newsUMState = NewsUMState.ERROR,
-                ),
+    private fun handleErrorState(currentState: FeedListUM): FeedListUM {
+        return currentState.copy(
+            trendingArticle = null,
+            news = NewsUM(
+                content = persistentListOf(),
+                onRetryClicked = onRetryClicked,
+                newsUMState = NewsUMState.ERROR,
             ),
         )
     }
