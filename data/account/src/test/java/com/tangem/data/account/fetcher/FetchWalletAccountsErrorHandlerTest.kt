2f5045d7fa01d347e94823e2f4dcaa8f265335f2
Tangem <git@tangem.com>
2026-01-15 11:56:24 +0300

    Updated on 2026-09-21

--- data/account/src/test/java/com/tangem/data/account/fetcher/FetchWalletAccountsErrorHandlerTest.kt
+++ data/account/src/test/java/com/tangem/data/account/fetcher/FetchWalletAccountsErrorHandlerTest.kt
@@ -4,23 +4,22 @@ import com.tangem.data.account.converter.createGetWalletAccountsResponse
 import com.tangem.data.account.converter.createWalletAccountDTO
 import com.tangem.data.account.utils.DefaultWalletAccountsResponseFactory
 import com.tangem.data.common.currency.UserTokensSaver
+import com.tangem.data.common.wallet.WalletServerBinder
 import com.tangem.datasource.api.common.response.ApiResponse
 import com.tangem.datasource.api.common.response.ApiResponseError
 import com.tangem.datasource.api.common.response.ApiResponseError.HttpException.Code
 import com.tangem.datasource.api.common.response.ETAG_HEADER
 import com.tangem.datasource.api.tangemTech.TangemTechApi
 import com.tangem.datasource.api.tangemTech.models.UserTokensResponse
-import com.tangem.datasource.api.tangemTech.models.WalletIdBody
-import com.tangem.datasource.api.tangemTech.models.WalletType
 import com.tangem.datasource.api.tangemTech.models.account.GetWalletAccountsResponse
 import com.tangem.datasource.api.tangemTech.models.account.WalletAccountDTO
 import com.tangem.datasource.api.tangemTech.models.account.toUserTokensResponse
 import com.tangem.datasource.local.token.UserTokensResponseStore
-import com.tangem.datasource.local.userwallet.UserWalletsStore
-import com.tangem.domain.models.wallet.UserWallet
 import com.tangem.domain.models.wallet.UserWalletId
-import com.tangem.utils.coroutines.TestingCoroutineDispatcherProvider
-import io.mockk.*
+import io.mockk.clearMocks
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -33,18 +32,16 @@ import org.junit.jupiter.api.TestInstance
 class FetchWalletAccountsErrorHandlerTest {
 
     private val tangemTechApi: TangemTechApi = mockk()
-    private val userWalletsStore: UserWalletsStore = mockk()
+    private val walletServerBinder: WalletServerBinder = mockk()
     private val userTokensSaver: UserTokensSaver = mockk(relaxUnitFun = true)
     private val userTokensResponseStore: UserTokensResponseStore = mockk(relaxUnitFun = true)
     private val defaultWalletAccountsResponseFactory: DefaultWalletAccountsResponseFactory = mockk()
 
     private val handler = FetchWalletAccountsErrorHandler(
-        tangemTechApi = tangemTechApi,
-        userWalletsStore = userWalletsStore,
+        walletServerBinder = walletServerBinder,
         userTokensSaver = userTokensSaver,
         userTokensResponseStore = userTokensResponseStore,
         defaultWalletAccountsResponseFactory = defaultWalletAccountsResponseFactory,
-        dispatchers = TestingCoroutineDispatcherProvider(),
     )
 
     private val pushWalletAccounts: suspend (List<WalletAccountDTO>, String) -> GetWalletAccountsResponse =
@@ -55,7 +52,7 @@ class FetchWalletAccountsErrorHandlerTest {
     fun setupEach() {
         clearMocks(
             tangemTechApi,
-            userWalletsStore,
+            walletServerBinder,
             userTokensSaver,
             userTokensResponseStore,
             defaultWalletAccountsResponseFactory,
@@ -112,22 +109,7 @@ class FetchWalletAccountsErrorHandlerTest {
             code = Code.CREATED,
         )
 
-        val walletName = "Wallet"
-        val userWallet = mockk<UserWallet.Cold> {
-            every { this@mockk.walletId } returns userWalletId
-            every { this@mockk.name } returns walletName
-        }
-        coEvery { userWalletsStore.getSyncOrNull(userWalletId) } returns userWallet
-
-        coEvery {
-            tangemTechApi.createWallet(
-                WalletIdBody(
-                    walletId = userWalletId.stringValue,
-                    name = walletName,
-                    walletType = WalletType.COLD,
-                ),
-            )
-        } returns apiResponse
+        coEvery { walletServerBinder.bind(userWalletId) } returns apiResponse
         coEvery { pushWalletAccounts(listOf(accountDTO), eTagValue) } returns savedAccountsResponse
 
         // Act
@@ -142,13 +124,7 @@ class FetchWalletAccountsErrorHandlerTest {
         // Assert
         coVerify {
             userTokensSaver.pushWithRetryer(userWalletId, response = savedAccountsResponse.toUserTokensResponse())
-            tangemTechApi.createWallet(
-                WalletIdBody(
-                    walletId = userWalletId.stringValue,
-                    name = walletName,
-                    walletType = WalletType.COLD,
-                ),
-            )
+            walletServerBinder.bind(userWalletId)
             pushWalletAccounts(listOf(accountDTO), eTagValue)
             storeWalletAccounts(userWalletId, savedAccountsResponse)
         }
