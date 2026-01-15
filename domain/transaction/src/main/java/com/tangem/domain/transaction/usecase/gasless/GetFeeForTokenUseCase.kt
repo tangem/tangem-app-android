a94e7173526b98d3e392a57801d08d9a1e312145
Tangem <git@tangem.com>
2026-01-15 20:15:37 +0300

    Updated on 2026-03-25

--- domain/transaction/src/main/java/com/tangem/domain/transaction/usecase/gasless/GetFeeForTokenUseCase.kt
+++ domain/transaction/src/main/java/com/tangem/domain/transaction/usecase/gasless/GetFeeForTokenUseCase.kt
@@ -2,6 +2,7 @@ package com.tangem.domain.transaction.usecase.gasless
 
 import arrow.core.Either
 import arrow.core.raise.Raise
+import arrow.core.raise.catch
 import arrow.core.raise.either
 import com.tangem.blockchain.blockchains.ethereum.EthereumWalletManager
 import com.tangem.blockchain.common.TransactionData
@@ -15,6 +16,7 @@ import com.tangem.domain.tokens.repository.CurrenciesRepository
 import com.tangem.domain.tokens.repository.CurrencyChecksRepository
 import com.tangem.domain.transaction.GaslessTransactionRepository
 import com.tangem.domain.transaction.error.GetFeeError
+import com.tangem.domain.transaction.error.GetFeeError.GaslessError
 import com.tangem.domain.transaction.models.TransactionFeeExtended
 import com.tangem.domain.transaction.raiseIllegalStateError
 import com.tangem.domain.walletmanager.WalletManagersFacade
@@ -40,50 +42,57 @@ class GetFeeForTokenUseCase(
         transactionData: TransactionData,
     ): Either<GetFeeError, TransactionFeeExtended> {
         return either {
-            if (!currencyChecksRepository.isNetworkSupportedForGaslessTx(token.network)) {
-                raise(GetFeeError.GaslessError.NetworkIsNotSupported)
-            }
+            catch(
+                block = {
+                    if (!currencyChecksRepository.isNetworkSupportedForGaslessTx(token.network)) {
+                        raise(GaslessError.NetworkIsNotSupported)
+                    }
 
-            val walletManager = prepareWalletManager(userWallet, token.network)
+                    val walletManager = prepareWalletManager(userWallet, token.network)
 
-            val initialTxFee = tokenFeeCalculator.calculateInitialFee(
-                userWallet = userWallet,
-                network = token.network,
-                walletManager = walletManager,
-                transactionData = transactionData,
-            ).bind()
+                    val initialTxFee = tokenFeeCalculator.calculateInitialFee(
+                        userWallet = userWallet,
+                        network = token.network,
+                        walletManager = walletManager,
+                        transactionData = transactionData,
+                    ).bind()
 
-            val initialFeeEth = initialTxFee.normal as? Fee.Ethereum
-                ?: raiseIllegalStateError(
-                    error = "only Fee.Ethereum supported, but was different",
-                )
+                    val initialFeeEth = initialTxFee.normal as? Fee.Ethereum
+                        ?: raiseIllegalStateError(
+                            error = "only Fee.Ethereum supported, but was different",
+                        )
 
-            val nativeCurrency = currenciesRepository.getNetworkCoin(
-                userWalletId = userWallet.walletId,
-                networkId = token.network.id,
-                derivationPath = token.network.derivationPath,
+                    val nativeCurrency = currenciesRepository.getNetworkCoin(
+                        userWalletId = userWallet.walletId,
+                        networkId = token.network.id,
+                        derivationPath = token.network.derivationPath,
+                    )
+
+                    val userCurrenciesStatusesByNetwork = getMultiCryptoCurrencyStatusUseCase.invokeMultiWalletSync(
+                        userWallet.walletId,
+                    ).getOrNull()?.filter {
+                        it.currency.network.id == token.network.id
+                    } ?: raiseIllegalStateError("currencies list is null for userWalletId=${userWallet.walletId}")
+
+                    val nativeCurrencyStatus = userCurrenciesStatusesByNetwork.find {
+                        it.currency.id == nativeCurrency.id
+                    } ?: raiseIllegalStateError("native currency not found for network ${token.network.id}")
+
+                    val tokenCurrencyStatus = userCurrenciesStatusesByNetwork.find {
+                        it.currency.id == token.id
+                    } ?: raiseIllegalStateError("token currency not found for network ${token.network.id}")
+
+                    tokenFeeCalculator.calculateTokenFee(
+                        walletManager = walletManager,
+                        tokenForPayFeeStatus = tokenCurrencyStatus,
+                        nativeCurrencyStatus = nativeCurrencyStatus,
+                        initialFee = initialFeeEth,
+                    ).bind()
+                },
+                catch = {
+                    raise(GaslessError.DataError(it))
+                },
             )
-
-            val userCurrenciesStatusesByNetwork = getMultiCryptoCurrencyStatusUseCase.invokeMultiWalletSync(
-                userWallet.walletId,
-            ).getOrNull()?.filter {
-                it.currency.network.id == token.network.id
-            } ?: raiseIllegalStateError("currencies list is null for userWalletId=${userWallet.walletId}")
-
-            val nativeCurrencyStatus = userCurrenciesStatusesByNetwork.find {
-                it.currency.id == nativeCurrency.id
-            } ?: raiseIllegalStateError("native currency not found for network ${token.network.id}")
-
-            val tokenCurrencyStatus = userCurrenciesStatusesByNetwork.find {
-                it.currency.id == token.id
-            } ?: raiseIllegalStateError("token currency not found for network ${token.network.id}")
-
-            tokenFeeCalculator.calculateTokenFee(
-                walletManager = walletManager,
-                tokenForPayFeeStatus = tokenCurrencyStatus,
-                nativeCurrencyStatus = nativeCurrencyStatus,
-                initialFee = initialFeeEth,
-            ).bind()
         }
     }
 
