3eccf7b4d9c929d3204af8eae88fa071654def2b
Tangem <git@tangem.com>
2026-01-16 13:36:43 +0300

    Updated on 2026-09-14

--- features/swap/impl/src/main/java/com/tangem/feature/swap/DefaultSwapComponent.kt
+++ features/swap/impl/src/main/java/com/tangem/feature/swap/DefaultSwapComponent.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import com.arkivanov.decompose.router.slot.SlotNavigation
 import com.arkivanov.decompose.router.slot.activate
@@ -17,15 +18,18 @@ import com.tangem.common.ui.swapStoriesScreen.SwapStoriesScreen
 import com.tangem.core.decompose.context.AppComponentContext
 import com.tangem.core.decompose.context.childByContext
 import com.tangem.core.decompose.model.getOrCreateModel
+import com.tangem.core.ui.decompose.ComposableBottomSheetComponent
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.domain.models.currency.CryptoCurrencyStatus
 import com.tangem.feature.swap.component.SwapFeeSelectorBlockComponent
 import com.tangem.feature.swap.model.SwapModel
+import com.tangem.feature.swap.models.AddToPortfolioRoute
 import com.tangem.feature.swap.models.SwapCardState.SwapCardData
 import com.tangem.feature.swap.router.SwapNavScreen
 import com.tangem.feature.swap.ui.SwapScreen
 import com.tangem.feature.swap.ui.SwapSelectTokenScreen
 import com.tangem.feature.swap.ui.SwapSuccessScreen
+import com.tangem.features.feed.components.market.details.portfolio.add.AddToPortfolioComponent
 import com.tangem.features.send.v2.api.SendFeatureToggles
 import com.tangem.features.send.v2.api.analytics.CommonSendAnalyticEvents
 import com.tangem.features.swap.SwapComponent
@@ -39,9 +43,18 @@ internal class DefaultSwapComponent @AssistedInject constructor(
     @Assisted private val params: SwapComponent.Params,
     private val swapFeeSelectorBlockComponentFactory: SwapFeeSelectorBlockComponent.Factory,
     private val sendFeatureToggles: SendFeatureToggles,
+    private val addToPortfolioComponentFactory: AddToPortfolioComponent.Factory,
 ) : SwapComponent, AppComponentContext by appComponentContext {
 
     private val model: SwapModel = getOrCreateModel(params)
+
+    private val bottomSheetSlot = childSlot(
+        source = model.bottomSheetNavigation,
+        serializer = AddToPortfolioRoute.serializer(),
+        key = BOTTOM_SHEET_SLOT_KEY,
+        handleBackButton = false,
+        childFactory = { configuration, context -> bottomSheetChild(context) },
+    )
 
     init {
         lifecycle.subscribe(
@@ -54,6 +67,7 @@ internal class DefaultSwapComponent @AssistedInject constructor(
     val childSlot = childSlot(
         source = slotNavigation,
         serializer = null,
+        key = FEE_SELECTOR_SLOT_KEY,
         childFactory = { config, context ->
             createSwapFeeSelectorBlockComponent(
                 context = childByContext(context),
@@ -114,6 +128,7 @@ internal class DefaultSwapComponent @AssistedInject constructor(
 
         val feeSelectorChildStackState by childSlot.subscribeAsState()
         val feeSelectorBlockComponent = feeSelectorChildStackState.child?.instance
+        val bottomSheet by bottomSheetSlot.subscribeAsState()
 
         Crossfade(
             modifier = Modifier.background(TangemTheme.colors.background.secondary),
@@ -165,10 +180,29 @@ internal class DefaultSwapComponent @AssistedInject constructor(
                 }
             }
         }
+
+        bottomSheet.child?.instance?.BottomSheet()
+    }
+
+    @Suppress("UnsafeCallOnNullableType")
+    private fun bottomSheetChild(componentContext: ComponentContext): ComposableBottomSheetComponent {
+        return addToPortfolioComponentFactory.create(
+            context = childByContext(componentContext),
+            params = AddToPortfolioComponent.Params(
+                addToPortfolioManager = model.addToPortfolioManager!!,
+                callback = model.addToPortfolioCallback,
+                shouldSkipTokenActionsScreen = true,
+            ),
+        )
     }
 
     @AssistedFactory
     interface Factory : SwapComponent.Factory {
         override fun create(context: AppComponentContext, params: SwapComponent.Params): DefaultSwapComponent
+    }
+
+    private companion object {
+        const val BOTTOM_SHEET_SLOT_KEY = "bottomSheetSlot"
+        const val FEE_SELECTOR_SLOT_KEY = "feeSelectorSlot"
     }
 }
