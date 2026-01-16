4ac9d2820d16f5e275970e9d9df321c6a12b0bac
Tangem <git@tangem.com>
2026-01-16 13:35:48 +0300

    Updated on 2026-04-06

--- features/feed/api/src/main/kotlin/com/tangem/features/feed/components/market/details/portfolio/add/AddToPortfolioComponent.kt
+++ features/feed/api/src/main/kotlin/com/tangem/features/feed/components/market/details/portfolio/add/AddToPortfolioComponent.kt
@@ -1,17 +1,20 @@
-package com.tangem.features.feed.components.market.details.portfolio.add.api
+package com.tangem.features.feed.components.market.details.portfolio.add
 
 import com.tangem.core.decompose.factory.ComponentFactory
 import com.tangem.core.ui.decompose.ComposableBottomSheetComponent
+import com.tangem.domain.models.currency.CryptoCurrency
 
-internal interface AddToPortfolioComponent : ComposableBottomSheetComponent {
+interface AddToPortfolioComponent : ComposableBottomSheetComponent {
 
     data class Params(
         val addToPortfolioManager: AddToPortfolioManager,
         val callback: Callback,
+        val shouldSkipTokenActionsScreen: Boolean = false,
     )
 
     interface Callback {
         fun onDismiss()
+        fun onSuccess(addedToken: CryptoCurrency)
     }
 
     interface Factory : ComponentFactory<Params, AddToPortfolioComponent>
