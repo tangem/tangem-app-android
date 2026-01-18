2673b87fb5ec75ce392cd9868144dced443efecc
Tangem <git@tangem.com>
2026-01-18 13:28:32 +0300

    Updated on 2026-06-05

--- features/send-v2/impl/src/main/java/com/tangem/features/send/v2/feeselector/component/token/entity/FeeTokenSelectorUM.kt
+++ features/send-v2/impl/src/main/java/com/tangem/features/send/v2/feeselector/component/token/entity/FeeTokenSelectorUM.kt
@@ -1,11 +1,10 @@
 package com.tangem.features.send.v2.feeselector.component.token.entity
 
-import com.tangem.core.ui.components.token.state.TokenItemState
 import com.tangem.features.send.v2.api.entity.FeeSelectorUM
 import kotlinx.collections.immutable.ImmutableList
 
-data class FeeTokenSelectorUM(
+internal data class FeeTokenSelectorUM(
     val parent: FeeSelectorUM.Content,
-    val selectedToken: TokenItemState,
-    val tokens: ImmutableList<TokenItemState>,
+    val selectedToken: FeeTokenItemState,
+    val tokens: ImmutableList<FeeTokenItemState>,
 )
