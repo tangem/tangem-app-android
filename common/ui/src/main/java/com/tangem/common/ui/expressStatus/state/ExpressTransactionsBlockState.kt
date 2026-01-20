7ef6cbafee99ab50186daa1c406800646f48e545
Tangem <git@tangem.com>
2026-01-20 14:14:10 +0400

    Updated on 2026-09-25

---
+++ common/ui/src/main/java/com/tangem/common/ui/expressStatus/state/ExpressTransactionsBlockState.kt
@@ -0,0 +1,22 @@
+package com.tangem.common.ui.expressStatus.state
+
+import androidx.compose.runtime.Composable
+import com.tangem.common.ui.tokendetails.TokenDetailsDialogConfig
+import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfig
+import kotlinx.collections.immutable.PersistentList
+
+data class ExpressTransactionsBlockState(
+    val transactions: PersistentList<ExpressTransactionStateUM>,
+    val bottomSheetSlot: BottomSheetSlot?,
+    val dialogSlot: DialogSlot?,
+)
+
+data class BottomSheetSlot(
+    val config: TangemBottomSheetConfig,
+    val content: @Composable () -> Unit,
+)
+
+data class DialogSlot(
+    val config: TokenDetailsDialogConfig,
+    val content: @Composable () -> Unit,
+)
