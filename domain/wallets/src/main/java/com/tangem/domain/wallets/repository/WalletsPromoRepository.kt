af0a34de06b58d3f9ec00b444a903af0cd260cf9
Tangem <git@tangem.com>
2026-01-21 14:21:10 +0400

    Updated on 2026-09-01

--- domain/wallets/src/main/java/com/tangem/domain/wallets/repository/WalletsPromoRepository.kt
+++ domain/wallets/src/main/java/com/tangem/domain/wallets/repository/WalletsPromoRepository.kt
@@ -1,15 +1,10 @@
 package com.tangem.domain.wallets.repository
 
-import arrow.core.Option
 import com.tangem.domain.wallets.models.AppsFlyerConversionData
 
 interface WalletsPromoRepository {
 
-    suspend fun getConversionData(): Option<AppsFlyerConversionData>
+    suspend fun bindRefcodeWithWallets(conversionData: AppsFlyerConversionData)
 
-    suspend fun saveConversionData(refcode: String, campaign: String?)
-
-    suspend fun bindRefcodeWithWallets(refcode: String, campaign: String?)
-
-    suspend fun bindSavedRefcodeWithWallets(): AppsFlyerConversionData
+    suspend fun retryBindRefcodeWithWallets()
 }
