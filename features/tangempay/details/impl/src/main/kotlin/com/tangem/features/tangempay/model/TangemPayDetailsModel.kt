bacf35c87267b5a617828c526d5d0902bdb9d326
Tangem <git@tangem.com>
2026-01-21 17:31:34 +0400

    Updated on 2026-09-21

--- features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/model/TangemPayDetailsModel.kt
+++ features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/model/TangemPayDetailsModel.kt
@@ -21,6 +21,7 @@ import com.tangem.domain.feedback.SendFeedbackEmailUseCase
 import com.tangem.domain.feedback.models.FeedbackEmailType
 import com.tangem.domain.feedback.models.WalletMetaInfo
 import com.tangem.domain.models.TokenReceiveConfig
+import com.tangem.domain.models.currency.CryptoCurrency
 import com.tangem.domain.pay.TangemPayCryptoCurrencyFactory
 import com.tangem.domain.pay.model.TangemPayCardBalance
 import com.tangem.domain.pay.model.TangemPayTopUpData
@@ -258,18 +259,9 @@ internal class TangemPayDetailsModel @Inject constructor(
                             .getOrNull()
                     }
                     if (currency != null) {
-                        router.push(
-                            AppRoute.Swap(
-                                currencyFrom = currency,
-                                userWalletId = params.userWalletId,
-                                isInitialReverseOrder = false,
-                                screenSource = AnalyticsParam.ScreensSources.TangemPay.value,
-                                tangemPayInput = AppRoute.Swap.TangemPayInput(
-                                    cryptoAmount = currentBalance.availableForWithdrawal,
-                                    fiatAmount = currentBalance.availableForWithdrawal,
-                                    depositAddress = depositAddress,
-                                    isWithdrawal = true,
-                                ),
+                        uiMessageSender.send(
+                            message = TangemPayMessagesFactory.createWithdrawWarning(
+                                onGotItClick = { onConfirmWithdrawal(currency, currentBalance, depositAddress) },
                             ),
                         )
                     } else {
@@ -278,6 +270,27 @@ internal class TangemPayDetailsModel @Inject constructor(
                 }
             }
         }
+    }
+
+    private fun onConfirmWithdrawal(
+        currency: CryptoCurrency,
+        currentBalance: TangemPayCardBalance,
+        depositAddress: String,
+    ) {
+        router.push(
+            AppRoute.Swap(
+                currencyFrom = currency,
+                userWalletId = params.userWalletId,
+                isInitialReverseOrder = false,
+                screenSource = AnalyticsParam.ScreensSources.TangemPay.value,
+                tangemPayInput = AppRoute.Swap.TangemPayInput(
+                    cryptoAmount = currentBalance.availableForWithdrawal,
+                    fiatAmount = currentBalance.availableForWithdrawal,
+                    depositAddress = depositAddress,
+                    isWithdrawal = true,
+                ),
+            ),
+        )
     }
 
     private fun fetchBalance(): Job {
