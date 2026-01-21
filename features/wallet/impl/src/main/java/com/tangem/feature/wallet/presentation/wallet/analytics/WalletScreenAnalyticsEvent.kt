10489d5f003d3f77821a18003f9abe89dde2909b
Tangem <git@tangem.com>
2026-01-21 17:12:52 +0300

    Updated on 2026-09-14

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/analytics/WalletScreenAnalyticsEvent.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/analytics/WalletScreenAnalyticsEvent.kt
@@ -2,7 +2,7 @@ package com.tangem.feature.wallet.presentation.wallet.analytics
 
 import com.tangem.core.analytics.models.AnalyticsEvent
 import com.tangem.core.analytics.models.AnalyticsParam
-import com.tangem.core.analytics.models.AppsFlyerOnlyEvent
+import com.tangem.core.analytics.models.AppsFlyerIncludedEvent
 import com.tangem.core.analytics.models.OneTimeAnalyticsEvent
 import com.tangem.domain.models.wallet.UserWalletId
 
@@ -18,13 +18,8 @@ sealed class WalletScreenAnalyticsEvent {
                 event = "Topped up",
                 params = mapOf(AnalyticsParam.CURRENCY to walletType.value),
             ),
-            OneTimeAnalyticsEvent {
+            OneTimeAnalyticsEvent, AppsFlyerIncludedEvent {
 
-            override val oneTimeEventId: String = id + userWalletId.stringValue
-        }
-
-        class AppsFlyerWalletFunded(userWalletId: UserWalletId) : Basic(event = "wallet_funded"),
-            AppsFlyerOnlyEvent, OneTimeAnalyticsEvent {
             override val oneTimeEventId: String = id + userWalletId.stringValue
         }
 
