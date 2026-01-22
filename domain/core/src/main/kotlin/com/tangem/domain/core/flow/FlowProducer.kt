019ba3bccc9e4aa7bd5920c6b69b11dc464bb965
Tangem <git@tangem.com>
2026-01-22 12:28:56 +0700

    Updated on 2026-02-06

--- domain/core/src/main/kotlin/com/tangem/domain/core/flow/FlowProducer.kt
+++ domain/core/src/main/kotlin/com/tangem/domain/core/flow/FlowProducer.kt
@@ -1,8 +1,11 @@
 package com.tangem.domain.core.flow
 
 import arrow.core.Option
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.retryWhen
 
 /**
@@ -12,7 +15,7 @@ import kotlinx.coroutines.flow.retryWhen
  *
 [REDACTED_AUTHOR]
  */
-interface FlowProducer<Data : Any> {
+interface FlowProducer<Data> {
 
     /** Fallback value if [Flow] throws exception */
     val fallback: Option<Data>
@@ -23,12 +26,16 @@ interface FlowProducer<Data : Any> {
     /** Produce [Flow] with retry mechanism */
     fun produceWithFallback(): Flow<Data> {
         return produce().retryWhen { _, _ ->
-            fallback.onSome { emit(value = it) }
-
-            delay(timeMillis = 2000)
-
-            true
+            retryWhen(this)
         }
+    }
+
+    suspend fun retryWhen(collector: FlowCollector<Data>): Boolean {
+        fallback.onSome { collector.emit(value = it) }
+
+        delay(timeMillis = 2000)
+
+        return true
     }
 
     /** Factory for creating [Producer]. It helps to provide [Params] by constructor */
@@ -36,5 +43,22 @@ interface FlowProducer<Data : Any> {
 
         /** Create [Producer] using [params] */
         fun create(params: Params): Producer
+    }
+}
+
+interface FlowProducerScope : CoroutineScope
+
+interface FlowProducerTools {
+
+    fun <T> shareInProducer(flow: Flow<T>, flowProducer: FlowProducer<T>, withRetryWhen: Boolean = true): SharedFlow<T>
+
+    companion object {
+
+        fun <T> Flow<T>.shareInProducer(
+            producerTools: FlowProducerTools,
+            flowProducer: FlowProducer<T>,
+            withRetryWhen: Boolean = true,
+        ): SharedFlow<T> = producerTools
+            .shareInProducer(this, flowProducer, withRetryWhen)
     }
 }
