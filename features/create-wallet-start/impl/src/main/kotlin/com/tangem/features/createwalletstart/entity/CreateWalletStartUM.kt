b14490173750eb82d9cb8d231c6ce17f6c24ec15
Tangem <git@tangem.com>
2026-01-24 15:09:31 +0300

    Updated on 2026-04-06

--- features/create-wallet-start/impl/src/main/kotlin/com/tangem/features/createwalletstart/entity/CreateWalletStartUM.kt
+++ features/create-wallet-start/impl/src/main/kotlin/com/tangem/features/createwalletstart/entity/CreateWalletStartUM.kt
@@ -17,6 +17,7 @@ internal data class CreateWalletStartUM(
     val otherMethodClick: () -> Unit,
     val onScanClick: () -> Unit,
     val onBackClick: () -> Unit,
+    val isHotWalletOptionVisible: Boolean = true,
 ) {
     data class FeatureItem(
         val iconResId: Int,
