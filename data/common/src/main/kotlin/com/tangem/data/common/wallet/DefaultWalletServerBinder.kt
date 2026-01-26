f0957eafa013aacb710424c8fed343131e512b5f
Tangem <git@tangem.com>
2026-01-26 20:34:55 +0400

    Updated on 2026-04-08

--- data/common/src/main/kotlin/com/tangem/data/common/wallet/DefaultWalletServerBinder.kt
+++ data/common/src/main/kotlin/com/tangem/data/common/wallet/DefaultWalletServerBinder.kt
@@ -3,7 +3,7 @@ package com.tangem.data.common.wallet
 import com.tangem.datasource.api.common.response.ApiResponse
 import com.tangem.datasource.api.tangemTech.TangemTechApi
 import com.tangem.datasource.api.tangemTech.converters.WalletIdBodyConverter
-import com.tangem.datasource.local.appsflyer.AppsFlyerConversionStore
+import com.tangem.datasource.local.appsflyer.AppsFlyerStore
 import com.tangem.datasource.local.userwallet.UserWalletsStore
 import com.tangem.domain.models.wallet.UserWallet
 import com.tangem.domain.models.wallet.UserWalletId
@@ -12,7 +12,7 @@ import kotlinx.coroutines.withContext
 
 internal class DefaultWalletServerBinder(
     private val userWalletsStore: UserWalletsStore,
-    private val appsFlyerConversionStore: AppsFlyerConversionStore,
+    private val appsFlyerStore: AppsFlyerStore,
     private val tangemTechApi: TangemTechApi,
     private val dispatchers: CoroutineDispatcherProvider,
 ) : WalletServerBinder {
@@ -24,7 +24,7 @@ internal class DefaultWalletServerBinder(
     }
 
     override suspend fun bind(userWallet: UserWallet): ApiResponse<Unit> {
-        val conversionData = appsFlyerConversionStore.get()
+        val conversionData = appsFlyerStore.get()
 
         return withContext(dispatchers.io) {
             tangemTechApi.createWallet(
