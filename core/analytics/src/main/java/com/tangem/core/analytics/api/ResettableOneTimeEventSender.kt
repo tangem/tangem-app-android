36e2606d6515294a590f4064ef48df4905c22e60
Tangem <git@tangem.com>
2026-01-27 15:55:41 +0300

    Updated on 2026-09-23

--- core/analytics/src/main/java/com/tangem/core/analytics/api/ResettableOneTimeEventSender.kt
+++ core/analytics/src/main/java/com/tangem/core/analytics/api/ResettableOneTimeEventSender.kt
@@ -3,14 +3,14 @@ package com.tangem.core.analytics.api
 import com.tangem.core.analytics.models.AnalyticsEvent
 import java.util.concurrent.ConcurrentHashMap
 
-class ResettableOneTimeEventSender(val analyticsEventHandler: AnalyticsEventHandler) {
+class ResettableOneTimeEventSender(private val analyticsEventHandler: AnalyticsEventHandler) {
 
     private val sentEvents = ConcurrentHashMap<String, AnalyticsEvent>()
 
     fun sendEventOnce(key: String, event: AnalyticsEvent) {
-        if (sentEvents[key] != null) return
-        analyticsEventHandler.send(event)
-        sentEvents[key] = event
+        if (sentEvents.putIfAbsent(key, event) == null) {
+            analyticsEventHandler.send(event)
+        }
     }
 
     fun reset(key: String) {
