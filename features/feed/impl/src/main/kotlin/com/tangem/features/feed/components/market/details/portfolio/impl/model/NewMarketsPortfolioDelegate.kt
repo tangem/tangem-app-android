e93b25cb6c822ef0fe4da6b227b217494f0b6d9a
Tangem <git@tangem.com>
2026-01-28 16:07:23 +0700

    Updated on 2026-09-25

--- features/feed/impl/src/main/kotlin/com/tangem/features/feed/components/market/details/portfolio/impl/model/NewMarketsPortfolioDelegate.kt
+++ features/feed/impl/src/main/kotlin/com/tangem/features/feed/components/market/details/portfolio/impl/model/NewMarketsPortfolioDelegate.kt
@@ -1,5 +1,6 @@
 package com.tangem.features.feed.components.market.details.portfolio.impl.model
 
+import com.tangem.blockchainsdk.compatibility.getTokenIdIfL2Network
 import com.tangem.common.ui.account.AccountTitleUM
 import com.tangem.common.ui.account.toUM
 import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfig
@@ -185,7 +186,10 @@ internal class NewMarketsPortfolioDelegate @AssistedInject constructor(
     private fun AccountStatusList.filterByRawID(): List<AccountWithAdded> {
         fun AccountStatus.filterByRawID(): List<CryptoCurrencyStatus> = when (this) {
             is AccountStatus.CryptoPortfolio -> this.tokenList.flattenCurrencies()
-                .filter { status -> status.currency.id.rawCurrencyId == currencyRawId }
+                .filter { status ->
+                    val currencyId = status.currency.id.rawCurrencyId ?: return@filter false
+                    getTokenIdIfL2Network(currencyId.value) == currencyRawId.value
+                }
         }
         return accountStatuses.map { accountStatus ->
             AccountWithAdded(
