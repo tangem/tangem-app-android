22e1760999a9fc2085eea7bee40bbe35b4088921
Tangem <git@tangem.com>
2026-01-30 19:33:43 +0400

    Updated on 2026-09-01

--- features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/DefaultHotWalletFeatureToggles.kt
+++ features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/DefaultHotWalletFeatureToggles.kt
@@ -5,11 +5,7 @@ import com.tangem.core.configtoggle.feature.FeatureTogglesManager
 internal class DefaultHotWalletFeatureToggles(
     private val featureTogglesManager: FeatureTogglesManager,
 ) : HotWalletFeatureToggles {
-    override val isHotWalletEnabled: Boolean
-        get() = featureTogglesManager.isFeatureEnabled(name = "HOT_WALLET_ENABLED")
+
     override val isWalletCreationRestrictionEnabled: Boolean
-        get() = isHotWalletEnabled &&
-            featureTogglesManager.isFeatureEnabled(name = "HOT_WALLET_CREATION_RESTRICTION_ENABLED")
-    override val isHotWalletVisible: Boolean
-        get() = featureTogglesManager.isFeatureEnabled(name = "HOT_WALLET_VISIBLE")
+        get() = featureTogglesManager.isFeatureEnabled(name = "HOT_WALLET_CREATION_RESTRICTION_ENABLED")
 }
