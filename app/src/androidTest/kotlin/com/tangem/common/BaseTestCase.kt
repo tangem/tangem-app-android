d60ef78eb4342562015aac6be67ad828e75e0dd6
Tangem <git@tangem.com>
2026-02-02 15:15:31 +0300

    Updated on 2026-09-21

--- app/src/androidTest/kotlin/com/tangem/common/BaseTestCase.kt
+++ app/src/androidTest/kotlin/com/tangem/common/BaseTestCase.kt
@@ -147,7 +147,9 @@ abstract class BaseTestCase : TestCase(
                 "NEW_ONRAMP_MAIN_ENABLED" to true,
                 "HOT_WALLET_ENABLED" to true,
                 "YIELD_SUPPLY_FEATURE_ENABLED" to true,
-                "ACCOUNTS_FEATURE_ENABLED" to true
+                "ACCOUNTS_FEATURE_ENABLED" to true,
+                "FEED_ENABLED" to true,
+                "GASLESS_TRANSACTIONS_ENABLED" to true,
             )
         )
     }
