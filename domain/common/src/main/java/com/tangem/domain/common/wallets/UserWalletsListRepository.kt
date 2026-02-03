ab4f6437e856fd5a647a77ae505b26cc7b24d739
Tangem <git@tangem.com>
2026-02-03 17:41:42 +0400

    Updated on 2026-07-01

--- domain/common/src/main/java/com/tangem/domain/common/wallets/UserWalletsListRepository.kt
+++ domain/common/src/main/java/com/tangem/domain/common/wallets/UserWalletsListRepository.kt
@@ -5,6 +5,7 @@ import com.tangem.domain.common.wallets.error.*
 import com.tangem.domain.models.scan.ScanResponse
 import com.tangem.domain.models.wallet.UserWallet
 import com.tangem.domain.models.wallet.UserWalletId
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
 
 /**
@@ -30,11 +31,20 @@ interface UserWalletsListRepository {
      */
     val selectedUserWallet: StateFlow<UserWallet?>
 
+    /** Get user wallet by [id] */
+    fun getSyncOrNull(id: UserWalletId): UserWallet?
+
+    /** Get user wallet by [id] */
+    fun getSyncStrict(id: UserWalletId): UserWallet
+
     /**
      * Loads user wallets list and selected wallet.
      * If the list is already loaded, it does nothing.
      */
     suspend fun load()
+
+    /** Loads user wallets list and selected wallet and returns a flow of the list */
+    fun loadAndGet(): Flow<List<UserWallet>>
 
     /**
      * Gets and if necessary loads user wallets list and selected wallet.
