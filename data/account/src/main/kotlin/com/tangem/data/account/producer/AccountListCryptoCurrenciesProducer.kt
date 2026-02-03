79cd57da1356e7589c24ead035ce616674c37733
Tangem <git@tangem.com>
2026-02-03 17:41:42 +0400

    Updated on 2026-09-23

--- data/account/src/main/kotlin/com/tangem/data/account/producer/AccountListCryptoCurrenciesProducer.kt
+++ data/account/src/main/kotlin/com/tangem/data/account/producer/AccountListCryptoCurrenciesProducer.kt
@@ -4,7 +4,7 @@ import arrow.core.Option
 import arrow.core.some
 import com.tangem.data.account.store.AccountsResponseStoreFactory
 import com.tangem.data.common.currency.ResponseCryptoCurrenciesFactory
-import com.tangem.datasource.local.userwallet.UserWalletsStore
+import com.tangem.domain.common.wallets.UserWalletsListRepository
 import com.tangem.domain.core.flow.FlowProducerTools
 import com.tangem.domain.models.account.DerivationIndex
 import com.tangem.domain.models.currency.CryptoCurrency
@@ -20,7 +20,7 @@ import kotlinx.coroutines.flow.*
  * Implementation of [MultiWalletCryptoCurrenciesProducer] that produces crypto currencies of all accounts
  *
  * @property params                          params
- * @property userWalletsStore                UserWallet's store
+ * @property userWalletsListRepository       repository for getting user wallets
  * @property accountsResponseStoreFactory    factory to create store with accounts response
  * @property responseCryptoCurrenciesFactory factory for creating [CryptoCurrency] from `UserTokensResponse`
  * @property dispatchers                     dispatchers
@@ -29,7 +29,7 @@ import kotlinx.coroutines.flow.*
  */
 internal class AccountListCryptoCurrenciesProducer @AssistedInject constructor(
     @Assisted val params: MultiWalletCryptoCurrenciesProducer.Params,
-    private val userWalletsStore: UserWalletsStore,
+    private val userWalletsListRepository: UserWalletsListRepository,
     private val accountsResponseStoreFactory: AccountsResponseStoreFactory,
     private val responseCryptoCurrenciesFactory: ResponseCryptoCurrenciesFactory,
     override val flowProducerTools: FlowProducerTools,
@@ -40,7 +40,7 @@ internal class AccountListCryptoCurrenciesProducer @AssistedInject constructor(
 
     @Suppress("NullableToStringCall")
     override fun produce(): Flow<Set<CryptoCurrency>> {
-        val userWallet = userWalletsStore.getSyncStrict(key = params.userWalletId)
+        val userWallet = userWalletsListRepository.getSyncStrict(id = params.userWalletId)
 
         if (!userWallet.isMultiCurrency) {
             error("${this::class.simpleName ?: this::class.toString()} supports only multi-currency wallet")
