a9f68d755594b254b25babf31be4008dd153a364
Tangem <git@tangem.com>
2026-02-03 11:52:19 +0100

    Updated on 2026-07-23

---
+++ data/earn/src/main/java/com/tangem/data/earn/repository/DefaultEarnRepository.kt
@@ -0,0 +1,157 @@
+package com.tangem.data.earn.repository
+
+import arrow.core.Either
+import com.tangem.blockchain.common.Blockchain
+import com.tangem.blockchainsdk.utils.fromNetworkId
+import com.tangem.data.common.currency.CryptoCurrencyFactory
+import com.tangem.data.earn.converter.EarnTokenConverter
+import com.tangem.data.earn.datastore.EarnNetworksStore
+import com.tangem.data.earn.datastore.EarnTopTokensStore
+import com.tangem.data.earn.repository.batch.EarnTokensBatchFetcher
+import com.tangem.datasource.api.common.response.getOrThrow
+import com.tangem.datasource.api.tangemTech.TangemTechApi
+import com.tangem.datasource.api.tangemTech.models.EarnResponse
+import com.tangem.domain.common.wallets.UserWalletsListRepository
+import com.tangem.domain.earn.EarnErrorResolver
+import com.tangem.domain.earn.model.EarnTokensBatchFlow
+import com.tangem.domain.earn.model.EarnTokensBatchingContext
+import com.tangem.domain.earn.repository.EarnRepository
+import com.tangem.domain.models.account.DerivationIndex
+import com.tangem.domain.models.currency.CryptoCurrency
+import com.tangem.domain.models.earn.EarnNetwork
+import com.tangem.domain.models.earn.EarnNetworks
+import com.tangem.domain.models.earn.EarnTokenWithCurrency
+import com.tangem.domain.models.earn.EarnTopToken
+import com.tangem.domain.models.wallet.UserWallet
+import com.tangem.domain.models.wallet.isLocked
+import com.tangem.pagination.BatchListSource
+import com.tangem.pagination.toBatchFlow
+import com.tangem.utils.coroutines.CoroutineDispatcherProvider
+import com.tangem.utils.coroutines.runCatching
+import kotlinx.coroutines.flow.Flow
+
+@Suppress("LongParameterList")
+internal class DefaultEarnRepository(
+    private val tangemTechApi: TangemTechApi,
+    private val dispatchers: CoroutineDispatcherProvider,
+    private val userWalletsListRepository: UserWalletsListRepository,
+    private val cryptoCurrencyFactory: CryptoCurrencyFactory,
+    private val earnNetworksStore: EarnNetworksStore,
+    private val earnTopTokensStore: EarnTopTokensStore,
+    private val earnErrorResolver: EarnErrorResolver,
+) : EarnRepository {
+
+    override fun getEarnTokensBatchFlow(context: EarnTokensBatchingContext, batchSize: Int): EarnTokensBatchFlow {
+        val batchFetcher = EarnTokensBatchFetcher(
+            tangemTechApi = tangemTechApi,
+            batchSize = batchSize,
+            userWalletsListRepository = userWalletsListRepository,
+            cryptoCurrencyFactory = cryptoCurrencyFactory,
+        )
+
+        return BatchListSource(
+            fetchDispatcher = dispatchers.io,
+            context = context,
+            generateNewKey = { keys -> keys.lastOrNull()?.inc() ?: INITIAL_BATCH_KEY },
+            batchFetcher = batchFetcher,
+        ).toBatchFlow()
+    }
+
+    override suspend fun fetchEarnNetworks(type: String) {
+        runCatching(dispatchers.io) {
+            val response = tangemTechApi.getEarnNetworks(type = type).getOrThrow()
+            val items = response.items.map { dto ->
+                EarnNetwork(
+                    networkId = dto.networkId,
+                    isAdded = false,
+                )
+            }
+            earnNetworksStore.store(Either.Right(items))
+        }.onFailure { error ->
+            val earnError = earnErrorResolver.resolve(error)
+            earnNetworksStore.store(Either.Left(earnError))
+        }
+    }
+
+    override fun observeEarnNetworks(): Flow<EarnNetworks> {
+        return earnNetworksStore.get()
+    }
+
+    override suspend fun fetchTopEarnTokens(limit: Int) {
+        runCatching(dispatchers.io) {
+            val response = tangemTechApi.getEarnTokens(
+                isForEarn = true,
+                limit = limit,
+            ).getOrThrow()
+
+            val userWallet = userWalletsListRepository
+                .selectedUserWallet
+                .value
+                .takeIf { wallet -> wallet?.isLocked?.not() == true }
+                ?: return@runCatching
+
+            val items = response.items.mapNotNull { dto ->
+                val earnToken = EarnTokenConverter.convert(dto)
+                createCryptoCurrencyForEarnToken(
+                    cryptoCurrencyFactory = cryptoCurrencyFactory,
+                    userWallet = userWallet,
+                    earnToken = dto,
+                )?.let { cryptoCurrency ->
+                    EarnTokenWithCurrency(
+                        earnToken = earnToken,
+                        cryptoCurrency = cryptoCurrency,
+                    )
+                }
+            }
+            earnTopTokensStore.store(Either.Right(items))
+        }.onFailure { error ->
+            val earnError = earnErrorResolver.resolve(error)
+            earnTopTokensStore.store(Either.Left(earnError))
+        }
+    }
+
+    override fun observeTopEarnTokens(): Flow<EarnTopToken> {
+        return earnTopTokensStore.get()
+    }
+
+    companion object {
+        internal const val FIRST_PAGE = 1
+        private const val INITIAL_BATCH_KEY = 0
+    }
+}
+
+/*
+use this CryptoCurrency only for creating cryptoCurrencyIcon!! because accountIndex = DerivationIndex.Main and it
+doesn't provide real all data.
+ */
+internal fun createCryptoCurrencyForEarnToken(
+    userWallet: UserWallet,
+    earnToken: EarnResponse,
+    cryptoCurrencyFactory: CryptoCurrencyFactory,
+): CryptoCurrency? {
+    val blockchain = Blockchain.fromNetworkId(earnToken.networkId) ?: Blockchain.Unknown
+    return if (earnToken.token.address.isEmpty()) {
+        cryptoCurrencyFactory.createCoin(
+            blockchain = blockchain,
+            extraDerivationPath = null,
+            userWallet = userWallet,
+            accountIndex = DerivationIndex.Main,
+        )
+    } else {
+        val network = cryptoCurrencyFactory.networkFactory.create(
+            blockchain = blockchain,
+            extraDerivationPath = null,
+            userWallet = userWallet,
+            accountIndex = DerivationIndex.Main,
+        ) ?: return null
+
+        cryptoCurrencyFactory.createToken(
+            network = network,
+            rawId = CryptoCurrency.RawID(earnToken.token.id),
+            name = earnToken.token.name,
+            symbol = earnToken.token.symbol,
+            decimals = blockchain.decimals(),
+            contractAddress = earnToken.token.address,
+        )
+    }
+}
---
+++ domain/models/src/main/kotlin/com/tangem/domain/models/earn/EarnTokenWithCurrency.kt
@@ -0,0 +1,16 @@
+package com.tangem.domain.models.earn
+
+import com.tangem.domain.models.currency.CryptoCurrency
+import kotlinx.serialization.Serializable
+
+/**
+ * Represents an earn token and its associated cryptocurrency. This model is only for main account.
+ *
+ * @property earnToken The earn token details.
+ * @property cryptoCurrency Use this [CryptoCurrency] only for creating the cryptoCurrencyIcon!!!!
+ */
+@Serializable
+data class EarnTokenWithCurrency(
+    val earnToken: EarnToken,
+    val cryptoCurrency: CryptoCurrency,
+)
