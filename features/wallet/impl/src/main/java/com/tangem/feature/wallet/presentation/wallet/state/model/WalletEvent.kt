f7a444a299c5e2a6abaeaef9d794a2e25812e1f0
Tangem <git@tangem.com>
2026-02-03 18:37:15 +0500

    Updated on 2026-09-14

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/model/WalletEvent.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/model/WalletEvent.kt
@@ -13,11 +13,7 @@ internal sealed class WalletEvent {
 
     data class ShowError(val text: TextReference) : WalletEvent()
 
-    data class ShowAlert(val state: WalletAlertState) : WalletEvent()
-
     data object CopyAddress : WalletEvent()
-
-    data class RateApp(val onDismissClick: () -> Unit) : WalletEvent()
 
     data class DemonstrateWalletsScrollPreview(val direction: Direction) : WalletEvent() {
 
