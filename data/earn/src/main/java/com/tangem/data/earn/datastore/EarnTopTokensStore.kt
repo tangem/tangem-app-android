51c434c6e5fd77a381a326013d8477f9842ef849
Tangem <git@tangem.com>
2026-02-05 15:13:10 +0100

    Updated on 2026-06-16

--- data/earn/src/main/java/com/tangem/data/earn/datastore/EarnTopTokensStore.kt
+++ data/earn/src/main/java/com/tangem/data/earn/datastore/EarnTopTokensStore.kt
@@ -1,6 +1,5 @@
 package com.tangem.data.earn.datastore
 
-import arrow.core.Either
 import com.tangem.datasource.local.datastore.RuntimeStateStore
 import com.tangem.domain.models.earn.EarnTopToken
 import javax.inject.Inject
@@ -8,6 +7,6 @@ import javax.inject.Singleton
 
 @Singleton
 class EarnTopTokensStore @Inject constructor() :
-    RuntimeStateStore<EarnTopToken> by RuntimeStateStore(
-        defaultValue = Either.Right(emptyList()),
+    RuntimeStateStore<EarnTopToken?> by RuntimeStateStore(
+        defaultValue = null,
     )
