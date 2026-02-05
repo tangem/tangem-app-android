ecad8c516ac7a21532f31c24e5562ee83e31325c
Tangem <git@tangem.com>
2026-02-05 16:51:25 +0500

    Updated on 2026-07-23

--- core/ui/src/main/java/com/tangem/core/ui/ds/badge/TangemBadgeUM.kt
+++ core/ui/src/main/java/com/tangem/core/ui/ds/badge/TangemBadgeUM.kt
@@ -8,13 +8,13 @@ import com.tangem.core.ui.extensions.TextReference
  * UI model for [TangemBadge] component
  *
  * @param text          TextReference for the badge label.
- * @param modifier      Modifier to be applied to the badge.
  * @param iconRes       Drawable resource ID for the icon to be displayed in the badge.
  * @param size          [TangemBadgeSize] defining the size of the badge.
  * @param shape         [TangemBadgeShape] defining the shape of the badge.
  * @param color         [TangemBadgeColor] defining the color scheme of the badge.
  * @param type          [TangemBadgeType] defining the style of the badge.
  * @param iconPosition  [TangemBadgeIconPosition] defining icon position of the badge.
+ * @param onClick       Lambda to be invoked when the badge is clicked (optional).
  */
 class TangemBadgeUM(
     val text: TextReference,
@@ -24,4 +24,5 @@ class TangemBadgeUM(
     val color: TangemBadgeColor = TangemBadgeColor.Gray,
     val type: TangemBadgeType = TangemBadgeType.Solid,
     val iconPosition: TangemBadgeIconPosition = TangemBadgeIconPosition.Start,
+    val onClick: (() -> Unit)? = null,
 )
