ad653fa7c690f0ab1e9722d331d447568316a926
Tangem <git@tangem.com>
2026-02-06 07:35:28 +0100

    Updated on 2026-09-14

--- data/earn/src/main/java/com/tangem/data/earn/converter/EarnTokenConverter.kt
+++ data/earn/src/main/java/com/tangem/data/earn/converter/EarnTokenConverter.kt
@@ -1,7 +1,9 @@
 package com.tangem.data.earn.converter
 
 import com.tangem.datasource.api.tangemTech.models.EarnResponse
+import com.tangem.domain.models.earn.EarnRewardType
 import com.tangem.domain.models.earn.EarnToken
+import com.tangem.domain.models.earn.EarnType
 import com.tangem.utils.converter.Converter
 
 internal object EarnTokenConverter : Converter<EarnResponse, EarnToken> {
@@ -10,12 +12,23 @@ internal object EarnTokenConverter : Converter<EarnResponse, EarnToken> {
         return EarnToken(
             apy = value.apy,
             networkId = value.networkId,
-            rewardType = value.rewardType,
-            type = value.type,
+            rewardType = convertEarnRewardType(value.rewardType),
+            type = convertEarnType(value.type),
             tokenId = value.token.id,
             tokenSymbol = value.token.symbol,
             tokenName = value.token.name,
             tokenAddress = value.token.address,
+            decimalCount = value.token.decimalCount,
         )
+    }
+
+    private fun convertEarnRewardType(type: String): EarnRewardType {
+        return enumValues<EarnRewardType>().find { it.name.equals(type, ignoreCase = true) }
+            ?: EarnRewardType.APY
+    }
+
+    private fun convertEarnType(type: String): EarnType {
+        return enumValues<EarnType>().find { it.name.equals(type, ignoreCase = true) }
+            ?: EarnType.STAKING
     }
 }
