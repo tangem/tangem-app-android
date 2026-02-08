3958e7c53afa7e36d9b5ca03788a321b405e6e14
Tangem <git@tangem.com>
2026-02-08 13:33:25 +0400

    Updated on 2026-09-14

--- features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/factory/express/TokenDetailsOnrampStatusFactory.kt
+++ features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/factory/express/TokenDetailsOnrampStatusFactory.kt
@@ -2,7 +2,6 @@ package com.tangem.feature.tokendetails.presentation.tokendetails.state.factory.
 
 import com.tangem.common.ui.expressStatus.ExpressStatusBottomSheetConfig
 import com.tangem.common.ui.expressStatus.state.ExpressTransactionStateUM
-import com.tangem.common.ui.expressStatus.state.ExpressTransactionsBlockState
 import com.tangem.core.analytics.api.AnalyticsEventHandler
 import com.tangem.datasource.local.swap.ExpressAnalyticsStatus
 import com.tangem.domain.appcurrency.model.AppCurrency
@@ -17,6 +16,7 @@ import com.tangem.domain.onramp.model.OnrampStatus
 import com.tangem.domain.onramp.model.OnrampStatus.Status.*
 import com.tangem.domain.tokens.model.analytics.TokenOnrampAnalyticsEvent
 import com.tangem.feature.tokendetails.presentation.tokendetails.model.ExpressTransactionsClickIntents
+import com.tangem.feature.tokendetails.presentation.tokendetails.state.TokenDetailsState
 import com.tangem.feature.tokendetails.presentation.tokendetails.state.factory.TokenDetailsOnrampTransactionStateConverter
 import com.tangem.utils.Provider
 import dagger.assisted.Assisted
@@ -28,13 +28,13 @@ import kotlinx.coroutines.flow.map
 import timber.log.Timber
 
 @Suppress("LongParameterList")
-internal class OnrampStatusFactory @AssistedInject constructor(
+internal class TokenDetailsOnrampStatusFactory @AssistedInject constructor(
     private val getOnrampTransactionsUseCase: GetOnrampTransactionsUseCase,
     private val getOnrampStatusUseCase: GetOnrampStatusUseCase,
     private val onrampRemoveTransactionUseCase: OnrampRemoveTransactionUseCase,
     private val onrampUpdateTransactionStatusUseCase: OnrampUpdateTransactionStatusUseCase,
     private val analyticsEventHandler: AnalyticsEventHandler,
-    @Assisted private val currentStateProvider: Provider<ExpressTransactionsBlockState>,
+    @Assisted private val currentStateProvider: Provider<TokenDetailsState>,
     @Assisted private val cryptoCurrencyStatusProvider: Provider<CryptoCurrencyStatus?>,
     @Assisted private val appCurrencyProvider: Provider<AppCurrency>,
     @Assisted private val clickIntents: ExpressTransactionsClickIntents,
@@ -70,7 +70,7 @@ internal class OnrampStatusFactory @AssistedInject constructor(
 
     suspend fun removeTransactionOnBottomSheetClosed(isForceDispose: Boolean) {
         val state = currentStateProvider()
-        val bottomSheetConfig = state.bottomSheetSlot?.config?.content as? ExpressStatusBottomSheetConfig ?: return
+        val bottomSheetConfig = state.bottomSheetConfig?.content as? ExpressStatusBottomSheetConfig ?: return
         val selectedTx = bottomSheetConfig.value as? ExpressTransactionStateUM.OnrampUM ?: return
 
         if (selectedTx.activeStatus.isAutoDisposable || isForceDispose) {
@@ -83,8 +83,8 @@ internal class OnrampStatusFactory @AssistedInject constructor(
             onrampTx
         } else {
             getOnrampStatusUseCase(userWallet = userWallet, onrampTx.info.txId).fold(
-                ifLeft = {
-                    Timber.e("Couldn't update onramp status. $it")
+                ifLeft = { error ->
+                    Timber.e("Couldn't update onramp status. $error")
                     onrampTx
                 },
                 ifRight = { statusModel ->
@@ -154,12 +154,12 @@ internal class OnrampStatusFactory @AssistedInject constructor(
     @AssistedFactory
     interface Factory {
         fun create(
-            currentStateProvider: Provider<ExpressTransactionsBlockState>,
+            currentStateProvider: Provider<TokenDetailsState>,
             cryptoCurrencyStatusProvider: Provider<CryptoCurrencyStatus?>,
             appCurrencyProvider: Provider<AppCurrency>,
             clickIntents: ExpressTransactionsClickIntents,
             cryptoCurrency: CryptoCurrency,
             userWallet: UserWallet,
-        ): OnrampStatusFactory
+        ): TokenDetailsOnrampStatusFactory
     }
 }
