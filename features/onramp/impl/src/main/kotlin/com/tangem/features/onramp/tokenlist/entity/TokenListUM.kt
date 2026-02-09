8209a71207a7b2ad0699e63fb53622c906b54a1f
Tangem <git@tangem.com>
2026-02-09 17:12:27 +0300

    Updated on 2026-09-14

--- features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/tokenlist/entity/TokenListUM.kt
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/tokenlist/entity/TokenListUM.kt
@@ -3,6 +3,7 @@ package com.tangem.features.onramp.tokenlist.entity
 import com.tangem.common.ui.notifications.NotificationUM
 import com.tangem.core.ui.components.fields.entity.SearchBarUM
 import com.tangem.core.ui.components.tokenlist.state.TokensListItemUM
+import com.tangem.features.onramp.swap.availablepairs.market.state.SwapMarketState
 import kotlinx.collections.immutable.ImmutableList
 
 /**
@@ -12,6 +13,7 @@ import kotlinx.collections.immutable.ImmutableList
  * @property availableItems   available items (search bar, header, tokens)
  * @property unavailableItems unavailable items (header, tokens)
  * @property isBalanceHidden  flag that indicates if balance should be hidden
+ * @property marketsState     markets list state (null when markets should not be shown)
  *
 [REDACTED_AUTHOR]
  */
@@ -22,16 +24,24 @@ internal data class TokenListUM(
     val tokensListData: TokenListUMData,
     val isBalanceHidden: Boolean,
     val warning: NotificationUM? = null,
+    val marketsState: SwapMarketState? = null,
 )
 
 internal sealed interface TokenListUMData {
+
+    val totalTokensCount: Int
+
     data class AccountList(
         val tokensList: ImmutableList<TokensListItemUM.Portfolio>,
+        override val totalTokensCount: Int,
     ) : TokenListUMData
 
     data class TokenList(
         val tokensList: ImmutableList<TokensListItemUM>,
+        override val totalTokensCount: Int,
     ) : TokenListUMData
 
-    data object EmptyList : TokenListUMData
+    data object EmptyList : TokenListUMData {
+        override val totalTokensCount: Int = 0
+    }
 }
