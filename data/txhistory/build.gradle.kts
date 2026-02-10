dc9d6671e5fa8ac2ee82a72bbe43e4a9eee3079c
Tangem <git@tangem.com>
2026-02-10 13:29:55 +0400

    Updated on 2026-06-24

--- data/txhistory/build.gradle.kts
+++ data/txhistory/build.gradle.kts
@@ -15,14 +15,17 @@ dependencies {
     implementation(projects.core.utils)
     implementation(projects.core.datasource)
     implementation(projects.core.pagination)
+
     implementation(projects.domain.legacy)
+    implementation(projects.domain.common)
     implementation(projects.domain.walletManager)
-    implementation(projects.libs.blockchainSdk)
     implementation(projects.domain.models)
     implementation(projects.domain.tokens.models)
     implementation(projects.domain.txhistory)
     implementation(projects.domain.txhistory.models)
     implementation(projects.domain.wallets.models)
+
+    implementation(projects.libs.blockchainSdk)
 
     implementation(deps.kotlin.coroutines)
     implementation(deps.androidx.paging.runtime)
