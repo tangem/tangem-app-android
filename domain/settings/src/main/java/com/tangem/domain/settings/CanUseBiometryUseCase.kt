ba714161407dffac2c45c835d1345b6362f7c060
Tangem <git@tangem.com>
2026-02-10 04:50:44 +0300

    Updated on 2026-05-14

--- domain/settings/src/main/java/com/tangem/domain/settings/CanUseBiometryUseCase.kt
+++ domain/settings/src/main/java/com/tangem/domain/settings/CanUseBiometryUseCase.kt
@@ -4,5 +4,8 @@ import com.tangem.domain.settings.repositories.LegacySettingsRepository
 
 class CanUseBiometryUseCase(private val legacySettingsRepository: LegacySettingsRepository) {
 
+    @Deprecated("You probably want to use strict() instead. Check implementation", ReplaceWith("strict()"))
     suspend operator fun invoke(): Boolean = legacySettingsRepository.canUseBiometry()
+
+    suspend fun strict(): Boolean = legacySettingsRepository.canUseBiometryStrict()
 }
