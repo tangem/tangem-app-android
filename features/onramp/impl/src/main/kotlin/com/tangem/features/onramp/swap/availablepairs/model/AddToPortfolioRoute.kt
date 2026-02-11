1ae3439d7d27f905ad7403da2432980c0ce205de
Tangem <git@tangem.com>
2026-02-11 14:07:02 +0300

    Updated on 2026-09-14

---
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/swap/availablepairs/model/AddToPortfolioRoute.kt
@@ -0,0 +1,7 @@
+package com.tangem.features.onramp.swap.availablepairs.model
+
+import com.tangem.core.decompose.navigation.Route
+import kotlinx.serialization.Serializable
+
+@Serializable
+internal data object AddToPortfolioRoute : Route
