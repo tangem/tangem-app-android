e1aa166ab0755ca2b811058158166c8ec47d722d
Tangem <git@tangem.com>
2026-02-12 08:05:42 +0100

    Updated on 2026-09-01

--- features/feed/impl/src/main/kotlin/com/tangem/features/feed/ui/earn/state/EarnFilterNetworkUM.kt
+++ features/feed/impl/src/main/kotlin/com/tangem/features/feed/ui/earn/state/EarnFilterNetworkUM.kt
@@ -1,29 +1,25 @@
 package com.tangem.features.feed.ui.earn.state
 
 import androidx.compose.runtime.Immutable
-import com.tangem.core.ui.extensions.TextReference
 
 @Immutable
 internal sealed class EarnFilterNetworkUM {
 
-    abstract val text: TextReference
     abstract val isSelected: Boolean
 
     data class AllNetworks(
-        override val text: TextReference,
         override val isSelected: Boolean,
     ) : EarnFilterNetworkUM()
 
     data class MyNetworks(
-        override val text: TextReference,
         override val isSelected: Boolean,
     ) : EarnFilterNetworkUM()
 
     data class Network(
-        val id: String,
-        override val text: TextReference,
-        val symbol: TextReference,
-        val iconRes: Int,
         override val isSelected: Boolean,
+        val id: String,
+        val text: String,
+        val symbol: String,
+        val iconRes: Int,
     ) : EarnFilterNetworkUM()
 }
