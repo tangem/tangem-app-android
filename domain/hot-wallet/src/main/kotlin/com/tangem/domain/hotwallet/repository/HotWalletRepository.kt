625752ca1d5b4005372fce20a2bf8af611e399e7
Tangem <git@tangem.com>
2026-02-12 16:24:37 +0500

    Updated on 2026-07-23

--- domain/hot-wallet/src/main/kotlin/com/tangem/domain/hotwallet/repository/HotWalletRepository.kt
+++ domain/hot-wallet/src/main/kotlin/com/tangem/domain/hotwallet/repository/HotWalletRepository.kt
@@ -12,4 +12,24 @@ interface HotWalletRepository {
     fun accessCodeSkipped(userWalletId: UserWalletId): Flow<Boolean>
 
     suspend fun setAccessCodeSkipped(userWalletId: UserWalletId, skipped: Boolean)
+
+    fun shouldShowUpgradeBanner(userWalletId: UserWalletId): Flow<Boolean>
+
+    suspend fun setShouldShowUpgradeBanner(userWalletId: UserWalletId, shouldShow: Boolean)
+
+    fun shouldShowNextTimeUpgradeBanner(userWalletId: UserWalletId): Flow<Boolean>
+
+    suspend fun setShouldShowNextTimeUpgradeBanner(userWalletId: UserWalletId, shouldShow: Boolean)
+
+    suspend fun getUpgradeBannerClosureTimestamp(userWalletId: UserWalletId): Long?
+
+    suspend fun setUpgradeBannerClosureTimestamp(userWalletId: UserWalletId, timestamp: Long)
+
+    suspend fun getWalletCreationTimestamp(userWalletId: UserWalletId): Long?
+
+    suspend fun setWalletCreationTimestamp(userWalletId: UserWalletId, timestamp: Long)
+
+    suspend fun hasHadFirstTopUp(userWalletId: UserWalletId): Boolean
+
+    suspend fun setHasHadFirstTopUp(userWalletId: UserWalletId, hasTopUp: Boolean)
 }
