483947308bed061aeae11f1cc9df03a40a766789
Tangem <git@tangem.com>
2026-02-12 11:50:44 +0400

    Updated on 2026-07-07

--- domain/account/src/main/java/com/tangem/domain/account/usecase/IsAccountsModeEnabledUseCase.kt
+++ domain/account/src/main/java/com/tangem/domain/account/usecase/IsAccountsModeEnabledUseCase.kt
@@ -5,6 +5,7 @@ import arrow.core.getOrElse
 import com.tangem.domain.account.featuretoggle.AccountsFeatureToggles
 import com.tangem.domain.account.repository.AccountsCRUDRepository
 import com.tangem.domain.common.wallets.UserWalletsListRepository
+import com.tangem.domain.common.wallets.loadAndGet
 import com.tangem.domain.models.wallet.UserWallet
 import com.tangem.domain.models.wallet.isMultiCurrency
 import kotlinx.coroutines.ExperimentalCoroutinesApi
