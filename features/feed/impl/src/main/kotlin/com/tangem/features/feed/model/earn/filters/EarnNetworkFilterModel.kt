ac7d3f1607ad920425c5b3b4fa0ee2677c147928
Tangem <git@tangem.com>
2026-02-12 09:37:22 +0100

    Updated on 2026-08-14

--- features/feed/impl/src/main/kotlin/com/tangem/features/feed/model/earn/filters/EarnNetworkFilterModel.kt
+++ features/feed/impl/src/main/kotlin/com/tangem/features/feed/model/earn/filters/EarnNetworkFilterModel.kt
@@ -1,13 +1,17 @@
 package com.tangem.features.feed.model.earn.filters
 
 import androidx.compose.runtime.Stable
+import com.tangem.core.analytics.api.AnalyticsEventHandler
 import com.tangem.core.decompose.di.ModelScoped
 import com.tangem.core.decompose.model.Model
 import com.tangem.core.decompose.model.ParamsContainer
 import com.tangem.features.feed.components.earn.EarnNetworkFilterComponent
-import com.tangem.features.feed.model.earn.filters.state.EarnFilterNetworkUMConverter
+import com.tangem.features.feed.model.earn.analytics.EarnAnalyticsEvent
+import com.tangem.features.feed.model.earn.analytics.FilterNetworkAnalytic
 import com.tangem.features.feed.model.earn.filters.state.EarnFilterNetworkConverter
+import com.tangem.features.feed.model.earn.filters.state.EarnFilterNetworkUMConverter
 import com.tangem.features.feed.ui.earn.state.EarnFilterByNetworkBottomSheetContentUM
+import com.tangem.features.feed.ui.earn.state.EarnFilterNetworkUM
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import kotlinx.collections.immutable.toPersistentList
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -18,6 +22,7 @@ import javax.inject.Inject
 @ModelScoped
 internal class EarnNetworkFilterModel @Inject constructor(
     override val dispatchers: CoroutineDispatcherProvider,
+    private val analyticsEventHandler: AnalyticsEventHandler,
     paramsContainer: ParamsContainer,
 ) : Model() {
 
@@ -31,7 +36,22 @@ internal class EarnNetworkFilterModel @Inject constructor(
             networks = params.allFilters
                 .map { EarnFilterNetworkConverter().convert(it) }
                 .toPersistentList(),
-            onOptionClick = { filterUM -> params.onFilterSelected(EarnFilterNetworkUMConverter().convert(filterUM)) },
+            onOptionClick = ::handleOnOptionClick,
+        )
+    }
+
+    private fun handleOnOptionClick(filter: EarnFilterNetworkUM) {
+        params.onFilterSelected(EarnFilterNetworkUMConverter().convert(filter))
+        val (networkId, filterType) = when (filter) {
+            is EarnFilterNetworkUM.AllNetworks -> "" to FilterNetworkAnalytic.ALL_NETWORKS
+            is EarnFilterNetworkUM.MyNetworks -> "" to FilterNetworkAnalytic.MY_NETWORKS
+            is EarnFilterNetworkUM.Network -> filter.id to FilterNetworkAnalytic.SPECIFIC
+        }
+        analyticsEventHandler.send(
+            EarnAnalyticsEvent.BestOpportunitiesFilterNetworkApplied(
+                networkId = networkId,
+                filterType = filterType,
+            ),
         )
     }
 }
