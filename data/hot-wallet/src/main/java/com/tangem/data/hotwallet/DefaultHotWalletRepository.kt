d81aa0c1170b3d82caa617414673945216fa997c
Tangem <git@tangem.com>
2026-02-12 16:24:37 +0500

    Updated on 2026-09-25

--- data/hot-wallet/src/main/java/com/tangem/data/hotwallet/DefaultHotWalletRepository.kt
+++ data/hot-wallet/src/main/java/com/tangem/data/hotwallet/DefaultHotWalletRepository.kt
@@ -5,6 +5,7 @@ import androidx.annotation.ChecksSdkIntAtLeast
 import com.tangem.datasource.local.preferences.AppPreferencesStore
 import com.tangem.datasource.local.preferences.PreferencesKeys
 import com.tangem.datasource.local.preferences.utils.getObjectMap
+import com.tangem.datasource.local.preferences.utils.getObjectMapSync
 import com.tangem.domain.hotwallet.repository.HotWalletRepository
 import com.tangem.domain.models.wallet.UserWalletId
 import kotlinx.coroutines.flow.Flow
@@ -31,6 +32,81 @@ internal class DefaultHotWalletRepository(
                 key = PreferencesKeys.ACCESS_CODE_SKIPPED_STATES_KEY,
                 value = mutablePreferences.getObjectMap<Boolean>(PreferencesKeys.ACCESS_CODE_SKIPPED_STATES_KEY)
                     .plus(userWalletId.stringValue to skipped),
+            )
+        }
+    }
+
+    override fun shouldShowUpgradeBanner(userWalletId: UserWalletId): Flow<Boolean> = appPreferencesStore
+        .getObjectMap<Boolean>(PreferencesKeys.SHOULD_SHOW_UPGRADE_BANNER_KEY)
+        .map { it[userWalletId.stringValue] == true }
+
+    override suspend fun setShouldShowUpgradeBanner(userWalletId: UserWalletId, shouldShow: Boolean) {
+        appPreferencesStore.editData { mutablePreferences ->
+            mutablePreferences.setObjectMap(
+                key = PreferencesKeys.SHOULD_SHOW_UPGRADE_BANNER_KEY,
+                value = mutablePreferences.getObjectMap<Boolean>(PreferencesKeys.SHOULD_SHOW_UPGRADE_BANNER_KEY)
+                    .plus(userWalletId.stringValue to shouldShow),
+            )
+        }
+    }
+
+    override fun shouldShowNextTimeUpgradeBanner(userWalletId: UserWalletId): Flow<Boolean> = appPreferencesStore
+        .getObjectMap<Boolean>(PreferencesKeys.SHOULD_SHOW_NEXT_TIME_UPGRADE_BANNER_KEY)
+        .map { it[userWalletId.stringValue] == true }
+
+    override suspend fun setShouldShowNextTimeUpgradeBanner(userWalletId: UserWalletId, shouldShow: Boolean) {
+        appPreferencesStore.editData { mutablePreferences ->
+            mutablePreferences.setObjectMap(
+                key = PreferencesKeys.SHOULD_SHOW_NEXT_TIME_UPGRADE_BANNER_KEY,
+                value = mutablePreferences.getObjectMap<Boolean>(
+                    PreferencesKeys.SHOULD_SHOW_NEXT_TIME_UPGRADE_BANNER_KEY,
+                )
+                    .plus(userWalletId.stringValue to shouldShow),
+            )
+        }
+    }
+
+    override suspend fun getUpgradeBannerClosureTimestamp(userWalletId: UserWalletId): Long? {
+        return appPreferencesStore
+            .getObjectMapSync<Long>(PreferencesKeys.UPGRADE_BANNER_CLOSURE_TIMESTAMP_KEY)[userWalletId.stringValue]
+    }
+
+    override suspend fun setUpgradeBannerClosureTimestamp(userWalletId: UserWalletId, timestamp: Long) {
+        appPreferencesStore.editData { mutablePreferences ->
+            mutablePreferences.setObjectMap(
+                key = PreferencesKeys.UPGRADE_BANNER_CLOSURE_TIMESTAMP_KEY,
+                value = mutablePreferences.getObjectMap<Long>(PreferencesKeys.UPGRADE_BANNER_CLOSURE_TIMESTAMP_KEY)
+                    .plus(userWalletId.stringValue to timestamp),
+            )
+        }
+    }
+
+    override suspend fun getWalletCreationTimestamp(userWalletId: UserWalletId): Long? {
+        return appPreferencesStore
+            .getObjectMapSync<Long>(PreferencesKeys.WALLET_CREATION_TIMESTAMP_KEY)[userWalletId.stringValue]
+    }
+
+    override suspend fun setWalletCreationTimestamp(userWalletId: UserWalletId, timestamp: Long) {
+        appPreferencesStore.editData { mutablePreferences ->
+            mutablePreferences.setObjectMap(
+                key = PreferencesKeys.WALLET_CREATION_TIMESTAMP_KEY,
+                value = mutablePreferences.getObjectMap<Long>(PreferencesKeys.WALLET_CREATION_TIMESTAMP_KEY)
+                    .plus(userWalletId.stringValue to timestamp),
+            )
+        }
+    }
+
+    override suspend fun hasHadFirstTopUp(userWalletId: UserWalletId): Boolean {
+        return appPreferencesStore
+            .getObjectMapSync<Boolean>(PreferencesKeys.HAS_HAD_FIRST_TOP_UP_KEY)[userWalletId.stringValue] == true
+    }
+
+    override suspend fun setHasHadFirstTopUp(userWalletId: UserWalletId, hasTopUp: Boolean) {
+        appPreferencesStore.editData { mutablePreferences ->
+            mutablePreferences.setObjectMap(
+                key = PreferencesKeys.HAS_HAD_FIRST_TOP_UP_KEY,
+                value = mutablePreferences.getObjectMap<Boolean>(PreferencesKeys.HAS_HAD_FIRST_TOP_UP_KEY)
+                    .plus(userWalletId.stringValue to hasTopUp),
             )
         }
     }
