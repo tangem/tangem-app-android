a3d311aa3f75a6d88dec7982c649c25c2e1a02e9
Tangem <git@tangem.com>
2026-02-12 08:05:42 +0100

    Updated on 2026-06-24

--- features/feed/impl/src/main/kotlin/com/tangem/features/feed/model/earn/state/transformers/UpdateEarnUMInitialStateTransformer.kt
+++ features/feed/impl/src/main/kotlin/com/tangem/features/feed/model/earn/state/transformers/UpdateEarnUMInitialStateTransformer.kt
@@ -1,12 +1,9 @@
 package com.tangem.features.feed.model.earn.state.transformers
 
-import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfig
 import com.tangem.features.feed.ui.earn.state.EarnUM
 
 internal class UpdateEarnUMInitialStateTransformer(
     private val onBackClick: () -> Unit,
-    private val filterByTypeBottomSheetConfig: TangemBottomSheetConfig,
-    private val filterByNetworkBottomSheetConfig: TangemBottomSheetConfig,
     private val onNetworkFilterClick: () -> Unit,
     private val onTypeFilterClick: () -> Unit,
 ) : EarnUMTransformer {
@@ -14,8 +11,6 @@ internal class UpdateEarnUMInitialStateTransformer(
     override fun transform(prevState: EarnUM): EarnUM {
         return prevState.copy(
             onBackClick = onBackClick,
-            filterByNetworkBottomSheet = filterByNetworkBottomSheetConfig,
-            filterByTypeBottomSheet = filterByTypeBottomSheetConfig,
             onNetworkFilterClick = onNetworkFilterClick,
             onTypeFilterClick = onTypeFilterClick,
         )
