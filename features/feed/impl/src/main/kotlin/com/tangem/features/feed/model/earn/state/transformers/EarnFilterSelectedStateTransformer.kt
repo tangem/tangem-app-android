aff54c74e93c7353ed1079b86aa4609bef589de6
Tangem <git@tangem.com>
2026-02-13 09:54:54 +0100

    Updated on 2026-06-27

--- features/feed/impl/src/main/kotlin/com/tangem/features/feed/model/earn/state/transformers/EarnFilterSelectedStateTransformer.kt
+++ features/feed/impl/src/main/kotlin/com/tangem/features/feed/model/earn/state/transformers/EarnFilterSelectedStateTransformer.kt
@@ -1,18 +1,27 @@
 package com.tangem.features.feed.model.earn.state.transformers
 
+import com.tangem.domain.models.earn.EarnNetworks
+import com.tangem.features.feed.ui.earn.state.EarnBestOpportunitiesUM
 import com.tangem.features.feed.ui.earn.state.EarnFilterNetworkUM
 import com.tangem.features.feed.ui.earn.state.EarnFilterTypeUM
 import com.tangem.features.feed.ui.earn.state.EarnUM
 
 internal class EarnFilterSelectedStateTransformer(
+    private val earnNetworks: EarnNetworks,
     private val filterType: EarnFilterTypeUM,
     private val filterNetwork: EarnFilterNetworkUM,
 ) : EarnUMTransformer {
 
     override fun transform(prevState: EarnUM): EarnUM {
+        val isFiltersApplicable = prevState.bestOpportunities !is EarnBestOpportunitiesUM.Error
+        val isNetworkFilterEnabled = earnNetworks.isRight()
         return prevState.copy(
-            selectedTypeFilter = filterType,
-            selectedNetworkFilter = filterNetwork,
+            earnFilterUM = prevState.earnFilterUM.copy(
+                selectedTypeFilter = filterType,
+                selectedNetworkFilter = filterNetwork,
+                isNetworkFilterEnabled = isFiltersApplicable && isNetworkFilterEnabled,
+                isTypeFilterEnabled = isFiltersApplicable,
+            ),
         )
     }
 }
