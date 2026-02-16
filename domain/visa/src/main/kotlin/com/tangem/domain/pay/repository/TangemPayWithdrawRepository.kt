a9dd5f5a07f8b74529a16b52ba3b252df4f27c75
Tangem <git@tangem.com>
2026-02-16 19:18:15 +0400

    Updated on 2026-06-27

--- domain/visa/src/main/kotlin/com/tangem/domain/pay/repository/TangemPayWithdrawRepository.kt
+++ domain/visa/src/main/kotlin/com/tangem/domain/pay/repository/TangemPayWithdrawRepository.kt
@@ -4,15 +4,22 @@ import arrow.core.Either
 import com.tangem.core.error.UniversalError
 import com.tangem.domain.models.currency.CryptoCurrency
 import com.tangem.domain.models.wallet.UserWallet
+import com.tangem.domain.pay.TangemPayWithdrawExchangeState
 import com.tangem.domain.pay.WithdrawalResult
+import com.tangem.domain.visa.error.VisaApiError
 import java.math.BigDecimal
 
-interface TangemPaySwapRepository {
+interface TangemPayWithdrawRepository {
 
     suspend fun withdraw(
         userWallet: UserWallet,
         receiverAddress: String,
         cryptoAmount: BigDecimal,
         cryptoCurrencyId: CryptoCurrency.RawID,
+        exchangeData: TangemPayWithdrawExchangeState,
     ): Either<UniversalError, WithdrawalResult>
+
+    suspend fun hasWithdrawOrder(userWallet: UserWallet): Boolean
+
+    suspend fun pollWithdrawOrderIfNeeds(userWallet: UserWallet): Either<VisaApiError, Unit>
 }
