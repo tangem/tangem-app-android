fe61092a9a644b88c0f0b5a70d15b9bb26dfdc2b
Tangem <git@tangem.com>
2026-02-16 19:18:15 +0400

    Updated on 2026-06-16

---
+++ data/visa/src/main/kotlin/com/tangem/data/pay/entity/WithdrawStoreData.kt
@@ -0,0 +1,19 @@
+package com.tangem.data.pay.entity
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = false)
+data class WithdrawStoreData(
+    @Json(name = "orderId") val orderId: String,
+    @Json(name = "exchangeData") val exchangeData: ExchangeStoreData?,
+)
+
+@JsonClass(generateAdapter = false)
+data class ExchangeStoreData(
+    @Json(name = "txId") val txId: String,
+    @Json(name = "fromNetwork") val fromNetwork: String,
+    @Json(name = "fromAddress") val fromAddress: String,
+    @Json(name = "payInAddress") val payInAddress: String,
+    @Json(name = "payInExtraId") val payInExtraId: String?,
+)
