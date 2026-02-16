64b52f730c606ade52b75ce01cfcfd61bce72a7b
Tangem <git@tangem.com>
2026-02-16 19:18:15 +0400

    Updated on 2026-05-13

--- domain/visa/src/main/kotlin/com/tangem/domain/tangempay/TangemPayWithdrawUseCase.kt
+++ domain/visa/src/main/kotlin/com/tangem/domain/tangempay/TangemPayWithdrawUseCase.kt
@@ -4,6 +4,7 @@ import arrow.core.Either
 import com.tangem.core.error.UniversalError
 import com.tangem.domain.models.currency.CryptoCurrency
 import com.tangem.domain.models.wallet.UserWallet
+import com.tangem.domain.pay.TangemPayWithdrawExchangeState
 import com.tangem.domain.pay.WithdrawalResult
 import java.math.BigDecimal
 
@@ -14,5 +15,6 @@ interface TangemPayWithdrawUseCase {
         cryptoAmount: BigDecimal,
         cryptoCurrencyId: CryptoCurrency.RawID,
         receiverCexAddress: String,
+        exchangeData: TangemPayWithdrawExchangeState,
     ): Either<UniversalError, WithdrawalResult>
 }
