f410e22b61896ef8822563f1d97b9930183ddb2c
Tangem <git@tangem.com>
2026-02-17 14:25:13 +0400

    Updated on 2026-07-07

--- features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/main/entity/OnrampAmountBlockUM.kt
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/main/entity/OnrampAmountBlockUM.kt
@@ -1,17 +1,17 @@
-package com.tangem.features.onramp.mainv2.entity
+package com.tangem.features.onramp.main.entity
 
 import androidx.compose.runtime.Immutable
 import com.tangem.common.ui.amountScreen.models.AmountFieldModel
 import com.tangem.core.ui.extensions.TextReference
 import kotlinx.collections.immutable.ImmutableList
 
-internal data class OnrampNewAmountBlockUM(
-    val currencyUM: OnrampNewCurrencyUM,
+internal data class OnrampAmountBlockUM(
+    val currencyUM: OnrampCurrencyUM,
     val amountFieldModel: AmountFieldModel,
     val secondaryFieldModel: OnrampSecondaryFieldErrorUM,
 )
 
-internal data class OnrampNewCurrencyUM(
+internal data class OnrampCurrencyUM(
     val unit: String,
     val code: String,
     val iconUrl: String?,
@@ -25,9 +25,9 @@ internal sealed interface OnrampSecondaryFieldErrorUM {
     data class Error(val error: TextReference) : OnrampSecondaryFieldErrorUM
 }
 
-internal sealed interface OnrampV2AmountButtonUMState {
-    data class Loaded(val amountButtons: ImmutableList<OnrampAmountButtonUM>) : OnrampV2AmountButtonUMState
-    data object None : OnrampV2AmountButtonUMState
+internal sealed interface OnrampAmountButtonUMState {
+    data class Loaded(val amountButtons: ImmutableList<OnrampAmountButtonUM>) : OnrampAmountButtonUMState
+    data object None : OnrampAmountButtonUMState
 }
 
 internal data class OnrampAmountButtonUM(
