6da9f025f995574ac6745012eb29ff7e0fc7454c
Tangem <git@tangem.com>
2026-02-19 11:10:14 +0400

    Updated on 2026-09-14

--- domain/transaction/src/main/java/com/tangem/domain/transaction/WalletAddressServiceRepository.kt
+++ domain/transaction/src/main/java/com/tangem/domain/transaction/WalletAddressServiceRepository.kt
@@ -23,7 +23,7 @@ interface WalletAddressServiceRepository {
 
     suspend fun validateAddress(userWalletId: UserWalletId, network: Network, address: String): Boolean
 
-    fun validateMemo(network: Network, memo: String): Boolean
+    suspend fun validateMemo(userWalletId: UserWalletId, network: Network, memo: String): Boolean
 
     suspend fun parseSharedAddress(input: String, network: Network): ParsedQrCode
 }
