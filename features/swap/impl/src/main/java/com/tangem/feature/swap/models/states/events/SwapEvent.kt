a0e74b03bec55efd50e247f87c3022d0c53b37ca
Tangem <git@tangem.com>
2026-02-20 11:56:20 +0500

    Updated on 2026-03-27

--- features/swap/impl/src/main/java/com/tangem/feature/swap/models/states/events/SwapEvent.kt
+++
@@ -1,9 +0,0 @@
-package com.tangem.feature.swap.models.states.events
-
-import androidx.compose.runtime.Immutable
-import com.tangem.common.ui.alerts.models.AlertUM
-
-@Immutable
-internal sealed class SwapEvent {
-    data class ShowAlert(val alert: AlertUM) : SwapEvent()
-}
