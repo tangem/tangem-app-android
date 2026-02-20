ec81f82594448095797db0cd9b54b3fac8933682
Tangem <git@tangem.com>
2026-02-20 11:46:23 +0400

    Updated on 2026-09-01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -305,6 +305,8 @@ dependencies {
     implementation(projects.features.tokenRecieve.impl)
     implementation(projects.features.yieldSupply.api)
     implementation(projects.features.yieldSupply.impl)
+    implementation(projects.features.approval.api)
+    implementation(projects.features.approval.impl)
 
     /** AndroidX libraries */
     implementation(deps.androidx.core.ktx)
