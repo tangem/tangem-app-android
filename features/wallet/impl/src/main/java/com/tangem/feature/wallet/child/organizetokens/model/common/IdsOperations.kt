652ce51f0b2c0b34486ce1d4d1f50d9065c0efd4
Tangem <git@tangem.com>
2026-02-24 13:08:09 +0500

    Updated on 2026-09-21

---
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/child/organizetokens/model/common/IdsOperations.kt
@@ -0,0 +1,8 @@
+package com.tangem.feature.wallet.child.organizetokens.model.common
+
+import com.tangem.domain.models.currency.CryptoCurrency
+import com.tangem.domain.models.network.Network
+
+internal fun getTokenItemId(currencyId: CryptoCurrency.ID): String = currencyId.value
+
+internal fun getGroupHeaderId(network: Network): Int = network.hashCode()
