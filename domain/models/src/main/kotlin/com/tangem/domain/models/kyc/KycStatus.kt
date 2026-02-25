fd6e2faa269713fa01eb2dd2d513c4b6b9f1e4b5
Tangem <git@tangem.com>
2026-02-25 16:00:39 +0500

    Updated on 2026-06-16

--- domain/models/src/main/kotlin/com/tangem/domain/models/kyc/KycStatus.kt
+++ domain/models/src/main/kotlin/com/tangem/domain/models/kyc/KycStatus.kt
@@ -1,20 +1,28 @@
 package com.tangem.domain.models.kyc
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
 private const val APPROVED_KYC_STATUS = "approved"
 private const val IN_PROGRESS_KYC_STATUS = "in_progress"
 private const val DECLINED_KYC_STATUS = "declined"
 
+@JsonClass(generateAdapter = false)
 enum class KycStatus {
     /** Initial state */
+    @Json(name = "init")
     INIT,
 
     /** Performing the check */
+    @Json(name = "in_progress")
     PENDING,
 
     /** SumSub approved */
+    @Json(name = "approved")
     APPROVED,
 
     /** The check failed, documents rejected */
+    @Json(name = "declined")
     REJECTED,
 
     ;
