856d200c682ef2251b9f7ad5ccd8e845e85d791a
Tangem <git@tangem.com>
2026-02-25 19:06:24 +0400

    Updated on 2026-09-21

--- features/feed/impl/src/main/kotlin/com/tangem/features/feed/components/market/details/portfolio/impl/ui/state/MyPortfolioUM.kt
+++ features/feed/impl/src/main/kotlin/com/tangem/features/feed/components/market/details/portfolio/impl/ui/state/MyPortfolioUM.kt
@@ -1,16 +1,12 @@
 package com.tangem.features.feed.components.market.details.portfolio.impl.ui.state
 
 import androidx.compose.runtime.Immutable
-import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfig
 import kotlinx.collections.immutable.ImmutableList
 
 @Immutable
 internal sealed class MyPortfolioUM {
 
-    abstract val addToPortfolioBSConfig: TangemBottomSheetConfig?
-
     data class Tokens(
-        override val addToPortfolioBSConfig: TangemBottomSheetConfig,
         val tokens: ImmutableList<PortfolioTokenUM>,
         val buttonState: AddButtonState,
         val onAddClick: () -> Unit,
@@ -27,25 +23,15 @@ internal sealed class MyPortfolioUM {
         val items: ImmutableList<PortfolioListItem>,
         val buttonState: Tokens.AddButtonState,
         val onAddClick: () -> Unit,
-    ) : MyPortfolioUM() {
-
-        override val addToPortfolioBSConfig: TangemBottomSheetConfig = TangemBottomSheetConfig.Empty
-    }
+    ) : MyPortfolioUM()
 
     data class AddFirstToken(
-        override val addToPortfolioBSConfig: TangemBottomSheetConfig,
         val onAddClick: () -> Unit,
     ) : MyPortfolioUM()
 
-    data object Loading : MyPortfolioUM() {
-        override val addToPortfolioBSConfig: TangemBottomSheetConfig? = null
-    }
+    data object Loading : MyPortfolioUM()
 
-    data object Unavailable : MyPortfolioUM() {
-        override val addToPortfolioBSConfig: TangemBottomSheetConfig? = null
-    }
+    data object Unavailable : MyPortfolioUM()
 
-    data object UnavailableForWallet : MyPortfolioUM() {
-        override val addToPortfolioBSConfig: TangemBottomSheetConfig? = null
-    }
+    data object UnavailableForWallet : MyPortfolioUM()
 }
