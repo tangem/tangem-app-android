c6578aa65247deb25b36c437931a125fb10d325e
Tangem <git@tangem.com>
2026-02-26 14:32:14 +0400

    Updated on 2026-08-14

--- features/referral/domain/src/main/java/com/tangem/feature/referral/domain/ReferralInteractorImpl.kt
+++ features/referral/domain/src/main/java/com/tangem/feature/referral/domain/ReferralInteractorImpl.kt
@@ -1,30 +1,22 @@
 package com.tangem.feature.referral.domain
 
-import arrow.core.getOrElse
 import com.tangem.common.core.TangemSdkError
 import com.tangem.domain.account.producer.SingleAccountProducer
 import com.tangem.domain.account.status.usecase.ManageCryptoCurrenciesUseCase
 import com.tangem.domain.account.supplier.SingleAccountSupplier
-import com.tangem.domain.models.PortfolioId
 import com.tangem.domain.models.account.Account
+import com.tangem.domain.models.account.AccountId
 import com.tangem.domain.models.account.DerivationIndex
 import com.tangem.domain.models.currency.CryptoCurrency
 import com.tangem.domain.models.wallet.UserWalletId
-import com.tangem.domain.tokens.AddCryptoCurrenciesUseCase
 import com.tangem.domain.walletmanager.WalletManagersFacade
-import com.tangem.domain.wallets.usecase.DerivePublicKeysUseCase
-import com.tangem.domain.wallets.usecase.GetUserWalletUseCase
 import com.tangem.feature.referral.domain.errors.ReferralError
 import com.tangem.feature.referral.domain.models.ReferralData
 import com.tangem.feature.referral.domain.models.TokenData
 import timber.log.Timber
 
-@Suppress("LongParameterList")
 internal class ReferralInteractorImpl(
     private val repository: ReferralRepository,
-    private val derivePublicKeysUseCase: DerivePublicKeysUseCase,
-    private val getUserWalletUseCase: GetUserWalletUseCase,
-    private val addCryptoCurrenciesUseCase: AddCryptoCurrenciesUseCase,
     private val manageCryptoCurrenciesUseCase: ManageCryptoCurrenciesUseCase,
     private val singleAccountSupplier: SingleAccountSupplier,
     private val walletManagersFacade: WalletManagersFacade,
@@ -40,64 +32,41 @@ internal class ReferralInteractorImpl(
         return referralData
     }
 
-    override suspend fun startReferral(portfolioId: PortfolioId): ReferralData {
+    override suspend fun startReferral(accountId: AccountId): ReferralData {
         if (tokensForReferral.isEmpty()) error("Tokens for ref is empty")
 
         val tokenData = tokensForReferral.first()
-        val userWalletId = portfolioId.userWalletId
-        val userWallet = getUserWalletUseCase(userWalletId).getOrElse {
-            error("Failed to get user wallet $userWalletId: $it")
-        }
+        val userWalletId = accountId.userWalletId
 
-        val accountIndex = when (portfolioId) {
-            is PortfolioId.Account -> {
-                val account = singleAccountSupplier.getSyncOrNull(
-                    params = SingleAccountProducer.Params(accountId = portfolioId.accountId),
-                )
-                    ?: error("Account not found: ${portfolioId.accountId}")
+        val account = singleAccountSupplier.getSyncOrNull(
+            params = SingleAccountProducer.Params(accountId = accountId),
+        )
+            ?: error("Account not found: $accountId")
 
-                when (account) {
-                    is Account.CryptoPortfolio -> account.derivationIndex
-                    is Account.Payment -> TODO("[REDACTED_JIRA]")
-                }
-            }
-            is PortfolioId.Wallet -> null
+        val accountIndex = when (account) {
+            is Account.CryptoPortfolio -> account.derivationIndex
+            is Account.Payment -> TODO("[REDACTED_JIRA]")
         }
 
         val cryptoCurrency = getCryptoCurrency(
-            userWalletId = portfolioId.userWalletId,
+            userWalletId = accountId.userWalletId,
             tokenData = tokenData,
             accountIndex = accountIndex,
         )
             ?: error("Failed to create crypto currency")
 
-        when (portfolioId) {
-            is PortfolioId.Account -> {
-                manageCryptoCurrenciesUseCase(
-                    accountId = portfolioId.accountId,
-                    add = cryptoCurrency,
-                    skipDerivationErrors = false,
-                ).mapLeft {
-                    it.mapToDomainError()
-                }.onLeft { error ->
-                    if (error is ReferralError.UserCancelledException) {
-                        throw error
-                    }
+        manageCryptoCurrenciesUseCase(
+            accountId = accountId,
+            add = cryptoCurrency,
+            skipDerivationErrors = false,
+        )
+            .mapLeft { it.mapToDomainError() }
+            .onLeft { error ->
+                Timber.e(error)
+                if (error is ReferralError.UserCancelledException) {
+                    throw error
                 }
             }
-            is PortfolioId.Wallet -> {
-                derivePublicKeysUseCase(userWallet.walletId, listOf(cryptoCurrency)).getOrElse { throwable ->
-                    Timber.e("Failed to derive public keys: $throwable")
-                    throw throwable.mapToDomainError()
-                }
-
-                addCryptoCurrenciesUseCase(
-                    userWalletId = userWallet.walletId,
-                    currency = cryptoCurrency,
-                )
-            }
-        }
-            .onLeft(Timber::e)
 
         val publicAddress = walletManagersFacade.getDefaultAddress(
             userWalletId = userWalletId,
