1d99b8e3c5908d674a64477bc1214416d0a3b52a
Tangem <git@tangem.com>
2026-02-27 13:22:44 +0300

    Updated on 2026-09-01

---
+++ features/tester/impl/src/main/java/com/tangem/feature/tester/presentation/storybook/page/buttons/Build.kt
@@ -0,0 +1,6 @@
+package com.tangem.feature.tester.presentation.storybook.page.buttons
+
+import com.tangem.feature.tester.presentation.storybook.entity.ButtonsStory
+import com.tangem.feature.tester.presentation.storybook.entity.StoryPageFactory
+
+internal val buttonsStoryFactory: StoryPageFactory = StoryPageFactory { ButtonsStory }
