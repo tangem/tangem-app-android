a92643d40a0d72f006c37f89b3dda6555ac2ce4c
Tangem <git@tangem.com>
2026-02-27 15:54:11 +0300

    Updated on 2026-07-07

--- features/tester/impl/src/main/java/com/tangem/feature/tester/presentation/storybook/entity/StoryBookPage.kt
+++ features/tester/impl/src/main/java/com/tangem/feature/tester/presentation/storybook/entity/StoryBookPage.kt
@@ -1,10 +1,34 @@
 package com.tangem.feature.tester.presentation.storybook.entity
 
+import com.tangem.core.ui.ds.badge.TangemBadgeColor
+import com.tangem.core.ui.ds.message.TangemMessageEffect
+
 internal sealed interface StoryBookPage
 
 internal data object StoryList : StoryBookPage
 
 internal data object ButtonsStory : StoryBookPage
+
+internal data class TangemBadgeStory(
+    val selectedColor: TangemBadgeColor,
+    val onColorChange: (TangemBadgeColor) -> Unit,
+) : StoryBookPage
+
+internal data object OpportunitiesBGStory : StoryBookPage
+
+internal data class TangemCheckboxStory(
+    val isRoundedChecked: Boolean,
+    val onRoundedCheckedChange: (Boolean) -> Unit,
+    val isCircleChecked: Boolean,
+    val onCircleCheckedChange: (Boolean) -> Unit,
+) : StoryBookPage
+
+internal data object TangemSegmentedPickerStory : StoryBookPage
+
+internal data class TangemMessageStory(
+    val selectedEffect: TangemMessageEffect,
+    val onEffectChange: (TangemMessageEffect) -> Unit,
+) : StoryBookPage
 
 internal data class NorthernLightsStory(
     val variant: Variant,
@@ -15,3 +39,8 @@ internal data class NorthernLightsStory(
         Simple,
     }
 }
+
+internal data class TangemTokenRowStory(
+    val isBalanceHidden: Boolean,
+    val onBalanceHiddenToggle: () -> Unit,
+) : StoryBookPage
