c52c07e64de637180c2da568c513186df32f6eb6
Tangem <git@tangem.com>
2026-02-27 12:16:41 +0400

    Updated on 2026-09-21

--- features/approval/impl/src/main/java/com/tangem/features/approval/impl/di/GiveApprovalBindsModule.kt
+++ features/approval/impl/src/main/java/com/tangem/features/approval/impl/di/GiveApprovalBindsModule.kt
@@ -3,7 +3,9 @@ package com.tangem.features.approval.impl.di
 import com.tangem.core.decompose.di.ModelComponent
 import com.tangem.core.decompose.model.Model
 import com.tangem.features.approval.api.GiveApprovalComponent
+import com.tangem.features.approval.api.GiveApprovalFeatureToggles
 import com.tangem.features.approval.impl.DefaultGiveApprovalComponent
+import com.tangem.features.approval.impl.DefaultGiveApprovalFeatureToggles
 import com.tangem.features.approval.impl.model.GiveApprovalModel
 import dagger.Binds
 import dagger.Module
@@ -16,6 +18,10 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 @Module
 internal interface GiveApprovalFeatureModule {
+
+    @Singleton
+    @Binds
+    fun bindGiveApprovalFeatureToggle(toggles: DefaultGiveApprovalFeatureToggles): GiveApprovalFeatureToggles
 
     @Binds
     @Singleton
