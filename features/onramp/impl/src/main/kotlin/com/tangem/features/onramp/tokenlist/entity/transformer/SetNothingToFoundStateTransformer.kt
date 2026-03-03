9910c9352aa9bae8e170cccb1a325d52f1ad7726
Tangem <git@tangem.com>
2026-03-03 14:49:04 +0400

    Updated on 2026-06-24

--- features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/tokenlist/entity/transformer/SetNothingToFoundStateTransformer.kt
+++ features/onramp/impl/src/main/kotlin/com/tangem/features/onramp/tokenlist/entity/transformer/SetNothingToFoundStateTransformer.kt
@@ -2,8 +2,6 @@ package com.tangem.features.onramp.tokenlist.entity.transformer
 
 import com.tangem.core.ui.components.tokenlist.state.TokensListItemUM
 import com.tangem.core.ui.extensions.TextReference
-import com.tangem.core.ui.extensions.resourceReference
-import com.tangem.features.onramp.impl.R
 import com.tangem.features.onramp.tokenlist.entity.TokenListUM
 import com.tangem.features.onramp.tokenlist.entity.TokenListUMData
 import com.tangem.features.onramp.tokenlist.entity.TokenListUMTransformer
@@ -16,22 +14,18 @@ internal class SetNothingToFoundStateTransformer(
 
     override fun transform(prevState: TokenListUM): TokenListUM {
         return prevState.copy(
-            availableItems = persistentListOf(
-                createGroupTitle(
-                    textReference = resourceReference(id = R.string.exchange_tokens_available_tokens_header),
-                ),
-                TokensListItemUM.Text(
-                    id = emptySearchMessageReference.hashCode(),
-                    text = emptySearchMessageReference,
-                ),
-            ),
+            availableItems = persistentListOf(),
             unavailableItems = persistentListOf(),
-            tokensListData = TokenListUMData.EmptyList,
+            tokensListData = TokenListUMData.TokenList(
+                tokensList = persistentListOf(
+                    TokensListItemUM.Text(
+                        id = emptySearchMessageReference.hashCode(),
+                        text = emptySearchMessageReference,
+                    ),
+                ),
+                totalTokensCount = 0,
+            ),
             isBalanceHidden = isBalanceHidden,
         )
-    }
-
-    private fun createGroupTitle(textReference: TextReference): TokensListItemUM.GroupTitle {
-        return TokensListItemUM.GroupTitle(id = textReference.hashCode(), text = textReference)
     }
 }
