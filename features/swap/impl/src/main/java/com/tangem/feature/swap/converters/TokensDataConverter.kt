f035cb4272264f32919696f1e0f6b6328eb84d1d
Tangem <git@tangem.com>
2026-03-03 11:21:00 +0400

    Updated on 2026-06-16

--- features/swap/impl/src/main/java/com/tangem/feature/swap/converters/TokensDataConverter.kt
+++ features/swap/impl/src/main/java/com/tangem/feature/swap/converters/TokensDataConverter.kt
@@ -1,113 +1,80 @@
 package com.tangem.feature.swap.converters
 
-import com.tangem.core.ui.components.currency.icon.CurrencyIconState
-import com.tangem.core.ui.extensions.*
-import com.tangem.core.ui.format.bigdecimal.crypto
-import com.tangem.core.ui.format.bigdecimal.fiat
-import com.tangem.core.ui.format.bigdecimal.format
+import com.tangem.core.ui.components.tokenlist.state.TokensListItemUM
+import com.tangem.core.ui.extensions.resourceReference
 import com.tangem.domain.appcurrency.model.AppCurrency
-import com.tangem.domain.models.currency.CryptoCurrency
-import com.tangem.domain.models.currency.CryptoCurrencyStatus
-import com.tangem.feature.swap.domain.models.domain.CryptoCurrencySwapInfo
-import com.tangem.feature.swap.models.CurrenciesGroupWithFromCurrency
+import com.tangem.feature.swap.domain.models.ui.CurrenciesGroup
 import com.tangem.feature.swap.models.SwapSelectTokenStateHolder
-import com.tangem.feature.swap.models.TokenBalanceData
+import com.tangem.feature.swap.models.SwapStateHolder
 import com.tangem.feature.swap.models.TokenListUMData
-import com.tangem.feature.swap.models.TokenToSelectState
 import com.tangem.feature.swap.presentation.R
 import com.tangem.utils.Provider
-import com.tangem.utils.converter.Converter
+import com.tangem.utils.transformer.Transformer
 import kotlinx.collections.immutable.persistentListOf
-import kotlinx.collections.immutable.toImmutableList
+import kotlinx.collections.immutable.plus
+import kotlinx.collections.immutable.toPersistentList
 
 internal class TokensDataConverter(
     private val onSearchEntered: (String) -> Unit,
     private val onTokenSelected: (String) -> Unit,
-    private val isBalanceHiddenProvider: Provider<Boolean>,
-    private val appCurrencyProvider: Provider<AppCurrency>,
-) : Converter<CurrenciesGroupWithFromCurrency, SwapSelectTokenStateHolder> {
+    private val tokensDataState: CurrenciesGroup,
+    private val isBalanceHidden: Boolean,
+    private val isAccountsMode: Boolean,
+    appCurrencyProvider: Provider<AppCurrency>,
+) : Transformer<SwapStateHolder> {
 
-    override fun convert(value: CurrenciesGroupWithFromCurrency): SwapSelectTokenStateHolder {
-        val group = value.group
-        val availableTitle = TokenToSelectState.Title(
-            resourceReference(R.string.exchange_tokens_available_tokens_header),
-        )
-        val allTokens = group.available + group.unavailable
-        return SwapSelectTokenStateHolder(
-            availableTokens = allTokens.map { tokenWithBalanceToTokenToSelect(it, true) }
-                .toMutableList()
-                .apply {
-                    if (this.isNotEmpty()) {
-                        this.add(0, availableTitle)
+    private val accountListItemConverter = AccountTokenItemConverter(
+        appCurrency = appCurrencyProvider(),
+        unavailableErrorText = resourceReference(R.string.tokens_list_unavailable_to_swap_source_header),
+        onItemClick = onTokenSelected,
+    )
+
+    override fun transform(prevState: SwapStateHolder): SwapStateHolder {
+        val accountList = tokensDataState.accountCurrencyList
+        val currentMarketsState = prevState.selectTokenState?.marketsState
+        return prevState.copy(
+            selectTokenState = SwapSelectTokenStateHolder(
+                availableTokens = persistentListOf(),
+                unavailableTokens = persistentListOf(),
+                tokensListData = if (isAccountsMode) {
+                    val portfolioList = accountListItemConverter.convertList(accountList).toPersistentList()
+                    val totalTokensCount = portfolioList.sumOf { it.tokens.size }
+                    if (totalTokensCount > 0) {
+                        TokenListUMData.AccountList(
+                            tokensList = portfolioList,
+                            totalTokensCount = totalTokensCount,
+                        )
+                    } else {
+                        TokenListUMData.EmptyList
                     }
-                }
-                .toImmutableList(),
-            unavailableTokens = persistentListOf(),
-            tokensListData = TokenListUMData.EmptyList,
-            onSearchEntered = onSearchEntered,
-            onTokenSelected = onTokenSelected,
-            isBalanceHidden = isBalanceHiddenProvider(),
-            isAfterSearch = group.isAfterSearch,
-        )
-    }
+                } else {
+                    val tokensList = accountList.flatMap { (_, currencyList) ->
+                        currencyList.asSequence().map { accountSwapCurrency ->
+                            accountListItemConverter.createAvailableItemConverter()
+                                .convert(accountSwapCurrency.cryptoCurrencyStatus)
+                        }.map(TokensListItemUM::Token).toPersistentList()
+                    }.toPersistentList()
 
-    private fun tokenWithBalanceToTokenToSelect(
-        cryptoCurrencySwapInfo: CryptoCurrencySwapInfo,
-        isAvailable: Boolean,
-    ): TokenToSelectState {
-        val cryptoCurrencyStatus = cryptoCurrencySwapInfo.currencyStatus
-        return TokenToSelectState.TokenToSelect(
-            id = cryptoCurrencyStatus.currency.id.value,
-            name = cryptoCurrencyStatus.currency.name,
-            symbol = cryptoCurrencyStatus.currency.symbol,
-            isAvailable = isAvailable,
-            tokenIcon = convertIcon(cryptoCurrencyStatus.currency, isAvailable),
-            addedTokenBalanceData = TokenBalanceData(
-                amount = formatCryptoAmount(cryptoCurrencyStatus),
-                amountEquivalent = formatFiatAmount(cryptoCurrencyStatus, appCurrencyProvider.invoke()),
-                isBalanceHidden = isBalanceHiddenProvider.invoke(),
+                    if (tokensList.isNotEmpty()) {
+                        TokenListUMData.TokenList(
+                            tokensList = persistentListOf(
+                                TokensListItemUM.GroupTitle(
+                                    id = "available_tokens_title",
+                                    text = resourceReference(R.string.exchange_tokens_available_tokens_header),
+                                ),
+                            ) + tokensList,
+                            totalTokensCount = tokensList.size,
+                        )
+                    } else {
+                        TokenListUMData.EmptyList
+                    }
+                },
+                marketsState = currentMarketsState,
+                onSearchEntered = onSearchEntered,
+                onTokenSelected = onTokenSelected,
+                isBalanceHidden = isBalanceHidden,
+                isAfterSearch = tokensDataState.isAfterSearch,
             ),
         )
-    }
-
-    private fun convertIcon(currency: CryptoCurrency, isAvailable: Boolean): CurrencyIconState {
-        return when (currency) {
-            is CryptoCurrency.Coin -> {
-                CurrencyIconState.CoinIcon(
-                    url = currency.iconUrl,
-                    fallbackResId = currency.networkIconResId,
-                    isGrayscale = !isAvailable,
-                    shouldShowCustomBadge = currency.isCustom,
-                )
-            }
-            is CryptoCurrency.Token -> {
-                val isGrayscale = currency.network.isTestnet
-                val background = currency.tryGetBackgroundForTokenIcon(isGrayscale)
-                val tint = getTintForTokenIcon(background)
-                CurrencyIconState.TokenIcon(
-                    url = currency.iconUrl,
-                    isGrayscale = !isAvailable,
-                    shouldShowCustomBadge = currency.isCustom,
-                    topBadgeIconResId = currency.networkIconResId,
-                    fallbackTint = tint,
-                    fallbackBackground = background,
-                )
-            }
-        }
-    }
-
-    private fun formatCryptoAmount(cryptoCurrencyStatus: CryptoCurrencyStatus): String {
-        return cryptoCurrencyStatus.value.amount.format {
-            crypto(cryptoCurrencyStatus.currency)
-        }
-    }
-
-    private fun formatFiatAmount(cryptoCurrencyStatus: CryptoCurrencyStatus, appCurrency: AppCurrency): String {
-        return cryptoCurrencyStatus.value.fiatAmount.format {
-            fiat(
-                fiatCurrencyCode = appCurrency.code,
-                fiatCurrencySymbol = appCurrency.symbol,
-            )
-        }
     }
 }
