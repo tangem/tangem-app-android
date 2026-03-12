e22331ec90e54e3c8be72aa5a4de90eba6f470b9
Tangem <git@tangem.com>
2026-03-12 15:28:52 +0400

    Updated on 2026-08-14

--- domain/account/status/src/main/java/com/tangem/domain/account/status/usecase/GetFeePaidCryptoCurrencyStatusSyncUseCase.kt
+++ domain/account/status/src/main/java/com/tangem/domain/account/status/usecase/GetFeePaidCryptoCurrencyStatusSyncUseCase.kt
@@ -1,17 +1,19 @@
-package com.tangem.domain.tokens
+package com.tangem.domain.account.status.usecase
 
 import arrow.core.Either
 import arrow.core.raise.either
+import com.tangem.domain.account.status.supplier.SingleAccountStatusListSupplier
+import com.tangem.domain.account.status.utils.CryptoCurrencyStatusOperations.getCoinStatus
+import com.tangem.domain.account.status.utils.CryptoCurrencyStatusOperations.getCryptoCurrencyStatus
 import com.tangem.domain.models.currency.CryptoCurrencyStatus
 import com.tangem.domain.models.wallet.UserWalletId
 import com.tangem.domain.tokens.error.TokenListError
 import com.tangem.domain.tokens.model.FeePaidCurrency
-import com.tangem.domain.tokens.operations.BaseCurrencyStatusOperations
 import com.tangem.domain.tokens.repository.CurrenciesRepository
 
 class GetFeePaidCryptoCurrencyStatusSyncUseCase(
-    internal val currenciesRepository: CurrenciesRepository,
-    private val currencyStatusOperations: BaseCurrencyStatusOperations,
+    private val currenciesRepository: CurrenciesRepository,
+    private val singleAccountStatusListSupplier: SingleAccountStatusListSupplier,
 ) {
 
     suspend operator fun invoke(
@@ -25,12 +27,17 @@ class GetFeePaidCryptoCurrencyStatusSyncUseCase(
         return either {
             when (feePaidCurrency) {
                 is FeePaidCurrency.Coin -> {
-                    currencyStatusOperations.getNetworkCoinSync(userWalletId, network.id, network.derivationPath)
-                        .getOrNull()
+                    val accountStatusList = singleAccountStatusListSupplier.getSyncOrNull(userWalletId)
+
+                    accountStatusList?.getCoinStatus(network)?.getOrNull()
                 }
                 is FeePaidCurrency.Token -> {
-                    currencyStatusOperations.getCurrencyStatusSync(userWalletId, feePaidCurrency.tokenId)
-                        .getOrNull()
+                    val accountStatusList = singleAccountStatusListSupplier.getSyncOrNull(userWalletId)
+
+                    accountStatusList.getCryptoCurrencyStatus(
+                        currencyId = feePaidCurrency.tokenId,
+                        network = network,
+                    ).getOrNull()
                 }
                 is FeePaidCurrency.SameCurrency,
                 is FeePaidCurrency.FeeResource,
