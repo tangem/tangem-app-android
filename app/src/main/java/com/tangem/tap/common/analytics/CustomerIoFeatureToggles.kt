7010d52e221ddaa018aea81222e9caf1a04e96eb
Tangem <git@tangem.com>
2026-03-12 14:58:58 +0400

    Updated on 2026-06-16

---
+++ app/src/main/java/com/tangem/tap/common/analytics/CustomerIoFeatureToggles.kt
@@ -0,0 +1,12 @@
+package com.tangem.tap.common.analytics
+
+import com.tangem.core.configtoggle.feature.FeatureTogglesManager
+import javax.inject.Inject
+
+class CustomerIoFeatureToggles @Inject constructor(
+    private val featureTogglesManager: FeatureTogglesManager,
+) {
+
+    val isFeatureEnabled: Boolean
+        get() = featureTogglesManager.isFeatureEnabled(name = "CUSTOMER_IO_ENABLED")
+}
