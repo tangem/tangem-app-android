608165503136ff61d73a2d638c730b07d5728ebd
Tangem <git@tangem.com>
2026-03-13 12:58:22 +0400

    Updated on 2026-04-08

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/wallet/BaseWalletBalanceFetcher.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/wallet/BaseWalletBalanceFetcher.kt
@@ -1,7 +1,7 @@
 package com.tangem.domain.tokens.wallet
 
 import com.tangem.domain.models.currency.CryptoCurrency
-import com.tangem.domain.models.wallet.UserWalletId
+import com.tangem.domain.models.wallet.UserWallet
 
 /**
  * Base contract for implementation of wallet's balance fetcher
@@ -13,6 +13,6 @@ internal interface BaseWalletBalanceFetcher {
     /** Fetching sources */
     val fetchingSources: Set<FetchingSource>
 
-    /** Get crypto currencies of wallet with [userWalletId] */
-    suspend fun getCryptoCurrencies(userWalletId: UserWalletId): Set<CryptoCurrency>
+    /** Get crypto currencies of [userWallet] */
+    suspend fun getCryptoCurrencies(userWallet: UserWallet): Set<CryptoCurrency>
 }
