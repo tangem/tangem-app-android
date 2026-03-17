852c30ac01d9fb0e428998afcf4ff6d4487eb628
Tangem <git@tangem.com>
2026-03-17 12:59:37 +0400

    Updated on 2026-08-19

--- features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/DefaultTangemPayFeatureToggles.kt
+++ features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/DefaultTangemPayFeatureToggles.kt
@@ -1,10 +1,11 @@
 package com.tangem.features.tangempay
 
+import com.tangem.core.configtoggle.FeatureToggles
 import com.tangem.core.configtoggle.feature.FeatureTogglesManager
 
 internal class DefaultTangemPayFeatureToggles(
     private val featureTogglesManager: FeatureTogglesManager,
 ) : TangemPayFeatureToggles {
     override val isTangemPayAccountsRefactorEnabled
-        get() = featureTogglesManager.isFeatureEnabled("TANGEM_PAY_ACCOUNTS_REFACTOR_ENABLED")
+        get() = featureTogglesManager.isFeatureEnabled(FeatureToggles.TANGEM_PAY_ACCOUNTS_REFACTOR_ENABLED)
 }
