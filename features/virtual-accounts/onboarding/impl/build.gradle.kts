14a079192e09f64d601c5f5af78a95286155665d
Tangem <git@tangem.com>
2026-03-17 19:36:09 +0100

    Updated on 2026-05-13

--- features/virtual-accounts/onboarding/impl/build.gradle.kts
+++ features/virtual-accounts/onboarding/impl/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(deps.plugins.android.library)
     alias(deps.plugins.kotlin.android)
+    alias(deps.plugins.kotlin.kapt)
+    alias(deps.plugins.hilt.android)
     id("configuration")
 }
 
@@ -9,4 +11,20 @@ android {
 }
 
 dependencies {
+    /** Api */
+    implementation(projects.features.virtualAccounts.onboarding.api)
+
+    /** Core modules */
+    implementation(projects.core.configToggles)
+
+    /** Compose */
+    implementation(deps.compose.foundation)
+    implementation(deps.compose.material3)
+    implementation(deps.compose.ui)
+    implementation(deps.compose.ui.tooling)
+    implementation(deps.decompose.ext.compose)
+
+    /** DI */
+    implementation(deps.hilt.android)
+    kapt(deps.hilt.kapt)
 }
