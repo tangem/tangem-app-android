2f237f56d7dba3f44b9f6b0f412fec9b987452ea
Tangem <git@tangem.com>
2026-03-17 13:10:09 +0200

    Updated on 2026-07-23

---
+++ features/promo-banners/impl/src/main/kotlin/com/tangem/features/promobanners/impl/model/PromoBannerDisplay.kt
@@ -0,0 +1,14 @@
+package com.tangem.features.promobanners.impl.model
+
+internal data class PromoBannerDisplay(
+    val id: String,
+    val placeholder: String,
+    val priority: PromoBannerPriority,
+    val title: String,
+    val subtitle: String,
+    val iconUrl: String?,
+    val deeplink: String?,
+    val isButtonEnabled: Boolean,
+    val buttonText: String?,
+    val isDismissable: Boolean,
+)
