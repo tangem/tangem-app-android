4ba364f33596cd5b8ce0367192d241e0371c1b0c
Tangem <git@tangem.com>
2026-03-17 12:59:37 +0400

    Updated on 2026-09-21

--- app/src/main/java/com/tangem/tap/core/ui/DefaultDesignFeatureToggles.kt
+++ app/src/main/java/com/tangem/tap/core/ui/DefaultDesignFeatureToggles.kt
@@ -1,11 +1,14 @@
 package com.tangem.tap.core.ui
 
+import com.tangem.core.configtoggle.FeatureToggles
 import com.tangem.core.configtoggle.feature.FeatureTogglesManager
 import com.tangem.core.ui.DesignFeatureToggles
 import javax.inject.Inject
 
 class DefaultDesignFeatureToggles @Inject constructor(
-    featureTogglesManager: FeatureTogglesManager,
+    private val featureTogglesManager: FeatureTogglesManager,
 ) : DesignFeatureToggles {
-    override val isRedesignEnabled: Boolean = featureTogglesManager.isFeatureEnabled("APP_REDESIGN_ENABLED")
+
+    override val isRedesignEnabled: Boolean
+        get() = featureTogglesManager.isFeatureEnabled(FeatureToggles.APP_REDESIGN_ENABLED)
 }
