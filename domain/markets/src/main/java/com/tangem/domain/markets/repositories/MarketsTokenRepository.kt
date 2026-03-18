3b0d480f7f6c2cca1a934cc67120821d1e3e8360
Tangem <git@tangem.com>
2026-03-18 18:16:55 +0100

    Updated on 2026-06-17

--- domain/markets/src/main/java/com/tangem/domain/markets/repositories/MarketsTokenRepository.kt
+++ domain/markets/src/main/java/com/tangem/domain/markets/repositories/MarketsTokenRepository.kt
@@ -1,6 +1,5 @@
 package com.tangem.domain.markets.repositories
 
-import com.tangem.domain.appcurrency.model.AppCurrency
 import com.tangem.domain.markets.*
 import com.tangem.domain.models.account.DerivationIndex
 import com.tangem.domain.models.currency.CryptoCurrency
@@ -54,6 +53,4 @@ interface MarketsTokenRepository {
      * @param tokenId token id
      */
     suspend fun getTokenExchanges(tokenId: CryptoCurrency.RawID): List<TokenMarketExchange>
-
-    suspend fun showYieldModePromo(appCurrency: AppCurrency, interval: TokenMarketListConfig.Interval): Boolean
 }
