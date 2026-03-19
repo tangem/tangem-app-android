20cc96813643e3c54b0ae9e420325a86b9a74b9a
Tangem <git@tangem.com>
2026-03-19 18:24:41 +0500

    Updated on 2026-08-27

--- core/datasource/src/main/java/com/tangem/datasource/api/pay/models/request/OrderRequest.kt
+++ core/datasource/src/main/java/com/tangem/datasource/api/pay/models/request/OrderRequest.kt
@@ -4,6 +4,11 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class OrderRequest(
-    @Json(name = "wallet_address") val walletAddress: String,
-)
+data class OrderRequest(@Json(name = "data") val data: Data) {
+    @JsonClass(generateAdapter = true)
+    data class Data(
+        @Json(name = "customer_wallet_address") val customerWalletAddress: String,
+        @Json(name = "specification_name") val specificationName: String = "SP_000004",
+        @Json(name = "type") val type: String = "CARD_ISSUE_VIRTUAL_RAIN_KYC",
+    )
+}
