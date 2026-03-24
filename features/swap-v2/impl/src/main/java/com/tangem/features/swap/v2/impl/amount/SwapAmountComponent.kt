6a697c609f14c9237b404560cff2ab263ebe82eb
Tangem <git@tangem.com>
2026-03-24 16:36:05 +0700

    Updated on 2026-08-19

--- features/swap-v2/impl/src/main/java/com/tangem/features/swap/v2/impl/amount/SwapAmountComponent.kt
+++ features/swap-v2/impl/src/main/java/com/tangem/features/swap/v2/impl/amount/SwapAmountComponent.kt
@@ -5,15 +5,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.decompose.extensions.compose.subscribeAsState
+import com.arkivanov.decompose.router.slot.childSlot
+import com.arkivanov.decompose.router.slot.dismiss
 import com.arkivanov.essenty.lifecycle.subscribe
 import com.tangem.common.ui.navigationButtons.NavigationModelCallback
 import com.tangem.core.decompose.context.AppComponentContext
+import com.tangem.core.decompose.context.childByContext
 import com.tangem.core.decompose.model.getOrCreateModel
+import com.tangem.core.ui.decompose.ComposableBottomSheetComponent
 import com.tangem.core.ui.decompose.ComposableContentComponent
 import com.tangem.core.ui.res.TangemTheme
+import com.tangem.domain.express.models.ExpressRateType
 import com.tangem.features.swap.v2.impl.amount.entity.SwapAmountUM
 import com.tangem.features.swap.v2.impl.amount.model.SwapAmountModel
 import com.tangem.features.swap.v2.impl.amount.ui.SwapAmountContent
+import com.tangem.features.swap.v2.impl.sendviaswap.rateinfo.SwapRateInfoComponent
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 
@@ -23,6 +31,14 @@ internal class SwapAmountComponent @AssistedInject constructor(
 ) : ComposableContentComponent, AppComponentContext by appComponentContext {
 
     private val model: SwapAmountModel = getOrCreateModel(params = params)
+
+    private val rateInfoSlot = childSlot(
+        source = model.rateInfoNavigation,
+        key = "rateInfoSlot",
+        serializer = null,
+        handleBackButton = true,
+        childFactory = ::rateInfoChild,
+    )
 
     init {
         lifecycle.subscribe(
@@ -36,11 +52,25 @@ internal class SwapAmountComponent @AssistedInject constructor(
     @Composable
     override fun Content(modifier: Modifier) {
         val amountUM by model.uiState.collectAsStateWithLifecycle()
+        val rateInfo by rateInfoSlot.subscribeAsState()
 
         SwapAmountContent(
             amountUM = amountUM,
             modifier = Modifier.background(TangemTheme.colors.background.tertiary),
             clickIntents = model,
+        )
+
+        rateInfo.child?.instance?.BottomSheet()
+    }
+
+    private fun rateInfoChild(
+        config: ExpressRateType,
+        componentContext: ComponentContext,
+    ): ComposableBottomSheetComponent {
+        return SwapRateInfoComponent(
+            appComponentContext = childByContext(componentContext),
+            expressRateType = config,
+            onDismiss = { model.rateInfoNavigation.dismiss() },
         )
     }
 
