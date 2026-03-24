d78e9321ec1fe302a2eb608cea0f04e9a8b13f6f
Tangem <git@tangem.com>
2026-03-24 12:05:48 +0500

    Updated on 2026-08-07

---
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/transformers/converter/TangemPayMainBlockConverter.kt
@@ -0,0 +1,110 @@
+package com.tangem.feature.wallet.presentation.wallet.state.transformers.converter
+
+import com.tangem.common.ui.R
+import com.tangem.core.ui.extensions.TextReference
+import com.tangem.core.ui.extensions.stringReference
+import com.tangem.core.ui.format.bigdecimal.fiat
+import com.tangem.core.ui.format.bigdecimal.format
+import com.tangem.domain.models.StatusSource
+import com.tangem.domain.models.account.AccountStatus
+import com.tangem.domain.models.account.PaymentAccountStatusValue
+import com.tangem.domain.models.kyc.KycStatus
+import com.tangem.domain.pay.TangemPayDetailsConfig
+import com.tangem.domain.visa.model.TangemPayCardFrozenState
+import com.tangem.feature.wallet.child.wallet.model.intents.TangemPayIntents
+import com.tangem.features.tangempay.entity.TangemPayMainUM
+import com.tangem.utils.converter.Converter
+import java.math.BigDecimal
+import java.util.Currency
+
+private const val POLYGON_CHAIN_ID = 137
+
+internal class TangemPayMainBlockConverter(
+    private val tangemPayClickIntents: TangemPayIntents,
+) : Converter<AccountStatus.Payment, TangemPayMainUM> {
+    @Suppress("LongMethod", "CyclomaticComplexMethod")
+    override fun convert(value: AccountStatus.Payment): TangemPayMainUM {
+        return when (val statusValue = value.value) {
+            is PaymentAccountStatusValue.Error.CardIssueFailed -> TangemPayMainUM.FailedToIssue(
+                onClick = { tangemPayClickIntents.onIssuingFailedClicked(statusValue.customerId) },
+            )
+            is PaymentAccountStatusValue.Error.ExposedDevice -> TangemPayMainUM.ExposedDevice
+            is PaymentAccountStatusValue.Error.NotSynced -> TangemPayMainUM.SyncNeeded
+            is PaymentAccountStatusValue.Error.Unavailable -> TangemPayMainUM.TemporaryUnavailable
+            is PaymentAccountStatusValue.IssuingCard -> TangemPayMainUM.IssuingCard(
+                onClick = { tangemPayClickIntents.onIssuingCardClicked() },
+            )
+            is PaymentAccountStatusValue.UnderReview -> TangemPayMainUM.UnderReview(
+                subtitle = when (statusValue.kycStatus) {
+                    KycStatus.REJECTED -> TextReference.Res(R.string.tangempay_kyc_has_failed)
+                    else -> TextReference.Res(R.string.tangempay_kyc_in_progress)
+                },
+                onClick = {
+                    when (statusValue.kycStatus) {
+                        KycStatus.REJECTED -> tangemPayClickIntents.onKycRejectedClicked(
+                            userWalletId = value.account.userWalletId,
+                            customerId = statusValue.customerId,
+                        )
+                        else -> tangemPayClickIntents.onKycProgressClicked(value.account.userWalletId)
+                    }
+                },
+            )
+            is PaymentAccountStatusValue.NotCreated -> TangemPayMainUM.Empty
+            is PaymentAccountStatusValue.Loading -> TangemPayMainUM.Loading
+            is PaymentAccountStatusValue.Locked -> TangemPayMainUM.Content(
+                subtitle = stringReference("*${statusValue.lastFourDigits}"),
+                isBalanceFlickering = statusValue.source == StatusSource.CACHE,
+                balance = getBalanceText(
+                    currencyCode = statusValue.currencyCode,
+                    balance = statusValue.fiatBalance.availableBalance,
+                ),
+                balanceSubtitle = stringReference("USDC"), // TODO hardcode for now
+                shouldShowOnlyCacheWarning = statusValue.source == StatusSource.ONLY_CACHE,
+                onClick = {
+                    tangemPayClickIntents.openDetails(
+                        value.account.userWalletId,
+                        TangemPayDetailsConfig(
+                            customerId = statusValue.customerId,
+                            cardId = statusValue.cardId,
+                            isPinSet = statusValue.isPinSet,
+                            cardFrozenState = TangemPayCardFrozenState.Frozen,
+                            cardNumberEnd = statusValue.lastFourDigits,
+                            chainId = POLYGON_CHAIN_ID,
+                        ),
+                    )
+                },
+            )
+            is PaymentAccountStatusValue.Loaded -> TangemPayMainUM.Content(
+                subtitle = stringReference("*${statusValue.lastFourDigits}"),
+                isBalanceFlickering = statusValue.source == StatusSource.CACHE,
+                balance = getBalanceText(
+                    currencyCode = statusValue.currencyCode,
+                    balance = statusValue.fiatBalance.availableBalance,
+                ),
+                balanceSubtitle = stringReference("USDC"), // TODO hardcode for now
+                shouldShowOnlyCacheWarning = statusValue.source == StatusSource.ONLY_CACHE,
+                onClick = {
+                    tangemPayClickIntents.openDetails(
+                        value.account.userWalletId,
+                        TangemPayDetailsConfig(
+                            customerId = statusValue.customerId,
+                            cardId = statusValue.cardId,
+                            isPinSet = statusValue.isPinSet,
+                            cardFrozenState = TangemPayCardFrozenState.Unfrozen,
+                            cardNumberEnd = statusValue.lastFourDigits,
+                            chainId = POLYGON_CHAIN_ID,
+                        ),
+                    )
+                },
+            )
+        }
+    }
+
+    private fun getBalanceText(currencyCode: String, balance: BigDecimal): TextReference {
+        val currency = Currency.getInstance(currencyCode)
+        val formattedBalance = balance.format {
+            fiat(fiatCurrencyCode = currency.currencyCode, fiatCurrencySymbol = currency.symbol)
+        }
+        return stringReference(formattedBalance)
+    }
+}
