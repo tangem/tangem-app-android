ec45bc780093743e5be7cfb73810bec613ef5699
Tangem <git@tangem.com>
2026-03-24 12:17:28 +0200

    Updated on 2026-09-21

--- features/promo-banners/impl/src/main/kotlin/com/tangem/features/promobanners/impl/model/PromoBannerPriority.kt
+++ features/promo-banners/impl/src/main/kotlin/com/tangem/features/promobanners/impl/model/PromoBannerPriority.kt
@@ -1,7 +1,7 @@
 package com.tangem.features.promobanners.impl.model
 
 internal enum class PromoBannerPriority(val order: Int) {
-    IMPORTANT(order = 0),
+    TOP(order = 0),
     HIGH(order = 1),
     MEDIUM(order = 2),
     LOW(order = 3),
