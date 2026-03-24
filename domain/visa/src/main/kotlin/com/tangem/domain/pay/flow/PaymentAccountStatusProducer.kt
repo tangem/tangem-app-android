d254099560b325532453ac3f26bd4cfd8ba786b0
Tangem <git@tangem.com>
2026-03-24 12:05:48 +0500

    Updated on 2026-06-10

--- domain/visa/src/main/kotlin/com/tangem/domain/pay/flow/PaymentAccountStatusProducer.kt
+++ domain/visa/src/main/kotlin/com/tangem/domain/pay/flow/PaymentAccountStatusProducer.kt
@@ -1,10 +1,10 @@
 package com.tangem.domain.pay.flow
 
 import com.tangem.domain.core.flow.FlowProducer
+import com.tangem.domain.models.account.AccountStatus
 import com.tangem.domain.models.wallet.UserWalletId
-import com.tangem.domain.pay.PaymentAccountStatus
 
-interface PaymentAccountStatusProducer : FlowProducer<PaymentAccountStatus> {
+interface PaymentAccountStatusProducer : FlowProducer<AccountStatus.Payment> {
     data class Params(val userWalletId: UserWalletId)
 
     interface Factory : FlowProducer.Factory<Params, PaymentAccountStatusProducer>
