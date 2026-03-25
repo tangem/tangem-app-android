ff4d54454a233a31a0abaceed7b7fe7970cb362c
Tangem <git@tangem.com>
2026-03-25 12:08:04 +0400

    Updated on 2026-09-01

--- domain/legacy/src/main/java/com/tangem/domain/redux/OnboardingManageTokensAction.kt
+++
@@ -1,7 +0,0 @@
-package com.tangem.domain.redux
-
-import org.rekotlin.Action
-
-sealed class OnboardingManageTokensAction : Action {
-    data object CurrenciesSaved : OnboardingManageTokensAction()
-}
