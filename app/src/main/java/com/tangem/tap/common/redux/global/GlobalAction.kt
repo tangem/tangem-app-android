c76d1de3cfc19a09ffd70e80d9958a56c80440aa
Tangem <git@tangem.com>
2026-03-26 16:28:00 +0400

    Updated on 2026-08-14

--- app/src/main/java/com/tangem/tap/common/redux/global/GlobalAction.kt
+++ app/src/main/java/com/tangem/tap/common/redux/global/GlobalAction.kt
@@ -1,22 +1,10 @@
 package com.tangem.tap.common.redux.global
 
-import com.tangem.common.CompletionResult
-import com.tangem.core.analytics.models.AnalyticsParam
 import com.tangem.domain.appcurrency.model.AppCurrency
 import com.tangem.domain.models.scan.ScanResponse
 import org.rekotlin.Action
 
 sealed class GlobalAction : Action {
-
-    object ScanFailsCounter {
-        data class ChooseBehavior(
-            val result: CompletionResult<ScanResponse>,
-            val analyticsSource: AnalyticsParam.ScreensSources,
-        ) : GlobalAction()
-
-        object Reset : GlobalAction()
-        object Increment : GlobalAction()
-    }
 
     data class SaveScanResponse(val scanResponse: ScanResponse) : GlobalAction()
 
