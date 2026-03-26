9518a5b972cfbfec464b6d749decf303f8188537
Tangem <git@tangem.com>
2026-03-26 11:03:16 +0100

    Updated on 2026-05-19

--- core/ui/src/main/java/com/tangem/core/ui/res/TangemTypography2.kt
+++ core/ui/src/main/java/com/tangem/core/ui/res/TangemTypography2.kt
@@ -114,6 +114,18 @@ class TangemTypography2 internal constructor(
         ),
     )
 
+    val headingSemibold22: TextStyle = TextStyle(
+        fontFamily = fontFamily,
+        fontSize = 22.sp,
+        fontWeight = FontWeight.SemiBold,
+        letterSpacing = TextUnit(value = 0.38f, type = TextUnitType.Sp),
+        lineHeight = TextUnit(value = 28f, type = TextUnitType.Sp),
+        lineHeightStyle = LineHeightStyle(
+            alignment = LineHeightStyle.Alignment.Center,
+            trim = LineHeightStyle.Trim.None,
+        ),
+    )
+
     val headingRegular20: TextStyle = TextStyle(
         fontFamily = fontFamily,
         fontSize = 20.sp,
