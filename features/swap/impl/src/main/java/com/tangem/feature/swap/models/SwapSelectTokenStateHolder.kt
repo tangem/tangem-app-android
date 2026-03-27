1b77bf35a39efa240688f0667baac04a19be0f6d
Tangem <git@tangem.com>
2026-03-27 06:24:04 +0300

    Updated on 2026-08-19

--- features/swap/impl/src/main/java/com/tangem/feature/swap/models/SwapSelectTokenStateHolder.kt
+++ features/swap/impl/src/main/java/com/tangem/feature/swap/models/SwapSelectTokenStateHolder.kt
@@ -1,43 +1,20 @@
 package com.tangem.feature.swap.models
 
-import com.tangem.core.ui.components.currency.icon.CurrencyIconState
+import androidx.compose.runtime.Immutable
 import com.tangem.core.ui.components.tokenlist.state.TokensListItemUM
-import com.tangem.core.ui.extensions.TextReference
 import com.tangem.feature.swap.models.market.state.SwapMarketState
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 
 internal data class SwapSelectTokenStateHolder(
-    val availableTokens: ImmutableList<TokenToSelectState>,
-    val unavailableTokens: ImmutableList<TokenToSelectState>,
-    val marketsState: SwapMarketState? = null,
+    val marketsState: SwapMarketState,
     val tokensListData: TokenListUMData,
     val isBalanceHidden: Boolean,
     val isAfterSearch: Boolean,
     val onSearchEntered: (String) -> Unit,
-    val onTokenSelected: (String) -> Unit,
 )
 
-internal sealed class TokenToSelectState {
-
-    data class Title(val title: TextReference) : TokenToSelectState()
-
-    data class TokenToSelect(
-        val id: String,
-        val name: String,
-        val symbol: String,
-        val tokenIcon: CurrencyIconState,
-        val isAvailable: Boolean = true,
-        val addedTokenBalanceData: TokenBalanceData? = null,
-    ) : TokenToSelectState()
-}
-
-internal data class TokenBalanceData(
-    val amount: String?,
-    val amountEquivalent: String?,
-    val isBalanceHidden: Boolean,
-)
-
+@Immutable
 internal sealed interface TokenListUMData {
 
     val tokensList: ImmutableList<TokensListItemUM>
@@ -64,11 +41,11 @@ internal sealed interface TokenListUMData {
 }
 
 internal val SwapSelectTokenStateHolder.isNotFoundState: Boolean
-    get() = availableTokens.isEmpty() && unavailableTokens.isEmpty() &&
+    get() =
         tokensListData.tokensList.isEmpty() && isAfterSearch &&
-        marketsState !is SwapMarketState.Content && marketsState !is SwapMarketState.Loading
+            marketsState !is SwapMarketState.Content && marketsState !is SwapMarketState.Loading
 
 internal val SwapSelectTokenStateHolder.isEmptyState: Boolean
-    get() = availableTokens.isEmpty() && unavailableTokens.isEmpty() &&
+    get() =
         tokensListData.tokensList.isEmpty() && !isAfterSearch &&
-        marketsState !is SwapMarketState.Content && marketsState !is SwapMarketState.Loading
+            marketsState !is SwapMarketState.Content && marketsState !is SwapMarketState.Loading
