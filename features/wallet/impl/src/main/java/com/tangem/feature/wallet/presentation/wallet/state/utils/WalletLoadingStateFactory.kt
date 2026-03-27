d812263d01a14075ffddd12b11382851f0e0e0ad
Tangem <git@tangem.com>
2026-03-27 08:23:48 +0500

    Updated on 2026-09-25

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/utils/WalletLoadingStateFactory.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/state/utils/WalletLoadingStateFactory.kt
@@ -69,7 +69,7 @@ internal class WalletLoadingStateFactory(
                 is UserWallet.Cold -> WalletType.Cold
                 is UserWallet.Hot -> WalletType.Hot
             },
-            tangemPayState = TangemPayState.Empty,
+            tangemPayMainUM = TangemPayMainUM.Empty,
         )
     }
 
