fd54539913e43fad786b0b0a24f6441c9dd5a31d
Tangem <git@tangem.com>
2026-03-30 14:35:20 +0700

    Updated on 2026-05-19

--- data/transaction/src/main/java/com/tangem/data/transaction/di/TransactionDataModule.kt
+++ data/transaction/src/main/java/com/tangem/data/transaction/di/TransactionDataModule.kt
@@ -3,15 +3,18 @@ package com.tangem.data.transaction.di
 import com.tangem.data.common.currency.ResponseCryptoCurrenciesFactory
 import com.tangem.data.transaction.DefaultFeeRepository
 import com.tangem.data.transaction.DefaultGaslessTransactionRepository
+import com.tangem.data.transaction.DefaultMemoValidatorFacade
 import com.tangem.data.transaction.DefaultTransactionRepository
 import com.tangem.data.transaction.DefaultWalletAddressServiceRepository
 import com.tangem.data.transaction.error.DefaultFeeErrorResolver
+import com.tangem.blockchainsdk.BlockchainSDKFactory
 import com.tangem.datasource.api.gasless.GaslessTxServiceApi
 import com.tangem.datasource.api.tangemTech.TangemTechApi
 import com.tangem.datasource.local.walletmanager.WalletManagersStore
 import com.tangem.domain.demo.models.DemoConfig
 import com.tangem.domain.transaction.FeeRepository
 import com.tangem.domain.transaction.GaslessTransactionRepository
+import com.tangem.domain.transaction.MemoValidatorFacade
 import com.tangem.domain.transaction.TransactionRepository
 import com.tangem.domain.transaction.WalletAddressServiceRepository
 import com.tangem.domain.transaction.error.FeeErrorResolver
@@ -54,12 +57,26 @@ internal object TransactionDataModule {
 
     @Provides
     @Singleton
+    fun providesMemoValidatorFacade(
+        blockchainSDKFactory: BlockchainSDKFactory,
+        coroutineDispatcherProvider: CoroutineDispatcherProvider,
+    ): MemoValidatorFacade {
+        return DefaultMemoValidatorFacade(
+            blockchainSDKFactory = blockchainSDKFactory,
+            dispatchers = coroutineDispatcherProvider,
+        )
+    }
+
+    @Provides
+    @Singleton
     fun providesWalletAddressServiceRepository(
         walletManagersFacade: WalletManagersFacade,
+        memoValidatorFacade: MemoValidatorFacade,
         coroutineDispatcherProvider: CoroutineDispatcherProvider,
     ): WalletAddressServiceRepository {
         return DefaultWalletAddressServiceRepository(
             walletManagersFacade = walletManagersFacade,
+            memoValidatorFacade = memoValidatorFacade,
             dispatchers = coroutineDispatcherProvider,
         )
     }
