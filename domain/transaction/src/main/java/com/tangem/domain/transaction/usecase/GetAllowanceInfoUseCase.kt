495fe1ce87123e3d170892d74d4bff19b5c9690a
Tangem <git@tangem.com>
2026-03-31 16:03:56 +0500

    Updated on 2026-06-24

--- domain/transaction/src/main/java/com/tangem/domain/transaction/usecase/GetAllowanceInfoUseCase.kt
+++ domain/transaction/src/main/java/com/tangem/domain/transaction/usecase/GetAllowanceInfoUseCase.kt
@@ -4,12 +4,13 @@ import arrow.core.Either
 import com.tangem.domain.models.currency.CryptoCurrency
 import com.tangem.domain.models.wallet.UserWalletId
 import com.tangem.domain.transaction.AllowanceRepository
+import com.tangem.domain.transaction.models.AllowanceInfo
 import java.math.BigDecimal
 
 /**
- * Use case for retrieving the current allowance for a specific spender.
+ * Use case for retrieving the allowance information for a specific spender and required amount.
  */
-class GetAllowanceUseCase(
+class GetAllowanceInfoUseCase(
     private val allowanceRepository: AllowanceRepository,
 ) {
 
@@ -17,12 +18,14 @@ class GetAllowanceUseCase(
         userWalletId: UserWalletId,
         cryptoCurrency: CryptoCurrency,
         spenderAddress: String,
-    ): Either<Throwable, BigDecimal> {
+        requiredAmount: BigDecimal,
+    ): Either<Throwable, AllowanceInfo> {
         return Either.catch {
-            allowanceRepository.getAllowance(
+            allowanceRepository.getAllowanceInfo(
                 userWalletId = userWalletId,
                 cryptoCurrency = cryptoCurrency,
                 spenderAddress = spenderAddress,
+                requiredAmount = requiredAmount,
             )
         }
     }
