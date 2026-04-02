ab79a4974ca137da7e9e128b3f8faf45474320c9
Tangem <git@tangem.com>
2026-04-02 16:17:48 +0300

    Updated on 2026-06-02

--- features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/model/CloreMigrationModel.kt
+++ features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/model/CloreMigrationModel.kt
@@ -10,18 +10,18 @@ import com.tangem.domain.models.wallet.UserWallet
 import com.tangem.domain.transaction.error.SignCloreMessageError
 import com.tangem.domain.transaction.usecase.SignCloreMessageUseCase
 import com.tangem.feature.tokendetails.presentation.router.InnerTokenDetailsRouter
-import com.tangem.feature.tokendetails.presentation.tokendetails.state.TokenDetailsState
-import com.tangem.feature.tokendetails.presentation.tokendetails.state.factory.TokenDetailsStateFactory
+import com.tangem.feature.tokendetails.presentation.tokendetails.ui.components.clore.CloreMigrationBottomSheetConfig
 import com.tangem.features.tokendetails.impl.R
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 // TODO: Remove after Clore migration ends ([REDACTED_TASK_KEY])
 @Suppress("LongParameterList")
 internal class CloreMigrationModel(
-    private val stateFactory: TokenDetailsStateFactory,
     private val signCloreMessageUseCase: SignCloreMessageUseCase,
     private val clipboardManager: ClipboardManager,
     private val uiMessageSender: UiMessageSender,
@@ -30,10 +30,12 @@ internal class CloreMigrationModel(
     private val cryptoCurrency: CryptoCurrency,
     private val coroutineScope: CoroutineScope,
     private val dispatchers: CoroutineDispatcherProvider,
-    private val onStateUpdate: (TokenDetailsState) -> Unit,
 ) {
 
     private val state = MutableStateFlow(CloreMigrationState())
+
+    private val _bottomSheetConfig = MutableStateFlow(createBottomSheetConfig())
+    val bottomSheetConfig: StateFlow<CloreMigrationBottomSheetConfig> = _bottomSheetConfig.asStateFlow()
 
     fun onCloreMigrationClick() {
         state.value = CloreMigrationState()
@@ -66,7 +68,7 @@ internal class CloreMigrationModel(
                 },
                 ifRight = { signature ->
                     state.value = state.value.copy(signature = signature)
-                    onStateUpdate(stateFactory.getStateWithUpdatedCloreMigrationSignature(signature))
+                    updateBottomSheet()
                 },
             )
         }
@@ -77,28 +79,30 @@ internal class CloreMigrationModel(
     }
 
     private fun updateBottomSheet(isSigningInProgress: Boolean = false) {
+        _bottomSheetConfig.value = createBottomSheetConfig(isSigningInProgress)
+    }
+
+    private fun createBottomSheetConfig(isSigningInProgress: Boolean = false): CloreMigrationBottomSheetConfig {
         val currentState = state.value
-        onStateUpdate(
-            stateFactory.getStateWithCloreMigrationBottomSheet(
-                message = currentState.message,
-                signature = currentState.signature,
-                isSigningInProgress = isSigningInProgress,
-                onMessageChange = { newMessage ->
-                    state.value = state.value.copy(message = newMessage)
-                    updateBottomSheet()
-                },
-                onSignClick = { onCloreSignMessage(state.value.message) },
-                onCopyClick = {
-                    val signature = state.value.signature
-                    if (signature.isNotBlank()) {
-                        clipboardManager.setText(text = signature, isSensitive = false)
-                        uiMessageSender.send(
-                            SnackbarMessage(resourceReference(R.string.wallet_notification_address_copied)),
-                        )
-                    }
-                },
-                onOpenPortalClick = { onOpenCloreClaimPortal() },
-            ),
+        return CloreMigrationBottomSheetConfig(
+            message = currentState.message,
+            signature = currentState.signature,
+            isSigningInProgress = isSigningInProgress,
+            onMessageChange = { newMessage ->
+                state.value = state.value.copy(message = newMessage)
+                updateBottomSheet()
+            },
+            onSignClick = { onCloreSignMessage(state.value.message) },
+            onCopyClick = {
+                val signature = state.value.signature
+                if (signature.isNotBlank()) {
+                    clipboardManager.setText(text = signature, isSensitive = false)
+                    uiMessageSender.send(
+                        SnackbarMessage(resourceReference(R.string.wallet_notification_address_copied)),
+                    )
+                }
+            },
+            onOpenPortalClick = { onOpenCloreClaimPortal() },
         )
     }
 
