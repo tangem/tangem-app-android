ad4a5a90d75e66b062eb0218a34bb4505143f641
Tangem <git@tangem.com>
2026-04-06 21:14:08 +0300

    Updated on 2026-05-04

--- domain/dynamic-addresses/src/main/kotlin/com/tangem/domain/dynamicaddresses/repository/ConsolidationRepository.kt
+++ domain/dynamic-addresses/src/main/kotlin/com/tangem/domain/dynamicaddresses/repository/ConsolidationRepository.kt
@@ -1,21 +1,14 @@
 package com.tangem.domain.dynamicaddresses.repository
 
 import arrow.core.Either
-import com.tangem.blockchain.common.TransactionSigner
-import com.tangem.domain.dynamicaddresses.model.ConsolidationInfo
+import com.tangem.blockchain.common.TransactionData
 import com.tangem.domain.models.network.Network
 import com.tangem.domain.models.wallet.UserWalletId
 
 interface ConsolidationRepository {
 
-    suspend fun getConsolidationInfo(
+    suspend fun createConsolidationTransaction(
         userWalletId: UserWalletId,
         network: Network,
-    ): Either<Throwable, ConsolidationInfo>
-
-    suspend fun sendConsolidationTransaction(
-        userWalletId: UserWalletId,
-        network: Network,
-        signer: TransactionSigner,
-    ): Either<Throwable, String>
+    ): Either<Throwable, TransactionData>
 }
