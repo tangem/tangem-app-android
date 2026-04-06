26cab979a1e84935a31dedb21f7122f209b31ee2
Tangem <git@tangem.com>
2026-04-06 11:40:31 +0200

    Updated on 2026-09-21

--- features/feed/impl/src/main/kotlin/com/tangem/features/feed/ui/search/state/SearchCallbacks.kt
+++ features/feed/impl/src/main/kotlin/com/tangem/features/feed/ui/search/state/SearchCallbacks.kt
@@ -1,8 +1,10 @@
 package com.tangem.features.feed.ui.search.state
 
+import com.tangem.common.ui.markets.models.MarketsListItemUM
+
 internal data class SearchCallbacks(
     val onLoadMore: () -> Unit,
     val onClearHintsClick: () -> Unit,
     val onTextHintClick: (hint: String) -> Unit,
-    val onResultMarketTokenClick: () -> Unit,
+    val onResultMarketTokenClick: (MarketsListItemUM) -> Unit,
 )
