845b7a2cf4a1d34ab22e4c69928e0cb116220268
Tangem <git@tangem.com>
2026-04-06 21:14:08 +0300

    Updated on 2026-07-23

--- domain/dynamic-addresses/models/src/main/kotlin/com/tangem/domain/dynamicaddresses/model/ConsolidationInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.tangem.domain.dynamicaddresses.model
-
-import java.math.BigDecimal
-
-data class ConsolidationInfo(
-    val fee: BigDecimal,
-    val inputCount: Int,
-    val canCoverFee: Boolean,
-)
