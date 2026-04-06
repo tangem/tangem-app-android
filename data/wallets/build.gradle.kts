72103854a0c288e504a5159c215e34c4e1787d72
Tangem <git@tangem.com>
2026-04-06 21:14:08 +0300

    Updated on 2026-06-12

--- data/wallets/build.gradle.kts
+++ data/wallets/build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
     /** Domain */
     implementation(projects.domain.account)
     implementation(projects.domain.card)
+    implementation(projects.domain.dynamicAddresses)
     implementation(projects.domain.models)
     implementation(projects.domain.tokens.models)
     implementation(projects.domain.wallets)
