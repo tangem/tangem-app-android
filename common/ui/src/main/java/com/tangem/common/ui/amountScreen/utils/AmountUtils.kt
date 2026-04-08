43ee50253d364a82d5271e65ea2b0fefd8c002bc
Tangem <git@tangem.com>
2026-04-08 12:30:19 +0400

    Updated on 2026-09-23

--- common/ui/src/main/java/com/tangem/common/ui/amountScreen/utils/AmountUtils.kt
+++ common/ui/src/main/java/com/tangem/common/ui/amountScreen/utils/AmountUtils.kt
@@ -1,10 +1,17 @@
 package com.tangem.common.ui.amountScreen.utils
 
 import androidx.compose.ui.text.input.ImeAction
+import com.tangem.common.ui.R
 import com.tangem.common.ui.amountScreen.models.AmountFieldModel
 import com.tangem.common.ui.amountScreen.models.EnterAmountBoundary
+import com.tangem.core.ui.extensions.TextReference
+import com.tangem.core.ui.extensions.resourceReference
+import com.tangem.core.ui.extensions.wrappedList
+import com.tangem.core.ui.format.bigdecimal.crypto
+import com.tangem.core.ui.format.bigdecimal.format
 import com.tangem.core.ui.utils.parseBigDecimal
 import com.tangem.core.ui.utils.parseToBigDecimal
+import com.tangem.domain.models.currency.CryptoCurrency
 import com.tangem.utils.extensions.isZero
 import java.math.BigDecimal
 import java.math.RoundingMode
@@ -57,3 +64,20 @@ internal fun getKeyboardAction(isCheckFailed: Boolean, decimalCryptoValue: BigDe
     } else {
         ImeAction.None
     }
+
+internal fun getAmountValidationError(
+    isExceedBalance: Boolean,
+    isLessThanMinimum: Boolean,
+    minimumTransactionAmount: EnterAmountBoundary?,
+    cryptoCurrency: CryptoCurrency,
+): TextReference = when {
+    isExceedBalance -> resourceReference(R.string.common_insufficient_balance)
+    isLessThanMinimum -> {
+        val minimumAmount = minimumTransactionAmount?.amount.format { crypto(cryptoCurrency) }
+        resourceReference(
+            R.string.transfer_notification_invalid_minimum_transaction_amount_text,
+            wrappedList(minimumAmount, minimumAmount),
+        )
+    }
+    else -> TextReference.EMPTY
+}
