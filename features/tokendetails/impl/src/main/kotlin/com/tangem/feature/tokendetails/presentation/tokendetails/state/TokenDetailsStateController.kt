a035fef53d5a09f6fcf64df7a4f89a31b24f0885
Tangem <git@tangem.com>
2026-04-09 17:43:43 +0300

    Updated on 2026-07-01

--- features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/TokenDetailsStateController.kt
+++ features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/state/TokenDetailsStateController.kt
@@ -4,7 +4,12 @@ import com.tangem.core.decompose.di.ModelScoped
 import com.tangem.core.ui.components.containers.pullToRefresh.PullToRefreshConfig
 import com.tangem.core.ui.components.currency.icon.CurrencyIconState
 import com.tangem.core.ui.components.marketprice.MarketPriceBlockState
+import com.tangem.core.ui.ds.button.TangemButtonType
+import com.tangem.core.ui.ds.button.TangemButtonUM
+import com.tangem.core.ui.ds.image.TangemIconUM
 import com.tangem.core.ui.extensions.TextReference
+import com.tangem.core.ui.extensions.resourceReference
+import com.tangem.features.tokendetails.impl.R
 import com.tangem.utils.transformer.Transformer
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -37,7 +42,22 @@ internal class TokenDetailsStateController @Inject constructor() {
                 menuItems = persistentListOf(),
             ),
             balanceBlockUM = TokenDetailsBalanceBlockUM.Loading(
-                actionButtons = persistentListOf(),
+                actionButtons = persistentListOf(
+                    TangemButtonUM(
+                        text = resourceReference(R.string.tangempay_card_details_add_funds),
+                        tangemIconUM = TangemIconUM.Icon(iconRes = R.drawable.ic_arrow_down_24),
+                        onClick = { },
+                        isEnabled = true,
+                        type = TangemButtonType.Secondary,
+                    ),
+                    TangemButtonUM(
+                        text = resourceReference(R.string.common_transfer),
+                        tangemIconUM = TangemIconUM.Icon(iconRes = R.drawable.ic_arrow_up_24),
+                        onClick = { },
+                        isEnabled = true,
+                        type = TangemButtonType.Secondary,
+                    ),
+                ),
                 tokenBalanceTypeUM = TokenBalanceTypeUM.Single,
                 currencyIconState = CurrencyIconState.Loading,
             ),
