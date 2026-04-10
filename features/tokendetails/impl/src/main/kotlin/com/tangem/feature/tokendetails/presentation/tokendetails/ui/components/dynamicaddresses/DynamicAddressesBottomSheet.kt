a325cacab7b3721a5e4cd290c3a1cf366ac58953
Tangem <git@tangem.com>
2026-04-10 11:10:57 +0300

    Updated on 2026-08-19

--- features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/ui/components/dynamicaddresses/DynamicAddressesBottomSheet.kt
+++ features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/ui/components/dynamicaddresses/DynamicAddressesBottomSheet.kt
@@ -4,12 +4,14 @@ import androidx.compose.runtime.Composable
 import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfig
 import com.tangem.core.ui.components.bottomsheets.modal.TangemModalBottomSheet
 import com.tangem.core.ui.components.bottomsheets.modal.TangemModalBottomSheetTitle
+import com.tangem.core.ui.res.TangemTheme
 import com.tangem.core.ui.R as CoreR
 
 @Composable
 internal fun DynamicAddressesBottomSheet(config: TangemBottomSheetConfig) {
     TangemModalBottomSheet<DynamicAddressesBottomSheetConfig>(
         config = config,
+        containerColor = TangemTheme.colors.background.tertiary,
         title = {
             TangemModalBottomSheetTitle(
                 endIconRes = CoreR.drawable.ic_close_24,
@@ -19,10 +21,18 @@ internal fun DynamicAddressesBottomSheet(config: TangemBottomSheetConfig) {
     ) { content ->
         when (content) {
             is DynamicAddressesBottomSheetConfig.Enable -> DynamicAddressesEnableContent(content = content)
-            is DynamicAddressesBottomSheetConfig.Unavailable -> DynamicAddressesUnavailableContent(content = content)
-            is DynamicAddressesBottomSheetConfig.ServiceUnavailable -> DynamicAddressesServiceUnavailableContent(
-                content = content,
-            )
+            is DynamicAddressesBottomSheetConfig.DisableWithoutConsolidation -> {
+                DynamicAddressesDisableWithoutConsolidationContent(content = content)
+            }
+            is DynamicAddressesBottomSheetConfig.DisableWithConsolidation -> {
+                DynamicAddressesDisableWithConsolidationContent(content = content)
+            }
+            is DynamicAddressesBottomSheetConfig.ConflictingCustomTokens -> {
+                DynamicAddressesConflictingCustomTokensContent(content = content)
+            }
+            is DynamicAddressesBottomSheetConfig.ServiceUnavailable -> {
+                DynamicAddressesServiceUnavailableContent(content = content)
+            }
         }
     }
 }
