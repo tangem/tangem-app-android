bde5cedf7e7e1b27863183dbb3822da48c43a577
Tangem <git@tangem.com>
2026-04-14 15:45:39 +0400

    Updated on 2026-06-05

--- domain/account/src/main/java/com/tangem/domain/account/repository/AccountsExpandedRepository.kt
+++ domain/account/src/main/java/com/tangem/domain/account/repository/AccountsExpandedRepository.kt
@@ -12,4 +12,13 @@ interface AccountsExpandedRepository {
     suspend fun syncStore(walletId: UserWalletId, existAccounts: Set<AccountId>)
     suspend fun clearStore()
     suspend fun update(accountState: AccountExpandedState)
+
+    interface Factory {
+        fun create(storeFileName: String): AccountsExpandedRepository
+    }
+
+    companion object {
+        const val MAIN_STORE_FILE_NAME = "account_expanded_store"
+        const val CHOOSE_TOKEN_FILE_NAME = "choose_token_account_expanded_store"
+    }
 }
