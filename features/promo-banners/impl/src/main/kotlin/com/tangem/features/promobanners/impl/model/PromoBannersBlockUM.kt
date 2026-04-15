4337ede15889d3d8b82ebd9e29ff467471f943d8
Tangem <git@tangem.com>
2026-04-15 19:00:32 +0300

    Updated on 2026-09-23

--- features/promo-banners/impl/src/main/kotlin/com/tangem/features/promobanners/impl/model/PromoBannersBlockUM.kt
+++ features/promo-banners/impl/src/main/kotlin/com/tangem/features/promobanners/impl/model/PromoBannersBlockUM.kt
@@ -1,5 +1,6 @@
 package com.tangem.features.promobanners.impl.model
 
+import com.tangem.features.promobanners.api.PromoBannersBlockComponent
 import kotlinx.collections.immutable.ImmutableList
 
 internal data class PromoBannersBlockUM(
@@ -7,6 +8,7 @@ internal data class PromoBannersBlockUM(
     val initialPage: Int,
     val banners: ImmutableList<PromoBannerNotificationUM>,
     val isVisibleOnScreen: Boolean,
+    val placeholder: PromoBannersBlockComponent.Placeholder,
     val onBannerShown: (displayId: Int) -> Unit,
     val onCarouselScrolled: (displayId: Int) -> Unit,
     val onPageChanged: (displayId: Int) -> Unit,
