77a12bcb1394fbff52bc9558f6add12e7e3b8791
Tangem <git@tangem.com>
2026-04-16 16:23:08 +0400

    Updated on 2026-08-14

--- features/swap/impl/src/main/java/com/tangem/feature/swap/choosetoken/impl/model/ChooseTokenModel.kt
+++ features/swap/impl/src/main/java/com/tangem/feature/swap/choosetoken/impl/model/ChooseTokenModel.kt
@@ -5,27 +5,20 @@ import com.tangem.core.decompose.di.ModelScoped
 import com.tangem.core.decompose.model.Model
 import com.tangem.core.decompose.model.ParamsContainer
 import com.tangem.core.ui.components.fields.entity.SearchBarUM
-import com.tangem.core.ui.extensions.TextReference
 import com.tangem.core.ui.extensions.resourceReference
-import com.tangem.core.ui.extensions.stringReference
 import com.tangem.domain.models.account.AccountId
-import com.tangem.domain.models.wallet.UserWallet
-import com.tangem.domain.models.wallet.UserWalletId
-import com.tangem.domain.wallets.usecase.GetWalletsUseCase
 import com.tangem.feature.swap.choosetoken.api.*
 import com.tangem.feature.swap.choosetoken.api.ChooseTokenBridgeInternal.SearchQuery
 import com.tangem.feature.swap.choosetoken.api.ChooseTokenBridgeInternal.SearchQuery.Companion.isSearchingState
 import com.tangem.feature.swap.choosetoken.impl.converter.SearchBarToggleTransformer
 import com.tangem.feature.swap.choosetoken.impl.converter.SearchBarUpdateQueryTransformer
-import com.tangem.feature.swap.choosetoken.impl.ui.*
+import com.tangem.feature.swap.choosetoken.impl.ui.ChooseTokenFullUM
+import com.tangem.feature.swap.choosetoken.impl.ui.ChooseTokenInitialUM
 import com.tangem.feature.swap.converters.TokensDataConverter
 import com.tangem.feature.swap.models.SwapSelectTokenStateHolder
 import com.tangem.feature.swap.models.market.state.SwapMarketState
 import com.tangem.feature.swap.presentation.R
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
-import kotlinx.collections.immutable.persistentListOf
-import kotlinx.collections.immutable.toPersistentList
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
@@ -34,7 +27,6 @@ import javax.inject.Inject
 internal class ChooseTokenModel @Inject constructor(
     override val dispatchers: CoroutineDispatcherProvider,
     private val settingContextUseCase: SettingContextUseCase,
-    private val getWalletsUseCase: GetWalletsUseCase,
     marketBlockDelegateFactory: MarketBlockDelegate.Factory,
     paramsContainer: ParamsContainer,
 ) : Model() {
@@ -61,25 +53,29 @@ internal class ChooseTokenModel @Inject constructor(
     }
 
     private val expandedAccountsFlow: MutableStateFlow<Map<AccountId, Boolean>> = MutableStateFlow(emptyMap())
-    private val onWalletSelected = Channel<UserWalletId>()
 
     val stateOld: StateFlow<SwapSelectTokenStateHolder?> = combineUIOld()
 
-    private val contentState: StateFlow<ChooseTokenUM?> = combineUI()
     private val initialState: MutableStateFlow<ChooseTokenInitialUM> = MutableStateFlow(getInitState())
     val state: StateFlow<ChooseTokenFullUM> = combine(
         flow = initialState,
-        flow2 = contentState,
-        transform = { initial, content ->
+        flow2 = bridge.fullPortfolioBlock,
+        flow3 = marketsStateFlow,
+        transform = { initial, content, marketBlock ->
             ChooseTokenFullUM(
                 initialUM = initial,
-                contentUM = content,
+                portfolioBlock = content,
+                marketsBlock = marketBlock,
             )
         },
     ).stateIn(
         scope = modelScope,
         started = SharingStarted.Eagerly,
-        initialValue = ChooseTokenFullUM(initialState.value, contentState.value),
+        initialValue = ChooseTokenFullUM(
+            initialUM = initialState.value,
+            portfolioBlock = bridge.fullPortfolioBlock.value,
+            marketsBlock = null,
+        ),
     )
 
     init {
@@ -140,68 +136,6 @@ internal class ChooseTokenModel @Inject constructor(
             ).transform()
         },
     )
-        .flowOn(dispatchers.default)
-        .stateIn(modelScope, SharingStarted.Eagerly, initialValue = null)
-
-    @Suppress("LongMethod")
-    private fun combineUI(): StateFlow<ChooseTokenUM?> = channelFlow {
-        val allWalletsFlow: StateFlow<Map<UserWalletId, UserWallet>> =
-            getWalletsUseCase.invokeAsMap().stateIn(this)
-
-        // todo swap add optional param, store, and GetSelectedWalletUseCase
-        val firstSelectedWallet = allWalletsFlow.value.values.first()
-        val selectedWalletFlow: StateFlow<UserWallet> =
-            onWalletSelected.receiveAsFlow()
-                .mapNotNull { walletId -> allWalletsFlow.value[walletId] }
-                .stateIn(this, SharingStarted.Eagerly, firstSelectedWallet)
-
-        val fullPortfolioBlockFlow = combine(
-            flow = allWalletsFlow,
-            flow2 = bridge.portfolioListBlock,
-            flow3 = selectedWalletFlow.map { wallet -> wallet.walletId }.distinctUntilChanged(),
-            transform = { allWallets, portfolioList, selectedWalletId ->
-                val tokensListData = portfolioList[selectedWalletId] ?: return@combine null
-                val walletsUM = allWallets.entries
-                    .map { (walletId, wallet) ->
-                        val searchResultCount: TextReference? = portfolioList[walletId]?.totalTokensCount
-                            ?.toString()
-                            ?.let(::stringReference)
-                            ?.takeIf { isSearchingState }
-                        WalletTabUM(
-                            text = stringReference(wallet.name),
-                            onClick = { onWalletSelected.trySend(walletId) },
-                            isSelected = selectedWalletId == walletId,
-                            count = searchResultCount,
-                        )
-                    }
-                walletsUM to tokensListData
-            },
-        )
-            .filterNotNull()
-            .distinctUntilChanged()
-
-        combine(
-            flow = fullPortfolioBlockFlow,
-            flow2 = settingContextUseCase.invoke(),
-            flow3 = marketsStateFlow,
-            transform = { (walletList, tokensData), settings, marketsData ->
-                val walletsUM = if (walletList.size != 1) {
-                    WalletListUM(walletList.toPersistentList())
-                } else {
-                    WalletListUM(persistentListOf())
-                }
-                ChooseTokenUM(
-                    walletList = walletsUM,
-                    isBalanceHidden = settings.isBalanceHidden,
-                    isSearching = isSearchingState,
-                    tokensListData = tokensData,
-                    marketsState = marketsData,
-                )
-            },
-        )
-            .distinctUntilChanged()
-            .collect { newUM -> channel.send(newUM) }
-    }
         .flowOn(dispatchers.default)
         .stateIn(modelScope, SharingStarted.Eagerly, initialValue = null)
 
