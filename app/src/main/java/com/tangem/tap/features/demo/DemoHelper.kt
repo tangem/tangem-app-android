c9fb017264a4c25fe48ce3f976f0f03205e6304a
Tangem <git@tangem.com>
2026-04-17 14:39:32 +0400

    Updated on 2026-07-01

--- app/src/main/java/com/tangem/tap/features/demo/DemoHelper.kt
+++ app/src/main/java/com/tangem/tap/features/demo/DemoHelper.kt
@@ -2,7 +2,6 @@ package com.tangem.tap.features.demo
 
 import com.tangem.domain.demo.models.DemoConfig
 import com.tangem.domain.models.scan.ScanResponse
-import com.tangem.tap.common.redux.AppState
 
 object DemoHelper {
     val config = DemoConfig
@@ -12,15 +11,4 @@ object DemoHelper {
     fun isTestDemoCard(scanResponse: ScanResponse): Boolean = config.isTestDemoCardId(scanResponse.card.cardId)
 
     fun isDemoCardId(cardId: String): Boolean = config.isDemoCardId(cardId)
-
-    fun tryHandle(appState: () -> AppState?): Boolean {
-        val scanResponse = getScanResponse(appState) ?: return false
-        if (!scanResponse.isDemoCard()) return false
-
-        return false
-    }
-
-    private fun getScanResponse(appState: () -> AppState?): ScanResponse? {
-        return appState()?.globalState?.scanResponse
-    }
 }
