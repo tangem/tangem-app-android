dbc5d574f73330f0da9d01eccc0a27f2566bb880
Tangem <git@tangem.com>
2026-04-21 13:10:18 +0400

    Updated on 2026-06-10

--- app/src/androidTest/kotlin/com/tangem/screens/accounts/AccountDetailsPageObject.kt
+++ app/src/androidTest/kotlin/com/tangem/screens/accounts/AccountDetailsPageObject.kt
@@ -1,8 +1,8 @@
-package com.tangem.screens
+package com.tangem.screens.accounts
 
 import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
 import com.tangem.common.BaseTestCase
-import com.tangem.core.ui.test.AccountDetailsScreenTestTags
+import com.tangem.core.ui.test.accounts.AccountDetailsScreenTestTags
 import com.tangem.core.ui.test.TopAppBarTestTags
 import io.github.kakaocup.compose.node.element.ComposeScreen
 import io.github.kakaocup.compose.node.element.ComposeScreen.Companion.onComposeScreen
@@ -11,6 +11,10 @@ import io.github.kakaocup.compose.node.element.KNode
 class AccountDetailsPageObject(semanticsProvider: SemanticsNodeInteractionsProvider) :
     ComposeScreen<AccountDetailsPageObject>(semanticsProvider = semanticsProvider) {
 
+    val screenContainer: KNode = child {
+        hasTestTag(AccountDetailsScreenTestTags.ACCOUNT_DETAILS_CONTAINER)
+    }
+
     val topAppBarBackButton: KNode = child {
         hasTestTag(TopAppBarTestTags.CLOSE_BUTTON)
     }
@@ -18,7 +22,16 @@ class AccountDetailsPageObject(semanticsProvider: SemanticsNodeInteractionsProvi
     val manageTokensButton: KNode = child {
         hasTestTag(AccountDetailsScreenTestTags.MANAGE_TOKENS_BUTTON)
     }
+
+    val archiveAccountButton: KNode = child {
+        hasTestTag(AccountDetailsScreenTestTags.ARCHIVE_ACCOUNT_BUTTON)
+    }
+
+    val editAccountButton: KNode = child {
+        hasTestTag(AccountDetailsScreenTestTags.EDIT_ACCOUNT_BUTTON)
+    }
+
 }
 
-internal fun BaseTestCase.onAccountDetails(function: AccountDetailsPageObject.() -> Unit) =
+internal fun BaseTestCase.onAccountDetailsScreen(function: AccountDetailsPageObject.() -> Unit) =
     onComposeScreen(composeTestRule, function)
