9e2a8f018ea43b9d4a71f9fb7239f5e6df32daa8
Tangem <git@tangem.com>
2026-04-21 14:29:35 +0300

    Updated on 2026-06-12

---
+++ core/ui/src/main/java/com/tangem/core/ui/res/generated/TangemDimensionTokens.kt
@@ -0,0 +1,72 @@
+package com.tangem.core.ui.res.generated
+
+import androidx.compose.ui.unit.dp
+
+/**
+ * Auto-generated from design tokens. Do not edit manually.
+ */
+internal class TangemDimensionTokens {
+    // blur
+    val blurSizeCard = 48.dp
+    val blurSizeButton = 32.dp
+
+    // border-radius
+    val borderRadius100 = 8.dp
+    val borderRadius150 = 12.dp
+    val borderRadius200 = 16.dp
+    val borderRadius250 = 20.dp
+    val borderRadius300 = 24.dp
+    val borderRadius400 = 32.dp
+    val borderRadiusNone = 0.dp
+    val borderRadius075 = 6.dp
+    val borderRadius050 = 4.dp
+    val borderRadiusFull = 999.dp
+
+    // border-width
+    val borderWidthNone = 0.dp
+    val borderWidthXs = 0.5.dp
+    val borderWidthSm = 1.dp
+    val borderWidthMd = 2.dp
+    val borderWidthLg = 4.dp
+
+    // size
+    val size100 = 8.dp
+    val size125 = 10.dp
+    val size150 = 12.dp
+    val size200 = 16.dp
+    val size250 = 20.dp
+    val size300 = 24.dp
+    val size350 = 28.dp
+    val size400 = 32.dp
+    val size450 = 36.dp
+    val size500 = 40.dp
+    val size550 = 44.dp
+    val size600 = 48.dp
+    val size700 = 56.dp
+    val size800 = 64.dp
+    val size1000 = 80.dp
+    val size1100 = 88.dp
+    val size1200 = 96.dp
+    val size025 = 2.dp
+    val size050 = 4.dp
+    val sizeCardSm = 128.dp
+
+    // spacing
+    val spacing100 = 8.dp
+    val spacing150 = 12.dp
+    val spacing200 = 16.dp
+    val spacing250 = 20.dp
+    val spacing300 = 24.dp
+    val spacing350 = 28.dp
+    val spacing400 = 32.dp
+    val spacing450 = 36.dp
+    val spacing500 = 40.dp
+    val spacing550 = 44.dp
+    val spacing600 = 48.dp
+    val spacing700 = 56.dp
+    val spacing800 = 64.dp
+    val spacing1000 = 80.dp
+    val spacing025 = 2.dp
+    val spacing050 = 4.dp
+    val spacingNone = 0.dp
+}
