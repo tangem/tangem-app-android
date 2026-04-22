374d02f926b93509f66c29214183641d820f4c28
Tangem <git@tangem.com>
2026-04-22 11:55:20 +0400

    Updated on 2026-08-27

--- app/src/main/java/com/tangem/tap/data/DefaultAppInfoProvider.kt
+++ app/src/main/java/com/tangem/tap/data/DefaultAppInfoProvider.kt
@@ -1,26 +1,27 @@
-package com.tangem.datasource.info
+package com.tangem.tap.data
 
 import android.os.Build
 import com.tangem.utils.info.AppInfoProvider
-import com.tangem.utils.version.AppVersionProvider
-import java.util.*
+import com.tangem.wallet.BuildConfig
+import java.util.Locale
+import java.util.TimeZone
 import javax.inject.Inject
 
-internal class AndroidAppInfoProvider @Inject constructor(
-    private val appVersionProvider: AppVersionProvider,
-) : AppInfoProvider {
+internal class DefaultAppInfoProvider @Inject constructor() : AppInfoProvider {
     override val platform: String
         get() = "Android"
     override val device: String
         get() = "${Build.MANUFACTURER} ${Build.MODEL}"
     override val osVersion: String
         get() = Build.VERSION.RELEASE
+    override val sdkVersion: Int
+        get() = Build.VERSION.SDK_INT
     override val language: String
-        get() = Locale.getDefault().language
+        get() = Locale.getDefault().toLanguageTag()
     override val timezone: String
         get() = TimeZone.getDefault().id
-    override val appVersion: String
-        get() = appVersionProvider.versionName
+    override val appVersion: String = BuildConfig.VERSION_NAME
+    override val appVersionCode: Int = BuildConfig.VERSION_CODE
     override val isHuaweiDevice: Boolean
         get() = Build.MANUFACTURER.equals("HUAWEI", ignoreCase = true) ||
             Build.BRAND.equals("HUAWEI", ignoreCase = true)
