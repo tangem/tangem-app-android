279a6bba59dd4c9fcf5a1d4ffa2f153e435ad83c
Tangem <git@tangem.com>
2026-04-24 05:01:25 -0700

    Updated on 2026-09-01

--- core/datasource/src/main/java/com/tangem/datasource/api/pay/models/request/UpdateCardDisplayNameRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.tangem.datasource.api.pay.models.request
-
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
-
-@JsonClass(generateAdapter = true)
-data class UpdateCardDisplayNameRequest(
-    @Json(name = "display_name") val displayName: String,
-)
