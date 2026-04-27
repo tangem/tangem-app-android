c0f56c763c745450e7c8e90dbb3e911a797d494d
Tangem <git@tangem.com>
2026-04-27 13:44:10 +0200

    Updated on 2026-07-23

--- features/common-features/impl/src/main/java/com/tangem/features/commonfeatures/impl/addtoportfolio/ui/state/TokenActionsUM.kt
+++ features/common-features/impl/src/main/java/com/tangem/features/commonfeatures/impl/addtoportfolio/ui/state/TokenActionsUM.kt
@@ -2,9 +2,12 @@ package com.tangem.features.commonfeatures.impl.addtoportfolio.ui.state
 
 import com.tangem.common.ui.markets.action.QuickActions
 import com.tangem.core.ui.components.token.state.TokenItemState
+import com.tangem.core.ui.ds.badge.TangemBadgeUM
 
 internal data class TokenActionsUM(
     val token: TokenItemState,
     val quickActions: QuickActions,
     val onLaterClick: () -> Unit,
+    val isBalancesHidden: Boolean = false,
+    val portfolioBadge: TangemBadgeUM? = null,
 )
