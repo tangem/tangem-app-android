3900453fca63586c42b1beb6228cb3e2e036b11c
Tangem <git@tangem.com>
2026-04-28 13:07:24 +0400

    Updated on 2026-07-07

--- features/common-features/impl/src/main/java/com/tangem/features/commonfeatures/impl/addtoportfolio/TokenActionsComponent.kt
+++ features/common-features/impl/src/main/java/com/tangem/features/commonfeatures/impl/addtoportfolio/TokenActionsComponent.kt
@@ -72,7 +72,7 @@ internal class TokenActionsComponent @AssistedInject constructor(
     )
 
     data class Params(
-        val eventBuilder: Flow<PortfolioAnalyticsEvent.EventBuilder>,
+        val eventBuilder: PortfolioAnalyticsEvent.EventBuilder,
         val data: Flow<CryptoCurrencyData>,
         val callbacks: Callbacks,
     )
