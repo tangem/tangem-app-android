d98841d30772b4888eff10f6fdba0a77d71f9806
Tangem <git@tangem.com>
2026-04-28 17:03:15 +0400

    Updated on 2026-06-02

--- features/feed/impl/src/main/kotlin/com/tangem/features/feed/components/feed/FeedBottomSheetRoute.kt
+++ features/feed/impl/src/main/kotlin/com/tangem/features/feed/components/feed/FeedBottomSheetRoute.kt
@@ -2,12 +2,10 @@ package com.tangem.features.feed.components.feed
 
 import com.tangem.features.feed.components.earn.EarnNetworkFilterComponent
 import com.tangem.features.feed.components.earn.EarnTypeFilterComponent
-import com.tangem.features.commonfeatures.api.addtoportfolio.AddToPortfolioPreselectedDataComponent
 
 internal sealed interface FeedBottomSheetRoute {
 
     data class AddToPortfolio(
-        val tokenToAdd: AddToPortfolioPreselectedDataComponent.TokenToAdd,
         val source: String,
     ) : FeedBottomSheetRoute
 
