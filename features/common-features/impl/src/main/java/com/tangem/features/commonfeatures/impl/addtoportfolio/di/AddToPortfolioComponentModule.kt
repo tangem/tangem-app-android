d64ae899e67b73c93d30086465f97158f2c038ef
Tangem <git@tangem.com>
2026-04-28 17:03:15 +0400

    Updated on 2026-09-14

--- features/common-features/impl/src/main/java/com/tangem/features/commonfeatures/impl/addtoportfolio/di/AddToPortfolioComponentModule.kt
+++ features/common-features/impl/src/main/java/com/tangem/features/commonfeatures/impl/addtoportfolio/di/AddToPortfolioComponentModule.kt
@@ -2,9 +2,7 @@ package com.tangem.features.commonfeatures.impl.addtoportfolio.di
 
 import com.tangem.features.commonfeatures.api.addtoportfolio.AddToPortfolioComponent
 import com.tangem.features.commonfeatures.api.addtoportfolio.AddToPortfolioManager
-import com.tangem.features.commonfeatures.api.addtoportfolio.AddToPortfolioPreselectedDataComponent
 import com.tangem.features.commonfeatures.impl.addtoportfolio.DefaultAddToPortfolioComponent
-import com.tangem.features.commonfeatures.impl.addtoportfolio.DefaultAddToPortfolioPreselectedDataComponent
 import com.tangem.features.commonfeatures.impl.addtoportfolio.ui.DefaultAddToPortfolioManager
 import com.tangem.features.commonfeatures.impl.addtoportfolio.userportfolio.DefaultUserPortfolioComponent
 import com.tangem.features.commonfeatures.impl.addtoportfolio.userportfolio.UserPortfolioComponent
@@ -22,11 +20,6 @@ internal interface AddToPortfolioComponentModule {
 
     @Binds
     fun bindAddToPortfolioManagerFactory(factory: DefaultAddToPortfolioManager.Factory): AddToPortfolioManager.Factory
-
-    @Binds
-    fun bindAddToPortfolioPreselectedDataComponent(
-        factory: DefaultAddToPortfolioPreselectedDataComponent.Factory,
-    ): AddToPortfolioPreselectedDataComponent.Factory
 
     @Binds
     fun bindUserPortfolioComponentFactory(
