d657825629033c7d2ce497a72de5413d96a33848
Tangem <git@tangem.com>
2026-04-28 17:32:04 +0500

    Updated on 2026-06-12

--- features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/entity/TangemPayDetailsStateFactory.kt
+++ features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/entity/TangemPayDetailsStateFactory.kt
@@ -1,19 +1,15 @@
 package com.tangem.features.tangempay.entity
 
-import androidx.compose.ui.unit.dp
 import com.tangem.core.ui.components.buttons.actions.ActionButtonConfig
 import com.tangem.core.ui.components.containers.pullToRefresh.PullToRefreshConfig
 import com.tangem.core.ui.components.dropdownmenu.TangemDropdownMenuItem
-import com.tangem.core.ui.components.notifications.NotificationConfig
 import com.tangem.core.ui.extensions.resourceReference
 import com.tangem.core.ui.extensions.themedColor
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.domain.visa.model.TangemPayCardFrozenState
 import com.tangem.features.tangempay.details.impl.R
-import com.tangem.features.tangempay.model.transformers.TangemPayCardFrozenStateConverter
 import com.tangem.features.tangempay.utils.TangemPayDetailIntents
 import kotlinx.collections.immutable.persistentListOf
-import kotlinx.collections.immutable.toPersistentList
 
 @Suppress("LongParameterList")
 internal class TangemPayDetailsStateFactory(
@@ -21,53 +17,26 @@ internal class TangemPayDetailsStateFactory(
     private val onOpenMenu: () -> Unit,
     private val intents: TangemPayDetailIntents,
     private val cardFrozenState: TangemPayCardFrozenState,
-    private val converter: TangemPayCardFrozenStateConverter,
 ) {
 
     @Suppress("LongMethod")
-    fun getInitialState(): TangemPayDetailsUM {
-        val cardFrozenStateItem = when (cardFrozenState) {
-            is TangemPayCardFrozenState.Pending -> null
-            is TangemPayCardFrozenState.Frozen -> TangemPayDetailsTopBarMenuItem(
-                type = TangemPayDetailsTopBarMenuItemType.UnfreezeCard,
-                dropdownItem = TangemDropdownMenuItem(
-                    title = resourceReference(R.string.tangempay_card_details_unfreeze_card),
-                    textColor = themedColor { TangemTheme.colors.text.primary1 },
-                    onClick = intents::onClickUnfreezeCard,
-                ),
-            )
-            is TangemPayCardFrozenState.Unfrozen -> TangemPayDetailsTopBarMenuItem(
-                type = TangemPayDetailsTopBarMenuItemType.FreezeCard,
-                dropdownItem = TangemDropdownMenuItem(
-                    title = resourceReference(R.string.tangempay_card_details_freeze_card),
-                    textColor = themedColor { TangemTheme.colors.text.primary1 },
-                    onClick = intents::onClickFreezeCard,
-                ),
-            )
-        }
+    fun getInitialState(cardNumberEnd: String): TangemPayDetailsUM {
         return TangemPayDetailsUM(
             topBarConfig = TangemPayDetailsTopBarConfig(
                 onBackClick = onBack,
                 onOpenMenu = onOpenMenu,
-                items = listOfNotNull(
-                    TangemPayDetailsTopBarMenuItem(
-                        type = TangemPayDetailsTopBarMenuItemType.ChangePin,
-                        dropdownItem = TangemDropdownMenuItem(
-                            title = resourceReference(R.string.tangempay_card_details_pin_code),
-                            textColor = themedColor { TangemTheme.colors.text.primary1 },
-                            onClick = intents::onClickPinCode,
-                        ),
+                items = persistentListOf(
+                    TangemDropdownMenuItem(
+                        title = resourceReference(R.string.tangem_pay_terms_limits),
+                        textColor = themedColor { TangemTheme.colors.text.primary1 },
+                        onClick = intents::onClickTermsAndLimits,
                     ),
-                    TangemPayDetailsTopBarMenuItem(
-                        type = TangemPayDetailsTopBarMenuItemType.TermsAndLimits,
-                        dropdownItem = TangemDropdownMenuItem(
-                            title = resourceReference(R.string.tangem_pay_terms_limits),
-                            textColor = themedColor { TangemTheme.colors.text.primary1 },
-                            onClick = intents::onClickTermsAndLimits,
-                        ),
+                    TangemDropdownMenuItem(
+                        title = resourceReference(R.string.tangempay_pay_support),
+                        textColor = themedColor { TangemTheme.colors.text.primary1 },
+                        onClick = intents::onContactSupportClicked,
                     ),
-                    cardFrozenStateItem,
-                ).toPersistentList(),
+                ),
             ),
             pullToRefreshConfig = PullToRefreshConfig(
                 isRefreshing = false,
@@ -88,21 +57,18 @@ internal class TangemPayDetailsStateFactory(
                         isEnabled = cardFrozenState == TangemPayCardFrozenState.Unfrozen,
                     ),
                 ),
+                cardsBlockState = TangemPayDetailsBalanceBlockState.CardsBlockState(
+                    cards = persistentListOf(
+                        TangemPayDetailsBalanceBlockState.Card(
+                            lastDigits = cardNumberEnd,
+                            onClick = intents::onCardClick,
+                        ),
+                    ),
+                    onAddCardClick = intents::onAddCardClick,
+                ),
             ),
-            addToWalletBlockState = null,
             isBalanceHidden = false,
             addFundsEnabled = true,
-            cardFrozenState = converter.convert(cardFrozenState),
-            betaNotificationConfig = NotificationConfig(
-                title = resourceReference(R.string.tangem_pay_beta_notification_title),
-                subtitle = resourceReference(R.string.tangem_pay_beta_notification_subtitle),
-                iconResId = R.drawable.img_visa_notification,
-                buttonsState = NotificationConfig.ButtonsState.SecondaryButtonConfig(
-                    text = resourceReference(R.string.common_contact_support),
-                    onClick = intents::onContactSupportClicked,
-                ),
-                iconSize = 36.dp,
-            ),
         )
     }
 }
