913ac89daa931d08956fa7b911d3e968c7429a04
Tangem <git@tangem.com>
2026-04-30 13:16:11 +0300

    Updated on 2026-08-27

---
+++ core/ui/src/main/java/com/tangem/core/ui/res/generated/TangemColorPalette.kt
@@ -0,0 +1,149 @@
+@file:Suppress("all")
+
+package com.tangem.core.ui.res.generated
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * Auto-generated from design tokens. Do not edit manually.
+ */
+internal object TangemColorPalette {
+    object Base {
+        val black = Color(0xFF000000)
+        val white = Color(0xFFFFFFFF)
+    }
+
+    object Neutral {
+        val `5` = Color(0xFFF4F4F4)
+        val `10` = Color(0xFFEAEAEA)
+        val `20` = Color(0xFFD0D0D0)
+        val `30` = Color(0xFFB5B5B5)
+        val `40` = Color(0xFF989898)
+        val `50` = Color(0xFF838383)
+        val `60` = Color(0xFF6F6F6F)
+        val `70` = Color(0xFF4A4A4A)
+        val `80` = Color(0xFF2C2C2C)
+        val `90` = Color(0xFF1B1B1B)
+        val `95` = Color(0xFF0F0F0F)
+    }
+
+    object Blue {
+        val `5` = Color(0xFFE9F7FF)
+        val `10` = Color(0xFFDBF1FF)
+        val `20` = Color(0xFF98D7FF)
+        val `30` = Color(0xFF5EBDF9)
+        val `40` = Color(0xFF109FF0)
+        val `50` = Color(0xFF0090F9)
+        val `60` = Color(0xFF0077E1)
+        val `70` = Color(0xFF0C58AF)
+        val `80` = Color(0xFF143C70)
+        val `90` = Color(0xFF1B304A)
+        val `95` = Color(0xFF101C2C)
+    }
+
+    object Violet {
+        val `5` = Color(0xFFF6F1FF)
+        val `10` = Color(0xFFEEE7FD)
+        val `20` = Color(0xFFDAC8FB)
+        val `30` = Color(0xFFC5A5FC)
+        val `40` = Color(0xFFB07BFD)
+        val `50` = Color(0xFFA967FD)
+        val `60` = Color(0xFF9258DC)
+        val `70` = Color(0xFF67419B)
+        val `80` = Color(0xFF473068)
+        val `90` = Color(0xFF332846)
+        val `95` = Color(0xFF201B2A)
+    }
+
+    object Red {
+        val `5` = Color(0xFFFFF0F7)
+        val `10` = Color(0xFFFFE8EC)
+        val `20` = Color(0xFFFFC0C3)
+        val `30` = Color(0xFFFF979D)
+        val `40` = Color(0xFFFF5E66)
+        val `50` = Color(0xFFFE4142)
+        val `60` = Color(0xFFE12C2E)
+        val `70` = Color(0xFF9E2729)
+        val `80` = Color(0xFF6D2323)
+        val `90` = Color(0xFF4C2121)
+        val `95` = Color(0xFF2B1818)
+    }
+
+    object Orange {
+        val `5` = Color(0xFFFFF0EA)
+        val `10` = Color(0xFFFFE5DB)
+        val `20` = Color(0xFFFFC3AD)
+        val `30` = Color(0xFFFF9976)
+        val `40` = Color(0xFFFA6931)
+        val `50` = Color(0xFFF25508)
+        val `60` = Color(0xFFD3480E)
+        val `70` = Color(0xFF953715)
+        val `80` = Color(0xFF652A18)
+        val `90` = Color(0xFF43251A)
+        val `95` = Color(0xFF2A1A13)
+    }
+
+    object Green {
+        val `5` = Color(0xFFEBF6ED)
+        val `10` = Color(0xFFDCFBE3)
+        val `20` = Color(0xFF9EE1AB)
+        val `30` = Color(0xFF64C973)
+        val `40` = Color(0xFF2DAE3B)
+        val `50` = Color(0xFF2DA30D)
+        val `60` = Color(0xFF208900)
+        val `70` = Color(0xFF1E6110)
+        val `80` = Color(0xFF1C4415)
+        val `90` = Color(0xFF1D3319)
+        val `95` = Color(0xFF162114)
+    }
+
+    object Yellow {
+        val `5` = Color(0xFFFAF3E5)
+        val `10` = Color(0xFFFEF5E5)
+        val `20` = Color(0xFFF7CA75)
+        val `30` = Color(0xFFF4B42F)
+        val `40` = Color(0xFFEFA210)
+        val `50` = Color(0xFFE68A03)
+        val `60` = Color(0xFFCD7A11)
+        val `70` = Color(0xFF965001)
+        val `80` = Color(0xFF573414)
+        val `90` = Color(0xFF3D2918)
+        val `95` = Color(0xFF241B13)
+    }
+
+    object Opaque {
+        object BaseBlack {
+            val `5` = Color(0x0D000000)
+            val `10` = Color(0x1A000000)
+            val `15` = Color(0x26000000)
+            val `20` = Color(0x33000000)
+            val `25` = Color(0x40000000)
+            val `30` = Color(0x4D000000)
+            val `40` = Color(0x66000000)
+            val `50` = Color(0x80000000)
+            val `60` = Color(0x99000000)
+            val `80` = Color(0xCC000000)
+        }
+
+        object BaseWhite {
+            val `5` = Color(0x0DFFFFFF)
+            val `10` = Color(0x1AFFFFFF)
+            val `15` = Color(0x26FFFFFF)
+            val `20` = Color(0x33FFFFFF)
+            val `25` = Color(0x40FFFFFF)
+            val `30` = Color(0x4DFFFFFF)
+            val `40` = Color(0x66FFFFFF)
+            val `50` = Color(0x80FFFFFF)
+            val `60` = Color(0x99FFFFFF)
+            val `80` = Color(0xCCFFFFFF)
+        }
+
+        object Neutral95 {
+            val `5` = Color(0x0D0F0F0F)
+            val `10` = Color(0x1A0F0F0F)
+            val `15` = Color(0x260F0F0F)
+            val `40` = Color(0x660F0F0F)
+            val `60` = Color(0x990F0F0F)
+        }
+    }
+}
