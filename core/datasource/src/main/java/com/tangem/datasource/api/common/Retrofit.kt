50fdf36725d1500fab8c70a0fe294c1221e8130c
Tangem <git@tangem.com>
2026-04-30 15:29:56 +0400

    Updated on 2026-09-21

--- core/datasource/src/main/java/com/tangem/datasource/api/common/Retrofit.kt
+++ core/datasource/src/main/java/com/tangem/datasource/api/common/Retrofit.kt
@@ -4,34 +4,6 @@ import android.util.Log
 import com.ihsanbal.logging.Level
 import com.ihsanbal.logging.LoggingInterceptor
 import okhttp3.Interceptor
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import java.util.concurrent.TimeUnit
-
-@Suppress("MagicNumber")
-@Deprecated("Create and provide by DI")
-fun createRetrofitInstance(
-    baseUrl: String,
-    okHttpBuilder: OkHttpClient.Builder = OkHttpClient.Builder(),
-    interceptors: List<Interceptor> = emptyList(),
-    logEnabled: Boolean,
-): Retrofit {
-    okHttpBuilder.apply {
-        callTimeout(10, TimeUnit.SECONDS)
-        connectTimeout(20, TimeUnit.SECONDS)
-        readTimeout(20, TimeUnit.SECONDS)
-        writeTimeout(20, TimeUnit.SECONDS)
-    }
-    interceptors.forEach { okHttpBuilder.addInterceptor(it) }
-
-    if (logEnabled) okHttpBuilder.addInterceptor(createNetworkLoggingInterceptor())
-
-    return Retrofit.Builder()
-        .baseUrl(baseUrl)
-        .addConverterFactory(MoshiConverter.networkMoshiConverter)
-        .client(okHttpBuilder.build())
-        .build()
-}
 
 fun createNetworkLoggingInterceptor(): Interceptor {
     return LoggingInterceptor.Builder()
