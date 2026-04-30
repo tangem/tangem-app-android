6bbdad78dec20e86af4743e121fc6ab9f659e7eb
Tangem <git@tangem.com>
2026-04-30 10:28:27 +0200

    Updated on 2026-08-27

--- app/src/prodDi/java/com/tangem/tap/di/core/security/SecurityProductionModule.kt
+++ app/src/prodDi/java/com/tangem/tap/di/core/security/SecurityProductionModule.kt
@@ -10,7 +10,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-internal object SecurityModule {
+internal object SecurityProductionModule {
 
     @Provides
     @Singleton
