9d23eb89b1ccb7d853e2e11e0a2209a9f194fad2
Tangem <git@tangem.com>
2026-05-04 11:10:18 +0400

    Updated on 2026-09-23

--- app/src/main/java/com/tangem/tap/di/data/TangemLoggingModule.kt
+++ app/src/main/java/com/tangem/tap/di/data/TangemLoggingModule.kt
@@ -1,13 +1,10 @@
 package com.tangem.tap.di.data
 
-import com.tangem.Log
-import com.tangem.LogFormat
-import com.tangem.TangemSdkLogger
 import com.tangem.blockchain.common.logging.BlockchainSDKLogger
 import com.tangem.datasource.local.logs.AppLogsStore
-import com.tangem.tap.common.log.TangemAppLoggerInitializer
+import com.tangem.tap.common.log.TangemBlockchainSDKLogger
 import com.tangem.tap.common.log.TangemCardSDKLogger
-import com.tangem.tap.data.TangemBlockchainSDKLogger
+import com.tangem.tap.common.log.TangemLoggingInitializer
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -20,31 +17,10 @@ internal object TangemLoggingModule {
 
     @Provides
     @Singleton
-    fun provideAppLoggerInitializer(appLogsStore: AppLogsStore): TangemAppLoggerInitializer {
-        return TangemAppLoggerInitializer(appLogsStore)
-    }
-
-    @Provides
-    @Singleton
-    fun provideCardSDKLogger(appLogsStore: AppLogsStore): TangemSdkLogger {
-        val logLevels = listOf(
-            Log.Level.ApduCommand,
-            Log.Level.Apdu,
-            Log.Level.Tlv,
-            Log.Level.Nfc,
-            Log.Level.Command,
-            Log.Level.Session,
-            Log.Level.View,
-            Log.Level.Network,
-            Log.Level.Error,
-            Log.Level.Biometric,
-            Log.Level.Info,
-        )
-
-        return TangemCardSDKLogger(
-            levels = logLevels,
-            messageFormatter = LogFormat.StairsFormatter(),
+    fun provideLoggingInitializer(appLogsStore: AppLogsStore): TangemLoggingInitializer {
+        return TangemLoggingInitializer(
             appLogsStore = appLogsStore,
+            tangemSdkLogger = TangemCardSDKLogger(appLogsStore),
         )
     }
 
