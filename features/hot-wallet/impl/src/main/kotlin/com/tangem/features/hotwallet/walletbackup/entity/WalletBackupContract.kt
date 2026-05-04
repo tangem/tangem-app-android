7626892962e3194a64c19ce37a9943636ca1759c
Tangem <git@tangem.com>
2026-05-04 17:08:11 +0100

    Updated on 2026-06-17

--- features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/walletbackup/entity/WalletBackupContract.kt
+++ features/hot-wallet/impl/src/main/kotlin/com/tangem/features/hotwallet/walletbackup/entity/WalletBackupContract.kt
@@ -3,10 +3,14 @@ package com.tangem.features.hotwallet.walletbackup.entity
 import com.tangem.core.ui.components.label.entity.LabelUM
 
 internal data class WalletBackupUM(
+    val onBackClick: () -> Unit,
     val hardwareWalletOption: LabelUM?,
     val recoveryPhraseOption: LabelUM?,
     val googleDriveOption: LabelUM?,
     val googleDriveStatus: BackupStatus,
+    val onRecoveryPhraseClick: () -> Unit,
+    val onGoogleDriveAction: (Boolean) -> Unit, // boolean is for show and hide dialog
+    val onHardwareWalletClick: () -> Unit,
     val isGoogleDriveDialogShown: Boolean,
     val isBackedUp: Boolean,
 )
@@ -15,11 +19,4 @@ internal sealed class BackupStatus {
     object Done : BackupStatus()
     object ComingSoon : BackupStatus()
     object NoBackup : BackupStatus()
-}
-
-internal sealed interface Action {
-    data object RecoveryPhrase : Action
-    data object HardwareWallet : Action
-    data class GoogleDriveBackup(val isDialogShown: Boolean) : Action
-    data object OnBack : Action
 }
