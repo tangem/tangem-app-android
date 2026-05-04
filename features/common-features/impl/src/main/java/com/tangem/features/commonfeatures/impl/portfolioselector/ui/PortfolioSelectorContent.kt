8b23a7e7fb89910d9fc90eabd7a927b72c17700b
Tangem <git@tangem.com>
2026-05-04 18:50:14 +0400

    Updated on 2026-07-07

--- features/common-features/impl/src/main/java/com/tangem/features/commonfeatures/impl/portfolioselector/ui/PortfolioSelectorContent.kt
+++ features/common-features/impl/src/main/java/com/tangem/features/commonfeatures/impl/portfolioselector/ui/PortfolioSelectorContent.kt
@@ -11,9 +11,11 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
@@ -30,6 +32,7 @@ import com.tangem.core.ui.extensions.conditional
 import com.tangem.core.ui.extensions.resolveReference
 import com.tangem.core.ui.extensions.resourceReference
 import com.tangem.core.ui.extensions.stringReference
+import com.tangem.core.ui.res.LocalCanScrollBackward
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.core.ui.res.TangemThemePreview
 import com.tangem.features.commonfeatures.impl.R
@@ -46,51 +49,59 @@ internal fun PortfolioSelectorContent(
     modifier: Modifier = Modifier,
     contentPadding: PaddingValues = PaddingValues(),
 ) {
-    LazyColumn(
-        modifier = modifier,
-        contentPadding = contentPadding,
+    val lazyListState = rememberLazyListState()
+
+    CompositionLocalProvider(
+        LocalCanScrollBackward provides
+            lazyListState.canScrollBackward,
     ) {
-        val items = state.items
-        itemsIndexed(
-            items = items,
-            key = { _, item -> item.id },
-        ) { index, item ->
-            val previewItem = items.getOrNull(index.dec())
-            val offsetModifier = when {
-                previewItem == null -> Modifier
-                item is PortfolioSelectorItemUM.GroupTitle -> Modifier.padding(
-                    top = TangemTheme.dimens.spacing16,
-                )
-                else -> Modifier.padding(
-                    top = TangemTheme.dimens.spacing8,
-                )
-            }
+        LazyColumn(
+            state = lazyListState,
+            modifier = modifier,
+            contentPadding = contentPadding,
+        ) {
+            val items = state.items
+            itemsIndexed(
+                items = items,
+                key = { _, item -> item.id },
+            ) { index, item ->
+                val previewItem = items.getOrNull(index.dec())
+                val offsetModifier = when {
+                    previewItem == null -> Modifier
+                    item is PortfolioSelectorItemUM.GroupTitle -> Modifier.padding(
+                        top = TangemTheme.dimens.spacing16,
+                    )
+                    else -> Modifier.padding(
+                        top = TangemTheme.dimens.spacing8,
+                    )
+                }
 
-            val portfolioShape = RoundedCornerShape(TangemTheme.dimens.radius14)
-            val border = BorderStroke(
-                width = 1.dp,
-                color = TangemTheme.colors.text.accent,
-            )
+                val portfolioShape = RoundedCornerShape(TangemTheme.dimens.radius14)
+                val border = BorderStroke(
+                    width = 1.dp,
+                    color = TangemTheme.colors.text.accent,
+                )
 
-            when (item) {
-                is PortfolioSelectorItemUM.Portfolio -> UserWalletItemRow(
-                    state = item.item,
-                    modifier = offsetModifier
-                        .fillMaxWidth()
-                        .heightIn(min = TangemTheme.dimens.size68)
-                        .clip(portfolioShape)
-                        .background(TangemTheme.colors.background.action)
-                        .conditional(item.isSelected) { border(border, portfolioShape) }
-                        .clickable(enabled = item.item.isEnabled, onClick = item.item.onClick)
-                        .padding(all = TangemTheme.dimens.spacing12)
-                        .conditional(!item.item.isEnabled) { alpha(DISABLED_WALLET_ALPHA) },
-                )
-                is PortfolioSelectorItemUM.GroupTitle -> WalletNameRow(
-                    model = item,
-                    modifier = offsetModifier
-                        .fillMaxWidth()
-                        .padding(horizontal = TangemTheme.dimens.spacing16),
-                )
+                when (item) {
+                    is PortfolioSelectorItemUM.Portfolio -> UserWalletItemRow(
+                        state = item.item,
+                        modifier = offsetModifier
+                            .fillMaxWidth()
+                            .heightIn(min = TangemTheme.dimens.size68)
+                            .clip(portfolioShape)
+                            .background(TangemTheme.colors.background.action)
+                            .conditional(item.isSelected) { border(border, portfolioShape) }
+                            .clickable(enabled = item.item.isEnabled, onClick = item.item.onClick)
+                            .padding(all = TangemTheme.dimens.spacing12)
+                            .conditional(!item.item.isEnabled) { alpha(DISABLED_WALLET_ALPHA) },
+                    )
+                    is PortfolioSelectorItemUM.GroupTitle -> WalletNameRow(
+                        model = item,
+                        modifier = offsetModifier
+                            .fillMaxWidth()
+                            .padding(horizontal = TangemTheme.dimens.spacing16),
+                    )
+                }
             }
         }
     }
