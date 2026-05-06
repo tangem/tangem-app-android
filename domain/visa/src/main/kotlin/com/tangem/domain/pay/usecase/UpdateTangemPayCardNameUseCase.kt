d3a150386fabef9a5013aeb925c89e0379ddc8f4
Tangem <git@tangem.com>
2026-05-06 22:27:57 +0500

    Updated on 2026-06-12

--- domain/visa/src/main/kotlin/com/tangem/domain/pay/usecase/UpdateTangemPayCardNameUseCase.kt
+++ domain/visa/src/main/kotlin/com/tangem/domain/pay/usecase/UpdateTangemPayCardNameUseCase.kt
@@ -2,21 +2,21 @@ package com.tangem.domain.pay.usecase
 
 import arrow.core.Either
 import com.tangem.core.error.UniversalError
+import com.tangem.domain.models.account.CardDisplayName
 import com.tangem.domain.models.wallet.UserWalletId
 import com.tangem.domain.pay.flow.PaymentAccountStatusFetcher
 import com.tangem.domain.pay.repository.TangemPayCardDetailsRepository
-import java.math.BigDecimal
 
-class SetTangemPayCardLimitUseCase(
+class UpdateTangemPayCardNameUseCase(
     private val cardDetailsRepository: TangemPayCardDetailsRepository,
     private val paymentAccountStatusFetcher: PaymentAccountStatusFetcher,
 ) {
     suspend operator fun invoke(
         cardId: String,
         userWalletId: UserWalletId,
-        amount: BigDecimal,
+        displayName: CardDisplayName,
     ): Either<UniversalError, Unit> {
-        return cardDetailsRepository.updateCardLimit(cardId, userWalletId, amount.toPlainString())
-            .onRight { paymentAccountStatusFetcher.invoke(userWalletId) }
+        return cardDetailsRepository.updateCardDisplayName(cardId, userWalletId, displayName)
+            .onRight { paymentAccountStatusFetcher(userWalletId) }
     }
 }
