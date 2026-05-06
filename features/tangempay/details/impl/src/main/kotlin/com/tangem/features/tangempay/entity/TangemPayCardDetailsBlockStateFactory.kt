6dd6249564baa6a41f9bb2179d4600663b4f3463
Tangem <git@tangem.com>
2026-05-06 22:27:57 +0500

    Updated on 2026-08-07

--- features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/entity/TangemPayCardDetailsBlockStateFactory.kt
+++ features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/entity/TangemPayCardDetailsBlockStateFactory.kt
@@ -1,6 +1,7 @@
 package com.tangem.features.tangempay.entity
 
 import com.tangem.core.ui.extensions.resourceReference
+import com.tangem.domain.models.account.CardDisplayName
 import com.tangem.domain.visa.model.TangemPayCardFrozenState
 import com.tangem.features.tangempay.details.impl.R
 import com.tangem.features.tangempay.model.CardDataType
@@ -8,21 +9,33 @@ import com.tangem.utils.StringsSigns
 
 internal class TangemPayCardDetailsBlockStateFactory(
     private val cardNumberEnd: String,
-    private val displayNameState: DisplayNameState?,
+    private val displayName: CardDisplayName?,
+    private val isEditingNameEnabled: Boolean,
+    private val onEditNameClick: () -> Unit,
     private val onReveal: () -> Unit,
     private val onCopy: (String, CardDataType) -> Unit,
 ) {
 
-    fun getInitialState() = TangemPayCardDetailsUM(
-        number = "",
-        numberShort = "${StringsSigns.ASTERISK}$cardNumberEnd",
-        expiry = "",
-        cvv = "",
-        buttonText = resourceReference(R.string.tangempay_card_details_reveal_text),
-        onClick = onReveal,
-        onCopy = onCopy,
-        isHidden = true,
-        cardFrozenState = TangemPayCardFrozenState.Unfrozen,
-        displayNameState = displayNameState,
-    )
+    fun getInitialState(): TangemPayCardDetailsUM {
+        return TangemPayCardDetailsUM(
+            number = "",
+            numberShort = "${StringsSigns.ASTERISK}$cardNumberEnd",
+            expiry = "",
+            cvv = "",
+            buttonText = resourceReference(R.string.tangempay_card_details_reveal_text),
+            onClick = onReveal,
+            onCopy = onCopy,
+            isHidden = true,
+            cardFrozenState = TangemPayCardFrozenState.Unfrozen,
+            displayNameState = if (displayName != null) {
+                DisplayNameState.Display(
+                    displayName = displayName.value,
+                    onClick = onEditNameClick,
+                    isEditingEnabled = isEditingNameEnabled,
+                )
+            } else {
+                null
+            },
+        )
+    }
 }
