28b596c67eebaa736755cd38101137cc346f1132
Tangem <git@tangem.com>
2026-05-07 19:13:32 +0300

    Updated on 2026-06-17

--- features/tester/impl/src/main/java/com/tangem/feature/tester/presentation/storybook/entity/StoryBookPage.kt
+++ features/tester/impl/src/main/java/com/tangem/feature/tester/presentation/storybook/entity/StoryBookPage.kt
@@ -2,10 +2,18 @@ package com.tangem.feature.tester.presentation.storybook.entity
 
 import com.tangem.core.ui.ds.badge.TangemBadgeColor
 import com.tangem.core.ui.ds.field.search.TangemFieldShape
+import com.tangem.core.ui.ds2.loader.TangemLoaderSize
 import com.tangem.core.ui.ds.message.TangemMessageEffect
 import com.tangem.core.ui.ds.topbar.TangemTopBarType
 
 internal sealed interface StoryBookPage
+
+/**
+ * Marker for pages that live inside the DS components sub-list.
+ * The view model uses it to route back navigation to the DS list
+ * instead of the root [StoryList].
+ */
+internal sealed interface DsStoryBookPage : StoryBookPage
 
 internal data object StoryList : StoryBookPage
 
@@ -84,3 +92,12 @@ internal data object PlaceholderStory : StoryBookPage
 internal data object ProgressIndicatorStory : StoryBookPage
 
 internal data object DeviceIconStory : StoryBookPage
+
+internal data class DsComponentsListStory(
+    val onStoryClick: (StoryPageFactory) -> Unit,
+) : StoryBookPage
+
+internal data class TangemLoaderStory(
+    val selectedSize: TangemLoaderSize,
+    val onSizeChange: (TangemLoaderSize) -> Unit,
+) : DsStoryBookPage
