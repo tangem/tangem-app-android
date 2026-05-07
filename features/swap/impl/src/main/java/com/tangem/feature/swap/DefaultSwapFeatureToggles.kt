ee45dc7d9d0a49320e274799fdb4ab1fb8f2b70c
Tangem <git@tangem.com>
2026-05-07 18:47:43 +0400

    Updated on 2026-07-23

--- features/swap/impl/src/main/java/com/tangem/feature/swap/DefaultSwapFeatureToggles.kt
+++ features/swap/impl/src/main/java/com/tangem/feature/swap/DefaultSwapFeatureToggles.kt
@@ -12,7 +12,12 @@ internal class DefaultSwapFeatureToggles @Inject constructor(
     override val isSwapSwitchToTransferEnabled: Boolean = featureTogglesManager.isFeatureEnabled(
         toggle = FeatureToggles.SWAP_SWITCH_TO_TRANSFER_ENABLED,
     )
+
     override val isSwapIntegratedApproveEnabled: Boolean = featureTogglesManager.isFeatureEnabled(
         toggle = FeatureToggles.SWAP_INTEGRATED_APPROVE,
+    )
+
+    override val isSwapAbEnabled: Boolean = featureTogglesManager.isFeatureEnabled(
+        toggle = FeatureToggles.SWAP_AB_ENABLED,
     )
 }
