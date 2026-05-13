c36ba85816f4243c21e913d113fe32d158309c96
Tangem <git@tangem.com>
2026-05-13 11:43:39 +0400

    Updated on 2026-08-14

--- features/swap/data/src/main/java/com/tangem/feature/swap/DefaultSwapRepository.kt
+++ features/swap/data/src/main/java/com/tangem/feature/swap/DefaultSwapRepository.kt
@@ -21,6 +21,7 @@ import com.tangem.datasource.exchangeservice.swap.ExpressUtils
 import com.tangem.datasource.local.preferences.AppPreferencesStore
 import com.tangem.datasource.local.preferences.PreferencesKeys
 import com.tangem.datasource.local.preferences.utils.getObjectSyncOrNull
+import com.tangem.datasource.local.preferences.utils.storeObject
 import com.tangem.domain.exchange.RampStateManager
 import com.tangem.domain.express.models.ExpressOperationType
 import com.tangem.domain.models.currency.CryptoCurrency
@@ -418,5 +419,9 @@ internal class DefaultSwapRepository(
 
     override suspend fun getStoredSwapUiMode(): SwapUIMode? {
         return appPreferencesStore.getObjectSyncOrNull(PreferencesKeys.SWAP_UI_MODE_KEY)
+    }
+
+    override suspend fun storeSwapUiMode(mode: SwapUIMode) {
+        appPreferencesStore.storeObject(PreferencesKeys.SWAP_UI_MODE_KEY, mode)
     }
 }
