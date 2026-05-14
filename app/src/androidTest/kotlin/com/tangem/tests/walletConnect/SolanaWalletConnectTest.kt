f8c9f7818d2a2cc55abebf70f761786282ceff07
Tangem <git@tangem.com>
2026-05-14 11:48:39 +0300

    Updated on 2026-09-25

--- app/src/androidTest/kotlin/com/tangem/tests/walletConnect/SolanaWalletConnectTest.kt
+++ app/src/androidTest/kotlin/com/tangem/tests/walletConnect/SolanaWalletConnectTest.kt
@@ -4,19 +4,11 @@ import android.Manifest
 import com.tangem.common.BaseTestCase
 import com.tangem.common.constants.TestConstants
 import com.tangem.common.constants.TestConstants.USER_TOKENS_API_SCENARIO
-import com.tangem.common.extensions.clickWithAssertion
 import com.tangem.common.utils.getWcUri
 import com.tangem.common.utils.resetWireMockScenarioState
 import com.tangem.common.utils.setClipboardText
 import com.tangem.common.utils.setWireMockScenarioState
-import com.tangem.scenarios.checkWalletConnectBottomSheet
-import com.tangem.scenarios.checkWalletConnectDetailsBottomSheet
-import com.tangem.scenarios.checkWalletConnectScreen
-import com.tangem.scenarios.openAppByDeepLink
-import com.tangem.scenarios.openMainScreen
-import com.tangem.scenarios.openWalletConnectScreen
-import com.tangem.scenarios.synchronizeAddresses
-import com.tangem.screens.onScanQrScreen
+import com.tangem.scenarios.*
 import com.tangem.screens.onWalletConnectBottomSheet
 import com.tangem.screens.onWalletConnectDetailsBottomSheet
 import com.tangem.screens.onWalletConnectScreen
@@ -64,13 +56,8 @@ class SolanaWalletConnectTest : BaseTestCase() {
             step("Assert 'Connect' button is enabled") {
                 onWalletConnectBottomSheet { connectButton.assertIsEnabled() }
             }
-            step("Click on 'Connect' button") {
-                waitForIdle()
-                onWalletConnectBottomSheet { connectButton.performClick() }
-            }
-            step("Assert 'Connect' button is not displayed") {
-                waitForIdle()
-                onWalletConnectBottomSheet { connectButton.assertIsNotDisplayed() }
+            step("Click on 'Connect' button and dismiss 'Unknown domain' alert if shown") {
+                confirmWcConnection()
             }
             step("Open 'Wallet Connect' screen") {
                 openWalletConnectScreen()
@@ -131,13 +118,8 @@ class SolanaWalletConnectTest : BaseTestCase() {
                     checkWalletConnectBottomSheet()
                 }
             }
-            step("Click on 'Connect' button") {
-                waitForIdle()
-                onWalletConnectBottomSheet { connectButton.performClick() }
-            }
-            step("Assert 'Connect' button is not displayed") {
-                waitForIdle()
-                onWalletConnectBottomSheet { connectButton.assertIsNotDisplayed() }
+            step("Click on 'Connect' button and dismiss 'Unknown domain' alert if shown") {
+                confirmWcConnection()
             }
             step("Check 'Wallet Connect' screen with connections") {
                 flakySafely(TestConstants.WAIT_UNTIL_TIMEOUT) {
@@ -197,11 +179,8 @@ class SolanaWalletConnectTest : BaseTestCase() {
                     checkWalletConnectBottomSheet()
                 }
             }
-            step("Click on 'Connect' button") {
-                onWalletConnectBottomSheet { connectButton.performClick() }
-            }
-            step("Assert 'Connect' button is not displayed") {
-                onWalletConnectBottomSheet { connectButton.assertIsNotDisplayed() }
+            step("Click on 'Connect' button and dismiss 'Unknown domain' alert if shown") {
+                confirmWcConnection()
             }
             step("Open 'Wallet Connect' screen") {
                 openWalletConnectScreen()
@@ -261,11 +240,8 @@ class SolanaWalletConnectTest : BaseTestCase() {
             step("Open 'Wallet Connect' screen") {
                 openWalletConnectScreen()
             }
-            step("Click 'New connection' button") {
-                onWalletConnectScreen { newConnectionButton.performClick() }
-            }
-            step("CLick 'Paste from clipboard' button") {
-                onScanQrScreen { pasteFromClipboardButton.clickWithAssertion() }
+            step("Create connection via 'Paste from clipboard' button") {
+                createConnectionViaPasteFromClipboardButton()
             }
             step("Check 'Wallet Connect' bottom sheet") {
                 waitForIdle()
@@ -273,13 +249,8 @@ class SolanaWalletConnectTest : BaseTestCase() {
                     checkWalletConnectBottomSheet()
                 }
             }
-            step("Click on 'Connect' button") {
-                waitForIdle()
-                onWalletConnectBottomSheet { connectButton.performClick() }
-            }
-            step("Assert 'Connect' button is not displayed") {
-                waitForIdle()
-                onWalletConnectBottomSheet { connectButton.assertIsNotDisplayed() }
+            step("Click on 'Connect' button and dismiss 'Unknown domain' alert if shown") {
+                confirmWcConnection()
             }
             step("Check 'Wallet Connect' screen with connections") {
                 checkWalletConnectScreen(withConnections = true)
