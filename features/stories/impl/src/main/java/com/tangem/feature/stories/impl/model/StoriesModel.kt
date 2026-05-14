0a2e17b407fe5e7b95c74f2a3c50a930eac96c14
Tangem <git@tangem.com>
2026-05-14 13:35:52 +0300

    Updated on 2026-09-25

--- features/stories/impl/src/main/java/com/tangem/feature/stories/impl/model/StoriesModel.kt
+++ features/stories/impl/src/main/java/com/tangem/feature/stories/impl/model/StoriesModel.kt
@@ -5,8 +5,8 @@ import com.tangem.core.decompose.model.Model
 import com.tangem.core.decompose.model.ParamsContainer
 import com.tangem.core.decompose.navigation.Router
 import com.tangem.core.ui.extensions.resourceReference
-import com.tangem.domain.promo.GetStoryContentUseCase
-import com.tangem.domain.promo.ShouldShowStoriesUseCase
+import com.tangem.domain.stories.GetStoryContentUseCase
+import com.tangem.domain.stories.ShouldShowStoriesUseCase
 import com.tangem.feature.stories.api.StoriesComponent
 import com.tangem.feature.stories.api.StoriesUM
 import com.tangem.feature.stories.impl.StoriesSlideConfigs
