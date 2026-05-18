ca7cc3f362ad66cd1e0d486e2c725d5ab452cc83
Tangem <git@tangem.com>
2026-05-18 14:03:33 +0500

    Updated on 2026-09-01

--- features/swap/impl/src/test/java/com/tangem/feature/swap/StateBuilderQuotesTest.kt
+++ features/swap/impl/src/test/java/com/tangem/feature/swap/StateBuilderQuotesTest.kt
@@ -1,8 +1,8 @@
 package com.tangem.feature.swap
 
 import com.google.common.truth.Truth.assertThat
+import com.tangem.common.routing.AppRouter
 import com.tangem.domain.appcurrency.model.AppCurrency
-import com.tangem.domain.models.currency.CryptoCurrencyStatus
 import com.tangem.domain.models.wallet.UserWallet
 import com.tangem.domain.models.wallet.UserWalletId
 import com.tangem.domain.swap.models.SwapCurrencyStatus
@@ -14,12 +14,10 @@ import com.tangem.feature.swap.domain.models.ui.*
 import com.tangem.feature.swap.models.*
 import com.tangem.feature.swap.models.states.FeeItemState
 import com.tangem.feature.swap.models.states.ProviderState
-import com.tangem.feature.swap.models.states.SwapNotificationUM
 import com.tangem.feature.swap.ui.StateBuilder
 import com.tangem.utils.Provider
 import io.mockk.every
 import io.mockk.mockk
-import kotlinx.collections.immutable.persistentListOf
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -32,6 +30,7 @@ internal class StateBuilderQuotesTest {
     private val appCurrencyProvider: Provider<AppCurrency> = mockk()
     private val isAccountsModeProvider: Provider<Boolean> = mockk()
     private val iGaslessFeeSupportedForNetwork: IsGaslessFeeSupportedForNetwork = mockk()
+    private val appRouter: AppRouter = mockk()
 
     private lateinit var sut: StateBuilder
 
@@ -60,6 +59,7 @@ internal class StateBuilderQuotesTest {
             appCurrencyProvider = appCurrencyProvider,
             isAccountsModeProvider = isAccountsModeProvider,
             iGaslessFeeSupportedForNetwork = iGaslessFeeSupportedForNetwork,
+            appRouter = appRouter,
         )
     }
 
