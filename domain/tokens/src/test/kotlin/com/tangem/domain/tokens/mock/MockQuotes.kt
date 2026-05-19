119d58141af3c7f045006054c7f74aff89a804b3
Tangem <git@tangem.com>
2026-05-19 14:55:23 +0400

    Updated on 2026-09-01

--- domain/tokens/src/test/kotlin/com/tangem/domain/tokens/mock/MockQuotes.kt
+++ domain/tokens/src/test/kotlin/com/tangem/domain/tokens/mock/MockQuotes.kt
@@ -3,6 +3,7 @@ package com.tangem.domain.tokens.mock
 import arrow.core.nonEmptySetOf
 import com.tangem.domain.models.StatusSource
 import com.tangem.domain.models.currency.CryptoCurrency
+import com.tangem.domain.models.currency.FiatCurrency
 import com.tangem.domain.models.quote.QuoteStatus
 import java.math.BigDecimal
 
@@ -12,6 +13,7 @@ internal object MockQuotes {
     val quote1 = QuoteStatus(
         rawCurrencyId = MockTokens.token1.id.rawCurrencyId!!,
         value = QuoteStatus.Data(
+            fiatCurrency = FiatCurrency.Default,
             fiatRate = BigDecimal("1.23"),
             fiatRateUSD = BigDecimal("1.23"),
             priceChange = BigDecimal("0.01"),
@@ -22,6 +24,7 @@ internal object MockQuotes {
     val quote2 = QuoteStatus(
         rawCurrencyId = MockTokens.token2.id.rawCurrencyId!!,
         value = QuoteStatus.Data(
+            fiatCurrency = FiatCurrency.Default,
             fiatRate = BigDecimal("2.34"),
             fiatRateUSD = BigDecimal("2.34"),
             priceChange = BigDecimal("-0.02"),
@@ -32,6 +35,7 @@ internal object MockQuotes {
     val quote3 = QuoteStatus(
         rawCurrencyId = MockTokens.token3.id.rawCurrencyId!!,
         value = QuoteStatus.Data(
+            fiatCurrency = FiatCurrency.Default,
             fiatRate = BigDecimal("3.45"),
             fiatRateUSD = BigDecimal("3.45"),
             priceChange = BigDecimal("0.03"),
@@ -42,6 +46,7 @@ internal object MockQuotes {
     val quote4 = QuoteStatus(
         rawCurrencyId = MockTokens.token4.id.rawCurrencyId!!,
         value = QuoteStatus.Data(
+            fiatCurrency = FiatCurrency.Default,
             fiatRate = BigDecimal("4.56"),
             fiatRateUSD = BigDecimal("4.56"),
             priceChange = BigDecimal("-0.04"),
@@ -52,6 +57,7 @@ internal object MockQuotes {
     val quote5 = QuoteStatus(
         rawCurrencyId = MockTokens.token5.id.rawCurrencyId!!,
         value = QuoteStatus.Data(
+            fiatCurrency = FiatCurrency.Default,
             fiatRate = BigDecimal("5.67"),
             fiatRateUSD = BigDecimal("5.67"),
             priceChange = BigDecimal("0.05"),
@@ -62,6 +68,7 @@ internal object MockQuotes {
     val quote6 = QuoteStatus(
         rawCurrencyId = MockTokens.token6.id.rawCurrencyId!!,
         value = QuoteStatus.Data(
+            fiatCurrency = FiatCurrency.Default,
             fiatRate = BigDecimal("6.78"),
             fiatRateUSD = BigDecimal("6.78"),
             priceChange = BigDecimal("-0.06"),
@@ -72,6 +79,7 @@ internal object MockQuotes {
     val quote7 = QuoteStatus(
         rawCurrencyId = MockTokens.token7.id.rawCurrencyId!!,
         value = QuoteStatus.Data(
+            fiatCurrency = FiatCurrency.Default,
             fiatRate = BigDecimal("7.89"),
             fiatRateUSD = BigDecimal("7.89"),
             priceChange = BigDecimal("0.07"),
@@ -82,6 +90,7 @@ internal object MockQuotes {
     val quote8 = QuoteStatus(
         rawCurrencyId = MockTokens.token8.id.rawCurrencyId!!,
         value = QuoteStatus.Data(
+            fiatCurrency = FiatCurrency.Default,
             fiatRate = BigDecimal("8.90"),
             fiatRateUSD = BigDecimal("8.90"),
             priceChange = BigDecimal("-0.08"),
@@ -92,6 +101,7 @@ internal object MockQuotes {
     val quote9 = QuoteStatus(
         rawCurrencyId = MockTokens.token9.id.rawCurrencyId!!,
         value = QuoteStatus.Data(
+            fiatCurrency = FiatCurrency.Default,
             fiatRate = BigDecimal("9.01"),
             fiatRateUSD = BigDecimal("9.01"),
             priceChange = BigDecimal("0.09"),
@@ -102,6 +112,7 @@ internal object MockQuotes {
     val quote10 = QuoteStatus(
         rawCurrencyId = MockTokens.token10.id.rawCurrencyId!!,
         value = QuoteStatus.Data(
+            fiatCurrency = FiatCurrency.Default,
             fiatRate = BigDecimal("10.12"),
             fiatRateUSD = BigDecimal("10.12"),
             priceChange = BigDecimal("-0.10"),
