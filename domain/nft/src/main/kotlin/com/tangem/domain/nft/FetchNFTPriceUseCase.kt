27754d3dfc6ef6fa8dfa1f9df70a3b7eaff30787
Tangem <git@tangem.com>
2026-05-21 12:58:45 +0400

    Updated on 2026-07-01

--- domain/nft/src/main/kotlin/com/tangem/domain/nft/FetchNFTPriceUseCase.kt
+++ domain/nft/src/main/kotlin/com/tangem/domain/nft/FetchNFTPriceUseCase.kt
@@ -1,21 +1,29 @@
 package com.tangem.domain.nft
 
 import arrow.core.Either
+import arrow.core.raise.either
+import arrow.core.raise.ensureNotNull
 import com.tangem.domain.models.network.Network
 import com.tangem.domain.nft.repository.NFTRepository
 import com.tangem.domain.quotes.single.SingleQuoteStatusFetcher
+import com.tangem.utils.logging.TangemLogger
 
 class FetchNFTPriceUseCase(
     private val nftRepository: NFTRepository,
     private val singleQuoteStatusFetcher: SingleQuoteStatusFetcher,
 ) {
 
-    suspend operator fun invoke(network: Network): Either<Throwable, Unit> {
-        return Either.catch {
-            val nftCurrency = nftRepository.getNFTCurrency(network)
-            val rawId = nftCurrency.id.rawCurrencyId ?: error("Invalid nft currency id")
+    suspend operator fun invoke(network: Network): Either<Throwable, Unit> = either {
+        val nftCurrency = arrow.core.raise.catch(
+            block = { nftRepository.getNFTCurrency(network) },
+            catch = ::raise,
+        )
 
-            singleQuoteStatusFetcher(params = SingleQuoteStatusFetcher.Params(rawCurrencyId = rawId))
+        val rawId = ensureNotNull(nftCurrency.id.rawCurrencyId) {
+            IllegalStateException("Invalid nft currency id: ${nftCurrency.id}")
         }
+
+        singleQuoteStatusFetcher(params = SingleQuoteStatusFetcher.Params(rawCurrencyId = rawId)).bind()
     }
+        .onLeft { TangemLogger.e(messageString = "Error fetching nft price", throwable = it) }
 }
