d1e6d41305f14af951422138df2ed9e96f061afc
Tangem <git@tangem.com>
2026-05-25 22:45:58 +0400

    Updated on 2026-06-17

--- common/routing/src/test/kotlin/com/tangem/common/routing/deeplink/PayloadToDeeplinkConverterTest.kt
+++ common/routing/src/test/kotlin/com/tangem/common/routing/deeplink/PayloadToDeeplinkConverterTest.kt
@@ -2,11 +2,14 @@ package com.tangem.common.routing.deeplink
 
 import com.google.common.truth.Truth.assertThat
 import com.tangem.common.routing.deeplink.DeeplinkConst.DEEPLINK_KEY
+import com.tangem.common.routing.deeplink.DeeplinkConst.CUSTOMER_WALLET_ID_KEY
 import com.tangem.common.routing.deeplink.DeeplinkConst.DERIVATION_PATH_KEY
 import com.tangem.common.routing.deeplink.DeeplinkConst.NETWORK_ID_KEY
 import com.tangem.common.routing.deeplink.DeeplinkConst.TOKEN_ID_KEY
+import com.tangem.common.routing.deeplink.DeeplinkConst.TRANSACTION_ID_KEY
 import com.tangem.common.routing.deeplink.DeeplinkConst.TYPE_KEY
 import com.tangem.common.routing.deeplink.DeeplinkConst.WALLET_ID_KEY
+import com.tangem.domain.visa.model.TangemPayPushNotificationType
 import org.junit.Test
 
 internal class PayloadToDeeplinkConverterTest {
@@ -138,6 +141,90 @@ internal class PayloadToDeeplinkConverterTest {
     fun `GIVEN empty payload WHEN convert THEN should return null`() {
         // GIVEN
         val payload = emptyMap<String, String>()
+
+        // WHEN
+        val result = PayloadToDeeplinkConverter.convert(payload)
+
+        // THEN
+        assertThat(result).isNull()
+    }
+
+    @Test
+    fun `GIVEN tangem pay card_ready push payload WHEN convert THEN should return pay-app-main deeplink`() {
+        // GIVEN
+        val payload = mapOf(
+            TYPE_KEY to TangemPayPushNotificationType.CARD_READY.value,
+            CUSTOMER_WALLET_ID_KEY to "wallet123",
+        )
+
+        // WHEN
+        val result = PayloadToDeeplinkConverter.convert(payload)
+
+        // THEN
+        assertThat(result).isEqualTo(
+            "tangem://pay-app-main?type=card_ready&customer_wallet_id=wallet123",
+        )
+    }
+
+    @Test
+    fun `GIVEN tangem pay transaction_spend push payload WHEN convert THEN should return pay-app-main deeplink with transaction_id`() {
+        // GIVEN
+        val payload = mapOf(
+            TYPE_KEY to TangemPayPushNotificationType.TRANSACTION_SPEND.value,
+            CUSTOMER_WALLET_ID_KEY to "wallet123",
+            TRANSACTION_ID_KEY to "test456",
+        )
+
+        // WHEN
+        val result = PayloadToDeeplinkConverter.convert(payload)
+
+        // THEN
+        assertThat(result).isEqualTo(
+            "tangem://pay-app-main?type=transaction_spend&customer_wallet_id=wallet123&transaction_id=test456",
+        )
+    }
+
+    @Test
+    fun `GIVEN tangem pay top_up push payload WHEN convert THEN should return pay-app-main deeplink`() {
+        // GIVEN
+        val payload = mapOf(
+            TYPE_KEY to TangemPayPushNotificationType.TOP_UP.value,
+            CUSTOMER_WALLET_ID_KEY to "wallet123",
+            TRANSACTION_ID_KEY to "test456",
+        )
+
+        // WHEN
+        val result = PayloadToDeeplinkConverter.convert(payload)
+
+        // THEN
+        assertThat(result).isEqualTo(
+            "tangem://pay-app-main?type=declined_top_up&customer_wallet_id=wallet123&transaction_id=test456",
+        )
+    }
+
+    @Test
+    fun `GIVEN tangem pay collateral push payload WHEN convert THEN should return pay-app-main deeplink`() {
+        // GIVEN
+        val payload = mapOf(
+            TYPE_KEY to TangemPayPushNotificationType.COLLATERAL.value,
+            CUSTOMER_WALLET_ID_KEY to "wallet123",
+        )
+
+        // WHEN
+        val result = PayloadToDeeplinkConverter.convert(payload)
+
+        // THEN
+        assertThat(result).isEqualTo(
+            "tangem://pay-app-main?type=collateral&customer_wallet_id=wallet123",
+        )
+    }
+
+    @Test
+    fun `GIVEN tangem pay push payload with missing customer_wallet_id WHEN convert THEN should return null`() {
+        // GIVEN
+        val payload = mapOf(
+            TYPE_KEY to TangemPayPushNotificationType.CARD_READY.value,
+        )
 
         // WHEN
         val result = PayloadToDeeplinkConverter.convert(payload)
