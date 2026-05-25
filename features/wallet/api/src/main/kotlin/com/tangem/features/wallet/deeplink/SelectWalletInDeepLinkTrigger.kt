d9f8fd3bb54d32ac67a22219451d70aa186a104b
Tangem <git@tangem.com>
2026-05-25 22:45:58 +0400

    Updated on 2026-09-21

--- features/wallet/api/src/main/kotlin/com/tangem/features/wallet/deeplink/SelectWalletInDeepLinkTrigger.kt
+++ features/wallet/api/src/main/kotlin/com/tangem/features/wallet/deeplink/SelectWalletInDeepLinkTrigger.kt
@@ -1,12 +1,20 @@
 package com.tangem.features.wallet.deeplink
 
 import com.tangem.domain.models.wallet.UserWalletId
+import com.tangem.domain.visa.model.TangemPayTxHistoryItem
 import kotlinx.coroutines.flow.Flow
 
 interface WalletDeepLinkActionTrigger {
     fun selectWallet(userWalletId: UserWalletId)
+    fun showTangemPayTransaction(transaction: TangemPayTxHistoryItem, customerId: String)
 }
 
 interface WalletDeepLinkActionListener {
     val selectWalletFlow: Flow<UserWalletId>
+    val showTangemPayTransactionFlow: Flow<TangemPayTransactionDeepLinkData>
 }
+
+data class TangemPayTransactionDeepLinkData(
+    val transaction: TangemPayTxHistoryItem,
+    val customerId: String,
+)
