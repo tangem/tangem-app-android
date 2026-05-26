d14ec6244e69b7b7346ed2842724f89b34b0a478
Tangem <git@tangem.com>
2026-05-26 19:04:40 +0400

    Updated on 2026-07-07

--- data/common/src/main/kotlin/com/tangem/data/common/network/NetworkFactory.kt
+++ data/common/src/main/kotlin/com/tangem/data/common/network/NetworkFactory.kt
@@ -375,6 +375,7 @@ class NetworkFactory @Inject constructor(
             Blockchain.Linea, Blockchain.LineaTestnet,
             Blockchain.ArbitrumNova,
             Blockchain.Plasma, Blockchain.PlasmaTestnet,
+            Blockchain.Adi, Blockchain.AdiTestnet,
             Blockchain.Monad, Blockchain.MonadTestnet,
             -> Network.TransactionExtrasType.NONE
             // endregion
