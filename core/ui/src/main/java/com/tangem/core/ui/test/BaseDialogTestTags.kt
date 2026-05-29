16fb10f38d0bb5070b00a5f2a6cb652f1eb63cf7
Tangem <git@tangem.com>
2026-05-29 14:00:48 +0200

    Updated on 2026-08-07

--- core/ui/src/main/java/com/tangem/core/ui/test/BaseDialogTestTags.kt
+++ core/ui/src/main/java/com/tangem/core/ui/test/BaseDialogTestTags.kt
@@ -4,4 +4,5 @@ object BaseDialogTestTags {
     const val CONTAINER = "BASE_DIALOG_CONTAINER"
     const val TITLE = "BASE_DIALOG_TITLE"
     const val TEXT = "BASE_DIALOG_TEXT"
+    const val TEXT_INPUT_FIELD = "BASE_DIALOG_TEXT_INPUT_FIELD"
 }
