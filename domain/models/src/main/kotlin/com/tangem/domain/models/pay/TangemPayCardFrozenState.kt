140a2aba7ad31de124d4d2c1a2f0f7cbc451f700
Tangem <git@tangem.com>
2026-05-29 01:34:49 -0700

    Updated on 2026-07-23

--- domain/models/src/main/kotlin/com/tangem/domain/models/pay/TangemPayCardFrozenState.kt
+++ domain/models/src/main/kotlin/com/tangem/domain/models/pay/TangemPayCardFrozenState.kt
@@ -2,6 +2,7 @@ package com.tangem.domain.models.pay
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import java.util.Locale
 
 @Serializable
 enum class TangemPayCardFrozenState {
@@ -22,9 +23,9 @@ enum class TangemPayCardFrozenState {
     }
 
     companion object {
-        fun fromString(value: String) = when (value) {
-            "Frozen" -> Frozen
-            "Unfrozen" -> Unfrozen
+        fun fromString(value: String) = when (value.lowercase(Locale.US)) {
+            "frozen" -> Frozen
+            "unfrozen" -> Unfrozen
             else -> Pending
         }
     }
