67784d933a4329c7cc05172868a839105eedb5fb
Tangem <git@tangem.com>
2026-06-01 18:58:08 +0200

    Updated on 2026-09-21

--- features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/DefaultTangemPayFeatureToggles.kt
+++ features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/DefaultTangemPayFeatureToggles.kt
@@ -1,3 +1,11 @@
 package com.tangem.features.tangempay
 
-internal class DefaultTangemPayFeatureToggles : TangemPayFeatureToggles
+import com.tangem.core.configtoggle.FeatureToggles
+import com.tangem.core.configtoggle.feature.FeatureTogglesManager
+
+internal class DefaultTangemPayFeatureToggles(
+    private val featureTogglesManager: FeatureTogglesManager,
+) : TangemPayFeatureToggles {
+    override val isRedesignEnabled: Boolean
+        get() = featureTogglesManager.isFeatureEnabled(FeatureToggles.AND_15368_VISA_PAY_REDESIGN)
+}
