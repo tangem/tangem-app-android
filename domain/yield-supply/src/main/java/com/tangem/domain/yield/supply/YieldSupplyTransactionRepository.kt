b02406295ba5fcd28e1a79ac8b3349f00dd3f9f4
Tangem <git@tangem.com>
2026-06-02 16:15:59 +0400

    Updated on 2026-07-01

--- domain/yield-supply/src/main/java/com/tangem/domain/yield/supply/YieldSupplyTransactionRepository.kt
+++ domain/yield-supply/src/main/java/com/tangem/domain/yield/supply/YieldSupplyTransactionRepository.kt
@@ -1,8 +1,10 @@
 package com.tangem.domain.yield.supply
 
+import com.tangem.blockchain.common.Amount
 import com.tangem.blockchain.common.TransactionData
 import com.tangem.blockchain.common.smartcontract.SmartContractCallData
 import com.tangem.blockchain.common.transaction.Fee
+import com.tangem.blockchain.yieldsupply.providers.YieldModuleVersionStatus
 import com.tangem.domain.models.currency.CryptoCurrency
 import com.tangem.domain.models.currency.CryptoCurrencyStatus
 import com.tangem.domain.models.network.Network
@@ -35,5 +37,19 @@ interface YieldSupplyTransactionRepository {
         userWalletId: UserWalletId,
         network: Network,
         callData: SmartContractCallData,
+    ): SmartContractCallData
+
+    /** Returns the on-chain version status of the user's yield module for [network]. */
+    suspend fun getYieldModuleVersionStatus(userWalletId: UserWalletId, network: Network): YieldModuleVersionStatus
+
+    /**
+     * Builds an upgrade-wrapped `withdraw(yieldToken, amount)` call data for the user's yield module.
+     * @throws com.tangem.blockchain.yieldsupply.providers.YieldModuleUpgradeUnavailableException
+     * @throws com.tangem.blockchain.yieldsupply.providers.YieldModuleVersionIndeterminateException
+     */
+    suspend fun createPartialWithdrawCallData(
+        userWalletId: UserWalletId,
+        cryptoCurrency: CryptoCurrency,
+        amount: Amount,
     ): SmartContractCallData
 }
