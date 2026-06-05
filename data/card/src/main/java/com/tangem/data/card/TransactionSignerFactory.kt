4a06930e56d17ff9099b4bf221eb92947f37706a
Tangem <git@tangem.com>
2026-06-05 12:32:13 +0400

    Updated on 2026-06-27

--- data/card/src/main/java/com/tangem/data/card/TransactionSignerFactory.kt
+++ data/card/src/main/java/com/tangem/data/card/TransactionSignerFactory.kt
@@ -3,11 +3,17 @@ package com.tangem.data.card
 import com.tangem.TangemSdk
 import com.tangem.blockchain.common.TransactionSigner
 import com.tangem.domain.card.models.TwinKey
+import com.tangem.domain.models.wallet.UserWalletId
 
 /**
 [REDACTED_AUTHOR]
  */
 interface TransactionSignerFactory {
 
-    fun createTransactionSigner(cardId: String?, sdk: TangemSdk, twinKey: TwinKey?): TransactionSigner
+    fun createTransactionSigner(
+        cardId: String?,
+        sdk: TangemSdk,
+        twinKey: TwinKey?,
+        userWalletId: UserWalletId,
+    ): TransactionSigner
 }
