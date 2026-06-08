1101e0fcb6bd1cdf1c1ec0797c3d1a599f6ad772
Tangem <git@tangem.com>
2026-06-08 18:11:18 +0200

    Updated on 2026-09-23

--- features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/model/transformers/DetailsAddToWalletBannerTransformer.kt
+++ features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/model/transformers/DetailsAddToWalletBannerTransformer.kt
@@ -18,7 +18,6 @@ internal class DetailsAddToWalletBannerTransformer(
                 AddToWalletBlockState(
                     onClick = onClickBanner,
                     onClickClose = onClickCloseBanner,
-                    shouldUseMagicEffect = true,
                 )
             },
         )
