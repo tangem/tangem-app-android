4486dc8f026303934083b00f684be5728d39befd
Tangem <git@tangem.com>
2026-06-08 16:39:52 +0100

    Updated on 2026-09-23

--- features/address-book/impl/src/main/kotlin/com/tangem/features/addressbook/list/model/AddressBookListModel.kt
+++ features/address-book/impl/src/main/kotlin/com/tangem/features/addressbook/list/model/AddressBookListModel.kt
@@ -2,9 +2,6 @@ package com.tangem.features.addressbook.list.model
 
 import com.tangem.core.decompose.di.ModelScoped
 import com.tangem.core.decompose.model.Model
-import com.tangem.core.decompose.model.ParamsContainer
-import com.tangem.features.addressbook.list.AddressBookListComponent
-import com.tangem.features.addressbook.list.contract.AddressBookListEvent
 import com.tangem.features.addressbook.list.contract.AddressBookListUM
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -13,22 +10,10 @@ import javax.inject.Inject
 
 @ModelScoped
 internal class AddressBookListModel @Inject constructor(
-    paramsContainer: ParamsContainer,
     override val dispatchers: CoroutineDispatcherProvider,
 ) : Model() {
-
-    private val params: AddressBookListComponent.Params = paramsContainer.require()
 
     val state: StateFlow<AddressBookListUM> = MutableStateFlow(
         AddressBookListUM.Empty,
     )
-
-    fun onAction(event: AddressBookListEvent) {
-        when (event) {
-            AddressBookListEvent.NewContactClick -> params
-            is AddressBookListEvent.ContactClick -> Unit
-            is AddressBookListEvent.ChipClick -> Unit
-            is AddressBookListEvent.SearchInput -> Unit
-        }
-    }
 }
