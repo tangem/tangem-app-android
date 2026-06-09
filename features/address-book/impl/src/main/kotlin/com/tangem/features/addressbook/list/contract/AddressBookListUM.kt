8300098873ca556f436d2ada21f8e41c6ff8afc0
Tangem <git@tangem.com>
2026-06-09 10:12:31 +0100

    Updated on 2026-09-21

--- features/address-book/impl/src/main/kotlin/com/tangem/features/addressbook/list/contract/AddressBookListUM.kt
+++ features/address-book/impl/src/main/kotlin/com/tangem/features/addressbook/list/contract/AddressBookListUM.kt
@@ -2,10 +2,11 @@ package com.tangem.features.addressbook.list.contract
 
 import androidx.compose.runtime.Immutable
 import com.tangem.domain.addressbook.model.Contact
+import kotlinx.collections.immutable.ImmutableList
 
 @Immutable
 internal sealed class AddressBookListUM {
 
     data object Empty : AddressBookListUM()
-    data class AddressList(val contacts: List<Contact>) : AddressBookListUM()
+    data class AddressList(val contacts: ImmutableList<Contact>) : AddressBookListUM()
 }
