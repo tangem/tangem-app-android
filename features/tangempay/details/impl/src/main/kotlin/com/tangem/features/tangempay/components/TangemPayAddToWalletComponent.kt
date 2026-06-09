29d402198223eb947551d998ec3e9ebfeab54c7b
Tangem <git@tangem.com>
2026-06-09 13:36:12 +0200

    Updated on 2026-09-21

--- features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/components/TangemPayAddToWalletComponent.kt
+++ features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/components/TangemPayAddToWalletComponent.kt
@@ -13,6 +13,7 @@ import com.tangem.features.tangempay.components.cardDetails.DefaultTangemPayCard
 import com.tangem.features.tangempay.components.cardDetails.TangemPayCardDetailsBlockComponent
 import com.tangem.features.tangempay.model.TangemPayAddToWalletModel
 import com.tangem.features.tangempay.ui.TangemPayAddToWalletScreen
+import com.tangem.features.tangempay.ui.TangemPayAddToWalletScreenV2
 import com.tangem.features.tangempay.utils.userWalletId
 
 internal class TangemPayAddToWalletComponent(
@@ -35,9 +36,13 @@ internal class TangemPayAddToWalletComponent(
     override fun Content(modifier: Modifier) {
         val state by model.uiState.collectAsStateWithLifecycle()
         BackHandler(onBack = router::pop)
-        TangemPayAddToWalletScreen(
-            state = state,
-            cardDetailsBlockComponent = cardDetailsBlockComponent,
-        )
+        if (model.isRedesignEnabled()) {
+            TangemPayAddToWalletScreenV2(state = state)
+        } else {
+            TangemPayAddToWalletScreen(
+                state = state,
+                cardDetailsBlockComponent = cardDetailsBlockComponent,
+            )
+        }
     }
 }
