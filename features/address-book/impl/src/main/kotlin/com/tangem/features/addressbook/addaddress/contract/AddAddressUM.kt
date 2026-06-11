367bdec4ab9c9632cfe118256752ed671a78e55a
Tangem <git@tangem.com>
2026-06-11 11:33:19 +0100

    Updated on 2026-09-01

--- features/address-book/impl/src/main/kotlin/com/tangem/features/addressbook/addaddress/contract/AddAddressUM.kt
+++ features/address-book/impl/src/main/kotlin/com/tangem/features/addressbook/addaddress/contract/AddAddressUM.kt
@@ -1,6 +1,7 @@
 package com.tangem.features.addressbook.addaddress.contract
 
 import androidx.annotation.DrawableRes
+import androidx.compose.runtime.Immutable
 import com.tangem.core.ui.ds.button.TangemButtonUM
 import com.tangem.domain.models.network.Network
 import kotlinx.collections.immutable.ImmutableList
@@ -9,19 +10,20 @@ internal data class AddAddressUM(
     val addressField: AddressFieldUM,
     val availableNetworks: ImmutableList<Network>,
     val buttonUM: TangemButtonUM,
-    val chosenNetworkState: ChosenNetworkState,
+    val chosenNetworkStateUM: ChosenNetworkStateUM,
     val onAddressChange: (String) -> Unit,
     val onAddressClear: () -> Unit,
     val onPasteClick: () -> Unit,
     val onQrClick: () -> Unit,
     val onBackClick: () -> Unit,
 ) {
-    sealed class ChosenNetworkState {
-        data object Loading : ChosenNetworkState()
-        data object Empty : ChosenNetworkState()
+    @Immutable
+    sealed class ChosenNetworkStateUM {
+        data object Loading : ChosenNetworkStateUM()
+        data object Empty : ChosenNetworkStateUM()
         data class Result(
             val networkUMList: ImmutableList<NetworkUM>,
-        ) : ChosenNetworkState() {
+        ) : ChosenNetworkStateUM() {
 
             data class NetworkUM(
                 val networkName: String,
