5618f44c8b234525e85f37a27a12e129c52207a0
Tangem <git@tangem.com>
2026-06-17 11:00:59 +0300

    Updated on 2026-08-14

--- app/src/androidTest/kotlin/com/tangem/screens/AddFundsBottomSheetPageObject.kt
+++ app/src/androidTest/kotlin/com/tangem/screens/AddFundsBottomSheetPageObject.kt
@@ -3,11 +3,17 @@ package com.tangem.screens
 import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
 import com.tangem.common.BaseTestCase
 import com.tangem.core.ui.R
+import com.tangem.core.ui.test.AddFundsBottomSheetTestTags
 import com.tangem.core.ui.test.BaseBottomSheetTestTags
+import com.tangem.core.ui.test.BuyTokenScreenTestTags
+import com.tangem.core.ui.test.MarketsTestTags
+import com.tangem.core.ui.test.TokenActionsTestTags
+import com.tangem.core.ui.test.TokenElementsTestTags
 import io.github.kakaocup.compose.node.element.ComposeScreen
 import io.github.kakaocup.compose.node.element.ComposeScreen.Companion.onComposeScreen
 import io.github.kakaocup.compose.node.element.KNode
 import io.github.kakaocup.kakao.common.utilities.getResourceString
+import androidx.compose.ui.test.hasTestTag as withTestTag
 import androidx.compose.ui.test.hasText as withText
 
 class AddFundsBottomSheetPageObject(semanticsProvider: SemanticsNodeInteractionsProvider) :
@@ -32,7 +38,25 @@ class AddFundsBottomSheetPageObject(semanticsProvider: SemanticsNodeInteractions
     }
 
     val closeButton: KNode = child {
-        hasAnyChild(withText(getResourceString(R.string.common_close)))
+        hasTestTag(AddFundsBottomSheetTestTags.CLOSE_BUTTON)
+        useUnmergedTree = true
+    }
+
+    val buyTokenButton: KNode = child {
+        hasTestTag(TokenActionsTestTags.BUY_ACTION)
+        useUnmergedTree = true
+    }
+
+    fun userTokenWithTitle(tokenTitle: String): KNode = child {
+        hasTestTag(BuyTokenScreenTestTags.LAZY_LIST_ITEM)
+        hasAnyDescendant(withTestTag(TokenElementsTestTags.TOKEN_TITLE))
+        hasAnyDescendant(withText(tokenTitle))
+        useUnmergedTree = true
+    }
+
+    fun trendingTokenWithTitle(tokenTitle: String): KNode = child {
+        hasTestTag(MarketsTestTags.TOKENS_LIST_ITEM)
+        hasAnyDescendant(withText(tokenTitle))
         useUnmergedTree = true
     }
 }
