7c87aa740a8472485c280613b5b70d53ceef7e1d
Tangem <git@tangem.com>
2026-06-18 13:50:25 +0500

    Updated on 2026-09-23

--- features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/model/TangemPayChangePinModel.kt
+++ features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/model/TangemPayChangePinModel.kt
@@ -9,16 +9,16 @@ import com.tangem.core.decompose.navigation.Router
 import com.tangem.core.decompose.ui.UiMessageSender
 import com.tangem.core.ui.extensions.resourceReference
 import com.tangem.core.ui.message.ToastMessage
+import com.tangem.domain.pay.flow.PaymentAccountStatusFetcher
 import com.tangem.domain.pay.model.SetPinResult
 import com.tangem.domain.pay.repository.TangemPayCardDetailsRepository
 import com.tangem.domain.tangempay.TangemPayAnalyticsEvents
 import com.tangem.features.tangempay.TangemPayFeatureToggles
-import com.tangem.features.tangempay.components.TangemPayDetailsContainerComponent
+import com.tangem.features.tangempay.components.TangemPayChangePinComponent
 import com.tangem.features.tangempay.details.impl.R
 import com.tangem.features.tangempay.entity.TangemPayChangePinUM
 import com.tangem.features.tangempay.model.transformers.PinCodeChangeTransformer
 import com.tangem.features.tangempay.navigation.TangemPayCardDetailsInnerRoute
-import com.tangem.features.tangempay.utils.userWalletId
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import com.tangem.utils.logging.TangemLogger
 import com.tangem.utils.transformer.update
@@ -39,9 +39,10 @@ internal class TangemPayChangePinModel @Inject constructor(
     private val cardDetailsRepository: TangemPayCardDetailsRepository,
     private val analytics: AnalyticsEventHandler,
     private val featureToggles: TangemPayFeatureToggles,
+    private val fetcher: PaymentAccountStatusFetcher,
 ) : Model() {
 
-    private val params: TangemPayDetailsContainerComponent.Params = paramsContainer.require()
+    private val params: TangemPayChangePinComponent.Params = paramsContainer.require()
     val uiState: StateFlow<TangemPayChangePinUM>
         field = MutableStateFlow(getInitialState())
 
@@ -66,7 +67,8 @@ internal class TangemPayChangePinModel @Inject constructor(
             uiState.update { it.copy(submitButtonLoading = true) }
             val result = try {
                 cardDetailsRepository.setPin(
-                    userWalletId = params.initialStatus.userWalletId,
+                    userWalletId = params.userWalletId,
+                    cardId = params.card.id,
                     pin = uiState.value.pinCode,
                 ).getOrNull()
             } catch (e: Exception) {
@@ -83,6 +85,7 @@ internal class TangemPayChangePinModel @Inject constructor(
                     )
                 }
                 SetPinResult.SUCCESS -> {
+                    fetcher.invoke(params = PaymentAccountStatusFetcher.Params(userWalletId = params.userWalletId))
                     analytics.send(TangemPayAnalyticsEvents.ChangePinSuccessShown())
                     router.push(TangemPayCardDetailsInnerRoute.ChangePINSuccess)
                 }
