5aaf09f7fbab9d1394fdbb7acaddda116e120c86
Tangem <git@tangem.com>
2026-06-19 13:20:36 +0200

    Updated on 2026-09-21

--- core/analytics/models/src/main/java/com/tangem/core/analytics/models/event/SwapAnalyticsEvent.kt
+++ core/analytics/models/src/main/java/com/tangem/core/analytics/models/event/SwapAnalyticsEvent.kt
@@ -15,19 +15,6 @@ sealed class SwapAnalyticsEvent(
     params: Map<String, String> = emptyMap(),
 ) : AnalyticsEvent("Swap", event, params) {
 
-    data class TokenSelected(
-        val token: String,
-        val source: ScreensSources,
-        val isSearched: Boolean,
-    ) : SwapAnalyticsEvent(
-        event = "Token Selected",
-        params = mapOf(
-            TOKEN_PARAM to token,
-            SOURCE to source.value,
-            SEARCHED to if (isSearched) "True" else "False",
-        ),
-    )
-
     class FilterProvider(filterType: String) : SwapAnalyticsEvent(
         event = "Filter Provider",
         params = mapOf(TYPE to filterType),
