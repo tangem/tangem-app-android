d5032a800e080ca2d814a815bc18ce393fb4f6f3
Tangem <git@tangem.com>
2026-06-19 11:38:22 +0400

    Updated on 2026-09-21

--- libs/auth/src/main/java/com/tangem/lib/auth/session/internal/DisabledDeviceRegistrar.kt
+++ libs/auth/src/main/java/com/tangem/lib/auth/session/internal/DisabledDeviceRegistrar.kt
@@ -4,7 +4,9 @@ import arrow.core.Either
 import arrow.core.left
 import com.tangem.lib.auth.session.DeviceRegistrar
 import com.tangem.lib.auth.session.DeviceRegistrationError
+import com.tangem.utils.annotations.RemoveWithToggle
 
+@RemoveWithToggle("AND_15438_BACKEND_AUTHENTICATION_ENABLED")
 internal object DisabledDeviceRegistrar : DeviceRegistrar {
 
     override suspend fun register(): Either<DeviceRegistrationError, Unit> {
