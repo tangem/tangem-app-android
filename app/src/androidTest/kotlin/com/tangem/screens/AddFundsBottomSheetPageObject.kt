633666c3ce84ac07543106d15689879df7527326
Tangem <git@tangem.com>
2026-06-22 11:20:46 +0300

    Updated on 2026-08-14

--- app/src/androidTest/kotlin/com/tangem/screens/AddFundsBottomSheetPageObject.kt
+++ app/src/androidTest/kotlin/com/tangem/screens/AddFundsBottomSheetPageObject.kt
@@ -1,16 +1,21 @@
 package com.tangem.screens
 
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.SemanticsMatcher
 import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
 import com.tangem.common.BaseTestCase
+import com.tangem.common.utils.LazyListItemNode
 import com.tangem.core.ui.R
 import com.tangem.core.ui.test.BaseBottomSheetTestTags
 import com.tangem.core.ui.test.BuyTokenScreenTestTags
 import com.tangem.core.ui.test.MarketsTestTags
 import com.tangem.core.ui.test.TokenActionsTestTags
 import com.tangem.core.ui.test.TokenElementsTestTags
+import com.tangem.core.ui.utils.LazyListItemPositionSemantics
 import io.github.kakaocup.compose.node.element.ComposeScreen
 import io.github.kakaocup.compose.node.element.ComposeScreen.Companion.onComposeScreen
 import io.github.kakaocup.compose.node.element.KNode
+import io.github.kakaocup.compose.node.element.lazylist.KLazyListNode
 import io.github.kakaocup.kakao.common.utilities.getResourceString
 import androidx.compose.ui.test.hasTestTag as withTestTag
 import androidx.compose.ui.test.hasText as withText
@@ -53,11 +58,22 @@ class AddFundsBottomSheetPageObject(semanticsProvider: SemanticsNodeInteractions
         useUnmergedTree = true
     }
 
-    fun trendingTokenWithTitle(tokenTitle: String): KNode = child {
-        hasTestTag(MarketsTestTags.TOKENS_LIST_ITEM)
-        hasAnyDescendant(withText(tokenTitle))
-        useUnmergedTree = true
-    }
+    private val trendingTokensList = KLazyListNode(
+        semanticsProvider = semanticsProvider,
+        viewBuilderAction = { hasTestTag(BuyTokenScreenTestTags.LAZY_LIST) },
+        itemTypeBuilder = { itemType(::LazyListItemNode) },
+        positionMatcher = { position ->
+            SemanticsMatcher.expectValue(LazyListItemPositionSemantics, position)
+        },
+    )
+
+    @OptIn(ExperimentalTestApi::class)
+    fun trendingTokenWithTitle(tokenTitle: String): LazyListItemNode =
+        trendingTokensList.childWith<LazyListItemNode> {
+            hasTestTag(MarketsTestTags.TOKENS_LIST_ITEM)
+            hasText(tokenTitle)
+            useUnmergedTree = true
+        }
 }
 
 internal fun BaseTestCase.onAddFundsBottomSheet(function: AddFundsBottomSheetPageObject.() -> Unit) =
