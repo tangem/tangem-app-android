bf9fd48ff631ee1ad00c5590d9e04ba96eeeda79
Tangem <git@tangem.com>
2026-06-22 16:36:07 +0400

    Updated on 2026-08-27

--- features/home/impl/src/main/kotlin/com/tangem/features/home/impl/di/HomeFeatureModule.kt
+++ features/home/impl/src/main/kotlin/com/tangem/features/home/impl/di/HomeFeatureModule.kt
@@ -1,12 +1,16 @@
 package com.tangem.features.home.impl.di
 
+import com.tangem.core.configtoggle.feature.FeatureTogglesManager
 import com.tangem.core.decompose.di.ModelComponent
 import com.tangem.core.decompose.model.Model
 import com.tangem.features.home.api.HomeComponent
+import com.tangem.features.home.api.HomeFeatureToggles
 import com.tangem.features.home.impl.DefaultHomeComponent
+import com.tangem.features.home.impl.DefaultHomeFeatureToggles
 import com.tangem.features.home.impl.model.HomeModel
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import dagger.multibindings.ClassKey
@@ -20,6 +24,17 @@ internal interface ComponentModule {
     @Binds
     @Singleton
     fun bindComponent(factory: DefaultHomeComponent.Factory): HomeComponent.Factory
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object HomeFeatureTogglesModule {
+
+    @Provides
+    @Singleton
+    fun provideHomeFeatureToggles(featureTogglesManager: FeatureTogglesManager): HomeFeatureToggles {
+        return DefaultHomeFeatureToggles(featureTogglesManager)
+    }
 }
 
 @Module
