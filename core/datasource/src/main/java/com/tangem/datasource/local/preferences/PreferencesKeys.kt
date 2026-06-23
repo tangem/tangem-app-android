3c215ad5731c8140bd9a67d344c56882f6c6f8d5
Tangem <git@tangem.com>
2026-06-23 19:45:31 +0400

    Updated on 2026-08-19

--- core/datasource/src/main/java/com/tangem/datasource/local/preferences/PreferencesKeys.kt
+++ core/datasource/src/main/java/com/tangem/datasource/local/preferences/PreferencesKeys.kt
@@ -117,6 +117,9 @@ object PreferencesKeys {
 
     val IS_DEVICE_REGISTERED_KEY by lazy { booleanPreferencesKey(name = "isDeviceRegistered") }
 
+    /** Base64 `UserWalletId`s already registered with the Tangem Auth Service (`/auth/wallet`). */
+    val REGISTERED_WALLET_IDS_KEY by lazy { stringSetPreferencesKey(name = "registeredWalletIds") }
+
     val WAS_LOG_FILE_CLEARED by lazy { booleanPreferencesKey(name = "wasLogFileCleared") }
 
     val SEED_FIRST_NOTIFICATION_SHOW_TIME by lazy { longPreferencesKey("seedFirstNotificationTime") }
