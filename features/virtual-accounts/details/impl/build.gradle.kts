3fcc32c5f2903fa8d214027e6d3a41b1a16181a6
Tangem <git@tangem.com>
2026-06-24 01:21:15 -0700

    Updated on 2026-09-14

--- features/virtual-accounts/details/impl/build.gradle.kts
+++ features/virtual-accounts/details/impl/build.gradle.kts
@@ -11,11 +11,25 @@ android {
 }
 
 dependencies {
+    /** Core */
+    implementation(projects.core.configToggles)
+    implementation(projects.core.decompose)
+    implementation(projects.core.res)
+    implementation(projects.core.ui)
+    implementation(projects.core.utils)
+
+    /** Domain */
+    implementation(projects.domain.models)
+
+    /** Features */
     implementation(projects.features.virtualAccounts.details.api)
 
-    implementation(projects.core.configToggles)
-
-    implementation(deps.compose.runtime)
+    /** Compose */
+    implementation(deps.compose.foundation)
+    implementation(deps.compose.material3)
+    implementation(deps.compose.ui)
+    implementation(deps.compose.ui.tooling)
+    implementation(deps.decompose.ext.compose)
 
     /** DI */
     implementation(deps.hilt.android)
