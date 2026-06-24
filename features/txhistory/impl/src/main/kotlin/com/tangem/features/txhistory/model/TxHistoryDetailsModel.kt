f2bc6fc7142ca167db02a0e46154f91f6511381a
Tangem <git@tangem.com>
2026-06-24 19:26:35 +0300

    Updated on 2026-09-21

--- features/txhistory/impl/src/main/kotlin/com/tangem/features/txhistory/model/TxHistoryDetailsModel.kt
+++ features/txhistory/impl/src/main/kotlin/com/tangem/features/txhistory/model/TxHistoryDetailsModel.kt
@@ -6,7 +6,7 @@ import com.tangem.core.decompose.model.Model
 import com.tangem.core.decompose.model.ParamsContainer
 import com.tangem.core.ui.clipboard.ClipboardManager
 import com.tangem.features.txhistory.component.TxHistoryDetailsComponent
-import com.tangem.features.txhistory.converter.TxInfoToTxHistoryDetailsUMConverter
+import com.tangem.features.txhistory.converter.TxHistoryInfoToTxHistoryDetailsUMConverter
 import com.tangem.features.txhistory.entity.TxHistoryDetailsUM
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import kotlinx.coroutines.flow.SharingStarted
@@ -26,12 +26,12 @@ internal class TxHistoryDetailsModel @Inject constructor(
 
     private val params: TxHistoryDetailsComponent.Params = paramsContainer.require()
 
-    private val converter = TxInfoToTxHistoryDetailsUMConverter(
+    private val converter = TxHistoryInfoToTxHistoryDetailsUMConverter(
         currency = params.currency,
         onCopyAddress = ::onCopyAddress,
     )
 
-    val uiState: StateFlow<TxHistoryDetailsUM?> = params.txInfo
+    val uiState: StateFlow<TxHistoryDetailsUM?> = params.txHistoryInfo
         .map(converter::convert)
         .flowOn(dispatchers.default)
         .stateIn(modelScope, SharingStarted.WhileSubscribed(), initialValue = null)
