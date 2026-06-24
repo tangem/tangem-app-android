473cb0fcb3f639234865a96b235d01ffbb2f087d
Tangem <git@tangem.com>
2026-06-24 18:31:37 +0500

    Updated on 2026-09-25

--- features/send/impl/src/main/java/com/tangem/features/send/subcomponents/amount/DefaultSendAmountReduceTrigger.kt
+++ features/send/impl/src/main/java/com/tangem/features/send/subcomponents/amount/DefaultSendAmountReduceTrigger.kt
@@ -1,45 +1,14 @@
 package com.tangem.features.send.subcomponents.amount
 
 import com.tangem.common.ui.amountScreen.converters.AmountReduceByTransformer.ReduceByData
-import kotlinx.coroutines.flow.Flow
+import com.tangem.features.send.api.subcomponents.amount.SendAmountReduceListener
+import com.tangem.features.send.api.subcomponents.amount.SendAmountReduceTrigger
+import com.tangem.features.send.api.subcomponents.amount.SendAmountUpdateListener
+import com.tangem.features.send.api.subcomponents.amount.SendAmountUpdateTrigger
 import kotlinx.coroutines.flow.MutableSharedFlow
 import java.math.BigDecimal
 import javax.inject.Inject
 import javax.inject.Singleton
-
-/**
- * Trigger for reducing amount from another component
- */
-interface SendAmountReduceTrigger {
-    suspend fun triggerReduceBy(reduceBy: ReduceByData)
-    suspend fun triggerReduceTo(reduceTo: BigDecimal)
-    suspend fun triggerIgnoreReduce()
-}
-
-/**
- * Trigger for reducing amount from another component
- */
-interface SendAmountReduceListener {
-    val reduceToTriggerFlow: Flow<BigDecimal>
-    val reduceByTriggerFlow: Flow<ReduceByData>
-    val ignoreReduceTriggerFlow: Flow<Unit>
-}
-
-/**
- * Trigger amount change from another component.
- * Different from another triggers because it takes raw string instead of BigDecimal
- */
-interface SendAmountUpdateTrigger {
-    suspend fun triggerUpdateAmount(amountValue: String, isEnterInFiatSelected: Boolean?)
-}
-
-/**
- * Trigger amount change from another component.
- * Different from another triggers because it takes raw string instead of BigDecimal
- */
-interface SendAmountUpdateListener {
-    val updateAmountTriggerFlow: Flow<Pair<String, Boolean?>>
-}
 
 @Singleton
 internal class DefaultSendAmountReduceTrigger @Inject constructor() :
