81b7492080f7d0973bd5bfcc3ab7ce23705c85e8
Tangem <git@tangem.com>
2026-06-24 16:33:17 +0300

    Updated on 2026-09-23

--- features/txhistory/impl/src/main/kotlin/com/tangem/features/txhistory/utils/TxHistoryListManager.kt
+++ features/txhistory/impl/src/main/kotlin/com/tangem/features/txhistory/utils/TxHistoryListManager.kt
@@ -16,6 +16,7 @@ import com.tangem.pagination.BatchAction
 import com.tangem.pagination.BatchFetchResult
 import com.tangem.pagination.BatchListState
 import com.tangem.pagination.PaginationStatus
+import com.tangem.utils.annotations.RemoveWithToggle
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
 import com.tangem.utils.coroutines.JobHolder
 import com.tangem.utils.coroutines.saveIn
@@ -26,6 +27,8 @@ import kotlinx.coroutines.flow.*
 private typealias TxHistoryBatchAction = BatchAction<Int, TxHistoryListConfig, Nothing>
 
 @Suppress("LongParameterList")
+@Deprecated("Remove with toggle [TxHistoryFeatureToggles.isNewTxHistoryEnabled]. Replaced by HistoryTxListManager.")
+@RemoveWithToggle("AND_15767_NEW_TX_HISTORY_ENABLED")
 internal class TxHistoryListManager(
     private val repository: TxHistoryRepositoryV2,
     private val dispatchers: CoroutineDispatcherProvider,
