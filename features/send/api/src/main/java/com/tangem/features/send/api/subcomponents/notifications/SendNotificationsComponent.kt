2d255d87f592e455cd11969e38f4520a0e5ba461
Tangem <git@tangem.com>
2026-06-24 18:00:16 +0500

    Updated on 2026-09-21

--- features/send/api/src/main/java/com/tangem/features/send/api/subcomponents/notifications/SendNotificationsComponent.kt
+++ features/send/api/src/main/java/com/tangem/features/send/api/subcomponents/notifications/SendNotificationsComponent.kt
@@ -1,4 +1,4 @@
-package com.tangem.features.send.api
+package com.tangem.features.send.api.subcomponents.notifications
 
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.ui.Modifier
