53e4dc700145d5fcfb793df34e6d7c6d8905bb13
Tangem <git@tangem.com>
2026-06-24 17:33:46 +0400

    Updated on 2026-08-07

--- features/home/impl/build.gradle.kts
+++ features/home/impl/build.gradle.kts
@@ -13,59 +13,41 @@ android {
 dependencies {
     /** Api */
     implementation(projects.features.home.api)
-    implementation(projects.features.hotWallet.api)
 
     /** Core modules */
     implementation(projects.core.decompose)
     implementation(projects.core.ui)
-    implementation(projects.core.res)
     implementation(projects.core.analytics)
     implementation(projects.core.analytics.models)
-    implementation(projects.core.navigation)
     implementation(projects.core.utils)
     implementation(projects.core.configToggles)
 
     /** Common */
     implementation(projects.common.routing)
-    
+
     /** Domain */
     implementation(projects.domain.common)
     implementation(projects.domain.models)
-    implementation(projects.domain.core)
     implementation(projects.domain.card)
     implementation(projects.domain.settings)
-    implementation(projects.domain.tokens)
     implementation(projects.domain.wallets)
-    implementation(projects.domain.wallets.models)
-    implementation(projects.domain.legacy)
-    implementation(projects.domain.feedback)
-    implementation(projects.domain.feedback.models)
-    implementation(projects.domain.referral)
 
     /** Referral */
     implementation(projects.features.referral.domain)
 
-    /** AndroidX libraries */
-    implementation(deps.androidx.activity.compose)
-    implementation(deps.lifecycle.runtime.ktx)
-    
     /** Compose libraries */
     implementation(deps.compose.ui)
     implementation(deps.compose.ui.tooling)
     implementation(deps.compose.foundation)
     implementation(deps.compose.material3)
     implementation(deps.compose.animation)
-    implementation(deps.compose.coil)
-    implementation(deps.decompose.ext.compose)
-    
+
     /** Tangem libraries */
-    implementation(tangemDeps.card.android)
     implementation(tangemDeps.card.core)
-    implementation(tangemDeps.blockchain)
-    
+
     /** Other libraries */
     implementation(deps.kotlin.immutable.collections)
-    
+
     /** DI */
     implementation(deps.hilt.android)
     kapt(deps.hilt.kapt)
