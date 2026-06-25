f6d895f02427a49f515b24ecb0d7fee9932009f5
Tangem <git@tangem.com>
2026-06-25 03:20:02 -0700

    Updated on 2026-07-01

--- domain/models/src/main/kotlin/com/tangem/domain/models/pay/TangemPayEligibilityType.kt
+++ domain/models/src/main/kotlin/com/tangem/domain/models/pay/TangemPayEligibilityType.kt
@@ -4,14 +4,42 @@ enum class TangemPayEligibilityType {
 
     BANNER,
     DETAILS,
+    DEEPLINK,
+
+    BANNER_VIRTUAL_ACCOUNT,
+    DETAILS_VIRTUAL_ACCOUNT,
+    DEEPLINK_VIRTUAL_ACCOUNT,
+
     UNKNOWN,
     ;
 
     companion object {
-        fun fromString(value: String): TangemPayEligibilityType = when (value.lowercase()) {
-            "banner" -> BANNER
-            "details" -> DETAILS
+        fun fromString(value: String): TangemPayEligibilityType = when (value.uppercase()) {
+            "BANNER" -> BANNER
+            "DETAILS" -> DETAILS
+            "DEEPLINK" -> DEEPLINK
+            "BANNER_VIRTUAL_ACCOUNT" -> BANNER_VIRTUAL_ACCOUNT
+            "DETAILS_VIRTUAL_ACCOUNT" -> DETAILS_VIRTUAL_ACCOUNT
+            "DEEPLINK_VIRTUAL_ACCOUNT" -> DEEPLINK_VIRTUAL_ACCOUNT
             else -> UNKNOWN
         }
     }
 }
+
+val TangemPayEligibilityType.isVirtualAccountType: Boolean
+    get() = this in VIRTUAL_ACCOUNT_TYPES
+
+val TangemPayEligibilityType.isTangemPayType: Boolean
+    get() = this in TANGEM_PAY_TYPES
+
+private val VIRTUAL_ACCOUNT_TYPES = setOf(
+    TangemPayEligibilityType.BANNER_VIRTUAL_ACCOUNT,
+    TangemPayEligibilityType.DETAILS_VIRTUAL_ACCOUNT,
+    TangemPayEligibilityType.DEEPLINK_VIRTUAL_ACCOUNT,
+)
+
+private val TANGEM_PAY_TYPES = setOf(
+    TangemPayEligibilityType.BANNER,
+    TangemPayEligibilityType.DETAILS,
+    TangemPayEligibilityType.DEEPLINK,
+)
--- domain/virtual-account/models/build.gradle.kts
+++ domain/virtual-account/models/build.gradle.kts
@@ -10,4 +10,5 @@ android {
 }
 
 dependencies {
+    api(projects.domain.models)
 }
