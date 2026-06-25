462fa47417687d6624711392e21b636eb95d4241
Tangem <git@tangem.com>
2026-06-25 20:02:05 +0500

    Updated on 2026-09-23

--- core/config-toggles/src/test/kotlin/com/tangem/core/configtoggle/feature/FeatureTogglesNamingConventionTest.kt
+++ core/config-toggles/src/test/kotlin/com/tangem/core/configtoggle/feature/FeatureTogglesNamingConventionTest.kt
@@ -50,7 +50,6 @@ internal class FeatureTogglesNamingConventionTest {
             "SOLANA_TX_HISTORY_ENABLED",
             "STAKING_ETH_ENABLED",
             "SWAP_AB_ENABLED",
-            "USEDESK_ENABLED",
             "VIRTUAL_ACCOUNTS_ENABLED",
             "VISA_ONBOARDING_ENABLED",
             "WALLET_CONNECT_BITCOIN_ENABLED",
