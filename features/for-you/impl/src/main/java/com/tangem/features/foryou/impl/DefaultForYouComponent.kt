9b986be878d5795852152d0dbb8d58f98d70c909
Tangem <git@tangem.com>
2026-06-29 13:27:31 +0500

    Updated on 2026-07-01

--- features/for-you/impl/src/main/java/com/tangem/features/foryou/impl/DefaultForYouComponent.kt
+++ features/for-you/impl/src/main/java/com/tangem/features/foryou/impl/DefaultForYouComponent.kt
@@ -1,11 +1,27 @@
 package com.tangem.features.foryou.impl
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
 import com.tangem.core.decompose.context.AppComponentContext
+import com.tangem.core.ui.R
 import com.tangem.core.ui.components.bottomsheets.state.BottomSheetState
+import com.tangem.core.ui.components.haze.hazeEffectTangem
+import com.tangem.core.ui.ds.topbar.TangemTopBar
+import com.tangem.core.ui.ds.topbar.TangemTopBarType
+import com.tangem.core.ui.extensions.clickableSingle
+import com.tangem.core.ui.extensions.resourceReference
+import com.tangem.core.ui.res.TangemTheme
 import com.tangem.features.foryou.ForYouComponent
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
@@ -18,7 +34,26 @@ internal class DefaultForYouComponent @AssistedInject constructor(
 
     @Composable
     override fun Title(bottomSheetState: State<BottomSheetState>) {
-        TODO("Not yet implemented")
+        TangemTopBar(
+            title = resourceReference(R.string.for_you_title),
+            type = TangemTopBarType.BottomSheet,
+            startContent = {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_arrow_back_28),
+                    contentDescription = null,
+                    tint = TangemTheme.colors3.icon.primary,
+                    modifier = Modifier
+                        .size(44.dp)
+                        .clip(CircleShape)
+                        .hazeEffectTangem { blurRadius = 8.dp }
+                        .clickableSingle(
+                            onClick = router::pop,
+                            enabled = bottomSheetState.value == BottomSheetState.EXPANDED,
+                        )
+                        .padding(8.dp),
+                )
+            },
+        )
     }
 
     @Composable
@@ -27,7 +62,7 @@ internal class DefaultForYouComponent @AssistedInject constructor(
         contentPadding: PaddingValues,
         modifier: Modifier,
     ) {
-        TODO("Not yet implemented")
+        Text("FOR YOU")
     }
 
     @AssistedFactory
