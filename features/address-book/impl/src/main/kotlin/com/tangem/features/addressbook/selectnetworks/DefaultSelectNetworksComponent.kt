145d6dee61066f32fd7ac0948a71a0ab1400f33d
Tangem <git@tangem.com>
2026-06-29 14:54:25 +0200

    Updated on 2026-09-14

--- features/address-book/impl/src/main/kotlin/com/tangem/features/addressbook/selectnetworks/DefaultSelectNetworksComponent.kt
+++ features/address-book/impl/src/main/kotlin/com/tangem/features/addressbook/selectnetworks/DefaultSelectNetworksComponent.kt
@@ -1,4 +1,4 @@
-package com.tangem.features.addressbook.addaddress
+package com.tangem.features.addressbook.selectnetworks
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.Composable
@@ -8,21 +8,20 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.tangem.core.decompose.context.AppComponentContext
 import com.tangem.core.decompose.model.getOrCreateModel
 import com.tangem.core.ui.decompose.ComposableContentComponent
-import com.tangem.features.addressbook.addaddress.model.AddAddressModel
-import com.tangem.features.addressbook.addaddress.ui.AddAddressContent
-import com.tangem.features.addressbook.editcontact.ui.state.ValidatedAddress
+import com.tangem.features.addressbook.selectnetworks.model.SelectNetworksModel
+import com.tangem.features.addressbook.selectnetworks.ui.SelectNetworksContent
 
-internal class DefaultAddAddressComponent(
+internal class DefaultSelectNetworksComponent(
     appComponentContext: AppComponentContext,
     params: Params,
 ) : ComposableContentComponent, AppComponentContext by appComponentContext {
 
-    private val model: AddAddressModel = getOrCreateModel(params)
+    private val model: SelectNetworksModel = getOrCreateModel(params)
 
     @Composable
     override fun Content(modifier: Modifier) {
         val state by model.state.collectAsStateWithLifecycle()
-        AddAddressContent(
+        SelectNetworksContent(
             state = state,
             modifier = modifier,
         )
@@ -30,8 +29,9 @@ internal class DefaultAddAddressComponent(
     }
 
     data class Params(
+        val address: String,
+        val selectedNetworkIds: List<String>,
         val onBackClick: () -> Unit,
-        val onSelectNetworksClick: (address: String, selectedNetworkIds: List<String>) -> Unit,
-        val onConfirm: (ValidatedAddress) -> Unit,
+        val onDone: (selectedNetworkIds: Set<String>) -> Unit,
     )
 }
