62b1174d07de07944cc5d3605ac462d17c032949
Tangem <git@tangem.com>
2026-06-30 19:47:48 +0400

    Updated on 2026-09-23

--- domain/common/build.gradle.kts
+++ domain/common/build.gradle.kts
@@ -6,15 +6,11 @@ plugins {
 
 dependencies {
     api(deps.arrow.core)
-    api(deps.arrow.fx)
     api(deps.kotlin.coroutines)
-    implementation(deps.kotlin.serialization)
 
     api(projects.core.analytics.models)
     api(projects.domain.models)
 
     testImplementation(deps.test.coroutine)
-    testImplementation(deps.test.junit)
     testImplementation(deps.test.mockk)
-    testImplementation(deps.test.truth)
 }
