32a2299d37f90bb1e4a1c9494b8432fb80700164
Tangem <git@tangem.com>
2026-07-02 18:18:34 +0400

    Updated on 2026-09-21

--- data/txhistory/build.gradle.kts
+++ data/txhistory/build.gradle.kts
@@ -9,47 +9,66 @@ android {
     namespace = "com.tangem.data.txhistory"
 }
 
-tasks.withType<Test>().configureEach {
-    useJUnitPlatform()
-}
-
 dependencies {
-    implementation(projects.data.common)
 
-    implementation(projects.core.utils)
-    implementation(projects.core.datasource)
-    implementation(projects.core.pagination)
-    implementation(projects.core.analytics)
+    // region Kotlin
+    api(deps.kotlin.coroutines)
+    // endregion
 
-    implementation(projects.domain.legacy)
-    implementation(projects.domain.common)
-    implementation(projects.domain.walletManager)
-    implementation(projects.domain.models)
-    implementation(projects.domain.tokens.models)
-    implementation(projects.domain.txhistory)
-    implementation(projects.domain.txhistory.models)
-    implementation(projects.domain.express.models)
-    implementation(projects.domain.wallets.models)
-    implementation(projects.domain.wallets)
-    implementation(projects.domain.onramp)
-    implementation(projects.domain.onramp.models)
-    implementation(projects.domain.account)
-    implementation(projects.domain.account.status)
-
-    implementation(projects.libs.blockchainSdk)
-
-    implementation(deps.kotlin.coroutines)
+    // region Other libraries
+    implementation(deps.androidx.annotation)
     implementation(deps.androidx.paging.runtime)
+    implementation(deps.arrow.core)
     implementation(deps.jodatime)
-    implementation(tangemDeps.blockchain)
+    implementation(deps.room.runtime)
+    // endregion
 
+    // region Tangem SDK
+    implementation(tangemDeps.blockchain)
+    // endregion
+
+    // region DI
     implementation(deps.hilt.core)
     kapt(deps.hilt.kapt)
+    // endregion
+
+    // region Core modules
+    api(projects.core.analytics)
+    api(projects.core.datasource)
+    api(projects.core.utils)
+    implementation(projects.core.pagination)
+    // endregion
+
+    // region Data
+    api(projects.data.common)
+    // endregion
+
+    // region Domain
+    api(projects.domain.account)
+    api(projects.domain.common)
+    api(projects.domain.express)
+    api(projects.domain.models)
+    api(projects.domain.onramp)
+    api(projects.domain.txhistory)
+    api(projects.domain.walletManager)
+    api(projects.domain.wallets)
+    implementation(projects.domain.account.status)
+    // endregion
+
+    // region Domain models
+    implementation(projects.domain.express.models)
+    implementation(projects.domain.onramp.models)
+    implementation(projects.domain.tokens.models)
+    implementation(projects.domain.txhistory.models)
+    // endregion
+
+    // region Libs
+    api(projects.libs.blockchainSdk)
+    // endregion
 
     // region Test
     testImplementation(projects.common.test)
     testImplementation(projects.test.core)
     testImplementation(projects.test.mock)
-    testRuntimeOnly(deps.test.junit5.engine)
     // endregion
 }
