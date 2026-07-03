f82d79b3011b47df08763038aff1a74f93ae6f37
Tangem <git@tangem.com>
2026-07-03 17:01:57 +0100

    Updated on 2026-08-19

--- domain/address-book/src/main/kotlin/com/tangem/domain/addressbook/model/AddressEntry.kt
+++ domain/address-book/src/main/kotlin/com/tangem/domain/addressbook/model/AddressEntry.kt
@@ -1,15 +1,24 @@
 package com.tangem.domain.addressbook.model
 
+import com.tangem.domain.addressbook.model.serialization.NetworkRawIdAsStringSerializer
 import com.tangem.domain.models.network.Network
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 /** A single saved address belonging to a [Contact]. */
 @Serializable
 data class AddressEntry(
+    @SerialName("id")
     val id: AddressEntryId,
+    @SerialName("address")
     val address: String,
+    @SerialName("networkId")
+    @Serializable(with = NetworkRawIdAsStringSerializer::class)
     val networkId: Network.RawID,
+    @SerialName("networkName")
     val networkName: String,
+    @SerialName("memo")
     val memo: String?,
+    @SerialName("signature")
     val signature: String,
 )
