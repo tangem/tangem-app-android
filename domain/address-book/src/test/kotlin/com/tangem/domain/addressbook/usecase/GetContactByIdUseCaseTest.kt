34e22d576462e9724f612771cefb4cb7f3053189
Tangem <git@tangem.com>
2026-07-03 12:29:53 +0100

    Updated on 2026-08-07

--- domain/address-book/src/test/kotlin/com/tangem/domain/addressbook/usecase/GetContactByIdUseCaseTest.kt
+++ domain/address-book/src/test/kotlin/com/tangem/domain/addressbook/usecase/GetContactByIdUseCaseTest.kt
@@ -60,6 +60,6 @@ class GetContactByIdUseCaseTest {
         iconColor = "Azure",
         createdAt = "2026-01-01T00:00:00.000Z",
         updatedAt = "2026-01-01T00:00:00.000Z",
-        addressEntries = emptyList(),
+        addresses = emptyList(),
     )
 }
