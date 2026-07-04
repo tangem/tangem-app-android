9897dee0bd58a8b4cface47351c52f65f522540c
Tangem <git@tangem.com>
2026-07-04 12:13:53 +0300

    Updated on 2026-09-21

--- features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/DefaultOnboardingMultiWalletComponent.kt
+++ features/onboarding-v2/impl/src/main/kotlin/com/tangem/features/onboarding/v2/multiwallet/impl/DefaultOnboardingMultiWalletComponent.kt
@@ -1,5 +1,6 @@
 package com.tangem.features.onboarding.v2.multiwallet.impl
 
+import android.util.Log
 import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -211,6 +212,7 @@ internal class DefaultOnboardingMultiWalletComponent @AssistedInject constructor
     }
 
     private fun handleNavigationEvent(nextStep: OnboardingMultiWalletState.Step) {
+        Log.e("wallet3", "handleNavigationEvent nextStep: $nextStep")
         when (nextStep) {
             ChooseBackupOption -> {
                 artworksState.value = WalletArtworksState.Fan
