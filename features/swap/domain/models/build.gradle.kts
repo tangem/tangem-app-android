a606d1934c2d1d86bf3fe8b0b097601ee9484204
Tangem <git@tangem.com>
2026-07-06 17:54:40 +0400

    Updated on 2026-09-23

--- features/swap/domain/models/build.gradle.kts
+++ features/swap/domain/models/build.gradle.kts
@@ -1,30 +1,9 @@
 plugins {
     alias(deps.plugins.android.library)
     alias(deps.plugins.kotlin.android)
-    alias(deps.plugins.ksp)
     id("configuration")
 }
 
 android {
     namespace = "com.tangem.feature.swap.domain.models"
-}
-
-dependencies {
-    /** Domain */
-    api(projects.domain.models)
-    implementation(projects.domain.tokens.models)
-    implementation(projects.domain.transaction.models)
-
-    /** Core modules */
-    implementation(projects.core.utils)
-    implementation(projects.core.datasource)
-
-    /** Other Libraries **/
-    implementation(tangemDeps.blockchain)
-    implementation(deps.kotlin.serialization)
-    implementation(deps.arrow.core)
-    implementation(deps.moshi.kotlin)
-    implementation(deps.jodatime)
-    ksp(deps.moshi.kotlin.codegen)
-
 }
