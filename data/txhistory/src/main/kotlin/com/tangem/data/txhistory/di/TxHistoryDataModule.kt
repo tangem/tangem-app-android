e44a15de634713ebed5ab42a51068d812951f6a3
Tangem <git@tangem.com>
2026-07-07 17:09:10 +0400

    Updated on 2026-09-21

--- data/txhistory/src/main/kotlin/com/tangem/data/txhistory/di/TxHistoryDataModule.kt
+++ data/txhistory/src/main/kotlin/com/tangem/data/txhistory/di/TxHistoryDataModule.kt
@@ -4,10 +4,12 @@ import com.tangem.data.common.txhistory.ExpressHistoryRepository
 import com.tangem.data.txhistory.fetcher.DefaultAppTxHistoryFetcher
 import com.tangem.data.txhistory.fetcher.DefaultTxHistoryFetcherUtils
 import com.tangem.data.txhistory.fetcher.TxHistoryFetcherUtils
+import com.tangem.data.txhistory.list.DefaultHistoryTxListManager
 import com.tangem.data.txhistory.repository.DefaultExpressHistoryRepository
 import com.tangem.data.txhistory.repository.DefaultTxHistoryRepository
 import com.tangem.data.txhistory.repository.RefactoredTxHistoryRepository
 import com.tangem.domain.txhistory.fetcher.AppTxHistoryFetcher
+import com.tangem.domain.txhistory.list.HistoryTxListManager
 import com.tangem.domain.txhistory.repository.TxHistoryRepository
 import com.tangem.domain.txhistory.repository.TxHistoryRepositoryV2
 import dagger.Binds
@@ -38,4 +40,7 @@ internal interface TxHistoryDataModule {
     @Binds
     @Singleton
     fun provideExpressHistoryRepository(default: DefaultExpressHistoryRepository): ExpressHistoryRepository
+
+    @Binds
+    fun provideHistoryTxListManagerFactory(default: DefaultHistoryTxListManager.Factory): HistoryTxListManager.Factory
 }
