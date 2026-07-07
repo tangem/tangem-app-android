48405cc58e8ad7d8a6d18e3226bda1be6e1ac7a2
Tangem <git@tangem.com>
2026-07-07 16:37:19 +0200

    Updated on 2026-09-21

--- features/promo-banners/impl/src/main/kotlin/com/tangem/features/promobanners/impl/campaigns/component/CampaignAlreadyActivatedBottomSheetComponent.kt
+++ features/promo-banners/impl/src/main/kotlin/com/tangem/features/promobanners/impl/campaigns/component/CampaignAlreadyActivatedBottomSheetComponent.kt
@@ -5,7 +5,11 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.tangem.core.decompose.context.AppComponentContext
+import com.tangem.core.decompose.model.getOrCreateModel
 import com.tangem.core.ui.components.PrimaryButton
 import com.tangem.core.ui.components.bottomsheets.state.BottomSheetState
 import com.tangem.core.ui.ds2.button.Close
@@ -13,24 +17,30 @@ import com.tangem.core.ui.ds2.button.TangemButton
 import com.tangem.core.ui.ds2.topnavigation.TangemTopNavigation
 import com.tangem.core.ui.extensions.stringResourceSafe
 import com.tangem.features.promobanners.impl.R
-import com.tangem.features.promobanners.impl.campaigns.ui.NotActiveCampaignMessageContent
+import com.tangem.features.promobanners.impl.campaigns.model.CampaignAlreadyActivatedModel
+import com.tangem.features.promobanners.impl.campaigns.ui.ActivateCampaignContent
 
-internal class NotActiveCampaignBottomSheetComponent(
-    private val onDismissRequest: () -> Unit,
-) : CampaignsModularComponent {
+internal class CampaignAlreadyActivatedBottomSheetComponent(
+    appComponentContext: AppComponentContext,
+    private val params: CampaignAlreadyActivatedModel.Params,
+) : CampaignsModularComponent, AppComponentContext by appComponentContext {
+
+    private val model: CampaignAlreadyActivatedModel = getOrCreateModel(params)
 
     @Composable
     override fun Title(bottomSheetState: State<BottomSheetState>) {
         TangemTopNavigation(
             windowInsets = WindowInsets(0),
             blurBackground = false,
-            endButton = { TangemButton.Close(onClick = onDismissRequest) },
+            endButton = { TangemButton.Close(onClick = params.onDismiss) },
         )
     }
 
     @Composable
     override fun Content(bottomSheetState: State<BottomSheetState>, contentPadding: PaddingValues, modifier: Modifier) {
-        NotActiveCampaignMessageContent()
+        val state by model.uiState.collectAsStateWithLifecycle()
+
+        ActivateCampaignContent(um = state)
     }
 
     @Composable
@@ -38,7 +48,7 @@ internal class NotActiveCampaignBottomSheetComponent(
         PrimaryButton(
             modifier = Modifier.fillMaxWidth(),
             text = stringResourceSafe(R.string.common_close),
-            onClick = { onDismissRequest() },
+            onClick = params.onDismiss,
         )
     }
 }
--- features/promo-banners/impl/src/main/kotlin/com/tangem/features/promobanners/impl/campaigns/entity/CampaignsBottomSheetConfig.kt
+++ features/promo-banners/impl/src/main/kotlin/com/tangem/features/promobanners/impl/campaigns/entity/CampaignsBottomSheetConfig.kt
@@ -1,5 +1,8 @@
 package com.tangem.features.promobanners.impl.campaigns.entity
 
+import com.tangem.domain.appcurrency.model.AppCurrency
+import com.tangem.domain.models.account.Account
+import com.tangem.domain.models.currency.CryptoCurrencyStatus
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -16,5 +19,13 @@ internal sealed class CampaignsBottomSheetConfig {
     @Serializable
     data class Activate(
         val campaignType: CampaignType,
+    ) : CampaignsBottomSheetConfig()
+
+    @Serializable
+    data class AlreadyActivated(
+        val campaignType: CampaignType,
+        val appCurrency: AppCurrency,
+        val account: Account?,
+        val currency: CryptoCurrencyStatus,
     ) : CampaignsBottomSheetConfig()
 }
