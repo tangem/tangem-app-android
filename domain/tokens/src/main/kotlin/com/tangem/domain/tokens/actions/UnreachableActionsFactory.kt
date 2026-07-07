f185b2b4194312343e74d8ef3987ed9909db083e
Tangem <git@tangem.com>
2026-07-07 15:49:08 +0200

    Updated on 2026-09-14

--- domain/tokens/src/main/kotlin/com/tangem/domain/tokens/actions/UnreachableActionsFactory.kt
+++ domain/tokens/src/main/kotlin/com/tangem/domain/tokens/actions/UnreachableActionsFactory.kt
@@ -35,14 +35,6 @@ internal class UnreachableActionsFactory(
                 null
             }
 
-            val onrampUnavailabilityReasonDeferred = async {
-                getOnrampUnavailabilityReason(
-                    userWallet = userWallet,
-                    currency = cryptoCurrencyStatus.currency,
-                    requirementsDeferred = requirementsDeferred,
-                )
-            }
-
             val hideTokenUnavailabilityReason = getTokenHideUnavailabilityReason(userWallet)
             // endregion
 
@@ -52,7 +44,7 @@ internal class UnreachableActionsFactory(
                 // endregion
 
                 // region Buy
-                addBuyAction(reason = onrampUnavailabilityReasonDeferred.await())
+                addBuyAction(reason = ScenarioUnavailabilityReason.None)
                 // endregion
 
                 // region Receive
