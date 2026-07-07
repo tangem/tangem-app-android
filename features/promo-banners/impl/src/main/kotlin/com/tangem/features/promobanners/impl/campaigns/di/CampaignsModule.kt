face4ecb729d88471e01727c35a2280e9c52125d
Tangem <git@tangem.com>
2026-07-07 13:31:41 +0200

    Updated on 2026-08-19

--- features/promo-banners/impl/src/main/kotlin/com/tangem/features/promobanners/impl/campaigns/di/CampaignsModule.kt
+++ features/promo-banners/impl/src/main/kotlin/com/tangem/features/promobanners/impl/campaigns/di/CampaignsModule.kt
@@ -2,10 +2,14 @@ package com.tangem.features.promobanners.impl.campaigns.di
 
 import com.tangem.core.decompose.di.ModelComponent
 import com.tangem.core.decompose.model.Model
+import com.tangem.features.promobanners.api.deeplink.CampaignsDeepLinkHandler
 import com.tangem.features.promobanners.api.swapcashback.CampaignsComponent
 import com.tangem.features.promobanners.impl.campaigns.component.DefaultCampaignsComponent
+import com.tangem.features.promobanners.impl.campaigns.deeplink.DefaultCampaignsDeepLinkHandler
 import com.tangem.features.promobanners.impl.campaigns.model.ActivateCampaignsModel
 import com.tangem.features.promobanners.impl.campaigns.model.CampaignsModel
+import com.tangem.features.promobanners.impl.campaigns.service.CampaignsService
+import com.tangem.features.promobanners.impl.campaigns.service.DefaultCampaignsService
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -21,6 +25,16 @@ internal interface CampaignsModule {
     @Binds
     @Singleton
     fun bindCampaignsComponentFactory(factory: DefaultCampaignsComponent.Factory): CampaignsComponent.Factory
+
+    @Binds
+    @Singleton
+    fun bindCampaignsDeepLinkHandlerFactory(
+        factory: DefaultCampaignsDeepLinkHandler.Factory,
+    ): CampaignsDeepLinkHandler.Factory
+
+    @Binds
+    @Singleton
+    fun bindCampaignsService(service: DefaultCampaignsService): CampaignsService
 }
 
 @Module
