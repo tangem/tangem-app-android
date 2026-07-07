978cf55f5fbd14fcf5781614a6ec81543469f699
Tangem <git@tangem.com>
2026-07-07 14:54:07 +0400

    Updated on 2026-08-07

--- features/usedesk/impl/build.gradle.kts
+++ features/usedesk/impl/build.gradle.kts
@@ -11,22 +11,19 @@ android {
 }
 
 dependencies {
-
     /** Core */
-    implementation(projects.core.ui)
-    implementation(projects.core.decompose)
-    implementation(projects.core.utils)
-    implementation(projects.core.navigation)
-    implementation(projects.common.routing)
-    implementation(projects.features.usedesk.api)
+    api(projects.core.analytics)
+    api(projects.core.datasource)
+    api(projects.core.decompose)
+    api(projects.core.utils)
+    implementation(projects.core.analytics.models)
 
-    /** Core */
-    implementation(projects.core.datasource)
-    implementation(projects.core.analytics)
+    /** Features api */
+    api(projects.features.usedesk.api)
 
     /** Domain */
-    implementation(projects.domain.feedback)
-    implementation(projects.domain.settings)
+    api(projects.domain.feedback)
+    api(projects.domain.settings)
 
     /** DI */
     implementation(deps.hilt.android)
@@ -36,9 +33,18 @@ dependencies {
     implementation(deps.compose.foundation)
     implementation(deps.compose.ui)
 
+    /** AndroidX */
+    implementation(deps.androidx.appCompat)
+    implementation(deps.androidx.core)
+    implementation(deps.androidx.fragment)
+    implementation(deps.lifecycle.compose)
+    implementation(deps.lifecycle.runtime.ktx)
+
+    /** Other */
+    implementation(deps.decompose)
+    implementation(deps.kotlin.coroutines)
+
     /** Usedesk */
     implementation(tangemDeps.usedesk.chat.sdk)
     implementation(tangemDeps.usedesk.chat.gui)
-
-
 }
