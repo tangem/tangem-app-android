3acd83eff91a762e2f30c24ab92b23c0987504c0
Tangem <git@tangem.com>
2026-07-07 14:54:07 +0400

    Updated on 2026-09-21

--- features/txhistory/api/build.gradle.kts
+++ features/txhistory/api/build.gradle.kts
@@ -9,22 +9,21 @@ android {
 }
 
 dependencies {
+    /** Kotlin */
+    api(deps.kotlin.coroutines)
+    api(deps.kotlin.immutable.collections)
+
     /** Project - Core */
-    implementation(projects.core.ui)
-    implementation(projects.core.decompose)
+    api(projects.core.decompose)
+    api(projects.core.ui)
 
     /** Domain models */
     api(projects.domain.models)
     api(projects.domain.txhistory)
-    implementation(projects.domain.tokens.models)
-    implementation(projects.domain.wallets.models)
 
     /** Compose */
+    api(deps.compose.foundation)
     implementation(deps.compose.runtime)
-    implementation(deps.compose.foundation)
     implementation(deps.compose.material3)
     implementation(deps.compose.ui.tooling)
-
-    /** Other */
-    implementation(deps.kotlin.immutable.collections)
 }
