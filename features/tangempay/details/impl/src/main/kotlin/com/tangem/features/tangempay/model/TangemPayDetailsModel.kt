6146252b10a5a8566bd8eb36c31069aae229048c
Tangem <git@tangem.com>
2026-07-08 13:19:07 +0500

    Updated on 2026-08-19

--- features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/model/TangemPayDetailsModel.kt
+++ features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/model/TangemPayDetailsModel.kt
@@ -367,6 +367,7 @@ internal class TangemPayDetailsModel @Inject constructor(
     override fun onClickBankTransfer() {
         val loaded = currentStatus.value.ifLoadedOrNull { it } ?: return
         val onramp = loaded.virtualAccount ?: return
+        analytics.send(TangemPayAnalyticsEvents.VaTopupButtonClicked())
         bottomSheetNavigation.dismiss()
         bottomSheetNavigation.activate(
             TangemPayDetailsNavigation.VirtualAccountDeposit(
@@ -378,6 +379,7 @@ internal class TangemPayDetailsModel @Inject constructor(
     }
 
     fun onVirtualAccountOrderCreated() {
+        analytics.send(TangemPayAnalyticsEvents.VaSuccessScreenActivation())
         bottomSheetNavigation.dismiss()
         router.push(TangemPayAccountDetailsInnerRoute.VirtualAccountDepositSuccess)
     }
@@ -390,6 +392,18 @@ internal class TangemPayDetailsModel @Inject constructor(
                 bankCredentials = onramp.bankCredentials,
             ),
         )
+    }
+
+    fun onVaBankingDetailsShown() {
+        analytics.send(TangemPayAnalyticsEvents.VaBankingDetailsShowed())
+    }
+
+    fun onVaShareDetailsClicked() {
+        analytics.send(TangemPayAnalyticsEvents.VaShareDetailsButtonClicked())
+    }
+
+    fun onVaFieldCopied(field: String) {
+        analytics.send(TangemPayAnalyticsEvents.VaCopyFieldClicked(field))
     }
 
     override fun onClickReceive(data: TangemPayTopUpData) {
