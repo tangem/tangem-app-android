9b7ebed123cb1c5f2523d7f5737ffc65ba13c190
Tangem <git@tangem.com>
2026-07-08 20:13:28 +0500

    Updated on 2026-08-07

---
+++ core/datasource/src/main/java/com/tangem/datasource/api/promotion/models/PromotionRegistrationResponse.kt
@@ -0,0 +1,19 @@
+package com.tangem.datasource.api.promotion.models
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class PromotionRegistrationResponse(
+    @Json(name = "status") val status: String,
+    @Json(name = "message") val message: String?,
+    @Json(name = "data") val data: RegistrationData,
+) {
+
+    @JsonClass(generateAdapter = true)
+    data class RegistrationData(
+        @Json(name = "campaignId") val campaignId: String,
+        @Json(name = "registeredAt") val registeredAt: String?,
+        @Json(name = "tokenReward") val tokenReward: CreatePromotionRegistrationBody.TokenRewardDto,
+    )
+}
