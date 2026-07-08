9b95744c131c925573d56a66437f3a9e7dabae41
Tangem <git@tangem.com>
2026-07-08 17:27:49 +0400

    Updated on 2026-09-23

--- core/datasource/src/test/kotlin/com/tangem/datasource/local/converter/ExpressHistoryConverterTest.kt
+++ core/datasource/src/test/kotlin/com/tangem/datasource/local/converter/ExpressHistoryConverterTest.kt
@@ -34,8 +34,7 @@ internal class ExpressHistoryConverterTest {
         Truth.assertThat(entity.refundNetwork).isEqualTo(item.refundNetwork)
         Truth.assertThat(entity.refundContractAddress).isEqualTo(item.refundContractAddress)
         Truth.assertThat(entity.createdAt).isEqualTo(item.createdAt)
-        // todo txHistory uncomment
-        // Truth.assertThat(entity.updatedAt).isEqualTo(item.updatedAt)
+        Truth.assertThat(entity.updatedAt).isEqualTo(item.updatedAt)
         Truth.assertThat(entity.payTill).isEqualTo(item.payTill)
         Truth.assertThat(entity.averageDuration).isEqualTo(item.averageDuration)
     }
@@ -233,8 +232,7 @@ internal class ExpressHistoryConverterTest {
         refundNetwork = refundNetwork,
         refundContractAddress = refundContractAddress,
         createdAt = "2026-06-01T00:00:00Z",
-        // todo txHistory uncomment
-        // updatedAt = "2026-06-01T00:05:00Z",
+        updatedAt = "2026-06-01T00:05:00Z",
         payTill = payTill,
         averageDuration = averageDuration,
         fromContractAddress = "0xfromContract",
@@ -266,8 +264,7 @@ internal class ExpressHistoryConverterTest {
         externalTxUrl = externalTxUrl,
         payoutHash = payoutHash,
         createdAt = "2026-06-01T00:00:00Z",
-        // todo txHistory uncomment
-        // updatedAt = "2026-06-01T00:05:00Z",
+        updatedAt = "2026-06-01T00:05:00Z",
         fromCurrencyCode = "USD",
         fromAmount = "100.0",
         fromPrecision = 2,
