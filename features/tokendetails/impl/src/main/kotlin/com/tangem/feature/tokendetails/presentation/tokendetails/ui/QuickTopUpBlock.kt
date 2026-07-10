c7d844edd75dc408961bae785cbbe8745244610f
Tangem <git@tangem.com>
2026-07-10 11:50:42 +0200

    Updated on 2026-08-07

--- features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/ui/QuickTopUpBlock.kt
+++ features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/ui/QuickTopUpBlock.kt
@@ -1,12 +1,7 @@
 package com.tangem.feature.tokendetails.presentation.tokendetails.ui
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -103,7 +98,6 @@ private fun QuickTopUpBlock_Preview() {
                     ),
                 ),
             ),
-            modifier = Modifier.padding(TangemTheme.dimens2.x3),
         )
     }
 }
