02d8bdecd77a05e27b865be1fdc708e7292b8d12
Tangem <git@tangem.com>
2026-07-10 15:07:42 +0300

    Updated on 2026-09-23

---
+++ domain/visa/src/main/kotlin/com/tangem/domain/pay/model/TangemPayCashback.kt
@@ -0,0 +1,18 @@
+package com.tangem.domain.pay.model
+
+import org.joda.time.DateTime
+import java.math.BigDecimal
+
+data class TangemPayCashback(
+    val confirmedAmount: BigDecimal,
+    val currency: String,
+    val period: Period,
+) {
+    data class Period(
+        val year: Int,
+        /** 1-based calendar month (6 = June). */
+        val month: Int,
+        val payoutStart: DateTime,
+        val payoutEnd: DateTime,
+    )
+}
