1d39da9bef203e4c59896e1fb43ccc7ed7538039
Tangem <git@tangem.com>
2026-07-10 09:44:21 +0200

    Updated on 2026-09-01

--- features/promo-banners/impl/src/main/kotlin/com/tangem/features/promobanners/impl/campaigns/service/DefaultCampaignsService.kt
+++ features/promo-banners/impl/src/main/kotlin/com/tangem/features/promobanners/impl/campaigns/service/DefaultCampaignsService.kt
@@ -1,5 +1,6 @@
 package com.tangem.features.promobanners.impl.campaigns.service
 
+import com.tangem.domain.models.wallet.UserWalletId
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.receiveAsFlow
@@ -9,10 +10,10 @@ import javax.inject.Singleton
 @Singleton
 internal class DefaultCampaignsService @Inject constructor() : CampaignsService {
 
-    private val _campaignFlow: Channel<String> = Channel(Channel.BUFFERED)
-    override val campaignFlow: Flow<String> = _campaignFlow.receiveAsFlow()
+    private val _campaignFlow: Channel<CampaignRequest> = Channel(Channel.BUFFERED)
+    override val campaignFlow: Flow<CampaignRequest> = _campaignFlow.receiveAsFlow()
 
-    override fun show(campaignId: String) {
-        _campaignFlow.trySend(campaignId)
+    override fun show(campaignId: String, userWalletId: UserWalletId) {
+        _campaignFlow.trySend(CampaignRequest(campaignId = campaignId, userWalletId = userWalletId))
     }
 }
