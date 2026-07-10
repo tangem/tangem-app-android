4b797ef8da52b43219e62dd42082669def5c6b7d
Tangem <git@tangem.com>
2026-07-10 17:10:31 +0200

    Updated on 2026-09-01

--- features/promo-banners/impl/build.gradle.kts
+++ features/promo-banners/impl/build.gradle.kts
@@ -54,7 +54,6 @@ dependencies {
     kapt(deps.hilt.kapt)
 
     /** Tests */
-    testImplementation(deps.test.junit5)
-    testImplementation(deps.test.truth)
-    testImplementation(deps.kotlin.serialization)
+    testImplementation(projects.test.core)
+    testImplementation(projects.common.test)
 }
