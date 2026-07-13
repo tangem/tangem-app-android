cb568712ddee5b3ba41933724d6ff56738cfc6de
Tangem <git@tangem.com>
2026-07-13 12:53:59 +0400

    Updated on 2026-08-19

--- data/yield-supply/src/test/java/com/tangem/data/yield/supply/promo/DefaultYieldPromoRepositoryTest.kt
+++ data/yield-supply/src/test/java/com/tangem/data/yield/supply/promo/DefaultYieldPromoRepositoryTest.kt
@@ -224,10 +224,12 @@ internal class DefaultYieldPromoRepositoryTest {
             ),
             tokens = listOf(
                 PromotionsResponse.PromotionDto.PromoToken(
+                    tokenId = "usd-coin",
                     tokenAddress = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
                     tokenSymbol = "USDC",
                     tokenName = "USD Coin",
                     networkId = "ethereum",
+                    decimals = 6,
                 ),
             ),
             status = "active",
