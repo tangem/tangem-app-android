b5def946622b85bb8a9f1f636a89caf80b615ebf
Tangem <git@tangem.com>
2026-07-13 02:20:35 -0700

    Updated on 2026-09-23

--- features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/entity/TangemPayCardDetailsBlockStateFactory.kt
+++ features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/entity/TangemPayCardDetailsBlockStateFactory.kt
@@ -13,6 +13,7 @@ internal class TangemPayCardDetailsBlockStateFactory(
     private val displayName: CardDisplayName?,
     private val isEditingNameEnabled: Boolean,
     private val cardState: TangemPayCardState,
+    private val cardImageUrl: String?,
     private val onEditNameClick: () -> Unit,
     private val onReveal: () -> Unit,
     private val onCopy: (String, CardDataType) -> Unit,
@@ -41,6 +42,7 @@ internal class TangemPayCardDetailsBlockStateFactory(
             },
             shouldShowCardDetailsButtonOnCard = shouldShowCardDetailsButtonOnCard,
             cardState = cardState,
+            cardImageUrl = cardImageUrl,
         )
     }
 }
