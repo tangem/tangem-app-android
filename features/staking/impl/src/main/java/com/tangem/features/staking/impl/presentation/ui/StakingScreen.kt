d82474324bb59b8a5db04e687cef34c0792e0c14
Tangem <git@tangem.com>
2026-07-15 11:12:11 +0500

    Updated on 2026-08-14

--- features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/ui/StakingScreen.kt
+++ features/staking/impl/src/main/java/com/tangem/features/staking/impl/presentation/ui/StakingScreen.kt
@@ -21,6 +21,7 @@ import com.tangem.core.ui.components.bottomsheets.TangemBottomSheetConfig
 import com.tangem.core.ui.extensions.resolveReference
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.core.ui.test.SendScreenTestTags
+import com.tangem.features.marketing.api.MarketingBannerComponent
 import com.tangem.features.staking.impl.R
 import com.tangem.features.staking.impl.presentation.state.StakingStates
 import com.tangem.features.staking.impl.presentation.state.StakingStep
@@ -37,7 +38,7 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.withIndex
 
 @Composable
-internal fun StakingScreen(uiState: StakingUiState) {
+internal fun StakingScreen(uiState: StakingUiState, marketingBannerComponent: MarketingBannerComponent) {
     val confirmationState = uiState.confirmationState as? StakingStates.ConfirmationState.Data
 
     BackHandler(onBack = uiState.clickIntents::onPrevClick)
@@ -55,6 +56,7 @@ internal fun StakingScreen(uiState: StakingUiState) {
         )
         StakingScreenContent(
             uiState = uiState,
+            marketingBannerComponent = marketingBannerComponent,
             modifier = Modifier.weight(1f),
         )
         NavigationButtonsBlock(
@@ -107,7 +109,11 @@ private fun StakingAppBar(uiState: StakingUiState) {
 
 @Suppress("LongMethod")
 @Composable
-private fun StakingScreenContent(uiState: StakingUiState, modifier: Modifier = Modifier) {
+private fun StakingScreenContent(
+    uiState: StakingUiState,
+    marketingBannerComponent: MarketingBannerComponent,
+    modifier: Modifier = Modifier,
+) {
     val currentScreen = uiState.currentStep
     var currentStateProxy by remember { mutableStateOf(currentScreen) }
     var isTransitionAnimationRunning by remember { mutableStateOf(false) }
@@ -152,6 +158,7 @@ private fun StakingScreenContent(uiState: StakingUiState, modifier: Modifier = M
                     buttonState = uiState.buttonsState,
                     clickIntents = uiState.clickIntents,
                     isBalanceHidden = uiState.isBalanceHidden,
+                    marketingBannerComponent = marketingBannerComponent,
                 )
                 StakingStep.RewardsValidators -> {
                     StakingClaimRewardsValidatorContent(
