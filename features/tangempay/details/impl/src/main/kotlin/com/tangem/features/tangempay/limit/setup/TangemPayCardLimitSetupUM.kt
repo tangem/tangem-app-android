c351f8aff5e2edda301d09411f79d3b313729bf0
Tangem <git@tangem.com>
2026-07-15 14:33:07 +0200

    Updated on 2026-09-21

--- features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/limit/setup/TangemPayCardLimitSetupUM.kt
+++ features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/limit/setup/TangemPayCardLimitSetupUM.kt
@@ -28,6 +28,7 @@ internal data class TangemPayCardLimitSetupUM(
     @Immutable
     internal data class LimitPresetUM(
         val label: String,
+        val testTag: String,
         val onClick: () -> Unit,
     )
 
@@ -42,10 +43,10 @@ internal data class TangemPayCardLimitSetupUM(
             subtitle: TextReference = TextReference.Str("Set a limit from $0 to $50,000"),
             currencyCode: String = "$",
             presets: ImmutableList<LimitPresetUM> = persistentListOf(
-                LimitPresetUM(label = "$0", onClick = {}),
-                LimitPresetUM(label = "$5,000", onClick = {}),
-                LimitPresetUM(label = "$10,000", onClick = {}),
-                LimitPresetUM(label = "$25,000", onClick = {}),
+                LimitPresetUM(label = "$0", testTag = "", onClick = {}),
+                LimitPresetUM(label = "$5,000", testTag = "", onClick = {}),
+                LimitPresetUM(label = "$10,000", testTag = "", onClick = {}),
+                LimitPresetUM(label = "$25,000", testTag = "", onClick = {}),
             ),
             submitButtonEnabled: Boolean = true,
             submitButtonLoading: Boolean = false,
