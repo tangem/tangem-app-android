9dd9a0deb99f912f0406ec40873f2f524b39c36f
Tangem <git@tangem.com>
2026-07-15 11:02:46 +0200

    Updated on 2026-09-25

--- data/promo/src/main/kotlin/com/tangem/data/promo/di/PromoDataModule.kt
+++ data/promo/src/main/kotlin/com/tangem/data/promo/di/PromoDataModule.kt
@@ -2,11 +2,8 @@ package com.tangem.data.promo.di
 
 import com.squareup.moshi.Moshi
 import com.tangem.data.promo.DefaultPromoRepository
-import com.tangem.data.promo.store.DefaultPromoEnrollmentStore
-import com.tangem.data.promo.store.PromoEnrollmentStore
 import com.tangem.datasource.api.tangemTech.TangemTechApi
 import com.tangem.datasource.di.NetworkMoshi
-import com.tangem.datasource.local.preferences.AppPreferencesStore
 import com.tangem.datasource.local.promotion.PromotionsSupplier
 import com.tangem.domain.promo.PromoRepository
 import com.tangem.utils.coroutines.CoroutineDispatcherProvider
@@ -22,23 +19,15 @@ object PromoDataModule {
 
     @Provides
     @Singleton
-    fun providePromoEnrollmentStore(appPreferencesStore: AppPreferencesStore): PromoEnrollmentStore {
-        return DefaultPromoEnrollmentStore(appPreferencesStore)
-    }
-
-    @Provides
-    @Singleton
     fun providePromoRepository(
         promotionsSupplier: PromotionsSupplier,
         tangemApi: TangemTechApi,
-        enrollmentStore: PromoEnrollmentStore,
         @NetworkMoshi moshi: Moshi,
         dispatchers: CoroutineDispatcherProvider,
     ): PromoRepository {
         return DefaultPromoRepository(
             promotionsSupplier = promotionsSupplier,
             tangemApi = tangemApi,
-            enrollmentStore = enrollmentStore,
             moshi = moshi,
             dispatchers = dispatchers,
         )
