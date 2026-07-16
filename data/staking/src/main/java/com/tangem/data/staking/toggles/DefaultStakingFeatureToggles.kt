9534419f433c1de3e4a8fc5e8a8ad2780941c0ea
Tangem <git@tangem.com>
2026-07-16 13:36:16 +0200

    Updated on 2026-09-25

--- data/staking/src/main/java/com/tangem/data/staking/toggles/DefaultStakingFeatureToggles.kt
+++ data/staking/src/main/java/com/tangem/data/staking/toggles/DefaultStakingFeatureToggles.kt
@@ -20,6 +20,12 @@ internal class DefaultStakingFeatureToggles(
         )
     }
 
+    override fun isRegionUnavailableHandlingEnabled(): Boolean {
+        return featureTogglesManager.isFeatureEnabled(
+            FeatureToggles.AND_15231_STAKING_REGION_UNAVAILABLE_ENABLED,
+        )
+    }
+
     private fun StakingIntegrationID.getFeatureToggle(): FeatureToggles? = when (this) {
         is StakingIntegrationID.P2PEthPool -> null
         is StakingIntegrationID.StakeKit -> this.getStakeKitFeatureToggle()
