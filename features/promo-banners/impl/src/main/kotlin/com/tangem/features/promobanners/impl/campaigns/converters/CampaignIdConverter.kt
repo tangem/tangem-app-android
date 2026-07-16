c26fe5a3f93f3f5f1ceff86759659b096a2b90e8
Tangem <git@tangem.com>
2026-07-16 09:36:16 +0200

    Updated on 2026-08-27

--- features/promo-banners/impl/src/main/kotlin/com/tangem/features/promobanners/impl/campaigns/converters/CampaignIdConverter.kt
+++ features/promo-banners/impl/src/main/kotlin/com/tangem/features/promobanners/impl/campaigns/converters/CampaignIdConverter.kt
@@ -1,5 +1,6 @@
 package com.tangem.features.promobanners.impl.campaigns.converters
 
+import com.tangem.domain.promo.models.PromoCampaignId
 import com.tangem.features.promobanners.impl.campaigns.entity.CampaignType
 import com.tangem.utils.converter.Converter
 import javax.inject.Inject
@@ -9,14 +10,9 @@ internal class CampaignIdConverter @Inject constructor() :
 
     override fun convert(value: String): CampaignType? {
         return when (value) {
-            CAMPAIGN_ID_REACTIVATION -> CampaignType.ReactivationCashback(campaignId = value)
-            CAMPAIGN_ID_WHALE -> CampaignType.WhaleSwapCashback(campaignId = value)
+            PromoCampaignId.ReactivationCashback.slug -> CampaignType.ReactivationCashback(campaignId = value)
+            PromoCampaignId.WhaleSwapCashback.slug -> CampaignType.WhaleSwapCashback(campaignId = value)
             else -> null
         }
-    }
-
-    private companion object {
-        const val CAMPAIGN_ID_WHALE = "1"
-        const val CAMPAIGN_ID_REACTIVATION = "2"
     }
 }
