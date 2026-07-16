d1061db69189846c3270a8dab360f2df5566a68c
Tangem <git@tangem.com>
2026-07-16 22:46:06 +0400

    Updated on 2026-09-21

--- features/txhistory/impl/src/main/kotlin/com/tangem/features/txhistory/utils/TxHistoryListState.kt
+++ features/txhistory/impl/src/main/kotlin/com/tangem/features/txhistory/utils/TxHistoryListState.kt
@@ -3,7 +3,6 @@ package com.tangem.features.txhistory.utils
 import com.tangem.domain.models.network.TxInfo
 import com.tangem.domain.txhistory.models.PaginationWrapper
 import com.tangem.features.txhistory.entity.TxHistoryItemsUM
-import com.tangem.features.txhistory.entity.TxHistoryUM
 import com.tangem.pagination.Batch
 import com.tangem.pagination.PaginationStatus
 import com.tangem.utils.annotations.RemoveWithToggle
@@ -14,5 +13,4 @@ internal data class TxHistoryListState(
     val status: PaginationStatus<*> = PaginationStatus.None,
     val rawBatches: List<Batch<Int, PaginationWrapper<TxInfo>>> = emptyList(),
     val uiBatches: List<Batch<Int, List<TxHistoryItemsUM.TxHistoryItemUM>>> = emptyList(),
-    val legacyUiBatches: List<Batch<Int, List<TxHistoryUM.TxHistoryItemUM>>> = emptyList(),
 )
