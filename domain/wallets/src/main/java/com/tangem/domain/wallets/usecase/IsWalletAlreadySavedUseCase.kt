86c25cc814b32a1a5d75031b814d2ccd32615da9
Tangem <git@tangem.com>
2026-07-18 00:29:34 +0300

    Updated on 2026-09-25

--- domain/wallets/src/main/java/com/tangem/domain/wallets/usecase/IsWalletAlreadySavedUseCase.kt
+++ domain/wallets/src/main/java/com/tangem/domain/wallets/usecase/IsWalletAlreadySavedUseCase.kt
@@ -2,19 +2,36 @@ package com.tangem.domain.wallets.usecase
 
 import arrow.core.Either
 import arrow.core.raise.either
+import com.tangem.common.card.EllipticCurve
+import com.tangem.crypto.bip39.Mnemonic
+import com.tangem.crypto.hdWallet.masterkey.AnyMasterKeyFactory
 import com.tangem.domain.common.wallets.UserWalletsListRepository
 import com.tangem.domain.common.wallets.error.SaveWalletError
-import com.tangem.domain.models.wallet.UserWallet
+import com.tangem.domain.wallets.builder.UserWalletIdBuilder
 
 class IsWalletAlreadySavedUseCase(
     private val userWalletsListRepository: UserWalletsListRepository,
 ) {
 
     suspend operator fun invoke(
-        userWallet: UserWallet,
-        canOverride: Boolean = false,
+        mnemonic: Mnemonic,
+        passphrase: String?,
     ): Either<SaveWalletError, Boolean> = either {
+        val userWalletId = UserWalletIdBuilder.walletPublicKey(
+            publicKey = deriveMasterPublicKey(mnemonic = mnemonic, passphrase = passphrase),
+        )
+
         userWalletsListRepository.userWalletsSync()
-            .any { it.walletId == userWallet.walletId }
+            .any { it.walletId == userWalletId }
+    }
+
+    private fun deriveMasterPublicKey(mnemonic: Mnemonic, passphrase: String?): ByteArray {
+        val curve = EllipticCurve.Secp256k1
+        val extendedPrivateKey = AnyMasterKeyFactory(
+            mnemonic = mnemonic,
+            passphrase = passphrase.orEmpty(),
+        ).makeMasterKey(curve)
+
+        return extendedPrivateKey.makePublicKey(curve).publicKey
     }
 }
