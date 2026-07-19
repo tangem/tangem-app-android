9921f85f33489a8d4c6353420b1b1f3bbfdbfca4
Tangem <git@tangem.com>
2026-07-19 20:12:53 +0300

    Updated on 2026-09-23

--- app/src/main/java/com/tangem/tap/domain/tasks/product/CreateWalletsTask.kt
+++ app/src/main/java/com/tangem/tap/domain/tasks/product/CreateWalletsTask.kt
@@ -1,6 +1,5 @@
 package com.tangem.tap.domain.tasks.product
 
-import android.util.Log
 import com.tangem.common.CompletionResult
 import com.tangem.common.card.EllipticCurve
 import com.tangem.common.core.CardSession
@@ -54,7 +53,6 @@ class CreateWalletsTask(
                         return@run
                     }
                     createWallet(curves[createdWalletsResponses.size], session, callback)
-                    Log.e("wallet3", "wallet created: ${curve.curve}")
                 }
 
                 is CompletionResult.Failure -> callback(CompletionResult.Failure(result.error))
