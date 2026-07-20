4f60fa2f5f0f34eb517844381aa6daa7027b58cd
Tangem <git@tangem.com>
2026-07-20 20:09:20 +0500

    Updated on 2026-09-21

--- core/ui/src/main/java/com/tangem/core/ui/ds/row/token/internal/TokenRowTitle.kt
+++ core/ui/src/main/java/com/tangem/core/ui/ds/row/token/internal/TokenRowTitle.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
@@ -23,6 +22,7 @@ import com.tangem.core.ui.components.TextShimmer
 import com.tangem.core.ui.ds.badge.TangemBadge
 import com.tangem.core.ui.ds.placeholder.TextPlaceholder
 import com.tangem.core.ui.ds.row.token.TangemTokenRowUM
+import com.tangem.core.ui.extensions.rememberLastNonNull
 import com.tangem.core.ui.extensions.resolveAnnotatedReference
 import com.tangem.core.ui.res.TangemTheme
 import com.tangem.core.ui.res.TangemThemePreviewRedesign
@@ -80,11 +80,13 @@ private fun ContentTitle(titleUM: TangemTokenRowUM.TitleUM.Content, modifier: Mo
             )
         }
 
+        val badge = rememberLastNonNull(titleUM.badge)
         AnimatedVisibility(
             visible = titleUM.badge != null,
         ) {
-            val wrappedBadge = remember(this) { requireNotNull(titleUM.badge) }
-            TangemBadge(wrappedBadge, modifier = Modifier.testTag(TokenElementsTestTags.TOKEN_EARN_APY_BADGE))
+            if (badge != null) {
+                TangemBadge(badge, modifier = Modifier.testTag(TokenElementsTestTags.TOKEN_EARN_APY_BADGE))
+            }
         }
     }
 }
