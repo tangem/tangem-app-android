b78512a2246b380a9eb83cfdbcdc76334eae8b2c
Tangem <git@tangem.com>
2026-07-20 15:14:29 +0200

    Updated on 2026-09-25

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/components/common/WalletContent.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/wallet/ui/components/common/WalletContent.kt
@@ -46,8 +46,6 @@ internal fun WalletListContent(
     promoBannersBlockComponent: PromoBannersBlockComponent? = null,
     walletId: String? = null,
 ) {
-    val containerColor = TangemTheme.colors2.surface.level1
-
     val movableItemModifier = Modifier.padding(horizontal = TangemTheme.dimens2.x3)
     val itemModifier = movableItemModifier.padding(top = TangemTheme.dimens2.x3)
 
@@ -60,11 +58,9 @@ internal fun WalletListContent(
     ) {
         notifications(
             notifications = currentWallet.notifications.map { it.messageUM }.toPersistentList(),
-            contentColor = containerColor,
             modifier = movableItemModifier,
         )
         notificationsCarousel(
-            containerColor = containerColor,
             modifier = movableItemModifier,
             notifications = currentWallet.notificationsCarousel.map { it.messageUM }.toPersistentList(),
         )
