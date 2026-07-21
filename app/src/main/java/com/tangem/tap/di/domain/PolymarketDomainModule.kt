d1981b62b587e657aef8181934783d315b67aa2c
Tangem <git@tangem.com>
2026-07-21 14:29:10 +0300

    Updated on 2026-09-21

--- app/src/main/java/com/tangem/tap/di/domain/PolymarketDomainModule.kt
+++ app/src/main/java/com/tangem/tap/di/domain/PolymarketDomainModule.kt
@@ -1,6 +1,5 @@
 package com.tangem.tap.di.domain
 
-import com.tangem.domain.polymarket.PolymarketRepository
 import com.tangem.domain.polymarket.usecase.GetPolymarketEventsUseCase
 import dagger.Module
 import dagger.Provides
@@ -14,7 +13,5 @@ internal object PolymarketDomainModule {
 
     @Provides
     @Singleton
-    fun provideGetPolymarketEventsUseCase(polymarketRepository: PolymarketRepository): GetPolymarketEventsUseCase {
-        return GetPolymarketEventsUseCase(polymarketRepository = polymarketRepository)
-    }
+    fun provideGetPolymarketEventsUseCase(): GetPolymarketEventsUseCase = GetPolymarketEventsUseCase()
 }
