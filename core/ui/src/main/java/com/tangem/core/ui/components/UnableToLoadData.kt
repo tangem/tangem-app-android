e3fcbca9e77f2dc3b4968a8650e66144afd84c3e
Tangem <git@tangem.com>
2026-07-21 16:15:31 +0200

    Updated on 2026-09-23

--- core/ui/src/main/java/com/tangem/core/ui/components/UnableToLoadData.kt
+++ core/ui/src/main/java/com/tangem/core/ui/components/UnableToLoadData.kt
@@ -10,7 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.tangem.core.ui.R
-import com.tangem.core.ui.ds.button.*
+import com.tangem.core.ui.ds2.button.TangemButton
 import com.tangem.core.ui.extensions.resourceReference
 import com.tangem.core.ui.extensions.stringResourceSafe
 import com.tangem.core.ui.res.TangemTheme
@@ -18,11 +18,6 @@ import com.tangem.core.ui.res.TangemThemePreviewRedesign
 
 @Composable
 fun UnableToLoadData(onRetryClick: () -> Unit, modifier: Modifier = Modifier) {
-    UnableToLoadDataV2(onRetryClick, modifier)
-}
-
-@Composable
-private fun UnableToLoadDataV2(onRetryClick: () -> Unit, modifier: Modifier = Modifier) {
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(8.dp),
@@ -30,25 +25,22 @@ private fun UnableToLoadDataV2(onRetryClick: () -> Unit, modifier: Modifier = Mo
     ) {
         Text(
             text = stringResourceSafe(R.string.markets_loading_error_title),
-            style = TangemTheme.typography2.bodyRegular14,
-            color = TangemTheme.colors2.text.neutral.secondary,
+            style = TangemTheme.typography3.subheading.medium,
+            color = TangemTheme.colors3.text.secondary,
         )
         TangemButton(
-            buttonUM = TangemButtonUM(
-                text = resourceReference(R.string.try_to_load_data_again_button_title),
-                onClick = onRetryClick,
-                type = TangemButtonType.Secondary,
-                size = TangemButtonSize.X8,
-                shape = TangemButtonShape.Rounded,
-            ),
+            text = resourceReference(R.string.try_to_load_data_again_button_title),
+            onClick = onRetryClick,
+            variant = TangemButton.Variant.Secondary,
+            size = TangemButton.Size.X8,
         )
     }
 }
 
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, showBackground = true)
 @Composable
-private fun PreviewV2() {
+private fun Preview() {
     TangemThemePreviewRedesign {
-        UnableToLoadDataV2(onRetryClick = {})
+        UnableToLoadData(onRetryClick = {})
     }
 }
