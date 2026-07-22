b18596dd52e1c56fab3bf6549816dd18437ee270
Tangem <git@tangem.com>
2026-07-22 16:13:06 +0400

    Updated on 2026-08-19

--- domain/card/src/main/kotlin/com/tangem/domain/card/common/extensions/CardSdk.kt
+++ domain/card/src/main/kotlin/com/tangem/domain/card/common/extensions/CardSdk.kt
@@ -68,6 +68,7 @@ private fun CardDTO.isBlockchainUnsupported(blockchain: Blockchain): Boolean {
         Blockchain.Quai, Blockchain.QuaiTestnet,
         Blockchain.Adi, Blockchain.AdiTestnet,
         Blockchain.SeiEvm, Blockchain.SeiEvmTestnet,
+        Blockchain.Robinhood, Blockchain.RobinhoodTestnet,
         -> {
             firmwareVersion <= FirmwareVersion.HDWalletAvailable
         }
