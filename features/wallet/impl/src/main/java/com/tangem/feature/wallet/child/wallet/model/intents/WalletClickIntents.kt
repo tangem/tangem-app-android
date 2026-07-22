869031eed14f43788a5d7a59c256626fd5535ef8
Tangem <git@tangem.com>
2026-07-22 12:33:36 +0400

    Updated on 2026-08-27

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/child/wallet/model/intents/WalletClickIntents.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/child/wallet/model/intents/WalletClickIntents.kt
@@ -91,6 +91,7 @@ internal class WalletClickIntents @Inject constructor(
     }
 
     fun onRefreshSwipe(showRefreshState: Boolean) {
+        if (!stateController.isInitialized) return
         when (stateController.getSelectedWalletUM()) {
             is WalletUM.Content -> refreshMultiCurrencyContent(showRefreshState)
             is WalletUM.Locked -> Unit
