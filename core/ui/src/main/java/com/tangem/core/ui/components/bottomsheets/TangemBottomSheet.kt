de2ea080ea00a18a89a8331600c99e1b0e6ab875
Tangem <git@tangem.com>
2026-07-23 15:19:53 +0200

    Updated on 2026-09-14

--- core/ui/src/main/java/com/tangem/core/ui/components/bottomsheets/TangemBottomSheet.kt
+++ core/ui/src/main/java/com/tangem/core/ui/components/bottomsheets/TangemBottomSheet.kt
@@ -33,6 +33,7 @@ import com.tangem.core.ui.components.bottomsheets.internal.collapse
 import com.tangem.core.ui.components.bottomsheets.modal.MODAL_SHEET_MAX_HEIGHT
 import com.tangem.core.ui.components.bottomsheets.modal.TangemModalBottomSheetTitle
 import com.tangem.core.ui.components.bottomsheets.sheet.TangemBottomSheetDraggableHeader
+import com.tangem.core.ui.components.haze.hazeSourceTangem
 import com.tangem.core.ui.components.sheetscaffold.TangemSheetState
 import com.tangem.core.ui.components.sheetscaffold.TangemSheetValue
 import com.tangem.core.ui.components.sheetscaffold.rememberSheetState
@@ -258,6 +259,12 @@ inline fun <reified T : TangemBottomSheetConfigContent> BasicBottomSheet(
                     .testTag(BaseBottomSheetTestTags.CONTAINER),
             ) {
                 Box(modifier = Modifier.fillMaxWidth()) {
+                    Box(
+                        modifier = Modifier
+                            .matchParentSize()
+                            .hazeSourceTangem()
+                            .background(containerColor),
+                    )
                     title(model)
                 }
                 Box(modifier = Modifier.fillMaxWidth()) {
