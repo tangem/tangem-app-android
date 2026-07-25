499c693197d1cd9180dbcf493234f067b9328e49
Tangem <git@tangem.com>
2026-07-25 10:46:18 +0400

    Updated on 2026-09-25

---
+++ data/polymarket/src/main/java/com/tangem/data/polymarket/derivation/PolymarketAddressFactory.kt
@@ -0,0 +1,20 @@
+package com.tangem.data.polymarket.derivation
+
+import com.tangem.blockchain.common.Blockchain
+import com.tangem.crypto.hdWallet.bip32.ExtendedPublicKey
+import javax.inject.Inject
+
+/**
+ * Converts a secp256k1 [ExtendedPublicKey] into a Polygon ERC-55 checksummed address.
+ * Standalone (no Pay/Visa dependency) per the Polymarket epic ground rule.
+ */
+class PolymarketAddressFactory @Inject constructor() {
+
+    fun createAddress(extendedPublicKey: ExtendedPublicKey): String {
+        return Blockchain.Polygon.makeAddressesFromExtendedPublicKey(
+            extendedPublicKey = extendedPublicKey,
+            rawPath = null,
+            cachedIndex = null,
+        ).address
+    }
+}
