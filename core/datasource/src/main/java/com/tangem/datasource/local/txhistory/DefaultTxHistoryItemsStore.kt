80a30619f9ca074ff3901e9689f5ceb3937029b4
Tangem <git@tangem.com>
2026-07-27 16:50:04 +0400

    Updated on 2026-09-25

--- core/datasource/src/main/java/com/tangem/datasource/local/txhistory/DefaultTxHistoryItemsStore.kt
+++ core/datasource/src/main/java/com/tangem/datasource/local/txhistory/DefaultTxHistoryItemsStore.kt
@@ -1,31 +1,26 @@
 package com.tangem.datasource.local.txhistory
 
-import com.tangem.datasource.local.datastore.core.StringKeyDataStore
-import com.tangem.datasource.local.datastore.core.StringKeyDataStoreDecorator
+import com.tangem.core.local.datastore.RuntimeSharedMapStore
 import com.tangem.domain.models.network.TxInfo
 import com.tangem.domain.txhistory.models.Page
 import com.tangem.domain.txhistory.models.PaginationWrapper
 import com.tangem.utils.extensions.addOrReplace
 
 internal class DefaultTxHistoryItemsStore(
-    dataStore: StringKeyDataStore<Set<PaginationWrapper<TxInfo>>>,
-) : TxHistoryItemsStore,
-    StringKeyDataStoreDecorator<TxHistoryItemsStore.Key, Set<PaginationWrapper<TxInfo>>>(dataStore) {
-
-    override fun provideStringKey(key: TxHistoryItemsStore.Key): String = key.toString()
+    private val store: RuntimeSharedMapStore<TxHistoryItemsStore.Key, Set<PaginationWrapper<TxInfo>>>,
+) : TxHistoryItemsStore {
 
     override suspend fun getSyncOrNull(key: TxHistoryItemsStore.Key, page: Page): PaginationWrapper<TxInfo>? {
-        val storedValue = getSyncOrNull(key)
+        val storedValue = store.getSyncOrNull(key)
 
         return storedValue?.firstOrNull { it.currentPage == page }
     }
 
-    override suspend fun store(key: TxHistoryItemsStore.Key, value: PaginationWrapper<TxInfo>) {
-        val oldValue = getSyncOrNull(key).orEmpty()
-        val newValue = oldValue.addOrReplace(value) {
-            it.currentPage == value.currentPage
-        }
+    override suspend fun remove(key: TxHistoryItemsStore.Key) = store.remove(key)
 
-        store(key, newValue)
+    override suspend fun store(key: TxHistoryItemsStore.Key, value: PaginationWrapper<TxInfo>) {
+        store.update(key = key, default = emptySet()) { stored ->
+            stored.addOrReplace(value) { it.currentPage == value.currentPage }
+        }
     }
 }
