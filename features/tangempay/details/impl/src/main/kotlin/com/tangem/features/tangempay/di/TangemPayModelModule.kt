5e2bbedc24124cedfdb5573270febaa5a9fcc1bb
Tangem <git@tangem.com>
2026-07-28 19:05:02 +0500

    Updated on 2026-09-21

--- features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/di/TangemPayModelModule.kt
+++ features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/di/TangemPayModelModule.kt
@@ -6,6 +6,9 @@ import com.tangem.features.tangempay.cashback.impl.model.TangemPayCashbackModel
 import com.tangem.features.tangempay.closure.TangemPayCloseCardModel
 import com.tangem.features.tangempay.limit.setup.TangemPayCardLimitSetupModel
 import com.tangem.features.tangempay.model.*
+import com.tangem.features.tangempay.multichain.choosenetwork.PaymentChooseNetworkModel
+import com.tangem.features.tangempay.multichain.othernetworks.PaymentOtherNetworksModel
+import com.tangem.features.tangempay.multichain.receive.PaymentReceiveModel
 import com.tangem.features.tangempay.tiers.current.TangemPayCurrentPlanModel
 import com.tangem.features.tangempay.tiers.select.TangemPaySelectPlanModel
 import dagger.Binds
@@ -16,6 +19,7 @@ import dagger.multibindings.IntoMap
 
 @Module
 @InstallIn(ModelComponent::class)
+@Suppress("TooManyFunctions")
 internal interface TangemPayModelModule {
 
     @Binds
@@ -107,4 +111,19 @@ internal interface TangemPayModelModule {
     @IntoMap
     @ClassKey(TangemPayCashbackModel::class)
     fun bindTangemPayCashbackModel(model: TangemPayCashbackModel): Model
+
+    @Binds
+    @IntoMap
+    @ClassKey(PaymentChooseNetworkModel::class)
+    fun bindPaymentChooseNetworkModel(model: PaymentChooseNetworkModel): Model
+
+    @Binds
+    @IntoMap
+    @ClassKey(PaymentOtherNetworksModel::class)
+    fun bindPaymentOtherNetworksModel(model: PaymentOtherNetworksModel): Model
+
+    @Binds
+    @IntoMap
+    @ClassKey(PaymentReceiveModel::class)
+    fun bindPaymentReceiveModel(model: PaymentReceiveModel): Model
 }
