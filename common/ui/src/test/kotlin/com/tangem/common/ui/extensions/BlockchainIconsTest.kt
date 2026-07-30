04a491dfac30f39d47eb631cdfac39cf05621c10
Tangem <git@tangem.com>
2026-07-30 11:15:36 +0400

    Updated on 2026-09-21

--- common/ui/src/test/kotlin/com/tangem/common/ui/extensions/BlockchainIconsTest.kt
+++ common/ui/src/test/kotlin/com/tangem/common/ui/extensions/BlockchainIconsTest.kt
@@ -108,6 +108,7 @@ internal class BlockchainIconsTest {
                 Blockchain.Radiant -> R.drawable.img_radiant_22
                 Blockchain.Ravencoin, Blockchain.RavencoinTestnet -> R.drawable.img_ravencoin_22
                 Blockchain.Robinhood, Blockchain.RobinhoodTestnet -> R.drawable.img_robinhood_22
+                Blockchain.Igra, Blockchain.IgraTestnet -> R.drawable.img_igra_22
                 Blockchain.Scroll, Blockchain.ScrollTestnet -> R.drawable.img_scroll_22
                 Blockchain.Sei, Blockchain.SeiTestnet,
                 Blockchain.SeiEvm, Blockchain.SeiEvmTestnet,
@@ -233,6 +234,7 @@ internal class BlockchainIconsTest {
                 Blockchain.Radiant -> R.drawable.ic_radiant_22
                 Blockchain.Ravencoin, Blockchain.RavencoinTestnet -> R.drawable.ic_ravencoin_22
                 Blockchain.Robinhood, Blockchain.RobinhoodTestnet -> R.drawable.ic_robinhood_22
+                Blockchain.Igra, Blockchain.IgraTestnet -> R.drawable.ic_igra_22
                 Blockchain.Scroll, Blockchain.ScrollTestnet -> R.drawable.ic_scroll_22
                 Blockchain.Sei, Blockchain.SeiTestnet,
                 Blockchain.SeiEvm, Blockchain.SeiEvmTestnet,
