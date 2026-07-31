d72a9421e6f235d7059a8e24cf8c23d07191ae00
Tangem <git@tangem.com>
2026-07-31 15:10:01 +0400

    Updated on 2026-09-14

---
+++ domain/cloud-backup/src/main/kotlin/com/tangem/domain/cloudbackup/password/PasswordStrengthHint.kt
@@ -0,0 +1,12 @@
+package com.tangem.domain.cloudbackup.password
+
+enum class PasswordStrengthHint {
+    USE_ALL_CRITERIA,
+    KEEP_GOING,
+    ADD_SYMBOL,
+    ADD_NUMBER,
+    ADD_UPPERCASE,
+    ADD_LOWERCASE,
+    ALMOST_LONG,
+    STRONG,
+}
