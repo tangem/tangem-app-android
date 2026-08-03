037ba657a2dbb83630b03b406e88d921b3b9bb7c
Tangem <git@tangem.com>
2026-08-03 20:05:45 +0300

    Updated on 2026-09-23

--- features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/ui/TokenDetailsTopBar.kt
+++ features/tokendetails/impl/src/main/kotlin/com/tangem/feature/tokendetails/presentation/tokendetails/ui/TokenDetailsTopBar.kt
@@ -178,7 +178,7 @@ private fun TokenDetailsTitle(titleState: TitleState) {
                     AccountIcon(
                         name = titleState.accountName,
                         icon = titleState.accountIconUM,
-                        size = AccountIconSize.RedesignExtraSmall,
+                        size = AccountIconSize.ExtraSmall,
                     )
                 },
             )
