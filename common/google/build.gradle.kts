44cf833e6ab5abf70d736bb3783188ac38a30f05
Tangem <git@tangem.com>
2026-08-03 13:22:36 +0400

    Updated on 2026-09-21

--- common/google/build.gradle.kts
+++ common/google/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(deps.plugins.android.library)
     alias(deps.plugins.kotlin.android)
+    alias(deps.plugins.kotlin.kapt)
+    alias(deps.plugins.hilt.android)
     id("configuration")
 }
 
@@ -9,7 +11,15 @@ android {
 }
 
 dependencies {
+    implementation(projects.core.utils)
+
+    implementation(deps.hilt.android)
+    kapt(deps.hilt.kapt)
+
+    implementation(deps.arrow.core)
+    implementation(deps.kotlin.coroutines)
+    implementation(deps.androidx.activity)
 
     implementation(deps.googlePlay.services.wallet)
-
+    implementation(deps.googlePlay.services.auth)
 }
