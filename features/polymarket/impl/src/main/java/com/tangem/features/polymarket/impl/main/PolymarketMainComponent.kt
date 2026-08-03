0787bcb4035677afc75b38bf1150e0180f5cc9c4
Tangem <git@tangem.com>
2026-08-03 12:42:41 +0400

    Updated on 2026-09-21

--- features/polymarket/impl/src/main/java/com/tangem/features/polymarket/impl/main/PolymarketMainComponent.kt
+++ features/polymarket/impl/src/main/java/com/tangem/features/polymarket/impl/main/PolymarketMainComponent.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.tangem.core.decompose.context.AppComponentContext
 import com.tangem.core.decompose.model.getOrCreateModel
 import com.tangem.core.ui.decompose.ComposableContentComponent
+import com.tangem.domain.polymarket.model.PolymarketAccessMode
 import com.tangem.features.polymarket.impl.main.model.PolymarketMainModel
 import com.tangem.features.polymarket.impl.main.ui.PolymarketMainScreen
 
@@ -18,14 +19,15 @@ import com.tangem.features.polymarket.impl.main.ui.PolymarketMainScreen
  */
 internal class PolymarketMainComponent(
     appComponentContext: AppComponentContext,
+    private val accessMode: PolymarketAccessMode,
 ) : ComposableContentComponent, AppComponentContext by appComponentContext {
 
-    private val model: PolymarketMainModel = getOrCreateModel()
+    private val model: PolymarketMainModel = getOrCreateModel(params = accessMode)
 
     @Composable
     override fun Content(modifier: Modifier) {
         val state by model.uiState.collectAsStateWithLifecycle()
 
-        PolymarketMainScreen(state = state, modifier = modifier)
+        PolymarketMainScreen(state = state, accessMode = accessMode, modifier = modifier)
     }
 }
