d83061c58a8c4dc068f6a3fe7df0c3681be2b2d1
Tangem <git@tangem.com>
2026-08-05 16:10:17 +0400

    Updated on 2026-09-21

--- data/visa/src/main/kotlin/com/tangem/data/pay/TangemPayUserWalletDataCleaner.kt
+++ data/visa/src/main/kotlin/com/tangem/data/pay/TangemPayUserWalletDataCleaner.kt
@@ -1,6 +1,9 @@
 package com.tangem.data.pay
 
+import com.tangem.data.pay.repository.TangemPayRequestPerformer
 import com.tangem.data.pay.store.PaymentAccountStatusesStore
+import com.tangem.data.pay.store.TangemPayCustomerInfoStore
+import com.tangem.data.pay.store.TangemPayStorage
 import com.tangem.datasource.local.visa.TangemPayTxHistoryItemsStore
 import com.tangem.domain.common.wallets.UserWalletDataCleaner
 import com.tangem.domain.models.wallet.UserWalletId
@@ -9,10 +12,25 @@ import javax.inject.Inject
 internal class TangemPayUserWalletDataCleaner @Inject constructor(
     private val paymentAccountStatusesStore: PaymentAccountStatusesStore,
     private val txHistoryItemsStore: TangemPayTxHistoryItemsStore,
+    private val tangemPayStorage: TangemPayStorage,
+    private val customerInfoStore: TangemPayCustomerInfoStore,
+    private val requestPerformer: TangemPayRequestPerformer,
 ) : UserWalletDataCleaner {
 
     override suspend fun clear(userWalletIds: List<UserWalletId>) {
+        userWalletIds.forEach { clearStoredData(it) }
+
         paymentAccountStatusesStore.remove(userWalletIds)
         txHistoryItemsStore.remove(userWalletIds.map { it.stringValue })
+        customerInfoStore.update { cache -> cache - userWalletIds }
+        requestPerformer.removeCachedCustomerWalletAddresses(userWalletIds)
+    }
+
+    private suspend fun clearStoredData(userWalletId: UserWalletId) {
+        // The auth tokens are keyed by the customer wallet address, so read it before clearAll wipes it.
+        val customerWalletAddress = tangemPayStorage.getCustomerWalletAddress(userWalletId)
+
+        tangemPayStorage.clearAll(userWalletId = userWalletId, customerWalletAddress = customerWalletAddress)
+        tangemPayStorage.clearIsTangemPayDeactivated(userWalletId)
     }
 }
