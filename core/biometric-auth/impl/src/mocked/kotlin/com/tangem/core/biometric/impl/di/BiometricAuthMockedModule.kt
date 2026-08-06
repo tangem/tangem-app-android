d2b46b90d03aa7a00bb93f4e55640f421f3740cf
Tangem <git@tangem.com>
2026-08-06 14:56:28 +0200

    Updated on 2026-09-01

--- core/biometric-auth/impl/src/mocked/kotlin/com/tangem/core/biometric/impl/di/BiometricAuthMockedModule.kt
+++ core/biometric-auth/impl/src/mocked/kotlin/com/tangem/core/biometric/impl/di/BiometricAuthMockedModule.kt
@@ -1,7 +1,7 @@
 package com.tangem.core.biometric.impl.di
 
 import com.tangem.core.biometric.BiometricAuthManager
-import com.tangem.core.biometric.impl.DefaultBiometricAuthManager
+import com.tangem.core.biometric.impl.MockedBiometricAuthManager
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -10,9 +10,9 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-internal interface BiometricAuthModule {
+internal interface BiometricAuthMockedModule {
 
     @Binds
     @Singleton
-    fun bindBiometricAuthManager(impl: DefaultBiometricAuthManager): BiometricAuthManager
+    fun bindBiometricAuthManager(impl: MockedBiometricAuthManager): BiometricAuthManager
 }
