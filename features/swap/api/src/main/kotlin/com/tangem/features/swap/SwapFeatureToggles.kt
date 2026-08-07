d5c037accb17423ccbc0d2847b92537002adb8b3
Tangem <git@tangem.com>
2026-08-07 14:45:33 +0200

    Updated on 2026-09-23

--- features/swap/api/src/main/kotlin/com/tangem/features/swap/SwapFeatureToggles.kt
+++ features/swap/api/src/main/kotlin/com/tangem/features/swap/SwapFeatureToggles.kt
@@ -1,6 +1,7 @@
 package com.tangem.features.swap
 
 interface SwapFeatureToggles {
+    val isYieldDexTransferEnabled: Boolean
     val isTronDexSwapEnabled: Boolean
     val isChooseTokenPulseEnabled: Boolean
 }
