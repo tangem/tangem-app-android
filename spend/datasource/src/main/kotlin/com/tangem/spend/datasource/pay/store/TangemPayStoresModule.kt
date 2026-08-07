faeb0902a22a4b6b63572ff3addeaa639b0bf7eb
Tangem <git@tangem.com>
2026-08-07 10:13:29 +0400

    Updated on 2026-09-14

--- spend/datasource/src/main/kotlin/com/tangem/spend/datasource/pay/store/TangemPayStoresModule.kt
+++ spend/datasource/src/main/kotlin/com/tangem/spend/datasource/pay/store/TangemPayStoresModule.kt
@@ -1,15 +1,7 @@
-package com.tangem.datasource.di
+package com.tangem.spend.datasource.pay.store
 
 import com.tangem.core.local.datastore.RuntimeSharedMapStore
 import com.tangem.datasource.local.preferences.AppPreferencesStore
-import com.tangem.datasource.local.visa.DefaultTangemPayCardFrozenStateStore
-import com.tangem.datasource.local.visa.DefaultTangemPayCloseCardStore
-import com.tangem.datasource.local.visa.DefaultTangemPayIssueCardStore
-import com.tangem.datasource.local.visa.DefaultTangemPayReissueCardStore
-import com.tangem.datasource.local.visa.TangemPayCardFrozenStateStore
-import com.tangem.datasource.local.visa.TangemPayCloseCardStore
-import com.tangem.datasource.local.visa.TangemPayIssueCardStore
-import com.tangem.datasource.local.visa.TangemPayReissueCardStore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
