00aa86bb1879de62350fe31f60aaf9594c5ca87b
Tangem <git@tangem.com>
2026-08-07 12:56:36 +0400

    Updated on 2026-09-01

--- features/send/impl/src/main/java/com/tangem/features/send/send/model/SendModel.kt
+++ features/send/impl/src/main/java/com/tangem/features/send/send/model/SendModel.kt
@@ -72,6 +72,7 @@ import com.tangem.utils.coroutines.saveIn
 import com.tangem.utils.logging.TangemLogger
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
+import java.math.BigDecimal
 import javax.inject.Inject
 import kotlin.properties.Delegates
 
