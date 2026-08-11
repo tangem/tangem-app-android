67355264c0750b289f03a427046f054381becdf7
Tangem <git@tangem.com>
2026-08-11 12:40:49 +0400

    Updated on 2026-09-21

--- features/send/impl/src/main/java/com/tangem/features/send/subcomponents/notifications/model/NotificationsModel.kt
+++ features/send/impl/src/main/java/com/tangem/features/send/subcomponents/notifications/model/NotificationsModel.kt
@@ -6,6 +6,7 @@ import com.tangem.blockchain.common.AmountType
 import com.tangem.blockchain.common.transaction.Fee
 import com.tangem.common.routing.AppRoute
 import com.tangem.common.routing.AppRouter
+import com.tangem.common.routing.utils.popAndPush
 import com.tangem.common.ui.R
 import com.tangem.common.ui.notifications.NotificationUM
 import com.tangem.common.ui.notifications.NotificationsFactory.addDustWarningNotification
@@ -195,16 +196,12 @@ internal class NotificationsModel @Inject constructor(
     }
 
     private fun showTokenDetails(currency: CryptoCurrency) {
-        appRouter.pop { isSuccess ->
-            if (isSuccess) {
-                appRouter.push(
-                    AppRoute.CurrencyDetails(
-                        userWalletId = userWalletId,
-                        currency = currency,
-                    ),
-                )
-            }
-        }
+        appRouter.popAndPush(
+            AppRoute.CurrencyDetails(
+                userWalletId = userWalletId,
+                currency = currency,
+            ),
+        )
     }
 
     private suspend fun MutableList<NotificationUM>.addDomainNotifications(
