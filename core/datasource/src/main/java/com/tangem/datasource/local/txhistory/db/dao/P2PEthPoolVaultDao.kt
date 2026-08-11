e8529460ecb069acd65675891801bfe223c02007
Tangem <git@tangem.com>
2026-08-11 12:30:35 +0400

    Updated on 2026-09-25

--- core/datasource/src/main/java/com/tangem/datasource/local/txhistory/db/dao/P2PEthPoolVaultDao.kt
+++ core/datasource/src/main/java/com/tangem/datasource/local/txhistory/db/dao/P2PEthPoolVaultDao.kt
@@ -3,11 +3,16 @@ package com.tangem.datasource.local.txhistory.db.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.tangem.datasource.local.txhistory.db.entity.staking.P2PEthPoolVaultEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface P2PEthPoolVaultDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun upsert(items: List<P2PEthPoolVaultEntity>)
+
+    @Query("SELECT * FROM p2p_eth_pool_vault")
+    fun getAllAsFlow(): Flow<List<P2PEthPoolVaultEntity>>
 }
