906720c95582e02c0f24b12d8906f704755c9399
Tangem <git@tangem.com>
2026-08-11 12:30:35 +0400

    Updated on 2026-09-21

--- core/datasource/src/main/java/com/tangem/datasource/local/txhistory/db/dao/StakingValidatorDao.kt
+++ core/datasource/src/main/java/com/tangem/datasource/local/txhistory/db/dao/StakingValidatorDao.kt
@@ -3,11 +3,16 @@ package com.tangem.datasource.local.txhistory.db.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.tangem.datasource.local.txhistory.db.entity.staking.StakingValidatorEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface StakingValidatorDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun upsert(items: List<StakingValidatorEntity>)
+
+    @Query("SELECT * FROM staking_validator")
+    fun getAllAsFlow(): Flow<List<StakingValidatorEntity>>
 }
