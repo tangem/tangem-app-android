225b3a10d485dcca01c967584eb31c8e5ab64487
Tangem <git@tangem.com>
2026-08-12 14:55:30 +0400

    Updated on 2026-09-25

--- data/txhistory/src/main/kotlin/com/tangem/data/txhistory/list/chain/BsdkOnChainHistory.kt
+++ data/txhistory/src/main/kotlin/com/tangem/data/txhistory/list/chain/BsdkOnChainHistory.kt
@@ -10,7 +10,6 @@ import com.tangem.domain.txhistory.list.HistoryTxListManager.HistoryState
 import com.tangem.domain.txhistory.model.TxHistoryInfo
 import com.tangem.domain.txhistory.model.TxHistoryListBatchingContext
 import com.tangem.domain.txhistory.model.TxHistoryListConfig
-import com.tangem.domain.txhistory.model.identityKey
 import com.tangem.domain.txhistory.models.PaginationWrapper
 import com.tangem.domain.txhistory.repository.TxHistoryRepositoryV2
 import com.tangem.pagination.BatchAction
@@ -77,10 +76,8 @@ internal class BsdkOnChainHistory @AssistedInject constructor(
             currency = currency,
             fromOnChainTimestampMillis = oldestLoadedTimestamp(batchState),
         ).map { express ->
-            val onChain = batchState.data.asSequence()
-                .flatMap { it.data.items.asSequence() }
-                .distinctBy(TxInfo::identityKey)
-                .toList()
+            val onChain = batchState.data
+                .flatMap { it.data.items }
             mergeTxHistoryInfos(onChain = onChain, express = express, currency = currency)
         }
 
