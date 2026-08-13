4acfaeddcdf5a18c300bedf9f0a9e835ef4cdf6c
Tangem <git@tangem.com>
2026-08-13 15:44:52 +0300

    Updated on 2026-09-14

--- features/joint-account/api/build.gradle.kts
+++ features/joint-account/api/build.gradle.kts
@@ -13,14 +13,17 @@ dependencies {
     api(projects.core.decompose)
     api(projects.core.ui)
 
-    /** Common */
-    api(projects.common.ui)
-
     /** Domain */
     api(projects.domain.models)
+
+    /** Common */
+    api(projects.common.ui)
 
     /** Compose */
     api(deps.compose.foundation)
     implementation(deps.compose.runtime)
     implementation(deps.compose.ui)
+
+    /** Other */
+    api(deps.kotlin.immutable.collections)
 }
