8df15332a37985f7ab0bd9e0af060ef71ff47055
Tangem <git@tangem.com>
2026-08-14 20:49:13 +0400

    Updated on 2026-09-23

--- data/express/src/main/java/com/tangem/data/express/di/ExpressDataModule.kt
+++ data/express/src/main/java/com/tangem/data/express/di/ExpressDataModule.kt
@@ -5,8 +5,8 @@ import com.tangem.data.express.DefaultExpressRepository
 import com.tangem.data.express.DefaultExpressServiceFetcher
 import com.tangem.data.express.converter.ExpressErrorConverter
 import com.tangem.data.express.error.DefaultExpressErrorResolver
-import com.tangem.datasource.api.express.TangemExpressApi
-import com.tangem.datasource.api.express.models.response.ExpressErrorResponse
+import com.tangem.grow.datasource.express.TangemExpressApi
+import com.tangem.grow.datasource.express.models.response.ExpressErrorResponse
 import com.tangem.datasource.di.NetworkMoshi
 import com.tangem.datasource.local.preferences.AppPreferencesStore
 import com.tangem.datasource.local.txhistory.db.dao.ExpressHistoryDao
