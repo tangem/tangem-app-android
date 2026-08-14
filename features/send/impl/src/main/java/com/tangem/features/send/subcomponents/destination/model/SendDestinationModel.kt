c035621d7e0d289726c302c83bd7bba28cd1dd28
Tangem <git@tangem.com>
2026-08-14 12:29:02 +0400

    Updated on 2026-09-25

--- features/send/impl/src/main/java/com/tangem/features/send/subcomponents/destination/model/SendDestinationModel.kt
+++ features/send/impl/src/main/java/com/tangem/features/send/subcomponents/destination/model/SendDestinationModel.kt
@@ -373,6 +373,7 @@ internal class SendDestinationModel @Inject constructor(
                                 is AccountStatus.CryptoPortfolio -> accountStatus.getDestinationWalletUM(wallet)
                                 is AccountStatus.Payment -> listOfNotNull(accountStatus.getDestinationWalletUM(wallet))
                                 is AccountStatus.Virtual -> emptyList()
+                                is AccountStatus.Prediction -> emptyList()
                             }
                         }
                     }
