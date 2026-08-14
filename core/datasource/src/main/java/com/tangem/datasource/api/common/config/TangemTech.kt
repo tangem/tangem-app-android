025e3e2680765c32e3e696e09b93ea940c478451
Tangem <git@tangem.com>
2026-08-14 12:58:09 +0400

    Updated on 2026-09-25

--- core/datasource/src/main/java/com/tangem/datasource/api/common/config/TangemTech.kt
+++ core/datasource/src/main/java/com/tangem/datasource/api/common/config/TangemTech.kt
@@ -3,10 +3,12 @@ package com.tangem.datasource.api.common.config
 import com.tangem.core.remote.config.ApiConfig
 import com.tangem.core.remote.config.ApiEnvironment
 import com.tangem.core.remote.config.ApiEnvironmentConfig
+import com.tangem.core.remote.header.RequestHeader
 
 import com.tangem.datasource.BuildConfig
 import com.tangem.datasource.api.common.AuthProvider
-import com.tangem.datasource.utils.RequestHeader
+import com.tangem.datasource.utils.AuthenticationHeader
+import com.tangem.datasource.utils.TangemApiKeyHeader
 import com.tangem.utils.Provider
 import com.tangem.utils.info.AppInfoProvider
 
@@ -65,9 +67,9 @@ class TangemTech(
     )
 
     private fun createHeaders(apiEnvironment: ApiEnvironment) = buildMap {
-        putAll(from = RequestHeader.TangemApiKeyHeader(authProvider, Provider { apiEnvironment }).values)
+        putAll(from = TangemApiKeyHeader(authProvider, Provider { apiEnvironment }).values)
         putAll(from = RequestHeader.AppVersionPlatformHeaders(appInfoProvider).values)
-        putAll(from = RequestHeader.AuthenticationHeader(authProvider).values)
+        putAll(from = AuthenticationHeader(authProvider).values)
     }
 
     companion object {
