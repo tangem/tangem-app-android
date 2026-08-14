70d089a3db84d07dfc971b927b6563b106d7cd13
Tangem <git@tangem.com>
2026-08-14 22:02:10 +0200

    Updated on 2026-09-23

--- app/src/main/java/com/tangem/tap/di/TangemSdkManagerModule.kt
+++ app/src/main/java/com/tangem/tap/di/TangemSdkManagerModule.kt
@@ -10,7 +10,6 @@ import com.tangem.features.onboarding.v2.OnboardingV2FeatureToggles
 import com.tangem.sdk.api.TangemSdkManager
 import com.tangem.tap.domain.sdk.impl.DefaultTangemSdkManager
 import com.tangem.tap.domain.sdk.impl.MockTangemSdkManager
-import com.tangem.tap.domain.tasks.jointaccount.JointAccountCreationSignTask
 import com.tangem.tap.domain.tasks.visa.TangemPayGenerateAddressAndSignChallengeTask
 import com.tangem.tap.domain.tasks.visa.TangemPayGenerateVirtualAccountAddressTask
 import com.tangem.tap.domain.tasks.visa.VisaCardActivationTask
@@ -37,7 +36,6 @@ internal class TangemSdkManagerModule {
         visaCardActivationTaskFactory: VisaCardActivationTask.Factory,
         tangemPayChallengeTaskFactory: TangemPayGenerateAddressAndSignChallengeTask.Factory,
         tangemPayVirtualAccountTaskFactory: TangemPayGenerateVirtualAccountAddressTask.Factory,
-        jointAccountCreationSignTaskFactory: JointAccountCreationSignTask.Factory,
         onboardingV2FeatureToggles: OnboardingV2FeatureToggles,
         analyticsErrorHandler: AnalyticsErrorHandler,
         cardRepository: CardRepository,
@@ -54,7 +52,6 @@ internal class TangemSdkManagerModule {
                 visaCardActivationTaskFactory = visaCardActivationTaskFactory,
                 tangemPayChallengeTaskFactory = tangemPayChallengeTaskFactory,
                 tangemPayVirtualAccountTaskFactory = tangemPayVirtualAccountTaskFactory,
-                jointAccountCreationSignTaskFactory = jointAccountCreationSignTaskFactory,
                 onboardingV2FeatureToggles = onboardingV2FeatureToggles,
                 analyticsErrorHandler = analyticsErrorHandler,
                 cardRepository = cardRepository,
