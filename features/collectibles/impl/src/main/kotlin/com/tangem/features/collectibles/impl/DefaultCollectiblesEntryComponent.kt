65fd45d557aea512c6b4e241962dca0b552a7698
Tangem <git@tangem.com>
2026-08-14 17:06:41 +0200

    Updated on 2026-09-23

--- features/collectibles/impl/src/main/kotlin/com/tangem/features/collectibles/impl/DefaultCollectiblesEntryComponent.kt
+++ features/collectibles/impl/src/main/kotlin/com/tangem/features/collectibles/impl/DefaultCollectiblesEntryComponent.kt
@@ -1,13 +1,12 @@
 package com.tangem.features.collectibles.impl
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.arkivanov.decompose.childContext
 import com.tangem.core.decompose.context.AppComponentContext
-import com.tangem.core.ui.res.TangemTheme
+import com.tangem.core.decompose.context.childByContext
 import com.tangem.features.collectibles.api.CollectiblesEntryComponent
+import com.tangem.features.collectibles.impl.onboarding.CollectiblesOnboardingComponent
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
@@ -15,19 +14,24 @@ import dagger.assisted.AssistedInject
 internal class DefaultCollectiblesEntryComponent @AssistedInject constructor(
     @Assisted context: AppComponentContext,
     @Assisted @Suppress("UnusedPrivateProperty") private val params: Unit,
+    onboardingComponentFactory: CollectiblesOnboardingComponent.Factory,
 ) : CollectiblesEntryComponent, AppComponentContext by context {
+
+    private val onboardingComponent = onboardingComponentFactory.create(
+        context = childByContext(childContext(key = ONBOARDING_CHILD_KEY)),
+    )
 
     @Composable
     override fun Content(modifier: Modifier) {
-        Box(
-            modifier = modifier
-                .fillMaxSize()
-                .background(TangemTheme.colors3.bg.primary),
-        )
+        onboardingComponent.Content(modifier = modifier)
     }
 
     @AssistedFactory
     interface Factory : CollectiblesEntryComponent.Factory {
         override fun create(context: AppComponentContext, params: Unit): DefaultCollectiblesEntryComponent
+    }
+
+    private companion object {
+        const val ONBOARDING_CHILD_KEY = "onboarding"
     }
 }
