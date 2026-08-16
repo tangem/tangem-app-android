bfed60fa984ff709e1ebe53b6f6bfaba1ed12426
Tangem <git@tangem.com>
2026-08-16 19:52:55 +0300

    Updated on 2026-09-21

--- features/collectibles/impl/build.gradle.kts
+++ features/collectibles/impl/build.gradle.kts
@@ -18,11 +18,17 @@ dependencies {
     implementation(projects.core.configToggles)
     implementation(projects.core.decompose)
     implementation(projects.core.ui)
+    implementation(projects.core.utils)
 
     /** Compose */
     implementation(deps.compose.foundation)
     implementation(deps.compose.material3)
     implementation(deps.compose.ui)
+
+    /** Other */
+    implementation(deps.androidx.appCompat)
+    implementation(deps.decompose)
+    implementation(deps.kotlin.coroutines)
 
     /** DI */
     implementation(deps.hilt.android)
