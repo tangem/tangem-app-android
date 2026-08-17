7f7347ca70e03e93863fca429fc7b343909c9094
Tangem <git@tangem.com>
2026-08-17 17:58:30 +0400

    Updated on 2026-09-14

--- domain/account/status/build.gradle.kts
+++ domain/account/status/build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     implementation(projects.core.utils)
     implementation(projects.core.analytics.models)
     implementation(projects.domain.express.models)
+    implementation(projects.domain.polymarket)
+    api(projects.features.polymarket.api) // POLYMARKET_ENABLED
 
     implementation(deps.kotlin.serialization)
 
