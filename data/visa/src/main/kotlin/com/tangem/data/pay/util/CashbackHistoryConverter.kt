983c3da1ec09c047b71582b7f77076bea41deb74
Tangem <git@tangem.com>
2026-08-17 15:05:18 +0200

    Updated on 2026-09-25

--- data/visa/src/main/kotlin/com/tangem/data/pay/util/CashbackHistoryConverter.kt
+++ data/visa/src/main/kotlin/com/tangem/data/pay/util/CashbackHistoryConverter.kt
@@ -5,13 +5,12 @@ import com.tangem.domain.pay.model.CashbackHistory
 import com.tangem.utils.converter.Converter
 import java.math.BigDecimal
 
-/** Maps [CashbackHistoryResponse] (BFF) to the domain [CashbackHistory]. */
 internal object CashbackHistoryConverter : Converter<CashbackHistoryResponse, CashbackHistory> {
 
     override fun convert(value: CashbackHistoryResponse): CashbackHistory {
         return CashbackHistory(
-            currency = value.currency.orEmpty(),
-            months = value.items.orEmpty().map { item ->
+            currency = value.result?.currency.orEmpty(),
+            months = value.result?.items.orEmpty().map { item ->
                 CashbackHistory.MonthlyCashback(
                     year = item.year,
                     month = item.month,
