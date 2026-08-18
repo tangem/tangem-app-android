060422f31d78327787a4c73db2b1b054b9749808
Tangem <git@tangem.com>
2026-08-18 14:50:36 +0200

    Updated on 2026-09-21

--- features/collectibles/impl/src/main/kotlin/com/tangem/features/collectibles/impl/stories/ui/state/CollectiblesStorySlideUM.kt
+++ features/collectibles/impl/src/main/kotlin/com/tangem/features/collectibles/impl/stories/ui/state/CollectiblesStorySlideUM.kt
@@ -1,6 +1,5 @@
 package com.tangem.features.collectibles.impl.stories.ui.state
 
-import androidx.compose.runtime.Immutable
 import com.tangem.core.ui.extensions.TextReference
 
 /**
@@ -10,7 +9,6 @@ import com.tangem.core.ui.extensions.TextReference
  * @property subtitle supporting text under the headline.
  * @property continueButtonText label of the main button while this slide is shown.
  */
-@Immutable
 internal data class CollectiblesStorySlideUM(
     val title: TextReference,
     val subtitle: TextReference,
