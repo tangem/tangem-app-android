d41e2dd5e09846fae63e5e907dfe2cf883b9653c
Tangem <git@tangem.com>
2026-08-18 23:15:04 +0400

    Updated on 2026-09-21

--- data/staking/src/main/java/com/tangem/datasource/local/token/converter/PendingActionConverter.kt
+++ data/staking/src/main/java/com/tangem/datasource/local/token/converter/PendingActionConverter.kt
@@ -7,36 +7,35 @@ import com.tangem.utils.converter.Converter
 internal object PendingActionConverter : Converter<BalanceDTO.PendingAction, PendingAction> {
 
     override fun convert(value: BalanceDTO.PendingAction): PendingAction {
+        val args = value.args
         return PendingAction(
             type = StakingActionTypeConverter.convert(value.type),
             passthrough = value.passthrough,
-            args = with(value.args) {
-                PendingAction.PendingActionArgs(
-                    amount = this?.amount?.let { amount ->
-                        PendingAction.PendingActionArgs.Amount(
-                            required = amount.required,
-                            minimum = amount.minimum,
-                            maximum = amount.maximum,
-                        )
-                    },
-                    duration = this?.duration?.let { duration ->
-                        PendingAction.PendingActionArgs.Duration(
-                            required = duration.required,
-                            minimum = duration.minimum,
-                            maximum = duration.maximum,
-                        )
-                    },
-                    validatorAddress = this?.validatorAddress?.required,
-                    validatorAddresses = this?.validatorAddresses?.required,
-                    tronResource = this?.tronResource?.let { tronResource ->
-                        PendingAction.PendingActionArgs.TronResource(
-                            required = tronResource.required,
-                            options = tronResource.options,
-                        )
-                    },
-                    signatureVerification = this?.signatureVerification?.required,
-                )
-            },
+            args = PendingAction.PendingActionArgs(
+                amount = args?.amount?.let { amount ->
+                    PendingAction.PendingActionArgs.Amount(
+                        required = amount.required,
+                        minimum = amount.minimum,
+                        maximum = amount.maximum,
+                    )
+                },
+                duration = args?.duration?.let { duration ->
+                    PendingAction.PendingActionArgs.Duration(
+                        required = duration.required,
+                        minimum = duration.minimum,
+                        maximum = duration.maximum,
+                    )
+                },
+                validatorAddress = args?.validatorAddress?.required,
+                validatorAddresses = args?.validatorAddresses?.required,
+                tronResource = args?.tronResource?.let { tronResource ->
+                    PendingAction.PendingActionArgs.TronResource(
+                        required = tronResource.required,
+                        options = tronResource.options,
+                    )
+                },
+                signatureVerification = args?.signatureVerification?.required,
+            ),
         )
     }
 }
