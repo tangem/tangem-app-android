e09dab864d031c9dd2ba439ed1ac84d617059593
Tangem <git@tangem.com>
2026-08-19 18:18:44 +0300

    Updated on 2026-09-14

--- features/wallet/impl/build.gradle.kts
+++ features/wallet/impl/build.gradle.kts
@@ -157,6 +157,5 @@ dependencies {
     testImplementation(deps.test.mockk)
     testImplementation(deps.kotlin.coroutines)
     testImplementation(projects.common.test)
-    testImplementation(projects.features.polymarket.api)
     testImplementation(projects.test.core)
 }
