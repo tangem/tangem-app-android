dbafbcaed28b202f06613ca0743d2c55e6ace02c
Tangem <git@tangem.com>
2026-08-20 21:44:21 +0200

    Updated on 2026-09-25

---
+++ core/ui/src/main/java/com/tangem/core/ui/res/generated/icons/IcUsers16.kt
@@ -0,0 +1,62 @@
+@file:Suppress("all")
+
+package com.tangem.core.ui.res.generated.icons
+
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.addPathNodes
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+/**
+ * Auto-generated from design tokens. Do not edit manually.
+ */
+
+private var _ic_users_16: ImageVector? = null
+
+val Icons.ic_users_16: ImageVector
+    get() {
+        if (_ic_users_16 != null) return _ic_users_16!!
+        _ic_users_16 = ImageVector.Builder(
+            name = "ic_users_16",
+            defaultWidth = 16.dp,
+            defaultHeight = 16.dp,
+            viewportWidth = 16f,
+            viewportHeight = 16f,
+        ).apply {
+            addPath(
+                fill = SolidColor(Color.Black),
+                pathFillType = PathFillType.NonZero,
+                pathData = addPathNodes("M7.11718 8.60156C8.39552 8.60156 9.47742 9.6012 9.47753 10.8896V11.3144C9.47727 11.6594 9.19755 11.9394 8.85253 11.9394C8.50758 11.9394 8.2278 11.6594 8.22753 11.3144V10.8896C8.22742 10.3408 7.75551 9.85156 7.11718 9.85156H5.36035C4.72202 9.85156 4.25011 10.3408 4.25 10.8896V11.3144C4.24974 11.6594 3.97001 11.9394 3.625 11.9394C3.27998 11.9394 3.00026 11.6594 3 11.3144V10.8896C3.00011 9.6012 4.08201 8.60156 5.36035 8.60156H7.11718Z"),
+            )
+            addPath(
+                fill = SolidColor(Color.Black),
+                pathFillType = PathFillType.NonZero,
+                pathData = addPathNodes("M10.6406 8.60156C11.9186 8.60202 12.9999 9.6015 13 10.8896V11.3144C12.9997 11.6592 12.7198 11.9392 12.375 11.9394C12.0302 11.9392 11.7503 11.6593 11.75 11.3144V10.8896C11.7499 10.3411 11.2785 9.85201 10.6406 9.85156H10.1602C9.81514 9.85156 9.53541 9.57151 9.53515 9.22656C9.53515 8.88138 9.81497 8.60156 10.1602 8.60156H10.6406Z"),
+            )
+            addPath(
+                fill = SolidColor(Color.Black),
+                pathFillType = PathFillType.NonZero,
+                pathData = addPathNodes("M6.21875 4.06055C7.39213 4.06081 8.34375 5.01308 8.34375 6.18652C8.34348 7.35974 7.39196 8.31126 6.21875 8.31152C5.04531 8.31152 4.09401 7.3599 4.09375 6.18652C4.09375 5.01292 5.04514 4.06055 6.21875 4.06055ZM6.21875 5.31152C5.7355 5.31152 5.34375 5.70327 5.34375 6.18652C5.34401 6.66955 5.73566 7.06152 6.21875 7.06152C6.70161 7.06126 7.09348 6.66938 7.09375 6.18652C7.09375 5.70344 6.70177 5.31179 6.21875 5.31152Z"),
+            )
+            addPath(
+                fill = SolidColor(Color.Black),
+                pathFillType = PathFillType.NonZero,
+                pathData = addPathNodes("M10.0713 5.05859C10.9685 5.05886 11.6963 5.78617 11.6963 6.68359C11.696 7.58079 10.9684 8.30833 10.0713 8.30859C9.17409 8.30848 8.44655 7.58088 8.44628 6.68359C8.44628 5.78608 9.17393 5.05871 10.0713 5.05859ZM10.0713 6.30859C9.86403 6.30871 9.69628 6.47668 9.69628 6.68359C9.69655 6.89028 9.8642 7.05848 10.0713 7.05859C10.2782 7.05833 10.446 6.89019 10.4463 6.68359C10.4463 6.47677 10.2784 6.30886 10.0713 6.30859Z"),
+            )
+        }.build()
+        return _ic_users_16!!
+    }
+
+@Composable
+@Preview(showBackground = true)
+private fun IcUsers16Preview() {
+    Icon(
+        imageVector = Icons.ic_users_16,
+        contentDescription = null,
+    )
+}
