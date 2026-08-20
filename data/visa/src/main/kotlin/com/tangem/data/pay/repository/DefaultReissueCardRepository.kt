ec3c9cbf1e38e380da7a9bf8c07cec545b824b9e
Tangem <git@tangem.com>
2026-08-20 12:54:01 +0300

    Updated on 2026-09-14

--- data/visa/src/main/kotlin/com/tangem/data/pay/repository/DefaultReissueCardRepository.kt
+++ data/visa/src/main/kotlin/com/tangem/data/pay/repository/DefaultReissueCardRepository.kt
@@ -48,6 +48,22 @@ internal class DefaultReissueCardRepository @Inject constructor(
             fee
         }
 
+    override suspend fun getPlasticReissueCardFee(
+        userWalletId: UserWalletId,
+    ): Either<VisaApiError, TangemPayReissueCardFee> = either {
+        val response = requestHelper.performRequest(userWalletId) { authHeader ->
+            tangemPayApi.getFee(
+                authHeader = authHeader,
+                type = CARD_REPLACEMENT_PLASTIC_FEE_TYPE,
+            )
+        }.bind()
+
+        TangemPayReissueCardFee(
+            amount = response.result.amount.toBigDecimal(),
+            currencyCode = response.result.currency,
+        )
+    }
+
     override suspend fun reissueCard(
         userWalletId: UserWalletId,
         cardId: String,
@@ -88,5 +104,6 @@ internal class DefaultReissueCardRepository @Inject constructor(
 
     private companion object {
         const val CARD_REPLACEMENT_FEE_TYPE = "CARD_REPLACEMENT"
+        const val CARD_REPLACEMENT_PLASTIC_FEE_TYPE = "CARD_REPLACEMENT_PLASTIC"
     }
 }
