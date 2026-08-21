7078f0c0381799960feb7c3de3be819af9304d04
Tangem <git@tangem.com>
2026-08-21 21:07:32 +0300

    Updated on 2026-09-25

---
+++ features/introduction/api/src/main/kotlin/com/tangem/features/introduction/IntroductionComponent.kt
@@ -0,0 +1,16 @@
+package com.tangem.features.introduction
+
+import com.tangem.common.routing.entity.InitScreenLaunchMode
+import com.tangem.core.decompose.factory.ComponentFactory
+import com.tangem.core.ui.decompose.ComposableContentComponent
+
+interface IntroductionComponent : ComposableContentComponent {
+
+    /**
+     * @param launchMode how the app was started. [InitScreenLaunchMode.WithCardScan] means an NFC tap
+     * brought the user here and the card scan is expected to start on its own.
+     */
+    data class Params(val launchMode: InitScreenLaunchMode)
+
+    interface Factory : ComponentFactory<Params, IntroductionComponent>
+}
