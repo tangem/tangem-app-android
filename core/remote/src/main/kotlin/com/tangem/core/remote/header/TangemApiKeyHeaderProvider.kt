828a264f669aff8719de5a63619c7550ce9270ed
Tangem <git@tangem.com>
2026-08-24 18:50:22 +0400

    Updated on 2026-09-21

---
+++ core/remote/src/main/kotlin/com/tangem/core/remote/header/TangemApiKeyHeaderProvider.kt
@@ -0,0 +1,16 @@
+package com.tangem.core.remote.header
+
+import com.tangem.core.remote.config.ApiEnvironment
+import com.tangem.utils.Provider
+
+/**
+ * Supplies the auth-backed `api-key` [RequestHeader] for a given API environment.
+ *
+ * The implementation lives in the module that owns the api-key source, so [ApiConfig][com.tangem.core.remote.config.ApiConfig]s
+ * declared in other modules can attach the header by injecting this contract instead of depending on
+ * that source directly.
+ */
+fun interface TangemApiKeyHeaderProvider {
+
+    fun forEnvironment(environment: Provider<ApiEnvironment>): RequestHeader
+}
