cf9875a6f5f2416bf247fea2919fb59e80ec3680
Tangem <git@tangem.com>
2026-08-29 13:45:14 +0500

    Updated on 2026-09-25

--- features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/cashback/impl/model/TangemPayCashbackDateFormatter.kt
+++ features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/cashback/impl/model/TangemPayCashbackDateFormatter.kt
@@ -2,6 +2,8 @@ package com.tangem.features.tangempay.cashback.impl.model
 
 import com.tangem.core.ui.utils.DateTimeFormatters
 import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+import org.joda.time.format.DateTimeFormatter
 
 internal class TangemPayCashbackDateFormatter {
 
@@ -13,7 +15,7 @@ internal class TangemPayCashbackDateFormatter {
 
     fun formatMonthDay(date: DateTime): String = DateTimeFormatters.formatDate(date, DateTimeFormatters.dateDMMM)
 
-    fun formatNumericDate(date: DateTime): String = DateTimeFormatters.formatDate(date, DateTimeFormatters.dateDDMMYYYY)
+    fun formatNumericDate(date: DateTime): String = NUMERIC_DATE_FORMATTER.print(date)
 
     fun formatWindow(start: DateTime?, end: DateTime?): String? {
         if (start == null || end == null) return null
@@ -22,5 +24,9 @@ internal class TangemPayCashbackDateFormatter {
 
     private companion object {
         const val MONTH_DAY_SKELETON = "MMMMd"
+
+        // ICU's best-pattern lookup localizes the numeric skeleton to "MM/dd/yyyy" in some locales,
+        // while this date must always be dot-separated ([REDACTED_TASK_KEY])
+        val NUMERIC_DATE_FORMATTER: DateTimeFormatter = DateTimeFormat.forPattern("dd.MM.yyyy")
     }
 }
