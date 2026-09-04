58222dee2a996da2afcd46c3ce2f34e920df3ee2
Tangem <git@tangem.com>
2026-09-04 15:13:15 +0500

    Updated on 2026-09-23

--- features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/account/TangemPayDetailsUM.kt
+++ features/tangempay/details/impl/src/main/kotlin/com/tangem/features/tangempay/account/TangemPayDetailsUM.kt
@@ -30,7 +30,6 @@ internal sealed interface CashbackBlockUM {
 
     data class Widget(
         val title: TextReference,
-        val subtitle: TextReference?,
         val isNegative: Boolean,
         val onClick: () -> Unit,
     ) : CashbackBlockUM
